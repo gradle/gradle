9f47e215784c48c0924a79a2521d2d437fb55f93
Hans Dockter <mail@dockter.biz>
2008-03-27 06:54:04 +0000

    Fixed bug with svn release
    git-svn-id: http://svn.codehaus.org/gradle/gradle-core/trunk@195 004c2c75-fc45-0410-b1a2-da8352e2331b

--- buildSrc/src/main/groovy/org/gradle/build/release/Svn.groovy
+++ buildSrc/src/main/groovy/org/gradle/build/release/Svn.groovy
@@ -86,9 +86,9 @@ class Svn {
 
     def exitIfReleaseBranchDirectoryExists() {
         try {
-            clientManager.WCClient.doInfo(new SVNURL('https://svn.codehaus.org/gradle/gradle-core/branches/', true),
+            clientManager.WCClient.doInfo(new SVNURL(releaseBranchUrl, true),
                     SVNRevision.UNDEFINED, SVNRevision.HEAD)
-            throw new GradleException("Release branch directpry already exists. You can't release from trunk therefore")
+            throw new GradleException("Release branch directory $releaseBranchUrl already exists. You can't release from trunk therefore.")
         } catch (SVNException ignore) {
             // SVNException means directory does not exists. Which is what we want.
         }
