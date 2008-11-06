6542ad1fbeb2c0b030ac604012b7f1f13542fc61
Adam Murdoch <a@rubygrapefruit.net>
2008-11-06 20:23:20 +0000

    Added DependencyManagerInternal.getIvy()
    git-svn-id: http://svn.codehaus.org/gradle/gradle-core/trunk@1014 004c2c75-fc45-0410-b1a2-da8352e2331b

--- src/main/groovy/org/gradle/api/internal/dependencies/DependencyManagerInternal.java
+++ src/main/groovy/org/gradle/api/internal/dependencies/DependencyManagerInternal.java
@@ -1,6 +1,8 @@
 package org.gradle.api.internal.dependencies;
 
 import org.gradle.api.DependencyManager;
+import org.apache.ivy.Ivy;
 
 public interface DependencyManagerInternal extends DependencyManager {
+    Ivy getIvy();
 }
