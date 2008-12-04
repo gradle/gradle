4695eb335d2f92d2ce2333904ed9a1cf52cea62c
Hans Dockter <mail@dockter.biz>
2008-12-04 13:23:52 +0000

    Added Javadoc
    git-svn-id: http://svn.codehaus.org/gradle/gradle-core/trunk@1082 004c2c75-fc45-0410-b1a2-da8352e2331b

--- src/main/groovy/org/gradle/api/plugins/osgi/OsgiManifest.java
+++ src/main/groovy/org/gradle/api/plugins/osgi/OsgiManifest.java
@@ -17,10 +17,10 @@ package org.gradle.api.plugins.osgi;
 
 import org.gradle.api.tasks.bundling.GradleManifest;
 
-import java.util.jar.Manifest;
+import java.io.File;
 import java.util.List;
 import java.util.Map;
-import java.io.File;
+import java.util.jar.Manifest;
 
 /**
  * @author Hans Dockter
@@ -28,50 +28,182 @@ import java.io.File;
 public interface OsgiManifest {
     Manifest generateManifest();
 
+    /**
+     * Returns the list of arguments for a particular instruction.
+     *
+     * @param instructionName
+     * @return The list of arguments
+     * @see #instruction(String, String[])
+     */
     List<String> instructionValue(String instructionName);
 
+    /**
+     * Adds arguments to an instruction. If the instruction does not exists, it is created. If it does exists, the
+     * arguments are appended to the existing arguments.
+     * 
+     * @param name
+     * @param values
+     * @return this
+     * @see #instructionFirst(String, String[])
+     */
     OsgiManifest instruction(String name, String... values);
 
+    /**
+     * Adds arguments to an instruction. If the instruction does not exists, it is created. If it does exists, the
+     * arguments are inserted before the existing arguments.
+     *
+     * @param name
+     * @param values
+     * @return this
+     * @see #instructionFirst(String, String[])
+     */
     OsgiManifest instructionFirst(String name, String... values);
 
+    /**
+     * Returns all exisiting instruction.
+     *
+     * @return A map with instructions. The key of the map is the instruction name, the value a list of arguments.
+     */
     Map<String, List<String>> getInstructions();
 
+    /**
+     * Returns the symbolic name.
+     *
+     * @see #setSymbolicName(String) 
+     */
     String getSymbolicName();
 
+    /**
+     * A convenient method for setting a Bundle-SymbolicName instruction.
+     *
+     * @param symbolicName the symbolicName to set
+     */
     void setSymbolicName(String symbolicName);
 
+    /**
+     * Returns the name
+     *
+     * @see #setName(String)
+     */
     String getName();
 
+    /**
+     * A convenient method for setting a Bundle-Name instruction.
+     *
+     * @param name the name to set
+     */
     void setName(String name);
 
+    /**
+     * Returns the version
+     *
+     * @see #setVersion(String) 
+     */
     String getVersion();
 
+    /**
+     * A convenient method for setting a Bundle-Version instruction.
+     *
+     * @param version the version to set
+     */
     void setVersion(String version);
 
+    /**
+     * Returns the description.
+     *
+     * @see #setDescription(String) 
+     */
     String getDescription();
 
+    /**
+     * A convenient method for setting a Bundle-Description instruction.
+     *
+     * @param description the description to set
+     */
     void setDescription(String description);
 
+    /**
+     * Returns the license
+     * @see #setLicense(String) 
+     */
     String getLicense();
 
+    /**
+     * A convenient method for setting a Bundle-License instruction.
+     *
+     * @param license The license to set
+     */
     void setLicense(String license);
 
+    /**
+     * Returns the vendor.
+     *
+     * @see #setVendor(String) 
+     */
     String getVendor();
 
+    /**
+     * A convenient method for setting a Bundle-Vendor instruction.
+     *
+     * @param vendor The vendor to set
+     */
     void setVendor(String vendor);
 
+    /**
+     * Returns the docURL value.
+     *
+     * @see #setDocURL(String)
+     */
     String getDocURL();
 
+    /**
+     * A convenient method for setting a Bundle-DocURL instruction.
+     *
+     * @param docURL the docURL to set.
+     */
     void setDocURL(String docURL);
 
+    /**
+     * Returns the classes dir.
+     *
+     * @see #setClassesDir(java.io.File) 
+     */
     File getClassesDir();
 
+    /**
+     * Sets the classes dir. This directory is the major source of input for generation the OSGi manifest. All classes
+     * are analyzed for its packages and package dependencies. Based on this the Import-Package value is set.
+     * This auto generated value can be overwritten by explicitly setting an instruction.
+     *
+     * @param classesDir
+     * 
+     * @see #instruction(String, String[])
+     */
     void setClassesDir(File classesDir);
 
+    /**
+     * Generated and writes the OSGi manifest information into a GradleManifest. Existing information in the GradleManifest
+     * is removed.
+     *
+     * @param manifest The GradleManifest to write into.
+     * @return this
+     */
     OsgiManifest overwrite(GradleManifest manifest);
 
+    /**
+     * Returns the classpath.
+     *
+     * @see #setClasspath(java.util.List) 
+     */
     List<File> getClasspath();
 
+    /**
+     * A convenient method for setting a Bundle-Classpath instruction. The information of the classpath elements are only used if they are OSGi bundles. In this
+     * case for example the version information provided by the bundle is used in the Import-Package of the generated
+     * OSGi bundle.
+     *
+     * @param classpath The classpath elements
+     */
     void setClasspath(List<File> classpath);
 
     /**
