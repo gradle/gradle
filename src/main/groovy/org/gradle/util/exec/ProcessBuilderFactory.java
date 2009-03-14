23a357e679fb992e29d4c98f72965c599440c784
Tom Eyckmans <teyckmans@gmail.com>
2009-03-14 14:51:44 +0000

    added logging to ProcessBuilderFactory to diagnose the windows problem
    git-svn-id: http://svn.codehaus.org/gradle/gradle-core/trunk@1268 004c2c75-fc45-0410-b1a2-da8352e2331b

--- src/main/groovy/org/gradle/util/exec/ProcessBuilderFactory.java
+++ src/main/groovy/org/gradle/util/exec/ProcessBuilderFactory.java
@@ -1,5 +1,8 @@
 package org.gradle.util.exec;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
@@ -10,12 +13,26 @@ import java.util.ArrayList;
  * @author Tom Eyckmans
  */
 public class ProcessBuilderFactory {
+    private static final Logger logger = LoggerFactory.getLogger(ProcessBuilderFactory.class);
+
     public ProcessBuilder createProcessBuilder(ExecHandle execHandle) {
-        final List<String> command = new ArrayList<String>();
-        command.add(execHandle.getCommand());
-        command.addAll(execHandle.getArguments());
+        final List<String> commandWithArguments = new ArrayList<String>();
+        final String command = execHandle.getCommand();
+        if (logger.isDebugEnabled()) {
+            logger.debug("creating process builder for command {}", command);
+        }
+        commandWithArguments.add(command);
+        final List<String> arguments = execHandle.getArguments();
+        if (logger.isDebugEnabled()) {
+            int argumentIndex = 0;
+            for ( String argument : arguments ) {
+                logger.debug("with argument#{} = {}", argumentIndex, argument);
+                argumentIndex++;
+            }
+        }
+        commandWithArguments.addAll(arguments);
         
-        final ProcessBuilder processBuilder = new ProcessBuilder(command);
+        final ProcessBuilder processBuilder = new ProcessBuilder(commandWithArguments);
 
         processBuilder.directory(execHandle.getDirectory());
         final Map<String, String> environment = processBuilder.environment();
