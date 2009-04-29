1a757ef029eb7ba492f2e5fd2e78b09c203a4633
Adam Murdoch <a@rubygrapefruit.net>
2009-04-29 03:58:38 +0000

    GRADLE-419: Fixed task DSL to work in script methods
    git-svn-id: http://svn.codehaus.org/gradle/gradle-core/trunk@1405 004c2c75-fc45-0410-b1a2-da8352e2331b

--- src/main/groovy/org/gradle/api/internal/artifacts/dsl/BuildScriptTransformer.java
+++ src/main/groovy/org/gradle/api/internal/artifacts/dsl/BuildScriptTransformer.java
@@ -19,6 +19,8 @@ import org.codehaus.groovy.control.CompilationUnit;
 import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.codehaus.groovy.ast.CodeVisitorSupport;
+import org.codehaus.groovy.ast.MethodNode;
+import org.codehaus.groovy.ast.GroovyCodeVisitor;
 import org.codehaus.groovy.ast.expr.*;
 
 import java.util.List;
@@ -26,68 +28,12 @@ import java.util.Collections;
 
 public class BuildScriptTransformer extends CompilationUnit.SourceUnitOperation {
     public void call(SourceUnit source) throws CompilationFailedException {
-        source.getAST().getStatementBlock().visit(new CodeVisitorSupport() {
-            @Override
-            public void visitMethodCallExpression(MethodCallExpression call) {
-                if (!isInstanceMethod(call, "task")) {
-                    super.visitMethodCallExpression(call);
-                    return;
-                }
-
-                ArgumentListExpression args = (ArgumentListExpression) call.getArguments();
-                if (args.getExpressions().size() != 1) {
-                    super.visitMethodCallExpression(call);
-                    return;
-                }
-
-                if (args.getExpression(0) instanceof VariableExpression) {
-                    String taskName = args.getExpression(0).getText();
-                    call.setMethod(new ConstantExpression("createTask"));
-                    args.getExpressions().clear();
-                    args.addExpression(new ConstantExpression(taskName));
-                    return;
-                }
-
-                if (!(args.getExpression(0) instanceof MethodCallExpression)) {
-                    super.visitMethodCallExpression(call);
-                    return;
-                }
-
-                MethodCallExpression nestedMethod = (MethodCallExpression) args.getExpressions().get(0);
-                if (!isInstanceMethod(nestedMethod)) {
-                    super.visitMethodCallExpression(call);
-                    return;
-                }
-
-                Expression taskName = nestedMethod.getMethod();
-                Expression mapArg = null;
-                List<Expression> extraArgs = Collections.emptyList();
-
-                if (nestedMethod.getArguments() instanceof NamedArgumentListExpression) {
-                    mapArg = nestedMethod.getArguments();
-                }
-                else if (nestedMethod.getArguments() instanceof ArgumentListExpression) {
-                    ArgumentListExpression nestedArgs = (ArgumentListExpression) nestedMethod.getArguments();
-                    if (nestedArgs.getExpression(0) instanceof MapExpression) {
-                        mapArg = nestedArgs.getExpression(0);
-                        extraArgs = nestedArgs.getExpressions().subList(1, nestedArgs.getExpressions().size());
-                    }
-                    else {
-                        extraArgs = nestedArgs.getExpressions();
-                    }
-                }
-
-                call.setMethod(new ConstantExpression("createTask"));
-                args.getExpressions().clear();
-                if (mapArg != null) {
-                    args.addExpression(mapArg);
-                }
-                args.addExpression(taskName);
-                for (Expression extraArg : extraArgs) {
-                    args.addExpression(extraArg);
-                }
-            }
-        });
+        GroovyCodeVisitor transformer = new TaskDefinitionTransformer();
+        source.getAST().getStatementBlock().visit(transformer);
+        for (Object method : source.getAST().getMethods()) {
+            MethodNode methodNode = (MethodNode) method;
+            methodNode.getCode().visit(transformer);
+        }
     }
 
     private boolean isInstanceMethod(MethodCallExpression call, String name) {
@@ -113,5 +59,68 @@ public class BuildScriptTransformer extends CompilationUnit.SourceUnitOperation 
 
         return call.getObjectExpression() instanceof VariableExpression && call.getObjectExpression().getText().equals(
                 "this");
+    }
+
+    private class TaskDefinitionTransformer extends CodeVisitorSupport {
+        @Override
+            public void visitMethodCallExpression(MethodCallExpression call) {
+            if (!isInstanceMethod(call, "task")) {
+                super.visitMethodCallExpression(call);
+                return;
+            }
+
+            ArgumentListExpression args = (ArgumentListExpression) call.getArguments();
+            if (args.getExpressions().size() != 1) {
+                super.visitMethodCallExpression(call);
+                return;
+            }
+
+            if (args.getExpression(0) instanceof VariableExpression) {
+                String taskName = args.getExpression(0).getText();
+                call.setMethod(new ConstantExpression("createTask"));
+                args.getExpressions().clear();
+                args.addExpression(new ConstantExpression(taskName));
+                return;
+            }
+
+            if (!(args.getExpression(0) instanceof MethodCallExpression)) {
+                super.visitMethodCallExpression(call);
+                return;
+            }
+
+            MethodCallExpression nestedMethod = (MethodCallExpression) args.getExpressions().get(0);
+            if (!isInstanceMethod(nestedMethod)) {
+                super.visitMethodCallExpression(call);
+                return;
+            }
+
+            Expression taskName = nestedMethod.getMethod();
+            Expression mapArg = null;
+            List<Expression> extraArgs = Collections.emptyList();
+
+            if (nestedMethod.getArguments() instanceof NamedArgumentListExpression) {
+                mapArg = nestedMethod.getArguments();
+            }
+            else if (nestedMethod.getArguments() instanceof ArgumentListExpression) {
+                ArgumentListExpression nestedArgs = (ArgumentListExpression) nestedMethod.getArguments();
+                if (nestedArgs.getExpression(0) instanceof MapExpression) {
+                    mapArg = nestedArgs.getExpression(0);
+                    extraArgs = nestedArgs.getExpressions().subList(1, nestedArgs.getExpressions().size());
+                }
+                else {
+                    extraArgs = nestedArgs.getExpressions();
+                }
+            }
+
+            call.setMethod(new ConstantExpression("createTask"));
+            args.getExpressions().clear();
+            if (mapArg != null) {
+                args.addExpression(mapArg);
+            }
+            args.addExpression(taskName);
+            for (Expression extraArg : extraArgs) {
+                args.addExpression(extraArg);
+            }
+        }
     }
 }
--- src/test/groovy/org/gradle/integtests/TaskExecutionIntegrationTest.java
+++ src/test/groovy/org/gradle/integtests/TaskExecutionIntegrationTest.java
@@ -19,10 +19,18 @@ public class TaskExecutionIntegrationTest extends AbstractIntegrationTest {
                 "task withAction { }",
                 "task nothing",
                 "2.times { task \"dynamic$it\" {} }",
-                "task withMap(dependsOn: [withAction, nothing, dynamic0, dynamic1])",
+                "task task {}",
+                "if (task) { task conditional }",
+                "def task() { task inMethod }",
+                "task()",
+                "def cl = { -> task inClosure }",
+                "cl()",
+                "task withMap(dependsOn: [withAction, nothing, dynamic0, dynamic1, task, conditional, inMethod, inClosure])",
                 "task withMapAndAction(dependsOn: withMap) { }"
         );
-        inTestDirectory().withTasks("withMapAndAction").run().assertTasksExecuted(":dynamic0", ":dynamic1", ":nothing", ":withAction", ":withMap", ":withMapAndAction");
+        inTestDirectory().withTasks("withMapAndAction").run().assertTasksExecuted(":conditional", ":dynamic0",
+                ":dynamic1", ":inClosure", ":inMethod", ":nothing", ":task", ":withAction", ":withMap",
+                ":withMapAndAction");
     }
     
     @Test
@@ -30,7 +38,7 @@ public class TaskExecutionIntegrationTest extends AbstractIntegrationTest {
         TestFile buildFile = testFile("build.gradle");
         buildFile.writelns(
                 "import org.gradle.integtests.TaskExecutionIntegrationTest",
-                "createTask('a', dependsOn: 'b') { task ->",
+                "task a(dependsOn: 'b') { task ->",
                 "    assertTrue(build.taskGraph.hasTask(task))",
                 "    assertTrue(build.taskGraph.hasTask(':a'))",
                 "    assertTrue(build.taskGraph.hasTask(a))",
@@ -39,7 +47,7 @@ public class TaskExecutionIntegrationTest extends AbstractIntegrationTest {
                 "    assertTrue(build.taskGraph.allTasks.contains(task))",
                 "    assertTrue(build.taskGraph.allTasks.contains(project.task('b')))",
                 "}",
-                "createTask('b')",
+                "task b",
                 "build.taskGraph.whenReady { graph ->",
                 "    assertTrue(graph.hasTask(':a'))",
                 "    assertTrue(graph.hasTask(a))",
@@ -60,14 +68,14 @@ public class TaskExecutionIntegrationTest extends AbstractIntegrationTest {
         TestFile buildFile = testFile("build.gradle");
         buildFile.writelns(
                 "build.taskGraph.whenReady { assertFalse(project.hasProperty('graphReady')); graphReady = true }",
-                "createTask('a') { task -> project.executedA = task }",
-                "createTask('b') { ",
+                "task a { task -> project.executedA = task }",
+                "task b { ",
                 "    assertSame(a, project.executedA);",
                 "    assertTrue(build.taskGraph.hasTask(':a'))",
                 "}",
-                "createTask('c', dependsOn: 'a')",
-                "createTask('d', dependsOn: 'a')",
-                "createTask('e', dependsOn: ['a', 'd'])"
+                "task c(dependsOn: a)",
+                "task d(dependsOn: a)",
+                "task e(dependsOn: [a, d])"
                 );
         usingBuildFile(buildFile).withTasks("a", "b").run().assertTasksExecuted(":a", ":b");
         usingBuildFile(buildFile).withTasks("a", "a").run().assertTasksExecuted(":a");
@@ -80,10 +88,10 @@ public class TaskExecutionIntegrationTest extends AbstractIntegrationTest {
         testFile("settings.gradle").writelns("include 'child1', 'child2'");
         TestFile buildFile = testFile("build.gradle");
         buildFile.writelns(
-                "createTask('a')",
+                "task a",
                 "allprojects {",
-                "    createTask('b')",
-                "    createTask('c', dependsOn: ['b', ':a'])",
+                "    task b",
+                "    task c(dependsOn: ['b', ':a'])",
                 "}"
                 );
         usingBuildFile(buildFile).withTasks("a", "c").run().assertTasksExecuted(":a", ":b", ":c", ":child1:b", ":child1:c", ":child2:b", ":child2:c");
@@ -96,42 +104,12 @@ public class TaskExecutionIntegrationTest extends AbstractIntegrationTest {
         TestFile buildFile = testFile("build.gradle");
         buildFile.writelns(
                 "defaultTasks 'a', 'b'",
-                "createTask('a')",
+                "task a",
                 "subprojects {",
-                "    createTask('a', dependsOn: ':a')",
-                "    createTask('b', dependsOn: ':a')",
+                "    task a(dependsOn: ':a')",
+                "    task b(dependsOn: ':a')",
                 "}"
                 );
         usingBuildFile(buildFile).run().assertTasksExecuted(":a", ":child1:a", ":child2:a", ":child1:b", ":child2:b");
     }
-
-    @Test @Ignore
-    public void archiveWithImplicitAndExplicitDependencies() {
-        testFile("settings.gradle").write("include ('a', 'b')");
-        testFile("a/build.gradle").writelns(
-                "dependsOn(':b')",
-                "createTask('libs', type: org.gradle.api.tasks.bundling.Bundle)",
-                // todo - should not have to do these next 2 lines
-                "version = 'none'",
-                "libs.defaultArchiveTypes = org.gradle.api.plugins.JavaPluginConvention.DEFAULT_ARCHIVE_TYPES",
-                "libs {",
-                "    zip() {",
-                "        destinationDir = file('something.zip')",
-                "        files(file('src'))",
-                "    }",
-                "}",
-                "libs.dependsOn('compile')",
-                "createTask('compile')"
-        );
-        testFile("b/build.gradle").writelns(
-                "createTask('libs')",
-                "createTask('archive_zip')"
-        );
-
-        // todo - should archive_zip depend on b:libs, as it is a dependencu of its parent libs task?
-        usingBuildFile(testFile("build.gradle")).withTasks("a:archive_zip").run().assertTasksExecuted(":a:compile", "b:archive_zip", "a:archive_zip");
-
-        usingBuildFile(testFile("build.gradle")).withTasks("a:libs").run().assertTasksExecuted(":a:compile", "b:archive_zip", "b:libs", "a:archive_zip");
-    }
-
 }
