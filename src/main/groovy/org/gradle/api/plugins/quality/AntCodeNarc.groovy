66b8a61ef716d31116d586487729b8722cb7c24f
Adam Murdoch <a@rubygrapefruit.net>
2009-07-29 02:45:24 +0000

    GRADLE-555 Try a different style of ugliness to get the CodeNarc config file into the CodeNarc ant task.
    git-svn-id: http://svn.codehaus.org/gradle/gradle-core/trunk@1714 004c2c75-fc45-0410-b1a2-da8352e2331b

--- src/main/groovy/org/gradle/api/plugins/quality/AntCodeNarc.groovy
+++ src/main/groovy/org/gradle/api/plugins/quality/AntCodeNarc.groovy
@@ -1,32 +1,20 @@
 package org.gradle.api.plugins.quality
 
-import org.gradle.util.ClasspathUtil
-import org.gradle.util.HashUtil
-import org.gradle.api.tasks.AntBuilderAware
 import org.apache.tools.ant.BuildException
+import org.codenarc.CodeNarcRunner
+import org.codenarc.ant.CodeNarcTask
+import org.codenarc.ruleset.RuleSet
+import org.codenarc.ruleset.XmlReaderRuleSet
 import org.gradle.api.GradleException
+import org.gradle.api.tasks.AntBuilderAware
 
 class AntCodeNarc {
     def execute(def ant, AntBuilderAware source, File configFile, File reportFile) {
-        // This approach to getting the config file on the CodeNarc classpath may not be the greatest idea in the world
-
-        String suffix = HashUtil.createHash(configFile.text)
-        String resourceName = "${configFile.name}-$suffix"
-
         reportFile.parentFile.mkdirs()
 
-        ant.copy(file: configFile, toFile: new File(reportFile.parentFile, resourceName))
-
-        File zipFile = new File(reportFile.parentFile, "codenarc-config-${suffix}.jar")
-        ant.zip(destfile: zipFile) {
-            fileset(dir: reportFile.parentFile, includes: resourceName)
-        }
-
-        ClasspathUtil.addUrl(Class.forName('org.codenarc.ant.CodeNarcTask').classLoader, [zipFile])
-        
-        ant.taskdef(name: 'codenarc', classname: 'org.codenarc.ant.CodeNarcTask')
+        ant.project.addTaskDefinition('codenarc', CodeNarcTaskWithRuleFile)
         try {
-            ant.codenarc(ruleSetFiles: resourceName, maxPriority1Violations: 0, maxPriority2Violations: 0, maxPriority3Violations: 0) {
+            ant.codenarc(ruleSetFiles: 'ignore-me', ruleSetFile: configFile, maxPriority1Violations: 0, maxPriority2Violations: 0, maxPriority3Violations: 0) {
                 report(type: 'html', toFile: reportFile)
                 source.addToAntBuilder(ant, null)
             }
@@ -36,5 +24,25 @@ class AntCodeNarc {
             }
             throw e
         }
+    }
+}
+
+class CodeNarcTaskWithRuleFile extends CodeNarcTask {
+    File ruleSetFile
+
+    def CodeNarcTaskWithRuleFile() {
+        createCodeNarcRunner = { return new CodeNarcRunnerWithRuleFile(ruleSetFile) }
+    }
+}
+
+class CodeNarcRunnerWithRuleFile extends CodeNarcRunner {
+    File ruleSetFile
+
+    def CodeNarcRunnerWithRuleFile(File ruleSetFile) {
+        this.ruleSetFile = ruleSetFile
+    }
+
+    protected RuleSet createRuleSet() {
+        ruleSetFile.withReader { new XmlReaderRuleSet(it) };
     }
 }
