aad06e8dd7185068f3aabc3f8132fccccd2d7c84
Adam Murdoch <a@rubygrapefruit.net>
2009-09-02 21:09:57 +0000

    Removed the code which was needed to inject the config file into the CodeNarc Ant task, as CodeNarc 0.7 lets you pass a file directly to the task.
    git-svn-id: http://svn.codehaus.org/gradle/gradle-core/trunk@1819 004c2c75-fc45-0410-b1a2-da8352e2331b

--- subprojects/gradle-core/src/main/groovy/org/gradle/api/plugins/quality/AntCodeNarc.groovy
+++ subprojects/gradle-core/src/main/groovy/org/gradle/api/plugins/quality/AntCodeNarc.groovy
@@ -1,20 +1,15 @@
 package org.gradle.api.plugins.quality
 
 import org.apache.tools.ant.BuildException
-import org.codenarc.CodeNarcRunner
 import org.codenarc.ant.CodeNarcTask
-import org.codenarc.ruleset.RuleSet
-import org.codenarc.ruleset.XmlReaderRuleSet
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.AntBuilderAware
 
 class AntCodeNarc {
     def execute(def ant, AntBuilderAware source, File configFile, File reportFile) {
-        reportFile.parentFile.mkdirs()
-
-        ant.project.addTaskDefinition('codenarc', CodeNarcTaskWithRuleFile)
+        ant.project.addTaskDefinition('codenarc', CodeNarcTask)
         try {
-            ant.codenarc(ruleSetFiles: 'ignore-me', ruleSetFile: configFile, maxPriority1Violations: 0, maxPriority2Violations: 0, maxPriority3Violations: 0) {
+            ant.codenarc(ruleSetFiles: "file:$configFile", maxPriority1Violations: 0, maxPriority2Violations: 0, maxPriority3Violations: 0) {
                 report(type: 'html', toFile: reportFile)
                 source.addToAntBuilder(ant, null)
             }
@@ -24,25 +19,5 @@ class AntCodeNarc {
             }
             throw e
         }
-    }
-}
-
-class CodeNarcTaskWithRuleFile extends CodeNarcTask {
-    File ruleSetFile
-
-    def CodeNarcTaskWithRuleFile() {
-        createCodeNarcRunner = { return new CodeNarcRunnerWithRuleFile(ruleSetFile) }
-    }
-}
-
-class CodeNarcRunnerWithRuleFile extends CodeNarcRunner {
-    File ruleSetFile
-
-    def CodeNarcRunnerWithRuleFile(File ruleSetFile) {
-        this.ruleSetFile = ruleSetFile
-    }
-
-    protected RuleSet createRuleSet() {
-        ruleSetFile.withReader { new XmlReaderRuleSet(it) };
     }
 }
