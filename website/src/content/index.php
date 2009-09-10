ea5a8372d84d1d1a38bd0ce752052fc8b0b3722f
hansd <mail@dockter.biz>
2009-09-10 11:13:15 +0200

    Added website project

---
+++ website/src/content/index.php
@@ -0,0 +1,31 @@
+<h1>About Gradle</h1>
+<p>
+    Gradle is a build system which provides:
+</p>
+<ul>
+    <li>A very flexible general purpose build tool like Ant.</li>
+    <li>Switchable, build-by-convention frameworks a la Maven (for Java and Groovy projects). But we never lock you
+        in!
+    </li>
+    <li>Powerful support for multi-project builds.</li>
+    <li>Powerful dependency management (based on Apache Ivy).</li>
+    <li>Full support for your existing Maven or Ivy repository infrastructure.</li>
+    <li>Support for transitive dependency management without the need for remote repositories and <tt>pom.xml</tt>
+        or <tt>ivy.xml</tt> files (optional).
+    </li>
+    <li>Ant tasks as first class citizens.</li>
+    <li><i>Groovy</i> build scripts.</li>
+</ul>
+
+Our 150+ pages <a href="http://www.gradle.org/${currentRelease}/docs/userguide/userguide.html">user guide</a>
+(<a href="http://www.gradle.org/${currentRelease}/docs/userguide/userguide.pdf">pdf</a>)
+contains tutorials, a detailed overview and in-depth coverage.
+
+<p>The Gradle project itself is highly automated by its Gradle build. <a href="http://gradle.org/ci-server.html">As
+    documented</a>, it runs happily on Teamcity and Bamboo (where Gradle is not pre-installed).
+</p>
+<br>
+<h1>News</h1>
+<a href="http://docs.codehaus.org/createrssfeed.action?types=blogpost&sort=created&showContent=true&showDiff=true&spaces=GRADLE&labelString=&rssType=rss&maxResults=20&timeSpan=500&publicFeed=true&title=Gradle+RSS+Feed">
+    <img src="/img/rss20.gif" height=15 width=80 border=0 align=absmiddle><br>
+</a>
---
+++ website/src/php/indexPhpNews.php
@@ -0,0 +1,17 @@
+<?php
+	/*
+	Here, we'll loop through all of the items in the feed, and $item represents the current item in the loop.
+	*/
+	foreach ($feed->get_items() as $item):
+?>
+
+<div class="item">
+    <h2><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h2>
+
+    <p><?php echo $item->get_description(); ?></p>
+
+    <p>
+        <small>Posted on <?php echo $item->get_date('j F Y | g:i a'); ?></small>
+    </p>
+</div>
+<?php endforeach; ?>
