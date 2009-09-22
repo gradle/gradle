24aadef22b960a7f1555c677b3198029856210ea
adammurdoch <a@rubygrapefruit.net>
2009-09-22 16:02:32 +1000

    Added support for generating groovy projects to the performanceTest project.

---
+++ performanceTest/src/templates/Production.groovy
@@ -0,0 +1,13 @@
+package org.gradle.test.performance;
+
+public class ${productionClassName} {
+    private final String property;
+
+    public ${productionClassName}(String param) {
+        this.property = param;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+}
