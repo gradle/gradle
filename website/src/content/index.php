ad53fdf4d3395a5775fabb598bbc0da6e4e9325e
adammurdoch <a@rubygrapefruit.net>
2009-09-27 16:56:26 +1000

    Some changes to website home page.

--- website/src/content/index.php
+++ website/src/content/index.php
@@ -1,30 +1,38 @@
-<h1>About Gradle</h1>
+<h1>What is Gradle?</h1>
 <p>
-    Gradle is a build system which provides:
+    Gradle is an enterprise-grade build system which provides:
 </p>
 <ul>
     <li>A very flexible general purpose build tool like Ant.</li>
-    <li>Switchable, build-by-convention frameworks a la Maven (for Java and Groovy projects). But we never lock you
+    <li>Switchable, build-by-convention frameworks a la Maven, for Java, Groovy and Scala projects. But we never lock you
         in!
     </li>
+    <li><i>Groovy</i> build scripts.</li>
     <li>Powerful support for multi-project builds.</li>
     <li>Powerful dependency management (based on Apache Ivy).</li>
     <li>Full support for your existing Maven or Ivy repository infrastructure.</li>
     <li>Support for transitive dependency management without the need for remote repositories and <tt>pom.xml</tt>
         or <tt>ivy.xml</tt> files (optional).
     </li>
-    <li>Ant tasks as first class citizens.</li>
-    <li><i>Groovy</i> build scripts.</li>
+    <li>Ant tasks and builds as first class citizens.</li>
 </ul>
 
-Our 150+ pages <a href="http://www.gradle.org/${currentRelease}/docs/userguide/userguide.html">user guide</a>
-(<a href="http://www.gradle.org/${currentRelease}/docs/userguide/userguide.pdf">pdf</a>)
-contains tutorials, a detailed overview and in-depth coverage.
+<p>Gradle is licensed under the <a href="license.html">Apache License</a>.</p>
 
-<p>The Gradle project itself is highly automated by its Gradle build. <a href="http://gradle.org/ci-server.html">As
-    documented</a>, it runs happily on Teamcity and Bamboo (where Gradle is not pre-installed).
-</p>
-<br>
+<h2>Download It</h2>
+
+<p>You can download <a href="http://dist.codehaus.org/gradle/gradle-${currentRelease}-all.zip">gradle-${currentRelease}-all.zip</a> or
+<a href="build.html">build it from source</a>.</p>
+
+<h2>Use It</h2>
+
+<p>Our 200+ pages <a href="http://www.gradle.org/${currentRelease}/docs/userguide/userguide.html">user guide</a>
+(<a href="http://www.gradle.org/${currentRelease}/docs/userguide/userguide.pdf">pdf</a>)
+contains tutorials, a detailed overview and in-depth coverage.</p>
+
+<p>If you need help, you can find more resources on the <a href="http://docs.codehaus.org/display/GRADLE/Home">Wiki</a>.
+You could also try the <a href="lists.html">mailing lists</a>.</p>
+
 <h1>News</h1>
 <a href="http://docs.codehaus.org/createrssfeed.action?types=blogpost&sort=created&showContent=true&showDiff=true&spaces=GRADLE&labelString=&rssType=rss&maxResults=20&timeSpan=500&publicFeed=true&title=Gradle+RSS+Feed">
     <img src="/img/rss20.gif" height=15 width=80 border=0 align=absmiddle><br>
