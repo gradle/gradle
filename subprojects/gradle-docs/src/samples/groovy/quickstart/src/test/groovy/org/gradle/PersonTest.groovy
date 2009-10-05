b3d5ac6ce7fae6d8f9958b2cccb09ea6295e6132
adammurdoch <a@rubygrapefruit.net>
2009-10-05 15:32:05 +1100

    GRADLE-666 - Copy .java, .groovy and .scala resources into classes dir

--- subprojects/gradle-docs/src/samples/groovy/quickstart/src/test/groovy/org/gradle/PersonTest.groovy
+++ subprojects/gradle-docs/src/samples/groovy/quickstart/src/test/groovy/org/gradle/PersonTest.groovy
@@ -6,11 +6,21 @@ import static org.junit.Assert.*
 
 class PersonTest {
     @Test public void canConstructAPerson() {
+        Person p = new Person()
+        assertEquals('Barry', p.name)
+    }
+
+    @Test public void canConstructAPersonUsingName() {
         Person p = new Person(name: 'name')
         assertEquals('name', p.name)
     }
 
     @Test public void usingCorrectVersionOfGroovy() {
         assertEquals('1.6.0', InvokerHelper.version)
+    }
+    
+    @Test public void testResourcesAreAvailable() {
+        assertNotNull(getClass().getResource('/testResource.txt'))
+        assertNotNull(getClass().getResource('/testScript.groovy'))
     }
 }
