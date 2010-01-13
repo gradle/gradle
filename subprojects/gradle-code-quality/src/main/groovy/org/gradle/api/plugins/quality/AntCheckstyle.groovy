4761e70de3d8bdcc7cd2da149f66853c9b5f2417
adammurdoch <a@rubygrapefruit.net>
2010-01-13 12:44:40 +1100

    GRADLE-632 - Fixed Checkstyle and CodeNarc tasks after ClassLoader reorganisation.

--- subprojects/gradle-code-quality/src/main/groovy/org/gradle/api/plugins/quality/AntCheckstyle.groovy
+++ subprojects/gradle-code-quality/src/main/groovy/org/gradle/api/plugins/quality/AntCheckstyle.groovy
@@ -15,16 +15,16 @@
  */
 package org.gradle.api.plugins.quality
 
-import org.gradle.api.GradleException
-import org.gradle.api.tasks.AntBuilderAware
 import org.gradle.api.AntBuilder
+import org.gradle.api.GradleException
 import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.AntBuilderAware
 
 class AntCheckstyle {
     def checkstyle(AntBuilder ant, FileCollection source, File configFile, File resultFile, AntBuilderAware classpath, Map<String, ?> properties, boolean ignoreFailures) {
         String propertyName = "org.gradle.checkstyle.violations"
 
-        ant.typedef(resource: 'checkstyletask.properties')
+        ant.project.addTaskDefinition('checkstyle', getClass().classLoader.loadClass('com.puppycrawl.tools.checkstyle.CheckStyleTask'))
         ant.checkstyle(config: configFile, failOnViolation: false, failureProperty: propertyName) {
             source.addToAntBuilder(ant, 'fileset', FileCollection.AntType.FileSet)
             classpath.addToAntBuilder(ant, 'classpath')
---
+++ subprojects/gradle-core/src/integTest/groovy/org/gradle/integtests/SamplesCodeQualityIntegrationTest.groovy
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.integtests
+
+import org.gradle.util.TestFile
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * @author Hans Dockter
+ */
+@RunWith(DistributionIntegrationTestRunner.class)
+class SamplesCodeQualityIntegrationTest {
+    // Injected by test runner
+    private GradleDistribution dist
+    private GradleExecuter executer
+
+    @Test
+    public void checkReportsGenerated() {
+        TestFile projectDir = dist.samplesDir.file('codeQuality')
+        TestFile buildDir = projectDir.file('build')
+
+        executer.inDirectory(projectDir).withTasks('check').run()
+
+        buildDir.file('checkstyle/main.xml').assertIsFile()
+        buildDir.file('reports/codenarc/main.html').assertIsFile()
+        buildDir.file('reports/codenarc/test.html').assertIsFile()
+    }
+}
