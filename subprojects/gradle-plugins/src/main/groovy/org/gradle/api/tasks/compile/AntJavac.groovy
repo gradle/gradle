d5aac769915c545d3c119f15aa5232c5d9b1d8a2
adammurdoch <a@rubygrapefruit.net>
2010-01-21 15:44:17 +1100

    - Fix for Compile.depend for changed classloader structure.

--- subprojects/gradle-plugins/src/main/groovy/org/gradle/api/tasks/compile/AntJavac.groovy
+++ subprojects/gradle-plugins/src/main/groovy/org/gradle/api/tasks/compile/AntJavac.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 the original author or authors.
+ * Copyright 2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,7 +55,7 @@ class AntJavac {
                 dependOptions['cache'] = depCacheDir
             }
             logger.debug("Running ant depend with the following options {}", dependOptions)
-            ant.taskdef(name: 'gradleDepend', classname: AntDepend.class.name)
+            ant.project.addTaskDefinition('gradleDepend', AntDepend.class)
             ant.gradleDepend(dependOptions) {
                 source.addToAntBuilder(ant, 'src', FileCollection.AntType.MatchingTask)
             }
