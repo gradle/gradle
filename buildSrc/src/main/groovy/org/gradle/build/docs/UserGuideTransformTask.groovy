a3ecb5f11a26de62c27d116a325ed4dad1af6594
adammurdoch <a@rubygrapefruit.net>
2010-02-04 08:59:47 +1100

    - Added some @Input anotations to the docbook generation task

--- buildSrc/src/main/groovy/org/gradle/build/docs/UserGuideTransformTask.groovy
+++ buildSrc/src/main/groovy/org/gradle/build/docs/UserGuideTransformTask.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gradle.build.docs
 
 import groovy.xml.MarkupBuilder
@@ -6,23 +21,21 @@ import groovy.xml.dom.DOMUtil
 import javax.xml.parsers.DocumentBuilder
 import javax.xml.parsers.DocumentBuilderFactory
 import org.gradle.api.DefaultTask
-import org.gradle.api.GradleException
-import org.gradle.api.tasks.TaskAction
 import org.gradle.api.file.FileCollection
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.*
 
 /**
  * Transforms userguide source into docbook, replacing custom xml elements.
  */
 public class UserGuideTransformTask extends DefaultTask {
-    String version
+    @Input
+    String getVersion() { return project.version.toString() }
+    @Input
     String javadocUrl
+    @Input
     String groovydocUrl
     @InputFile
     File sourceFile
@@ -30,21 +43,15 @@ public class UserGuideTransformTask extends DefaultTask {
     File destFile
     @InputDirectory
     File snippetsDir
+    @Input
     boolean standalone
     @InputFiles
     FileCollection classpath;
-    SampleElementValidator validator = new SampleElementValidator();
+
+    final SampleElementValidator validator = new SampleElementValidator();
 
     @TaskAction
     def transform() {
-        version = project.version.toString()
-
-        ['sourceFile', 'destFile', 'classpath', 'javadocUrl', 'groovydocUrl', 'snippetsDir'].each {
-            if (getProperty(it) == null) {
-                throw new GradleException("Property not set: $it")
-            }
-        }
-
         Element root
 
         destFile.parentFile.mkdirs()
