24d708e5cbe29abd4163d1fbbbe099950a5671cc
adammurdoch <a@rubygrapefruit.net>
2010-02-28 10:35:45 +1100

    - Added news - Some style tweaks

--- website/src/content/index.php
+++ website/src/content/index.php
@@ -1,39 +1,40 @@
-<h1>What is Gradle?</h1>
-<p>
-    Gradle is an enterprise-grade build system which provides:
-</p>
-<ul>
-    <li>A very flexible general purpose build tool like Ant.</li>
-    <li>Switchable, build-by-convention frameworks a la Maven, for Java, Groovy and Scala projects. But we never lock you
-        in!
-    </li>
-    <li><i>Groovy</i> build scripts.</li>
-    <li>Powerful support for multi-project builds.</li>
-    <li>Powerful dependency management (based on Apache Ivy).</li>
-    <li>Full support for your existing Maven or Ivy repository infrastructure.</li>
-    <li>Support for transitive dependency management without the need for remote repositories and <tt>pom.xml</tt>
-        or <tt>ivy.xml</tt> files (optional).
-    </li>
-    <li>Ant tasks and builds as first class citizens.</li>
-</ul>
+<div id="homeContent">
+<h1 id="homeTitle">A better way to build</h1>
 
+<div id="about">
+
+<h2>Gradle is a build system</h2>
+    <p>A very flexible general purpose build tool like Ant.
+    >Switchable, build-by-convention frameworks a la Maven, for Java, Groovy and Scala projects. But we never lock you
+        in!
+    <i>Groovy</i> build scripts.</p>
+<h2>Gradle does some stuff</h2>
+    <p>Powerful support for multi-project builds.
+    Powerful dependency management (based on Apache Ivy).</p>
+<h2>Gradle does some other stuff</h2>
+    <p>Full support for your existing Maven or Ivy repository infrastructure.
+    Support for transitive dependency management without the need for remote repositories and <tt>pom.xml</tt>
+        or <tt>ivy.xml</tt> files (optional).
+    </p>
+    <p>Ant tasks and builds as first class citizens.</p>
+<h2>Gradle is free</h2>
 <p>Gradle is licensed under the <a href="license.html">Apache License</a>.</p>
 
-<h2>Download It</h2>
+</div>
 
-<p>You can download <a href="http://dist.codehaus.org/gradle/gradle-${currentRelease}-all.zip">gradle-${currentRelease}-all.zip</a> or
-<a href="build.html">build it from source</a>.</p>
+<div id="rightcolumn">
+<div id="gettingstarted">
+    <h2>Get started</h2>
+    <p>1. Download <a href="http://dist.codehaus.org/gradle/gradle-${currentRelease}-all.zip">gradle-${currentRelease}-all.zip</a></p>
+    <p>2. Try the <a href="tutorials.html">tutorials</a></p>
+    <p>3. Read the <a href="documentation.html">documentation</a></p>
+    <p>4. Join the <a href="community.html">community</a></p>
+</div>
 
-<h2>Use It</h2>
-
-<p>Our 200+ pages <a href="http://www.gradle.org/${currentRelease}/docs/userguide/userguide.html">user guide</a>
-(<a href="http://www.gradle.org/${currentRelease}/docs/userguide/userguide.pdf">pdf</a>)
-contains tutorials, a detailed overview and in-depth coverage.</p>
-
-<p>If you need help, you can find more resources on the <a href="http://docs.codehaus.org/display/GRADLE/Home">Wiki</a>.
-You could also try the <a href="lists.html">mailing lists</a>.</p>
-
-<h1>News</h1>
-<a href="http://docs.codehaus.org/createrssfeed.action?types=blogpost&sort=created&showContent=true&showDiff=true&spaces=GRADLE&labelString=&rssType=rss&maxResults=20&timeSpan=500&publicFeed=true&title=Gradle+RSS+Feed">
-    <img src="/img/rss20.gif" height=15 width=80 border=0 align=absmiddle><br>
-</a>
+<div id="news">
+    <h2>Recent News</h2>
+    <ul>${php.indexNews}</ul>
+    <a class="more" href="news.php">more</a>
+</div>
+</div>
+</div>
