1a57075d64b38904888cd6dc2d6be9f0b0f51acc
Hans Dockter <hans@gradle.biz>
2010-03-15 11:41:30 +0100

    Fixed detection of gradle core jar to work with the newest Gradle version.

---
+++ subprojects/gradle-open-api/src/test/groovy/org/gradle/openapi/external/ExternalUtilityTest.groovy
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gradle.openapi.external
+
+import spock.lang.Specification
+import org.gradle.util.TemporaryFolder
+import org.junit.Rule
+import org.gradle.openapi.external.ExternalUtility
+import org.gradle.util.TestFile
+
+public class ExternalUtilityTest extends Specification {
+    @Rule
+    public TemporaryFolder tmpDir = new TemporaryFolder();
+
+    def getTheRightGradleCore() {
+        TestFile libDir = tmpDir.dir.file('lib')
+
+        when:
+        libDir.deleteDir()
+        libDir.createDir().files('gradle-core-0.9.jar', 'gradle-core-worker-0.9.jar', 'gradle-open-api-0.9.jar')*.touch()
+
+        then:
+        ExternalUtility.getGradleJar(tmpDir.dir).absolutePath == new File(libDir, 'gradle-core-0.9.jar').absolutePath
+
+        when:
+        libDir.deleteDir()
+        libDir.createDir().files('gradle-core-0.9-20100315080959+0100.jar', 'gradle-core-worker-0.9-20100315080959+0100.jar', 'gradle-open-api-0.9-20100315080959+0100.jar')*.touch()
+
+        then:
+        ExternalUtility.getGradleJar(tmpDir.dir).absolutePath == new File(libDir, 'gradle-core-0.9-20100315080959+0100.jar').absolutePath
+    }
+
+    def failWithMultipleGradleCore() {
+        tmpDir.dir.file('lib').createDir().files('gradle-core-0.9.jar', 'gradle-core-0.10.jar', 'gradle-open-api-0.9.jar')*.touch()
+
+        when:
+        ExternalUtility.getGradleJar(tmpDir.dir)
+
+        then:
+        thrown(RuntimeException)
+    }
+
+    def returnNullWitNonExistingGradleCore() {
+        tmpDir.dir.file('lib').createDir().files('gradle-open-api-0.9.jar')*.touch()
+
+        expect:
+        ExternalUtility.getGradleJar(tmpDir.dir) == null
+    }
+
+    def failWitNonExistingGradleHome() {
+        expect:
+        ExternalUtility.getGradleJar(tmpDir.dir) == null
+    }
+
+}
