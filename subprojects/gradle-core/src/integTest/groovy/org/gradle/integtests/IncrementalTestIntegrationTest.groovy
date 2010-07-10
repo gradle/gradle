ec034f0f1637939cae8f0e3fdc62695c31e67649
adammurdoch <a@rubygrapefruit.net>
2010-07-11 08:49:11 +1000

    GRADLE-623 - Added int test to check stale test classes are no longer executed

---
+++ subprojects/gradle-core/src/integTest/groovy/org/gradle/integtests/IncrementalTestIntegrationTest.groovy
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.integtests
+
+import org.gradle.integtests.fixtures.TestResources
+import org.junit.Rule
+import org.gradle.integtests.fixtures.GradleDistributionExecuter
+import org.gradle.integtests.fixtures.GradleDistribution
+import org.junit.Test
+import static org.hamcrest.Matchers.*
+
+class IncrementalTestIntegrationTest {
+    @Rule public final GradleDistribution distribution = new GradleDistribution()
+    @Rule public final GradleDistributionExecuter executer = new GradleDistributionExecuter()
+    @Rule public final TestResources resources = new TestResources()
+
+    @Test
+    public void doesNotRunStaleTests() {
+        def failure = executer.withTasks('test').runWithFailure()
+        failure.assertThatCause(startsWith('There were failing tests.'))
+
+        distribution.testFile('src/test/java/Broken.java').assertIsFile().delete()
+
+        executer.withTasks('test').run()
+    }
+}
