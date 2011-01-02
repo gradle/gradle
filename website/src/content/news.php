e3ad14541179b85e05de17a84caa4161f65e7094
adammurdoch <a@rubygrapefruit.net>
2011-01-03 10:29:07 +1100

    Some website updates

--- website/src/content/news.php
+++ website/src/content/news.php
@@ -1,5 +1,5 @@
 <h1>News</h1>
-<a href="http://docs.codehaus.org/createrssfeed.action?types=blogpost&amp;sort=created&amp;showContent=true&amp;showDiff=true&amp;spaces=GRADLE&amp;labelString=&amp;rssType=rss&amp;maxResults=20&amp;timeSpan=500&amp;publicFeed=true&amp;title=Gradle+RSS+Feed">
+<a href="http://docs.codehaus.org/createrssfeed.action?types=blogpost&amp;sort=created&amp;showContent=true&amp;showDiff=true&amp;spaces=GRADLE&amp;labelString=&amp;rssType=rss&amp;maxResults=20&amp;timeSpan=400&amp;publicFeed=true&amp;title=Gradle+RSS+Feed">
     <img src="img/rss20.gif" height="15" width="80"/>
 </a>
 ${php.newsItems}
