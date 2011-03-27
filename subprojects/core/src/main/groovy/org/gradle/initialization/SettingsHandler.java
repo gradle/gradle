384f61103afbdd897379a807d955d2409c9f939c
Fred Simon <freds@jfrog.org>
2011-03-27 17:25:59 +0200

    GRADLE-1457: Init script passed from command line are not executed for buildSrc project

--- subprojects/core/src/main/groovy/org/gradle/initialization/SettingsHandler.java
+++ subprojects/core/src/main/groovy/org/gradle/initialization/SettingsHandler.java
@@ -24,6 +24,7 @@ import org.gradle.groovy.scripts.StringScriptSource;
 
 import java.io.File;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 
 /**
  * Handles locating and processing setting.gradle files.  Also deals with the buildSrc module, since that modules is
@@ -82,6 +83,8 @@ public class SettingsHandler {
         StartParameter buildSrcStartParameter = startParameter.newBuild();
         buildSrcStartParameter.setCurrentDir(new File(settingsLocation.getSettingsDir(),
                 BaseSettings.DEFAULT_BUILD_SRC_DIR));
+        buildSrcStartParameter.setInitScripts(new ArrayList<File>(startParameter.getInitScripts()));
+
         URLClassLoader buildSourceClassLoader = buildSourceBuilder.buildAndCreateClassLoader(buildSrcStartParameter);
 
         return loadSettings(gradle, settingsLocation, buildSourceClassLoader, startParameter, gradlePropertiesLoader);
