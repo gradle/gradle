3b5d66bab0affce5ce23f99aa60116c777b5203a
Fred Simon <freds@jfrog.org>
2011-04-12 14:07:03 +0300

    Used @RESOLVER_ID@ variable for integration tests

--- subprojects/ide/src/integTest/groovy/org/gradle/plugins/ide/idea/IdeaIntegrationTest.groovy
+++ subprojects/ide/src/integTest/groovy/org/gradle/plugins/ide/idea/IdeaIntegrationTest.groovy
@@ -156,7 +156,7 @@ idea << {
 
         def cache = distribution.userHomeDir.file("cache")
         def cachePath = cache.absolutePath.replace(File.separator, '/')
-        def expectedXml = expectedFile.text.replace('@CACHE_DIR@', cachePath)
+        def expectedXml = expectedFile.text.replace('@CACHE_DIR@', cachePath).replace('@RESOLVER_ID@', 'abad2d3f6ce1e83c75e1e977ba10f6e1')
 
         Diff diff = new Diff(expectedXml, file.text)
         diff.overrideElementQualifier(new ElementNameAndAttributeQualifier())
