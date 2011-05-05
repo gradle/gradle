3d4d295683c1850b7c4a91b02994e4dea1f0fb09
Szczepan Faber <szczepiq@gmail.com>
2011-05-05 17:50:19 +0200

    added some javadoc to make the maintenance easier

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/UserGuideSamplesIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/UserGuideSamplesIntegrationTest.groovy
@@ -27,6 +27,7 @@ class UserGuideSamplesIntegrationTest {
      If you're working in samples area there're gradle tasks that you should know of:
      - gradle intTestImage makes sure that the samples' resources are copied to the right place
      - gradle docs (or one of its dependent tasks) makes sure that samples' info is extracted from XMLs
+     - the 'expected' content of the asserting mechanism lives under docs/samples/userguideOutput
 
     */
 }
