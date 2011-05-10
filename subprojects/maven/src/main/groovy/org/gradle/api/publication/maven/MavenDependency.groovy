09dcdb75ec938c4026660eb290208f3d19f8a972
Peter Niederwieser <pniederw@gmail.com>
2011-05-10 21:01:43 +0200

    made CodeNarc happy

--- subprojects/maven/src/main/groovy/org/gradle/api/publication/maven/MavenDependency.groovy
+++ subprojects/maven/src/main/groovy/org/gradle/api/publication/maven/MavenDependency.groovy
@@ -16,9 +16,9 @@
 package org.gradle.api.publication.maven
 
 interface MavenDependency {
-    String groupId
-    String artifactId
-    String classifier
-    MavenScope scope
-    boolean optional
+    String getGroupId()
+    String getArtifactId()
+    String getClassifier()
+    MavenScope getScope()
+    boolean isOptional()
 }
