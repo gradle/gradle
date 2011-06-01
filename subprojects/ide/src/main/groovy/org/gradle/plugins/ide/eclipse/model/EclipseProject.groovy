4858ef98abaa6e61fa286f70e46e7b0ff40b7229
Szczepan Faber <szczepiq@gmail.com>
2011-06-01 19:32:13 +0200

    Removed TODOs as we won't work on them at this time. It should be fixed in more cross-cutting way or refactored to configure by closure rather than the map.

--- subprojects/ide/src/main/groovy/org/gradle/plugins/ide/eclipse/model/EclipseProject.groovy
+++ subprojects/ide/src/main/groovy/org/gradle/plugins/ide/eclipse/model/EclipseProject.groovy
@@ -223,7 +223,6 @@ class EclipseProject {
         if (illegalArgs) {
             throw new InvalidUserDataException("You provided illegal argument for a link: $illegalArgs. Valid link args are: $validKeys")
         }
-        //TODO SF: move validation here, update tests
         linkedResources << new Link(args.name, args.type, args.location, args.locationUri)
     }
 
