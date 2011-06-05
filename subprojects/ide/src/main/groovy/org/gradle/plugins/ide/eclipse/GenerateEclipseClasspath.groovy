d5f8de4f0e8ab1fe57807bf0d29830a46eb12c37
Szczepan Faber <szczepiq@gmail.com>
2011-06-05 21:49:58 +0200

    Deprecated lots of GenerateEclipseClasspath task stuff in favor of EclipseClasspath model. Polished the dsl guide documentation.

--- subprojects/ide/src/main/groovy/org/gradle/plugins/ide/eclipse/GenerateEclipseClasspath.groovy
+++ subprojects/ide/src/main/groovy/org/gradle/plugins/ide/eclipse/GenerateEclipseClasspath.groovy
@@ -20,11 +20,12 @@ import org.gradle.api.tasks.SourceSet
 import org.gradle.plugins.ide.api.XmlGeneratorTask
 import org.gradle.plugins.ide.eclipse.model.Classpath
 import org.gradle.plugins.ide.eclipse.model.EclipseClasspath
+import org.gradle.util.DeprecationLogger
 
 /**
  * Generates an Eclipse <code>.classpath</code> file. If you want to fine tune the eclipse configuration
  * <p>
- * Please refer to interesting examples on eclipse configuration in {@link EclipseClasspath}.
+ * At this moment nearly all configuration is done via {@link EclipseClasspath}.
  *
  * @author Hans Dockter
  */
@@ -45,113 +46,153 @@ class GenerateEclipseClasspath extends XmlGeneratorTask<Classpath> {
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.sourceSets. See examples in {@link EclipseClasspath}.
+     * <p>
      * The source sets to be added to the classpath.
      */
     Iterable<SourceSet> getSourceSets() {
+        DeprecationLogger.nagUser("eclipseClasspath.sourceSets", "eclipse.classpath.sourceSets")
         classpath.sourceSets
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.sourceSets. See examples in {@link EclipseClasspath}.
+     * <p>
      * The source sets to be added to the classpath.
      */
     void setSourceSets(Iterable<SourceSet> sourceSets) {
+        DeprecationLogger.nagUser("eclipseClasspath.sourceSets", "eclipse.classpath.sourceSets")
         classpath.sourceSets = sourceSets
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.plusConfigurations. See examples in {@link EclipseClasspath}.
+     * <p>
      * The configurations which files are to be transformed into classpath entries.
      */
     Collection<Configuration> getPlusConfigurations() {
+        DeprecationLogger.nagUser("eclipseClasspath.plusConfigurations", "eclipse.classpath.plusConfigurations")
         classpath.plusConfigurations
     }
 
     void setPlusConfigurations(Collection<Configuration> plusConfigurations) {
+        DeprecationLogger.nagUser("eclipseClasspath.plusConfigurations", "eclipse.classpath.plusConfigurations")
         classpath.plusConfigurations = plusConfigurations
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.minusConfigurations. See examples in {@link EclipseClasspath}.
+     * <p>
      * The configurations which files are to be excluded from the classpath entries.
      */
     Collection<Configuration> getMinusConfigurations() {
+        DeprecationLogger.nagUser("eclipseClasspath.minusConfigurations", "eclipse.classpath.minusConfigurations")
         classpath.minusConfigurations
     }
 
     void setMinusConfigurations(Collection<Configuration> minusConfigurations) {
+        DeprecationLogger.nagUser("eclipseClasspath.minusConfigurations", "eclipse.classpath.minusConfigurations")
         classpath.minusConfigurations = minusConfigurations
     }
 
     /**
+     * Deprecated. Please use #eclipse.pathVariables. See examples in {@link EclipseClasspath}.
+     * <p>
      * Adds path variables to be used for replacing absolute paths in classpath entries.
      *
      * @param pathVariables A map with String->File pairs.
      */
     Map<String, File> getVariables() {
+        DeprecationLogger.nagUser("eclipseClasspath.variables", "eclipse.pathVariables")
         classpath.pathVariables
     }
 
     void setVariables(Map<String, File> variables) {
+        DeprecationLogger.nagUser("eclipseClasspath.variables", "eclipse.pathVariables")
         classpath.pathVariables = variables
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.containers. See examples in {@link EclipseClasspath}.
+     * <p>
      * Containers to be added to the classpath
      */
     Set<String> getContainers() {
+        DeprecationLogger.nagUser("eclipseClasspath.containers", "eclipse.classpath.containers")
         classpath.containers
     }
 
     void setContainers(Set<String> containers) {
+        DeprecationLogger.nagUser("eclipseClasspath.containers", "eclipse.classpath.containers")
         classpath.containers = containers
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.defaultOutputDir. See examples in {@link EclipseClasspath}.
+     * <p>
      * The default output directory for eclipse generated files, eg classes.
      */
     File getDefaultOutputDir() {
+        DeprecationLogger.nagUser("eclipseClasspath.defaultOutputDir", "eclipse.classpath.defaultOutputDir")
         classpath.defaultOutputDir
     }
 
     void setDefaultOutputDir(File defaultOutputDir) {
+        DeprecationLogger.nagUser("eclipseClasspath.defaultOutputDir", "eclipse.classpath.defaultOutputDir")
         classpath.defaultOutputDir = defaultOutputDir
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.downloadSources. See examples in {@link EclipseClasspath}.
+     * <p>
      * Whether to download and add sources associated with the dependency jars. Defaults to true.
      */
     boolean getDownloadSources() {
+        DeprecationLogger.nagUser("eclipseClasspath.downloadSources", "eclipse.classpath.downloadSources")
         classpath.downloadSources
     }
 
     void setDownloadSources(boolean downloadSources) {
+        DeprecationLogger.nagUser("eclipseClasspath.downloadSources", "eclipse.classpath.downloadSources")
         classpath.downloadSources = downloadSources
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.downloadJavadoc. See examples in {@link EclipseClasspath}.
+     * <p>
      * Whether to download and add javadocs associated with the dependency jars. Defaults to false.
      */
     boolean getDownloadJavadoc() {
+        DeprecationLogger.nagUser("eclipseClasspath.downloadJavadoc", "eclipse.classpath.downloadJavadoc")
         classpath.downloadJavadoc
     }
 
     void setDownloadJavadoc(boolean downloadJavadoc) {
+        DeprecationLogger.nagUser("eclipseClasspath.downloadJavadoc", "eclipse.classpath.downloadJavadoc")
         classpath.downloadJavadoc = downloadJavadoc
     }
 
     /**
+     * Deprecated. Please use #eclipse.classpath.containers. See examples in {@link EclipseClasspath}.
+     * <p>
      * Adds containers to the .classpath.
      *
      * @param containers the container names to be added to the .classpath.
      */
     void containers(String... containers) {
+        DeprecationLogger.nagUser("eclipseClasspath.containers", "eclipse.classpath.containers")
         classpath.containers(containers)
     }
 
     /**
+     * Deprecated. Please use #eclipse.pathVariables. See examples in {@link EclipseClasspath}.
+     * <p>
      * Adds variables to be used for replacing absolute paths in classpath entries.
      *
      * @param variables A map where the keys are the variable names and the values are the variable values.
      */
     void variables(Map<String, File> variables) {
+        DeprecationLogger.nagUser("eclipseClasspath.variables", "eclipse.pathVariables")
         assert variables != null
         classpath.pathVariables.putAll variables
     }
