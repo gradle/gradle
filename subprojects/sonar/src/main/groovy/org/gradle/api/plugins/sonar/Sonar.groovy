51b3262f82eee0fc907788ae0703db68e367e104
Joshua Shrader <jshrader83@gmail.com>
2011-06-07 09:01:26 -0400

    Sonar plugin conventionMapped collections must be assigned to properties on modification.
    If these collections are not set in the plugin, then changes
    made via a call to, e.g., projectProperty('key', 'value'), will
    be lost as the convention closure will be re-evaluated on the next
    property access.  Calling one of these collection modifiers will
    cause pre-mature evaluation of the convention property, but will
    at least record the user's change.
    
    This should complete GRADLE-1591

--- subprojects/sonar/src/main/groovy/org/gradle/api/plugins/sonar/Sonar.groovy
+++ subprojects/sonar/src/main/groovy/org/gradle/api/plugins/sonar/Sonar.groovy
@@ -123,7 +123,7 @@ class Sonar extends ConventionTask {
      * @param sourceDirs the main source directory to be added
      */
     void projectMainSourceDir(File sourceDir) {
-        getProjectMainSourceDirs() << sourceDir
+        projectMainSourceDirs = getProjectMainSourceDirs() << sourceDir
     }
 
     /**
@@ -132,7 +132,7 @@ class Sonar extends ConventionTask {
      * @param sourceDirs the main source directories to be added
      */
     void projectMainSourceDirs(File... sourceDirs) {
-        getProjectMainSourceDirs().addAll(sourceDirs as List)
+        projectMainSourceDirs = getProjectMainSourceDirs() + (sourceDirs as List)
     }
 
     /**
@@ -141,7 +141,7 @@ class Sonar extends ConventionTask {
      * @param sourceDirs the testsource directory to be added
      */
     void projectTestSourceDir(File sourceDir) {
-        getProjectTestSourceDirs() << sourceDir
+        projectTestSourceDirs = getProjectTestSourceDirs() << sourceDir
     }
 
     /**
@@ -150,7 +150,7 @@ class Sonar extends ConventionTask {
      * @param sourceDirs the test source directories to be added
      */
     void projectTestSourceDirs(File... sourceDirs) {
-        getProjectTestSourceDirs().addAll(sourceDirs as List)
+        projectTestSourceDirs = getProjectTestSourceDirs() + (sourceDirs as List)
     }
 
     /**
@@ -159,7 +159,7 @@ class Sonar extends ConventionTask {
      * @param classesDir the classes directory to be added
      */
     void projectClassesDir(File classesDir) {
-        getProjectClassesDirs() << classesDir
+        projectClassesDirs = getProjectClassesDirs() << classesDir
     }
 
     /**
@@ -168,7 +168,7 @@ class Sonar extends ConventionTask {
      * @param classesDirs the classes directories to be added
      */
     void projectClassesDirs(File... classesDirs) {
-        getProjectClassesDirs().addAll(classesDirs as List)
+        projectClassesDirs = getProjectClassesDirs() + (classesDirs as List)
     }
 
     /**
@@ -177,7 +177,7 @@ class Sonar extends ConventionTask {
      * @param dependency the depedency to be added
      */
     void projectDependency(File dependency) {
-        getProjectDependencies() << dependency
+        projectDependencies = getProjectDependencies() << dependency
     }
 
     /**
@@ -186,7 +186,7 @@ class Sonar extends ConventionTask {
      * @param dependencies the dependencies to be added
      */
     void projectDependencies(File... dependencies) {
-        getProjectDependencies().addAll(dependencies as List)
+        projectDependencies = getProjectDependencies() + (dependencies as List)
     }
 
     /**
@@ -198,7 +198,7 @@ class Sonar extends ConventionTask {
      * @param value the value of the global property
      */
     void globalProperty(String name, String value) {
-        getGlobalProperties().put(name, value)
+        globalProperties = getGlobalProperties() << [(name): value]
     }
 
     /**
@@ -208,7 +208,7 @@ class Sonar extends ConventionTask {
      * @param properties the global properties to be added
      */
     void globalProperties(Map properties) {
-        getGlobalProperties().putAll(properties)
+        globalProperties = getGlobalProperties() << properties
     }
 
     /**
@@ -220,7 +220,7 @@ class Sonar extends ConventionTask {
      * @param value the value of the project property
      */
     void projectProperty(String name, String value) {
-        getProjectProperties().put(name, value)
+        projectProperties = getProjectProperties() << [(name): value]
     }
 
     /**
@@ -230,7 +230,7 @@ class Sonar extends ConventionTask {
      * @param properties the project properties to be added
      */
     void projectProperties(Map properties) {
-        getProjectProperties().putAll(properties)
+        projectProperties = getProjectProperties() << properties
     }
 
     protected URL findGradleSonarJar() {
