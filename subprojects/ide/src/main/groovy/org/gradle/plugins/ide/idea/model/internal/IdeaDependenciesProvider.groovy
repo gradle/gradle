1a6de265289614f3ed9c97a54c39338162802ef3
Szczepan Faber <szczepiq@gmail.com>
2011-07-03 01:17:27 +0200

    Follow up on recent refactoring. Unified the remaining parts of the dependency extracting logic between eclipse & idea plugins.

--- subprojects/ide/src/main/groovy/org/gradle/plugins/ide/idea/model/internal/IdeaDependenciesProvider.groovy
+++ subprojects/ide/src/main/groovy/org/gradle/plugins/ide/idea/model/internal/IdeaDependenciesProvider.groovy
@@ -17,14 +17,12 @@
 package org.gradle.plugins.ide.idea.model.internal
 
 import org.gradle.api.Project
-import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
-import org.gradle.api.specs.Spec
+import org.gradle.api.artifacts.Configuration
 import org.gradle.plugins.ide.idea.model.IdeaModule
 import org.gradle.plugins.ide.idea.model.ModuleLibrary
 import org.gradle.plugins.ide.idea.model.Path
 import org.gradle.plugins.ide.idea.model.PathFactory
 import org.gradle.plugins.ide.internal.IdeDependenciesExtractor
-import org.gradle.api.artifacts.*
 
 /**
  * All code was refactored from the GenerateIdeaModule class.
@@ -80,116 +78,18 @@ class IdeaDependenciesProvider {
     protected Set getModuleLibraries(String scope) {
         if (!scopes[scope]) { return [] }
 
+        LinkedHashSet moduleLibraries = []
         def repoFileDependencies = dependenciesExtractor.extractRepoFileDependencies(
                 project.configurations, scopes[scope].plus, scopes[scope].minus, downloadSources, downloadJavadoc)
 
-        List moduleLibraries = repoFileDependencies.collect {
-            new ModuleLibrary([getPath(it.dependency)] as Set, it.javadoc ? [getPath(it.javadoc)] as Set : [] as Set, it.source ? [getPath(it.source)] as Set : [] as Set, [] as Set, scope)
+        repoFileDependencies.each {
+            moduleLibraries << new ModuleLibrary([getPath(it.dependency)] as Set, it.javadoc ? [getPath(it.javadoc)] as Set : [] as Set, it.source ? [getPath(it.source)] as Set : [] as Set, [] as Set, scope)
         }
 
-        moduleLibraries.addAll(getSelfResolvingFiles(getScopeDependencies(scopes[scope],
-                { it instanceof SelfResolvingDependency && !(it instanceof ProjectDependency)}), scope))
-        return moduleLibraries as LinkedHashSet
-    }
-
-    private def getSelfResolvingFiles(Collection dependencies, String scope) {
-        dependencies.inject([] as LinkedHashSet) { result, SelfResolvingDependency selfResolvingDependency ->
-            result.addAll(selfResolvingDependency.resolve().collect { File file ->
-                new ModuleLibrary([getPath(file)] as Set, [] as Set, [] as Set, [] as Set, scope)
-            })
-            result
+        dependenciesExtractor.extractLocalFileDependencies(scopes[scope].plus, scopes[scope].minus).each {
+            moduleLibraries << new ModuleLibrary([getPath(it.dependency)] as Set, [] as Set, [] as Set, [] as Set, scope)
         }
-    }
-
-    private Set getScopeDependencies(Map<String, Collection<Configuration>> configurations, Closure filter) {
-        Set firstLevelDependencies = new LinkedHashSet()
-        configurations.plus.each { Configuration configuration ->
-            firstLevelDependencies.addAll(configuration.getAllDependencies().findAll(filter))
-        }
-        configurations.minus.each { Configuration configuration ->
-            configuration.getAllDependencies().findAll(filter).each { minusDep ->
-                // This deals with dependencies that are defined in different scopes with different
-                // artifacts. Right now we accept the fact, that in such a situation some artifacts
-                // might be duplicated in Idea (they live in different scopes then).
-                if (minusDep instanceof ExternalDependency) {
-                    ExternalDependency removeCandidate = firstLevelDependencies.find { it == minusDep }
-                    if (removeCandidate && removeCandidate.artifacts == minusDep.artifacts) {
-                        firstLevelDependencies.remove(removeCandidate)
-                    }
-                } else {
-                    firstLevelDependencies.remove(minusDep)
-                }
-            }
-        }
-        return firstLevelDependencies
-    }
-
-    private Set<ResolvedDependency> resolveDependencies(Collection<Configuration> plusConfigurations, Collection<Configuration> minusConfigurations) {
-        def result = new LinkedHashSet()
-        for (plusConfiguration in plusConfigurations) {
-            result.addAll(getAllDeps(plusConfiguration.resolvedConfiguration.getFirstLevelModuleDependencies({ it instanceof ExternalDependency } as Spec)))
-        }
-        for (minusConfiguration in minusConfigurations) {
-            result.removeAll(getAllDeps(minusConfiguration.resolvedConfiguration.getFirstLevelModuleDependencies({ it instanceof ExternalDependency } as Spec)))
-        }
-        result
-    }
-
-    private Set<File> resolveFiles(Collection<Configuration> plusConfigurations, Collection<Configuration> minusConfigurations) {
-        def result = new LinkedHashSet()
-        for (plusConfiguration in plusConfigurations) {
-            result.addAll(plusConfiguration.files { it instanceof ExternalDependency })
-        }
-        for (minusConfiguration in minusConfigurations) {
-            result.removeAll(minusConfiguration.files { it instanceof ExternalDependency })
-        }
-        result
-    }
-
-    private getFiles(Set dependencies, String classifier) {
-        return project.configurations.detachedConfiguration((dependencies as Dependency[])).files.inject([:]) { result, sourceFile ->
-            String key = sourceFile.name.replace("-${classifier}.jar", '.jar')
-            result[key] = sourceFile
-            result
-        }
-    }
-
-    private List getResolvableDependenciesForAllResolvedDependencies(Set allResolvedDependencies, Closure configureClosure) {
-        return allResolvedDependencies.collect { ResolvedDependency resolvedDependency ->
-            def dependency = new DefaultExternalModuleDependency(resolvedDependency.moduleGroup, resolvedDependency.moduleName, resolvedDependency.moduleVersion,
-                    resolvedDependency.configuration)
-            dependency.transitive = false
-            configureClosure.call(dependency)
-            dependency
-        }
-    }
-
-    protected Set getAllDeps(Collection deps, Set allDeps = []) {
-        deps.each { ResolvedDependency resolvedDependency ->
-            def notSeenBefore = allDeps.add(resolvedDependency)
-            if (notSeenBefore) { // defend against circular dependencies
-                getAllDeps(resolvedDependency.children, allDeps)
-            }
-        }
-        allDeps
-    }
-
-    protected addSourceArtifact(DefaultExternalModuleDependency dependency) {
-        dependency.artifact { artifact ->
-            artifact.name = dependency.name
-            artifact.type = 'source'
-            artifact.extension = 'jar'
-            artifact.classifier = 'sources'
-        }
-    }
-
-    protected addJavadocArtifact(DefaultExternalModuleDependency dependency) {
-        dependency.artifact { artifact ->
-            artifact.name = dependency.name
-            artifact.type = 'javadoc'
-            artifact.extension = 'jar'
-            artifact.classifier = 'javadoc'
-        }
+        moduleLibraries
     }
 
     protected Path getPath(File file) {
