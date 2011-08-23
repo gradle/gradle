60e40fc5043495515eb8642eda802e24c5f71784
adammurdoch <adam.murdoch@gradleware.com>
2011-08-23 12:55:30 +1000

    Fix for broken int test when running using the daemon

--- subprojects/core/src/integTest/groovy/org/gradle/integtests/fixtures/MavenRepository.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/integtests/fixtures/MavenRepository.groovy
@@ -16,9 +16,10 @@
 package org.gradle.integtests.fixtures
 
 import org.gradle.util.TestFile
+import java.text.SimpleDateFormat
 
 class MavenRepository {
-    private final TestFile rootDir
+    final TestFile rootDir
 
     MavenRepository(TestFile rootDir) {
         this.rootDir = rootDir
@@ -37,7 +38,9 @@ class MavenModule {
     final String version
     private final List dependencies = []
     final String classifier
-    int publishCount = 0
+    int publishCount = 1
+    final updateFormat = new SimpleDateFormat("yyyyMMddHHmmss")
+    final timestampFormat = new SimpleDateFormat("yyyyMMdd.HHmmss")
 
     MavenModule(TestFile moduleDir, String groupId, String artifactId, String version, String classifier = null) {
         this.moduleDir = moduleDir
@@ -66,14 +69,14 @@ class MavenModule {
     }
 
     File getPomFile() {
-        return new File(moduleDir, "$artifactId-${version}.pom")
+        return new File(moduleDir, "$artifactId-${publishArtifactVersion}.pom")
     }
 
 
     TestFile getArtifactFile() {
-        def fileName = "$artifactId-${version}.jar"
+        def fileName = "$artifactId-${publishArtifactVersion}.jar"
         if (classifier) {
-            fileName = "$artifactId-$version-${classifier}.jar"
+            fileName = "$artifactId-$publishArtifactVersion-${classifier}.jar"
         }
         return moduleDir.file(fileName)
     }
@@ -83,8 +86,34 @@ class MavenModule {
         publishArtifact()
     }
 
+    String getPublishArtifactVersion() {
+        return version.endsWith("-SNAPSHOT") ? "${version.replaceFirst('-SNAPSHOT$', '')}-${timestampFormat.format(publishTimestamp)}-${publishCount}" : version
+    }
+
+    Date getPublishTimestamp() {
+        return new Date(updateFormat.parse("20100101120000").time + publishCount * 1000)
+    }
+
     File publishArtifact() {
         moduleDir.createDir()
+
+        if (version.endsWith("-SNAPSHOT")) {
+            def metaDataFile = moduleDir.file('maven-metadata.xml')
+            metaDataFile.text = """
+<metadata>
+  <groupId>$groupId</groupId>
+  <artifactId>$artifactId</artifactId>
+  <version>$version</version>
+  <versioning>
+    <snapshot>
+      <timestamp>${timestampFormat.format(publishTimestamp)}</timestamp>
+      <buildNumber>$publishCount</buildNumber>
+    </snapshot>
+    <lastUpdated>${updateFormat.format(publishTimestamp)}</lastUpdated>
+  </versioning>
+</metadata>
+"""
+        }
 
         pomFile.text = ""
         pomFile << """
