d05ef58ae12615eced5270e8a53333de665f9b42
adammurdoch <adam.murdoch@gradleware.com>
2011-09-02 11:58:45 +1000

    Fix the worker process classpath.

--- subprojects/core/src/main/groovy/org/gradle/process/internal/child/WorkerProcessClassPathProvider.java
+++ subprojects/core/src/main/groovy/org/gradle/process/internal/child/WorkerProcessClassPathProvider.java
@@ -36,7 +36,7 @@ public class WorkerProcessClassPathProvider extends AbstractClassPathProvider {
 
     public WorkerProcessClassPathProvider(CacheRepository cacheRepository) {
         this.cacheRepository = cacheRepository;
-        add("WORKER_PROCESS", toPatterns("gradle-core", "slf4j-api", "logback-classic", "logback-core", "jul-to-slf4j"));
+        add("WORKER_PROCESS", toPatterns("gradle-core", "gradle-cli", "slf4j-api", "logback-classic", "logback-core", "jul-to-slf4j"));
     }
 
     public Set<File> findClassPath(String name) {
