6f84fc1be1d9d176c85bebcf6b891cedcb208583
Luke Daley <ld@ldaley.com>
2011-09-21 19:58:41 +0100

    Extract the output scraping execution result and failure classes from the forking executer.

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/ForkingGradleExecuter.java
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/ForkingGradleExecuter.java
@@ -16,25 +16,22 @@
 
 package org.gradle.integtests.fixtures;
 
-import org.gradle.api.Action;
 import org.gradle.os.OperatingSystem;
 import org.gradle.process.internal.ExecHandle;
 import org.gradle.process.internal.ExecHandleBuilder;
-import org.gradle.util.GUtil;
 import org.gradle.util.Jvm;
 import org.gradle.util.TestFile;
-import org.hamcrest.Matcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.util.*;
-import java.util.regex.Pattern;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import static org.gradle.util.Matchers.containsLine;
-import static org.gradle.util.Matchers.matchesRegexp;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 public class ForkingGradleExecuter extends AbstractGradleExecuter {
@@ -53,14 +50,14 @@ public class ForkingGradleExecuter extends AbstractGradleExecuter {
 
     @Override
     protected ExecutionResult doRun() {
-        Map result = doRun(false);
-        return new ForkedExecutionResult(result);
+        Map<String, String> result = doRun(false);
+        return new OutputScrapingExecutionResult(result.get("output"), result.get("error"));
     }
 
     @Override
     protected ExecutionFailure doRunWithFailure() {
-        Map result = doRun(true);
-        return new ForkedExecutionFailure(result);
+        Map<String, String> result = doRun(true);
+        return new OutputScrapingExecutionFailure(result.get("output"), result.get("error"));
     }
 
     /**
@@ -70,7 +67,7 @@ public class ForkingGradleExecuter extends AbstractGradleExecuter {
         gradleOpts.addAll(Arrays.asList(opts));
     }
 
-    protected Map doRun(boolean expectFailure) {
+    protected Map<String, String> doRun(boolean expectFailure) {
         if (!gradleHomeDir.isDirectory()) {
             fail(gradleHomeDir + " is not a directory.\n"
                     + "If you are running tests from IDE make sure that gradle tasks that prepare the test image were executed. Last time it was 'intTestImage' task.");
@@ -122,7 +119,11 @@ public class ForkingGradleExecuter extends AbstractGradleExecuter {
             System.out.println(message);
             throw new RuntimeException(message);
         }
-        return GUtil.map("output", output, "error", error);
+        
+        Map<String, String> map = new HashMap<String, String>(2);
+        map.put("output", output);
+        map.put("error", error);
+        return map;
     }
 
     private String formatGradleOpts() {
@@ -189,154 +190,4 @@ public class ForkingGradleExecuter extends AbstractGradleExecuter {
         }
     }
 
-    private static class ForkedExecutionResult extends AbstractExecutionResult {
-        private final Map result;
-        private final Pattern skippedTaskPattern = Pattern.compile("(:\\S+?(:\\S+?)*)\\s+((SKIPPED)|(UP-TO-DATE))");
-        private final Pattern notSkippedTaskPattern = Pattern.compile("(:\\S+?(:\\S+?)*)");
-        private final Pattern taskPattern = Pattern.compile("(:\\S+?(:\\S+?)*)(\\s+.+)?");
-
-        public ForkedExecutionResult(Map result) {
-            this.result = result;
-        }
-
-        public String getOutput() {
-            return result.get("output").toString();
-        }
-
-        public String getError() {
-            return result.get("error").toString();
-        }
-
-        public List<String> getExecutedTasks() {
-            return grepTasks(taskPattern);
-        }
-        
-        public ExecutionResult assertTasksExecuted(String... taskPaths) {
-            List<String> expectedTasks = Arrays.asList(taskPaths);
-            assertThat(String.format("Expected tasks %s not found in process output:%n%s", expectedTasks, getOutput()), getExecutedTasks(), equalTo(expectedTasks));
-            return this;
-        }
-
-        public Set<String> getSkippedTasks() {
-            return new HashSet<String>(grepTasks(skippedTaskPattern));
-        }
-        
-        public ExecutionResult assertTasksSkipped(String... taskPaths) {
-            Set<String> expectedTasks = new HashSet<String>(Arrays.asList(taskPaths));
-            assertThat(String.format("Expected skipped tasks %s not found in process output:%n%s", expectedTasks, getOutput()), getSkippedTasks(), equalTo(expectedTasks));
-            return this;
-        }
-
-        public ExecutionResult assertTaskSkipped(String taskPath) {
-            Set<String> tasks = new HashSet<String>(getSkippedTasks());
-            assertThat(String.format("Expected skipped task %s not found in process output:%n%s", taskPath, getOutput()), tasks, hasItem(taskPath));
-            return this;
-        }
-
-        public ExecutionResult assertTasksNotSkipped(String... taskPaths) {
-            Set<String> tasks = new HashSet<String>(grepTasks(notSkippedTaskPattern));
-            Set<String> expectedTasks = new HashSet<String>(Arrays.asList(taskPaths));
-            assertThat(String.format("Expected executed tasks %s not found in process output:%n%s", expectedTasks, getOutput()), tasks, equalTo(expectedTasks));
-            return this;
-        }
-
-        public ExecutionResult assertTaskNotSkipped(String taskPath) {
-            Set<String> tasks = new HashSet<String>(grepTasks(notSkippedTaskPattern));
-            assertThat(String.format("Expected executed task %s not found in process output:%n%s", taskPath, getOutput()), tasks, hasItem(taskPath));
-            return this;
-        }
-
-        private List<String> grepTasks(final Pattern pattern) {
-            final List<String> tasks = new ArrayList<String>();
-
-            eachLine(new Action<String>() {
-                public void execute(String s) {
-                    java.util.regex.Matcher matcher = pattern.matcher(s);
-                    if (matcher.matches()) {
-                        String taskName = matcher.group(1);
-                        if (!taskName.startsWith(":buildSrc:")) {
-                            tasks.add(taskName);
-                        }
-                    }
-                }
-            });
-
-            return tasks;
-        }
-
-        private void eachLine(Action<String> action) {
-            BufferedReader reader = new BufferedReader(new StringReader(getOutput()));
-            String line;
-            try {
-                while ((line = reader.readLine()) != null) {
-                    action.execute(line);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private static class ForkedExecutionFailure extends ForkedExecutionResult implements ExecutionFailure {
-        private final Pattern causePattern = Pattern.compile("(?m)^Cause: ");
-
-        public ForkedExecutionFailure(Map result) {
-            super(result);
-        }
-
-        public ExecutionFailure assertHasLineNumber(int lineNumber) {
-            assertThat(getError(), containsString(String.format(" line: %d", lineNumber)));
-            return this;
-        }
-
-        public ExecutionFailure assertHasFileName(String filename) {
-            assertThat(getError(), containsLine(startsWith(filename)));
-            return this;
-        }
-
-        public ExecutionFailure assertHasCause(String description) {
-            assertThatCause(startsWith(description));
-            return this;
-        }
-
-        public ExecutionFailure assertThatCause(Matcher<String> matcher) {
-            String error = getError();
-            java.util.regex.Matcher regExpMatcher = causePattern.matcher(error);
-            int pos = 0;
-            while (pos < error.length()) {
-                if (!regExpMatcher.find(pos)) {
-                    break;
-                }
-                int start = regExpMatcher.end();
-                String cause;
-                if (regExpMatcher.find(start)) {
-                    cause = error.substring(start, regExpMatcher.start());
-                    pos = regExpMatcher.start();
-                } else {
-                    cause = error.substring(start);
-                    pos = error.length();
-                }
-                if (matcher.matches(cause)) {
-                    return this;
-                }
-            }
-            fail(String.format("No matching cause found in '%s'", error));
-            return this;
-        }
-
-        public ExecutionFailure assertHasNoCause() {
-            assertThat(getError(), not(matchesRegexp(causePattern)));
-            return this;
-        }
-
-        public ExecutionFailure assertHasDescription(String context) {
-            assertThatDescription(startsWith(context));
-            return this;
-        }
-
-        public ExecutionFailure assertThatDescription(Matcher<String> matcher) {
-            assertThat(getError(), containsLine(matcher));
-            return this;
-        }
-    }
 }
