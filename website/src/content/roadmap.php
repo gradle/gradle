00da60a7d8574739e0342ce5651c157a8342fa1b
Luke Daley <ld@ldaley.com>
2011-09-26 17:50:06 +0100

    Renamed the status board to roadmap and added a link.

---
+++ website/src/content/roadmap.php
@@ -0,0 +1,16 @@
+<? if (is_dir("board") ): ?>
+  <link href="board/style.css" rel="stylesheet" type="text/css"/>
+  <div id="status-board-container">
+    <h1>Development Roadmap</h1>
+    <p>This page gives a very <em>high level</em> view of the current project roadmap and status.</p>
+    <p style="text-align: center; font-weight: bold">You can hover over each item for more information, or click the item on a touchscreen device (e.g. iPad).</p>
+    <? require "board/board.php"; ?>
+  </div>
+
+  <div id="legend-container">
+    <h1>Status Legend</h1>
+    <? require "board/legend.php"; ?>
+  </div>
+<? else: ?>
+  <p style="text-align: center; color: red">Roadmap information is currently unavailable.</p>
+<? endif; ?>
