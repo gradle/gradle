ad5c609035ab51625fcdf673919a60729e348b7c
Luke Daley <ld@ldaley.com>
2011-09-29 15:27:21 +0100

    Update roadmap wording.

--- website/src/content/roadmap.php
+++ website/src/content/roadmap.php
@@ -2,8 +2,9 @@
   <link href="board/style.css" rel="stylesheet" type="text/css"/>
   <div id="status-board-container">
     <h1>Development Roadmap</h1>
-    <p>This page gives a very <em>high level</em> view of the current project roadmap and status.</p>
-    <p style="font-weight: bold">You can hover over each item for more information, or click the item on a touchscreen device (e.g. iPad).</p>
+    <p>This page gives a very <em>high level</em> view of the current project roadmap and status and shouldn't be considered exhaustive. For a more fine grained view you can take a look at the <a href="http://issues.gradle.org/browse/GRADLE" title="Gradle">issue tracker</a>.</p>
+    
+    <p style="font-weight: bold">You can click on any item to read a corresponding forum post giving more context and rationale.</p>
     <? require "board/board.php"; ?>
   </div>
 
