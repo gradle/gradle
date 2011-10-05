b891be5db3848aea7dc97f62bd8535893169db53
Luke Daley <ld@ldaley.com>
2011-10-05 16:52:48 +0100

    bumped logging message that occurs when a busy daemon receives a command down as it's not an error state and added a comment about why this is the case.

--- subprojects/launcher/src/main/java/org/gradle/launcher/daemon/server/DaemonStateCoordinator.java
+++ subprojects/launcher/src/main/java/org/gradle/launcher/daemon/server/DaemonStateCoordinator.java
@@ -169,7 +169,12 @@ public class DaemonStateCoordinator implements Stoppable, AsyncStoppable {
         lock.lock();
         try {
             if (currentCommandExecution != null) { // daemon is busy
-                LOGGER.warn("onStartCommand({}) called while currentCommandExecution = {}", execution, currentCommandExecution);
+                /*
+                    This is not particularly abnormal as daemon can become busy between a particular client connecting to it and then
+                    sending a command. The UpdateDaemonStateAndHandleBusyDaemon command action will send back a DaemonBusy result
+                    to the client which will then just try another daemon, making this a non-error condition.
+                */
+                LOGGER.debug("onStartCommand({}) called while currentCommandExecution = {}", execution, currentCommandExecution);
                 return currentCommandExecution;
             } else {
                 LOGGER.debug("onStartCommand({}) called after {} mins of idle", execution, getIdleMinutes());
