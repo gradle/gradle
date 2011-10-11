be03218ab064dfebe19504a480a980d7b0a7025f
daz <daz@bigdaz.com>
2011-10-11 11:07:06 -0600

    Use appropriate cache options when asking resolver for cached meta-data

--- subprojects/core-impl/src/main/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheFirstChainResolver.java
+++ subprojects/core-impl/src/main/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheFirstChainResolver.java
@@ -4,9 +4,12 @@ import org.apache.ivy.core.cache.CacheMetadataOptions;
 import org.apache.ivy.core.module.descriptor.DependencyDescriptor;
 import org.apache.ivy.core.resolve.ResolveData;
 import org.apache.ivy.core.resolve.ResolvedModuleRevision;
+import org.apache.ivy.plugins.resolver.AbstractResolver;
 import org.apache.ivy.plugins.resolver.ChainResolver;
 import org.apache.ivy.plugins.resolver.DependencyResolver;
+import org.gradle.api.GradleException;
 
+import java.lang.reflect.Method;
 import java.text.ParseException;
 import java.util.List;
 
@@ -28,8 +31,21 @@ public class CacheFirstChainResolver extends ChainResolver {
     }
 
     private ResolvedModuleRevision findModuleInCache(DependencyResolver resolver, DependencyDescriptor dd, ResolveData resolveData) {
-        // TODO construct cache options relevant for resolver and resolveData (use details from AbstractResolver if appropriate)
-        return resolver.getRepositoryCacheManager().findModuleInCache(dd, dd.getDependencyRevisionId(), new CacheMetadataOptions(), resolver.getName());
+        CacheMetadataOptions cacheOptions = getCacheMetadataOptions(resolver, resolveData);
+        return resolver.getRepositoryCacheManager().findModuleInCache(dd, dd.getDependencyRevisionId(), cacheOptions, resolver.getName());
+    }
+    
+    private CacheMetadataOptions getCacheMetadataOptions(DependencyResolver resolver, ResolveData resolveData) {
+        if (resolver instanceof AbstractResolver) {
+            try {
+                Method method = AbstractResolver.class.getDeclaredMethod("getCacheOptions", ResolveData.class);
+                method.setAccessible(true);
+                return (CacheMetadataOptions) method.invoke(resolver, resolveData);
+            } catch (Exception e) {
+                throw new GradleException("Could not get cache options from AbstractResolver", e);
+            }
+        }
+        return new CacheMetadataOptions();
     }
 
     @Override
