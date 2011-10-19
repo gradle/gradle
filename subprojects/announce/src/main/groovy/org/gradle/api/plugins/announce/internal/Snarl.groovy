6c392fce5608768bf67d8f10228832fe1346ef51
Adam Murdoch <adam.murdoch@gradleware.com>
2011-10-19 19:19:31 +1100

    Handle case where snarl is not running.

--- subprojects/announce/src/main/groovy/org/gradle/api/plugins/announce/internal/Snarl.groovy
+++ subprojects/announce/src/main/groovy/org/gradle/api/plugins/announce/internal/Snarl.groovy
@@ -17,23 +17,28 @@
 package org.gradle.api.plugins.announce.internal
 
 import org.gradle.api.plugins.announce.Announcer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class Snarl implements Announcer {
     private static final float SNP_VERSION = 1.1f
     private static final String HEAD = "type=SNP#?version=" + SNP_VERSION
+    private static final Logger LOGGER = LoggerFactory.getLogger(Snarl)
 
     public void send(String title, String message) {
-        send("localhost", title, message)
+        send(InetAddress.getByName(null), title, message)
     }
 
-    public void send(Collection<String> hosts, String title, String message) {
-        hosts.each { host ->
-            send(host, title, message)
+    public void send(InetAddress host, String title, String message) {
+        Socket socket
+        try {
+            socket = new Socket(host, 9887)
+        } catch (ConnectException e) {
+            // Snarl is not running - ignore
+            LOGGER.debug("Snarl is not running on host $host. Discarding announcement with title: $title, message: $message")
+            return
         }
-    }
-
-    public void send(String host, String title, String message) {
-        with(new Socket(InetAddress.getByName(host), 9887)) { sock ->
+        with(socket) { sock ->
             with(new PrintWriter(sock.getOutputStream(), true)) { out ->
                 out.println(formatMessage(title, message))
             }
