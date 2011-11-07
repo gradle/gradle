df90cffe56a7828a2b80bd63af7150c10cb348da
Luke Daley <ld@ldaley.com>
2011-11-07 15:45:24 +0000

    Updated description of daemon communication protocol.

--- subprojects/launcher/src/main/java/org/gradle/launcher/daemon/client/DaemonClient.java
+++ subprojects/launcher/src/main/java/org/gradle/launcher/daemon/client/DaemonClient.java
@@ -28,20 +28,23 @@ import org.gradle.messaging.remote.internal.Connection;
 
 /**
  * The client piece of the build daemon.
- *
- * <p>Protocol is this:</p>
- *
- * <ol> <li>Client connects to the server.</li>
- *
- * <li>Client sends a {@link org.gradle.launcher.daemon.protocol.Command} message.</li>
- *
- * <li>Server sends zero or more {@link org.gradle.logging.internal.OutputEvent} messages. Note that the server may send output messages before it receives the command message. </li>
- *
- * <li>Server sends a {@link org.gradle.launcher.daemon.protocol.Result} message.</li>
- *
- * <li>Connection is closed.</li>
- *
- * </ol>
+ * <p>
+ * Immediately upon forming a connection, the daemon may send {@link OutputEvent} messages back to the client and may do so
+ * for as long as the connection is open.
+ * <p>
+ * The client is expected to send exactly one {@link Build} message as the first message it sends to the daemon. After this
+ * it may send zero to many {@link ForwardInput} messages. If the client's stdin stream is closed before the connection to the
+ * daemon is terminated, the client must send a {@link CloseInput} command to instruct that daemon that no more input is to be
+ * expected.
+ * <p>
+ * After receiving the {@link Build} message from the client, the daemon will at some time return a {@link Result} message
+ * indicating either that the daemon encountered an internal failure or that the build failed dependending on the specific
+ * type of the {@link Result} object returned.
+ * <p>
+ * After receiving the {@link Result} message, the client must send a {@link CloseInput} command if it has not already done so
+ * due the stdin stream being closed. At this point the client is expected to terminate the connection with the daemon.
+ * <p>
+ * If the daemon returns a {@code null} message before returning a {@link Result} object, it has terminated unexpectedly for some reason.
  */
 public class DaemonClient implements GradleLauncherActionExecuter<BuildActionParameters> {
     private static final Logger LOGGER = Logging.getLogger(DaemonClient.class);
