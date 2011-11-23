39aa212024e363233f02a4a9287bbf8084ccfcaa
Peter Niederwieser <pniederw@gmail.com>
2011-11-23 08:27:16 +0100

    fixed wrong comparison that led to failing tests

--- subprojects/native/src/main/java/org/gradle/os/MyFileSystem.java
+++ subprojects/native/src/main/java/org/gradle/os/MyFileSystem.java
@@ -194,7 +194,7 @@ public abstract class MyFileSystem {
             try {
                 link = generateUniqueTempFileName();
                 int returnCode = PosixUtil.current().symlink(file.getPath(), link.getPath());
-                return returnCode != 0 && hasContent(link, content);
+                return returnCode == 0 && hasContent(link, content);
             } catch (IOException e) {
                 LOGGER.warn("Failed to determine if file system can create symbolic links. Assuming it can't.");
                 return false;
