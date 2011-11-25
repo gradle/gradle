03c6912d43d8d692ae0501f395c65b3934f9dc9b
Luke Daley <ld@ldaley.com>
2011-11-25 16:23:49 +0000

    remove css link

--- website/src/php/roadmap.php
+++ website/src/php/roadmap.php
@@ -1,7 +1,6 @@
 <?
   require_once('php/simplepie.inc');
 ?>
-<link href="board/style.css" rel="stylesheet" type="text/css"/>
 <div id="status-board-container">
   <p>This page gives a very <em>high level</em> view of the current project roadmap and status and shouldn't be considered exhaustive. For a more fine grained view you can take a look at the <a href="http://issues.gradle.org/browse/GRADLE" title="Gradle">issue tracker</a>.</p>
   <p style="font-weight: bold">You can click on any item to read a corresponding forum post giving more context and rationale.</p>
