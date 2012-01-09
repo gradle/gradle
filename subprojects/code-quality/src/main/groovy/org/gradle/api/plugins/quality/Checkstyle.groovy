50201687ea4acc75f1d7801613238b1707ff33a7
Peter Niederwieser <pniederw@gmail.com>
2012-01-09 19:26:41 +0100

    reworked checkstyle plugin
    notably:
    - turn into its own plugin
    - introduce checkstyle configuration (don't hardcode the checkstyle version)
    - configure via extension object
    - improve tests and move them into code-quality project
    - implement in groovy

---
+++ subprojects/code-quality/src/main/groovy/org/gradle/api/plugins/quality/Checkstyle.groovy
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.plugins.quality
+
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.*
+import org.gradle.api.GradleException
+import org.gradle.util.DeprecationLogger
+
+/**
+ * Runs Checkstyle against some source files.
+ */
+class Checkstyle extends SourceTask implements VerificationTask {
+    /**
+     * The class path containing the Checkstyle library to be used.
+     */
+    @InputFiles
+    FileCollection checkstyleClassPath
+
+    /**
+     * The class path containing the compiled classes for the source files to be analyzed.
+     */
+    @InputFiles
+    FileCollection classpath
+
+    /**
+     * The Checkstyle configuration file to use.
+     */
+    @InputFile
+    File configFile
+
+    /**
+     * The properties available for use in the configuration file. These are substituted into the configuration
+     * file.
+     */
+    Map<String, Object> configProperties
+
+    /**
+     * The properties available for use in the configuration file. These are substituted into the configuration
+     * file.
+     * 
+     * @deprecated renamed to <tt>configProperties</tt>
+     */
+    @Deprecated
+    Map<String, Object> getProperties() {
+        DeprecationLogger.nagUserOfReplacedProperty("properties", "configProperties")
+        getConfigProperties()
+    }
+
+    /**
+     * The properties available for use in the configuration file. These are substituted into the configuration
+     * file.
+     *
+     * @deprecated renamed to <tt>configProperties</tt>
+     */
+    @Deprecated
+    void setProperties(Map<String, Object> properties) {
+        DeprecationLogger.nagUserOfReplacedProperty("properties", "configProperties")
+        configProperties = properties
+    }
+
+    /**
+     * The file in which the XML report will be saved.
+     */
+    @OutputFile
+    File reportFile
+
+    /**
+     * @deprecated renamed to <tt>reportFile</tt>
+     */
+    @Deprecated
+    File getResultFile() {
+        DeprecationLogger.nagUserOfReplacedProperty("resultFile", "reportFile")
+        getReportFile()
+    }
+
+    /**
+     * @deprecated renamed to <tt>reportFile</tt>
+     */
+    @Deprecated
+    void setResultFile(File file) {
+        DeprecationLogger.nagUserOfReplacedProperty("resultFile", "reportFile")
+        reportFile = file
+    }
+
+    /**
+     * Whether or not this task will ignore failures and continue running the build.
+     */
+    Boolean ignoreFailures
+
+    @TaskAction
+    public void run() {
+        def propertyName = "org.gradle.checkstyle.violations"
+
+        ant.taskdef(name: 'checkstyle', classname: 'com.puppycrawl.tools.checkstyle.CheckStyleTask', classpath: getCheckstyleClassPath().asPath)
+
+        ant.checkstyle(config: getConfigFile(), failOnViolation: false, failureProperty: propertyName) {
+            getSource().addToAntBuilder(ant, 'fileset', FileCollection.AntType.FileSet)
+            getClasspath().addToAntBuilder(ant, 'classpath')
+            formatter(type: 'plain', useFile: false)
+            formatter(type: 'xml', toFile: getReportFile())
+            getConfigProperties().each { key, value ->
+                property(key: key, value: value.toString())
+            }
+        }
+
+        if (!getIgnoreFailures() && ant.properties[propertyName]) {
+            throw new GradleException("Checkstyle rule violations were found. See the report at ${getReportFile()}.")
+        }
+    }
+}
---
+++ subprojects/code-quality/src/main/groovy/org/gradle/api/plugins/quality/CheckstylePlugin.groovy
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.plugins.quality
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.internal.Instantiator
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.plugins.JavaBasePlugin
+
+class CheckstylePlugin implements Plugin<Project> {
+    private Project project
+    private Instantiator instantiator
+    private CheckstyleExtension extension
+    
+    void apply(Project project) {
+        this.project = project
+        instantiator = project.services.get(Instantiator)
+
+        project.plugins.apply(JavaBasePlugin)
+
+        configureCheckstyleConfiguration()
+        configureCheckstyleExtension()
+        configureCheckstyleTasks()
+        configureCheckTask()
+    }
+    
+    private void configureCheckstyleConfiguration() {
+        project.configurations.add('checkstyle').with {
+            visible = false
+            transitive = true
+            description = 'The Checkstyle libraries to be used for this project.'
+        }
+    }
+
+    private void configureCheckstyleExtension() {
+        extension = instantiator.newInstance(CheckstyleExtension, project)
+        project.extensions.checkstyle = extension
+
+        extension.with {
+            toolVersion = "5.5"
+        }
+
+        extension.conventionMapping.with {
+            configFile = { project.file("config/checkstyle/checkstyle.xml") }
+            configProperties = { [:] }
+            reportsDir = { new File(project.reportsDir, "checkstyle") }
+        }
+    }
+
+    private void configureCheckstyleTasks() {
+        project.sourceSets.all { SourceSet sourceSet ->
+            def task = project.tasks.add(sourceSet.getTaskName('checkstyle', null), Checkstyle)
+            task.with {
+                description = "Run Checkstyle analysis for ${sourceSet.name} classes"
+                classpath = sourceSet.output
+            }
+            task.conventionMapping.with {
+                checkstyleClassPath = {
+                    def config = project.configurations['checkstyle']
+                    if (config.dependencies.empty) {
+                        project.dependencies {
+                            checkstyle "com.puppycrawl.tools:checkstyle:$extension.toolVersion"
+                        }
+                    }
+                    config
+                }
+                defaultSource = { sourceSet.allJava }
+                configFile = { extension.configFile }
+                configProperties = { extension.configProperties }
+                reportFile = { new File(extension.reportsDir, "${sourceSet.name}.xml") }
+                ignoreFailures = { extension.ignoreFailures }
+            }
+        }
+    }
+
+    private void configureCheckTask() {
+        project.tasks['check'].dependsOn { extension.sourceSets.collect { it.getTaskName('checkstyle', null) }}
+    }
+}
