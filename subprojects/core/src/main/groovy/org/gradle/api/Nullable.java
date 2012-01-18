50dc1b3aa10ac698d54809f063a3d063905254e0
Peter Niederwieser <pniederw@gmail.com>
2012-01-18 04:26:50 +0100

    added @Nullable annotation

---
+++ subprojects/core/src/main/groovy/org/gradle/api/Nullable.java
@@ -0,0 +1,10 @@
+package org.gradle.api;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Indicates that the value of an element can be null.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Nullable {}
