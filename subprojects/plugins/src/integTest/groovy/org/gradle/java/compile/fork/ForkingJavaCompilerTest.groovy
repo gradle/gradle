095fdfae2c7ef775c02feba671b44937c4069dfd
Peter Niederwieser <pniederw@gmail.com>
2012-01-19 00:26:10 +0100

    moved test into different package

--- subprojects/plugins/src/integTest/groovy/org/gradle/java/compile/fork/ForkingJavaCompilerTest.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/java/compile/fork/ForkingJavaCompilerTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gradle.java.compile
+package org.gradle.java.compile.fork
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.TestResources
