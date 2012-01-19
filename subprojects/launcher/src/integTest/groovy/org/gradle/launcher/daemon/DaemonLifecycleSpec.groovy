6994c3a2356bbc8cde10cb05ad2ccfdde18a9096
Szczepan Faber <szczepiq@gmail.com>
2012-01-19 20:57:23 +0100

    Added more coverage for daemon log output (duplicate entries, log levels, subsequent builds)

--- subprojects/launcher/src/integTest/groovy/org/gradle/launcher/daemon/DaemonLifecycleSpec.groovy
+++ subprojects/launcher/src/integTest/groovy/org/gradle/launcher/daemon/DaemonLifecycleSpec.groovy
@@ -430,31 +430,78 @@ assert System.getProperty('some-prop') == 'some-value'
     def "daemon log contains all necessary logging"() {
         given:
         def baseDir = distribution.file("daemonBaseDir").createDir()
-        assert baseDir.exists()
         executer.withDaemonBaseDir(baseDir)
-
         distribution.file("build.gradle") << "println 'Hello build!'"
-        
+
         when:
         executer.withArguments("-i").run()
 
         then:
+        def log = readSingleDaemonLog(baseDir)
+
+        //output before started relying logs via connection
+        log.count(DaemonMessages.PROCESS_STARTED) == 1
+        //output after started relying logs via connection
+        log.count(DaemonMessages.STARTED_RELAYING_LOGS) == 1
+        //output from the build
+        log.count('Hello build!') == 1
+
+        when: "another build requested with the same daemon"
+        executer.withArguments("-i").run()
+
+        then:
+        def aLog = readSingleDaemonLog(baseDir)
+
+        aLog.count(DaemonMessages.PROCESS_STARTED) == 1
+        aLog.count(DaemonMessages.STARTED_RELAYING_LOGS) == 2
+        aLog.count('Hello build!') == 2
+    }
+
+    def "daemon log honors log levels for logging"() {
+        given:
+        def baseDir = distribution.file("daemonBaseDir").createDir()
+        executer.withDaemonBaseDir(baseDir)
+        distribution.file("build.gradle") << """
+            println 'println me!'
+
+            logger.debug('debug me!')
+            logger.info('info me!')
+            logger.quiet('quiet me!')
+            logger.lifecycle('lifecycle me!')
+            logger.warn('warn me!')
+            logger.error('error me!')
+        """
+
+        when:
+        executer.run()
+
+        then:
+        def log = readSingleDaemonLog(baseDir)
+
+        //before the build is requested we don't know the log level so we print eagerly
+        log.count(DaemonMessages.PROCESS_STARTED) == 1
+        //after the build started log level is understood
+        log.count(DaemonMessages.STARTED_RELAYING_LOGS) == 0
+        //output from the build:
+        log.count('debug me!') == 0
+        log.count('info me!') == 0
+        log.count('println me!') == 1
+        log.count('quiet me!') == 1
+        log.count('lifecycle me!') == 1
+        log.count('warn me!') == 1
+        log.count('error me!') == 1
+    }
+
+    String readSingleDaemonLog(baseDir) {
         //the gradle version dir
         baseDir.listFiles().length == 1
         def daemonFiles = baseDir.listFiles()[0].listFiles()
 
-        //single build means single log
-        daemonFiles.count { it.name.endsWith('.log') } == 1
+        //assert single log
+        assert daemonFiles.count { it.name.endsWith('.log') } == 1
         def daemonLog = daemonFiles.find { it.name.endsWith('.log') }
 
-        def log = daemonLog.text
-
-        //output before started relying logs via connection
-        assert log.contains(DaemonMessages.PROCESS_STARTED)
-        //output after started relying logs via connection
-        assert log.contains(DaemonMessages.STARTED_RELAYING_LOGS) //TODO SF add coverage for log levels
-        //output from the build
-        assert log.contains('Hello build!')
+        daemonLog.text
     }
 
     def cleanup() {
