57e7dae3e371c8f38bccba4e31b186dd92f3e3dc
Szczepan Faber <szczepiq@gmail.com>
2012-01-31 18:28:25 +0100

    Improved the daemon client log message for the unexpected daemon result.

--- subprojects/launcher/src/main/java/org/gradle/launcher/daemon/client/DaemonClient.java
+++ subprojects/launcher/src/main/java/org/gradle/launcher/daemon/client/DaemonClient.java
@@ -124,7 +124,9 @@ public class DaemonClient implements GradleLauncherActionExecuter<BuildActionPar
             } else if (firstResult == null) {
                 LOGGER.warn("The first result from the daemon was empty. Most likely the daemon has died. Trying a different daemon...");
             } else {
-                throw new IllegalStateException(String.format("Daemon returned %s for which there is no strategy to handle (i.e. is an unknown Result type)", firstResult));
+                throw new IllegalStateException(String.format(
+                    "The first result from the Daemon: %s is a Result of a type we don't have a strategy to handle."
+                    + "Earlier, %s request was sent to the daemon.", firstResult, build));
             }
         }
     }
