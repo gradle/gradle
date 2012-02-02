682c70f04b858819cbe36a556307887e6c2435d4
Luke Daley <ld@ldaley.com>
2012-02-02 12:18:51 +0100

    replace package-info.groovy with package-info.java

--- subprojects/code-quality/src/main/groovy/org/gradle/api/plugins/quality/package-info.java
+++ subprojects/code-quality/src/main/groovy/org/gradle/api/plugins/quality/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 the original author or authors.
+ * Copyright 2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,6 @@
  */
 
 /**
- * Class for the code quality tasks
+ * Plugins which measure and enforce code quality.
  */
 package org.gradle.api.plugins.quality;
