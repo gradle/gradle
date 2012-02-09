7159511235a3b47da3c4c91b815db67bfb84a902
Szczepan Faber <szczepiq@gmail.com>
2012-02-09 20:54:34 +0100

    Added some initial stuff for the performance test

---
+++ performanceTest/src/test/groovy/org/gradle/peformance/PerformanceTest.groovy
@@ -0,0 +1,14 @@
+package org.gradle.peformance
+
+import spock.lang.Specification
+
+/**
+ * by Szczepan Faber, created at: 2/9/12
+ */
+class PerformanceTest extends Specification {
+
+    def "foo"() {
+        expect:
+        1==1
+    }
+}
