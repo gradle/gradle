530ee28946909b86ee4f4f50dc7c8ae24d966744
Peter Niederwieser <pniederw@gmail.com>
2012-02-20 21:30:51 +0100

    updated released version

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/ReleasedVersions.java
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/ReleasedVersions.java
@@ -43,7 +43,8 @@ public class ReleasedVersions {
                 "1.0-milestone-5",
                 "1.0-milestone-6",
                 "1.0-milestone-7",
-                "1.0-milestone-8");
+                "1.0-milestone-8",
+                "1.0-milestone-8a");
 
     private final GradleDistribution current;
 
