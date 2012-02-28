1d4eb33171318c2baf69d5e039d6b79d40080013
Luke Daley <ld@ldaley.com>
2012-02-28 21:51:56 +0000

    Revert "GRADLE-2132 - don't let default empty map and collection properties override convention mappings"
    This reverts commit 5af48398fb3e8c649aafc956dd81fa26dbabc28b.
    
    At the least, the Eclipse plugin is relying on this behaviour. It uses it to invalidate convention mappings through additive methods that mutate the real value instead of the convention mapping value. This means that once the first additive method is called, the convention mapping value is no longer used.
    
    After discussion, it was resolved that this behaviour needs to change sometime in the future. However, it's a larger piece of work that requires adding some more sophistication to convention mappings and class enhancement so it is being deferred. GRADLE-2132 will be reopened.

--- subprojects/core/src/main/groovy/org/gradle/api/internal/ConventionAwareHelper.java
+++ subprojects/core/src/main/groovy/org/gradle/api/internal/ConventionAwareHelper.java
@@ -25,6 +25,7 @@ import org.gradle.api.tasks.ConventionValue;
 import org.gradle.internal.UncheckedException;
 import org.gradle.util.ReflectionUtil;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -112,11 +113,23 @@ public class ConventionAwareHelper implements ConventionMapping, HasConvention {
     }
 
     public <T> T getConventionValue(T actualValue, String propertyName, boolean isExplicitValue) {
-        if (isExplicitValue || !conventionMapping.containsKey(propertyName)) {
+        if (isExplicitValue) {
             return actualValue;
-        } else {
-            return (T) conventionMapping.get(propertyName).getValue(convention, source);
         }
+
+        Object returnValue = actualValue;
+        if (conventionMapping.keySet().contains(propertyName)) {
+            boolean useMapping = true;
+            if (actualValue instanceof Collection && !((Collection<?>) actualValue).isEmpty()) {
+                useMapping = false;
+            } else if (actualValue instanceof Map && !((Map<?, ?>) actualValue).isEmpty()) {
+                useMapping = false;
+            }
+            if (useMapping) {
+                returnValue = conventionMapping.get(propertyName).getValue(convention, source);
+            }
+        }
+        return (T) returnValue;
     }
 
     public Convention getConvention() {
