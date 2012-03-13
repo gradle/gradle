231a202107f61116fe05db53a46384625a7b7ff9
Szczepan Faber <szczepiq@gmail.com>
2012-03-13 15:24:57 +0100

    Automated the released versions information that is needed for the compatibility tests. We now acquire the information at build time from the our services. We use that information when running compatibility tests (tooling api + cross version compatibility test). Details:
    -Now there is no need to update the tests after the full release or the RC release. However, it also means that after full release or RC release the compatibility tests run against different versions. We'll see how it works out.
    -When we are in RC mode, that is the RC has not yet been released, the RC plays as a regular version for the purposes of compatiblity tests.
    -After releasing the RC, we use the released version.
    -We exclude ceratain versions that we no longer want to cover or if they are less interesting.
    -I've added the milestone-4 as excluded from tests because this release is considered broken anyway.

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/ReleasedVersions.java
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/ReleasedVersions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 the original author or authors.
+ * Copyright 2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,10 +18,10 @@ package org.gradle.integtests.fixtures;
 
 import com.google.common.collect.Lists;
 import org.gradle.api.Transformer;
+import org.gradle.integtests.fixtures.versions.VersionsInfo;
 import org.gradle.util.CollectionUtils;
 import org.gradle.util.GradleVersion;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -29,22 +29,7 @@ import java.util.List;
  */
 public class ReleasedVersions {
 
-    //TODO SF - it should feed from releases.xml
-    private final static List<String> RELEASED = Arrays.asList(
-                "0.8",
-                "0.9-rc-3",
-                "0.9",
-                "0.9.1",
-                "0.9.2",
-                "1.0-milestone-1",
-                "1.0-milestone-2",
-                "1.0-milestone-3",
-                "1.0-milestone-4",
-                "1.0-milestone-5",
-                "1.0-milestone-6",
-                "1.0-milestone-7",
-                "1.0-milestone-8",
-                "1.0-milestone-8a");
+    private final static List<String> RELEASED = Lists.reverse(new VersionsInfo().getVersions());
 
     private final GradleDistribution current;
 
