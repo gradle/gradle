ca813d02261f1f24ec6fc51549fab9892dd03d9d
daz <daz@bigdaz.com>
2012-03-22 11:05:15 -0600

    Create a 'classpath-only' jar file for including application classes in the system classloader for a worker process

--- subprojects/core/src/main/groovy/org/gradle/process/internal/child/ApplicationClassesInSystemClassLoaderWorkerFactory.java
+++ subprojects/core/src/main/groovy/org/gradle/process/internal/child/ApplicationClassesInSystemClassLoaderWorkerFactory.java
@@ -16,6 +16,7 @@
 
 package org.gradle.process.internal.child;
 
+import org.gradle.api.UncheckedIOException;
 import org.gradle.api.internal.ClassPathRegistry;
 import org.gradle.messaging.remote.Address;
 import org.gradle.process.internal.WorkerProcessBuilder;
@@ -23,11 +24,16 @@ import org.gradle.process.internal.launcher.BootstrapClassLoaderWorker;
 import org.gradle.util.GUtil;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.jar.Attributes;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
 
 /**
  * A factory for a worker process which loads the application classes using the JVM's system ClassLoader.
@@ -50,7 +56,6 @@ import java.util.concurrent.Callable;
  *                          implementation
  *         (ActionExecutionWorker + worker action implementation)
  * </pre>
- *
  */
 public class ApplicationClassesInSystemClassLoaderWorkerFactory implements WorkerFactory {
     private final Object workerId;
@@ -59,22 +64,35 @@ public class ApplicationClassesInSystemClassLoaderWorkerFactory implements Worke
     private final List<URL> implementationClassPath;
     private final Address serverAddress;
     private final ClassPathRegistry classPathRegistry;
+    private final File classpathJarFile;
 
     public ApplicationClassesInSystemClassLoaderWorkerFactory(Object workerId, String displayName, WorkerProcessBuilder processBuilder,
-                                          List<URL> implementationClassPath, Address serverAddress,
-                                          ClassPathRegistry classPathRegistry) {
+                                                              List<URL> implementationClassPath, Address serverAddress,
+                                                              ClassPathRegistry classPathRegistry) {
         this.workerId = workerId;
         this.displayName = displayName;
         this.processBuilder = processBuilder;
         this.implementationClassPath = implementationClassPath;
         this.serverAddress = serverAddress;
         this.classPathRegistry = classPathRegistry;
+        classpathJarFile = createClasspathJarFile(processBuilder);
+    }
+
+    private File createClasspathJarFile(WorkerProcessBuilder processBuilder) {
+        try {
+            File classpathJarFile = File.createTempFile("GradleWorkerProcess", "classpath.jar");
+            new ClasspathJarFactory().createClasspathJarFile(classpathJarFile, processBuilder.getApplicationClasspath());
+            classpathJarFile.deleteOnExit();
+            return classpathJarFile;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     public Collection<File> getSystemClasspath() {
         List<File> systemClasspath = new ArrayList<File>();
         systemClasspath.addAll(classPathRegistry.getClassPath("WORKER_MAIN").getAsFiles());
-        systemClasspath.addAll(processBuilder.getApplicationClasspath());
+        systemClasspath.add(classpathJarFile);
         return systemClasspath;
     }
 
@@ -87,4 +105,25 @@ public class ApplicationClassesInSystemClassLoaderWorkerFactory implements Worke
 
         return new BootstrapClassLoaderWorker(classPathRegistry.getClassPath("WORKER_PROCESS").getAsURLs(), processBuilder.getApplicationClasspath(), serializedWorker);
     }
+
+    /**
+     * Creates an empty jar file that contains a manifest with a Class-path entry that will load the supplied classpath.
+     * This can be used to circumvent command-line length limit on windows when the classpath is very long.
+     * Note that the main class must be placed on the classpath explicitly, and cannot be loaded via a classpath jar.
+     */
+    private static class ClasspathJarFactory {
+        public void createClasspathJarFile(File file, Collection<File> classpath) throws IOException {
+            Manifest manifest = new Manifest();
+            Attributes attributes = manifest.getMainAttributes();
+            attributes.put(Attributes.Name.MANIFEST_VERSION, "1.0");
+            attributes.putValue("Class-Path", GUtil.join(classpath, " "));
+            writeManifestOnlyJarFile(file, manifest);
+        }
+
+        private void writeManifestOnlyJarFile(File file, Manifest manifest) throws IOException {
+            JarOutputStream jarOutputStream = new JarOutputStream(new FileOutputStream(file), manifest);
+            jarOutputStream.close();
+        }
+    }
+
 }
