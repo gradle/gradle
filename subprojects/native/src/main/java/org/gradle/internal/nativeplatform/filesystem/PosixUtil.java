ec41b8b472016dba3c3ff6196fecf1c779cf376b
Rene Groeschke <rene@breskeby.com>
2012-03-29 18:22:47 +0200

    GRADLE-2149: Replace PosixWrapper usage by FallbackPOSIX & FallbackFileStat - FallbackPOSIX used on Windows- and unknown- platform - FallbackPOSIX#chmod() returns 0. - FallbackPOSIX#symlink() returns 0. - FallbackPOSIX#stat() returns FallbackFileStat - Other FallbackPOSIX methods return UnsupportedOperationException - FallbackFileStat#mode() returns FileSystem.DEFAULT_DIR_MODE for directories - FallbackFileStat#mode() returns FileSystem.DEFAULT_FILE_MODE for files - Other FallbackFileStat methods return UnsupportedOperationException

--- subprojects/native/src/main/java/org/gradle/internal/nativeplatform/filesystem/PosixUtil.java
+++ subprojects/native/src/main/java/org/gradle/internal/nativeplatform/filesystem/PosixUtil.java
@@ -16,19 +16,27 @@
 
 package org.gradle.internal.nativeplatform.filesystem;
 
-import org.jruby.ext.posix.POSIX;
-import org.jruby.ext.posix.POSIXFactory;
-import org.jruby.ext.posix.POSIXHandler;
+import org.jruby.ext.posix.*;
 
 import java.io.File;
 import java.io.InputStream;
 import java.io.PrintStream;
 
 public class PosixUtil {
-    private static final POSIX POSIX = PosixWrapper.wrap(POSIXFactory.getPOSIX(new POSIXHandlerImpl(), true));
+    private static final POSIX POSIX = FallbackAwarePosixFactory.getPOSIX();
 
     public static POSIX current() {
         return POSIX;
+    }
+
+    private static class FallbackAwarePosixFactory{
+        public static POSIX getPOSIX() {
+            POSIX posix = POSIXFactory.getPOSIX(new POSIXHandlerImpl(), true);
+            if(posix instanceof JavaPOSIX || posix instanceof WindowsPOSIX){
+                return new FallbackPOSIX();
+            }
+            return posix;
+        }
     }
 
     private static class POSIXHandlerImpl implements POSIXHandler {
