39a6eb3347106c6b61f83831dc7f3b8d7233efa5
Rene Groeschke <rene@breskeby.com>
2012-06-15 15:04:07 +0200

    Revert "Bumped the wrapper to use more recent nightly." as our nightly (uploadArchives) does not work anymore.
    This reverts commit c40a6ff2ef02b263b75454d28a86f899129d2efc.

--- subprojects/core/src/test/groovy/org/gradle/process/internal/child/BootstrapSecurityManagerTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/process/internal/child/BootstrapSecurityManagerTest.groovy
@@ -78,9 +78,9 @@ class BootstrapSecurityManagerTest extends Specification {
         System.securityManager instanceof TestSecurityManager
     }
 
-    InputStream createStdInContent(File... classpath) {
+    def createStdInContent(File... classpath) {
         def out = new ByteArrayOutputStream()
-        def dataOut = new DataOutputStream(new EncodedStream.EncodedOutput(out))
+        def dataOut = new DataOutputStream(out)
         dataOut.writeInt(classpath.length)
         classpath.each { dataOut.writeUTF(it.absolutePath) }
         return new ByteArrayInputStream(out.toByteArray())
