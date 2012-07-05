c4c8878b02b7f61d519b387ef16f33de700aa45f
Rene Groeschke <rene@breskeby.com>
2012-07-05 23:51:11 +0200

    GRADLE-2238: refactor jarjar task draft to use the Main method instead of using api directly. - needed because of https://code.google.com/p/jarjar/issues/detail?id=54&sort=-id - todo: current implementation does not yet handle multiple input jars. we need our own ueberjar mechanism here since Main does not support multiple input jars atm

--- buildSrc/src/main/groovy/org/gradle/build/JarJar.groovy
+++ buildSrc/src/main/groovy/org/gradle/build/JarJar.groovy
@@ -13,57 +13,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.gradle.build
 
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.OutputFile
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.file.FileCollection
-import com.tonicsystems.jarjar.JarJarTask as JarJarAntTask
-import com.tonicsystems.jarjar.Rule;
-import com.tonicsystems.jarjar.Keep;
+import com.tonicsystems.jarjar.Main as JarJarMain
 
-import org.apache.tools.ant.Project;
-import org.apache.tools.ant.types.ZipFileSet
+import com.tonicsystems.jarjar.Keep
+import com.tonicsystems.jarjar.Rule
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.file.FileCollection
+import org.gradle.api.internal.file.TemporaryFileProvider
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
 
 class JarJar extends DefaultTask {
     @InputFiles FileCollection inputFiles
-    @OutputFile outputFile
+    @OutputFile File outputFile
 
-    def rules = [] //todo: mark as input property
-    def keeps = [] //todo: mark as input property
+    def patterns = [] //todo mark these patterns as task input
 
     @TaskAction
     void nativeJarJar() {
-        JarJarAntTask jjTask = new JarJarAntTask();
-        jjTask.setProject(new Project());
-        jjTask.setDestFile(new File("jaroutput.jar"));
-
-        addInputLibs(jjTask)
-
-        rules.each { rule ->
-            jjTask.addConfiguredRule(rule);
-        }
-        rules.each { keep ->
-            jjTask.addConfiguredRule(keep);
-        }
-        jjTask.execute();
-    }
-
-    private void addInputLibs(JarJarAntTask jjTask) {
-        inputFiles.each { inputFile ->
-            ZipFileSet zipFileSet = new ZipFileSet();
-            zipFileSet.setSrc(inputFile);
-            jjTask.addZipfileset(zipFileSet);
+        try {
+            File tempRuleFile = createRuleFile()
+            JarJarMain.main("process", tempRuleFile .absolutePath, inputFiles.getSingleFile().absolutePath, outputFile.absolutePath)
+        } catch (IOException e) {
+            throw new GradleException("unable to run jarjar task", e);
         }
     }
 
-    void addRule(String pattern, String result) {
-        rules.add(new Rule(pattern: pattern, result: result))
+    private File createRuleFile() {
+        TemporaryFileProvider tmpFileProvider = getServices().get(TemporaryFileProvider);
+        File tempRuleFile = tmpFileProvider.createTemporaryFile("jarjar", "jar")
+        tempRuleFile.withPrintWriter { writer ->
+            patterns.findAll {it instanceof Rule}.each {rule ->
+                writer.println("rule ${rule.pattern} ${rule.result}")
+            }
+        }
+
+        return tempRuleFile;
+    }
+
+    void rule(String pattern, String result) {
+        patterns << new Rule(pattern: pattern, result: result)
     }
 
     void keep(String pattern) {
-        this.keeps << new Keep(pattern: pattern)
+        patterns << new Keep(pattern: pattern)
     }
+
 }
