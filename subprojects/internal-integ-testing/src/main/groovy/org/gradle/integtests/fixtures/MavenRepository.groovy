4716f0cb4b49f15a3fc542b7653620c4116db342
Peter Niederwieser <pniederw@gmail.com>
2012-07-10 23:44:29 +0200

    improved explanation of maven pom generation/customization in user guide
    - some more integration tests around maven pom generation

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/MavenRepository.groovy
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/MavenRepository.groovy
@@ -326,10 +326,20 @@ class MavenModule {
 }
 
 class MavenPom {
+    String groupId
+    String artifactId
+    String version
+    String packaging
     final Map<String, MavenScope> scopes = [:]
 
     MavenPom(File pomFile) {
         def pom = new XmlParser().parse(pomFile)
+
+        groupId = pom.groupId[0]?.text()
+        artifactId = pom.artifactId[0]?.text()
+        version = pom.version[0]?.text()
+        packaging = pom.packaging[0]?.text()
+
         pom.dependencies.dependency.each { dep ->
             def scopeElement = dep.scope
             def scopeName = scopeElement ? scopeElement.text() : "runtime"
