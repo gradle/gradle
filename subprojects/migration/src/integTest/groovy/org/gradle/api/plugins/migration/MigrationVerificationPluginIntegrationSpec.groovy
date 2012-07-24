b16078022a640b1498f01406b819d37924d6e09f
Peter Niederwieser <pniederw@gmail.com>
2012-07-24 09:06:53 +0200

    do not check CRCs in spec but tolerate differences

--- subprojects/migration/src/integTest/groovy/org/gradle/api/plugins/migration/MigrationVerificationPluginIntegrationSpec.groovy
+++ subprojects/migration/src/integTest/groovy/org/gradle/api/plugins/migration/MigrationVerificationPluginIntegrationSpec.groovy
@@ -19,9 +19,7 @@ package org.gradle.api.plugins.migration
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.TestResources
 import org.junit.Rule
-import spock.lang.Ignore
 
-@Ignore
 class MigrationVerificationPluginIntegrationSpec extends AbstractIntegrationSpec {
     @Rule TestResources testResources
 
@@ -32,17 +30,17 @@ class MigrationVerificationPluginIntegrationSpec extends AbstractIntegrationSpec
         run("compare")
 
         then:
-        output.contains("""
+        output =~ """
 Comparing build 'target build' with 'source build'
 Comparing outputs of project ':'
 Comparing archive 'testBuild.jar'
 Archive entry 'org/gradle/ChangedClass.class' has different sizes: 409 vs. 486
-Archive entry 'org/gradle/DifferentCrcClass.class' has different CRCs: 4201388239 vs. 137896582
+Archive entry 'org/gradle/DifferentCrcClass.class' has different CRCs: \\d+ vs. \\d+
 Archive entry 'org/gradle/SourceBuildOnlyClass.class' only exists in build 'source build'
 Archive entry 'org/gradle/TargetBuildOnlyClass.class' only exists in build 'target build'
 Finished comparing archive 'testBuild.jar'
 Finished comparing outputs of project ':'
 Finished comparing build 'target build' with 'source build'
-        """.trim())
+        """.trim()
     }
 }
