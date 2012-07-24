68650ee288a83a6a9570be6ee6471f4e40e50fd8
Peter Niederwieser <pniederw@gmail.com>
2012-07-24 19:35:19 +0200

    extracted class ArchiveEntriesComparator from ArchivesComparator

--- subprojects/migration/src/main/groovy/org/gradle/api/plugins/migration/internal/ArchivesComparator.groovy
+++ subprojects/migration/src/main/groovy/org/gradle/api/plugins/migration/internal/ArchivesComparator.groovy
@@ -20,9 +20,6 @@ import org.gradle.tooling.model.internal.migration.Archive
 
 import com.google.common.collect.Sets
 
-import java.util.zip.ZipEntry
-import java.util.zip.ZipInputStream
-
 class ArchivesComparator {
     private final ProjectComparison projectComparison
     private final BuildComparisonListener listener
@@ -52,16 +49,7 @@ class ArchivesComparator {
             projectComparison.archiveComparisons << archiveComparison
 
             listener.archiveComparisonStarted(archiveComparison)
-
-            def archiveEntriesByPath1 = getArchiveEntriesByPath(archive1)
-            def archiveEntriesByPath2 = getArchiveEntriesByPath(archive2)
-            archiveComparison.archive1.numberOfEntries = archiveEntriesByPath1.size()
-            archiveComparison.archive2.numberOfEntries = archiveEntriesByPath2.size()
-
-            handleCommonArchiveEntries(archiveComparison, archiveEntriesByPath1, archiveEntriesByPath2)
-            handleOrphanArchiveEntries(archiveComparison, archiveComparison.archive1, archiveEntriesByPath1, archiveEntriesByPath2)
-            handleOrphanArchiveEntries(archiveComparison, archiveComparison.archive2, archiveEntriesByPath2, archiveEntriesByPath1)
-
+            new ArchiveEntriesComparator(archiveComparison, listener).compareEntries()
             listener.archiveComparisonFinished(archiveComparison)
         }
     }
@@ -74,46 +62,5 @@ class ArchivesComparator {
             projectComparison.orphanArchives << comparedArchive
             listener.orphanArchiveFound(comparedArchive)
         }
-    }
-
-    private void handleCommonArchiveEntries(ArchiveComparison archiveComparison, Map<String, ZipEntry> archiveEntriesByPath1, Map<String, ZipEntry> archiveEntriesByPath2) {
-        def entryComparator = new ZipEntryComparator()
-        def commonEntries = Sets.newTreeSet(Sets.intersection(archiveEntriesByPath1.keySet(), archiveEntriesByPath2.keySet()))
-        for (entryName in commonEntries) {
-            def entry1 = archiveEntriesByPath1[entryName]
-            def entry2 = archiveEntriesByPath2[entryName]
-            if (entryComparator.compare(entry1, entry2)) {
-                def entryDifference = new ArchiveEntryComparison(parent: archiveComparison)
-                entryDifference.entry1 = new ComparedArchiveEntry(parent: archiveComparison.archive1, path: entry1.name, directory: entry1.directory, size: entry1.size, crc: entry1.crc)
-                entryDifference.entry2 = new ComparedArchiveEntry(parent: archiveComparison.archive2, path: entry2.name, directory: entry2.directory, size: entry2.size, crc: entry2.crc)
-                archiveComparison.entryDifferences << entryDifference
-                listener.archiveEntryDifferenceFound(entryDifference)
-            }
-        }
-    }
-
-    private void handleOrphanArchiveEntries(ArchiveComparison archiveComparison, ComparedArchive archive, Map<String, ZipEntry> archiveEntries, Map<String, ZipEntry> otherArchiveEntries) {
-        def orphanEntryNames1 = Sets.newTreeSet(Sets.difference(archiveEntries.keySet(), otherArchiveEntries.keySet()))
-        for (entryName in orphanEntryNames1) {
-            def entry = archiveEntries[entryName]
-            def comparedEntry = new ComparedArchiveEntry(parent: archive, path: entry.name, directory: entry.directory, size: entry.size, crc: entry.crc)
-            archiveComparison.orphanEntries << comparedEntry
-            listener.orphanArchiveEntryFound(comparedEntry)
-        }
-    }
-
-    private Map<String, ZipEntry> getArchiveEntriesByPath(Archive archive) {
-        def result = [:]
-
-        archive.file.withInputStream { stream ->
-            ZipInputStream zipStream = new ZipInputStream(stream)
-            def entry = zipStream.nextEntry
-            while (entry != null) {
-                result.put(entry.name, entry)
-                entry = zipStream.nextEntry
-            }
-        }
-
-        result
     }
 }
