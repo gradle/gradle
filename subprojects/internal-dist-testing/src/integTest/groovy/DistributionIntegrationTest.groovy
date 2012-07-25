63c8a913717c2110060d4c4c474cdf24b279feeb
Luke Daley <ld@ldaley.com>
2012-07-25 13:08:32 +0100

    Move the distribution tests out into a separate project.
    The idea is to make it easier to reuse these for the promotion builds.

--- subprojects/internal-dist-testing/src/integTest/groovy/DistributionIntegrationTest.groovy
+++ subprojects/internal-dist-testing/src/integTest/groovy/DistributionIntegrationTest.groovy
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gradle.integtests
-
 import org.apache.tools.ant.taskdefs.Expand
 import org.gradle.integtests.fixtures.GradleDistribution
 import org.gradle.integtests.fixtures.GradleDistributionExecuter
