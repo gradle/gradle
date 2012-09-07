b02870f47d11f174fb671058a90bb011b79fa0d6
Peter Niederwieser <pniederw@gmail.com>
2012-09-08 00:38:13 +0200

    fixed GRADLE-2460: Scala compiler is unable to find a javac compiler under Mac OS with Java 1.7, gradle 1.1
    - really a fix for the underlying issue that system properties like os.name and os.version were carried over from client JVM to daemon JVM, which would cause troubles if they weren't using the same JDK

--- subprojects/base-services/src/main/java/org/gradle/internal/SystemProperties.java
+++ subprojects/base-services/src/main/java/org/gradle/internal/SystemProperties.java
@@ -15,12 +15,42 @@
  */
 package org.gradle.internal;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * Provides access to frequently used system properties.
  */
 public class SystemProperties {
+    private static final Set<String> STANDARD_PROPERTIES = new HashSet<String>(Arrays.asList(
+            "java.version",
+            "java.vendor",
+            "java.vendor.url",
+            "java.home",
+            "java.vm.specification.version",
+            "java.vm.specification.vendor",
+            "java.vm.specification.name",
+            "java.vm.version",
+            "java.vm.vendor",
+            "java.vm.name",
+            "java.specification.version",
+            "java.specification.vendor",
+            "java.specification.name",
+            "java.class.version",
+            "java.class.path",
+            "java.library.path",
+            "java.io.tmpdir",
+            "java.compiler",
+            "java.ext.dirs",
+            "os.name",
+            "os.arch",
+            "os.version",
+            "file.separator",
+            "path.separator",
+            "line.separator",
+            "user.name",
+            "user.home",
+            "user.dir"));
+
     @SuppressWarnings("unchecked")
     public static Map<String, String> asMap() {
         return (Map) System.getProperties();
@@ -40,5 +70,13 @@ public class SystemProperties {
 
     public static String getJavaVersion() {
         return System.getProperty("java.version");
+    }
+
+    /**
+     * Returns the keys that are guaranteed to be contained in System.getProperties() by default,
+     * as specified in the Javadoc for that method.
+     */
+    public static Set<String> getStandardProperties() {
+        return STANDARD_PROPERTIES;
     }
 }
