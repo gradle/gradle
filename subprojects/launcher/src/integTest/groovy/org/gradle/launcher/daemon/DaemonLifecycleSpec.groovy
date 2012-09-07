f27b1856565bf3eb610f9aa188bb00426f7a5d1f
Szczepan Faber <szczepiq@gmail.com>
2012-09-07 18:20:49 +0200

    Updated the spec according to the new daemon behavior.

--- subprojects/launcher/src/integTest/groovy/org/gradle/launcher/daemon/DaemonLifecycleSpec.groovy
+++ subprojects/launcher/src/integTest/groovy/org/gradle/launcher/daemon/DaemonLifecycleSpec.groovy
@@ -18,12 +18,13 @@ package org.gradle.launcher.daemon
 
 import org.gradle.integtests.fixtures.AvailableJavaHomes
 import org.gradle.integtests.fixtures.GradleHandle
+import org.gradle.internal.jvm.Jvm
 import org.gradle.internal.os.OperatingSystem
 import org.gradle.launcher.daemon.client.DaemonDisappearedException
 import org.gradle.launcher.daemon.testing.DaemonContextParser
 import org.gradle.launcher.daemon.testing.DaemonEventSequenceBuilder
-import org.gradle.internal.jvm.Jvm
 import spock.lang.IgnoreIf
+
 import static org.gradle.tests.fixtures.ConcurrentTestUtil.poll
 
 /**
@@ -407,10 +408,8 @@ class DaemonLifecycleSpec extends DaemonIntegrationSpec {
         buildFailedWithDaemonDisappearedMessage()
 
         and:
-        // The daemon crashed so didn't remove itself from the registry.
-        // This doesn't produce a registry state change, so we have to test
-        // That we are still in the same state this way
-        run { assert executer.daemonRegistry.busy.size() == 1; }
+        // The daemon attempts to remove its address on shutdown
+        run { assert executer.daemonRegistry.all.empty }
     }
 
     @IgnoreIf({ AvailableJavaHomes.bestAlternative == null})
