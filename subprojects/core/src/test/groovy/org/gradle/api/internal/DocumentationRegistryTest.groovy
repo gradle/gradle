0b30f68f246c86c91a0b780bdca227e8385c488f
Luke Daley <ld@ldaley.com>
2012-09-18 13:38:27 +0100

    Use versioned documentation links instead of just linking to current.

--- subprojects/core/src/test/groovy/org/gradle/api/internal/DocumentationRegistryTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/DocumentationRegistryTest.groovy
@@ -19,11 +19,14 @@ package org.gradle.api.internal
 import spock.lang.Specification
 import org.junit.Rule
 import org.gradle.util.TemporaryFolder
+import org.gradle.util.GradleVersion
 
 class DocumentationRegistryTest extends Specification {
     @Rule TemporaryFolder tmpDir
     final GradleDistributionLocator locator = Mock()
-    final DocumentationRegistry registry = new DocumentationRegistry(locator)
+    final GradleVersion gradleVersion = GradleVersion.current()
+    final DocumentationRegistry registry = new DocumentationRegistry(locator, gradleVersion)
+
 
     def "points users at the local user guide when target page is present in distribution"() {
         def distDir = tmpDir.createDir("home")
@@ -60,7 +63,7 @@ class DocumentationRegistryTest extends Specification {
         _ * locator.gradleHome >> distDir
 
         expect:
-        registry.getDocumentationFor('gradle_daemon') == "http://gradle.org/docs/current/userguide/gradle_daemon.html"
+        registry.getDocumentationFor('gradle_daemon') == "http://gradle.org/docs/${gradleVersion.version}/userguide/gradle_daemon.html"
     }
 
     def "points users at the remote user guide when no distribution"() {
@@ -68,6 +71,6 @@ class DocumentationRegistryTest extends Specification {
         _ * locator.gradleHome >> null
 
         expect:
-        registry.getDocumentationFor('gradle_daemon') == "http://gradle.org/docs/current/userguide/gradle_daemon.html"
+        registry.getDocumentationFor('gradle_daemon') == "http://gradle.org/docs/${gradleVersion.version}/userguide/gradle_daemon.html"
     }
 }
