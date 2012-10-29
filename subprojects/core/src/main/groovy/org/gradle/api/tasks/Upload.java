43729964841a18de73c027b0d9267711dc4c1281
Luke Daley <ld@ldaley.com>
2012-10-29 16:55:23 +0000

    Remove the ability to modify descriptors from the Upload task (which was never in an actual release).

--- subprojects/core/src/main/groovy/org/gradle/api/tasks/Upload.java
+++ subprojects/core/src/main/groovy/org/gradle/api/tasks/Upload.java
@@ -17,8 +17,6 @@
 package org.gradle.api.tasks;
 
 import groovy.lang.Closure;
-import org.gradle.api.Action;
-import org.gradle.api.XmlProvider;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.file.FileCollection;
@@ -45,7 +43,6 @@ public class Upload extends ConventionTask {
     private boolean uploadDescriptor;
 
     private File descriptorDestination;
-    private Action<XmlProvider> descriptorModification;
 
     /**
      * The resolvers to delegate the uploads to. Usually a resolver corresponds to a repository.
@@ -67,7 +64,7 @@ public class Upload extends ConventionTask {
         artifactPublisher.publish(
                 configurationInternal.getModule(), configurationInternal,
                 isUploadDescriptor() ? getDescriptorDestination() : null,
-                isUploadDescriptor() ? getDescriptorModification() : null
+                null
         );
     }
 
@@ -137,29 +134,4 @@ public class Upload extends ConventionTask {
         this.artifactPublisher = artifactPublisher;
     }
 
-    /**
-     * The current descriptor modification action.
-     *
-     * @return The current descriptor modifcation action. Or null.
-     */
-    public Action<XmlProvider> getDescriptorModification() {
-        return descriptorModification;
-    }
-
-    /**
-     * Use to modify the descriptor before publication.
-     *
-     * <pre>
-     * uploadTask {
-     *     setDescriptorModification { XmlProvider descriptor ->
-     *         …
-     *     }
-     * }
-     * </pre>
-     *
-     * @param descriptorModification The modification action
-     */
-    public void setDescriptorModification(Action<XmlProvider> descriptorModification) {
-        this.descriptorModification = descriptorModification;
-    }
 }
