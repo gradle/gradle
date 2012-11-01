10a5be4ace3feedf61ca7276a4e74cac4da00bcf
Rene Groeschke <rene@breskeby.com>
2012-11-01 20:44:41 +0100

    Fix beginners mistake / checkstyle issue

--- subprojects/plugins/src/main/groovy/org/gradle/external/javadoc/internal/JavadocOptionFileWriter.java
+++ subprojects/plugins/src/main/groovy/org/gradle/external/javadoc/internal/JavadocOptionFileWriter.java
@@ -54,7 +54,7 @@ public class JavadocOptionFileWriter {
 
             final Map<String, JavadocOptionFileOption> optionsWithoutLocale = CollectionUtils.filter(options, new Spec<Map.Entry<String, JavadocOptionFileOption>>() {
                 public boolean isSatisfiedBy(Map.Entry<String, JavadocOptionFileOption> element) {
-                    return element.getKey() != "locale";
+                    return "locale".equals(element.getKey());
                 }
             });
             for (final String option : optionsWithoutLocale.keySet()) {
