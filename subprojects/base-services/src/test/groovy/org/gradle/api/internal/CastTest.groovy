746240c949daf1e0072cb2cda4bba7acd954dc03
Luke Daley <ld@ldaley.com>
2012-11-09 11:00:36 +0000

    Extract the standard casting mechanism out of the Transformers utilities.
    We wrap the standard Class.cast() method because its error message is terrible on some platforms.

--- subprojects/base-services/src/test/groovy/org/gradle/api/internal/CastTest.groovy
+++ subprojects/base-services/src/test/groovy/org/gradle/api/internal/CastTest.groovy
@@ -16,59 +16,29 @@
 
 package org.gradle.api.internal
 
-import org.gradle.api.Named
-import org.gradle.api.Namer
 import spock.lang.Specification
 
-import static org.gradle.api.internal.Transformers.*
+import static org.gradle.api.internal.Cast.cast
 
-class TransformersTest extends Specification {
+class CastTest extends Specification {
 
     def "casting"() {
         given:
         def arg = "1"
 
         when:
-        cast(Integer).transform(arg)
+        cast(Integer, arg)
 
         then:
         def e = thrown(ClassCastException)
         e.message == "Failed to cast object $arg of type ${arg.class.name} to target type ${Integer.name}"
 
         when:
-        def result = cast(CharSequence).transform(arg)
+        def result = cast(CharSequence, arg)
 
         then:
         notThrown(ClassCastException)
         result.is arg
     }
 
-    def "as string"() {
-        expect:
-        asString().transform(1) == "1"
-        asString().transform(null) == null
-    }
-
-    def "naming"() {
-        expect:
-        name().transform(named("foo")) == "foo"
-        name().transform(named(null)) == null
-
-        and:
-        def namer = new Namer() {
-            String determineName(Object object) {
-                object.toString()
-            }
-        }
-
-        name(namer).transform(3) == "3"
-    }
-
-    Named named(String name) {
-        new Named() {
-            String getName() {
-                name
-            }
-        }
-    }
 }
--- subprojects/core/src/main/groovy/org/gradle/api/internal/artifacts/DefaultArtifactRepositoryContainer.java
+++ subprojects/core/src/main/groovy/org/gradle/api/internal/artifacts/DefaultArtifactRepositoryContainer.java
@@ -36,7 +36,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.gradle.api.internal.Transformers.cast;
+import static org.gradle.api.internal.Cast.cast;
 
 /**
  * @author Hans Dockter
