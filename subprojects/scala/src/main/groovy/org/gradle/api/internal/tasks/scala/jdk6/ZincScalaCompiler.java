e3cd8f9787adcd9258237069848555f7ffc8683c
Peter Niederwieser <pniederw@gmail.com>
2012-11-12 19:09:55 +0100

    fixed mapping of zinc scala compiler log levels

--- subprojects/scala/src/main/groovy/org/gradle/api/internal/tasks/scala/jdk6/ZincScalaCompiler.java
+++ subprojects/scala/src/main/groovy/org/gradle/api/internal/tasks/scala/jdk6/ZincScalaCompiler.java
@@ -88,26 +88,25 @@ public class ZincScalaCompiler implements Compiler<ScalaJavaJointCompileSpec>, S
         }
     }
 
-    // TODO: change log levels
     private static class SbtLoggerAdapter implements xsbti.Logger {
         public void error(F0<String> msg) {
-            LOGGER.lifecycle(msg.apply());
+            LOGGER.error(msg.apply());
         }
 
         public void warn(F0<String> msg) {
-            LOGGER.lifecycle(msg.apply());
+            LOGGER.warn(msg.apply());
         }
 
         public void info(F0<String> msg) {
-            LOGGER.lifecycle(msg.apply());
+            LOGGER.info(msg.apply());
         }
 
         public void debug(F0<String> msg) {
-            LOGGER.lifecycle(msg.apply());
+            LOGGER.debug(msg.apply());
         }
 
         public void trace(F0<Throwable> exception) {
-            LOGGER.lifecycle(exception.apply().toString());
+            LOGGER.trace(exception.apply().toString());
         }
     }
 }
