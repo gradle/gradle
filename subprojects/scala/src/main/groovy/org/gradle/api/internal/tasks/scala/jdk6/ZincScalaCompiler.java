165bcc67fb0c11380fb6f55f739ff83397e71b08
Peter Niederwieser <pniederw@gmail.com>
2012-11-12 22:02:36 +0100

    Revert "fixed mapping of zinc scala compiler log levels"
    This reverts commit d4a8761184f6c209e822e874d3e0e0d993b2324b.

--- subprojects/scala/src/main/groovy/org/gradle/api/internal/tasks/scala/jdk6/ZincScalaCompiler.java
+++ subprojects/scala/src/main/groovy/org/gradle/api/internal/tasks/scala/jdk6/ZincScalaCompiler.java
@@ -88,25 +88,26 @@ public class ZincScalaCompiler implements Compiler<ScalaJavaJointCompileSpec>, S
         }
     }
 
+    // TODO: change log levels
     private static class SbtLoggerAdapter implements xsbti.Logger {
         public void error(F0<String> msg) {
-            LOGGER.error(msg.apply());
+            LOGGER.lifecycle(msg.apply());
         }
 
         public void warn(F0<String> msg) {
-            LOGGER.warn(msg.apply());
+            LOGGER.lifecycle(msg.apply());
         }
 
         public void info(F0<String> msg) {
-            LOGGER.info(msg.apply());
+            LOGGER.lifecycle(msg.apply());
         }
 
         public void debug(F0<String> msg) {
-            LOGGER.debug(msg.apply());
+            LOGGER.lifecycle(msg.apply());
         }
 
         public void trace(F0<Throwable> exception) {
-            LOGGER.trace(exception.apply().toString());
+            LOGGER.lifecycle(exception.apply().toString());
         }
     }
 }
