f2c47c7776635868afa3ce74fac60181a655e832
Luke Daley <ld@ldaley.com>
2012-11-22 19:28:52 +0000

    Use RepositoryHandler instead of a RepositoryFactory for the javascript repos.

--- subprojects/javascript/src/main/groovy/org/gradle/plugins/javascript/base/JavaScriptExtension.java
+++ subprojects/javascript/src/main/groovy/org/gradle/plugins/javascript/base/JavaScriptExtension.java
@@ -16,45 +16,8 @@
 
 package org.gradle.plugins.javascript.base;
 
-import groovy.lang.Closure;
-import org.gradle.api.artifacts.repositories.ArtifactRepository;
-import org.gradle.api.artifacts.repositories.IvyArtifactRepository;
-import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
-import org.gradle.api.internal.artifacts.BaseRepositoryFactory;
-import org.gradle.api.internal.artifacts.repositories.layout.PatternRepositoryLayout;
-
 public class JavaScriptExtension {
 
     public static final String NAME = "javaScript";
-
-    public static final String GRADLE_PUBLIC_JAVASCRIPT_REPO_URL = "http://repo.gradle.org/gradle/javascript-public";
-    public static final String GOOGLE_APIS_REPO_URL = "http://ajax.googleapis.com/ajax/libs";
-
-    private final BaseRepositoryFactory baseRepositoryFactory;
-
-    public JavaScriptExtension(BaseRepositoryFactory baseRepositoryFactory) {
-        this.baseRepositoryFactory = baseRepositoryFactory;
-    }
-
-    public ArtifactRepository getGradlePublicJavaScriptRepository() {
-        MavenArtifactRepository repo = baseRepositoryFactory.createMavenRepository();
-        repo.setUrl(GRADLE_PUBLIC_JAVASCRIPT_REPO_URL);
-        repo.setName("Gradle Public JavaScript Repository");
-        return repo;
-    }
-
-    public ArtifactRepository getGoogleApisRepository() {
-        IvyArtifactRepository repo = baseRepositoryFactory.createIvyRepository();
-        repo.setName("Google Libraries Repository");
-        repo.setUrl(GOOGLE_APIS_REPO_URL);
-        repo.layout("pattern", new Closure(this) {
-            public void doCall() {
-                PatternRepositoryLayout layout = (PatternRepositoryLayout) getDelegate();
-                layout.artifact("[organization]/[revision]/[module].[ext]");
-                layout.ivy("[organization]/[revision]/[module].xml");
-            }
-        });
-        return repo;
-    }
 
 }
