c58d4f05f80c206a0ba3febfc87d8a75890224b4
Peter Niederwieser <pniederw@gmail.com>
2012-11-27 00:32:21 +0100

    fixed checkstyle tests

--- subprojects/code-quality/src/test/groovy/org/gradle/api/plugins/quality/CheckstylePluginTest.groovy
+++ subprojects/code-quality/src/test/groovy/org/gradle/api/plugins/quality/CheckstylePluginTest.groovy
@@ -82,7 +82,7 @@ class CheckstylePluginTest extends Specification {
             assert configFile == project.file("config/checkstyle/checkstyle.xml")
             assert configProperties == [:]
             assert resultFile == project.file("build/reports/checkstyle/${sourceSet.name}.xml")
-            assert ignoreFailures
+            assert !ignoreFailures
             assert showViolations
         }
     }
@@ -97,7 +97,7 @@ class CheckstylePluginTest extends Specification {
         task.configFile == project.file("config/checkstyle/checkstyle.xml")
         task.configProperties == [:]
         task.resultFile == project.file("build/reports/checkstyle/custom.xml")
-        task.ignoreFailures
+        !task.ignoreFailures
     }
 
     def "adds checkstyle tasks to check lifecycle task"() {
