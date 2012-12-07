7cb1759c3ef64bd662d3ec89d958733b84aafea0
Szczepan Faber <szczepiq@gmail.com>
2012-12-07 12:12:35 +0100

    Refactored test to use mocks so that it does not depend on class from coreImpl

--- subprojects/core/src/test/groovy/org/gradle/api/internal/artifacts/configurations/ModuleForcingResolveActionSpec.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/artifacts/configurations/ModuleForcingResolveActionSpec.groovy
@@ -16,8 +16,9 @@
 
 package org.gradle.api.internal.artifacts.configurations
 
-import org.gradle.api.internal.artifacts.ivyservice.DefaultDependencyResolveDetails
+import org.gradle.api.artifacts.DependencyResolveDetails
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static org.gradle.api.internal.artifacts.DefaultModuleVersionSelector.newSelector
 
@@ -26,17 +27,24 @@ import static org.gradle.api.internal.artifacts.DefaultModuleVersionSelector.new
  */
 class ModuleForcingResolveActionSpec extends Specification {
 
+    @Unroll
     def "forces modules"() {
         given:
         def forceModule1 = newSelector("org",  "module1", "1.0")
         def forceModule2 = newSelector("org",  "module2", "1.0")
+        def details = Mock(DependencyResolveDetails)
 
         when:
-        def details = new DefaultDependencyResolveDetails(requested)
         new ModuleForcingResolveAction([forceModule1, forceModule2]).execute(details)
 
         then:
-        details.forcedVersion == forcedVersion
+        1 * details.getRequested() >> requested
+        if (forcedVersion) {
+            1 * details.forceVersion(forcedVersion)
+        } else {
+            0 * details.forceVersion(_)
+        }
+        0 * details._
 
         where:
         requested                              | forcedVersion
@@ -46,11 +54,12 @@ class ModuleForcingResolveActionSpec extends Specification {
     }
 
     def "does not force anything when input empty"() {
-        def details = new DefaultDependencyResolveDetails(newSelector("org",  "foo", "1.0"))
+        def details = Mock(DependencyResolveDetails)
+
         when:
         new ModuleForcingResolveAction([]).execute(details)
 
         then:
-        details.forcedVersion == null
+        0 * details._
     }
 }
