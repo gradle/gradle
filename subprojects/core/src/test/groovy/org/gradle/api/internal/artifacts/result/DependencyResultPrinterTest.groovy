4098b070d6247dd3f74661718d365971215e9a83
Peter Niederwieser <pniederw@gmail.com>
2012-12-08 01:16:39 +0100

    remove usage of DefaultResolvedModuleVersionResult, which is located in project core-impl

--- subprojects/core/src/test/groovy/org/gradle/api/internal/artifacts/result/DependencyResultPrinterTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/artifacts/result/DependencyResultPrinterTest.groovy
@@ -1,5 +1,9 @@
 package org.gradle.api.internal.artifacts.result
 
+import org.gradle.api.artifacts.ModuleVersionIdentifier
+import org.gradle.api.artifacts.result.DependencyResult
+import org.gradle.api.artifacts.result.ModuleVersionSelectionReason
+import org.gradle.api.artifacts.result.ResolvedModuleVersionResult
 import org.gradle.api.internal.artifacts.DefaultModuleVersionIdentifier
 import org.gradle.api.internal.artifacts.DefaultModuleVersionSelector
 import spock.lang.Specification
@@ -9,11 +13,21 @@ class DependencyResultPrinterTest extends Specification {
         def dependency = new DefaultUnresolvedDependencyResult(
                 new DefaultModuleVersionSelector("group", "name", "version"),
                 null,
-                new DefaultResolvedModuleVersionResult(new DefaultModuleVersionIdentifier("group", "origin", "version")),
+                new SimpleResolvedModuleVersionResult(id: new DefaultModuleVersionIdentifier("group", "origin", "version")),
                 new Exception()
         )
 
         expect:
         DependencyResultPrinter.print(dependency) == "group:name:version"
+    }
+
+    static class SimpleResolvedModuleVersionResult implements ResolvedModuleVersionResult {
+        ModuleVersionIdentifier id
+
+        Set<? extends DependencyResult> dependencies = []
+
+        Set<? extends DependencyResult> dependents = []
+
+        ModuleVersionSelectionReason selectionReason
     }
 }
