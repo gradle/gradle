b7d3bdc1bf27211ed727f58c6954c5dc38b86528
Szczepan Faber <szczepiq@gmail.com>
2012-12-18 10:09:26 +0100

    Moved the dependency resolve integ tests from integTest/integTest -> coreImpl/integTest. If you know any other integ tests that should also travel from integTest -> coreImpl, don't hesitate to move them :)

--- subprojects/core-impl/src/integTest/groovy/org/gradle/integtests/resolve/DependencyResolveActionsIntegrationTest.groovy
+++ subprojects/core-impl/src/integTest/groovy/org/gradle/integtests/resolve/DependencyResolveActionsIntegrationTest.groovy
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 package org.gradle.integtests.resolve
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
