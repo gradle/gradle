e941c806659bcedb3dde92f2afa761b46aebae53
Adam Murdoch <adam.murdoch@gradleware.com>
2012-12-27 07:59:00 +1100

    Reworked the 'lots of tests' builds so they will work with previous releases of Gradle.

--- subprojects/performance/src/templates/with-junit/Test.java
+++ subprojects/performance/src/templates/with-junit/Test.java
@@ -1,13 +1,13 @@
 package ${packageName};
 
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class ${testClassName} {
+
     private final ${productionClassName} production = new ${productionClassName}("value");
 
-<% 20.times { index -> %>
-    @Test
+<% 20.times { index ->  %>
+    @org.junit.Test
     public void test${index}() {
         assertEquals(production.getProperty(), "value");
     }
