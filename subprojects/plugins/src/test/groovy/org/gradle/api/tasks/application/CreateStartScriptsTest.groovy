216d845c8329c872cc9ed0c627c9af495746dfe7
Olaf Klischat <olaf.klischat@gmail.com>
2012-12-30 00:40:51 +0100

    ApplicationPlugin/CreateStartScripts: defaultJvmOpts settable

--- subprojects/plugins/src/test/groovy/org/gradle/api/tasks/application/CreateStartScriptsTest.groovy
+++ subprojects/plugins/src/test/groovy/org/gradle/api/tasks/application/CreateStartScriptsTest.groovy
@@ -71,4 +71,12 @@ class CreateStartScriptsTest extends Specification {
         then:
         task.exitEnvironmentVar == 'APP_EXIT'
     }
+
+    def defaultJvmOptsDefaultsToEmptyList() {
+        when:
+        task.defaultJvmOpts = null
+
+        then:
+        task.defaultJvmOpts == []
+    }
 }
