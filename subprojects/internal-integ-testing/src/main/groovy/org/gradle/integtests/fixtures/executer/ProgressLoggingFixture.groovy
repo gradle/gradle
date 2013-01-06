b60cf3ede877818391150abe44e3f20513196f28
Luke Daley <ld@ldaley.com>
2013-01-06 16:37:22 +0000

    Fix for use of GradleExecuter as field type.

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/ProgressLoggingFixture.groovy
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/ProgressLoggingFixture.groovy
@@ -49,7 +49,7 @@ class ProgressLoggingFixture implements MethodRule {
 
     Statement apply(Statement base, FrameworkMethod method, Object target) {
         TestFile initFile
-        GradleExecuter executer = RuleHelper.getField(target, GradleContextualExecuter)
+        GradleExecuter executer = RuleHelper.getField(target, GradleExecuter)
         TestDirectoryProvider testDirectoryProvider = new TestDirectoryProviderFinder().findFor(target)
         TestFile temporaryFolder = testDirectoryProvider.testDirectory
         loggingOutputFile = temporaryFolder.file("loggingoutput.log")
