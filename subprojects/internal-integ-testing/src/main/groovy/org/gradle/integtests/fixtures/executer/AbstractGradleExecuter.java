60cd222c44edaffabb9e27c6e380c31d49e737e0
Luke Daley <ld@ldaley.com>
2013-01-22 17:14:08 +0000

    Remove some temp file post execution checking that wasn't actually doing anything.

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/AbstractGradleExecuter.java
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/AbstractGradleExecuter.java
@@ -525,25 +525,6 @@ public abstract class AbstractGradleExecuter implements GradleExecuter {
             });
         }
 
-        if (getExecutable() == null) {
-            assertions.add(new Action<ExecutionResult>() {
-                public void execute(ExecutionResult executionResult) {
-                    // Assert that no temp files are left lying around
-                    // Note: don't do this if a custom executable is used, as we don't know (and probably don't care) whether the executable cleans up or not
-                    TestFile[] testFiles = getTmpDir().listFiles();
-                    if (testFiles != null) {
-                        List<String> unexpectedFiles = new ArrayList<String>();
-                        for (File file : testFiles) {
-                            if (!file.getName().matches("maven-artifact\\d+.tmp")) {
-                                unexpectedFiles.add(file.getName());
-                            }
-                        }
-//            Assert.assertThat(unexpectedFiles, Matchers.isEmpty());
-                    }
-                }
-            });
-        }
-
         return assertions;
     }
 
