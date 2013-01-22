5d0483b4a7bd0e63d770d15b08afc7ce70d78b0b
Luke Daley <ld@ldaley.com>
2013-01-22 18:33:42 +0000

    Refactored the javascript in the release notes to remove some duplication.

--- subprojects/docs/src/docs/release/content/script.js
+++ subprojects/docs/src/docs/release/content/script.js
@@ -40,32 +40,29 @@ $(function() {
     });
   }
 
-  // Do fixed issues
-  var fixedIssues = $("h2#fixed-issues");
-  if (fixedIssues.size() > 0) {
-    $("<p class='loading-issues'>Retrieving the fixed issue information for @versionBase@ …</p>").insertAfter(fixedIssues);
-    var para = $("p.loading-issues")
+  function injectIssues(url, insertAfter, idBase, loadingText, messageFunction) {
+    var loadingPara = $("<p class='" + idBase + "-loading'>" + loadingText + " …</p>").insertAfter(insertAfter);
     var animate = true;
     var paraFadeOut = function() {
-      para.fadeOut("80", animate ? paraFadeIn : null);
+      loadingPara.fadeOut("80", animate ? paraFadeIn : null);
     };
     var paraFadeIn = function() {
-      para.fadeIn("80", animate ? paraFadeOut : null);
+      loadingPara.fadeIn("80", animate ? paraFadeOut : null);
     };
     var finishAnimation = function() {
       animate = false;
-      para.remove();
+      loadingPara.remove();
     };
     paraFadeOut();
 
-    $.ajax("http://services.gradle.org/fixed-issues/@versionBase@?callback=?", {
+    $.ajax(url + "?callback=?", {
       dataType: "jsonp",
       cache: true,
       success: function(data, textStatus, jqXHR) {
         finishAnimation();
-        var para = $("<p>" + data.length + " issues have been fixed in Gradle @versionBase@.</p>").insertAfter(fixedIssues);
+        var para = $("<p>" + messageFunction(data.length) + "</p>").insertAfter(insertAfter);
         if (data.length > 0) {
-          var list = $("<ul id='fixed-issues-list'></ul>").hide().insertAfter(para)
+          var list = $("<ul id='" + idBase + "-list'></ul>").hide().insertAfter(para);
           $.each(data, function (i, issue) {
             $("<li>[<a href='" + issue["link"] + "'>"+ issue["key"] + "</a>] - " + issue["summary"] + "</li>").appendTo(list);
           });
@@ -75,63 +72,37 @@ $(function() {
       timeout: 10000,
       error: function() {
         finishAnimation();
-        $("<p>Unable to retrieve the fixed issue information. You may not be connected to the Internet, or there may have been an error.</p>").insertAfter(fixedIssues).css({fontWeight: "bold", color: "red"});
+        $("<p>Unable to retrieve the issue information. You may not be connected to the Internet, or there may have been an error.</p>").insertAfter(insertAfter).css({fontWeight: "bold", color: "red"});
       }
     });
   }
-
-  // Do known issues
-  var knownIssues = $("h2#known-issues");
-  if (knownIssues.size() > 0) {
-    var insertAfter = knownIssues;
-
-    var knownIssuesNext = knownIssues.next();
-    if (knownIssuesNext.is("p")) {
-        insertAfter = knownIssuesNext;
+  
+  injectIssues(
+    "http://services.gradle.org/fixed-issues/@versionBase@", 
+    $("h2#fixed-issues"), 
+    "fixed-issues", 
+    "Retrieving the fixed issue information for @versionBase@", 
+    function(i) {
+      return i + " issues have been fixed in Gradle @versionBase@.";
     }
-    $("<p class='loading-issues'>Retrieving the known issue information for @versionBase@ …</p>").insertAfter(insertAfter);
-    var para = $("p.loading-issues")
-    var animate = true;
-    var paraFadeOut = function() {
-      para.fadeOut("80", animate ? paraFadeIn : null);
-    };
-    var paraFadeIn = function() {
-      para.fadeIn("80", animate ? paraFadeOut : null);
-    };
-    var finishAnimation = function() {
-      animate = false;
-      para.remove();
-    };
-    paraFadeOut();
-
-    $.ajax("http://services.gradle.org/known-issues/@versionBase@?callback=?", {
-      dataType: "jsonp",
-      cache: true,
-      success: function(data, textStatus, jqXHR) {
-        finishAnimation();
-
-        if (data.length > 0) {
-          var para = $("<p>There are " + data.length + " known issues of Gradle @versionBase@.</p>").insertAfter(insertAfter);
-          var list = $("<ul id='fixed-issues-list'></ul>").hide().insertAfter(para)
-          $.each(data, function (i, issue) {
-            $("<li>[<a href='" + issue["link"] + "'>"+ issue["key"] + "</a>] - " + issue["summary"] + "</li>").appendTo(list);
-          });
-          list.slideDown("slow");
-        } else {
-            $("<p>There are no known issues of Gradle @versionBase@ at this time.</p>").insertAfter(insertAfter);
-        }
-      },
-      timeout: 10000,
-      error: function() {
-        finishAnimation();
-        $("<p>Unable to retrieve the known issue information. You may not be connected to the Internet, or there may have been an error.</p>").insertAfter(insertAfter).css({fontWeight: "bold", color: "red"});
+  );
+  
+  injectIssues(
+    "http://services.gradle.org/known-issues/@versionBase@", 
+    $("h2#known-issues").next("p"), 
+    "known-issues", 
+    "Retrieving the known issue information for @versionBase@", 
+    function(i) {
+      if (i > 0) {
+        return i + " issues have been fixed in Gradle @versionBase@.";
+      } else {
+        return "There are no known issues of Gradle @versionBase@ at this time.";
       }
-    });
-  }
+    } 
+  );
 
   $("section.major-detail").each(function() {
     addDetailCollapsing($(this));
   });
-
 
 });
