72358d926da7f084d04ece212018bda1f06f470d
Szczepan Faber <szczepiq@gmail.com>
2013-02-18 17:51:14 +0100

    Checkstyle

--- subprojects/core/src/test/groovy/org/gradle/configuration/LifecycleProjectEvaluatorTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/configuration/LifecycleProjectEvaluatorTest.groovy
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License")
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.gradle.configuration
 
 import org.gradle.api.ProjectEvaluationListener
