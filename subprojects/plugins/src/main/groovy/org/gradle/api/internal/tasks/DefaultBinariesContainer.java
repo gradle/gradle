21a34570613414ba7fad17a4887705b5b81187c8
Peter Niederwieser <pniederw@gmail.com>
2013-02-20 11:20:13 +0100

    initial implementation for new binaries model

---
+++ subprojects/plugins/src/main/groovy/org/gradle/api/internal/tasks/DefaultBinariesContainer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.internal.tasks;
+
+import org.gradle.api.tasks.BinaryContainer;
+import org.gradle.api.tasks.JvmBinaryContainer;
+
+public class DefaultBinariesContainer implements BinaryContainer {
+    private final JvmBinaryContainer container;
+
+    public DefaultBinariesContainer(JvmBinaryContainer container) {
+        this.container = container;
+    }
+
+    public JvmBinaryContainer getJvm() {
+        return container;
+    }
+}
