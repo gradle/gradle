638844a87ef3351d6186cce054e0c31b554040bf
Peter Niederwieser <pniederw@gmail.com>
2013-02-20 11:19:31 +0100

    initial implementations for new source set model

---
+++ subprojects/plugins/src/main/groovy/org/gradle/api/internal/tasks/DefaultFunctionalSourceSet.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.internal.tasks;
+
+import org.gradle.api.Named;
+import org.gradle.api.internal.AbstractNamedDomainObjectContainer;
+import org.gradle.api.tasks.FunctionalSourceSet;
+import org.gradle.api.tasks.LanguageSourceSet;
+import org.gradle.internal.reflect.Instantiator;
+
+public class DefaultFunctionalSourceSet extends AbstractNamedDomainObjectContainer<LanguageSourceSet> implements FunctionalSourceSet, Named {
+    private final String name;
+
+    public DefaultFunctionalSourceSet(String name, Instantiator instantiator) {
+        super(LanguageSourceSet.class, instantiator);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    protected LanguageSourceSet doCreate(String name) {
+        return null; // TODO
+    }
+}
