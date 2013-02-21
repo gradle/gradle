a77ef2c740e7af4580a5aeba96db43df397291b6
Peter Niederwieser <pniederw@gmail.com>
2013-02-21 16:34:24 +0100

    make checkstyle happy

--- subprojects/core/src/main/groovy/org/gradle/api/internal/ConventionAwareHelper.java
+++ subprojects/core/src/main/groovy/org/gradle/api/internal/ConventionAwareHelper.java
@@ -33,10 +33,12 @@ import java.util.concurrent.Callable;
  * @author Hans Dockter
  */
 public class ConventionAwareHelper implements ConventionMapping, HasConvention {
-    // prefix internal fields with _ so that they don't get into the way of propertyMissing()
+    //prefix internal fields with _ so that they don't get into the way of propertyMissing()
+    //CHECKSTYLE:OFF
     private final Convention _convention;
     private IConventionAware _source;
     private final Map<String, MappedPropertyImpl> _mappings = new HashMap<String, MappedPropertyImpl>();
+    //CHECKSTYLE:ON
 
     /**
      * @see org.gradle.api.internal.AsmBackedClassGenerator.ClassBuilderImpl#mixInConventionAware()
