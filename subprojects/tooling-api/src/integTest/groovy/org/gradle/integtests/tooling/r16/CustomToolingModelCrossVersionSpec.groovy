81ed3e065909d28ea3e2023b9c94c94115e551cb
Adam Murdoch <adam.murdoch@gradleware.com>
2013-03-16 12:28:41 +1100

    Added an ignored int test for custom tooling model.

---
+++ subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r16/CustomToolingModelCrossVersionSpec.groovy
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.integtests.tooling.r16
+
+import org.gradle.integtests.tooling.fixture.MinTargetGradleVersion
+import org.gradle.integtests.tooling.fixture.MinToolingApiVersion
+import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
+import spock.lang.Ignore
+
+@Ignore
+@MinToolingApiVersion("1.6")
+@MinTargetGradleVersion("1.6")
+class CustomToolingModelCrossVersionSpec extends ToolingApiSpecification {
+    def "plugin can contribute a custom tooling model"() {
+        file('build.gradle') << """
+import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
+import org.gradle.tooling.provider.model.ToolingModelBuilder
+import javax.inject.Inject
+import org.gradle.api.internal.project.ProjectInternal
+
+apply plugin: CustomPlugin
+
+class CustomModel {
+    String getValue() { 'greetings' }
+}
+class CustomBuilder implements ToolingModelBuilder {
+    boolean canBuild(Class<?> type) {
+        return type.simpleName == 'CustomModel'
+    }
+    Object buildAll(Class<?> type, ProjectInternal project) {
+        return new CustomModel()
+    }
+}
+class CustomPlugin implements Plugin<Project> {
+    @Inject
+    CustomPlugin(ToolingModelBuilderRegistry registry) {
+        registry.register(new CustomBuilder())
+    }
+
+    public void apply(Project project) {
+    }
+}
+"""
+
+        when:
+        def model = withConnection { connection ->
+            connection.model(CustomModel).get()
+        }
+
+        then:
+        model.value == 'greetings'
+    }
+
+    def "gives reasonable error message for unknown model"() {
+        expect: false
+    }
+}
