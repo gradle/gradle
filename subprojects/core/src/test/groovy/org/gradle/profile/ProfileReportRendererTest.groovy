7ca55d3f6fe39fabb22a4770e46aba1d16a0fc93
Szczepan Faber <szczepiq@gmail.com>
2013-03-19 13:19:52 +0100

    Added basic test for profile report generation. Will get more coverage and improvements.

---
+++ subprojects/core/src/test/groovy/org/gradle/profile/ProfileReportRendererTest.groovy
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.profile
+
+import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider
+import org.junit.Rule
+import spock.lang.Specification
+
+import static org.gradle.util.TextUtil.toPlatformLineSeparators
+
+/**
+ * by Szczepan Faber, created at: 3/19/13
+ */
+class ProfileReportRendererTest extends Specification {
+
+    @Rule TestNameTestDirectoryProvider temp = new TestNameTestDirectoryProvider()
+
+    def "renders report"() {
+        def model = new BuildProfile(description: "build description")
+        def file = temp.file("report.html")
+
+        model.profilingStarted  = time(12, 20, 0)
+        model.buildStarted      = time(12, 20, 0, 700)
+        model.settingsEvaluated = time(12, 20, 3)
+        model.projectsLoaded    = time(12, 20, 6)
+        model.projectsEvaluated = time(12, 20, 15)
+
+        model.buildFinished     = time(12, 35, 30)
+
+        when:
+        new ProfileReportRenderer().writeTo(model, file)
+
+        then:
+        file.text.contains(toPlatformLineSeparators("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Profile report</title>
+<link href="base-style.css" rel="stylesheet" type="text/css"/>
+<link href="style.css" rel="stylesheet" type="text/css"/>
+<script src="report.js" type="text/javascript"></script>
+</head>
+<body>
+<div id="content">
+<h1>Profile report</h1>
+<div id="header">
+<p>build description</p>
+<p>Started on: 2010/02/05 - 12:20:00</p>
+</div>
+<div id="tabs">
+<ul class="tabLinks">
+<li>
+<a href="#tab0">Summary</a>
+</li>
+<li>
+<a href="#tab1">Configuration</a>
+</li>
+<li>
+<a href="#tab2">Dependency Resolution</a>
+</li>
+<li>
+<a href="#tab3">Task Execution</a>
+</li>
+</ul>
+<div class="tab" id="tab0">
+<h2>Summary</h2>
+<table>
+<thead>
+<tr>
+<th>Description</th>
+<th class="numeric">Duration</th>
+</tr>
+</thead>
+<tr>
+<td>Total Build Time</td>
+<td class="numeric">15m30.00s</td>
+</tr>
+<tr>
+<td>Startup</td>
+<td class="numeric">0.700s</td>
+</tr>
+<tr>
+<td>Settings and BuildSrc</td>
+<td class="numeric">2.300s</td>
+</tr>
+<tr>
+<td>Loading Projects</td>
+<td class="numeric">3.000s</td>
+</tr>
+<tr>
+<td>Configuring Projects</td>
+<td class="numeric">15m15.00s</td>
+</tr>
+<tr>
+<td>Task Execution</td>
+<td class="numeric">0s</td>
+</tr>
+</table>
+</div>
+<div class="tab" id="tab1">
+<h2>Configuration</h2>
+<table>
+<thead>
+<tr>
+<th>Project</th>
+<th class="numeric">Duration</th>
+</tr>
+</thead>
+<tr>
+<td>All projects</td>
+<td class="numeric">0s</td>
+</tr>
+</table>
+</div>
+<div class="tab" id="tab2">
+<h2>Dependency Resolution</h2>
+<table>
+<thead>
+<tr>
+<th>Dependencies</th>
+<th class="numeric">Duration</th>
+</tr>
+</thead>
+<tr>
+<td>All dependencies</td>
+<td class="numeric">0s</td>
+</tr>
+</table>
+</div>
+<div class="tab" id="tab3">
+<h2>Task Execution</h2>
+<table>
+<thead>
+<tr>
+<th>Task</th>
+<th class="numeric">Duration</th>
+<th>Result</th>
+</tr>
+</thead>
+</table>
+</div>
+</div>"""))
+    }
+
+    private long time(int hour, int mins, int secs, int ms = 0) {
+        def cal = new GregorianCalendar(2010, 1, 5, hour, mins, secs)
+        cal.add(Calendar.MILLISECOND, ms)
+        cal.getTimeInMillis()
+    }
+}
