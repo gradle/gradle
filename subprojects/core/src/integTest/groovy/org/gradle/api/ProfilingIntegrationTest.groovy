875c4857447755c4d587ccb7602125634bcca86b
Szczepan Faber <szczepiq@gmail.com>
2013-03-19 14:50:36 +0100

    Moved profiling integration test to core integ tests.

--- subprojects/core/src/integTest/groovy/org/gradle/api/ProfilingIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/ProfilingIntegrationTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gradle.integtests
+package org.gradle.api
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.jsoup.Jsoup
