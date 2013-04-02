f5ba404b34ebec30d19ea5cceeb897c16e9487e3
Rene Groeschke <rene@breskeby.com>
2013-04-02 21:30:19 +0200

    REVIEW-1967: cleanup junit sample testcase

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesJUnitIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesJUnitIntegrationTest.groovy
@@ -35,7 +35,7 @@ public class SamplesJUnitIntegrationTest extends AbstractIntegrationTest {
         TestFile projectDir = sample.dir.file("categories")
 
         // Build and test projects
-        executer.inDirectory(projectDir).withTasks('clean', 'build').withArguments("--no-opt").run()
+        executer.inDirectory(projectDir).withTasks('clean', 'build').run()
 
         // Check tests have run
         DefaultTestExecutionResult result = new DefaultTestExecutionResult(projectDir)
