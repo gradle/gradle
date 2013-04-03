44009cf348f3ff4aac54d8c4cb2f7164260be24d
Rene Groeschke <rene@breskeby.com>
2013-04-04 00:50:11 +0200

    REVIEW-1982: rename test according to renamed prodcution code.

--- subprojects/core/src/test/groovy/org/gradle/api/internal/plugins/DefaultPluginContainerTest.java
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/plugins/DefaultPluginContainerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 the original author or authors.
+ * Copyright 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ import static org.junit.Assert.assertThat;
 /**
  * @author Hans Dockter
  */
-public class DefaultProjectsPluginContainerTest {
+public class DefaultPluginContainerTest {
     protected String pluginId = "somePluginId";
     protected JUnit4Mockery context = new JUnit4Mockery();
     private final DefaultProject project = HelperUtil.createRootProject();
