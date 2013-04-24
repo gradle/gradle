146014bcc0576ca9f9f9218ff648eb57186d057e
Luke Daley <ld@ldaley.com>
2013-04-24 13:17:30 +0100

    GRADLE-2358: fix test to actually test this, and mark as expecting failure as the issue is unresolved.

--- subprojects/core/src/integTest/groovy/org/gradle/api/ApplyPluginIntegSpec.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/ApplyPluginIntegSpec.groovy
@@ -17,33 +17,65 @@
 package org.gradle.api
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.executer.UnexpectedBuildFailure
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.FailsWith
 import spock.lang.Issue
 
+// TODO: This needs a better home - Possibly in the test kit package in the future
 class ApplyPluginIntegSpec extends AbstractIntegrationSpec {
 
     @Issue("GRADLE-2358")
+    @FailsWith(UnexpectedBuildFailure) // Test is currently failing
     def "can reference plugin by id in unitest"() {
 
         given:
-        file("src/main/groovy/org/acme/TestPlugin") << """class TestPlugin implements Plugin<Project>{
-            apply(Project project){
-                println "testplugin applied"
+        file("src/main/groovy/org/acme/TestPlugin.groovy") << """
+            package com.acme
+            import org.gradle.api.*
 
+            class TestPlugin implements Plugin<Project> {
+                void apply(Project project) {
+                    println "testplugin applied"
+                }
             }
-        }"""
+        """
 
         file("src/main/resources/META-INF/gradle-plugins/testplugin.properties") << "implementation-class=org.acme.TestPlugin"
 
-        file("src/test/groovy/org/acme/TestPluginSpec") << """class TestPluginSpec extends SpecificationPlugin{
-            def "can apply plugin by id"(){
-                Project project = ProjectBuilder.builder().build()
-                project.apply(plugin:"testdplugin")
-                assert project.plugins.withType(TestPlugin).size() == 1
+        file("src/test/groovy/org/acme/TestPluginSpec.groovy") << """
+            import spock.lang.Specification
+            import ${ProjectBuilder.name}
+            import ${Project.name}
+            import com.acme.TestPlugin
+
+            class TestPluginSpec extends Specification {
+                def "can apply plugin by id"() {
+                    when:
+                    Project project = ProjectBuilder.builder().build()
+                    project.apply(plugin: "testplugin")
+
+                    then:
+                    project.plugins.withType(TestPlugin).size() == 1
+                }
             }
-        }
         """
+
+        and:
         buildFile << '''
             apply plugin: 'groovy'
+
+            repositories {
+                mavenCentral()
+            }
+
+            dependencies {
+                compile gradleApi()
+                compile localGroovy()
+                compile "org.spockframework:spock-core:0.7-groovy-1.8", {
+                    exclude module: "groovy-all"
+                }
+            }
         '''
 
         expect:
