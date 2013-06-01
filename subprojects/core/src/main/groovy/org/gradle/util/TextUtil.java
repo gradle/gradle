b98cd79aab654c05ae137619db627669686b934b
Adam Murdoch <adam.murdoch@gradleware.com>
2013-06-01 10:52:08 +1000

    Actually normalise the line endings.

--- subprojects/core/src/main/groovy/org/gradle/util/TextUtil.java
+++ subprojects/core/src/main/groovy/org/gradle/util/TextUtil.java
@@ -64,7 +64,7 @@ public class TextUtil {
      * Converts all line separators in the specified string to a single new line character.
      */
     public static String normaliseLineSeparators(String str) {
-        return str == null ? null : convertLineSeparators(str, getPlatformLineSeparator());
+        return str == null ? null : convertLineSeparators(str, "\n");
     }
 
     /**
