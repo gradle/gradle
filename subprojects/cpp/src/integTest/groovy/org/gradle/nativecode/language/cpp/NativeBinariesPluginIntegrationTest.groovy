8b5e98ad6c2dbff83ab6d812305dfbdd0d466750
daz <darrell.deboer@gradleware.com>
2013-07-19 15:16:58 -0600

    Integration test coverage for various ways of supplying sources to native binaries

--- subprojects/cpp/src/integTest/groovy/org/gradle/nativecode/language/cpp/NativeBinariesPluginIntegrationTest.groovy
+++ subprojects/cpp/src/integTest/groovy/org/gradle/nativecode/language/cpp/NativeBinariesPluginIntegrationTest.groovy
@@ -15,11 +15,133 @@
  */
 package org.gradle.nativecode.language.cpp
 import org.gradle.nativecode.language.cpp.fixtures.AbstractBinariesIntegrationSpec
+import spock.lang.Ignore
 
 class NativeBinariesPluginIntegrationTest extends AbstractBinariesIntegrationSpec {
+    static final HELLO_WORLD = "Hello, World!"
 
     def "setup"() {
         settingsFile << "rootProject.name = 'test'"
+    }
+
+    // TODO:DAZ Fix this
+    @Ignore("Not sure what the behaviour should be")
+    def "assemble executable from component with no source"() {
+        given:
+        buildFile << """
+            apply plugin: "cpp"
+            executables {
+                main {}
+            }
+        """
+
+        when:
+        succeeds "mainExecutable"
+
+        then:
+        executable("build/binaries/mainExecutable/main").assertExists()
+    }
+
+    def "assemble executable from component with multiple language source sets"() {
+        given:
+        useMixedSources()
+
+        when:
+        buildFile << """
+            apply plugin: "cpp"
+            sources {
+                main {}
+            }
+            executables {
+                main {
+                    source sources.main.cpp
+                    source sources.main.c
+                }
+            }
+        """
+
+        then:
+        succeeds "mainExecutable"
+
+        and:
+        executable("build/binaries/mainExecutable/main").exec().out == HELLO_WORLD
+    }
+
+    def "assemble executable binary directly from language source sets"() {
+        given:
+        useMixedSources()
+
+        when:
+        buildFile << """
+            apply plugin: "cpp"
+            sources {
+                main {}
+            }
+            executables {
+                main {}
+            }
+            binaries.all {
+                source sources.main.cpp
+                source sources.main.c
+            }
+        """
+
+        then:
+        succeeds "mainExecutable"
+
+        and:
+        executable("build/binaries/mainExecutable/main").exec().out == HELLO_WORLD
+    }
+
+    def "assemble executable binary directly from functional source set"() {
+        given:
+        useMixedSources()
+
+        when:
+        buildFile << """
+            apply plugin: "cpp"
+            sources {
+                main {}
+            }
+            executables {
+                main {}
+            }
+            binaries.all {
+                source sources.main
+            }
+        """
+        
+        then:
+        succeeds "mainExecutable"
+
+        and:
+        executable("build/binaries/mainExecutable/main").exec().out == HELLO_WORLD
+    }
+
+    private def useMixedSources() {
+        file("src/main/headers/hello.h") << """
+            void hello();
+        """
+
+        file("src/main/c/hello.c") << """
+            #include <stdio.h>
+            #include "hello.h"
+
+            void hello () {
+                printf("${HELLO_WORLD}");
+            }
+        """
+
+        file("src/main/cpp/main.cpp") << """
+            extern "C" {
+                #include "hello.h"
+            }
+
+            int main () {
+                hello();
+                return 0;
+            }
+        """
     }
 
     def "build fails when link executable fails"() {
