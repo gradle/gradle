9757962281cc957c30dcacfce12a9450f306bb43
Adam Murdoch <adam.murdoch@gradleware.com>
2013-07-25 14:02:48 +1000

    Moved IncrementalTestIntegrationTest to plugins project.

--- subprojects/plugins/src/integTest/groovy/org/gradle/testing/IncrementalTestIntegrationTest.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/testing/IncrementalTestIntegrationTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gradle.integtests
+package org.gradle.testing
 
 import org.gradle.integtests.fixtures.AbstractIntegrationTest
 import org.gradle.integtests.fixtures.JUnitXmlTestExecutionResult
