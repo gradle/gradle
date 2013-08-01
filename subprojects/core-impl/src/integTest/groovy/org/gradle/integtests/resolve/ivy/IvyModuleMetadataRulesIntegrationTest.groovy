bfdacccf58b4a98ea25b7efa1bcf91b32d8dd20d
Peter Niederwieser <pniederw@gmail.com>
2013-08-01 02:25:22 +0200

    moved test back into ivy package

--- subprojects/core-impl/src/integTest/groovy/org/gradle/integtests/resolve/ivy/IvyModuleMetadataRulesIntegrationTest.groovy
+++ subprojects/core-impl/src/integTest/groovy/org/gradle/integtests/resolve/ivy/IvyModuleMetadataRulesIntegrationTest.groovy
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gradle.integtests.resolve
+package org.gradle.integtests.resolve.ivy
 
 import org.gradle.integtests.fixtures.AbstractDependencyResolutionTest
 
 import static org.hamcrest.Matchers.containsString
 
-class ModuleMetadataRulesIntegrationTest extends AbstractDependencyResolutionTest {
+class IvyModuleMetadataRulesIntegrationTest extends AbstractDependencyResolutionTest {
     def setup() {
         buildFile <<
 """
