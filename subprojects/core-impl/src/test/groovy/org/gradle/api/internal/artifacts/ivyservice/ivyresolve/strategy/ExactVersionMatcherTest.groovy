6bf2ede0bdf68922a1468fb0a0ee8e8b9b587f1a
Peter Niederwieser <pniederw@gmail.com>
2013-08-08 04:50:56 +0200

    beefed up spec

--- subprojects/core-impl/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/ivyresolve/strategy/ExactVersionMatcherTest.groovy
+++ subprojects/core-impl/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/ivyresolve/strategy/ExactVersionMatcherTest.groovy
@@ -44,12 +44,33 @@ class ExactVersionMatcherTest extends Specification {
 
     def "accepts candidate version iff it literally matches the selector"() {
         expect:
+        matcher.accept("", "")
         matcher.accept("1.0", "1.0")
         matcher.accept("2.0", "2.0")
         matcher.accept("!@#%", "!@#%")
+        matcher.accept("hey joe", "hey joe")
         !matcher.accept("1.0", "1.1")
         !matcher.accept("2.0", "3.0")
         !matcher.accept("!@#%", "%#@!")
+        !matcher.accept("hey joe", "hoe hey")
+    }
+
+    def "does not accept candidate version that differs in separator"() {
+        expect:
+        !matcher.accept("1.0", "1_0")
+        !matcher.accept("1_0", "1-0")
+        !matcher.accept("1-0", "1+0")
+        !matcher.accept("1.a.2", "1a2")
+    }
+
+    def "does not accept candidate version that has different number of trailing .0's"() {
+        expect:
+        !matcher.accept("1.0.0", "1.0")
+        !matcher.accept("1", "1.0.0")
+    }
+
+    def "does not accept candidate version that has different capitalization"() {
+        !matcher.accept("1.0-alpha", "1.0-ALPHA")
     }
 
     def "supports metadata-aware accept method (with same result)"() {
@@ -70,51 +91,91 @@ class ExactVersionMatcherTest extends Specification {
 
     def "compares versions lexicographically"() {
         expect:
-        matcher.compare("1.0", "2.0") < 0
-        matcher.compare("1.0", "1.1") < 0
-        matcher.compare("1.0.1", "1.1.0") < 0
+        matcher.compare(v1, v2) < 0
+        matcher.compare(v2, v1) > 0
+        matcher.compare(v1, v1) == 0
+        matcher.compare(v2, v2) == 0
 
-        matcher.compare("1", "1") == 0
-        matcher.compare("1.0", "1.0") == 0
-
-        matcher.compare("2.0", "1.0") > 0
-        matcher.compare("1.1", "1.0") > 0
-        matcher.compare("1.1.0", "1.0.1") > 0
-
-        matcher.compare("1.2.3", "1.2") > 0
-        matcher.compare("1.2", "1.2.3") < 0
+        where:
+        v1          | v2
+        "1.0"       | "2.0"
+        "1.0"       | "1.1"
+        "1.0.1"     | "1.1.0"
+        "1.2"       | "1.2.3"
+        "1.0-1"     | "1.0-2"
+        "1.0.a"     | "1.0.b"
+        "1.0-alpha" | "1.0"
+        "1.0-alpha" | "1.0-beta"
+        "1.0.alpha" | "1.0.b"
     }
 
-    def "gives special treatment to 'dev', 'rc', and 'final' qualifiers"() {
+    def "gives some special treatment to 'dev', 'rc', and 'final' qualifiers"() {
         expect:
-        matcher.compare("1.0-dev-1", "1.0") < 0
-        matcher.compare("1.0", "1.0-dev-1") > 0
-        matcher.compare("1.0-dev-1", "1.0-dev-2") < 0
-        matcher.compare("1.0-dev-2", "1.0-dev-1") > 0
+        matcher.compare(v1, v2) < 0
+        matcher.compare(v2, v1) > 0
+        matcher.compare(v1, v1) == 0
+        matcher.compare(v2, v2) == 0
 
-        matcher.compare("1.0-rc-1", "1.0") < 0
-        matcher.compare("1.0", "1.0-rc-1") > 0
-        matcher.compare("1.0-rc-1", "1.0-rc-2") < 0
-        matcher.compare("1.0-rc-2", "1.0-rc-1") > 0
-
-        matcher.compare("1.0-final", "1.0") < 0
-        matcher.compare("1.0", "1.0-final") > 0
-
-        matcher.compare("1.0-dev-1", "1.0-rc-1") < 0
-        matcher.compare("1.0-dev-2", "1.0-rc-1") < 0
-
-        matcher.compare("1.0-rc-1", "1.0-final") < 0
-        matcher.compare("1.0-rc-2", "1.0-final") < 0
-
-        matcher.compare("1.0-final", "1.0-dev-1") > 0
-        matcher.compare("1.0-final", "1.0-dev-2") > 0
+        where:
+        v1          | v2
+        "1.0-dev-1" | "1.0"
+        "1.0-dev-1" | "1.0-dev-2"
+        "1.0-rc-1"  | "1.0"
+        "1.0-rc-1"  | "1.0-rc-2"
+        "1.0-final" | "1.0"
+        "1.0-dev-1" | "1.0-rc-1"
+        "1.0-rc-1"  | "1.0-final"
+        "1.0-dev-1" | "1.0-final"
     }
 
-    def "versions that differ only in separators compare equal"() {
+    def "compares identical versions equal"() {
+        expect:
+        matcher.compare(v1, v2) == 0
+        matcher.compare(v2, v1) == 0
+
+        where:
+        v1        | v2
+        ""        | ""
+        "1"       | "1"
+        "1.0.0"   | "1.0.0"
+        "!@#%"    | "!@#%"
+        "hey joe" | "hey joe"
+    }
+
+    def "compares versions that differ only in separators equal"() {
         expect:
         matcher.compare("1.0", "1_0") == 0
         matcher.compare("1_0", "1-0") == 0
         matcher.compare("1-0", "1+0") == 0
         matcher.compare("1.a.2", "1a2") == 0 // number-word and word-number boundaries are considered separators
+    }
+
+    def "compares unrelated versions unequal"() {
+        expect:
+        matcher.compare("1.0", "") != 0
+        matcher.compare("1.0", "!@#%") != 0
+        matcher.compare("1.0", "hey joe") != 0
+    }
+
+    // original Ivy behavior - should we change it?
+    def "does not compare versions with different number of trailing .0's equal"() {
+        expect:
+        matcher.compare(v1, v2) > 0
+        matcher.compare(v2, v1) < 0
+
+        where:
+        v1          | v2
+        "1.0.0"     | "1.0"
+        "1.0.0"     | "1"
+    }
+
+    def "does not compare versions with different capitalization equal"() {
+        expect:
+        matcher.compare(v1, v2) > 0
+        matcher.compare(v2, v1) < 0
+
+        where:
+        v1          | v2
+        "1.0-alpha" | "1.0-ALPHA"
     }
 }
