1b5bad511c417db1126559035fccb1833c5ad55b
Szczepan Faber <szczepiq@gmail.com>
2013-08-14 00:25:31 +0200

    Move a class that is really a test utility to the test source dir.

--- subprojects/core-impl/src/main/groovy/org/gradle/api/internal/artifacts/ivyservice/resolveengine/result/DummyStore.groovy
+++
@@ -1,9 +0,0 @@
-package org.gradle.api.internal.artifacts.ivyservice.resolveengine.result
-
-import org.gradle.api.internal.cache.Store
-
-class DummyStore implements Store<Object> {
-    Object load(org.gradle.internal.Factory<Object> createIfNotPresent) {
-        return createIfNotPresent.create();
-    }
-}
