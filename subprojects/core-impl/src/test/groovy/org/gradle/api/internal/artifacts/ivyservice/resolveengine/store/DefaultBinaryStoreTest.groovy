879b8eb413b3c900205b9768c8a3d7a4ee415b81
Szczepan Faber <szczepiq@gmail.com>
2013-08-30 22:59:33 +0200

    Fixed a problem with close method of the binary store (cherry picked from commit 1c4360b)

--- subprojects/core-impl/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/resolveengine/store/DefaultBinaryStoreTest.groovy
+++ subprojects/core-impl/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/resolveengine/store/DefaultBinaryStoreTest.groovy
@@ -13,29 +13,30 @@ class DefaultBinaryStoreTest extends Specification {
         def store = new DefaultBinaryStore(temp.file("foo.bin"))
 
         when:
-        store.write({it.writeInt(10)} as BinaryStore.WriteAction)
-        store.write({it.writeUTF("x")} as BinaryStore.WriteAction)
+        store.write({ it.writeInt(10) } as BinaryStore.WriteAction)
+        store.write({ it.writeUTF("x") } as BinaryStore.WriteAction)
         def data1 = store.done()
-        store.write({it.writeUTF("y")} as BinaryStore.WriteAction)
+        store.write({ it.writeUTF("y") } as BinaryStore.WriteAction)
         def data2 = store.done()
 
         then:
-        data1.read({it.readInt()} as BinaryStore.ReadAction) == 10
-        data1.read({it.readUTF()} as BinaryStore.ReadAction) == "x"
+        data1.read({ it.readInt() } as BinaryStore.ReadAction) == 10
+        data1.read({ it.readUTF() } as BinaryStore.ReadAction) == "x"
         data1.done()
 
-        data2.read({it.readUTF()} as BinaryStore.ReadAction) == "y"
+        data2.read({ it.readUTF() } as BinaryStore.ReadAction) == "y"
         data2.done()
 
         cleanup:
         store.close()
-       }
+    }
 
     def "may be empty"() {
         def store = new DefaultBinaryStore(temp.file("foo.bin"))
 
         when:
         def data = store.done()
+        store.close()
 
         then:
         data.done()
