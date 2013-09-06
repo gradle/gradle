4a91495b2a6893c4c0b36c334b98720c9ae46cbd
Adam Murdoch <adam.murdoch@gradleware.com>
2013-09-06 11:20:46 +1000

    Added some meta-data to the annotation type.

--- subprojects/core/src/main/groovy/org/gradle/tooling/provider/model/internal/LegacyConsumerInterface.java
+++ subprojects/core/src/main/groovy/org/gradle/tooling/provider/model/internal/LegacyConsumerInterface.java
@@ -16,10 +16,14 @@
 
 package org.gradle.tooling.provider.model.internal;
 
+import java.lang.annotation.*;
+
 /**
  * Indicates that a given marker interface should be mixed in to instances of the annotated type before they
  * are passed to the tooling API client.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface LegacyConsumerInterface {
     String value();
 }
