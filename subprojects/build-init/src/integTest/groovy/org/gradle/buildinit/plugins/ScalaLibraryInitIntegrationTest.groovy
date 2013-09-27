ea8e86c7d9e37cccffdde01af6638b47f8583d9b
Adam Murdoch <adam.murdoch@gradleware.com>
2013-09-27 11:37:14 +1000

    Only run the scala build init test on >= java 6.

--- subprojects/build-init/src/integTest/groovy/org/gradle/buildinit/plugins/ScalaLibraryInitIntegrationTest.groovy
+++ subprojects/build-init/src/integTest/groovy/org/gradle/buildinit/plugins/ScalaLibraryInitIntegrationTest.groovy
@@ -20,8 +20,10 @@ import org.gradle.buildinit.plugins.fixtures.WrapperTestFixture
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.DefaultTestExecutionResult
 import org.gradle.integtests.fixtures.TestExecutionResult
+import org.gradle.util.Requires
+import org.gradle.util.TestPrecondition
 
-
+@Requires(TestPrecondition.JDK6_OR_LATER)
 class ScalaLibraryInitIntegrationTest extends AbstractIntegrationSpec {
 
     public static final String SAMPLE_LIBRARY_CLASS = "src/main/scala/Library.scala"
