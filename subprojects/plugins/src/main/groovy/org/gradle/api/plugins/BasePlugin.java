d036a1df4b269ca133acce3199d870e98fa8785a
Justin Ryan <jryan@netflix.com>
2013-09-30 12:08:01 -0700

    GRADLE-2087 Stop BasePlugin from overwriting project status

--- subprojects/plugins/src/main/groovy/org/gradle/api/plugins/BasePlugin.java
+++ subprojects/plugins/src/main/groovy/org/gradle/api/plugins/BasePlugin.java
@@ -127,7 +127,6 @@ public class BasePlugin implements Plugin<Project> {
 
     private void configureConfigurations(final Project project) {
         ConfigurationContainer configurations = project.getConfigurations();
-        project.setProperty("status", "integration");
 
         Configuration archivesConfiguration = configurations.create(Dependency.ARCHIVES_CONFIGURATION).
                 setDescription("Configuration for archive artifacts.");
