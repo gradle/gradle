gradle/gradle

commit 5ed4fa1f4cad251424c2cb64917097f0e3dad51a

Added some progress logging to show jetty status.

subprojects/jetty/src/main/java/org/gradle/api/plugins/jetty/AbstractJettyRunTask.java
@@ -26,6 +26,8 @@ import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.logging.ProgressLogger;
+import org.gradle.logging.ProgressLoggerFactory;
 import org.gradle.util.GFileUtils;
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.RequestLog;
@@ -68,26 +70,23 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
     private File webDefaultXml;
 
     /**
-     * A web.xml file to be applied AFTER the webapp's web.xml file. Useful for applying different build profiles, eg
-     * test, production etc. Optional.
+     * A web.xml file to be applied AFTER the webapp's web.xml file. Useful for applying different build profiles, eg test, production etc. Optional.
      */
     private File overrideWebXml;
 
     /**
-     * The interval in seconds to scan the webapp for changes and restart the context if necessary. Ignored if reload is
-     * enabled. Disabled by default.
+     * The interval in seconds to scan the webapp for changes and restart the context if necessary. Ignored if reload is enabled. Disabled by default.
      */
     private int scanIntervalSeconds;
 
     /**
-     * reload can be set to either 'automatic' or 'manual' <p/> if 'manual' then the context can be reloaded by a
-     * linefeed in the console if 'automatic' then traditional reloading on changed files is enabled.
+     * reload can be set to either 'automatic' or 'manual' <p/> if 'manual' then the context can be reloaded by a linefeed in the console if 'automatic' then traditional reloading on changed files is
+     * enabled.
      */
     protected String reload;
 
     /**
-     * Location of a jetty xml configuration file whose contents will be applied before any plugin configuration.
-     * Optional.
+     * Location of a jetty xml configuration file whose contents will be applied before any plugin configuration. Optional.
      */
     private File jettyConfig;
 
@@ -102,11 +101,9 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
     private String stopKey;
 
     /**
-     * <p> Determines whether or not the server blocks when started. The default behavior (daemon = false) will cause
-     * the server to pause other processes while it continues to handle web requests. This is useful when starting the
-     * server with the intent to work with it interactively. </p><p> Often, it is desirable to let the server start and
-     * continue running subsequent processes in an automated build environment. This can be facilitated by setting
-     * daemon to true. </p>
+     * <p> Determines whether or not the server blocks when started. The default behavior (daemon = false) will cause the server to pause other processes while it continues to handle web requests.
+     * This is useful when starting the server with the intent to work with it interactively. </p><p> Often, it is desirable to let the server start and continue running subsequent processes in an
+     * automated build environment. This can be facilitated by setting daemon to true. </p>
      */
     private boolean daemon;
 
@@ -202,9 +199,12 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
     }
 
     public void startJettyInternal() {
+        ProgressLoggerFactory progressLoggerFactory = getServices().get(ProgressLoggerFactory.class);
+        ProgressLogger progressLogger = progressLoggerFactory.newOperation(AbstractJettyRunTask.class);
+        progressLogger.setDescription("Start Jetty server");
+        progressLogger.setShortDescription("Starting Jetty");
+        progressLogger.started();
         try {
-            logger.debug("Starting Jetty Server ...");
-
             setServer(createServer());
 
             applyJettyXml();
@@ -246,7 +246,9 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
             // start Jetty
             server.start();
 
-            logger.info("Started Jetty Server");
+            if (daemon) {
+                return;
+            }
 
             if (getStopPort() != null && getStopPort() > 0 && getStopKey() != null) {
                 Monitor monitor = new Monitor(getStopPort(), getStopKey(),
@@ -261,16 +263,23 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
             // start the new line scanner thread if necessary
             startConsoleScanner();
 
-            // keep the thread going if not in daemon mode
-            if (!daemon) {
-                server.join();
-            }
         } catch (Exception e) {
-            throw new GradleException("An error occurred starting the Jetty server.", e);
+            throw new GradleException("Could not start the Jetty server.", e);
         } finally {
-            if (!daemon) {
-                logger.info("Jetty server exiting.");
-            }
+            progressLogger.completed();
+        }
+
+        progressLogger = progressLoggerFactory.newOperation(AbstractJettyRunTask.class);
+        progressLogger.setDescription("Jetty listening on port " + getHttpPort());
+        progressLogger.setShortDescription("Listening on port " + getHttpPort());
+        progressLogger.started();
+        try {
+            // keep the thread going if not in daemon mode
+            server.join();
+        } catch (Exception e) {
+            throw new GradleException("Failed to wait for the Jetty server to stop.", e);
+        } finally {
+            progressLogger.completed();
         }
     }
 
@@ -313,8 +322,7 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
     }
 
     /**
-     * Run a scanner thread on the given list of files and directories, calling stop/start on the given list of
-     * LifeCycle objects if any of the watched files change.
+     * Run a scanner thread on the given list of files and directories, calling stop/start on the given list of LifeCycle objects if any of the watched files change.
      */
     private void startScanner() {
 
@@ -477,8 +485,8 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
     }
 
     /**
-     * Specifies whether the Jetty server should run in the background. When {@code true}, this task completes as
-     * soon as the server has started. When {@code false}, this task blocks until the Jetty server is stopped.
+     * Specifies whether the Jetty server should run in the background. When {@code true}, this task completes as soon as the server has started. When {@code false}, this task blocks until the Jetty
+     * server is stopped.
      */
     public boolean isDaemon() {
         return daemon;

subprojects/jetty/src/main/java/org/gradle/api/plugins/jetty/JettyStop.java
@@ -19,6 +19,8 @@ package org.gradle.api.plugins.jetty;
 import org.gradle.api.InvalidUserDataException;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.internal.ConventionTask;
+import org.gradle.logging.ProgressLogger;
+import org.gradle.logging.ProgressLoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,6 +48,10 @@ public class JettyStop extends ConventionTask {
             throw new InvalidUserDataException("Please specify a valid stopKey");
         }
 
+        ProgressLogger progressLogger = getServices().get(ProgressLoggerFactory.class).newOperation(JettyStop.class);
+        progressLogger.setDescription("Stop Jetty server");
+        progressLogger.setShortDescription("Stopping Jetty");
+        progressLogger.started();
         try {
             Socket s = new Socket(InetAddress.getByName("127.0.0.1"), getStopPort());
             s.setSoLinger(false, 0);
@@ -58,6 +64,8 @@ public class JettyStop extends ConventionTask {
             logger.info("Jetty not running!");
         } catch (Exception e) {
             logger.error("Exception during stopping", e);
+        } finally {
+            progressLogger.completed();
         }
     }
 

commit 77ea37706ae3b1037179de77cc5f5a2628d0f786

Fix shutting down jetty container when running in daemon mode.
This fixes GRADLE-2263. The breakage occurred in 5ed4fa1f4cad.

subprojects/jetty/src/main/java/org/gradle/api/plugins/jetty/AbstractJettyRunTask.java
@@ -239,10 +239,6 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
             // start Jetty
             server.start();
 
-            if (daemon) {
-                return;
-            }
-
             if (getStopPort() != null && getStopPort() > 0 && getStopKey() != null) {
                 Monitor monitor = new Monitor(getStopPort(), getStopKey(), (Server) server.getProxiedObject());
                 monitor.start();
@@ -265,6 +261,11 @@ public abstract class AbstractJettyRunTask extends ConventionTask {
         progressLogger.setDescription(String.format("Run Jetty at http://localhost:%d/%s", getHttpPort(), getContextPath()));
         progressLogger.setShortDescription(String.format("Running at http://localhost:%d/%s", getHttpPort(), getContextPath()));
         progressLogger.started();
+
+        if (daemon) {
+            return;
+        }
+
         try {
             // keep the thread going if not in daemon mode
             server.join();

subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesWebProjectIntegrationTest.groovy
@@ -92,4 +92,41 @@ private void callServlet() {
         then:
         output.contains('Hello Gradle')
     }
+
+    def "can stop container"() {
+        given:
+        // Inject some int test stuff
+        sample.dir.file('build.gradle') << """
+def portFinder = org.gradle.util.AvailablePortFinder.createPrivate()
+
+httpPort = portFinder.nextAvailable
+stopPort = portFinder.nextAvailable
+println "http port = \$httpPort, stop port = \$stopPort"
+
+[jettyRun, jettyRunWar]*.daemon = true
+
+task runTest(dependsOn: jettyRun) << {
+    URL url = new URL("http://localhost:\$httpPort/customized/hello")
+    println url.text
+
+    jettyStop.execute()
+    Thread.sleep(500)
+
+    try {
+        url.text
+        assert false
+    } catch (Exception e) {
+        println e
+    }
+}
+"""
+
+        when:
+        sample sample
+        run 'runTest'
+
+        then:
+        output.contains('Hello Gradle')
+        output.contains('ConnectException')
+    }
 }