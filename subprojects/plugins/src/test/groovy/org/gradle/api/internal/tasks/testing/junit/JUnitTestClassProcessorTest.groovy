5399d31824764acdf7f3e7f520386298a8ca8608
Szczepan Faber <szczepiq@gmail.com>
2013-11-22 13:57:37 +0100

    Rename job

--- subprojects/plugins/src/test/groovy/org/gradle/api/internal/tasks/testing/junit/JUnitTestClassProcessorTest.groovy
+++ subprojects/plugins/src/test/groovy/org/gradle/api/internal/tasks/testing/junit/JUnitTestClassProcessorTest.groovy
@@ -35,10 +35,10 @@ class JUnitTestClassProcessorTest extends Specification {
     
     @Rule TestNameTestDirectoryProvider tmp = new TestNameTestDirectoryProvider()
 
-    def resultProcessor = Mock(TestResultProcessor)
+    def processor = Mock(TestResultProcessor)
     def spec = new JUnitSpec(new JUnitOptions(), new DefaultTestSelection())
     
-    @Subject processor = withSpec(spec)
+    @Subject classProcessor = withSpec(spec)
 
     JUnitTestClassProcessor withSpec(spec) {
         new JUnitTestClassProcessor(spec, new LongIdGenerator(), new TestActorFactory(), {} as StandardOutputRedirector)
@@ -49,127 +49,127 @@ class JUnitTestClassProcessorTest extends Specification {
     }
 
     void process(Iterable<String> classNames) {
-        processor.startProcessing(resultProcessor)
+        classProcessor.startProcessing(processor)
         for (String c : classNames) {
-            processor.processTestClass(new DefaultTestClassRunInfo(c))
+            classProcessor.processTestClass(new DefaultTestClassRunInfo(c))
         }
-        processor.stop()
+        classProcessor.stop()
     }
 
     def executesAJUnit4TestClass() {
         when: process(ATestClass)
 
-        then: 1 * resultProcessor.started({ it.id == 1 && it.name == ATestClass.name && it.className == ATestClass.name }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "ok" && it.className == ATestClass.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == null }) //wondering why result type is null? Failures are notified via failure() method
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 && it.name == ATestClass.name && it.className == ATestClass.name }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == "ok" && it.className == ATestClass.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == null }) //wondering why result type is null? Failures are notified via failure() method
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesAJUnit4TestClassWithIgnoredTest() {
         when: process(ATestClassWithIgnoredMethod)
 
-        then: 1 * resultProcessor.started({it.id == 1}, {it.parentId == null})
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "ignored" && it.className == ATestClassWithIgnoredMethod.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == SKIPPED })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({it.id == 1}, {it.parentId == null})
+        then: 1 * processor.started({ it.id == 2 && it.name == "ignored" && it.className == ATestClassWithIgnoredMethod.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == SKIPPED })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesAJUnit4TestClassWithFailedTestAssumption() {
         when: process(ATestClassWithFailedTestAssumption)
 
-        then: 1 * resultProcessor.started({it.id == 1}, {it.parentId == null})
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "assumed" && it.className == ATestClassWithFailedTestAssumption.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == SKIPPED })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({it.id == 1}, {it.parentId == null})
+        then: 1 * processor.started({ it.id == 2 && it.name == "assumed" && it.className == ATestClassWithFailedTestAssumption.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == SKIPPED })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesAnIgnoredJUnit4TestClass() {
         when: process(AnIgnoredTestClass)
 
-        then: 1 * resultProcessor.started({it.id == 1}, {it.parentId == null})
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "ignored2" && it.className == AnIgnoredTestClass.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == SKIPPED })
-        then: 1 * resultProcessor.started({ it.id == 3 && it.name == "ignored" && it.className == AnIgnoredTestClass.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(3, { it.resultType == SKIPPED })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({it.id == 1}, {it.parentId == null})
+        then: 1 * processor.started({ it.id == 2 && it.name == "ignored2" && it.className == AnIgnoredTestClass.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == SKIPPED })
+        then: 1 * processor.started({ it.id == 3 && it.name == "ignored" && it.className == AnIgnoredTestClass.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(3, { it.resultType == SKIPPED })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesAJUnit3TestClass() {
         when: process(AJunit3TestClass)
 
-        then: 1 * resultProcessor.started({it.id == 1}, {it.parentId == null})
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "testOk" && it.className == AJunit3TestClass.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({it.id == 1}, {it.parentId == null})
+        then: 1 * processor.started({ it.id == 2 && it.name == "testOk" && it.className == AJunit3TestClass.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesMultipleTestClasses() {
         when: process(ATestClass, AJunit3TestClass)
 
-        then: 1 * resultProcessor.started({ it.id == 1 && it.name == ATestClass.name && it.className == ATestClass.name }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "ok" && it.className == ATestClass.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
+        then: 1 * processor.started({ it.id == 1 && it.name == ATestClass.name && it.className == ATestClass.name }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == "ok" && it.className == ATestClass.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
 
-        then: 1 * resultProcessor.started({ it.id == 3 && it.name == AJunit3TestClass.name && it.className == AJunit3TestClass.name }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 4 && it.name == "testOk" && it.className == AJunit3TestClass.name }, { it.parentId == 3 })
-        then: 1 * resultProcessor.completed(4, { it.resultType == null })
-        then: 1 * resultProcessor.completed(3, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 3 && it.name == AJunit3TestClass.name && it.className == AJunit3TestClass.name }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 4 && it.name == "testOk" && it.className == AJunit3TestClass.name }, { it.parentId == 3 })
+        then: 1 * processor.completed(4, { it.resultType == null })
+        then: 1 * processor.completed(3, { it.resultType == null })
+        0 * processor._
     }
 
     def executesATestClassWithRunWithAnnotation() {
         when: process(ATestClassWithRunner)
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "broken" && it.className == ATestClassWithRunner.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.started({ it.id == 3 && it.name == "ok" && it.className == ATestClassWithRunner.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.failure(2, CustomRunner.failure)
-        then: 1 * resultProcessor.completed(3, { it.resultType == null })
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == "broken" && it.className == ATestClassWithRunner.name }, { it.parentId == 1 })
+        then: 1 * processor.started({ it.id == 3 && it.name == "ok" && it.className == ATestClassWithRunner.name }, { it.parentId == 1 })
+        then: 1 * processor.failure(2, CustomRunner.failure)
+        then: 1 * processor.completed(3, { it.resultType == null })
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesATestClassWithASuiteMethod() {
         when: process(ATestClassWithSuiteMethod)
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "testOk" && it.className == AJunit3TestClass.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.started({ it.id == 3 && it.name == "testOk" && it.className == AJunit3TestClass.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(3, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == "testOk" && it.className == AJunit3TestClass.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.started({ it.id == 3 && it.name == "testOk" && it.className == AJunit3TestClass.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(3, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesATestClassWithBrokenBeforeAndAfterMethod() {
         when: process(ATestClassWithBrokenBeforeAndAfterMethod)
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == 'test' && it.className == ATestClassWithBrokenBeforeAndAfterMethod.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.failure(2, ATestClassWithBrokenBeforeAndAfterMethod.beforeFailure)
-        then: 1 * resultProcessor.failure(2, ATestClassWithBrokenBeforeAndAfterMethod.afterFailure)
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == 'test' && it.className == ATestClassWithBrokenBeforeAndAfterMethod.name }, { it.parentId == 1 })
+        then: 1 * processor.failure(2, ATestClassWithBrokenBeforeAndAfterMethod.beforeFailure)
+        then: 1 * processor.failure(2, ATestClassWithBrokenBeforeAndAfterMethod.afterFailure)
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     @Unroll
     def "#testClass reports failure"() {
         when: process(testClass)
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == testMethodName && it.className == testClass.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.failure(2, failure)
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == testMethodName && it.className == testClass.name }, { it.parentId == 1 })
+        then: 1 * processor.failure(2, failure)
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
 
         where:
         testClass                               |testMethodName         |failure
@@ -187,60 +187,60 @@ class JUnitTestClassProcessorTest extends Specification {
     def executesATestClassWithRunnerThatBreaksAfterRunningSomeTests() {
         when: process(ATestClassWithRunnerThatBreaksAfterRuningSomeTests)
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
 
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == 'ok1' && it.className == ATestClassWithRunnerThatBreaksAfterRuningSomeTests.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == 'ok1' && it.className == ATestClassWithRunnerThatBreaksAfterRuningSomeTests.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == null })
 
-        then: 1 * resultProcessor.started({ it.id == 3 && it.name == 'broken' && it.className == ATestClassWithRunnerThatBreaksAfterRuningSomeTests.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.failure(3, CustomRunnerWithRunMethodThatBreaksAfterRunningSomeTests.failure)
-        then: 1 * resultProcessor.completed(3, { it.resultType == null })
+        then: 1 * processor.started({ it.id == 3 && it.name == 'broken' && it.className == ATestClassWithRunnerThatBreaksAfterRuningSomeTests.name }, { it.parentId == 1 })
+        then: 1 * processor.failure(3, CustomRunnerWithRunMethodThatBreaksAfterRunningSomeTests.failure)
+        then: 1 * processor.completed(3, { it.resultType == null })
 
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesATestClassWhichCannotBeLoaded() {
         String testClassName = 'org.gradle.api.internal.tasks.testing.junit.ATestClassWhichCannotBeLoaded'
         when: process([testClassName])
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == 'initializationError' && it.className == testClassName }, { it.parentId == 1 })
-        then: 1 * resultProcessor.failure(2, _ as NoClassDefFoundError)
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == 'initializationError' && it.className == testClassName }, { it.parentId == 1 })
+        then: 1 * processor.failure(2, _ as NoClassDefFoundError)
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def executesAJUnit3TestClassThatRenamesItself() {
         when: process(AJunit3TestThatRenamesItself)
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == 'testOk' && it.className == AJunit3TestThatRenamesItself.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == 'testOk' && it.className == AJunit3TestThatRenamesItself.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def "executes specific method"() {
-        processor = withSpec(new JUnitSpec(new JUnitOptions(), new DefaultTestSelection().includeMethod("pass")))
+        classProcessor = withSpec(new JUnitSpec(new JUnitOptions(), new DefaultTestSelection().includeMethod("pass")))
 
         when: process(ATestClassWith2Methods)
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
-        then: 1 * resultProcessor.started({ it.id == 2 && it.name == "pass" && it.className == ATestClassWith2Methods.name }, { it.parentId == 1 })
-        then: 1 * resultProcessor.completed(2, { it.resultType == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.started({ it.id == 2 && it.name == "pass" && it.className == ATestClassWith2Methods.name }, { it.parentId == 1 })
+        then: 1 * processor.completed(2, { it.resultType == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 
     def "executes no methods when method name does not match"() {
-        processor = withSpec(new JUnitSpec(new JUnitOptions(), new DefaultTestSelection().includeMethod("does not exist")))
+        classProcessor = withSpec(new JUnitSpec(new JUnitOptions(), new DefaultTestSelection().includeMethod("does not exist")))
 
         when: process(ATestClassWith2Methods)
 
-        then: 1 * resultProcessor.started({ it.id == 1 }, { it.parentId == null })
-        then: 1 * resultProcessor.completed(1, { it.resultType == null })
-        0 * resultProcessor._
+        then: 1 * processor.started({ it.id == 1 }, { it.parentId == null })
+        then: 1 * processor.completed(1, { it.resultType == null })
+        0 * processor._
     }
 }
