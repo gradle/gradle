f0a86af0622559f4d454592bd51051549d3f8deb
Adam Murdoch <adam.murdoch@gradleware.com>
2013-11-30 09:26:07 +1100

    Fleshed out some javadocs.

--- subprojects/core/src/main/groovy/org/gradle/api/artifacts/result/ResolvedComponentResult.java
+++ subprojects/core/src/main/groovy/org/gradle/api/artifacts/result/ResolvedComponentResult.java
@@ -17,41 +17,56 @@
 package org.gradle.api.artifacts.result;
 
 import org.gradle.api.Incubating;
+import org.gradle.api.Nullable;
 import org.gradle.api.artifacts.component.ComponentIdentifier;
 
 import java.util.Set;
 
 /**
- * A node in the resolved dependency graph.
- * Contains the identifier of the component and its dependencies.
+ * Represents a component instance in the resolved dependency graph. Provides some basic identity and dependency information about the component.
  */
 @Incubating
 public interface ResolvedComponentResult {
 
     /**
-     * Returns the identifier of this component.
+     * <p>Returns the identifier of this component. This can be used to uniquely identify the component within the current build, but it is not necessarily unique between
+     * different builds.
+     *
+     * <p>The return type is declared as an opaque {@link ComponentIdentifier}, however the identifier may also implement one of the following interfaces:</p>
+     *
+     * <ul>
+     *     <li>{@link org.gradle.api.artifacts.component.BuildComponentIdentifier} for those component instances which are produced by the current build.</li>
+     *     <li>{@link org.gradle.api.artifacts.component.ModuleComponentIdentifier} for those component instances which are found in some repository.</li>
+     * </ul>
      *
      * @return the identifier of this component
      */
     ComponentIdentifier getId();
 
     /**
-     * Returns the dependencies of this component.
-     * Includes resolved and unresolved dependencies (if any).
+     * <p>Returns the dependencies of this component. Includes resolved and unresolved dependencies (if any).
+     *
+     * <p>The elements of the returned collection are declared as {@link DependencyResult}, however the dependency instances will also implement one of the
+     * following instances:</p>
+     *
+     * <ul>
+     *     <li>{@link ResolvedDependencyResult} for dependencies which were successfully resolved.</li>
+     *     <li>{@link UnresolvedDependencyResult} for dependencies which could not be resolved for some reason.</li>
+     * </ul>
      *
      * @return the dependencies of this component
      */
     Set<? extends DependencyResult> getDependencies();
 
     /**
-     * Returns the dependents of this component.
+     * Returns the incoming dependencies of this component.
      *
      * @return the dependents of this component
      */
     Set<? extends ResolvedDependencyResult> getDependents();
 
     /**
-     * Returns the reason for selecting this component.
+     * Returns the reason why this particular component was selected in the result.
      * Useful if multiple candidate components were found during dependency resolution.
      *
      * @return the reason for selecting the component
@@ -61,7 +76,8 @@ public interface ResolvedComponentResult {
     /**
      * Returns the identifier which this component is published as.
      *
-     * @return the identifier of the component
+     * @return the identifier of the component, or {@code null} if this component is not published.
      */
+    @Nullable
     ComponentIdentifier getPublishedAs();
 }
