372a60206e3a0b703149f635b86ebe99322452ce
daz <darrell.deboer@gradleware.com>
2013-12-01 10:07:08 -0700

    Fix sample for integTest on MinGW/Cygwin

--- subprojects/docs/src/samples/native-binaries/tool-chains/src/main/cpp/main.cpp
+++ subprojects/docs/src/samples/native-binaries/tool-chains/src/main/cpp/main.cpp
@@ -3,6 +3,10 @@
 int main () {
     #if defined(__clang__)
         std::cout << "Hello from clang!" << std::endl;
+    #elif defined(__GNUC__) && defined(__MINGW32__)
+        std::cout << "Hello from mingw!" << std::endl;
+    #elif defined(__GNUC__) && defined(__CYGWIN__)
+        std::cout << "Hello from gcc cygwin!" << std::endl;
     #elif defined(__GNUC__)
         std::cout << "Hello from gcc!" << std::endl;
     #elif defined(_MSC_VER)
