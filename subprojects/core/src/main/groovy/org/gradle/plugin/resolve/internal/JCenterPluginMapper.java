79f908f636888e51c53eab66929469702dd7e430
Luke Daley <ld@ldaley.com>
2014-01-02 11:52:54 +0000

    GRADLEREV-53 - fix exception name.

--- subprojects/core/src/main/groovy/org/gradle/plugin/resolve/internal/JCenterPluginMapper.java
+++ subprojects/core/src/main/groovy/org/gradle/plugin/resolve/internal/JCenterPluginMapper.java
@@ -44,15 +44,15 @@ public class JCenterPluginMapper implements ModuleMappingPluginResolver.Mapper {
                 search();
 
         if (results.isEmpty()) {
-            throw new InvalidPluginRequest("No plugins found for plugin id " + pluginId);
+            throw new InvalidPluginRequestException("No plugins found for plugin id " + pluginId);
         }
         if (results.size() > 1) {
-            throw new InvalidPluginRequest("Found more than one plugin for plugin id " + pluginId);
+            throw new InvalidPluginRequestException("Found more than one plugin for plugin id " + pluginId);
         }
         Pkg pluginPackage = results.get(0);
         List<String> systemIds = pluginPackage.systemIds();
         if (systemIds.isEmpty()) {
-            throw new InvalidPluginRequest("No artifacts in maven layout found for plugin id" + pluginId);
+            throw new InvalidPluginRequestException("No artifacts in maven layout found for plugin id" + pluginId);
         }
         String version = request.getVersion();
         if (version == null) {
--- subprojects/core/src/main/groovy/org/gradle/plugin/resolve/internal/TomcatPluginMapper.java
+++ subprojects/core/src/main/groovy/org/gradle/plugin/resolve/internal/TomcatPluginMapper.java
@@ -23,7 +23,7 @@ public class TomcatPluginMapper implements ModuleMappingPluginResolver.Mapper {
     public Dependency map(PluginRequest request, DependencyHandler dependencyHandler) {
         if (request.getId().equals("tomcat")) {
             if (request.getVersion() == null) {
-                throw new InvalidPluginRequest("A version must be specified for the tomcat plugin");
+                throw new InvalidPluginRequestException("A version must be specified for the tomcat plugin");
             }
             String version = request.getVersion();
             return dependencyHandler.create("org.gradle.api.plugins:gradle-tomcat-plugin:" + version);
