88ef30f55b7093922f730a9f707e7d12750950b8
Benjamin Muschko <benjamin.muschko@gmail.com>
2014-01-02 11:48:00 -0500

    REVIEW-4003: Cut off AbstractRenderableDependencyResult and implement RenderableDependency instead to avoid having to use the ComponentIdentifierFactory. An unresolved dependency also doesn't have an actual.

--- subprojects/diagnostics/src/main/groovy/org/gradle/api/tasks/diagnostics/internal/graph/nodes/RenderableUnresolvedDependencyResult.java
+++ subprojects/diagnostics/src/main/groovy/org/gradle/api/tasks/diagnostics/internal/graph/nodes/RenderableUnresolvedDependencyResult.java
@@ -18,37 +18,72 @@ package org.gradle.api.tasks.diagnostics.internal.graph.nodes;
 
 import org.gradle.api.artifacts.component.ComponentIdentifier;
 import org.gradle.api.artifacts.component.ComponentSelector;
+import org.gradle.api.artifacts.component.ModuleComponentSelector;
+import org.gradle.api.artifacts.component.ProjectComponentSelector;
 import org.gradle.api.artifacts.result.UnresolvedDependencyResult;
-import org.gradle.api.internal.artifacts.component.ComponentIdentifierFactory;
+import org.gradle.api.internal.artifacts.component.DefaultModuleComponentIdentifier;
+import org.gradle.api.internal.artifacts.component.DefaultProjectComponentIdentifier;
 
 import java.util.Collections;
 import java.util.Set;
 
-public class RenderableUnresolvedDependencyResult extends AbstractRenderableDependencyResult {
-    private final ComponentIdentifier actual;
+public class RenderableUnresolvedDependencyResult implements RenderableDependency {
     private final UnresolvedDependencyResult dependency;
 
     public RenderableUnresolvedDependencyResult(UnresolvedDependencyResult dependency) {
         this.dependency = dependency;
-        this.actual = ComponentIdentifierFactory.getInstance().createIdentifier(dependency.getAttempted());
     }
 
-    @Override
     public boolean isResolvable() {
         return false;
     }
 
-    @Override
     protected ComponentSelector getRequested() {
         return dependency.getRequested();
     }
 
-    @Override
     protected ComponentIdentifier getActual() {
-        return actual;
+        return null;
     }
 
     public Set<RenderableDependency> getChildren() {
         return Collections.emptySet();
+    }
+
+    public ComponentIdentifier getId() {
+        ComponentSelector attempted = dependency.getAttempted();
+
+        if(attempted instanceof ProjectComponentSelector) {
+            return DefaultProjectComponentIdentifier.newId(((ProjectComponentSelector) attempted).getProjectPath());
+        }
+
+        ModuleComponentSelector moduleComponentSelector = (ModuleComponentSelector)attempted;
+        return DefaultModuleComponentIdentifier.newId(moduleComponentSelector.getGroup(), moduleComponentSelector.getModule(), moduleComponentSelector.getVersion());
+    }
+
+    public String getDescription() {
+        return null;
+    }
+
+    public String getName() {
+        ComponentSelector requested = dependency.getRequested();
+        ComponentSelector attempted = dependency.getAttempted();
+
+        if(requested.equals(attempted)) {
+            return requested.getDisplayName();
+        }
+
+        if(requested instanceof ModuleComponentSelector && attempted instanceof ModuleComponentSelector) {
+            ModuleComponentSelector requestedSelector = (ModuleComponentSelector)requested;
+            ModuleComponentSelector attemptedSelector = (ModuleComponentSelector)attempted;
+
+            if(requestedSelector.getGroup().equals(attemptedSelector.getGroup())
+               && requestedSelector.getModule().equals(attemptedSelector.getModule())
+               && !requestedSelector.getVersion().equals(attemptedSelector.getVersion())) {
+                return requested.getDisplayName() + " -> " + ((ModuleComponentSelector) attempted).getVersion();
+            }
+        }
+
+        return requested.getDisplayName() + " -> " + attempted.getDisplayName();
     }
 }
