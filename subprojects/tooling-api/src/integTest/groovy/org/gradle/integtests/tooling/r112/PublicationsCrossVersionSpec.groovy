ed91ab67d768021633d0c2b714c90147bf733655
Peter Niederwieser <pniederw@gmail.com>
2014-01-21 19:36:09 +0100

    changed package name to make codenarc happy

--- subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r112/PublicationsCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r112/PublicationsCrossVersionSpec.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.integtests.tooling.r1_12
+package org.gradle.integtests.tooling.r112
 
 import org.gradle.integtests.tooling.fixture.TargetGradleVersion
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
