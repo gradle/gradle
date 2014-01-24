56fd77b7a2fde776444f0058c4a510d4951308f3
Rene Groeschke <rene@breskeby.com>
2014-01-24 12:20:06 +0100

    cleanup imports

--- subprojects/cpp/src/integTest/groovy/org/gradle/nativebinaries/language/objectivec/ObjectiveCppLanguageIntegrationTest.groovy
+++ subprojects/cpp/src/integTest/groovy/org/gradle/nativebinaries/language/objectivec/ObjectiveCppLanguageIntegrationTest.groovy
@@ -22,7 +22,6 @@ import org.gradle.nativebinaries.language.cpp.fixtures.app.HelloWorldApp
 import org.gradle.nativebinaries.language.cpp.fixtures.app.ObjectiveCppHelloWorldApp
 import org.gradle.util.Requires
 import org.gradle.util.TestPrecondition
-import spock.lang.Ignore
 
 @Requires(TestPrecondition.NOT_WINDOWS)
 class ObjectiveCppLanguageIntegrationTest extends AbstractLanguageIntegrationTest {
