b8a1b04dc7f6cb8834e8aaab86565a4fbfa509c8
Adam Murdoch <adam.murdoch@gradleware.com>
2014-01-26 18:06:50 +1100

    Changed DefaultTaskArtifactStateCacheAccess so that it opens the cache and creates indexed caches eagerly, as the implementations take care of all the laziness already.

--- subprojects/core/src/main/groovy/org/gradle/api/internal/changedetection/state/DefaultTaskArtifactStateCacheAccess.java
+++ subprojects/core/src/main/groovy/org/gradle/api/internal/changedetection/state/DefaultTaskArtifactStateCacheAccess.java
@@ -19,72 +19,50 @@ import org.gradle.api.invocation.Gradle;
 import org.gradle.cache.CacheRepository;
 import org.gradle.cache.PersistentCache;
 import org.gradle.cache.PersistentIndexedCache;
+import org.gradle.cache.PersistentIndexedCacheParameters;
 import org.gradle.cache.internal.CacheDecorator;
 import org.gradle.cache.internal.FileLockManager;
-import org.gradle.cache.PersistentIndexedCacheParameters;
 import org.gradle.internal.Factory;
-import org.gradle.listener.LazyCreationProxy;
 import org.gradle.messaging.serialize.Serializer;
 
 import static org.gradle.cache.internal.filelock.LockOptionsBuilder.mode;
 
 public class DefaultTaskArtifactStateCacheAccess implements TaskArtifactStateCacheAccess {
-    private final Gradle gradle;
-    private final CacheRepository cacheRepository;
-    private CacheDecorator inMemoryDecorator;
-    private PersistentCache cache;
-    private final Object lock = new Object();
+    private final CacheDecorator inMemoryDecorator;
+    private final PersistentCache cache;
 
     public DefaultTaskArtifactStateCacheAccess(Gradle gradle, CacheRepository cacheRepository, CacheDecorator decorator) {
-        this.gradle = gradle;
-        this.cacheRepository = cacheRepository;
         this.inMemoryDecorator = decorator;
-    }
-
-    private PersistentCache getCache() {
-        //TODO SF just do it in the constructor
-        synchronized (lock) {
-            if (cache == null) {
-                cache = cacheRepository
-                        .cache(gradle, "taskArtifacts")
-                        .withDisplayName("task history cache")
-                        .withLockOptions(mode(FileLockManager.LockMode.None)) // Lock on demand
-                        .open();
-            }
-            return cache;
-        }
+        cache = cacheRepository
+                .cache(gradle, "taskArtifacts")
+                .withDisplayName("task history cache")
+                .withLockOptions(mode(FileLockManager.LockMode.None)) // Lock on demand
+                .open();
     }
 
     public void close() {
-        if (cache != null) {
-            cache.close();
-        }
+        cache.close();
     }
 
     public <K, V> PersistentIndexedCache<K, V> createCache(final String cacheName, final Class<K> keyType, final Serializer<V> valueSerializer) {
-        Factory<PersistentIndexedCache> factory = new Factory<PersistentIndexedCache>() {
-            public PersistentIndexedCache create() {
-                PersistentIndexedCacheParameters<K, V> parameters = new PersistentIndexedCacheParameters<K, V>(cacheName, keyType, valueSerializer)
-                        .cacheDecorator(inMemoryDecorator);
-                return getCache().createCache(parameters);
-            }
-        };
-        return new LazyCreationProxy<PersistentIndexedCache>(PersistentIndexedCache.class, factory).getSource();
+        PersistentIndexedCacheParameters<K, V> parameters = new PersistentIndexedCacheParameters<K, V>(cacheName, keyType, valueSerializer)
+                .cacheDecorator(inMemoryDecorator);
+        return cache.createCache(parameters);
     }
 
     public <T> T useCache(String operationDisplayName, Factory<? extends T> action) {
-        return getCache().useCache(operationDisplayName, action);
+        return cache.useCache(operationDisplayName, action);
     }
 
     public void useCache(String operationDisplayName, Runnable action) {
-        getCache().useCache(operationDisplayName, action);
+        cache.useCache(operationDisplayName, action);
     }
 
     public <T> T longRunningOperation(String operationDisplayName, Factory<? extends T> action) {
-        return getCache().longRunningOperation(operationDisplayName, action);
+        return cache.longRunningOperation(operationDisplayName, action);
     }
 
     public void longRunningOperation(String operationDisplayName, Runnable action) {
-        getCache().longRunningOperation(operationDisplayName, action);
+        cache.longRunningOperation(operationDisplayName, action);
     }
 }
