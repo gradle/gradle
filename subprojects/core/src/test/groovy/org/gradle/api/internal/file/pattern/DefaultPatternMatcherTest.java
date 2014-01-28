60172251295889d5fbcd3d70ccbcc714689eaf4a
Adam Murdoch <adam.murdoch@gradleware.com>
2014-01-28 13:06:16 +1100

    Don't use iterators in pattern matching.

--- subprojects/core/src/test/groovy/org/gradle/api/internal/file/pattern/DefaultPatternMatcherTest.java
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/file/pattern/DefaultPatternMatcherTest.java
@@ -16,18 +16,20 @@
 
 package org.gradle.api.internal.file.pattern;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
 import org.gradle.api.file.RelativePath;
+import org.junit.Test;
 
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class DefaultPatternMatcherTest {
     private DefaultPatternMatcher matcher;
     private RelativePath path;
 
 
-    @Test public void testParsing() {
+    @Test
+    public void testParsing() {
         List<PatternStep> steps;
         PatternStep step;
 
@@ -49,170 +51,140 @@ public class DefaultPatternMatcherTest {
         assertTrue(step.isGreedy());
     }
 
-    @Test public void testEmpty() {
+    @Test
+    public void testEmpty() {
         DefaultPatternMatcher matcher = new DefaultPatternMatcher(true, true);
         List<PatternStep> steps = matcher.getStepsForTest();
         assertEquals(0, steps.size());
 
         // both empty
-        RelativePath path = new RelativePath(true);
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file()));
 
         // empty matcher, non-empty path
-        path = new RelativePath(true, "a");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertFalse(matcher.isSatisfiedBy(file("a")));
 
         // non-empty matcher, empty path
         matcher = new DefaultPatternMatcher(true, true, "a");
-        path = new RelativePath(true);
-        assertFalse(matcher.isSatisfiedBy(path));
-
+        assertFalse(matcher.isSatisfiedBy(file()));
     }
 
-    @Test public void testLiterals() {
+    @Test
+    public void testLiterals() {
         matcher = new DefaultPatternMatcher(true, true, "a");
-        path = new RelativePath(true, "a");
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("a")));
+        assertFalse(matcher.isSatisfiedBy(file("b")));
 
-        path = new RelativePath(true, "b");
-        assertFalse(matcher.isSatisfiedBy(path));
-        
         matcher = new DefaultPatternMatcher(true, true, "a", "b");
-        path = new RelativePath(true, "a", "b");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "a", "c");
-        assertFalse(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "b", "c");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("a", "b")));
+        assertFalse(matcher.isSatisfiedBy(file("a", "c")));
+        assertFalse(matcher.isSatisfiedBy(file("b", "c")));
 
         // short path
-        path = new RelativePath(true, "a");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertFalse(matcher.isSatisfiedBy(file("a")));
 
         // long path
-        path = new RelativePath(true, "a", "b", "c");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertFalse(matcher.isSatisfiedBy(file("a", "b", "c")));
     }
 
-    @Test public void testPartials() {
+    @Test
+    public void testPartials() {
         matcher = new DefaultPatternMatcher(true, true, "a", "b", "c");
-        path = new RelativePath(false, "a", "b");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "a", "b");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(dir("a", "b")));
+        assertFalse(matcher.isSatisfiedBy(file("a", "b")));
 
         matcher = new DefaultPatternMatcher(false, true, "a", "b", "c");
-        path = new RelativePath(false, "a", "b");
-        assertFalse(matcher.isSatisfiedBy(path));
-
+        assertFalse(matcher.isSatisfiedBy(dir("a", "b")));
     }
 
-    @Test public void testWildCards() {
+    @Test
+    public void testWildCards() {
         matcher = new DefaultPatternMatcher(true, true, "*");
-        path = new RelativePath(true, "anything");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "anything", "b");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("anything")));
+        assertFalse(matcher.isSatisfiedBy(file("anything", "b")));
 
         matcher = new DefaultPatternMatcher(true, true, "any??ing");
-        path = new RelativePath(true, "anything");
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("anything")));
     }
 
-    @Test public void testGreedy() {
+    @Test
+    public void testGreedy() {
         matcher = new DefaultPatternMatcher(true, true, "a", "**");
-        path = new RelativePath(true, "a", "b", "c");
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("a", "b", "c")));
 
         //leading greedy
         matcher = new DefaultPatternMatcher(true, true, "**", "c");
-        path = new RelativePath(true, "a", "b", "c");
-        assertTrue(matcher.isSatisfiedBy(path));
-        
-        path = new RelativePath(true, "a", "b", "d");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("a", "b", "c")));
+        assertFalse(matcher.isSatisfiedBy(file("a", "b", "d")));
 
         // inner greedy
         matcher = new DefaultPatternMatcher(true, true, "a", "**", "c");
-        path = new RelativePath(true, "a", "b", "c");
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("a", "b", "c")));
+        assertTrue(matcher.isSatisfiedBy(file("a", "aa", "bb", "c")));
 
-        path = new RelativePath(true, "a", "aa", "bb", "c");
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(dir("a")));
+        assertTrue(matcher.isSatisfiedBy(dir("a", "b")));
+        assertTrue(matcher.isSatisfiedBy(dir("a", "aa", "bb", "d")));
+        assertTrue(matcher.isSatisfiedBy(dir("a", "c")));
+        assertTrue(matcher.isSatisfiedBy(dir("a", "b", "c")));
+        assertTrue(matcher.isSatisfiedBy(dir("a", "c", "d")));
+        assertTrue(matcher.isSatisfiedBy(dir("a", "b", "c", "d")));
 
-        path = new RelativePath(false, "a", "aa", "bb", "d");
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertFalse(matcher.isSatisfiedBy(file("a", "aa", "bb", "d")));
 
-        path = new RelativePath(true, "a", "aa", "bb", "d");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertFalse(matcher.isSatisfiedBy(dir("b", "c")));
 
         // fake trail
         matcher = new DefaultPatternMatcher(true, true, "a", "**", "c", "d");
-        path = new RelativePath(true, "a", "b", "c", "e", "c", "d");
-        assertTrue(matcher.isSatisfiedBy(path));
-        
+        assertTrue(matcher.isSatisfiedBy(file("a", "b", "c", "e", "c", "d")));
+
         // multiple greedies
         matcher = new DefaultPatternMatcher(true, true, "a", "**", "c", "**", "e");
-        path = new RelativePath(true, "a", "b", "c", "d", "e");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "a", "b", "bb", "c", "d", "e");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "a", "q", "bb", "c", "d", "c", "d", "e");
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("a", "b", "c", "d", "e")));
+        assertTrue(matcher.isSatisfiedBy(file("a", "b", "bb", "c", "d", "e")));
+        assertTrue(matcher.isSatisfiedBy(file("a", "q", "bb", "c", "d", "c", "d", "e")));
 
         // Missing greedy
         matcher = new DefaultPatternMatcher(true, true, "a", "**", "c");
         path = new RelativePath(true, "a", "c");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "a", "d");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("a", "c")));
+        assertFalse(matcher.isSatisfiedBy(file("a", "d")));
     }
 
-    @Test public void testTypical() {
+    @Test
+    public void testTypical() {
         matcher = new DefaultPatternMatcher(true, true, "**", "CVS", "*");
-        path = new RelativePath(true, "CVS", "Repository");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "org", "gradle", "CVS", "Entries");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "org", "gradle", "CVS", "foo", "bar", "Entries");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("CVS", "Repository")));
+        assertTrue(matcher.isSatisfiedBy(file("org", "gradle", "CVS", "Entries")));
+        assertFalse(matcher.isSatisfiedBy(file("org", "gradle", "CVS", "foo", "bar", "Entries")));
 
         matcher = new DefaultPatternMatcher(true, true, "src", "main", "**");
-        path = new RelativePath(true, "src", "main", "groovy", "org");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "src", "test", "groovy", "org");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("src", "main", "groovy", "org")));
+        assertFalse(matcher.isSatisfiedBy(file("src", "test", "groovy", "org")));
 
         matcher = new DefaultPatternMatcher(true, true, "**", "test", "**");
-        // below fails, trailing ** not ignored
-        path = new RelativePath(true, "src", "main", "test");
-        assertTrue(matcher.isSatisfiedBy(path));
+        assertTrue(matcher.isSatisfiedBy(file("src", "main", "test")));
+        assertTrue(matcher.isSatisfiedBy(file("src", "test", "main")));
+        assertTrue(matcher.isSatisfiedBy(file("test", "main")));
 
-        path = new RelativePath(true, "src", "test", "main");
-        assertTrue(matcher.isSatisfiedBy(path));
-
-        path = new RelativePath(true, "src", "main", "fred");
-        assertFalse(matcher.isSatisfiedBy(path));
+        assertFalse(matcher.isSatisfiedBy(file("src", "main", "fred")));
     }
 
-    @Test public void testCase() {
+    @Test
+    public void testCase() {
         matcher = new DefaultPatternMatcher(true, true, "a", "b");
-        assertTrue(matcher.isSatisfiedBy(new RelativePath(true, "a", "b")));
-        assertFalse(matcher.isSatisfiedBy(new RelativePath(true, "A", "B")));
+        assertTrue(matcher.isSatisfiedBy(file("a", "b")));
+        assertFalse(matcher.isSatisfiedBy(file("A", "B")));
 
         matcher = new DefaultPatternMatcher(true, false, "a", "b");
-        assertTrue(matcher.isSatisfiedBy(new RelativePath(true, "a", "b")));
-        assertTrue(matcher.isSatisfiedBy(new RelativePath(true, "A", "B")));
+        assertTrue(matcher.isSatisfiedBy(file("a", "b")));
+        assertTrue(matcher.isSatisfiedBy(file("A", "B")));
     }
 
+    RelativePath file(String... path) {
+        return new RelativePath(true, path);
+    }
+
+    RelativePath dir(String... path) {
+        return new RelativePath(false, path);
+    }
 }
