e89b2646c5311deb40b4a34de87ddd93aab7a9f3
Peter Niederwieser <pniederw@gmail.com>
2014-01-29 04:34:07 +0100

    fixed instantation of base plugin in unit test
    - problem only surfaced when second parameter was added to constructor (Groovy substitutes null for missing single argument)

--- subprojects/plugins/src/test/groovy/org/gradle/api/plugins/BasePluginTest.groovy
+++ subprojects/plugins/src/test/groovy/org/gradle/api/plugins/BasePluginTest.groovy
@@ -21,12 +21,14 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.PublishArtifact
+import org.gradle.api.internal.artifacts.ivyservice.projectmodule.ProjectPublicationRegistry
 import org.gradle.api.internal.plugins.DefaultArtifactPublicationSet
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.Upload
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.bundling.Tar
 import org.gradle.api.tasks.bundling.Zip
+import org.gradle.configuration.project.ProjectConfigurationActionContainer
 import org.gradle.util.TestUtil
 import spock.lang.Specification
 
@@ -35,7 +37,7 @@ import static org.hamcrest.Matchers.instanceOf
 
 class BasePluginTest extends Specification {
     private final Project project = TestUtil.createRootProject()
-    private final BasePlugin plugin = new BasePlugin()
+    private final BasePlugin plugin = new BasePlugin(Stub(ProjectPublicationRegistry), Stub(ProjectConfigurationActionContainer))
 
     public void addsConventionObjects() {
         when:
