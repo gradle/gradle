e5e4f0adc109f31415a52ce60484f01733234f6c
daz <darrell.deboer@gradleware.com>
2014-02-04 17:26:20 -0700

    Sample output is different on windows, so it's impossible to include in User Guide presently.

--- subprojects/docs/src/samples/native-binaries/cunit/src/operatorsTest/cunit/test_plus.c
+++ subprojects/docs/src/samples/native-binaries/cunit/src/operatorsTest/cunit/test_plus.c
@@ -1,7 +1,7 @@
 #include <CUnit/Basic.h>
 #include "operators.h"
 
-void test_plus(void) {
+void test_plus() {
   CU_ASSERT(plus(0, 2) == 2);
   CU_ASSERT(plus(0, -2) == -2);
   CU_ASSERT(plus(2, 2) == 4);
