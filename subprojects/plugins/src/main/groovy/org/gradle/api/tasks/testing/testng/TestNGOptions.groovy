42316f4de6413b435c71e6c3b951a31d14361fe9
Kamil Szymanski <kamil.szymanski.dev@gmail.com>
2014-02-07 16:03:03 +0100

    TestNGOptions parallel property javadoc fix

--- subprojects/plugins/src/main/groovy/org/gradle/api/tasks/testing/testng/TestNGOptions.groovy
+++ subprojects/plugins/src/main/groovy/org/gradle/api/tasks/testing/testng/TestNGOptions.groovy
@@ -85,7 +85,7 @@ class TestNGOptions extends TestFrameworkOptions {
     Set<String> listeners = new LinkedHashSet<String>()
 
     /**
-     * The parallel mode to use for running the tests - either methods or tests.
+     * The parallel mode to use for running the tests - one of the following modes: methods, tests, classes or instances.
      *
      * Not required.
      *
