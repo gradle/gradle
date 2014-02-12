8fd49b193e4dc13683201ee2927093dc328a7c81
Rene Groeschke <rene@breskeby.com>
2014-02-12 21:53:21 +0100

    Add coverage for objective-c objective-cpp on unsupported platform (visual studio)

---
+++ subprojects/cpp/src/integTest/groovy/org/gradle/nativebinaries/language/objectivec/ObjectiveCUnsupportedIntegrationTest.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.nativebinaries.language.objectivec
+
+import org.gradle.nativebinaries.language.cpp.fixtures.AbstractInstalledToolChainIntegrationSpec
+import org.gradle.nativebinaries.language.cpp.fixtures.RequiresInstalledToolChain
+import org.gradle.nativebinaries.language.cpp.fixtures.ToolChainRequirement
+import org.gradle.nativebinaries.language.cpp.fixtures.app.ObjectiveCHelloWorldApp
+
+import static org.hamcrest.CoreMatchers.containsString
+
+@RequiresInstalledToolChain(ToolChainRequirement.VisualCpp)
+class ObjectiveCUnsupportedIntegrationTest extends AbstractInstalledToolChainIntegrationSpec{
+
+    def helloWorldApp = new ObjectiveCHelloWorldApp();
+
+    def "setup"() {
+        buildFile << helloWorldApp.pluginScript
+        buildFile << helloWorldApp.extraConfiguration
+    }
+
+    def "fails with decent error message with visual studio toolchain"() {
+        given:
+        buildFile << """
+            executables {
+                main {}
+            }
+        """
+
+        and:
+        helloWorldApp.writeSources(file("src/main"))
+
+        when:
+        fails "compileMainExecutableMainObjc"
+
+        then:
+        failure.assertThatCause(containsString("Objective-C is not available on the Visual C++ toolchain"))
+    }
+}
