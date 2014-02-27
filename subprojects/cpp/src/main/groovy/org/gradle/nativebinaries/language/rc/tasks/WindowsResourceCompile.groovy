7ccb73e75c50661beba83a8a45df928ac5fc7b6e
daz <darrell.deboer@gradleware.com>
2014-02-27 10:46:31 -0700

    FIX: Update WindowsResourceCompile task for recent changes

--- subprojects/cpp/src/main/groovy/org/gradle/nativebinaries/language/rc/tasks/WindowsResourceCompile.groovy
+++ subprojects/cpp/src/main/groovy/org/gradle/nativebinaries/language/rc/tasks/WindowsResourceCompile.groovy
@@ -18,12 +18,13 @@ package org.gradle.nativebinaries.language.rc.tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.Incubating
 import org.gradle.api.file.FileCollection
+import org.gradle.api.internal.changedetection.state.FileSnapshotter
+import org.gradle.api.internal.changedetection.state.TaskArtifactStateCacheAccess
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.incremental.IncrementalTaskInputs
-import org.gradle.cache.CacheRepository
 import org.gradle.nativebinaries.language.c.internal.incremental.IncrementalCompilerBuilder
 import org.gradle.nativebinaries.language.rc.internal.DefaultWindowsResourceCompileSpec
 import org.gradle.nativebinaries.platform.Platform
@@ -39,8 +40,9 @@ class WindowsResourceCompile extends DefaultTask {
     private final IncrementalCompilerBuilder incrementalCompilerBuilder
 
     @Inject
-    WindowsResourceCompile(CacheRepository cacheRepository) {
-        incrementalCompilerBuilder = new IncrementalCompilerBuilder(cacheRepository, this)
+    WindowsResourceCompile() {
+        incrementalCompilerBuilder = new IncrementalCompilerBuilder(services.get(TaskArtifactStateCacheAccess),
+                                                                    services.get(FileSnapshotter), this)
         includes = project.files()
         source = project.files()
     }
