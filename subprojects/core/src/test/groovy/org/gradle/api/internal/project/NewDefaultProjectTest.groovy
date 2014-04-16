5d3821a7e371dd79ad88292da4185c15d00ca306
Szczepan Faber <szczepiq@gmail.com>
2014-04-16 20:46:26 +0200

    Cleaned up some test code debt, moved over a couple of unit tests from DefaultProjectTest. I need that for a next change I'm planning to do.

--- subprojects/core/src/test/groovy/org/gradle/api/internal/project/NewDefaultProjectTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/project/NewDefaultProjectTest.groovy
@@ -16,22 +16,30 @@
 
 package org.gradle.api.internal.project
 
+import org.gradle.api.DefaultTask
+import org.gradle.api.InvalidUserDataException
+import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.artifacts.dsl.ArtifactHandler
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.internal.artifacts.configurations.ConfigurationContainerInternal
 import org.gradle.util.TestUtil
+import org.junit.Test
 import spock.lang.Specification
+
+import static org.gradle.util.TestUtil.createChildProject
+import static org.junit.Assert.assertEquals
 
 class NewDefaultProjectTest extends Specification {
 
-    def project = TestUtil.createRootProject()
+    DefaultProject root = TestUtil.createRootProject()
 
     void "delegates to artifacts handler"() {
         def handler = Mock(ArtifactHandler)
-        project.artifactHandler = handler
+        root.artifactHandler = handler
 
         when:
-        project.artifacts {
+        root.artifacts {
             add('conf', 'art')
         }
 
@@ -41,10 +49,10 @@ class NewDefaultProjectTest extends Specification {
 
     void "delegates to dependency handler"() {
         def handler = Mock(DependencyHandler)
-        project.dependencyHandler = handler
+        root.dependencyHandler = handler
 
         when:
-        project.dependencies {
+        root.dependencies {
             add('conf', 'dep')
         }
 
@@ -55,12 +63,91 @@ class NewDefaultProjectTest extends Specification {
     void "delegates to configuration container"() {
         Closure cl = {}
         def container = Mock(ConfigurationContainerInternal)
-        project.configurationContainer = container
+        root.configurationContainer = container
 
         when:
-        project.configurations cl
+        root.configurations cl
 
         then:
         1 * container.configure(cl)
+    }
+
+    def "provides all tasks recursively"() {
+        def a = createChildProject(root, "a")
+
+        [root, a].each { it.task "foo"; it.task "bar" }
+
+        when:
+        def rootTasks = root.getAllTasks(true)
+        def aTasks = a.getAllTasks(true)
+
+        then:
+        rootTasks.size() == 2
+        rootTasks[root]*.path == [":bar", ":foo"]
+        rootTasks[a]*.path == [":a:bar", ":a:foo"]
+
+        aTasks.size() == 1
+        aTasks[a]*.path == [":a:bar", ":a:foo"]
+    }
+
+    def "provides all tasks non-recursively"() {
+        def a = createChildProject(root, "a")
+
+        [root, a].each { it.task "foo"; it.task "bar" }
+
+        when:
+        def rootTasks = root.getAllTasks(false)
+        def aTasks = a.getAllTasks(false)
+
+        then:
+        rootTasks.size() == 1
+        rootTasks[root]*.path == [":bar", ":foo"]
+
+        aTasks.size() == 1
+        aTasks[a]*.path == [":a:bar", ":a:foo"]
+    }
+
+    def "provides task by name recursively"() {
+        def a = createChildProject(root, "a")
+
+        [root, a].each { it.task "foo"; it.task "bar" }
+
+        when:
+        def rootTasks = root.getTasksByName("foo", true)
+        def aTasks = a.getTasksByName("bar", true)
+
+        then:
+        rootTasks*.path == [":foo", ":a:foo"]
+        aTasks*.path == [":a:bar"]
+    }
+
+    def "provides task by name non-recursively"() {
+        def a = createChildProject(root, "a")
+
+        [root, a].each { it.task "foo"; it.task "bar" }
+
+        when:
+        def rootTasks = root.getTasksByName("foo", false)
+        def aTasks = a.getTasksByName("bar", false)
+
+        then:
+        rootTasks*.path == [":foo"]
+        aTasks*.path == [":a:bar"]
+    }
+
+    def "does not allow asking for tasks using empty name"() {
+        when: root.getTasksByName('', true)
+        then: thrown(InvalidUserDataException)
+
+        when: root.getTasksByName(null, true)
+        then: thrown(InvalidUserDataException)
+    }
+
+    def "allows asking for unknown tasks"() {
+        root.task "bar"
+
+        expect:
+        root.getTasksByName('foo', true).empty
+        root.getTasksByName('foo', false).empty
     }
 }
