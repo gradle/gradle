14f2a80aecf144a341cc28f451c17a791b193db7
Adam Murdoch <adam.murdoch@gradleware.com>
2014-04-29 13:31:38 +1000

    Changed the objective-c and objective-cpp samples so that they don't produce warnings when run on linux.

--- subprojects/docs/src/samples/native-binaries/objective-c/src/main/objc/main.m
+++ subprojects/docs/src/samples/native-binaries/objective-c/src/main/objc/main.m
@@ -2,9 +2,14 @@
 
 int main (int argc, const char * argv[])
 {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
     NSString *helloWorld = @"Hello world!\n";
     NSFileHandle *stdout = [NSFileHandle fileHandleWithStandardOutput];
     NSData *strData = [helloWorld dataUsingEncoding: NSASCIIStringEncoding];
     [stdout writeData: strData];
+
+    [pool drain];
+
     return 0;
 }
--- subprojects/docs/src/samples/native-binaries/objective-cpp/src/main/objcpp/main.mm
+++ subprojects/docs/src/samples/native-binaries/objective-cpp/src/main/objcpp/main.mm
@@ -1,14 +1,16 @@
-#define __STDC_LIMIT_MACROS
-#include <stdint.h>
 #import <Foundation/Foundation.h>
 #include <iostream>
 
 int main (int argc, const char * argv[])
 {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
     NSFileHandle *stdout = [NSFileHandle fileHandleWithStandardOutput];
     NSData *strData = [@"Hello " dataUsingEncoding: NSASCIIStringEncoding];
     [stdout writeData: strData];
     std::cout << "world!" << std::endl;
+
+    [pool drain];
 
     return 0;
 }
