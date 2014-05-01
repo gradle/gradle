023e0c79ebe5d786a7fb4659f72c5f491874f859
Adam Murdoch <adam.murdoch@gradleware.com>
2014-05-01 19:34:50 +1000

    Changed the objective-c++ sample to be a bit more objective-c++ like.

--- subprojects/docs/src/samples/native-binaries/objective-cpp/src/main/objcpp/main.mm
+++ subprojects/docs/src/samples/native-binaries/objective-cpp/src/main/objcpp/main.mm
@@ -1,16 +1,12 @@
-#define __STDC_LIMIT_MACROS
-#include <stdint.h>
-#import <Foundation/Foundation.h>
-#include <iostream>
+#import "greeter.h"
 
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-    NSFileHandle *stdout = [NSFileHandle fileHandleWithStandardOutput];
-    NSData *strData = [@"Hello " dataUsingEncoding: NSASCIIStringEncoding];
-    [stdout writeData: strData];
-    std::cout << "world!" << std::endl;
+    Greeter *greeter = [Greeter new];
+    [greeter sayHello];
+    [greeter release];
 
     [pool drain];
 
