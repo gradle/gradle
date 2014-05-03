be8ba5a75a99e9fa2c9bfcfdb7bea2bc591436b8
Adam Murdoch <adam.murdoch@gradleware.com>
2014-05-04 09:17:07 +1000

    Renamed an int test class.

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/BuildDefaultCharacterEncodingIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/BuildDefaultCharacterEncodingIntegrationTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 the original author or authors.
+ * Copyright 2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ import spock.lang.Unroll
 
 import java.nio.charset.Charset
 
-class CharacterEncodingIntegTest extends AbstractIntegrationSpec {
+class BuildDefaultCharacterEncodingIntegrationTest extends AbstractIntegrationSpec {
 
     def executerEncoding(String inputEncoding) {
         if (inputEncoding) {
