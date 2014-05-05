c29158a773af9fc362d4be505e1b94aa112370b8
Peter Niederwieser <pniederw@gmail.com>
2014-05-06 01:19:10 +0200

    added comment

--- subprojects/core-impl/src/integTest/groovy/org/gradle/integtests/resolve/portal/PluginPortalClientIntegrationTest.groovy
+++ subprojects/core-impl/src/integTest/groovy/org/gradle/integtests/resolve/portal/PluginPortalClientIntegrationTest.groovy
@@ -33,6 +33,7 @@ public class PluginPortalClientIntegrationTest extends AbstractIntegrationSpec {
     }
 
     def "plugin declared in plugins {} block gets resolved from portal and applied"() {
+        // communicates test portal hostname/port to PluginPortalClient
         def pluginVersion = "test_localhost_${server.port}_1.0"
 
         TestFile metadataFile = generatePortalResponse(pluginVersion)
