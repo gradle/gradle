86d3060c309a196e4a3091335f08fa61d994cda7
Adam Murdoch <adam.murdoch@gradleware.com>
2014-05-06 16:49:39 +1000

    Moved some int tests to live in the core project.

--- subprojects/core/src/integTest/groovy/org/gradle/api/BuildDefaultCharacterEncodingIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/BuildDefaultCharacterEncodingIntegrationTest.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.integtests
+package org.gradle.api
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import spock.lang.Unroll
