e2a7e11284fc98e7e6776bbd6f081a077ffee20b
Adam Murdoch <adam.murdoch@gradleware.com>
2014-05-08 06:56:08 +1000

    Don't discard exception

--- subprojects/core/src/main/groovy/org/gradle/api/internal/ClosureBackedAction.java
+++ subprojects/core/src/main/groovy/org/gradle/api/internal/ClosureBackedAction.java
@@ -21,6 +21,8 @@ import org.gradle.api.Action;
 import org.gradle.api.InvalidActionClosureException;
 import org.gradle.util.Configurable;
 
+import java.util.Objects;
+
 public class ClosureBackedAction<T> implements Action<T> {
     private final Closure closure;
     private final boolean configureableAware;
@@ -47,7 +49,7 @@ public class ClosureBackedAction<T> implements Action<T> {
 
         try {
             if (configureableAware && delegate instanceof Configurable) {
-                ((Configurable)delegate).configure(closure);
+                ((Configurable) delegate).configure(closure);
             } else {
                 Closure copy = (Closure) closure.clone();
                 copy.setResolveStrategy(resolveStrategy);
@@ -59,9 +61,10 @@ public class ClosureBackedAction<T> implements Action<T> {
                 }
             }
         } catch (groovy.lang.MissingMethodException e) {
-            if (e.getType().equals(closure.getClass()) && e.getMethod().equals("doCall")) {
+            if (Objects.equals(e.getType(), closure.getClass()) && Objects.equals(e.getMethod(), "doCall")) {
                 throw new InvalidActionClosureException(closure, delegate);
             }
+            throw e;
         }
     }
 
