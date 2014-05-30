8eb3270c9950fcda2c464582ae3ea6fe3229665f
Peter Niederwieser <pniederw@gmail.com>
2014-05-30 08:36:19 +0200

    fixes after porting class to java

--- subprojects/scala/src/main/groovy/org/gradle/api/tasks/ScalaRuntime.java
+++ subprojects/scala/src/main/groovy/org/gradle/api/tasks/ScalaRuntime.java
@@ -53,10 +53,9 @@ import java.util.regex.Pattern;
 public class ScalaRuntime {
     private static final Pattern SCALA_JAR_PATTERN = Pattern.compile("scala-(\\w.*?)-(\\d.*).jar");
 
-    // should be private but Groovy can't handle this
-    protected final Project project;
+    private final Project project;
 
-    ScalaRuntime(Project project) {
+    public ScalaRuntime(Project project) {
         this.project = project;
     }
 
@@ -127,7 +126,7 @@ public class ScalaRuntime {
     public File findScalaJar(Iterable<File> classpath, String appendix) {
         for (File file : classpath) {
             Matcher matcher = SCALA_JAR_PATTERN.matcher(file.getName());
-            if (matcher.matches() && matcher.group(1) == appendix) {
+            if (matcher.matches() && matcher.group(1).equals(appendix)) {
                 return file;
             }
         }
