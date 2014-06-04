0b4d197afcd800847b879746e62ade9da7b7e0aa
Adam Murdoch <adam.murdoch@gradleware.com>
2014-06-04 18:12:11 +1000

    Spockify test.

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesCodeQualityIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesCodeQualityIntegrationTest.groovy
@@ -15,28 +15,26 @@
  */
 package org.gradle.integtests.samples
 
-import org.gradle.integtests.fixtures.AbstractIntegrationTest
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.Sample
 import org.gradle.test.fixtures.file.TestFile
-import org.gradle.util.PreconditionVerifier
 import org.gradle.util.Requires
 import org.gradle.util.TestPrecondition
 import org.junit.Rule
-import org.junit.Test
 
 // FindBugs does not support Java 8 yet
 @Requires(TestPrecondition.JDK7_OR_EARLIER)
-class SamplesCodeQualityIntegrationTest extends AbstractIntegrationTest {
-    @Rule public PreconditionVerifier verifier = new PreconditionVerifier()
-    @Rule public final Sample sample = new Sample(testDirectoryProvider, 'codeQuality')
+class SamplesCodeQualityIntegrationTest extends AbstractIntegrationSpec {
+    @Rule public final Sample sample = new Sample(temporaryFolder, 'codeQuality')
 
-    @Test
-    public void checkReportsGenerated() {
+    def checkReportsGenerated() {
         TestFile projectDir = sample.dir
         TestFile buildDir = projectDir.file('build')
 
+        when:
         executer.inDirectory(projectDir).requireGradleHome().withTasks('check').run()
 
+        then:
         buildDir.file('reports/checkstyle/main.xml').assertIsFile()
         buildDir.file('reports/codenarc/main.html').assertIsFile()
         buildDir.file('reports/codenarc/test.html').assertIsFile()
