8142d5d1c660cb75cfff4cf886ae509ee268b200
Adam Murdoch <adam.murdoch@gradleware.com>
2014-06-05 12:36:53 +1000

    Improvements to javadoc parsing to deal with '<<' in code snippets.

--- buildSrc/src/main/groovy/org/gradle/build/docs/dsl/docbook/BasicJavadocLexer.java
+++ buildSrc/src/main/groovy/org/gradle/build/docs/dsl/docbook/BasicJavadocLexer.java
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
  * Converts the main description of a javadoc comment into a stream of tokens.
  */
 class BasicJavadocLexer implements JavadocLexer {
-    private static final Pattern HTML_ELEMENT = Pattern.compile("(?s)<\\\\?.+?>");
+    private static final Pattern HTML_ELEMENT = Pattern.compile("(?s)<\\\\?[^<]+?>");
     private static final Pattern ELEMENT_ATTRIBUTE = Pattern.compile("(?s)\\w+(\\s*=\\s*('.*?')|(\".*?\"))?");
     private static final Pattern END_ATTRIBUTE_NAME = Pattern.compile("=|(\\s)|(/>)|>");
     private static final Pattern ATTRIBUTE_SEPARATOR = Pattern.compile("\\s*=\\s*");
