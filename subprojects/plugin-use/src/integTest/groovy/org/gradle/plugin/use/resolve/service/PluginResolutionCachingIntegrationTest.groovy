d91e144ba3cc8b73875c592af60293672afc1bee
Luke Daley <ld@ldaley.com>
2014-08-22 12:27:41 +1000

    Don't check for location of cache as indicator for cache scope of plugin resolution cache.
    +review REVIEW-5078

--- subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/use/resolve/service/PluginResolutionCachingIntegrationTest.groovy
+++ subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/use/resolve/service/PluginResolutionCachingIntegrationTest.groovy
@@ -17,7 +17,6 @@
 package org.gradle.plugin.use.resolve.service
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
-import org.gradle.plugin.use.internal.PluginUsePluginServiceRegistry
 import org.gradle.test.fixtures.plugin.PluginBuilder
 import org.gradle.test.fixtures.server.http.MavenHttpModule
 import org.junit.Rule
@@ -57,10 +56,6 @@ class PluginResolutionCachingIntegrationTest extends AbstractIntegrationSpec {
 
         reset()
         build()
-
-        // test that the right scoped cache was created.
-        // would be nice to have a less fragile way to do this.
-        new File(executer.gradleUserHomeDir, "caches/${executer.distribution.version.version}/${PluginUsePluginServiceRegistry.CACHE_NAME}".toString()).exists()
     }
 
     def "--refresh-dependencies invalidates cache"() {
