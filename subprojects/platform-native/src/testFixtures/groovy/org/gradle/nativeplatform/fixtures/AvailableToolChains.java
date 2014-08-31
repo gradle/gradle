5d4e3d0c7b3c99af2f085debd85651636b8424c2
Adam Murdoch <adam.murdoch@gradleware.com>
2014-08-31 12:47:38 +1000

    Fixed test fixture for api changes.

--- subprojects/platform-native/src/testFixtures/groovy/org/gradle/nativeplatform/fixtures/AvailableToolChains.java
+++ subprojects/platform-native/src/testFixtures/groovy/org/gradle/nativeplatform/fixtures/AvailableToolChains.java
@@ -148,7 +148,7 @@ public class AvailableToolChains {
         List<File> gppCandidates = OperatingSystem.current().findAllInPath("g++");
         for (int i = 0; i < gppCandidates.size(); i++) {
             File candidate = gppCandidates.get(i);
-            GccVersionResult version = versionDeterminer.getGccMetaData(candidate);
+            GccVersionResult version = versionDeterminer.getGccMetaData(candidate, Collections.<String>emptyList());
             if (version.isAvailable()) {
                 InstalledGcc gcc = new InstalledGcc("gcc");
                 if (i > 0) {
