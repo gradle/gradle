d234e4c7eae63338321a56e5c654ab2a540a337c
Luke Daley <ld@ldaley.com>
2014-09-04 15:08:02 +1000

    Expanded test coverage for transform.
     +review REVIEW-5177

--- subprojects/model-groovy/src/integTest/groovy/org/gradle/model/dsl/internal/transform/ModelBlockTransformSpec.groovy
+++ subprojects/model-groovy/src/integTest/groovy/org/gradle/model/dsl/internal/transform/ModelBlockTransformSpec.groovy
@@ -18,10 +18,136 @@ package org.gradle.model.dsl.internal.transform
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 
+import static org.hamcrest.Matchers.containsString
+
 class ModelBlockTransformSpec extends AbstractIntegrationSpec {
 
     def setup() {
         executer.requireOwnGradleUserHomeDir()
+    }
+
+    def "only literal strings can be given to dollar - #code"() {
+        when:
+        buildScript """
+        model {
+          foo {
+            $code
+          }
+        }
+        """
+
+        then:
+        fails "tasks"
+        failure.assertHasLineNumber 4
+        failure.assertHasFileName("Build file '${buildFile}'")
+        failure.assertThatCause(containsString(RuleClosureVisitor.INVALID_ARGUMENT_LIST))
+
+        where:
+        code << [
+            '$(1)',
+            '$("$name")',
+            '$("a" + "b")',
+            'def a = "foo"; $(a)',
+            '$("foo", "bar")',
+        ]
+    }
+
+    def "dollar method is only detected with no explicit receiver"() {
+        when:
+        buildScript """
+            import org.gradle.model.*
+
+            class MyPlugin implements Plugin<Project> {
+              void apply(Project project) {}
+              @RuleSource
+              static class Rules {
+                @Model
+                String foo() {
+                  "foo"
+                }
+              }
+            }
+
+            apply plugin: MyPlugin
+
+            model {
+              foo {
+                $code
+              }
+            }
+        """
+
+        then:
+        succeeds "tasks" // succeeds because we don't fail on invalid usage, and don't fail due to unbound inputs
+
+        where:
+        code << [
+            'something.$(1)',
+            'this.$("$name")',
+            'foo.bar().$("a" + "b")',
+        ]
+    }
+
+    def "input references are found in nested code - #code"() {
+        when:
+        buildScript """
+            import org.gradle.model.*
+            import org.gradle.model.collection.*
+
+            class MyPlugin implements Plugin<Project> {
+                void apply(Project project) {}
+                @RuleSource
+                static class Rules {
+                    @Mutate void addPrintTask(CollectionBuilder<Task> tasks, List<String> strings) {
+                        tasks.create("printMessage", PrintTask) {
+                            it.message = strings
+                        }
+                    }
+
+                    @Model String foo() {
+                        "foo"
+                    }
+
+                    @Model List<String> strings() {
+                        []
+                    }
+                }
+            }
+
+            class PrintTask extends DefaultTask {
+                String message
+
+                @TaskAction
+                void doPrint() {
+                    println "message: " + message
+                }
+            }
+
+            apply plugin: MyPlugin
+
+            model {
+                strings {
+                    $code
+                }
+            }
+        """
+
+        then:
+        succeeds "printMessage"
+        output.contains("message: [foo]")
+
+        where:
+        code << [
+            'if (true) { add $("foo") }',
+            'if (false) {} else if (true) { add $("foo") }',
+            'if (false) {} else { add $("foo") }',
+            'def i = true; while(i) { add $("foo"); i = false }',
+            '[1].each { add $("foo") }',
+            'add "${$("foo")}"',
+            'def v = $("foo"); add(v)',
+            'add($("foo"))',
+            'add($("foo").toString())',
+        ]
     }
 
     def "model blocks are detected"() {
