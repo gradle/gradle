ef4f06c46f5c8060df677c9f340ed038bf0baba5
Björn Kautler <Bjoern@Kautler.net>
2014-09-25 19:28:01 +0200

    Create the test results folder to prevent SonarQube from emitting a warning if a project does not contain any tests, but production sources

---
+++ subprojects/sonar/src/integTest/resources/org/gradle/sonar/runner/SonarRunnerSmokeIntegrationTest/shared/javaProjectWithoutTestClasses/src/main/java/Production1.java
@@ -0,0 +1,12 @@
+public class Production1 {
+    private final String property;
+
+    public Production1(String param) {
+        this.property = param;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+}
