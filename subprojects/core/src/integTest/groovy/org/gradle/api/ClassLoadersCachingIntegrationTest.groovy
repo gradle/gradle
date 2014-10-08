bf8762ac205fba04cd2a10f9100ebcdbe913bed5
Szczepan Faber <szczepiq@gmail.com>
2014-10-08 21:58:15 +0200

    Renamed the file (checkstyle).

--- subprojects/core/src/integTest/groovy/org/gradle/api/ClassLoadersCachingIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/ClassLoadersCachingIntegrationTest.groovy
@@ -22,7 +22,7 @@ import spock.lang.IgnoreIf
 
 //classloaders are cached in process so the test only makes sense if gradle invocations share the process
 @IgnoreIf({ !GradleContextualExecuter.longLivingProcess })
-class CachingClassLoadersIntegrationTest extends AbstractIntegrationSpec {
+class ClassLoadersCachingIntegrationTest extends AbstractIntegrationSpec {
 
     def setup() {
         executer.requireIsolatedDaemons()
