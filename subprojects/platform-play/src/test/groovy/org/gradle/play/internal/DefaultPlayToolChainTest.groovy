af38e2c8134334b7f17a9537cb2decd4d83af53d
Rene Groeschke <rene@breskeby.com>
2014-10-23 19:40:11 +0200

    add some unit test coverage to DefaultPlayToolChain
    +review REVIEW-5241

---
+++ subprojects/platform-play/src/test/groovy/org/gradle/play/internal/DefaultPlayToolChainTest.groovy
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.play.internal
+
+import org.gradle.internal.jvm.Jvm
+import spock.lang.Specification
+
+class DefaultPlayToolChainTest extends Specification {
+    DefaultPlayToolChain toolChain = new DefaultPlayToolChain("2.3.2")
+
+    def "provides meaningful name"() {
+        expect:
+        toolChain.getName() == "PlayFramework2.3.2"
+    }
+
+    def "provides meaningful displayname"() {
+        def javaVersion = Jvm.current().javaVersion
+        expect:
+        toolChain.getDisplayName() == "Play Framework 2.3.2 / JDK ${javaVersion.majorVersion} ($javaVersion)"
+    }
+}
