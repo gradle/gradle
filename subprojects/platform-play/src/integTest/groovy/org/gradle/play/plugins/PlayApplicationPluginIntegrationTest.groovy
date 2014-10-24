a15675298bff3d8bbe02a8e37a2f9557b51b3e4d
Rene Groeschke <rene@breskeby.com>
2014-10-24 17:33:03 +0200

    fix integration tests on java <> 7
    +review REVIEW-5241

--- subprojects/platform-play/src/integTest/groovy/org/gradle/play/plugins/PlayApplicationPluginIntegrationTest.groovy
+++ subprojects/platform-play/src/integTest/groovy/org/gradle/play/plugins/PlayApplicationPluginIntegrationTest.groovy
@@ -50,7 +50,7 @@ Source sets
 Binaries
     DefaultPlayApplicationBinarySpec 'myAppBinary'
         build using task: :myAppBinary
-        platform: java7
+        platform: java${javaVersion.majorVersion}
         tool chain: Play Framework 2.11-2.3.5 (JDK ${javaVersion.majorVersion} (${javaVersion.toString()})"""))
     }
 
