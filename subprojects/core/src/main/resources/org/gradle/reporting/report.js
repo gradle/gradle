268d732ae7cdf032a280e4b9d4a90693b40ba016
Wojciech Gawronski <afronski@gmail.com>
2014-10-25 14:08:01 +0200

    Toggle for wrapping longer lines in 'code' blocks.
    - If browser supports `localStorage` on `file://` protocol at the bottom of the page check-box will appear, which enables/disables line wrapping.
    - Tested on recent versions of desktop browsers: Opera, Firefox Google Chrome and Chromium - everything works, settings are shared across all Gradle reports.
      - However IE (even the newest versions) even if it supports `localStorage` it does not work with `file://` protocol.
        - So feature will not work on IE right now, but it is completely unobtrusive - it checks the ability to read / write `localStorage` and do not detect the browser vendor/name.
        - If in future any browser (IE or any other untested by me) will enable API compliant with `localStorage` and it will work properly with `file://` protocol, feature will work automatically on such environment.
    - If user will clean browser cache, this setting will be purged as well.
    - Visually check-box is not distinctive from the footer, but is a part of it.

--- subprojects/core/src/main/resources/org/gradle/reporting/report.js
+++ subprojects/core/src/main/resources/org/gradle/reporting/report.js
@@ -1,101 +1,246 @@
-var tabs = new Object();
+(function (window, document) {
+    "use strict";
 
-function initTabs() {
-    var container = document.getElementById('tabs');
-    tabs.tabs = findTabs(container);
-    tabs.titles = findTitles(tabs.tabs);
-    tabs.headers = findHeaders(container);
-    tabs.select = select;
-    tabs.deselectAll = deselectAll;
-    tabs.select(0);
-    return true;
-}
+    var GRADLE_LINE_WRAPPING_KEY = "gradle.line-wrapping";
 
-window.onload = initTabs;
+    var tabs = {};
+    var storage = false;
 
-function switchTab() {
-    var id = this.id.substr(1);
-    for (var i = 0; i < tabs.tabs.length; i++) {
-        if (tabs.tabs[i].id == id) {
-            tabs.select(i);
-            break;
+    function changeElementClass(element, classValue) {
+        if (element.getAttribute("className")) {
+            element.setAttribute("className", classValue);
+        } else {
+            element.setAttribute("class", classValue);
         }
     }
-    return false;
-}
 
-function select(i) {
-    this.deselectAll();
-    changeElementClass(this.tabs[i], 'tab selected');
-    changeElementClass(this.headers[i], 'selected');
-    while (this.headers[i].firstChild) {
-        this.headers[i].removeChild(this.headers[i].firstChild);
+    function getClassAttribute(element) {
+        if (element.getAttribute("className")) {
+            return element.getAttribute("className");
+        } else {
+            return element.getAttribute("class");
+        }
     }
-    var h2 = document.createElement('H2');
-    h2.appendChild(document.createTextNode(this.titles[i]));
-    this.headers[i].appendChild(h2);
-}
 
-function deselectAll() {
-    for (var i = 0; i < this.tabs.length; i++) {
-        changeElementClass(this.tabs[i], 'tab deselected');
-        changeElementClass(this.headers[i], 'deselected');
+    function addClass(element, classValue) {
+        changeElementClass(element, getClassAttribute(element) + " " + classValue);
+    }
+
+    function removeClass(element, classValue) {
+        changeElementClass(element, getClassAttribute(element).replace(classValue, ""));
+    }
+
+    function initTabs() {
+        var container = document.getElementById("tabs");
+
+        tabs.tabs = findTabs(container);
+        tabs.titles = findTitles(tabs.tabs);
+        tabs.headers = findHeaders(container);
+        tabs.select = select;
+        tabs.deselectAll = deselectAll;
+        tabs.select(0);
+
+        return true;
+    }
+
+    function getCheckBox() {
+        return document.getElementById("line-wrapping-toggle");
+    }
+
+    function getLabelForCheckBox() {
+        return document.getElementById("label-for-line-wrapping-toggle");
+    }
+
+    function checkIsStorageAvailable() {
+        try {
+            var key = "testField";
+            var expected = "true";
+            var actual;
+
+            window.localStorage.setItem(key, expected);
+            actual = window.localStorage.getItem(key);
+            window.localStorage.removeItem(key);
+
+            return expected === actual;
+        } catch (exception) {
+            return false;
+        }
+    }
+
+    function storeState(state) {
+        if (storage) {
+            storage.setItem(GRADLE_LINE_WRAPPING_KEY, state.toString());
+        }
+    }
+
+    function getState() {
+        if (storage) {
+            return storage.getItem(GRADLE_LINE_WRAPPING_KEY) === "true";
+        }
+
+        return false;
+    }
+
+    function initializeFromStorage() {
+        var checkBox = getCheckBox();
+        var state = getState();
+
+        if (state) {
+            checkBox.checked = true;
+
+            storeState(true);
+            forAllCodeBlocks(addClass);
+        } else {
+            checkBox.checked = false;
+
+            storeState(false);
+            forAllCodeBlocks(removeClass);
+        }
+    }
+
+    function initStorage() {
+        if ("localStorage" in window) {
+            if (checkIsStorageAvailable()) {
+                storage = window.localStorage;
+                initializeFromStorage();
+            }
+        }
+    }
+
+    function forAllCodeBlocks(operation) {
+        var codeBlocks;
+        var i;
+
+        codeBlocks = document.getElementById("tabs").getElementsByTagName("span");
+
+        for (i = 0; i < codeBlocks.length; ++i) {
+            operation(codeBlocks[i], "wrapped");
+        }
+    }
+
+    function toggleLineWrapping() {
+        var checkBox = getCheckBox();
+
+        if (checkBox.checked) {
+            forAllCodeBlocks(addClass);
+            storeState(true);
+        } else {
+            forAllCodeBlocks(removeClass);
+            storeState(false);
+        }
+    }
+
+    function initControls() {
+        if (storage) {
+            var checkBox = getCheckBox();
+            var label = getLabelForCheckBox();
+
+            checkBox.onclick = toggleLineWrapping;
+            removeClass(label, "hidden");
+        }
+    }
+
+    function switchTab() {
+        var id = this.id.substr(1);
+
+        for (var i = 0; i < tabs.tabs.length; i++) {
+            if (tabs.tabs[i].id === id) {
+                tabs.select(i);
+                break;
+            }
+        }
+
+        return false;
+    }
+
+    function select(i) {
+        this.deselectAll();
+
+        changeElementClass(this.tabs[i], "tab selected");
+        changeElementClass(this.headers[i], "selected");
+
         while (this.headers[i].firstChild) {
             this.headers[i].removeChild(this.headers[i].firstChild);
         }
-        var a = document.createElement('A');
-        a.setAttribute('id', 'ltab' + i);
-        a.setAttribute('href', '#tab' + i);
-        a.onclick = switchTab;
-        a.appendChild(document.createTextNode(this.titles[i]));
-        this.headers[i].appendChild(a);
+
+        var h2 = document.createElement("H2");
+
+        h2.appendChild(document.createTextNode(this.titles[i]));
+        this.headers[i].appendChild(h2);
     }
-}
 
-function changeElementClass(element, classValue) {
-    if (element.getAttribute('className')) {
-        /* IE */
-        element.setAttribute('className', classValue)
-    } else {
-        element.setAttribute('class', classValue)
-    }
-}
+    function deselectAll() {
+        for (var i = 0; i < this.tabs.length; i++) {
+            changeElementClass(this.tabs[i], "tab deselected");
+            changeElementClass(this.headers[i], "deselected");
 
-function findTabs(container) {
-    return findChildElements(container, 'DIV', 'tab');
-}
-
-function findHeaders(container) {
-    var owner = findChildElements(container, 'UL', 'tabLinks');
-    return findChildElements(owner[0], 'LI', null);
-}
-
-function findTitles(tabs) {
-    var titles = new Array();
-    for (var i = 0; i < tabs.length; i++) {
-        var tab = tabs[i];
-        var header = findChildElements(tab, 'H2', null)[0];
-        header.parentNode.removeChild(header);
-        if (header.innerText) {
-            titles.push(header.innerText)
-        } else {
-            titles.push(header.textContent)
-        }
-    }
-    return titles;
-}
-
-function findChildElements(container, name, targetClass) {
-    var elements = new Array();
-    var children = container.childNodes;
-    for (var i = 0; i < children.length; i++) {
-        var child = children.item(i);
-        if (child.nodeType == 1 && child.nodeName == name) {
-            if (targetClass && child.className.indexOf(targetClass) < 0) {
-                continue;
+            while (this.headers[i].firstChild) {
+                this.headers[i].removeChild(this.headers[i].firstChild);
             }
-            elements.push(child);
+
+            var a = document.createElement("A");
+
+            a.setAttribute("id", "ltab" + i);
+            a.setAttribute("href", "#tab" + i);
+            a.onclick = switchTab;
+            a.appendChild(document.createTextNode(this.titles[i]));
+
+            this.headers[i].appendChild(a);
         }
     }
-    return elements;
-}
+
+    function findTabs(container) {
+        return findChildElements(container, "DIV", "tab");
+    }
+
+    function findHeaders(container) {
+        var owner = findChildElements(container, "UL", "tabLinks");
+        return findChildElements(owner[0], "LI", null);
+    }
+
+    function findTitles(tabs) {
+        var titles = [];
+
+        for (var i = 0; i < tabs.length; i++) {
+            var tab = tabs[i];
+            var header = findChildElements(tab, "H2", null)[0];
+
+            header.parentNode.removeChild(header);
+
+            if (header.innerText) {
+                titles.push(header.innerText);
+            } else {
+                titles.push(header.textContent);
+            }
+        }
+
+        return titles;
+    }
+
+    function findChildElements(container, name, targetClass) {
+        var elements = [];
+        var children = container.childNodes;
+
+        for (var i = 0; i < children.length; i++) {
+            var child = children.item(i);
+
+            if (child.nodeType === 1 && child.nodeName === name) {
+                if (targetClass && child.className.indexOf(targetClass) < 0) {
+                    continue;
+                }
+
+                elements.push(child);
+            }
+        }
+
+        return elements;
+    }
+
+    // Entry point.
+
+    window.onload = function() {
+        initTabs();
+        initStorage();
+        initControls();
+    };
+} (window, window.document));
