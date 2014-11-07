b72f0eb55f5e4402a950f4689801132af7f20c4f
Szczepan Faber <szczepiq@gmail.com>
2014-11-07 12:51:15 +0100

    Covered the capturing logging from logging tools (TestNG)

--- subprojects/plugins/src/integTest/groovy/org/gradle/testing/testng/TestNGStaticLoggingIntegrationTest.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/testing/testng/TestNGStaticLoggingIntegrationTest.groovy
@@ -18,29 +18,33 @@ package org.gradle.testing.testng
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.testing.fixture.TestNGCoverage
-import spock.lang.Ignore
 import spock.lang.Issue
 
 class TestNGStaticLoggingIntegrationTest extends AbstractIntegrationSpec {
 
     @Issue("GRADLE-2841")
-    @Ignore
     def "captures output from logging tools"() {
         TestNGCoverage.enableTestNG(buildFile)
+        buildFile << """
+            test.onOutput { id, event -> println 'captured ' + event.message }
+            dependencies { compile "org.slf4j:slf4j-simple:1.7.7", "org.slf4j:slf4j-api:1.7.7" }
+        """
 
         file("src/test/java/FooTest.java") << """
             import org.testng.annotations.*;
+            import org.slf4j.*;
 
             public class FooTest {
-                static { System.out.println("cool output from initializer"); }
-                @Test public void foo() { System.out.println("cool output from test"); }
+                private final static Logger LOGGER = LoggerFactory.getLogger(FooTest.class);
+                @Test public void foo() {
+                  LOGGER.info("cool output from test");
+                }
             }
         """
 
         when: run("test")
         then:
-        result.output.contains("captured cool output from test")
-        result.output.contains("captured cool output from initializer")
+        result.output.contains("captured [Test worker] INFO FooTest - cool output from test")
     }
 
     @Issue("GRADLE-2841")
