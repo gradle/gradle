ef9985a6974a5986607ea4944c5259f21e609465
Luke Daley <ld@ldaley.com>
2014-11-10 16:12:52 +1000

    Decouple the managed element proxy creator from any particular implementation of the backing state.

--- subprojects/model-core/src/main/java/org/gradle/model/internal/manage/instance/ManagedModelElement.java
+++ subprojects/model-core/src/main/java/org/gradle/model/internal/manage/instance/ManagedModelElement.java
@@ -22,15 +22,21 @@ import org.gradle.internal.Cast;
 import org.gradle.model.internal.core.ModelType;
 import org.gradle.model.internal.manage.schema.ModelProperty;
 import org.gradle.model.internal.manage.schema.ModelSchema;
+import org.gradle.model.internal.manage.schema.ModelSchemaStore;
 
 @ThreadSafe
 public class ManagedModelElement<T> {
 
     private final ImmutableSortedMap<String, ModelPropertyInstance<?>> properties;
     private final ModelSchema<T> schema;
+    private final ModelSchemaStore schemaStore;
+    private final ModelInstantiator instantiator;
 
-    public ManagedModelElement(ModelSchema<T> schema) {
+    public ManagedModelElement(ModelSchema<T> schema, ModelSchemaStore schemaStore, ModelInstantiator instantiator) {
         this.schema = schema;
+        this.schemaStore = schemaStore;
+        this.instantiator = instantiator;
+
         ImmutableSortedMap.Builder<String, ModelPropertyInstance<?>> builder = ImmutableSortedMap.naturalOrder();
         for (ModelProperty<?> property : schema.getProperties().values()) {
             builder.put(property.getName(), ModelPropertyInstance.of(property));
@@ -51,7 +57,31 @@ public class ManagedModelElement<T> {
         return Cast.uncheckedCast(modelPropertyInstance);
     }
 
-    ImmutableSortedMap<String, ModelPropertyInstance<?>> getProperties() {
+    public ImmutableSortedMap<String, ModelPropertyInstance<?>> getProperties() {
         return properties;
+    }
+
+    public ModelElementState getState() {
+        return new ModelElementState() {
+            public <P> P get(ModelType<P> type, String name) {
+                ModelPropertyInstance<P> propertyInstance = ManagedModelElement.this.get(type, name);
+                P value = propertyInstance.get();
+                if (value == null && !propertyInstance.getMeta().isWritable()) {
+                    value = instantiator.newInstance(schemaStore.getSchema(type));
+                    propertyInstance.set(value);
+                }
+
+                return value;
+            }
+
+            public <P> void set(ModelType<P> propertyType, String name, P value) {
+                ModelPropertyInstance<P> modelPropertyInstance = ManagedModelElement.this.get(propertyType, name);
+                ModelSchema<P> propertySchema = schemaStore.getSchema(propertyType);
+                if (propertySchema.getKind().equals(ModelSchema.Kind.STRUCT) && !ManagedInstance.class.isInstance(value)) {
+                    throw new IllegalArgumentException(String.format("Only managed model instances can be set as property '%s' of class '%s'", name, getType()));
+                }
+                modelPropertyInstance.set(Cast.cast(propertyType.getConcreteClass(), value));
+            }
+        };
     }
 }
--- subprojects/model-core/src/main/java/org/gradle/model/internal/manage/instance/StructModelInstantiator.java
+++ subprojects/model-core/src/main/java/org/gradle/model/internal/manage/instance/StructModelInstantiator.java
@@ -16,75 +16,26 @@
 
 package org.gradle.model.internal.manage.instance;
 
-import org.apache.commons.lang.StringUtils;
-import org.gradle.internal.Cast;
-import org.gradle.model.internal.core.ModelType;
 import org.gradle.model.internal.manage.schema.ModelSchema;
 import org.gradle.model.internal.manage.schema.ModelSchemaStore;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-
 public class StructModelInstantiator implements ModelInstantiatorStrategy {
+
+    private final ManagedProxyFactory proxyFactory;
+
+    public StructModelInstantiator(ManagedProxyFactory proxyFactory) {
+        this.proxyFactory = proxyFactory;
+    }
 
     public <T> T newInstance(ModelSchema<T> schema, ModelSchemaStore schemaStore, ModelInstantiator instantiator) {
         if (schema.getKind().equals(ModelSchema.Kind.STRUCT)) {
             Class<T> concreteType = schema.getType().getConcreteClass();
-            ManagedModelElement<T> element = new ManagedModelElement<T>(schema);
-            ManagedModelElementInvocationHandler invocationHandler = new ManagedModelElementInvocationHandler(element, schemaStore, instantiator);
-            return Cast.uncheckedCast(Proxy.newProxyInstance(concreteType.getClassLoader(), new Class<?>[]{concreteType, ManagedInstance.class}, invocationHandler));
+            ManagedModelElement<T> element = new ManagedModelElement<T>(schema, schemaStore, instantiator);
+            return proxyFactory.createProxy(element.getState(), concreteType.getClassLoader(), concreteType, ManagedInstance.class);
         } else {
             return null;
         }
 
-    }
-
-    private static class ManagedModelElementInvocationHandler implements InvocationHandler {
-
-        private final ManagedModelElement<?> element;
-        private final ModelSchemaStore schemaStore;
-        private final ModelInstantiator instantiator;
-
-        private ManagedModelElementInvocationHandler(ManagedModelElement<?> element, ModelSchemaStore schemaStore, ModelInstantiator instantiator) {
-            this.element = element;
-            this.schemaStore = schemaStore;
-            this.instantiator = instantiator;
-        }
-
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            String methodName = method.getName();
-            String propertyName = StringUtils.uncapitalize(methodName.substring(3));
-            if (methodName.startsWith("get")) {
-                return getInstanceProperty(ModelType.of(method.getGenericReturnType()), propertyName);
-            } else if (methodName.startsWith("set")) {
-                setInstanceProperty(ModelType.of(method.getGenericParameterTypes()[0]), propertyName, args[0]);
-                return null;
-            } else if (methodName.equals("hashCode")) {
-                return hashCode();
-            }
-            throw new Exception("Unexpected method called: " + methodName);
-        }
-
-        private <U> void setInstanceProperty(ModelType<U> propertyType, String propertyName, Object value) {
-            ModelPropertyInstance<U> modelPropertyInstance = element.get(propertyType, propertyName);
-            ModelSchema<U> propertySchema = schemaStore.getSchema(propertyType);
-            if (propertySchema.getKind().equals(ModelSchema.Kind.STRUCT) && !ManagedInstance.class.isInstance(value)) {
-                throw new IllegalArgumentException(String.format("Only managed model instances can be set as property '%s' of class '%s'", propertyName, element.getType()));
-            }
-            modelPropertyInstance.set(Cast.cast(propertyType.getConcreteClass(), value));
-        }
-
-        private <U> U getInstanceProperty(ModelType<U> propertyType, String propertyName) {
-            ModelPropertyInstance<U> propertyInstance = element.get(propertyType, propertyName);
-            U value = propertyInstance.get();
-            if (value == null && !propertyInstance.getMeta().isWritable()) {
-                value = instantiator.newInstance(schemaStore.getSchema(propertyType));
-                propertyInstance.set(value);
-            }
-
-            return value;
-        }
     }
 
 }
