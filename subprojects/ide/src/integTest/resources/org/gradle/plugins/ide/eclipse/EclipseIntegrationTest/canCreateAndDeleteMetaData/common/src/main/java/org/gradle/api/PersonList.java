b1fe8e54c882c6ee02b264aac54aba82ed3db146
Andreas Schmid <service@aaschmid.de>
2014-11-16 21:06:41 +0100

    added test for minusConfiguration with "eclipse-wtp" on "java" project

---
+++ subprojects/ide/src/integTest/resources/org/gradle/plugins/ide/eclipse/EclipseIntegrationTest/canCreateAndDeleteMetaData/common/src/main/java/org/gradle/api/PersonList.java
@@ -0,0 +1,5 @@
+package org.gradle.plugins.ide.eclipse.EclipseIntegrationTest.canCreateAndDeleteMetaData.common.src.main.java.org.gradle.api;
+
+public class PersonList {
+   
+}
---
+++ subprojects/ide/src/integTest/resources/org/gradle/plugins/ide/eclipse/EclipseIntegrationTest/canCreateAndDeleteMetaData/common/src/test/java/org/gradle/shared/PersonTest.java
@@ -0,0 +1,9 @@
+package org.gradle.plugins.ide.eclipse.EclipseIntegrationTest.canCreateAndDeleteMetaData.common.src.test.java.org.gradle.shared;
+
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase {
+    public void testTest() {
+        assertTrue(true);
+    }
+}
