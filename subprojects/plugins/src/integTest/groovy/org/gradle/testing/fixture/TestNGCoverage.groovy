b96ec150bd4120c7686ca5844ae545f8981c24eb
Adam Murdoch <adam.murdoch@gradleware.com>
2014-11-28 18:12:16 +1100

    Added another version of TestNG to test against.

--- subprojects/plugins/src/integTest/groovy/org/gradle/testing/fixture/TestNGCoverage.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/testing/fixture/TestNGCoverage.groovy
@@ -18,7 +18,7 @@ package org.gradle.testing.fixture;
 
 class TestNGCoverage {
     final static String NEWEST = '6.8.7'
-    final static String[] STANDARD_COVERAGE = ['5.14.10', NEWEST]
+    final static String[] STANDARD_COVERAGE = ['5.14.10', '6.2', NEWEST]
 
     /**
      * Adds java plugin and configures TestNG support in given build script file.
