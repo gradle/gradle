13493c13e7960df5e00c1f9047a6f5e9bd31f40d
Daz DeBoer <darrell.deboer@gradleware.com>
2014-12-07 20:43:08 -0700

    Added remaining non-incubating tasks implemented in Groovy to compile-compatibility test
    +review REVIEW-5306

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/TaskSubclassingBinaryCompatibilityCrossVersionSpec.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/TaskSubclassingBinaryCompatibilityCrossVersionSpec.groovy
@@ -14,16 +14,14 @@
  * limitations under the License.
  */
 package org.gradle.integtests
-
 import org.gradle.api.DefaultTask
 import org.gradle.api.internal.ConventionTask
-import org.gradle.api.plugins.quality.Checkstyle
-import org.gradle.api.plugins.quality.CodeNarc
-import org.gradle.api.plugins.quality.FindBugs
-import org.gradle.api.plugins.quality.Pmd
+import org.gradle.api.plugins.quality.*
+import org.gradle.api.plugins.sonar.SonarAnalyze
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.Sync
+import org.gradle.api.tasks.application.CreateStartScripts
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.bundling.Tar
 import org.gradle.api.tasks.bundling.War
@@ -34,8 +32,12 @@ import org.gradle.api.tasks.testing.Test
 import org.gradle.integtests.fixtures.CrossVersionIntegrationSpec
 import org.gradle.integtests.fixtures.TargetVersions
 import org.gradle.plugins.ear.Ear
+import org.gradle.plugins.ide.eclipse.*
+import org.gradle.plugins.ide.idea.GenerateIdeaModule
+import org.gradle.plugins.ide.idea.GenerateIdeaProject
+import org.gradle.plugins.ide.idea.GenerateIdeaWorkspace
+import org.gradle.plugins.signing.Sign
 import org.gradle.util.GradleVersion
-
 /**
  * Tests that task classes compiled against earlier versions of Gradle are still compatible.
  */
@@ -53,29 +55,39 @@ class TaskSubclassingBinaryCompatibilityCrossVersionSpec extends CrossVersionInt
                 Jar,
                 Tar,
                 War,
-                Ear,
-                Test,
                 ScalaCompile,
                 GroovyCompile,
 //                JavaCompile,
                 CodeNarc,
-                FindBugs,
                 Checkstyle,
-                Pmd
         ]
-
-        if (previous.version < GradleVersion.version("1.0-milestone-4")) {
-            taskClasses.remove(Ear)
+        if (previous.version >= GradleVersion.version("1.0")) {
+            taskClasses += [
+                    Ear,
+                    FindBugs,
+                    Pmd,
+                    JDepend,
+                    Sign,
+            ]
         }
-        if (previous.version < GradleVersion.version("1.0-milestone-8")) {
-            taskClasses.remove(FindBugs)
-            taskClasses.remove(Pmd)
-        }
-        if (previous.version < GradleVersion.version("1.1")) {
+        if (previous.version >= GradleVersion.version("1.1")) {
             // Breaking changes were made to Test between 1.0 and 1.1
-            taskClasses.remove(Test)
+            taskClasses << Test
         }
-
+        if (previous.version >= GradleVersion.version("2.0")) {
+            taskClasses += [
+                    CreateStartScripts,
+                    GenerateEclipseJdt,
+                    GenerateEclipseClasspath,
+                    GenerateEclipseProject,
+                    GenerateEclipseWtpComponent,
+                    GenerateEclipseWtpFacet,
+                    GenerateIdeaModule,
+                    GenerateIdeaProject,
+                    GenerateIdeaWorkspace,
+                    SonarAnalyze,
+            ]
+        }
 
         Map<String, String> subclasses = taskClasses.collectEntries { ["custom" + it.simpleName, it.name] }
 
