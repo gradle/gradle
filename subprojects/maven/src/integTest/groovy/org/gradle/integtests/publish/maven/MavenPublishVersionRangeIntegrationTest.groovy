e01bf5105bfea04401f05f0c3059f46fe87ac044
Rene Groeschke <rene@breskeby.com>
2014-12-10 19:14:07 +0100

    Add integration test for publishing version ranged dependencies using old/new DSL
    +review REVIEW-5225

---
+++ subprojects/maven/src/integTest/groovy/org/gradle/integtests/publish/maven/MavenPublishVersionRangeIntegrationTest.groovy
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.integtests.publish.maven
+
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+
+class MavenPublishVersionRangeIntegrationTest extends AbstractIntegrationSpec {
+
+    public void "version range is mapped to maven syntax in published pom file"() {
+        given:
+        file("settings.gradle") << "rootProject.name = 'publishTest' "
+        and:
+        buildFile << """
+apply plugin: 'java'
+apply plugin: 'maven'
+
+group = 'org.gradle.test'
+version = '1.9'
+
+dependencies {
+    compile "group:projectA:latest.release"
+    compile "group:projectB:latest.integration"
+    runtime "group:projectC:1.1.+"
+    runtime "group:projectD:1+"
+}
+
+uploadArchives {
+    repositories {
+        mavenDeployer {
+            repository(url: "${mavenRepo.uri}")
+        }
+    }
+}
+"""
+
+        when:
+        run "uploadArchives"
+
+        then:
+        def mavenModule = mavenRepo.module("org.gradle.test", "publishTest", "1.9")
+        mavenModule.assertArtifactsPublished("publishTest-1.9.pom", "publishTest-1.9.jar")
+        mavenModule.parsedPom.scopes.compile.assertDependsOn("group:projectA:RELEASE", "group:projectB:LATEST")
+        mavenModule.parsedPom.scopes.runtime.assertDependsOn("group:projectC:[1.1,1.2)", "group:projectD:[1,2)")
+    }
+}
