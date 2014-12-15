091f74bc946416989096fdc339de7833ab3d3039
Adam Murdoch <adam.murdoch@gradleware.com>
2014-12-16 06:59:11 +1100

    Moved component report + cunit int test to live in cunit project.

---
+++ subprojects/diagnostics/src/testFixtures/groovy/org/gradle/api/reporting/components/AbstractComponentReportIntegrationTest.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+package org.gradle.api.reporting.components
+
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.nativeplatform.fixtures.AvailableToolChains
+
+abstract class AbstractComponentReportIntegrationTest extends AbstractIntegrationSpec {
+    def setup() {
+        settingsFile << "rootProject.name = 'test'"
+    }
+
+    boolean outputMatches(String actualOutput, String expectedOutput) {
+        String cleaned = actualOutput.substring(0, actualOutput.lastIndexOf("BUILD SUCCESSFUL"))
+        assert cleaned == expected(expectedOutput)
+        return true
+    }
+
+    String expected(String normalised) {
+        String raw = """:components
+
+------------------------------------------------------------
+Root project
+------------------------------------------------------------
+""" + normalised + """
+Note: currently not all plugins register their components, so some components may not be visible here.
+
+"""
+        return new ComponentReportOutputFormatter(toolChain).transform(raw)
+    }
+
+    AvailableToolChains.InstalledToolChain getToolChain() {
+        return AvailableToolChains.defaultToolChain
+    }
+}
