83e0f3465b68412e4327d30ca46b2e848c171cc9
Marcin Erdmann <erdi84@gmail.com>
2015-01-07 10:20:53 +0000

    Simplify describing generic rule methods in error reporting.
    +review REVIEW-5335

--- subprojects/model-core/src/main/java/org/gradle/model/internal/inspect/ModelRuleInspector.java
+++ subprojects/model-core/src/main/java/org/gradle/model/internal/inspect/ModelRuleInspector.java
@@ -20,9 +20,9 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import net.jcip.annotations.ThreadSafe;
 import org.gradle.api.Transformer;
+import org.gradle.internal.reflect.MethodDescription;
 import org.gradle.model.InvalidModelRuleDeclarationException;
 import org.gradle.model.internal.core.ModelRuleRegistration;
-import org.gradle.model.internal.core.rule.describe.MethodModelRuleDescriptor;
 import org.gradle.model.internal.core.rule.describe.ModelRuleDescriptor;
 import org.gradle.model.internal.type.ModelType;
 import org.gradle.util.CollectionUtils;
@@ -55,14 +55,23 @@ public class ModelRuleInspector {
         return new InvalidModelRuleDeclarationException("Type " + source.getName() + " is not a valid model rule source: " + reason);
     }
 
-    private static RuntimeException invalidMethod(Class<?> target, Method method, String reason) {
-        return invalid("model rule method", MethodModelRuleDescriptor.of(target, method), reason);
+    private static RuntimeException invalidMethod(Method method, String reason) {
+        String description = MethodDescription.name(method.getName())
+                .owner(method.getDeclaringClass())
+                .takes(method.getGenericParameterTypes())
+                .toString();
+        return invalid(description, reason);
     }
 
-    private static RuntimeException invalid(String description, ModelRuleDescriptor rule, String reason) {
+    private static RuntimeException invalid(ModelRuleDescriptor rule, String reason) {
         StringBuilder sb = new StringBuilder();
         rule.describeTo(sb);
-        sb.append(" is not a valid ").append(description).append(": ").append(reason);
+        return invalid(sb.toString(), reason);
+    }
+
+    private static RuntimeException invalid(String ruleDescription, String reason) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ruleDescription).append(" is not a valid model rule method").append(": ").append(reason);
         return new InvalidModelRuleDeclarationException(sb.toString());
     }
 
@@ -81,13 +90,13 @@ public class ModelRuleInspector {
 
         for (Method method : methods) {
             if (method.getTypeParameters().length > 0) {
-                throw invalidMethod(source, method, "cannot have type variables (i.e. cannot be a generic method)");
+                throw invalidMethod(method, "cannot have type variables (i.e. cannot be a generic method)");
             }
 
             MethodRuleDefinition<?, ?> ruleDefinition = DefaultMethodRuleDefinition.create(source, method);
             MethodModelRuleExtractor handler = getMethodHandler(ruleDefinition);
             if (handler != null) {
-                validateMethod(source, method);
+                validateMethod(method);
                 ModelRuleRegistration registration = handler.registration(ruleDefinition, dependencies);
                 if (registration != null) {
                     registrations.add(registration);
@@ -104,7 +113,7 @@ public class ModelRuleInspector {
                 if (handler == null) {
                     handler = candidateHandler;
                 } else {
-                    throw invalid("model rule method", ruleDefinition.getDescriptor(), "can only be one of " + describeHandlers());
+                    throw invalid(ruleDefinition.getDescriptor(), "can only be one of " + describeHandlers());
                 }
             }
         }
@@ -160,11 +169,11 @@ public class ModelRuleInspector {
         }
     }
 
-    private void validateMethod(Class<?> target, Method ruleMethod) {
+    private void validateMethod(Method ruleMethod) {
         // TODO validations on method: synthetic, bridge methods, varargs, abstract, native
         ModelType<?> returnType = ModelType.returnType(ruleMethod);
         if (returnType.isRawClassOfParameterizedType()) {
-            throw invalidMethod(target, ruleMethod, "raw type " + returnType + " used for return type (all type parameters must be specified of parameterized type)");
+            throw invalidMethod(ruleMethod, "raw type " + returnType + " used for return type (all type parameters must be specified of parameterized type)");
         }
 
         int i = 0;
@@ -172,7 +181,7 @@ public class ModelRuleInspector {
             ++i;
             ModelType<?> modelType = ModelType.of(type);
             if (modelType.isRawClassOfParameterizedType()) {
-                throw invalidMethod(target, ruleMethod, "raw type " + modelType + " used for parameter " + i + " (all type parameters must be specified of parameterized type)");
+                throw invalidMethod(ruleMethod, "raw type " + modelType + " used for parameter " + i + " (all type parameters must be specified of parameterized type)");
             }
         }
     }
