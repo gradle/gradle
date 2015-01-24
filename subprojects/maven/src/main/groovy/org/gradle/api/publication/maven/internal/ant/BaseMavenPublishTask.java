4bd1b076e30cdae3eb03108235372c3af6c438be
Daz DeBoer <darrell.deboer@gradleware.com>
2015-01-24 14:57:19 -0700

    Don't instantiate a MavenProject when deploying to Maven repository
    - Construction of MavenProject was only required to parse the pom, so
      use MavenXpp3Reader to do this instead

--- subprojects/maven/src/main/groovy/org/gradle/api/publication/maven/internal/ant/BaseMavenPublishTask.java
+++ subprojects/maven/src/main/groovy/org/gradle/api/publication/maven/internal/ant/BaseMavenPublishTask.java
@@ -19,20 +19,23 @@ package org.gradle.api.publication.maven.internal.ant;
  * under the License.
  */
 
+import com.beust.jcommander.internal.Lists;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.ant.*;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.ant.Authentication;
+import org.apache.maven.artifact.ant.LocalRepository;
+import org.apache.maven.artifact.ant.Proxy;
+import org.apache.maven.artifact.ant.RemoteRepository;
+import org.apache.maven.artifact.handler.DefaultArtifactHandler;
 import org.apache.maven.artifact.manager.WagonManager;
 import org.apache.maven.artifact.metadata.ArtifactMetadata;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
 import org.apache.maven.artifact.repository.DefaultArtifactRepository;
 import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
-import org.apache.maven.profiles.DefaultProfileManager;
-import org.apache.maven.profiles.ProfileManager;
-import org.apache.maven.project.MavenProjectBuilder;
-import org.apache.maven.project.MavenProjectHelper;
+import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.maven.project.artifact.AttachedArtifact;
 import org.apache.maven.project.artifact.ProjectArtifactMetadata;
-import org.apache.maven.project.injection.ModelDefaultsInjector;
 import org.apache.maven.settings.Settings;
 import org.apache.maven.wagon.events.TransferEvent;
 import org.apache.maven.wagon.events.TransferListener;
@@ -48,13 +51,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 public abstract class BaseMavenPublishTask implements MavenPublishTaskSupport {
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseMavenPublishTask.class);
@@ -65,16 +64,16 @@ public abstract class BaseMavenPublishTask implements MavenPublishTaskSupport {
 
     private static ClassLoader plexusClassLoader;
 
+    private final File pomFile;
+
     private Settings settings;
 
     private PlexusContainer container;
 
-    private final File pomFile;
-
     private LocalRepository localRepository;
 
     protected File mainArtifact;
-    protected List<AttachedArtifact> attachedArtifacts = new ArrayList<AttachedArtifact>();
+    protected List<AdditionalArtifact> additionalArtifacts = Lists.newArrayList();
 
     protected BaseMavenPublishTask(File pomFile) {
         this.pomFile = pomFile;
@@ -86,33 +85,12 @@ public abstract class BaseMavenPublishTask implements MavenPublishTaskSupport {
 
     @Override
     public void addAdditionalArtifact(File file, String type, String classifier) {
-        AttachedArtifact attach = new AttachedArtifact();
+        AdditionalArtifact artifact = new AdditionalArtifact();
+        artifact.setFile(file);
+        artifact.setType(type);
+        artifact.setClassifier(classifier);
 
-        attach.setFile(file);
-        attach.setType(type);
-        attach.setClassifier(classifier);
-
-        attachedArtifacts.add(attach);
-    }
-
-    protected MavenArtifactBuilder getArtifactBuilder(ArtifactRepository localArtifactRepository) {
-        MavenProjectBuilder projectBuilder = (MavenProjectBuilder) lookup(MavenProjectBuilder.ROLE);
-        ModelDefaultsInjector modelDefaultsInjector = (ModelDefaultsInjector) lookup(ModelDefaultsInjector.ROLE);
-        MavenProjectHelper helper = (MavenProjectHelper) lookup(MavenProjectHelper.ROLE);
-        ProfileManager profileManager = new DefaultProfileManager(getContainer(), settings, System.getProperties());
-
-        MavenArtifactBuilder mavenArtifactBuilder = new MavenArtifactBuilder(projectBuilder, modelDefaultsInjector, helper, profileManager);
-        mavenArtifactBuilder.setPomFile(pomFile);
-        mavenArtifactBuilder.initialiseMavenProject(localArtifactRepository);
-
-        // attach artifacts
-        if (attachedArtifacts != null) {
-            for (AttachedArtifact attached : attachedArtifacts) {
-                mavenArtifactBuilder.attach(attached);
-            }
-        }
-
-        return mavenArtifactBuilder;
+        additionalArtifacts.add(artifact);
     }
 
     protected ArtifactRepository createLocalArtifactRepository() {
@@ -227,9 +205,6 @@ public abstract class BaseMavenPublishTask implements MavenPublishTaskSupport {
      * @noinspection RefusedBequest
      */
     public void execute() {
-        // Display the version if the log level is verbose
-        showVersion();
-
         ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
         try {
             if (plexusClassLoader != null) {
@@ -243,16 +218,16 @@ public abstract class BaseMavenPublishTask implements MavenPublishTaskSupport {
     }
 
     public void doExecute() {
-        if (mainArtifact == null && (attachedArtifacts.size() == 0)) {
+        if (mainArtifact == null && (additionalArtifacts.size() == 0)) {
             throw new BuildException("You must specify a file and/or an attached artifact for Maven publishing.");
         }
 
         ArtifactRepository localRepo = createLocalArtifactRepository();
-        MavenArtifactBuilder mavenArtifactBuilder = getArtifactBuilder(localRepo);
+        ParsedMavenPom parsedMavenPom = new ParsedMavenPom(pomFile);
 
+        Artifact artifact = createMainArtifact(parsedMavenPom);
         if (mainArtifact != null) {
-            Artifact artifact = mavenArtifactBuilder.getMainArtifact();
-            boolean isPomArtifact = mavenArtifactBuilder.isPomPackaging();
+            boolean isPomArtifact = "pom".equals(parsedMavenPom.getPackaging());
             if (isPomArtifact) {
                 doPublish(artifact, pomFile, localRepo);
             } else {
@@ -262,11 +237,23 @@ public abstract class BaseMavenPublishTask implements MavenPublishTaskSupport {
             }
         }
 
-        if (attachedArtifacts != null) {
-            for (Artifact attachedArtifact : mavenArtifactBuilder.getAttachedArtifacts()) {
-                doPublish(attachedArtifact, attachedArtifact.getFile(), localRepo);
-            }
+        for (AdditionalArtifact attachedArtifact : additionalArtifacts) {
+            Artifact attach = createAttachedArtifact(artifact, attachedArtifact.getType(), attachedArtifact.getClassifier());
+            doPublish(attach, attachedArtifact.getFile(), localRepo);
         }
+    }
+    public Artifact createMainArtifact(ParsedMavenPom builder)
+    {
+        return new DefaultArtifact(builder.getGroup(), builder.getArtifactId(), VersionRange.createFromVersion(builder.getVersion()),
+                null, builder.getPackaging(), null, artifactHandler(builder.getPackaging()));
+    }
+
+    public Artifact createAttachedArtifact(Artifact mainArtifact, String type, String classifier) {
+        return new AttachedArtifact(mainArtifact, type, classifier, artifactHandler(type));
+    }
+
+    private DefaultArtifactHandler artifactHandler(String type) {
+        return new DefaultArtifactHandler(type);
     }
 
     /**
@@ -330,32 +317,6 @@ public abstract class BaseMavenPublishTask implements MavenPublishTaskSupport {
         }
     }
 
-    /**
-     * Log the current version of the ant-tasks to the verbose output.
-     */
-    protected void showVersion() {
-
-        Properties properties = new Properties();
-        final String antTasksPropertiesPath = "META-INF/maven/org.apache.maven/maven-ant-tasks/pom.properties";
-        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(antTasksPropertiesPath);
-
-        try {
-            if (resourceAsStream != null) {
-                properties.load(resourceAsStream);
-            }
-
-            String version = properties.getProperty("version", "unknown");
-            String builtOn = properties.getProperty("builtOn");
-            if (builtOn != null) {
-                log("Maven Ant Tasks version: " + version + " built on " + builtOn);
-            } else {
-                log("Maven Ant Tasks version: " + version);
-            }
-        } catch (IOException e) {
-            log("Unable to determine version from Maven Ant Tasks JAR file: " + e.getMessage());
-        }
-    }
-
     protected void log(String message) {
         LOGGER.info(message);
     }
@@ -403,6 +364,36 @@ public abstract class BaseMavenPublishTask implements MavenPublishTaskSupport {
             if ((contentLength > 0) && (event.getRequestType() == TransferEvent.REQUEST_PUT)) {
                 LOGGER.info("Uploaded " + ((contentLength + KILO / 2) / KILO) + "K");
             }
+        }
+    }
+
+    private static class AdditionalArtifact {
+        File file;
+        String type;
+        String classifier;
+
+        public void setFile(File file) {
+            this.file = file;
+        }
+
+        public File getFile() {
+            return file;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setClassifier(String classifier) {
+            this.classifier = classifier;
+        }
+
+        public String getClassifier() {
+            return classifier;
         }
     }
 
