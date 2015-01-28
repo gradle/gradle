c0feab9f6aed8f4134baad7374760d90aac85454
Luke Daley <ld@ldaley.com>
2015-01-28 13:15:10 +1100

    Checkstyle.

--- subprojects/antlr/src/main/groovy/org/gradle/api/plugins/antlr/internal/AntlrExecuter.java
+++ subprojects/antlr/src/main/groovy/org/gradle/api/plugins/antlr/internal/AntlrExecuter.java
@@ -22,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.Serializable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
