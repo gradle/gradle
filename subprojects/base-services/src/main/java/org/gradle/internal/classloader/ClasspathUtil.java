85a411c0fc10a5f840276bbf1de204c886af06f4
Gary Hale <ghhale@computer.org>
2015-02-18 02:41:01 -0500

    Fixing issue with Tooling API and Netbeans gradle plugin (GRADLE-3245)
    +review REVIEW-5409

--- subprojects/base-services/src/main/java/org/gradle/internal/classloader/ClasspathUtil.java
+++ subprojects/base-services/src/main/java/org/gradle/internal/classloader/ClasspathUtil.java
@@ -64,10 +64,12 @@ public class ClasspathUtil {
             CodeSource codeSource = targetClass.getProtectionDomain().getCodeSource();
             if (codeSource != null && codeSource.getLocation() != null) {
                 location = codeSource.getLocation().toURI();
-                if (!location.getScheme().equals("file")) {
-                    throw new GradleException(String.format("Cannot determine classpath for %s from codebase '%s'.", targetClass.getName(), location));
+                if (!location.getScheme().equals("jar")) {
+                    if (!location.getScheme().equals("file")) {
+                        throw new GradleException(String.format("Cannot determine classpath for %s from codebase '%s'.", targetClass.getName(), location));
+                    }
+                    return new File(location);
                 }
-                return new File(location);
             }
             String resourceName = targetClass.getName().replace(".", "/") + ".class";
             URL resource = targetClass.getClassLoader().getResource(resourceName);
