4e53d4f8d290e5e4f82a319e033bc96abf97955e
Adam Murdoch <adam.murdoch@gradleware.com>
2015-02-20 10:42:39 +1100

    Removed unused method.

--- subprojects/performance/src/testFixtures/groovy/org/gradle/performance/fixture/OperationTimer.java
+++ subprojects/performance/src/testFixtures/groovy/org/gradle/performance/fixture/OperationTimer.java
@@ -16,7 +16,6 @@
 
 package org.gradle.performance.fixture;
 
-import groovy.lang.Closure;
 import org.gradle.api.Action;
 import org.gradle.performance.measure.Duration;
 import org.gradle.performance.measure.MeasuredOperation;
@@ -28,21 +27,6 @@ public class OperationTimer {
         DateTime start = DateTime.now();
         try {
             action.execute(result);
-        } catch (Exception e) {
-            result.setException(e);
-        }
-        DateTime end = DateTime.now();
-        result.setStart(start);
-        result.setEnd(end);
-        result.setExecutionTime(Duration.millis(end.getMillis() - start.getMillis()));
-        return result;
-    }
-
-    public MeasuredOperation measure(Closure operation) {
-        MeasuredOperation result = new MeasuredOperation();
-        DateTime start = DateTime.now();
-        try {
-            operation.call(result);
         } catch (Exception e) {
             result.setException(e);
         }
