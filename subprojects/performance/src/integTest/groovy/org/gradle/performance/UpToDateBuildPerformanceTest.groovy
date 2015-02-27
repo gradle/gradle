d5f4fc56de8ea79dda3c85439cf09e8bab76804a
Adam Murdoch <adam.murdoch@gradleware.com>
2015-02-27 16:30:19 +1100

    Tweaked some performance tests.

--- subprojects/performance/src/integTest/groovy/org/gradle/performance/UpToDateBuildPerformanceTest.groovy
+++ subprojects/performance/src/integTest/groovy/org/gradle/performance/UpToDateBuildPerformanceTest.groovy
@@ -28,7 +28,7 @@ class UpToDateBuildPerformanceTest extends AbstractCrossVersionPerformanceTest {
         runner.testProject = testProject
         runner.tasksToRun = ['build']
         runner.maxExecutionTimeRegression = maxExecutionTimeRegression
-        runner.targetVersions = ['1.0', '2.0', '2.2.1', 'last']
+        runner.targetVersions = ['1.0', '2.0', '2.2.1', '2.4', 'last']
 
         when:
         def result = runner.run()
