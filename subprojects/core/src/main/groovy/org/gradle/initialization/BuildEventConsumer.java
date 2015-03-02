80248e76b0fbc4a5ccc0a3d73abfa66dfcef7764
Adam Murdoch <adam.murdoch@gradleware.com>
2015-03-02 16:35:05 +1100

    Javadocs.

--- subprojects/core/src/main/groovy/org/gradle/initialization/BuildEventConsumer.java
+++ subprojects/core/src/main/groovy/org/gradle/initialization/BuildEventConsumer.java
@@ -18,5 +18,8 @@ package org.gradle.initialization;
 
 import org.gradle.messaging.dispatch.Dispatch;
 
+/**
+ * A consumer for build events provided by the build requester. This can be used to forward events to the build requester.
+ */
 public interface BuildEventConsumer extends Dispatch<Object> {
 }
