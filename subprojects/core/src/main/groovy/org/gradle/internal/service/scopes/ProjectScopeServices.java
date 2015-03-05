a1f9dc21ef5a1560e5347ca31d194f10c2f8c0c5
Sterling Greene <sterling.greene@gmail.com>
2015-03-05 16:44:47 -0500

    Fix checkstyle
    +review REVIEW-5406

--- subprojects/core/src/main/groovy/org/gradle/internal/service/scopes/ProjectScopeServices.java
+++ subprojects/core/src/main/groovy/org/gradle/internal/service/scopes/ProjectScopeServices.java
@@ -44,8 +44,6 @@ import org.gradle.configuration.project.ProjectConfigurationActionContainer;
 import org.gradle.initialization.ProjectAccessListener;
 import org.gradle.internal.Factory;
 import org.gradle.internal.nativeintegration.filesystem.FileSystem;
-import org.gradle.internal.operations.logging.BuildOperationLoggerFactory;
-import org.gradle.internal.operations.logging.DefaultBuildOperationLoggerFactory;
 import org.gradle.internal.reflect.Instantiator;
 import org.gradle.internal.service.DefaultServiceRegistry;
 import org.gradle.internal.service.ServiceRegistration;
