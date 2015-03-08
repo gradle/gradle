5b62b565c43c8142a28ae1af7a963e4ec6e00690
Lorant Pinter <lorant.pinter@prezi.com>
2015-03-08 17:01:59 +0100

    Mark project dependency configurations as observed
    Note that dependency project configurations are observed _after_ the originating
    configuration. This cannot be otherwise, as we need to observe the originating
    config in order to evaluate the project dependency (or substitute another one for
    it).

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/DependencyResolutionEventsIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/DependencyResolutionEventsIntegrationTest.groovy
@@ -15,7 +15,7 @@
  */
 package org.gradle.integtests.resolve
 
-import org.gradle.integtests.fixtures.*
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import spock.lang.Issue
 
 class DependencyResolutionEventsIntegrationTest extends AbstractIntegrationSpec {
@@ -77,6 +77,49 @@ before observe configuration ':grandParent'
 before observe configuration ':parent'
 before observe configuration ':things'
 after resolve configuration ':things'""")
+    }
+
+    def "listeners are called on dependency project's configurations"() {
+        given:
+        settingsFile << 'include "api", "impl"'
+        buildFile << """
+            allprojects {
+                apply plugin: "java"
+                configurations.all { conf ->
+                    conf.incoming.beforeObserve { incoming ->
+                        println "before observe \$conf"
+                    }
+                    conf.incoming.beforeResolve { incoming ->
+                        println "before resolve \$conf"
+                    }
+                    conf.incoming.afterResolve { incoming ->
+                        println "after resolve \$conf"
+                    }
+                }
+            }
+            project(":impl") {
+                dependencies {
+                    compile project(":api")
+                }
+            }
+
+            dependencies {
+                compile project(":impl")
+            }
+            configurations.compile.resolve()
+        """
+
+        when: succeeds("dependencies", "--configuration", "compile")
+        then: output.contains(
+"""before resolve configuration ':compile'
+before observe configuration ':compile'
+before observe configuration ':impl:compile'
+before observe configuration ':impl:runtime'
+before observe configuration ':impl:default'
+before observe configuration ':api:compile'
+before observe configuration ':api:runtime'
+before observe configuration ':api:default'
+after resolve configuration ':compile'""")
     }
 
 }
