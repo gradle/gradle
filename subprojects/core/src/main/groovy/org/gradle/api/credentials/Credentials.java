bad9f05fa6a61e849beb471dec88db16000fea3d
Rene Groeschke <rene@breskeby.com>
2015-03-25 14:25:07 +0100

    mark Credentials as NonExtensible and Incubating
    +review REVIEW-5361

--- subprojects/core/src/main/groovy/org/gradle/api/credentials/Credentials.java
+++ subprojects/core/src/main/groovy/org/gradle/api/credentials/Credentials.java
@@ -16,9 +16,14 @@
 
 package org.gradle.api.credentials;
 
+import org.gradle.api.Incubating;
+import org.gradle.api.NonExtensible;
+
 /**
  * Base interface for credentials used for different authentication purposes.
  * (e.g authenticated {@link org.gradle.api.artifacts.dsl.RepositoryHandler})
  * */
+@Incubating
+@NonExtensible
 public interface Credentials {
 }
