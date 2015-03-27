29e2f794d546068658f50558a9a2057a64bb04bf
Marcin Erdmann <erdi84@gmail.com>
2015-03-27 14:50:14 +0000

    Move test to a more appropriate package.
    +review REVIEW-5426

--- subprojects/launcher/src/integTest/groovy/org/gradle/launcher/daemon/DaemonUsageSuggestionIntegrationTest.groovy
+++ subprojects/launcher/src/integTest/groovy/org/gradle/launcher/daemon/DaemonUsageSuggestionIntegrationTest.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.launcher
+package org.gradle.launcher.daemon
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
