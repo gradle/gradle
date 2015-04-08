2608d5f8d8ae0d468e47871c2c2226b2a0edafe3
Luke Daley <ld@ldaley.com>
2015-04-08 20:59:19 +1000

    Tweak to deprecation message for --parallel-threads.
    +review REVIEW-5434

--- subprojects/core/src/main/groovy/org/gradle/initialization/DefaultCommandLineConverter.java
+++ subprojects/core/src/main/groovy/org/gradle/initialization/DefaultCommandLineConverter.java
@@ -81,7 +81,7 @@ public class DefaultCommandLineConverter extends AbstractCommandLineConverter<St
         parser.option(REFRESH_DEPENDENCIES).hasDescription("Refresh the state of dependencies.");
         parser.option(PARALLEL).hasDescription("Build projects in parallel. Gradle will attempt to determine the optimal number of executor threads to use.").incubating();
         parser.option(PARALLEL_THREADS).hasArgument().hasDescription("Build projects in parallel, using the specified number of executor threads.").
-                deprecated("Prefer to use --parallel if you want to build in parallel and use --max-workers to set the number of executor threads.").incubating();
+                deprecated("Please use --parallel, optionally in conjunction with --max-workers.").incubating();
         parser.option(MAX_WORKERS).hasArgument().hasDescription("Configure the number of concurrent workers Gradle is allowed to use.").incubating();
         parser.option(CONFIGURE_ON_DEMAND).hasDescription("Only relevant projects are configured in this build run. This means faster build for large multi-project builds.").incubating();
         parser.allowOneOf(MAX_WORKERS, PARALLEL_THREADS);
