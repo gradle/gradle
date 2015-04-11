11c3cf99f5930bca458fbb7791e0e565bc27ed4c
Gary Hale <ghhale@computer.org>
2015-04-11 11:22:19 -0400

    Performance test for precompiled headers
    +review REVIEW-5419

---
+++ subprojects/performance/src/templates/native-pch-source/pch.h
@@ -0,0 +1,10 @@
+#ifndef PCH_H
+#define PCH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
+#include <getopt.h>
+#endif
