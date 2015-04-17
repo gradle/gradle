c857a50dc812a4f2f0b04b168610651e08bda2ea
Cedric Champeau <cedric.champeau@gradleware.com>
2015-04-17 14:08:51 +0200

    Make checkstyle happy

--- subprojects/tooling-api/src/main/java/org/gradle/tooling/model/build/JavaEnvironment.java
+++ subprojects/tooling-api/src/main/java/org/gradle/tooling/model/build/JavaEnvironment.java
@@ -23,8 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Informs about the Java environment, for example the Java home or the JVM args used.
- * See example in {@link BuildEnvironment}.
+ * Informs about the Java environment, for example the Java home or the JVM args used. See example in {@link BuildEnvironment}.
  *
  * @since 1.0-milestone-8
  */
@@ -38,21 +37,17 @@ public interface JavaEnvironment {
     File getJavaHome();
 
     /**
-     * The JVM arguments the user has provided to start the Java process that handles Gradle operations
-     * (for example running tasks or acquiring model information).
-     * The returned arguments do not include system properties passed as -Dfoo=bar.
-     * They may include extra properties added by default if no user jvm arguments are specified,
-     * like those required by the Gradle daemon (eg. MaxPermSize).
+     * The JVM arguments the user has provided to start the Java process that handles Gradle operations (for example running tasks or acquiring model information). The returned arguments do not
+     * include system properties passed as -Dfoo=bar. They may include extra properties added by default if no user jvm arguments are specified, like those required by the Gradle daemon (eg.
+     * MaxPermSize).
      *
      * @since 1.0-milestone-8
      */
     List<String> getJvmArguments();
 
     /**
-     * The effective JVM arguments used to start the Java process that handles Gradle operations
-     * (for example running tasks or acquiring model information).
-     * The returned arguments do not include system properties passed as -Dfoo=bar.
-     * They may include implicitly immutable system properties like "file.encoding".
+     * The effective JVM arguments used to start the Java process that handles Gradle operations (for example running tasks or acquiring model information). The returned arguments do not include
+     * system properties passed as -Dfoo=bar. They may include implicitly immutable system properties like "file.encoding".
      *
      * @since 2.5
      */
@@ -60,10 +55,8 @@ public interface JavaEnvironment {
     List<String> getEffectiveJvmArguments();
 
     /**
-     * The effective JVM arguments used to start the Java process that handles Gradle operations
-     * (for example running tasks or acquiring model information) including
-     * system properties passed as -Dfoo=bar.
-     * They may include implicitly immutable system properties like "file.encoding".
+     * The effective JVM arguments used to start the Java process that handles Gradle operations (for example running tasks or acquiring model information) including system properties passed as
+     * -Dfoo=bar. They may include implicitly immutable system properties like "file.encoding".
      *
      * @since 2.5
      */
@@ -71,20 +64,19 @@ public interface JavaEnvironment {
     List<String> getAllJvmArguments();
 
     /**
-     * The system properties the user provided to start the Java process that handles Gradle operations
-     * (for example running tasks or acquiring model information).
+     * The system properties the user provided to start the Java process that handles Gradle operations (for example running tasks or acquiring model information).
      *
      * @since 2.5
      */
     @Incubating
-    Map<String,String> getSystemProperties();
+    Map<String, String> getSystemProperties();
 
     /**
-     * The effective system properties used to start the Java process that handles Gradle operations
-     * (for example running tasks or acquiring model information), including the default system properties.
+     * The effective system properties used to start the Java process that handles Gradle operations (for example running tasks or acquiring model information), including the default system
+     * properties.
      *
      * @since 2.5
      */
     @Incubating
-    Map<String,String> getEffectiveSystemProperties();
+    Map<String, String> getEffectiveSystemProperties();
 }
