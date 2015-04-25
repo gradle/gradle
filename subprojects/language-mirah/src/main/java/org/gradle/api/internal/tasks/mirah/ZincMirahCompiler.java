fdbfb2aa7b91463a22525542fe9a9ca23d1b69c9
Felix von Ferey <felix+gradle@noreply.ferey.de>
2015-04-26 00:27:57 +0200

    Replace Zinc compiler by Mirah compiler.

--- subprojects/language-mirah/src/main/java/org/gradle/api/internal/tasks/mirah/ZincMirahCompiler.java
+++ subprojects/language-mirah/src/main/java/org/gradle/api/internal/tasks/mirah/ZincMirahCompiler.java
@@ -16,17 +16,14 @@
 
 package org.gradle.api.internal.tasks.mirah;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import org.gradle.api.internal.tasks.SimpleWorkResult;
 import org.gradle.api.internal.tasks.compile.CompilationFailedException;
 import org.gradle.language.base.internal.compile.Compiler;
-import org.gradle.api.internal.tasks.compile.JavaCompilerArgumentsBuilder;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.WorkResult;
-import org.gradle.internal.jvm.Jvm;
-// import mirah.Option;
+
+import org.mirah.tool.Mirahc;
 
 import java.io.File;
 import java.io.Serializable;
@@ -52,9 +49,7 @@ public class ZincMirahCompiler implements Compiler<MirahJavaJointCompileSpec>, S
         static WorkResult execute(Iterable<File> mirahClasspath, Iterable<File> zincClasspath, MirahJavaJointCompileSpec spec) {
             LOGGER.info("Compiling with Zinc Mirah compiler.");
 
-            xsbti.Logger logger = new SbtLoggerAdapter();
-
-            com.typesafe.zinc.Compiler compiler = createCompiler(mirahClasspath, zincClasspath, logger);
+            org.mirah.tool.Mirahc compiler = new Mirahc();
             List<String> mirahcOptions = new ZincMirahCompilerArgumentsGenerator().generate(spec);
             List<String> javacOptions = new JavaCompilerArgumentsBuilder(spec).includeClasspath(false).build();
             Inputs inputs = Inputs.create(ImmutableList.copyOf(spec.getClasspath()), ImmutableList.copyOf(spec.getSource()), spec.getDestinationDir(),
@@ -63,45 +58,13 @@ public class ZincMirahCompiler implements Compiler<MirahJavaJointCompileSpec>, S
                 Inputs.debug(inputs, logger);
             }
 
-            try {
-                compiler.compile(inputs, logger);
-            } catch (xsbti.CompileFailed e) {
-                throw new CompilationFailedException(e);
+            int result = compiler.compile(mirahcOptions.toArray(new String[0]));
+            
+            if (result!=0) {
+                throw new CompilationFailedException();
             }
 
             return new SimpleWorkResult(true);
-        }
-
-        static com.typesafe.zinc.Compiler createCompiler(Iterable<File> mirahClasspath, Iterable<File> zincClasspath, xsbti.Logger logger) {
-            MirahLocation mirahLocation = MirahLocation.fromPath(Lists.newArrayList(mirahClasspath));
-            SbtJars sbtJars = SbtJars.fromPath(Lists.newArrayList(zincClasspath));
-            Setup setup = Setup.create(mirahLocation, sbtJars, Jvm.current().getJavaHome(), true);
-            if (LOGGER.isDebugEnabled()) {
-                Setup.debug(setup, logger);
-            }
-            return com.typesafe.zinc.Compiler.getOrCreate(setup, logger);
-        }
-    }
-
-    private static class SbtLoggerAdapter implements xsbti.Logger {
-        public void error(F0<String> msg) {
-            LOGGER.error(msg.apply());
-        }
-
-        public void warn(F0<String> msg) {
-            LOGGER.warn(msg.apply());
-        }
-
-        public void info(F0<String> msg) {
-            LOGGER.info(msg.apply());
-        }
-
-        public void debug(F0<String> msg) {
-            LOGGER.debug(msg.apply());
-        }
-
-        public void trace(F0<Throwable> exception) {
-            LOGGER.trace(exception.apply().toString());
         }
     }
 }
