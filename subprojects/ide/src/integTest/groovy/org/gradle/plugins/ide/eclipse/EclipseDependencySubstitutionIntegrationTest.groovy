cbe5c60f9fba2b27a6a2ce8edebcff456c797446
Daz DeBoer <darrell.deboer@gradleware.com>
2015-05-21 08:28:30 -0600

    Substituted project dependencies are now honoured in generated IDE classpaths
    +review REVIEW-5436

--- subprojects/ide/src/integTest/groovy/org/gradle/plugins/ide/eclipse/EclipseDependencySubstitutionIntegrationTest.groovy
+++ subprojects/ide/src/integTest/groovy/org/gradle/plugins/ide/eclipse/EclipseDependencySubstitutionIntegrationTest.groovy
@@ -16,7 +16,6 @@
 package org.gradle.plugins.ide.eclipse
 
 import org.gradle.integtests.fixtures.TestResources
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -51,7 +50,6 @@ project(":project2") {
     }
 
     @Test
-    @Ignore("not supported in 2.4 - LD - 14/4/15")
     void "transitive external dependency substituted with project dependency"() {
         mavenRepo.module("org.gradle", "module1").dependsOn("module2").publish()
         mavenRepo.module("org.gradle", "module2").publish()
--- subprojects/ide/src/integTest/groovy/org/gradle/plugins/ide/idea/IdeaDependencySubstitutionIntegrationTest.groovy
+++ subprojects/ide/src/integTest/groovy/org/gradle/plugins/ide/idea/IdeaDependencySubstitutionIntegrationTest.groovy
@@ -15,10 +15,8 @@
  */
 
 package org.gradle.plugins.ide.idea
-
 import org.gradle.integtests.fixtures.TestResources
 import org.gradle.plugins.ide.AbstractIdeIntegrationTest
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -54,7 +52,6 @@ project(":project2") {
     }
 
     @Test
-    @Ignore("not supported in 2.4 - LD - 14/4/15")
     void "transitive external dependency substituted with project dependency"() {
         mavenRepo.module("org.gradle", "module1").dependsOn("module2").publish()
         mavenRepo.module("org.gradle", "module2").publish()
