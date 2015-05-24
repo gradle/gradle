acb09ddbb70bd64b12956c9f0b43e108d2df99d5
Adrian Kelly <adrianbkelly@gmail.com>
2015-05-25 09:56:16 +1000

    Runs all checks, except for integTests, on all subprojects for PR's

--- travisci_build.sh
+++ travisci_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST_FIXME" == "false" ]; then
   echo "==== This was not a pull request, nothing executed! ===="
 else
-  ./gradlew pullRequestValidation
+  ./gradlew prBuild$CI_TEST_SPLIT -x integTest --continue
 fi
