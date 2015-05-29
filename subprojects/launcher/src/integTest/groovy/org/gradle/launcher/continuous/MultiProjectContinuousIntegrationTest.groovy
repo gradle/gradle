377c8331af85b0a709ffcb84551ff9eadabf5a85
Luke Daley <ld@ldaley.com>
2015-05-29 10:29:41 +1000

    Don't allow extra logging for this test.
    +review REVIEW-5511

--- subprojects/launcher/src/integTest/groovy/org/gradle/launcher/continuous/MultiProjectContinuousIntegrationTest.groovy
+++ subprojects/launcher/src/integTest/groovy/org/gradle/launcher/continuous/MultiProjectContinuousIntegrationTest.groovy
@@ -21,6 +21,7 @@ class MultiProjectContinuousIntegrationTest extends Java7RequiringContinuousInte
     def upstreamSource, downstreamSource
 
     def setup() {
+        executer.noExtraLogging()
         settingsFile << "include 'upstream', 'downstream'"
         buildFile << """
             subprojects {
