05294e2fc5b5f0f84ea400e0379db36b281fa22b
Adam Murdoch <adam.murdoch@gradleware.com>
2015-06-16 09:03:40 -0700

    Use correct Script subtypes for init scripts.

--- subprojects/core/src/main/groovy/org/gradle/configuration/InitScriptTarget.java
+++ subprojects/core/src/main/groovy/org/gradle/configuration/InitScriptTarget.java
@@ -17,10 +17,17 @@
 package org.gradle.configuration;
 
 import org.gradle.api.internal.GradleInternal;
+import org.gradle.groovy.scripts.BasicScript;
+import org.gradle.initialization.InitScript;
 
 public class InitScriptTarget extends DefaultScriptTarget {
     public InitScriptTarget(GradleInternal target) {
         super(target);
+    }
+
+    @Override
+    public Class<? extends BasicScript> getScriptClass() {
+        return InitScript.class;
     }
 
     @Override
