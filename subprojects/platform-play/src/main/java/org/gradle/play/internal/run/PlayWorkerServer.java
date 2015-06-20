531c94e544ffea11b2561f10e86b8550c7e93157
Lari Hotari <lari.hotari@gradle.com>
2015-06-19 23:06:33 -0700

    Play reload: fix updating jar files by disabling URL caching
    +review REVIEW-5521

--- subprojects/platform-play/src/main/java/org/gradle/play/internal/run/PlayWorkerServer.java
+++ subprojects/platform-play/src/main/java/org/gradle/play/internal/run/PlayWorkerServer.java
@@ -22,8 +22,12 @@ import org.gradle.internal.UncheckedException;
 import org.gradle.internal.classpath.DefaultClassPath;
 import org.gradle.process.internal.WorkerProcessContext;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.net.URLConnection;
 import java.util.concurrent.CountDownLatch;
 
 public class PlayWorkerServer implements Action<WorkerProcessContext>, PlayRunWorkerServerProtocol, Serializable {
@@ -65,6 +69,7 @@ public class PlayWorkerServer implements Action<WorkerProcessContext>, PlayRunWo
     }
 
     private void run() {
+        disableUrlConnectionCaching();
         final Thread thread = Thread.currentThread();
         final ClassLoader previousContextClassLoader = thread.getContextClassLoader();
         final ClassLoader classLoader = new URLClassLoader(new DefaultClassPath(runSpec.getClasspath()).getAsURLArray());
@@ -80,6 +85,22 @@ public class PlayWorkerServer implements Action<WorkerProcessContext>, PlayRunWo
             throw UncheckedException.throwAsUncheckedException(e);
         } finally {
             thread.setContextClassLoader(previousContextClassLoader);
+        }
+    }
+
+    private void disableUrlConnectionCaching() {
+        // fix problems in updating jar files by disabling default caching of URL connections.
+        // URLConnection default caching should be disabled since it causes jar file locking issues and JVM crashes in updating jar files.
+        // Changes to jar files won't be noticed in all cases when caching is enabled.
+        // sun.net.www.protocol.jar.JarURLConnection leaves the JarFile instance open if URLConnection caching is enabled.
+        try {
+            URL url = new URL("jar:file://valid_jar_url_syntax.jar!/");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDefaultUseCaches(false);
+        } catch (MalformedURLException e) {
+            UncheckedException.throwAsUncheckedException(e);
+        } catch (IOException e) {
+            UncheckedException.throwAsUncheckedException(e);
         }
     }
 
