85349073fc6350fcfbfb4d072037193cef4196ca
Adrian Kelly <adrianbkelly@gmail.com>
2015-06-30 15:55:51 +0100

    Changes test utility to do a full text assertion on model nodes

--- subprojects/diagnostics/src/testFixtures/groovy/org/gradle/api/reporting/model/ModelReportOutput.groovy
+++ subprojects/diagnostics/src/testFixtures/groovy/org/gradle/api/reporting/model/ModelReportOutput.groovy
@@ -36,7 +36,7 @@ class ModelReportOutput {
         List<String> subject = text.trim().readLines()
         assert subject.size() == parsedModelReport.nodeOnlyLines.size()
         parsedModelReport.nodeOnlyLines.eachWithIndex { String line, i ->
-            assert line.startsWith(subject[i]): "\n\n Expected Line:${line} to start with:${subject[i]} line#($i)\n\n"
+            assert line == subject[i]: "\n\n Expected Line:${line} to start with:${subject[i]} line#($i)\n\n"
         }
     }
 
