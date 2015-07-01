af9e2f7c82e0c11a3a808d23a8a80c2be89bfbe6
Amit Portnoy <amit.portnoy@gmail.com>
2015-07-01 17:19:43 +0300

    revert clean task group back to 'build'
    see [#9a56608 ](https://github.com/gradle/gradle/commit/9a56608c6b0cd909a3165008be6f2771b10e5331)

--- subprojects/platform-base/src/main/java/org/gradle/language/base/plugins/LifecycleBasePlugin.java
+++ subprojects/platform-base/src/main/java/org/gradle/language/base/plugins/LifecycleBasePlugin.java
@@ -57,7 +57,7 @@ public class LifecycleBasePlugin implements Plugin<ProjectInternal> {
             @Override
             public void execute(Delete clean) {
                 clean.setDescription("Deletes the build directory.");
-                clean.setGroup(VERIFICATION_GROUP);
+                clean.setGroup(BUILD_GROUP);
                 clean.delete(new Callable<File>() {
                     public File call() throws Exception {
                         return project.getBuildDir();
