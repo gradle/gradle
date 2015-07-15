0d5f55fd79836e78f81888e0b0bfff00c121a2d8
Benjamin Muschko <benjamin.muschko@gmail.com>
2015-07-15 15:59:49 -0400

    Added integration tests that simulate the use of the test-kit from an end-users perspective,
    +review REVIEW-5533

---
+++ subprojects/test-kit/src/integTest/groovy/org/gradle/testkit/TestKitEndUserIntegrationTest.groovy
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.testkit
+
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.executer.ExecutionFailure
+import org.gradle.integtests.fixtures.executer.ExecutionResult
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.util.GFileUtils
+
+class TestKitEndUserIntegrationTest extends AbstractIntegrationSpec {
+    public static final String[] GROOVY_SRC_DIR = ['src', 'test', 'groovy'] as String[]
+    public static final String[] TEST_CLASS_PACKAGE = ['org', 'gradle', 'test'] as String[]
+
+    def setup() {
+        executer.requireGradleHome()
+    }
+
+    def "use of GradleRunner API in test class without declaring test-kit dependency causes compilation error"() {
+        given:
+        buildFile << buildFileForGroovyProject()
+        File groovySrcDir = testDirectoryProvider.createDir(GROOVY_SRC_DIR + TEST_CLASS_PACKAGE)
+        GFileUtils.writeFile("""package org.gradle.test
+
+import org.gradle.testkit.runner.GradleRunner
+
+class BuildLogicFunctionalTest {
+    def "create GradleRunner"() {
+        when:
+        GradleRunner.create()
+
+        then:
+        noExceptionThrown()
+    }
+}
+""", new File(groovySrcDir, 'BuildLogicFunctionalTest.groovy'))
+
+        when:
+        ExecutionFailure failure = fails('build')
+
+        then:
+        result.executedTasks.contains(':compileTestGroovy')
+        !result.skippedTasks.contains(':compileTestGroovy')
+        failure.error.contains("unable to resolve class $GradleRunner.name")
+        failure.error.contains("""Execution failed for task ':compileTestGroovy'.
+> Compilation failed; see the compiler error output for details.""")
+    }
+
+    def "successfully execute functional test and verify expected result"() {
+        buildFile << buildFileForGroovyProject()
+        buildFile << gradleTestKitDependency()
+        File groovySrcDir = testDirectoryProvider.createDir(GROOVY_SRC_DIR + TEST_CLASS_PACKAGE)
+        GFileUtils.writeFile("""package org.gradle.test
+
+import org.gradle.testkit.runner.GradleRunner
+import static org.gradle.testkit.runner.TaskResult.*
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class BuildLogicFunctionalTest {
+    @Rule final TemporaryFolder testProjectDir = TemporaryFolder()
+    File buildFile
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+    }
+
+    def "execute helloWorld task"() {
+        given:
+        buildFile << '''
+            task helloWorld {
+                doLast {
+                    println 'Hello world!'
+                }
+            }
+        '''
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments('helloWorld')
+            .build()
+
+        then:
+        result.standardOutput.contains('Hello world!')
+        result.standardError == ''
+        result.taskPaths(SUCCESS) == [':helloWorld']
+        result.tasks(SKIPPED).empty
+        result.tasks(UP_TO_DATE).empty
+        result.tasks(FAILED).empty
+    }
+}
+""", new File(groovySrcDir, 'BuildLogicFunctionalTest.groovy'))
+
+        when:
+        ExecutionResult result = succeeds('build')
+
+        then:
+        result.executedTasks.containsAll([':test', ':build'])
+        !result.skippedTasks.contains(':test')
+    }
+
+    def "functional test fails due to invalid JVM parameter for test execution"() {
+        buildFile << buildFileForGroovyProject()
+        buildFile << gradleTestKitDependency()
+        GFileUtils.writeFile('org.gradle.jvmargs=-unknown', testDirectory.file('gradle.properties'))
+        File groovySrcDir = testDirectoryProvider.createDir(GROOVY_SRC_DIR + TEST_CLASS_PACKAGE)
+        GFileUtils.writeFile("""package org.gradle.test
+
+import org.gradle.testkit.runner.GradleRunner
+import static org.gradle.testkit.runner.TaskResult.*
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class BuildLogicFunctionalTest {
+    @Rule final TemporaryFolder testProjectDir = TemporaryFolder()
+    File buildFile
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+    }
+
+    def "execute helloWorld task"() {
+        given:
+        buildFile << '''
+            task helloWorld {
+                doLast {
+                    println 'Hello world!'
+                }
+            }
+        '''
+
+        when:
+        GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments('helloWorld')
+            .build()
+
+        then:
+        notExceptionThrown()
+    }
+}
+""", new File(groovySrcDir, 'BuildLogicFunctionalTest.groovy'))
+
+        when:
+        ExecutionFailure failure = fails('build')
+
+        then:
+        !result.executedTasks.contains(':test')
+        !result.skippedTasks.contains(':test')
+        failure.error.contains('Unrecognized option: -unknown')
+    }
+
+    private String buildFileForGroovyProject() {
+        """
+            apply plugin: 'groovy'
+
+            dependencies {
+                testCompile localGroovy()
+                testCompile 'org.spockframework:spock-core:1.0-groovy-2.3'
+            }
+
+            repositories {
+                mavenCentral()
+            }
+        """
+    }
+
+    private String gradleTestKitDependency() {
+        """
+            dependencies {
+                testCompile gradleTestKit()
+            }
+        """
+    }
+}
