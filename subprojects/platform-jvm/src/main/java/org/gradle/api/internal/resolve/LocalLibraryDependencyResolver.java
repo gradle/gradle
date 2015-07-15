5ec21b6a44f58d57aa5b6630e4a52aa721ca70d3
Cedric Champeau <cedric.champeau@gradleware.com>
2015-07-15 21:44:47 +0200

    Second attempt at fixing Java 6 build
    +review REVIEW-5554

--- subprojects/platform-jvm/src/main/java/org/gradle/api/internal/resolve/LocalLibraryDependencyResolver.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/api/internal/resolve/LocalLibraryDependencyResolver.java
@@ -212,7 +212,7 @@ public class LocalLibraryDependencyResolver implements DependencyToComponentIdRe
                             message = String.format("'%s'", value);
                         }
                         if (incompatibleDimensionTypes.contains(dimension)) {
-                            message = String.format("%s but with an incompatible type (expected '%s' was '%s')", message, variantsMetaData.getDimensionType(dimension).getType().toString(), md.getDimensionType(dimension).getType().toString());
+                            message = String.format("%s but with an incompatible type (expected '%s' was '%s')", message, variantsMetaData.getDimensionType(dimension).getConcreteClass().getName(), md.getDimensionType(dimension).getConcreteClass().getName());
                         }
                         variants.put(dimension, message);
                     }
