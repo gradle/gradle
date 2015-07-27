7c80da32759d48dc5516bb84c08b91b4759743ca
Benjamin Muschko <benjamin.muschko@gmail.com>
2015-07-27 17:02:50 -0400

    Escape file path to fix test on Windows.
    +review REVIEW-5555

--- subprojects/test-kit/src/integTest/groovy/org/gradle/testkit/TestKitEndUserIntegrationTest.groovy
+++ subprojects/test-kit/src/integTest/groovy/org/gradle/testkit/TestKitEndUserIntegrationTest.groovy
@@ -300,8 +300,7 @@ class TestKitEndUserIntegrationTest extends AbstractIntegrationSpec {
     }
 
     private DaemonLogsAnalyzer createDaemonLogAnalyzer() {
-        File gradleUserHomeDir = new IsolatedDaemonHomeTmpDirectoryProvider().createDir()
-        File daemonBaseDir = new File(gradleUserHomeDir, 'daemon')
+        File daemonBaseDir = new File(IsolatedDaemonHomeTmpDirectoryProvider.TMP_DIR, 'daemon')
         DaemonLogsAnalyzer.newAnalyzer(daemonBaseDir, executer.distribution.version.version)
     }
 
