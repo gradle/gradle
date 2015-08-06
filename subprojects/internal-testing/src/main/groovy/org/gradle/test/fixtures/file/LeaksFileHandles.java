57dc63cd1a236e79e26cc038e0ed1227de9f5e8a
Luke Daley <ld@ldaley.com>
2015-08-06 14:30:38 +1000

    Add back LeaksFileHandles for cases that are too hard to fix.

--- subprojects/internal-testing/src/main/groovy/org/gradle/test/fixtures/file/LeaksFileHandles.java
+++ subprojects/internal-testing/src/main/groovy/org/gradle/test/fixtures/file/LeaksFileHandles.java
@@ -18,8 +18,14 @@ package org.gradle.test.fixtures.file;
 
 import java.lang.annotation.*;
 
+/**
+ * Declares that the test holds files open and therefore not to error if the test workspace can't be cleaned up.
+ *
+ * @see AbstractTestDirectoryProvider
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Inherited
 public @interface LeaksFileHandles {
+    String value() default "";
 }
