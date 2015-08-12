54328f36824850b69dcd031d0f248e364770c374
Luke Daley <ld@ldaley.com>
2015-08-12 20:49:24 +1000

    Add leaks file handles.

--- subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r22/ClientShutdownCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r22/ClientShutdownCrossVersionSpec.groovy
@@ -20,6 +20,7 @@ import org.gradle.integtests.fixtures.executer.GradleExecuter
 import org.gradle.integtests.tooling.fixture.TargetGradleVersion
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
 import org.gradle.integtests.tooling.fixture.ToolingApiVersion
+import org.gradle.test.fixtures.file.LeaksFileHandles
 import org.gradle.test.fixtures.server.http.CyclicBarrierHttpServer
 import org.gradle.tooling.GradleConnector
 import org.gradle.tooling.internal.consumer.DefaultGradleConnector
@@ -113,6 +114,7 @@ task slow << { new URL("${server.uri}").text }
     }
 
     @TargetGradleVersion(">=2.2")
+    @LeaksFileHandles
     def "shutdown ignores daemons that were not started by client"() {
         given:
         toolingApi.requireIsolatedDaemons()
