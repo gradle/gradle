197b3aabe721b967c244df234769fd70fe2c4adc
Sterling Greene <sterling.greene@gmail.com>
2015-08-14 19:20:08 -0400

    Make it easier to find supported architecture names

--- subprojects/platform-native/src/main/groovy/org/gradle/nativeplatform/platform/NativePlatform.java
+++ subprojects/platform-native/src/main/groovy/org/gradle/nativeplatform/platform/NativePlatform.java
@@ -45,40 +45,10 @@ public interface NativePlatform extends Platform {
 
     /**
      * Sets the cpu architecture being targeted.
+     * <p>
      * The architecture is provided as a string name, which is translated into one of the supported architecture types.
-     *
-     * <table>
-     *     <tr>
-     *         <th>Instruction Set</th>
-     *         <th>32-bit names</th>
-     *         <th>64-bit names</th>
-     *     </tr>
-     *     <tr>
-     *         <td>Intel x86</td>
-     *         <td>"x86", "i386", "ia-32"</td>
-     *         <td>"x86_64", "amd64", "x64", "x86-64"</td>
-     *     </tr>
-     *     <tr>
-     *         <td>Intel Itanium</td>
-     *         <td></td>
-     *         <td>"ia-64"</td>
-     *     </tr>
-     *     <tr>
-     *         <td>Power PC</td>
-     *         <td>"ppc"</td>
-     *         <td>"ppc64"</td>
-     *     </tr>
-     *     <tr>
-     *         <td>Sparc</td>
-     *         <td>"sparc", "sparc32", "sparc-v7", "sparc-v8"</td>
-     *         <td>"sparc64", "ultrasparc", "sparc-v9"</td>
-     *     </tr>
-     *     <tr>
-     *         <td>ARM</td>
-     *         <td>"arm"</td>
-     *         <td></td>
-     *     </tr>
-     * </table>
+     * </p>
+     * @see Architecture Supported notations.
      */
     void architecture(String name);
 
@@ -90,30 +60,10 @@ public interface NativePlatform extends Platform {
 
     /**
      * Sets the operating system being targeted.
+     * <p>
      * The operating system is provided as a string name, which is translated into one of the supported operating system types.
-     *
-     * <table>
-     *     <tr>
-     *         <th>Operating System</th>
-     *         <th>Aliases</th>
-     *     </tr>
-     *     <tr>
-     *         <td>Windows</td>
-     *         <td>"windows"</td>
-     *     </tr>
-     *     <tr>
-     *         <td>GNU/Linux</td>
-     *         <td>"linux"</td>
-     *     </tr>
-     *     <tr>
-     *         <td>Mac OS X</td>
-     *         <td>"osx", "mac os x", "darwin"</td>
-     *     </tr>
-     *     <tr>
-     *         <td>Solaris</td>
-     *         <td>"solaris", "sunos"</td>
-     *     </tr>
-     * </table>
+     * </p>
+     * @see OperatingSystem Supported notations.
      */
     void operatingSystem(String name);
 }
