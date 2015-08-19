12db5e0985c128c6d4b7f341f0b78a18848379f1
Sterling Greene <sterling.greene@gmail.com>
2015-08-19 16:08:51 -0400

    Validate PlayApplicationBinarySpec instead of PlayApplicationSpec
    +review REVIEW-5572

--- subprojects/platform-play/src/main/java/org/gradle/play/plugins/PlayApplicationPlugin.java
+++ subprojects/platform-play/src/main/java/org/gradle/play/plugins/PlayApplicationPlugin.java
@@ -158,12 +158,12 @@ public class PlayApplicationPlugin implements Plugin<Project> {
         }
 
         @Validate
-        void failIfInjectedRouterIsUsedwithOldVersion(ModelMap<PlayApplicationSpec> playApplications, final PlatformResolvers platforms) {
-            playApplications.afterEach(new Action<PlayApplicationSpec>() {
+        void failIfInjectedRouterIsUsedwithOldVersion(ModelMap<PlayApplicationBinarySpec> playApplicationBinaries, final PlatformResolvers platforms) {
+            playApplicationBinaries.afterEach(new Action<PlayApplicationBinarySpec>() {
                 @Override
-                public void execute(PlayApplicationSpec playApplication) {
-                    if (!playApplication.getUseStaticRouter()) {
-                        final PlayPlatform playPlatform = resolveTargetPlatform(playApplication, platforms);
+                public void execute(PlayApplicationBinarySpec playApplicationBinary) {
+                    if (!playApplicationBinary.getApplication().getUseStaticRouter()) {
+                        final PlayPlatform playPlatform = playApplicationBinary.getTargetPlatform();
                         VersionNumber minSupportedVersion = VersionNumber.parse("2.4.0");
                         VersionNumber playVersion = VersionNumber.parse(playPlatform.getPlayVersion());
                         if (playVersion.compareTo(minSupportedVersion) < 0) {
