0a0acdd8a1ee3ff4f49398ed3a8999ce357007c8
Adam Murdoch <adam.murdoch@gradleware.com>
2015-08-22 14:29:40 +1000

    Fixed int test on windows 1.6 commit build.

--- subprojects/launcher/src/integTest/groovy/org/gradle/launcher/continuous/JdkVersionsContinuousIntegrationTest.groovy
+++ subprojects/launcher/src/integTest/groovy/org/gradle/launcher/continuous/JdkVersionsContinuousIntegrationTest.groovy
@@ -18,6 +18,7 @@ package org.gradle.launcher.continuous
 
 import org.gradle.api.JavaVersion
 import org.gradle.integtests.fixtures.AvailableJavaHomes
+import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
 import org.gradle.internal.jvm.JavaInfo
 import org.gradle.util.Requires
 
@@ -34,7 +35,10 @@ class JdkVersionsContinuousIntegrationTest extends AbstractContinuousIntegration
         failureDescriptionContains("Continuous build requires Java 7 or later.")
     }
 
-    @Requires(adhoc = { JdkVersionsContinuousIntegrationTest.java6() && JdkVersionsContinuousIntegrationTest.java7OrBetter() })
+    @Requires(adhoc = { JdkVersionsContinuousIntegrationTest.java6() && JdkVersionsContinuousIntegrationTest.java7OrBetter() &&
+            // This test doesn't work on Java 1.6 CI commit builds as these builds rebuild the distribution using Java 6 rather than reuse
+            // the distribution that is shared by the coverage builds. The following approximates "don't run this test on the Java 1.6 CI commit builds"
+            (JavaVersion.current().java7Compatible || !GradleContextualExecuter.embedded) })
     def "can use java6 client with later build runtime"() {
         given:
         executer
