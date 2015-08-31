ba8988acebf0d8450d1df1d4b6bc757f08237368
Cedric Champeau <cedric.champeau@gradleware.com>
2015-08-31 09:56:09 +0200

    Fix checkstyle
    +review REVIEW-5606

--- subprojects/core/src/main/groovy/org/gradle/api/internal/project/antbuilder/DefaultIsolatedAntBuilder.java
+++ subprojects/core/src/main/groovy/org/gradle/api/internal/project/antbuilder/DefaultIsolatedAntBuilder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gradle.api.internal.project.antbuilder;
 
 import com.google.common.collect.Lists;
@@ -130,7 +145,7 @@ public class DefaultIsolatedAntBuilder implements IsolatedAntBuilder {
 
     public void execute(Closure antClosure) {
         ClassPathToClassLoader cached = getCachedLoader(libClasspath);
-        ClassLoader classLoader = (cached == null ? null : cached.getClassLoader());
+        ClassLoader classLoader = cached == null ? null : cached.getClassLoader();
         boolean cacheLoader = classLoader == null;
         if (cacheLoader) {
             classLoader = new URLClassLoader(libClasspath.getAsURLArray(), baseAntLoader);
