65a99a7c11c5ae9a343c3432931c3a9e8e43eda3
Lari Hotari <lari.hotari@gradle.com>
2015-09-04 13:31:00 -0400

    manual performance test for incremental task inputs
    example command to launch tests:
    ./gradlew :performance:manualPerformanceTest -D:performance:manualPerformanceTest.single=IncrementalTaskInputsPerformanceTest
    add -Porg.gradle.performance.use_yourkit to command to activate profiling
    
    +review REVIEW-5611

---
+++ subprojects/performance/src/integTest/groovy/org/gradle/performance/IncrementalTaskInputsPerformanceTest.groovy
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.gradle.performance
+
+import org.gradle.performance.fixture.BuildExperimentSpec
+import org.junit.experimental.categories.Category
+import spock.lang.Unroll
+
+@Category(ManualPerformanceTest)
+class IncrementalTaskInputsPerformanceTest extends AbstractCrossBuildPerformanceTest {
+
+    @Override
+    protected void defaultSpec(BuildExperimentSpec.Builder builder) {
+        builder.invocationCount(1).warmUpCount(1)
+    }
+
+    @Unroll
+    def "compare incremental task inputs to ordinary task inputs #inputCount"() {
+        when:
+        runner.testGroup = "incremental task inputs"
+        runner.testId = "compare incremental task inputs to ordinary task inputs"
+        runner.buildSpec {
+            projectName("compareTaskInputs").displayName("incremental inputs $inputCount").invocation {
+                tasksToRun("clean", "buildIncremental").args("-PinputCount=$inputCount").useDaemon()
+            }
+        }
+        runner.baseline {
+            projectName("compareTaskInputs").displayName("ordinary inputs $inputCount").invocation {
+                tasksToRun("clean", "buildOrdinary").args("-PinputCount=$inputCount").useDaemon()
+            }
+        }
+
+        then:
+        runner.run()
+
+        where:
+        inputCount << [1, 10, 100, 1000, 10000]
+    }
+}
