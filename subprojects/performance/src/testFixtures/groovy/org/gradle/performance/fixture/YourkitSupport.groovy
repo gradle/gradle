796b7ff8d3b38454b357127ec8f0cb499762d6bf
Sterling Greene <sterling@gradle.com>
2015-09-04 13:38:46 -0400

    clean up yourkit docs
    +review REVIEW-5607

--- subprojects/performance/src/testFixtures/groovy/org/gradle/performance/fixture/YourkitSupport.groovy
+++ subprojects/performance/src/testFixtures/groovy/org/gradle/performance/fixture/YourkitSupport.groovy
@@ -24,78 +24,76 @@ import org.gradle.internal.os.OperatingSystem
 /**
  *
  * Helper class for adding Yourkit Java Profiler (YJP) agent start up arguments for builds launched in performance tests.
+ * <p>
+ * Currently, YJP_HOME environment variable or YJP_AGENT_PATH environment variable is used to locate the agent library file. This is not needed if you install YJP in /opt/yjp on Linux
+ * or rename the application as "yjp" on Mac OS X (directory: /Applications/yjp.app). These locations are searched by default.
  *
- * For adding the arguments, the Yourkit installation directory should be known by this implemantation. Currently YJP_HOME environment variable or YJP_AGENT_PATH environment variable is used to locate the agent library file.
- * This is not needed if you install YJP in /opt/yjp on Linux or rename the application as "yjp" on Mac OS X (directory: /Applications/yjp.app). These locations are searched by default.
- *
+ * <p>
  * Examples of specifying YJP_HOME
  * <pre>
- * {@code
- * export YJP_HOME=/opt/yjp # on linux
- * export YJP_HOME=/Applications/yjp.app/Contents/Resources # on Mac OS X
- *}
+ * export YJP_HOME="/opt/yjp" # on linux
+ * export YJP_HOME="/Applications/yjp.app/Contents/Resources" # on Mac OS X
  * </pre>
  *
+ * <p>
  * Examples of specifying YJP_AGENT_PATH directly
  * <pre>
- * {@code
- * export YJP_AGENT_PATH=/opt/yjp/bin/linux-x86-64/libyjpagent.so # on linux
- * export YJP_AGENT_PATH=/Applications/yjp.app/Contents/Resources/bin/mac/libyjpagent.jnilib # on Mac OS X
- *}
+ * export YJP_AGENT_PATH="/opt/yjp/bin/linux-x86-64/libyjpagent.so" # on linux
+ * export YJP_AGENT_PATH="/Applications/yjp.app/Contents/Resources/bin/mac/libyjpagent.jnilib" # on Mac OS X
  * </pre>
  *
+ * <p>
  * If you profile on Windows, you will have to add YJP_AGENT_PATH environment variable pointing to the yjpagent.dll file (YJP installation directory\bin\win64\yjpagent.dll).
  *
- * Performance tests extending AbstractCrossBuildPerformanceTest or AbstractCrossVersionPerformanceTest will add YJP agent parameters to the Gradle build process when you add {@code -Porg.gradle.performance.use_yourkit } parameter to the command line.
+ * <p>
+ * Performance tests extending {@link org.gradle.performance.AbstractCrossBuildPerformanceTest} or {@link org.gradle.performance.AbstractCrossVersionPerformanceTest} will add
+ * YJP agent parameters to the Gradle build process when you add {@code -Porg.gradle.performance.use_yourkit } parameter to the command line.
  *
+ * <p>
  * Example of running a single performance test with YJP agent options:
  * <pre>
- * {@code
  * ./gradlew performance:performanceTest -Porg.gradle.performance.use_yourkit -D:performance:performanceTest.single=NativePreCompiledHeaderPerformanceTest
- *}
  * </pre>
  *
+ * <p>
  * By default, YJP will create a snapshot in ~/Snapshots when the profiled process terminates. The file name contains the test project name and display name from the performance test.
  *
- * This integration reads Yourkit agent options from {@code ~/.gradle/yourkit.properties } file. <a href="https://www.yourkit.com/docs/java/help/startup_options.jsp">Yourkit startup options</a>
+ * <p>
+ * This integration reads YourKit agent options from {@code ~/.gradle/yourkit.properties} file. <a href="https://www.yourkit.com/docs/java/help/startup_options.jsp">YourKit startup options</a>
  *
- * example settings for tracing profiling
+ * <p>
+ * Example settings for tracing profiling
  * <pre>
- * {@code
  * tracing=true
  * disablealloc=true
  * monitors=true
  * probe_disable=*
  * delay=0
- *}
  * </pre>
- * tweak tracing settings in {@code ~/.yjp/tracing.txt } , <a href="http://www.yourkit.com/docs/java/help/tracing_settings.jsp">tracing.txt reference</a>
+ * <p>
+ * Tweak tracing settings in {@code ~/.yjp/tracing.txt}, <a href="http://www.yourkit.com/docs/java/help/tracing_settings.jsp">tracing.txt reference</a>
  * useful setting is "adaptive=false" to trace all method calls.
- *
- * example settings for sampling profiling
+ * <p>
+ * Example settings for sampling profiling
  * <pre>
- * {@code
  * sampling=true
  * disablealloc=true
  * monitors=true
  * probe_disable=*
  * delay=0
- *}
  * </pre>
- * tweak sampling settings in {@code ~/.yjp/sampling.txt } , <a href="http://www.yourkit.com/docs/java/help/sampling_settings.jsp">sampling.txt reference</a>
+ * <p>
+ * Tweak sampling settings in {@code ~/.yjp/sampling.txt}, <a href="http://www.yourkit.com/docs/java/help/sampling_settings.jsp">sampling.txt reference</a>
  * useful setting is "sampling_period_ms", which is 20 ms by default.
  *
- * example settings for call counting profiling
+ * Example settings for call counting profiling
  * <pre>
- * {@code
  * call_counting=true
  * disablealloc=true
  * monitors=true
  * probe_disable=*
  * delay=0
- *}
  * </pre>
- *
  */
 @CompileStatic
 class YourkitSupport {
@@ -169,7 +167,7 @@ class YourkitSupport {
             }
         }
         if (yjpOptionsStringBuilder.length() == 0) {
-            throw new IllegalArgumentException("You must specify some Yourkit agent options.")
+            throw new IllegalArgumentException("You must specify some YourKit agent options.")
         }
         yjpOptionsStringBuilder.toString()
     }
@@ -192,7 +190,7 @@ class YourkitSupport {
             }
         }
         if (!resolvedYjpAgentPath || !new File(resolvedYjpAgentPath).isFile()) {
-            throw new IllegalStateException("Cannot find Yourkit agent library ($resolvedYjpAgentPath). Add YJP_AGENT_PATH environment variable that points to Yourkit JVM agent library or add YJP_HOME environment variable that points to Yourkit installation directory.")
+            throw new IllegalStateException("Cannot find YourKit agent library ($resolvedYjpAgentPath). Add YJP_AGENT_PATH environment variable that points to Yourkit JVM agent library or add YJP_HOME environment variable that points to Yourkit installation directory.")
         }
         resolvedYjpAgentPath
     }
