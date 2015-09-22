25884ddc51e33682a927b9265a538d55d70d25fd
Lóránt Pintér <lorant@gradle.com>
2015-09-22 19:18:45 +0200

    Code cleanup

--- subprojects/model-core/src/main/java/org/gradle/model/internal/core/NodeBackedModelMap.java
+++ subprojects/model-core/src/main/java/org/gradle/model/internal/core/NodeBackedModelMap.java
@@ -66,8 +66,7 @@ public class NodeBackedModelMap<T> implements ModelMap<T>, ManagedInstance {
                 if (baseItemModelType.asSubclass(type) == null) {
                     throw new IllegalArgumentException(String.format("%s is not a subtype of %s", type, baseItemModelType));
                 }
-                NodeInitializer nodeInitializer = nodeInitializerRegistry.getNodeInitializer(type);
-                return nodeInitializer;
+                return nodeInitializerRegistry.getNodeInitializer(type);
             }
         };
     }
