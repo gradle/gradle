02ea4f1f171c3122c3f862bdc1ac790dba4e42ca
Luke Daley <ld@ldaley.com>
2015-09-24 12:38:31 +1000

    Checkstyle.
    +review REVIEW-5570

--- subprojects/plugin-use/src/main/java/org/gradle/plugin/use/resolve/internal/PluginResolveContext.java
+++ subprojects/plugin-use/src/main/java/org/gradle/plugin/use/resolve/internal/PluginResolveContext.java
@@ -17,7 +17,6 @@
 package org.gradle.plugin.use.resolve.internal;
 
 import org.gradle.api.internal.plugins.PluginImplementation;
-import org.gradle.internal.classpath.ClassPath;
 import org.gradle.plugin.internal.PluginId;
 
 public interface PluginResolveContext {
