f55ef090bb54560a7a45d296e1f262188f129643
Lari Hotari <lari.hotari@gradle.com>
2015-10-09 13:28:39 -0400

    use real FileCollection implementation in test and fix invalid tests
    +review REVIEW-5623

--- subprojects/core/src/test/groovy/org/gradle/api/internal/changedetection/state/DefaultFileCollectionSnapshotterTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/changedetection/state/DefaultFileCollectionSnapshotterTest.groovy
@@ -16,14 +16,16 @@
 package org.gradle.api.internal.changedetection.state
 
 import org.gradle.api.file.FileCollection
-import org.gradle.api.file.FileTree
+import org.gradle.api.internal.file.collections.SimpleFileCollection
 import org.gradle.internal.hash.HashUtil
 import org.gradle.test.fixtures.file.TestFile
 import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider
 import org.gradle.util.ChangeListener
+import org.gradle.util.UsesNativeServices
 import org.junit.Rule
 import spock.lang.Specification
 
+@UsesNativeServices
 public class DefaultFileCollectionSnapshotterTest extends Specification {
     def fileSnapshotter = Stub(FileSnapshotter)
     def cacheAccess = Stub(TaskArtifactStateCacheAccess)
@@ -56,7 +58,7 @@ public class DefaultFileCollectionSnapshotterTest extends Specification {
         then:
         snapshot.files.files as List == [file]
     }
-    
+
     def notifiesListenerWhenFileAdded() {
         given:
         TestFile file1 = tmpDir.createFile('file1')
@@ -101,13 +103,15 @@ public class DefaultFileCollectionSnapshotterTest extends Specification {
 
     def fileHasChangedWhenTypeHasChanged() {
         given:
-        TestFile file = tmpDir.createFile('file')
+        TestFile root = tmpDir.createDir('root')
+        TestFile file = root.createFile('file')
+        def fileCollection = files(root)
 
         when:
-        FileCollectionSnapshot snapshot = snapshotter.snapshot(files(file))
+        FileCollectionSnapshot snapshot = snapshotter.snapshot(fileCollection)
         file.delete()
         file.createDir()
-        snapshotter.snapshot(files(file)).iterateChangesSince(snapshot).next(listener)
+        snapshotter.snapshot(fileCollection).iterateChangesSince(snapshot).next(listener)
 
         then:
         1 * listener.changed(file.path)
@@ -150,13 +154,15 @@ public class DefaultFileCollectionSnapshotterTest extends Specification {
     }
 
     def directoryHasChangedWhenTypeHasChanged() {
-        TestFile dir = tmpDir.createDir('dir')
+        TestFile root = tmpDir.createDir('root')
+        def fileCollection = files(root)
+        TestFile dir = root.createDir('dir')
 
         when:
-        FileCollectionSnapshot snapshot = snapshotter.snapshot(files(dir))
+        FileCollectionSnapshot snapshot = snapshotter.snapshot(fileCollection)
         dir.deleteDir()
         dir.createFile()
-        snapshotter.snapshot(files(dir)).iterateChangesSince(snapshot).next(listener)
+        snapshotter.snapshot(fileCollection).iterateChangesSince(snapshot).next(listener)
 
         then:
         1 * listener.changed(dir.path)
@@ -175,16 +181,18 @@ public class DefaultFileCollectionSnapshotterTest extends Specification {
         0 * _
     }
 
-    def nonExistentFileIsChangedWhenTypeHasChanged() {
-        TestFile file = tmpDir.file('unknown')
+    def nonExistentFileIsAdded() {
+        TestFile root = tmpDir.createDir('root')
+        def fileCollection = files(root)
+        TestFile file = root.file('unknown')
 
         when:
-        FileCollectionSnapshot snapshot = snapshotter.snapshot(files(file))
+        FileCollectionSnapshot snapshot = snapshotter.snapshot(fileCollection)
         file.createFile()
-        snapshotter.snapshot(files(file)).iterateChangesSince(snapshot).next(listener)
+        snapshotter.snapshot(fileCollection).iterateChangesSince(snapshot).next(listener)
 
         then:
-        1 * listener.changed(file.path)
+        1 * listener.added(file.path)
     }
 
     def ignoresDuplicatesInFileCollection() {
@@ -343,10 +351,7 @@ public class DefaultFileCollectionSnapshotterTest extends Specification {
     }
 
     private FileCollection files(File... files) {
-        FileTree collection = Mock(FileTree.class)
-        _ * collection.asFileTree >> collection
-        _ * collection.getFiles() >> files
-        return collection
+        new SimpleFileCollection(files)
     }
-    
+
 }
