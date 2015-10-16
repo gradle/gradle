gradle/gradle

commit d9d68d916fbc507db0ab507919b1ca802924f73a

Create API jar according to API specification
This change ensures that a corresponding API jar is automatically
created alongside every normal "runtime" jar, and ensures that the
contents of the API jar include only classes declared in packages
exported by the build script's API spec.

If no API spec is present in the build script, an API jar is still
created, but contains all packages present in the runtime jar. That is,
the API and runtime jar contents will be identical. A later story will
introduce so-called "API stripping", which will ensure that non-public
members are removed from API jars in a safe and effective manner.

The related design doc has been updated and implemented test cases have
been marked complete. The test cases that remain deal with avoiding
triggering recompilation when it is not necessary, and these will be
addressed together in a subsequent commit.

Implementation notes:

 - The changes made in `JvmComponentPlugin#createTasks` are non-ideal, a
   workaround to compensate for the fact that we do not yet have
   first-class "variants" at the `JvmComponentSpec` level. That is, we
   do not yet have the capability to express that a single logical JVM
   library is manifested physically in two or more binary variants (in
   this case, the API and runtime jar). This modeling and refactoring
   work is planned for and addressed by other stories intended to be
   started soon; as such, this implementation in this commit should be
   considered stopgap.

 - The use of `JvmBinaryTasks#getJar`, along with its assumptions about
   any given binary being associated with one jar and one jar only are
   now no longer valid as of this change. This constraint has been
   relaxed by replacing `#getJar` calls with `DomainObjectSet#withType`
   which allows for returning zero or more matches. The `#getJar` method
   is now used in only one place in production code
   (`ScalaLanguagePlugin`), and as such it may be best to refactor away

subprojects/platform-jvm/src/main/java/org/gradle/jvm/plugins/JvmComponentPlugin.java
@@ -16,8 +16,8 @@
 
 package org.gradle.jvm.plugins;
 
-import org.apache.commons.lang.StringUtils;
 import org.gradle.api.*;
+import org.gradle.api.tasks.Copy;
 import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.jvm.JarBinarySpec;
 import org.gradle.jvm.JvmLibrarySpec;
@@ -39,6 +39,9 @@ import javax.inject.Inject;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+
+import static org.apache.commons.lang.StringUtils.capitalize;
 
 /**
  * Base plugin for JVM component support. Applies the
@@ -99,12 +102,14 @@ public class JvmComponentPlugin implements Plugin<Project> {
         public void createBinaries(ModelMap<JarBinarySpec> binaries, BinaryNamingSchemeBuilder namingSchemeBuilder,
                                    PlatformResolvers platforms, final JvmLibrarySpec jvmLibrary) {
             List<JavaPlatform> selectedPlatforms = resolvePlatforms(platforms, jvmLibrary);
+            final Set<String> exportedPackages = jvmLibrary.getExportedPackages();
             for (final JavaPlatform platform : selectedPlatforms) {
                 String binaryName = buildBinaryName(jvmLibrary, namingSchemeBuilder, selectedPlatforms, platform);
                 binaries.create(binaryName, new Action<JarBinarySpec>() {
                     @Override
                     public void execute(JarBinarySpec jarBinary) {
                         jarBinary.setTargetPlatform(platform);
+                        jarBinary.setExportedPackages(exportedPackages);
                     }
                 });
             }
@@ -139,16 +144,49 @@ public class JvmComponentPlugin implements Plugin<Project> {
 
         @BinaryTasks
         public void createTasks(ModelMap<Task> tasks, final JarBinarySpec binary) {
-            String taskName = "create" + StringUtils.capitalize(binary.getName());
-            tasks.create(taskName, Jar.class, new Action<Jar>() {
+            final String jarArchiveName = binary.getJarFile().getName();
+
+            String runtimeJarName = binary.getName();
+            final File runtimeClassesDir = binary.getClassesDir();
+            final File runtimeJarDestDir = binary.getJarFile().getParentFile();
+            final String createRuntimeJar = "create" + capitalize(binary.getName());
+            tasks.create(createRuntimeJar, Jar.class, new Action<Jar>() {
                 @Override
                 public void execute(Jar jar) {
                     jar.setDescription(String.format("Creates the binary file for %s.", binary));
-                    jar.from(binary.getClassesDir());
+                    jar.from(runtimeClassesDir);
                     jar.from(binary.getResourcesDir());
+                    jar.setDestinationDir(runtimeJarDestDir);
+                    jar.setArchiveName(jarArchiveName);
+                }
+            });
 
-                    jar.setDestinationDir(binary.getJarFile().getParentFile());
-                    jar.setArchiveName(binary.getJarFile().getName());
+            String libName = runtimeJarName.replace("Jar", "");
+            String apiJarName = runtimeJarName.replace("Jar", "ApiJar");
+            final File apiClassesDir = new File(runtimeClassesDir.getParent(), "apiClasses");
+            final String extractApiClasses = "extract" + capitalize(libName + "ApiClasses");
+            tasks.create(extractApiClasses, Copy.class, new Action<Copy>() {
+                @Override
+                public void execute(Copy copy) {
+                    copy.from(runtimeClassesDir);
+                    copy.into(apiClassesDir);
+                    for (String packageName : binary.getExportedPackages()) {
+                        copy.include(packageName.replace('.', '/') + "/**/*");
+                    }
+                    copy.dependsOn(createRuntimeJar);
+                }
+            });
+
+            final File apiJarDestDir = new File(runtimeJarDestDir.getParentFile(), apiJarName);
+            String createApiJar = "create" + capitalize(apiJarName);
+            tasks.create(createApiJar, Jar.class, new Action<Jar>() {
+                @Override
+                public void execute(Jar jar) {
+                    jar.setDescription(String.format("Creates the API binary file for %s.", binary));
+                    jar.from(apiClassesDir);
+                    jar.setDestinationDir(apiJarDestDir);
+                    jar.setArchiveName(jarArchiveName);
+                    jar.dependsOn(extractApiClasses);
                 }
             });
         }

subprojects/language-java/src/integTest/groovy/org/gradle/language/java/ApiSpecIntegrationTest.groovy
@@ -16,6 +16,7 @@
 
 package org.gradle.language.java
 
+import org.gradle.integtests.fixtures.jvm.JvmSourceFile
 import org.gradle.integtests.fixtures.jvm.TestJvmComponent
 import org.gradle.integtests.language.AbstractJvmLanguageIntegrationTest
 import org.gradle.language.fixtures.TestJavaComponent
@@ -125,4 +126,144 @@ class ApiSpecIntegrationTest extends AbstractJvmLanguageIntegrationTest {
         fails "assemble"
         failure.assertHasCause("Invalid public API specification: package 'com.example.p1' has already been exported")
     }
+
+    def "api jar should contain only classes declared in packages exported in api spec"() {
+        when:
+        addNonApiClass()
+
+        and:
+        buildFile << """
+            model {
+                components {
+                    myLib(JvmLibrarySpec) {
+                        api {
+                            exports 'compile.test'
+                        }
+                    }
+                }
+            }
+        """
+        then:
+        succeeds "assemble"
+
+        and:
+        def allClasses = app.sources*.classFile.fullPath as String[]
+        def apiClassesOnly = (allClasses - ["non_api/pkg/InternalPerson.class"]) as String[];
+        jarFile("build/jars/myLibJar/myLib.jar").hasDescendants(allClasses)
+        jarFile("build/jars/myLibApiJar/myLib.jar").hasDescendants(apiClassesOnly)
+    }
+
+    def "api jar should include all library packages when no api specification is declared"() {
+        when:
+        addNonApiClass()
+
+        and:
+        buildFile << """
+            model {
+                components {
+                    myLib(JvmLibrarySpec) {
+                    }
+                }
+            }
+        """
+        then:
+        succeeds "assemble"
+
+        and:
+        def allClasses = app.sources*.classFile.fullPath as String[]
+        jarFile("build/jars/myLibJar/myLib.jar").hasDescendants(allClasses)
+        jarFile("build/jars/myLibApiJar/myLib.jar").hasDescendants(allClasses)
+    }
+
+    def "api jar should include all library packages api specification is declared empty"() {
+        when:
+        addNonApiClass()
+
+        and:
+        buildFile << """
+            model {
+                components {
+                    myLib(JvmLibrarySpec) {
+                        api {
+                        }
+                    }
+                }
+            }
+        """
+        then:
+        succeeds "assemble"
+
+        and:
+        def allClasses = app.sources*.classFile.fullPath as String[]
+        jarFile("build/jars/myLibJar/myLib.jar").hasDescendants(allClasses)
+        jarFile("build/jars/myLibApiJar/myLib.jar").hasDescendants(allClasses)
+    }
+
+    def "api jar should be built for each variant and contain only api classes"() {
+        when:
+        addNonApiClass()
+
+        and:
+        buildFile << """
+            model {
+                components {
+                    myLib(JvmLibrarySpec) {
+                        api {
+                            exports 'compile.test'
+                        }
+                        targetPlatform 'java6'
+                        targetPlatform 'java7'
+                    }
+                }
+            }
+        """
+        then:
+        succeeds "assemble"
+
+        and:
+        def allClasses = app.sources*.classFile.fullPath as String[]
+        def apiClassesOnly = (allClasses - ["non_api/pkg/InternalPerson.class"]) as String[];
+        jarFile("build/jars/java6MyLibJar/myLib.jar").hasDescendants(allClasses)
+        jarFile("build/jars/java6MyLibApiJar/myLib.jar").hasDescendants(apiClassesOnly)
+        jarFile("build/jars/java7MyLibJar/myLib.jar").hasDescendants(allClasses)
+        jarFile("build/jars/java7MyLibApiJar/myLib.jar").hasDescendants(apiClassesOnly)
+    }
+
+    def "building api jar should trigger compilation of classes"() {
+        when:
+        addNonApiClass()
+
+        and:
+        buildFile << """
+            model {
+                components {
+                    myLib(JvmLibrarySpec) {
+                        api {
+                            exports 'compile.test'
+                        }
+                    }
+                }
+            }
+        """
+        then:
+        succeeds "createMyLibApiJar"
+
+        and:
+        def allClasses = app.sources*.classFile.fullPath as String[]
+        def apiClassesOnly = (allClasses - ["non_api/pkg/InternalPerson.class"]) as String[];
+        jarFile("build/jars/myLibApiJar/myLib.jar").hasDescendants(apiClassesOnly)
+        jarFile("build/jars/myLibJar/myLib.jar").hasDescendants(allClasses)
+    }
+
+    def addNonApiClass() {
+        app.sources.add(new JvmSourceFile("non_api/pkg", "InternalPerson.java", '''
+            package non_api.pkg;
+
+            public class InternalPerson {
+            }
+            '''
+        ))
+        app.sources*.writeToDir(file("src/myLib/java"))
+    }
+
 }

subprojects/language-java/src/main/java/org/gradle/language/java/plugins/JavaLanguagePlugin.java
@@ -29,6 +29,7 @@ import org.gradle.jvm.JarBinarySpec;
 import org.gradle.jvm.JvmBinarySpec;
 import org.gradle.jvm.JvmByteCode;
 import org.gradle.jvm.internal.DependencyResolvingClasspath;
+import org.gradle.jvm.tasks.Jar;
 import org.gradle.language.base.LanguageSourceSet;
 import org.gradle.language.base.internal.DependentSourceSetInternal;
 import org.gradle.language.base.internal.SourceTransformTaskConfig;
@@ -129,7 +130,9 @@ public class JavaLanguagePlugin implements Plugin<Project> {
 
                     compile.setDependencyCacheDir(new File(compile.getProject().getBuildDir(), "jvm-dep-cache"));
                     compile.dependsOn(javaSourceSet);
-                    binary.getTasks().getJar().dependsOn(compile);
+                    for (Task jarTask : binary.getTasks().withType(Jar.class)) {
+                        jarTask.dependsOn(compile);
+                    }
                 }
             };
         }

subprojects/language-jvm/src/main/java/org/gradle/language/jvm/plugins/JvmResourcesPlugin.java
@@ -19,6 +19,7 @@ package org.gradle.language.jvm.plugins;
 import org.gradle.api.*;
 import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.jvm.JvmBinarySpec;
+import org.gradle.jvm.tasks.Jar;
 import org.gradle.language.base.LanguageSourceSet;
 import org.gradle.language.base.internal.SourceTransformTaskConfig;
 import org.gradle.language.base.internal.registry.LanguageTransform;
@@ -90,7 +91,9 @@ public class JvmResourcesPlugin implements Plugin<Project> {
                     JvmBinarySpec jvmBinary = (JvmBinarySpec) binary;
                     resourcesTask.from(resourceSet.getSource());
                     resourcesTask.setDestinationDir(jvmBinary.getResourcesDir());
-                    jvmBinary.getTasks().getJar().dependsOn(resourcesTask);
+                    for (Task jarTask : binary.getTasks().withType(Jar.class)) {
+                        jarTask.dependsOn(resourcesTask);
+                    }
                 }
             };
         }

subprojects/platform-jvm/src/main/java/org/gradle/jvm/JarBinarySpec.java
@@ -21,6 +21,7 @@ import org.gradle.api.artifacts.component.LibraryBinaryIdentifier;
 import org.gradle.internal.HasInternalProtocol;
 
 import java.io.File;
+import java.util.Set;
 
 /**
  * Definition of a Jar file binary that is to be built by Gradle.
@@ -41,4 +42,8 @@ public interface JarBinarySpec extends JvmBinarySpec {
      * Sets the jar file output for this binary.
      */
     void setJarFile(File jarFile);
+
+    void setExportedPackages(Set<String> exportedPackages);
+
+    Set<String> getExportedPackages();
 }

subprojects/platform-jvm/src/main/java/org/gradle/jvm/JvmLibrarySpec.java
@@ -21,9 +21,13 @@ import org.gradle.internal.HasInternalProtocol;
 import org.gradle.platform.base.LibrarySpec;
 import org.gradle.platform.base.PlatformAwareComponentSpec;
 
+import java.util.Set;
+
 /**
  * Definition of a JVM library component that is to be built by Gradle.
  */
 @Incubating @HasInternalProtocol
 public interface JvmLibrarySpec extends LibrarySpec, JvmComponentSpec, PlatformAwareComponentSpec {
+
+    Set<String> getExportedPackages();
 }

subprojects/platform-jvm/src/main/java/org/gradle/jvm/internal/ApiSpec.java
@@ -57,4 +57,8 @@ class ApiSpec {
                 format("Invalid public API specification: package '%s' has already been exported", packageName));
         }
     }
+
+    public Set<PackageName> getExports() {
+        return exports;
+    }
 }

subprojects/platform-jvm/src/main/java/org/gradle/jvm/internal/DefaultJarBinarySpec.java
@@ -28,6 +28,8 @@ import org.gradle.platform.base.internal.BinaryBuildAbility;
 import org.gradle.platform.base.internal.ToolSearchBuildAbility;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Set;
 
 public class DefaultJarBinarySpec extends BaseBinarySpec implements JarBinarySpecInternal {
     private final JvmBinaryTasks tasks = new DefaultJvmBinaryTasks(super.getTasks());
@@ -37,6 +39,7 @@ public class DefaultJarBinarySpec extends BaseBinarySpec implements JarBinarySpe
     private File resourcesDir;
     private File jarFile;
     private ComponentSpec component;
+    private Set<String> exportedPackages = Collections.emptySet();
 
     @Override
     protected String getTypeName() {
@@ -106,6 +109,16 @@ public class DefaultJarBinarySpec extends BaseBinarySpec implements JarBinarySpe
     @Override
     public void setResourcesDir(File resourcesDir) {
         this.resourcesDir = resourcesDir;
+    }
+
+    @Override
+    public void setExportedPackages(Set<String> exportedPackages) {
+        this.exportedPackages = exportedPackages;
+    }
+
+    @Override
+    public Set<String> getExportedPackages() {
+        return exportedPackages;
     }
 
     @Override

commit 2f293ec796f9c65b8f5841d9b85b1f1bb8a1121c

Fix API jar creation-related integ test breakages
This change fixes integration test breakages introduced by commit
d9d68d916fbc507db0ab507919b1ca802924f73a. These breakages were mainly
related to violated expectations around console output when building
user guide snippets. Because API jar creation introduces new tasks into
the mix for all jars, this broke output assertions for all software
model user guide snippets that deal with jars.

The second type of breakage was in the `ScalaLanguagePlugin`,
specifically in its use of `JvmBinaryTasks#getJar`. As mentioned in
d9d68d916fbc507db0ab507919b1ca802924f73a, this convenience method is now
essentially obsolete and `DomainObjectSet#withType` should be favored
instead. The #getJar method is now unused throughout the codebase and
should be considered for removal.

The third breakage was due to violating an assumption made in
`JvmComponentPlugin#createTasks` that all `JarBinarySpec` names end in
the string "Jar". This is almost universally true, but not quite in the
edge case presented by `CustomComponentInternalViewsIntegrationTest`.
This change simply short-circuits #createTest, returning early if an
oddly-named `JarBinarySpec` is encountered. This kind of one-off
handling is far from elegant, but again, as mentioned in the
original commit, this implementation is a stopgap on the way to the
proper remodeling of these types that is soon to come.

subprojects/docs/src/samples/userguideOutput/newJavaMultiComponents-clientJar.out
@@ -1,9 +1,12 @@
 :util:compileMainJarMainJava
 :util:createMainJar
+:util:extractMainApiClasses
+:util:createMainApiJar
 :util:mainJar
 :compileClientJarClientJava
 :createClientJar
+:extractClientApiClasses
+:createClientApiJar
 :clientJar
 
 BUILD SUCCESSFUL
-

subprojects/docs/src/samples/userguideOutput/newJavaMultiComponents-serverJar.out
@@ -1,9 +1,13 @@
 :compileCoreJarCoreJava
 :processCoreJarCoreResources
 :createCoreJar
+:extractCoreApiClasses
+:createCoreApiJar
 :coreJar
 :compileServerJarServerJava
 :createServerJar
+:extractServerApiClasses
+:createServerApiJar
 :serverJar
 
 BUILD SUCCESSFUL

subprojects/docs/src/samples/userguideOutput/newJavaQuickstart.out
@@ -1,6 +1,8 @@
 :compileMainJarMainJava
 :processMainJarMainResources
 :createMainJar
+:extractMainApiClasses
+:createMainApiJar
 :mainJar
 :assemble
 :check UP-TO-DATE

subprojects/docs/src/samples/userguideOutput/newJavaTargetPlatforms-java6MainJar.out
@@ -4,7 +4,8 @@
 :core:compileJava6MainJarMainJava6
 :core:processJava6MainJarMainResources
 :core:createJava6MainJar
+:core:extractJava6MainApiClasses
+:core:createJava6MainApiJar
 :core:java6MainJar
 
 BUILD SUCCESSFUL
-

subprojects/docs/src/samples/userguideOutput/newJavaTargetPlatforms-server.out
@@ -3,17 +3,25 @@
 :core:compileJava5MainJarMainJava
 :core:processJava5MainJarMainResources
 :core:createJava5MainJar
+:core:extractJava5MainApiClasses
+:core:createJava5MainApiJar
 :core:java5MainJar
 :server:compileJava5MainJarMainJava
 :server:createJava5MainJar
+:server:extractJava5MainApiClasses
+:server:createJava5MainApiJar
 :server:java5MainJar
 :core:compileJava6MainJarMainJava
 :core:compileJava6MainJarMainJava6
 :core:processJava6MainJarMainResources
 :core:createJava6MainJar
+:core:extractJava6MainApiClasses
+:core:createJava6MainApiJar
 :core:java6MainJar
 :server:compileJava6MainJarMainJava
 :server:createJava6MainJar
+:server:extractJava6MainApiClasses
+:server:createJava6MainApiJar
 :server:java6MainJar
 :server:assemble
 :server:check UP-TO-DATE

subprojects/docs/src/samples/userguideOutput/newJavaTargetPlatforms.out
@@ -1,11 +1,15 @@
 :core:compileJava5MainJarMainJava
 :core:processJava5MainJarMainResources
 :core:createJava5MainJar
+:core:extractJava5MainApiClasses
+:core:createJava5MainApiJar
 :core:java5MainJar
 :core:compileJava6MainJarMainJava
 :core:compileJava6MainJarMainJava6
 :core:processJava6MainJarMainResources
 :core:createJava6MainJar
+:core:extractJava6MainApiClasses
+:core:createJava6MainApiJar
 :core:java6MainJar
 :core:assemble
 :core:check UP-TO-DATE

subprojects/language-scala/src/main/java/org/gradle/language/scala/plugins/ScalaLanguagePlugin.java
@@ -21,6 +21,7 @@ import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.jvm.JvmBinarySpec;
 import org.gradle.jvm.JvmByteCode;
 import org.gradle.jvm.platform.JavaPlatform;
+import org.gradle.jvm.tasks.Jar;
 import org.gradle.language.base.LanguageSourceSet;
 import org.gradle.language.base.internal.SourceTransformTaskConfig;
 import org.gradle.language.base.internal.registry.LanguageTransform;
@@ -119,7 +120,9 @@ public class ScalaLanguagePlugin implements Plugin<Project> {
                     compile.setSourceCompatibility(javaPlatform.getTargetCompatibility().toString());
 
                     compile.dependsOn(scalaSourceSet);
-                    binary.getTasks().getJar().dependsOn(compile);
+                    for (Task jarTask : binary.getTasks().withType(Jar.class)) {
+                        jarTask.dependsOn(compile);
+                    }
                 }
             };
         }

subprojects/platform-jvm/src/main/java/org/gradle/jvm/plugins/JvmComponentPlugin.java
@@ -146,7 +146,6 @@ public class JvmComponentPlugin implements Plugin<Project> {
         public void createTasks(ModelMap<Task> tasks, final JarBinarySpec binary) {
             final String jarArchiveName = binary.getJarFile().getName();
 
-            String runtimeJarName = binary.getName();
             final File runtimeClassesDir = binary.getClassesDir();
             final File runtimeJarDestDir = binary.getJarFile().getParentFile();
             final String createRuntimeJar = "create" + capitalize(binary.getName());
@@ -161,7 +160,12 @@ public class JvmComponentPlugin implements Plugin<Project> {
                 }
             });
 
-            String libName = runtimeJarName.replace("Jar", "");
+            String binaryName = binary.getName();
+            if (!binaryName.endsWith("Jar")) {
+                return;
+            }
+
+            String libName = binaryName.replace("Jar", "");
             final File apiClassesDir = new File(new File(runtimeClassesDir.getParentFile().getParentFile(), "apiClasses"), runtimeClassesDir.getName());
             final String extractApiClasses = "extract" + capitalize(libName + "ApiClasses");
             tasks.create(extractApiClasses, Copy.class, new Action<Copy>() {