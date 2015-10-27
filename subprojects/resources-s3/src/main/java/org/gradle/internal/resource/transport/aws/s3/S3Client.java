a3375df6ee2c28bfaa34eaaeacca891b66ce525e
Brian O'Neill <boneill@monetate.com>
2015-10-27 16:48:15 -0400

    Revert "Patch that allows the S3 client to use DefaultCredentials with AWS."

--- subprojects/resources-s3/src/main/java/org/gradle/internal/resource/transport/aws/s3/S3Client.java
+++ subprojects/resources-s3/src/main/java/org/gradle/internal/resource/transport/aws/s3/S3Client.java
@@ -58,12 +58,7 @@ public class S3Client {
     }
 
     private AmazonS3Client createAmazonS3Client(AWSCredentials credentials) {
-        AmazonS3Client amazonS3Client;
-        if(credentials != null) {
-            amazonS3Client = new AmazonS3Client(credentials, createConnectionProperties());
-        } else {
-            amazonS3Client = new AmazonS3Client(createConnectionProperties());
-        }
+        AmazonS3Client amazonS3Client = new AmazonS3Client(credentials, createConnectionProperties());
         S3ClientOptions clientOptions = new S3ClientOptions();
         Optional<URI> endpoint = s3ConnectionProperties.getEndpoint();
         if (endpoint.isPresent()) {
