40a2ddbe366023e8aecff5d0e653b3385eaa2f69
Cedric Champeau <cedric@gradle.com>
2015-11-10 14:43:40 +0100

    Add comparison between API declared and no-API declared
      +review REVIEW-5688

--- subprojects/performance/src/integTest/groovy/org/gradle/performance/JavaSoftwareModelCompileAvoidancePerformanceTest.groovy
+++ subprojects/performance/src/integTest/groovy/org/gradle/performance/JavaSoftwareModelCompileAvoidancePerformanceTest.groovy
@@ -23,112 +23,21 @@ import org.gradle.performance.fixture.BuildExperimentRunner
 import spock.lang.Unroll
 
 class JavaSoftwareModelCompileAvoidancePerformanceTest extends AbstractCrossBuildPerformanceTest {
-    static int perc(int perc, int total) {
+    private static int perc(int perc, int total) {
         (int) Math.ceil(total * (double) perc / 100d)
     }
 
-    @Unroll("Project '#testProject' measuring compile avoidance speed when #cardinalityDesc #scenario")
+    @Unroll("CompileAvoidance '#testCompileAvoidance' measuring compile avoidance speed when #cardinalityDesc #scenario #apiDesc")
     def "build java software model project"() {
         given:
-        runner.testGroup = "compile avoidance using API declaration"
-        runner.testId = "$size project compile avoidance with API $cardinalityDesc $scenario build"
-        def nonApiChangesCount = { projectCount ->
-            perc(nonApiChanges, projectCount)
-        }
-        def compatibleApiChangesCount = { projectCount ->
-            perc(abiCompatibleChanges, projectCount)
-        }
-        def breakingApiChangesCount = { projectCount ->
-            perc(abiBreakingChanges, projectCount)
-        }
-        runner.buildExperimentListener = new BuildExperimentListenerAdapter() {
-            private List<File> projects
-            private int projectCount
-
-            private final Map<File, byte[]> backup = [:]
-
-            @Override
-            void beforeInvocation(BuildExperimentInvocationInfo invocationInfo) {
-                def projectDir = invocationInfo.projectDir
-                if (projects == null) {
-                    projects = projectDir.listFiles().findAll { it.directory && it.name.startsWith('project') }.sort { it.name }
-                    projectCount = projects.size()
-
-                    // forcefully delete build directories (so that dirty local runs do not interfere with results)
-                    projects.each { pDir ->
-                        FileUtils.deleteDirectory(new File(pDir, 'build'))
-                    }
-
-                    // make sure execution is consistent independently of time
-                    Collections.shuffle(projects, new Random(31 * projectCount))
-                    // we create an initial copy of all source files so that we are consistent accross builds even if interrupted
-                    def backups = []
-                    def sources = []
-                    projectDir.eachFileRecurse { file ->
-                        if (file.name.endsWith('~')) {
-                            backups << file
-                        } else if (file.name.endsWith('.java')) {
-                            sources << file
-                        }
-                    }
-                    // restore files first
-                    backups.each { file ->
-                        FileUtils.copyFile(file, new File(file.parentFile, file.name - '~'), true)
-                    }
-                    // then create a copy of each source file
-                    sources.each { file ->
-                        FileUtils.copyFile(file, new File(file.parentFile, file.name + '~'), true)
-                    }
-                }
-                if (!backup.isEmpty()) {
-                    backup.each { File path, byte[] origContents ->
-                        println "Reverting changes to $path"
-                        path.withOutputStream { out -> out.write(origContents) }
-                    }
-                    backup.clear()
-                } else if (invocationInfo.phase != BuildExperimentRunner.Phase.WARMUP) {
-                    projects.take(nonApiChangesCount(projectCount)).each { subproject ->
-                        def internalDir = new File(subproject, 'src/main/java/org/gradle/test/performance/internal'.replace((char) '/', File.separatorChar))
-                        def updatedFile = pickFirstJavaSource(internalDir)
-                        println "Updating non-API source file $updatedFile"
-                        backup[updatedFile] = updatedFile.bytes
-                        updatedFile.text = updatedFile.text.replace('private final String property;', '''
-private final String property;
-public String addedProperty;
-''')
-                    }
-
-                    projects.take(compatibleApiChangesCount(projectCount)).each { subproject ->
-                        def internalDir = new File(subproject, 'src/main/java/org/gradle/test/performance/'.replace((char) '/', File.separatorChar))
-                        def updatedFile = pickFirstJavaSource(internalDir)
-                        println "Updating API source file $updatedFile in ABI compatible way"
-                        backup[updatedFile] = updatedFile.bytes
-                        updatedFile.text = updatedFile.text.replace('return property;', 'return property.toUpperCase();')
-                    }
-
-                    projects.take(breakingApiChangesCount(projectCount)).each { subproject ->
-                        def internalDir = new File(subproject, 'src/main/java/org/gradle/test/performance/'.replace((char) '/', File.separatorChar))
-                        def updatedFile = pickFirstJavaSource(internalDir)
-                        println "Updating API source file $updatedFile in ABI breaking way"
-                        backup[updatedFile] = updatedFile.bytes
-                        updatedFile.text = updatedFile.text.replace('public String getProperty() {', '''public String getPropertyToUpper() {
-   return property.toUpperCase();
-}
-
-public String getProperty() {''')
-                    }
-                }
-            }
-
-            private File pickFirstJavaSource(File internalDir) {
-                internalDir.listFiles().find { it.name.endsWith('.java') }
-            }
-        }
+        runner.testGroup = "compile avoidance using Java software model"
+        runner.testId = "$size project compile avoidance $apiDesc and $cardinalityDesc $scenario"
+        runner.buildExperimentListener = new SourceFileUpdater(nonApiChanges, abiCompatibleChanges, abiBreakingChanges)
 
         runner.buildSpec {
-            projectName("${size}JavaSwModelProjectWithApi").displayName("$size project compile avoidance with API declaration").invocation {
+            projectName(testCompileAvoidance).displayName(runner.testId).invocation {
                 tasksToRun('assemble').useDaemon().gradleOpts('-Xmx2G')
-            }.invocationCount(10)
+            }
         }
 
         when:
@@ -141,23 +50,142 @@ public String getProperty() {''')
 
         // nonApiChanges, abiCompatibleChanges and abiBreakingChanges are expressed in percentage of projects that are going to be
         // updated in the test
-        scenario                     | testProject                      | nonApiChanges | abiCompatibleChanges | abiBreakingChanges
-        'internal API change made'   | "smallJavaSwModelProjectWithApi" | 10            | 0                    | 0
-        'internal API change made'   | "smallJavaSwModelProjectWithApi" | 50            | 0                    | 0
-        'internal API change made'   | "largeJavaSwModelProjectWithApi" | 10            | 0                    | 0
-        'internal API change made'   | "largeJavaSwModelProjectWithApi" | 50            | 0                    | 0
+        scenario                 | testCompileAvoidance                         | nonApiChanges | abiCompatibleChanges | abiBreakingChanges
+        'internal API changes'   | "smallJavaSwModelCompileAvoidanceWithApi"    | 10            | 0                    | 0
+        'internal API changes'   | "smallJavaSwModelCompileAvoidanceWithoutApi" | 10            | 0                    | 0
+        'internal API changes'   | "smallJavaSwModelCompileAvoidanceWithApi"    | 50            | 0                    | 0
+        'internal API changes'   | "smallJavaSwModelCompileAvoidanceWithoutApi" | 50            | 0                    | 0
+        'internal API changes'   | "largeJavaSwModelCompileAvoidanceWithApi"    | 10            | 0                    | 0
+        'internal API changes'   | "largeJavaSwModelCompileAvoidanceWithoutApi" | 10            | 0                    | 0
+        'internal API changes'   | "largeJavaSwModelCompileAvoidanceWithApi"    | 50            | 0                    | 0
+        'internal API changes'   | "largeJavaSwModelCompileAvoidanceWithoutApi" | 50            | 0                    | 0
 
-        'ABI compatible change made' | "smallJavaSwModelProjectWithApi" | 0             | 10                   | 0
-        'ABI compatible change made' | "smallJavaSwModelProjectWithApi" | 0             | 50                   | 0
-        'ABI compatible change made' | "largeJavaSwModelProjectWithApi" | 0             | 10                   | 0
-        'ABI compatible change made' | "largeJavaSwModelProjectWithApi" | 0             | 50                   | 0
+        'ABI compatible changes' | "smallJavaSwModelCompileAvoidanceWithApi"    | 0             | 10                   | 0
+        'ABI compatible changes' | "smallJavaSwModelCompileAvoidanceWithoutApi" | 0             | 10                   | 0
+        'ABI compatible changes' | "smallJavaSwModelCompileAvoidanceWithApi"    | 0             | 50                   | 0
+        'ABI compatible changes' | "smallJavaSwModelCompileAvoidanceWithoutApi" | 0             | 50                   | 0
+        'ABI compatible changes' | "largeJavaSwModelCompileAvoidanceWithApi"    | 0             | 10                   | 0
+        'ABI compatible changes' | "largeJavaSwModelCompileAvoidanceWithoutApi" | 0             | 10                   | 0
+        'ABI compatible changes' | "largeJavaSwModelCompileAvoidanceWithApi"    | 0             | 50                   | 0
+        'ABI compatible changes' | "largeJavaSwModelCompileAvoidanceWithoutApi" | 0             | 50                   | 0
 
-        'ABI breaking change made'   | "smallJavaSwModelProjectWithApi" | 0             | 0                    | 10
-        'ABI breaking change made'   | "smallJavaSwModelProjectWithApi" | 0             | 0                    | 50
-        'ABI breaking change made'   | "largeJavaSwModelProjectWithApi" | 0             | 0                    | 10
-        'ABI breaking change made'   | "largeJavaSwModelProjectWithApi" | 0             | 0                    | 50
+        'ABI breaking changes'   | "smallJavaSwModelCompileAvoidanceWithApi"    | 0             | 0                    | 10
+        'ABI breaking changes'   | "smallJavaSwModelCompileAvoidanceWithoutApi" | 0             | 0                    | 10
+        'ABI breaking changes'   | "smallJavaSwModelCompileAvoidanceWithApi"    | 0             | 0                    | 50
+        'ABI breaking changes'   | "smallJavaSwModelCompileAvoidanceWithoutApi" | 0             | 0                    | 50
+        'ABI breaking changes'   | "largeJavaSwModelCompileAvoidanceWithApi"    | 0             | 0                    | 10
+        'ABI breaking changes'   | "largeJavaSwModelCompileAvoidanceWithoutApi" | 0             | 0                    | 10
+        'ABI breaking changes'   | "largeJavaSwModelCompileAvoidanceWithApi"    | 0             | 0                    | 50
+        'ABI breaking changes'   | "largeJavaSwModelCompileAvoidanceWithoutApi" | 0             | 0                    | 50
 
-        size = testProject[0..4]
+        size = testCompileAvoidance[0..4]
         cardinalityDesc = (nonApiChanges + abiCompatibleChanges + abiBreakingChanges) < 50 ? 'some' : 'many'
+        apiDesc = testCompileAvoidance.contains('Without') ? 'without declared API' : 'with declared API'
+    }
+
+    private static class SourceFileUpdater extends BuildExperimentListenerAdapter {
+        private List<File> projects
+        private int projectCount
+
+        private final Map<File, byte[]> backup = [:]
+        private final int nonApiChanges
+        private final int abiCompatibleChanges
+        private final int abiBreakingChanges
+
+        SourceFileUpdater(int nonApiChanges, int abiCompatibleChanges, int abiBreakingChanges) {
+            this.abiBreakingChanges = abiBreakingChanges
+            this.nonApiChanges = nonApiChanges
+            this.abiCompatibleChanges = abiCompatibleChanges
+        }
+
+        int nonApiChangesCount() {
+            perc(nonApiChanges, projectCount)
+        }
+
+        int compatibleApiChangesCount() {
+            perc(abiCompatibleChanges, projectCount)
+        }
+
+        int breakingApiChangesCount() {
+            perc(abiBreakingChanges, projectCount)
+        }
+
+        @Override
+        void beforeInvocation(BuildExperimentInvocationInfo invocationInfo) {
+            def projectDir = invocationInfo.projectDir
+            if (projects == null) {
+
+                projects = projectDir.listFiles().findAll { it.directory && it.name.startsWith('project') }.sort { it.name }
+                projectCount = projects.size()
+
+                // forcefully delete build directories (so that dirty local runs do not interfere with results)
+                projects.each { pDir ->
+                    FileUtils.deleteDirectory(new File(pDir, 'build'))
+                }
+
+                // make sure execution is consistent independently of time
+                Collections.shuffle(projects, new Random(31 * projectCount))
+                // we create an initial copy of all source files so that we are consistent accross builds even if interrupted
+                def backups = []
+                def sources = []
+                projectDir.eachFileRecurse { file ->
+                    if (file.name.endsWith('~')) {
+                        backups << file
+                    } else if (file.name.endsWith('.java')) {
+                        sources << file
+                    }
+                }
+                // restore files first
+                backups.each { file ->
+                    FileUtils.copyFile(file, new File(file.parentFile, file.name - '~'), true)
+                }
+                // then create a copy of each source file
+                sources.each { file ->
+                    FileUtils.copyFile(file, new File(file.parentFile, file.name + '~'), true)
+                }
+            }
+            if (!backup.isEmpty()) {
+                backup.each { File path, byte[] origContents ->
+                    println "Reverting changes to $path"
+                    path.withOutputStream { out -> out.write(origContents) }
+                }
+                backup.clear()
+            } else if (invocationInfo.phase != BuildExperimentRunner.Phase.WARMUP) {
+                projects.take(nonApiChangesCount()).each { subproject ->
+                    def internalDir = new File(subproject, 'src/main/java/org/gradle/test/performance/internal'.replace((char) '/', File.separatorChar))
+                    def updatedFile = pickFirstJavaSource(internalDir)
+                    println "Updating non-API source file $updatedFile"
+                    backup[updatedFile] = updatedFile.bytes
+                    updatedFile.text = updatedFile.text.replace('private final String property;', '''
+private final String property;
+public String addedProperty;
+''')
+                }
+
+                projects.take(compatibleApiChangesCount()).each { subproject ->
+                    def internalDir = new File(subproject, 'src/main/java/org/gradle/test/performance/'.replace((char) '/', File.separatorChar))
+                    def updatedFile = pickFirstJavaSource(internalDir)
+                    println "Updating API source file $updatedFile in ABI compatible way"
+                    backup[updatedFile] = updatedFile.bytes
+                    updatedFile.text = updatedFile.text.replace('return property;', 'return property.toUpperCase();')
+                }
+
+                projects.take(breakingApiChangesCount()).each { subproject ->
+                    def internalDir = new File(subproject, 'src/main/java/org/gradle/test/performance/'.replace((char) '/', File.separatorChar))
+                    def updatedFile = pickFirstJavaSource(internalDir)
+                    println "Updating API source file $updatedFile in ABI breaking way"
+                    backup[updatedFile] = updatedFile.bytes
+                    updatedFile.text = updatedFile.text.replace('public String getProperty() {', '''public String getPropertyToUpper() {
+   return property.toUpperCase();
+}
+
+public String getProperty() {''')
+                }
+            }
+        }
+
+        private static File pickFirstJavaSource(File internalDir) {
+            internalDir.listFiles().find { it.name.endsWith('.java') }
+        }
     }
 }
