b62321372eca1f3d4cea20e0c54457f846aceea1
Cedric Champeau <cedric@gradle.com>
2015-12-10 09:12:43 +0100

    Increment number of files in distribution by one, because of the new `testing-base` module
    Story: gradle/langos#103
    Item: refactor-plugins

--- subprojects/distributions/src/integTest/groovy/org/gradle/AllDistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/AllDistributionIntegrationSpec.groovy
@@ -30,7 +30,7 @@ class AllDistributionIntegrationSpec extends DistributionIntegrationSpec {
 
     @Override
     int getLibJarsCount() {
-        159
+        160
     }
 
     def allZipContents() {
--- subprojects/distributions/src/integTest/groovy/org/gradle/BinDistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/BinDistributionIntegrationSpec.groovy
@@ -27,7 +27,7 @@ class BinDistributionIntegrationSpec extends DistributionIntegrationSpec {
 
     @Override
     int getLibJarsCount() {
-        159
+        160
     }
 
     def binZipContents() {
