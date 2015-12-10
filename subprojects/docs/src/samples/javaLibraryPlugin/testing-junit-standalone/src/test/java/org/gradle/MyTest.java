5dd3546a571d7eb0fc31a8d45ba03842c31a11ef
Cedric Champeau <cedric@gradle.com>
2015-12-10 18:40:35 +0100

    Add first sample for JUnit test execution in JVM software model
    Story: gradle/langos#103

---
+++ subprojects/docs/src/samples/javaLibraryPlugin/testing-junit-standalone/src/test/java/org/gradle/MyTest.java
@@ -0,0 +1,12 @@
+package org.gradle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyTest {
+    @Test
+    public void myTestMethod() {
+        assertEquals(4, "test".length());
+    }
+}
