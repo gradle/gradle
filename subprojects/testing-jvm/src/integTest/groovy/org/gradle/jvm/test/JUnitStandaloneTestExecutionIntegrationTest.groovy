8ad0f1db277a61f3cf92021c57d3e2722014e011
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2015-12-11 16:30:24 -0200

    Remove unnecessary test case
    At this point it doesn't seem to be testing anything that's not already
    tested by `JUnitTestSuitePluginGoodBehaviourTest`

--- subprojects/testing-jvm/src/integTest/groovy/org/gradle/jvm/test/JUnitStandaloneTestExecutionIntegrationTest.groovy
+++ subprojects/testing-jvm/src/integTest/groovy/org/gradle/jvm/test/JUnitStandaloneTestExecutionIntegrationTest.groovy
@@ -23,17 +23,6 @@ import spock.lang.Unroll
 
 class JUnitStandaloneTestExecutionIntegrationTest extends AbstractIntegrationSpec {
 
-    def "should apply junit plugin using explicit class reference"() {
-        given:
-        applyJUnitPlugin()
-
-        when:
-        run 'tasks'
-
-        then:
-        noExceptionThrown()
-    }
-
     def "creates a JUnit test suite binary"() {
         given:
         applyJUnitPlugin()
