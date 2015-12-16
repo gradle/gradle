bd909f9ab8f91b65de5ee2ac870b7adb28ce0481
Daz DeBoer <daz@gradle.com>
2015-12-16 11:34:16 -0700

    Added a sample to the play-ide feature spec to make it easier to try out

---
+++ design-docs/features/play-framework/play-application-in-ide/play-app/app/controllers/Application.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+class Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+}
