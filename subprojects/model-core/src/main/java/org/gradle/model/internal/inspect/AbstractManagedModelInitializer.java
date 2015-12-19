4b3bc560442d03934ec53665f0e53bcabeffe11f
Lóránt Pintér <lorant@gradle.com>
2015-12-19 14:09:35 +0100

    Use different projection for empty reference nodes
    When a reference node points to a target, the projection of the target
    node is used. So we need a projection here only for the case when the
    reference node doesn’t point anywhere, i.e. the “value” of the node is
    null. For that case we don’t need a full-blown managed projection, but
    can use something much simpler.
    
    +review

--- subprojects/model-core/src/main/java/org/gradle/model/internal/inspect/AbstractManagedModelInitializer.java
+++ subprojects/model-core/src/main/java/org/gradle/model/internal/inspect/AbstractManagedModelInitializer.java
@@ -21,7 +21,6 @@ import org.gradle.internal.typeconversion.TypeConverter;
 import org.gradle.model.internal.core.*;
 import org.gradle.model.internal.core.rule.describe.ModelRuleDescriptor;
 import org.gradle.model.internal.manage.instance.ManagedProxyFactory;
-import org.gradle.model.internal.manage.projection.ManagedModelProjection;
 import org.gradle.model.internal.manage.schema.*;
 import org.gradle.model.internal.type.ModelType;
 
@@ -91,8 +90,7 @@ public abstract class AbstractManagedModelInitializer<T> implements NodeInitiali
                     // This shouldn't be needed, but if there's no actual value referenced, model report can only
                     // show the type of the node if we do this for now. It should use the schema instead to find
                     // the type of the property node instead.
-                    ManagedImplStructSchema<P> structSchema = (ManagedImplStructSchema<P>) propertySchema;
-                    ModelProjection projection = new ManagedModelProjection<P>(structSchema, null, proxyFactory, typeConverter);
+                    ModelProjection projection = new EmptyReferenceProjection<P>(propertySchema.getType());
                     ModelRegistration registration = ModelRegistrations.of(childPath)
                         .withProjection(projection)
                         .descriptor(descriptor).build();
