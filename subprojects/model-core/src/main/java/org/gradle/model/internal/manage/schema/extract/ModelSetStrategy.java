04b6de9c966de570a8e7d119de62b85023a1c230
Adam Murdoch <adam@gradle.com>
2015-12-21 06:59:07 +1100

    Inlined `CollectionStrategy` and `SetStrategy` into `ModelSetStrategy`.
    +review REVIEW-5756

--- subprojects/model-core/src/main/java/org/gradle/model/internal/manage/schema/extract/ModelSetStrategy.java
+++ subprojects/model-core/src/main/java/org/gradle/model/internal/manage/schema/extract/ModelSetStrategy.java
@@ -16,14 +16,57 @@
 
 package org.gradle.model.internal.manage.schema.extract;
 
+import org.gradle.api.Action;
 import org.gradle.model.ModelSet;
+import org.gradle.model.internal.manage.schema.CollectionSchema;
+import org.gradle.model.internal.manage.schema.ModelSchema;
+import org.gradle.model.internal.manage.schema.ModelSetSchema;
 import org.gradle.model.internal.type.ModelType;
 
-public class ModelSetStrategy extends SetStrategy {
+import java.util.List;
+
+public class ModelSetStrategy implements ModelSchemaExtractionStrategy {
+
+    private final ModelType<?> modelType;
 
     public ModelSetStrategy() {
-        super(new ModelType<ModelSet<?>>() {
-        });
+        modelType = new ModelType<ModelSet<?>>() {
+        };
     }
 
+    public <T> void extract(ModelSchemaExtractionContext<T> extractionContext) {
+        ModelType<T> type = extractionContext.getType();
+        if (modelType.isAssignableFrom(type)) {
+            validateType(modelType, extractionContext, type);
+
+            ModelType<?> elementType = type.getTypeVariables().get(0);
+
+            extractionContext.found(getModelSchema(extractionContext, elementType));
+        }
+    }
+
+    private <T> void validateType(ModelType<?> modelType, ModelSchemaExtractionContext<T> extractionContext, ModelType<T> type) {
+        if (!type.getRawClass().equals(modelType.getConcreteClass())) {
+            throw new InvalidManagedModelElementTypeException(extractionContext, String.format("subtyping %s is not supported", modelType.getConcreteClass().getName()));
+        }
+        if (type.isHasWildcardTypeVariables()) {
+            throw new InvalidManagedModelElementTypeException(extractionContext, String.format("type parameter of %s cannot be a wildcard", modelType.getConcreteClass().getName()));
+        }
+
+        List<ModelType<?>> typeVariables = type.getTypeVariables();
+        if (typeVariables.isEmpty()) {
+            throw new InvalidManagedModelElementTypeException(extractionContext, String.format("type parameter of %s has to be specified", modelType.getConcreteClass().getName()));
+        }
+    }
+
+    private <T, E> ModelSchema<T> getModelSchema(final ModelSchemaExtractionContext<T> extractionContext, final ModelType<E> elementType) {
+        final CollectionSchema<T, E> schema = new ModelSetSchema<T, E>(extractionContext.getType(), elementType);
+        extractionContext.child(elementType, "element type", new Action<ModelSchema<E>>() {
+            @Override
+            public void execute(ModelSchema<E> elementTypeSchema) {
+                schema.setElementTypeSchema(elementTypeSchema);
+            }
+        });
+        return schema;
+    }
 }
