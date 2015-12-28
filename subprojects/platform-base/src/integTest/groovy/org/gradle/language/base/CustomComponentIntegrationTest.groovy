a3ca2cabf9f13849c6498a0279e88c7edc1a1901
Adam Murdoch <adam@gradle.com>
2015-12-28 15:41:37 +1100

    Added some test coverage for `@ComponentType`.
    +review REVIEW-5756

--- subprojects/platform-base/src/integTest/groovy/org/gradle/language/base/CustomComponentIntegrationTest.groovy
+++ subprojects/platform-base/src/integTest/groovy/org/gradle/language/base/CustomComponentIntegrationTest.groovy
@@ -534,5 +534,64 @@ class CustomComponentIntegrationTest extends AbstractIntegrationSpec {
         """
     }
 
+    def "reports failure in @ComponentType rule"() {
+        buildFile << """
+            @Managed
+            interface BrokenComponentSpec extends ComponentSpec {
+            }
 
+            class Broken extends RuleSource {
+                @ComponentType
+                void broken(ComponentTypeBuilder<BrokenComponentSpec> builder) {
+                    throw new RuntimeException('broken')
+                }
+            }
+            apply plugin: Broken
+        """
+
+        expect:
+        fails "help"
+        failure.assertHasCause("Failed to apply plugin [class 'Broken']")
+        failure.assertHasCause("broken")
+    }
+
+    def "fails when @ComponentType registration is badly formed"() {
+        buildFile << """
+            @Managed
+            interface BrokenComponentSpec extends ComponentSpec {
+            }
+
+            class Broken extends RuleSource {
+                @ComponentType
+                void broken(ComponentTypeBuilder<BrokenComponentSpec> builder) {
+                    builder.internalView(String)
+                }
+            }
+            apply plugin: Broken
+        """
+
+        expect:
+        fails "help"
+        failure.assertHasCause("Failed to apply plugin [class 'Broken']")
+        failure.assertHasCause("Broken#broken is not a valid component model rule method.")
+        failure.assertHasCause("Internal view java.lang.String must be an interface.")
+    }
+
+    def "reports badly formed @ComponentType rule"() {
+        buildFile << """
+            class Broken extends RuleSource {
+                @ComponentType
+                private void broken(ComponentTypeBuilder<?> builder) {
+                }
+            }
+            apply plugin: Broken
+        """
+
+        expect:
+        fails "help"
+        failure.assertHasCause("Failed to apply plugin [class 'Broken']")
+        failure.assertHasCause("""Type Broken is not a valid rule source:
+- Method broken(org.gradle.platform.base.ComponentTypeBuilder<?>) is not a valid rule method: A rule method cannot be private
+- Method broken(org.gradle.platform.base.ComponentTypeBuilder<?>) is not a valid rule method: Component type '?' cannot be a wildcard type (i.e. cannot use ? super, ? extends etc.).""")
+    }
 }
