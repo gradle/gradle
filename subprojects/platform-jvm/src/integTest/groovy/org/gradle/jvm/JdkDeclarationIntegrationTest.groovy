bc44f0f6042cf39566e92410cfaf5424f4c4cd0e
Cedric Champeau <cedric@gradle.com>
2016-01-28 13:58:53 +0100

    Attempt to re-enable Windows test now that working directory is set
    Story: gradle/langos#153

--- subprojects/platform-jvm/src/integTest/groovy/org/gradle/jvm/JdkDeclarationIntegrationTest.groovy
+++ subprojects/platform-jvm/src/integTest/groovy/org/gradle/jvm/JdkDeclarationIntegrationTest.groovy
@@ -19,8 +19,6 @@ import groovy.json.StringEscapeUtils
 import org.gradle.api.reporting.model.ModelReportOutput
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.AvailableJavaHomes
-import org.gradle.util.Requires
-import org.gradle.util.TestPrecondition
 
 class JdkDeclarationIntegrationTest extends AbstractIntegrationSpec {
 
@@ -32,7 +30,6 @@ class JdkDeclarationIntegrationTest extends AbstractIntegrationSpec {
         """
     }
 
-    @Requires(TestPrecondition.NOT_WINDOWS)
     def "can declare an installed JDK and model report shows the resolved installed JDK"() {
         given:
         def jdks = AvailableJavaHomes.availableJdks.indexed().collect { i, jdk ->
