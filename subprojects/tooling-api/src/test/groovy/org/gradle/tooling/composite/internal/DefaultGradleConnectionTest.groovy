846456b166fc75a8ec53d37fd4b005bcb0350bc8
Sterling Greene <sterling@gradle.com>
2016-02-03 18:08:02 -0500

    Unit tests for DefaultGradleConnection
    - check that we can create a ModelBuilder of the correct type
    - check that the close/stop is propagated down
    - check that the project dir is honored in a participant
    
    +review REVIEW-5809

---
+++ subprojects/tooling-api/src/test/groovy/org/gradle/tooling/composite/internal/DefaultGradleConnectionTest.groovy
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.tooling.composite.internal
+
+import org.gradle.tooling.composite.GradleConnection
+import org.gradle.tooling.model.eclipse.EclipseProject
+import spock.lang.Specification
+
+class DefaultGradleConnectionTest extends Specification {
+
+    GradleParticipantBuild build = Mock()
+    GradleConnection connection = new DefaultGradleConnection(null, [ build ] as Set)
+
+    def "participant project uses specified project dir"() {
+        def builder = new DefaultGradleConnection.Builder()
+        File projectDir = Mock()
+        builder.addBuild(projectDir)
+        DefaultGradleConnection connection = builder.build()
+        expect:
+        connection.participants[0].projectDir == projectDir
+    }
+
+    def "can get model builder"() {
+        expect:
+        connection.models(EclipseProject) instanceof DefaultCompositeModelBuilder
+    }
+
+    def modelTypeMustBeAnInterface() {
+        when:
+        connection.models(String)
+
+        then:
+        IllegalArgumentException e = thrown()
+        e.message == "Cannot fetch a model of type 'java.lang.String' as this type is not an interface."
+    }
+
+    def "close stops underlying project connections"() {
+        when:
+        connection.close()
+        then:
+        1 * build.stop()
+    }
+}
