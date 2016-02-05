30efb2f285dfc698acb09236c6555f4c02f7df19
Lari Hotari <lari.hotari@gradle.com>
2016-02-05 16:32:15 -0500

    Pass composite parameters to daemon
    +review REVIEW-5817

--- subprojects/launcher/src/main/java/org/gradle/tooling/internal/provider/DaemonBuildActionExecuter.java
+++ subprojects/launcher/src/main/java/org/gradle/tooling/internal/provider/DaemonBuildActionExecuter.java
@@ -27,15 +27,21 @@ import org.gradle.launcher.daemon.configuration.DaemonParameters;
 import org.gradle.launcher.exec.BuildActionExecuter;
 import org.gradle.launcher.exec.BuildActionParameters;
 import org.gradle.launcher.exec.DefaultBuildActionParameters;
+import org.gradle.launcher.exec.DefaultCompositeBuildActionParameters;
 import org.gradle.tooling.UnsupportedVersionException;
 import org.gradle.tooling.internal.protocol.BuildExceptionVersion1;
 import org.gradle.tooling.internal.protocol.InternalBuildCancelledException;
 import org.gradle.tooling.internal.protocol.InternalCancellationToken;
 import org.gradle.tooling.internal.protocol.ModelIdentifier;
+import org.gradle.tooling.internal.provider.connection.CompositeParameters;
+import org.gradle.tooling.internal.provider.connection.DefaultGradleParticipantBuild;
+import org.gradle.tooling.internal.provider.connection.GradleParticipantBuild;
 import org.gradle.tooling.internal.provider.connection.ProviderOperationParameters;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class DaemonBuildActionExecuter implements BuildActionExecuter<ProviderOperationParameters> {
     private final BuildActionExecuter<BuildActionParameters> executer;
@@ -52,8 +58,21 @@ public class DaemonBuildActionExecuter implements BuildActionExecuter<ProviderOp
             throw new UnsupportedVersionException("Continuous build requires Tooling API client version 2.1 or later.");
         }
         ClassPath classPath = DefaultClassPath.of(parameters.getInjectedPluginClasspath(Collections.<File>emptyList()));
-        BuildActionParameters actionParameters = new DefaultBuildActionParameters(daemonParameters.getEffectiveSystemProperties(),
-            System.getenv(), SystemProperties.getInstance().getCurrentDir(), parameters.getBuildLogLevel(), daemonParameters.getDaemonUsage(), continuous, false, classPath);
+
+        BuildActionParameters actionParameters;
+        List<GradleParticipantBuild> compositeParticipants = parameters.getBuilds(null);
+        if (compositeParticipants != null) {
+            List<GradleParticipantBuild> clonedCompositeParticipants = new ArrayList<GradleParticipantBuild>();
+            for (GradleParticipantBuild build : compositeParticipants) {
+                clonedCompositeParticipants.add(new DefaultGradleParticipantBuild(build));
+            }
+            CompositeParameters compositeParameters = new CompositeParameters(clonedCompositeParticipants);
+            actionParameters = new DefaultCompositeBuildActionParameters(daemonParameters.getEffectiveSystemProperties(),
+                System.getenv(), SystemProperties.getInstance().getCurrentDir(), parameters.getBuildLogLevel(), daemonParameters.getDaemonUsage(), continuous, false, classPath, compositeParameters);
+        } else {
+            actionParameters = new DefaultBuildActionParameters(daemonParameters.getEffectiveSystemProperties(),
+                System.getenv(), SystemProperties.getInstance().getCurrentDir(), parameters.getBuildLogLevel(), daemonParameters.getDaemonUsage(), continuous, false, classPath);
+        }
         try {
             return executer.execute(action, buildRequestContext, actionParameters, contextServices);
         } catch (ReportedException e) {
