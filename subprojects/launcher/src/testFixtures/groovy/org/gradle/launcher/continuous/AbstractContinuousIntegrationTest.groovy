a752cfbe8bff2f22aaade5c6abc1c930cdfa8766
Daz DeBoer <daz@gradle.com>
2016-02-12 11:54:25 -0700

    Bump the shutdown timeout to 20s for continuous integration tests
    +review REVIEW-5827

--- subprojects/launcher/src/testFixtures/groovy/org/gradle/launcher/continuous/AbstractContinuousIntegrationTest.groovy
+++ subprojects/launcher/src/testFixtures/groovy/org/gradle/launcher/continuous/AbstractContinuousIntegrationTest.groovy
@@ -29,7 +29,7 @@ import static org.gradle.util.TestPrecondition.PULL_REQUEST_BUILD
 
 abstract class AbstractContinuousIntegrationTest extends AbstractIntegrationSpec {
     private static final int WAIT_FOR_WATCHING_TIMEOUT_SECONDS = PULL_REQUEST_BUILD.fulfilled ? 60 : 30
-    private static final int WAIT_FOR_SHUTDOWN_TIMEOUT_SECONDS = 10
+    private static final int WAIT_FOR_SHUTDOWN_TIMEOUT_SECONDS = 20
     private static final boolean OS_IS_WINDOWS = OperatingSystem.current().isWindows()
     private static final String CHANGE_DETECTED_OUTPUT = "Change detected, executing build..."
     private static final String WAITING_FOR_CHANGES_OUTPUT = "Waiting for changes to input files of tasks..."
