823047906bbe7b79bc57f648e21f465b8b964a1c
Evgeny Mandrikov <mandrikov@gmail.com>
2016-02-14 12:06:54 +0100

    Fix regular expression
    It seems clear that acceptance of "org.jacoco.agent-0.6...jar"
    as version <= 0.6.2 was not intentional.
    
    Signed-off-by: Evgeny Mandrikov <mandrikov@gmail.com>

--- subprojects/jacoco/src/main/groovy/org/gradle/internal/jacoco/JacocoAgentJar.groovy
+++ subprojects/jacoco/src/main/groovy/org/gradle/internal/jacoco/JacocoAgentJar.groovy
@@ -50,7 +50,7 @@ class JacocoAgentJar {
 
     boolean supportsJmx() {
         def pre062 = getAgentConf().any {
-            it.name ==~ /org.jacoco.agent-([0]\.[0-6]\.[0-1\.].*)\.jar/
+            it.name ==~ /org.jacoco.agent-([0]\.[0-6]\.[0-1]\..*)\.jar/
         }
         return !pre062
     }
