82e7de1a3751a8a2a995b10d48e5b75035c10b8b
Lóránt Pintér <lorant@gradle.com>
2016-02-16 12:46:41 +0100

    Added some Javadoc
    +review REVIEW-5808

--- subprojects/model-core/src/main/java/org/gradle/model/internal/registry/ModelRegistry.java
+++ subprojects/model-core/src/main/java/org/gradle/model/internal/registry/ModelRegistry.java
@@ -103,10 +103,28 @@ public interface ModelRegistry {
 
     ModelRegistry register(ModelRegistration registration);
 
+    /**
+     * Bind the given action directly to its subject node in the given role. Calling {@link #bindAllReferences()} fails
+     * if the subject of the action is not matched by any node.
+     */
     ModelRegistry configure(ModelActionRole role, ModelAction action);
 
+    /**
+     * Registers a listener and binds the given action in the given role whenever a node that matches the spec is discovered.
+     * Matching nodes that are already discovered when {@code configureMatching()} is called are bound directly.
+     * Unlike with {@link #configure(ModelActionRole, ModelAction)}, {@link #bindAllReferences()} will <em>not</em> fail
+     * if no nodes match the given spec.
+     *
+     * @throws IllegalArgumentException if the given action has a <code>path</code> set.
+     */
     ModelRegistry configureMatching(ModelSpec spec, ModelActionRole role, ModelAction action);
 
+    /**
+     * Registers a listener and applies the given {@link RuleSource} whenever a node that matches the spec is discovered.
+     * Matching nodes that are already discovered when {@code configureMatching()} is called are bound directly.
+     * Unlike with {@link #configure(ModelActionRole, ModelAction)}, {@link #bindAllReferences()} will <em>not</em> fail
+     * if no nodes match the given spec.
+     */
     ModelRegistry configureMatching(ModelSpec spec, Class<? extends RuleSource> rules);
 
     MutableModelNode getRoot();
