45254f12b43ad2a5378d30b2fee22ed120788e48
Sterling Greene <sterling@gradle.com>
2016-02-22 13:50:30 -0500

    Remove GradleConnection builder from public TAPI

--- subprojects/tooling-api/src/main/java/org/gradle/tooling/GradleConnector.java
+++ subprojects/tooling-api/src/main/java/org/gradle/tooling/GradleConnector.java
@@ -15,8 +15,6 @@
  */
 package org.gradle.tooling;
 
-import org.gradle.api.Incubating;
-import org.gradle.tooling.composite.GradleConnection;
 import org.gradle.tooling.internal.consumer.ConnectorServices;
 
 import java.io.File;
@@ -64,17 +62,6 @@ public abstract class GradleConnector {
      */
     public static GradleConnector newConnector() {
         return ConnectorServices.createConnector();
-    }
-
-    /**
-     * Creates a new connection builder instance for creating Gradle composite builds.
-     *
-     * @return The instance. Never returns null.
-     * @since 2.12
-     */
-    @Incubating
-    public static GradleConnection.Builder newGradleConnectionBuilder() {
-        return ConnectorServices.createGradleConnectionBuilder();
     }
 
     /**
