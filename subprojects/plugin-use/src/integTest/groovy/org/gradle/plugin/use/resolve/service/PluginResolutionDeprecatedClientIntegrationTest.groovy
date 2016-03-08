e52dd4e77637eb394e8afa05ca56d5f7f618a9af
Cedric Champeau <cedric@gradle.com>
2016-03-08 13:52:17 +0100

    Fix `PluginResolutionDeprecatedClientIntegrationTest` to use plugin names with different sizes

--- subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/use/resolve/service/PluginResolutionDeprecatedClientIntegrationTest.groovy
+++ subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/use/resolve/service/PluginResolutionDeprecatedClientIntegrationTest.groovy
@@ -35,7 +35,7 @@ class PluginResolutionDeprecatedClientIntegrationTest extends AbstractIntegratio
     // We need to use a string of a different size here, otherwise we reach a limit of Gradle in
     // detecting changes of the same file that happen within a few milliseconds, and do not change
     // the size of the file. See File#lastModified() for details
-    public static final String PLUGIN_ID_2 = "org.my.myplugin_2"
+    public static final String PLUGIN_ID_2 = "org.my.myplugin__2"
     public static final String VERSION = "1.0"
     public static final String GROUP = "my"
     public static final String ARTIFACT_1 = "plugin_1"
