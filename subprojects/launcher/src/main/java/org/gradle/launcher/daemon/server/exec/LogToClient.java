9fcae96d62ee94024b8b1bf60d39113e959f6780
Cedric Champeau <cedric@gradle.com>
2016-03-14 11:35:18 +0100

    Fixed async sending of messages to client being incomplete because of thread interruption

--- subprojects/launcher/src/main/java/org/gradle/launcher/daemon/server/exec/LogToClient.java
+++ subprojects/launcher/src/main/java/org/gradle/launcher/daemon/server/exec/LogToClient.java
@@ -28,7 +28,9 @@ import org.gradle.logging.internal.OutputEvent;
 import org.gradle.logging.internal.OutputEventListener;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class LogToClient extends BuildCommandOnly {
 
@@ -37,6 +39,8 @@ public class LogToClient extends BuildCommandOnly {
 
     private final LoggingOutputInternal loggingOutput;
     private final DaemonDiagnostics diagnostics;
+
+    private volatile AsynchronousLogDispatcher dispatcher;
 
     public LogToClient(LoggingOutputInternal loggingOutput, DaemonDiagnostics diagnostics) {
         this.loggingOutput = loggingOutput;
@@ -49,35 +53,36 @@ public class LogToClient extends BuildCommandOnly {
             return;
         }
 
-        final LogLevel buildLogLevel = build.getParameters().getLogLevel();
-        final AsynchronousLogDispatcher dispatcher = new AsynchronousLogDispatcher(execution.getConnection());
-        OutputEventListener listener = new OutputEventListener() {
-            public void onOutput(OutputEvent event) {
-                if (event.getLogLevel() != null && event.getLogLevel().compareTo(buildLogLevel) >= 0) {
-                    dispatcher.submit(event);
-                }
-            }
-        };
-
-        LOGGER.debug(DaemonMessages.ABOUT_TO_START_RELAYING_LOGS);
-        loggingOutput.addOutputEventListener(listener);
+        dispatcher = new AsynchronousLogDispatcher(execution.getConnection(), build.getParameters().getLogLevel());
+        LOGGER.info("{}{}). The daemon log file: {}", DaemonMessages.STARTED_RELAYING_LOGS, diagnostics.getPid(), diagnostics.getDaemonLog());
+        dispatcher.start();
         try {
-            LOGGER.info("{}{}). The daemon log file: {}", DaemonMessages.STARTED_RELAYING_LOGS, diagnostics.getPid(), diagnostics.getDaemonLog());
-            dispatcher.start();
             execution.proceed();
         } finally {
             dispatcher.waitForCompletion();
-            loggingOutput.removeOutputEventListener(listener);
         }
     }
 
     private class AsynchronousLogDispatcher extends Thread {
+        private final CountDownLatch completionLock = new CountDownLatch(1);
         private final BlockingQueue<OutputEvent> eventQueue = new LinkedBlockingDeque<OutputEvent>();
         private final DaemonConnection connection;
-        private boolean shouldStop;
+        private final OutputEventListener listener;
+        private volatile boolean shouldStop;
+        private boolean unableToSend;
 
-        private AsynchronousLogDispatcher(DaemonConnection conn) {
+        private AsynchronousLogDispatcher(DaemonConnection conn, final LogLevel buildLogLevel) {
+            super("Asynchronous log dispatcher for " + conn);
             this.connection = conn;
+            this.listener = new OutputEventListener() {
+                public void onOutput(OutputEvent event) {
+                    if (event.getLogLevel() != null && event.getLogLevel().compareTo(buildLogLevel) >= 0) {
+                        dispatcher.submit(event);
+                    }
+                }
+            };
+            LOGGER.debug(DaemonMessages.ABOUT_TO_START_RELAYING_LOGS);
+            loggingOutput.addOutputEventListener(listener);
         }
 
         public void submit(OutputEvent event) {
@@ -86,36 +91,53 @@ public class LogToClient extends BuildCommandOnly {
 
         @Override
         public void run() {
-            while (!shouldStop) {
-                try {
-                    OutputEvent event = eventQueue.take();
-                    dispatchAsync(event);
-                } catch (InterruptedException ex) {
-                    shouldStop = true;
+            OutputEvent event;
+            try {
+                while (!shouldStop) {
+                    // we must not use interrupt() because it would automatically
+                    // close the connection (sending data from an interrupted thread
+                    // automatically closes the connection)
+                    event = eventQueue.poll(10, TimeUnit.MILLISECONDS);
+                    if (event != null) {
+                        dispatchAsync(event);
+                    }
                 }
+            } catch (InterruptedException ex) {
+                shouldStop = true;
             }
+            sendRemainingEvents();
+            completionLock.countDown();
         }
 
         private void sendRemainingEvents() {
-            while (!eventQueue.isEmpty()) {
-                OutputEvent event = eventQueue.remove();
+            OutputEvent event;
+            while ((event = eventQueue.poll()) != null) {
                 dispatchAsync(event);
             }
         }
 
         private void dispatchAsync(OutputEvent event) {
+            if (unableToSend) {
+                return;
+            }
             try {
                 connection.logEvent(event);
             } catch (Exception ex) {
                 shouldStop = true;
+                unableToSend = true;
                 //Ignore. It means the client has disconnected so no point sending him any log output.
                 //we should be checking if client still listens elsewhere anyway.
             }
         }
 
         public void waitForCompletion() {
-            interrupt();
-            sendRemainingEvents();
+            loggingOutput.removeOutputEventListener(listener);
+            shouldStop = true;
+            try {
+                completionLock.await();
+            } catch (InterruptedException e) {
+                // the caller has been interrupted
+            }
         }
     }
 }
