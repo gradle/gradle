fb95c26c6661ab5e227cea95326a0e0971533057
Pepper Lebeck-Jobe <pepper@gradle.com>
2016-03-15 11:18:35 +0000

    Remove slower versions of Gradle from NativeBuildPerformanceTest
    +review REVIEW-5876

--- subprojects/performance/src/integTest/groovy/org/gradle/performance/NativeBuildPerformanceTest.groovy
+++ subprojects/performance/src/integTest/groovy/org/gradle/performance/NativeBuildPerformanceTest.groovy
@@ -29,9 +29,9 @@ class NativeBuildPerformanceTest extends AbstractCrossVersionPerformanceTest {
         given:
         runner.testId = "native build ${type}"
         runner.testProject = "${type}Native"
-        runner.tasksToRun = [ "clean", "assemble" ]
+        runner.tasksToRun = ["clean", "assemble"]
         runner.maxExecutionTimeRegression = maxExecutionTimeRegression
-        runner.targetVersions = ['2.8', 'last' ]
+        runner.targetVersions = ['2.11', 'last']
         runner.useDaemon = true
 
         when:
@@ -41,7 +41,7 @@ class NativeBuildPerformanceTest extends AbstractCrossVersionPerformanceTest {
         result.assertCurrentVersionHasNotRegressed()
 
         where:
-        type           | maxExecutionTimeRegression
+        test           | maxExecutionTimeRegression
         "small"        | millis(500)
         "medium"       | millis(500)
         "big"          | millis(2500)
@@ -52,9 +52,9 @@ class NativeBuildPerformanceTest extends AbstractCrossVersionPerformanceTest {
         given:
         runner.testId = "native build many projects"
         runner.testProject = "manyProjectsNative"
-        runner.tasksToRun = [ "clean", "assemble" ]
+        runner.tasksToRun = ["clean", "assemble"]
         runner.maxExecutionTimeRegression = millis(1000)
-        runner.targetVersions = [ '2.8', '2.11', 'last' ]
+        runner.targetVersions = ['2.11', 'last']
         runner.useDaemon = true
 
         when:
