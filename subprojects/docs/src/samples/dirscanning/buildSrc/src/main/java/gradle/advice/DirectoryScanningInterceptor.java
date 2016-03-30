c274856e9aa4dd14460ebd73a14f637c4afa0b2c
Lari Hotari <lari.hotari@gradle.com>
2016-03-30 17:33:18 -0400

    Upgrade to byte-buddy 1.3.5
    +review REVIEW-5869

--- subprojects/docs/src/samples/dirscanning/buildSrc/src/main/java/gradle/advice/DirectoryScanningInterceptor.java
+++ subprojects/docs/src/samples/dirscanning/buildSrc/src/main/java/gradle/advice/DirectoryScanningInterceptor.java
@@ -4,9 +4,8 @@ package gradle.advice;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.asm.Advice;
-import net.bytebuddy.asm.AsmVisitorWrapper;
+import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
-import net.bytebuddy.jar.asm.ClassWriter;
 import net.bytebuddy.matcher.ElementMatchers;
 import org.gradle.api.internal.file.collections.DirectoryFileTree;
 
@@ -20,26 +19,20 @@ public class DirectoryScanningInterceptor {
 
         // interceptor class must be injected to the same classloader as the target class that is intercepted
         for (Class<?> clazz : new Class<?>[]{CountDirectoryScans.class, CountCacheStats.class, CountCacheStats.Hits.class, CountCacheStats.Misses.class}) {
-            byteBuddy.redefine(clazz)
-                    .make()
-                    .load(targetClassLoader,
-                            ClassReloadingStrategy.fromInstalledAgent());
+            makeAndLoad(byteBuddy.redefine(clazz), targetClassLoader);
         }
 
-        byteBuddy.redefine(DirectoryFileTree.class)
-                .visit(new AsmVisitorWrapper.ForDeclaredMethods().writerFlags(ClassWriter.COMPUTE_FRAMES).method(ElementMatchers.named("visitFrom"), Advice.to(CountDirectoryScans.class)))
-                .make()
-                .load(targetClassLoader,
-                        ClassReloadingStrategy.fromInstalledAgent());
+        makeAndLoad(byteBuddy.redefine(DirectoryFileTree.class)
+                .visit(Advice.to(CountDirectoryScans.class).on(ElementMatchers.named("visitFrom"))), targetClassLoader);
 
 
-        byteBuddy.redefine(targetClassLoader.loadClass("org.gradle.api.internal.changedetection.state.CachingTreeVisitor"))
-                .visit(new AsmVisitorWrapper.ForDeclaredMethods().writerFlags(ClassWriter.COMPUTE_FRAMES)
-                        .method(ElementMatchers.named("recordCacheHit"), Advice.to(CountCacheStats.Hits.class))
+        makeAndLoad(byteBuddy.redefine(targetClassLoader.loadClass("org.gradle.api.internal.changedetection.state.CachingTreeVisitor"))
+                .visit(Advice.to(CountCacheStats.Hits.class).on(ElementMatchers.named("recordCacheHit"))
                         .method(ElementMatchers.named("recordCacheMiss"), Advice.to(CountCacheStats.Misses.class))
-                )
-                .make()
-                .load(targetClassLoader,
-                        ClassReloadingStrategy.fromInstalledAgent());
+                ), targetClassLoader);
+    }
+
+    private static void makeAndLoad(DynamicType.Builder<?> builder, ClassLoader targetClassLoader) {
+        builder.make().load(targetClassLoader, ClassReloadingStrategy.fromInstalledAgent());
     }
 }
