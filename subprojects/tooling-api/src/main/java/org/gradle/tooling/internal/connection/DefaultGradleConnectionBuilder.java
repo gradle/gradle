7855dce831e2cc17eea2fc2a0a5666887dff1def
Daz DeBoer <daz@gradle.com>
2016-04-06 12:35:34 -0600

    Avoid use of `DeprecationLogger` in the tooling API client

--- subprojects/tooling-api/src/main/java/org/gradle/tooling/internal/connection/DefaultGradleConnectionBuilder.java
+++ subprojects/tooling-api/src/main/java/org/gradle/tooling/internal/connection/DefaultGradleConnectionBuilder.java
@@ -27,8 +27,9 @@ import org.gradle.tooling.internal.consumer.DefaultCompositeConnectionParameters
 import org.gradle.tooling.internal.consumer.Distribution;
 import org.gradle.tooling.internal.consumer.DistributionFactory;
 import org.gradle.util.CollectionUtils;
-import org.gradle.util.DeprecationLogger;
 import org.gradle.util.GradleVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URI;
@@ -36,10 +37,13 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class DefaultGradleConnectionBuilder implements GradleConnectionBuilderInternal {
-    private static final String WARNING_MESSAGE = "Limitations include:\n"
+    private static final String WARNING_MESSAGE =
+        "Integrated composite build is an incubating feature.\n"
         + "   - All participant builds will be executed in a single daemon process.\n"
         + "   - Java home settings for participants will be ignored.\n"
-        + "   - Immutable JVM arguments (e.g. memory settings) will be ignored.\n";
+        + "   - Immutable JVM arguments (e.g. memory settings) for participants will be ignored.\n";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultGradleConnectionBuilder.class);
 
     private final Set<DefaultGradleConnectionParticipantBuilder> participantBuilders = Sets.newLinkedHashSet();
     private final GradleConnectionFactory gradleConnectionFactory;
@@ -91,8 +95,9 @@ public class DefaultGradleConnectionBuilder implements GradleConnectionBuilderIn
         DefaultCompositeConnectionParameters connectionParameters = compositeConnectionParametersBuilder.build();
 
         if (integrated) {
-            DeprecationLogger.incubatingFeatureUsed("Integrated composite build", WARNING_MESSAGE);
-            // TODO:DAZ Log a warning here about participant settings being ignored
+            LOGGER.warn(WARNING_MESSAGE);
+            // TODO:DAZ Work out why we can't use the deprecation logger (get NoClassDefFoundError in forkingIntegTest)
+            // DeprecationLogger.incubatingFeatureUsed("Integrated composite build", WARNING_MESSAGE);
             Distribution distribution = coordinatorDistribution;
             if (distribution == null) {
                 distribution = distributionFactory.getDistribution(GradleVersion.current().getVersion());
