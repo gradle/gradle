bb050e5a819dc2d7be038b72487933a086e5a1e8
Michael Barnathan <michael@gradle.com>
2016-04-30 23:55:56 -0400

    Flip LRU on: expire the least recently busy daemon when memory is low.

--- subprojects/launcher/src/main/java/org/gradle/launcher/daemon/bootstrap/DaemonMain.java
+++ subprojects/launcher/src/main/java/org/gradle/launcher/daemon/bootstrap/DaemonMain.java
@@ -32,13 +32,8 @@ import org.gradle.launcher.bootstrap.ExecutionListener;
 import org.gradle.launcher.daemon.configuration.DaemonServerConfiguration;
 import org.gradle.launcher.daemon.configuration.DefaultDaemonServerConfiguration;
 import org.gradle.launcher.daemon.context.DaemonContext;
-import org.gradle.launcher.daemon.server.CompositeDaemonExpirationStrategy;
-import org.gradle.launcher.daemon.server.DaemonExpirationStrategy;
-import org.gradle.launcher.daemon.server.DaemonIdleTimeoutExpirationStrategy;
-import org.gradle.launcher.daemon.server.DaemonRegistryUnavailableExpirationStrategy;
 import org.gradle.launcher.daemon.logging.DaemonMessages;
-import org.gradle.launcher.daemon.server.Daemon;
-import org.gradle.launcher.daemon.server.DaemonServices;
+import org.gradle.launcher.daemon.server.*;
 import org.gradle.process.internal.streams.EncodedStream;
 
 import java.io.*;
@@ -179,7 +174,12 @@ public class DaemonMain extends EntryPoint {
     private DaemonExpirationStrategy initializeExpirationStrategy(final DaemonServerConfiguration params) {
         DaemonIdleTimeoutExpirationStrategy timeoutStrategy = new DaemonIdleTimeoutExpirationStrategy(params.getIdleTimeout(), TimeUnit.MILLISECONDS);
         DaemonRegistryUnavailableExpirationStrategy registryUnavailableStrategy = new DaemonRegistryUnavailableExpirationStrategy();
-        return new CompositeDaemonExpirationStrategy(ImmutableList.of(timeoutStrategy, registryUnavailableStrategy));
+        DaemonExpirationStrategy lowMemoryLruStrategy = new AllDaemonExpirationStrategy(ImmutableList.of(
+            LowMemoryDaemonExpirationStrategy.belowFreePercentage(0.2),
+            new LruDaemonExpirationStrategy())
+        );
+
+        return new AnyDaemonExpirationStrategy(ImmutableList.of(registryUnavailableStrategy, lowMemoryLruStrategy));
     }
 
     private void redirectOutputsAndInput(PrintStream printStream) {
