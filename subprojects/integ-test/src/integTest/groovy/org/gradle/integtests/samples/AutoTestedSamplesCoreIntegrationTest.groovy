be42c77464c97b27987cd6b38b9acf382a033a69
Lóránt Pintér <lorant@gradle.com>
2016-05-02 20:05:00 +0200

    Fixed reference to `core/src/main/groovy`
    It should now be `src/main/java` instead.

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/AutoTestedSamplesCoreIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/AutoTestedSamplesCoreIntegrationTest.groovy
@@ -25,6 +25,6 @@ class AutoTestedSamplesCoreIntegrationTest extends AbstractAutoTestedSamplesTest
     void runSamples() {
         //Uncomment below to run test only for single class (much faster)
 //        includeOnly '**/Copy.java'
-        runSamplesFrom("subprojects/core/src/main/groovy/org/gradle/api")
+        runSamplesFrom("subprojects/core/src/main/java/org/gradle/api")
     }
 }
