a9ae3a645d37d8b18179bdd90322cbc36d51c601
Donat Csikos <donat@gradle.com>
2016-05-02 14:08:11 +0200

    Adjust integration test
    +review REVIEW-5935

--- subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r29/ToolingApiEclipseModelCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r29/ToolingApiEclipseModelCrossVersionSpec.groovy
@@ -164,7 +164,6 @@ class ToolingApiEclipseModelCrossVersionSpec extends ToolingApiSpecification {
         ['groovy']                  | ['org.eclipse.jdt.core.javabuilder']
         ['java', 'scala']           | ['org.scala-ide.sdt.core.scalabuilder']
         ['java', 'scala', 'groovy'] | ['org.scala-ide.sdt.core.scalabuilder']
-        ['java','ear']              | []
     }
 
     @TargetGradleVersion(">=2.9")
