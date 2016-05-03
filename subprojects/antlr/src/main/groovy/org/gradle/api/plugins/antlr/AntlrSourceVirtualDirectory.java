ed3d9e9c4c349b2bf7b2085bd92445c30ee1002f
Paul Merlin <paul@gradle.com>
2016-05-03 19:09:38 +0200

    Refine AntlrSourceVirtualDirectory
    Remove reference to internal package from Javadoc, fixes dslDocbook task
    warning.
    
    Remove unnecessary modifiers.

--- subprojects/antlr/src/main/groovy/org/gradle/api/plugins/antlr/AntlrSourceVirtualDirectory.java
+++ subprojects/antlr/src/main/groovy/org/gradle/api/plugins/antlr/AntlrSourceVirtualDirectory.java
@@ -22,18 +22,17 @@ import org.gradle.api.file.SourceDirectorySet;
 /**
  * Contract for a Gradle "convention object" that acts as a handler for what I call a virtual directory mapping,
  * injecting a virtual directory named 'antlr' into the project's various {@link org.gradle.api.tasks.SourceSet source
- * sets}.  Its implementation gets pushed onto the {@link org.gradle.api.internal.DynamicObjectAware} portion of the
- * source set under the name 'antlr'.
+ * sets}.
  */
 public interface AntlrSourceVirtualDirectory {
-    public static final String NAME = "antlr";
+    String NAME = "antlr";
 
     /**
      * All Antlr source for this source set.
      *
      * @return The Antlr source.  Never returns null.
      */
-    public SourceDirectorySet getAntlr();
+    SourceDirectorySet getAntlr();
 
     /**
      * Configures the Antlr source for this set. The given closure is used to configure the {@link org.gradle.api.file.SourceDirectorySet} (see
@@ -42,6 +41,6 @@ public interface AntlrSourceVirtualDirectory {
      * @param configureClosure The closure to use to configure the Antlr source.
      * @return this
      */
-    public AntlrSourceVirtualDirectory antlr(Closure configureClosure);
+    AntlrSourceVirtualDirectory antlr(Closure configureClosure);
 
 }
