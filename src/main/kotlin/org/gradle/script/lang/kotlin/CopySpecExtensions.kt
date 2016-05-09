6ed44ad6645341392910e96c44772f3c1469da5c
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2016-05-09 16:14:12 -0300

    Don't rely on `CopySpecInternal`

--- src/main/kotlin/org/gradle/script/lang/kotlin/CopySpecExtensions.kt
+++ src/main/kotlin/org/gradle/script/lang/kotlin/CopySpecExtensions.kt
@@ -1,20 +1,10 @@
 package org.gradle.script.lang.kotlin
 
 import org.gradle.api.file.CopySpec
-import org.gradle.api.internal.file.copy.CopySpecInternal
 
 // Interim extensions until those methods are added to CopySpec
 fun CopySpec.from(path: Any, setup: (CopySpec) -> Unit): CopySpec =
-    addChild {
-        from(path)
-        setup(this)
-    }
+    from(path).apply { setup(this) }
 
 fun CopySpec.into(path: Any, setup: (CopySpec) -> Unit): CopySpec =
-    addChild {
-        into(path)
-        setup(this)
-    }
-
-private inline fun CopySpec.addChild(crossinline setup: CopySpec.() -> Unit): CopySpec =
-    (this as CopySpecInternal).addChild().apply(setup)
+    into(path).apply { setup(this) }
