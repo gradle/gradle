df0bfbb60f12f7c3448f5f77ddda3e500d8c76a9
Pepper Lebeck-Jobe <pepper@gradle.com>
2016-05-09 15:26:39 +0100

    Add samples of publishing and consuming plugins

---
+++ subprojects/docs/src/samples/plugins/publishing/src/main/java/org/gradle/sample/goodbye/GoodbyePlugin.java
@@ -0,0 +1,18 @@
+package org.gradle.sample.goodbye;
+
+import org.gradle.api.Action;
+import org.gradle.api.Project;
+import org.gradle.api.Plugin;
+import org.gradle.api.Task;
+
+public class GoodbyePlugin implements Plugin<Project> {
+  public void apply(Project project) {
+    Task goodbye = project.getTasks().create("goodbye");
+    goodbye.doLast(new Action<Task>() {
+      @Override
+      public void execute(Task task) {
+        task.getLogger().lifecycle("Goodbye!");
+      }
+    });
+  }
+}
