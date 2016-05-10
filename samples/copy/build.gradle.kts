609e10ad6b0b6faa3372d3d75f32f46dfc417df8
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2016-05-10 16:17:04 -0300

    Add convenience `CopySpec#filter` extensions

--- samples/copy/build.gradle.kts
+++ samples/copy/build.gradle.kts
@@ -1,9 +1,6 @@
 import org.gradle.api.tasks.*
 import org.gradle.script.lang.kotlin.*
-
-// TODO: ReplaceTokens
-//for Ant filter
-//import org.apache.tools.ant.filters.*
+import org.apache.tools.ant.filters.*
 
 //for including in the copy task
 val dataContent = copySpec {
@@ -16,9 +13,8 @@ task<Copy>("initConfig") {
     from("src/main/config") {
         it.include("**/*.properties")
         it.include("**/*.xml")
-//        it.filter<ReplaceTokens> {
-//           token("version", "2.3.1")
-//        }
+        it.filter<ReplaceTokens>(
+            "tokens" to mapOf("version" to "2.3.1"))
     }
 
     from("src/main/languages") {
--- src/main/kotlin/org/gradle/script/lang/kotlin/CopySpecExtensions.kt
+++ src/main/kotlin/org/gradle/script/lang/kotlin/CopySpecExtensions.kt
@@ -1,6 +1,7 @@
 package org.gradle.script.lang.kotlin
 
 import org.gradle.api.file.CopySpec
+import java.io.FilterReader
 
 // Interim extensions until those methods are added to CopySpec
 fun CopySpec.from(path: Any, setup: (CopySpec) -> Unit): CopySpec =
@@ -8,3 +9,9 @@ fun CopySpec.from(path: Any, setup: (CopySpec) -> Unit): CopySpec =
 
 fun CopySpec.into(path: Any, setup: (CopySpec) -> Unit): CopySpec =
     into(path).apply { setup(this) }
+
+inline fun <reified T : FilterReader> CopySpec.filter(vararg properties: Pair<String, *>) =
+    filter(mapOf(*properties), T::class.java)
+
+inline fun <reified T : FilterReader> CopySpec.filter(properties: Map<String, *>) =
+    filter(properties, T::class.java)
