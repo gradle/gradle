eda33a7d6477dfc9ab07eef8d984cc7da5ee4ed9
Adam Murdoch <adam@gradle.com>
2016-05-12 16:22:27 +1000

    Fixed expected distribution jar count.

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -39,7 +39,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        164
+        160
     }
 
     def "no duplicate entries"() {
