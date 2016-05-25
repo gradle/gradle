96df4323f34aec49df9720d2a79a13321be2056f
Lóránt Pintér <lorant@gradle.com>
2016-05-25 22:25:32 +0200

    Make task property validation parallelizable
    +review REVIEW-5989

--- subprojects/plugin-development/src/main/java/org/gradle/plugin/devel/tasks/ValidateTaskProperties.java
+++ subprojects/plugin-development/src/main/java/org/gradle/plugin/devel/tasks/ValidateTaskProperties.java
@@ -37,6 +37,7 @@ import org.gradle.api.internal.project.taskfactory.TaskPropertyValidationAccess;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.ParallelizableTask;
 import org.gradle.api.tasks.SkipWhenEmpty;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskValidationException;
@@ -96,6 +97,7 @@ import java.util.Map;
  * </ul>
  */
 @Incubating
+@ParallelizableTask
 public class ValidateTaskProperties extends DefaultTask implements VerificationTask {
     private File classesDir;
     private FileCollection classpath;
