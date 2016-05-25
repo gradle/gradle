c5f5318f6a228cbf40929f896d23f76c7ce18623
Lóránt Pintér <lorant@gradle.com>
2016-05-25 21:16:57 +0200

    Don't break on missing `GeneratorTask.inputFile`
    +review REVIEW-5932

--- subprojects/ide/src/main/groovy/org/gradle/plugins/ide/api/GeneratorTask.java
+++ subprojects/ide/src/main/groovy/org/gradle/plugins/ide/api/GeneratorTask.java
@@ -18,8 +18,7 @@ package org.gradle.plugins.ide.api;
 import org.gradle.api.GradleException;
 import org.gradle.api.internal.ConventionTask;
 import org.gradle.api.specs.Specs;
-import org.gradle.api.tasks.InputFile;
-import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.internal.reflect.Instantiator;
@@ -94,7 +93,8 @@ public class GeneratorTask<T> extends ConventionTask {
      *
      * @return The input file.
      */
-    @Optional @InputFile
+    // @Optional @InputFile
+    @Internal
     public File getInputFile() {
         return inputFile != null ? inputFile : getOutputFile();
     }
