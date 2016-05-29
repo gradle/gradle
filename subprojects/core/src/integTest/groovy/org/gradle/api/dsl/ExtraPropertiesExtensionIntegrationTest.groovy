7787841b0d1cd39d8889ced230230199652f9826
Adam Murdoch <adam@gradle.com>
2016-05-29 16:03:02 +1000

    Moved int test to live with tests that cover similar stuff.

--- subprojects/core/src/integTest/groovy/org/gradle/api/dsl/ExtraPropertiesExtensionIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/dsl/ExtraPropertiesExtensionIntegrationTest.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.api.internal.plugins
+package org.gradle.api.dsl
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import spock.lang.Issue
