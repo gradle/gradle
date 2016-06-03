3df75b98a77b94249ed94fb724f38fa9746f5746
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2016-06-03 10:53:56 -0300

    Declare inputs and outputs of `CheckSamples`

--- buildSrc/src/main/kotlin/integration/CheckSamples.kt
+++ buildSrc/src/main/kotlin/integration/CheckSamples.kt
@@ -1,40 +1,63 @@
 package integration
 
+import org.apache.tools.ant.util.TeeOutputStream
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.tooling.GradleConnector.newConnector
 import org.gradle.tooling.ProjectConnection
 import java.io.File
+import java.io.FileOutputStream
 
 open class CheckSamples : DefaultTask() {
 
     override fun getDescription() =
         "Checks each sample by running `gradle help` on it."
 
-    @get:InputDirectory
-    val samples: File by lazy { project.file("samples") }
+    @get:InputFiles
+    val samples: FileCollection by lazy {
+        project.fileTree(mapOf("dir" to samplesDir)).apply {
+            include("**/*.gradle.kts")
+            include("**/gradle-wrapper.properties")
+        }
+    }
+
+    @get:OutputDirectory
+    val outputDir: File by lazy {
+        File(project.buildDir, "check-samples")
+    }
 
     @TaskAction
     fun run() {
         sampleDirs().forEach { sampleDir ->
             println("Checking ${relativeFile(sampleDir)}...")
-            runGradleHelpOn(sampleDir)
+            OutputStreamForResultOf(sampleDir).use { stdout ->
+                runGradleHelpOn(sampleDir, stdout)
+            }
         }
     }
 
     private fun sampleDirs() =
-        samples.listFiles().filter { it.isDirectory }
+        samplesDir.listFiles().filter { it.isDirectory }
+
+    private val samplesDir: File by lazy {
+        project.file("samples")
+    }
+
+    private fun OutputStreamForResultOf(sampleDir: File) =
+        File(outputDir, "${sampleDir.name}.txt").outputStream()
 
     private fun relativeFile(file: File) =
         file.relativeTo(project.projectDir)
 
-    private fun runGradleHelpOn(projectDir: File) {
+    private fun runGradleHelpOn(projectDir: File, stdout: FileOutputStream) {
         connectionFor(projectDir)
             .newBuild()
             .forTasks("help")
-            .setStandardOutput(System.out)
-            .setStandardError(System.err)
+            .setStandardOutput(TeeOutputStream(System.out, stdout))
+            .setStandardError(TeeOutputStream(System.err, stdout))
             .run()
     }
 
