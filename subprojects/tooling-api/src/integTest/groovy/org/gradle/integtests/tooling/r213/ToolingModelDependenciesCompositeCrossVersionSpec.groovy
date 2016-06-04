cf7e7a1b983fb12bd7230fdc190e65c137722ab8
Daz DeBoer <daz@gradle.com>
2016-06-04 11:23:28 -0600

    Integration test coverage for renamed IDE modules in TAPI models

--- subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r213/ToolingModelDependenciesCompositeCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r213/ToolingModelDependenciesCompositeCrossVersionSpec.groovy
@@ -15,45 +15,53 @@
  */
 
 package org.gradle.integtests.tooling.r213
-
 import org.gradle.integtests.tooling.fixture.CompositeToolingApiSpecification
+import org.gradle.integtests.tooling.fixture.RequiresIntegratedComposite
+import org.gradle.test.fixtures.file.TestFile
 import org.gradle.test.fixtures.maven.MavenFileRepository
-import org.gradle.tooling.internal.connection.DefaultBuildIdentifier
+import org.gradle.tooling.internal.connection.DefaultProjectIdentifier
 import org.gradle.tooling.model.eclipse.EclipseProject
 import org.gradle.tooling.model.idea.IdeaModule
 import org.gradle.tooling.model.idea.IdeaModuleDependency
 import org.gradle.tooling.model.idea.IdeaProject
 import org.gradle.tooling.model.idea.IdeaSingleEntryLibraryDependency
 
+import static org.gradle.util.CollectionUtils.single
+
 /**
  * Dependency substitution is performed for composite build accessed via the `GradleConnection` API.
  */
-// TODO:DAZ Need coverage for builds that have customized Idea/Eclipse configuration (including module names)
 class ToolingModelDependenciesCompositeCrossVersionSpec extends CompositeToolingApiSpecification {
     def stdOut = new ByteArrayOutputStream()
-    def buildA
-    def buildB
+    TestFile buildA
+    TestFile buildB
     def builds = []
-    def publishedModuleB
+    def publishedModuleB1
+    EclipseProject eclipseProjectA
+    EclipseProject eclipseProjectB1
+    IdeaModule ideaModuleA
+    IdeaModule ideaModuleB1
 
     def setup() {
         def mavenRepo = new MavenFileRepository(file("maven-repo"))
-        publishedModuleB = mavenRepo.module("org.test", "buildB", "1.0").publish()
+        publishedModuleB1 = mavenRepo.module("org.test", "b1", "1.0").publish()
 
         buildA = singleProjectBuild("buildA") {
-                    buildFile << """
+            buildFile << """
         apply plugin: 'java'
         dependencies {
-            compile "org.test:buildB:1.0"
+            compile "org.test:b1:1.0"
         }
         repositories {
             maven { url '${mavenRepo.uri}' }
         }
 """
 }
-        buildB = singleProjectBuild("buildB") {
-                    buildFile << """
-        apply plugin: 'java'
+        buildB = multiProjectBuild("buildB", ['b1', 'b2']) {
+            buildFile << """
+        allprojects {
+            apply plugin: 'java'
+        }
 """
 }
         builds << buildA << buildB
@@ -61,58 +69,135 @@ class ToolingModelDependenciesCompositeCrossVersionSpec extends CompositeTooling
 
     def "EclipseProject model has dependencies substituted in composite"() {
         when:
-        def eclipseProjects = withCompositeConnection(builds) { connection ->
-            connection.getModels(EclipseProject).asList()*.model
-        }
+        loadEclipseProjectModels()
 
         then:
-        eclipseProjects.size() == 2
-        def eclipseProjectA = eclipseProjects.find { it.projectDirectory.absoluteFile == buildA.absoluteFile }
-        def eclipseProjectB = eclipseProjects.find { it.projectDirectory.absoluteFile == buildB.absoluteFile }
-        eclipseProjectA != null
-        eclipseProjectB != null
         if (isIntegratedComposite()) {
             assert eclipseProjectA.classpath.empty
             assert eclipseProjectA.projectDependencies.size() == 1
             with(eclipseProjectA.projectDependencies.first()) {
-                assert path == 'buildB'
+                assert path == 'b1'
                 assert targetProject == null
-                assert target == eclipseProjectB.identifier
+                assert target == eclipseProjectB1.identifier
             }
         } else {
             assert eclipseProjectA.projectDependencies.empty
             assert eclipseProjectA.classpath.size() == 1
             def externalDependency = eclipseProjectA.classpath.first()
-            assert externalDependency.file == publishedModuleB.artifactFile
+            assert externalDependency.file == publishedModuleB1.artifactFile
         }
+    }
+
+    @RequiresIntegratedComposite
+    def "EclipseProject model honours custom project name"() {
+        when:
+        buildB.buildFile << """
+            subprojects {
+                apply plugin: 'eclipse'
+                eclipse {
+                    project.name = project.name + "-renamed"
+                }
+            }
+            project(":b1") {
+                dependencies {
+                    compile project(":b2")
+                }
+            }
+"""
+
+        def eclipseProjects = loadEclipseProjectModels()
+        def eclipseProjectB2 = eclipseProjects.find { it.projectDirectory.absoluteFile == buildB.file('b2').absoluteFile }
+
+        then:
+        eclipseProjectA.projectDependencies.size() == 2
+        def depB1 = eclipseProjectA.projectDependencies.find { it.target == eclipseProjectB1.identifier }
+        depB1 != null
+        depB1.path == 'b1-renamed'
+
+        and:
+        def depB2 = eclipseProjectA.projectDependencies.find { it.target == eclipseProjectB2.identifier }
+        depB2 != null
+        depB2.path == 'b2-renamed'
+
+        and:
+        def depB1onB2 = single(eclipseProjectB1.projectDependencies)
+        depB1onB2.path == 'b2-renamed'
+        depB1onB2.target == eclipseProjectB2.identifier
     }
 
     def "Idea model has dependencies substituted in composite"() {
         when:
-        def ideaProjects = withCompositeConnection(builds) { connection ->
-            connection.getModels(IdeaProject).asList()*.model
-        }
-        def ideaModules = ideaProjects*.modules.flatten() as List<IdeaModule>
+        loadIdeaModuleModels()
 
         then:
-        ideaModules.size() == 2
-        def ideaModuleA = ideaModules.find { it.gradleProject.projectIdentifier.buildIdentifier == new DefaultBuildIdentifier(buildA) }
-        def ideaModuleB = ideaModules.find { it.gradleProject.projectIdentifier.buildIdentifier == new DefaultBuildIdentifier(buildB) }
-        ideaModuleA != null
-        ideaModuleB != null
         if (isIntegratedComposite()) {
-            assert ideaModuleB.identifier != null
+            assert ideaModuleB1.identifier != null
             assert ideaModuleA.dependencies.size() == 1
             with(ideaModuleA.dependencies.first()) {
                 assert it instanceof IdeaModuleDependency
                 assert dependencyModule == null
-                assert target == ideaModuleB.identifier
+                assert target == ideaModuleB1.identifier
             }
         } else {
             assert ideaModuleA.dependencies.size() == 1
             def externalDependency = ideaModuleA.dependencies.first()
             assert externalDependency instanceof IdeaSingleEntryLibraryDependency
-            assert externalDependency.file == publishedModuleB.artifactFile
+            assert externalDependency.file == publishedModuleB1.artifactFile
         }
+    }
+
+    @RequiresIntegratedComposite
+    def "Idea model honours custom module name"() {
+        when:
+        buildB.buildFile << """
+            subprojects {
+                apply plugin: 'idea'
+                idea {
+                    module.name = module.name + "-renamed"
+                }
+            }
+            project(":b1") {
+                dependencies {
+                    compile project(":b2")
+                }
+            }
+"""
+
+        def ideaModules = loadIdeaModuleModels()
+        def ideaModuleB2 = ideaModules.find { it.gradleProject.projectIdentifier == new DefaultProjectIdentifier(buildB, ":b2") }
+
+        then:
+        ideaModuleA.dependencies.size() == 2
+        ideaModuleA.dependencies.any { it instanceof IdeaModuleDependency && it.target == ideaModuleB1.identifier }
+        ideaModuleA.dependencies.any { it instanceof IdeaModuleDependency && it.target == ideaModuleB2.identifier }
+
+        and:
+        ideaModuleB1.dependencies.size() == 1
+        ideaModuleA.dependencies.any { it instanceof IdeaModuleDependency && it.target == ideaModuleB2.identifier }
+    }
+
+    private ArrayList<EclipseProject> loadEclipseProjectModels() {
+        def eclipseProjects = withCompositeConnection(builds) { connection ->
+            connection.getModels(EclipseProject).asList()*.model
+        }
+        assert eclipseProjects.size() == 4
+        eclipseProjectA = eclipseProjects.find { it.projectDirectory.absoluteFile == buildA.absoluteFile }
+        eclipseProjectB1 = eclipseProjects.find { it.projectDirectory.absoluteFile == buildB.file('b1').absoluteFile }
+        assert eclipseProjectA != null
+        assert eclipseProjectB1 != null
+        return eclipseProjects
+    }
+
+    private List<IdeaModule> loadIdeaModuleModels() {
+        def ideaProjects = withCompositeConnection(builds) { connection ->
+            connection.getModels(IdeaProject).asList()*.model
+        }
+        def ideaModules = ideaProjects*.modules.flatten() as List<IdeaModule>
+        assert ideaModules.size() == 4
+        ideaModuleA = ideaModules.find { it.gradleProject.projectIdentifier == new DefaultProjectIdentifier(buildA, ":") }
+        ideaModuleB1 = ideaModules.find { it.gradleProject.projectIdentifier == new DefaultProjectIdentifier(buildB, ":b1") }
+        assert ideaModuleA != null
+        assert ideaModuleB1 != null
+        ideaModules
     }
 }
