cf732aacd22017390d205357652863d980c76a19
Matias Hernandez <mhernand@gmail.com>
2016-06-08 02:05:42 -0300

    GRADLE-3476 Removed 4NT-specific code in bat files
    It's not necessary since it doesn't use SHIFT anymore, and actually
    causes some parameters (those containing dashes, such as
    "-Dorg.gradle.jvmargs=-Xmx2048m") to be processed incorrectly in
    TCC/4NT.

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/ApplicationIntegrationSpec.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/ApplicationIntegrationSpec.groovy
@@ -303,7 +303,7 @@ installDist.destinationDir = buildDir
         then:
         File generatedWindowsStartScript = file("build/scripts/application.bat")
         generatedWindowsStartScript.exists()
-        assertLineSeparators(generatedWindowsStartScript, TextUtil.windowsLineSeparator, 90);
+        assertLineSeparators(generatedWindowsStartScript, TextUtil.windowsLineSeparator, 84);
 
         File generatedLinuxStartScript = file("build/scripts/application")
         generatedLinuxStartScript.exists()
