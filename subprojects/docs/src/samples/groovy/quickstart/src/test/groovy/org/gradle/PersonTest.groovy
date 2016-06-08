5959b63d38dc7adcc7651ce2c4c5003bc699b1b4
Marco Vermeulen <marco@gradle.com>
2016-06-08 10:38:56 +0100

    Upgrade to Groovy 2.4.7.
    + review REVIEW-5995

--- subprojects/docs/src/samples/groovy/quickstart/src/test/groovy/org/gradle/PersonTest.groovy
+++ subprojects/docs/src/samples/groovy/quickstart/src/test/groovy/org/gradle/PersonTest.groovy
@@ -17,7 +17,7 @@ class PersonTest {
     }
 
     @Test public void usingCorrectVersionOfGroovy() {
-        assertEquals('2.4.6', GroovySystem.version)
+        assertEquals('2.4.7', GroovySystem.version)
     }
 
     @Test public void testResourcesAreAvailable() {
--- subprojects/language-groovy/src/main/java/org/gradle/api/tasks/javadoc/Groovydoc.java
+++ subprojects/language-groovy/src/main/java/org/gradle/api/tasks/javadoc/Groovydoc.java
@@ -191,7 +191,7 @@ public class Groovydoc extends SourceTask {
     }
 
     /**
-     * Returns whether to include timestamp within hidden comment in generated HTML (Groovy >= 2.4.6).
+     * Returns whether to include timestamp within hidden comment in generated HTML (Groovy >= 2.4.7).
      */
     @Input
     public boolean isNoTimestamp() {
@@ -199,14 +199,14 @@ public class Groovydoc extends SourceTask {
     }
 
     /**
-     * Sets whether to include timestamp within hidden comment in generated HTML (Groovy >= 2.4.6).
+     * Sets whether to include timestamp within hidden comment in generated HTML (Groovy >= 2.4.7).
      */
     public void setNoTimestamp(boolean noTimestamp) {
         this.noTimestamp = noTimestamp;
     }
 
     /**
-     * Returns whether to include version stamp within hidden comment in generated HTML (Groovy >= 2.4.6).
+     * Returns whether to include version stamp within hidden comment in generated HTML (Groovy >= 2.4.7).
      */
     @Input
     public boolean isNoVersionStamp() {
@@ -214,7 +214,7 @@ public class Groovydoc extends SourceTask {
     }
 
     /**
-     * Sets whether to include version stamp within hidden comment in generated HTML (Groovy >= 2.4.6).
+     * Sets whether to include version stamp within hidden comment in generated HTML (Groovy >= 2.4.7).
      */
     public void setNoVersionStamp(boolean noVersionStamp) {
         this.noVersionStamp = noVersionStamp;
