8003d86302709ba02b3c31365c19a917d3fd2cfe
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2016-06-17 16:45:35 -0300

    Add missing header

--- src/main/kotlin/org/gradle/script/lang/kotlin/support/KotlinBuildScriptSection.kt
+++ src/main/kotlin/org/gradle/script/lang/kotlin/support/KotlinBuildScriptSection.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.gradle.script.lang.kotlin.support
 
 import org.gradle.api.Project
@@ -6,6 +22,11 @@ import org.gradle.script.lang.kotlin.KotlinScriptHandler
 
 abstract class KotlinBuildScriptSection(project: Project) : KotlinBuildScript(project) {
 
+    /**
+     * Configures the build script classpath for this project.
+     *
+     * @see [Project.buildscript]
+     */
     override fun buildscript(configuration: KotlinScriptHandler.() -> Unit) {
         KotlinScriptHandler(project.buildscript).configuration()
     }
