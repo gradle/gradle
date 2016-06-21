c00bbef864749b02b43e17a9f50a5c29a08b447d
Lari Hotari <lari.hotari@gradle.com>
2016-06-21 18:08:30 +0300

    Create unit test for resolving cacheable tasks
    +review REVIEW-5972

--- subprojects/core/src/test/groovy/org/gradle/api/internal/changedetection/state/TreeVisitorCacheExpirationStrategyTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/changedetection/state/TreeVisitorCacheExpirationStrategyTest.groovy
@@ -17,12 +17,25 @@
 package org.gradle.api.internal.changedetection.state
 
 import org.gradle.BuildListener
+import org.gradle.api.Task
+import org.gradle.api.execution.TaskExecutionGraph
 import org.gradle.api.execution.TaskExecutionGraphListener
 import org.gradle.api.execution.TaskExecutionListener
+import org.gradle.api.internal.file.collections.SimpleFileCollection
+import org.gradle.api.tasks.TaskInputs
+import org.gradle.api.tasks.TaskOutputs
+import org.gradle.internal.event.DefaultListenerManager
 import org.gradle.internal.event.ListenerManager
+import org.gradle.test.fixtures.file.TestFile
+import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider
+import org.gradle.util.UsesNativeServices
+import org.junit.Rule
 import spock.lang.Specification
 
+@UsesNativeServices
 class TreeVisitorCacheExpirationStrategyTest extends Specification {
+    @Rule
+    public final TestNameTestDirectoryProvider testDir = new TestNameTestDirectoryProvider();
 
     def "listeners get registered in constructor and removed when stopping"() {
         given:
@@ -67,6 +80,64 @@ class TreeVisitorCacheExpirationStrategyTest extends Specification {
         1 * listenerManager.removeListener(taskExecutionListener)
         1 * listenerManager.removeListener(taskExecutionGraphListener)
         0 * _._
+    }
+
+    def "cacheable tasks get resolved"() {
+        given:
+        CachingTreeVisitor cachingTreeVisitor = Mock()
+        DefaultListenerManager listenerManager = new DefaultListenerManager()
+        def treeVisitorCacheExpirationStrategy = new TreeVisitorCacheExpirationStrategy(cachingTreeVisitor, listenerManager)
+        TaskExecutionGraphListener taskExecutionGraphListener = listenerManager.getBroadcaster(TaskExecutionGraphListener)
+        def taskExecutionGraph = Mock(TaskExecutionGraph)
+        def allTasks = createTasksForScenario(scenario)
+        def cacheableFilePaths = filePathsForScenario(scenario)
+
+        when:
+        taskExecutionGraphListener.graphPopulated(taskExecutionGraph)
+
+        then:
+        1 * taskExecutionGraph.getAllTasks() >> allTasks
+        1 * cachingTreeVisitor.updateCacheableFilePaths(cacheableFilePaths)
+        0 * _._
+
+        where:
+        scenario << ['none_cacheable', 'b_uses_a']
+    }
+
+    List<Task> createTasksForScenario(String scenario) {
+        switch (scenario) {
+            case 'none_cacheable':
+                return [createTaskStub(":a", [file("a/input")], [file("a/output")]), createTaskStub(":b", [file("b/input")], [file("b/output")]), createTaskStub(":c", [file("c/input")], [file("c/output")])]
+            case 'b_uses_a':
+                return [createTaskStub(":a", [file("a/input")], [file("a/output")]), createTaskStub(":b", [file("a/output")], [file("b/output")])]
+        }
+        throw new IllegalArgumentException("Unknown scenario")
+    }
+
+    Collection<String> filePathsForScenario(String scenario) {
+        switch (scenario) {
+            case 'none_cacheable':
+                return []
+            case 'b_uses_a':
+                return [file("a/output").absolutePath]
+        }
+        throw new IllegalArgumentException("Unknown scenario")
+    }
+
+    Task createTaskStub(String path, List<File> inputs, List<File> outputs) {
+        Task task = Stub(Task)
+        task.getPath() >> path
+        TaskInputs taskInputs = Stub(TaskInputs)
+        task.getInputs() >> taskInputs
+        taskInputs.getFiles() >> new SimpleFileCollection(inputs)
+        TaskOutputs taskOutputs = Stub(TaskOutputs)
+        task.getOutputs() >> taskOutputs
+        taskOutputs.getFiles() >> new SimpleFileCollection(outputs)
+        task
+    }
+
+    TestFile file(Object... path) {
+        testDir.file(path)
     }
 
 }
