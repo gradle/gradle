70c449178da9c233e94eb97d8369dba6957e0d7b
Donat Csikos <donat@gradle.com>
2016-07-21 16:03:16 +0200

    Revert "Exclude milestone releases from cross-version testing"
    This reverts commit 204ea26e26e54c94bc7bca804f9d91332df4ad41.

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/versions/ReleasedVersionDistributions.java
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/versions/ReleasedVersionDistributions.java
@@ -82,14 +82,7 @@ public class ReleasedVersionDistributions {
 
     public List<GradleDistribution> getAll() {
         if (distributions == null) {
-            List<String> versions = CollectionUtils.filter(getProperties().getProperty("versions").split("\\s+"), new Spec<String>() {
-                @Override
-                public boolean isSatisfiedBy(String version) {
-                    return !version.contains("-milestone-");
-                }
-            });
-
-            distributions = CollectionUtils.collect(versions, new Transformer<GradleDistribution, String>() {
+            distributions = CollectionUtils.collect(getProperties().getProperty("versions").split("\\s+"), new Transformer<GradleDistribution, String>() {
                 public GradleDistribution transform(String version) {
                     return buildContext.distribution(version);
                 }
