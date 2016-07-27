57a29928c0c0ee3c19b2c0c77667c6c311b22b7d
Stefan Wolf <wolf@gradle.com>
2016-07-27 15:54:32 +0200

    Use example projects instead of inline Strings for Kotlin smoke tests

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/KotlinPluginSmokeSpec.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/KotlinPluginSmokeSpec.groovy
@@ -19,49 +19,13 @@ package org.gradle.smoketests
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class KotlinPluginSmokeSpec extends AbstractSmokeSpec {
+    private kotlinVersion = '1.0.3'
+    private androidPluginVersion = '2.2.0-alpha6'
 
     def 'kotlin plugin'() {
         given:
-        def kotlinVersion = '1.0.2'
-        buildFile << """
-            buildscript {
-                ext.kotlin_version = '$kotlinVersion'
-
-                repositories {
-                    mavenCentral()
-                }
-
-                dependencies {
-                    classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
-                }
-            }
-
-            apply plugin: 'kotlin'
-
-            repositories {
-                mavenCentral()
-            }
-
-            dependencies {
-                compile "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
-            }
-        """.stripIndent()
-
-        file('src/main/kotlin/pkg/HelloWorld.kt') << """
-        package pkg
-
-        fun getGreeting(): String {
-            val words = mutableListOf<String>()
-            words.add("Hello,")
-            words.add("world!")
-
-            return words.joinToString(separator = " ")
-        }
-
-        fun main(args: Array<String>) {
-            println(getGreeting())
-        }
-        """.stripIndent()
+        useSample("kotlin-example")
+        replaceVariablesInBuildFile(kotlinVersion: this.kotlinVersion)
 
         when:
         def result = runner('build').build()
@@ -71,160 +35,11 @@ class KotlinPluginSmokeSpec extends AbstractSmokeSpec {
     }
 
     def 'kotlin android plugin'() {
-
         given:
-        buildFile << """
-buildscript {
-    repositories {
-        jcenter()
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:2.2.0-alpha6'
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.0.3'
-        classpath 'org.jetbrains.kotlin:kotlin-android-extensions:1.0.3'
-    }
-}
-
-System.properties['com.android.build.gradle.overrideVersionCheck'] = 'true'
-
-repositories {
-    jcenter()
-}
-
-apply plugin: 'com.android.application'
-apply plugin: 'kotlin-android'
-apply plugin: 'kotlin-android-extensions'
-
-android {
-    compileSdkVersion 24
-    buildToolsVersion "24.0.0"
-    defaultConfig {
-        applicationId "org.gradle.smoketest.kotlin.android"
-        minSdkVersion 16
-        targetSdkVersion 24
-        versionCode 1
-        versionName "1.0"
-        testInstrumentationRunner "android.support.test.runner.AndroidJUnitRunner"
-    }
-    buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android.txt'), 'proguard-rules.pro'
-        }
-    }
-    sourceSets {
-        main.java.srcDirs += 'src/main/kotlin'
-        test.java.srcDirs += 'src/test/kotlin'
-        androidTest.java.srcDirs += 'src/androidTest/kotlin'
-    }
-    testOptions {
-        unitTests.returnDefaultValues = true
-    }
-}
-
-apply plugin: 'jacoco'
-
-project.afterEvaluate {
-    // Grab all build types and product flavors
-    def buildTypes = android.buildTypes.collect { type ->
-        type.name
-    }
-    def productFlavors = android.productFlavors.collect { flavor ->
-        flavor.name
-    }
-    // When no product flavors defined, use empty
-    if (!productFlavors) productFlavors.add('')
-    productFlavors.each { productFlavorName ->
-        buildTypes.each { buildTypeName ->
-            def sourceName, sourcePath
-            if (!productFlavorName) {
-                sourceName = sourcePath = "\${buildTypeName}"
-            } else {
-                sourceName = "\${productFlavorName}\${buildTypeName.capitalize()}"
-                sourcePath = "\${productFlavorName}/\${buildTypeName}"
-            }
-            def testTaskName = "test\${sourceName.capitalize()}UnitTest"
-            // Create coverage task of form 'testFlavorTypeCoverage' depending on 'testFlavorTypeUnitTest'
-            task "\${testTaskName}Coverage" (type:JacocoReport, dependsOn: "\$testTaskName") {
-                group = "Reporting"
-                description = "Generate Jacoco coverage reports on the \${sourceName.capitalize()} build."
-                classDirectories = fileTree(
-                        dir: "\${project.buildDir}/intermediates/classes/\${sourcePath}",
-                        excludes: [
-                                '**/R.class',
-                                '**/R\$*.class',
-                                '**/*\$ViewInjector*.*',
-                                '**/*\$ViewBinder*.*',
-                                '**/BuildConfig.*',
-                                '**/Manifest*.*'
-                        ]
-                )
-                def coverageSourceDirs = [
-                        "src/main/kotlin",
-                        "src/\$productFlavorName/kotlin",
-                        "src/\$buildTypeName/kotlin"
-                ]
-                additionalSourceDirs = files(coverageSourceDirs)
-                sourceDirectories = files(coverageSourceDirs)
-                executionData = files("\${project.buildDir}/jacoco/\${testTaskName}.exec")
-                reports {
-                    xml.enabled = true
-                    html.enabled = true
-                }
-            }
-        }
-    }
-}
-
-dependencies {
-    compile 'org.jetbrains.kotlin:kotlin-stdlib:1.0.3'
-    compile 'org.jetbrains.anko:anko-common:0.9'
-
-    testCompile 'junit:junit:4.12'
-}
-""".stripIndent()
-
-        file("src/main/AndroidManifest.xml") << """<manifest package="me.egorand.jacocokotlinissue"
-          xmlns:android="http://schemas.android.com/apk/res/android">
-
-    <application
-        android:allowBackup="true"
-        android:supportsRtl="true">
-    </application>
-</manifest>"""
-
-        file("src/main/kotlin/org/gradle/smoketest/kotlin/android/StringPrinterFragment.kt") << """
-package org.gradle.smoketest.kotlin.android
-
-import android.app.Fragment
-import android.util.Log
-import org.jetbrains.anko.runOnUiThread
-
-class StringPrinterFragment : Fragment() {
-
-    fun printStringLength(str: String) = runOnUiThread {
-        Log.d("StringPrinter", str.length.toString())
-    }
-}"""
-
-        file("src/test/kotlin/org/gradle/smoketest/kotlin/android/StringPrinterTest.kt") << """
-package org.gradle.smoketest.kotlin.android
-
-import org.junit.Before
-import org.junit.Test
-
-class StringPrinterTest {
-
-    lateinit private var stringPrinter: StringPrinterFragment
-
-    @Before fun setUp() {
-        stringPrinter = StringPrinterFragment()
-    }
-
-    @Test fun shouldPrintStringLength() {
-        stringPrinter.printStringLength("Hello world!")
-    }
-}"""
+        useSample("android-kotlin-example")
+        replaceVariablesInBuildFile(
+            kotlinVersion: kotlinVersion,
+            androidPluginVersion: androidPluginVersion)
 
         when:
         def build = runner('clean', 'testDebugUnitTestCoverage').build()
@@ -232,5 +47,4 @@ class StringPrinterTest {
         then:
         build.task(':testDebugUnitTestCoverage').outcome == SUCCESS
     }
-
 }
