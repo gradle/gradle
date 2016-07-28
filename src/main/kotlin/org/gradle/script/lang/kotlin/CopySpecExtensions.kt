71d488d7eb8f8f9fafd97e9c2385e5ad7d5105c3
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2016-07-28 14:21:05 -0300

    Bump Gradle dependency to recent nightly
    To get the latest changes to the CopySpec API and more.

--- src/main/kotlin/org/gradle/script/lang/kotlin/CopySpecExtensions.kt
+++ src/main/kotlin/org/gradle/script/lang/kotlin/CopySpecExtensions.kt
@@ -21,12 +21,6 @@ import org.gradle.api.file.CopySpec
 import java.io.FilterReader
 
 // Interim extensions until those methods are added to CopySpec
-fun CopySpec.from(path: Any, setup: (CopySpec) -> Unit): CopySpec =
-    from(path).apply { setup(this) }
-
-fun CopySpec.into(path: Any, setup: (CopySpec) -> Unit): CopySpec =
-    into(path).apply { setup(this) }
-
 inline fun <reified T : FilterReader> CopySpec.filter(vararg properties: Pair<String, *>) =
     filter(mapOf(*properties), T::class.java)
 
