556ac76130fd2bd4319ae0f83ff62d346be2fd89
justin.gardner <justin.gardner@workday.com>
2016-08-03 10:13:07 +0100

    Gradle code convention -- remove semi-colons, place issue name on test annotation.

--- subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/JUnitJdkNavigationTest.groovy
+++ subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/JUnitJdkNavigationTest.groovy
@@ -2,25 +2,27 @@ package org.gradle.testing.junit
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.DefaultTestExecutionResult
+import spock.lang.Issue
 
+@Issue("GRADLE-1682")
 class JUnitJdkNavigationTest extends AbstractIntegrationSpec {
 
 
     def setup() {
-        executer.noExtraLogging();
+        executer.noExtraLogging()
     }
 
-    def doesntTraverseThroughJdkClasses() {
+    def "should not navigate through JDK classes"() {
         given:
         buildFile
 
         when:
-        executer.withTasks('test').run();
+        executer.withTasks('test').run()
 
         then:
-        DefaultTestExecutionResult result = new DefaultTestExecutionResult(testDirectory);
-        result.assertTestClassesExecuted('org.gradle.Test');
-        result.testClass('org.gradle.Test').assertTestPassed('shouldPass');
+        DefaultTestExecutionResult result = new DefaultTestExecutionResult(testDirectory)
+        result.assertTestClassesExecuted('org.gradle.Test')
+        result.testClass('org.gradle.Test').assertTestPassed('shouldPass')
     }
 
 }
