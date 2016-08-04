c4327be086d272b5ea7201b74e592cdefa2ae992
Luke Daley <luke@gradle.com>
2016-08-04 19:32:01 +1000

    Make tests more realistic by doing some “work” other than writing console output.

--- subprojects/build-scan-performance/src/templates/with-verbose-junit/Test.java
+++ subprojects/build-scan-performance/src/templates/with-verbose-junit/Test.java
@@ -11,6 +11,9 @@ public class ${testClassName} {
         for (int i = 0; i < 500; i++) {
             System.out.println("Some test output from ${testClassName}.testOne - " + i);
             System.err.println("Some test error  from ${testClassName}.testOne - " + i);
+            if (i % 50 == 0) {
+                Thread.sleep(1);
+            }
         }
         assertEquals(production.getProperty(), "value");
     }
@@ -20,6 +23,9 @@ public class ${testClassName} {
         for (int i = 0; i < 500; i++) {
             System.out.println("Some test output from ${testClassName}.testTwo - " + i);
             System.err.println("Some test error  from ${testClassName}.testTwo - " + i);
+            if (i % 50 == 0) {
+                Thread.sleep(1);
+            }
         }
         String expected = <%= binding.hasVariable("halfTestsFail") && binding.halfTestsFail ? "\"foo\"" : "\"value\"" %>;
         assertEquals(production.getProperty(), expected);
