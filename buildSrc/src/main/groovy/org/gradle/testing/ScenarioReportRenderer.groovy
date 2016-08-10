9a5c0866f2b50ba3bc712f57e2752fa6b6659ef3
Lari Hotari <lari.hotari@gradle.com>
2016-08-10 17:26:53 +0300

    Add statusText and direct link to junit html report

--- buildSrc/src/main/groovy/org/gradle/testing/ScenarioReportRenderer.groovy
+++ buildSrc/src/main/groovy/org/gradle/testing/ScenarioReportRenderer.groovy
@@ -15,14 +15,20 @@ class ScenarioReportRenderer {
                         th("Status")
                         th("")
                         th("")
+                        th("")
+                        th("")
                     }
                 }
                 finishedBuilds.each { build ->
                     tr {
                         td(this.getScenarioId(build))
                         td(build.@status)
+                        td(build.statusText.text())
                         td {
                             a(href : build.@webUrl, "Test Report")
+                        }
+                        td {
+                            a(href : "https://builds.gradle.org/repository/download/${build.@buildTypeId}/${build.@id}:id/reports/performance/performanceScenario/index.html", "Test Summary")
                         }
                         td {
                             a(href : "report/tests/${this.getScenarioId(build).replaceAll(" ", "-")}.html", "Performance Report")
