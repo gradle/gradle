cee51770824513ddf9f82a1ba31904be691160e4
Daz DeBoer <daz@gradle.com>
2016-08-22 15:11:33 -0600

    Explicit delegate task type for included build task execution

---
+++ subprojects/composite-builds/src/integTest/groovy/org/gradle/integtests/composite/CompositeBuildTaskDelegationIntegrationTest.groovy
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.integtests.composite
+
+import org.gradle.integtests.fixtures.build.BuildTestFile
+
+/**
+ * Tests for resolving dependency artifacts with substitution within a composite build.
+ */
+class CompositeBuildTaskDelegationIntegrationTest extends AbstractCompositeBuildIntegrationTest {
+    BuildTestFile buildB
+
+    def setup() {
+        buildB = multiProjectBuild("buildB", ['b1', 'b2']) {
+            buildFile << """
+                allprojects {
+                    task logProject << {
+                        println "Executing build '" + project.rootProject.name + "' project '" + project.path + "' task '" + path + "'"
+                    }
+                }
+"""
+        }
+        includedBuilds << buildB
+    }
+
+    def "can delegate to task in root project of included build"() {
+        when:
+        buildA.buildFile << """
+    task delegate(type: org.gradle.api.internal.artifacts.ivyservice.projectmodule.CompositeBuildTaskDelegate) {
+        build = 'buildB'
+        task = ':logProject'
+    }
+"""
+
+        execute(buildA, ":delegate")
+
+        then:
+        output.contains("Executing build 'buildB' project ':' task ':logProject'")
+    }
+
+    def "can delegate to task in subproject of included build"() {
+        when:
+        buildA.buildFile << """
+    task delegate(type: org.gradle.api.internal.artifacts.ivyservice.projectmodule.CompositeBuildTaskDelegate) {
+        build = 'buildB'
+        task = ':b1:logProject'
+    }
+"""
+
+        execute(buildA, ":delegate")
+
+        then:
+        output.contains("Executing build 'buildB' project ':b1' task ':b1:logProject'")
+    }
+}
---
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/projectmodule/CompositeBuildTaskDelegate.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.artifacts.ivyservice.projectmodule;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.artifacts.component.ProjectComponentIdentifier;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.initialization.IncludedBuildExecuter;
+import org.gradle.internal.component.local.model.DefaultProjectComponentIdentifier;
+
+import java.util.Collections;
+
+public class CompositeBuildTaskDelegate extends DefaultTask {
+    private String build;
+    private String task;
+
+    @Input
+    public String getBuild() {
+        return build;
+    }
+
+    public void setBuild(String build) {
+        this.build = build;
+    }
+
+    @Input
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    @TaskAction
+    public void executeTaskInOtherBuild() {
+        IncludedBuildExecuter builder = getServices().get(IncludedBuildExecuter.class);
+        ProjectComponentIdentifier id = DefaultProjectComponentIdentifier.newId(build + "::");
+        builder.execute(id, Collections.singleton(task));
+    }
+}
