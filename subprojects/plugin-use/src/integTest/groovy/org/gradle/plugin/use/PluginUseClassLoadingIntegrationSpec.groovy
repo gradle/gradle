27b43a9f19365ab7b428f41b8cf98a61353f6179
Gary Hale <gary@gradle.com>
2016-08-23 18:02:46 -0400

    Adding LeaksFileHandles back for test that puts gradle user home in int test dir
    +review REVIEW-6182

--- subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/use/PluginUseClassLoadingIntegrationSpec.groovy
+++ subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/use/PluginUseClassLoadingIntegrationSpec.groovy
@@ -20,10 +20,12 @@ import org.gradle.api.Project
 import org.gradle.api.specs.AndSpec
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.plugin.use.resolve.service.PluginResolutionServiceTestServer
+import org.gradle.test.fixtures.file.LeaksFileHandles
 import org.gradle.test.fixtures.plugin.PluginBuilder
 import org.junit.Rule
 import spock.lang.Issue
 
+@LeaksFileHandles
 class PluginUseClassLoadingIntegrationSpec extends AbstractIntegrationSpec {
 
     public static final String PLUGIN_ID = "org.myplugin"
