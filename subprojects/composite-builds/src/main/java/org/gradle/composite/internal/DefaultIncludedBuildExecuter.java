8ea3eaadc594f41a1f4c0a075a9b427e38b052f5
Daz DeBoer <daz@gradle.com>
2016-08-25 09:13:11 -0600

    Revert fat locking of `IncludedBuildExecuter`
    This fat lock was introduced in order to avoid thread-safety issues when
    executing a composite build with `--parallel`. However this change
    introduced a deadlock condition due to the lock being too coarse-
    grained.
    
    Reverting this change should fix the deadlock, however the potential for
    a 'dependency cycle' being reported when executing composite builds with
    `--parallel` remains, I think.

--- subprojects/composite-builds/src/main/java/org/gradle/composite/internal/DefaultIncludedBuildExecuter.java
+++ subprojects/composite-builds/src/main/java/org/gradle/composite/internal/DefaultIncludedBuildExecuter.java
@@ -43,9 +43,8 @@ class DefaultIncludedBuildExecuter implements IncludedBuildExecuter {
         this.includedBuilds = includedBuilds;
     }
 
-    // TODO Allow parallel execution of different included builds
     @Override
-    public synchronized void execute(ProjectComponentIdentifier buildIdentifier, Iterable<String> taskNames) {
+    public void execute(ProjectComponentIdentifier buildIdentifier, Iterable<String> taskNames) {
         String buildName = getBuildName(buildIdentifier);
         buildStarted(buildName);
         try {
@@ -62,14 +61,16 @@ class DefaultIncludedBuildExecuter implements IncludedBuildExecuter {
         return buildIdentifier.getProjectPath().split("::", 2)[0];
     }
 
-    private void buildStarted(String build) {
+    private synchronized void buildStarted(String build) {
+        // Ensure that a particular build is never executing concurrently
+        // TODO:DAZ We might need to hold a lock per-build for the parallel build case
         if (!executingBuilds.add(build)) {
             ProjectComponentSelector selector = DefaultProjectComponentSelector.newSelector(build + "::");
             throw new ModuleVersionResolveException(selector, "Dependency cycle including project " + selector.getProjectPath());
         }
     }
 
-    private void buildCompleted(String project) {
+    private synchronized void buildCompleted(String project) {
         executingBuilds.remove(project);
     }
 
