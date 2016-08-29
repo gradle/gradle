e2006396ed80bd0e38f6b44e6b2686e0e826d8ed
Lóránt Pintér <lorant@gradle.com>
2016-08-29 12:00:17 +0200

    Simplify test code
    +review REVIEW-6115

--- subprojects/core/src/test/groovy/org/gradle/util/DiffUtilTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/util/DiffUtilTest.groovy
@@ -16,45 +16,18 @@
 package org.gradle.util
 
 import org.gradle.internal.concurrent.CompositeStoppable
-import org.gradle.test.fixtures.file.TestFile
-import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider
 import org.jmock.integration.junit4.JMock
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.objectweb.asm.AnnotationVisitor
-import org.objectweb.asm.ClassWriter
-import org.objectweb.asm.FieldVisitor
-import org.objectweb.asm.Label
-import org.objectweb.asm.MethodVisitor
-import org.objectweb.asm.Type
-import org.objectweb.asm.tree.ClassNode
-import org.objectweb.asm.tree.MethodNode
 
 import static org.hamcrest.Matchers.anything
 import static org.hamcrest.Matchers.equalTo
 import static org.junit.Assert.*
-import static org.objectweb.asm.Opcodes.*
 
 @RunWith(JMock.class)
 class DiffUtilTest {
-    @Rule
-    private final TestNameTestDirectoryProvider tmpDir = new TestNameTestDirectoryProvider()
-    private TestFile distDir
 
     private final JUnit4GroovyMockery context = new JUnit4GroovyMockery()
-
-    @Before
-    public void setup() {
-        distDir = tmpDir.createDir("dist")
-    }
-
-    @After
-    public void cleanup() {
-        distDir.deleteDir()
-    }
 
     @Test
     public void notifiesListenerOfElementAddedToSet() {
@@ -179,14 +152,11 @@ class DiffUtilTest {
     // We may want to change this behavior in the future, so don't be afraid to remove this test.
     @Test
     public void differentClassLoadersNotEqual() {
-        TestFile jar = distDir.file("testclass-1.2.jar")
-        createJarFile(jar)
-
         List<Closeable> loaders = []
 
         try {
-            Class<?> clazz1 = loadClassFromJar(jar, "org.gradle.MyClass", loaders)
-            Class<?> clazz2 = loadClassFromJar(jar, "org.gradle.MyClass", loaders)
+            Class<?> clazz1 = createClass(loaders)
+            Class<?> clazz2 = createClass(loaders)
 
             assertNotSame(clazz1, clazz2)
 
@@ -200,16 +170,22 @@ class DiffUtilTest {
         }
     }
 
+    Class<?> createClass(List<Closeable> loaders) {
+        def loader = new GroovyClassLoader(getClass().classLoader)
+        loaders.add loader
+        loader.parseClass """
+            class MyClass {
+            }
+        """
+    }
+
     @Test
     public void enumsWithDifferentClassLoadersEqual() {
-        TestFile jar = distDir.file("testclass-1.2.jar")
-        createJarFile(jar)
-
         List<Closeable> loaders = []
 
         try {
-            Class<?> clazz1 = loadClassFromJar(jar, "org.gradle.MyEnum", loaders)
-            Class<?> clazz2 = loadClassFromJar(jar, "org.gradle.MyEnum", loaders)
+            Class<?> clazz1 = createEnumClass(loaders)
+            Class<?> clazz2 = createEnumClass(loaders)
 
             assertNotSame(clazz1, clazz2)
 
@@ -230,181 +206,14 @@ class DiffUtilTest {
         }
     }
 
-    private void createJarFile(TestFile jar) {
-        TestFile contents = tmpDir.createDir('contents')
-        TestFile classFile = contents.createFile('org/gradle/MyClass.class')
-        TestFile enumFile = contents.createFile('org/gradle/MyEnum.class')
-
-        ClassNode myClass = createMyClass()
-        ClassNode myEnum = createMyEnum()
-
-        writeClassNode(myClass, classFile)
-        writeClassNode(myEnum, enumFile)
-
-        contents.zipTo(jar)
-    }
-
-    private static void writeClassNode(ClassNode classNode, TestFile classFile) {
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES)
-        classNode.accept(cw)
-
-        classFile.withDataOutputStream {
-            it.write(cw.toByteArray())
-        }
-    }
-
-    private Class loadClassFromJar(TestFile jar, String className, Collection<ClassLoader> loaders) {
-        // This is to prevent the jar file being held open
-        URL url = new URL("jar:file://valid_jar_url_syntax.jar!/")
-        URLConnection urlConnection = url.openConnection()
-        def original = urlConnection.getDefaultUseCaches()
-        urlConnection.setDefaultUseCaches(false)
-
-        try {
-            URL[] urls = [new URL("jar:${jar.toURI().toURL()}!/")] as URL[]
-            URLClassLoader ucl = new URLClassLoader(urls)
-            if (ucl instanceof Closeable) {
-                loaders << ucl
+    Class<?> createEnumClass(List<Closeable> loaders) {
+        def loader = new GroovyClassLoader(getClass().classLoader)
+        loaders.add loader
+        loader.parseClass """
+            enum MuEnum {
+                OTTER,
+                SEAL
             }
-            Class.forName(className, true, ucl)
-        } finally {
-            urlConnection.setDefaultUseCaches(original)
-        }
-    }
-
-    private static ClassNode createMyClass() {
-        ClassNode classNode = new ClassNode()
-        classNode.version = V1_6
-        classNode.access = ACC_PUBLIC
-        classNode.name = 'org/gradle/MyClass'
-        classNode.superName = 'java/lang/Object'
-
-        // Default constructor for myClass.
-        MethodNode constructor = new MethodNode(
-            ACC_PUBLIC,
-            "<init>",
-            "()V",
-            null,
-            null);
-
-        constructor.visitCode();
-        constructor.visitVarInsn(ALOAD, 0);
-        constructor.visitMethodInsn(INVOKESPECIAL, classNode.superName, "<init>", "()V");
-        constructor.visitInsn(RETURN);
-        constructor.visitMaxs(1, 1);
-        constructor.visitEnd();
-
-        constructor.accept(classNode)
-        return classNode
-    }
-
-    private static ClassNode createMyEnum() {
-        // Enums have some lengthy bytecode.
-
-        ClassNode classNode = new ClassNode()
-        FieldVisitor fv;
-        MethodVisitor mv;
-        AnnotationVisitor av0;
-
-        classNode.visit(V1_5, ACC_PUBLIC + ACC_FINAL + ACC_SUPER + ACC_ENUM, "org/gradle/MyEnum", "Ljava/lang/Enum<Lorg/gradle/MyEnum;>;", "java/lang/Enum", null);
-
-        fv = classNode.visitField(ACC_PUBLIC + ACC_FINAL + ACC_STATIC + ACC_ENUM, "OTTER", "Lorg/gradle/MyEnum;", null, null);
-        fv.visitEnd();
-        fv = classNode.visitField(ACC_PUBLIC + ACC_FINAL + ACC_STATIC + ACC_ENUM, "SEAL", "Lorg/gradle/MyEnum;", null, null);
-        fv.visitEnd();
-
-        fv = classNode.visitField(ACC_PRIVATE + ACC_FINAL + ACC_STATIC + ACC_SYNTHETIC, 'ENUM$VALUES', "[Lorg/gradle/MyEnum;", null, null);
-        fv.visitEnd();
-
-        mv = classNode.visitMethod(ACC_STATIC, "<clinit>", "()V", null, null);
-        mv.visitCode();
-        Label l0 = new Label();
-        mv.visitLabel(l0);
-        mv.visitLineNumber(2, l0);
-        mv.visitTypeInsn(NEW, "org/gradle/MyEnum");
-        mv.visitInsn(DUP);
-        mv.visitLdcInsn("OTTER");
-        mv.visitInsn(ICONST_0);
-        mv.visitMethodInsn(INVOKESPECIAL, "org/gradle/MyEnum", "<init>", "(Ljava/lang/String;I)V");
-        mv.visitFieldInsn(PUTSTATIC, "org/gradle/MyEnum", "OTTER", "Lorg/gradle/MyEnum;");
-        mv.visitTypeInsn(NEW, "org/gradle/MyEnum");
-        mv.visitInsn(DUP);
-        mv.visitLdcInsn("SEAL");
-        mv.visitInsn(ICONST_1);
-        mv.visitMethodInsn(INVOKESPECIAL, "org/gradle/MyEnum", "<init>", "(Ljava/lang/String;I)V");
-        mv.visitFieldInsn(PUTSTATIC, "org/gradle/MyEnum", "SEAL", "Lorg/gradle/MyEnum;");
-        Label l1 = new Label();
-        mv.visitLabel(l1);
-        mv.visitLineNumber(1, l1);
-        mv.visitInsn(ICONST_2);
-        mv.visitTypeInsn(ANEWARRAY, "org/gradle/MyEnum");
-        mv.visitInsn(DUP);
-        mv.visitInsn(ICONST_0);
-        mv.visitFieldInsn(GETSTATIC, "org/gradle/MyEnum", "OTTER", "Lorg/gradle/MyEnum;");
-        mv.visitInsn(AASTORE);
-        mv.visitInsn(DUP);
-        mv.visitInsn(ICONST_1);
-        mv.visitFieldInsn(GETSTATIC, "org/gradle/MyEnum", "SEAL", "Lorg/gradle/MyEnum;");
-        mv.visitInsn(AASTORE);
-        mv.visitFieldInsn(PUTSTATIC, "org/gradle/MyEnum", 'ENUM$VALUES', "[Lorg/gradle/MyEnum;");
-        mv.visitInsn(RETURN);
-        mv.visitMaxs(4, 0);
-        mv.visitEnd();
-
-        mv = classNode.visitMethod(ACC_PRIVATE, "<init>", "(Ljava/lang/String;I)V", null, null);
-        mv.visitCode();
-        l0 = new Label();
-        mv.visitLabel(l0);
-        mv.visitLineNumber(1, l0);
-        mv.visitVarInsn(ALOAD, 0);
-        mv.visitVarInsn(ALOAD, 1);
-        mv.visitVarInsn(ILOAD, 2);
-        mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Enum", "<init>", "(Ljava/lang/String;I)V");
-        mv.visitInsn(RETURN);
-        l1 = new Label();
-        mv.visitLabel(l1);
-        mv.visitLocalVariable("this", "Lorg/gradle/MyEnum;", null, l0, l1, 0);
-        mv.visitMaxs(3, 3);
-        mv.visitEnd();
-
-        mv = classNode.visitMethod(ACC_PUBLIC + ACC_STATIC, "values", "()[Lorg/gradle/MyEnum;", null, null);
-        mv.visitCode();
-        l0 = new Label();
-        mv.visitLabel(l0);
-        mv.visitLineNumber(1, l0);
-        mv.visitFieldInsn(GETSTATIC, "org/gradle/MyEnum", 'ENUM$VALUES', "[Lorg/gradle/MyEnum;");
-        mv.visitInsn(DUP);
-        mv.visitVarInsn(ASTORE, 0);
-        mv.visitInsn(ICONST_0);
-        mv.visitVarInsn(ALOAD, 0);
-        mv.visitInsn(ARRAYLENGTH);
-        mv.visitInsn(DUP);
-        mv.visitVarInsn(ISTORE, 1);
-        mv.visitTypeInsn(ANEWARRAY, "org/gradle/MyEnum");
-        mv.visitInsn(DUP);
-        mv.visitVarInsn(ASTORE, 2);
-        mv.visitInsn(ICONST_0);
-        mv.visitVarInsn(ILOAD, 1);
-        mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "arraycopy", "(Ljava/lang/Object;ILjava/lang/Object;II)V");
-        mv.visitVarInsn(ALOAD, 2);
-        mv.visitInsn(ARETURN);
-        mv.visitMaxs(5, 3);
-        mv.visitEnd();
-
-        mv = classNode.visitMethod(ACC_PUBLIC + ACC_STATIC, "valueOf", "(Ljava/lang/String;)Lorg/gradle/MyEnum;", null, null);
-        mv.visitCode();
-        l0 = new Label();
-        mv.visitLabel(l0);
-        mv.visitLineNumber(1, l0);
-        mv.visitLdcInsn(Type.getType("Lorg/gradle/MyEnum;"));
-        mv.visitVarInsn(ALOAD, 0);
-        mv.visitMethodInsn(INVOKESTATIC, "java/lang/Enum", "valueOf", "(Ljava/lang/Class;Ljava/lang/String;)Ljava/lang/Enum;");
-        mv.visitTypeInsn(CHECKCAST, "org/gradle/MyEnum");
-        mv.visitInsn(ARETURN);
-        mv.visitMaxs(2, 1);
-        mv.visitEnd();
-
-        classNode.visitEnd();
-        return classNode
+        """
     }
 }
