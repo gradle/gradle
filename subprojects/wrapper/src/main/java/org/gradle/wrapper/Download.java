b991ab18f1f27e403ec219f9968ac98b2a1d4c9c
Paul Merlin <paul@gradle.com>
2016-09-05 17:09:38 -0700

    Fix handling of Wrapper HTTP Basic Auth on Java 5
    IOException does not have a (String, Exception) CTOR on Java 5.
    Use RuntimeException instead.
    
    +review REVIEW-6222

--- subprojects/wrapper/src/main/java/org/gradle/wrapper/Download.java
+++ subprojects/wrapper/src/main/java/org/gradle/wrapper/Download.java
@@ -122,7 +122,7 @@ public class Download implements IDownload {
             Method base64Encode = datatypeConverter.getMethod("printBase64Binary", byte[].class);
             return (String) base64Encode.invoke(null, new Object[]{userInfo.getBytes("UTF-8")});
         } catch (Exception ex) {
-            throw new IOException("Downloading Gradle distributions with HTTP Basic Authentication is not supported on your JVM.", ex);
+            throw new RuntimeException("Downloading Gradle distributions with HTTP Basic Authentication is not supported on your JVM.", ex);
         }
     }
 
