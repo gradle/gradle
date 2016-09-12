947cc32c288633223909269b0c111667599cfb66
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2016-09-12 17:48:10 -0300

    Revert accidental `hello-kotlin` sample changes

--- samples/hello-kotlin/build.gradle.kts
+++ samples/hello-kotlin/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath(kotlinModule("gradle-plugin", version = "1.0.3"))
+        classpath(kotlinModule("gradle-plugin"))
     }
 }
 
@@ -23,5 +23,5 @@ repositories {
 }
 
 dependencies {
-    compile(kotlinModule("stdlib", version = "1.0.3"))
+    compile(kotlinModule("stdlib"))
 }
