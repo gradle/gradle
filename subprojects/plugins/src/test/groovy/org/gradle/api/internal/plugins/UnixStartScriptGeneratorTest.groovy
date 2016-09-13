9cb61993f4c0a6c2547078699310035f958821fb
Jendrik Johannes <jendrik@gradle.com>
2016-09-13 16:16:59 +0200

    Unix Start Script: do not escape VM options which are read from properties (and are therefore already escaped strings)
    +review REVIEW-6235

--- subprojects/plugins/src/test/groovy/org/gradle/api/internal/plugins/UnixStartScriptGeneratorTest.groovy
+++ subprojects/plugins/src/test/groovy/org/gradle/api/internal/plugins/UnixStartScriptGeneratorTest.groovy
@@ -46,7 +46,7 @@ class UnixStartScriptGeneratorTest extends Specification {
         generator.generateScript(details, destination)
 
         then:
-        destination.toString().split(TextUtil.unixLineSeparator).length == 185
+        destination.toString().split(TextUtil.unixLineSeparator).length == 171
     }
 
     def "defaultJvmOpts is expanded properly in unix script"() {
