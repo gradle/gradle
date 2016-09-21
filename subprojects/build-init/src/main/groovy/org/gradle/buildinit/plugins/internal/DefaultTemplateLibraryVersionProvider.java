de74e41ba9ee2d4574888858c34cc096db51fea4
Paul Merlin <paul@gradle.com>
2016-09-20 20:10:49 -0700

    Revert "Remove temporary patch for broken TestNG 6.9.13"
    This reverts commit 3aaab7db09e801d66e66029a7c0719fe24084d39.
    
    CI still has the dynamic versions lookup result in cache.
    Will resubmit later.

--- subprojects/build-init/src/main/groovy/org/gradle/buildinit/plugins/internal/DefaultTemplateLibraryVersionProvider.java
+++ subprojects/build-init/src/main/groovy/org/gradle/buildinit/plugins/internal/DefaultTemplateLibraryVersionProvider.java
@@ -16,6 +16,7 @@
 package org.gradle.buildinit.plugins.internal;
 
 import com.google.common.base.Throwables;
+import org.gradle.api.JavaVersion;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -33,6 +34,10 @@ public class DefaultTemplateLibraryVersionProvider implements TemplateLibraryVer
     }
 
     public String getVersion(String module) {
+        if (module.equals("testng") && !JavaVersion.current().isJava8Compatible()) {
+            // Use the highest version that runs on Java 7
+            return "6.9.12";
+        }
         return libraryVersions.getProperty(module);
     }
 }
