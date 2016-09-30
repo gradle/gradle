67aee67f79d2b54a032264ec72bff4c4d1317e80
Adam Murdoch <adam@gradle.com>
2016-09-30 17:23:31 +1000

    Added some int test coverage for when the artifact cache is used concurrently by multiple builds, including some cross version coverage.

---
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/caching/ConcurrentBuildsCachingCrossVersionIntegrationTest.groovy
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.integtests.resolve.caching
+
+import org.gradle.api.internal.artifacts.ivyservice.DefaultCacheLockingManager
+import org.gradle.integtests.fixtures.IgnoreVersions
+import org.gradle.integtests.resolve.artifactreuse.AbstractCacheReuseCrossVersionIntegrationTest
+import org.gradle.test.fixtures.server.http.CyclicBarrierHttpServer
+import org.junit.Rule
+
+@IgnoreVersions({ it.artifactCacheLayoutVersion == DefaultCacheLockingManager.CACHE_LAYOUT_VERSION })
+class ConcurrentBuildsCachingCrossVersionIntegrationTest extends AbstractCacheReuseCrossVersionIntegrationTest {
+    @Rule CyclicBarrierHttpServer server1 = new CyclicBarrierHttpServer()
+    @Rule CyclicBarrierHttpServer server2 = new CyclicBarrierHttpServer()
+
+    def "can interleave resolution across multiple build processes"() {
+        def mod1 = mavenHttpRepo.module("group1", "module1", "1.0").publish()
+        def mod2 = mavenHttpRepo.module("group1", "module2", "0.99").dependsOn(mod1).publish()
+
+        buildFile << """
+repositories {
+    maven { url "${mavenHttpRepo.uri}" }
+}
+configurations {
+    a
+    b
+}
+dependencies {
+    a "group1:module1:1.0"
+    b "group1:module2:0.99"
+}
+task a {
+    doLast {
+        configurations.a.files
+    }
+}
+task b {
+    doLast {
+        configurations.b.files
+    }
+}
+task block1 {
+    doLast {
+        new URL("$server1.uri").text
+    }
+}
+block1.mustRunAfter a
+b.mustRunAfter block1
+
+task block2 {
+    doLast {
+        new URL("$server2.uri").text
+    }
+}
+block2.mustRunAfter b
+"""
+        expect:
+        // Build 1 should download module 1 and check whether it can reuse module 2 files
+        mod1.pom.expectGet()
+        mod1.artifact.expectGet()
+        mod2.pom.expectHead()
+        mod2.pom.sha1.expectGet()
+        mod2.artifact.expectHead()
+        mod2.artifact.sha1.expectGet()
+
+        // Build 2 should check whether it can reuse module 1 files and download module 2 files
+        mod1.pom.expectHead()
+        mod1.pom.sha1.expectGet()
+        mod1.artifact.expectHead()
+        mod1.artifact.sha1.expectGet()
+        mod2.pom.expectGet()
+        mod2.artifact.expectGet()
+
+        // Start build 1 then wait until it has run task 'a'.
+        def previousExecuter = version(previous)
+        previousExecuter.withTasks("a", "block1", "b")
+        def build1 = previousExecuter.start()
+        server1.waitFor()
+
+        // Start build 2 then wait until it has run both 'a' and 'b'.
+        def currentExecuter = version(current)
+        currentExecuter.withTasks("a", "b", "block2")
+        def build2 = currentExecuter.start()
+        server2.waitFor()
+
+        // Finish up build 1 and 2
+        server1.release() // finish build 1 while build 2 is still running
+        build1.waitForFinish()
+        server2.release()
+        build2.waitForFinish()
+    }
+}
