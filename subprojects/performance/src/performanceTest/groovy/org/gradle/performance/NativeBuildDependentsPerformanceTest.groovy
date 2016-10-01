1722a015471fd8569ad774dd663666cf7bc46a8b
Lari Hotari <lari.hotari@gradle.com>
2016-10-01 15:40:32 +0300

    Remove references to maxExecutionTimeRegression and maxMemoryRegression
    - removed by https://github.com/gradle/gradle/commit/0add83a8#diff-d321f7aa2466b0064f15244086a07ce1L60

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/NativeBuildDependentsPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/NativeBuildDependentsPerformanceTest.groovy
@@ -17,11 +17,8 @@
 package org.gradle.performance
 
 import org.gradle.performance.categories.NativePerformanceTest
-import org.gradle.performance.measure.DataAmount
 import org.junit.experimental.categories.Category
 import spock.lang.Unroll
-
-import static org.gradle.performance.measure.Duration.millis
 
 @Category(NativePerformanceTest)
 class NativeBuildDependentsPerformanceTest extends AbstractCrossVersionPerformanceTest {
@@ -31,13 +28,10 @@ class NativeBuildDependentsPerformanceTest extends AbstractCrossVersionPerforman
         runner.testId = "build dependents of native project $testProject"
         runner.testProject = testProject
         runner.tasksToRun = [ "$subprojectPath:$taskName" ]
-        runner.maxExecutionTimeRegression = maxExecutionTimeRegression
         runner.targetVersions = ['nightly']
         runner.useDaemon = true
 
         runner.args += ["--parallel", "--max-workers=4"]
-
-        runner.maxMemoryRegression = DataAmount.mbytes(100)
 
         when:
         def result = runner.run()
@@ -46,13 +40,13 @@ class NativeBuildDependentsPerformanceTest extends AbstractCrossVersionPerforman
         result.assertCurrentVersionHasNotRegressed()
 
         where:
-        testProject        | subprojectPath | taskName                | maxExecutionTimeRegression
-        "nativeDependents"     | ':libA0' | 'buildDependentsLibA00' | millis(1000) // has the most dependent components
-        "nativeDependents"     | ':libA6' | 'buildDependentsLibA60' | millis(1000) // has a few dependent components
-        "nativeDependents"     | ':exeA0' | 'buildDependentsExeA00' | millis(1000) // has no dependent components
+        testProject        | subprojectPath | taskName
+        "nativeDependents" | ':libA0'       | 'buildDependentsLibA00'
+        "nativeDependents" | ':libA6'       | 'buildDependentsLibA60'
+        "nativeDependents" | ':exeA0'       | 'buildDependentsExeA00'
         // TODO: Re-enable these once our memory troubles are over.
-        // "nativeDependentsDeep" | ':libA0' | 'buildDependentsLibA00' | millis(1000) // has the most dependent components
-        // "nativeDependentsDeep" | ':exeA0' | 'buildDependentsExeA00' | millis(1000) // has no dependent components
+        // "nativeDependentsDeep" | ':libA0' | 'buildDependentsLibA00'
+        // "nativeDependentsDeep" | ':exeA0' | 'buildDependentsExeA00'
     }
 
     @Unroll("Project '#testProject' measuring build dependents report speed for #subprojectPath")
@@ -61,13 +55,10 @@ class NativeBuildDependentsPerformanceTest extends AbstractCrossVersionPerforman
         runner.testId = "build dependents of native project $testProject"
         runner.testProject = testProject
         runner.tasksToRun = [ "$subprojectPath:dependentComponents" ]
-        runner.maxExecutionTimeRegression = maxExecutionTimeRegression
         runner.targetVersions = ['nightly']
         runner.useDaemon = true
 
         runner.args += ["--parallel", "--max-workers=4"]
-
-        runner.maxMemoryRegression = DataAmount.mbytes(100)
 
         when:
         def result = runner.run()
@@ -76,12 +67,12 @@ class NativeBuildDependentsPerformanceTest extends AbstractCrossVersionPerforman
         result.assertCurrentVersionHasNotRegressed()
 
         where:
-        testProject            | subprojectPath | maxExecutionTimeRegression
-        "nativeDependents"     | ':libA0'       | millis(1000) // has the most dependent components
-        "nativeDependents"     | ':libA6'       | millis(1000) // has a few dependent components
-        "nativeDependents"     | ':exeA0'       | millis(1000) // has no dependent components
+        testProject        | subprojectPath
+        "nativeDependents" | ':libA0'
+        "nativeDependents" | ':libA6'
+        "nativeDependents" | ':exeA0'
         // TODO: Re-enable these once our memory troubles are over.
-        // "nativeDependentsDeep" | ':libA0'       | millis(1000) // has the most dependent components
-        // "nativeDependentsDeep" | ':exeA0'       | millis(1000) // has no dependent components
+        // "nativeDependentsDeep" | ':libA0'
+        // "nativeDependentsDeep" | ':exeA0'
     }
 }
