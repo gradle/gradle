8348a7a01dbcfedb504ca7bb9fa0f67a3c4cc3ef
Cedric Champeau <cedric@gradle.com>
2016-10-10 13:29:13 +0200

    Restore commented out test

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/DependencyResolutionPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/DependencyResolutionPerformanceTest.groovy
@@ -44,7 +44,7 @@ class DependencyResolutionPerformanceTest extends AbstractCrossVersionPerformanc
         // TODO(pepper): Revert this to 'last' when 3.2 is released
         // The regression was determined acceptable in this discussion:
         // https://issues.gradle.org/browse/GRADLE-1346
-//        "lotDependencies"        | 'local'  | ['3.2-20160915000027+0000']
+        "lotDependencies"        | 'local'  | ['3.2-20160915000027+0000']
         "lotProjectDependencies" | 'local'  | ['3.2-20160915000027+0000']
     }
 }
