fe2c509434f3c701faa207e918b28942ba0769a0
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2016-10-14 12:29:21 -0300

    Introduce GradleApiSpecProvider SPI
    So gradle-script-kotlin can make the `kotlin` package visible from the
    Gradle API ClassLoader.
    
    This commit introduces the required infrastructure but still leaves the
    `kotlin` package hardcoded.
    
    A future gradle-script-kotlin upgrade is needed before removing the
    hardcoded reference to `kotlin`.
    
    See gradle/gradle-script-kotlin#149
    +review REVIEW-6279

--- subprojects/core/src/main/java/org/gradle/initialization/DefaultClassLoaderRegistry.java
+++ subprojects/core/src/main/java/org/gradle/initialization/DefaultClassLoaderRegistry.java
@@ -28,26 +28,29 @@ public class DefaultClassLoaderRegistry implements ClassLoaderRegistry {
 
     public DefaultClassLoaderRegistry(ClassPathRegistry classPathRegistry, ClassLoaderFactory classLoaderFactory, LegacyTypesSupport legacyTypesSupport) {
         ClassLoader runtimeClassLoader = getClass().getClassLoader();
-        this.apiOnlyClassLoader = restrictToGradleApi(runtimeClassLoader, classLoaderFactory);
+        this.apiOnlyClassLoader = restrictToGradleApi(runtimeClassLoader);
         this.pluginsClassLoader = new MixInLegacyTypesClassLoader(runtimeClassLoader, classPathRegistry.getClassPath("GRADLE_EXTENSIONS"), legacyTypesSupport);
-        this.apiAndPluginsClassLoader = restrictToGradleApi(pluginsClassLoader, classLoaderFactory);
+        this.apiAndPluginsClassLoader = restrictToGradleApi(pluginsClassLoader);
     }
 
-    private static ClassLoader restrictToGradleApi(ClassLoader parent, ClassLoaderFactory classLoaderFactory) {
-        FilteringClassLoader.Spec rootSpec = new FilteringClassLoader.Spec();
-        rootSpec.allowPackage("org.gradle");
-        rootSpec.allowResources("META-INF/gradle-plugins");
-        rootSpec.allowPackage("org.apache.tools.ant");
-        rootSpec.allowPackage("groovy");
-        rootSpec.allowPackage("org.codehaus.groovy");
-        rootSpec.allowPackage("groovyjarjarantlr");
-        rootSpec.allowPackage("org.slf4j");
-        rootSpec.allowPackage("org.apache.commons.logging");
-        rootSpec.allowPackage("org.apache.log4j");
-        rootSpec.allowPackage("javax.inject");
-        rootSpec.allowPackage("kotlin");
-        ClassLoader rootClassLoader = new FilteringClassLoader(parent, rootSpec);
-        return new CachingClassLoader(rootClassLoader);
+    private ClassLoader restrictToGradleApi(ClassLoader classLoader) {
+        return restrictTo(apiSpecFor(classLoader), classLoader);
+    }
+
+    private static ClassLoader restrictTo(FilteringClassLoader.Spec spec, ClassLoader parent) {
+        return new CachingClassLoader(new FilteringClassLoader(parent, spec));
+    }
+
+    private static FilteringClassLoader.Spec apiSpecFor(ClassLoader classLoader) {
+        FilteringClassLoader.Spec apiSpec = new FilteringClassLoader.Spec();
+        GradleApiSpecProvider.Spec apiAggregate = new GradleApiSpecAggregator(classLoader).aggregate();
+        for (String resources : apiAggregate.getAllowedResources()) {
+            apiSpec.allowResources(resources);
+        }
+        for (String packageName : apiAggregate.getAllowedPackages()) {
+            apiSpec.allowPackage(packageName);
+        }
+        return apiSpec;
     }
 
     @Override
---
+++ subprojects/core/src/main/java/org/gradle/initialization/GradleApiSpecAggregator.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.initialization;
+
+import org.gradle.internal.reflect.DirectInstantiator;
+import org.gradle.internal.service.DefaultServiceLocator;
+
+import java.util.Collection;
+
+class GradleApiSpecAggregator {
+
+    private final ClassLoader classLoader;
+
+    public GradleApiSpecAggregator(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    public GradleApiSpecProvider.Spec aggregate() {
+        GradleApiSpecProvider.SpecBuilder builder = new GradleApiSpecProvider.SpecBuilder();
+        for (Class<? extends GradleApiSpecProvider> providerClass : providers()) {
+            builder.merge(instantiate(providerClass).get());
+        }
+        return builder.build();
+    }
+
+    private Collection<Class<? extends GradleApiSpecProvider>> providers() {
+        return new DefaultServiceLocator(classLoader).implementationsOf(GradleApiSpecProvider.class);
+    }
+
+    private GradleApiSpecProvider instantiate(Class<? extends GradleApiSpecProvider> providerClass) {
+        return DirectInstantiator.INSTANCE.newInstance(providerClass);
+    }
+}
