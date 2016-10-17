4e60b78b97c44a3afc0991bd9ef35e14b2e4bd25
Lóránt Pintér <lorant@gradle.com>
2016-10-17 13:38:06 +0200

    Allow 2% difference in Jacoco execution data
    +review REVIEW-6301

--- subprojects/jacoco/src/integTest/groovy/org/gradle/testing/jacoco/plugins/JacocoTestRelocationIntegrationTest.groovy
+++ subprojects/jacoco/src/integTest/groovy/org/gradle/testing/jacoco/plugins/JacocoTestRelocationIntegrationTest.groovy
@@ -72,12 +72,12 @@ class JacocoTestRelocationIntegrationTest extends AbstractTaskRelocationIntegrat
         def movedLength = movedBytes.length
         def lengthDiff = Math.abs(originalLength - movedLength)
         def length = Math.min(originalLength, movedLength)
-        println String.format("Original is %d bytes, moved is %d bytes (%.2f%%)", originalLength, movedLength, 100d * lengthDiff / length)
+        println String.format("Original is %d bytes, moved is %d bytes (%.2f%% difference)", originalLength, movedLength, 100d * lengthDiff / length)
 
         def distance = levenshteinDistance(originalBytes, movedBytes)
-        println String.format("Levenshtein distance if %s (%.2f%%)", distance, 100d * distance / length)
-        // We are okay with 0.5% distance
-        if (distance > length * 0.005) {
+        println String.format("Levenshtein distance if %s (%.2f%% difference)", distance, 100d * distance / length)
+        // We are okay with 2% distance
+        if (distance > length * 0.02) {
             // If we had too big a difference we fall back to Groovy reporting it
             def originalAsHex = toHexStrings(originalBytes)
             def movedAsHex = toHexStrings(movedBytes)
