gradle/gradle

commit 1b20ea8d5aa3f5b06e45b244344a38ef212ec8db

Add TimeProvider.currentTimeForDuration and use it
There were many places we were measuring a duration by comparing
`System.currentTimeMillis()` at some start and end times. This is a
known anti-pattern which is subject to incorrect duration measurements
due to how clock skew affects the implementation of that function.

The new `currentTimeForDuration()` method uses `System.nanoTime()` which
is much more reliable for measuring durations within a single thread of
control in a single process.

Replacing our uses of `System.currentTimeMillis()` with
`currentTimeForDuration()` will make Gradle and our tests much more
reliable.

In service of issue #740

subprojects/build-scan-performance/src/testFixtures/groovy/org/gradle/performance/fixture/BuildScanPerformanceTestRunner.groovy
@@ -40,7 +40,7 @@ class BuildScanPerformanceTestRunner extends CrossBuildPerformanceTestRunner {
             versionUnderTest: GradleVersion.current().getVersion(),
             vcsBranch: Git.current().branchName,
             vcsCommits: [Git.current().commitId, pluginCommitSha],
-            startTime: System.currentTimeMillis(),
+            startTime: timeProvider.getCurrentTimeForDuration(),
             channel: determineChannel()
         )
     }

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/AbstractGradleBuildPerformanceTestRunner.groovy
@@ -18,6 +18,8 @@ package org.gradle.performance.fixture
 
 import org.gradle.integtests.fixtures.executer.GradleDistribution
 import org.gradle.integtests.fixtures.executer.UnderDevelopmentGradleDistribution
+import org.gradle.internal.TimeProvider
+import org.gradle.internal.TrueTimeProvider
 import org.gradle.performance.results.DataReporter
 import org.gradle.performance.results.MeasuredOperationList
 import org.gradle.performance.results.PerformanceTestResult
@@ -29,6 +31,7 @@ abstract class AbstractGradleBuildPerformanceTestRunner<R extends PerformanceTes
     final GradleDistribution gradleDistribution = new UnderDevelopmentGradleDistribution()
     final BuildExperimentRunner experimentRunner
     final TestProjectLocator testProjectLocator = new TestProjectLocator()
+    final TimeProvider timeProvider = new TrueTimeProvider()
 
     String testId
     String testGroup
@@ -91,7 +94,7 @@ abstract class AbstractGradleBuildPerformanceTestRunner<R extends PerformanceTes
 
         runAllSpecifications(results)
 
-        results.endTime = System.currentTimeMillis()
+        results.endTime = timeProvider.getCurrentTimeForDuration()
 
         results.assertEveryBuildSucceeds()
         reporter.report(results)

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/CrossBuildPerformanceTestRunner.groovy
@@ -59,7 +59,7 @@ class CrossBuildPerformanceTestRunner extends AbstractGradleBuildPerformanceTest
             versionUnderTest: GradleVersion.current().getVersion(),
             vcsBranch: Git.current().branchName,
             vcsCommits: [Git.current().commitId],
-            startTime: System.currentTimeMillis(),
+            startTime: timeProvider.getCurrentTimeForDuration(),
             channel: determineChannel()
         )
     }

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/CrossVersionPerformanceTestRunner.groovy
@@ -23,6 +23,8 @@ import org.gradle.integtests.fixtures.executer.GradleDistribution
 import org.gradle.integtests.fixtures.executer.GradleExecuterDecorator
 import org.gradle.integtests.fixtures.executer.IntegrationTestBuildContext
 import org.gradle.integtests.fixtures.versions.ReleasedVersionDistributions
+import org.gradle.internal.TimeProvider
+import org.gradle.internal.TrueTimeProvider
 import org.gradle.internal.jvm.Jvm
 import org.gradle.internal.os.OperatingSystem
 import org.gradle.performance.measure.MeasuredOperation
@@ -46,6 +48,7 @@ public class CrossVersionPerformanceTestRunner extends PerformanceTestSpec {
     TestProjectLocator testProjectLocator = new TestProjectLocator()
     final BuildExperimentRunner experimentRunner
     final ReleasedVersionDistributions releases
+    final TimeProvider timeProvider = new TrueTimeProvider()
 
     String testProject
     File workingDir
@@ -99,7 +102,7 @@ public class CrossVersionPerformanceTestRunner extends PerformanceTestSpec {
             versionUnderTest: GradleVersion.current().getVersion(),
             vcsBranch: Git.current().branchName,
             vcsCommits: [Git.current().commitId],
-            startTime: System.currentTimeMillis(),
+            startTime: timeProvider.getCurrentTimeForDuration(),
             channel: ResultsStoreHelper.determineChannel()
         )
 
@@ -112,7 +115,7 @@ public class CrossVersionPerformanceTestRunner extends PerformanceTestSpec {
             runVersion(buildContext.distribution(baselineVersion.version), perVersionWorkingDirectory(baselineVersion.version), baselineVersion.results)
         }
 
-        results.endTime = System.currentTimeMillis()
+        results.endTime = timeProvider.getCurrentTimeForDuration()
 
         results.assertEveryBuildSucceeds()
 

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/GradleVsMavenPerformanceTestRunner.groovy
@@ -90,7 +90,7 @@ class GradleVsMavenPerformanceTestRunner extends AbstractGradleBuildPerformanceT
             versionUnderTest: GradleVersion.current().getVersion(),
             vcsBranch: Git.current().branchName,
             vcsCommits: [Git.current().commitId],
-            startTime: System.currentTimeMillis(),
+            startTime: timeProvider.getCurrentTimeForDuration(),
             channel: determineChannel()
         )
     }

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/AbstractToolingApiCrossVersionPerformanceTest.groovy
@@ -24,6 +24,8 @@ import org.gradle.integtests.fixtures.versions.ReleasedVersionDistributions
 import org.gradle.integtests.tooling.fixture.ToolingApi
 import org.gradle.integtests.tooling.fixture.ToolingApiClasspathProvider
 import org.gradle.integtests.tooling.fixture.ToolingApiDistributionResolver
+import org.gradle.internal.TimeProvider
+import org.gradle.internal.TrueTimeProvider
 import org.gradle.internal.classloader.ClasspathUtil
 import org.gradle.internal.jvm.Jvm
 import org.gradle.internal.os.OperatingSystem
@@ -116,6 +118,7 @@ abstract class AbstractToolingApiCrossVersionPerformanceTest extends Specificati
     }
 
     private class Measurement implements ToolingApiClasspathProvider {
+        private final TimeProvider timeProvider = new TrueTimeProvider();
 
         private CrossVersionPerformanceResults run() {
             def testId = experimentSpec.displayName
@@ -134,7 +137,7 @@ abstract class AbstractToolingApiCrossVersionPerformanceTest extends Specificati
                 versionUnderTest: GradleVersion.current().getVersion(),
                 vcsBranch: Git.current().branchName,
                 vcsCommits: [Git.current().commitId],
-                startTime: System.currentTimeMillis(),
+                startTime: timeProvider.getCurrentTimeForDuration(),
                 tasks: [],
                 args: [],
                 gradleOpts: [],
@@ -168,7 +171,7 @@ abstract class AbstractToolingApiCrossVersionPerformanceTest extends Specificati
                 resolver.stop()
             }
 
-            results.endTime = System.currentTimeMillis();
+            results.endTime = timeProvider.getCurrentTimeForDuration()
 
             results.assertEveryBuildSucceeds()
             resultStore.report(results)

subprojects/base-services/src/main/java/org/gradle/internal/TimeProvider.java
@@ -21,4 +21,5 @@ public interface TimeProvider extends Serializable {
 
     long getCurrentTime();
 
+    long getCurrentTimeForDuration();
 }

subprojects/base-services/src/main/java/org/gradle/internal/TrueTimeProvider.java
@@ -16,10 +16,16 @@
 
 package org.gradle.internal;
 
-public class TrueTimeProvider implements TimeProvider {
+import java.util.concurrent.TimeUnit;
 
+public class TrueTimeProvider implements TimeProvider {
+    @Override
     public long getCurrentTime() {
         return System.currentTimeMillis();
     }
 
+    @Override
+    public long getCurrentTimeForDuration() {
+        return TimeUnit.MILLISECONDS.convert(System.nanoTime(), TimeUnit.NANOSECONDS);
+    }
 }

commit aab83c907ee5c19f1f0613e527373ebc7c91dae6

Fix startTime and endTime in performance tests
- broke by 1b20ea8d

subprojects/build-scan-performance/src/testFixtures/groovy/org/gradle/performance/fixture/BuildScanPerformanceTestRunner.groovy
@@ -40,7 +40,7 @@ class BuildScanPerformanceTestRunner extends CrossBuildPerformanceTestRunner {
             versionUnderTest: GradleVersion.current().getVersion(),
             vcsBranch: Git.current().branchName,
             vcsCommits: [Git.current().commitId, pluginCommitSha],
-            startTime: timeProvider.getCurrentTimeForDuration(),
+            startTime: timeProvider.getCurrentTime(),
             channel: determineChannel()
         )
     }

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/AbstractGradleBuildPerformanceTestRunner.groovy
@@ -98,7 +98,7 @@ abstract class AbstractGradleBuildPerformanceTestRunner<R extends PerformanceTes
 
         runAllSpecifications(results)
 
-        results.endTime = timeProvider.getCurrentTimeForDuration()
+        results.endTime = timeProvider.getCurrentTime()
 
         results.assertEveryBuildSucceeds()
         reporter.report(results)

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/CrossBuildPerformanceTestRunner.groovy
@@ -53,7 +53,7 @@ class CrossBuildPerformanceTestRunner extends AbstractGradleBuildPerformanceTest
             versionUnderTest: GradleVersion.current().getVersion(),
             vcsBranch: Git.current().branchName,
             vcsCommits: [Git.current().commitId],
-            startTime: timeProvider.getCurrentTimeForDuration(),
+            startTime: timeProvider.getCurrentTime(),
             channel: determineChannel()
         )
     }

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/CrossVersionPerformanceTestRunner.groovy
@@ -100,7 +100,7 @@ public class CrossVersionPerformanceTestRunner extends PerformanceTestSpec {
             versionUnderTest: GradleVersion.current().getVersion(),
             vcsBranch: Git.current().branchName,
             vcsCommits: [Git.current().commitId],
-            startTime: timeProvider.getCurrentTimeForDuration(),
+            startTime: timeProvider.getCurrentTime(),
             channel: ResultsStoreHelper.determineChannel()
         )
 
@@ -113,7 +113,7 @@ public class CrossVersionPerformanceTestRunner extends PerformanceTestSpec {
             runVersion(buildContext.distribution(baselineVersion.version), perVersionWorkingDirectory(baselineVersion.version), baselineVersion.results)
         }
 
-        results.endTime = timeProvider.getCurrentTimeForDuration()
+        results.endTime = timeProvider.getCurrentTime()
 
         results.assertEveryBuildSucceeds()
 

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/GradleVsMavenPerformanceTestRunner.groovy
@@ -89,7 +89,7 @@ class GradleVsMavenPerformanceTestRunner extends AbstractGradleBuildPerformanceT
             versionUnderTest: GradleVersion.current().getVersion(),
             vcsBranch: Git.current().branchName,
             vcsCommits: [Git.current().commitId],
-            startTime: timeProvider.getCurrentTimeForDuration(),
+            startTime: timeProvider.getCurrentTime(),
             channel: determineChannel()
         )
     }

subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/AbstractToolingApiCrossVersionPerformanceTest.groovy
@@ -144,7 +144,7 @@ abstract class AbstractToolingApiCrossVersionPerformanceTest extends Specificati
                 versionUnderTest: GradleVersion.current().getVersion(),
                 vcsBranch: Git.current().branchName,
                 vcsCommits: [Git.current().commitId],
-                startTime: timeProvider.getCurrentTimeForDuration(),
+                startTime: timeProvider.getCurrentTime(),
                 tasks: [],
                 args: [],
                 gradleOpts: [],
@@ -178,7 +178,7 @@ abstract class AbstractToolingApiCrossVersionPerformanceTest extends Specificati
                 resolver.stop()
             }
 
-            results.endTime = timeProvider.getCurrentTimeForDuration()
+            results.endTime = timeProvider.getCurrentTime()
 
             results.assertEveryBuildSucceeds()
             resultStore.report(results)