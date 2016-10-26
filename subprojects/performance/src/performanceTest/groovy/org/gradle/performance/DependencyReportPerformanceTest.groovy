767da78e476331dd04663cc9cfac3b4984afe88c
Lari Hotari <lari.hotari@gradle.com>
2016-10-26 16:12:24 +0300

    Rebase flaky performance tests

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/DependencyReportPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/DependencyReportPerformanceTest.groovy
@@ -38,11 +38,8 @@ class DependencyReportPerformanceTest extends AbstractCrossVersionPerformanceTes
 
         where:
         testProject       | targetVersions
-        "small"           | ['3.2-20161004202618+0000']
-        "multi"           | ['3.2-20161004202618+0000']
-        // TODO(pepper): Revert this to 'last' when 3.2 is released
-        // The regression was determined acceptable in this discussion:
-        // https://issues.gradle.org/browse/GRADLE-1346
-        "lotDependencies" | ['3.2-20161004202618+0000']
+        "small"           | ['3.3-20161026000020+0000']
+        "multi"           | ['3.3-20161026000020+0000']
+        "lotDependencies" | ['3.3-20161026000020+0000']
     }
 }
