8fdb0d0947c9b52252dce392c15161367f876d02
Lari Hotari <lari.hotari@gradle.com>
2016-11-02 10:57:04 +0200

    Make OutputCapturer thread safe
    - StreamByteBuffer isn't thread safe by design and concurrency must be
      handled in OutputCapturer in this case
    - Fixes continuous build integration tests failures
       "java.lang.IllegalStateException: Unexpected state.
       Buffer has remaining bytes without underflow in decoding."

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/OutputCapturer.java
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/OutputCapturer.java
@@ -16,22 +16,28 @@
 
 package org.gradle.integtests.fixtures.executer;
 
+import net.jcip.annotations.ThreadSafe;
 import org.apache.commons.io.output.CloseShieldOutputStream;
 import org.apache.commons.io.output.TeeOutputStream;
 import org.gradle.internal.io.StreamByteBuffer;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+@ThreadSafe
 class OutputCapturer {
     private final StreamByteBuffer buffer;
     private final OutputStream outputStream;
     private final StringBuilder outputStringBuilder = new StringBuilder();
     private final String outputEncoding;
     private String cachedOutputString;
+    private final Lock lock = new ReentrantLock();
 
     public OutputCapturer(OutputStream standardStream, String outputEncoding) {
         this.buffer = new StreamByteBuffer();
-        this.outputStream = new CloseShieldOutputStream(new TeeOutputStream(standardStream, buffer.getOutputStream()));
+        this.outputStream = new CloseShieldOutputStream(new TeeOutputStream(standardStream, new LockingOutputStream(buffer.getOutputStream(), lock)));
         this.outputEncoding = outputEncoding;
     }
 
@@ -40,16 +46,86 @@ class OutputCapturer {
     }
 
     public String getOutputAsString() {
-        if (cachedOutputString == null || buffer.totalBytesUnread() > 0) {
-            outputStringBuilder.append(buffer.readAsString(outputEncoding));
-            cachedOutputString = outputStringBuilder.toString();
+        lock.lock();
+        try {
+            if (cachedOutputString == null || buffer.totalBytesUnread() > 0) {
+                outputStringBuilder.append(buffer.readAsString(outputEncoding));
+                cachedOutputString = outputStringBuilder.toString();
+            }
+            return cachedOutputString;
+        } finally {
+            lock.unlock();
         }
-        return cachedOutputString;
     }
 
     public void reset() {
-        buffer.clear();
-        outputStringBuilder.setLength(0);
-        cachedOutputString = null;
+        lock.lock();
+        try {
+            buffer.clear();
+            outputStringBuilder.setLength(0);
+            cachedOutputString = null;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private static class LockingOutputStream extends OutputStream {
+        private final OutputStream delegate;
+        private final Lock lock;
+
+        LockingOutputStream(OutputStream delegate, Lock lock) {
+            this.delegate = delegate;
+            this.lock = lock;
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            lock.lock();
+            try {
+                delegate.write(b);
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void write(byte[] b) throws IOException {
+            lock.lock();
+            try {
+                delegate.write(b);
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void write(byte[] b, int off, int len) throws IOException {
+            lock.lock();
+            try {
+                delegate.write(b, off, len);
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void flush() throws IOException {
+            lock.lock();
+            try {
+                delegate.flush();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void close() throws IOException {
+            lock.lock();
+            try {
+                delegate.close();
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 }
