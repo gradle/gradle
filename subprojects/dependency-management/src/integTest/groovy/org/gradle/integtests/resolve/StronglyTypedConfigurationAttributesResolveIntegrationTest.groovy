b7ab02f58ba90322ca4ea42860e40babbabf845f
Cedric Champeau <cedric@gradle.com>
2016-11-08 09:47:59 +0100

    Add test case showing that type mismatch would fallback to the `default` configuration

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/StronglyTypedConfigurationAttributesResolveIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/StronglyTypedConfigurationAttributesResolveIntegrationTest.groovy
@@ -59,4 +59,70 @@ class StronglyTypedConfigurationAttributesResolveIntegrationTest extends Abstrac
     String getFree() {
         '(flavor): Flavor.of("free")'
     }
+
+    // This documents the current behavior, not necessarily the one we would want. Maybe
+    // we will prefer failing with an error indicating incompatible types
+    def "fails if two configurations use the same attribute name with different types"() {
+        given:
+        file('settings.gradle') << "include 'a', 'b'"
+        buildFile << """
+            $typeDefs
+
+            project(':a') {
+                configurations {
+                    _compileFreeDebug.attributes($freeDebug)
+                    _compileFreeRelease.attributes($freeRelease)
+                }
+                dependencies {
+                    _compileFreeDebug project(':b')
+                    _compileFreeRelease project(':b')
+                }
+                task checkDebug(dependsOn: configurations._compileFreeDebug) {
+                    doLast {
+                       assert configurations._compileFreeDebug.collect { it.name } == ['b-default.jar']
+                    }
+                }
+                task checkRelease(dependsOn: configurations._compileFreeRelease) {
+                    doLast {
+                       assert configurations._compileFreeRelease.collect { it.name } == ['b-default.jar']
+                    }
+                }
+            }
+            project(':b') {
+                configurations {
+                    create('default')
+                    foo {
+                        attributes(flavor: 'free', buildType: 'debug') // use String type instead of Flavor/BuildType
+                    }
+                    bar {
+                        attributes(flavor: 'free', buildType: 'release') // use String type instead of Flavor/BuildType
+                    }
+                }
+                task fooJar(type: Jar) {
+                   baseName = 'b-foo'
+                }
+                task barJar(type: Jar) {
+                   baseName = 'b-bar'
+                }
+                artifacts {
+                    'default' file('b-default.jar')
+                    foo fooJar
+                    bar barJar
+                }
+            }
+
+        """
+
+        when:
+        run ':a:checkDebug'
+
+        then:
+        notExecuted ':b:fooJar', ':b:barJar'
+
+        when:
+        run ':a:checkRelease'
+
+        then:
+        notExecuted ':b:fooJar', ':b:barJar'
+    }
 }
