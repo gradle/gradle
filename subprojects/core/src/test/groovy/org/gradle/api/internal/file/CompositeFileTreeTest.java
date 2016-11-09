11f9a3561fa1024e3a8b38dea7a9c745be73ca80
Paul Merlin <paul@gradle.com>
2016-11-09 16:50:33 +0100

    Add missing Action<T> overloads to FileTree
    Closure overloads now delegate to the Action based one using
    ClosureBackedAction#of.
    
    See gradle/gradle-script-kotlin#126

--- subprojects/core/src/test/groovy/org/gradle/api/internal/file/CompositeFileTreeTest.java
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/file/CompositeFileTreeTest.java
@@ -20,6 +20,7 @@ import org.gradle.api.Action;
 import org.gradle.api.file.FileTree;
 import org.gradle.api.file.FileVisitDetails;
 import org.gradle.api.file.FileVisitor;
+import org.gradle.api.internal.ClosureBackedAction;
 import org.gradle.api.internal.file.collections.FileCollectionResolveContext;
 import org.gradle.api.tasks.util.PatternFilterable;
 import org.gradle.api.tasks.util.PatternSet;
@@ -65,13 +66,14 @@ public class CompositeFileTreeTest {
     @Test
     public void matchingWithClosureReturnsUnionOfFilteredSets() {
         final Closure closure = TestUtil.TEST_CLOSURE;
+        final Action<PatternFilterable> action = ClosureBackedAction.of(closure);
         final FileTreeInternal filtered1 = context.mock(FileTreeInternal.class);
         final FileTreeInternal filtered2 = context.mock(FileTreeInternal.class);
 
         context.checking(new Expectations() {{
-            oneOf(source1).matching(closure);
+            oneOf(source1).matching(action);
             will(returnValue(filtered1));
-            oneOf(source2).matching(closure);
+            oneOf(source2).matching(action);
             will(returnValue(filtered2));
         }});
 
@@ -147,10 +149,11 @@ public class CompositeFileTreeTest {
     @Test
     public void visitsEachTreeWithClosure() {
         final Closure visitor = TestUtil.TEST_CLOSURE;
+        final Action<FileVisitDetails> action = ClosureBackedAction.of(visitor);
 
         context.checking(new Expectations() {{
-            oneOf(source1).visit(visitor);
-            oneOf(source2).visit(visitor);
+            oneOf(source1).visit(action);
+            oneOf(source2).visit(action);
         }});
 
         tree.visit(visitor);
