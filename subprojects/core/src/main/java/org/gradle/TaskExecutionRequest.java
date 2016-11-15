b8bc3211a9e494b0c1feb44bf1ac493395c492a3
Sterling Greene <sterling@gradle.com>
2016-11-15 16:11:22 -0500

    Fix warning from JavaDoc generation

--- subprojects/core/src/main/java/org/gradle/TaskExecutionRequest.java
+++ subprojects/core/src/main/java/org/gradle/TaskExecutionRequest.java
@@ -46,7 +46,7 @@ public interface TaskExecutionRequest {
     /**
      * The root folder of the build that this task was defined in.
      *
-     * @return the root project folder or {@null} if the information is not available.
+     * @return the root project folder or {@code null} if the information is not available.
      */
     @Nullable
     File getRootDir();
