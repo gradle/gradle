d91725d83c265844499a8e9ed0401a4f0973ec69
Cedric Champeau <cedric@gradle.com>
2016-11-17 11:25:46 +0100

    Initial setup of the API and API compile configurations
    This is the initial step to provide API/impl separation for the current model. The commit
    introduces two new configurations:
    
    - `api` is a bucket of dependencies configuration where the user would declare the dependencies of its
    API. That configuration is not transitive, and reflects the dependencies which are strictly required
    when some component needs to compile against this component.
    - `apiCompile` is a consumer only configuration which extends the `api` configuration and provides
    the compile classpath when a component compiles against this component. It will therefore contain
    the dependencies of `api`, plus the API classes. The form in which we provide the API classes has
    yet to be defined (could be a jar, a class directory, stubs, ...).
    
    See: gradle/performance#180

--- subprojects/plugins/src/main/java/org/gradle/api/plugins/JavaBasePlugin.java
+++ subprojects/plugins/src/main/java/org/gradle/api/plugins/JavaBasePlugin.java
@@ -220,9 +220,25 @@ public class JavaBasePlugin implements Plugin<ProjectInternal> {
     }
 
     private void defineConfigurationsForSourceSet(SourceSet sourceSet, ConfigurationContainer configurations) {
+        Configuration apiConfiguration = configurations.maybeCreate(sourceSet.getApiConfigurationName());
+        apiConfiguration.setVisible(false);
+        apiConfiguration.setDescription("API dependencies for " + sourceSet + ".");
+        apiConfiguration.setCanBeResolved(false);
+        apiConfiguration.setCanBeConsumed(false);
+        apiConfiguration.setTransitive(false);
+
+        Configuration apiCompileConfiguration = configurations.maybeCreate(sourceSet.getApiCompileConfigurationName());
+        apiCompileConfiguration.setVisible(false);
+        apiCompileConfiguration.setDescription("API compile classpath for " + sourceSet + ".");
+        apiCompileConfiguration.setCanBeResolved(true);
+        apiCompileConfiguration.setCanBeConsumed(true);
+        apiCompileConfiguration.setTransitive(false);
+        apiCompileConfiguration.extendsFrom(apiConfiguration);
+
         Configuration compileConfiguration = configurations.maybeCreate(sourceSet.getCompileConfigurationName());
         compileConfiguration.setVisible(false);
         compileConfiguration.setDescription("Dependencies for " + sourceSet + ".");
+        compileConfiguration.extendsFrom(apiConfiguration);
 
         Configuration runtimeConfiguration = configurations.maybeCreate(sourceSet.getRuntimeConfigurationName());
         runtimeConfiguration.setVisible(false);
