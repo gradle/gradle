27fe937b6e5cf07416330e1df04f6d34795e96bc
Jendrik Johannes <jendrik@gradle.com>
2016-11-17 09:04:20 +0100

    Remove reference to java plugin and clarify test scenario configuration

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/AARFilterLocalIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/AARFilterLocalIntegrationTest.groovy
@@ -18,7 +18,7 @@ package org.gradle.integtests.resolve.transform;
 
 public class AARFilterLocalIntegrationTest extends AbstractAARFilterAndTransformIntegrationTest {
 
-    def availableFeatures() {
+    def enabledFeatures() {
         [Feature.FILTER_LOCAL]
     }
 
