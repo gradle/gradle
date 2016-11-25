908d16bdf85b8817f9c7f39706a845c80a7c5fc4
Cedric Champeau <cedric@gradle.com>
2016-11-25 19:32:14 +0100

    Fix runtime classpath expectations

--- subprojects/plugins/src/test/groovy/org/gradle/api/plugins/WarPluginTest.groovy
+++ subprojects/plugins/src/test/groovy/org/gradle/api/plugins/WarPluginTest.groovy
@@ -129,7 +129,7 @@ class WarPluginTest extends AbstractProjectBuilderSpec {
 
         then:
         def task = project.tasks[WarPlugin.WAR_TASK_NAME]
-        task.classpath.files as List == [project.sourceSets.main.output.classesDir, project.sourceSets.main.output.resourcesDir, runtimeJar, compileJar]
+        task.classpath.files as List == [project.sourceSets.main.output.classesDir, project.sourceSets.main.output.resourcesDir, compileJar, runtimeJar]
     }
 
     def "applies mappings to archive tasks"() {
