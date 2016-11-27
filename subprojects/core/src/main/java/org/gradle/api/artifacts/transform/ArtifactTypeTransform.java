8f36e5e4e7695e7c4eafa5a1e36716b401158792
Daz DeBoer <daz@gradle.com>
2016-11-27 13:45:41 -0700

    Fix tests for changed `ArtifactTransform` API
    The new `ArtifactTransform` API allows arbitrary attribute mapping,
    but makes it a bit more difficult to code a transform that only cares
    about the core 'type' attribute. This commit adds an
    `ArtifactTypeTransform` subtype that only deals with the 'artifactType'
    attribute, and updates the various AAR* integration tests to the new
    API.

---
+++ subprojects/core/src/main/java/org/gradle/api/artifacts/transform/ArtifactTypeTransform.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.artifacts.transform;
+
+import org.gradle.api.Attribute;
+import org.gradle.api.AttributeContainer;
+import org.gradle.api.Incubating;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ * Base class for simple type-based artifact transformations.
+ */
+@Incubating
+public abstract class ArtifactTypeTransform extends ArtifactTransform {
+
+    private static final Attribute<String> ARTIFACT_TYPE_ATTRIBUTE = Attribute.of("artifactType", String.class);
+
+    protected abstract String getInputType();
+
+    protected abstract Collection<String> getOutputTypes();
+
+    protected abstract File transform(File input, String outputType);
+
+    @Override
+    public void configure(AttributeContainer from, AttributeTransformTargetRegistry targetRegistry) {
+        from.attribute(ARTIFACT_TYPE_ATTRIBUTE, getInputType());
+
+        for (String outputType : getOutputTypes()) {
+            targetRegistry.newTarget().attribute(ARTIFACT_TYPE_ATTRIBUTE, outputType);
+        }
+    }
+
+    @Override
+    public File transform(File input, AttributeContainer target) {
+        String targetType = target.getAttribute(ARTIFACT_TYPE_ATTRIBUTE);
+        return transform(input, targetType);
+    }
+}
