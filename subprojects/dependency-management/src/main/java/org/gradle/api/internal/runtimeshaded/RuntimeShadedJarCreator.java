0332688569b44d1dbaeeb6300b56fc2f867d6557
Stefan Wolf <wolf@gradle.com>
2016-11-30 10:22:03 +0100

    Use a constant for fixed time in shaded jar entry
    +review REVIEW-6388

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/runtimeshaded/RuntimeShadedJarCreator.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/runtimeshaded/RuntimeShadedJarCreator.java
@@ -74,6 +74,7 @@ class RuntimeShadedJarCreator {
     private static final int BUFFER_SIZE = 8192;
     private static final String SERVICES_DIR_PREFIX = "META-INF/services/";
     private static final String CLASS_DESC = "Ljava/lang/Class;";
+    private static final long FIXED_TIME_FOR_ZIP_ENTRY = fixedTimeForZipEntry();
 
     private final ProgressLoggerFactory progressLoggerFactory;
     private final ImplementationDependencyRelocator remapper;
@@ -81,6 +82,26 @@ class RuntimeShadedJarCreator {
     public RuntimeShadedJarCreator(ProgressLoggerFactory progressLoggerFactory, ImplementationDependencyRelocator remapper) {
         this.progressLoggerFactory = progressLoggerFactory;
         this.remapper = remapper;
+    }
+
+    /**
+     * Note that setting the January 1st 1980 (or even worse, "0", as time) won't work due
+     * to Java 8 doing some interesting time processing: It checks if this date is before January 1st 1980
+     * and if it is it starts setting some extra fields in the zip. Java 7 does not do that - but in the
+     * zip not the milliseconds are saved but values for each of the date fields - but no time zone. And
+     * 1980 is the first year which can be saved.
+     * If you use January 1st 1980 then it is treated as a special flag in Java 8.
+     * Moreover, only even seconds can be stored in the zip file. Java 8 uses the upper half of
+     * some other long to store the remaining millis while Java 7 doesn't do that. So make sure
+     * that your seconds are even.
+     * Moreover, parsing happens via `new Date(millis)` in {@link java.util.zip.ZipUtils}#javaToDosTime() so we
+     * must use default timezone and locale.
+     */
+    private static long fixedTimeForZipEntry() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.clear();
+        calendar.set(1980, Calendar.FEBRUARY, 1, 0, 0, 0);
+        return calendar.getTimeInMillis();
     }
 
     public void create(final File outputJar, final Iterable<? extends File> files) {
@@ -175,7 +196,7 @@ class RuntimeShadedJarCreator {
             @Override
             public void visitDir(FileVisitDetails dirDetails) {
                 try {
-                    ZipEntry zipEntry = newZipEntry(dirDetails.getPath() + "/");
+                    ZipEntry zipEntry = newZipEntryWithFixedTime(dirDetails.getPath() + "/");
                     processEntry(outputStream, null, zipEntry, buffer, seenPaths, services);
                 } catch (IOException e) {
                     throw new UncheckedIOException(e);
@@ -185,7 +206,7 @@ class RuntimeShadedJarCreator {
             @Override
             public void visitFile(FileVisitDetails fileDetails) {
                 try {
-                    ZipEntry zipEntry = newZipEntry(fileDetails.getPath());
+                    ZipEntry zipEntry = newZipEntryWithFixedTime(fileDetails.getPath());
                     InputStream inputStream = fileDetails.open();
                     try {
                         processEntry(outputStream, inputStream, zipEntry, buffer, seenPaths, services);
@@ -288,33 +309,21 @@ class RuntimeShadedJarCreator {
     }
 
     private void writeResourceEntry(ZipOutputStream outputStream, InputStream inputStream, byte[] buffer, String resourceFileName) throws IOException {
-        outputStream.putNextEntry(newZipEntry(resourceFileName));
+        outputStream.putNextEntry(newZipEntryWithFixedTime(resourceFileName));
         pipe(inputStream, outputStream, buffer);
         outputStream.closeEntry();
     }
 
     private void writeEntry(ZipOutputStream outputStream, String name, byte[] content) throws IOException {
-        ZipEntry zipEntry = newZipEntry(name);
+        ZipEntry zipEntry = newZipEntryWithFixedTime(name);
         outputStream.putNextEntry(zipEntry);
         outputStream.write(content);
         outputStream.closeEntry();
     }
 
-    private ZipEntry newZipEntry(String name) {
+    private ZipEntry newZipEntryWithFixedTime(String name) {
         ZipEntry entry = new ZipEntry(name);
-        // Note that setting the January 1st 1980 (or even worse, "0", as time) won't work due
-        // to Java 8 doing some interesting time processing: It checks if this date is before January 1st 1980
-        // and if it is it starts setting some extra fields in the zip. Java 7 does not do that - but in the
-        // zip not the milliseconds are saved but values for each of the date fields - but no time zone. And
-        // 1980 is the first year which can be saved.
-        // If you use January 1st 1980 then it is treated as a special flag in Java 8.
-        // Moreover, only even seconds can be stored in the zip file. Java 8 uses the upper half of
-        // some other long to store the remaining millis while Java 7 doesn't do that. So make sure
-        // that your seconds are even.
-        Calendar calendar = new GregorianCalendar();
-        calendar.clear();
-        calendar.set(1980, Calendar.FEBRUARY, 1, 0, 0, 0);
-        entry.setTime(calendar.getTimeInMillis());
+        entry.setTime(FIXED_TIME_FOR_ZIP_ENTRY);
         return entry;
     }
 
