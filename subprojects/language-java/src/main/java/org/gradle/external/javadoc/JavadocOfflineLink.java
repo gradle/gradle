b9ca88d97344ec3b8c2bba06d5ac6d7f086154a0
Lóránt Pintér <lorant@gradle.com>
2017-01-10 10:44:01 +0100

    Implement equals() and hashCode() for serializable type

--- subprojects/language-java/src/main/java/org/gradle/external/javadoc/JavadocOfflineLink.java
+++ subprojects/language-java/src/main/java/org/gradle/external/javadoc/JavadocOfflineLink.java
@@ -16,6 +16,8 @@
 
 package org.gradle.external.javadoc;
 
+import com.google.common.base.Objects;
+
 import java.io.Serializable;
 
 /**
@@ -37,6 +39,24 @@ public class JavadocOfflineLink implements Serializable {
 
     public String getPackagelistLoc() {
         return packagelistLoc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        JavadocOfflineLink that = (JavadocOfflineLink) o;
+        return Objects.equal(extDocUrl, that.extDocUrl)
+            && Objects.equal(packagelistLoc, that.packagelistLoc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(extDocUrl, packagelistLoc);
     }
 
     public String toString() {
