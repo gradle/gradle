f7bfc7e207240212b937ea68a57b384311f8d9aa
Benjamin Muschko <ben@gradle.com>
2017-01-10 14:40:01 -0500

    Capture violations in Ant property
    Throws any failure as GradleException instead of letting Ant take charge of it.

--- subprojects/jacoco/src/main/java/org/gradle/internal/jacoco/AntJacocoReport.java
+++ subprojects/jacoco/src/main/java/org/gradle/internal/jacoco/AntJacocoReport.java
@@ -18,6 +18,8 @@ package org.gradle.internal.jacoco;
 
 import com.google.common.collect.ImmutableMap;
 import groovy.lang.GroovyObjectSupport;
+import org.gradle.api.Action;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.internal.project.IsolatedAntBuilder;
 import org.gradle.testing.jacoco.tasks.JacocoReportsContainer;
 
@@ -25,6 +27,17 @@ public class AntJacocoReport extends AbstractAntJacocoReport<JacocoReportsContai
 
     public AntJacocoReport(IsolatedAntBuilder ant) {
         super(ant);
+    }
+
+    public void execute(FileCollection classpath, final String projectName,
+                        final FileCollection allClassesDirs, final FileCollection allSourcesDirs,
+                        final FileCollection executionData, final JacocoReportsContainer reports) {
+        configureAntReportTask(classpath, new Action<GroovyObjectSupport>() {
+            @Override
+            public void execute(GroovyObjectSupport antBuilder) {
+                invokeJacocoReport(antBuilder, projectName, allClassesDirs, allSourcesDirs, executionData, reports);
+            }
+        });
     }
 
     @Override
