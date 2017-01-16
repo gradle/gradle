d822a7f5976beae9384933d140d47f260baa3202
Cedric Champeau <cedric@gradle.com>
2017-01-16 09:13:18 +0100

    Small optimization : wrapped types need not be wrapped into weak references since they will never be collected

--- subprojects/base-services/src/main/java/org/gradle/internal/reflect/JavaReflectionUtil.java
+++ subprojects/base-services/src/main/java/org/gradle/internal/reflect/JavaReflectionUtil.java
@@ -517,7 +517,7 @@ public class JavaReflectionUtil {
         private final WeakReference<Class<?>>[] parameterTypes;
         private final int paramCount;
         private final boolean[] isPrimitive;
-        private final WeakReference<Class<?>>[] wrappedParameterTypes;
+        private final Class<?>[] wrappedParameterTypes;
 
         @SuppressWarnings("unchecked")
         public CachedConstructor(Constructor<?> ctor) {
@@ -525,12 +525,12 @@ public class JavaReflectionUtil {
             this.parameterTypes = weakReference(ctor.getParameterTypes());
             this.paramCount = this.parameterTypes.length;
             this.isPrimitive = new boolean[paramCount];
-            this.wrappedParameterTypes = new WeakReference[paramCount];
+            this.wrappedParameterTypes = new Class[paramCount];
             for (int i = 0; i < paramCount; i++) {
                 WeakReference<Class<?>> parameterType = parameterTypes[i];
                 boolean primitive = parameterType.get().isPrimitive();
                 this.isPrimitive[i] = primitive;
-                this.wrappedParameterTypes[i] = primitive ? new WeakReference<Class<?>>(getWrapperTypeForPrimitiveType(parameterType.get())) : parameterType;
+                this.wrappedParameterTypes[i] = primitive ? getWrapperTypeForPrimitiveType(parameterType.get()) : null;
             }
         }
 
@@ -549,9 +549,12 @@ public class JavaReflectionUtil {
             }
             for (int i = 0; i < paramCount; i++) {
                 Object param = params[i];
+                // There's no need for a null check below because the parameter types are referenced
+                // by the class that uses them, so cannot be collected if the class itself is not
+                // collected.
                 Class<?> parameterType = parameterTypes[i].get();
                 if (isPrimitive[i]) {
-                    if (!wrappedParameterTypes[i].get().isInstance(param)) {
+                    if (!wrappedParameterTypes[i].isInstance(param)) {
                         return false;
                     }
                 } else {
