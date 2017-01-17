14c9355588b98c6deb026fcc2cc51da056cde9a7
Adam Murdoch <adam@gradle.com>
2017-01-17 14:24:58 +1000

    Reworked method and property dispatch on `BasicScript` to dispatch directly to `DynamicObject` representations of the script and target object.

--- subprojects/core/src/main/java/org/gradle/groovy/scripts/Script.java
+++ subprojects/core/src/main/java/org/gradle/groovy/scripts/Script.java
@@ -16,8 +16,8 @@
 
 package org.gradle.groovy.scripts;
 
-import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.internal.logging.StandardOutputCapture;
+import org.gradle.internal.service.ServiceRegistry;
 
 /**
  * The base class for all scripts executed by Gradle.
@@ -45,15 +45,4 @@ public abstract class Script extends groovy.lang.Script {
     public abstract void init(Object target, ServiceRegistry services);
 
     public abstract StandardOutputCapture getStandardOutputCapture();
-
-    @Override
-    public Object getProperty(String property) {
-        // Refactoring of groovy.lang.Script.getProperty logic
-        // to avoid unnecessary MissingPropertyException in binding variable lookup
-        if (getBinding().hasVariable(property)) {
-            return super.getProperty(property);
-        } else {
-            return getMetaClass().getProperty(this, property);
-        }
-    }
 }
