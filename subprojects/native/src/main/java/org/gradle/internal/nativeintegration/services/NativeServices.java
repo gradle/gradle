04e9468eff62010cbe3c01a1ab08feb38b630c87
Cedric Champeau <cedric@gradle.com>
2017-01-19 11:36:14 +0100

    Don't use native platform for Windows as we are seeing weird test failures
    ... that may be related to this. If failures go away it will confirm that
    stat with native platform on Windows would be broken.

--- subprojects/native/src/main/java/org/gradle/internal/nativeintegration/services/NativeServices.java
+++ subprojects/native/src/main/java/org/gradle/internal/nativeintegration/services/NativeServices.java
@@ -220,11 +220,11 @@ public class NativeServices extends DefaultServiceRegistry implements ServiceReg
     protected FileMetadataAccessor createFileMetadataAccessor(OperatingSystem operatingSystem) {
         // Based on the benchmark found in org.gradle.internal.nativeintegration.filesystem.FileMetadataAccessorBenchmark
         // and the results in the PR https://github.com/gradle/gradle/pull/1183
-        // we're using "native platform" for Mac OS and Windows and a  mix of File and NIO API for Linux
+        // we're using "native platform" for Mac OS and a  mix of File and NIO API for Linux and Windows
         // Once JDK 9 is out, we need to revisit the choice, because testing for file.exists() should become much
         // cheaper using the pure NIO implementation.
 
-        if ((operatingSystem.isMacOsX() || operatingSystem.isWindows()) && useNativeIntegrations) {
+        if ((operatingSystem.isMacOsX()) && useNativeIntegrations) {
             try {
                 return new NativePlatformBackedFileMetadataAccessor(net.rubygrapefruit.platform.Native.get(Files.class));
             } catch (NativeIntegrationUnavailableException e) {
