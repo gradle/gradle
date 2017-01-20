2e336a1f80a8d133144dd4ae0c3a6c98c47308ba
Stefan Wolf <wolf@gradle.com>
2017-01-20 22:16:09 +0100

    Fix cache init script smoke tests
    Now all tasks are analyzed for cacheability and that changes the
    statistics.
    
    +review REVIEW-6426

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/CacheInitScriptsSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/CacheInitScriptsSmokeTest.groovy
@@ -55,11 +55,12 @@ class CacheInitScriptsSmokeTest extends AbstractSmokeTest {
                   Not cacheable - 5 tasks took 2 ms (avg 0.40 ms, stddev 0.49 ms, min 0 ms, max 1 ms)
                     org.gradle.api.DefaultTask - 5 tasks took 2 ms (avg 0.40 ms, stddev 0.49 ms, min 0 ms, max 1 ms)
                 NO_SOURCE - 6 tasks took 9 ms (avg 1.50 ms, stddev 0.76 ms, min 1 ms, max 3 ms)
-                  Not cacheable - 6 tasks took 9 ms (avg 1.50 ms, stddev 0.76 ms, min 1 ms, max 3 ms)
-                    org.gradle.api.tasks.compile.GroovyCompile - 2 tasks took 4 ms (avg 2.00 ms, stddev 1.00 ms, min 1 ms, max 3 ms)
-                    org.gradle.api.tasks.compile.JavaCompile - 1 task took 1 ms
-                    org.gradle.api.tasks.testing.Test - 1 task took 1 ms
+                  Not cacheable - 2 tasks took 2 ms (avg 1,00 ms, stddev 0,00 ms, min 1 ms, max 1 ms)
                     org.gradle.language.jvm.tasks.ProcessResources - 2 tasks took 3 ms (avg 1.50 ms, stddev 0.50 ms, min 1 ms, max 2 ms)
+                  Cacheable - 4 tasks took 8 ms (avg 2.00 ms, stddev 0.71 ms, min 1 ms, max 3 ms)
+                    org.gradle.api.tasks.compile.GroovyCompile - 2 tasks took 5 ms (avg 2.50 ms, stddev 0.50 ms, min 2 ms, max 3 ms)
+                    org.gradle.api.tasks.compile.JavaCompile - 1 task took 1 ms
+                    org.gradle.api.tasks.testing.Test - 1 task took 2 ms
                   
               (*) denotes tasks with custom actions
         """.stripIndent().split("\n").findAll { !it.empty }.collect { it.replaceAll(TIME_PATTERN, "TIME") }
@@ -72,6 +73,7 @@ class CacheInitScriptsSmokeTest extends AbstractSmokeTest {
 
         when:
         def result = runner('-I', 'taskCacheInit.gradle', 'build').build()
+        println result.output
         def actualLines = result.output.split("\n").collect { it.replaceAll(TIME_PATTERN, "TIME") }
 
         then:
@@ -89,9 +91,9 @@ class CacheInitScriptsSmokeTest extends AbstractSmokeTest {
             "FROM_CACHE","Cacheable","Jar","org.gradle.api.tasks.bundling",1,6,6.0,0.0,6,6
             "FROM_CACHE","Cacheable","JavaCompile (*)","org.gradle.api.tasks.compile",1,5,5.0,0.0,5,5
             "UP_TO_DATE","Not cacheable","DefaultTask","org.gradle.api",5,1,0.2,0.39999999999999997,0,1
-            "NO_SOURCE","Not cacheable","GroovyCompile","org.gradle.api.tasks.compile",2,0,0.0,0.0,0,0
-            "NO_SOURCE","Not cacheable","JavaCompile","org.gradle.api.tasks.compile",1,2,2.0,0.0,2,2
-            "NO_SOURCE","Not cacheable","Test","org.gradle.api.tasks.testing",1,1,1.0,0.0,1,1
+            "NO_SOURCE","Cacheable","GroovyCompile","org.gradle.api.tasks.compile",2,0,0.0,0.0,0,0
+            "NO_SOURCE","Cacheable","JavaCompile","org.gradle.api.tasks.compile",1,2,2.0,0.0,2,2
+            "NO_SOURCE","Cacheable","Test","org.gradle.api.tasks.testing",1,1,1.0,0.0,1,1
             "NO_SOURCE","Not cacheable","ProcessResources","org.gradle.language.jvm.tasks",2,1,0.5,0.5,0,1
         """.stripIndent().split("\n").findAll { !it.empty }.collect { it.replaceAll(NUMBER_PATTERN, "NUMBER") }
 
