7164418390ebfa366eac77a8064d94ab386b7857
Eric Wendelin <eric@gradle.com>
2017-01-20 20:40:11 -0700

    Making the pluginRepositories avaliable in the init scripts
    Given this feature users the ability to configure their
    pluginRepositories though an init script to make it easier for orgs to
    load all plugin repositories up.
    
    PR: #952

--- subprojects/core/src/main/java/org/gradle/api/invocation/Gradle.java
+++ subprojects/core/src/main/java/org/gradle/api/invocation/Gradle.java
@@ -28,6 +28,7 @@ import org.gradle.api.initialization.IncludedBuild;
 import org.gradle.api.initialization.Settings;
 import org.gradle.api.plugins.PluginAware;
 import org.gradle.internal.HasInternalProtocol;
+import org.gradle.plugin.repository.PluginRepositoriesSpec;
 
 import java.io.File;
 import java.util.Collection;
@@ -181,6 +182,22 @@ public interface Gradle extends PluginAware {
      * @param action The action to execute.
      */
     void buildStarted(Action<? super Gradle> action);
+
+    /**
+     * Adds a action to be called to configure {@code PluginRepositoriesSpec}
+     *
+     * @param pluginSettings The action to execute
+     * @throws IllegalStateException if a plugin has already been applied with the <code>plugins { }</code> block.
+     */
+    void pluginRepositories(Action<? super PluginRepositoriesSpec> pluginSettings);
+
+    /**
+     * Adds a action to be called to configure {@code PluginRepositoriesSpec}
+     *
+     * @param pluginSettings The action to execute
+     * @throws IllegalStateException if a plugin has already been applied with the <code>plugins { }</code> block.
+     */
+    void pluginRepositories(Closure pluginSettings);
 
     /**
      * Adds a closure to be called when the build settings have been loaded and evaluated.
