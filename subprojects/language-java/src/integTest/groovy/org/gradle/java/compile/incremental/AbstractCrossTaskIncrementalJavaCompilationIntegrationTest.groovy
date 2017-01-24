68c90696f2914cbb57576ffbbfa5eb12eb7ad13f
Cedric Champeau <cedric@gradle.com>
2017-01-24 21:49:14 +0100

    Add special handling for `null` if the class we are analyzing is `java.lang.Object`

--- subprojects/language-java/src/integTest/groovy/org/gradle/java/compile/incremental/AbstractCrossTaskIncrementalJavaCompilationIntegrationTest.groovy
+++ subprojects/language-java/src/integTest/groovy/org/gradle/java/compile/incremental/AbstractCrossTaskIncrementalJavaCompilationIntegrationTest.groovy
@@ -38,6 +38,9 @@ abstract class AbstractCrossTaskIncrementalJavaCompilationIntegrationTest extend
                     options.fork = true
                 }
                 repositories { mavenCentral() }
+dependencies {
+    compile files('/home/cchampeau/DEV/PROJECTS/GITHUB/android-plugin/prebuilts/sdk/25/android.jar')
+}
             }
             $projectDependencyBlock
         """
