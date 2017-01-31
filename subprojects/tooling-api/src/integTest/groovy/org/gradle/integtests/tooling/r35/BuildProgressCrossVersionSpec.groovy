265ade3142d6ec261e4304f38519e89efbc7b87f
Rene Groeschke <rene@gradle.com>
2017-02-01 00:36:32 +0100

    Next try fixing file lock issue in tapi integtest on windows

--- subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r35/BuildProgressCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/r35/BuildProgressCrossVersionSpec.groovy
@@ -26,7 +26,6 @@ import org.gradle.test.fixtures.server.http.RepositoryHttpServer
 import org.gradle.tooling.ProjectConnection
 import org.junit.Rule
 
-
 @ToolingApiVersion(">=2.5")
 @TargetGradleVersion(">=3.5")
 class BuildProgressCrossVersionSpec extends ToolingApiSpecification {
@@ -173,7 +172,13 @@ class BuildProgressCrossVersionSpec extends ToolingApiSpecification {
         resolveArtifactC.children == [downloadCArtifact]
 
         cleanup:
-        toolingApi.close()
+        try {
+            toolingApi.getDaemons().killAll()
+        } catch (RuntimeException ex) {
+            //TODO once we figured out why pid from logfile can be null we should remove this again
+            LOGGER.warn("Unable to kill daemon(s)", ex);
+        }
+
     }
 
     MavenHttpRepository getMavenHttpRepo() {
