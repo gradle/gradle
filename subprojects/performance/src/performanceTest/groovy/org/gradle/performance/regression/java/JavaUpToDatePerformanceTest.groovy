f8b8ca207b627e7ef93594c4023ee8daeb8a0935
Jendrik Johannes <jendrik@gradle.com>
2017-02-22 13:29:39 +0100

    Added UpToDate Java performance test

---
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaUpToDatePerformanceTest.groovy
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.performance.regression.java
+
+import org.gradle.performance.AbstractCrossVersionPerformanceTest
+import spock.lang.Unroll
+
+class JavaUpToDatePerformanceTest extends AbstractCrossVersionPerformanceTest {
+
+    @Unroll
+    def "#testProject"() {
+        //This test scenario can potentially be replaced with an incremental change test
+
+        given:
+        runner.tasksToRun = ['tasks']
+        runner.gradleOpts = ["-Xms${memory}", "-Xmx${memory}"]
+
+        when:
+        def result = runner.run()
+
+        then:
+        result.assertCurrentVersionHasNotRegressed()
+
+        where:
+        testProject                  | memory
+        "largeMonolithicProjectJava" | "608m"
+        "largeMultiProjectJava"      | "608m"
+    }
+}
