9fc4ac67224abb93d891559c0c888f07949d6fd8
Adam Murdoch <adam@gradle.com>
2017-03-04 12:08:04 +1100

    Javadocs.

--- subprojects/resources/src/main/java/org/gradle/internal/resource/local/FileStore.java
+++ subprojects/resources/src/main/java/org/gradle/internal/resource/local/FileStore.java
@@ -19,13 +19,29 @@ import org.gradle.api.Action;
 
 import java.io.File;
 
+/**
+ * An indexed store that maps a key to a file or directory.
+ *
+ * Most implementations do not provide locking, which must be coordinated by the caller.
+ */
 public interface FileStore<K> {
-
+    /**
+     * Moves the given file into the store.
+     */
     LocallyAvailableResource move(K key, File source);
 
+    /**
+     * Copies the given file into the store.
+     */
     LocallyAvailableResource copy(K key, File source);
 
+    /**
+     * Moves the contents of this store to the given destination.
+     */
     void moveFilestore(File destination);
 
+    /**
+     * Adds an entry to the store, using the given action to produce the file.
+     */
     LocallyAvailableResource add(K key, Action<File> addAction);
 }
