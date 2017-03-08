38759f667b46acc7018b4886783afa5953f9cb45
Gary Hale <gary@gradle.com>
2017-03-08 10:36:50 -0500

    Fix gaps in serialization error handling in workers

--- subprojects/workers/src/integTest/groovy/org/gradle/workers/internal/WorkerExecutorErrorHandlingIntegrationTest.groovy
+++ subprojects/workers/src/integTest/groovy/org/gradle/workers/internal/WorkerExecutorErrorHandlingIntegrationTest.groovy
@@ -70,7 +70,7 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
 
     def "produces a sensible error when a parameter can't be serialized"() {
         withRunnableClassInBuildSrc()
-        withUnserializableParameterInBuildSrc()
+        withParameterMemberThatFailsSerialization()
 
         buildFile << """
             $alternateRunnable
@@ -91,7 +91,7 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
         then:
         failureHasCause("A failure occurred while executing org.gradle.test.TestRunnable")
         failureCauseContains("Could not write message")
-        errorOutput.contains("Caused by: java.io.NotSerializableException: org.gradle.error.Bar")
+        failureHasCause("Broken")
 
         and:
         executedAndNotSkipped(":runAgainInDaemon")
@@ -99,9 +99,8 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
     }
 
     def "produces a sensible error when a parameter can't be de-serialized in the worker"() {
-        def parameterJar = file("parameter.jar")
         withRunnableClassInBuildSrc()
-        withUnserializableParameterMemberInExternalJar(parameterJar)
+        withParameterMemberThatFailsDeserialization()
 
         buildFile << """  
             $alternateRunnable
@@ -111,7 +110,6 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
             }
 
             task runInDaemon(type: DaemonTask) {
-                additionalClasspath = files('${parameterJar.name}')
                 foo = new FooWithUnserializableBar()
                 finalizedBy runAgainInDaemon
             }
@@ -123,7 +121,7 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
         then:
         failureHasCause("A failure occurred while executing org.gradle.test.TestRunnable")
         failureCauseContains("Could not read message")
-        errorOutput.contains("Caused by: java.lang.ClassNotFoundException: org.gradle.error.Bar")
+        failureHasCause("Broken")
 
         and:
         executedAndNotSkipped(":runAgainInDaemon")
@@ -241,11 +239,32 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
         """
     }
 
-    String getUnserializableClass() {
+    String getClassThatFailsDeserialization() {
         return """
-            package org.gradle.error;
+            package org.gradle.other;
             
-            public class Bar {
+            import java.io.Serializable;
+            import java.io.IOException;
+            
+            public class Bar implements Serializable {
+                private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+                    throw new IOException("Broken");
+                }
+            }
+        """
+    }
+
+    String getClassThatFailsSerialization() {
+        return """
+            package org.gradle.other;
+            
+            import java.io.Serializable;
+            import java.io.IOException;
+            
+            public class Bar implements Serializable {
+                private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+                    throw new IOException("Broken");
+                }
             }
         """
     }
@@ -254,7 +273,6 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
         return """
             package org.gradle.other;
             
-            import org.gradle.error.Bar;
             import java.io.Serializable;
             
             public class FooWithUnserializableBar extends Foo implements Serializable {
@@ -276,10 +294,10 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
         """
     }
 
-    void withUnserializableParameterInBuildSrc() {
+    void withParameterMemberThatFailsSerialization() {
         // Create an un-serializable class
-        file('buildSrc/src/main/java/org/gradle/error/Bar.java').text = """
-            $unserializableClass
+        file('buildSrc/src/main/java/org/gradle/other/Bar.java').text = """
+            $classThatFailsSerialization
         """
 
         // Create a Foo class with an un-serializable member
@@ -290,29 +308,17 @@ class WorkerExecutorErrorHandlingIntegrationTest extends AbstractWorkerExecutorI
         addImportToBuildScript("org.gradle.other.FooWithUnserializableBar")
     }
 
-    void withUnserializableParameterMemberInExternalJar(File parameterJar) {
-        def builder = artifactBuilder()
-
-        builder.sourceFile("org/gradle/error/Bar.java") << """
-            $unserializableClass
-        """
-
+    void withParameterMemberThatFailsDeserialization() {
         // Overwrite the Foo class with a class with an un-serializable member
         file('buildSrc/src/main/java/org/gradle/other/FooWithUnserializableBar.java').text = """
             $parameterClassWithUnserializableMember
         """
 
-        // A serializable form of the class so we can get past sending the message
+        // An unserializable member class
         file('buildSrc/src/main/java/org/gradle/error/Bar.java').text = """
-            package org.gradle.error;
-            
-            import java.io.Serializable;
-            
-            public class Bar implements Serializable {
-            }
+            $classThatFailsDeserialization
         """
 
-        builder.buildJar(parameterJar)
         addImportToBuildScript("org.gradle.other.FooWithUnserializableBar")
     }
 
