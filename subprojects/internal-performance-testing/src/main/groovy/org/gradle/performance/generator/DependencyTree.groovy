ee0eb9f141bf4a22cb965a7edd377dac7c576a25
Jendrik Johannes <jendrik@gradle.com>
2017-03-10 17:31:33 +0100

    Cleanup, document and test dependency tree generation

--- subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/generator/DependencyTree.groovy
+++ subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/generator/DependencyTree.groovy
@@ -18,80 +18,148 @@ package org.gradle.performance.generator
 
 import org.gradle.internal.Pair
 
+/**
+ * Generates a tree of dependencies between classes and sub-projects. It can be configured with
+ * the number of projects and classes in each project by multiple calls to calculateClassDependencies().
+ *
+ * Reflects project dependencies in class dependencies and respects 'api' dependencies.
+ * See DependencyTreeTest for details of the tree construction.
+ */
 class DependencyTree {
 
-    private List<Pair<Integer, Integer>> nodeSets = []
+    /**
+     * Index of project dependency (declaration order)that is treated as 'api' dependency
+     */
+    static final API_DEPENDENCY_INDEX = 0
+    /**
+     * Classes on which 'level' are used for cross-project dependencies
+     */
+    static final CROSS_PROJECT_CLASS_DEPENDENCY_LEVEL = 2
+    /**
+     * How many sibling classes are put onto one 'level' in the class dependency tree
+     */
+    static final CLASSES_ON_LEVEL = 3
+    /**
+     * How many sibling project are put onto one 'level' in the project dependency tree
+     */
+    static final PROJECTS_ON_LEVEL = 3
 
-    private List<List<Integer>> levelToNodes = []
-    private Map<Integer, List<Integer>> parentToChildrenNodes = new HashMap<>()
+    private List<Pair<Integer, Integer>> projectClassIdRanges = []
 
-    private List<List<Integer>> levelToNodeSets = []
-    private Map<Integer, List<Integer>> parentToChildrenNodeSets = new HashMap<>()
+    private List<List<Integer>> projectLevelToProjectIds = []
+    private List<List<Integer>> classLevelToClassIds = []
 
-    private Set<Integer> transitiveNodes = new HashSet<>()
+    private Map<Integer, List<Integer>> parentToChildClassIds = new HashMap<>()
+    private Map<Integer, List<Integer>> parentToChildProjectIds = new HashMap<>()
 
-    Map<Integer, List<Integer>> getParentToChildrenNodes() {
-        return parentToChildrenNodes
+    private Set<Integer> transitiveClassIds = new HashSet<>()
+
+    def getChildProjectIds(Integer parentProjectId) {
+        return parentToChildProjectIds.get(parentProjectId)
     }
 
-    Map<Integer, List<Integer>> getParentToChildrenNodeSets() {
-        return parentToChildrenNodeSets
+    def hasParentProject(Integer childProjectId) {
+        if (childProjectId == null) {
+            return false
+        }
+        for (def childLists : parentToChildProjectIds.values()) {
+            if (childLists.contains(childProjectId)) {
+                return true
+            }
+        }
+        false
     }
 
-    DependencyTree calcNodeDependencies(int firstNodeInSet, int lastNodeInSet, int groupSize) {
-        nodeSets.add(Pair.of(firstNodeInSet, lastNodeInSet))
-        (firstNodeInSet..lastNodeInSet).each {
-            calcLevel(it, 0, firstNodeInSet, lastNodeInSet, groupSize, levelToNodes, parentToChildrenNodes)
+    def getProjectIdForClass(int classId) {
+        for (int projectId = 0; projectId < projectClassIdRanges.size(); projectId++) {
+            if (isInProject(classId, projectId)) {
+                return projectId
+            }
+        }
+    }
+
+    def isInProject(int classId, int projectId) {
+        if (projectClassIdRanges.size() <= projectId) {
+            return false
+        }
+        def range = projectClassIdRanges.get(projectId)
+        isInRange(classId, range.left, range.right)
+    }
+
+    def getTransitiveChildClassIds(int parentClassId) {
+        List<Integer> result = []
+        result.addAll(parentToChildClassIds.get(parentClassId))
+        parentToChildClassIds.get(parentClassId).each {
+            allVisibleChildClassIds(it, result)
+        }
+        return result
+    }
+
+    private allVisibleChildClassIds(int parentClassId, List<Integer> result) {
+        def directChildrenIds = parentToChildClassIds.get(parentClassId)
+        if (directChildrenIds && !directChildrenIds.isEmpty()) {
+            directChildrenIds.each {
+                if (transitiveClassIds.contains(it)) {
+                    result.add(it)
+                    allVisibleChildClassIds(it, result)
+                }
+            }
+        }
+    }
+
+    DependencyTree calculateClassDependencies(int first, int last) {
+        projectClassIdRanges.add(Pair.of(first, last))
+        (first..last).each {
+            placeOnLevel(it, 0, first, last, CLASSES_ON_LEVEL, classLevelToClassIds, parentToChildClassIds)
         }
         this
     }
 
-    DependencyTree calcNodeSetDependencies(int groupSize) {
-        (0..nodeSets.size() - 1).each {
-            calcLevel(it, 0, 0, Integer.MAX_VALUE, groupSize, levelToNodeSets, parentToChildrenNodeSets)
+    DependencyTree calculateProjectDependencies() {
+        (0..projectClassIdRanges.size() - 1).each {
+            placeOnLevel(it, 0, 0, Integer.MAX_VALUE, PROJECTS_ON_LEVEL, projectLevelToProjectIds, parentToChildProjectIds)
         }
-        def dependencyLevel = 2
-        //for each class on the consumer side, add a dependency on the provider side
-        for (int parentNumber = 0; parentNumber < nodeSets.size(); parentNumber++) {
-            def firstNodeSet = true
-            for (int childNumber : parentToChildrenNodeSets.get(parentNumber)) {
-                Pair<Integer, Integer> parentNodeSet = nodeSets.get(parentNumber)
-                Pair<Integer, Integer> childNodeSet = nodeSets.get(childNumber)
-                def parentNodes = []
-                for (Integer parentNode : levelToNodes.get(dependencyLevel)) {
-                    if (isInNodeSet(parentNode, parentNodeSet.left, parentNodeSet.right)) {
-                        parentNodes.add(parentNode)
-                    }
-                }
-                int idx = 0
-                for (Integer childNode : levelToNodes.get(dependencyLevel)) {
-                    if (isInNodeSet(childNode, childNodeSet.left, childNodeSet.right)) {
-                        parentToChildrenNodes.get(parentNodes.get(idx)).add(childNode)
-                        if (firstNodeSet) {
-                            transitiveNodes.add(childNode)
+
+        //for each class on CROSS_PROJECT_CLASS_DEPENDENCY_LEVEL of the parent project, add a dependency to a class on the same level of each child project
+        for (int parentProjectId = 0; parentProjectId < projectClassIdRanges.size(); parentProjectId++) {
+            // the first project dependency is treated as `api` dependency and corresponding classes are marked as transient
+            def projectIndex = 0
+            for (int childProjectId : parentToChildProjectIds.get(parentProjectId)) {
+                def parentClassIds = []
+                if (classLevelToClassIds.size() > CROSS_PROJECT_CLASS_DEPENDENCY_LEVEL) {
+                    for (Integer parentClassInParentProject : classLevelToClassIds.get(CROSS_PROJECT_CLASS_DEPENDENCY_LEVEL)) {
+                        if (isInProject(parentClassInParentProject, parentProjectId)) {
+                            parentClassIds.add(parentClassInParentProject)
                         }
-                        idx++
                     }
                 }
-                firstNodeSet = false
+                if (!parentClassIds.empty) {
+                    int classIndex = 0
+                    for (Integer childClassInChildProject : classLevelToClassIds.get(CROSS_PROJECT_CLASS_DEPENDENCY_LEVEL)) {
+                        if (isInProject(childClassInChildProject, childProjectId)) {
+                            parentToChildClassIds.get(parentClassIds.get(classIndex)).add(childClassInChildProject)
+                            if (projectIndex == API_DEPENDENCY_INDEX) {
+                                transitiveClassIds.add(childClassInChildProject)
+                            }
+                            classIndex++
+                        }
+                    }
+                }
+                projectIndex++
             }
         }
         this
     }
 
-    private isInNodeSet(int node, int firstNodeInSet, int lastNodeInSet) {
-        firstNodeInSet <= node && node <= lastNodeInSet
-    }
-
-    private calcLevel(int node, int level, int firstNodeInSet, int lastNodeInSet, nodesPerGroup, List<List<Integer>> levelList, Map<Integer, List<Integer>> parentMap) {
-        if (freeSpotAbove(level, firstNodeInSet, lastNodeInSet, nodesPerGroup, levelList)) {
+    private placeOnLevel(int node, int level, int firstInRange, int lastInRange, nodesPerGroup, List<List<Integer>> levelList, Map<Integer, List<Integer>> parentMap) {
+        if (levelAboveHasFreeSpot(level, firstInRange, lastInRange, nodesPerGroup, levelList)) {
             //a free spot on one of the level above
-            calcLevel(node, level + 1, firstNodeInSet, lastNodeInSet, nodesPerGroup, levelList, parentMap)
+            placeOnLevel(node, level + 1, firstInRange, lastInRange, nodesPerGroup, levelList, parentMap)
         } else {
             addToLevel(levelList, level, node)
             List<Integer> lastGroupOnLevelBelow
             if (level > 0) {
-                def nodesOnLevelBelow = getFromLevel(levelList, firstNodeInSet, lastNodeInSet, level - 1)
+                def nodesOnLevelBelow = getFromLevel(levelList, firstInRange, lastInRange, level - 1)
                 lastGroupOnLevelBelow = new ArrayList<>(nodesOnLevelBelow.subList(nodesOnLevelBelow.size() - nodesPerGroup, nodesOnLevelBelow.size()))
             } else {
                 lastGroupOnLevelBelow = []
@@ -100,14 +168,14 @@ class DependencyTree {
         }
     }
 
-    private freeSpotAbove(int level, int firstNodeInSet, int lastNodeInSet, nodesPerGroup, List<List<Integer>> levelList) {
-        int nodesOnCurrentLevel = getFromLevel(levelList, firstNodeInSet, lastNodeInSet, level).size()
+    private levelAboveHasFreeSpot(int level, int firstInRange, int lastInRange, nodesPerGroup, List<List<Integer>> levelList) {
+        int nodesOnCurrentLevel = getFromLevel(levelList, firstInRange, lastInRange, level).size()
         int fullGroupsOnCurrentLevel = (int) (nodesOnCurrentLevel / nodesPerGroup)
-        int nodesOnLevelAbove = getFromLevel(levelList, firstNodeInSet, lastNodeInSet, level + 1).size()
+        int nodesOnLevelAbove = getFromLevel(levelList, firstInRange, lastInRange, level + 1).size()
         if (nodesOnCurrentLevel == 0 && nodesOnLevelAbove == 0) {
             return false
         }
-        nodesOnLevelAbove < fullGroupsOnCurrentLevel || freeSpotAbove(level + 1, firstNodeInSet, lastNodeInSet, nodesPerGroup, levelList)
+        nodesOnLevelAbove < fullGroupsOnCurrentLevel || levelAboveHasFreeSpot(level + 1, firstInRange, lastInRange, nodesPerGroup, levelList)
     }
 
     private getFromLevel(List<List<Integer>> list, int firstNodeInSet, int lastNodeInSet, int level) {
@@ -124,85 +192,18 @@ class DependencyTree {
         list.get(level).add(node)
     }
 
-    def allChildrenNodes(int node) {
-        List<Integer> result = []
-        result.addAll(parentToChildrenNodes.get(node))
-        parentToChildrenNodes.get(node).each {
-            allVisibleChildrenNodes(it, result)
-        }
-        return result
-    }
-
-    private allVisibleChildrenNodes(int node, List<Integer> result) {
-        if (parentToChildrenNodes.containsKey(node) && !parentToChildrenNodes.get(node).isEmpty()) {
-            parentToChildrenNodes.get(node).each {
-                if (transitiveNodes.contains(it)) {
-                    result.add(it)
-                    allVisibleChildrenNodes(it, result)
-                }
-            }
-        }
-    }
-
     private filterBySet(List<Integer> list, int firstNodeInSet, int lastNodeInSet) {
         def filtered = []
         for (Integer node : list) {
-            if (isInNodeSet(node, firstNodeInSet, lastNodeInSet)) {
+            if (isInRange(node, firstNodeInSet, lastNodeInSet)) {
                 filtered.add(node)
             }
         }
         return filtered
     }
 
-    int findNodeSet(int node) {
-        for (int nodeSet = 0; nodeSet < nodeSets.size(); nodeSet++) {
-            if (isInNodeSet(node, nodeSets.get(nodeSet).left, nodeSets.get(nodeSet).right)) {
-                return nodeSet
-            }
-        }
+    private isInRange(int id, int first, int last) {
+        first <= id && id <= last
     }
 
-    def hasParentNodeSets(Integer nodeSet) {
-        if (nodeSet == null) {
-            return false
-        }
-        for (def childLists : parentToChildrenNodeSets.values()) {
-            if (childLists.contains(nodeSet)) {
-                return true
-            }
-        }
-        false
-    }
-
-    @Override
-    String toString() {
-        printTree("", "")
-    }
-
-    def printTree(String prefix, String suffix) {
-        def printed = '**Levels: ' + levelToNodes.size() - 1 + "**\n"
-        for (int node : levelToNodes.get(levelToNodes.size() - 1)) {
-            printed += printNodeWithChildren(node, '', prefix, suffix)
-        }
-        printed
-    }
-
-    def printNodeWithChildren(int node, String indent, String prefix, String suffix) {
-        def printed = indent + "->" + prefix + node + suffix + '\n'
-        for (int child : parentToChildrenNodes.get(node)) {
-            printed += printNodeWithChildren(child, indent + '--', prefix, suffix)
-        }
-        printed
-    }
-
-    static void main(String[] args) {
-        def tree = new DependencyTree()
-        tree.calcNodeDependencies(0, 99, 3)
-        tree.calcNodeDependencies(100, 199, 3)
-        tree.calcNodeDependencies(200, 299, 3)
-        tree.calcNodeDependencies(300, 399, 3)
-        tree.calcNodeSetDependencies(3)
-
-        print tree.printTree("Class", ".java")
-    }
 }
