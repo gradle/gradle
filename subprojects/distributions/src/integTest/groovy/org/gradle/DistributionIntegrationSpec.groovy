7673335204e2f1cad810ec84fc85449264a0737c
Lóránt Pintér <lorant@gradle.com>
2017-03-16 18:26:57 +0100

    Fix core libs count
    +review REVIEW-6484

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -112,7 +112,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
         def coreLibs = contentsDir.file("lib").listFiles().findAll {
             it.name.startsWith("gradle-") && !it.name.startsWith("gradle-script-kotlin")
         }
-        assert coreLibs.size() == 20
+        assert coreLibs.size() == 21
         coreLibs.each { assertIsGradleJar(it) }
 
         def toolingApiJar = contentsDir.file("lib/gradle-tooling-api-${baseVersion}.jar")
