d14a384d0a0ec88b37c76889509f9b9f2b033386
Adam Murdoch <adam@gradle.com>
2017-03-22 11:39:36 +1100

    Fixed broken unit tests.

--- subprojects/core/src/testFixtures/groovy/org/gradle/util/TestUtil.groovy
+++ subprojects/core/src/testFixtures/groovy/org/gradle/util/TestUtil.groovy
@@ -18,7 +18,6 @@ package org.gradle.util
 import org.codehaus.groovy.control.CompilerConfiguration
 import org.gradle.api.Task
 import org.gradle.api.internal.AsmBackedClassGenerator
-import org.gradle.api.internal.ClassGeneratorBackedInstantiator
 import org.gradle.api.internal.DefaultInstantiatorFactory
 import org.gradle.api.internal.InstantiatorFactory
 import org.gradle.api.internal.project.ProjectInternal
@@ -26,7 +25,6 @@ import org.gradle.api.internal.project.taskfactory.ITaskFactory
 import org.gradle.groovy.scripts.DefaultScript
 import org.gradle.groovy.scripts.Script
 import org.gradle.groovy.scripts.ScriptSource
-import org.gradle.internal.reflect.DirectInstantiator
 import org.gradle.test.fixtures.file.TestDirectoryProvider
 import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.testfixtures.internal.NativeServicesTestFixture
@@ -45,7 +43,7 @@ class TestUtil {
 
     static InstantiatorFactory instantiatorFactory() {
         def generator = new AsmBackedClassGenerator()
-        return new DefaultInstantiatorFactory(generator, new ClassGeneratorBackedInstantiator(generator, DirectInstantiator.INSTANCE))
+        return new DefaultInstantiatorFactory(generator)
     }
 
     static TestUtil create(File rootDir) {
