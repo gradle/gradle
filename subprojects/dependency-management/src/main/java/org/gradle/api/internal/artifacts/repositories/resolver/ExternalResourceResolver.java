c275a4222573b7aca1f2b0550207928c2f0a632e
Cedric Champeau <cedric@gradle.com>
2017-04-27 17:54:47 +0200

    Check the remote access too, when a descriptor is not found in the local one, nor in the binary cache

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/repositories/resolver/ExternalResourceResolver.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/repositories/resolver/ExternalResourceResolver.java
@@ -60,6 +60,7 @@ import org.gradle.internal.resolve.result.BuildableModuleVersionListingResolveRe
 import org.gradle.internal.resolve.result.BuildableTypedResolveResult;
 import org.gradle.internal.resolve.result.DefaultResourceAwareResolveResult;
 import org.gradle.internal.resolve.result.ResourceAwareResolveResult;
+import org.gradle.internal.resource.ExternalResourceName;
 import org.gradle.internal.resource.local.ByteArrayLocalResource;
 import org.gradle.internal.resource.local.FileLocalResource;
 import org.gradle.internal.resource.local.FileStore;
@@ -99,6 +100,7 @@ public abstract class ExternalResourceResolver<T extends ModuleComponentResolveM
     private final VersionLister versionLister;
 
     private String id;
+    private ExternalResourceArtifactResolver cachedArtifactResolver;
 
     protected ExternalResourceResolver(String name,
                                        boolean local,
@@ -295,7 +297,12 @@ public abstract class ExternalResourceResolver<T extends ModuleComponentResolveM
     }
 
     protected ExternalResourceArtifactResolver createArtifactResolver() {
-        return createArtifactResolver(ivyPatterns, artifactPatterns);
+        if (cachedArtifactResolver != null) {
+            return cachedArtifactResolver;
+        }
+        ExternalResourceArtifactResolver artifactResolver = createArtifactResolver(ivyPatterns, artifactPatterns);
+        cachedArtifactResolver = artifactResolver;
+        return artifactResolver;
     }
 
     protected ExternalResourceArtifactResolver createArtifactResolver(List<ResourcePattern> ivyPatterns, List<ResourcePattern> artifactPatterns) {
@@ -349,12 +356,17 @@ public abstract class ExternalResourceResolver<T extends ModuleComponentResolveM
     }
 
     protected void addIvyPattern(ResourcePattern pattern) {
-        id = null;
+        invalidateCaches();
         ivyPatterns.add(pattern);
     }
 
-    protected void addArtifactPattern(ResourcePattern pattern) {
+    private void invalidateCaches() {
         id = null;
+        cachedArtifactResolver = null;
+    }
+
+    protected void addArtifactPattern(ResourcePattern pattern) {
+        invalidateCaches();
         artifactPatterns.add(pattern);
     }
 
@@ -375,13 +387,13 @@ public abstract class ExternalResourceResolver<T extends ModuleComponentResolveM
     }
 
     protected void setIvyPatterns(Iterable<? extends ResourcePattern> patterns) {
-        id = null;
+        invalidateCaches();
         ivyPatterns.clear();
         CollectionUtils.addAll(ivyPatterns, patterns);
     }
 
     protected void setArtifactPatterns(List<ResourcePattern> patterns) {
-        id = null;
+        invalidateCaches();
         artifactPatterns.clear();
         CollectionUtils.addAll(artifactPatterns, patterns);
     }
@@ -495,7 +507,8 @@ public abstract class ExternalResourceResolver<T extends ModuleComponentResolveM
 
         @Override
         public boolean isMetadataAvailableLocally(ModuleComponentIdentifier moduleComponentIdentifier) {
-            return false;
+            ModuleComponentArtifactMetadata artifact = getMetaDataArtifactFor(moduleComponentIdentifier);
+            return createArtifactResolver().artifactExists(artifact, NoOpResourceAwareResolveResult.INSTANCE);
         }
 
     }
@@ -520,6 +533,31 @@ public abstract class ExternalResourceResolver<T extends ModuleComponentResolveM
             if (i < len - 1) {
                 sb.append(",");
             }
+        }
+    }
+
+    private static class NoOpResourceAwareResolveResult implements ResourceAwareResolveResult {
+
+        private static final NoOpResourceAwareResolveResult INSTANCE = new NoOpResourceAwareResolveResult();
+
+        @Override
+        public List<String> getAttempted() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public void attempted(String locationDescription) {
+
+        }
+
+        @Override
+        public void attempted(ExternalResourceName location) {
+
+        }
+
+        @Override
+        public void applyTo(ResourceAwareResolveResult target) {
+            throw new UnsupportedOperationException();
         }
     }
 }
