f22472b8fc6aca79f2b20da80f96ae52dfb80058
Rene Groeschke <rene@gradle.com>
2017-04-29 03:05:02 +0200

    Revert "bring back OperationResult for compatibility reasons"
    This reverts commit c343bea46d9d1efd0ee088a8bee7b4f884d86317.

--- subprojects/base-services/src/main/java/org/gradle/internal/progress/OperationFinishEvent.java
+++ subprojects/base-services/src/main/java/org/gradle/internal/progress/OperationFinishEvent.java
@@ -16,11 +16,38 @@
 
 package org.gradle.internal.progress;
 
+import org.gradle.api.Nullable;
 /**
  * This class is consumed by the build scan plugin.
  * */
-public final class OperationFinishEvent extends OperationResult {
+public final class OperationFinishEvent {
+    private final long startTime;
+    private final long endTime;
+    private final Throwable failure;
+    private final Object result;
+
     public OperationFinishEvent(long startTime, long endTime, Throwable failure, Object result) {
-        super(startTime, endTime, failure, result);
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.failure = failure;
+        this.result = result;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    @Nullable
+    public Throwable getFailure() {
+        return failure;
+    }
+
+    @Nullable
+    public Object getResult() {
+        return result;
     }
 }
