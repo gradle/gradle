d5bd6fde53a9c91672ddd7009dc2dfff9d9def7a
Luke Daley <luke@gradle.com>
2017-05-05 15:34:19 +1000

    Move test to a more appropriate home.

--- subprojects/core/src/test/groovy/org/gradle/caching/internal/FinalizeBuildCacheConfigurationBuildOperationIntegrationTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/caching/internal/FinalizeBuildCacheConfigurationBuildOperationIntegrationTest.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.caching.http.internal
+package org.gradle.caching.internal
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.BuildOperationsFixture
