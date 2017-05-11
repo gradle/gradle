8e93f7c393fbb9d352a724f5a3625e04f0ad28b8
Benjamin Muschko <ben@gradle.com>
2017-05-11 12:53:19 -0400

    Revert move of methods

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/DefaultGradleDistribution.java
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/DefaultGradleDistribution.java
@@ -59,7 +59,7 @@ public class DefaultGradleDistribution implements GradleDistribution {
 
     public boolean worksWith(Jvm jvm) {
         // Milestone 4 was broken on the IBM jvm
-        if (jvm.isIbmJvm() && version.isVersion("1.0-milestone-4")) {
+        if (jvm.isIbmJvm() && isVersion("1.0-milestone-4")) {
             return false;
         }
 
@@ -73,17 +73,17 @@ public class DefaultGradleDistribution implements GradleDistribution {
 
     private boolean worksWith(JavaVersion javaVersion) {
         // 0.9-rc-1 was broken for Java 5
-        if (version.isVersion("0.9-rc-1") && javaVersion == JavaVersion.VERSION_1_5) {
+        if (isVersion("0.9-rc-1") && javaVersion == JavaVersion.VERSION_1_5) {
             return false;
         }
 
         // 1.x works on Java 5 - 8
-        if (version.isSameOrOlder("1.12")) {
+        if (isSameOrOlder("1.12")) {
             return javaVersion.compareTo(JavaVersion.VERSION_1_5) >= 0 && javaVersion.compareTo(JavaVersion.VERSION_1_8) <= 0;
         }
 
         // 2.x and 3.0-milestone-1 work on Java 6 - 8
-        if (version.isSameOrOlder("3.0-milestone-1")) {
+        if (isSameOrOlder("3.0-milestone-1")) {
             return javaVersion.compareTo(JavaVersion.VERSION_1_6) >= 0 && javaVersion.compareTo(JavaVersion.VERSION_1_8) <= 0;
         }
 
@@ -94,7 +94,7 @@ public class DefaultGradleDistribution implements GradleDistribution {
     public boolean worksWith(OperatingSystem os) {
         // 1.0-milestone-5 was broken where jna was not available
         //noinspection SimplifiableIfStatement
-        if (version.isVersion("1.0-milestone-5")) {
+        if (isVersion("1.0-milestone-5")) {
             return os.isWindows() || os.isMacOsX() || os.isLinux();
         } else {
             return true;
@@ -102,15 +102,15 @@ public class DefaultGradleDistribution implements GradleDistribution {
     }
 
     public boolean isDaemonIdleTimeoutConfigurable() {
-        return version.isSameOrNewer("1.0-milestone-7");
+        return isSameOrNewer("1.0-milestone-7");
     }
 
     public boolean isOpenApiSupported() {
-        return version.isSameOrNewer("0.9-rc-1") && !version.isSameOrNewer("2.0-rc-1");
+        return isSameOrNewer("0.9-rc-1") && !isSameOrNewer("2.0-rc-1");
     }
 
     public boolean isToolingApiSupported() {
-        return version.isSameOrNewer("1.0-milestone-3");
+        return isSameOrNewer("1.0-milestone-3");
     }
 
     @Override
@@ -120,62 +120,62 @@ public class DefaultGradleDistribution implements GradleDistribution {
 
     public boolean isToolingApiNonAsciiOutputSupported() {
         if (OperatingSystem.current().isWindows()) {
-            return !version.isVersion("1.0-milestone-7") && !version.isVersion("1.0-milestone-8") && !version.isVersion("1.0-milestone-8a");
+            return !isVersion("1.0-milestone-7") && !isVersion("1.0-milestone-8") && !isVersion("1.0-milestone-8a");
         }
         return true;
     }
 
     public boolean isToolingApiDaemonBaseDirSupported() {
-        return version.isSameOrNewer("2.2-rc-1");
+        return isSameOrNewer("2.2-rc-1");
     }
 
     @Override
     public boolean isToolingApiEventsInEmbeddedModeSupported() {
-        return version.isSameOrNewer("2.6-rc-1");
+        return isSameOrNewer("2.6-rc-1");
     }
 
     @Override
     public boolean isToolingApiLocksBuildActionClasses() {
-        return version.isSameOrOlder("3.0");
+        return isSameOrOlder("3.0");
     }
 
     @Override
     public boolean isToolingApiLoggingInEmbeddedModeSupported() {
-        return version.isSameOrNewer("2.9-rc-1");
+        return isSameOrNewer("2.9-rc-1");
     }
 
     public VersionNumber getArtifactCacheLayoutVersion() {
-        if (version.isSameOrNewer("3.2-rc-1")) {
+        if (isSameOrNewer("3.2-rc-1")) {
             return VersionNumber.parse("2.23");
-        } else if (version.isSameOrNewer("3.1-rc-1")) {
+        } else if (isSameOrNewer("3.1-rc-1")) {
             return VersionNumber.parse("2.21");
-        } else if (version.isSameOrNewer("3.0-milestone-1")) {
+        } else if (isSameOrNewer("3.0-milestone-1")) {
             return VersionNumber.parse("2.17");
-        } else if (version.isSameOrNewer("2.8-rc-1")) {
+        } else if (isSameOrNewer("2.8-rc-1")) {
             return VersionNumber.parse("2.16");
-        } else if (version.isSameOrNewer("2.4-rc-1")) {
+        } else if (isSameOrNewer("2.4-rc-1")) {
             return VersionNumber.parse("2.15");
-        } else if (version.isSameOrNewer("2.2-rc-1")) {
+        } else if (isSameOrNewer("2.2-rc-1")) {
             return VersionNumber.parse("2.14");
-        } else if (version.isSameOrNewer("2.1-rc-3")) {
+        } else if (isSameOrNewer("2.1-rc-3")) {
             return VersionNumber.parse("2.13");
-        } else if (version.isSameOrNewer("2.0-rc-1")) {
+        } else if (isSameOrNewer("2.0-rc-1")) {
             return VersionNumber.parse("2.12");
-        } else if (version.isSameOrNewer("1.12-rc-1")) {
+        } else if (isSameOrNewer("1.12-rc-1")) {
             return VersionNumber.parse("2.6");
-        } else if (version.isSameOrNewer("1.11-rc-1")) {
+        } else if (isSameOrNewer("1.11-rc-1")) {
             return VersionNumber.parse("2.2");
-        } else if (version.isSameOrNewer("1.9-rc-2")) {
+        } else if (isSameOrNewer("1.9-rc-2")) {
             return VersionNumber.parse("2.1");
-        } else if (version.isSameOrNewer("1.9-rc-1")) {
+        } else if (isSameOrNewer("1.9-rc-1")) {
             return VersionNumber.parse("1.31");
-        } else if (version.isSameOrNewer("1.7-rc-1")) {
+        } else if (isSameOrNewer("1.7-rc-1")) {
             return VersionNumber.parse("0.26");
-        } else if (version.isSameOrNewer("1.6-rc-1")) {
+        } else if (isSameOrNewer("1.6-rc-1")) {
             return VersionNumber.parse("0.24");
-        } else if (version.isSameOrNewer("1.4-rc-1")) {
+        } else if (isSameOrNewer("1.4-rc-1")) {
             return VersionNumber.parse("0.23");
-        } else if (version.isSameOrNewer("1.3")) {
+        } else if (isSameOrNewer("1.3")) {
             return VersionNumber.parse("0.15");
         } else {
             return VersionNumber.parse("0.1");
@@ -183,15 +183,15 @@ public class DefaultGradleDistribution implements GradleDistribution {
     }
 
     public boolean wrapperCanExecute(GradleVersion version) {
-        if (version.equals(GradleVersion.version("0.8")) || version.isVersion("0.8")) {
+        if (version.equals(GradleVersion.version("0.8")) || isVersion("0.8")) {
             // There was a breaking change after 0.8
             return false;
         }
-        if (version.isVersion("0.9.1")) {
+        if (isVersion("0.9.1")) {
             // 0.9.1 couldn't handle anything with a timestamp whose timezone was behind GMT
             return version.getVersion().matches(".*+\\d{4}");
         }
-        if (version.isSameOrNewer("0.9.2") && version.isSameOrOlder("1.0-milestone-2")) {
+        if (isSameOrNewer("0.9.2") && isSameOrOlder("1.0-milestone-2")) {
             // These versions couldn't handle milestone patches
             if (version.getVersion().matches("1.0-milestone-\\d+[a-z]-.+")) {
                 return false;
@@ -201,15 +201,27 @@ public class DefaultGradleDistribution implements GradleDistribution {
     }
 
     public boolean isWrapperSupportsGradleUserHomeCommandLineOption() {
-        return version.isSameOrNewer("1.7");
+        return isSameOrNewer("1.7");
     }
 
     public boolean isSupportsSpacesInGradleAndJavaOpts() {
-        return version.isSameOrNewer("1.0-milestone-5");
+        return isSameOrNewer("1.0-milestone-5");
     }
 
     public boolean isFullySupportsIvyRepository() {
-        return version.isSameOrNewer("1.0-milestone-7");
+        return isSameOrNewer("1.0-milestone-7");
     }
 
+    protected boolean isSameOrNewer(String otherVersion) {
+        return isVersion(otherVersion) || version.compareTo(GradleVersion.version(otherVersion)) > 0;
+    }
+
+    protected boolean isSameOrOlder(String otherVersion) {
+        return isVersion(otherVersion) || version.compareTo(GradleVersion.version(otherVersion)) <= 0;
+    }
+
+    protected boolean isVersion(String otherVersionString) {
+        GradleVersion otherVersion = GradleVersion.version(otherVersionString);
+        return version.compareTo(otherVersion) == 0 || (version.isSnapshot() && version.getBaseVersion().equals(otherVersion.getBaseVersion()));
+    }
 }
