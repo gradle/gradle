94237bb8672e1b28d8f2af5c64f2e84d7af9422f
Pepper Lebeck-Jobe <pepper@gradle.com>
2017-05-21 18:43:55 +0900

    Add ValueSnapshotStrategy
    This allows us to break apart the creation of an isolatable value
    snapshot and a regular value snapshot so that we don't have to pay the
    overhead of creatig the extra metadata when we don't plan to use it.

--- subprojects/core/src/main/java/org/gradle/api/internal/changedetection/state/NullValueSnapshot.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/changedetection/state/NullValueSnapshot.java
@@ -34,6 +34,14 @@ public class NullValueSnapshot implements ValueSnapshot, Isolatable<Object> {
     }
 
     @Override
+    public ValueSnapshot isolatableSnapshot(Object value, ValueSnapshotter snapshotter) {
+        if (value == null) {
+            return this;
+        }
+        return snapshotter.isolatableSnapshot(value);
+    }
+
+    @Override
     public void appendToHasher(BuildCacheHasher hasher) {
         hasher.putNull();
     }
