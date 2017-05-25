5367f27a63c5e4a96322177f6218de84790d22e0
Sterling Greene <sterling@gradle.com>
2017-05-25 15:45:08 -0400

    Make a TAPI test also use the plain console

--- subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/ToolingApiIntegrationTest.groovy
+++ subprojects/tooling-api/src/integTest/groovy/org/gradle/integtests/tooling/ToolingApiIntegrationTest.groovy
@@ -241,8 +241,8 @@ allprojects {
                         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
                         launcher.setStandardOutput(outputStream);
                         launcher.setStandardError(outputStream);
-                        launcher.setColorOutput(true);
-                        System.out.println("About to run the build");
+                        launcher.setColorOutput(${withColor});
+                        System.out.println("About to run the build with color=" + ${withColor});
                         // Run the build
                         launcher.run();
                         System.out.println("Build was successful");
@@ -298,5 +298,8 @@ allprojects {
         }
 
         handle.waitForFinish()
+
+        where:
+        withColor << [true, false]
     }
 }
