a558531a3851fe9ca0645a13a1d138863fa52b69
Luke Daley <luke@gradle.com>
2017-05-25 21:05:45 +1000

    Expose the “up to date messages” to build scans.

--- subprojects/core/src/main/java/org/gradle/internal/operations/notify/BuildOperationNotificationBridge.java
+++ subprojects/core/src/main/java/org/gradle/internal/operations/notify/BuildOperationNotificationBridge.java
@@ -16,6 +16,7 @@
 
 package org.gradle.internal.operations.notify;
 
+import org.gradle.api.Nullable;
 import org.gradle.api.execution.internal.ExecuteTaskBuildOperationDetails;
 import org.gradle.internal.concurrent.Stoppable;
 import org.gradle.internal.progress.BuildOperationDescriptor;
@@ -115,12 +116,12 @@ class BuildOperationNotificationBridge implements BuildOperationNotificationList
         @Override
         public void finished(BuildOperationDescriptor buildOperation, OperationFinishEvent finishEvent) {
             Object id = buildOperation.getId();
-            parents.remove(id);
+            Object parentId = parents.remove(id);
             if (active.remove(id) == null) {
                 return;
             }
 
-            Finished notification = new Finished(id, buildOperation.getDetails(), finishEvent.getResult(), finishEvent.getFailure());
+            Finished notification = new Finished(id, parentId, buildOperation.getDetails(), finishEvent.getResult(), finishEvent.getFailure());
             try {
                 notificationListener.finished(notification);
             } catch (Throwable e) {
@@ -177,12 +178,14 @@ class BuildOperationNotificationBridge implements BuildOperationNotificationList
     private static class Finished implements BuildOperationFinishedNotification {
 
         private final Object id;
+        private final Object parentId;
         private final Object details;
         private final Object result;
         private final Throwable failure;
 
-        private Finished(Object id, Object details, Object result, Throwable failure) {
+        private Finished(Object id, Object parentId, Object details, Object result, Throwable failure) {
             this.id = id;
+            this.parentId = parentId;
             this.details = details;
             this.result = result;
             this.failure = failure;
@@ -191,6 +194,12 @@ class BuildOperationNotificationBridge implements BuildOperationNotificationList
         @Override
         public Object getNotificationOperationId() {
             return id;
+        }
+
+        @Nullable
+        @Override
+        public Object getNotificationOperationParentId() {
+            return parentId;
         }
 
         @Override
@@ -212,6 +221,7 @@ class BuildOperationNotificationBridge implements BuildOperationNotificationList
         public String toString() {
             return "BuildOperationFinishedNotification{"
                 + "id=" + id
+                + ", parentId=" + parentId
                 + ", details=" + details
                 + ", result=" + result
                 + ", failure=" + failure
