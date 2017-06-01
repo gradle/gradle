4957661bbf497f82dd7f6e40e8107ab9e5d8ea6b
Paul Merlin <paul@gradle.com>
2017-06-01 15:37:23 +0200

    Move IDE TAPI model integration tests from :toolingApi to :ide

---
+++ subprojects/ide/src/integTest/groovy/org/gradle/plugins/ide/tooling/r30/ToolingApiEclipseModelSourceFolderClasspathAttributesCrossVersionSpec.groovy
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.plugins.ide.tooling.r30
+
+import org.gradle.integtests.tooling.fixture.TargetGradleVersion
+import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
+import org.gradle.integtests.tooling.fixture.ToolingApiVersion
+import org.gradle.tooling.model.UnsupportedMethodException
+import org.gradle.tooling.model.eclipse.EclipseProject
+
+@ToolingApiVersion('>=3.0')
+@TargetGradleVersion(">=3.0")
+class ToolingApiEclipseModelSourceFolderClasspathAttributesCrossVersionSpec extends ToolingApiSpecification {
+
+    @TargetGradleVersion(">=1.2 <3.0")
+    def "Older versions throw runtime exception when querying classpath attributes"() {
+        settingsFile << 'rootProject.name = "root"'
+        buildFile <<
+        """apply plugin: 'java'
+           apply plugin: 'eclipse'
+           eclipse {
+               classpath {
+                   file {
+                       whenMerged { classpath ->
+                           classpath.entries.find { it.kind == 'src' && it.path == 'src/main/java' }.entryAttributes.customKey = 'customValue'
+                       }
+                   }
+               }
+           }
+        """
+        file('src/main/java').mkdirs()
+
+        when:
+        EclipseProject project = loadToolingModel(EclipseProject)
+        project.sourceDirectories.find {it.path == 'src/main/java' }.getClasspathAttributes()
+
+        then:
+        thrown UnsupportedMethodException
+    }
+
+    def "Source folder doesn't define classpath attributes"() {
+        setup:
+        settingsFile << 'rootProject.name = "root"'
+        buildFile << "apply plugin: 'java'"
+        file('src/main/java').mkdirs()
+
+        when:
+        EclipseProject project = loadToolingModel(EclipseProject)
+
+        then:
+        project.sourceDirectories.find {it.path == 'src/main/java' }.classpathAttributes.isEmpty()
+    }
+
+    def "Source folder defines one classpath attribute"() {
+        settingsFile << 'rootProject.name = "root"'
+        buildFile <<
+            """apply plugin: 'java'
+           apply plugin: 'eclipse'
+           eclipse {
+               classpath {
+                   file {
+                       whenMerged { classpath ->
+                           classpath.entries.find { it.kind == 'src' && it.path == 'src/main/java' }.entryAttributes.customKey = 'customValue'
+                       }
+                   }
+               }
+           }
+        """
+        file('src/main/java').mkdirs()
+
+        when:
+        EclipseProject project = loadToolingModel(EclipseProject)
+
+        then:
+        project.sourceDirectories.size() == 1
+        project.sourceDirectories[0].classpathAttributes.size() == 1
+        project.sourceDirectories[0].classpathAttributes[0].name == 'customKey'
+        project.sourceDirectories[0].classpathAttributes[0].value == 'customValue'
+    }
+
+    def "Source folder defines multiple classpath attributes"() {
+        settingsFile << 'rootProject.name = "root"'
+        buildFile <<
+            """apply plugin: 'java'
+           apply plugin: 'eclipse'
+           eclipse {
+               classpath {
+                   file {
+                       whenMerged { classpath ->
+                           classpath.entries.find { it.kind == 'src' && it.path == 'src/main/java' }.entryAttributes.key1 = 'value1'
+                           classpath.entries.find { it.kind == 'src' && it.path == 'src/main/java' }.entryAttributes.key2 = 'value2'
+                       }
+                   }
+               }
+           }
+        """
+        file('src/main/java').mkdirs()
+
+        when:
+        EclipseProject project = loadToolingModel(EclipseProject)
+
+        then:
+        project.sourceDirectories.size() == 1
+        project.sourceDirectories[0].classpathAttributes.size() == 2
+        project.sourceDirectories[0].classpathAttributes.find { it.name == 'key1' && it.value == 'value1'}
+        project.sourceDirectories[0].classpathAttributes.find { it.name == 'key2' && it.value == 'value2'}
+    }
+}
