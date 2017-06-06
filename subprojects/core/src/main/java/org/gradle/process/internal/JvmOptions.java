80a26a35f22200c23d90a2d2d6387d721f4fe5d4
Jendrik Johannes <jendrik@gradle.com>
2017-06-06 16:24:13 +0200

    Remove unused import

--- subprojects/core/src/main/java/org/gradle/process/internal/JvmOptions.java
+++ subprojects/core/src/main/java/org/gradle/process/internal/JvmOptions.java
@@ -29,7 +29,6 @@ import java.io.File;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
