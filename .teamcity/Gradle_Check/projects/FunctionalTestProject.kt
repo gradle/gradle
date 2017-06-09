6db0c28ed567b49ac27f4aa580435325501b392f
Jendrik Johannes <jendrik@gradle.com>
2017-06-09 13:12:17 +0200

    Fix test coverage project name

--- .teamcity/Gradle_Check/projects/FunctionalTestProject.kt
+++ .teamcity/Gradle_Check/projects/FunctionalTestProject.kt
@@ -8,7 +8,7 @@ import model.TestCoverage
 class FunctionalTestProject(testConfig : TestCoverage) : Project({
     this.uuid = testConfig.asId()
     this.extId = uuid
-    this.name = "Test Coverage - " + testConfig.asName()
+    this.name = testConfig.asName()
 
     (1..CIBuildModel.testBucketCount).forEach { bucket ->
         buildType(FunctionalTest(testConfig, bucket))
