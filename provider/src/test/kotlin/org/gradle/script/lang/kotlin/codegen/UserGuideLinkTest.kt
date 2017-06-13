36a728b3924ac0bb50473348c4eef2a43fba6bf0
Paul Merlin <paul@gradle.com>
2017-06-13 09:47:17 +0200

    Update UserGuideLinkTest after adding more plugin links
    See #314

--- provider/src/test/kotlin/org/gradle/script/lang/kotlin/codegen/UserGuideLinkTest.kt
+++ provider/src/test/kotlin/org/gradle/script/lang/kotlin/codegen/UserGuideLinkTest.kt
@@ -14,30 +14,35 @@ class UserGuideLinkTest {
         val linkedPlugins =
             listOf(
                 "announce", "antlr", "application", "assembler", "assembler-lang",
-                "base", "build-announcements", "build-dashboard", "build-init",
+                "base", "binary-base", "build-announcements", "build-dashboard", "build-init",
                 "c", "c-lang", "checkstyle", "clang-compiler", "codenarc",
                 "cpp", "cpp-executable", "cpp-lang", "cpp-library",
+                "compare-gradle-builds", "component-base", "component-model-base",
                 "cunit", "cunit-test-suite",
                 "distribution",
                 "ear", "eclipse", "eclipse-wtp",
                 "findbugs",
-                "google-test", "google-test-test-suite",
+                "gcc-compiler", "google-test", "google-test-test-suite",
                 "groovy", "groovy-base",
+                "help-tasks",
                 "idea", "ivy-publish",
                 "jacoco", "java", "java-base", "java-gradle-plugin", "java-lang",
                 "java-library", "java-library-distribution", "jdepend", "junit-test-suite",
                 "jvm-component", "jvm-resources",
                 "maven", "maven-publish",
+                "microsoft-visual-cpp-compiler",
                 "native-component", "native-component-model",
                 "objective-c", "objective-c-lang", "objective-cpp", "objective-cpp-lang", "osgi",
                 "play", "play-application", "play-cofeescript", "play-ide", "play-javascript", "pmd",
+                "project-report", "project-reports",
                 "reporting-base",
-                "scala", "scala-base", "signing",
+                "scala", "scala-base", "signing", "standard-tool-chains",
                 "visual-studio",
                 "war",
                 "windows-resource-script", "windows-resources", "wrapper")
         linkedPlugins.forEach {
             assertThat(
+                "$it is linked",
                 UserGuideLink.forPlugin(it),
                 notNullValue())
         }
@@ -47,13 +52,14 @@ class UserGuideLinkTest {
     fun `unlinked plugins`() {
         val unlinkedPlugins =
             listOf(
-                "binary-base", "coffeescript-base", "compare-gradle-builds", "component-base",
-                "component-model-base", "envjs", "gcc-compiler", "help-tasks",
-                "javascript-base", "jshint", "language-base", "lifecycle-base",
-                "microsoft-visual-cpp-compiler", "project-report", "project-reports",
-                "publishing", "rhino", "scala-lang", "standard-tool-chains")
+                "coffeescript-base", "envjs", "javascript-base", "jshint",
+                "language-base", "lifecycle-base",
+                "publishing",
+                "rhino",
+                "scala-lang")
         unlinkedPlugins.forEach {
             assertThat(
+                "$it is not linked",
                 UserGuideLink.forPlugin(it),
                 nullValue())
         }
