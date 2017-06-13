912b0681796d250e85095c50579cfe28350f1b87
Paul Merlin <paul@gradle.com>
2017-06-13 15:36:19 +0200

    Dedupe assertions with a list of task names

--- samples-tests/src/test/kotlin/org/gradle/script/lang/kotlin/samples/CodeQualitySampleTest.kt
+++ samples-tests/src/test/kotlin/org/gradle/script/lang/kotlin/samples/CodeQualitySampleTest.kt
@@ -15,18 +15,19 @@ class CodeQualitySampleTest : AbstractSampleTest("code-quality") {
 
         val result = build("build")
 
-        assertThat(result.task(":checkstyleMain").outcome, equalTo(TaskOutcome.SUCCESS))
-        assertThat(result.task(":checkstyleTest").outcome, equalTo(TaskOutcome.SUCCESS))
+        val successfulTasks = listOf(
+            ":checkstyleMain",
+            ":checkstyleTest",
+            ":findbugsMain",
+            ":findbugsTest",
+            ":pmdMain",
+            ":pmdTest",
+            ":jdependMain",
+            ":jdependTest",
+            ":jacocoTestCoverageVerification")
 
-        assertThat(result.task(":findbugsMain").outcome, equalTo(TaskOutcome.SUCCESS))
-        assertThat(result.task(":findbugsTest").outcome, equalTo(TaskOutcome.SUCCESS))
-
-        assertThat(result.task(":pmdMain").outcome, equalTo(TaskOutcome.SUCCESS))
-        assertThat(result.task(":pmdTest").outcome, equalTo(TaskOutcome.SUCCESS))
-
-        assertThat(result.task(":jdependMain").outcome, equalTo(TaskOutcome.SUCCESS))
-        assertThat(result.task(":jdependTest").outcome, equalTo(TaskOutcome.SUCCESS))
-
-        assertThat(result.task(":jacocoTestCoverageVerification").outcome, equalTo(TaskOutcome.SUCCESS))
+        successfulTasks.forEach { taskName ->
+            assertThat(result.task(taskName).outcome, equalTo(TaskOutcome.SUCCESS))
+        }
     }
 }
