924111ed3c4e65162c66eeaae0c06aafbdeaebdf
Jendrik Johannes <jendrik@gradle.com>
2017-06-15 10:27:17 +0200

    Fix performance worker ID

--- .teamcity/Gradle_Check/configurations/PerformanceTest.kt
+++ .teamcity/Gradle_Check/configurations/PerformanceTest.kt
@@ -45,7 +45,7 @@ class PerformanceTest(model: CIBuildModel, type: PerformanceTestType) : BuildTyp
         gradle {
             name = "GRADLE_RUNNER"
             tasks = ""
-            gradleParams = "cleanDistributed${type.taskId} distributed${type.taskId}s -x prepareSamples %performance.baselines% ${type.extraParameters} -PtimestampedVersion -Porg.gradle.performance.branchName=%teamcity.build.branch% -Porg.gradle.performance.db.url=%performance.db.url% -Porg.gradle.performance.db.username=%performance.db.username% -PteamCityUsername=%TC_USERNAME% -PteamCityPassword=%teamcity.password.restbot% -Porg.gradle.performance.buildTypeId=${PerformanceSupportProject(model).extId} -Porg.gradle.performance.workerTestTaskName=fullPerformanceTest -Porg.gradle.performance.coordinatorBuildId=%teamcity.build.id% -Porg.gradle.performance.db.password=%performance.db.password.tcagent% " + gradleParameters.joinToString(separator = " ")
+            gradleParams = "cleanDistributed${type.taskId} distributed${type.taskId}s -x prepareSamples %performance.baselines% ${type.extraParameters} -PtimestampedVersion -Porg.gradle.performance.branchName=%teamcity.build.branch% -Porg.gradle.performance.db.url=%performance.db.url% -Porg.gradle.performance.db.username=%performance.db.username% -PteamCityUsername=%TC_USERNAME% -PteamCityPassword=%teamcity.password.restbot% -Porg.gradle.performance.buildTypeId=${IndividualPerformanceScenarioWorkers(model).extId} -Porg.gradle.performance.workerTestTaskName=fullPerformanceTest -Porg.gradle.performance.coordinatorBuildId=%teamcity.build.id% -Porg.gradle.performance.db.password=%performance.db.password.tcagent% " + gradleParameters.joinToString(separator = " ")
             useGradleWrapper = true
         }
         script {
