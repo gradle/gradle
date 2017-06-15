4d62ee17fd0718e54d7627ce1e759f8f9d8ee327
Seth Jackson <sethjackson@gmail.com>
2017-06-15 14:25:03 -0400

    Fix JVM args on Java 8+.

--- subprojects/launcher/src/main/java/org/gradle/launcher/daemon/configuration/DaemonParameters.java
+++ subprojects/launcher/src/main/java/org/gradle/launcher/daemon/configuration/DaemonParameters.java
@@ -35,7 +35,7 @@ public class DaemonParameters {
     public static final int DEFAULT_PERIODIC_CHECK_INTERVAL_MILLIS = 10 * 1000;
 
     public static final List<String> DEFAULT_JVM_ARGS = ImmutableList.of("-Xmx1024m", "-XX:MaxPermSize=256m", "-XX:+HeapDumpOnOutOfMemoryError");
-    public static final List<String> DEFAULT_JVM_9_ARGS = ImmutableList.of("-Xmx1024m", "-XX:+HeapDumpOnOutOfMemoryError");
+    public static final List<String> DEFAULT_JVM_8_ARGS = ImmutableList.of("-Xmx1024m", "-XX:+HeapDumpOnOutOfMemoryError");
     public static final String INTERACTIVE_TOGGLE = "org.gradle.interactive";
 
     private final File gradleUserHomeDir;
@@ -123,8 +123,8 @@ public class DaemonParameters {
         if (hasJvmArgs) {
             return;
         }
-        if (javaVersion.compareTo(JavaVersion.VERSION_1_9) >= 0) {
-            jvmOptions.jvmArgs(DEFAULT_JVM_9_ARGS);
+        if (javaVersion.compareTo(JavaVersion.VERSION_1_8) >= 0) {
+            jvmOptions.jvmArgs(DEFAULT_JVM_8_ARGS);
         } else {
             jvmOptions.jvmArgs(DEFAULT_JVM_ARGS);
         }
