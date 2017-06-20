b7002fe155357c5df7d2939efe28f0d92e47cfd0
Gary Hale <gary@gradle.com>
2017-06-20 08:23:17 -0700

    Removing unnnecessary comment

--- subprojects/base-services/src/main/java/org/gradle/internal/concurrent/ManagedExecutorImpl.java
+++ subprojects/base-services/src/main/java/org/gradle/internal/concurrent/ManagedExecutorImpl.java
@@ -92,8 +92,6 @@ class ManagedExecutorImpl extends AbstractDelegatingExecutorService implements M
     public void setFixedPoolSize(int numThreads) {
         if (executor instanceof ThreadPoolExecutor) {
             ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
-            // we do this because in jdk9 these methods throw an exception if you
-            // try to set one such that maximumPoolSize < corePoolSize
             if (numThreads < threadPoolExecutor.getCorePoolSize()) {
                 threadPoolExecutor.setCorePoolSize(numThreads);
                 threadPoolExecutor.setMaximumPoolSize(numThreads);
