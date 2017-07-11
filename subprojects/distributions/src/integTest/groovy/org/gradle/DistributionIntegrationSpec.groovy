b6a32791c4f1db3da136bd32f5d2b1bffbe6cfdf
Lóránt Pintér <lorant@gradle.com>
2017-07-11 13:50:18 +0200

    Introduce JSR-305 as a dependency javax.annotations.Nullable
    Instead of our own (now deprecated) org.gradle.api.Nullable.

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -38,7 +38,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        162
+        163
     }
 
     def "no duplicate entries"() {
