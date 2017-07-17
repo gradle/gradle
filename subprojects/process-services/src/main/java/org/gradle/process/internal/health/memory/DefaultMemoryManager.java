e93800ad0ae35d3f1f3967cac9432562b5ca8403
Cedric Champeau <cedric@gradle.com>
2017-07-17 08:43:10 +0200

    Copy under lock, iterate out of lock

--- subprojects/process-services/src/main/java/org/gradle/process/internal/health/memory/DefaultMemoryManager.java
+++ subprojects/process-services/src/main/java/org/gradle/process/internal/health/memory/DefaultMemoryManager.java
@@ -116,16 +116,19 @@ public class DefaultMemoryManager implements MemoryManager, Stoppable {
         long toReleaseMemory = requestedFreeMemory;
         if (freeMemory < requestedFreeMemory) {
             LOGGER.debug("{} memory requested, {} free", requestedFreeMemory, freeMemory);
+            List<MemoryHolder> memoryHolders;
             synchronized (holdersLock) {
-                for (MemoryHolder holder : new ArrayList<MemoryHolder>(holders)) {
-                    long released = holder.attemptToRelease(toReleaseMemory);
-                    toReleaseMemory -= released;
-                    freeMemory += released;
-                    if (freeMemory >= requestedFreeMemory) {
-                        break;
-                    }
+                memoryHolders = new ArrayList<MemoryHolder>(holders);
+            }
+            for (MemoryHolder holder : memoryHolders) {
+                long released = holder.attemptToRelease(toReleaseMemory);
+                toReleaseMemory -= released;
+                freeMemory += released;
+                if (freeMemory >= requestedFreeMemory) {
+                    break;
                 }
             }
+
             LOGGER.debug("{} memory requested, {} released, {} free", requestedFreeMemory, requestedFreeMemory - toReleaseMemory, freeMemory);
         }
         return freeMemory;
