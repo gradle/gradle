ef475e9205eb3b805f01535517479ea0396bf6d9
jokubasdargissc <jokubas.dargis@snapchat.com>
2017-07-18 22:56:55 -0700

    [Distributions] Update expected jar count

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -38,7 +38,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        163
+        169
     }
 
     def "no duplicate entries"() {
