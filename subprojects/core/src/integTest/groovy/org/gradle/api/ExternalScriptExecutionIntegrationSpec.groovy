7b6edfa549d6bc1b0d49b6d9750590220e199f9e
Daz DeBoer <daz@gradle.com>
2017-07-23 17:50:00 -0600

    Add test for cached URL resources

--- subprojects/core/src/integTest/groovy/org/gradle/api/ExternalScriptExecutionIntegrationSpec.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/ExternalScriptExecutionIntegrationSpec.groovy
@@ -17,6 +17,7 @@ package org.gradle.api
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.test.fixtures.server.http.HttpServer
+import spock.lang.Unroll
 
 class ExternalScriptExecutionIntegrationSpec extends AbstractIntegrationSpec {
     @org.junit.Rule
@@ -72,5 +73,125 @@ task check {
 
         expect:
         succeeds 'check'
+    }
+
+    @Unroll
+    def "will used cached #source resource when run with --offline"() {
+        given:
+        String scriptName = "script-${System.currentTimeMillis()}.gradle"
+        executer.withDefaultCharacterEncoding("ISO-8859-15")
+        server.start()
+
+        and:
+        def scriptFile = file("script.gradle")
+        scriptFile.setText("""println 'loaded external script'""", "UTF-8")
+        server.expectGet('/' + scriptName, scriptFile)
+
+        and:
+        file("init.gradle").createFile()
+        buildFile << """
+            task check {}
+        """
+
+        when:
+        def scriptUri = "http://localhost:${server.port}/${scriptName}"
+        file(sourceFile) << """
+            apply from: '${scriptUri}'
+        """
+
+        then:
+        args('-I', 'init.gradle')
+        succeeds 'check'
+        outputContains 'loaded external script'
+
+        when:
+        server.stop()
+
+        then:
+        args('-I', 'init.gradle')
+        fails 'check'
+        errorOutput.contains("Could not get resource '${scriptUri}'")
+
+        when:
+        scriptFile.setText("""throw new RuntimeException('NOT CACHED')""", "UTF-8")
+        args('--offline', '-I', 'init.gradle')
+
+        then:
+        succeeds 'check'
+
+        where:
+        source        | sourceFile
+        "buildscript" | "build.gradle"
+        "settings"    | "settings.gradle"
+        "initscript"  | "init.gradle"
+    }
+
+    def "will only request resource once for build invocation"() {
+        given:
+        String scriptName = "script-${System.currentTimeMillis()}.gradle"
+        executer.withDefaultCharacterEncoding("ISO-8859-15")
+        server.start()
+
+        and:
+        def scriptFile = file("script.gradle")
+        scriptFile.setText("""println 'loaded external script'""", "UTF-8")
+        server.expectGet('/' + scriptName, scriptFile)
+
+        and:
+        settingsFile << """
+            include ':projA'
+            include ':projB'
+        """
+
+        and:
+        [file('settings.gradle'), file('init.gradle'), file("projA/build.gradle"), file("projB/build.gradle")].each {
+            it << "apply from: 'http://localhost:${server.port}/${scriptName}'"
+        }
+
+        when:
+        args('-I', 'init.gradle')
+        succeeds 'tasks'
+
+        then:
+        output.count('loaded external script') == 4
+    }
+
+    def "will refresh cached value on subsequent build invocation"() {
+        given:
+        String scriptName = "script-${System.currentTimeMillis()}.gradle"
+        executer.withDefaultCharacterEncoding("ISO-8859-15")
+        server.start()
+
+        and:
+        File scriptFile = file("script.gradle")
+        scriptFile.setText("""println 'loaded external script 1'""", "UTF-8")
+
+        and:
+        buildFile << "apply from: 'http://localhost:${server.port}/${scriptName}'"
+
+        when:
+        server.expectGet('/' + scriptName, scriptFile)
+
+        then:
+        succeeds 'tasks'
+        output.contains('loaded external script 1')
+
+        when:
+        scriptFile.setText("""println 'loaded external script 2'""", "UTF-8")
+        server.expectHead('/' + scriptName, scriptFile)
+        server.expectGetMissing('/' + scriptName + '.sha1')
+        server.expectGet('/' + scriptName, scriptFile)
+
+        then:
+        succeeds 'tasks'
+        output.contains('loaded external script 2')
+
+        when:
+        server.stop()
+        args("--offline")
+
+        then:
+        succeeds 'tasks'
+        output.contains('loaded external script 2')
     }
 }
