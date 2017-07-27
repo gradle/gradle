4933a97755529590c170c9b2063a66c4ea474295
Sterling Greene <sterling@gradle.com>
2017-07-27 11:08:46 -0400

    Remove dependency on run-support
    We weren't really using it anyways

--- subprojects/platform-play/src/main/java/org/gradle/play/internal/run/PlayRunAdapterV23X.java
+++ subprojects/platform-play/src/main/java/org/gradle/play/internal/run/PlayRunAdapterV23X.java
@@ -16,25 +16,15 @@
 
 package org.gradle.play.internal.run;
 
-import com.google.common.collect.ImmutableList;
 import org.gradle.api.Transformer;
 import org.gradle.api.artifacts.Dependency;
-import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency;
-import org.gradle.internal.Cast;
-import org.gradle.internal.UncheckedException;
-import org.gradle.internal.reflect.DirectInstantiator;
-import org.gradle.scala.internal.reflect.ScalaMethod;
-import org.gradle.scala.internal.reflect.ScalaReflectionUtil;
 import org.gradle.util.CollectionUtils;
-import org.gradle.util.VersionNumber;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.List;
 
 public class PlayRunAdapterV23X extends DefaultVersionedPlayRunAdapter {
-    protected static final String RUN_SUPPORT_PLAY_MODULE = "run-support";
-    private static final VersionNumber MINIMUM_PLAY_VERSION_WITH_RUN_SUPPORT = VersionNumber.parse("2.3.7");
-
     @Override
     protected Class<?> getBuildLinkClass(ClassLoader classLoader) throws ClassNotFoundException {
         return classLoader.loadClass("play.core.BuildLink");
@@ -52,54 +42,19 @@ public class PlayRunAdapterV23X extends DefaultVersionedPlayRunAdapter {
 
     @Override
     protected ClassLoader createAssetsClassLoader(File assetsJar, Iterable<File> assetsDirs, ClassLoader classLoader) {
-        Class<?> assetsClassLoaderClass;
-
-        assetsClassLoaderClass = loadClass(classLoader, "play.runsupport.AssetsClassLoader");
-
-        final Class<?> tuple2Class = loadClass(classLoader, "scala.Tuple2");
-
-        List<?> tuples = CollectionUtils.collect(assetsDirs, new Transformer<Object, File>() {
+        List<AssetsClassLoader.AssetDir> assetDirs = CollectionUtils.collect(assetsDirs, new Transformer<AssetsClassLoader.AssetDir, File>() {
             @Override
-            public Object transform(File file) {
-                return DirectInstantiator.instantiate(tuple2Class, "public", file);
+            public AssetsClassLoader.AssetDir transform(File file) {
+                // TODO: This prefix shouldn't be hardcoded
+                return new AssetsClassLoader.AssetDir("public", file);
             }
         });
-
-        ScalaMethod listToScalaSeqMethod = ScalaReflectionUtil.scalaMethod(classLoader, "scala.collection.convert.WrapAsScala", "asScalaBuffer", List.class);
-        Object scalaTuples = listToScalaSeqMethod.invoke(tuples);
-
-        return Cast.uncheckedCast(DirectInstantiator.instantiate(assetsClassLoaderClass, classLoader, scalaTuples));
-    }
-
-    private Class<?> loadClass(ClassLoader classLoader, String className) {
-        try {
-            return classLoader.loadClass(className);
-        } catch (ClassNotFoundException e) {
-            throw UncheckedException.throwAsUncheckedException(e);
-        }
+        return new AssetsClassLoader(classLoader, assetDirs);
     }
 
     @Override
     public Iterable<Dependency> getRunsupportClasspathDependencies(String playVersion, String scalaCompatibilityVersion) {
-        ImmutableList.Builder<Dependency> listBuilder = ImmutableList.builder();
-
-        String runsupportPlayVersion = playVersion;
-        boolean transitive = true;
-        // use run-support from 2.3.7 for versions before Play 2.3.7
-        if (VersionNumber.parse(playVersion).compareTo(MINIMUM_PLAY_VERSION_WITH_RUN_SUPPORT) < 0) {
-            runsupportPlayVersion = "2.3.7";
-            transitive = false;
-        }
-        DefaultExternalModuleDependency runSupportDependency = new DefaultExternalModuleDependency("com.typesafe.play", RUN_SUPPORT_PLAY_MODULE + "_" + scalaCompatibilityVersion, runsupportPlayVersion);
-        runSupportDependency.setTransitive(transitive);
-        listBuilder.add(runSupportDependency);
-
-        String name = scalaCompatibilityVersion.equals("2.10") ? "io" : ("io_" + scalaCompatibilityVersion);
-        DefaultExternalModuleDependency dependency = new DefaultExternalModuleDependency("org.scala-sbt", name, getIOSupportDependencyVersion(), "runtime");
-        dependency.setTransitive(false);
-        listBuilder.add(dependency);
-
-        return listBuilder.build();
+        return Collections.emptyList();
     }
 
     protected String getIOSupportDependencyVersion() {
