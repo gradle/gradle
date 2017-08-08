9baa5c2987d801b36eecf73c62bc9d52f7fa3700
Pepper Lebeck-Jobe <pepper@gradle.com>
2017-08-08 12:02:51 +0200

    Add deprecation advice (#2672)
    * Add deprecation advice
    
    Before, users who were using the `dependsOnTaskDidWork()` weren't
    given any advice about how to get back on the happy path.
    
    Fixes #2671

--- subprojects/core/src/main/java/org/gradle/api/internal/AbstractTask.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/AbstractTask.java
@@ -541,7 +541,9 @@ public abstract class AbstractTask implements TaskInternal, DynamicObjectAware {
 
     @Override
     public boolean dependsOnTaskDidWork() {
-        DeprecationLogger.nagUserOfDiscontinuedMethod("Task.dependsOnTaskDidWork()");
+        DeprecationLogger.nagUserOfDiscontinuedMethod(
+            "Task.dependsOnTaskDidWork()",
+            "Instead, check the value of \"didWork()\" for each task, or declare the task inputs and outputs and let Gradle decide what needs to be run.");
         TaskDependency dependency = getTaskDependencies();
         for (Task depTask : dependency.getDependencies(this)) {
             if (depTask.getDidWork()) {
