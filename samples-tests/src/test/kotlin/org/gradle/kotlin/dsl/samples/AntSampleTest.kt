75748ad72f231c197c1d81f620727a6c05d4da5d
Paul Merlin <paul@gradle.com>
2017-08-09 09:57:00 +0200

    Integration test Ant builder sample

---
+++ samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/AntSampleTest.kt
@@ -0,0 +1,28 @@
+package org.gradle.kotlin.dsl.samples
+
+import org.junit.Test
+import java.io.File
+
+
+class AntSampleTest : AbstractSampleTest("ant") {
+
+    @Test
+    fun `hello ant task`() {
+
+        // when:
+        val result = build("hello")
+
+        // then:
+        result.output.contains("Hello from Ant!")
+    }
+
+    @Test
+    fun `zip ant task`() {
+
+        // when:
+        build("zip")
+
+        // then:
+        File(projectRoot, "build/archive.zip").isFile
+    }
+}
