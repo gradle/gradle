f20c9e4b387bc10880b991503cea01ad46d9ddf2
Stefan Wolf <wolf@gradle.com>
2017-08-11 16:33:06 +0200

    Publish status for SanityCheck to Github
    So we see a build is running earlier on the PR.

--- .teamcity/Gradle_Check/configurations/SanityCheck.kt
+++ .teamcity/Gradle_Check/configurations/SanityCheck.kt
@@ -1,6 +1,7 @@
 package configurations
 
 import jetbrains.buildServer.configs.kotlin.v10.BuildType
+import jetbrains.buildServer.configs.kotlin.v10.buildFeatures.commitStatusPublisher
 import model.CIBuildModel
 
 class SanityCheck(model: CIBuildModel) : BuildType({
@@ -11,6 +12,18 @@ class SanityCheck(model: CIBuildModel) : BuildType({
 
     params {
         param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+    }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "Gradle_Branches_GradlePersonalBranches"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:5306bfc7-041e-46e8-8d61-1d49424e7b04"
+                }
+            }
+        }
     }
 
     applyDefaults(model, this, "compileAll sanityCheck", extraParameters = "-DenableCodeQuality=true --parallel")
