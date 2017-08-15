896c1d60fdf3683d3c3400cfebef58f05c9b952a
Jendrik Johannes <jendrik@gradle.com>
2017-08-15 17:12:45 +0200

    Add integration test for cross-process cache lock contention handling

---
+++ subprojects/persistent-cache/src/integTest/groovy/org/gradle/cache/internal/DefaultFileLockManagerContentionIntegrationTest.groovy
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.cache.internal
+
+import org.gradle.cache.FileLock
+import org.gradle.cache.FileLockManager
+import org.gradle.cache.internal.filelock.LockOptionsBuilder
+import org.gradle.cache.internal.locklistener.DefaultFileLockContentionHandler
+import org.gradle.cache.internal.locklistener.FileLockContentionHandler
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.executer.GradleHandle
+import org.gradle.internal.concurrent.DefaultExecutorFactory
+import org.gradle.internal.remote.internal.inet.InetAddressFactory
+import org.gradle.internal.time.TrueTimeProvider
+
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+import static org.gradle.test.fixtures.ConcurrentTestUtil.poll
+
+class DefaultFileLockManagerContentionIntegrationTest extends AbstractIntegrationSpec {
+    def timeProvider = new TrueTimeProvider()
+    def addressFactory = new InetAddressFactory()
+
+    FileLockContentionHandler receivingFileLockContentionHandler
+    DatagramSocket receivingSocket
+    FileLock receivingLock
+
+    def setup() {
+        executer.withArguments("-d")
+        executer.requireOwnGradleUserHomeDir().withDaemonBaseDir(file("daemonsRequestingLock")).requireDaemon()
+        buildFile << ""
+    }
+
+    def "the lock holder is not hammered with ping requests for the shared fileHashes lock"() {
+        given:
+        setupLockOwner()
+        def pingRequestCount = 0
+        //do not handle requests: this simulates the situation were pings do not arrive
+        replaceSocketReceiver { pingRequestCount++ }
+
+        when:
+        def build = executer.withTasks("help").start()
+        def startTime = timeProvider.currentTime
+        poll {
+            assert (build.standardOutput =~ 'Pinged owner at port').count == 3
+        }
+        receivingLock.close()
+
+        then:
+        build.waitForFinish()
+        pingRequestCount == 3 || pingRequestCount == 4
+        timeProvider.currentTime - startTime > 3000 // See: DefaultFileLockContentionHandler.PING_DELAY
+    }
+
+    def "the lock holder starts the release request only once and discards additional requests in the meantime"() {
+        given:
+        def requestReceived = false
+        setupLockOwner {
+            requestReceived = true
+            while(true) { Thread.sleep(100000) } //block the release action thread
+        }
+
+        when:
+        def build = executer.withTasks("help").start()
+        poll { assert requestReceived }
+
+        // simulate additional requests
+        def socket = new DatagramSocket(0, addressFactory.localBindingAddress)
+        (1..500).each {
+            addressFactory.communicationAddresses.each { address ->
+                byte[] bytes = [1, 0, 0, 0, 0, 0, 0, 0, 0]
+                DatagramPacket confirmPacket = new DatagramPacket(bytes, bytes.length, address, receivingSocket.localPort)
+                socket.send(confirmPacket)
+            }
+        }
+
+        then:
+        waitCloseAndFinish(build)
+        assertReceivingSocketEmpty()
+    }
+
+    def "if the lock holder confirmed the request, it is not pinged again"() {
+        given:
+        def requestReceived = false
+        def additionalRequests = 0
+        setupLockOwner() { requestReceived = true }
+
+        when:
+        def build = executer.withTasks("help").start()
+        poll {
+            assert requestReceived
+        }
+        replaceSocketReceiver {
+            additionalRequests++
+        }
+
+        then:
+        waitCloseAndFinish(build)
+        countPingsSent(build) == 1
+        additionalRequests == 0
+    }
+
+    def "the lock holder confirms that a request is in process to multiple requesters"() {
+        given:
+        def requestReceived = false
+        def additionalRequests = 0
+        setupLockOwner() { requestReceived = true }
+
+        when:
+        def build1 = executer.withArguments("-d").withTasks("help").start()
+        def build2 = executer.withArguments("-d").withTasks("help").start()
+        def build3 = executer.withArguments("-d").withTasks("help").start()
+        poll {
+            assert requestReceived
+            assert assertConfirmationCount(build1)
+            assert assertConfirmationCount(build2)
+            assert assertConfirmationCount(build3)
+        }
+        replaceSocketReceiver { additionalRequests++ }
+
+        then:
+        waitCloseAndFinish(build1)
+        build2.waitForFinish()
+        build3.waitForFinish()
+        assertConfirmationCount(build1)
+        assertConfirmationCount(build2)
+        assertConfirmationCount(build3)
+        additionalRequests == 0
+    }
+
+    def "if the lock was released by one lock holder, but taken away again, the new lock holder is pinged once"() {
+        given:
+        def requestReceived = false
+        def lock1 = setupLockOwner() { requestReceived = true }
+
+        when:
+        def build = executer.withTasks("help").start()
+        poll {
+            assert requestReceived
+            assert countPingsSent(build) == 1
+        }
+
+        requestReceived = false
+        def prevReceivingSocket = receivingSocket
+        lock1.close()
+        def lock2 = setupLockOwner() {
+            requestReceived = true
+        }
+        poll {
+            assert requestReceived
+            assert countPingsSent(build, prevReceivingSocket) == 1
+            assert countPingsSent(build, receivingSocket) == 1
+        }
+        lock2.close()
+
+        then:
+        build.waitForFinish()
+        countPingsSent(build, prevReceivingSocket) == 1
+        countPingsSent(build, receivingSocket) == 1
+        assertConfirmationCount(build, prevReceivingSocket)
+        assertConfirmationCount(build, receivingSocket)
+        assertConfirmationCount(build, prevReceivingSocket)
+    }
+
+    def assertConfirmationCount(GradleHandle build, DatagramSocket socket = receivingSocket) {
+        (build.standardOutput =~ "Gradle process at port ${socket.localPort} confirmed unlock request").count == addressFactory.communicationAddresses.size()
+    }
+
+    def assertReceivingSocketEmpty() {
+        try {
+            Executors.newSingleThreadExecutor().submit {
+                DatagramPacket packet = new DatagramPacket(new byte[9], 9)
+                receivingSocket.receive(packet)
+            }.get(1, TimeUnit.SECONDS)
+        } catch (TimeoutException e) {
+            return true
+        }
+        return false
+    }
+
+    def countPingsSent(GradleHandle build, DatagramSocket socket = receivingSocket) {
+        (build.standardOutput =~ "Pinged owner at port ${socket.localPort}").count
+    }
+
+    def waitCloseAndFinish(GradleHandle build) {
+        Thread.sleep(10 * 1000) //wait 10 seconds, in which the build should only wait for the lock to become available
+        receivingLock.close()
+        build.waitForFinish()
+    }
+
+    def replaceSocketReceiver(Runnable reactOnRequest) {
+        receivingFileLockContentionHandler.fileLockRequestListener?.shutdownNow()
+        new Thread() {
+            void run() {
+                InetAddress selectedAddress = null
+                while (true) {
+                    byte[] bytes = new byte[9]
+                    DatagramPacket packet = new DatagramPacket(bytes, bytes.length)
+                    receivingSocket.receive(packet)
+                    if (selectedAddress == null) {
+                        selectedAddress = packet.address
+                    }
+                    if (selectedAddress == packet.address) {
+                        reactOnRequest.run()
+                    }
+                }
+
+            }
+        }.start()
+    }
+
+    def setupLockOwner(Runnable whenContended = null) {
+        receivingFileLockContentionHandler = new DefaultFileLockContentionHandler(new DefaultExecutorFactory(), addressFactory)
+        def fileLockManager = new DefaultFileLockManager(new ProcessMetaDataProvider() {
+            String getProcessIdentifier() { return "pid" }
+            String getProcessDisplayName() { return "process" }
+        }, receivingFileLockContentionHandler)
+        receivingSocket = receivingFileLockContentionHandler.communicator.socket
+        receivingLock = fileLockManager.lock(new File(executer.gradleUserHomeDir, "caches/${executer.gradleVersion.version}/fileHashes/fileHashes"), LockOptionsBuilder.mode(FileLockManager.LockMode.Exclusive), "fileHashes", "", whenContended)
+    }
+
+}
