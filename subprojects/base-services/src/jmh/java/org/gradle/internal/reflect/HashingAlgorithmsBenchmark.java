1e9813b9bd30278645957d2e22bbf16cfe6b2675
Lóránt Pintér <lorant@gradle.com>
2017-08-23 00:37:07 +0200

    Add Murmur3 benchmark

--- subprojects/base-services/src/jmh/java/org/gradle/internal/reflect/HashingAlgorithmsBenchmark.java
+++ subprojects/base-services/src/jmh/java/org/gradle/internal/reflect/HashingAlgorithmsBenchmark.java
@@ -17,10 +17,12 @@
 package org.gradle.internal.reflect;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
 import org.bouncycastle.jcajce.provider.digest.Blake2b;
 import org.bouncycastle.jcajce.provider.digest.MD5;
 import org.bouncycastle.jcajce.provider.digest.SHA1;
-import org.gradle.internal.Factory;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
@@ -53,46 +55,105 @@ public class HashingAlgorithmsBenchmark {
         }
     }
 
-    static Map<String, MessageDigest> DIGESTS = ImmutableMap.<String, MessageDigest>builder()
-        .put("md5.java", getDigest("MD5"))
-        .put("md5.bc", new MD5.Digest())
-        .put("sha1.java", getDigest("SHA-1"))
-        .put("sha1.bc", new SHA1.Digest())
-        .put("blake2b", new Blake2b.Blake2b160())
+    static Map<String, HashProcessorFactory> HASHERS = ImmutableMap.<String, HashProcessorFactory>builder()
+        .put("md5.java", new MessageDigestHashProcessorFactory(getDigest("MD5")))
+        .put("md5.bc", new MessageDigestHashProcessorFactory(new MD5.Digest()))
+        .put("sha1.java", new MessageDigestHashProcessorFactory(getDigest("SHA-1")))
+        .put("sha1.bc", new MessageDigestHashProcessorFactory(new SHA1.Digest()))
+        .put("blake2b.bc", new MessageDigestHashProcessorFactory(new Blake2b.Blake2b160()))
+        .put("murmur3.guava", new GuavaProcessorFactory(Hashing.murmur3_128()))
         .build();
 
     Random random = new Random(1234L);
 
+    // @Param({"16", "1024", "65536"})
     @Param({"16", "1024", "65536"})
     int hashSize;
 
-    @Param({"md5.java", "md5.bc", "sha1.java", "sha1.bc", "blake2b"})
+    // @Param({"md5.java", "md5.bc", "sha1.java", "sha1.bc", "blake2b.bc"})
+    @Param({"md5.java", "murmur3.guava"})
     String type;
 
     byte[] input;
-    Factory<MessageDigest> digestFactory;
+    HashProcessorFactory processorFactory;
 
     @Setup(Level.Iteration)
     public void setup() throws CloneNotSupportedException {
         input = new byte[hashSize];
         random.nextBytes(input);
-        digestFactory = new Factory<MessageDigest>() {
-            @Override
-            public MessageDigest create() {
-                try {
-                    return (MessageDigest) DIGESTS.get(type).clone();
-                } catch (CloneNotSupportedException e) {
-                    throw new AssertionError();
-                }
-            }
-        };
+        processorFactory = HASHERS.get(type);
     }
 
     @Benchmark
     public void measure(Blackhole blackhole) {
-        MessageDigest digest = digestFactory.create();
-        digest.update(input);
-        byte[] hash = digest.digest();
-        blackhole.consume(hash);
+        HashProcessor processor = processorFactory.create();
+        processor.process(input, blackhole);
+    }
+
+    interface HashProcessor {
+        void process(byte[] input, Blackhole blackhole);
+    }
+
+    interface HashProcessorFactory {
+        HashProcessor create();
+    }
+
+    private static class MessageDigestHashProcessorFactory implements HashProcessorFactory {
+        private final MessageDigest baseDigest;
+
+        public MessageDigestHashProcessorFactory(MessageDigest baseDigest) {
+            this.baseDigest = baseDigest;
+        }
+
+        @Override
+        public HashProcessor create() {
+            try {
+                return new MessageDigestProcessor((MessageDigest) baseDigest.clone());
+            } catch (CloneNotSupportedException e) {
+                throw new AssertionError(e);
+            }
+        }
+    }
+
+    private static class MessageDigestProcessor implements HashProcessor {
+        private final MessageDigest digest;
+
+        public MessageDigestProcessor(MessageDigest digest) {
+            this.digest = digest;
+        }
+
+        @Override
+        public void process(byte[] input, Blackhole blackhole) {
+            digest.update(input);
+            byte[] hash = digest.digest();
+            blackhole.consume(hash);
+        }
+    }
+
+    private static class GuavaProcessorFactory implements HashProcessorFactory {
+        private final HashFunction hashFunction;
+
+        public GuavaProcessorFactory(HashFunction hashFunction) {
+            this.hashFunction = hashFunction;
+        }
+
+        @Override
+        public HashProcessor create() {
+            return new GuavaProcessor(hashFunction.newHasher());
+        }
+    }
+
+    private static class GuavaProcessor implements HashProcessor {
+        private final Hasher hasher;
+
+        public GuavaProcessor(Hasher hasher) {
+            this.hasher = hasher;
+        }
+
+        @Override
+        public void process(byte[] input, Blackhole blackhole) {
+            hasher.putBytes(input);
+            blackhole.consume(hasher.hash());
+        }
     }
 }
