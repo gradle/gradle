87502dfe301252c2ce9b4ceeed97501a2eaf5ee4
Bo Zhang <bo@gradle.com>
2017-08-23 09:12:49 +0800

    Refactor relocation integration test and add a test for double-relocatio

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/maven/MavenPomRelocationIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/maven/MavenPomRelocationIntegrationTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,38 +18,19 @@ package org.gradle.integtests.resolve.maven
 
 import org.gradle.integtests.fixtures.AbstractHttpDependencyResolutionTest
 import spock.lang.Issue
+import spock.lang.Unroll
 
 class MavenPomRelocationIntegrationTest extends AbstractHttpDependencyResolutionTest {
-    @Issue('https://github.com/gradle/gradle/issues/1789')
-    def "ignore original artifact when <relocation> exists"() {
-        given:
-        def original = mavenHttpRepo.module("groupA", "projectA", "1.2").publishPom()
-        original.pomFile.text = """
-<project>
-    <groupId>groupA</groupId>
-    <artifactId>projectA</artifactId>
-    <version>1.2</version>
-    <distributionManagement>
-        <relocation>
-            <groupId>groupB</groupId>
-            <artifactId>projectB</artifactId>
-        </relocation>
-    </distributionManagement>
-</project>
-"""
 
-        def newModule = mavenHttpRepo.module("groupB", "projectB", "1.2").publish()
+    @Issue('https://github.com/gradle/gradle/issues/1789')
+    @Unroll
+    def "succeed when relocation exists"() {
+        given:
+        def original = publishPomWithRelocation('groupA', 'projectA', relocationGroupId, relocationArtifactId)
+        def newModule = mavenHttpRepo.module(newGroupId, newArtifactId, "1.0").publish()
 
         and:
-        buildFile << """
-repositories { maven { url '${mavenHttpRepo.uri}' } }
-configurations { compile }
-dependencies { compile 'groupA:projectA:1.2' }
-task retrieve(type: Sync) {
-    into 'libs'
-    from configurations.compile
-}
-"""
+        createBuildFileWithDependency('groupA', 'projectA')
 
         and:
         original.pom.expectGet()
@@ -60,79 +41,43 @@ task retrieve(type: Sync) {
         run "retrieve"
 
         then:
-        file("libs").assertHasDescendants("projectB-1.2.jar")
+        file("libs").assertHasDescendants("${newArtifactId}-1.0.jar")
+
+        where:
+        relocationGroupId | relocationArtifactId | newGroupId  | newArtifactId
+        'groupB'          | 'projectB'           | 'groupB'    | 'projectB'
+        'newGroupA'       | ''                   | 'newGroupA' | 'projectA'
     }
 
-    def "retrieve artifact when only groupId in <relocation>"() {
+    def "double-relocation should succeed"() {
         given:
-        def original = mavenHttpRepo.module("groupA", "projectA", "1.2").publishPom()
-        original.pomFile.text = """
-<project>
-    <groupId>groupA</groupId>
-    <artifactId>projectA</artifactId>
-    <version>1.2</version>
-    <distributionManagement>
-        <relocation>
-            <groupId>newProjectA</groupId>
-        </relocation>
-    </distributionManagement>
-</project>
-"""
-
-        def newModule = mavenHttpRepo.module("newProjectA", "projectA", "1.2").publish()
+        def moduleA = publishPomWithRelocation('groupA', 'projectA', 'groupB', 'projectB')
+        def moduleB = publishPomWithRelocation('groupB', 'projectB', 'groupC', 'projectC')
+        def moduleC = mavenHttpRepo.module('groupC', 'projectC', "1.0").publish()
 
         and:
-        buildFile << """
-repositories { maven { url '${mavenHttpRepo.uri}' } }
-configurations { compile }
-dependencies { compile 'groupA:projectA:1.2' }
-task retrieve(type: Sync) {
-    into 'libs'
-    from configurations.compile
-}
-"""
+        createBuildFileWithDependency('groupA', 'projectA')
 
         and:
-        original.pom.expectGet()
-        newModule.pom.expectGet()
-        newModule.artifact.expectGet()
+        moduleA.pom.expectGet()
+        moduleB.pom.expectGet()
+        moduleC.pom.expectGet()
+        moduleC.artifact.expectGet()
 
         when:
         run "retrieve"
 
         then:
-        file("libs").assertHasDescendants("projectA-1.2.jar")
+        file("libs").assertHasDescendants("projectC-1.0.jar")
     }
 
     def "fail when artifact in <relocation> doesn't exist"() {
         given:
-        def original = mavenHttpRepo.module("groupA", "projectA", "1.2").publishPom()
-        original.pomFile.text = """
-<project>
-    <groupId>groupA</groupId>
-    <artifactId>projectA</artifactId>
-    <version>1.2</version>
-    <distributionManagement>
-        <relocation>
-            <groupId>notExist</groupId>
-            <artifactId>notExist</artifactId>
-        </relocation>
-    </distributionManagement>
-</project>
-"""
-
-        def newModule = mavenHttpRepo.module("notExist", "notExist", "1.2")
+        def original = publishPomWithRelocation('groupA', 'projectA', 'notExist', 'notExist')
+        def newModule = mavenHttpRepo.module("notExist", "notExist", "1.0")
 
         and:
-        buildFile << """
-repositories { maven { url '${mavenHttpRepo.uri}' } }
-configurations { compile }
-dependencies { compile 'groupA:projectA:1.2' }
-task retrieve(type: Sync) {
-    into 'libs'
-    from configurations.compile
-}
-"""
+        createBuildFileWithDependency('groupA', 'projectA')
 
         and:
         original.pom.expectGet()
@@ -141,6 +86,38 @@ task retrieve(type: Sync) {
 
         expect:
         fails "retrieve"
-        failure.assertHasCause('Could not find notExist:notExist:1.2')
+        failure.assertHasCause('Could not find notExist:notExist:1.0')
+    }
+
+    def createBuildFileWithDependency(String groupId, String artifactId) {
+        buildFile << """
+repositories { maven { url '${mavenHttpRepo.uri}' } }
+configurations { compile }
+dependencies { compile '${groupId}:${artifactId}:1.0' }
+task retrieve(type: Sync) {
+    into 'libs'
+    from configurations.compile
+}
+"""
+    }
+
+    def publishPomWithRelocation(String groupId, String artifactId, String relocationGroupId, String relocationArtifactId) {
+        def module = mavenHttpRepo.module(groupId, artifactId, '1.0').publishPom()
+        def relocation = "<groupId>${relocationGroupId}</groupId>"
+        relocation += relocationArtifactId ? "<artifactId>${relocationArtifactId}</artifactId>" : ''
+
+        module.pomFile.text = """
+<project>
+    <groupId>${groupId}</groupId>
+    <artifactId>${artifactId}</artifactId>
+    <version>1.0</version>
+    <distributionManagement>
+        <relocation>
+            ${relocation}
+        </relocation>
+    </distributionManagement>
+</project>
+"""
+        return module
     }
 }
