7888e6433154d458c7b6d3681e668929cbe7e134
Jendrik Johannes <jendrik@gradle.com>
2017-08-25 08:56:12 +0200

    Remove outdated and unused PR build configuration files

--- travisci_build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo "==== This was not a pull request, nothing executed! ===="
-else
-  ./gradlew prBuild$CI_TEST_SPLIT -x integTest --continue --stacktrace
-fi
