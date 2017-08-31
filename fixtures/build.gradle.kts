f425efa64d10cfbaf248a064449bce70dffb6b57
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2017-08-31 17:15:59 -0400

    Skip testing the `plugin-compiled-against-kotlin-1.0` for JDK 9

--- fixtures/build.gradle.kts
+++ fixtures/build.gradle.kts
@@ -3,7 +3,14 @@ import org.gradle.api.tasks.GradleBuild
 fun isProjectDir(candidate: File) =
     candidate.isDirectory && File(candidate, "settings.gradle").exists()
 
-val subProjectTasks = rootDir.listFiles().filter { isProjectDir(it) }.map { subProjectDir ->
+fun isCompatibleWithJDK(candidate: File) =
+    candidate.name.endsWith("kotlin-1.0") && JavaVersion.current() < JavaVersion.VERSION_1_9
+
+val subProjectTasks = rootDir
+    .listFiles()
+    .filter { isProjectDir(it) }
+    .filter { isCompatibleWithJDK(it) }
+    .map { subProjectDir ->
     task<GradleBuild>("prepare-${subProjectDir.name}") {
         setDir(subProjectDir)
     }
