10d073ab321e2daa987d06202aed969f0d416534
Luke Daley <luke@gradle.com>
2017-08-31 11:29:16 +1000

    Fix Javadoc formatting.

--- subprojects/logging/src/main/java/org/gradle/internal/logging/services/LoggingServiceRegistry.java
+++ subprojects/logging/src/main/java/org/gradle/internal/logging/services/LoggingServiceRegistry.java
@@ -40,8 +40,13 @@ import org.gradle.internal.time.TimeProvider;
 /**
  * A {@link org.gradle.internal.service.ServiceRegistry} implementation that provides the logging services. To use this:
  *
- * <ol> <li>Create an instance using one of the static factory methods below.</li> <li>Create an instance of {@link LoggingManagerInternal}.</li> <li>Configure the logging manager as appropriate.</li>
- * <li>Start the logging manager using {@link LoggingManagerInternal#start()}.</li> <li>When finished, stop the logging manager using {@link LoggingManagerInternal#stop()}.</li> </ol>
+ * <ol>
+ *     <li>Create an instance using one of the static factory methods below.</li>
+ *     <li>Create an instance of {@link LoggingManagerInternal}.</li>
+ *     <li>Configure the logging manager as appropriate.</li>
+ *     <li>Start the logging manager using {@link LoggingManagerInternal#start()}.</li>
+ *     <li>When finished, stop the logging manager using {@link LoggingManagerInternal#stop()}.</li>
+ * </ol>
  */
 public abstract class LoggingServiceRegistry extends DefaultServiceRegistry {
     private TextStreamOutputEventListener stdoutListener;
@@ -49,14 +54,16 @@ public abstract class LoggingServiceRegistry extends DefaultServiceRegistry {
     /**
      * Creates a set of logging services which are suitable to use globally in a process. In particular:
      *
-     * <ul> <li>Replaces System.out and System.err with implementations that route output through the logging system as per {@link LoggingManagerInternal#captureSystemSources()}.</li> <li>Configures
-     * slf4j, log4j and java util logging to route log messages through the logging system.</li> <li>Routes logging output to the original System.out and System.err as per {@link
-     * LoggingManagerInternal#attachSystemOutAndErr()}.</li> <li>Sets log level to {@link org.gradle.api.logging.LogLevel#LIFECYCLE}.</li> </ul>
+     * <ul>
+     *     <li>Replaces System.out and System.err with implementations that route output through the logging system as per {@link LoggingManagerInternal#captureSystemSources()}.</li>
+     *     <li>Configures slf4j, log4j and java util logging to route log messages through the logging system.</li>
+     *     <li>Routes logging output to the original System.out and System.err as per {@link LoggingManagerInternal#attachSystemOutAndErr()}.</li>
+     *     <li>Sets log level to {@link org.gradle.api.logging.LogLevel#LIFECYCLE}.</li>
+     * </ul>
      *
      * <p>Does nothing until started.</p>
      *
-     * <p>Allows dynamic and colored output to be written to the console. Use {@link LoggingManagerInternal#attachProcessConsole(org.gradle.api.logging.configuration.ConsoleOutput)} to enable
-     * this.</p>
+     * <p>Allows dynamic and colored output to be written to the console. Use {@link LoggingManagerInternal#attachProcessConsole(org.gradle.api.logging.configuration.ConsoleOutput)} to enable this.</p>
      */
     public static LoggingServiceRegistry newCommandLineProcessLogging() {
         CommandLineLogging loggingServices = new CommandLineLogging();
@@ -69,13 +76,18 @@ public abstract class LoggingServiceRegistry extends DefaultServiceRegistry {
     /**
      * Creates a set of logging services which are suitable to use embedded in another application. In particular:
      *
-     * <ul> <li>Configures slf4j and log4j to route log messages through the logging system.</li> <li>Sets log level to {@link org.gradle.api.logging.LogLevel#LIFECYCLE}.</li> </ul>
+     * <ul>
+     *     <li>Configures slf4j and log4j to route log messages through the logging system.</li>
+     *     <li>Sets log level to {@link org.gradle.api.logging.LogLevel#LIFECYCLE}.</li>
+     * </ul>
      *
      * <p>Does not:</p>
      *
-     * <ul> <li>Replace System.out and System.err to capture output written to these destinations. Use {@link LoggingManagerInternal#captureSystemSources()} to enable this.</li> <li>Configure java
-     * util logging. Use {@link LoggingManagerInternal#captureSystemSources()} to enable this.</li> <li>Route logging output to the original System.out and System.err. Use {@link
-     * LoggingManagerInternal#attachSystemOutAndErr()} to enable this.</li> </ul>
+     * <ul>
+     *     <li>Replace System.out and System.err to capture output written to these destinations. Use {@link LoggingManagerInternal#captureSystemSources()} to enable this.</li>
+     *     <li>Configure java util logging. Use {@link LoggingManagerInternal#captureSystemSources()} to enable this.</li>
+     *     <li>Route logging output to the original System.out and System.err. Use {@link LoggingManagerInternal#attachSystemOutAndErr()} to enable this.</li>
+     * </ul>
      *
      * <p>Does nothing until started.</p>
      */
@@ -84,8 +96,8 @@ public abstract class LoggingServiceRegistry extends DefaultServiceRegistry {
     }
 
     /**
-     * Creates a set of logging services to set up a new logging scope that does nothing by default. The methods on {@link LoggingManagerInternal} can be used to configure the logging services do
-     * useful things.
+     * Creates a set of logging services to set up a new logging scope that does nothing by default. The methods on {@link LoggingManagerInternal} can be used to configure the
+     * logging services do useful things.
      *
      * <p>Sets log level to {@link org.gradle.api.logging.LogLevel#LIFECYCLE}.</p>
      */
