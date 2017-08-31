a56b0f90aadd0abfd662a9f9d5b53fb4b3e46dce
Benjamin Muschko <ben@gradle.com>
2017-08-31 12:16:45 -0400

    Extracted options into another factory

---
+++ subprojects/core/src/main/java/org/gradle/initialization/BuildLayoutParametersBuildOptionFactory.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.initialization;
+
+import org.gradle.api.Transformer;
+import org.gradle.api.initialization.Settings;
+import org.gradle.api.internal.file.BasicFileResolver;
+import org.gradle.initialization.option.BuildOption;
+import org.gradle.initialization.option.BuildOptionFactory;
+import org.gradle.initialization.option.CommandLineOptionConfiguration;
+import org.gradle.initialization.option.NoArgumentBuildOption;
+import org.gradle.initialization.option.StringBuildOption;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuildLayoutParametersBuildOptionFactory implements BuildOptionFactory<BuildLayoutParameters> {
+
+    @Override
+    public List<BuildOption<BuildLayoutParameters>> create() {
+        List<BuildOption<BuildLayoutParameters>> options = new ArrayList<BuildOption<BuildLayoutParameters>>();
+        options.add(new GradleUserHomeOption());
+        options.add(new ProjectDirOption());
+        options.add(new NoSearchUpwardsOption());
+        return options;
+    }
+
+    public static class GradleUserHomeOption extends StringBuildOption<BuildLayoutParameters> {
+        public GradleUserHomeOption() {
+            super(BuildLayoutParameters.class, "gradle.user.home", CommandLineOptionConfiguration.create("gradle-user-home", "g", "Specifies the gradle user home directory."));
+        }
+
+        @Override
+        public void applyTo(String value, BuildLayoutParameters settings) {
+            Transformer<File, String> resolver = new BasicFileResolver(settings.getCurrentDir());
+            settings.setGradleUserHomeDir(resolver.transform(value));
+        }
+    }
+
+    public static class ProjectDirOption extends StringBuildOption<BuildLayoutParameters> {
+        public ProjectDirOption() {
+            super(BuildLayoutParameters.class, null, CommandLineOptionConfiguration.create("project-dir", "p", "Specifies the start directory for Gradle. Defaults to current directory."));
+        }
+
+        @Override
+        public void applyTo(String value, BuildLayoutParameters settings) {
+            Transformer<File, String> resolver = new BasicFileResolver(settings.getCurrentDir());
+            settings.setProjectDir(resolver.transform(value));
+        }
+    }
+
+    public static class NoSearchUpwardsOption extends NoArgumentBuildOption<BuildLayoutParameters> {
+        public NoSearchUpwardsOption() {
+            super(BuildLayoutParameters.class, null, CommandLineOptionConfiguration.create("no-search-upward", "u", "Don't search in parent folders for a " + Settings.DEFAULT_SETTINGS_FILE + " file."));
+        }
+
+        @Override
+        public void applyTo(BuildLayoutParameters settings) {
+            settings.setSearchUpwards(false);
+        }
+    }
+}
