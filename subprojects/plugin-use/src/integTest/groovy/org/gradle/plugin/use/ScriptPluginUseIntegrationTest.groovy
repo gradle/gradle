502b496e3f49614245b1491fd1fa08bc8e98c11a
Paul Merlin <paul@gradle.com>
2017-09-01 19:43:28 +0200

    Move ScriptPluginUseIntegrationTest to its right package

--- subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/use/ScriptPluginUseIntegrationTest.groovy
+++ subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/use/ScriptPluginUseIntegrationTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gradle.plugin.use.resolve.service
+package org.gradle.plugin.use
 
 import org.gradle.api.internal.plugins.ApplyPluginBuildOperationType
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
