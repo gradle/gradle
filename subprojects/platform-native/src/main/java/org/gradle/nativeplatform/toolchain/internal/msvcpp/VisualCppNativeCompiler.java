e60fb980855c434c6823fe1b3651b93942a5be44
Adam Murdoch <adam@gradle.com>
2017-09-02 15:48:34 +1000

    Generate a separate `pdb` file for each object file when compiling using Visual C++, to allow compilation of multiple source files to happen in parallel.

--- subprojects/platform-native/src/main/java/org/gradle/nativeplatform/toolchain/internal/msvcpp/VisualCppNativeCompiler.java
+++ subprojects/platform-native/src/main/java/org/gradle/nativeplatform/toolchain/internal/msvcpp/VisualCppNativeCompiler.java
@@ -29,7 +29,6 @@ import org.gradle.nativeplatform.toolchain.internal.OptionsFileArgsWriter;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 class VisualCppNativeCompiler<T extends NativeCompileSpec> extends NativeCompiler<T> {
@@ -39,9 +38,14 @@ class VisualCppNativeCompiler<T extends NativeCompileSpec> extends NativeCompile
     }
 
     @Override
-    protected List<String> getOutputArgs(File outputFile) {
+    protected List<String> getOutputArgs(T spec, File outputFile) {
+        List<String> args = new ArrayList<String>();
+        if (spec.isDebuggable()) {
+            args.add("/Fd" + new File(outputFile.getParentFile(), outputFile.getName() + ".pdb"));
+        }
         // MSVC doesn't allow a space between Fo and the file name
-        return Collections.singletonList("/Fo" + outputFile.getAbsolutePath());
+        args.add("/Fo" + outputFile.getAbsolutePath());
+        return args;
     }
 
     @Override
