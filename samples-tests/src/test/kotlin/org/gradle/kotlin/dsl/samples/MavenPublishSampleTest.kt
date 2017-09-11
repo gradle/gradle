ac5b883ccf454e1fde6f1e09d4679c573f3f34d6
Joshua Shrader <jshrader83@gmail.com>
2017-09-11 10:05:22 -0400

    Cleaned up integration test.
    Use static imports for assertions.
    Insure JarFile instances are closed.

--- samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/MavenPublishSampleTest.kt
+++ samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/MavenPublishSampleTest.kt
@@ -1,6 +1,6 @@
 package org.gradle.kotlin.dsl.samples
 
-import org.junit.Assert
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import java.nio.file.Paths
 import java.util.jar.JarFile
@@ -18,22 +18,26 @@ class MavenPublishSampleTest : AbstractSampleTest("maven-publish") {
         val repo = projectRoot.toPath().resolve("build").resolve("repo")
         val mavenPath = repo.resolve(Paths.get("org", "gradle", "sample", projectName, projectVersion))
 
-        // repo contains jar file
+        // repo contains default jar file
         mavenPath.resolve("$projectName-$projectVersion.jar").toFile().let { jarFile ->
-            Assert.assertTrue("jar file $jarFile exists", jarFile.exists())
+            assertTrue("jar file $jarFile exists", jarFile.exists())
 
             // jar file contains .class entries
-            val numClassEntries = JarFile(jarFile).stream().filter { it.name.endsWith(".class") }.count()
-            Assert.assertTrue(numClassEntries > 0)
+            JarFile(jarFile).use {
+                val numClassEntries = it.stream().filter { it.name.endsWith(".class") }.count()
+                assertTrue(numClassEntries > 0)
+            }
         }
 
         // repo contains source file
         mavenPath.resolve("$projectName-$projectVersion-sources.jar").toFile().let { sourceFile ->
-            Assert.assertTrue("source file $sourceFile exists", sourceFile.exists())
+            assertTrue("source file $sourceFile exists", sourceFile.exists())
 
             // source file contains java and kt files
-            Assert.assertTrue(JarFile(sourceFile).stream().anyMatch { it.name.endsWith(".java") })
-            Assert.assertTrue(JarFile(sourceFile).stream().anyMatch { it.name.endsWith(".kt") })
+            JarFile(sourceFile).use {
+                assertTrue(it.stream().anyMatch { it.name.endsWith(".java") })
+                assertTrue(it.stream().anyMatch { it.name.endsWith(".kt") })
+            }
         }
     }
 }
