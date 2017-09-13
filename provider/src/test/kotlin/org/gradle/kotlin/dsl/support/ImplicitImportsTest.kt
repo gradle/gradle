32b440ad03c3fd03d6e80faef2f3ca73c433bb70
Paul Merlin <paul@gradle.com>
2017-09-13 10:38:04 +0200

    Simplify ImplicitImportsTest

--- provider/src/test/kotlin/org/gradle/kotlin/dsl/support/ImplicitImportsTest.kt
+++ provider/src/test/kotlin/org/gradle/kotlin/dsl/support/ImplicitImportsTest.kt
@@ -1,11 +1,9 @@
 package org.gradle.kotlin.dsl.support
 
 import org.gradle.kotlin.dsl.fixtures.AbstractIntegrationTest
-import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.Assert.assertThat
 import org.junit.Test
-import java.io.File
-import java.util.jar.JarFile
 
 class ImplicitImportsTest : AbstractIntegrationTest() {
 
@@ -13,30 +11,16 @@ class ImplicitImportsTest : AbstractIntegrationTest() {
     fun `implicit imports are fully qualified to allow use of the preferred type amongst those with same simple name in different Gradle API packages`() {
 
         // given:
-        existing("settings.gradle").appendText("""
-            rootProject.name = "some-project"
-        """)
         withBuildScript("""
 
-            plugins {
-                java
-            }
-
-            // Prefer org.gradle.api.tasks.bundling.Jar over org.gradle.jvm.tasks.Jar
-            tasks.withType<Jar> {
-                manifest {
-                    attributes(mapOf("Foo" to "Bar"))
-                }
-            }
+            println("*" + Jar::class.qualifiedName + "*")
 
         """)
 
         // when:
-        build("jar")
+        val result = build("help")
 
         // then:
-        JarFile(File(projectRoot, "build/libs/some-project.jar")).use { jar ->
-            assertThat(jar.manifest.mainAttributes.getValue("Foo"), equalTo("Bar"))
-        }
+        assertThat(result.output, containsString("*org.gradle.jvm.tasks.Jar*"))
     }
 }
