18a0b44e98dd6187fb41b4c6230f11173c318fc3
Paul Merlin <paul@gradle.com>
2017-09-13 10:23:36 +0200

    Refine JavaScript Sample build script
    Prefer using the `tasks` block
    
    Split web assembly into two tasks:
    - unpacking the Kotlin JS stdlib
    - assembling all web bits
    
    Model these tasks with proper inputs and outputs
    Remove unnecessary files from the output

--- samples/hello-js/build.gradle.kts
+++ samples/hello-js/build.gradle.kts
@@ -21,32 +21,46 @@ repositories {
     jcenter()
 }
 
-tasks.withType<Kotlin2JsCompile> {
-    kotlinOptions.outputFile = "$buildDir/web/output.js"
-    kotlinOptions.sourceMap = true
-}
+val mainSourceSet = the<JavaPluginConvention>().sourceSets["main"]!!
 
-val assembleWeb by tasks.creating
-assembleWeb.doLast {
-    configurations["compileClasspath"].forEach { file: File ->
-        copy {
-            includeEmptyDirs = false
-
-            from(zipTree(file.absoluteFile))
-            into("$buildDir/web")
-            include { fileTreeElement ->
-                val path = fileTreeElement.path
-                path.endsWith(".js") && (path.startsWith("META-INF/resources/") || !path.startsWith("META-INF/"))
+tasks {
+    "compileKotlin2Js"(Kotlin2JsCompile::class) {
+        kotlinOptions {
+            outputFile = "${mainSourceSet.output.resourcesDir}/output.js"
+            sourceMap = true
+        }
+    }
+    val unpackKotlinJsStdlib by creating {
+        group = "build"
+        description = "Unpack the Kotlin JavaScript standard library"
+        val outputDir = file("$buildDir/$name")
+        val compileClasspath = configurations["compileClasspath"]
+        inputs.property("compileClasspath", compileClasspath)
+        outputs.dir(outputDir)
+        doLast {
+            val kotlinStdLibJar = compileClasspath.single {
+                it.name.matches(Regex("kotlin-stdlib-js-.+\\.jar"))
+            }
+            copy {
+                includeEmptyDirs = false
+                from(zipTree(kotlinStdLibJar))
+                into(outputDir)
+                include("**/*.js")
+                exclude("META-INF/**")
             }
         }
     }
-    copy {
-        from("${the<JavaPluginConvention>().sourceSets["main"].output.resourcesDir}/index.html")
+    val assembleWeb by creating(Copy::class) {
+        group = "build"
+        description = "Assemble the web application"
+        includeEmptyDirs = false
+        from(unpackKotlinJsStdlib)
+        from(mainSourceSet.output) {
+            exclude("**/*.kjsm")
+        }
         into("$buildDir/web")
     }
+    "assemble" {
+        dependsOn(assembleWeb)
+    }
 }
-
-val assemble by tasks
-val classes by tasks
-assemble.dependsOn(assembleWeb)
-assembleWeb.dependsOn(classes)
