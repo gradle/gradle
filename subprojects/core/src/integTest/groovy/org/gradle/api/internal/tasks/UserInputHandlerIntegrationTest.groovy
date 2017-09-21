5e75dca5a7a8b1ebba3d01e971b317e9365e5960
Benjamin Muschko <ben@gradle.com>
2017-09-21 09:35:14 -0600

    More testing

--- subprojects/core/src/integTest/groovy/org/gradle/api/internal/tasks/UserInputHandlerIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/internal/tasks/UserInputHandlerIntegrationTest.groovy
@@ -17,15 +17,20 @@
 package org.gradle.api.internal.tasks
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.executer.GradleHandle
+import org.gradle.test.fixtures.ConcurrentTestUtil
+import org.gradle.util.ToBeImplemented
+import spock.lang.Ignore
 import spock.lang.Unroll
 
 import static org.gradle.util.TextUtil.getPlatformLineSeparator
 
 class UserInputHandlerIntegrationTest extends AbstractIntegrationSpec {
 
-    public static final String USER_INPUT_SUPPORT_TASK_NAME = 'userInputSupport'
-    public static final String USER_INPUT_REQUEST_TASK_NAME = 'userInputRequest'
-    public static final String USER_INPUT = 'Hello World'
+    private static final String USER_INPUT_SUPPORT_TASK_NAME = 'userInputSupport'
+    private static final String USER_INPUT_REQUEST_TASK_NAME = 'userInputRequest'
+    private static final String HELLO_WORLD_USER_INPUT = 'Hello World'
+    private static final int WAIT_FOR_SHUTDOWN_TIMEOUT_SECONDS = 20
 
     @Unroll
     def "can capture user input for interactive build [daemon enabled: #useDaemon, rich console: #richConsole]"() {
@@ -48,12 +53,34 @@ class UserInputHandlerIntegrationTest extends AbstractIntegrationSpec {
         def gradleHandle = executer.start()
 
         then:
-        gradleHandle.stdinPipe.write(USER_INPUT.bytes)
+        gradleHandle.stdinPipe.write(HELLO_WORLD_USER_INPUT.bytes)
         gradleHandle.stdinPipe.write(getPlatformLineSeparator().bytes)
         gradleHandle.waitForFinish()
 
         where:
         [useDaemon, richConsole] << [[false, true], [false, true]].combinations()
+    }
+
+    def "can request user input multiple times"() {
+        given:
+        def ageInput = '15'
+        interactiveExecution()
+        buildFile << userInputRequestedTask()
+        buildFile << """
+            ${USER_INPUT_REQUEST_TASK_NAME}.doLast {
+                ${verifyUserInput('Please provide your age', ageInput)}
+            }
+        """
+
+        when:
+        def gradleHandle = executer.withTasks(USER_INPUT_REQUEST_TASK_NAME).start()
+
+        then:
+        gradleHandle.stdinPipe.write(HELLO_WORLD_USER_INPUT.bytes)
+        gradleHandle.stdinPipe.write(getPlatformLineSeparator().bytes)
+        gradleHandle.stdinPipe.write(ageInput.bytes)
+        gradleHandle.stdinPipe.write(getPlatformLineSeparator().bytes)
+        gradleHandle.waitForFinish()
     }
 
     def "fails gracefully if console is not interactive"() {
@@ -69,34 +96,64 @@ class UserInputHandlerIntegrationTest extends AbstractIntegrationSpec {
         failure.assertHasCause('Console does not support capturing input')
     }
 
+    @Ignore
+    @ToBeImplemented
+    def "can cancel build during user input"() {
+        given:
+        interactiveExecution()
+        buildFile << userInputRequestedTask()
+
+        when:
+        def gradleHandle = executer.withTasks(USER_INPUT_REQUEST_TASK_NAME).start()
+
+        then:
+        gradleHandle.cancelWithEOT().waitForFinish()
+        waitForNotRunning(gradleHandle)
+        assert gradleHandle.standardOutput.contains('Build cancelled.')
+    }
+
     private void interactiveExecution() {
         executer.withStdinPipe().withForceInteractive(true)
     }
 
+    private waitForNotRunning(GradleHandle gradleHandle) {
+        ConcurrentTestUtil.poll(WAIT_FOR_SHUTDOWN_TIMEOUT_SECONDS) {
+            assert !gradleHandle.running
+        }
+    }
+
     static String userInputSupportedTask(boolean supported) {
         """
-            import org.gradle.api.internal.tasks.UserInputHandler
-
             task $USER_INPUT_SUPPORT_TASK_NAME {
                 doLast {
-                    def userInputHandler = project.services.get(UserInputHandler)
+                    ${createUserInputHandler()}
                     assert userInputHandler.userInputSupported == $supported
                 }
             }
         """
     }
 
-    static String userInputRequestedTask() {
+    static String userInputRequestedTask(String prompt = 'Enter your response', String expectedInput = HELLO_WORLD_USER_INPUT) {
         """
-            import org.gradle.api.internal.tasks.UserInputHandler
-
             task $USER_INPUT_REQUEST_TASK_NAME {
                 doLast {
-                    def userInputHandler = project.services.get(UserInputHandler)
-                    def response = userInputHandler.getUserResponse('Enter your response:')
-                    assert response == '$USER_INPUT'
+                    ${verifyUserInput(prompt, expectedInput)}
                 }
             }
+        """
+    }
+
+    static String verifyUserInput(String prompt, String expectedInput) {
+        """
+            ${createUserInputHandler()}
+            def response = userInputHandler.getUserResponse('$prompt:')
+            assert response == '$expectedInput'
+        """
+    }
+
+    static String createUserInputHandler() {
+        """
+            def userInputHandler = project.services.get(${UserInputHandler.class.getName()})
         """
     }
 }
