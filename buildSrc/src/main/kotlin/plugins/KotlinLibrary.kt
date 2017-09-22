97291af2b04de5932e82c8589d27150469043e26
Paul Merlin <paul@gradle.com>
2017-09-22 16:36:45 +0200

    :arrow-up: Kotlin 1.1.50 :tada:
    And migrate to the new compiler flags for JSR-305 support

--- buildSrc/src/main/kotlin/plugins/KotlinLibrary.kt
+++ buildSrc/src/main/kotlin/plugins/KotlinLibrary.kt
@@ -18,7 +18,7 @@ open class KotlinLibrary : Plugin<Project> {
         tasks.withType(KotlinCompile::class.java) {
             it.kotlinOptions.apply {
                 freeCompilerArgs = listOf(
-                    "-Xjsr305-annotations=enable",
+                    "-Xjsr305=strict",
                     "-Xskip-runtime-version-check")
             }
         }
