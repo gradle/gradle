f26d6dc8213605ecb6443d0137707e142c45be1a
Cedric Champeau <cedric@gradle.com>
2017-09-25 20:52:49 +0200

    Re-enable test

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
@@ -18,7 +18,6 @@ package org.gradle.performance.regression.corefeature
 
 import org.gradle.performance.AbstractCrossVersionPerformanceTest
 import org.gradle.performance.WithExternalRepository
-import spock.lang.Ignore
 
 class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceTest implements WithExternalRepository {
 
@@ -48,7 +47,6 @@ class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceT
         stopServer()
     }
 
-    @Ignore
     def "merge exclude rules (parallel)"() {
         runner.testProject = TEST_PROJECT_NAME
         startServer()
