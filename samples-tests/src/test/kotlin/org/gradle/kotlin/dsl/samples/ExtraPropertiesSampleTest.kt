d102df02602bc49f7a3d3956f3e3f047062f5dd7
Paul Merlin <paul@gradle.com>
2017-09-29 12:03:14 +0200

    Restore extra properties type info & allow nullability
    Optional extra properties can now be expressed via nullability of the
    property type.
    
    val name by extra { null } // creates null extra property
    
    val name: String by extra  // throws NPE on property usage
    val name: String? by extra // works as expected
    
    This commit enhances the `extra-properties` sample to demonstrate this.
    
    See #512

--- samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/ExtraPropertiesSampleTest.kt
+++ samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/ExtraPropertiesSampleTest.kt
@@ -2,6 +2,7 @@ package org.gradle.kotlin.dsl.samples
 
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.not
 import org.junit.Assert.assertThat
 import org.junit.Test
 
@@ -12,7 +13,12 @@ class ExtraPropertiesSampleTest : AbstractSampleTest("extra-properties") {
     fun `extra properties`() {
         assertThat(
             build("myTask").output,
-            allOf(containsString("myTask.foo = 42"),
-                  containsString("Extra property value: 42")))
+            allOf(
+                containsString("myTask.foo = 42"),
+                containsString("Extra foo property value: 42"),
+                containsString("myTask.bar = null"),
+                containsString("Optional extra bar property value: null"),
+                not(containsString("myTask.bazar")),
+                containsString("Optional extra bazar property value: null")))
     }
 }
