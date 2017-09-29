f99bbfc61266c34c13408ece12d96c4b9f6de99d
Paul Merlin <paul@gradle.com>
2017-09-29 16:14:21 +0200

    Move extra props nullability mismatch test case to an integration test

--- samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/ExtraPropertiesSampleTest.kt
+++ samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/ExtraPropertiesSampleTest.kt
@@ -2,7 +2,6 @@ package org.gradle.kotlin.dsl.samples
 
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.containsString
-import org.hamcrest.CoreMatchers.not
 import org.junit.Assert.assertThat
 import org.junit.Test
 
@@ -17,8 +16,6 @@ class ExtraPropertiesSampleTest : AbstractSampleTest("extra-properties") {
                 containsString("myTask.foo = 42"),
                 containsString("Extra foo property value: 42"),
                 containsString("myTask.bar = null"),
-                containsString("Optional extra bar property value: null"),
-                not(containsString("myTask.bazar")),
-                containsString("Optional extra bazar property value: null")))
+                containsString("Optional extra bar property value: null")))
     }
 }
--- samples/extra-properties/build.gradle.kts
+++ samples/extra-properties/build.gradle.kts
@@ -2,28 +2,19 @@ val myTask = task("myTask") {
 
     val foo by extra { 42 }
     val bar by extra<Int?>(null)
-    val bazar: Int? by extra { null }
 
     doLast {
         println("Extra foo property value: $foo")
         println("Optional extra bar property value: $bar")
-        println("Optional extra bazar property value: $bazar")
     }
 }
 
 val foo: Int by myTask.extra
 val bar: Int? by myTask.extra
-val bazar: Int by myTask.extra
 
 afterEvaluate {
     println("myTask.foo = $foo")
     println("myTask.bar = $bar")
-    try {
-        println("myTask.bazar = $bazar")
-        require(false, { "Should not happen as `bazar`, requested as a Int is effectively null" })
-    } catch (ex: NullPointerException) {
-        // expected
-    }
 }
 
 defaultTasks(myTask.name)
