f77a6300c706056ad5ce92fea233f2a2f6f18be7
Stefan Wolf <wolf@gradle.com>
2017-10-06 17:16:47 +0200

    Only create depend tasks for binary tasks
    And not for all compile tasks.

--- subprojects/language-native/src/main/java/org/gradle/language/nativeplatform/plugins/DependPlugin.java
+++ subprojects/language-native/src/main/java/org/gradle/language/nativeplatform/plugins/DependPlugin.java
@@ -16,18 +16,31 @@
 
 package org.gradle.language.nativeplatform.plugins;
 
+import org.apache.commons.lang.StringUtils;
 import org.gradle.api.Action;
 import org.gradle.api.Incubating;
 import org.gradle.api.NonNullApi;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.specs.Spec;
+import org.gradle.language.base.LanguageSourceSet;
+import org.gradle.language.base.internal.LanguageSourceSetInternal;
+import org.gradle.language.c.CSourceSet;
 import org.gradle.language.c.tasks.CCompile;
+import org.gradle.language.cpp.CppBinary;
+import org.gradle.language.cpp.CppSourceSet;
 import org.gradle.language.cpp.tasks.CppCompile;
+import org.gradle.language.nativeplatform.internal.Names;
 import org.gradle.language.nativeplatform.tasks.AbstractNativeCompileTask;
 import org.gradle.language.nativeplatform.tasks.Depend;
+import org.gradle.model.ModelMap;
+import org.gradle.model.RuleSource;
+import org.gradle.nativeplatform.internal.NativeBinarySpecInternal;
 import org.gradle.nativeplatform.toolchain.Clang;
 import org.gradle.nativeplatform.toolchain.Gcc;
 import org.gradle.nativeplatform.toolchain.NativeToolChain;
+import org.gradle.platform.base.BinaryTasks;
 
 import java.util.concurrent.Callable;
 
@@ -46,36 +59,80 @@ public class DependPlugin implements Plugin<Project> {
 
     @Override
     public void apply(final Project project) {
-        CreateDependTaskAction createDependAction = new CreateDependTaskAction(project);
-        project.getTasks().withType(CppCompile.class, createDependAction);
-        project.getTasks().withType(CCompile.class, createDependAction);
+        project.getComponents().withType(CppBinary.class, new Action<CppBinary>() {
+            @Override
+            public void execute(CppBinary binary) {
+                final Names names = Names.of(binary.getName());
+                final String language = "cpp";
+                project.getTasks().matching(new Spec<Task>() {
+                    @Override
+                    public boolean isSatisfiedBy(Task element) {
+                        return element.getName().equals(names.getCompileTaskName(language));
+                    }
+                }).all(new Action<Task>() {
+                    @Override
+                    public void execute(Task task) {
+                        createDependTask(project, names.withSuffix(language), (AbstractNativeCompileTask) task);
+                    }
+                });
+            }
+        });
     }
 
-    private static class CreateDependTaskAction implements Action<AbstractNativeCompileTask> {
-        private final Project project;
+    static class Rules extends RuleSource {
+        @BinaryTasks
+        void addDependTask(final ModelMap<Task> tasks, final NativeBinarySpecInternal binary) {
+            CreateDependTaskAction createDependTaskAction = new CreateDependTaskAction(binary, tasks);
+            binary.getInputs().withType(CppSourceSet.class, createDependTaskAction);
+            binary.getInputs().withType(CSourceSet.class, createDependTaskAction);
+        }
+    }
 
-        public CreateDependTaskAction(Project project) {
-            this.project = project;
+    private static class CreateDependTaskAction implements Action<LanguageSourceSet> {
+        private final NativeBinarySpecInternal binary;
+        private final ModelMap<Task> tasks;
+
+        public CreateDependTaskAction(NativeBinarySpecInternal binary, ModelMap<Task> tasks) {
+            this.binary = binary;
+            this.tasks = tasks;
         }
 
         @Override
-        public void execute(final AbstractNativeCompileTask compile) {
-            String compileTaskName = compile.getName();
-            String dependTaskName = "depend" + (
-                compileTaskName.startsWith("compile") ? compileTaskName.substring("compile".length()) : capitalize(compileTaskName)
-            );
-            Depend depend = project.getTasks().create(dependTaskName, Depend.class);
-            depend.source(compile.getSource());
-            depend.includes(compile.getIncludes());
-            depend.getHeaderDependenciesFile().set(project.getLayout().getBuildDirectory().file(depend.getName() + "/" + "inputs.txt"));
-            depend.getImportsAreIncludes().set(project.provider(new Callable<Boolean>() {
+        public void execute(LanguageSourceSet cppSourceSet) {
+            final String suffix = capitalize(binary.getProjectScopedName()) + capitalize(((LanguageSourceSetInternal) cppSourceSet).getProjectScopedName());
+            tasks.named("compile" + suffix, new Action<Task>() {
                 @Override
-                public Boolean call() throws Exception {
-                    NativeToolChain toolChain = compile.getToolChain();
-                    return Clang.class.isAssignableFrom(toolChain.getClass()) || Gcc.class.isAssignableFrom(toolChain.getClass());
+                public void execute(Task task) {
+                    final AbstractNativeCompileTask compileTask = (AbstractNativeCompileTask) task;
+                    // When I create the task by using `tasks` CppLanguageIncrementalBuildIntegrationTest."recompiles but does not relink executable with source comment change" fails
+                    final Project project = compileTask.getProject();
+                    project.getTasks().create("depend" + suffix, Depend.class, new Action<Depend>() {
+                        @Override
+                        public void execute(Depend depend) {
+                            configureDependTask(project, compileTask, depend);
+                        }
+                    });
                 }
-            }));
-            compile.getHeaderDependenciesFile().set(depend.getHeaderDependenciesFile());
+            });
         }
+    }
+
+    private static void createDependTask(Project project, String name, final AbstractNativeCompileTask compile) {
+        Depend discoverInputs = project.getTasks().create("depend" + StringUtils.capitalize(name), Depend.class);
+        configureDependTask(project, compile, discoverInputs);
+    }
+
+    private static void configureDependTask(Project project, final AbstractNativeCompileTask compile, Depend depend) {
+        depend.source(compile.getSource());
+        depend.includes(compile.getIncludes());
+        depend.getHeaderDependenciesFile().set(project.getLayout().getBuildDirectory().file(depend.getName() + "/" + "inputs.txt"));
+        depend.getImportsAreIncludes().set(project.provider(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                NativeToolChain toolChain = compile.getToolChain();
+                return Clang.class.isAssignableFrom(toolChain.getClass()) || Gcc.class.isAssignableFrom(toolChain.getClass());
+            }
+        }));
+        compile.getHeaderDependenciesFile().set(depend.getHeaderDependenciesFile());
     }
 }
