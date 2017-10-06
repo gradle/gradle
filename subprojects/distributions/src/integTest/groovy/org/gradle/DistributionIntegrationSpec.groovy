44153103683c94bd30d0ace6f7aa9584b40e5149
Sterling Greene <sterling@gradle.com>
2017-10-06 15:53:01 -0400

    Remove jgit as a dependency for now

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -38,7 +38,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        177
+        176
     }
 
     def "no duplicate entries"() {
