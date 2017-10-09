38e5eae81bbf12a4f53cfd6d55ebc8397df2295e
Pepper Lebeck-Jobe <pepper@gradle.com>
2017-10-10 01:33:40 +0200

    Add the new scenarios to the native perf test
    This change also switches to using header guards instead of `#pragma
    once` because nobody in the real world is using `#pragma once`
    
    Part of gradle/gradle-native#158

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/nativeplatform/NativeBuildPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/nativeplatform/NativeBuildPerformanceTest.groovy
@@ -41,11 +41,14 @@ class NativeBuildPerformanceTest extends AbstractCrossVersionPerformanceTest {
         result.assertCurrentVersionHasNotRegressed()
 
         where:
-        testProject    | maxMemory    | iterations
-        "smallNative"  | '256m'       | 40
-        "mediumNative" | '256m'       | null
-        "bigNative"    | '1g'         | null
-        "multiNative"  | '256m'       | null
+        testProject        | maxMemory    | iterations
+        "smallNative"      | '256m'       | 40
+        "mediumNative"     | '256m'       | null
+        "bigNative"        | '1g'         | null
+        "multiNative"      | '256m'       | null
+        "smallCppLibrary"  | '256m'       | 40
+        "mediumCppLibrary" | '256m'       | null
+        "bigCppLibrary"    | '256m'       | null
     }
 
     def "clean assemble on manyProjectsNative"() {
--- subprojects/performance/src/templates/cpp-source/src.h
+++ subprojects/performance/src/templates/cpp-source/src.h
@@ -1,5 +1,6 @@
-#pragma once
-
+#ifndef PROJECT_HEADER_${functionName}_H
+#define PROJECT_HEADER_${functionName}_H
 <% functionCount.times { %>
 int CPP_${functionName}_${it + 1} ();
 <% } %>
+#endif // PROJECT_HEADER_${functionName}_H
