6fb2ee7ad54224f8128e77187e429f7ddf3b2f75
Cedric Champeau <cedric@gradle.com>
2017-10-11 12:20:21 +0200

    Update `PomReader` to use an empty immutable map for child properties
    Instead of creating an empty map that is never updated.

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/ivyresolve/parser/PomReader.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/ivyresolve/parser/PomReader.java
@@ -16,7 +16,6 @@
 package org.gradle.api.internal.artifacts.ivyservice.ivyresolve.parser;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import org.apache.commons.io.IOUtils;
 import org.apache.ivy.core.IvyPatternHelper;
 import org.gradle.api.Transformer;
@@ -157,7 +156,7 @@ public class PomReader implements PomParent {
     }
 
     public PomReader(final LocallyAvailableExternalResource resource, ImmutableModuleIdentifierFactory moduleIdentifierFactory) throws SAXException {
-        this(resource, moduleIdentifierFactory, Maps.<String, String>newHashMap());
+        this(resource, moduleIdentifierFactory, Collections.<String, String>emptyMap());
     }
 
     public void setPomParent(PomParent pomParent) {
@@ -180,9 +179,11 @@ public class PomReader implements PomParent {
     }
 
     private void setPomProperties(Map<String, String> pomProperties) {
-        this.pomProperties.putAll(pomProperties);
-        for (Map.Entry<String, String> pomProperty : pomProperties.entrySet()) {
-            maybeSetEffectiveProperty(pomProperty.getKey(), pomProperty.getValue());
+        if (!pomProperties.isEmpty()) {
+            this.pomProperties.putAll(pomProperties);
+            for (Map.Entry<String, String> pomProperty : pomProperties.entrySet()) {
+                maybeSetEffectiveProperty(pomProperty.getKey(), pomProperty.getValue());
+            }
         }
     }
 
