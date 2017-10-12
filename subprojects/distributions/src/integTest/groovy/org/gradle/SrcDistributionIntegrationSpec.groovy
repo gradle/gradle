9e7bb272601a7b0349ac6d0b807c3d78b2ddf6ed
Benjamin Muschko <ben@gradle.com>
2017-10-12 11:57:22 -0600

    JMH plugin uses deprecated API
    Needs to be fixed by changing the plugin code, releasing a new version,
    using the version in the Gradle build and removing expectDeprecationWarning again.

--- subprojects/distributions/src/integTest/groovy/org/gradle/SrcDistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/SrcDistributionIntegrationSpec.groovy
@@ -47,6 +47,7 @@ class SrcDistributionIntegrationSpec extends DistributionIntegrationSpec {
             inDirectory(contentsDir)
             usingExecutable('gradlew')
             withTasks('binZip')
+            expectDeprecationWarning()
         }.run()
 
         then:
