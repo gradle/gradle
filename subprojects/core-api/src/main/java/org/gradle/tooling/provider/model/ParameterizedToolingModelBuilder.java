17ea5662fcbbfe7ba110d926473829d264527c40
Stefan Oehme <st.oehme@gmail.com>
2017-10-13 17:21:58 +0200

    Simplify JavaDoc of ParameterizedToolingModelBuilder

--- subprojects/core-api/src/main/java/org/gradle/tooling/provider/model/ParameterizedToolingModelBuilder.java
+++ subprojects/core-api/src/main/java/org/gradle/tooling/provider/model/ParameterizedToolingModelBuilder.java
@@ -20,17 +20,7 @@ import org.gradle.api.Incubating;
 import org.gradle.api.Project;
 
 /**
- * Responsible for building parameterized tooling models to return to the tooling API client.
- *
- * <p>The {@link #buildAll(String, Object, Project)} method is called to create a model for a given project using a given parameter.
- * The model is serialized to the client process and passed to the client application.</p>
- *
- * <p>The model object is adapted to the Java type that is used by the client by generating a view, or wrapper object, over the model object.
- * The model object does not need to implement the client Java type, but it does need to have the same structure as the client type.
- * This means that the model object should have the same properties and methods as those defined on the client type. The tooling API deals with
- * missing properties and methods, to allow evolution of the models. It will also adapt the values returned by the methods of the model object to the
- * types used by the client.
- * </p>
+ * A {@link ToolingModelBuilder} which can be parametrized by the client.
  *
  * <p>The parameter type {@code T} does not need to implement the interface defined in the client side, but it does need to have the same structure.
  * The Tooling API will create a view from the client side parameter type to the one defined in this model builder, and deal automatically with
@@ -42,9 +32,6 @@ import org.gradle.api.Project;
  * The parameter type should be bound to the model type.
  * </p>
  *
- * <p>Although it is not enforced, the model object should be immutable, as the tooling API will do some caching and other performance optimizations on the
- * assumption that the model is effectively immutable. The tooling API does not make any guarantees about how the client application will use the model object.</p>
- *
  * @param <T> The type of parameter used by this model builder.
  * @since 4.4
  * @see ToolingModelBuilder
@@ -53,7 +40,7 @@ import org.gradle.api.Project;
 public interface ParameterizedToolingModelBuilder<T> extends ToolingModelBuilder {
     /**
      * Returns the expected type of the parameter.
-     * It should be an interface only with getters and setters.
+     * It should be an interface with only getters and setters.
      *
      * @return The type of the parameter.
      */
