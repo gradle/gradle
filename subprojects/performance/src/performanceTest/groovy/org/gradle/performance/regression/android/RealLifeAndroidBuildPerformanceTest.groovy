eae4633c08ee14b22324e31760d7e1ad99285fd5
Cedric Champeau <cedric@gradle.com>
2017-10-17 09:44:00 +0200

    Rebaseline performance test

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/android/RealLifeAndroidBuildPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/android/RealLifeAndroidBuildPerformanceTest.groovy
@@ -30,7 +30,7 @@ class RealLifeAndroidBuildPerformanceTest extends AbstractAndroidPerformanceTest
         runner.warmUpRuns = warmUpRuns
         runner.runs = runs
         runner.minimumVersion = "3.4"
-        runner.targetVersions = ["4.2-20170817235727+0000"]
+        runner.targetVersions = ["4.4-20171016130954+0000"]
 
         when:
         def result = runner.run()
