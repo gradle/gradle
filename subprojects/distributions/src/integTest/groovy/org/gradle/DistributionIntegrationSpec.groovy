93c43cb2aa283f1e5987034cdfd00552be3df116
Bo Zhang <bo@gradle.com>
2017-10-19 10:29:50 +0800

    Fix jar count to 177

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -38,7 +38,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        175
+        177
     }
 
     def "no duplicate entries"() {
