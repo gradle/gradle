19de696b85dc4815c2b948d3432bce58590709bb
Bo Zhang <bo@gradle.com>
2017-10-23 09:40:22 +0800

    Increase default HTTP timeout to 30s

--- subprojects/resources-http/src/main/java/org/gradle/internal/resource/transport/http/JavaSystemPropertiesHttpTimeoutSettings.java
+++ subprojects/resources-http/src/main/java/org/gradle/internal/resource/transport/http/JavaSystemPropertiesHttpTimeoutSettings.java
@@ -25,8 +25,8 @@ public class JavaSystemPropertiesHttpTimeoutSettings implements HttpTimeoutSetti
     private static final Logger LOGGER = LoggerFactory.getLogger(JavaSystemPropertiesHttpTimeoutSettings.class);
     public static final String CONNECTION_TIMEOUT_SYSTEM_PROPERTY = "http.connectionTimeout";
     public static final String SOCKET_TIMEOUT_SYSTEM_PROPERTY = "http.socketTimeout";
-    public static final int DEFAULT_CONNECTION_TIMEOUT = 10000;
-    public static final int DEFAULT_SOCKET_TIMEOUT = 10000;
+    public static final int DEFAULT_CONNECTION_TIMEOUT = 30000;
+    public static final int DEFAULT_SOCKET_TIMEOUT = 30000;
     private final int connectionTimeoutMs;
     private final int socketTimeoutMs;
 
