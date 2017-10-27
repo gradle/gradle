9e7dcb8a0dee786eebc27cb7270787aa125464d3
Paul Merlin <paul@gradle.com>
2017-10-27 15:04:11 +0200

    Leverage ScriptApi in source-control sample

--- samples/source-control/sample/settings.gradle.kts
+++ samples/source-control/sample/settings.gradle.kts
@@ -2,7 +2,7 @@ sourceControl {
     vcsMappings {
         val computeGit = vcs(GitVersionControlSpec::class.java) {
             // A real life example would use a remote git repository
-            setUrl(File(rootDir, "../external/build/git-repo").toURI())
+            url = uri("../external/build/git-repo")
         }
         withModule("org.gradle.kotlin.dsl.samples.source-control:compute") {
             from(computeGit)
