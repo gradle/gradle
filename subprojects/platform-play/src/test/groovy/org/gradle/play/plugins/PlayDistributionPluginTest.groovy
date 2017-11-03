05823fd632e062d8328f50c909b61608cf67f1fd
Bo Zhang <bo@gradle.com>
2017-11-03 10:23:35 +0800

    Fix typo

--- subprojects/platform-play/src/test/groovy/org/gradle/play/plugins/PlayDistributionPluginTest.groovy
+++ subprojects/platform-play/src/test/groovy/org/gradle/play/plugins/PlayDistributionPluginTest.groovy
@@ -163,8 +163,8 @@ class PlayDistributionPluginTest extends Specification {
         where:
         playVersion | mainClass
         "2.3.10"    | "play.core.server.NettyServer"
-        "2.4.11"    | "play.core.server.NettyServer"
-        "2.5.18"    | "play.core.server.NettyServer"
+        "2.4.11"    | "play.core.server.ProdServerStart"
+        "2.5.18"    | "play.core.server.ProdServerStart"
         "2.6.6"     | "play.core.server.ProdServerStart"
     }
 
