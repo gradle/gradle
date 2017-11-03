292b405dc175ff535a414d46a3b820f412c8f328
Daniel Lacasse <daniel@gradle.com>
2017-11-03 10:38:36 -0400

    Tries to flush stdout/stderr when printing

--- subprojects/testing-native/src/integTest/groovy/org/gradle/nativeplatform/test/xctest/XCTestTestFrameworkIntegrationTest.groovy
+++ subprojects/testing-native/src/integTest/groovy/org/gradle/nativeplatform/test/xctest/XCTestTestFrameworkIntegrationTest.groovy
@@ -105,7 +105,9 @@ allprojects { p ->
         ]
 
         private static final String FAILING_TEST = """
+            fflush(stdout)
             fputs("some error output\\n", stderr)
+            fflush(stderr)
             XCTAssert(false, "test failure message")
         """
 
