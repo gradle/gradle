2a139ce49626228aee4ae0af34581a79f4f424ab
Rene Groeschke <rene@gradle.com>
2017-11-06 11:24:44 +0100

    Add coverage for test build operations in continuous builds

--- subprojects/testing-base/src/integTest/groovy/org/gradle/testing/TestExecutionBuildOperationsIntegrationTest.groovy
+++ subprojects/testing-base/src/integTest/groovy/org/gradle/testing/TestExecutionBuildOperationsIntegrationTest.groovy
@@ -21,10 +21,10 @@ import org.gradle.api.internal.tasks.testing.operations.TestOutputBuildOperation
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.BuildOperationsFixture
 import org.gradle.integtests.fixtures.TestResources
-import org.gradle.internal.operations.trace.BuildOperationRecord
 import org.junit.Rule
 
-import static org.gradle.util.TextUtil.normaliseLineSeparators
+import static org.gradle.testing.TestExecutionBuildOperationTestUtils.assertJunit
+import static org.gradle.testing.TestExecutionBuildOperationTestUtils.assertTestNg
 
 class TestExecutionBuildOperationsIntegrationTest extends AbstractIntegrationSpec {
 
@@ -35,7 +35,7 @@ class TestExecutionBuildOperationsIntegrationTest extends AbstractIntegrationSpe
 
     def "emitsBuildOperationsForJUnitTests"() {
         when:
-        runAndFail("test")
+        run "test"
 
         then: "test build operations are emitted in expected hierarchy"
         def rootTestOp = operations.first(ExecuteTestBuildOperationType)
@@ -43,12 +43,12 @@ class TestExecutionBuildOperationsIntegrationTest extends AbstractIntegrationSpe
         rootTestOp.details.testDescriptor.className == null
         rootTestOp.details.testDescriptor.composite == true
 
-        assertJunit(rootTestOp)
+        assertJunit(rootTestOp, operations)
     }
 
     def "emitsBuildOperationsForTestNgTests"() {
         when:
-        runAndFail "test"
+        run "test"
 
         then: "test build operations are emitted in expected hierarchy"
         def rootTestOp = operations.first(ExecuteTestBuildOperationType)
@@ -56,7 +56,7 @@ class TestExecutionBuildOperationsIntegrationTest extends AbstractIntegrationSpe
         rootTestOp.details.testDescriptor.className == null
         rootTestOp.details.testDescriptor.composite == true
 
-        assertTestNg(rootTestOp)
+        assertTestNg(rootTestOp, operations)
     }
 
 
@@ -65,23 +65,23 @@ class TestExecutionBuildOperationsIntegrationTest extends AbstractIntegrationSpe
         resources.maybeCopy('TestExecutionBuildOperationsIntegrationTest/emitsBuildOperationsForJUnitTests')
 
         when:
-        runAndFail("test")
+        run "test"
 
         then: "test build operations are emitted in expected hierarchy"
         operations.all(ExecuteTestBuildOperationType).size() == 10
         operations.all(TestOutputBuildOperationType).size() == 10
         def rootTestOp = operations.first(ExecuteTestBuildOperationType)
-        assertJunit(rootTestOp)
+        assertJunit(rootTestOp, operations)
 
         when:
-        runAndFail("test")
+        run "test", "--rerun-tasks"
 
         rootTestOp = operations.first(ExecuteTestBuildOperationType)
 
         then:
         operations.all(ExecuteTestBuildOperationType).size() == 10
         operations.all(TestOutputBuildOperationType).size() == 10
-        assertJunit(rootTestOp)
+        assertJunit(rootTestOp, operations)
     }
 
     def "emits test operations as expected for composite builds"() {
@@ -102,90 +102,14 @@ class TestExecutionBuildOperationsIntegrationTest extends AbstractIntegrationSpe
             }
         """
         when:
-        runAndFail "junit", "testng", "--continue"
+        run "junit", "testng"
 
         then:
         def rootTestOps = operations.all(ExecuteTestBuildOperationType) {
             it.details.testDescriptor.name.startsWith("Gradle Test Run")
         }
         assert rootTestOps.size() == 2
-        assertJunit(rootTestOps.find { it.details.testDescriptor.name.startsWith("Gradle Test Run :junit:test") })
-        assertTestNg(rootTestOps.find { it.details.testDescriptor.name.startsWith("Gradle Test Run :testng:test") })
-    }
-
-    private void assertJunit(BuildOperationRecord rootTestOp) {
-        def executorTestOps = directChildren(rootTestOp, ExecuteTestBuildOperationType)
-        assert executorTestOps.size() == 1
-        assert executorTestOps[0].details.testDescriptor.name.startsWith("Gradle Test Executor ")
-        assert executorTestOps[0].details.testDescriptor.className == null
-        assert executorTestOps[0].details.testDescriptor.composite == true
-
-        def firstLevelTestOps = directChildren(executorTestOps[0], ExecuteTestBuildOperationType)
-        assert firstLevelTestOps.size() == 2
-        assert firstLevelTestOps*.details.testDescriptor.name as Set == ["org.gradle.Test", "org.gradle.TestSuite"] as Set
-        assert firstLevelTestOps*.details.testDescriptor.className as Set == ["org.gradle.Test", "org.gradle.TestSuite"] as Set
-        assert firstLevelTestOps*.details.testDescriptor.composite == [true, true]
-
-        def suiteTestOps = directChildren(firstLevelTestOps[1], ExecuteTestBuildOperationType)
-        assert suiteTestOps.size() == 4
-        assert suiteTestOps*.details.testDescriptor.name == ["ok", "fail", "otherFail", "otherOk"]
-        assert suiteTestOps*.details.testDescriptor.className == ["org.gradle.Test", "org.gradle.Test", "org.gradle.OtherTest", "org.gradle.OtherTest"]
-        assert suiteTestOps*.details.testDescriptor.composite == [false, false, false, false]
-
-        def testTestOps = directChildren(firstLevelTestOps[0], ExecuteTestBuildOperationType)
-        assert testTestOps.size() == 2
-        assert testTestOps*.details.testDescriptor.name == ["ok", "fail"]
-        assert testTestOps*.details.testDescriptor.className == ["org.gradle.Test", "org.gradle.Test"]
-        assert testTestOps*.details.testDescriptor.composite == [false, false]
-
-        // outputs are emitted in test build operation hierarchy
-        def testSuiteOutput = directChildren(firstLevelTestOps[1], TestOutputBuildOperationType)
-        assert testSuiteOutput.size() == 4
-        assert testSuiteOutput*.result.output.message.collect {
-            normaliseLineSeparators(it)
-        } == ["before suite class out\n", "before suite class err\n", "after suite class out\n", "after suite class err\n"]
-        assert testSuiteOutput*.result.output.destination == ["StdOut", "StdErr", "StdOut", "StdErr"]
-
-        def testOutput = directChildren(testTestOps[0], TestOutputBuildOperationType)
-        testOutput.size() == 2
-
-        testOutput*.result.output.message.collect { normaliseLineSeparators(it) } == ["sys out ok\n", "sys err ok\n"]
-        testOutput*.result.output.destination == ["StdOut", "StdErr"]
-    }
-
-    private void assertTestNg(BuildOperationRecord rootTestOp) {
-        def executorTestOps = directChildren(rootTestOp, ExecuteTestBuildOperationType)
-        assert executorTestOps.size() == 1
-        assert executorTestOps[0].details.testDescriptor.name.startsWith("Gradle Test Executor ")
-        assert executorTestOps[0].details.testDescriptor.className == null
-        assert executorTestOps[0].details.testDescriptor.composite == true
-
-        def firstLevelTestOps = directChildren(executorTestOps[0], ExecuteTestBuildOperationType)
-        assert firstLevelTestOps.size() == 1
-        assert firstLevelTestOps.details.testDescriptor.name == ["SimpleSuite"]
-        assert firstLevelTestOps.details.testDescriptor.className == [null]
-        assert firstLevelTestOps.details.testDescriptor.composite == [true]
-
-        def suiteTestOps = directChildren(firstLevelTestOps[0], ExecuteTestBuildOperationType)
-        assert suiteTestOps.size() == 1
-        assert suiteTestOps*.details.testDescriptor.name == ["SimpleTest"]
-        assert suiteTestOps*.details.testDescriptor.className == [null]
-        assert suiteTestOps*.details.testDescriptor.composite == [true]
-
-        def suiteTestTestOps = directChildren(suiteTestOps[0], ExecuteTestBuildOperationType)
-        assert suiteTestTestOps.size() == 3
-        assert suiteTestTestOps*.details.testDescriptor.name == ["fail", "ok", "anotherOk"]
-        assert suiteTestTestOps*.details.testDescriptor.className == ["org.gradle.FooTest", "org.gradle.FooTest", "org.gradle.BarTest"]
-        assert suiteTestTestOps*.details.testDescriptor.composite == [false, false, false]
-
-        // outputs are emitted in test build operation hierarchy
-        def testOutput = directChildren(suiteTestTestOps[1], TestOutputBuildOperationType)
-        assert testOutput.size() == 2
-        assert testOutput*.result.output.message.collect { normaliseLineSeparators(it) } == ["sys out ok\n", "sys err ok\n"]
-        assert testOutput*.result.output.destination == ["StdOut", "StdErr"]
-    }
-
-    def directChildren(BuildOperationRecord parent, Class<ExecuteTestBuildOperationType> operationType) {
-        return operations.search(parent, operationType) { it.parentId == parent.id }
+        assertJunit(rootTestOps.find { it.details.testDescriptor.name.startsWith("Gradle Test Run :junit:test") }, operations)
+        assertTestNg(rootTestOps.find { it.details.testDescriptor.name.startsWith("Gradle Test Run :testng:test") }, operations)
     }
 }
