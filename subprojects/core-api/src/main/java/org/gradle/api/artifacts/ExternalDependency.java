610238d6e346063b194af4db6b936295b9e73e7a
Daz DeBoer <daz@gradle.com>
2017-11-06 18:23:45 -0700

    Use  in dependency API

--- subprojects/core-api/src/main/java/org/gradle/api/artifacts/ExternalDependency.java
+++ subprojects/core-api/src/main/java/org/gradle/api/artifacts/ExternalDependency.java
@@ -47,5 +47,5 @@ public interface ExternalDependency extends ModuleDependency, ModuleVersionSelec
      * @since 4.4
      */
     @Incubating
-    void version(Action<? super VersionConstraint> configureAction);
+    void version(Action<? super MutableVersionConstraint> configureAction);
 }
