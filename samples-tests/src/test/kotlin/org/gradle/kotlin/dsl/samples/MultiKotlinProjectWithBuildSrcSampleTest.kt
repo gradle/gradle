9ca5ffdaac749b22b1d767594ad8d0b97234f1c8
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2017-11-13 15:08:56 -0200

    Temporarily disable tests requiring new `kotlin-dsl` version
    See #551

--- samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/MultiKotlinProjectWithBuildSrcSampleTest.kt
+++ samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/MultiKotlinProjectWithBuildSrcSampleTest.kt
@@ -2,8 +2,9 @@ package org.gradle.kotlin.dsl.samples
 
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.Assert.assertThat
+import org.junit.Ignore
 
-
+@Ignore("Requires published `kotlin-dsl` plugin depending on Kotlin 1.1.60")
 class MultiKotlinProjectWithBuildSrcSampleTest : AbstractSampleTest("multi-kotlin-project-with-buildSrc") {
 
     @org.junit.Test
