12b57b6c076cf98f1c4c22bd071b6331c58938df
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2017-11-13 14:43:14 -0200

    Let Kotlin work out the current JRE classes by itself
    See #551

--- provider/src/main/kotlin/org/gradle/kotlin/dsl/support/KotlinCompiler.kt
+++ provider/src/main/kotlin/org/gradle/kotlin/dsl/support/KotlinCompiler.kt
@@ -187,7 +187,6 @@ private
 fun compilerConfigurationFor(messageCollector: MessageCollector, sourceFiles: Iterable<File>): CompilerConfiguration =
     CompilerConfiguration().apply {
         addKotlinSourceRoots(sourceFiles.map { it.canonicalPath })
-        addJvmClasspathRoots(PathUtil.getJdkClassesRootsFromCurrentJre())
         /*
          * Without this the compiler won't work with JDK 9.
          * See the discussion here:
