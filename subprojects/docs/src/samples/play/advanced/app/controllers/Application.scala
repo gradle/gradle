eebdde8848942f9c12e39d4ac79331a4bbb01d0e
Marcos Pereira <marcos.silva@gmail.com>
2017-11-14 11:05:03 -0200

    Updated advanced code sample

--- subprojects/docs/src/samples/play/advanced/app/controllers/Application.scala
+++ subprojects/docs/src/samples/play/advanced/app/controllers/Application.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
+class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
