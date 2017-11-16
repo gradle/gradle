10de30b43e7b49582c1ce148ecfeb8729c4b857c
Cedric Champeau <cedric@gradle.com>
2017-11-16 10:18:21 +0100

    Adapt Ivy and Maven test fixtures for Gradle metadata publication
    This commit introduces an adapter for generating Gradle module metadata files from either
    Ivy or Maven module test fixtures. This should make it easier to add new features to the
    Gradle metadata file format.

---
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/test/fixtures/gradle/GradleFileModuleAdapter.groovy
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.test.fixtures.gradle
+
+import groovy.json.JsonBuilder
+import org.gradle.test.fixtures.file.TestFile
+
+class GradleFileModuleAdapter {
+    private final String group
+    private final String module
+    private final String version
+    private final List<VariantMetadata> variants
+
+    public GradleFileModuleAdapter(String group, String module, String version, List<VariantMetadata> variants) {
+        this.group = group
+        this.module = module
+        this.version = version
+        this.variants = variants
+    }
+
+    void publishTo(TestFile moduleDir) {
+        moduleDir.createDir()
+        def file = moduleDir.file("$module-${version}.module")
+        def jsonBuilder = new JsonBuilder()
+        jsonBuilder {
+            formatVersion '0.2'
+            builtBy {
+                gradle { }
+            }
+            variants(this.variants.collect { v ->
+                { ->
+                    name v.name
+                    attributes {
+                        v.attributes.each { key, value ->
+                            "$key" value
+                        }
+                    }
+                    files(v.artifacts.collect { a ->
+                        { ->
+                            name a.file.name
+                            url a.file.name
+                        }
+                    })
+                    dependencies(v.dependencies.collect { d ->
+                        { ->
+                            group d.group
+                            module d.module
+                            version {
+                                prefers d.prefers
+                                rejects d.rejects
+                            }
+                        }
+                    })
+                }
+            })
+        }
+        file.withWriter('utf-8') {
+            jsonBuilder.writeTo(it)
+        }
+    }
+
+}
