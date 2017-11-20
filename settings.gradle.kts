b69b61aceebffdd2e92dac9b135fcc398d58cbd7
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2017-11-20 10:10:48 -0200

    Convert `settings.gradle` to Kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+apply {
+    from(File(settingsDir, "gradle/remoteHttpCacheSettings.gradle"))
+}
+
+include("distributions")
+include("baseServices")
+include("baseServicesGroovy")
+include("logging")
+include("processServices")
+include("jvmServices")
+include("core")
+include("dependencyManagement")
+include("wrapper")
+include("cli")
+include("launcher")
+include("messaging")
+include("resources")
+include("resourcesHttp")
+include("resourcesGcs")
+include("resourcesS3")
+include("resourcesSftp")
+include("plugins")
+include("scala")
+include("ide")
+include("ideNative")
+include("idePlay")
+include("osgi")
+include("maven")
+include("announce")
+include("codeQuality")
+include("antlr")
+include("toolingApi")
+include("toolingApiBuilders")
+include("docs")
+include("integTest")
+include("signing")
+include("ear")
+include("native")
+include("internalTesting")
+include("internalIntegTesting")
+include("internalPerformanceTesting")
+include("internalAndroidPerformanceTesting")
+include("performance")
+include("buildScanPerformance")
+include("javascript")
+include("buildComparison")
+include("reporting")
+include("diagnostics")
+include("publish")
+include("ivy")
+include("jacoco")
+include("buildInit")
+include("buildOption")
+include("platformBase")
+include("platformNative")
+include("platformJvm")
+include("languageJvm")
+include("languageJava")
+include("languageGroovy")
+include("languageNative")
+include("languageScala")
+include("pluginUse")
+include("pluginDevelopment")
+include("modelCore")
+include("modelGroovy")
+include("buildCacheHttp")
+include("testingBase")
+include("testingNative")
+include("testingJvm")
+include("platformPlay")
+include("testKit")
+include("installationBeacon")
+include("soak")
+include("smokeTest")
+include("compositeBuilds")
+include("workers")
+include("runtimeApiInfo")
+include("persistentCache")
+include("buildCache")
+include("coreApi")
+include("versionControl")
+
+rootProject.name = "gradle"
+for (project in rootProject.children) {
+    val fileBaseName = project.name.replace("\\p{Upper}".toRegex()) { "-${it.value.toLowerCase()}" }
+    val projectDirName = "subprojects/$fileBaseName"
+    project.projectDir = File(settingsDir, projectDirName)
+    project.buildFileName = "${fileBaseName}.gradle"
+    assert(project.projectDir.isDirectory)
+    assert(project.buildFile.isFile)
+}
