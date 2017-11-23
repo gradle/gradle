472ecf4916bf58a1c637268e0c4c9ed704f803d6
Cedric Champeau <cedric.champeau@gmail.com>
2017-11-23 10:45:33 +0100

    Implement smarter strategy to resolve sources
    This commit changes the way we find sources, by only looking into the appropriate repository. If the
    version is a released version, then we will only look into the `distributions` repository. If it's
    a snapshot, then we will look into the `distributions-snapshot` repository.
    
    However, it's possible that this project depends on a Kotlin DSL custom distribution for which the
    sources haven't been published. So to make it possible to resolve, we use a version range, ending
    with the current version, and starting with the previous minor.

--- provider/src/main/kotlin/org/gradle/kotlin/dsl/resolver/SourceDistributionProvider.kt
+++ provider/src/main/kotlin/org/gradle/kotlin/dsl/resolver/SourceDistributionProvider.kt
@@ -17,17 +17,13 @@
 package org.gradle.kotlin.dsl.resolver
 
 import org.gradle.api.Project
-
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.api.artifacts.repositories.IvyArtifactRepository
 import org.gradle.api.artifacts.repositories.IvyPatternRepositoryLayout
-
 import org.gradle.api.attributes.Attribute
-
 import org.gradle.kotlin.dsl.create
-
 import java.io.File
+import java.lang.Integer.max
 
 interface SourceDistributionProvider {
     fun sourceDirs(): Collection<File>
@@ -40,51 +36,78 @@ class StandardSourceDistributionResolver(val project: Project) : SourceDistribut
         val sourceDirectory = "src-directory"
     }
 
-    override fun sourceDirs(): Collection<File> {
-        val repositories = project.repositories
-        val repos = createSourceRepositories(repositories)
+    override
+    fun sourceDirs(): Collection<File> = createSourceRepository().run {
         try {
             registerTransforms()
             return transientConfigurationForSourcesDownload().files
         } finally {
-            repos.forEach {
-                repositories.remove(it)
-            }
+            project.repositories.remove(this)
         }
     }
 
+
     private
     fun transientConfigurationForSourcesDownload(): Configuration {
-        val sourceDependency = project.dependencies.create("gradle", "gradle", project.gradle.gradleVersion, null, "src", "zip")
+        val sourceDependency = project.dependencies.create(
+            "gradle",
+            "gradle",
+            dependencyVersion(project.gradle.gradleVersion),
+            null,
+            "src",
+            "zip")
         val configuration = project.configurations.detachedConfiguration(sourceDependency)
         configuration.attributes.attribute(artifactType, sourceDirectory)
         return configuration
     }
 
     private
-    fun createSourceRepositories(repositories: RepositoryHandler): List<IvyArtifactRepository> =
-        listOf("distributions", "distributions-snapshots").map { repoName ->
-            val repo = repositories.ivy {
-                it.setName("Gradle ${repoName}")
-                it.setUrl("https://services.gradle.org")
-                it.layout("pattern") {
-                    val layout = it as IvyPatternRepositoryLayout
-                    layout.artifact("/${repoName}/[module]-[revision]-[classifier](.[ext])")
+    fun createSourceRepository(): IvyArtifactRepository = project.repositories.ivy {
+            val repoName = repositoryName(project.gradle.gradleVersion)
+            it.setName("Gradle ${repoName}")
+            it.setUrl("https://services.gradle.org/${repoName}")
+            it.layout("pattern") {
+                val layout = it as IvyPatternRepositoryLayout
+                if (isSnapshot(project.gradle.gradleVersion)) {
+                    layout.ivy("/dummy") // avoids a lookup that interferes with version listing
                 }
+                layout.artifact("[module]-[revision](-[classifier])(.[ext])")
             }
+        }.apply {
             // push the repository first in the list, for performance
-            repositories.remove(repo)
-            repositories.addFirst(repo)
-            repo
+            project.repositories.remove(this)
+            project.repositories.addFirst(this)
         }
 
+
     private
-    fun registerTransforms() {
-        project.dependencies.registerTransform {
+    fun registerTransforms() = project.dependencies.registerTransform {
             it.from.attribute(artifactType, zipType)
             it.to.attribute(artifactType, sourceDirectory)
             it.artifactTransform(ExtractGradleSourcesTransform::class.java)
-        }
     }
 
+    private
+    fun repositoryName(gradleVersion: String) =
+        if (isSnapshot(gradleVersion)) "distributions-snapshots" else "distributions"
+
+    private
+    fun dependencyVersion(gradleVersion: String) =
+        if (isSnapshot(gradleVersion)) toVersionRange(gradleVersion) else gradleVersion
+
+    private
+    fun isSnapshot(gradleVersion: String) = gradleVersion.contains('+')
+
+    private
+    fun toVersionRange(gradleVersion: String) =
+        "(${previousMinor(gradleVersion)}, $gradleVersion]"
+
+    private
+    fun previousMinor(gradleVersion: String): String =
+        gradleVersion.split('.')
+            .take(2)
+            .map { it.takeWhile { it != '-' }. toInt() }
+            .mapIndexed { i, v -> if (i==0) v else max(v-1,0) }
+            .map(Int::toString)
+            .joinToString(".")
 }
