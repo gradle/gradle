5d2a0cf21e0e7a3f08c2f1e769d69f4efb2903ae
Daz DeBoer <daz@gradle.com>
2017-11-24 21:32:17 -0700

    Bump cache layout version

--- subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheLayoutTest.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheLayoutTest.groovy
@@ -46,10 +46,10 @@ class CacheLayoutTest extends Specification {
         CacheLayout cacheLayout = CacheLayout.META_DATA
 
         then:
-        cacheLayout.key == 'metadata-2.39'
-        cacheLayout.version == VersionNumber.parse("2.39.0")
-        cacheLayout.formattedVersion == '2.39'
-        cacheLayout.getPath(new File('some/dir')) == new File('some/dir/metadata-2.39')
+        cacheLayout.key == 'metadata-2.40'
+        cacheLayout.version == VersionNumber.parse("2.40.0")
+        cacheLayout.formattedVersion == '2.40'
+        cacheLayout.getPath(new File('some/dir')) == new File('some/dir/metadata-2.40')
     }
 
     def "use transforms layout"() {
