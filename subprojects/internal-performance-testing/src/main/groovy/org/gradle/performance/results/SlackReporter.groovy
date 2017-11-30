84275e76231325018a0617a5b087ac08df721cdb
Lóránt Pintér <lorant@gradle.com>
2017-11-30 15:03:44 +0100

    Make Slack channel configurable

--- subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/results/SlackReporter.groovy
+++ subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/results/SlackReporter.groovy
@@ -28,15 +28,18 @@ import org.slf4j.LoggerFactory
 
 class SlackReporter implements DataReporter<CrossVersionPerformanceResults> {
     private static final Logger LOGGER = LoggerFactory.getLogger(SlackReporter)
-    private static final String SLACK_WEBHOOK_URL_ENV_VAR = "SLACK_WEBHOOK_URL"
+    private static final String SLACK_WEBHOOK_URL_ENV = "SLACK_WEBHOOK_URL"
+    private static final String SLACK_PERFORMANCE_REPORT_CHANNEL_ENV = "SLACK_PERFORMANCE_REPORT_CHANNEL"
     private static final String[] EXCLAMATIONS = ["Hurrah!", "Whoopee!", "Excellent!", "Yippee!", "Splendid!", "OMG!", "Amazing!", "Fantastic!", "Awesome!", "Brilliant!"]
     private final URI webhook
+    private final String slackPerformanceReportChannel
     private final CloseableHttpClient httpClient
     private final DataReporter<CrossVersionPerformanceResults> delegate
 
-    SlackReporter(URI webhook, DataReporter<CrossVersionPerformanceResults> delegate) {
-        this.delegate = delegate
+    SlackReporter(URI webhook, String slackPerformanceReportChannel, DataReporter<CrossVersionPerformanceResults> delegate) {
         this.webhook = webhook
+        this.slackPerformanceReportChannel = slackPerformanceReportChannel
+        this.delegate = delegate
         this.httpClient = HttpClients.createDefault()
     }
 
@@ -62,7 +65,7 @@ class SlackReporter implements DataReporter<CrossVersionPerformanceResults> {
             text: message,
             username: "Performance tests",
             icon_emoji: ":dash:",
-            channel: "#test-lptr"
+            channel: slackPerformanceReportChannel
         )
 
         def post = new HttpPost(webhook)
@@ -81,9 +84,10 @@ class SlackReporter implements DataReporter<CrossVersionPerformanceResults> {
     }
 
     static DataReporter<CrossVersionPerformanceResults> wrap(DataReporter<CrossVersionPerformanceResults> reporter) {
-        def slackWebhookUrl = System.getenv(SLACK_WEBHOOK_URL_ENV_VAR)
+        def slackWebhookUrl = System.getenv(SLACK_WEBHOOK_URL_ENV)
+        def slackPerformanceReportChannel = System.getenv(SLACK_PERFORMANCE_REPORT_CHANNEL_ENV)
         if (slackWebhookUrl) {
-            new SlackReporter(URI.create(slackWebhookUrl), reporter)
+            new SlackReporter(URI.create(slackWebhookUrl), slackPerformanceReportChannel, reporter)
         } else {
             return reporter
         }
