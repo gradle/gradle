ca3fb77af19ced95bf89da7d22047f2b0e41262c
Andrey Mischenko <andrey.mischenko@bandlab.com>
2017-12-05 19:09:38 +0800

    Add sample demonstrating Kapt plugin usage with Kotlin DSL
    Signed-off-by: Andrey Mischenko <andrey.mischenko@bandlab.com>

---
+++ samples/hello-kapt/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.plugin.KaptAnnotationProcessorOptions
+import org.jetbrains.kotlin.gradle.plugin.KaptJavacOptionsDelegate
+
+plugins {
+    application
+    kotlin("jvm") version "1.2.10"
+    kotlin("kapt") version "1.2.10"
+}
+
+application {
+    mainClassName = "samples.HelloAutoValueKt"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    testCompile("junit:junit:4.12")
+
+    // Use AutoValue to check that annotation processing works
+    // https://github.com/google/auto/tree/master/value
+    compileOnly("com.google.auto.value:auto-value:1.5")
+    // Kapt configuration for an annotation processor
+    kapt("com.google.auto.value:auto-value:1.5")
+}
+
+kapt {
+    // Type safe accessors for Kapt DSL
+    correctErrorTypes = true
+
+    // Example of Javac Options configuration
+    javacOptions(delegateClosureOf<KaptJavacOptionsDelegate> {
+        option("SomeJavacOption", "OptionValue")
+    })
+
+    // Kapt Arguments configuration
+    arguments(delegateClosureOf<KaptAnnotationProcessorOptions> {
+        arg("SomeKaptArgument", "ArgumentValue")
+    })
+}
