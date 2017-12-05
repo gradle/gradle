74dbac376cbec45d2e1114afb7bcb0b6c14ab790
Cedric Champeau <cedric@gradle.com>
2017-12-05 13:24:50 +0100

    Add internal documentation about how to process when changing the Gradle metadata format

--- subprojects/publish/src/main/java/org/gradle/api/publish/internal/ModuleMetadataFileGenerator.java
+++ subprojects/publish/src/main/java/org/gradle/api/publish/internal/ModuleMetadataFileGenerator.java
@@ -54,6 +54,20 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+/**
+ * <p>The Gradle module metadata file generator is responsible for generating a JSON file
+ * describing module metadata. In particular, this file format is capable of handling different
+ * variants with different dependency sets.</p>
+ *
+ * <p>Whenever you change this class, make sure you also:</p>
+ *
+ * <ul>
+ *     <li>Update the corresponding {@link ModuleMetadataParser module metadata parser}</li>
+ *     <li>Update the module metadata specification (subprojects/docs/src/docs/design/gradle-module-metadata-specification.md)</li>
+ *     <li>Update {@link org.gradle.api.internal.artifacts.ivyservice.modulecache.ModuleMetadataSerializer the module metadata serializer} </li>
+ *     <li>Add a sample for the module metadata serializer test, to make sure that serialized metadata is idempotent</li>
+ * </ul>
+ */
 public class ModuleMetadataFileGenerator {
     private final BuildInvocationScopeId buildInvocationScopeId;
     private final ProjectDependencyPublicationResolver projectDependencyResolver;
