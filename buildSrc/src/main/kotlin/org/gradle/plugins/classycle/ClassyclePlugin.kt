4437dcc1332da5151c8e522faec797b3cdabb3d4
Paul Merlin <paul@gradle.com>
2017-12-14 13:45:55 +0100

    Convert the Classycle plugin to Kotlin
    The plugin implementation is quite straightforward but a few things
    require an explanation.
    
    In `buildSrc`, the Kotlin DSL can't know what plugins are to be applied.
    There's no plugins {} block. This prevents having generated static
    Kotlin extensions for Gradle extensions, conventions or configurations.
    To make the Kotlin code closer to what you would have in a .gradle.kts
    file, the Kotlin extensions on which the Classycle plugin depends, are
    declared explicitly, see `accessors.kt`.
    
    The Classycle task makes use of the internal IsolatedAntBuilder that
    relies on Groovy method missing, this is handled thanks to the
    withGroovyBuilder {} helper provided by the Kotlin DSL.
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

---
+++ buildSrc/src/main/kotlin/org/gradle/plugins/classycle/ClassyclePlugin.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.plugins.classycle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import org.gradle.kotlin.dsl.*
+
+import accessors.java
+import accessors.reporting
+
+
+internal
+val classycleBaseName = "classycle"
+
+
+@Suppress("unused")
+open class ClassyclePlugin : Plugin<Project> {
+
+    override fun apply(project: Project): Unit = project.run {
+        configurations.create(classycleBaseName)
+        dependencies.add(classycleBaseName, "classycle:classycle:1.4@jar")
+        tasks {
+            val classycle by creating
+            java.sourceSets.all {
+                val taskName = getTaskName(classycle.name, null)
+                val sourceSetTask = createTask(taskName, Classycle::class) {
+                    classesDirs = output.classesDirs
+                    reportName = name
+                    reportDir = reporting.file(classycle.name)
+                    dependsOn(output)
+                }
+                classycle.dependsOn(sourceSetTask)
+                "check" { dependsOn(sourceSetTask) }
+                "codeQuality" { dependsOn(sourceSetTask) }
+            }
+        }
+    }
+}
