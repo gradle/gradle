fa4c4a8edd0803e6d6f93c3a8e98a2ab2c06db3d
Erhard Pointl <epeee@users.noreply.github.com>
2018-01-02 20:02:28 +0100

    Update CodeNarc version to 1.0
    Signed-off-by: Erhard Pointl <epeee@users.noreply.github.com>

--- subprojects/code-quality/src/main/groovy/org/gradle/api/plugins/quality/CodeNarcPlugin.java
+++ subprojects/code-quality/src/main/groovy/org/gradle/api/plugins/quality/CodeNarcPlugin.java
@@ -37,7 +37,7 @@ import java.util.concurrent.Callable;
  */
 public class CodeNarcPlugin extends AbstractCodeQualityPlugin<CodeNarc> {
 
-    public static final String DEFAULT_CODENARC_VERSION = "0.25.2";
+    public static final String DEFAULT_CODENARC_VERSION = "1.0";
     private CodeNarcExtension extension;
 
     @Override
