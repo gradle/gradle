6d30b6e53a1cef7eda6fad8361adf023bcc558d7
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-01-09 11:37:33 -0700

    Introduce a use case-driven section on "managing transitive dependencies" in the user guide (#3973)

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesDeclaringDependenciesIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesDeclaringDependenciesIntegrationTest.groovy
@@ -19,12 +19,15 @@ package org.gradle.integtests.samples
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.Sample
 import org.gradle.integtests.fixtures.UsesSample
+import org.gradle.test.fixtures.file.TestFile
 import org.gradle.util.Requires
 import org.junit.Rule
 
 import static org.gradle.util.TestPrecondition.JDK8_OR_LATER
 
 class SamplesDeclaringDependenciesIntegrationTest extends AbstractIntegrationSpec {
+
+    private static final String COPY_LIBS_TASK_NAME = 'copyLibs'
 
     @Rule
     Sample sample = new Sample(testDirectoryProvider)
@@ -34,7 +37,7 @@ class SamplesDeclaringDependenciesIntegrationTest extends AbstractIntegrationSpe
         executer.inDirectory(sample.dir)
 
         when:
-        succeeds('copyLibs')
+        succeeds(COPY_LIBS_TASK_NAME)
 
         then:
         sample.dir.file('build/libs/spring-web-5.0.2.RELEASE.jar').isFile()
@@ -56,7 +59,7 @@ class SamplesDeclaringDependenciesIntegrationTest extends AbstractIntegrationSpe
         executer.inDirectory(sample.dir)
 
         when:
-        succeeds('copyLibs')
+        succeeds(COPY_LIBS_TASK_NAME)
 
         then:
         sample.dir.file('build/libs/spring-web-5.0.3.BUILD-SNAPSHOT.jar').isFile()
@@ -67,7 +70,7 @@ class SamplesDeclaringDependenciesIntegrationTest extends AbstractIntegrationSpe
         executer.inDirectory(sample.dir)
 
         when:
-        succeeds('copyLibs')
+        succeeds(COPY_LIBS_TASK_NAME)
 
         then:
         sample.dir.file('build/libs/antcontrib.jar').isFile()
@@ -97,5 +100,37 @@ class SamplesDeclaringDependenciesIntegrationTest extends AbstractIntegrationSpe
 
         then:
         sample.dir.file('build/compiled-jsps/org/apache/jsp/hello_jsp.java').isFile()
+    }
+
+    @UsesSample("userguide/dependencies/resolvingArtifactOnly")
+    def "can resolve dependency with artifact-only declaration"() {
+        executer.inDirectory(sample.dir)
+
+        when:
+        succeeds(COPY_LIBS_TASK_NAME)
+
+        then:
+        assertSingleLib('jquery-3.2.1.js')
+    }
+
+    @UsesSample("userguide/dependencies/resolvingArtifactOnlyWithClassifier")
+    def "can resolve dependency with artifact-only declaration with classifier"() {
+        executer.inDirectory(sample.dir)
+
+        when:
+        succeeds(COPY_LIBS_TASK_NAME)
+
+        then:
+        assertSingleLib('jquery-3.2.1-min.js')
+    }
+
+    private TestFile[] listFilesInBuildLibsDir() {
+        sample.dir.file('build/libs').listFiles()
+    }
+
+    private void assertSingleLib(String filename) {
+        def libs = listFilesInBuildLibsDir()
+        assert libs.size() == 1
+        assert libs[0].name == filename
     }
 }
