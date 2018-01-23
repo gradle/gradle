cbf8c8eaf2f4533540e008a51d7e796293e34cbc
Benjamin Muschko <ben@gradle.com>
2018-01-23 14:29:21 -0700

    Normalize file separators to make tests pass on Windows

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesWorkingWithDependenciesIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/SamplesWorkingWithDependenciesIntegrationTest.groovy
@@ -21,6 +21,8 @@ import org.gradle.integtests.fixtures.Sample
 import org.gradle.integtests.fixtures.UsesSample
 import org.junit.Rule
 
+import static org.gradle.util.TextUtil.normaliseFileSeparators
+
 class SamplesWorkingWithDependenciesIntegrationTest extends AbstractIntegrationSpec {
 
     @Rule
@@ -46,14 +48,15 @@ commons-codec:commons-codec:1.7""")
         succeeds('iterateScmArtifacts')
 
         then:
-        outputContains('org.eclipse.jgit/org.eclipse.jgit/4.9.2.201712150930-r/a3a2d1df793245ebfc7322db3c2b9828ee184850/org.eclipse.jgit-4.9.2.201712150930-r.jar')
-        outputContains('org.apache.httpcomponents/httpclient/4.3.6/4c47155e3e6c9a41a28db36680b828ced53b8af4/httpclient-4.3.6.jar')
-        outputContains('commons-codec/commons-codec/1.7/9cd61d269c88f9fb0eb36cea1efcd596ab74772f/commons-codec-1.7.jar')
-        outputContains('com.jcraft/jsch/0.1.54/da3584329a263616e277e15462b387addd1b208d/jsch-0.1.54.jar')
-        outputContains('com.googlecode.javaewah/JavaEWAH/1.1.6/94ad16d728b374d65bd897625f3fbb3da223a2b6/JavaEWAH-1.1.6.jar')
-        outputContains('org.slf4j/slf4j-api/1.7.2/81d61b7f33ebeab314e07de0cc596f8e858d97/slf4j-api-1.7.2.jar')
-        outputContains('org.apache.httpcomponents/httpcore/4.3.3/f91b7a4aadc5cf486df6e4634748d7dd7a73f06d/httpcore-4.3.3.jar')
-        outputContains('commons-logging/commons-logging/1.1.3/f6f66e966c70a83ffbdb6f17a0919eaf7c8aca7f/commons-logging-1.1.3.jar')
+        def normalizedContent = normaliseFileSeparators(output)
+        normalizedContent.contains('org.eclipse.jgit/org.eclipse.jgit/4.9.2.201712150930-r/a3a2d1df793245ebfc7322db3c2b9828ee184850/org.eclipse.jgit-4.9.2.201712150930-r.jar')
+        normalizedContent.contains('org.apache.httpcomponents/httpclient/4.3.6/4c47155e3e6c9a41a28db36680b828ced53b8af4/httpclient-4.3.6.jar')
+        normalizedContent.contains('commons-codec/commons-codec/1.7/9cd61d269c88f9fb0eb36cea1efcd596ab74772f/commons-codec-1.7.jar')
+        normalizedContent.contains('com.jcraft/jsch/0.1.54/da3584329a263616e277e15462b387addd1b208d/jsch-0.1.54.jar')
+        normalizedContent.contains('com.googlecode.javaewah/JavaEWAH/1.1.6/94ad16d728b374d65bd897625f3fbb3da223a2b6/JavaEWAH-1.1.6.jar')
+        normalizedContent.contains('org.slf4j/slf4j-api/1.7.2/81d61b7f33ebeab314e07de0cc596f8e858d97/slf4j-api-1.7.2.jar')
+        normalizedContent.contains('org.apache.httpcomponents/httpcore/4.3.3/f91b7a4aadc5cf486df6e4634748d7dd7a73f06d/httpcore-4.3.3.jar')
+        normalizedContent.contains('commons-logging/commons-logging/1.1.3/f6f66e966c70a83ffbdb6f17a0919eaf7c8aca7f/commons-logging-1.1.3.jar')
     }
 
     @UsesSample("userguide/dependencies/accessingMetadataArtifact")
@@ -64,7 +67,8 @@ commons-codec:commons-codec:1.7""")
         succeeds('printGuavaMetadata')
 
         then:
-        outputContains("""com.google.guava/guava/18.0/2ec12f8d27a64e970b8be0fbd1d52dfec51cd41c/guava-18.0.pom
+        def normalizedContent = normaliseFileSeparators(output)
+        normalizedContent.contains("""com.google.guava/guava/18.0/2ec12f8d27a64e970b8be0fbd1d52dfec51cd41c/guava-18.0.pom
 Guava: Google Core Libraries for Java
 
     Guava is a suite of core and expanded libraries that include
