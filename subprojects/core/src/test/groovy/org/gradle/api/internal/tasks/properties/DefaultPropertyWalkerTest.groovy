8d3cbb9f6356bd8fba8ddda2fb83c91d34494262
Stefan Wolf <wolf@gradle.com>
2018-01-26 14:11:05 +0100

    More polishing

--- subprojects/core/src/test/groovy/org/gradle/api/internal/tasks/properties/DefaultPropertyWalkerTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/tasks/properties/DefaultPropertyWalkerTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,7 +112,7 @@ class DefaultPropertyWalkerTest extends AbstractProjectBuilderSpec {
     }
 
     @Unroll
-    def "correct implementation #type coerced to Action is tracked"() {
+    def "correct implementation for #type coerced to Action is tracked"() {
         expect:
         DefaultPropertyWalker.getImplementationClass(implementation as Action) == implementation.getClass()
 
