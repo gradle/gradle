66aef2d45954c012ee2f359a87e4c7c33124205c
Benjamin Muschko <ben@gradle.com>
2018-01-29 10:32:20 -0700

    Fix type

--- .teamcity/Gradle_Check/model/CIBuildModel.kt
+++ .teamcity/Gradle_Check/model/CIBuildModel.kt
@@ -56,7 +56,7 @@ data class CIBuildModel (
                             PerformanceTestType.historical)),
             Stage("Experimental", "On demand: Run experimental tests",
                     trigger = Trigger.never,
-                    performanceTests = listOf(
+                    functionalTests = listOf(
                             TestCoverage(TestType.platform, OS.linux, JvmVersion.java10))))
     ) {
 
