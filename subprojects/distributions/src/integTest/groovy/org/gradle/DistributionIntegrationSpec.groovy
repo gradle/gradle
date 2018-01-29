955de630fb0a21fe0615212ff0fa869b6715e57c
Dennis Hoer <dennis.hoer@pearson.com>
2018-01-29 08:32:52 -0700

    Bump dist lib count to 186
    Signed-off-by: Dennis Hoer <dennis.hoer@pearson.com>

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -38,7 +38,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        184
+        186
     }
 
     def "no duplicate entries"() {
