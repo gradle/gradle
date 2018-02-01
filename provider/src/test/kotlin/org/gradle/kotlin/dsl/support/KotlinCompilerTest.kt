7cf8e37efe7042146f3e86f7fd96011c63ba7f2b
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-02-01 19:19:30 -0200

    Prove Kotlin compiler doesn't prevent output jar from being deleted

--- provider/src/test/kotlin/org/gradle/kotlin/dsl/support/KotlinCompilerTest.kt
+++ provider/src/test/kotlin/org/gradle/kotlin/dsl/support/KotlinCompilerTest.kt
@@ -29,15 +29,19 @@ class KotlinCompilerTest : TestWithTempFiles() {
         compileToJar(outputJar, listOf(sourceFile), loggerFor<KotlinCompilerTest>())
 
         val answer =
-            classLoaderFor(outputJar)
+            classLoaderFor(outputJar).use { it
                 .loadClass("hhgttg.DeepThought")
                 .newInstance()
                 .run {
                     this::class.java.getMethod("compute").invoke(this)
                 }
+            }
+
         assertThat(
             answer,
             equalTo<Any>(42))
+
+        assert(outputJar.delete())
     }
 
     private
