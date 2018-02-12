c3ef34724109f92374334519ff98072e4b119ae9
Felipe Fernández <felipe@gradle.com>
2018-02-12 12:35:45 +0000

    Update to build scan plugin 1.12

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/BuildScanPluginSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/BuildScanPluginSmokeTest.groovy
@@ -36,7 +36,8 @@ class BuildScanPluginSmokeTest extends AbstractSmokeTest {
         "1.10.1",
         "1.10.2",
         "1.10.3",
-        "1.11"
+        "1.11",
+        "1.12"
     ]
 
     @Unroll
