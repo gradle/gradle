fc4811bc24b021840cb4b8f446d8bd17010a6f34
Adam Murdoch <adam@gradle.com>
2018-02-15 09:18:20 -0800

    Fixed tooling api int tests for changes to resolve build operations.

--- subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r40/ResolveArtifactsProgressCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r40/ResolveArtifactsProgressCrossVersionSpec.groovy
@@ -20,16 +20,27 @@ import org.gradle.integtests.tooling.fixture.ProgressEvents
 import org.gradle.integtests.tooling.fixture.TargetGradleVersion
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
 import org.gradle.integtests.tooling.fixture.ToolingApiVersion
+import org.gradle.test.fixtures.maven.MavenFileRepository
+import org.gradle.test.fixtures.server.http.MavenHttpRepository
+import org.gradle.test.fixtures.server.http.RepositoryHttpServer
 import org.gradle.tooling.ProjectConnection
+import org.junit.Rule
 
 @ToolingApiVersion(">=2.5")
 @TargetGradleVersion(">=4.0")
 class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
+    @Rule
+    public final RepositoryHttpServer server = new RepositoryHttpServer(temporaryFolder, targetDist.version.version)
+
+    def setup() {
+        toolingApi.requireIsolatedUserHome()
+    }
 
     def "generates event for resolving intrinsic artifacts by iterating the configuration"() {
         given:
         settingsFile << settingsFileContent()
         buildFile << buildFileContent('')
+        expectDownload()
 
         when:
         def events = ProgressEvents.create()
@@ -44,13 +55,17 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         def resolveArtifacts = events.operation('Resolve files of :configurationWithDependency')
         resolveArtifacts.parent.descriptor.displayName.matches("Execute .* for :resolve")
         resolveArtifacts.children.size() == 1
-        resolveArtifacts.child("Resolve a.jar (project :provider)")
+        resolveArtifacts.child("Resolve provider.jar (test:provider:1.0)")
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def "generates event for resolving intrinsic artifacts via file collection"() {
         given:
         settingsFile << settingsFileContent()
         buildFile << buildFileContent('.files')
+        expectDownload()
 
         when:
         def events = ProgressEvents.create()
@@ -65,13 +80,17 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         def resolveArtifacts = events.operation('Resolve files of :configurationWithDependency')
         resolveArtifacts.parent.descriptor.displayName.matches("Execute .* for :resolve")
         resolveArtifacts.children.size() == 1
-        resolveArtifacts.child("Resolve a.jar (project :provider)")
+        resolveArtifacts.child("Resolve provider.jar (test:provider:1.0)")
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def "generates event for resolving intrinsic artifacts via incoming file collection"() {
         given:
         settingsFile << settingsFileContent()
         buildFile << buildFileContent('.incoming.files')
+        expectDownload()
 
         when:
         def events = ProgressEvents.create()
@@ -86,13 +105,17 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         def resolveArtifacts = events.operation('Resolve files of :configurationWithDependency')
         resolveArtifacts.parent.descriptor.displayName.matches("Execute .* for :resolve")
         resolveArtifacts.children.size() == 1
-        resolveArtifacts.child("Resolve a.jar (project :provider)")
+        resolveArtifacts.child("Resolve provider.jar (test:provider:1.0)")
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def "generates event for resolving intrinsic artifacts via incoming artifact collection"() {
         given:
         settingsFile << settingsFileContent()
         buildFile << buildFileContent('.incoming.artifacts')
+        expectDownload()
 
         when:
         def events = ProgressEvents.create()
@@ -107,14 +130,17 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         def resolveArtifacts = events.operation('Resolve files of :configurationWithDependency')
         resolveArtifacts.parent.descriptor.displayName.matches("Execute .* for :resolve")
         resolveArtifacts.children.size() == 1
-        resolveArtifacts.child("Resolve a.jar (project :provider)")
+        resolveArtifacts.child("Resolve provider.jar (test:provider:1.0)")
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def "generates event for resolving artifact view via artifact collection"() {
         given:
         settingsFile << settingsFileContent()
-        buildFile << buildFileContent('.incoming.artifactView { it.attributes { it.attribute(kind, "jar") } }.artifacts',
-            'variants { var1 { attributes.attribute(kind, "jar"); artifact bJar } }')
+        expectDownloadOtherTypes()
+        buildFile << buildFileContent('.incoming.artifactView { it.attributes { it.attribute(kind, "thing") } }.artifacts')
 
         when:
         def events = ProgressEvents.create()
@@ -129,14 +155,17 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         def resolveArtifacts = events.operation('Resolve files of :configurationWithDependency')
         resolveArtifacts.parent.descriptor.displayName.matches("Execute .* for :resolve")
         resolveArtifacts.children.size() == 1
-        resolveArtifacts.child("Resolve b.jar (project :provider)")
+        resolveArtifacts.child("Resolve other.thing (test:other:1.0)")
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def "generates event for resolving artifact view via file collection"() {
         given:
         settingsFile << settingsFileContent()
-        buildFile << buildFileContent('.incoming.artifactView { it.attributes { it.attribute(kind, "jar") } }.files',
-            'variants { var1 { attributes.attribute(kind, "jar"); artifact bJar } }')
+        expectDownloadOtherTypes()
+        buildFile << buildFileContent('.incoming.artifactView { it.attributes { it.attribute(kind, "thing") } }.files')
 
         when:
         def events = ProgressEvents.create()
@@ -151,13 +180,17 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         def resolveArtifacts = events.operation('Resolve files of :configurationWithDependency')
         resolveArtifacts.parent.descriptor.displayName.matches("Execute .* for :resolve")
         resolveArtifacts.children.size() == 1
-        resolveArtifacts.child("Resolve b.jar (project :provider)")
+        resolveArtifacts.child("Resolve other.thing (test:other:1.0)")
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def "generates event for resolving artifacts even if dependencies have no artifacts"() {
         given:
         settingsFile << settingsFileContent()
-        buildFile << buildFileContent('', '')
+        buildFile << buildFileContent('')
+        expectDownloadNoArtifacts()
 
         when:
         def events = ProgressEvents.create()
@@ -172,13 +205,16 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         def resolveArtifacts = events.operation('Resolve files of :configurationWithDependency')
         resolveArtifacts.parent.descriptor.displayName.matches("Execute .* for :resolve")
         resolveArtifacts.children.size() == 0
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def "generates event for resolving artifact view even if the view is empty"() {
         given:
         settingsFile << settingsFileContent()
-        buildFile << buildFileContent('.incoming.artifactView { it.attributes { it.attribute(kind, "jar") } }.files',
-            'variants { var1 { attributes.attribute(kind, "jar"); artifact bJar } }')
+        buildFile << buildFileContent('.incoming.artifactView { it.attributes { it.attribute(kind, "none") } }.files')
+        expectDownloadOtherTypes()
 
         when:
         def events = ProgressEvents.create()
@@ -192,14 +228,16 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         and:
         def resolveArtifacts = events.operation('Resolve files of :configurationWithDependency')
         resolveArtifacts.parent.descriptor.displayName.matches("Execute .* for :resolve")
-        resolveArtifacts.children.size() == 1
-        resolveArtifacts.child("Resolve b.jar (project :provider)")
+        resolveArtifacts.children.empty
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def "does not generate event if configuration has no dependencies"() {
         given:
         settingsFile << settingsFileContent()
-        buildFile << buildFileContent('', '', 'configurationWithoutDependency')
+        buildFile << buildFileContent('', 'configurationWithoutDependency')
 
         when:
         def events = ProgressEvents.create()
@@ -214,6 +252,9 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         def resolveDependencies = events.operation('Resolve dependencies of :configurationWithoutDependency')
         resolveDependencies.parent.descriptor.displayName.matches("Execute .* for :resolve")
         resolveDependencies.parent.children.size() == 1
+
+        cleanup:
+        toolingApi.daemons.killAll()
     }
 
     def settingsFileContent() {
@@ -223,27 +264,56 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
         """
     }
 
-    def buildFileContent(String artifactsAccessor, String artifactDeclaration = 'artifact aJar', String configuration = 'configurationWithDependency') {
+    def expectDownload() {
+        mavenHttpRepo.module("test", "provider", "1.0").publish().allowAll()
+    }
+
+    def expectDownloadNoArtifacts() {
+        mavenHttpRepo.module("test", "provider", "1.0")
+            .hasPackaging("pom")
+            .hasType("pom")
+            .dependsOn("test", "other", "1.0")
+            .publish()
+            .allowAll()
+        mavenHttpRepo.module("test", "other", "1.0")
+            .hasPackaging("pom")
+            .hasType("pom")
+            .publish()
+            .allowAll()
+    }
+
+    def expectDownloadOtherTypes() {
+        mavenHttpRepo.module("test", "provider", "1.0")
+            .dependsOn("test", "other", "1.0")
+            .publish()
+            .allowAll()
+        def m = mavenHttpRepo.module("test", "other", "1.0")
+            .hasPackaging("thing")
+            .hasType("thing")
+        m.artifact(type: "thing")
+        m.publish()
+        m.allowAll()
+    }
+
+    def buildFileContent(String artifactsAccessor, String configuration = 'configurationWithDependency') {
         """
             def kind = Attribute.of('kind', String)
 
-            project(':provider') {
-                task aJar(type: Jar) { baseName = 'a' }
-                task bJar(type: Jar) { baseName = 'b' }
-                configurations {
-                    'default' {
-                        outgoing { $artifactDeclaration }
-                    }
-                }
+            repositories {
+                maven { url = '${mavenHttpRepo.uri}' }
             }
-            
+
             configurations {
                 configurationWithDependency
                 configurationWithoutDependency
             }
             
             dependencies {
-                configurationWithDependency project(":provider")
+                configurationWithDependency "test:provider:1.0"
+                artifactTypes {
+                    thing { attributes.attribute(kind, "thing") }
+                    jar { attributes.attribute(kind, "jar") }
+                }
             }
             
             task resolve {
@@ -252,5 +322,13 @@ class ResolveArtifactsProgressCrossVersionSpec extends ToolingApiSpecification {
                 }
             }
         """
+    }
+
+    MavenHttpRepository getMavenHttpRepo() {
+        return new MavenHttpRepository(server, "/repo", mavenRepo)
+    }
+
+    MavenFileRepository getMavenRepo(String name = "repo") {
+        return new MavenFileRepository(file(name))
     }
 }
