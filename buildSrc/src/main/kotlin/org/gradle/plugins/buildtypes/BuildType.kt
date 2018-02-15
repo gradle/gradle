7b764137328478fafa40a2f0050680896abc2995
Paul Merlin <paul@gradle.com>
2018-02-15 17:19:41 +0100

    Refine build types plugin
    by splitting the register function into smaller named pieces.

--- buildSrc/src/main/kotlin/org/gradle/plugins/buildtypes/BuildType.kt
+++ buildSrc/src/main/kotlin/org/gradle/plugins/buildtypes/BuildType.kt
@@ -1,16 +1,20 @@
 package org.gradle.plugins.buildtypes
 
+
 class BuildType(val name: String) {
+
+    val abbreviation = name[0] + name.substring(1).replace(lowercaseAlphaRegex, "")
+
     var tasks: List<String> = emptyList()
-    var projectProperties: Map<String,Any> = emptyMap()
+    var projectProperties: Map<String, Any> = emptyMap()
     var active = true
-    var propertiesAction = { properties: Map<String,Any> ->  }
+    var propertiesAction: (Map<String, Any>) -> Unit = {}
 
     fun tasks(vararg tasks: String) {
         this.tasks = tasks.asList()
     }
 
-    fun projectProperties(projectProperties: Map<String,Any>) {
+    fun projectProperties(projectProperties: Map<String, Any>) {
         this.projectProperties = projectProperties
         if (active) {
             propertiesAction(projectProperties)
@@ -19,3 +23,5 @@ class BuildType(val name: String) {
 }
 
 
+private
+val lowercaseAlphaRegex = Regex("[a-z]")
--- buildSrc/src/main/kotlin/org/gradle/plugins/buildtypes/BuildTypesPlugin.kt
+++ buildSrc/src/main/kotlin/org/gradle/plugins/buildtypes/BuildTypesPlugin.kt
@@ -3,60 +3,86 @@ package org.gradle.plugins.buildtypes
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.extra
+
+import org.gradle.kotlin.dsl.*
+
 
 class BuildTypesPlugin : Plugin<Project> {
 
     override fun apply(project: Project): Unit = project.run {
-        val buildTypesContainer = project.container(BuildType::class.java)
-        project.extensions.add("buildTypes", buildTypesContainer)
+        val buildTypesContainer = container(BuildType::class.java)
+        extensions.add("buildTypes", buildTypesContainer)
         buildTypesContainer.all {
-            register(this, project)
+            register(this)
         }
     }
 
-    fun register(buildType: BuildType, project: Project) {
-        project.tasks.create(buildType.name) {
+    private
+    fun Project.register(buildType: BuildType) =
+        tasks.create(buildType.name) {
             group = "Build Type"
-            val abbreviation = name[0] + name.substring(1).replace(Regex("[a-z]"), "")
-            val taskNames = project.gradle.startParameter.taskNames
-            val usedName = taskNames.find { it in listOf(name, abbreviation) || it.endsWith(":$name") || it.endsWith(":$abbreviation") }
+            val taskNames = gradle.startParameter.taskNames
+            val usedName = buildType.findUsedTaskNameFrom(taskNames)
             val index = taskNames.indexOf(usedName)
-            if (usedName != null && !usedName.isEmpty() &&
-                !((index > 0) && (taskNames[index - 1] == "--task") &&
-                    ((index > 1) && Regex("h(e(lp?)?)?").matches(taskNames[index - 2])))) {
-                val subproject =
-                    if (usedName.contains(":")) usedName.substring(0, usedName.lastIndexOf(":") + 1)
-                    else ""
-                project.afterEvaluate {
+            if (usedName != null && usedName.isNotEmpty() && isNotHelpTaskInvocation(usedName, index, taskNames)) {
+                afterEvaluate {
                     taskNames.removeAt(index)
-                    if (subproject.isEmpty() || project.findProject(subproject) != null) {
-                        buildType.tasks.reversed().forEach {
-                            val path = subproject + it
-                            if (subproject.isEmpty() || project.tasks.findByPath(path) != null) {
-                                taskNames.add(index, path)
-                            } else {
-                                println("Skipping task '$path' requested by build type $name, as it does not exist.")
-                            }
-                        }
-                    } else {
-                        println("Skipping execution of build type '${buildType.name}'. Project '$subproject' not found in root project '${project.name}'.")
-                    }
-                    if (taskNames.isEmpty()) {
-                        taskNames.add("help") //do not trigger the default tasks
-                    }
-                    project.gradle.startParameter.setTaskNames(taskNames)
+                    insertBuildTypeTaskNames(buildType, usedName, index, taskNames)
+                    gradle.startParameter.setTaskNames(taskNames)
                 }
-                buildType.propertiesAction = { properties: Map<String, Any> ->
-                    properties.forEach { key, value ->
-                        project.extra.set(key, value)
-                    }
-                }
+                buildType.propertiesAsExtraOn(project)
             }
             doFirst {
-                throw GradleException("'$name' is a build type and has to be invoked directly, and its name can only be abbreviated to '$abbreviation'.")
+                throw GradleException("'$name' is a build type and has to be invoked directly, and its name can only be abbreviated to '${buildType.abbreviation}'.")
             }
+        }
+
+
+    private
+    fun BuildType.findUsedTaskNameFrom(taskNames: List<String>): String? {
+        val candidates = listOf(name, abbreviation)
+        val nameSuffix = ":$name"
+        val abbreviationSuffix = ":$abbreviation"
+        return taskNames.find { it in candidates || it.endsWith(nameSuffix) || it.endsWith(abbreviationSuffix) }
+    }
+
+
+    private
+    val helpTaskRegex = Regex("h(e(lp?)?)?")
+
+
+    private
+    fun isNotHelpTaskInvocation(usedName: String, index: Int, taskNames: List<String>) =
+        !(index > 0 && taskNames[index - 1] == "--task" && index > 1 && helpTaskRegex.matches(taskNames[index - 2]))
+
+
+    private
+    fun Project.insertBuildTypeTaskNames(buildType: BuildType, usedName: String, index: Int, taskNames: MutableList<String>) {
+        val subproject = usedName.substringBeforeLast(":", "")
+        if (subproject.isEmpty() || findProject(subproject) != null) {
+            buildType.tasks.reversed().forEach {
+                val path = subproject + it
+                if (subproject.isEmpty() || tasks.findByPath(path) != null) {
+                    taskNames.add(index, path)
+                } else {
+                    println("Skipping task '$path' requested by build type $name, as it does not exist.")
+                }
+            }
+        } else {
+            println("Skipping execution of build type '${buildType.name}'. Project '$subproject' not found in root project '$name'.")
+        }
+        if (taskNames.isEmpty()) {
+            taskNames.add("help") //do not trigger the default tasks
         }
     }
 
+
+    private
+    fun BuildType.propertiesAsExtraOn(project: Project) {
+        propertiesAction = { properties: Map<String, Any> ->
+            properties.forEach { key, value ->
+                project.extra.set(key, value)
+            }
+        }
+    }
 }
