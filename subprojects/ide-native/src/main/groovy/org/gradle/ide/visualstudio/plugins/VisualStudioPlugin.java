abc836ec262fecb1862cef315970c9548df3522d
Gary Hale <gary@gradle.com>
2018-02-16 11:59:44 -0800

    Pull visual studio tasks up to main VisualStudioPlugin

--- subprojects/ide-native/src/main/groovy/org/gradle/ide/visualstudio/plugins/VisualStudioPlugin.java
+++ subprojects/ide-native/src/main/groovy/org/gradle/ide/visualstudio/plugins/VisualStudioPlugin.java
@@ -18,14 +18,23 @@ package org.gradle.ide.visualstudio.plugins;
 
 import org.gradle.api.*;
 import org.gradle.api.internal.file.FileResolver;
+import org.gradle.api.tasks.Delete;
 import org.gradle.ide.visualstudio.VisualStudioExtension;
+import org.gradle.ide.visualstudio.VisualStudioProject;
 import org.gradle.ide.visualstudio.VisualStudioRootExtension;
+import org.gradle.ide.visualstudio.VisualStudioSolution;
 import org.gradle.ide.visualstudio.internal.CppApplicationVisualStudioTargetBinary;
 import org.gradle.ide.visualstudio.internal.CppSharedLibraryVisualStudioTargetBinary;
 import org.gradle.ide.visualstudio.internal.CppStaticLibraryVisualStudioTargetBinary;
 import org.gradle.ide.visualstudio.internal.DefaultVisualStudioExtension;
+import org.gradle.ide.visualstudio.internal.DefaultVisualStudioProject;
 import org.gradle.ide.visualstudio.internal.DefaultVisualStudioRootExtension;
 import org.gradle.ide.visualstudio.internal.VisualStudioExtensionInternal;
+import org.gradle.ide.visualstudio.internal.VisualStudioProjectInternal;
+import org.gradle.ide.visualstudio.internal.VisualStudioSolutionInternal;
+import org.gradle.ide.visualstudio.tasks.GenerateFiltersFileTask;
+import org.gradle.ide.visualstudio.tasks.GenerateProjectFileTask;
+import org.gradle.ide.visualstudio.tasks.GenerateSolutionFileTask;
 import org.gradle.internal.reflect.Instantiator;
 import org.gradle.language.base.plugins.LifecycleBasePlugin;
 import org.gradle.language.cpp.CppApplication;
@@ -108,8 +117,91 @@ public class VisualStudioPlugin extends IdePlugin {
             }
         });
 
+        includeBuildFileInProject(extension);
+        createTasksForVisualStudio(extension);
+
         // TODO: Figure out how to conditionally apply VisualStudioPluginRules but ensure that rules are still fired in subprojects
         project.getPluginManager().apply(NativeComponentModelPlugin.class);
         project.getPluginManager().apply(VisualStudioPluginRules.class);
+    }
+
+    private void includeBuildFileInProject(VisualStudioExtensionInternal extension) {
+        extension.getProjectRegistry().all(new Action<DefaultVisualStudioProject>() {
+            @Override
+            public void execute(DefaultVisualStudioProject vsProject) {
+                if (project.getBuildFile() != null) {
+                    vsProject.addSourceFile(project.getBuildFile());
+                }
+            }
+        });
+    }
+
+    private void createTasksForVisualStudio(VisualStudioExtensionInternal extension) {
+        extension.getProjectRegistry().all(new Action<DefaultVisualStudioProject>() {
+            @Override
+            public void execute(DefaultVisualStudioProject vsProject) {
+                addTasksForVisualStudioProject(vsProject);
+            }
+        });
+
+        if (isRoot()) {
+            VisualStudioRootExtension rootExtension = (VisualStudioRootExtension) extension;
+            VisualStudioSolutionInternal vsSolution = (VisualStudioSolutionInternal) rootExtension.getSolution();
+
+            vsSolution.builtBy(createSolutionTask(vsSolution));
+        }
+
+        configureCleanTask();
+    }
+
+    private void addTasksForVisualStudioProject(VisualStudioProjectInternal vsProject) {
+        vsProject.builtBy(createProjectsFileTask(vsProject), createFiltersFileTask(vsProject));
+
+        Task lifecycleTask = project.getTasks().maybeCreate(vsProject.getComponentName() + "VisualStudio");
+        lifecycleTask.dependsOn(vsProject);
+    }
+
+    private void configureCleanTask() {
+        final Delete cleanTask = (Delete) getCleanTask();
+
+        project.getTasks().withType(GenerateSolutionFileTask.class).all(new Action<GenerateSolutionFileTask>() {
+            @Override
+            public void execute(GenerateSolutionFileTask task) {
+                cleanTask.delete(task.getOutputs().getFiles());
+            }
+        });
+
+        project.getTasks().withType(GenerateFiltersFileTask.class).all(new Action<GenerateFiltersFileTask>() {
+            @Override
+            public void execute(GenerateFiltersFileTask task) {
+                cleanTask.delete(task.getOutputs().getFiles());
+            }
+        });
+
+        project.getTasks().withType(GenerateProjectFileTask.class).all(new Action<GenerateProjectFileTask>() {
+            @Override
+            public void execute(GenerateProjectFileTask task) {
+                cleanTask.delete(task.getOutputs().getFiles());
+            }
+        });
+    }
+
+    private Task createSolutionTask(VisualStudioSolution solution) {
+        GenerateSolutionFileTask solutionFileTask = project.getTasks().create(solution.getName() + "VisualStudioSolution", GenerateSolutionFileTask.class);
+        solutionFileTask.setVisualStudioSolution(solution);
+        return solutionFileTask;
+    }
+
+    private Task createProjectsFileTask(VisualStudioProject vsProject) {
+        GenerateProjectFileTask task = project.getTasks().create(vsProject.getName() + "VisualStudioProject", GenerateProjectFileTask.class);
+        task.setVisualStudioProject(vsProject);
+        task.initGradleCommand();
+        return task;
+    }
+
+    private Task createFiltersFileTask(VisualStudioProject vsProject) {
+        GenerateFiltersFileTask task = project.getTasks().create(vsProject.getName() + "VisualStudioFilters", GenerateFiltersFileTask.class);
+        task.setVisualStudioProject(vsProject);
+        return task;
     }
 }
--- subprojects/ide-native/src/main/groovy/org/gradle/ide/visualstudio/plugins/VisualStudioPluginRules.java
+++ subprojects/ide-native/src/main/groovy/org/gradle/ide/visualstudio/plugins/VisualStudioPluginRules.java
@@ -16,27 +16,15 @@
 
 package org.gradle.ide.visualstudio.plugins;
 
-import org.gradle.api.Action;
-import org.gradle.api.Task;
 import org.gradle.api.internal.project.ProjectIdentifier;
 import org.gradle.api.internal.project.ProjectInternal;
 import org.gradle.api.internal.project.ProjectRegistry;
 import org.gradle.api.internal.resolve.ProjectModelResolver;
 import org.gradle.api.plugins.ExtensionContainer;
-import org.gradle.api.tasks.Delete;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.ide.visualstudio.VisualStudioExtension;
-import org.gradle.ide.visualstudio.VisualStudioProject;
-import org.gradle.ide.visualstudio.VisualStudioRootExtension;
-import org.gradle.ide.visualstudio.VisualStudioSolution;
-import org.gradle.ide.visualstudio.internal.DefaultVisualStudioProject;
 import org.gradle.ide.visualstudio.internal.NativeSpecVisualStudioTargetBinary;
 import org.gradle.ide.visualstudio.internal.VisualStudioExtensionInternal;
-import org.gradle.ide.visualstudio.internal.VisualStudioProjectInternal;
-import org.gradle.ide.visualstudio.internal.VisualStudioSolutionInternal;
-import org.gradle.ide.visualstudio.tasks.GenerateFiltersFileTask;
-import org.gradle.ide.visualstudio.tasks.GenerateProjectFileTask;
-import org.gradle.ide.visualstudio.tasks.GenerateSolutionFileTask;
 import org.gradle.internal.Cast;
 import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.model.Model;
@@ -52,17 +40,6 @@ class VisualStudioPluginRules extends RuleSource {
     }
 
     @Mutate
-    public static void includeBuildFileInProject(VisualStudioExtensionInternal visualStudio, final ProjectIdentifier projectIdentifier) {
-        visualStudio.getProjects().all(new Action<VisualStudioProject>() {
-            public void execute(VisualStudioProject project) {
-                if (projectIdentifier.getBuildFile() != null) {
-                    ((DefaultVisualStudioProject) project).addSourceFile(projectIdentifier.getBuildFile());
-                }
-            }
-        });
-    }
-
-    @Mutate
     public static void createVisualStudioModelForBinaries(VisualStudioExtensionInternal visualStudioExtension, BinaryContainer binaries, ProjectIdentifier projectIdentifier, ServiceRegistry serviceRegistry) {
         for (NativeBinarySpec binary : binaries.withType(NativeBinarySpec.class)) {
             visualStudioExtension.getProjectRegistry().addProjectConfiguration(new NativeSpecVisualStudioTargetBinary(binary));
@@ -74,22 +51,8 @@ class VisualStudioPluginRules extends RuleSource {
     }
 
     @Mutate
-    public static void createTasksForVisualStudio(TaskContainer tasks, VisualStudioExtensionInternal visualStudioExtension, ProjectIdentifier projectIdentifier) {
-        for (VisualStudioProject vsProject : visualStudioExtension.getProjects()) {
-            ((VisualStudioProjectInternal)vsProject).builtBy(createProjectsFileTask(tasks, vsProject), createFiltersFileTask(tasks, vsProject));
-
-            Task lifecycleTask = tasks.maybeCreate(((VisualStudioProjectInternal) vsProject).getComponentName() + "VisualStudio");
-            lifecycleTask.dependsOn(vsProject);
-        }
-
-        if (isRoot(projectIdentifier)) {
-            VisualStudioRootExtension rootExtension = (VisualStudioRootExtension) visualStudioExtension;
-            VisualStudioSolutionInternal vsSolution = (VisualStudioSolutionInternal) rootExtension.getSolution();
-
-            vsSolution.builtBy(createSolutionTask(tasks, vsSolution));
-        }
-
-        addCleanTask(tasks);
+    public static void realizeExtension(TaskContainer tasks, VisualStudioExtensionInternal visualStudioExtensionInternal) {
+        // Dummy rule to cause the extension to be realized
     }
 
     // This ensures that subprojects are realized and register their project and project configuration IDE artifacts
@@ -106,37 +69,7 @@ class VisualStudioPluginRules extends RuleSource {
         return projectIdentifier.getParentIdentifier() == null;
     }
 
-    private static void addCleanTask(TaskContainer tasks) {
-        Delete cleanTask = tasks.maybeCreate("cleanVisualStudio", Delete.class);
-        for (Task task : tasks.withType(GenerateSolutionFileTask.class)) {
-            cleanTask.delete(task.getOutputs().getFiles());
-        }
-        for (Task task : tasks.withType(GenerateFiltersFileTask.class)) {
-            cleanTask.delete(task.getOutputs().getFiles());
-        }
-        for (Task task : tasks.withType(GenerateProjectFileTask.class)) {
-            cleanTask.delete(task.getOutputs().getFiles());
-        }
-        cleanTask.setGroup("IDE");
-        cleanTask.setDescription("Removes all generated Visual Studio project and solution files");
-    }
 
-    private static Task createSolutionTask(TaskContainer tasks, VisualStudioSolution solution) {
-        GenerateSolutionFileTask solutionFileTask = tasks.create(solution.getName() + "VisualStudioSolution", GenerateSolutionFileTask.class);
-        solutionFileTask.setVisualStudioSolution(solution);
-        return solutionFileTask;
-    }
 
-    private static Task createProjectsFileTask(TaskContainer tasks, VisualStudioProject vsProject) {
-        GenerateProjectFileTask task = tasks.create(vsProject.getName() + "VisualStudioProject", GenerateProjectFileTask.class);
-        task.setVisualStudioProject(vsProject);
-        task.initGradleCommand();
-        return task;
-    }
 
-    private static Task createFiltersFileTask(TaskContainer tasks, VisualStudioProject vsProject) {
-        GenerateFiltersFileTask task = tasks.create(vsProject.getName() + "VisualStudioFilters", GenerateFiltersFileTask.class);
-        task.setVisualStudioProject(vsProject);
-        return task;
-    }
 }
