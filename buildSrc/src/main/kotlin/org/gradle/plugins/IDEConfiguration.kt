317f97c43f5a306224e907195c20b88876f6a640
hansd <hans@gradle.com>
2018-02-19 15:26:09 -0800

    Reflect latest change in idea.gradle setting targetByteCodeLevel to 1.6.

--- buildSrc/src/main/kotlin/org/gradle/plugins/IDEConfiguration.kt
+++ buildSrc/src/main/kotlin/org/gradle/plugins/IDEConfiguration.kt
@@ -233,7 +233,7 @@ class IDEConfiguration : Plugin<Project> {
         val ideaLanguageLevel = if ((rootProject.findProperty("projectsRequiringJava8") as List<String>).contains(ideaModule.project.path)) "1.8" else "1.6"
         // Force everything to Java 6, pending detangling some int test cycles or switching to project-per-source-set mapping
         ideaModule.languageLevel = IdeaLanguageLevel(ideaLanguageLevel)
-        ideaModule.targetBytecodeVersion = JavaVersion.toVersion(ideaLanguageLevel)
+        ideaModule.targetBytecodeVersion = JavaVersion.VERSION_1_6
     }
 
     private fun removeGradleBuildOutputDirectories(module: Module) {
