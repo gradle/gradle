1b11cd97239512a994a817925e669f6851122bad
Paul Merlin <paul@gradle.com>
2018-02-20 14:47:02 +0100

    Polish MinifyTransform
    by simplifying expressions
    and fixing members visibility

--- buildSrc/src/main/kotlin/org/gradle/MinifyTransform.kt
+++ buildSrc/src/main/kotlin/org/gradle/MinifyTransform.kt
@@ -6,29 +6,23 @@ import com.google.common.io.Files
 
 import javax.inject.Inject
 import java.io.File
-import java.util.HashSet
 
 
-open class MinifyTransform @Inject constructor(val keepClassesByArtifact: Map<String, Set<String>>) : ArtifactTransform() {
-    override fun transform(artifact: File): List<File> {
-        val name = artifact.name
-        for ((key, value) in keepClassesByArtifact) {
-            if (name.startsWith(key)) {
-                return listOf(minify(artifact, value))
-            }
-        }
-        return listOf(artifact)
-    }
+open class MinifyTransform @Inject constructor(
+    private val keepClassesByArtifact: Map<String, Set<String>>) : ArtifactTransform() {
+
+    override fun transform(artifact: File) =
+        keepClassesByArtifact.asSequence()
+            .firstOrNull { (key, _) -> artifact.name.startsWith(key) }
+            ?.value?.let { keepClasses -> listOf(minify(artifact, keepClasses)) }
+            ?: listOf(artifact)
 
     private fun minify(artifact: File, keepClasses: Set<String>): File {
-        val out = outputDirectory
-        val jarFile = File(out, Files.getNameWithoutExtension(artifact.path) + "-min.jar")
-        val classesDir = File(out, "classes")
-        val analysisFile = File(out, "analysis.txt")
-        ShadedJarCreator(setOf(artifact), jarFile, analysisFile, classesDir, "", keepClasses, keepClasses, HashSet<String>()).createJar()
+        val jarFile = outputDirectory.resolve("${Files.getNameWithoutExtension(artifact.path)}-min.jar")
+        val classesDir = outputDirectory.resolve("classes")
+        val analysisFile = outputDirectory.resolve("analysis.txt")
+        ShadedJarCreator(setOf(artifact), jarFile, analysisFile, classesDir, "", keepClasses, keepClasses, hashSetOf())
+            .createJar()
         return jarFile
     }
 }
-
-
-
