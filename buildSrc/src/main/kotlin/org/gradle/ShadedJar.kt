5f6ff25ffaa90cbea1bc11efe07c2594193df0fa
Paul Merlin <paul@gradle.com>
2018-02-20 14:48:20 +0100

    Polish ShadedJar
    by using lateinit task properties where appropriate
    removing all the double bangs !!

--- buildSrc/src/main/kotlin/org/gradle/ShadedJar.kt
+++ buildSrc/src/main/kotlin/org/gradle/ShadedJar.kt
@@ -10,61 +10,63 @@ import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 import java.io.File
-import java.util.LinkedHashSet
 
 
 @CacheableTask
 open class ShadedJar : DefaultTask() {
+
     /**
      * The source files to generate the jar from.
      */
     @Classpath
-    var sourceFiles: FileCollection? = null
+    lateinit var sourceFiles: FileCollection
 
     /**
      * The directory to write temporary class files to.
      */
     @OutputDirectory
-    var classesDir: File? = null
+    lateinit var classesDir: File
 
     /**
      * The output Jar file.
      */
     @OutputFile
-    var jarFile: File? = null
+    lateinit var jarFile: File
 
     /**
      * File to write the text analysis report to.
      */
     @OutputFile
-    var analysisFile: File? = null
+    lateinit var analysisFile: File
 
     /**
      * The package name to prefix all shaded class names with.
      */
     @Input
-    var shadowPackage: String? = null
+    lateinit var shadowPackage: String
 
     /**
      * Retain only those classes in the keep package hierarchies, plus any classes that are reachable from these classes.
      */
     @Input
-    var keepPackages: Set<String> = LinkedHashSet()
+    var keepPackages: Set<String> = emptySet()
 
     /**
      * Do not rename classes in the unshaded package hierarchies. Always includes 'java'.
      */
     @Input
-    var unshadedPackages: Set<String> = LinkedHashSet()
+    var unshadedPackages: Set<String> = emptySet()
 
     /**
-     * Do not retain classes in the ingore packages hierarchies, unless reachable from some other retained class.
+     * Do not retain classes in the ignore packages hierarchies, unless reachable from some other retained class.
      */
     @Input
-    var ignorePackages: Set<String> = LinkedHashSet()
+    var ignorePackages: Set<String> = emptySet()
 
     @TaskAction
     fun run() {
-        ShadedJarCreator(sourceFiles!!, jarFile!!, analysisFile!!, classesDir!!, shadowPackage!!, keepPackages, unshadedPackages, ignorePackages).createJar()
+        ShadedJarCreator(
+            sourceFiles, jarFile, analysisFile, classesDir,
+            shadowPackage, keepPackages, unshadedPackages, ignorePackages).createJar()
     }
 }
