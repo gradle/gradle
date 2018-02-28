6f01f2838b43dac5bc435e3a813af218366c8dd2
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-02-28 10:45:38 -0300

    Introduce `TaskContainer.getByName<T>(String, T.() -> Unit)`

--- buildSrc/src/main/kotlin/org/gradle/kotlin/dsl/kotlin-dsl-upstream.kt
+++ buildSrc/src/main/kotlin/org/gradle/kotlin/dsl/kotlin-dsl-upstream.kt
@@ -1,5 +1,6 @@
 package org.gradle.kotlin.dsl
 
+import org.gradle.api.Action
 import org.gradle.api.Incubating
 import org.gradle.api.UnknownDomainObjectException
 import org.gradle.api.file.ContentFilterable
@@ -55,6 +56,28 @@ fun <reified T : Any> TaskContainer.getByName(name: String) =
             ?: throw IllegalStateException(
             "Element '$name' of type '${it::class.java.name}' from container '$this' cannot be cast to '${T::class.qualifiedName}'.")
     }
+
+
+/**
+ * Looks for the task of a given name, casts it to the expected type [T]
+ * then applies the given [configure] action.
+ *
+ * If none found it will throw an [UnknownDomainObjectException].
+ * If the task is found but cannot be cast to the expected type it will throw an [IllegalStateException].
+ *
+ * @param name task name
+ * @return task, never null
+ * @throws [UnknownDomainObjectException] When the given task is not found.
+ * @throws [IllegalStateException] When the given task cannot be cast to the expected type.
+ */
+@Suppress("extension_shadowed_by_member")
+inline
+fun <reified T : Any> TaskContainer.getByName(name: String, configure: T.() -> Unit) =
+    getByName(name).let {
+        it as? T
+            ?: throw IllegalStateException(
+            "Element '$name' of type '${it::class.java.name}' from container '$this' cannot be cast to '${T::class.qualifiedName}'.")
+    }.also(configure)
 
 
 /**
