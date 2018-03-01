22a6ef387b5d463ed856320544ff0a169ba0e34d
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-03-01 14:19:31 -0300

    Make target of `the<T>()` explicit now that it's available on any `ExtensionAware`

--- gradle/distributionTesting.gradle.kts
+++ gradle/distributionTesting.gradle.kts
@@ -38,7 +38,7 @@ tasks.withType<DistributionTest> {
         jvmArgs("-XX:MaxPermSize=768m")
     }
 
-    reports.junitXml.destination = the<JavaPluginConvention>().testResultsDir.resolve(name)
+    reports.junitXml.destination = project.the<JavaPluginConvention>().testResultsDir.resolve(name)
 
     // use -PtestVersions=all or -PtestVersions=1.2,1.3…
     val integTestVersionsSysProp = "org.gradle.integtest.versions"
