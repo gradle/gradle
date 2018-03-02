134807fe39320f6568c26be13003beb4e1f360a3
hansd <mail@dockter.de>
2018-03-01 17:12:19 -0800

    Fixed broken internal pdfUserguide task by excluding Xerces dependency

--- buildSrc/subprojects/testing/testing.gradle.kts
+++ buildSrc/subprojects/testing/testing.gradle.kts
@@ -1,13 +1,7 @@
 dependencies {
     compile(project(":build"))
-    compile("org.codehaus.groovy.modules.http-builder:http-builder:0.7.2")
-
-    components {
-        withModule("net.sourceforge.nekohtml:nekohtml") {
-            allVariants {
-                // Xerces on the runtime classpath is breaking some of our doc tasks
-                withDependencies { removeAll { it.group == "xerces" } }
-            }
-        }
+    compile("org.codehaus.groovy.modules.http-builder:http-builder:0.7.2") {
+        // Xerces on the runtime classpath is breaking some of our doc tasks
+        exclude(group = "xerces")
     }
 }
