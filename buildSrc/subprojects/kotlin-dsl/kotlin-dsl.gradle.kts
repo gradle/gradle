55ad128ecb3730e3a1b52587a3f264850c088a85
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-03-05 15:14:50 -0300

    Upgrade Kotlin DSL {0.16.0 => 0.16.1}

--- buildSrc/subprojects/kotlin-dsl/kotlin-dsl.gradle.kts
+++ buildSrc/subprojects/kotlin-dsl/kotlin-dsl.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    api("org.gradle:gradle-kotlin-dsl:0.16.0")
+    api("org.gradle:gradle-kotlin-dsl:0.16.1")
     testImplementation("junit:junit:4.12")
     testImplementation("com.nhaarman:mockito-kotlin:1.5.0")
 }
