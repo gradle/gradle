ebecd47f9784a1c5c00c547d1eefdedf086b0493
Stefan Wolf <wolf@gradle.com>
2018-03-06 09:02:44 +0100

    Add validateTaskProperties task to buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -71,6 +71,21 @@ subprojects {
     dependencies {
         compile(gradleApi())
     }
+
+    afterEvaluate {
+        if (tasks.withType<ValidateTaskProperties>().isEmpty()) {
+            tasks.create<ValidateTaskProperties>("validateTaskProperties") {
+                outputFile.set(the<ReportingExtension>().baseDirectory.file("task-properties/report.txt"))
+
+                val mainSourceSet = project.java.sourceSets[SourceSet.MAIN_SOURCE_SET_NAME]
+                classes = mainSourceSet.output.classesDirs
+                classpath = mainSourceSet.compileClasspath
+                dependsOn(mainSourceSet.output)
+
+                project.tasks["check"].dependsOn(this)
+            }
+        }
+    }
 }
 
 allprojects {
