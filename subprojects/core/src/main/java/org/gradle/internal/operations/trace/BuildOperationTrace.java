a0e5e0f537d0dabaabbbf58f067627db213ecf46
Luke Daley <luke@gradle.com>
2018-03-06 16:00:30 +1000

    Remove workaround for build operation trace not supporting GradleBuild tasks.
    It now does.

--- subprojects/core/src/main/java/org/gradle/internal/operations/trace/BuildOperationTrace.java
+++ subprojects/core/src/main/java/org/gradle/internal/operations/trace/BuildOperationTrace.java
@@ -258,12 +258,8 @@ public class BuildOperationTrace implements Stoppable {
                     } else if (map.containsKey("time")) {
                         SerializedOperationProgress serialized = new SerializedOperationProgress(map);
                         PendingOperation pending = pendings.get(serialized.id);
-                        if (pending != null) {
-                            // necessary as BuildCacheCompositeConfigurationIntegrationTest exposes problem with tracing composite settings compilation build operation not exposed
-                            // in build operation trace
-                            // assert pending != null;
-                            pending.progress.add(serialized);
-                        }
+                        assert pending != null : "did not find owner of progress event with ID " + serialized.id;
+                        pending.progress.add(serialized);
                     } else {
                         SerializedOperationFinish finish = new SerializedOperationFinish(map);
 
