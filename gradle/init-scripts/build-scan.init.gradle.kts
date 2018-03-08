7c82e5ca67323e15f88b1e4e9677a1d65d11f6f4
hansd <mail@dockter.de>
2018-03-08 05:50:10 -0800

    Use gradlePluginPortal() method and add missing disable property

--- gradle/init-scripts/build-scan.init.gradle.kts
+++ gradle/init-scripts/build-scan.init.gradle.kts
@@ -3,24 +3,24 @@
  */
 initscript {
     repositories {
-        maven {
-            setUrl("https://plugins.gradle.org/m2")
-        }
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.gradle:build-scan-plugin:1.12.1")
     }
 }
 
-rootProject {
-    pluginManager.withPlugin("com.gradle.build-scan") {
-        extensions["buildScan"].withGroovyBuilder {
-            if (gradle.startParameter.taskNames in listOf(listOf("verifyTestFilesCleanup"), listOf("killExistingProcessesStartedByGradle"))) {
-                "publishOnFailure"()
-            } else {
-                "publishAlways"()
+if (!gradle.startParameter.systemPropertiesArgs.containsKey("disableScanPlugin")) {
+    rootProject {
+        pluginManager.withPlugin("com.gradle.build-scan") {
+            extensions["buildScan"].withGroovyBuilder {
+                if (gradle.startParameter.taskNames in listOf(listOf("verifyTestFilesCleanup"), listOf("killExistingProcessesStartedByGradle"))) {
+                    "publishOnFailure"()
+                } else {
+                    "publishAlways"()
+                }
+                setProperty("server", "https://e.grdev.net")
             }
-            setProperty("server", "https://e.grdev.net")
         }
     }
 }
