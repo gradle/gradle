b85a0b518f24192afa4d153346d3afd9dd5f5eee
Cedric Champeau <cedric@gradle.com>
2018-03-13 09:20:13 +0100

    Bump cache layout format
    This is to avoid an issue with CI, format 54 was used in a previous spike.

--- subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheLayoutTest.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheLayoutTest.groovy
@@ -46,10 +46,10 @@ class CacheLayoutTest extends Specification {
         CacheLayout cacheLayout = CacheLayout.META_DATA
 
         then:
-        cacheLayout.key == 'metadata-2.54'
-        cacheLayout.version == VersionNumber.parse("2.54.0")
-        cacheLayout.formattedVersion == '2.54'
-        cacheLayout.getPath(new File('some/dir')) == new File('some/dir/metadata-2.54')
+        cacheLayout.key == 'metadata-2.55'
+        cacheLayout.version == VersionNumber.parse("2.55.0")
+        cacheLayout.formattedVersion == '2.55'
+        cacheLayout.getPath(new File('some/dir')) == new File('some/dir/metadata-2.55')
     }
 
     def "use transforms layout"() {
