e4eb184961d5c7b26d04249807058bff9e91601d
Stefan Wolf <wolf@gradle.com>
2018-03-15 09:52:48 +0100

    Add reason why we unpack absent providers

--- subprojects/core/src/main/java/org/gradle/api/internal/tasks/properties/bean/AbstractNestedRuntimeBeanNode.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/tasks/properties/bean/AbstractNestedRuntimeBeanNode.java
@@ -88,7 +88,13 @@ public abstract class AbstractNestedRuntimeBeanNode extends RuntimeBeanNode<Obje
                         }
                     }
                 });
-                return value instanceof Provider && !((Provider) value).isPresent() ? null : value;
+                // Replace absent Provider with null.
+                // This is required for allowing optional provider properties - all code which unpacks providers calls Provider.get() and would fail if an optional provider is passed.
+                // Returning null from a Callable is ignored, and PropertyValue is a callable.
+                if (value instanceof Provider && !((Provider<?>) value).isPresent()) {
+                    return null;
+                }
+                return value;
             }
         });
 
