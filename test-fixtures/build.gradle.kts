ad9be2499bd0c1c83fad18c789dbafc17006ed61
Paul Merlin <paul@gradle.com>
2018-03-19 15:17:35 +0100

    Upgrade to mockito 2.16 to support running tests with jdk10

--- test-fixtures/build.gradle.kts
+++ test-fixtures/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     compile(gradleTestKit())
     compile("junit:junit:4.12")
     compile("com.nhaarman:mockito-kotlin:1.5.0")
+    compile("org.mockito:mockito-core:2.16.0")
     compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.2")
     compile("org.ow2.asm:asm-all:5.2")
 }
