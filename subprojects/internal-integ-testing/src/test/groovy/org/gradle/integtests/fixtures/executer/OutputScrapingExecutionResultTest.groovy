a36c3bb8b31616951401fccab010c537faa0d7ad
Adam Murdoch <adam@gradle.com>
2018-03-19 13:21:00 +1100

    Fix `ExecutionResult` task assertion methods to handle build output containing task output that is spread across more than one group.
    Add more context to error messages when these assertion methods fail.

--- subprojects/internal-integ-testing/src/test/groovy/org/gradle/integtests/fixtures/executer/OutputScrapingExecutionResultTest.groovy
+++ subprojects/internal-integ-testing/src/test/groovy/org/gradle/integtests/fixtures/executer/OutputScrapingExecutionResultTest.groovy
@@ -20,7 +20,7 @@ import spock.lang.Specification
 
 
 class OutputScrapingExecutionResultTest extends Specification {
-    def "normalizes line ending"() {
+    def "normalizes line ending in output"() {
         def output = "\n\r\nabc\r\n\n"
         def error = "\r\n\nerror\n\r\n"
         def result = OutputScrapingExecutionResult.from(output, error)
@@ -30,7 +30,7 @@ class OutputScrapingExecutionResultTest extends Specification {
         result.error == "\n\nerror\n\n"
     }
 
-    def "retains trailing line ending"() {
+    def "retains trailing line ending in output"() {
         def output = "\n\nabc\n"
         def error = "\nerror\n"
         def result = OutputScrapingExecutionResult.from(output, error)
@@ -40,7 +40,7 @@ class OutputScrapingExecutionResultTest extends Specification {
         result.error == error
     }
 
-    def "can assert build output is present"() {
+    def "can assert build output is present in main content"() {
         def output = """
 message
 
@@ -110,6 +110,200 @@ post build
         then:
         def e3 = thrown(AssertionError)
         e3.message.startsWith('Substring not found in build output')
+    }
+
+    def "can assert task is present when task output is split into several groups"() {
+        def output = """
+before
+
+> Task :a
+> Task :b
+
+> Task :a
+some content
+
+> Task :b
+other content
+
+> Task :a
+all good
+
+> Task :b SKIPPED
+
+after
+
+BUILD SUCCESSFUL in 12s
+
+post build
+"""
+        when:
+        def result = OutputScrapingExecutionResult.from(output, "")
+
+        then:
+        result.assertTasksExecuted(":a", ":b")
+        result.assertTasksExecuted([":a", ":b"])
+        result.assertTasksExecuted(":a", ":b", ":a", [":a", ":b"], ":b")
+
+        and:
+        result.assertTasksExecutedInOrder(":a", ":b")
+        result.assertTasksExecutedInOrder([":a", ":b"])
+
+        and:
+        result.executedTasks == [":a", ":b"]
+        result.skippedTasks == [":b"] as Set
+
+        and:
+        result.assertTasksNotSkipped(":a")
+        result.assertTasksNotSkipped(":a", ":a", [":a"])
+
+        and:
+        result.assertTaskNotSkipped(":a")
+
+        and:
+        result.assertTasksSkipped(":b")
+        result.assertTasksSkipped(":b", ":b", [":b"])
+
+        and:
+        result.assertTaskSkipped(":b")
+
+        when:
+        result.assertTasksExecuted(":a")
+
+        then:
+        def e = thrown(AssertionError)
+        e.message.startsWith('''
+            Build output does not contain the expected tasks.
+            Expected: [:a]
+            Actual: [:a, :b]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksExecuted(":a", ":b", ":c")
+
+        then:
+        def e2 = thrown(AssertionError)
+        e2.message.startsWith('''
+            Build output does not contain the expected tasks.
+            Expected: [:a, :b, :c]
+            Actual: [:a, :b]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksExecutedInOrder(":b", ":a")
+
+        then:
+        def e3 = thrown(AssertionError)
+        e3.message.startsWith(":a does not occur in expected order (expected: exact([:b, :a]), actual [:a, :b])")
+
+        when:
+        result.assertTasksExecutedInOrder(":a")
+
+        then:
+        def e4 = thrown(AssertionError)
+        e4.message.startsWith('''
+            Build output does not contain the expected tasks.
+            Expected: [:a]
+            Actual: [:a, :b]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksExecutedInOrder(":a", ":b", ":c")
+
+        then:
+        def e5 = thrown(AssertionError)
+        e5.message.startsWith('''
+            Build output does not contain the expected tasks.
+            Expected: [:a, :b, :c]
+            Actual: [:a, :b]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksSkipped()
+
+        then:
+        def e6 = thrown(AssertionError)
+        e6.message.startsWith('''
+            Build output does not contain the expected skipped tasks.
+            Expected: []
+            Actual: [:b]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksSkipped(":a")
+
+        then:
+        def e7 = thrown(AssertionError)
+        e7.message.startsWith('''
+            Build output does not contain the expected skipped tasks.
+            Expected: [:a]
+            Actual: [:b]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksSkipped(":b", ":c")
+
+        then:
+        def e8 = thrown(AssertionError)
+        e8.message.startsWith('''
+            Build output does not contain the expected skipped tasks.
+            Expected: [:b, :c]
+            Actual: [:b]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTaskSkipped(":a")
+
+        then:
+        def e9 = thrown(AssertionError)
+        e9.message.startsWith('''
+            Build output does not contain the expected skipped task.
+            Expected: :a
+            Actual: [:b]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksNotSkipped()
+
+        then:
+        def e10 = thrown(AssertionError)
+        e10.message.startsWith('''
+            Build output does not contain the expected non skipped tasks.
+            Expected: []
+            Actual: [:a]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksNotSkipped(":b")
+
+        then:
+        def e11 = thrown(AssertionError)
+        e11.message.startsWith('''
+            Build output does not contain the expected non skipped tasks.
+            Expected: [:b]
+            Actual: [:a]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTasksNotSkipped(":a", ":c")
+
+        then:
+        def e12 = thrown(AssertionError)
+        e12.message.startsWith('''
+            Build output does not contain the expected non skipped tasks.
+            Expected: [:a, :c]
+            Actual: [:a]
+        '''.stripIndent().trim())
+
+        when:
+        result.assertTaskNotSkipped(":b")
+
+        then:
+        def e13 = thrown(AssertionError)
+        e13.message.startsWith('''
+            Build output does not contain the expected non skipped task.
+            Expected: :b
+            Actual: [:a]
+        '''.stripIndent().trim())
     }
 
     def "creates failure result"() {
