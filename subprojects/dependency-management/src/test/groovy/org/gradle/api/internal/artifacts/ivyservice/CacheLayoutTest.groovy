61127330f8b56d1ac67d72853d2425cb5cbeec4f
Jendrik Johannes <jendrik@gradle.com>
2018-03-20 15:59:53 +0100

    Bump cache layout format for #4765
    The scope of each dependency management entry is cached.

--- subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheLayoutTest.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheLayoutTest.groovy
@@ -46,10 +46,10 @@ class CacheLayoutTest extends Specification {
         CacheLayout cacheLayout = CacheLayout.META_DATA
 
         then:
-        cacheLayout.key == 'metadata-2.55'
-        cacheLayout.version == VersionNumber.parse("2.55.0")
-        cacheLayout.formattedVersion == '2.55'
-        cacheLayout.getPath(new File('some/dir')) == new File('some/dir/metadata-2.55')
+        cacheLayout.key == 'metadata-2.56'
+        cacheLayout.version == VersionNumber.parse("2.56.0")
+        cacheLayout.formattedVersion == '2.56'
+        cacheLayout.getPath(new File('some/dir')) == new File('some/dir/metadata-2.56')
     }
 
     def "use transforms layout"() {
