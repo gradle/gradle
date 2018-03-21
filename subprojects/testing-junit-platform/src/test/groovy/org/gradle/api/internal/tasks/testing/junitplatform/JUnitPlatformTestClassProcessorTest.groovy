abeb7ebb9e3cb17c826ad40caac6a54480beccad
Bo Zhang <bo@gradle.com>
2018-03-21 15:16:34 +0800

    Exclude anonymous class in JUnit Platform (#4774)
    This fixes https://github.com/gradle/gradle/issues/4544 https://github.com/gradle/gradle/issues/4618
    
    Previously we send anonymous class to JUnit Platform test worker, which caused some issues.
    Now they're excluded by name pattern .*\$\d+

---
+++ subprojects/testing-junit-platform/src/test/groovy/org/gradle/api/internal/tasks/testing/junitplatform/JUnitPlatformTestClassProcessorTest.groovy
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.tasks.testing.junitplatform
+
+import org.gradle.api.internal.tasks.testing.TestResultProcessor
+import org.gradle.api.internal.tasks.testing.junit.TestClassExecutionListener
+import org.gradle.internal.actor.ActorFactory
+import org.gradle.internal.id.IdGenerator
+import org.gradle.internal.time.Clock
+import org.junit.Ignore
+import org.junit.experimental.runners.Enclosed
+import org.junit.runner.RunWith
+import spock.lang.Specification
+import spock.lang.Subject
+
+class JUnitPlatformTestClassProcessorTest extends Specification {
+    JUnitPlatformSpec spec = Mock()
+    IdGenerator idGenerator = Mock()
+    ActorFactory actorFactory = Mock()
+    Clock clock = Mock()
+    TestResultProcessor resultProcessor = Mock()
+    TestClassExecutionListener listener = Mock()
+
+    @Subject
+    JUnitPlatformTestClassProcessor processor = new JUnitPlatformTestClassProcessor(spec, idGenerator, actorFactory, clock)
+
+    class InnerClass {}
+
+    static class NestedClass {}
+
+    def 'can exclude inner class but include nested class'() {
+        given:
+        def executor = processor.createTestExecutor(resultProcessor, listener)
+
+        when:
+        executor.execute(InnerClass.name)
+        executor.execute(NestedClass.name)
+
+        then:
+        executor.testClasses == [NestedClass]
+    }
+
+    def 'can exclude nested class in Enclosed runner'() {
+        given:
+        def executor = processor.createTestExecutor(resultProcessor, listener)
+
+        when:
+        executor.execute(EnclosedTest.NestedClass.name)
+
+        then:
+        executor.testClasses.isEmpty()
+    }
+}
+
+@RunWith(Enclosed.class)
+@Ignore
+class EnclosedTest {
+    static class NestedClass {
+    }
+}
