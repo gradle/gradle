6620fcaba9c0dc6b0b68ba96cb2b2783b3f507f5
Stefan Wolf <wolf@gradle.com>
2018-03-21 10:57:38 +0100

    Remove code duplication

--- buildSrc/subprojects/packaging/src/main/kotlin/org/gradle/gradlebuild/packaging/ShadeClassesTransform.kt
+++ buildSrc/subprojects/packaging/src/main/kotlin/org/gradle/gradlebuild/packaging/ShadeClassesTransform.kt
@@ -18,20 +18,7 @@ package org.gradle.gradlebuild.packaging
 
 import com.google.gson.Gson
 import org.gradle.api.artifacts.transform.ArtifactTransform
-import org.objectweb.asm.ClassReader
-import org.objectweb.asm.ClassWriter
-import org.objectweb.asm.commons.ClassRemapper
-import org.objectweb.asm.commons.Remapper
 import java.io.File
-import java.io.IOException
-import java.net.URI
-import java.nio.file.FileSystems
-import java.nio.file.FileVisitResult
-import java.nio.file.FileVisitor
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.attribute.BasicFileAttributes
-import java.util.jar.JarFile
 import javax.inject.Inject
 
 
@@ -61,20 +48,9 @@ open class ShadeClassesTransform @Inject constructor(
     override fun transform(input: File): List<File> {
         val classesDir = outputDirectory.resolve(relocatedClassesDirName)
         classesDir.mkdir()
+        val manifestFile = outputDirectory.resolve(manifestFileName)
 
-        val classGraph = ClassGraph(
-            PackagePatterns(keepPackages),
-            PackagePatterns(unshadedPackages),
-            PackagePatterns(ignorePackages),
-            shadowPackage
-        )
-
-        val jarUri = URI.create("jar:${input.toPath().toUri()}")
-        FileSystems.newFileSystem(jarUri, emptyMap<String, Any>()).use { jarFileSystem ->
-            jarFileSystem.rootDirectories.forEach {
-                visitClassDirectory(it, classGraph, ignoredPackagePatterns, classesDir, outputDirectory.resolve(manifestFileName).toPath())
-            }
-        }
+        val classGraph = JarAnalyzer(shadowPackage, keepPackages, unshadedPackages, ignorePackages).analyze(input, classesDir, manifestFile)
 
         outputDirectory.resolve(classTreeFileName).bufferedWriter().use {
             Gson().toJson(classGraph.getDependencies(), it)
@@ -84,78 +60,6 @@ open class ShadeClassesTransform @Inject constructor(
         }
 
         return listOf(outputDirectory)
-    }
-
-    private
-    fun visitClassDirectory(dir: Path, classes: ClassGraph, ignored: PackagePatterns, classesDir: File, manifest: Path) {
-        Files.walkFileTree(dir, object : FileVisitor<Path> {
-
-            private
-            var seenManifest: Boolean = false
-
-            override fun preVisitDirectory(dir: Path?, attrs: BasicFileAttributes?) =
-                FileVisitResult.CONTINUE
-
-            override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
-                when {
-                    file.isClassFilePath() -> {
-                        visitClassFile(file)
-                    }
-                    file.isUnseenManifestFilePath() -> {
-                        seenManifest = true
-                        Files.copy(file, manifest)
-                    }
-                }
-                return FileVisitResult.CONTINUE
-            }
-
-            override fun visitFileFailed(file: Path?, exc: IOException?) =
-                FileVisitResult.TERMINATE
-
-            override fun postVisitDirectory(dir: Path?, exc: IOException?) =
-                FileVisitResult.CONTINUE
-
-            private
-            fun Path.isClassFilePath() =
-                toString().endsWith(".class")
-
-            private
-            fun Path.isUnshadedPropertiesFilePath() =
-                toString().endsWith(".properties") && classes.unshadedPackages.matches(toString())
-
-            private
-            fun Path.isUnseenManifestFilePath() =
-                toString() == "/${JarFile.MANIFEST_NAME}" && !seenManifest
-
-            private
-            fun visitClassFile(file: Path) {
-                try {
-                    val reader = ClassReader(Files.newInputStream(file))
-                    val details = classes[reader.className]
-                    details.visited = true
-                    val classWriter = ClassWriter(0)
-                    reader.accept(ClassRemapper(classWriter, object : Remapper() {
-                        override fun map(name: String): String {
-                            if (ignored.matches(name)) {
-                                return name
-                            }
-                            val dependencyDetails = classes[name]
-                            if (dependencyDetails !== details) {
-                                details.dependencies.add(dependencyDetails)
-                            }
-                            return dependencyDetails.outputClassName
-                        }
-                    }), ClassReader.EXPAND_FRAMES)
-
-                    classesDir.resolve(details.outputClassFilename).apply {
-                        parentFile.mkdirs()
-                        writeBytes(classWriter.toByteArray())
-                    }
-                } catch (exception: Exception) {
-                    throw ClassAnalysisException("Could not transform class from ${file.toFile()}", exception)
-                }
-            }
-        })
     }
 }
 
--- buildSrc/subprojects/packaging/src/main/kotlin/org/gradle/gradlebuild/packaging/ShadedJar.kt
+++ buildSrc/subprojects/packaging/src/main/kotlin/org/gradle/gradlebuild/packaging/ShadedJar.kt
@@ -25,16 +25,13 @@ import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
-import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 import java.io.File
-import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.nio.file.Path
 import java.util.*
 import java.util.jar.JarFile
 import java.util.jar.JarOutputStream
-import java.util.zip.ZipEntry
 
 
 open class ShadedJar : DefaultTask() {
@@ -78,9 +75,9 @@ open class ShadedJar : DefaultTask() {
 
         JarOutputStream(BufferedOutputStream(FileOutputStream(jarFile.get().asFile))).use { jarOutputStream ->
             if (!manifests.isEmpty) {
-                addJarEntry(JarFile.MANIFEST_NAME, manifests.first(), jarOutputStream)
+                jarOutputStream.addJarEntry(JarFile.MANIFEST_NAME, manifests.first())
             }
-            addJarEntry("org/gradle/build-receipt.properties", buildReceiptFile.get().asFile, jarOutputStream)
+            jarOutputStream.addJarEntry("org/gradle/build-receipt.properties", buildReceiptFile.get().asFile)
             relocatedClassesConfiguration.files.forEach { classesDir ->
                 val classesDirPath = classesDir.toPath()
                 classesDir.walk().filter {
@@ -88,7 +85,7 @@ open class ShadedJar : DefaultTask() {
                     classesToInclude.contains(relativePath)
                 }.forEach {
                     val relativePath = classesDirPath.relativePath(it)
-                    addJarEntry(relativePath, it, jarOutputStream)
+                    jarOutputStream.addJarEntry(relativePath, it)
                 }
             }
         }
@@ -96,13 +93,6 @@ open class ShadedJar : DefaultTask() {
 
     private
     fun Path.relativePath(other: File) = relativize(other.toPath()).toString().replace(File.separatorChar, '/')
-
-    private
-    fun addJarEntry(entryName: String, sourceFile: File, jarOutputStream: JarOutputStream) {
-        jarOutputStream.putNextEntry(ZipEntry(entryName))
-        BufferedInputStream(FileInputStream(sourceFile)).use { inputStream -> inputStream.copyTo(jarOutputStream) }
-        jarOutputStream.closeEntry()
-    }
 
     private
     inline fun <reified T> readJson(file: File) =
