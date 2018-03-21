6cfb6a588e071a71b319ebca69f1b9e94b687d72
Adam Brown <adamb@gradle.com>
2018-03-21 14:29:05 -0700

    Add tests for Task constructor arguments in Groovy build script

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/TaskDefinitionIntegrationSpec.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/TaskDefinitionIntegrationSpec.groovy
@@ -47,4 +47,179 @@ class TaskDefinitionIntegrationSpec extends AbstractIntegrationSpec {
         output.contains(message)
         output.contains("The Task.leftShift(Closure) method has been deprecated and is scheduled to be removed in Gradle 5.0. Please use Task.doLast(Action) instead.")
     }
+
+    def "can construct a custom task without constructor arguments"() {
+        given:
+        buildFile << """
+            class CustomTask extends DefaultTask {
+                @TaskAction
+                void printIt() {
+                    println("hello world")
+                }
+            }
+
+            task myTask(type: CustomTask)
+        """
+
+        when:
+        run 'myTask'
+
+        then:
+        result.output.contains('hello world')
+    }
+
+    def "can construct a custom task with constructor arguments via Map"() {
+        given:
+        buildFile << """
+            import javax.inject.Inject
+
+            class CustomTask extends DefaultTask {
+                final String message
+                final int number
+
+                @Inject
+                CustomTask(String message, int number) {
+                    this.message = message
+                    this.number = number
+                }
+
+                @TaskAction
+                void printIt() {
+                    println("\$message \$number")
+                }
+            }
+
+            task myTask(type: CustomTask, constructorArgs: ['hello', 42])
+        """
+
+        when:
+        run 'myTask'
+
+        then:
+        result.output.contains("hello 42")
+    }
+
+    def "can construct a custom task with constructor arguments via API"() {
+        given:
+        buildFile << """
+            import javax.inject.Inject
+
+            class CustomTask extends DefaultTask {
+                final String message
+                final int number
+
+                @Inject
+                CustomTask(String message, int number) {
+                    this.message = message
+                    this.number = number
+                }
+
+                @TaskAction
+                void printIt() {
+                    println("\$message \$number")
+                }
+            }
+
+            tasks.create('myTask', CustomTask, 'hello', 42)
+        """
+
+        when:
+        run 'myTask'
+
+        then:
+        result.output.contains("hello 42")
+    }
+
+    def "fails to build custom task if constructor arguments missing"() {
+        given:
+        buildFile << """
+            import javax.inject.Inject
+
+            class CustomTask extends DefaultTask {
+                final String message
+                final int number
+
+                @Inject
+                CustomTask(String message, int number) {
+                    this.message = message
+                    this.number = number
+                }
+
+                @TaskAction
+                void printIt() {
+                    println("\$message \$number")
+                }
+            }
+
+            task myTask(type: CustomTask, constructorArgs: ['hello'])
+        """
+
+        when:
+        fails 'myTask'
+
+        then:
+        result.output.contains("org.gradle.internal.service.UnknownServiceException: No service of type int available")
+    }
+
+    def "can construct a task with @Inject services"() {
+        given:
+        buildFile << """
+            import org.gradle.workers.WorkerExecutor
+            import javax.inject.Inject
+
+            class CustomTask extends DefaultTask {
+                private final WorkerExecutor executor
+
+                @Inject
+                CustomTask(WorkerExecutor executor) {
+                    this.executor = executor
+                }
+
+                @TaskAction
+                void printIt() {
+                    println(executor != null ? "got it" : "NOT IT")
+                }
+            }
+
+            task myTask(type: CustomTask)
+        """
+
+        when:
+        run 'myTask'
+
+        then:
+        result.output.contains("got it")
+    }
+
+    def "can construct a task with @Inject services and constructor args"() {
+        given:
+        buildFile << """
+            import org.gradle.workers.WorkerExecutor
+            import javax.inject.Inject
+
+            class CustomTask extends DefaultTask {
+                private final int number
+                private final WorkerExecutor executor
+
+                @Inject
+                CustomTask(int number, WorkerExecutor executor) {
+                    this.number = number
+                    this.executor = executor
+                }
+
+                @TaskAction
+                void printIt() {
+                    println(executor != null ? "got it \$number" : "\$number NOT IT")
+                }
+            }
+
+            task myTask(type: CustomTask, constructorArgs: [15])
+        """
+
+        when:
+        run 'myTask'
+
+        then:
+        result.output.contains("got it 15")
+    }
 }
