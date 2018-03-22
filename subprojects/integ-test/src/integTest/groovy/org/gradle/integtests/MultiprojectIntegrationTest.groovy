b37c3667ef52775d8408d39087e65e1d40967b11
Paul Merlin <paul@gradle.com>
2018-03-22 11:44:05 +0100

    Add @ToBeImplemented integration tests for #4799

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/MultiprojectIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/MultiprojectIntegrationTest.groovy
@@ -15,14 +15,16 @@
  */
 package org.gradle.integtests
 
-import org.junit.Test
-import org.gradle.integtests.fixtures.AbstractIntegrationTest
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.util.ToBeImplemented
+import spock.lang.Issue
 
-class MultiprojectIntegrationTest extends AbstractIntegrationTest {
-    @Test
-    public void canInjectConfigurationFromParentProject() {
-        testFile('settings.gradle') << 'include "a", "b"'
-        testFile('build.gradle') << '''
+class MultiprojectIntegrationTest extends AbstractIntegrationSpec {
+
+    def "can inject configuration from parent project"() {
+        given:
+        settingsFile << 'include "a", "b"'
+        buildFile << '''
             allprojects {
                 def destDir = buildDir
                 task test {
@@ -40,11 +42,28 @@ class MultiprojectIntegrationTest extends AbstractIntegrationTest {
                     new File(destDir, 'afterEvaluate.txt') << 'content'
                 }
             }
-'''
-        inTestDirectory().withTasks('test').run()
+        '''
 
-        testFile('build').assertHasDescendants('test.txt', 'whenReady.txt', 'afterEvaluate.txt')
-        testFile('a/build').assertHasDescendants('test.txt', 'whenReady.txt', 'afterEvaluate.txt')
-        testFile('b/build').assertHasDescendants('test.txt', 'whenReady.txt', 'afterEvaluate.txt')
+        when:
+        run("test")
+
+        then:
+        file("build").assertHasDescendants('test.txt', 'whenReady.txt', 'afterEvaluate.txt')
+        file('a/build').assertHasDescendants('test.txt', 'whenReady.txt', 'afterEvaluate.txt')
+        file('b/build').assertHasDescendants('test.txt', 'whenReady.txt', 'afterEvaluate.txt')
+    }
+
+    @Issue("gradle/gradle#4799")
+    @ToBeImplemented("This should succeed")
+    def "evaluationDependsOn nephew whose name is AFTER mine in the alphabet"() {
+        given:
+        settingsFile << 'include ":a", ":b:c"'
+        file("a/build.gradle") << "evaluationDependsOn(':b:c')"
+
+        when:
+        fails("help")
+
+        then:
+        failureHasCause("Attempt to define scope class loader before scope is locked")
     }
 }
