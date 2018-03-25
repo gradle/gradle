11bbf8b0ce3c08234c989526849e9d6ef6b3a856
Adam Murdoch <adam@gradle.com>
2018-03-26 06:29:39 +1100

    Add some Javadoc comments.

--- subprojects/core-api/src/main/java/org/gradle/api/artifacts/component/ProjectComponentIdentifier.java
+++ subprojects/core-api/src/main/java/org/gradle/api/artifacts/component/ProjectComponentIdentifier.java
@@ -27,21 +27,22 @@ import org.gradle.internal.scan.UsedByScanPlugin;
 @UsedByScanPlugin
 public interface ProjectComponentIdentifier extends ComponentIdentifier {
     /**
-     * Identifies the build that contains the project component.
+     * Identifies the build that contains the project that produces this component.
      *
      * @return The build identifier
      */
     BuildIdentifier getBuild();
 
     /**
-     * Returns the path of the project which the component belongs to.
+     * Returns the path of the project that produces this component. This path is relative to the containing build, so for example will return ':' for the root project of a build.
      *
      * @since 1.10
      */
     String getProjectPath();
 
     /**
-     * Returns the simple name of the project.
+     * Returns the simple name of the project that produces this component.
+     *
      * @since 4.5
      */
     String getProjectName();
