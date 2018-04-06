6fcbe98948606981afbad3f909f8bbab32b064ac
Stefan Oehme <stefan@gradle.com>
2018-04-06 16:16:38 +0200

    Revert "Remove dead code"
    This reverts commit 26e4016029d4f2545e9caa4ef8584143f03f0b4e.

--- subprojects/core/src/main/java/org/gradle/util/NameValidator.java
+++ subprojects/core/src/main/java/org/gradle/util/NameValidator.java
@@ -22,11 +22,11 @@ import java.util.Arrays;
 
 public final class NameValidator {
 
-    private static final char[] FORBIDDEN_CHARACTERS = new char[]{' ', '/', '\\', ':', '<', '>', '"', '?', '*', '|'};
+    private static final char[] FORBIDDEN_CHARACTERS = new char[] {' ', '/', '\\', ':', '<', '>', '"', '?', '*', '|'};
     private static final char FORBIDDEN_LEADING_AND_TRAILING_CHARACTER = '.';
+    private static final char REPLACEMENT_CHARACTER = '_';
 
-    private NameValidator() {
-    }
+    private NameValidator() { }
 
     /**
      * Validates that a given name string does not contain any forbidden characters.
@@ -39,5 +39,22 @@ public final class NameValidator {
         } else if (name.charAt(0) == FORBIDDEN_LEADING_AND_TRAILING_CHARACTER || name.charAt(name.length() - 1) == FORBIDDEN_LEADING_AND_TRAILING_CHARACTER) {
             DeprecationLogger.nagUserOfDeprecatedThing("The " + nameDescription + " '" + name + "' starts or ends with a '" + FORBIDDEN_LEADING_AND_TRAILING_CHARACTER + "'", fixSuggestion);
         }
+    }
+
+    /**
+     * Turns the given name into a valid name by replacing each forbidden character occurrence with "_".
+     */
+    public static String asValidName(String name) {
+        //TODO activate this once the names which are now deprecated are forbidden
+        //for (char forbiddenCharacter : FORBIDDEN_CHARACTERS) {
+        //    name = name.replace(forbiddenCharacter, REPLACEMENT_CHARACTER);
+        //}
+        //if (name.charAt(0) == FORBIDDEN_LEADING_AND_TRAILING_CHARACTER) {
+        //    name = REPLACEMENT_CHARACTER + name.substring(1);
+        //}
+        //if (name.charAt(0) == FORBIDDEN_LEADING_AND_TRAILING_CHARACTER || name.charAt(name.length() - 1) == FORBIDDEN_LEADING_AND_TRAILING_CHARACTER) {
+        //    name = name.substring(0, name.length() - 1) + REPLACEMENT_CHARACTER;
+        //}
+        return name;
     }
 }
