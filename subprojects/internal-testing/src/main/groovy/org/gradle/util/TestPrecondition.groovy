f0adc45ebc7ebb6fa486bf43c62772a4e8c23d4b
Bo Zhang <bo@gradle.com>
2018-04-10 17:45:59 +0100

    Ignore Taglet tests on JDK 10
    See https://github.com/gradle/gradle/issues/4993

--- subprojects/internal-testing/src/main/groovy/org/gradle/util/TestPrecondition.groovy
+++ subprojects/internal-testing/src/main/groovy/org/gradle/util/TestPrecondition.groovy
@@ -100,6 +100,9 @@ enum TestPrecondition implements org.gradle.internal.Factory<Boolean> {
     JDK8_OR_EARLIER({
         JavaVersion.current() <= JavaVersion.VERSION_1_8
     }),
+    JDK9_OR_EARLIER({
+        JavaVersion.current() <= JavaVersion.VERSION_1_9
+    }),
     JDK7_POSIX({
         NOT_WINDOWS.fulfilled
     }),
