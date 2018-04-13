2e4faa46de3ff6cd6aa81c54f427fc15b6c9b965
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-04-13 10:57:24 -0300

    Polish `GreetPlugin`

--- samples/buildSrc-plugin/buildSrc/src/main/kotlin/GreetPlugin.kt
+++ samples/buildSrc-plugin/buildSrc/src/main/kotlin/GreetPlugin.kt
@@ -6,14 +6,14 @@ import org.gradle.kotlin.dsl.*
 
 class GreetPlugin : Plugin<Project> {
 
-    override fun apply(target: Project): Unit = target.run {
+    override fun apply(project: Project): Unit = project.run {
 
         tasks {
             "greet" {
-                group = "My"
+                group = "sample"
                 description = "Prints a description of ${project.name}."
                 doLast {
-                    println("I'm ${project.name}")
+                    println("I'm ${project.name}.")
                 }
             }
         }
