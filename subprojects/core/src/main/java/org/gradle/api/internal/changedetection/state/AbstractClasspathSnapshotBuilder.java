e3328177fb3a856d5ef90d6d6756c54544cb378b
Bo Zhang <bo@gradle.com>
2018-04-19 23:46:09 +0200

    Remove stacktrace print

--- subprojects/core/src/main/java/org/gradle/api/internal/changedetection/state/AbstractClasspathSnapshotBuilder.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/changedetection/state/AbstractClasspathSnapshotBuilder.java
@@ -19,7 +19,6 @@ package org.gradle.api.internal.changedetection.state;
 import org.gradle.api.GradleException;
 import org.gradle.api.UncheckedIOException;
 import org.gradle.api.internal.cache.StringInterner;
-import org.gradle.api.logging.Logging;
 import org.gradle.caching.internal.BuildCacheHasher;
 import org.gradle.caching.internal.DefaultBuildCacheHasher;
 import org.gradle.internal.FileUtils;
@@ -113,7 +112,6 @@ public abstract class AbstractClasspathSnapshotBuilder implements VisitingFileCo
                 throw new UncheckedIOException("Error snapshotting jar [" + jarFile.getName() + "]", e);
             } catch (Exception e) {
                 // Other Exceptions can be thrown by invalid zips, too. See https://github.com/gradle/gradle/issues/1581.
-                Logging.getLogger(getClass()).error(jarFile.getPath(), e);
                 return hashMalformedZip(jarFile);
             }
         }
