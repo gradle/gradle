92916e9a3858ff2e1d4d624fe08b944445677603
Paul Merlin <paul@gradle.com>
2018-04-20 12:53:29 +0200

    Remove usages of TestKit internals
    Gradle JVM arguments can be set using a gradle.properties file

--- samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/samples.kt
+++ samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/samples.kt
@@ -3,6 +3,7 @@ package org.gradle.kotlin.dsl.samples
 import org.gradle.kotlin.dsl.fixtures.rootProjectDir
 
 import java.io.File
+import java.util.Properties
 
 
 internal
@@ -11,8 +12,27 @@ val samplesRootDir = File(rootProjectDir, "samples")
 
 internal
 fun copySampleProject(from: File, to: File) {
-    from.copyRecursively(to)
-    listOf(".gradle", "build").map { File(to, it) }.filter { it.exists() }.forEach {
-        it.deleteRecursively()
+    withMergedGradleProperties(to.resolve("gradle.properties")) {
+        from.copyRecursively(to)
+        listOf(".gradle", "build").map { File(to, it) }.filter { it.exists() }.forEach {
+            it.deleteRecursively()
+        }
     }
 }
+
+
+private
+fun withMergedGradleProperties(gradlePropertiesFile: File, action: () -> Unit) =
+    loadPropertiesFrom(gradlePropertiesFile).also { baseProperties ->
+        gradlePropertiesFile.delete()
+        action()
+        loadPropertiesFrom(gradlePropertiesFile).also { sampleProperties ->
+            baseProperties.putAll(sampleProperties)
+            gradlePropertiesFile.outputStream().use { baseProperties.store(it, null) }
+        }
+    }
+
+
+private
+fun loadPropertiesFrom(file: File) =
+    file.takeIf { it.isFile }?.inputStream()?.use { Properties().apply { load(it) } } ?: Properties()
