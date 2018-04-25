7865e827ee1586dc631811e8ba36700ba8c83a7a
Stefan Oehme <stefan@gradle.com>
2018-04-25 12:10:56 +0200

    Remove model rules from maven plugin publishing

--- subprojects/plugin-development/src/main/java/org/gradle/plugin/devel/plugins/JavaGradlePluginPlugin.java
+++ subprojects/plugin-development/src/main/java/org/gradle/plugin/devel/plugins/JavaGradlePluginPlugin.java
@@ -192,7 +192,7 @@ public class JavaGradlePluginPlugin implements Plugin<Project> {
         project.getPluginManager().withPlugin("maven-publish", new Action<AppliedPlugin>() {
             @Override
             public void execute(AppliedPlugin appliedPlugin) {
-                project.getPluginManager().apply(MavenPluginPublishingRules.class);
+                project.getPluginManager().apply(MavenPluginPublishPlugin.class);
             }
         });
         project.getPluginManager().withPlugin("ivy-publish", new Action<AppliedPlugin>() {
