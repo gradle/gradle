c8359ff93aaf3845630e167b5e0c6a06730a2a20
Lóránt Pintér <lorant@gradle.com>
2018-04-25 11:06:46 +0200

    Don't use ProjectLayout.files() in buildSrc, take 3

--- buildSrc/subprojects/performance/src/main/groovy/org/gradle/testing/performance/generator/tasks/NativeProjectWithDepsGeneratorTask.groovy
+++ buildSrc/subprojects/performance/src/main/groovy/org/gradle/testing/performance/generator/tasks/NativeProjectWithDepsGeneratorTask.groovy
@@ -81,7 +81,7 @@ class NativeProjectWithDepsGeneratorTask extends TemplateProjectGeneratorTask {
      * These files do not have any filtering/expanding done to them.
      */
     @InputFiles
-    ConfigurableFileCollection resources = project.layout.configurableFiles()
+    ConfigurableFileCollection resources = project.files()
 
     /**
      * @return names of all generated subprojects
