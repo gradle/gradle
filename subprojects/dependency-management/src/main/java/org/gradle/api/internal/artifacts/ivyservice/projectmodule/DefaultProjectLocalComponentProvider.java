9221bf06ac3eee93a08b9608fcd0d80aaa14ff7a
Adam Murdoch <adam@gradle.com>
2018-04-26 08:20:39 +1000

    Reuse the `ProjectComponentIdentifier` assigned to a project when constructing the metadata for that project, rather than creating a new identifier instance.

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/projectmodule/DefaultProjectLocalComponentProvider.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/projectmodule/DefaultProjectLocalComponentProvider.java
@@ -29,6 +29,7 @@ import org.gradle.api.internal.artifacts.ivyservice.moduleconverter.LocalCompone
 import org.gradle.api.internal.attributes.AttributesSchemaInternal;
 import org.gradle.api.internal.project.ProjectInternal;
 import org.gradle.api.internal.project.ProjectRegistry;
+import org.gradle.api.internal.project.ProjectState;
 import org.gradle.api.internal.project.ProjectStateRegistry;
 import org.gradle.internal.Factory;
 import org.gradle.internal.UncheckedException;
@@ -38,8 +39,11 @@ import org.gradle.internal.component.local.model.LocalComponentMetadata;
 import javax.annotation.Nullable;
 import java.util.concurrent.ExecutionException;
 
-import static org.gradle.api.internal.artifacts.DefaultProjectComponentIdentifier.newProjectId;
-
+/**
+ * Provides the metadata for a component consumed from the same build that produces it.
+ *
+ * Currently, the metadata for a component is different based on whether it is consumed from the producing build or from another build. This difference should go away.
+ */
 public class DefaultProjectLocalComponentProvider implements LocalComponentProvider {
     private final ProjectStateRegistry projectStateRegistry;
     private final ProjectRegistry<ProjectInternal> projectRegistry;
@@ -85,19 +89,20 @@ public class DefaultProjectLocalComponentProvider implements LocalComponentProvi
         if (project == null) {
             throw new IllegalArgumentException(projectIdentifier + " not found.");
         }
-        return projectStateRegistry.stateFor(project).withMutableState(new Factory<LocalComponentMetadata>() {
+        final ProjectState projectState = projectStateRegistry.stateFor(project);
+        return projectState.withMutableState(new Factory<LocalComponentMetadata>() {
             @Nullable
             @Override
             public LocalComponentMetadata create() {
-                return getLocalComponentMetadata(project);
+                return getLocalComponentMetadata(projectState, project);
             }
         });
     }
 
-    private LocalComponentMetadata getLocalComponentMetadata(ProjectInternal project) {
+    private LocalComponentMetadata getLocalComponentMetadata(ProjectState projectState, ProjectInternal project) {
         Module module = project.getModule();
         ModuleVersionIdentifier moduleVersionIdentifier = moduleIdentifierFactory.moduleWithVersion(module.getGroup(), module.getName(), module.getVersion());
-        ProjectComponentIdentifier componentIdentifier = newProjectId(project);
+        ProjectComponentIdentifier componentIdentifier = projectState.getComponentIdentifier();
         DefaultLocalComponentMetadata metaData = new DefaultLocalComponentMetadata(moduleVersionIdentifier, componentIdentifier, module.getStatus(), (AttributesSchemaInternal) project.getDependencies().getAttributesSchema());
         for (ConfigurationInternal configuration : project.getConfigurations().withType(ConfigurationInternal.class)) {
             metadataBuilder.addConfiguration(metaData, configuration);
