a240ae7ad94995172abe8d841cdf3dd4fde58812
Stefan Oehme <stefan@gradle.com>
2018-05-03 10:31:05 +0200

    Fix wrapper cross-version test

--- subprojects/wrapper/src/crossVersionTest/groovy/org/gradle/integtests/WrapperCrossVersionIntegrationTest.groovy
+++ subprojects/wrapper/src/crossVersionTest/groovy/org/gradle/integtests/WrapperCrossVersionIntegrationTest.groovy
@@ -89,6 +89,8 @@ class WrapperCrossVersionIntegrationTest extends CrossVersionIntegrationSpec {
         println "use wrapper from $wrapperVersion to build using $executionVersion"
 
         buildFile << """
+//for old Gradle versions that didn't have a built-in wrapper task
+tasks.maybeCreate('wrapper', Wrapper) 
 
 wrapper {
     gradleVersion = '$executionVersion.version.version'
