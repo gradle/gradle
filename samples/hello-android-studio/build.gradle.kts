2df017a6db17db51651a6232270c52e0429bcfce
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-05-12 12:37:09 -0300

    Convert Android Studio sample build scripts to Kotlin

--- samples/hello-android-studio/build.gradle.kts
+++ samples/hello-android-studio/build.gradle.kts
@@ -1,17 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.2.41'
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.1.2'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.1.2")
+        classpath(kotlin("gradle-plugin", version = "1.2.41"))
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -22,6 +21,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
