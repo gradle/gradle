81a8283cecd4102de441ccb9527a07e951a8c166
Daniel Lacasse <daniel@gradle.com>
2018-05-25 20:34:22 +0200

    Interfaces for C++ model using tooling APIs

---
+++ subprojects/tooling-api/src/main/java/org/gradle/tooling/model/cpp/CompilationDetails.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.tooling.model.cpp;
+
+import org.gradle.api.Incubating;
+import org.gradle.tooling.model.SourceDirectory;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Represents the compilation details for a binary.
+ *
+ * @since 4.9
+ */
+@Incubating
+public interface CompilationDetails {
+    /**
+     * All framework search paths.
+     */
+    List<? extends SourceDirectory> getFrameworkSearchPaths();
+
+    /**
+     * All compilation unit used for this binary.
+     */
+    List<File> getSources();
+
+    /**
+     * All macro directives.
+     */
+    List<MacroDirective> getMacros();
+
+    /**
+     * All system search paths.
+     */
+    List<? extends SourceDirectory> getSystemHeaderSearchPaths();
+
+    /**
+     * All user search paths.
+     */
+    List<? extends SourceDirectory> getUserHeaderSearchPaths();
+
+    /**
+     * Returns additional tool arguments.
+     */
+    List<String> getAdditionalArgs();
+}
