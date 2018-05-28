318fa8f9928dc3793fcaec64f9ed9fc6a63c1583
Tom Dunstan <tom@gradle.com>
2018-05-28 11:09:23 +0930

    Fix checkstyle

--- subprojects/core/src/main/java/org/gradle/api/internal/project/taskfactory/InternalTaskInstanceId.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/taskfactory/InternalTaskInstanceId.java
@@ -38,8 +38,12 @@ public final class InternalTaskInstanceId {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         InternalTaskInstanceId that = (InternalTaskInstanceId) o;
 
