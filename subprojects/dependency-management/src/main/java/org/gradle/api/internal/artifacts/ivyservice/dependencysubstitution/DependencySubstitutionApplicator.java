f40e93e2fd94c61661e8795ce9bf9fe1fff331f1
Cedric Champeau <cedric@gradle.com>
2018-06-06 12:09:52 +0200

    Avoid using a caching substitution applicator when possible
    The caching dependency substitution applicator is expensive, as it
    needs to remember about all selectors in a hashmap. But often, there
    are no substitution rules whatsoever. In this case, we can avoid the
    creation of the cache and short-circuit the application.

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/dependencysubstitution/DependencySubstitutionApplicator.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/dependencysubstitution/DependencySubstitutionApplicator.java
@@ -24,6 +24,13 @@ import org.gradle.internal.component.model.DependencyMetadata;
  * is going to be different) or a failure.
  */
 public interface DependencySubstitutionApplicator {
+    DependencySubstitutionApplicator NO_OP = new DependencySubstitutionApplicator() {
+        @Override
+        public SubstitutionResult apply(DependencyMetadata dependency) {
+            return SubstitutionResult.of(new DefaultDependencySubstitution(dependency.getSelector(), dependency.getReason()));
+        }
+    };
+
     SubstitutionResult apply(DependencyMetadata dependency);
 
     class SubstitutionResult {
