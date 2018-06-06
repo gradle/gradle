40033fc9981c0aec0f4df50a85554c5a359bbd32
Paul Merlin <paul@gradle.com>
2018-06-06 11:49:08 +0200

    Assert on generated extensions for a sample project
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/build-plugins/src/test/kotlin/org/gradle/kotlin/dsl/build/plugins/KotlinDslJavaApiExtensionsPluginTest.kt
+++ subprojects/build-plugins/src/test/kotlin/org/gradle/kotlin/dsl/build/plugins/KotlinDslJavaApiExtensionsPluginTest.kt
@@ -16,19 +16,27 @@
 
 package org.gradle.kotlin.dsl.build.plugins
 
+import org.gradle.kotlin.dsl.fixtures.containsMultiLineString
 import org.gradle.kotlin.dsl.resolver.SourceDistributionResolver
 import org.gradle.kotlin.dsl.support.unzipTo
 
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.hasItems
+import org.junit.Assert.assertThat
 import org.junit.Test
+
+import java.io.File
 
 
 class KotlinDslJavaApiExtensionsPluginTest : AbstractBuildPluginTest() {
 
     @Test
-    fun `generates extensions for isolated project`() {
+    fun `generates compilable extensions for isolated project`() {
 
         withSettings("""
-            rootProject.name = "core-api"
+            rootProject.name = "some-example"
         """)
 
         withBuildScript("""
@@ -43,31 +51,17 @@ class KotlinDslJavaApiExtensionsPluginTest : AbstractBuildPluginTest() {
                 jcenter()
             }
 
-            $gradlePublicApiObjectDeclaration
-
             kotlinDslApiExtensions {
-                create("main") {
-                    // includes.set(PublicApi.includes)
-                    excludes.set(PublicApi.excludes)
-                }
+                create("main")
             }
 
             tasks.withType<GenerateKotlinDslApiExtensions> {
                 isUseEmbeddedKotlinDslProvider.set(true)
             }
-
-            configurations.all {
-                resolutionStrategy.cacheChangingModulesFor(0, TimeUnit.SECONDS)
-            }
-
-            repositories {
-                maven(url="https://repo.gradle.org/gradle/libs-snapshots-local")
-                maven(url="https://repo.gradle.org/gradle/libs-releases-local")
-            }
         """)
 
-        withFile("src/main/java/com/example/Some.java", """
-            package com.example;
+        withFile("src/main/java/some/example/Some.java", """
+            package some.example;
 
             public interface Some {
 
@@ -82,44 +76,83 @@ class KotlinDslJavaApiExtensionsPluginTest : AbstractBuildPluginTest() {
                 String groovyNamedArgumentsUnboundedMapTakingMethod(java.util.Map<?, ?> args);
                 String groovyNamedArgumentsStringMapTakingMethod(java.util.Map<String, ?> args);
                 String groovyNamedArgumentsStringObjectMapTakingMethod(java.util.Map<String, Object> args);
+
+                // TODO Add cases with more parameters and mixed class/groovy-maps
             }
         """)
 
-        println()
-        run("generateKotlinDslApiExtensions").apply {
-            println(output)
+        run("generateKotlinDslApiExtensions")
+
+        existing("build/java-parameter-names").walkTopDown().single { it.isFile }.let {
+            assertThat(it.name, equalTo("java-parameter-names.properties"))
+            val entries = """
+                some.example.Some.rawClassTakingMethod(java.lang.Class)=clazz
+                some.example.Some.typedClassTakingMethod(java.lang.Class)=clazz
+                some.example.Some.boundedClassTakingMethod(java.lang.Class)=clazz
+                some.example.Some.parameterizedClassTakingMethod(java.lang.Class)=clazz
+                some.example.Some.boundedParameterizedClassTakingMethod(java.lang.Class)=clazz
+                some.example.Some.groovyNamedArgumentsRawMapTakingMethod(java.util.Map)=args
+                some.example.Some.groovyNamedArgumentsUnboundedMapTakingMethod(java.util.Map)=args
+                some.example.Some.groovyNamedArgumentsStringMapTakingMethod(java.util.Map)=args
+                some.example.Some.groovyNamedArgumentsStringObjectMapTakingMethod(java.util.Map)=args
+            """.trimIndent().lines()
+            assertThat(it.readText(), allOf(entries.map { containsString(it) }))
         }
 
-        println("\n>> Java parameter names indices")
-        existing("build/java-parameter-names").walkTopDown().filter { it.isFile }.forEach {
-            println(it)
-            println(it.readText().prependIndent())
+        existing("build/generated-sources").walkTopDown().single { it.isFile }.let {
+            assertThat(it.name, equalTo("GeneratedSomeExampleMainKotlinDslApiExtensions.kt"))
+            val extensions = listOf(
+                """
+                fun some.example.Some.`rawClassTakingMethod`(`clazz`: kotlin.reflect.KClass<*>): Unit =
+                    `rawClassTakingMethod`(`clazz`.java)
+                """,
+                """
+                fun some.example.Some.`typedClassTakingMethod`(`clazz`: kotlin.reflect.KClass<some.example.Some>): Unit =
+                    `typedClassTakingMethod`(`clazz`.java)
+                """,
+                """
+                fun some.example.Some.`boundedClassTakingMethod`(`clazz`: kotlin.reflect.KClass<some.example.Some>): Unit =
+                    `boundedClassTakingMethod`(`clazz`.java)
+                """,
+                """
+                fun <T : Any> some.example.Some.`parameterizedClassTakingMethod`(`clazz`: kotlin.reflect.KClass<T>): Unit =
+                    `parameterizedClassTakingMethod`(`clazz`.java)
+                """,
+                """
+                fun <T : some.example.Some> some.example.Some.`boundedParameterizedClassTakingMethod`(`clazz`: kotlin.reflect.KClass<T>): Unit =
+                    `boundedParameterizedClassTakingMethod`(`clazz`.java)
+                """,
+                """
+                fun some.example.Some.`groovyNamedArgumentsRawMapTakingMethod`(vararg `args`: Pair<String, *>): String =
+                    `groovyNamedArgumentsRawMapTakingMethod`(mapOf(*`args`))
+                """,
+                """
+                fun some.example.Some.`groovyNamedArgumentsUnboundedMapTakingMethod`(vararg `args`: Pair<String, *>): String =
+                    `groovyNamedArgumentsUnboundedMapTakingMethod`(mapOf(*`args`))
+                """,
+                """
+                fun some.example.Some.`groovyNamedArgumentsStringMapTakingMethod`(vararg `args`: Pair<String, *>): String =
+                    `groovyNamedArgumentsStringMapTakingMethod`(mapOf(*`args`))
+                """,
+                """
+                fun some.example.Some.`groovyNamedArgumentsStringObjectMapTakingMethod`(vararg `args`: Pair<String, *>): String =
+                    `groovyNamedArgumentsStringObjectMapTakingMethod`(mapOf(*`args`))
+                """)
+            assertThat(it.readText(), allOf(extensions.map { containsMultiLineString(it) }))
         }
 
-        println("\n>> Generated sources")
-        existing("build/generated-sources").walkTopDown().filter { it.isFile }.forEach {
-            println(it)
-            println(it.readText().prependIndent())
-        }
+        run("assemble")
 
-        println()
-        run("assemble").apply {
-            println(output)
-        }
-
-        println("\n>> Classes")
-        existing("build/classes").walkTopDown().filter { it.isFile }.forEach { println(it) }
-
-        println("\n>> Libs")
-        existing("build/libs").walkTopDown().filter { it.isFile }.forEach { println(it) }
-
-        println("\n>> Jar content")
         existing("extract").let { extract ->
-            unzipTo(extract, existing("build/libs/core-api.jar"))
-            extract.walkTopDown().filter { it.isFile }.forEach { println(it) }
+            unzipTo(extract, existing("build/libs/some-example.jar"))
+            assertThat(
+                extract.walkTopDown().filter { it.isFile }.map { it.relativeTo(extract).path }.toList(),
+                hasItems(*listOf(
+                    "some/example/Some.class",
+                    "org/gradle/kotlin/dsl/GeneratedSomeExampleMainKotlinDslApiExtensions.kt",
+                    "org/gradle/kotlin/dsl/GeneratedSomeExampleMainKotlinDslApiExtensionsKt.class")
+                    .map { it.replace('/', File.separatorChar) }.toTypedArray()))
         }
-
-        // TODO add assertions
     }
 
     @Test
