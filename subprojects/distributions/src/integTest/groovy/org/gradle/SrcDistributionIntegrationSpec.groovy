06081242f8f976c12bcd28282ea7af6c053f67d5
Bo Zhang <bo@gradle.com>
2018-06-07 19:46:31 +0800

    Add -u to bin test

--- subprojects/distributions/src/integTest/groovy/org/gradle/SrcDistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/SrcDistributionIntegrationSpec.groovy
@@ -47,6 +47,7 @@ class SrcDistributionIntegrationSpec extends DistributionIntegrationSpec {
             inDirectory(contentsDir)
             usingExecutable('gradlew')
             withTasks('binZip')
+            withArgument('-u')
             withWarningMode(null)
         }.run()
 
