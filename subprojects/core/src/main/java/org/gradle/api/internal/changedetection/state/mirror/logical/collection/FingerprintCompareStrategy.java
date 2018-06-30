4112c5abece16b63c8f637fc7d5028c40c3d092a
Stefan Wolf <wolf@gradle.com>
2018-06-30 23:13:42 +0200

    Use trivial compares strategy

--- subprojects/core/src/main/java/org/gradle/api/internal/changedetection/state/mirror/logical/collection/FingerprintCompareStrategy.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/changedetection/state/mirror/logical/collection/FingerprintCompareStrategy.java
@@ -16,12 +16,22 @@
 
 package org.gradle.api.internal.changedetection.state.mirror.logical.collection;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Iterators;
+import org.gradle.api.internal.changedetection.rules.FileChange;
+import org.gradle.api.internal.changedetection.rules.TaskStateChange;
 import org.gradle.api.internal.changedetection.rules.TaskStateChangeVisitor;
+import org.gradle.api.internal.changedetection.state.FileContentSnapshot;
 import org.gradle.api.internal.changedetection.state.NormalizedFileSnapshot;
 import org.gradle.caching.internal.BuildCacheHasher;
 
+import javax.annotation.Nullable;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
+
+import static com.google.common.collect.Iterators.emptyIterator;
+import static com.google.common.collect.Iterators.singletonIterator;
 
 public enum FingerprintCompareStrategy {
     ABSOLUTE(new AbsolutePathFingerprintCompareStrategy()),
@@ -41,9 +51,97 @@ public enum FingerprintCompareStrategy {
     }
 
     public boolean visitChangesSince(TaskStateChangeVisitor visitor, Map<String, NormalizedFileSnapshot> current, Map<String, NormalizedFileSnapshot> previous, String propertyTitle, boolean includeAdded) {
+        // Handle trivial cases with 0 or 1 elements in both current and previous
+        Iterator<TaskStateChange> trivialResult = compareTrivialSnapshots(current, previous, propertyTitle, includeAdded);
+        if (trivialResult != null) {
+            while (trivialResult.hasNext()) {
+                if (!visitor.visitChange(trivialResult.next())) {
+                    return false;
+                }
+            }
+            return true;
+        }
         return delegate.visitChangesSince(visitor, current, previous, propertyTitle, includeAdded);
     }
     public void appendToHasher(BuildCacheHasher hasher, Map<String, NormalizedFileSnapshot> snapshots) {
         delegate.appendToHasher(hasher, snapshots.values());
+    }
+
+    /**
+     * Compares snapshot collections if both current and previous states have at most one element.
+     *
+     * @param current the current state of the snapshot.
+     * @param previous the previous state of the snapshot.
+     * @param fileType the file type to use when creating the {@link FileChange}.
+     * @param includeAdded    whether or not to include added files.
+     * @return either a single change representing the change that happened,
+     * or {@code null} if there are more than one element in either {@code current}
+     * or {@code previous}.
+     */
+    @VisibleForTesting
+    @Nullable
+    static Iterator<TaskStateChange> compareTrivialSnapshots(Map<String, NormalizedFileSnapshot> current, Map<String, NormalizedFileSnapshot> previous, String fileType, boolean includeAdded) {
+        switch (current.size()) {
+            case 0:
+                switch (previous.size()) {
+                    case 0:
+                        return emptyIterator();
+                    case 1:
+                        Map.Entry<String, NormalizedFileSnapshot> entry = previous.entrySet().iterator().next();
+                        TaskStateChange change = FileChange.removed(entry.getKey(), fileType, entry.getValue().getSnapshot().getType());
+                        return singletonIterator(change);
+                    default:
+                        return null;
+                }
+
+            case 1:
+                switch (previous.size()) {
+                    case 0:
+                        if (includeAdded) {
+                            Map.Entry<String, NormalizedFileSnapshot> entry = current.entrySet().iterator().next();
+                            TaskStateChange change = FileChange.added(entry.getKey(), fileType, entry.getValue().getSnapshot().getType());
+                            return singletonIterator(change);
+                        } else {
+                            return emptyIterator();
+                        }
+                    case 1:
+                        Map.Entry<String, NormalizedFileSnapshot> previousEntry = previous.entrySet().iterator().next();
+                        Map.Entry<String, NormalizedFileSnapshot> currentEntry = current.entrySet().iterator().next();
+                        return compareTrivialSnapshotEntries(currentEntry, previousEntry, fileType, includeAdded);
+                    default:
+                        return null;
+                }
+
+            default:
+                return null;
+        }
+    }
+
+    private static Iterator<TaskStateChange> compareTrivialSnapshotEntries(Map.Entry<String, NormalizedFileSnapshot> currentEntry, Map.Entry<String, NormalizedFileSnapshot> previousEntry, String fileType, boolean includeAdded) {
+        NormalizedFileSnapshot normalizedPrevious = previousEntry.getValue();
+        NormalizedFileSnapshot normalizedCurrent = currentEntry.getValue();
+        if (normalizedCurrent.getNormalizedPath().equals(normalizedPrevious.getNormalizedPath())) {
+            FileContentSnapshot previousSnapshot = normalizedPrevious.getSnapshot();
+            FileContentSnapshot currentSnapshot = normalizedCurrent.getSnapshot();
+            if (!currentSnapshot.isContentUpToDate(previousSnapshot)) {
+                String path = currentEntry.getKey();
+                TaskStateChange change = FileChange.modified(path, fileType, previousSnapshot.getType(), currentSnapshot.getType());
+                return singletonIterator(change);
+            } else {
+                return emptyIterator();
+            }
+        } else {
+            if (includeAdded) {
+                String previousPath = previousEntry.getKey();
+                String currentPath = currentEntry.getKey();
+                TaskStateChange remove = FileChange.removed(previousPath, fileType, normalizedPrevious.getSnapshot().getType());
+                TaskStateChange add = FileChange.added(currentPath, fileType, normalizedCurrent.getSnapshot().getType());
+                return Iterators.forArray(remove, add);
+            } else {
+                String path = previousEntry.getKey();
+                TaskStateChange change = FileChange.removed(path, fileType, previousEntry.getValue().getSnapshot().getType());
+                return singletonIterator(change);
+            }
+        }
     }
 }
