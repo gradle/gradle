e2544ce4ec0df7d23f3b4f44eaaaffe573cd54c4
Bo Zhang <bo@gradle.com>
2018-07-09 16:46:06 +0800

    Monitor GradleContextualExecuter.cleanup() timeout
    This commit monitors GradleContextualExecuter.cleanup() timeout
    and prints all JVMs' stacktraces on current machine upon timeout.

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/GradleContextualExecuter.java
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/executer/GradleContextualExecuter.java
@@ -15,7 +15,11 @@
  */
 package org.gradle.integtests.fixtures.executer;
 
+import org.gradle.api.Action;
+import org.gradle.integtests.fixtures.timeout.IntegrationTestTimeoutInterceptor;
 import org.gradle.test.fixtures.file.TestDirectoryProvider;
+
+import static org.gradle.integtests.fixtures.timeout.IntegrationTestTimeout.DEFAULT_TIMEOUT_SECONDS;
 
 /**
  * Selects a different executer implementation based on the value of a system property.
@@ -120,10 +124,16 @@ public class GradleContextualExecuter extends AbstractDelegatingGradleExecuter {
 
     @Override
     public void cleanup() {
-        if (gradleExecuter != null) {
-            gradleExecuter.stop();
-        }
-        super.cleanup();
+        new IntegrationTestTimeoutInterceptor(DEFAULT_TIMEOUT_SECONDS).intercept(new Action<Void>() {
+            @Override
+            public void execute(Void ignored) {
+                if (gradleExecuter != null) {
+                    gradleExecuter.stop();
+                }
+                GradleContextualExecuter.super.cleanup();
+            }
+        });
+
     }
 
     @Override
