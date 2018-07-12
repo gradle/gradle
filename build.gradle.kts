c17c21c31682f39538d99c2ecf598df1285cf4ed
Cedric Champeau <cedric@gradle.com>
2018-07-12 14:02:21 +0200

    Only cache task if running on CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -274,6 +274,7 @@ tasks.register<PatchExternalModules>("patchExternalModules") {
     coreModules = coreRuntime
     modulesToPatch = this@Build_gradle.externalModules
     destination = patchedExternalModulesDir
+    outputs.doNotCacheIf("Not running on CI") { !System.getenv().containsKey("CI") }
 }
 
 evaluationDependsOn(":distributions")
