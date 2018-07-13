d687b3d3ebd0f3a318e83b3995b4fd7ac9624302
Rene Groeschke <rene@breskeby.com>
2018-07-13 16:00:59 +0200

    fix merge conflict after rebasing

--- subprojects/core/src/integTest/groovy/org/gradle/api/tasks/IncrementalBuildIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/tasks/IncrementalBuildIntegrationTest.groovy
@@ -1252,11 +1252,7 @@ task generate(type: TransformerTask) {
 
         then:
         skippedTasks.contains(':myTask')
-<<<<<<< HEAD
-        output.contains('The ability to add non-directory-based file trees as declared outputs has been deprecated and is scheduled to be removed in Gradle 5.0')
-=======
-        output.contains('Adding file trees which are not directory trees as output files has been deprecated. This is scheduled to be removed in Gradle 5.0')
->>>>>>> rework deprecation handling/messages to support more a richer model
+        output.contains('The ability to add non-directory-based file trees as declared outputs has been deprecated. This is scheduled to be removed in Gradle 5.0')
     }
 
     def "task with no actions is skipped even if it has inputs"() {
