05397b145bf02f1dbb97caa29ffc0af1756bbcdf
Eric Wendelin <eric@gradle.com>
2018-07-16 16:40:14 -0700

    Remove unused output file name declarations

--- subprojects/docs/src/docs/userguide/relativfy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for adoc_file in *.adoc; do
-    echo "Processing $adoc_file..."
-    sed -e "s/<<#/<<${adoc_file}#/g" $adoc_file > "${adoc_file}.tmp"
-    mv "${adoc_file}.tmp" $adoc_file
-done
