f0594530b57607bd6c91d924701eb5dfcae06472
Bo Zhang <bo@gradle.com>
2018-07-26 09:26:15 +0800

    Upgrade asciidoctor plugin to 1.5.6
    asciidoctor plugin 1.5.3 did an ugly file name replacement for ruby files
    in requires, which caused some failures on Windows. This PR tries to upgrade
    that plugin to 1.5.6.

--- buildSrc/subprojects/build/build.gradle.kts
+++ buildSrc/subprojects/build/build.gradle.kts
@@ -3,5 +3,5 @@ dependencies {
     compile("com.google.guava:guava-jdk5:14.0.1")
     compile("org.asciidoctor:asciidoctorj-pdf:1.5.0-alpha.11")
     compile("org.asciidoctor:asciidoctorj:1.5.5")
-    compile("org.asciidoctor:asciidoctor-gradle-plugin:1.5.3")
+    compile("org.asciidoctor:asciidoctor-gradle-plugin:1.5.6")
 }
