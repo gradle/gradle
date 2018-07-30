d044d74061eb6a2e269371529797dd016a51137a
Paul Merlin <paul@gradle.com>
2018-07-30 17:16:22 +0200

    Bump :plugins version
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ allprojects {
     createOpenTestReportTasks()
 }
 
-val publishedPluginsVersion by extra { "0.19.5" }
-val futurePluginsVersion = "0.19.6"
+val publishedPluginsVersion by extra { "0.19.6" }
+val futurePluginsVersion = "0.19.7"
 project(":plugins") {
     group = "org.gradle.kotlin"
     version = futurePluginsVersion
