0633cd1f84686acc0fd4d20c663a2557a3a60704
Stefan Oehme <stefan@gradle.com>
2018-07-31 16:02:52 +0200

    Add test precondition for tests that rely on working dir modification

--- subprojects/internal-testing/src/main/groovy/org/gradle/util/TestPrecondition.groovy
+++ subprojects/internal-testing/src/main/groovy/org/gradle/util/TestPrecondition.groovy
@@ -47,7 +47,7 @@ enum TestPrecondition implements org.gradle.internal.Factory<Boolean> {
         !UNKNOWN_OS.fulfilled && JavaVersion.current() < JavaVersion.VERSION_1_9
     }),
     WORKING_DIR({
-        !UNKNOWN_OS.fulfilled
+        !UNKNOWN_OS.fulfilled && JavaVersion.current() < JavaVersion.VERSION_11
     }),
     PROCESS_ID({
         !UNKNOWN_OS.fulfilled
