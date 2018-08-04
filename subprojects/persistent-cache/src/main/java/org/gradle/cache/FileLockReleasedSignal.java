ce12136c57c011e017567590c5b141d101582eca
Marc Philipp <marc@gradle.com>
2018-08-04 21:19:44 +0200

    Document FileLockReleasedSignal

--- subprojects/persistent-cache/src/main/java/org/gradle/cache/FileLockReleasedSignal.java
+++ subprojects/persistent-cache/src/main/java/org/gradle/cache/FileLockReleasedSignal.java
@@ -16,6 +16,20 @@
 
 package org.gradle.cache;
 
+/**
+ * Signal that a file lock has been released.
+ *
+ * @see org.gradle.cache.internal.locklistener.FileLockContentionHandler
+ */
 public interface FileLockReleasedSignal {
+
+    /**
+     * Triggers this signal to notify the lock requesters that the file
+     * lock has been released.
+     *
+     * <p>Returns once the signal has been emitted but not necessarily
+     * received by the lock requesters.
+     */
     void trigger();
+
 }
