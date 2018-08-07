764b299c26505c72a3eb9f52449927cce1035020
Adam Murdoch <adam@gradle.com>
2018-08-07 10:35:10 +1000

    Revert using `-nostdinc` for Objective-C and Objective-C++ compilation, as the framework directories also need to be discovered and supplied to the compiler.

--- subprojects/platform-native/src/main/java/org/gradle/nativeplatform/toolchain/internal/gcc/GccCompilerArgsTransformer.java
+++ subprojects/platform-native/src/main/java/org/gradle/nativeplatform/toolchain/internal/gcc/GccCompilerArgsTransformer.java
@@ -56,7 +56,9 @@ abstract class GccCompilerArgsTransformer<T extends NativeCompileSpec> implement
     }
 
     protected void addIncludeArgs(T spec, List<String> args) {
-        args.add("-nostdinc");
+        if (isNoStandardIncludes()) {
+            args.add("-nostdinc");
+        }
 
         for (File file : spec.getIncludeRoots()) {
             args.add("-I");
@@ -77,6 +79,10 @@ abstract class GccCompilerArgsTransformer<T extends NativeCompileSpec> implement
 
     protected void addUserArgs(T spec, List<String> args) {
         args.addAll(spec.getAllArgs());
+    }
+
+    protected boolean isNoStandardIncludes() {
+        return true;
     }
 
     protected abstract String getLanguage();
