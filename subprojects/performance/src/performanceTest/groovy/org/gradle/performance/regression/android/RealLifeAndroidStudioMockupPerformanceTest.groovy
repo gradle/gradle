9863ef4ee3705a05132eabfab2bec560c91e831e
Stefan Wolf <wolf@gradle.com>
2018-08-08 13:29:41 +0200

    Rebaseline RealLifeAndroidStudioMockupPerformanceTest
    It seems like adding a listener for Artifact transforms caused a
    regression. Rebasing till we find a solution to the problem.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/android/RealLifeAndroidStudioMockupPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/android/RealLifeAndroidStudioMockupPerformanceTest.groovy
@@ -27,7 +27,7 @@ class RealLifeAndroidStudioMockupPerformanceTest extends AbstractAndroidStudioMo
 
         experiment(testProject) {
             minimumVersion = "4.3.1"
-            targetVersions = ["4.10-20180725235906+0000"]
+            targetVersions = ["4.11-20180807235914+0000"]
             action('org.gradle.performance.android.SyncAction') {
                 jvmArguments = ["-Xms4g", "-Xmx4g"]
             }
