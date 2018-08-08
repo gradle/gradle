bbf0aeb69fc41e4a98aa98b48a7f312d324f7e30
Daz DeBoer <daz@gradle.com>
2018-08-08 12:28:38 -0600

    Thread-safety for `Configuration.getExcludeRules()`
    A ConcurrentModificationException occurred in
    `DefaultLocalConfigurationMetadataBuilder.addExcludeRules`, when iterating
    over `DefaultConfiguration.getExcludeRules()`. I suspect that this was
    due to a race-condition populating the exclude rules set, which is done
    on demand. Synchronizing this population should fix this.
    
    Fixes gradle/gradle-private#1433

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfiguration.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfiguration.java
@@ -685,6 +685,14 @@ public class DefaultConfiguration extends AbstractFileCollection implements Conf
     }
 
     public Set<ExcludeRule> getExcludeRules() {
+        initExcludeRules();
+        return Collections.unmodifiableSet(parsedExcludeRules);
+    }
+
+    /**
+     * Synchronize read access to excludes. Mutation does not need to be thread-safe.
+     */
+    private synchronized void initExcludeRules() {
         if (parsedExcludeRules == null) {
             NotationParser<Object, ExcludeRule> parser = ExcludeRuleNotationConverter.parser();
             parsedExcludeRules = Sets.newLinkedHashSet();
@@ -692,7 +700,6 @@ public class DefaultConfiguration extends AbstractFileCollection implements Conf
                 parsedExcludeRules.add(parser.parseNotation(excludeRule));
             }
         }
-        return Collections.unmodifiableSet(parsedExcludeRules);
     }
 
     public void setExcludeRules(Set<ExcludeRule> excludeRules) {
