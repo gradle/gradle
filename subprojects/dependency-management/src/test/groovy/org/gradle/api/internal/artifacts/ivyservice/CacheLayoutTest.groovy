67621490bb25cf92aabcc8379a00adafc9055d3c
Daz DeBoer <daz@gradle.com>
2018-08-11 13:29:43 -0600

    Bump cache layout for changed interpretation of optional dependencies

--- subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheLayoutTest.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/CacheLayoutTest.groovy
@@ -54,10 +54,10 @@ class CacheLayoutTest extends Specification {
 
         then:
         cacheLayout.name == 'metadata'
-        cacheLayout.key == 'metadata-2.63'
-        cacheLayout.version == CacheVersion.parse("2.63")
-        cacheLayout.version.toString() == '2.63'
-        cacheLayout.getPath(new File('some/dir')) == new File('some/dir/metadata-2.63')
+        cacheLayout.key == 'metadata-2.64'
+        cacheLayout.version == CacheVersion.parse("2.64")
+        cacheLayout.version.toString() == '2.64'
+        cacheLayout.getPath(new File('some/dir')) == new File('some/dir/metadata-2.64')
         !cacheLayout.versionMapping.getVersionUsedBy(GradleVersion.version("1.9-rc-1")).present
         cacheLayout.versionMapping.getVersionUsedBy(GradleVersion.version("1.9-rc-2")).get() == CacheVersion.of(2, 1)
     }
