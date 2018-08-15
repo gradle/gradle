f027d6de68c133727adadce99c9bf0c5e27cf265
Arnout Engelen <arnouten@bzzt.net>
2018-08-15 14:20:51 +0200

    PlayRunAdapterV27X extends PlayRunAdapterV26X
    Signed-off-by: Arnout Engelen <arnout@bzzt.net>

--- subprojects/platform-play/src/main/java/org/gradle/play/internal/run/PlayRunAdapterV27X.java
+++ subprojects/platform-play/src/main/java/org/gradle/play/internal/run/PlayRunAdapterV27X.java
@@ -22,11 +22,5 @@ import org.gradle.scala.internal.reflect.ScalaReflectionUtil;
 
 import java.net.InetSocketAddress;
 
-public class PlayRunAdapterV27X extends PlayRunAdapterV24X {
-    @Override
-    public InetSocketAddress runDevHttpServer(ClassLoader classLoader, ClassLoader docsClassLoader, Object buildLink, Object buildDocHandler, int httpPort) throws ClassNotFoundException {
-        ScalaMethod runMethod = ScalaReflectionUtil.scalaMethod(classLoader, "play.core.server.DevServerStart", "mainDevHttpMode", getBuildLinkClass(classLoader), int.class, String.class);
-        Object reloadableServer = runMethod.invoke(buildLink, httpPort, "0.0.0.0");
-        return JavaReflectionUtil.method(reloadableServer, InetSocketAddress.class, "mainAddress").invoke(reloadableServer);
-    }
+public class PlayRunAdapterV27X extends PlayRunAdapterV26X {
 }
