0f7b9de6318e0a0dc19c6c73ed090b32df87f13c
Adam Murdoch <adam@gradle.com>
2018-08-17 08:10:12 -0700

    Fix merge issue.

--- subprojects/build-init/src/main/java/org/gradle/buildinit/plugins/internal/KotlinApplicationProjectInitDescriptor.java
+++ subprojects/build-init/src/main/java/org/gradle/buildinit/plugins/internal/KotlinApplicationProjectInitDescriptor.java
@@ -44,10 +44,10 @@ public class KotlinApplicationProjectInitDescriptor extends LanguageLibraryProje
             .fileComment("This generated file contains a sample Kotlin application project to get you started.")
             .plugin("Apply the Kotlin JVM plugin to add support for Kotlin on the JVM", "org.jetbrains.kotlin.jvm", kotlinVersion)
             .plugin("Apply the application to add support for building a CLI application", "application")
-            .compileDependency("Use the Kotlin standard library", "org.jetbrains.kotlin:kotlin-stdlib")
-            .compileDependency("Use the Kotlin JDK 8 standard library", "org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-            .testCompileDependency("Use the Kotlin test library", "org.jetbrains.kotlin:kotlin-test")
-            .testCompileDependency("Use the Kotlin JUnit integration", "org.jetbrains.kotlin:kotlin-test-junit")
+            .implementationDependency("Use the Kotlin standard library", "org.jetbrains.kotlin:kotlin-stdlib")
+            .implementationDependency("Use the Kotlin JDK 8 standard library", "org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+            .testImplementationDependency("Use the Kotlin test library", "org.jetbrains.kotlin:kotlin-test")
+            .testImplementationDependency("Use the Kotlin JUnit integration", "org.jetbrains.kotlin:kotlin-test-junit")
             .conventionPropertyAssignment(
                 "Define the main class for the application",
                 "application", "mainClassName", withPackage(settings, "AppKt"));
--- subprojects/build-init/src/main/java/org/gradle/buildinit/plugins/internal/KotlinLibraryProjectInitDescriptor.java
+++ subprojects/build-init/src/main/java/org/gradle/buildinit/plugins/internal/KotlinLibraryProjectInitDescriptor.java
@@ -43,10 +43,10 @@ public class KotlinLibraryProjectInitDescriptor extends LanguageLibraryProjectIn
         BuildScriptBuilder buildScriptBuilder = scriptBuilderFactory.script(settings.getDsl(), "build")
             .fileComment("This generated file contains a sample Kotlin library project to get you started.")
             .plugin("Apply the Kotlin JVM plugin to add support for Kotlin on the JVM", "org.jetbrains.kotlin.jvm", kotlinVersion)
-            .compileDependency("Use the Kotlin standard library", "org.jetbrains.kotlin:kotlin-stdlib")
-            .compileDependency("Use the Kotlin JDK 8 standard library", "org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-            .testCompileDependency("Use the Kotlin test library", "org.jetbrains.kotlin:kotlin-test")
-            .testCompileDependency("Use the Kotlin JUnit integration", "org.jetbrains.kotlin:kotlin-test-junit");
+            .implementationDependency("Use the Kotlin standard library", "org.jetbrains.kotlin:kotlin-stdlib")
+            .implementationDependency("Use the Kotlin JDK 8 standard library", "org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+            .testImplementationDependency("Use the Kotlin test library", "org.jetbrains.kotlin:kotlin-test")
+            .testImplementationDependency("Use the Kotlin JUnit integration", "org.jetbrains.kotlin:kotlin-test-junit");
 
         buildScriptBuilder.create().generate();
 
