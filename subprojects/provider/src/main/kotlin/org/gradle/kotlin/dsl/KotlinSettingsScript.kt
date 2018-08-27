af40743eac150e7be0c6576927faeb17207eb150
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-08-27 11:59:16 -0300

    Remove unnecessary `apply` method from `SettingsScriptApi`
    Resolves #1066

--- subprojects/provider/src/main/kotlin/org/gradle/kotlin/dsl/KotlinSettingsScript.kt
+++ subprojects/provider/src/main/kotlin/org/gradle/kotlin/dsl/KotlinSettingsScript.kt
@@ -89,14 +89,6 @@ abstract class KotlinSettingsScript(
     override val fileOperations: DefaultFileOperations
         get() = host.operations
 
-    /**
-     * Applies zero or more plugins or scripts.
-     *
-     * @param configuration the block to configure an {@link ObjectConfigurationAction} with before “executing” it
-     */
-    override fun apply(configuration: ObjectConfigurationAction.() -> Unit) =
-        host.applyObjectConfigurationAction(Action { it.configuration() })
-
     override fun apply(action: Action<in ObjectConfigurationAction>) =
         host.applyObjectConfigurationAction(action)
 }
@@ -420,14 +412,6 @@ abstract class SettingsScriptApi(settings: Settings) : Settings by settings {
     @Suppress("unused")
     open fun buildscript(@Suppress("unused_parameter") block: ScriptHandlerScope.() -> Unit): Unit =
         internalError()
-
-    /**
-     * Applies zero or more plugins or scripts.
-     *
-     * @param configuration the block to configure an {@link ObjectConfigurationAction} with before “executing” it
-     */
-    open fun apply(configuration: ObjectConfigurationAction.() -> Unit) =
-        settings.apply(configuration)
 }
 
 
