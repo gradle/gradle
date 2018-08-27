9f148ac627885cf58b748370e5bd0ab6c5e45ad2
James Justinic <13036935+jjustinic@users.noreply.github.com>
2018-08-26 23:37:57 -0500

    Include Kotlin DSL for taskWithNestedElements sample
    Signed-off-by: James Justinic <13036935+jjustinic@users.noreply.github.com>

---
+++ subprojects/docs/src/samples/userguide/ant/taskWithNestedElements/kotlin/build.gradle.kts
@@ -0,0 +1,12 @@
+tasks.create("zip") {
+    doLast {
+        ant.withGroovyBuilder {
+            "zip"("destfile" to "archive.zip") {
+                "fileset"("dir" to "src") {
+                    "include"("name" to "**.xml")
+                    "exclude"("name" to "**.java")
+                }
+            }
+        }
+    }
+}
