bb48c1f446ce9de909ca5940ab90cd494b565499
James Justinic <13036935+jjustinic@users.noreply.github.com>
2018-08-27 00:47:23 -0500

    Include Kotlin DSL for useExternalAntTask sample
    Signed-off-by: James Justinic <13036935+jjustinic@users.noreply.github.com>

---
+++ subprojects/docs/src/samples/userguide/ant/useExternalAntTask/kotlin/build.gradle.kts
@@ -0,0 +1,14 @@
+tasks.create("check") {
+    doLast {
+        ant.withGroovyBuilder {
+            "taskdef"("resource" to "checkstyletask.properties") {
+                "classpath" {
+                    "fileset"("dir" to "libs", "includes" to "*.jar")
+                }
+            }
+            "checkstyle"("config" to "checkstyle.xml") {
+                "fileset"("dir" to "src")
+            }
+        }
+    }
+}
