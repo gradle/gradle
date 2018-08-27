531675f465b98c2b0467be3a39792e2c2230abbf
jnizet <jb@ninja-squad.com>
2018-08-27 16:59:21 +0200

    improve the project-with-buildSrc exampe to show how to define extension properties and functions
    Signed-off-by: jnizet <jb@ninja-squad.com>

--- samples/project-with-buildSrc/build.gradle.kts
+++ samples/project-with-buildSrc/build.gradle.kts
@@ -1,2 +1,12 @@
 
 withHelloTask()
+
+tasks.register("printProfile") {
+    description = "Uses the extension property and the extension functions defined in buildSrc. Use with -Pprofile=prod."
+    group = "sample"
+
+    doLast {
+        println("The current profile is $profile")
+        logProfile()
+    }
+}
