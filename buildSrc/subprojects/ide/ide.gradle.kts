492b6361b46fafa00d3a849023df0914fd853256
Stefan Wolf <wolf@gradle.com>
2018-08-28 16:54:44 +0200

    Update to idea-ext 0.4.2
    Copyright is fixed there.

--- buildSrc/subprojects/ide/ide.gradle.kts
+++ buildSrc/subprojects/ide/ide.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation(project(":kotlinDsl"))
     // TODO remove dependency once docs has publications
     implementation(project(":docs"))
-    implementation("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:0.3")
+    implementation("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:0.4.2")
 }
 
 gradlePlugin {
