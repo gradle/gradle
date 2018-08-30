63f491b4748290c48c65643a9c7da912ee3a8851
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-08-29 23:55:45 -0300

    Remove redundant `.getting`

--- buildSrc/subprojects/integration-testing/src/main/kotlin/org/gradle/gradlebuild/test/fixtures/TestFixturesPlugin.kt
+++ buildSrc/subprojects/integration-testing/src/main/kotlin/org/gradle/gradlebuild/test/fixtures/TestFixturesPlugin.kt
@@ -83,10 +83,10 @@ open class TestFixturesPlugin : Plugin<Project> {
             "testRuntime" { extendsFrom(configurations["testFixturesUsageRuntime"]) }
         }
 
-        val outputDirs by configurations.getting
-        val testFixturesCompile by configurations.getting
-        val testFixturesRuntime by configurations.getting
-        val testFixturesUsageCompile by configurations.getting
+        val outputDirs by configurations
+        val testFixturesCompile by configurations
+        val testFixturesRuntime by configurations
+        val testFixturesUsageCompile by configurations
 
         val main by java.sourceSets
         val testFixtures by java.sourceSets.creating {
