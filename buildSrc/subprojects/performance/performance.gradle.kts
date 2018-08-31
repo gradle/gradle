874b1c16eb80476fd2a1e0533a3870727c94492b
Bo Zhang <bo@gradle.com>
2018-08-31 15:40:16 +0800

    Exclude lombok from runtime dependencies

--- buildSrc/subprojects/performance/performance.gradle.kts
+++ buildSrc/subprojects/performance/performance.gradle.kts
@@ -3,15 +3,13 @@ apply(plugin = "org.gradle.kotlin.kotlin-dsl")
 dependencies {
     api(project(":integrationTesting"))
     implementation(project(":build"))
-    implementation("org.openmbee.junit:junit-xml-parser:1.0.0")
     implementation("org.codehaus.groovy.modules.http-builder:http-builder:0.7.2") {
         // Xerces on the runtime classpath is breaking some of our doc tasks
         exclude(group = "xerces")
     }
-    if (JavaVersion.current().isJava9Compatible) {
-        // Java 9 throws ClassNotFoundException
-        implementation("javax.activation:activation:1.1.1")
-        // only high version of lombok supports Java 9
-        implementation("org.projectlombok:lombok:1.18.2")
+    implementation("org.openmbee.junit:junit-xml-parser:1.0.0") {
+        // don't need it at runtime
+        exclude(module = "lombok")
     }
+    implementation("javax.activation:activation:1.1.1")
 }
