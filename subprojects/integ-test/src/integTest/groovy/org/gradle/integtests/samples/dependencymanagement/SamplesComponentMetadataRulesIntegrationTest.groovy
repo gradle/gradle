685961e2e2e3abc36c1f327f7cf4059ea099fa0a
jnizet <jb@ninja-squad.com>
2018-09-01 13:40:24 +0200

    include Kotlin DSL sample for chapter customizing_dependency_resolution_behavior (metadataRule sample)
    Signed-off-by: jnizet <jb@ninja-squad.com>

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/dependencymanagement/SamplesComponentMetadataRulesIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/samples/dependencymanagement/SamplesComponentMetadataRulesIntegrationTest.groovy
@@ -19,26 +19,36 @@ package org.gradle.integtests.samples.dependencymanagement
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.Sample
 import org.gradle.integtests.fixtures.UsesSample
+import org.gradle.util.Requires
 import org.junit.Rule
+import spock.lang.Unroll
 
+import static org.gradle.util.TestPrecondition.KOTLIN_SCRIPT
+
+@Requires(KOTLIN_SCRIPT)
 class SamplesComponentMetadataRulesIntegrationTest extends AbstractIntegrationSpec {
 
     @Rule
     Sample sample = new Sample(testDirectoryProvider)
 
+    @Unroll
     @UsesSample('userguide/dependencyManagement/customizingResolution/metadataRule')
-    def "can run custom status scheme sample" () {
-        executer.inDirectory(sample.dir)
+    def "can run custom status scheme sample with #dsl dsl" () {
+        executer.inDirectory(sample.dir.file(dsl))
 
         when:
         succeeds "listApi"
 
         then:
         output.contains("Resolved: api-2.0.jar")
+
+        where:
+        dsl << ['groovy', 'kotlin']
     }
 
+    @Unroll
     @UsesSample('userguide/dependencyManagement/customizingResolution/metadataRule')
-    def "can run custom status scheme with module sample" () {
+    def "can run custom status scheme with module sample with #dsl dsl" () {
         executer.inDirectory(sample.dir)
 
         when:
@@ -46,10 +56,14 @@ class SamplesComponentMetadataRulesIntegrationTest extends AbstractIntegrationSp
 
         then:
         output.contains("Resolved: lib-1.9.jar")
+
+        where:
+        dsl << ['groovy', 'kotlin']
     }
 
+    @Unroll
     @UsesSample('userguide/dependencyManagement/customizingResolution/metadataRule')
-    def "can run ivy metadata rule" () {
+    def "can run ivy metadata rule with #dsl dsl" () {
         executer.inDirectory(sample.dir)
 
         when:
@@ -57,10 +71,14 @@ class SamplesComponentMetadataRulesIntegrationTest extends AbstractIntegrationSp
 
         then:
         output.contains("Resolved: lib-2.0.jar")
+
+        where:
+        dsl << ['groovy', 'kotlin']
     }
 
+    @Unroll
     @UsesSample('userguide/dependencyManagement/customizingResolution/metadataRule')
-    def "can run custom status scheme with parameterized class rule sample" () {
+    def "can run custom status scheme with parameterized class rule sample with #dsl dsl" () {
         executer.inDirectory(sample.dir)
 
         when:
@@ -68,5 +86,8 @@ class SamplesComponentMetadataRulesIntegrationTest extends AbstractIntegrationSp
 
         then:
         output.contains("Resolved: api-1.9.jar")
+
+        where:
+        dsl << ['groovy', 'kotlin']
     }
 }
