48f113593edecb3be168cb7569c4318a21c9a9a3
Adam Murdoch <adam@gradle.com>
2018-09-03 18:34:47 +1000

    Replace all usages of the property factory methods on `ProjectLayout` and `DefaultTask` with the factory methods on `ObjectFactory`.

--- subprojects/platform-native/src/main/java/org/gradle/nativeplatform/tasks/AbstractLinkTask.java
+++ subprojects/platform-native/src/main/java/org/gradle/nativeplatform/tasks/AbstractLinkTask.java
@@ -68,17 +68,17 @@ public abstract class AbstractLinkTask extends DefaultTask implements ObjectFile
     private final Property<NativeToolChain> toolChain;
 
     public AbstractLinkTask() {
-        ObjectFactory objectFactory = getProject().getObjects();
+        final ObjectFactory objectFactory = getProject().getObjects();
         this.libs = getProject().files();
         this.source = getProject().files();
-        this.linkedFile = newOutputFile();
-        this.destinationDirectory = newOutputDirectory();
+        this.linkedFile = objectFactory.fileProperty();
+        this.destinationDirectory = objectFactory.directoryProperty();
         destinationDirectory.set(linkedFile.map(new Transformer<Directory, RegularFile>() {
             @Override
             public Directory transform(RegularFile regularFile) {
                 // TODO: Get rid of destinationDirectory entirely and replace it with a
                 // collection of link outputs
-                DirectoryProperty dirProp = getProject().getLayout().directoryProperty();
+                DirectoryProperty dirProp = objectFactory.directoryProperty();
                 dirProp.set(regularFile.getAsFile().getParentFile());
                 return dirProp.get();
             }
