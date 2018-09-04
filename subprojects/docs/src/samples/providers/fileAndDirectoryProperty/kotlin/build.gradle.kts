126e8cfdc23779b7aad78d78d533552b3ebe21a5
Adam Murdoch <adam@gradle.com>
2018-09-05 06:47:00 +1000

    Update user guide content and samples for changes to property factory methods.

--- subprojects/docs/src/samples/providers/fileAndDirectoryProperty/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/providers/fileAndDirectoryProperty/kotlin/build.gradle.kts
@@ -1,31 +1,19 @@
-/*
- * Copyright 2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-open class FooExtension(project: Project) {
-    val someDirectory: DirectoryProperty = project.layout.directoryProperty()
-    val someFile: RegularFileProperty = project.layout.fileProperty()
-    val someFiles: ConfigurableFileCollection = project.layout.configurableFiles()
+open class FooExtension(objects: ObjectFactory, layout: ProjectLayout) {
+    // A directory
+    val someDirectory: DirectoryProperty = objects.directoryProperty()
+    //  A file
+    val someFile: RegularFileProperty = objects.fileProperty()
+    // A collection of files or directories
+    val someFiles: ConfigurableFileCollection = layout.configurableFiles()
 }
 
-project.extensions.create("foo", FooExtension::class, project)
+project.extensions.create("foo", FooExtension::class, project.objects, project.layout)
 
 configure<FooExtension> {
+    // Configure the locations
     someDirectory.set(project.layout.projectDirectory.dir("some-directory"))
     someFile.set(project.layout.buildDirectory.file("some-file"))
-    someFiles.from(project.layout.configurableFiles(someDirectory, someFile))
+    someFiles.from(someDirectory, someFile)
 }
 
 task("print") {
