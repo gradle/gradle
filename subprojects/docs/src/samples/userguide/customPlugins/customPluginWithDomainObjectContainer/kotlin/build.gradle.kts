24bb8e1dec802cda1bad4012c76830415de03097
jnizet <jb@ninja-squad.com>
2018-09-04 19:54:34 +0200

    include Kotlin DSL sample for chapter custom_plugins (customPluginWithDomainObjectContainer sample)
    Signed-off-by: jnizet <jb@ninja-squad.com>

---
+++ subprojects/docs/src/samples/userguide/customPlugins/customPluginWithDomainObjectContainer/kotlin/build.gradle.kts
@@ -0,0 +1,36 @@
+class Book(val name: String) {
+    var sourceFile: File? = null
+}
+
+class DocumentationPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        // Create a container of Book instances
+        val books = project.container<Book>()
+        books.all {
+            sourceFile = project.file("src/docs/$name")
+        }
+        // Add the container as an extension object
+        project.extensions.add("books", books)
+    }
+}
+
+apply<DocumentationPlugin>()
+
+// Configure the container
+val books: NamedDomainObjectContainer<Book> by extensions
+
+books {
+    create("quickStart") {
+        sourceFile = file("src/docs/quick-start")
+    }
+    create("userGuide")
+    create("developerGuide")
+}
+
+task("books") {
+    doLast {
+        books.forEach { book ->
+            println("${book.name} -> ${book.sourceFile}")
+        }
+    }
+}
