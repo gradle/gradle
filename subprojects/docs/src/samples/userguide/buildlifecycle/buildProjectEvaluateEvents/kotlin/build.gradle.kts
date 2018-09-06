83d858b66a916abc524b38865630141ad4d12ccb
JB Nizet <jb@ninja-squad.com>
2018-09-06 03:12:10 +0200

    include Kotlin DSL sample for chapter build_lifecycle (#6598)
    Signed-off-by: jnizet <jb@ninja-squad.com>

---
+++ subprojects/docs/src/samples/userguide/buildlifecycle/buildProjectEvaluateEvents/kotlin/build.gradle.kts
@@ -0,0 +1,11 @@
+// tag::evaluate-events[]
+gradle.afterProject {
+    if (state.failure != null) {
+        println("Evaluation of $project FAILED")
+    } else {
+        println("Evaluation of $project succeeded")
+    }
+}
+// end::evaluate-events[]
+
+task("test")
