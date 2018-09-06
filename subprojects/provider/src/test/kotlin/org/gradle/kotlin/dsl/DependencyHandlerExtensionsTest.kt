969f0949a5cc36a25c6bbf3971488ae71b70c886
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-09-06 16:02:56 -0300

    Remove unnecessary parentheses

--- subprojects/provider/src/test/kotlin/org/gradle/kotlin/dsl/DependencyHandlerExtensionsTest.kt
+++ subprojects/provider/src/test/kotlin/org/gradle/kotlin/dsl/DependencyHandlerExtensionsTest.kt
@@ -235,7 +235,7 @@ class DependencyHandlerExtensionsTest {
             on { add(any(), any()) }.thenReturn(null)
         }
 
-        val configuration = mock<Configuration>() {
+        val configuration = mock<Configuration> {
             on { name } doReturn "configuration"
         }
 
