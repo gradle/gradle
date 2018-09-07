aa281a9e473cb639a33eb062153f2e7036f91a09
Cliffred van Velzen <cliffred@cliffred.nl>
2018-09-07 11:29:20 +0200

    Handle logging of null value correctly
    This fixes a bug where calling logger with any level with null as argument would result in stopping the daemon. Fixed by calling readNullableString.
    
    Fixes #6661
    
    Signed-off-by: Cliffred van Velzen <cliffred@cliffred.nl>

--- subprojects/logging/src/main/java/org/gradle/internal/logging/serializer/LogEventSerializer.java
+++ subprojects/logging/src/main/java/org/gradle/internal/logging/serializer/LogEventSerializer.java
@@ -37,7 +37,7 @@ public class LogEventSerializer implements Serializer<LogEvent> {
         encoder.writeLong(event.getTimestamp());
         encoder.writeString(event.getCategory());
         logLevelSerializer.write(encoder, event.getLogLevel());
-        encoder.writeString(event.getMessage());
+        encoder.writeNullableString(event.getMessage());
         throwableSerializer.write(encoder, event.getThrowable());
         if (event.getBuildOperationId() == null) {
             encoder.writeBoolean(false);
@@ -52,7 +52,7 @@ public class LogEventSerializer implements Serializer<LogEvent> {
         long timestamp = decoder.readLong();
         String category = decoder.readString();
         LogLevel logLevel = logLevelSerializer.read(decoder);
-        String message = decoder.readString();
+        String message = decoder.readNullableString();
         Throwable throwable = throwableSerializer.read(decoder);
         OperationIdentifier buildOperationId = decoder.readBoolean() ? new OperationIdentifier(decoder.readSmallLong()) : null;
         return new LogEvent(timestamp, category, logLevel, message, throwable, buildOperationId);
