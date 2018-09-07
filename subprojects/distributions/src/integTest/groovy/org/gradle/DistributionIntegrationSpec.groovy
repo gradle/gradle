e55482f8450ed62e3feee4c69bb6ba1dbdd11c07
Lóránt Pintér <lorant@gradle.com>
2018-09-08 01:40:44 +0200

    Fix lib jar count

--- subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -41,7 +41,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
     abstract String getDistributionLabel()
 
     int getLibJarsCount() {
-        198
+        206
     }
 
     def "no duplicate entries"() {
