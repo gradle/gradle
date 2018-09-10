d919e7687042b1363baee6536129775475be756e
Stefan Wolf <wolf@gradle.com>
2018-09-10 15:42:19 +0200

    Add missing newline to copyright

--- buildSrc/subprojects/ide/src/main/kotlin/org/gradle/gradlebuild/ide/IdePlugin.kt
+++ buildSrc/subprojects/ide/src/main/kotlin/org/gradle/gradlebuild/ide/IdePlugin.kt
@@ -267,6 +267,7 @@ open class IdePlugin : Plugin<Project> {
                 create("ASL2") {
                     notice =
                         """Copyright ${"$"}{today.year} the original author or authors.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
