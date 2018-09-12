e1d367912465f2e785b50772233b146de81c7881
Bo Zhang <bo@gradle.com>
2018-09-12 13:16:02 +0800

    Use gloabl init script for AbstractPropertyLanguageInterOpIntegrationTest

--- subprojects/model-core/src/integTest/groovy/org/gradle/api/provider/AbstractPropertyLanguageInterOpIntegrationTest.groovy
+++ subprojects/model-core/src/integTest/groovy/org/gradle/api/provider/AbstractPropertyLanguageInterOpIntegrationTest.groovy
@@ -53,7 +53,7 @@ abstract class AbstractPropertyLanguageInterOpIntegrationTest extends AbstractIn
     }
 
     def setup() {
-        executer.withPluginRepositoryMirror()
+        executer.withGlobalRepositoryMirrors()
         file("buildSrc/settings.gradle") << """
             include("plugin")
         """
