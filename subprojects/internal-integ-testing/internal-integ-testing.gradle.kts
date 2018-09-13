356828a305f222e3fef7d03004408dd7da515f32
Marc Philipp <marc@gradle.com>
2018-09-13 11:28:57 +0200

    Update to custom 1.1.3 release of littleproxy
    Latest officially released version is incompatible with Guava >= 20.

--- subprojects/internal-integ-testing/internal-integ-testing.gradle.kts
+++ subprojects/internal-integ-testing/internal-integ-testing.gradle.kts
@@ -24,7 +24,9 @@ dependencies {
     compile(project(":launcher"))
     compile(project(":native"))
     compile(testLibrary("jetty"))
-    compile("org.littleshoot:littleproxy:1.1.0-beta1")
+    compile("org.gradle.org.littleshoot:littleproxy:1.1.3") {
+        because("latest officially released version is incompatible with Guava >= 20")
+    }
     compile(library("gcs"))
     compile(library("commons_httpclient"))
     compile(library("joda"))
