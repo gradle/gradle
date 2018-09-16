f8e082c42a6a1fafbe49b2316d8c448b08c2d5bb
Marc Philipp <marc@gradle.com>
2018-09-16 19:44:14 +0200

    Update jsoup to 1.11.3
    jsoup no longer converts Windows line separators to Unix ones. Thus, a
    few test reporting test fixtures had to be adjusted.

--- buildSrc/subprojects/profiling/profiling.gradle.kts
+++ buildSrc/subprojects/profiling/profiling.gradle.kts
@@ -9,7 +9,7 @@ apply<PrecompiledScriptPlugins>()
 
 dependencies {
     implementation("me.champeau.gradle:jmh-gradle-plugin:0.4.7")
-    implementation("org.jsoup:jsoup:1.11.2")
+    implementation("org.jsoup:jsoup:1.11.3")
     implementation("com.gradle:build-scan-plugin:1.16")
     implementation(project(":configuration"))
     implementation(project(":kotlinDsl"))
