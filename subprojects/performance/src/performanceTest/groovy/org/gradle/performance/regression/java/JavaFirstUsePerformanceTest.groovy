5fa6ed2c0f22588390ab21154595bb4b95284118
Bo Zhang <bo@gradle.com>
2018-09-20 19:45:06 +0800

    Rebaseline JavaFirstUsePerformanceTest
    The increasing amount of Gradle API jar entries brought 1% performance loss on snapshotting. Accepted.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaFirstUsePerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaFirstUsePerformanceTest.groovy
@@ -41,7 +41,7 @@ class JavaFirstUsePerformanceTest extends AbstractCrossVersionPerformanceTest {
         runner.tasksToRun = ['tasks']
         runner.runs = runs
         runner.useDaemon = false
-        runner.targetVersions = ["5.0-20180914112317+0000"]
+        runner.targetVersions = ["5.0-20180920090625+0000"]
         runner.addBuildExperimentListener(new BuildExperimentListenerAdapter() {
             @Override
             void afterInvocation(BuildExperimentInvocationInfo invocationInfo, MeasuredOperation operation, BuildExperimentListener.MeasurementCallback measurementCallback) {
