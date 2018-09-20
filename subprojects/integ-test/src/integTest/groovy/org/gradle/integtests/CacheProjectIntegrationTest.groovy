d4ef7d22e0b9cb8576ad5cbe41c425d9d1919d6e
Lóránt Pintér <lorant@gradle.com>
2018-09-20 21:13:56 +0200

    Remove unused code
    ContentHasherFactory has no real use, so let's decrease complexity.

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/CacheProjectIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/CacheProjectIntegrationTest.groovy
@@ -18,7 +18,6 @@ package org.gradle.integtests
 
 import org.gradle.api.internal.artifacts.ivyservice.CacheLayout
 import org.gradle.integtests.fixtures.AbstractIntegrationTest
-import org.gradle.internal.hash.DefaultContentHasherFactory
 import org.gradle.internal.hash.DefaultFileHasher
 import org.gradle.internal.hash.DefaultStreamHasher
 import org.gradle.internal.hash.FileHasher
@@ -36,7 +35,7 @@ import static org.junit.Assert.assertEquals
 class CacheProjectIntegrationTest extends AbstractIntegrationTest {
     static final String TEST_FILE = "build/test.txt"
 
-    final FileHasher fileHasher = new DefaultFileHasher(new DefaultStreamHasher(new DefaultContentHasherFactory()))
+    final FileHasher fileHasher = new DefaultFileHasher(new DefaultStreamHasher())
 
     @Rule public final HttpServer server = new HttpServer()
 
