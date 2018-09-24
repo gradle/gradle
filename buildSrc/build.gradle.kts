23c291148d79517a2a0f668d16838f5b0013deeb
Marc Philipp <marc@gradle.com>
2018-09-24 08:09:20 +0200

    Upgrade to Spock 1.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -155,7 +155,7 @@ fun Project.applyGroovyProjectConventions() {
     dependencies {
         compile(localGroovy())
         val spockGroovyVersion = groovy.lang.GroovySystem.getVersion().substring(0, 3)
-        testCompile("org.spockframework:spock-core:1.2-RC3-groovy-${spockGroovyVersion}") {
+        testCompile("org.spockframework:spock-core:1.2-groovy-${spockGroovyVersion}") {
             exclude(group = "org.codehaus.groovy")
         }
         testCompile("net.bytebuddy:byte-buddy:1.8.21")
