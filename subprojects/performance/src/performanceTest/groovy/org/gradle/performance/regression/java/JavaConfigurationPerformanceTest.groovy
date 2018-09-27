b11cf80a45dca0c8018c6fcc1ec1bc2d6996ac70
Paul Merlin <paul@gradle.com>
2018-09-27 09:00:38 +0200

    Rebaseline performance tests for Kotlin DSL 1.0-rc-10
    Adding the kotlin-eap repo for Kotlin 1.3-RC affected performance.
    Those tests should be rebaselined again when 1.3 final is used and the
    extra repo configuration is removed.
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaConfigurationPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaConfigurationPerformanceTest.groovy
@@ -31,7 +31,7 @@ class JavaConfigurationPerformanceTest extends AbstractCrossVersionPerformanceTe
         runner.testProject = testProject
         runner.gradleOpts = ["-Xms${testProject.daemonMemory}", "-Xmx${testProject.daemonMemory}"]
         runner.tasksToRun = ['help']
-        runner.targetVersions = ["5.0-20180914112317+0000"]
+        runner.targetVersions = ["5.0-20180926235905+0000"]
 
         when:
         def result = runner.run()
