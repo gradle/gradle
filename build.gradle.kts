3673e18f24613a4fa96102df9c4924dbf87bc87e
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-09-27 18:57:57 -0300

    Bump plugins version

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ allprojects {
     version = "1.0-SNAPSHOT"
 }
 
-val publishedPluginsVersion by extra { "1.0-rc-9" }
-val futurePluginsVersion = "1.0-rc-10"
+val publishedPluginsVersion by extra { "1.0-rc-10" }
+val futurePluginsVersion = "1.0-rc-11"
 project(":plugins") {
     group = "org.gradle.kotlin"
     version = futurePluginsVersion
