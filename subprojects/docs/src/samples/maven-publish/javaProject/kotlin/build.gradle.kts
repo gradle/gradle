c882b9d07b3fc8996803cd2d53eb1206b1919c88
Paul Merlin <paul@gradle.com>
2018-10-15 10:34:28 +0200

    Add Kotlin DSL samples to Publishing Maven chapter
    Signed-off-by: Paul Merlin <paul@gradle.com>

---
+++ subprojects/docs/src/samples/maven-publish/javaProject/kotlin/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "org.gradle.sample"
+version = "1.0"
+
+dependencies {
+   compile("commons-collections:commons-collections:3.2.2")
+}
+
+repositories {
+    mavenCentral()
+}
+
+// tag::publish-custom-artifact[]
+task<Jar>("sourcesJar") {
+    classifier = "sources"
+    from(sourceSets.main.get().allJava)
+}
+
+task<Jar>("javadocJar") {
+    classifier = "javadoc"
+    from(tasks.javadoc.get().destinationDir)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
+}
+// end::publish-custom-artifact[]
+
+// tag::repo-url-from-variable[]
+// tag::repo-url-from-project-property[]
+publishing {
+    repositories {
+        maven {
+            val releasesRepoUrl = "$buildDir/repos/releases"
+            val snapshotsRepoUrl = "$buildDir/repos/snapshots"
+// end::repo-url-from-variable[]
+            url = uri(if (project.hasProperty("release")) releasesRepoUrl else snapshotsRepoUrl)
+// end::repo-url-from-project-property[]
+// tag::repo-url-from-variable[]
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+// tag::repo-url-from-project-property[]
+        }
+    }
+}
+// end::repo-url-from-project-property[]
+// end::repo-url-from-variable[]
---
+++ subprojects/docs/src/samples/maven-publish/multiple-publications/kotlin/build.gradle.kts
@@ -0,0 +1,78 @@
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    repositories {
+        mavenCentral()
+    }
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                url = uri("${rootProject.buildDir}/repo") // change to point to your repo, e.g. http://my.org/repo
+            }
+        }
+    }
+}
+
+project(":project1") {
+    dependencies {
+        "compile"("org.slf4j:slf4j-api:1.7.10")
+    }
+
+    // tag::customize-identity[]
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "org.gradle.sample"
+                artifactId = "project1-sample"
+                version = "1.1"
+
+                from(components["java"])
+            }
+        }
+    }
+    // end::customize-identity[]
+}
+
+project(":project2") {
+    dependencies {
+        "compile"("commons-collections:commons-collections:3.2.2")
+        "compile"(project(":project1"))
+    }
+
+    // tag::multiple-publications[]
+    group = "org.gradle.sample"
+    version = "2.3"
+
+    task<Jar>("apiJar") {
+        baseName = "project2-api"
+        from(sourceSets.main.get().output)
+        exclude("**/impl/**")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("impl") {
+                artifactId = "project2-impl"
+
+                from(components["java"])
+            }
+            create<MavenPublication>("api") {
+                artifactId = "project2-api"
+
+                artifact(tasks["apiJar"])
+            }
+        }
+    }
+    // end::multiple-publications[]
+}
+
+fun Project.publishing(action: PublishingExtension.() -> Unit) =
+    configure<PublishingExtension>(action)
+
+val Project.sourceSets
+    get() = the<SourceSetContainer>()
+
+val SourceSetContainer.main
+    get() = named("main")
