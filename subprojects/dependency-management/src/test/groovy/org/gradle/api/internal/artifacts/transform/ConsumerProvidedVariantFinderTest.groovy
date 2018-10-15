aa5cf18bcbf3ddadeca03935ed69a30db9c6a3e6
noproxy <toxzcp@gmail.com>
2018-10-15 12:10:37 +0800

    Fix extra attributes are ignored when selecting transforms chains from candidates
    When selecting transforms chains from candidates, additional attributes
    should not be ignored. If we just use the last transform's from to
    search chains, we may get a wrong chain which cannot produce the requested
     artifact at all.
    
    Fixes #7061
    
    Signed-off-by: noproxy <toxzcp@gmail.com>

--- subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/transform/ConsumerProvidedVariantFinderTest.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/transform/ConsumerProvidedVariantFinderTest.groovy
@@ -26,7 +26,10 @@ import org.gradle.api.internal.attributes.AttributeContainerInternal
 import org.gradle.api.internal.attributes.AttributesSchemaInternal
 import org.gradle.internal.component.model.AttributeMatcher
 import org.gradle.util.AttributeTestUtil
+import spock.lang.Issue
 import spock.lang.Specification
+
+import static org.spockframework.util.CollectionUtil.mapOf
 
 class ConsumerProvidedVariantFinderTest extends Specification {
     def matcher = Mock(AttributeMatcher)
@@ -46,6 +49,20 @@ class ConsumerProvidedVariantFinderTest extends Specification {
 
         List<File> transform(File input) {
             return transformer.transform(input)
+        }
+    }
+
+    /**
+     * Match all AttributeContainer that contains the same attributes.
+     *
+     * This method is for writing argument constraint in spock interaction. When search for
+     * chains, ConsumerProvidedVariantFinder may create a new instance of the AttributeContainer
+     * to call {@link AttributeMatcher#isMatching(AttributeContainerInternal, AttributeContainerInternal)}.
+     * So we cannot use the origin object instance to write method specification in spock interaction.
+     */
+    def attributesIs(AttributeContainer except, Map<Attribute<Object>, Object> vals) {
+        return except.keySet().size() == vals.size() && vals.every { entry ->
+            except.getAttribute(entry.key) == entry.value
         }
     }
 
@@ -175,10 +192,10 @@ class ConsumerProvidedVariantFinderTest extends Specification {
         1 * matcher.isMatching(c2, requested) >> false
         1 * matcher.isMatching(c5, requested) >> true
         1 * matcher.isMatching(source, c4) >> false
-        1 * matcher.isMatching(c5, c4) >> false
-        1 * matcher.isMatching(c2, c4) >> true
-        1 * matcher.isMatching(c3, c4) >> false
+        1 * matcher.isMatching(c3, { attributesIs(it, mapOf(a1, "4")) }) >> false
+        1 * matcher.isMatching(c2, { attributesIs(it, mapOf(a1, "4")) }) >> true
         1 * matcher.isMatching(source, c1) >> true
+        1 * matcher.isMatching(c5, { attributesIs(it, mapOf(a1, "4")) }) >> false
         0 * matcher._
 
         when:
@@ -249,9 +266,9 @@ class ConsumerProvidedVariantFinderTest extends Specification {
         1 * matcher.isMatching(c4, requested) >> false
         1 * matcher.isMatching(c5, requested) >> true
         1 * matcher.isMatching(source, c4) >> false
-        1 * matcher.isMatching(c4, c4) >> true
-        1 * matcher.isMatching(c3, c4) >> false
-        1 * matcher.isMatching(c5, c4) >> false
+        1 * matcher.isMatching(c4, { attributesIs(it, mapOf(a1, "4")) }) >> true
+        1 * matcher.isMatching(c3, { attributesIs(it, mapOf(a1, "4")) }) >> false
+        1 * matcher.isMatching(c5, { attributesIs(it, mapOf(a1, "4")) }) >> false
         1 * matcher.isMatching(source, c2) >> true
         1 * matcher.isMatching(source, c3) >> false
         0 * matcher._
@@ -264,6 +281,47 @@ class ConsumerProvidedVariantFinderTest extends Specification {
         transform1.transform(new File("a")) >> [new File("b"), new File("c")]
         transform2.transform(new File("b")) >> [new File("d")]
         transform2.transform(new File("c")) >> [new File("e")]
+    }
+
+    @Issue("gradle/gradle#7061")
+    def "selects chain of transforms that only all the attributes are satisfied"() {
+        def c1 = attributes().attribute(a1, "1").attribute(a2, 1).asImmutable()
+        def c4 = attributes().attribute(a1, "2").attribute(a2, 2).asImmutable()
+        def c5 = attributes().attribute(a1, "2").attribute(a2, 3).asImmutable()
+        def c6 = attributes().attribute(a1, "2").asImmutable()
+        def c7 = attributes().attribute(a1, "3").asImmutable()
+        def requested = attributes().attribute(a1, "3").attribute(a2, 3).asImmutable()
+        def source = c1
+        def reg1 = registration(c1, c4, {})
+        def reg2 = registration(c1, c5, {})
+        def reg3 = registration(c6, c7, {})
+
+        given:
+        transformRegistrations.transforms >> [reg1, reg2, reg3]
+
+        when:
+        def result = new ConsumerVariantMatchResult()
+        matchingCache.collectConsumerVariants(source, requested, result)
+
+        then:
+        result.matches.size() == 1
+
+        and:
+        _ * schema.matcher() >> matcher
+        _ * matcher.ignoreAdditionalProducerAttributes() >> matcher
+        _ * matcher.ignoreAdditionalConsumerAttributes() >> matcher
+        1 * matcher.isMatching(c4, requested) >> false
+        1 * matcher.isMatching(c5, requested) >> false
+        1 * matcher.isMatching(c7, requested) >> true
+        1 * matcher.isMatching(source, c6) >> false
+        1 * matcher.isMatching(c4, { attributesIs(it, mapOf(a1, "2", a2, 3)) }) >> false // "2" 3 ; c5
+        1 * matcher.isMatching(c5, { attributesIs(it, mapOf(a1, "2", a2, 3)) }) >> true
+        1 * matcher.isMatching(source, c1) >> true
+        1 * matcher.isMatching(c7, { attributesIs(it, mapOf(a1, "2", a2, 3)) }) >> false
+        0 * matcher._
+
+        expect:
+        result.matches.size() == 1
     }
 
     def "returns empty list when no transforms are available to produce requested variant"() {
