70c23a8d8a089db687c893f3612bb8eb28d081fa
Felipe Fernández <felipe@gradle.com>
2018-10-16 10:48:08 +0100

    Apply 2.0-rc3 build scan plugin from snapshot repo

--- buildSrc/subprojects/profiling/profiling.gradle.kts
+++ buildSrc/subprojects/profiling/profiling.gradle.kts
@@ -10,7 +10,7 @@ apply<PrecompiledScriptPlugins>()
 dependencies {
     implementation("me.champeau.gradle:jmh-gradle-plugin:0.4.7")
     implementation("org.jsoup:jsoup:1.11.3")
-    implementation("com.gradle:build-scan-plugin:2.0-rc-2-20181012132135-release")
+    implementation("com.gradle:build-scan-plugin:2.0-rc-3-20181016094325-release")
     implementation(project(":configuration"))
     implementation(project(":kotlinDsl"))
 }
