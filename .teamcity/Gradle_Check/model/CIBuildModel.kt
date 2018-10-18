5750039c38de4db24915a225f965658f7f0b5e19
Bo Zhang <bo@gradle.com>
2018-10-18 10:46:10 +0800

    Revert 24172d78f03fc5413adc4c46ee1e083d217ced41 and 76fff830089abd9dd7ac38b1630a480a0f5384bd
    Seems like TeamCity doesn't support empty string parameter well. This reverts previous two commits.

--- .teamcity/Gradle_Check/model/CIBuildModel.kt
+++ .teamcity/Gradle_Check/model/CIBuildModel.kt
@@ -277,8 +277,8 @@ enum class JvmVendor {
 }
 
 enum class PerformanceTestType(val taskId: String, val timeout : Int, val defaultBaselines: String = "", val extraParameters : String = "") {
-    test("PerformanceTest", 420),
-    experiment("PerformanceExperiment", 420),
+    test("PerformanceTest", 420, "defaults"),
+    experiment("PerformanceExperiment", 420, "defaults"),
     historical("FullPerformanceTest", 2280, "2.14.1,3.5.1,4.0,last", "--checks none");
 
     fun asId(model : CIBuildModel): String {
