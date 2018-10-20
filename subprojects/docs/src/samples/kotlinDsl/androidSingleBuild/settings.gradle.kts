07728b92ac6bda3e098198b8bd459fd370cb2484
Paul Merlin <paul@gradle.com>
2018-10-20 17:23:45 +0200

    Single project build plugin resolution strategy matches com.android namespace
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/docs/src/samples/kotlinDsl/androidSingleBuild/settings.gradle.kts
+++ subprojects/docs/src/samples/kotlinDsl/androidSingleBuild/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     }
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.id == "com.android.application") {
+            if(requested.id.namespace == "com.android") {
                 useModule("com.android.tools.build:gradle:${requested.version}")
             }
         }
