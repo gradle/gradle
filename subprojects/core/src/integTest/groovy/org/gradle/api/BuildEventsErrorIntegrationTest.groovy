7fe1cb4270c70a1d6d5c76e6ded5ca12c4476099
Adam Murdoch <adam@gradle.com>
2018-10-23 17:47:27 +1100

    Ensure that exceptions thrown by various build event actions and listeners are reported on the console and cause the build to fail.
    Previously, failures thrown by `BuildListener`, `TaskExecutionGraphListener` and `ProjectEvaluationListener` listeners and also by `Action` instances passed to `taskGraph.whenReady()` would be ignored.

--- subprojects/core/src/integTest/groovy/org/gradle/api/BuildEventsErrorIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/BuildEventsErrorIntegrationTest.groovy
@@ -17,14 +17,14 @@
 package org.gradle.api
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
-import spock.lang.Ignore
+import spock.lang.Unroll
 
-public class BuildEventsErrorIntegrationTest extends AbstractIntegrationSpec {
+class BuildEventsErrorIntegrationTest extends AbstractIntegrationSpec {
 
-    def "produces reasonable error message when taskGraph.whenReady action fails"() {
+    def "produces reasonable error message when taskGraph.whenReady closure fails"() {
         buildFile << """
     gradle.taskGraph.whenReady {
-        throw new RuntimeException('broken closure')
+        throw new RuntimeException('broken')
     }
     task a
 """
@@ -33,33 +33,54 @@ public class BuildEventsErrorIntegrationTest extends AbstractIntegrationSpec {
         fails()
 
         then:
-        failure.assertHasDescription("broken closure")
+        failure.assertHasDescription("broken")
                 .assertHasNoCause()
                 .assertHasFileName("Build file '$buildFile'")
                 .assertHasLineNumber(3);
     }
 
-    def "produces reasonable error message when task dependency closure throws exception"() {
+    def "produces reasonable error message when taskGraph.whenReady action fails"() {
         buildFile << """
+    def action = {
+            throw new RuntimeException('broken')
+    } as Action
+    gradle.taskGraph.whenReady(action) 
     task a
-    a.dependsOn {
-        throw new RuntimeException('broken')
-    }
 """
+
         when:
-        fails "a"
+        fails()
 
         then:
-        failure.assertHasDescription("Could not determine the dependencies of task ':a'.")
-                .assertHasCause('broken')
+        failure.assertHasDescription("broken")
+                .assertHasNoCause()
                 .assertHasFileName("Build file '$buildFile'")
-                .assertHasLineNumber(4)
+                .assertHasLineNumber(3);
+    }
+
+    def "produces reasonable error message when taskGraph listener fails"() {
+        buildFile << """
+    def listener = {
+            throw new RuntimeException('broken')
+    } as TaskExecutionGraphListener
+    gradle.taskGraph.addTaskExecutionGraphListener(listener) 
+    task a
+"""
+
+        when:
+        fails()
+
+        then:
+        failure.assertHasDescription("broken")
+                .assertHasNoCause()
+                .assertHasFileName("Build file '$buildFile'")
+                .assertHasLineNumber(3);
     }
 
     def "produces reasonable error when Gradle.allprojects action fails"() {
         def initScript = file("init.gradle") << """
 allprojects {
-    throw new RuntimeException("broken closure")
+    throw new RuntimeException("broken")
 }
 """
         when:
@@ -67,28 +88,99 @@ allprojects {
         fails "a"
 
         then:
-        failure.assertHasDescription("broken closure")
+        failure.assertHasDescription("broken")
                 .assertHasNoCause()
                 .assertHasFileName("Initialization script '$initScript'")
                 .assertHasLineNumber(3);
     }
 
-    @Ignore
-    def "produces reasonable error when Gradle.buildFinished action fails"() {
-        def initScript = file("init.gradle") << """
-rootProject { task a }
-buildFinished {
-    throw new RuntimeException("broken closure")
+    @Unroll
+    def "produces reasonable error when Gradle.#method closure fails"() {
+        settingsFile << """
+gradle.${method} {
+    throw new RuntimeException("broken")
 }
+gradle.rootProject { task a }
 """
         when:
-        executer.usingInitScript(initScript)
         fails "a"
 
         then:
-        failure.assertHasDescription("broken closure")
+        failure.assertHasDescription("broken")
                 .assertHasNoCause()
-                .assertHasFileName("Initialization script '$initScript'")
-                .assertHasLineNumber(3);
+        // TODO - include location information for buildFinished failure and get rid of the misleading 'build successful'
+        if (hasLocation) {
+            failure.assertHasFileName("Settings file '$settingsFile'")
+                    .assertHasLineNumber(3)
+        }
+
+        where:
+        method              | hasLocation
+        "settingsEvaluated" | true
+        "projectsLoaded"    | true
+        "projectsEvaluated" | true
+        "buildFinished"     | false
+    }
+
+    @Unroll
+    def "produces reasonable error when Gradle.#method action fails"() {
+        settingsFile << """
+def action = {
+    throw new RuntimeException("broken")
+} as Action
+gradle.${method}(action)
+gradle.rootProject { task a }
+"""
+        when:
+        fails "a"
+
+        then:
+        failure.assertHasDescription("broken")
+                .assertHasNoCause()
+        // TODO - include location information for buildFinished failure and get rid of the misleading 'build successful'
+        if (hasLocation) {
+            failure.assertHasFileName("Settings file '$settingsFile'")
+                    .assertHasLineNumber(3)
+        }
+
+        where:
+        method              | hasLocation
+        "settingsEvaluated" | true
+        "projectsLoaded"    | true
+        "projectsEvaluated" | true
+        "buildFinished"     | false
+    }
+
+    @Unroll
+    def "produces reasonable error when BuildListener.#method method fails"() {
+        settingsFile << """
+def listener = new BuildAdapter() {
+    @Override
+    void ${method}(${params}) {
+        throw new RuntimeException("broken")
+    }
+}
+
+gradle.addListener(listener)
+gradle.rootProject { task a }
+"""
+        when:
+        fails "a"
+
+        then:
+        failure.assertHasDescription("broken")
+                .assertHasNoCause()
+        // TODO - include location information for buildFinished failure and get rid of the misleading 'build successful'
+        if (hasLocation) {
+            failure.assertHasFileName("Settings file '$settingsFile'")
+                    .assertHasLineNumber(5)
+        }
+
+        where:
+        method              | params               | hasLocation
+        "settingsEvaluated" | "Settings settings"  | true
+        "projectsLoaded"    | "Gradle gradle"      | true
+        "projectsEvaluated" | "Gradle gradle"      | true
+        "buildFinished"     | "BuildResult result" | false
     }
 }
