f7858106ab8b4fc0447df0ebf7d88730439bc190
Adam Murdoch <adam@gradle.com>
2018-10-29 08:01:07 +1100

    Move some console functional test coverage to `logging` to live with the other console test coverage. Also add more coverage for the reporting of the build result at the end of the build (actionable tasks, and so on).

---
+++ subprojects/logging/src/integTest/groovy/org/gradle/internal/logging/console/taskgrouping/AbstractConsoleBuildResultFunctionalTest.groovy
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.internal.logging.console.taskgrouping
+
+import org.fusesource.jansi.Ansi
+import org.gradle.api.logging.LogLevel
+import org.gradle.integtests.fixtures.console.AbstractConsoleGroupedTaskFunctionalTest
+import org.gradle.integtests.fixtures.executer.LogContent
+import spock.lang.Unroll
+
+abstract class AbstractConsoleBuildResultFunctionalTest extends AbstractConsoleGroupedTaskFunctionalTest {
+    protected final String buildFailed = 'BUILD FAILED'
+    protected final String buildSuccess = 'BUILD SUCCESSFUL'
+    protected final StyledOutput buildFailedStyled = styled(buildFailed, Ansi.Color.RED, Ansi.Attribute.INTENSITY_BOLD)
+    protected final StyledOutput buildSuccessStyled = styled(buildSuccess, Ansi.Color.GREEN, Ansi.Attribute.INTENSITY_BOLD)
+
+    abstract String getFailureMessage()
+
+    abstract String getSuccessMessage()
+
+    def "successful build result is logged with appropriate styling"() {
+        given:
+        buildFile << """
+            task noActions
+            task notUpToDate {
+                doLast { }
+            }
+            task upToDate {
+                outputs.file "file"
+                doLast { }
+            }
+            task all { dependsOn noActions, notUpToDate, upToDate }
+        """
+
+        when:
+        succeeds('all')
+
+        then:
+        result.assertRawOutputContains(successMessage)
+        LogContent.of(result.output).removeAnsiChars().withNormalizedEol().matches """(?s).*
+BUILD SUCCESSFUL in \\d+s
+2 actionable tasks: 2 executed
+\$"""
+
+        when:
+        succeeds('all')
+
+        then:
+        result.assertRawOutputContains(successMessage)
+        LogContent.of(result.output).removeAnsiChars().withNormalizedEol().matches """(?s).*
+BUILD SUCCESSFUL in \\d+s
+2 actionable tasks: 1 executed, 1 up-to-date
+\$"""
+    }
+
+    def "successful build result is not logged with --quiet"() {
+        given:
+        buildFile << """
+            task all
+        """
+
+        when:
+        executer.withArgument("--quiet")
+        succeeds('all')
+
+        then:
+        result.output.empty
+        result.error.empty
+    }
+
+    @Unroll
+    def "reports build failure with appropriate styling at the end of the build with log level #level"() {
+        buildFile << """
+            task broken {
+                doLast {
+                    throw new RuntimeException("broken")
+                }
+            }
+        """
+
+        expect:
+        executer.withArguments("-Dorg.gradle.logging.level=${level}")
+        fails("broken")
+
+        and:
+        // Ensure the failure is a location that the fixtures can see
+        failure.assertHasDescription("Execution failed for task ':broken'")
+        failure.assertHasCause("broken")
+
+        // Check that the failure text appears either stdout or stderr
+        def outputWithFailure = errorsShouldAppearOnStdout() ? failure.output : failure.error
+        def outputWithoutFailure = errorsShouldAppearOnStdout() ? failure.error : failure.output
+        def outputWithFailureAndNoDebugging = LogContent.of(outputWithFailure).removeEmptyLines().removeAnsiChars().removeDebugPrefix().withNormalizedEol()
+
+        outputWithFailure.contains("Build failed with an exception.")
+        outputWithFailureAndNoDebugging.contains("""
+            * What went wrong:
+            Execution failed for task ':broken'.
+        """.stripIndent().trim())
+
+        !outputWithoutFailure.contains("Build failed with an exception.")
+        !outputWithoutFailure.contains("* What went wrong:")
+
+        outputWithFailure.contains("BUILD FAILED")
+        failure.assertHasRawErrorOutput(failureMessage)
+
+        where:
+        level << [LogLevel.DEBUG, LogLevel.INFO, LogLevel.LIFECYCLE, LogLevel.WARN, LogLevel.QUIET]
+    }
+}
