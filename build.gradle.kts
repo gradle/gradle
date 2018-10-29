c96b95d73f4ae8e46c5a9fc2d7443b7340dfa879
Paul Merlin <paul@gradle.com>
2018-10-29 11:16:38 +0100

    :arrow_up: :plugins
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ allprojects {
     version = "1.0-SNAPSHOT"
 }
 
-val publishedPluginsVersion by extra { "1.0-rc-14" }
-val futurePluginsVersion = "1.0-rc-15"
+val publishedPluginsVersion by extra { "1.0-rc-15" }
+val futurePluginsVersion = "1.0-rc-16"
 project(":plugins") {
     group = "org.gradle.kotlin"
     version = futurePluginsVersion
