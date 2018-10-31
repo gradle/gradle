a21645158db409e969a8c438dece81fa374d3f05
Marc Philipp <marc@gradle.com>
2018-10-31 15:14:11 +0100

    Use implementation and api configurations instead of compile

--- buildSrc/subprojects/build/build.gradle.kts
+++ buildSrc/subprojects/build/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies {
-    compile(project(":buildPlatform"))
-    compile("commons-lang:commons-lang:2.6")
-    compile("com.google.guava:guava:26.0-jre")
-    compile("org.asciidoctor:asciidoctorj-pdf:1.5.0-alpha.11")
-    compile("org.asciidoctor:asciidoctorj:1.5.5")
-    compile("org.asciidoctor:asciidoctor-gradle-plugin:1.5.3")
-    compile("com.github.javaparser:javaparser-core")
+    implementation(project(":buildPlatform"))
+    implementation("commons-lang:commons-lang:2.6")
+    api("com.google.guava:guava:26.0-jre")
+    implementation("org.asciidoctor:asciidoctorj-pdf:1.5.0-alpha.11")
+    implementation("org.asciidoctor:asciidoctorj:1.5.5")
+    implementation("org.asciidoctor:asciidoctor-gradle-plugin:1.5.3")
+    implementation("com.github.javaparser:javaparser-core")
 }
