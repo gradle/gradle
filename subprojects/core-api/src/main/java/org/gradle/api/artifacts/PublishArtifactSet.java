b6e2169da336d0c0a1d2da7f68e904fe950db925
Sterling Greene <sterling@gradle.com>
2018-11-01 16:50:29 -0400

    Add documentation for new method

--- subprojects/core-api/src/main/java/org/gradle/api/artifacts/PublishArtifactSet.java
+++ subprojects/core-api/src/main/java/org/gradle/api/artifacts/PublishArtifactSet.java
@@ -18,6 +18,7 @@ package org.gradle.api.artifacts;
 import org.gradle.api.Action;
 import org.gradle.api.Buildable;
 import org.gradle.api.DomainObjectSet;
+import org.gradle.api.Incubating;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.FileSystemLocation;
 import org.gradle.api.provider.Provider;
@@ -26,6 +27,15 @@ import org.gradle.api.provider.Provider;
  * A set of artifacts to be published.
  */
 public interface PublishArtifactSet extends DomainObjectSet<PublishArtifact>, Buildable {
+    /**
+     * Registers a file for publication.
+     *
+     * @param artifactFile provider to file to register
+     * @param configuration action to configure a publish artifact
+     * @return a Provider to the publish artifact
+     * @since 5.1
+     */
+    @Incubating
     Provider<PublishArtifact> register(Provider<? extends FileSystemLocation> artifactFile, Action<ConfigurablePublishArtifact> configuration);
 
     FileCollection getFiles();
--- subprojects/core/src/main/java/org/gradle/api/internal/artifacts/publish/DefaultPublishArtifact.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/artifacts/publish/DefaultPublishArtifact.java
@@ -113,7 +113,7 @@ public class DefaultPublishArtifact extends AbstractPublishArtifact implements C
         if (this == o) {
             return true;
         }
-        
+
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
