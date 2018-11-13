86092b4aa8658f4aeaa585daeb97def43e144527
Marc Philipp <marc@gradle.com>
2018-11-13 10:09:48 +0100

    Document BuildScopeListenerManagerAction

--- subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeListenerManagerAction.java
+++ subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeListenerManagerAction.java
@@ -19,5 +19,9 @@ package org.gradle.internal.service.scopes;
 import org.gradle.api.Action;
 import org.gradle.internal.event.ListenerManager;
 
+/**
+ * Action executed when building a GradleLauncher, used by subprojects to
+ * register build-scoped listeners.
+ */
 public interface BuildScopeListenerManagerAction extends Action<ListenerManager> {
 }
