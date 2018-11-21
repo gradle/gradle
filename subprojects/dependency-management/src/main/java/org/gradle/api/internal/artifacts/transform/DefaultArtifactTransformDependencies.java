2e02b74a9c1a1cdaa79e4e023a388a660108063e
Lóránt Pintér <lorant@gradle.com>
2018-11-21 10:51:27 +0100

    Resolve artifact transform dependencies more eagerly

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultArtifactTransformDependencies.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultArtifactTransformDependencies.java
@@ -16,68 +16,20 @@
 
 package org.gradle.api.internal.artifacts.transform;
 
-import com.google.common.collect.Sets;
-import org.gradle.api.artifacts.ResolvableDependencies;
-import org.gradle.api.artifacts.component.ComponentArtifactIdentifier;
-import org.gradle.api.artifacts.component.ComponentIdentifier;
-import org.gradle.api.artifacts.result.DependencyResult;
-import org.gradle.api.artifacts.result.ResolutionResult;
-import org.gradle.api.artifacts.result.ResolvedComponentResult;
-import org.gradle.api.artifacts.result.ResolvedDependencyResult;
 import org.gradle.api.artifacts.transform.ArtifactTransformDependencies;
-import org.gradle.api.attributes.Attribute;
-import org.gradle.api.internal.attributes.ImmutableAttributes;
 
 import java.io.File;
-import java.util.Set;
+import java.util.Collection;
 
-/**
- * Class to extract the artifacts of the dependencies of the given artifact from dependency resolution result.
- */
 public class DefaultArtifactTransformDependencies implements ArtifactTransformDependencies {
-    private final ComponentArtifactIdentifier artifactId;
-    private final ResolvableDependencies resolvableDependencies;
-    private final ImmutableAttributes attributes;
+    private final Collection<File> files;
 
-    public DefaultArtifactTransformDependencies(ComponentArtifactIdentifier artifactId, ResolvableDependencies resolvableDependencies, ImmutableAttributes attributes) {
-        this.artifactId = artifactId;
-        this.resolvableDependencies = resolvableDependencies;
-        this.attributes = attributes;
+    public DefaultArtifactTransformDependencies(Collection<File> files) {
+        this.files = files;
     }
 
     @Override
-    public Iterable<File> getFiles() {
-        ResolutionResult resolutionResult = resolvableDependencies.getResolutionResult();
-        Set<ComponentIdentifier> dependenciesIdentifiers = Sets.newHashSet();
-        for (ResolvedComponentResult component : resolutionResult.getAllComponents()) {
-            if (component.getId().equals(artifactId.getComponentIdentifier())) {
-                getDependenciesIdentifiers(dependenciesIdentifiers, component.getDependencies());
-            }
-        }
-        return resolvableDependencies.artifactView(conf -> {
-            conf.componentFilter(element -> {
-                return dependenciesIdentifiers.contains(element);
-            });
-            if (!attributes.isEmpty()) {
-                conf.attributes(container -> {
-                    for (Attribute attribute : attributes.keySet()) {
-                        container.attribute(attribute, attributes.findEntry(attribute).get());
-                    }
-                });
-            }
-        }).getArtifacts().getArtifactFiles().getFiles();
-    }
-
-    private void getDependenciesIdentifiers(Set<ComponentIdentifier> dependenciesIdentifiers, Set<? extends DependencyResult> dependencies) {
-        for (DependencyResult dependency : dependencies) {
-            if (dependency instanceof ResolvedDependencyResult) {
-                ResolvedDependencyResult resolvedDependency = (ResolvedDependencyResult) dependency;
-                ResolvedComponentResult selected = resolvedDependency.getSelected();
-                if (dependenciesIdentifiers.add(selected.getId())) {
-                    // Do not traverse if seen already
-                    getDependenciesIdentifiers(dependenciesIdentifiers, selected.getDependencies());
-                }
-            }
-        }
+    public Collection<File> getFiles() {
+        return files;
     }
 }
--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultArtifactTransformDependenciesProvider.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultArtifactTransformDependenciesProvider.java
@@ -16,10 +16,21 @@
 
 package org.gradle.api.internal.artifacts.transform;
 
+import com.google.common.collect.Sets;
 import org.gradle.api.artifacts.ResolvableDependencies;
 import org.gradle.api.artifacts.component.ComponentArtifactIdentifier;
+import org.gradle.api.artifacts.component.ComponentIdentifier;
+import org.gradle.api.artifacts.result.DependencyResult;
+import org.gradle.api.artifacts.result.ResolutionResult;
+import org.gradle.api.artifacts.result.ResolvedComponentResult;
+import org.gradle.api.artifacts.result.ResolvedDependencyResult;
 import org.gradle.api.artifacts.transform.ArtifactTransformDependencies;
+import org.gradle.api.attributes.Attribute;
+import org.gradle.api.attributes.AttributeContainer;
 import org.gradle.api.internal.attributes.ImmutableAttributes;
+
+import java.io.File;
+import java.util.Set;
 
 class DefaultArtifactTransformDependenciesProvider implements ArtifactTransformDependenciesProvider {
 
@@ -33,6 +44,42 @@ class DefaultArtifactTransformDependenciesProvider implements ArtifactTransformD
 
     @Override
     public ArtifactTransformDependencies forAttributes(ImmutableAttributes attributes) {
-        return new DefaultArtifactTransformDependencies(artifactId, resolvableDependencies, attributes);
+        ResolutionResult resolutionResult = resolvableDependencies.getResolutionResult();
+        Set<ComponentIdentifier> dependenciesIdentifiers = Sets.newHashSet();
+        for (ResolvedComponentResult component : resolutionResult.getAllComponents()) {
+            if (component.getId().equals(artifactId.getComponentIdentifier())) {
+                getDependenciesIdentifiers(dependenciesIdentifiers, component.getDependencies());
+            }
+        }
+        Set<File> files = resolvableDependencies.artifactView(conf -> {
+            conf.componentFilter(element -> {
+                return dependenciesIdentifiers.contains(element);
+            });
+            if (!attributes.isEmpty()) {
+                conf.attributes(container -> {
+                    for (Attribute<?> attribute : attributes.keySet()) {
+                        copyAttribute(attributes, container, attribute);
+                    }
+                });
+            }
+        }).getArtifacts().getArtifactFiles().getFiles();
+        return new DefaultArtifactTransformDependencies(files);
+    }
+
+    private static void getDependenciesIdentifiers(Set<ComponentIdentifier> dependenciesIdentifiers, Set<? extends DependencyResult> dependencies) {
+        for (DependencyResult dependency : dependencies) {
+            if (dependency instanceof ResolvedDependencyResult) {
+                ResolvedDependencyResult resolvedDependency = (ResolvedDependencyResult) dependency;
+                ResolvedComponentResult selected = resolvedDependency.getSelected();
+                if (dependenciesIdentifiers.add(selected.getId())) {
+                    // Do not traverse if seen already
+                    getDependenciesIdentifiers(dependenciesIdentifiers, selected.getDependencies());
+                }
+            }
+        }
+    }
+
+    private static <T> void copyAttribute(ImmutableAttributes attributes, AttributeContainer container, Attribute<T> attribute) {
+        container.attribute(attribute, attributes.findEntry(attribute).get());
     }
 }
