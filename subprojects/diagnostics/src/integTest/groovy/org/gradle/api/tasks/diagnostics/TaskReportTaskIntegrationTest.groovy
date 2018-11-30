0094af5dead515a22328d8d0938a9f5312ae7da8
Sterling Greene <sterling@gradle.com>
2018-11-29 23:27:31 -0500

    Polish --group switch on tasks

--- subprojects/diagnostics/src/integTest/groovy/org/gradle/api/tasks/diagnostics/TaskReportTaskIntegrationTest.groovy
+++ subprojects/diagnostics/src/integTest/groovy/org/gradle/api/tasks/diagnostics/TaskReportTaskIntegrationTest.groovy
@@ -174,20 +174,31 @@ b
         TASKS_DETAILED_REPORT_TASK | true
     }
 
-    def "renders only tasks in help group running [tasks --group=\"build setup\"]"() {
+    def "renders only tasks in help group running [tasks, --group=build setup"() {
+        buildFile << """
+            task mytask {
+                group = "custom"
+            }
+        """
         when:
         succeeds "tasks", "--group=build setup"
 
         then:
         output.contains("""
+------------------------------------------------------------
+Tasks runnable from root project
+------------------------------------------------------------
+
 Build Setup tasks
 -----------------
 init - Initializes a new Gradle build.
-wrapper - Generates Gradle wrapper files.""")
-        !output.contains("""
-Help tasks
-----------
+wrapper - Generates Gradle wrapper files.
+
+To see all tasks and more detail, run gradle tasks --all
+
+To see more detail about a task, run gradle help --task <task>
 """)
+        !output.contains("custom")
     }
 
     def "renders tasks in a multi-project build running [tasks]"() {
