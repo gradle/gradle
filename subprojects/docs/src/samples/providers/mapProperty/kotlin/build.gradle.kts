917ff30a1043e60c9665f6e78057b56eeb344cb3
Adam Murdoch <adam@gradle.com>
2018-11-30 12:44:06 +1100

    Add a little user guide content for `MapProperty`.

---
+++ subprojects/docs/src/samples/providers/mapProperty/kotlin/build.gradle.kts
@@ -0,0 +1,26 @@
+open class Generator: DefaultTask() {
+    @Input
+    val properties: MapProperty<String, Int> = project.objects.mapProperty(String::class, Int::class).empty()
+
+    @TaskAction
+    fun generate() {
+        properties.get().forEach { entry ->
+            logger.quiet("${entry.key} = ${entry.value}")
+        }
+    }
+}
+
+// Some values to be configured later
+var b = 0
+var c = 0
+
+val generate by tasks.creating(Generator::class)
+
+generate.properties.put("a", 1)
+// Values have not been configured yet
+generate.properties.put("b", providers.provider { b })
+generate.properties.putAll(providers.provider { mapOf("c" to c, "d" to c + 1) })
+
+// Configure the values. There is no need to reconfigure the task
+b = 2
+c = 3
