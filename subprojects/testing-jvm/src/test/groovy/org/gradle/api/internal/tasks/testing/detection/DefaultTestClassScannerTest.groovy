d137b6703f884b5c400cc767c4f544905786b53c
Sterling Greene <sterling@gradle.com>
2018-12-04 15:24:57 -0500

    Fix bad merge of test class change

--- subprojects/testing-jvm/src/test/groovy/org/gradle/api/internal/tasks/testing/detection/DefaultTestClassScannerTest.groovy
+++ subprojects/testing-jvm/src/test/groovy/org/gradle/api/internal/tasks/testing/detection/DefaultTestClassScannerTest.groovy
@@ -46,10 +46,6 @@ class DefaultTestClassScannerTest extends Specification {
         then:
         1 * detector.startDetection(processor)
         then:
-        1 * detector.processTestClass(new File("class1.class"))
-        then:
-        1 * detector.processTestClass(new File("class2.class"))
-        then:
         1 * files.visit(_) >> { args ->
             FileVisitor visitor = args[0]
             assert visitor
