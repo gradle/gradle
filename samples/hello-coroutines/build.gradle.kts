0ee875507706434387deea86c7887dae885ef13c
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-12-06 15:39:31 -0200

    Upgrade samples to Kotlin 1.3.11

--- samples/hello-coroutines/build.gradle.kts
+++ samples/hello-coroutines/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.Coroutines
 
 plugins {
     application
-    kotlin("jvm") version "1.3.10"
+    kotlin("jvm") version "1.3.11"
 }
 
 application {
--- samples/maven-publish/build.gradle.kts
+++ samples/maven-publish/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.bundling.Jar
 
 plugins {
-    kotlin("jvm") version "1.3.10"
+    kotlin("jvm") version "1.3.11"
     `maven-publish`
 }
 
