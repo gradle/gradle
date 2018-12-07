1065b0b175a5c43b3bc9251ec702adc7fff293a6
Paul Merlin <paul@gradle.com>
2018-12-07 18:09:59 +0100

    Introduce @RequiresNonFinalSubClasses
    Signed-off-by: Paul Merlin <paul@gradle.com>

---
+++ subprojects/base-services/src/main/java/org/gradle/api/RequiresNonFinalSubClasses.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Marks a type as requiring non-final subclasses.
+ *
+ * Types decorated by the Gradle runtime need to be non-final. This annotation allows
+ * linters, IDEs, DSLs, compiler plugins to validate them or make them non-final.
+ *
+ * @since 5.1
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Incubating
+public @interface RequiresNonFinalSubClasses {
+}
