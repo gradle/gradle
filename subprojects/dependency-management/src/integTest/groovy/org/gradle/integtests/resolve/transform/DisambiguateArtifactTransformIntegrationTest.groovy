2bf3882707e6daddae26785b5e2ae581f166696e
Louis Jacomet <louis@gradle.com>
2018-12-10 15:59:40 +0100

    Transformation disambiguation
    This commit introduces further disambiguation for artifact transforms.
    We now consider that candidates resulting in a matching set of
    attributes are equivalent and prefer the shortest chain in that case.
    
    The commit also fixes the limited logic that prevented disambiguation
    when there were more than 2 candidates.
    
    Fixes #6718

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/DisambiguateArtifactTransformIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/DisambiguateArtifactTransformIntegrationTest.groovy
@@ -43,7 +43,6 @@ public class MyClass {
 
         buildFile << """
 def artifactType = Attribute.of('artifactType', String)
-//attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.class, Usage.JAVA_API))
 
 allprojects {
     repositories {
@@ -122,6 +121,126 @@ class FileSizer extends ArtifactTransform {
         output.count("Transforming main to main.txt") == 1
         output.count("Transforming test-1.3.jar to test-1.3.jar.txt") == 1
         output.count("Transforming test-1.3.jar.txt to test-1.3.jar.txt.txt") == 1
+    }
+
+    def "disambiguates A -> B -> C and D -> C by selecting the later iff attributes match"() {
+        def m1 = mavenRepo.module("test", "test", "1.3").publish()
+        m1.artifactFile.text = "1234"
+
+        given:
+        settingsFile << """
+            rootProject.name = 'root'
+            include 'lib'
+            include 'app'
+        """
+
+        file('lib/src/main/java/test/MyClass.java') << """
+package test;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+"""
+
+        buildFile << """
+def artifactType = Attribute.of('artifactType', String)
+def extraAttribute = Attribute.of('extra', String)
+
+allprojects {
+    repositories {
+        maven { url "${mavenRepo.uri}" }
+    }
+}
+project(':lib') {
+    apply plugin: 'java-library'
+}
+
+project(':app') {
+    apply plugin: 'java'
+
+    dependencies {
+        implementation 'test:test:1.3'
+        implementation project(':lib')
+    }
+
+    dependencies {
+        registerTransform {
+            from.attribute(artifactType, 'java-classes-directory')
+            to.attribute(artifactType, 'final')
+            
+            if (project.hasProperty('extraAttribute')) {
+                from.attribute(extraAttribute, 'whatever')
+                to.attribute(extraAttribute, 'value1')
+            }
+            
+            artifactTransform(TestTransform)
+        }
+        registerTransform {
+            from.attribute(artifactType, 'jar')
+            to.attribute(artifactType, 'magic-jar')
+
+            if (project.hasProperty('extraAttribute')) {
+                from.attribute(extraAttribute, 'whatever')
+                to.attribute(extraAttribute, 'value2')
+            }
+            
+            artifactTransform(TestTransform)
+        }
+        registerTransform {
+            from.attribute(artifactType, 'magic-jar')
+            to.attribute(artifactType, 'final')
+            artifactTransform(TestTransform)
+        }
+    }
+
+    task resolve(type: Copy) {
+        def artifacts = configurations.compileClasspath.incoming.artifactView {
+            attributes { it.attribute(artifactType, 'final') }
+            if (project.hasProperty("lenient")) {
+                lenient(true)
+            }
+        }.artifacts
+        from artifacts.artifactFiles
+        into "\${buildDir}/libs"
+        doLast {
+            println "files: " + artifacts.collect { it.file.name }
+            println "ids: " + artifacts.collect { it.id }
+            println "components: " + artifacts.collect { it.id.componentIdentifier }
+            println "variants: " + artifacts.collect { it.variant.attributes }
+        }
+    }
+}
+
+class TestTransform extends ArtifactTransform {
+    List<File> transform(File input) {
+        assert outputDirectory.directory && outputDirectory.list().length == 0
+        def output = new File(outputDirectory, input.name + ".txt")
+        println "Transforming \${input.name} to \${output.name}"
+        output.text = String.valueOf(input.length())
+        return [output]
+    }
+}
+
+"""
+
+
+
+        when:
+        run "resolve"
+
+        then:
+        output.count("Transforming") == 3
+        output.count("Transforming main to main.txt") == 1
+        output.count("Transforming test-1.3.jar to test-1.3.jar.txt") == 1
+        output.count("Transforming test-1.3.jar.txt to test-1.3.jar.txt.txt") == 1
+
+        when:
+        fails 'resolve', '-PextraAttribute'
+
+        then:
+        failureCauseContains('Found multiple transforms')
     }
 
     def "transform with two attributes will not confuse"() {
