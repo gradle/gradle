594d43de6754b73b77910473302280ea32c5256d
Cedric Champeau <cedric@gradle.com>
2018-12-11 15:55:54 +0100

    Document the Java Platform plugin
    Fixes #7990

---
+++ subprojects/docs/src/samples/java-platform/recommender/kotlin/platform/build.gradle.kts
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    `java-platform`
+}
+
+// tag::define-platform[]
+dependencies {
+    constraints {
+        // Platform declares some versions of libraries used in subprojects
+        api("commons-httpclient:commons-httpclient:3.1")
+        api("org.apache.commons:commons-lang3:3.8.1")
+    }
+}
+// end::define-platform[]
