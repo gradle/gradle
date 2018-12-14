a552221d8493a8672629bb8deb841ff9f58f97e3
Paul Merlin <paul@gradle.com>
2018-12-14 12:40:38 +0100

    Import :kotlin-dsl-integ-tests
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl-integ-tests/kotlin-dsl-integ-tests.gradle.kts
+++ subprojects/kotlin-dsl-integ-tests/kotlin-dsl-integ-tests.gradle.kts
@@ -14,8 +14,70 @@
  * limitations under the License.
  */
 
+import build.withParallelTests
 import org.gradle.gradlebuild.unittestandcompile.ModuleType
+import plugins.futurePluginVersionsFile
+
+plugins {
+    `kotlin-library`
+}
 
 gradlebuildJava {
     moduleType = ModuleType.INTERNAL
 }
+
+dependencies {
+    testImplementation(project(":kotlinDslTestFixtures"))
+    testImplementation("com.squareup.okhttp3:mockwebserver:3.9.1")
+}
+
+val pluginBundles = listOf(
+    ":kotlinDslPlugins")
+
+pluginBundles.forEach {
+    evaluationDependsOn(it)
+}
+
+tasks {
+
+    val testEnvironment by registering {
+        dependsOn(":prepareIntegrationTestFixtures")
+        dependsOn(":customInstallation")
+        pluginBundles.forEach {
+            dependsOn(":$it:publishPluginsToTestRepository")
+        }
+    }
+
+    withType<Test>().configureEach {
+        dependsOn(testEnvironment)
+    }
+
+    val writeFuturePluginVersions by registering {
+
+        group = "build"
+        description = "Merges all future plugin bundle versions so they can all be tested at once"
+
+        val futurePluginVersionsTasks =
+            pluginBundles.map {
+                project(it).tasks["writeFuturePluginVersions"] as WriteProperties
+            }
+
+        dependsOn(futurePluginVersionsTasks)
+        inputs.files(futurePluginVersionsTasks.map { it.outputFile })
+        outputs.file(processTestResources.get().futurePluginVersionsFile)
+
+        doLast {
+            outputs.files.singleFile.bufferedWriter().use { writer ->
+                inputs.files.forEach { input ->
+                    writer.appendln(input.readText())
+                }
+            }
+        }
+    }
+
+    processTestResources {
+        dependsOn(writeFuturePluginVersions)
+    }
+}
+
+withParallelTests()
