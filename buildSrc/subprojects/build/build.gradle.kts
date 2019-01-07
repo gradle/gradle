5a10376e325c8e44b5b98ffbaa3ef66a5aa1d752
Bo Zhang <bo@gradle.com>
2019-01-07 10:31:02 +0800

    Add docs:distDocs to seed build

--- buildSrc/subprojects/build/build.gradle.kts
+++ buildSrc/subprojects/build/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     api("com.google.guava:guava:26.0-jre")
     api("org.asciidoctor:asciidoctor-gradle-plugin:1.5.9.2")
     implementation("org.asciidoctor:asciidoctorj:1.5.8.1")
-    implementation("org.jruby:jruby-complete:9.2.5.0")
     implementation(project(":buildPlatform"))
     implementation("commons-lang:commons-lang:2.6")
     implementation("org.asciidoctor:asciidoctorj-pdf:1.5.0-alpha.16")
