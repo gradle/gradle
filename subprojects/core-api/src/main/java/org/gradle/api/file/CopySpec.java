4e33b9b22d2fc81716936369762f5a5700233a35
Bo Zhang <bo@gradle.com>
2019-01-08 17:21:19 +0800

    Revert "Fix Javadoc tags with are not supported by HTML5"
    This reverts commit 32c8d1337a08c48e7f45b127c4c9416ae195f925.

--- subprojects/core-api/src/main/java/org/gradle/api/file/CopySpec.java
+++ subprojects/core-api/src/main/java/org/gradle/api/file/CopySpec.java
@@ -103,14 +103,14 @@ public interface CopySpec extends CopySourceSpec, CopyProcessingSpec, PatternFil
     /**
      * Tells if empty target directories will be included in the copy.
      *
-     * @return <code>true</code> if empty target directories will be included in the copy, <code>false</code> otherwise
+     * @return <tt>true</tt> if empty target directories will be included in the copy, <tt>false</tt> otherwise
      */
     boolean getIncludeEmptyDirs();
 
     /**
      * Controls if empty target directories should be included in the copy.
      *
-     * @param includeEmptyDirs <code>true</code> if empty target directories should be included in the copy, <code>false</code> otherwise
+     * @param includeEmptyDirs <tt>true</tt> if empty target directories should be included in the copy, <tt>false</tt> otherwise
      */
     void setIncludeEmptyDirs(boolean includeEmptyDirs);
 
