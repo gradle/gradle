9a7102384089c11089d9dff1d81343bbd015b625
Thad House <thadhouse1@gmail.com>
2019-01-08 17:09:57 -0800

    Property handle UNC paths for the wrapper path.
    File::getPath() does not have the correct behavior specifically for network paths, but has the same behavior for all other types
    Signed-off-by: Thad House <thadhouse1@gmail.com>

--- subprojects/wrapper/src/integTest/groovy/org/gradle/integtests/WrapperGenerationIntegrationTest.groovy
+++ subprojects/wrapper/src/integTest/groovy/org/gradle/integtests/WrapperGenerationIntegrationTest.groovy
@@ -71,7 +71,7 @@ class WrapperGenerationIntegrationTest extends AbstractIntegrationSpec {
         executer.inDirectory(file("second")).withTasks("wrapper").run()
 
         then: "the checksum should be constant (unless there are code changes)"
-        sha256(file("first/gradle/wrapper/gradle-wrapper.jar")).asHexString() == "76b12da7f4a7cdd025e5996811a2e49bf5df0fb62d72554ab555c0e434b63aae"
+        sha256(file("first/gradle/wrapper/gradle-wrapper.jar")).asHexString() == "963ca8f92fb943ea0b8e17232fa18862b94358bc2723ecb14a163880cdc3e505"
 
         and:
         file("first/gradle/wrapper/gradle-wrapper.jar").md5Hash == file("second/gradle/wrapper/gradle-wrapper.jar").md5Hash
