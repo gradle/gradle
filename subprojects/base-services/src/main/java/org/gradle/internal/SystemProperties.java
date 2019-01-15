af923b12895609da1bdf1b005385577228be0382
Artem Zinnatullin <ceo@artemzin.com>
2019-01-14 17:53:43 -0800

    Prevent concurrent access to SystemProperties
    Fixes #7842 and expected to transitively fix #1782.
    
    This change prohibits reads of system properties while
    withSystemProperty(factory) is busy, preventing observing mutated state with
    temporary value.
    
    Signed-off-by: Artem Zinnatullin <ceo@artemzin.com>

--- subprojects/base-services/src/main/java/org/gradle/internal/SystemProperties.java
+++ subprojects/base-services/src/main/java/org/gradle/internal/SystemProperties.java
@@ -73,35 +73,35 @@ public class SystemProperties {
     }
 
     @SuppressWarnings("unchecked")
-    public Map<String, String> asMap() {
+    public synchronized Map<String, String> asMap() {
         return (Map) System.getProperties();
     }
 
-    public String getLineSeparator() {
+    public synchronized String getLineSeparator() {
         return System.getProperty("line.separator");
     }
 
-    public String getJavaIoTmpDir() {
+    public synchronized String getJavaIoTmpDir() {
         return System.getProperty("java.io.tmpdir");
     }
 
-    public String getUserHome() {
+    public synchronized String getUserHome() {
         return System.getProperty("user.home");
     }
 
-    public String getUserName() {
+    public synchronized String getUserName() {
         return System.getProperty("user.name");
     }
 
-    public String getJavaVersion() {
+    public synchronized String getJavaVersion() {
         return System.getProperty("java.version");
     }
 
-    public File getCurrentDir() {
+    public synchronized File getCurrentDir() {
         return new File(System.getProperty("user.dir"));
     }
 
-    public File getJavaHomeDir() {
+    public synchronized File getJavaHomeDir() {
         return new File(System.getProperty("java.home"));
     }
 
