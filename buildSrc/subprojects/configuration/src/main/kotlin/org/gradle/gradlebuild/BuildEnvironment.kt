53b963a2591c776b7bc1bdd512cedac3855be6e3
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2019-01-24 00:54:41 -0200

    Upgrade Kotlin DSL to 1.1.2
    Signed-off-by: Rodrigo B. de Oliveira <rodrigo@gradle.com>

--- buildSrc/subprojects/configuration/src/main/kotlin/org/gradle/gradlebuild/BuildEnvironment.kt
+++ buildSrc/subprojects/configuration/src/main/kotlin/org/gradle/gradlebuild/BuildEnvironment.kt
@@ -8,7 +8,7 @@ object BuildEnvironment {
     val isCiServer = "CI" in System.getenv()
     val isTravis = "TRAVIS" in System.getenv()
     val isJenkins = "JENKINS_HOME" in System.getenv()
-    val gradleKotlinDslVersion = "1.1.1"
+    val gradleKotlinDslVersion = "1.1.2"
     val kotlinVersion = "1.3.11"
     val jvm = org.gradle.internal.jvm.Jvm.current()
     val javaVersion = JavaVersion.current()
