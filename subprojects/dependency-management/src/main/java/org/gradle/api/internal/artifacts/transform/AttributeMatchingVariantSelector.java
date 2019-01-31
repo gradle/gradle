600a37e1eff73dab2dda1b2dba14f96920c36405
Cedric Champeau <cedric@gradle.com>
2019-01-31 11:02:03 +0100

    Fix transforms not properly checking that they are compatible together

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/AttributeMatchingVariantSelector.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/AttributeMatchingVariantSelector.java
@@ -165,7 +165,7 @@ class AttributeMatchingVariantSelector implements VariantSelector {
                                                                                                           Pair<ResolvedVariant, ConsumerVariantMatchResult.ConsumerVariant> firstCandidate,
                                                                                                           Pair<ResolvedVariant, ConsumerVariantMatchResult.ConsumerVariant> secondCandidate) {
 
-        if (matcher.isMatching(firstCandidate.right.attributes, secondCandidate.right.attributes)) {
+        if (matcher.isMatching(firstCandidate.right.attributes, secondCandidate.right.attributes) || matcher.isMatching(secondCandidate.right.attributes, firstCandidate.right.attributes)) {
             if (firstCandidate.right.depth >= secondCandidate.right.depth) {
                 return Optional.of(secondCandidate);
             } else {
