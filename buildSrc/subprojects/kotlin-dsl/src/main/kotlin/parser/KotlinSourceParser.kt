4dad7c528dbd2e0f048ac83013ed247c76515b6e
Paul Merlin <paul@gradle.com>
2019-02-20 15:50:58 +0100

    Tighten KotlinSourceParser
    removing all unneeded stuff
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- buildSrc/subprojects/kotlin-dsl/src/main/kotlin/parser/KotlinSourceParser.kt
+++ buildSrc/subprojects/kotlin-dsl/src/main/kotlin/parser/KotlinSourceParser.kt
@@ -16,8 +16,6 @@
 
 package parser
 
-import org.gradle.api.Action
-
 import org.gradle.internal.jvm.Jvm
 
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
@@ -30,17 +28,9 @@ import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.cli.jvm.config.addJvmClasspathRoots
 import org.jetbrains.kotlin.com.intellij.openapi.Disposable
 import org.jetbrains.kotlin.com.intellij.openapi.util.Disposer
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.com.intellij.psi.PsiRecursiveElementVisitor
-import org.jetbrains.kotlin.config.AnalysisFlags
-import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.config.JVMConfigurationKeys
-import org.jetbrains.kotlin.config.JvmTarget
-import org.jetbrains.kotlin.config.LanguageFeature
-import org.jetbrains.kotlin.config.LanguageVersion
-import org.jetbrains.kotlin.config.LanguageVersionSettingsImpl
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.utils.PathUtil
 
@@ -53,23 +43,6 @@ class KotlinSourceParser {
     val messageCollector: MessageCollector
         get() = PrintingMessageCollector(System.out, MessageRenderer.PLAIN_RELATIVE_PATHS, false)
 
-    fun forEachParsedKotlinFile(sourceRoots: List<File>, block: Action<KtFile>) {
-    }
-
-    fun <T : PsiElement> forEachParsedElementOfType(sourceRoots: List<File>, type: Class<T>, block: Action<T>): Unit =
-        withParsedKotlinSource(sourceRoots.toList()) { ktFiles ->
-            ktFiles.forEach { ktFile ->
-                ktFile.accept(object : PsiRecursiveElementVisitor() {
-                    override fun visitElement(element: PsiElement) {
-                        super.visitElement(element)
-                        if (type.isAssignableFrom(element.javaClass)) {
-                            block.execute(type.cast(element))
-                        }
-                    }
-                })
-            }
-        }
-
     fun <T : Any> mapParsedKotlinFiles(vararg sourceRoots: File, block: (KtFile) -> T): List<T> =
         withParsedKotlinSource(sourceRoots.toList()) { ktFiles ->
             ktFiles.map(block)
@@ -78,28 +51,12 @@ class KotlinSourceParser {
     private
     fun <T : Any> withParsedKotlinSource(sourceRoots: List<File>, block: (List<KtFile>) -> T) = withRootDisposable {
 
-        // TODO collect errors and react
         val configuration = CompilerConfiguration().apply {
 
             put(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, messageCollector)
             put(JVMConfigurationKeys.RETAIN_OUTPUT_IN_MEMORY, false)
             put(CommonConfigurationKeys.MODULE_NAME, "parser")
 
-            // TODO infer from source set configuration?
-            put(JVMConfigurationKeys.JVM_TARGET, JvmTarget.JVM_1_8)
-            put(CommonConfigurationKeys.LANGUAGE_VERSION_SETTINGS, LanguageVersionSettingsImpl(
-                languageVersion = LanguageVersion.KOTLIN_1_3,
-                apiVersion = ApiVersion.KOTLIN_1_3,
-                specificFeatures = mapOf(
-                    LanguageFeature.NewInference to LanguageFeature.State.ENABLED,
-                    LanguageFeature.SamConversionForKotlinFunctions to LanguageFeature.State.ENABLED
-                ),
-                analysisFlags = mapOf(
-                    AnalysisFlags.skipMetadataVersionCheck to true
-                )
-            ))
-
-            // TODO add source set compile classpath?
             addJvmClasspathRoots(PathUtil.getJdkClassesRoots(Jvm.current().javaHome))
             addKotlinSourceRoots(sourceRoots.map { it.canonicalPath })
         }
