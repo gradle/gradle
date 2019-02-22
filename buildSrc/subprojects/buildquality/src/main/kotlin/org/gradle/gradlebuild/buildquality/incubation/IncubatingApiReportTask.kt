6e319757f541bf196e87d7e00c101c983f2271bc
Paul Merlin <paul@gradle.com>
2019-02-22 12:57:02 +0100

    Refine IncubatingApiReportTask
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- buildSrc/subprojects/buildquality/src/main/kotlin/org/gradle/gradlebuild/buildquality/incubation/IncubatingApiReportTask.kt
+++ buildSrc/subprojects/buildquality/src/main/kotlin/org/gradle/gradlebuild/buildquality/incubation/IncubatingApiReportTask.kt
@@ -15,6 +15,7 @@ import com.github.javaparser.symbolsolver.JavaSymbolSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
@@ -29,17 +30,27 @@ import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import org.gradle.workers.IsolationMode
 import org.gradle.workers.WorkerExecutor
+
 import org.jetbrains.kotlin.psi.KtCallableDeclaration
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedDeclaration
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
+
 import parser.KotlinSourceParser
+
 import java.io.File
 import javax.inject.Inject
 
+import org.gradle.kotlin.dsl.*
+
 
 @CacheableTask
-open class IncubatingApiReportTask
-@Inject constructor(private val workerExecutor: WorkerExecutor) : DefaultTask() {
+open class IncubatingApiReportTask @Inject constructor(
+
+    private
+    val workerExecutor: WorkerExecutor
+
+) : DefaultTask() {
 
     @InputFile
     @PathSensitive(PathSensitivity.RELATIVE)
@@ -50,35 +61,53 @@ open class IncubatingApiReportTask
     val releasedVersionsFile: RegularFileProperty = project.objects.fileProperty()
 
     @Input
-    val title: Property<String> = project.objects.property(String::class.java).also {
-        it.set(project.provider { project.name })
-    }
+    val title: Property<String> = project.objects.property<String>().convention(project.provider {
+        project.name
+    })
 
     @InputFiles
     @PathSensitive(PathSensitivity.RELATIVE)
-    val sources: ConfigurableFileCollection = project.files()
+    val sources: ConfigurableFileCollection = project.objects.fileCollection()
 
     @OutputFile
-    val htmlReportFile: RegularFileProperty = project.objects.fileProperty().also {
-        it.set(project.layout.buildDirectory.file("reports/incubating.html"))
-    }
+    val htmlReportFile: RegularFileProperty = project.objects.fileProperty().convention(
+        project.layout.buildDirectory.file("reports/incubating.html")
+    )
 
     @OutputFile
-    val textReportFile: RegularFileProperty = project.objects.fileProperty().also {
-        it.set(project.layout.buildDirectory.file("reports/incubating.txt"))
-    }
+    val textReportFile: RegularFileProperty = project.objects.fileProperty().convention(
+        project.layout.buildDirectory.file("reports/incubating.txt")
+    )
 
     @TaskAction
+    @Suppress("unused")
     fun analyze() = workerExecutor.submit(Analyzer::class.java) {
         isolationMode = IsolationMode.NONE
-        params(versionFile.asFile.get(), sources.files, htmlReportFile.asFile.get(), textReportFile.asFile.get(), title.get(), releasedVersionsFile.asFile.get())
+        params(
+            sources.files,
+            htmlReportFile.asFile.get(),
+            textReportFile.asFile.get(),
+            title.get(),
+            releasedVersionsFile.asFile.get()
+        )
     }
 }
 
 
+private
+typealias Version = String
+
+
+private
+typealias IncubatingDescription = String
+
+
+private
+typealias VersionsToIncubating = Map<Version, MutableSet<IncubatingDescription>>
+
+
 open
 class Analyzer @Inject constructor(
-    private val versionFile: File,
     private val srcDirs: Set<File>,
     private val htmlReportFile: File,
     private val textReportFile: File,
@@ -88,20 +117,20 @@ class Analyzer @Inject constructor(
 
     override
     fun run() {
-        val versionToIncubating = mutableMapOf<String, MutableSet<String>>()
+        val versionToIncubating = mutableMapOf<Version, MutableSet<IncubatingDescription>>()
         srcDirs.forEach { srcDir ->
             if (srcDir.exists()) {
-                val sourceFileParser = CompositeSourceFileParser(listOf(
-                    JavaSourceFileParser(srcDir),
-                    KotlinSourceFileParser(srcDir)
+                val sourceFileParser = CompositeVersionsToIncubatingCollector(listOf(
+                    JavaVersionsToIncubatingCollector(srcDir),
+                    KotlinVersionsToIncubatingCollector(srcDir)
                 ))
-                srcDir.walkTopDown().forEach {
+                srcDir.walkTopDown().forEach { sourceFile ->
                     try {
-                        sourceFileParser.parseSourceFile(it).forEach { (version, incubating) ->
+                        sourceFileParser.collectFrom(sourceFile).forEach { (version, incubating) ->
                             versionToIncubating.getOrPut(version) { mutableSetOf() }.addAll(incubating)
                         }
                     } catch (e: Exception) {
-                        throw Exception("Unable to parse $it: ${e.message}")
+                        throw Exception("Unable to parse $sourceFile: ${e.message}")
                     }
                 }
             }
@@ -111,7 +140,7 @@ class Analyzer @Inject constructor(
     }
 
     private
-    fun generateHtmlReport(versionToIncubating: Map<String, Set<String>>) {
+    fun generateHtmlReport(versionToIncubating: VersionsToIncubating) {
         htmlReportFile.parentFile.mkdirs()
         htmlReportFile.printWriter(Charsets.UTF_8).use { writer ->
             writer.println("""<html lang="en">
@@ -127,13 +156,13 @@ class Analyzer @Inject constructor(
        <h1>Incubating APIs for $title</h1>
     """)
             val versions = versionsDates()
-            versionToIncubating.toSortedMap().forEach {
-                writer.println("<a name=\"sec_${it.key}\"></a>")
-                writer.println("<h2>Incubating since ${it.key} (${versions.get(it.key)?.run { "released on $this" }
+            versionToIncubating.toSortedMap().forEach { (version, incubatingDescriptions) ->
+                writer.println("<a name=\"sec_$version\"></a>")
+                writer.println("<h2>Incubating since $version (${versions[version]?.run { "released on $this" }
                     ?: "unreleased"})</h2>")
                 writer.println("<ul>")
-                it.value.sorted().forEach {
-                    writer.println("   <li>${it.escape()}</li>")
+                incubatingDescriptions.sorted().forEach { incubating ->
+                    writer.println("   <li>${incubating.escape()}</li>")
                 }
                 writer.println("</ul>")
             }
@@ -142,23 +171,22 @@ class Analyzer @Inject constructor(
     }
 
     private
-    fun generateTextReport(versionToIncubating: Map<String, Set<String>>) {
+    fun generateTextReport(versionToIncubating: VersionsToIncubating) {
         textReportFile.parentFile.mkdirs()
         textReportFile.printWriter(Charsets.UTF_8).use { writer ->
             val versions = versionsDates()
-            versionToIncubating.toSortedMap().forEach {
-                val version = it.key
-                val releaseDate = versions.get(it.key) ?: "unreleased"
-                it.value.sorted().forEach {
-                    writer.println("$version;$releaseDate;$it")
+            versionToIncubating.toSortedMap().forEach { (version, incubatingDescriptions) ->
+                val releaseDate = versions[version] ?: "unreleased"
+                incubatingDescriptions.sorted().forEach { incubating ->
+                    writer.println("$version;$releaseDate;$incubating")
                 }
             }
         }
     }
 
     private
-    fun versionsDates(): Map<String, String> {
-        val versions = mutableMapOf<String, String>()
+    fun versionsDates(): Map<Version, String> {
+        val versions = mutableMapOf<Version, String>()
         var version: String? = null
         releasedVersionsFile.forEachLine(Charsets.UTF_8) {
             val line = it.trim()
@@ -168,7 +196,7 @@ class Analyzer @Inject constructor(
             if (line.startsWith("\"buildTime\"")) {
                 var date = line.substring(line.indexOf("\"", 12) + 1, line.lastIndexOf("\""))
                 date = date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6, 8)
-                versions.put(version!!, date)
+                versions[version!!] = date
             }
         }
         return versions
@@ -181,63 +209,75 @@ class Analyzer @Inject constructor(
 
 
 private
-typealias VersionsToIncubating = Map<String, MutableSet<String>>
+interface VersionsToIncubatingCollector {
 
-
-private
-interface SourceFileParser {
-
-    fun parseSourceFile(sourceFile: File): VersionsToIncubating
+    fun collectFrom(sourceFile: File): VersionsToIncubating
 }
 
 
 private
-class CompositeSourceFileParser(private val sourceFileParsers: List<SourceFileParser>) : SourceFileParser {
+class CompositeVersionsToIncubatingCollector(
 
-    override fun parseSourceFile(sourceFile: File): VersionsToIncubating =
-        sourceFileParsers.flatMap { it.parseSourceFile(sourceFile).entries }.associate { it.key to it.value }
+    private
+    val collectors: List<VersionsToIncubatingCollector>
+
+) : VersionsToIncubatingCollector {
+
+    override fun collectFrom(sourceFile: File): VersionsToIncubating =
+        collectors
+            .flatMap { it.collectFrom(sourceFile).entries }
+            .associate { it.key to it.value }
 }
 
 
 private
-class JavaSourceFileParser(srcDir: File) : SourceFileParser {
+const val versionNotFound = "Not found"
+
+
+private
+class JavaVersionsToIncubatingCollector(srcDir: File) : VersionsToIncubatingCollector {
 
     private
     val solver = JavaSymbolSolver(CombinedTypeSolver(JavaParserTypeSolver(srcDir), ReflectionTypeSolver()))
 
-    override fun parseSourceFile(sourceFile: File): VersionsToIncubating {
-        val versionToIncubating = mutableMapOf<String, MutableSet<String>>()
+    override fun collectFrom(sourceFile: File): VersionsToIncubating {
+        val versionsToIncubating = mutableMapOf<Version, MutableSet<IncubatingDescription>>()
         if (!sourceFile.name.endsWith(".java")) {
-            return versionToIncubating
+            return versionsToIncubating
         }
         JavaParser.parse(sourceFile).run {
             solver.inject(this)
-            findAll(Node::class.java)
-                .filter {
-                    it is NodeWithAnnotations<*> &&
-                        it.annotations.any { it.nameAsString == "Incubating" }
-                }.map {
-                    val node = it as NodeWithAnnotations<*>
-                    val version = findVersionFromJavadoc(node)
-                        ?: "Not found"
-                    Pair(version, nodeName(it, this, sourceFile))
-                }.forEach {
-                    versionToIncubating.getOrPut(it.first) {
-                        mutableSetOf()
-                    }.add(it.second)
+            findAllIncubating()
+                .map { node -> toVersionIncubating(sourceFile, node) }
+                .forEach { (version, incubating) ->
+                    versionsToIncubating.getOrPut(version) { mutableSetOf() }.add(incubating)
                 }
         }
-        return versionToIncubating
+        return versionsToIncubating
     }
 
     private
+    fun CompilationUnit.findAllIncubating() =
+        findAll(Node::class.java).filter { it.isIncubating }
+
+    private
+    val Node.isIncubating: Boolean
+        get() = (this as? NodeWithAnnotations<*>)?.annotations?.any { it.nameAsString == "Incubating" } ?: false
+
+    private
+    fun CompilationUnit.toVersionIncubating(sourceFile: File, node: Node) =
+        Pair(
+            findVersionFromJavadoc(node as NodeWithAnnotations<*>) ?: versionNotFound,
+            nodeName(node, this, sourceFile)
+        )
+
+    private
     fun findVersionFromJavadoc(node: NodeWithAnnotations<*>): String? = if (node is NodeWithJavadoc<*>) {
-        node.javadoc.map {
-            it.blockTags.find {
-                it.tagName == "since"
-            }?.content?.elements?.find {
-                it is JavadocSnippet
-            }?.toText()
+        node.javadoc.map { javadoc ->
+            javadoc.blockTags
+                .find { tag -> tag.tagName == "since" }
+                ?.content?.elements?.find { description -> description is JavadocSnippet }
+                ?.toText()
         }.orElse(null)
     } else {
         null
@@ -254,7 +294,8 @@ class JavaSourceFileParser(srcDir: File) : SourceFileParser {
     }
 
     private
-    fun inferClassName(unit: CompilationUnit) = "${unit.packageDeclaration.map { it.nameAsString }.orElse("")}.${unit.primaryTypeName.orElse("")}"
+    fun inferClassName(unit: CompilationUnit) =
+        "${unit.packageDeclaration.map { it.nameAsString }.orElse("")}.${unit.primaryTypeName.orElse("")}"
 
     private
     inline fun tryResolve(resolver: () -> String, or: () -> String) = try {
@@ -266,35 +307,68 @@ class JavaSourceFileParser(srcDir: File) : SourceFileParser {
 
 
 private
-class KotlinSourceFileParser(srcDir: File) : SourceFileParser {
+class KotlinVersionsToIncubatingCollector(srcDir: File) : VersionsToIncubatingCollector {
 
-    override fun parseSourceFile(sourceFile: File): VersionsToIncubating {
+    override fun collectFrom(sourceFile: File): VersionsToIncubating {
 
-        val versionToIncubating = mutableMapOf<String, MutableSet<String>>()
+        val versionsToIncubating = mutableMapOf<Version, MutableSet<IncubatingDescription>>()
 
         if (!sourceFile.name.endsWith(".kt")) {
-            return versionToIncubating
+            return versionsToIncubating
         }
 
         KotlinSourceParser().mapParsedKotlinFiles(sourceFile) { ktFile ->
-            ktFile.collectDescendantsOfType<KtNamedDeclaration>().forEach { declaration ->
-                if (declaration.annotationEntries.any { it.shortName?.asString() == "Incubating" }) {
-                    var name = (declaration as? KtCallableDeclaration)?.typeParameterList?.let { "${it.text} " } ?: ""
-                    name += declaration.fqName!!.asString()
-                    if (declaration is KtCallableDeclaration) {
-                        name += declaration.valueParameterList?.text ?: ""
-                        if (declaration.receiverTypeReference != null) {
-                            name += " with ${declaration.receiverTypeReference!!.text} receiver"
-                        }
-                        if (declaration.parent == ktFile) {
-                            name += ", top-level in ${sourceFile.name}"
-                        }
-                    }
-                    val since = declaration.docComment?.getDefaultSection()?.findTagsByName("since")?.singleOrNull()?.getContent() ?: "Not found"
-                    versionToIncubating.getOrPut(since) { mutableSetOf() }.add(name)
-                }
+            ktFile.forEachIncubatingDeclaration { declaration ->
+                versionsToIncubating
+                    .getOrPut(declaration.sinceVersion) { mutableSetOf() }
+                    .add(buildIncubatingDescription(sourceFile, ktFile, declaration))
             }
         }
-        return versionToIncubating
+        return versionsToIncubating
     }
+
+    private
+    fun buildIncubatingDescription(sourceFile: File, ktFile: KtFile, declaration: KtNamedDeclaration): String {
+        var incubating = "${declaration.typeParametersString}${declaration.fullyQualifiedName}"
+        if (declaration is KtCallableDeclaration) {
+            incubating += declaration.valueParametersString
+            declaration.receiverTypeString?.let { receiver ->
+                incubating += " with $receiver receiver"
+            }
+            if (declaration.parent == ktFile) {
+                incubating += ", top-level in ${sourceFile.name}"
+            }
+        }
+        return incubating
+    }
+
+    private
+    fun KtFile.forEachIncubatingDeclaration(block: (KtNamedDeclaration) -> Unit) {
+        collectDescendantsOfType<KtNamedDeclaration>().filter { it.isIncubating }.forEach(block)
+    }
+
+    private
+    val KtNamedDeclaration.isIncubating: Boolean
+        get() = annotationEntries.any { it.shortName?.asString() == "Incubating" }
+
+    private
+    val KtNamedDeclaration.typeParametersString: String
+        get() = (this as? KtCallableDeclaration)?.typeParameterList?.let { "${it.text} " } ?: ""
+
+    private
+    val KtNamedDeclaration.fullyQualifiedName: String
+        get() = fqName!!.asString()
+
+    private
+    val KtCallableDeclaration.valueParametersString: String
+        get() = valueParameterList?.text ?: ""
+
+    private
+    val KtCallableDeclaration.receiverTypeString: String?
+        get() = receiverTypeReference?.text
+
+    private
+    val KtNamedDeclaration.sinceVersion: String
+        get() = docComment?.getDefaultSection()?.findTagsByName("since")?.singleOrNull()?.getContent()
+            ?: versionNotFound
 }
