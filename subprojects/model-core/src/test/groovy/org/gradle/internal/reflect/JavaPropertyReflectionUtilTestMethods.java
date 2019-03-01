e420034373f310109385471fba2e24342d3fbb98
Stefan Wolf <wolf@gradle.com>
2019-03-01 14:30:30 +0100

    Add more test coverage for resolving type variables

--- subprojects/model-core/src/test/groovy/org/gradle/internal/reflect/JavaPropertyReflectionUtilTestMethods.java
+++ subprojects/model-core/src/test/groovy/org/gradle/internal/reflect/JavaPropertyReflectionUtilTestMethods.java
@@ -20,18 +20,18 @@ import java.util.Collection;
 import java.util.List;
 import java.util.function.BiConsumer;
 
-public interface JavaReflectionUtilTestMethods {
-    <T> T simpleTypeVariable();
+public interface JavaPropertyReflectionUtilTestMethods<T> {
+    T simpleTypeVariable();
 
-    <T> List<T> encapsulatedTypeVariable();
+    List<T> encapsulatedTypeVariable();
 
-    <T> T[] arrayTypeWithTypeVariable();
+    T[] arrayTypeWithTypeVariable();
 
-    <T> List<Collection<? extends List<? super T>>[]> complexTypeWithTypeVariable();
+    List<Collection<? extends List<? super T>>[]> complexTypeWithTypeVariable();
 
-    <T> List<BiConsumer<Collection<? super Class<Integer>>, ? extends List<? extends T>>[]> anotherComplexTypeWithTypeVariable();
+    List<BiConsumer<Collection<? super Class<Integer>>, ? extends List<? extends T>>[]> anotherComplexTypeWithTypeVariable();
 
-    <T> List<BiConsumer<Collection<? super Class<Integer>>, ? extends List<T[]>>> complexTypeWithArrayTypeVariable();
+    List<BiConsumer<Collection<? super Class<Integer>>, ? extends List<T[]>>> complexTypeWithArrayTypeVariable();
 
     Class<?> wildcardParameterized();
 
@@ -51,5 +51,8 @@ public interface JavaReflectionUtilTestMethods {
 
     BiConsumer<List<String>, List<String>> anotherComplexParameterized();
 
+    <V extends String> V nonResolvedTypeVariable();
 
+    interface InterfaceRealizingTypeParameter extends JavaPropertyReflectionUtilTestMethods<List<Integer>> {
+    }
 }
