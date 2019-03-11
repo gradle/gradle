cb19a113017a9351f8d9fa767d50a9a0f590617a
Marc Philipp <marc@gradle.com>
2019-03-11 17:20:47 +0100

    Revert "Run tests against JUnit 4.13-beta-2"
    This reverts commit 05d0a7b3b44f29d01fac4a160e23999858c04851.

--- subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/fixture/JUnitCoverage.groovy
+++ subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/fixture/JUnitCoverage.groovy
@@ -25,17 +25,16 @@ import org.gradle.api.JavaVersion
  */
 class JUnitCoverage {
     final static String NEWEST = '4.12'
-    final static String PREVIEW = '4.13-beta-2'
     final static String LATEST_JUPITER_VERSION = '5.4.0'
     final static String LATEST_VINTAGE_VERSION = '5.4.0'
     final static String JUPITER = 'Jupiter:' + LATEST_JUPITER_VERSION
     final static String VINTAGE = 'Vintage:' + LATEST_VINTAGE_VERSION
-    final static String[] LARGE_COVERAGE = ['4.0', '4.4', '4.8.2', NEWEST, PREVIEW]
-    final static String[] IGNORE_ON_CLASS = ['4.4', '4.8.2', NEWEST, PREVIEW]
-    final static String[] ASSUMPTIONS = ['4.5', NEWEST, PREVIEW]
-    final static String[] CATEGORIES = ['4.8', NEWEST, PREVIEW]
-    final static String[] FILTER_JUNIT3_TESTS = ['3.8.1', '4.6', NEWEST, PREVIEW]
-    final static String[] JUNIT_4_LATEST = [NEWEST, PREVIEW]
+    final static String[] LARGE_COVERAGE = ['4.0', '4.4', '4.8.2', NEWEST]
+    final static String[] IGNORE_ON_CLASS = ['4.4', '4.8.2', NEWEST]
+    final static String[] ASSUMPTIONS = ['4.5', NEWEST]
+    final static String[] CATEGORIES = ['4.8', NEWEST]
+    final static String[] FILTER_JUNIT3_TESTS = ['3.8.1', '4.6', NEWEST]
+    final static String[] JUNIT_4_LATEST = [NEWEST]
     final static String[] JUNIT_VINTAGE = emptyIfJava7(VINTAGE)
     final static String[] JUNIT_VINTAGE_JUPITER = emptyIfJava7(VINTAGE, JUPITER)
 
