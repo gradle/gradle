c92f9e6f9d2664a32bf2d0f010d2ff0d7287cfc6
Bo Zhang <bo@gradle.com>
2019-03-20 11:34:45 +0800

    Update to 0.62

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ plugins {
     // We have to apply it here at the moment, so that when the build scan plugin is auto-applied via --scan can detect that
     // the plugin has been already applied. For that the plugin has to be applied with the new plugin DSL syntax.
     com.gradle.`build-scan`
-    id("org.gradle.ci.tag-single-build") version("0.61")
+    id("org.gradle.ci.tag-single-build") version("0.62")
 }
 
 defaultTasks("assemble")
