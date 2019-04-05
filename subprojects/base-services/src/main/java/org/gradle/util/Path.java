8101b14ce7e5e81eea18710b1913a8514aca2e34
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2019-04-05 18:23:32 +0200

    Polish `Path`
    Declare nullable parameter and return type.

--- subprojects/base-services/src/main/java/org/gradle/util/Path.java
+++ subprojects/base-services/src/main/java/org/gradle/util/Path.java
@@ -106,7 +106,7 @@ public class Path implements Comparable<Path> {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(@Nullable Object o) {
         if (this == o) {
             return true;
         }
@@ -157,6 +157,7 @@ public class Path implements Comparable<Path> {
      *
      * @return The parent of this path.
      */
+    @Nullable
     public Path getParent() {
         if (segments.length == 0) {
             return null;
