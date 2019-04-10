7e31c303171f422b70bba30692e6506249ec1d5e
Bo Zhang <bo@gradle.com>
2019-04-10 17:01:58 +0800

    Use assertions based on Gradle version

--- subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/CancellationSpec.groovy
+++ subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/CancellationSpec.groovy
@@ -22,6 +22,7 @@ import org.gradle.integtests.tooling.fixture.TestResultHandler
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
 import org.gradle.test.fixtures.server.http.BlockingHttpServer
 import org.gradle.tooling.BuildCancelledException
+import org.gradle.util.GradleVersion
 import org.junit.Rule
 
 import java.util.concurrent.CountDownLatch
@@ -73,13 +74,17 @@ latch.await()
         resultHandler.assertFailedWith(BuildCancelledException)
         assert resultHandler.failure.message.startsWith(failureMessage)
 
-        if (resultHandler.failure.cause != null) {
+        if (targetIsGradle51OrLater()) {
             // https://github.com/gradle/gradle-private/issues/1760
             assert resultHandler.failure.cause.message in ["Build cancelled.", "Daemon was stopped to handle build cancel request."]
         }
         def failure = OutputScrapingExecutionFailure.from(stdout.toString(), stderr.toString())
         failure.assertHasDescription('Build cancelled.')
         assertHasBuildFailedLogging()
+    }
+
+    private static void targetIsGradle51OrLater() {
+        targetVersion >= GradleVersion.version('5.1')
     }
 
     void configureWasCancelled(TestResultHandler resultHandler, String failureMessage) {
