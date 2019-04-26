95cc11ec7dee26f518e4f735f8261405989d05cb
François Guillot <francois@gradle.com>
2019-04-26 15:38:56 +0200

    Update build scan plugin to 2.3-rc-1

--- buildSrc/subprojects/profiling/profiling.gradle.kts
+++ buildSrc/subprojects/profiling/profiling.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     implementation("me.champeau.gradle:jmh-gradle-plugin:0.4.8")
     implementation("org.jsoup:jsoup:1.11.3")
-    implementation("com.gradle:build-scan-plugin:2.2.1")
+    implementation("com.gradle:build-scan-plugin:2.3-rc-1-20190426133611-release")
     implementation(project(":configuration"))
     implementation(project(":kotlinDsl"))
     implementation(project(":plugins"))
