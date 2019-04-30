f388bbb620c1ce64a58a5a6e139eb0106d9812be
Sterling Greene <sterling@gradle.com>
2019-04-30 16:02:53 -0400

    Polish C++ unit test plugin ref chapter

--- subprojects/docs/src/samples/userguide/cpp/cppApplication/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/userguide/cpp/cppApplication/kotlin/build.gradle.kts
@@ -22,6 +22,8 @@ plugins {
 
 // tag::configure-target-machines[]
 application {
-    targetMachines.set(listOf(machines.windows.x86, machines.windows.x86_64, machines.macOS.x86_64, machines.linux.x86_64))
+    targetMachines.set(listOf(machines.linux.x86_64,
+        machines.windows.x86, machines.windows.x86_64, 
+        machines.macOS.x86_64))
 }
 // end::configure-target-machines[]
--- subprojects/docs/src/samples/userguide/cpp/cppLibrary/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/userguide/cpp/cppLibrary/kotlin/build.gradle.kts
@@ -17,8 +17,7 @@ library {
 library {
     targetMachines.set(listOf(machines.linux.x86_64,
         machines.windows.x86, machines.windows.x86_64, 
-        machines.macOS.x86_64)
-    )
+        machines.macOS.x86_64))
 }
 // end::configure-target-machines[]
 
