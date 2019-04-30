400b566402b4391c2777aec345a3b0f3af471153
Sterling Greene <sterling@gradle.com>
2019-04-30 15:51:49 -0400

    Polish of C++ library plugin ref chapter

--- subprojects/docs/src/samples/userguide/cpp/cppLibrary/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/userguide/cpp/cppLibrary/kotlin/build.gradle.kts
@@ -15,7 +15,10 @@ library {
 
 // tag::configure-target-machines[]
 library {
-    targetMachines.set(listOf(machines.windows.x86, machines.windows.x86_64, machines.macOS.x86_64, machines.linux.x86_64))
+    targetMachines.set(listOf(machines.linux.x86_64,
+        machines.windows.x86, machines.windows.x86_64, 
+        machines.macOS.x86_64)
+    )
 }
 // end::configure-target-machines[]
 
