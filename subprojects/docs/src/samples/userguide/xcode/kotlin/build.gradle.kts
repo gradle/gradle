543399aedf277c81cce2be1e6b52a86181aa3eaa
Daniel Lacasse <daniel@gradle.com>
2019-05-01 19:39:09 +0200

    Remove workspace location configuration

--- subprojects/docs/src/samples/userguide/xcode/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/userguide/xcode/kotlin/build.gradle.kts
@@ -3,9 +3,3 @@ plugins {
     `xcode`
 }
 // end::apply-plugin[]
-
-// tag::configure-workspace-location[]
-xcode {
-    workspace.location.set(file("workspace.xcworkspace"))
-}
-// end::configure-workspace-location[]
