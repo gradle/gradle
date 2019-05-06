7ac86a581e295da752872ed47fc54270ad724c9f
Pap Lőrinc <lorinc@gradle.com>
2019-05-06 12:19:41 +0200

    Upgrade other ASM dependencies from `6.2.1` to `7.1`
    Signed-off-by: Pap Lőrinc <lorinc@gradle.com>

--- buildSrc/subprojects/kotlin-dsl/kotlin-dsl.gradle.kts
+++ buildSrc/subprojects/kotlin-dsl/kotlin-dsl.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("com.gradle.publish:plugin-publish-plugin:0.10.0")
 
     implementation("com.thoughtworks.qdox:qdox:2.0-M9")
-    implementation("org.ow2.asm:asm:6.2.1")
+    implementation("org.ow2.asm:asm:7.1")
 
     testImplementation("junit:junit:4.12")
     testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
