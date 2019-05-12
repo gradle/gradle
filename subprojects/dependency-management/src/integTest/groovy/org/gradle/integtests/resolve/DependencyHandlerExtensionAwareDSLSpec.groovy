cc5f046945c297de42225caff130ee285e1fc99d
Sterling Greene <sterling@gradle.com>
2019-05-12 23:05:27 +0200

    Polish tests

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/DependencyHandlerExtensionAwareDSLSpec.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/DependencyHandlerExtensionAwareDSLSpec.groovy
@@ -17,49 +17,46 @@
 package org.gradle.integtests.resolve
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
-import org.junit.Test
 
 class DependencyHandlerExtensionAwareDSLSpec extends AbstractIntegrationSpec {
+    def "can type-safely use DependencyHandler ExtensionAware with the Groovy DSL"() {
+        buildFile << """
+            dependencies {
+                extensions["theAnswer"] = {
+                    42
+                }
+            }
+            tasks.register("assertValue") {
+                doLast {
+                    assert dependencies.extensions["theAnswer"]() == 42
+                }
+            }
+        """
+        expect:
+        succeeds("assertValue")
+    }
 
-    def setup() {
+    def "can type-safely use DependencyHandler ExtensionAware with the Kotlin DSL"() {
         // Required because of: https://github.com/gradle/gradle/issues/7413
         executer.requireGradleDistribution()
-    }
 
-    @Test
-    void "Can type-safely use DependencyHandler ExtensionAware with the Groovy DSL"() {
-        when:
-        buildFile << """
-        dependencies {
-            extensions["theAnswer"] = {
-                42
-            }
-        }
-        dependencies {
-            assert(extensions["theAnswer"]() == 42) : "Can access from a different dependencies block"
-        }
-        """
-        then:
-        succeeds("help")
-    }
-
-    @Test
-    void "Can type-safely use DependencyHandler ExtensionAware with the Kotlin DSL"() {
-        when:
         buildKotlinFile << """
-        dependencies {
-            val theAnswer: () -> Int by extra {
-                { 42 }
+            dependencies {
+                val theAnswer: () -> Int by extra {
+                    { 42 }
+                }
             }
-        }
-        dependencies {
-            val theAnswer: () -> Int by dependencies.extra
-            assert(theAnswer() == 42) {
-                "Can access from a different dependencies block"
+            
+            tasks {
+                register("assertValue") {
+                    doLast {
+                        val theAnswer: () -> Int by project.dependencies.extra
+                        assert(theAnswer() == 42)
+                    }
+                }
             }
-        }
         """
-        then:
-        succeeds("help")
+        expect:
+        succeeds("assertValue")
     }
 }
