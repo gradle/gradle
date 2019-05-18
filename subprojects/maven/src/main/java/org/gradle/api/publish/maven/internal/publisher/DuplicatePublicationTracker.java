cbb0107fe3722eff5ff171e56c111584ad062014
Daz DeBoer <daz@gradle.com>
2019-05-18 15:35:27 -0600

    Warn about redundant publication
    We don't currently do any de-duplication of repositories for publication,
    which can result in the same module being published multiple times to
    the same location. This commit adds a warning in this case.

--- subprojects/maven/src/main/java/org/gradle/api/publish/maven/internal/publisher/DuplicatePublicationTracker.java
+++ subprojects/maven/src/main/java/org/gradle/api/publish/maven/internal/publisher/DuplicatePublicationTracker.java
@@ -18,26 +18,24 @@ package org.gradle.api.publish.maven.internal.publisher;
 
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Sets;
 import org.gradle.api.GradleException;
 import org.gradle.api.artifacts.ModuleVersionIdentifier;
-import org.gradle.api.artifacts.component.ModuleComponentIdentifier;
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
-import org.gradle.api.publish.internal.PublicationInternal;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 import org.gradle.api.publish.maven.internal.publication.MavenPublicationInternal;
-import org.gradle.internal.Pair;
 
 import java.net.URI;
-import java.util.Set;
 
 public class DuplicatePublicationTracker {
+    private final static Logger LOG = Logging.getLogger(DuplicatePublicationTracker.class);
     private final Multimap<String, MavenPublicationInternal> published = LinkedHashMultimap.create();
 
     public synchronized void checkCanPublish(MavenPublicationInternal publication, MavenArtifactRepository repository) {
         String repositoryKey = getRepositoryLocation(repository);
 
         if (published.get(repositoryKey).contains(publication)) {
-            // TODO:DAZ Should warn about publishing twice to the same repo
+            LOG.warn("Publication '" + publication.getCoordinates() + "' is published multiple times to the same location. It is likely that repository '" + repository.getName() + "' is duplicated.");
             return;
         }
 
