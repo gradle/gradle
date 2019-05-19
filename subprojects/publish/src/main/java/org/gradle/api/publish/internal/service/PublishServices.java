2ef90b001a510d1cb56f165ab4db085463c9b802
Daz DeBoer <daz@gradle.com>
2019-05-19 11:57:12 -0600

    Remove class cycle in publishing module

--- subprojects/publish/src/main/java/org/gradle/api/publish/internal/service/PublishServices.java
+++ subprojects/publish/src/main/java/org/gradle/api/publish/internal/service/PublishServices.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.api.publish.internal;
+package org.gradle.api.publish.internal.service;
 
 import org.gradle.api.internal.artifacts.ivyservice.projectmodule.DefaultProjectDependencyPublicationResolver;
 import org.gradle.api.publish.internal.validation.DuplicatePublicationTracker;
