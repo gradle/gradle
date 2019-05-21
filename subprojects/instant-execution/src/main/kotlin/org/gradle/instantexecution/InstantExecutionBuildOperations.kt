dd6a6e83ba2dd98d6900afbd7482ca8236339d34
Paul Merlin <paul@gradle.com>
2019-05-21 16:41:10 +0200

    Introduce load/store build ops for instant-execution
    Signed-off-by: Paul Merlin <paul@gradle.com>

---
+++ subprojects/instant-execution/src/main/kotlin/org/gradle/instantexecution/InstantExecutionBuildOperations.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.instantexecution
+
+import org.gradle.internal.operations.BuildOperationContext
+import org.gradle.internal.operations.BuildOperationDescriptor
+import org.gradle.internal.operations.BuildOperationExecutor
+import org.gradle.internal.operations.BuildOperationType
+import org.gradle.internal.operations.RunnableBuildOperation
+
+
+class InstantExecutionStateLoadBuildOperationType private constructor()
+    : BuildOperationType<InstantExecutionStateLoadBuildOperationType.Details, InstantExecutionStateLoadBuildOperationType.Results> {
+
+    interface Details
+    interface Results
+}
+
+
+class InstantExecutionStateStoreBuildOperationType private constructor()
+    : BuildOperationType<InstantExecutionStateStoreBuildOperationType.Details, InstantExecutionStateStoreBuildOperationType.Results> {
+
+    interface Details
+    interface Results
+}
+
+
+private
+object LoadDetails : InstantExecutionStateLoadBuildOperationType.Details
+
+
+private
+object LoadResults : InstantExecutionStateLoadBuildOperationType.Results
+
+
+private
+object StoreDetails : InstantExecutionStateStoreBuildOperationType.Details
+
+
+private
+object StoreResults : InstantExecutionStateLoadBuildOperationType.Results
+
+
+internal
+fun BuildOperationExecutor.withLoadOperation(block: () -> Unit) =
+    run(object : RunnableBuildOperation {
+
+        override fun description(): BuildOperationDescriptor.Builder =
+            BuildOperationDescriptor
+                .displayName("Load instant execution state")
+                .details(LoadDetails)
+
+        override fun run(context: BuildOperationContext) {
+            block()
+            context.setResult(LoadResults)
+        }
+    })
+
+
+internal
+fun BuildOperationExecutor.withStoreOperation(block: () -> Unit) =
+    run(object : RunnableBuildOperation {
+
+        override fun description(): BuildOperationDescriptor.Builder =
+            BuildOperationDescriptor
+                .displayName("Store instant execution state")
+                .details(StoreDetails)
+
+        override fun run(context: BuildOperationContext) {
+            block()
+            context.setResult(StoreResults)
+        }
+    })
