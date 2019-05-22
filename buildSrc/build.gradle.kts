f29d1395948e8e8450f32aedac3075b3a1ad7c2b
Jendrik Johannes <jendrik@gradle.com>
2019-05-22 16:51:28 +0200

    Use 'implementation' instead of 'compile' in buildSrc scripts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -44,7 +44,7 @@ subprojects {
         }
 
         dependencies {
-            compile(gradleApi())
+            implementation(gradleApi())
         }
 
         afterEvaluate {
@@ -95,7 +95,7 @@ allprojects {
 
 dependencies {
     subprojects.forEach {
-        runtime(project(it.path))
+        runtimeOnly(project(it.path))
     }
 }
 
@@ -179,12 +179,12 @@ fun Project.applyGroovyProjectConventions() {
     apply(plugin = "groovy")
 
     dependencies {
-        compile(localGroovy())
-        testCompile("org.spockframework:spock-core:1.2-groovy-2.5") {
+        implementation(localGroovy())
+        testImplementation("org.spockframework:spock-core:1.2-groovy-2.5") {
             exclude(group = "org.codehaus.groovy")
         }
-        testCompile("net.bytebuddy:byte-buddy:1.8.21")
-        testCompile("org.objenesis:objenesis:2.6")
+        testImplementation("net.bytebuddy:byte-buddy:1.8.21")
+        testImplementation("org.objenesis:objenesis:2.6")
     }
 
     tasks.withType<GroovyCompile>().configureEach {
