6c54be825012454f520d7782f343b48b227eddfd
Sterling Greene <sterling@gradle.com>
2019-05-29 22:14:30 -0400

    Polish for Checkstyle

--- subprojects/logging/src/main/java/org/slf4j/impl/StaticMDCBinder.java
+++ subprojects/logging/src/main/java/org/slf4j/impl/StaticMDCBinder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.slf4j.impl;
 
 import org.slf4j.helpers.NOPMDCAdapter;
@@ -6,7 +22,7 @@ import org.slf4j.spi.MDCAdapter;
 @SuppressWarnings("UnusedDeclaration")
 public class StaticMDCBinder {
 
-    public static final StaticMDCBinder SINGLETON = new StaticMDCBinder();
+    private static final StaticMDCBinder SINGLETON = new StaticMDCBinder();
 
     private StaticMDCBinder() {
     }
