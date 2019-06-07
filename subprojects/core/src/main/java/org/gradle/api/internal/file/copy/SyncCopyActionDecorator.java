8004145157477176184b6d50886ed99643ab17f7
Pap Lőrinc <lorinc@gradle.com>
2019-06-07 11:29:16 +0200

    Fix file case change handling in the case of sync task
    We remove everything first and copy afterwards, eliminating problems caused by file case changes during copy-and-delete

--- subprojects/core/src/main/java/org/gradle/api/internal/file/copy/SyncCopyActionDecorator.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/file/copy/SyncCopyActionDecorator.java
@@ -18,8 +18,6 @@ package org.gradle.api.internal.file.copy;
 import org.gradle.api.file.FileTreeElement;
 import org.gradle.api.file.FileVisitDetails;
 import org.gradle.api.file.FileVisitor;
-import org.gradle.api.file.RelativePath;
-import org.gradle.api.internal.file.CopyActionProcessingStreamAction;
 import org.gradle.api.internal.file.collections.DirectoryFileTreeFactory;
 import org.gradle.api.internal.file.collections.MinimalFileTree;
 import org.gradle.api.specs.Spec;
@@ -31,8 +29,6 @@ import org.gradle.util.GFileUtils;
 
 import javax.annotation.Nullable;
 import java.io.File;
-import java.util.HashSet;
-import java.util.Set;
 
 public class SyncCopyActionDecorator implements CopyAction {
     private final File baseDestDir;
@@ -40,7 +36,7 @@ public class SyncCopyActionDecorator implements CopyAction {
     private final PatternFilterable preserveSpec;
     private final DirectoryFileTreeFactory directoryFileTreeFactory;
 
-    public SyncCopyActionDecorator(File baseDestDir, CopyAction delegate, DirectoryFileTreeFactory directoryFileTreeFactory) {
+    SyncCopyActionDecorator(File baseDestDir, CopyAction delegate, DirectoryFileTreeFactory directoryFileTreeFactory) {
         this(baseDestDir, delegate, null, directoryFileTreeFactory);
     }
 
@@ -52,39 +48,31 @@ public class SyncCopyActionDecorator implements CopyAction {
     }
 
     @Override
-    public WorkResult execute(final CopyActionProcessingStream stream) {
-        final Set<RelativePath> visited = new HashSet<RelativePath>();
+    public WorkResult execute(CopyActionProcessingStream stream) {
+        boolean cleanupDidWork = cleanup();
+        boolean copyDidWork = copy(stream);
 
-        WorkResult didWork = delegate.execute(new CopyActionProcessingStream() {
-            @Override
-            public void process(final CopyActionProcessingStreamAction action) {
-                stream.process(new CopyActionProcessingStreamAction() {
-                    @Override
-                    public void processFile(FileCopyDetailsInternal details) {
-                        visited.add(details.getRelativePath());
-                        action.processFile(details);
-                    }
-                });
-            }
-        });
-
-        SyncCopyActionDecoratorFileVisitor fileVisitor = new SyncCopyActionDecoratorFileVisitor(visited, preserveSpec);
-
-        MinimalFileTree walker = directoryFileTreeFactory.create(baseDestDir).postfix();
-        walker.visit(fileVisitor);
-        visited.clear();
-
-        return WorkResults.didWork(didWork.getDidWork() || fileVisitor.didWork);
+        return WorkResults.didWork(cleanupDidWork || copyDidWork);
     }
 
-    private static class SyncCopyActionDecoratorFileVisitor implements FileVisitor {
-        private final Set<RelativePath> visited;
+    private boolean cleanup() {
+        DeletingFileVisitor fileVisitor = new DeletingFileVisitor(preserveSpec);
+        MinimalFileTree walker = directoryFileTreeFactory.create(baseDestDir).postfix();
+        walker.visit(fileVisitor);
+        return fileVisitor.didWork;
+    }
+
+    private boolean copy(CopyActionProcessingStream stream) {
+        WorkResult result = delegate.execute(stream);
+        return result.getDidWork();
+    }
+
+    private static class DeletingFileVisitor implements FileVisitor {
         private final Spec<FileTreeElement> preserveSpec;
         private final PatternSet preserveSet;
         private boolean didWork;
 
-        private SyncCopyActionDecoratorFileVisitor(Set<RelativePath> visited, @Nullable PatternFilterable preserveSpec) {
-            this.visited = visited;
+        private DeletingFileVisitor(@Nullable PatternFilterable preserveSpec) {
             PatternSet preserveSet = new PatternSet();
             if (preserveSpec != null) {
                 preserveSet.include(preserveSpec.getIncludes());
@@ -96,21 +84,18 @@ public class SyncCopyActionDecorator implements CopyAction {
 
         @Override
         public void visitDir(FileVisitDetails dirDetails) {
-            maybeDelete(dirDetails, true);
+            maybeDelete(dirDetails);
         }
 
         @Override
         public void visitFile(FileVisitDetails fileDetails) {
-            maybeDelete(fileDetails, false);
+            maybeDelete(fileDetails);
         }
 
-        private void maybeDelete(FileVisitDetails fileDetails, boolean isDir) {
-            RelativePath path = fileDetails.getRelativePath();
-            if (!visited.contains(path)) {
-                if (preserveSet.isEmpty() || !preserveSpec.isSatisfiedBy(fileDetails)) {
-                    GFileUtils.forceDelete(fileDetails.getFile());
-                    didWork = true;
-                }
+        private void maybeDelete(FileVisitDetails fileDetails) {
+            if (preserveSet.isEmpty() || !preserveSpec.isSatisfiedBy(fileDetails)) {
+                GFileUtils.forceDelete(fileDetails.getFile());
+                didWork = true;
             }
         }
     }
