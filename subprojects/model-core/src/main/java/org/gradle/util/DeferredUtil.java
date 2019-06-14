485cb0b3359a0db2bc21045a741f511c3e959666
Adam Murdoch <adam@gradle.com>
2019-06-15 09:45:48 +1000

    Remove unnecessary check.

--- subprojects/model-core/src/main/java/org/gradle/util/DeferredUtil.java
+++ subprojects/model-core/src/main/java/org/gradle/util/DeferredUtil.java
@@ -26,8 +26,6 @@ import static org.gradle.util.GUtil.uncheckedCall;
 
 public class DeferredUtil {
 
-    private static Boolean kotlinFunction0CanBeLoaded = null;
-
     /**
      * Successively unpacks a deferred value until it is resolved to null or something other than Callable (including Groovy Closure) or Kotlin lambda
      * then unpacks the remaining Provider or Factory.
@@ -72,24 +70,11 @@ public class DeferredUtil {
     }
 
     private static boolean isKotlinFunction0Deferrable(@Nullable Object value) {
-        return value != null && kotlinFunction0CanBeLoaded() && value instanceof kotlin.jvm.functions.Function0;
-    }
-
-    private static boolean kotlinFunction0CanBeLoaded() {
-        if (kotlinFunction0CanBeLoaded == null) {
-            try {
-                DeferredUtil.class.getClassLoader().loadClass("kotlin.jvm.functions.Function0");
-                kotlinFunction0CanBeLoaded = true;
-            } catch (ClassNotFoundException ex) {
-                kotlinFunction0CanBeLoaded = false;
-            }
-        }
-        return kotlinFunction0CanBeLoaded;
+        return value instanceof kotlin.jvm.functions.Function0;
     }
 
     @Nullable
     private static Object unpackKotlinFunction0(Object value) {
-        assert kotlinFunction0CanBeLoaded;
         return ((kotlin.jvm.functions.Function0) value).invoke();
     }
 }
