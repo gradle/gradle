fc0241eb8e92ab1b28534be0d836f4201d5ea74f
Cedric Champeau <cedric@gradle.com>
2019-06-19 10:04:47 +0200

    Rework test setup for exclude merging

--- subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/resolveengine/excludes/factories/IntersectionsTest.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/ivyservice/resolveengine/excludes/factories/IntersectionsTest.groovy
@@ -17,18 +17,11 @@
 package org.gradle.api.internal.artifacts.ivyservice.resolveengine.excludes.factories
 
 
-import org.gradle.api.internal.artifacts.ivyservice.resolveengine.excludes.simple.DefaultExcludeFactory
-import org.gradle.api.internal.artifacts.ivyservice.resolveengine.excludes.specs.ExcludeSpec
-import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Unroll
 
-import static org.gradle.api.internal.artifacts.DefaultModuleIdentifier.newId
-
-class IntersectionsTest extends Specification {
-    @Shared
-    private ExcludeFactory factory = new DefaultExcludeFactory()
+class IntersectionsTest extends Specification implements ExcludeTestSupport {
 
     @Subject
     private Intersections ops = new Intersections(factory)
@@ -103,55 +96,9 @@ class IntersectionsTest extends Specification {
         one                                                     | other                                                   | expected
         anyOf(group("foo"), group("bar"))                       | anyOf(group("foo"), group("baz"))                       | group("foo")
         anyOf(group("g1"), moduleIdSet(["a", "b"], ["a", "c"])) | anyOf(group("g1"), moduleIdSet(["a", "b"], ["a", "d"])) | anyOf(group("g1"), moduleId("a", "b"))
+
+
     }
 
-    private ExcludeSpec group(String group) {
-        factory.group(group)
-    }
 
-    private ExcludeSpec module(String name) {
-        factory.module(name)
-    }
-
-    private ExcludeSpec moduleSet(String... names) {
-        factory.moduleSet(names as Set<String>)
-    }
-
-    private ExcludeSpec groupSet(String... groups) {
-        factory.groupSet(groups as Set<String>)
-    }
-
-    private ExcludeSpec moduleId(String group, String name) {
-        factory.moduleId(newId(group, name))
-    }
-
-    private ExcludeSpec moduleIdSet(List<String>... ids) {
-        factory.moduleIdSet(ids.collect { newId(it[0], it[1]) } as Set)
-    }
-
-    private ExcludeSpec anyOf(ExcludeSpec... specs) {
-        switch (specs.length) {
-            case 0:
-                return factory.nothing()
-            case 1:
-                return specs[0]
-            case 2:
-                return factory.anyOf(specs[0], specs[1])
-            default:
-                return factory.anyOf(specs as Set)
-        }
-    }
-
-    private ExcludeSpec allOf(ExcludeSpec... specs) {
-        switch (specs.length) {
-            case 0:
-                return factory.nothing()
-            case 1:
-                return specs[0]
-            case 2:
-                return factory.allOf(specs[0], specs[1])
-            default:
-                return factory.allOf(specs as Set)
-        }
-    }
 }
