384f70fa366611b99db20e719f086f810cd46563
Ian Kerins <ianskerins@gmail.com>
2019-06-21 10:03:13 -0400

    Fix typo in ResolutionStrategy#sortArtifacts Javadoc
    Signed-off-by: Ian Kerins <ianskerins@gmail.com>

--- subprojects/core-api/src/main/java/org/gradle/api/artifacts/ResolutionStrategy.java
+++ subprojects/core-api/src/main/java/org/gradle/api/artifacts/ResolutionStrategy.java
@@ -294,8 +294,8 @@ public interface ResolutionStrategy {
      * Specifies the ordering for resolved artifacts. Options are:
      * <ul>
      * <li>{@link SortOrder#DEFAULT} : Don't specify the sort order. Gradle will provide artifacts in the default order.</li>
-     * <li>{@link SortOrder#CONSUMER_FIRST} : Artifacts for a consuming component should appear <em>before</em> artifacts for it's dependencies.</li>
-     * <li>{@link SortOrder#DEPENDENCY_FIRST} : Artifacts for a consuming component should appear <em>after</em> artifacts for it's dependencies.</li>
+     * <li>{@link SortOrder#CONSUMER_FIRST} : Artifacts for a consuming component should appear <em>before</em> artifacts for its dependencies.</li>
+     * <li>{@link SortOrder#DEPENDENCY_FIRST} : Artifacts for a consuming component should appear <em>after</em> artifacts for its dependencies.</li>
      * </ul>
      * A best attempt will be made to sort artifacts according the supplied {@link SortOrder}, but no guarantees will be made in the presence of dependency cycles.
      *
