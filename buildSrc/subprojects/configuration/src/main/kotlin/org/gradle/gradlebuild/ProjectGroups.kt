359857c0087146d34b18d2b0e113211b261f1dcf
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2019-06-24 10:08:14 -0300

    Polish `ProjectGroups.kt`

--- buildSrc/subprojects/configuration/src/main/kotlin/org/gradle/gradlebuild/ProjectGroups.kt
+++ buildSrc/subprojects/configuration/src/main/kotlin/org/gradle/gradlebuild/ProjectGroups.kt
@@ -21,8 +21,15 @@ object ProjectGroups {
     // TODO Why is smoke test not on that list
     private
     val Project.internalProjects
-        get() = rootProject.subprojects.filter { it.name.startsWith("internal") ||
-            it.name in setOf("integTest", "distributions", "performance", "buildScanPerformance", "kotlinCompilerEmbeddable", "kotlinDslTestFixtures", "kotlinDslIntegTests") }.toSet()
+        get() = rootProject.subprojects.filter {
+            it.name.startsWith("internal") || it.name in internalProjectNames
+        }.toSet()
+
+    private
+    val internalProjectNames = setOf(
+        "integTest", "distributions", "performance", "buildScanPerformance",
+        "kotlinCompilerEmbeddable", "kotlinDslTestFixtures", "kotlinDslIntegTests"
+    )
 
     val Project.javaProjects
         get() = rootProject.subprojects - listOf(project(":distributionsDependencies"))
