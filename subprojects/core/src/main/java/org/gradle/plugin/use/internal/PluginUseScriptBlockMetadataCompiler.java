41442d803ada9b119df8cbc77b41d9a49811ad79
Daz DeBoer <daz@gradle.com>
2019-06-28 17:15:17 -0600

    Allow property replacement in version strings in the `plugins` block
    Fixes #1697

--- subprojects/core/src/main/java/org/gradle/plugin/use/internal/PluginUseScriptBlockMetadataCompiler.java
+++ subprojects/core/src/main/java/org/gradle/plugin/use/internal/PluginUseScriptBlockMetadataCompiler.java
@@ -20,7 +20,9 @@ import org.codehaus.groovy.ast.expr.ArgumentListExpression;
 import org.codehaus.groovy.ast.expr.ClosureExpression;
 import org.codehaus.groovy.ast.expr.ConstantExpression;
 import org.codehaus.groovy.ast.expr.Expression;
+import org.codehaus.groovy.ast.expr.GStringExpression;
 import org.codehaus.groovy.ast.expr.MethodCallExpression;
+import org.codehaus.groovy.ast.expr.VariableExpression;
 import org.codehaus.groovy.ast.stmt.BlockStatement;
 import org.codehaus.groovy.ast.stmt.ExpressionStatement;
 import org.codehaus.groovy.ast.stmt.Statement;
@@ -35,7 +37,8 @@ import static org.gradle.groovy.scripts.internal.AstUtils.isOfType;
 public class PluginUseScriptBlockMetadataCompiler {
 
     public static final String NEED_SINGLE_BOOLEAN = "argument list must be exactly 1 literal boolean";
-    public static final String NEED_SINGLE_STRING = "argument list must be exactly 1 literal string";
+    public static final String NEED_LITERAL_STRING = "argument list must be exactly 1 literal String or String with property replacement";
+    public static final String NEED_INTERPOLATED_STRING = "argument list must be exactly 1 literal String or String with property replacement";
     public static final String BASE_MESSAGE = "only id(String) method calls allowed in plugins {} script block";
     public static final String EXTENDED_MESSAGE = "only version(String) and apply(boolean) method calls allowed in plugins {} script block";
     private static final String NOT_LITERAL_METHOD_NAME = "method name must be literal (i.e. not a variable)";
@@ -75,26 +78,28 @@ public class PluginUseScriptBlockMetadataCompiler {
                     ConstantExpression methodName = (ConstantExpression) call.getMethod();
                     if (isOfType(methodName, String.class)) {
                         String methodNameText = methodName.getText();
-                        if (methodNameText.equals("id") || methodNameText.equals("version")) {
+                        if (methodNameText.equals("id")) {
                             ConstantExpression argumentExpression = hasSingleConstantArgOfType(call, String.class);
                             if (argumentExpression == null) {
-                                restrict(call, formatErrorMessage(NEED_SINGLE_STRING));
+                                restrict(call, formatErrorMessage(NEED_LITERAL_STRING));
                                 return;
                             }
 
-                            if (methodName.getText().equals("id")) {
-                                if (!call.isImplicitThis()) {
-                                    restrict(call, formatErrorMessage(BASE_MESSAGE));
-                                } else {
-                                    ConstantExpression lineNumberExpression = new ConstantExpression(call.getLineNumber(), true);
-                                    call.setArguments(new ArgumentListExpression(argumentExpression, lineNumberExpression));
-                                }
+                            if (!call.isImplicitThis()) {
+                                restrict(call, formatErrorMessage(BASE_MESSAGE));
+                            } else {
+                                ConstantExpression lineNumberExpression = new ConstantExpression(call.getLineNumber(), true);
+                                call.setArguments(new ArgumentListExpression(argumentExpression, lineNumberExpression));
                             }
 
-                            if (methodName.getText().equals("version")) {
-                                if (call.isImplicitThis()) {
-                                    restrict(call, formatErrorMessage(BASE_MESSAGE));
-                                }
+                        } else if (methodNameText.equals("version")) {
+                            if (!hasSimpleInterpolatedStringType(call)) {
+                                restrict(call, formatErrorMessage(NEED_INTERPOLATED_STRING));
+                                return;
+                            }
+
+                            if (call.isImplicitThis()) {
+                                restrict(call, formatErrorMessage(BASE_MESSAGE));
                             }
                         } else if (methodNameText.equals("apply")) {
                             ConstantExpression arguments = hasSingleConstantArgOfType(call, boolean.class);
@@ -123,6 +128,33 @@ public class PluginUseScriptBlockMetadataCompiler {
                 statement.getExpression().visit(this);
             }
         });
+    }
+
+    /**
+     * Checks if this method has a single argument that is either:
+     * a) A constant String expression
+     * b) A GString expression containing only variable expressions
+     */
+    private static boolean hasSimpleInterpolatedStringType(MethodCallExpression call) {
+        if (hasSingleConstantArgOfType(call, String.class) != null) {
+            return true;
+        }
+
+        ArgumentListExpression argumentList = (ArgumentListExpression) call.getArguments();
+        if (argumentList.getExpressions().size() == 1) {
+            Expression argumentExpression = argumentList.getExpressions().get(0);
+            if (argumentExpression instanceof GStringExpression) {
+                GStringExpression gStringExpression = (GStringExpression) argumentExpression;
+                for (Expression value : gStringExpression.getValues()) {
+                    if (!(value instanceof VariableExpression)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+        return false;
     }
 
     public String formatErrorMessage(String message) {
