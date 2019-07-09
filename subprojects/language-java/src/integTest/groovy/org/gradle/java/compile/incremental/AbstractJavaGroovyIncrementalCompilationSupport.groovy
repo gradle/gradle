0478b5e24c696cb2bbaf9f4d8f475a0ef485245c
Bo Zhang <bo@gradle.com>
2019-07-09 09:51:40 +0800

    Fix failed tests

--- subprojects/language-java/src/integTest/groovy/org/gradle/java/compile/incremental/AbstractJavaGroovyIncrementalCompilationSupport.groovy
+++ subprojects/language-java/src/integTest/groovy/org/gradle/java/compile/incremental/AbstractJavaGroovyIncrementalCompilationSupport.groovy
@@ -48,7 +48,7 @@ abstract class AbstractJavaGroovyIncrementalCompilationSupport extends AbstractI
 
     def enableCompilationAvoidance() {
         if (language == CompiledLanguage.GROOVY) {
-            settingsFile << "enableFeaturePreview('GROOVY_COMPILATION_AVOIDANCE')\n"
+            settingsFile << "\nenableFeaturePreview('GROOVY_COMPILATION_AVOIDANCE')\n"
         }
     }
 
