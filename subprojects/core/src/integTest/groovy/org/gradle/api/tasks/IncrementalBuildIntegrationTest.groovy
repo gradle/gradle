6b61f7ce5f538885de6ca14ea098146736c6538a
Stefan Wolf <wolf@gradle.com>
2019-07-09 15:07:45 +0200

    Fix test

--- subprojects/core/src/integTest/groovy/org/gradle/api/tasks/IncrementalBuildIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/tasks/IncrementalBuildIntegrationTest.groovy
@@ -1254,7 +1254,7 @@ task generate(type: TransformerTask) {
     
     task myTask (type: MyTask){
         project.ext.inputDirs.split(',').each { inputs.dir(it) }
-        project.upToDateWhen { true }
+        outputs.upToDateWhen { true }
     }
 '''
 
