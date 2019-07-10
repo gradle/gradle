d8f7b686f7d73df584f944b40ab63402932d9b27
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-07-09 17:04:44 -0700

    StaticVersionComparator: react to checkstyle warnings
    Signed-off-by: Roberto Perez Alcolea <rperezalcolea@netflix.com>

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/ivyresolve/strategy/StaticVersionComparator.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/ivyresolve/strategy/StaticVersionComparator.java
@@ -30,7 +30,7 @@ import java.util.Map;
  */
 class StaticVersionComparator implements Comparator<Version> {
     private static final Map<String, Integer> SPECIAL_MEANINGS =
-            ImmutableMap.of("dev", -2, "snapshot", -1,"rc", 1, "release", 2, "final", 3);
+            ImmutableMap.of("dev", -2, "snapshot", -1, "rc", 1, "release", 2, "final", 3);
 
     /**
      * Compares 2 versions. Algorithm is inspired by PHP version_compare one.
