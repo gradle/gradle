89d0c38146733d7c45452193e304ec2738d503c9
Bo Zhang <bo@gradle.com>
2019-07-10 10:12:55 +0800

    Enable feature preview for Groovy incremental compilation (#9902)
    This fixes https://github.com/gradle/gradle/issues/9870

--- subprojects/plugins/src/integTest/groovy/org/gradle/java/compile/AbstractGroovyCompileAvoidanceIntegrationSpec.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/java/compile/AbstractGroovyCompileAvoidanceIntegrationSpec.groovy
@@ -96,7 +96,7 @@ public @interface MyAnnotation { }
 
     def 'always recompile if compilation avoidance is not enabled'() {
         given:
-        enableGroovyCompilationAvoidance = false
+        settingsFile.text = settingsFile.text.readLines().findAll { !it.contains("enableFeaturePreview") }.join('\n')
         buildFile << """
             project(':b') {
                 dependencies {
