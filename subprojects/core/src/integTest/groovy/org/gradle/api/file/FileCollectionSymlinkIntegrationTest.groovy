762cae3efe954057dc51cd7bd16fc1baabe1cdcb
Lőrinc Pap <lorinc@gradle.com>
2019-07-10 09:09:05 +0200

    Test that unbroken symlinks for inputs are detected

--- subprojects/core/src/integTest/groovy/org/gradle/api/file/FileCollectionSymlinkIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/file/FileCollectionSymlinkIntegrationTest.groovy
@@ -177,7 +177,8 @@ class FileCollectionSymlinkIntegrationTest extends AbstractIntegrationSpec {
     @Issue('https://github.com/gradle/gradle/issues/9904')
     def "task with broken symlink in inputs are valid"() {
         def inputDirectoryWithBrokenLink = file('inputDirectoryWithBrokenLink').createDir()
-        def brokenInputFile = inputDirectoryWithBrokenLink.file('brokenInputFile').createLink("brokenInputFileTarget")
+        def inputFileTarget = file("brokenInputFileTarget")
+        def inputFile = inputDirectoryWithBrokenLink.file('brokenInputFile').createLink(inputFileTarget)
         def output = file("output.txt").createFile()
 
         // Validation fails with broken links assigned directly to `@InputDirectory` or `@InputFile`
@@ -195,17 +196,28 @@ class FileCollectionSymlinkIntegrationTest extends AbstractIntegrationSpec {
             }
             task inputBrokenLinkNameCollector(type: CustomTask) {
                 inputDirectoryWithBrokenLink = file '${inputDirectoryWithBrokenLink}'
-                brokenInputFile = files '${brokenInputFile}'
+                brokenInputFile = files '${inputFile}'
                 output = file '${output}'
             }
         """
-        assert inputDirectoryWithBrokenLink.exists() && !brokenInputFile.exists() && output.text.empty
+        assert inputDirectoryWithBrokenLink.exists() && !inputFile.exists() && output.text.empty
 
         when:
         run 'inputBrokenLinkNameCollector', '--info'
         then:
         executedAndNotSkipped ':inputBrokenLinkNameCollector'
         output.text == "[brokenInputFile] inputDirectoryWithBrokenLink [brokenInputFile]"
+
+        when:
+        run 'inputBrokenLinkNameCollector'
+        then:
+        skipped ':inputBrokenLinkNameCollector'
+
+        when:
+        inputFileTarget.createFile()
+        run 'inputBrokenLinkNameCollector'
+        then:
+        executedAndNotSkipped ':inputBrokenLinkNameCollector'
 
         when:
         run 'inputBrokenLinkNameCollector'
