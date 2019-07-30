8450ccd5d5e0a98870dfecf1ffc402a0efb9c368
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2019-07-30 09:45:06 -0300

    Remove assertion on the relationship between a Settings script class `ClassLoader` and its `buildscript.classLoader`
    Given the relationship is not part of the settings script contract AND
    the other assertions still hold, this should be safe.
    
    The relationship between the `buildscript.classLoader` and the script
    class classloader is not so simple to assert anymore given the new
    `groovy-dsl:...` `ClassLoaderScope` and the way
    `DefaultDeprecatedClassLoader` works.

--- subprojects/core/src/integTest/groovy/org/gradle/api/SettingsScriptExecutionIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/SettingsScriptExecutionIntegrationTest.groovy
@@ -114,9 +114,9 @@ println 'quiet message'
 logging.captureStandardOutput(LogLevel.ERROR)
 println 'error message'
 assert settings != null
-// TODO:instant-execution
-//assert buildscript.classLoader == getClass().classLoader.parent
-//assert buildscript.classLoader == Thread.currentThread().contextClassLoader
+// TODO:instant-execution consider restoring assertion on the relationship
+//  between buildscript.classLoader and getClas().classLoader
+assert getClass().classLoader.parent == Thread.currentThread().contextClassLoader
 Gradle.class.classLoader.loadClass('${implClassName}')
 try {
     buildscript.classLoader.loadClass('${implClassName}')
