b28efaa881248b83eae91ec54edaf94044e40794
Lőrinc Pap <lorinc@gradle.com>
2019-08-02 11:06:33 +0200

    Upgrade JMH to 0.5.0-rc-2

--- buildSrc/subprojects/profiling/profiling.gradle.kts
+++ buildSrc/subprojects/profiling/profiling.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation("me.champeau.gradle:jmh-gradle-plugin:0.5.0-rc-1")
+    implementation("me.champeau.gradle:jmh-gradle-plugin:0.5.0-rc-2")
     implementation("org.jsoup:jsoup:1.11.3")
     implementation("com.gradle:build-scan-plugin:2.3")
     implementation(project(":configuration"))
