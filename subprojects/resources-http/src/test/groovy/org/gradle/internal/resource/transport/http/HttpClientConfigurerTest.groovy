dd92982ec9618eaf8a71ce74ffa212c8b01693cf
Sterling Greene <sterling@gradle.com>
2019-08-05 16:10:30 -0400

    Extract magic values into descriptive names

--- subprojects/resources-http/src/test/groovy/org/gradle/internal/resource/transport/http/HttpClientConfigurerTest.groovy
+++ subprojects/resources-http/src/test/groovy/org/gradle/internal/resource/transport/http/HttpClientConfigurerTest.groovy
@@ -34,6 +34,9 @@ import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 
 class HttpClientConfigurerTest extends Specification {
+    public static final String REMOTE_HOST = "host"
+    public static final int SOME_PORT = 1234
+    public static final String PROXY_HOST = "proxy"
     HttpClientBuilder httpClientBuilder = HttpClientBuilder.create()
 
     PasswordCredentials credentials = Mock()
@@ -53,7 +56,7 @@ class HttpClientConfigurerTest extends Specification {
     HttpClientConfigurer configurer = new HttpClientConfigurer(httpSettings)
 
     def setup() {
-        basicAuthentication.addHost("host", 1234)
+        basicAuthentication.addHost(REMOTE_HOST, SOME_PORT)
     }
 
     def "forces java.home racecondition"() {
@@ -110,18 +113,18 @@ class HttpClientConfigurerTest extends Specification {
     def "configures http client with proxy credentials"() {
         httpSettings.authenticationSettings >> []
         httpSettings.sslContextFactory >> sslContextFactory
-        proxySettings.proxy >> new HttpProxySettings.HttpProxy("host", 1111, "domain/proxyUser", "proxyPass")
+        proxySettings.proxy >> new HttpProxySettings.HttpProxy(PROXY_HOST, SOME_PORT, "domain/proxyUser", "proxyPass")
 
         when:
         configurer.configure(httpClientBuilder)
 
         then:
-        def proxyCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope("host", 1111))
+        def proxyCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope(PROXY_HOST, SOME_PORT))
         proxyCredentials.userPrincipal.name == "domain/proxyUser"
         proxyCredentials.password == "proxyPass"
 
         and:
-        def ntlmCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope("host", 1111, AuthScope.ANY_REALM, "ntlm"))
+        def ntlmCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope(PROXY_HOST, SOME_PORT, AuthScope.ANY_REALM, "ntlm"))
         ntlmCredentials.userPrincipal.name == 'DOMAIN\\proxyUser'
         ntlmCredentials.domain == 'DOMAIN'
         ntlmCredentials.userName == 'proxyUser'
@@ -139,12 +142,12 @@ class HttpClientConfigurerTest extends Specification {
         configurer.configure(httpClientBuilder)
 
         then:
-        def basicCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope("host", 1234))
+        def basicCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope(REMOTE_HOST, SOME_PORT))
         basicCredentials.userPrincipal.name == "domain/user"
         basicCredentials.password == "pass"
 
         and:
-        def ntlmCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope("host", 1234, AuthScope.ANY_REALM, "ntlm"))
+        def ntlmCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope(REMOTE_HOST, SOME_PORT, AuthScope.ANY_REALM, "ntlm"))
         ntlmCredentials.userPrincipal.name == 'DOMAIN\\user'
         ntlmCredentials.domain == 'DOMAIN'
         ntlmCredentials.userName == 'user'
@@ -160,14 +163,14 @@ class HttpClientConfigurerTest extends Specification {
         httpHeaderCredentials.setName("TestHttpHeaderName")
         httpHeaderCredentials.setValue("TestHttpHeaderValue")
         AllSchemesAuthentication httpHeaderAuthentication = new AllSchemesAuthentication(httpHeaderCredentials)
-        httpHeaderAuthentication.addHost("host", 1234)
+        httpHeaderAuthentication.addHost(REMOTE_HOST, SOME_PORT)
 
         httpSettings.authenticationSettings >> [httpHeaderAuthentication]
         httpSettings.sslContextFactory >> sslContextFactory
 
         when:
         configurer.configure(httpClientBuilder)
-        HttpClientHttpHeaderCredentials actualHttpHeaderCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope("host", 1234))
+        HttpClientHttpHeaderCredentials actualHttpHeaderCredentials = httpClientBuilder.credentialsProvider.getCredentials(new AuthScope(REMOTE_HOST, SOME_PORT))
 
         then:
         actualHttpHeaderCredentials.header.name == 'TestHttpHeaderName'
