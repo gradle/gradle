a7916cbe49505f8921da073bf3b5a985667784c0
Gary Hale <gary@gradle.com>
2019-08-08 08:43:13 -0400

    Remove some kotlin tests that are no longer necessary

--- subprojects/core/src/integTest/groovy/org/gradle/JansiEndUserIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/JansiEndUserIntegrationTest.groovy
@@ -17,12 +17,10 @@
 package org.gradle
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
-import org.gradle.util.Requires
 import spock.lang.Ignore
 import spock.lang.Issue
 
 import static org.gradle.internal.nativeintegration.jansi.JansiBootPathConfigurer.JANSI_LIBRARY_PATH_SYS_PROP
-import static org.gradle.util.TestPrecondition.JDK8_OR_EARLIER
 
 class JansiEndUserIntegrationTest extends AbstractIntegrationSpec {
 
@@ -129,53 +127,6 @@ class JansiEndUserIntegrationTest extends AbstractIntegrationSpec {
 
         then:
         outputContains('Hello World')
-    }
-
-    @Requires(JDK8_OR_EARLIER)
-    def "kotlin compiler bundles different version of Jansi than initialized by Gradle's native services"() {
-        given:
-        def kotlinVersion = '1.0.4'
-
-        buildFile << """
-            buildscript {
-                ${mavenCentralRepository()}
-                dependencies {
-                    classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion'
-                }
-            }
-
-            apply plugin: 'kotlin'
-
-            ${mavenCentralRepository()}
-
-            dependencies {
-                implementation 'org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion'
-            }
-        """
-
-        when:
-        file('src/main/kotlin/MyClass.kt') << """
-            class MyClass {}
-        """
-
-        executer.expectDeprecationWarning()
-        succeeds 'compileKotlin'
-
-        then:
-        executedAndNotSkipped(':compileKotlin')
-
-        when:
-        file('src/main/kotlin/FailingClass.kt') << """
-            class FailingClass { < }
-        """
-
-        executer.expectDeprecationWarning()
-        fails 'compileKotlin'
-
-        then:
-        executedAndNotSkipped(':compileKotlin')
-        failure.assertHasCause('Compilation error. See log for more details')
-        failure.assertHasErrorOutput('FailingClass.kt: (2, 34): Expecting member declaration')
     }
 
     static String basicJavaProject() {
