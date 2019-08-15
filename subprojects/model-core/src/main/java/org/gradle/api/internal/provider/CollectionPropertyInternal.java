afdb8200939561173aeeb7ba3c8645a0c61054b5
Dan Sanduleac <sanduleac.dan@gmail.com>
2019-08-15 15:56:18 +0100

    Fix CollectionPropertyInternal
    Signed-off-by: Dan Sanduleac <sanduleac.dan@gmail.com>

--- subprojects/model-core/src/main/java/org/gradle/api/internal/provider/CollectionPropertyInternal.java
+++ subprojects/model-core/src/main/java/org/gradle/api/internal/provider/CollectionPropertyInternal.java
@@ -20,6 +20,6 @@ import org.gradle.api.provider.HasMultipleValues;
 
 import java.util.Collection;
 
-public interface CollectionPropertyInternal<T, C extends Collection<T>> extends PropertyInternal<C>, HasMultipleValues<T>, ProviderInternal<C> {
+public interface CollectionPropertyInternal<T, C extends Collection<T>> extends PropertyInternal<C>, HasMultipleValues<T>, CollectionProviderInternal<T, C> {
     Class<T> getElementType();
 }
