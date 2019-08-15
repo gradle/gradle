8a191454e9c0209aa4456e46d760609c5a95f088
Dan Sanduleac <sanduleac.dan@gmail.com>
2019-08-15 16:16:19 +0100

    implement default for size()
    Signed-off-by: Dan Sanduleac <sanduleac.dan@gmail.com>

--- subprojects/model-core/src/main/java/org/gradle/api/internal/provider/CollectionPropertyInternal.java
+++ subprojects/model-core/src/main/java/org/gradle/api/internal/provider/CollectionPropertyInternal.java
@@ -22,4 +22,9 @@ import java.util.Collection;
 
 public interface CollectionPropertyInternal<T, C extends Collection<T>> extends PropertyInternal<C>, HasMultipleValues<T>, CollectionProviderInternal<T, C> {
     Class<T> getElementType();
+
+    @Override
+    default int size() {
+        return get().size();
+    }
 }
