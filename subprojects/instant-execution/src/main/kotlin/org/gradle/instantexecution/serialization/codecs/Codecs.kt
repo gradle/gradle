c5050d1170f93f6a00d3b3729ccb394b0e29e32b
Adam Murdoch <adam@gradle.com>
2019-08-19 11:01:30 +1000

    Refactor serialization for instant execution cache so that property values are writen/read as `Provider` instances, and these provider instances connected in as the value of the property.
    This will allow providers whose value cannot be eagerly evaluated to be serialized differently to those whose value is known when serializing.

--- subprojects/instant-execution/src/main/kotlin/org/gradle/instantexecution/serialization/codecs/Codecs.kt
+++ subprojects/instant-execution/src/main/kotlin/org/gradle/instantexecution/serialization/codecs/Codecs.kt
@@ -106,7 +106,6 @@ class Codecs(
         bind(BYTE_SERIALIZER)
         bind(FLOAT_SERIALIZER)
         bind(DOUBLE_SERIALIZER)
-        bind(FileTreeCodec(fileSetSerializer, directoryFileTreeFactory))
         bind(FILE_SERIALIZER)
         bind(PATH_SERIALIZER)
         bind(ClassCodec)
@@ -142,6 +141,7 @@ class Codecs(
         bind(ListenerBroadcastCodec(listenerManager))
         bind(LoggerCodec)
 
+        bind(FileTreeCodec(fileSetSerializer, directoryFileTreeFactory))
         bind(ConfigurableFileCollectionCodec(fileCollectionFactory))
         bind(FileCollectionCodec(fileCollectionFactory))
 
