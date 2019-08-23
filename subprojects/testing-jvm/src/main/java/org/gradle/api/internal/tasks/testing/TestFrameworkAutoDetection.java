bcd4351ec769247bbd231d23dd0a0f0519541be5
Marc Philipp <marc@gradle.com>
2019-08-23 20:47:07 +0200

    Check only files on the classpath

--- subprojects/testing-jvm/src/main/java/org/gradle/api/internal/tasks/testing/TestFrameworkAutoDetection.java
+++ subprojects/testing-jvm/src/main/java/org/gradle/api/internal/tasks/testing/TestFrameworkAutoDetection.java
@@ -16,15 +16,12 @@
 
 package org.gradle.api.internal.tasks.testing;
 
-import org.gradle.api.file.EmptyFileVisitor;
-import org.gradle.api.file.FileVisitDetails;
 import org.gradle.api.internal.tasks.testing.junit.JUnitTestFramework;
 import org.gradle.api.internal.tasks.testing.junitplatform.JUnitPlatformTestFramework;
 import org.gradle.api.internal.tasks.testing.testng.TestNGTestFramework;
 import org.gradle.api.tasks.testing.Test;
-import org.gradle.internal.MutableReference;
 
-import javax.annotation.Nonnull;
+import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -44,24 +41,22 @@ public class TestFrameworkAutoDetection {
     }
 
     private static Class<? extends TestFramework> detectTestFrameworkFromClasspath(Test task) {
-        final MutableReference<Class<? extends TestFramework>> testFramework = MutableReference.empty();
-        testFramework.set(JUnitTestFramework.class);
-        task.getClasspath().getAsFileTree().visit(new EmptyFileVisitor() {
-            @Override
-            public void visitFile(@Nonnull FileVisitDetails fileDetails) {
-                Matcher matcher = VERSIONED_JAR_PATTERN.matcher(fileDetails.getName());
+        Class<? extends TestFramework> testFramework = JUnitTestFramework.class;
+        for (File file : task.getClasspath().getFiles()) {
+            if (file.isFile()) {
+                Matcher matcher = VERSIONED_JAR_PATTERN.matcher(file.getName());
                 if (matcher.matches()) {
                     String artifactName = matcher.group(1);
                     if ("junit-platform-engine".equals(artifactName)) {
-                        testFramework.set(JUnitPlatformTestFramework.class);
-                        fileDetails.stopVisiting();
+                        testFramework = JUnitPlatformTestFramework.class;
+                        break;
                     } else if ("testng".equals(artifactName)) {
-                        testFramework.set(TestNGTestFramework.class);
+                        testFramework = TestNGTestFramework.class;
                     }
                 }
             }
-        });
-        return testFramework.get();
+        }
+        return testFramework;
     }
 
 }
