5540a73faee409a8106e934e291ecf5eb5c385a9
Adam Murdoch <adam@gradle.com>
2019-09-05 12:04:44 +1000

    Move some integration tests from `core` to `fileCollections` project.

--- subprojects/file-collections/src/integTest/groovy/org/gradle/api/file/FileCollectionPropertyIntegrationTest.groovy
+++ subprojects/file-collections/src/integTest/groovy/org/gradle/api/file/FileCollectionPropertyIntegrationTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import spock.lang.Unroll
 
 
-class FileSetPropertyIntegrationTest extends AbstractIntegrationSpec {
+class FileCollectionPropertyIntegrationTest extends AbstractIntegrationSpec {
     @Unroll
     def "task #annotation file property is implicitly finalized when task starts execution"() {
         buildFile << """
