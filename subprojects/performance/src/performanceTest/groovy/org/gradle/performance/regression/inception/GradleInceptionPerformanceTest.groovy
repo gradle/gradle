18d3d5ed8eb6ec4c3051e6962fc9477d2c228a51
Stefan Wolf <wolf@gradle.com>
2019-09-06 19:02:05 +0200

    Rebaseline `help on the gradle build comparing gradle`
    See gradle/gradle-private#2657

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/inception/GradleInceptionPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/inception/GradleInceptionPerformanceTest.groovy
@@ -53,7 +53,7 @@ class GradleInceptionPerformanceTest extends AbstractCrossVersionGradleInternalP
     }
 
     def setup() {
-        def targetVersion = "5.6-20190723234933+0000"
+        def targetVersion = "6.0-20190906163506+0000"
         runner.targetVersions = [targetVersion]
         runner.minimumVersion = targetVersion
     }
