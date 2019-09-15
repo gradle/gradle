dbe8304946db54a1c6e714ca9b9d62fd44106de4
Adam Murdoch <adam@gradle.com>
2019-09-15 15:34:15 +1000

    Do not run test in parallel mode, as it does not add anything much to the coverage.

--- subprojects/instant-execution/src/integTest/groovy/org/gradle/instantexecution/InstantExecutionTaskExecutionIntegrationTest.groovy
+++ subprojects/instant-execution/src/integTest/groovy/org/gradle/instantexecution/InstantExecutionTaskExecutionIntegrationTest.groovy
@@ -16,13 +16,18 @@
 
 package org.gradle.instantexecution
 
+import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
 import org.gradle.test.fixtures.server.http.BlockingHttpServer
 import org.junit.Rule
+import spock.lang.IgnoreIf
 
 class InstantExecutionTaskExecutionIntegrationTest extends AbstractInstantExecutionIntegrationTest {
     @Rule
     BlockingHttpServer server = new BlockingHttpServer()
 
+    // Don't run in parallel mode, as the expectation for the setup build are incorrect and running in parallel
+    // does not really make any difference to the coverage
+    @IgnoreIf({ GradleContextualExecuter.parallel})
     def "runs tasks in different projects in parallel by default"() {
         server.start()
 
@@ -64,6 +69,10 @@ class InstantExecutionTaskExecutionIntegrationTest extends AbstractInstantExecut
         noExceptionThrown()
     }
 
+    // Don't run in parallel mode, as the expectation for the setup build are incorrect
+    // It could potentially be worth running this in parallel mode to demonstrate the difference between
+    // parallel and instant execution
+    @IgnoreIf({ GradleContextualExecuter.parallel})
     def "runs tasks in same project in parallel by default"() {
         server.start()
 
