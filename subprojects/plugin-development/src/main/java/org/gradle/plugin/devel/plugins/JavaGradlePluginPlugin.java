43e6b91166b7aabea53d7597c1c83c59720e2da9
Lóránt Pintér <lorant@gradle.com>
2019-09-17 11:37:01 +0200

    Don't use lambda for task action

--- subprojects/plugin-development/src/main/java/org/gradle/plugin/devel/plugins/JavaGradlePluginPlugin.java
+++ subprojects/plugin-development/src/main/java/org/gradle/plugin/devel/plugins/JavaGradlePluginPlugin.java
@@ -241,7 +241,13 @@ public class JavaGradlePluginPlugin implements Plugin<Project> {
     private void configurePluginValidations(Project project) {
         SourceSet mainSourceSet = project.getConvention().getPlugin(JavaPluginConvention.class).getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME);
         TaskProvider<Task> naggerTask = project.getTasks().register("nagAboutValidateTaskProperties", task -> {
-            task.doFirst(taskWithAction -> nagAboutDeprecatedValidateTaskPropertiesTask());
+            //noinspection Convert2Lambda - Shouldn't use lambdas for task actions
+            task.doFirst(new Action<Task>() {
+                @Override
+                public void execute(Task taskWithAction) {
+                    nagAboutDeprecatedValidateTaskPropertiesTask();
+                }
+            });
         });
         TaskProvider<ValidatePlugins> validatorTask = project.getTasks().register(VALIDATE_PLUGINS_TASK_NAME, ValidatePlugins.class, task -> {
             task.setGroup(PLUGIN_DEVELOPMENT_GROUP);
