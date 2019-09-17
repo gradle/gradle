cf5a48b2b38cce72ffb10e38a22d2ff4bfd9d892
Cedric Champeau <cedric@gradle.com>
2019-09-17 15:15:22 +0200

    Update documentation on forcing dependencies
    Now that `force` is deprecated, we should recommend and
    better describe the semantics of "strictly".

--- subprojects/docs/src/samples/userguide/dependencyManagement/managingTransitiveDependencies/forceForDependency/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/userguide/dependencyManagement/managingTransitiveDependencies/forceForDependency/kotlin/build.gradle.kts
@@ -15,6 +15,17 @@ dependencies {
 }
 // end::force-per-dependency[]
 
+// tag::force-using-strictly[]
+dependencies {
+    implementation("org.apache.httpcomponents:httpclient:4.5.4")
+    implementation("commons-codec:commons-codec") {
+        version {
+            strictly("1.9")
+        }
+    }
+}
+// end::force-using-strictly[]
+
 tasks.register<Copy>("copyLibs") {
     from(configurations.compileClasspath)
     into("$buildDir/libs")
