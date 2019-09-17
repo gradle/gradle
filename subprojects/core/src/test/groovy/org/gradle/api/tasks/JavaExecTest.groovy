de4acd1ab291df0ff7e0eb25efee82bc5556c261
Nelson Osacky <nelson@osacky.com>
2019-09-17 23:44:24 +0200

    Add JavaExec#getExecResult.
    The `AbstractExecTask` includes a `getExecResult`. This is useful if you
    use the setIgnoreExitValue but want to know the result of the exec task
    later on. JavaExec should also include this ability.
    
    Was inspired by (this stackoverflow comment)[https://stackoverflow.com/questions/34234820/gradle-exec-task-returns-non-zero-exit-value-and-fails-the-build-but-i-want-to-n#comment77281250_34274614]
    
    Signed-off-by: Nelson Osacky <nelson@osacky.com>

---
+++ subprojects/core/src/test/groovy/org/gradle/api/tasks/JavaExecTest.groovy
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.tasks
+
+import org.gradle.api.internal.AbstractTask
+import org.gradle.process.ExecResult
+import org.gradle.process.internal.ExecAction
+import org.gradle.process.internal.ExecException
+
+class JavaExecTest extends AbstractTaskTest {
+    JavaExec execTask
+    def execAction = Mock(ExecAction) {
+        getArgumentProviders() >> []
+    }
+
+    def setup() {
+        execTask = createTask(JavaExec.class)
+        execTask.setExecAction(execAction)
+    }
+
+    AbstractTask getTask() {
+        return execTask
+    }
+
+    def "executes action on execute"() {
+        when:
+        execTask.setExecutable("java")
+        execute(execTask)
+
+        then:
+        1 * execAction.setExecutable("java")
+        1 * execAction.execute() >> new ExpectedExecResult(0)
+        execTask.execResult.exitValue == 0
+
+    }
+
+    def "execute with non-zero exit value and ignore exit value should not throw exception"() {
+        when:
+        execute(execTask)
+
+        then:
+        1 * execAction.execute() >> new ExpectedExecResult(1)
+        execTask.execResult.exitValue == 1
+    }
+
+    private class ExpectedExecResult implements ExecResult {
+        int exitValue
+
+        ExpectedExecResult(int exitValue) {
+            this.exitValue = exitValue
+        }
+
+        @Override
+        int getExitValue() {
+            return exitValue
+        }
+
+        @Override
+        ExecResult assertNormalExitValue() throws ExecException {
+            return this
+        }
+
+        @Override
+        ExecResult rethrowFailure() throws ExecException {
+            return this
+        }
+    }
+}
