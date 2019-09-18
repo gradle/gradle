6a00d31632871e878c096733d0b3a0c48557dcc9
Cedric Champeau <cedric@gradle.com>
2019-09-18 18:38:20 +0200

    Make feature dependencies available on test classpath
    Whenever a feature uses the main source set, it makes
    sense to expose its dependencies to the test compile
    and runtime classpath too, because the feature is tested
    "together with" the main source set.
    
    Fixes #10778

--- subprojects/plugins/src/integTest/groovy/org/gradle/java/JavaLibraryFeatureCompilationIntegrationTest.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/java/JavaLibraryFeatureCompilationIntegrationTest.groovy
@@ -18,6 +18,7 @@ package org.gradle.java
 
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import spock.lang.Issue
 import spock.lang.Unroll
 
 class JavaLibraryFeatureCompilationIntegrationTest extends AbstractIntegrationSpec {
@@ -447,6 +448,45 @@ class JavaLibraryFeatureCompilationIntegrationTest extends AbstractIntegrationSp
         compileClasspathPackaging | _
         false                     | _
         true                      | _
+    }
+
+    @Issue("gradle/gradle#10778")
+    def "dependencies of a feature that uses the main source set are available on test compile classpath"() {
+        buildFile << """
+            apply plugin: 'java-library'
+            
+            ${mavenCentralRepository()}
+            
+            java {
+                registerFeature('feat') {
+                   usingSourceSet(sourceSets.main)
+                }
+            }
+
+            dependencies {
+                testImplementation "junit:junit:4.12"
+                featApi "org.apache.commons:commons-math3:3.6.1"
+            }
+        """
+        file("src/test/java/com/acme/FeatureTest.java") << """package com.acme;
+            import org.apache.commons.math3.complex.Complex;
+            import org.junit.Test;
+            import static org.junit.Assert.*;
+
+            public class FeatureTest {
+                @Test
+                public void shouldCompileAndRun() {
+                    Complex complex = new Complex(2.0, 1);
+                    assertEquals(3, complex.pow(2.0).getReal(), 1e-5);
+                }
+            }
+        """
+
+        when:
+        run 'test'
+
+        then:
+        executedAndNotSkipped ':compileTestJava', ':test'
     }
 
     private void packagingTasks(boolean expectExecuted, String subproject, String feature = '') {
