8e87fa525b90ddac33414e27f4c024ff02c89f53
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2019-09-19 16:58:18 +0200

    Setter method name should match getter
    I have not looked at where this method is used, so this is more of a "codestyle" report.

--- subprojects/language-jvm/src/main/java/org/gradle/api/internal/tasks/compile/DefaultJvmLanguageCompileSpec.java
+++ subprojects/language-jvm/src/main/java/org/gradle/api/internal/tasks/compile/DefaultJvmLanguageCompileSpec.java
@@ -105,7 +105,7 @@ public class DefaultJvmLanguageCompileSpec implements JvmLanguageCompileSpec, Se
     }
 
     @Override
-    public void setSourcesRoots(List<File> sourceRoots) {
+    public void setSourceRoots(List<File> sourceRoots) {
         this.sourceRoots = sourceRoots;
     }
 }
