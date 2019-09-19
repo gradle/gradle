33056b0a740f7e530aa89eaf0f77ec2304af2eb5
Bo Zhang <bo@gradle.com>
2019-09-19 15:07:00 +0800

    Use a separate project

---
+++ subprojects/samples/samples.gradle.kts
@@ -0,0 +1,53 @@
+import org.gradle.gradlebuild.test.integrationtests.IntegrationTest
+import org.gradle.gradlebuild.testing.integrationtests.cleanup.WhenNotEmpty
+import org.gradle.gradlebuild.unittestandcompile.ModuleType
+
+plugins {
+    `java-library`
+    gradlebuild.classycle
+}
+
+dependencies {
+    integTestImplementation(project(":baseServices"))
+    integTestImplementation(project(":native"))
+    integTestImplementation(project(":logging"))
+    integTestImplementation(project(":processServices"))
+    integTestImplementation(project(":coreApi"))
+    integTestImplementation(project(":resources"))
+    integTestImplementation(project(":persistentCache"))
+    integTestImplementation(project(":dependencyManagement"))
+    integTestImplementation(project(":bootstrap"))
+    integTestImplementation(project(":launcher"))
+    integTestImplementation(library("groovy"))
+    integTestImplementation(library("slf4j_api"))
+    integTestImplementation(library("guava"))
+    integTestImplementation(library("ant"))
+    integTestImplementation(testLibrary("jsoup"))
+    integTestImplementation(testLibrary("jetty"))
+    integTestImplementation(testLibrary("sampleCheck")) {
+        exclude(group = "org.codehaus.groovy", module = "groovy-all")
+        exclude(module = "slf4j-simple")
+    }
+
+    val allTestRuntimeDependencies: DependencySet by rootProject.extra
+    allTestRuntimeDependencies.forEach {
+        integTestRuntimeOnly(it)
+    }
+
+    integTestImplementation(testFixtures(project(":core")))
+    integTestImplementation(testFixtures(project(":diagnostics")))
+    integTestImplementation(testFixtures(project(":platformNative")))
+}
+
+gradlebuildJava {
+    moduleType = ModuleType.INTERNAL
+}
+
+val integTestTasks: DomainObjectCollection<IntegrationTest> by extra
+integTestTasks.configureEach {
+    libsRepository.required = true
+}
+
+testFilesCleanup {
+    policy.set(WhenNotEmpty.REPORT)
+}
