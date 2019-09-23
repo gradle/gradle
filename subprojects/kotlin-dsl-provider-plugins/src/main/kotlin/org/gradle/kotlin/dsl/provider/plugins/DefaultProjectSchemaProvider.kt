c842920b1e6837aa1129d290db6131bef3bf615e
Cedric Champeau <cedric@gradle.com>
2019-09-23 17:37:03 +0200

    Add test for deprecated configuration accessors
    This commit adds a test that makes sure that the
    `kotlin.Deprecated` annotation is properly added
    to the extension methods for deprecated configu-
    rations.

--- subprojects/kotlin-dsl-provider-plugins/src/main/kotlin/org/gradle/kotlin/dsl/provider/plugins/DefaultProjectSchemaProvider.kt
+++ subprojects/kotlin-dsl-provider-plugins/src/main/kotlin/org/gradle/kotlin/dsl/provider/plugins/DefaultProjectSchemaProvider.kt
@@ -206,7 +206,7 @@ fun accessibleConfigurationsOf(project: Project) =
 
 private
 fun toConfigurationEntry(configuration: Configuration) = (configuration as DeprecatableConfiguration).run {
-    ConfigurationEntry(name, declarationAlternatives?:listOf())
+    ConfigurationEntry(name, declarationAlternatives ?: listOf())
 }
 
 
