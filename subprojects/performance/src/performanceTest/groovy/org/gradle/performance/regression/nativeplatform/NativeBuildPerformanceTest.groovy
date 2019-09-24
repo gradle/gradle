e7adcd6b75ee03551d230bfc3c5a8880ce6d3aeb
Daniel Lacasse <daniel@gradle.com>
2019-09-24 13:34:14 +0200

    Revert "Rebaseline NativeBuildPerformanceTest and SwiftCleanBuildPerformanceTest"
    This reverts commit f466fd76abcee9a55d88edc2cffad0fcc4dd16ce.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/nativeplatform/NativeBuildPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/nativeplatform/NativeBuildPerformanceTest.groovy
@@ -23,7 +23,7 @@ import spock.lang.Unroll
 class NativeBuildPerformanceTest extends AbstractCrossVersionGradleProfilerPerformanceTest {
     def setup() {
         runner.minimumVersion = '4.1' // minimum version that contains new C++ plugins
-        runner.targetVersions = ["6.0-20190920115514+0000"]
+        runner.targetVersions = ["6.0-20190823180744+0000"]
     }
 
     @Unroll
