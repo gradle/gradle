79dbbadbe91f22e24acc702534255e4f3349afd8
Paul Merlin <paul@gradle.com>
2019-09-26 11:49:21 +0200

    Refine KotlinDslScriptsModelCrossVersionSpec
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/r60/KotlinDslScriptsModelCrossVersionSpec.groovy
+++ subprojects/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/r60/KotlinDslScriptsModelCrossVersionSpec.groovy
@@ -21,6 +21,7 @@ import org.gradle.kotlin.dsl.tooling.builders.AbstractKotlinScriptModelCrossVers
 import org.gradle.kotlin.dsl.tooling.builders.KotlinDslScriptsModelClient
 import org.gradle.kotlin.dsl.tooling.builders.KotlinDslScriptsModelRequest
 import org.gradle.kotlin.dsl.tooling.models.KotlinDslScriptsModel
+import org.gradle.test.fixtures.file.LeaksFileHandles
 
 import java.lang.reflect.Proxy
 
@@ -28,6 +29,7 @@ import static org.gradle.integtests.tooling.fixture.TextUtil.escapeString
 
 
 @TargetGradleVersion(">=6.0")
+@LeaksFileHandles("Kotlin Compiler Daemon taking time to shut down")
 class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCrossVersionTest {
 
     def "can fetch model for the scripts of a build"() {
@@ -37,6 +39,7 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
         def rootJar = withEmptyJar("classes_root.jar").canonicalFile
         def aJar = withEmptyJar("classes_a.jar").canonicalFile
         def bJar = withEmptyJar("classes_b.jar").canonicalFile
+        def bPrecompiledJar = withEmptyJar("classes_b_precompiled.jar").canonicalFile
 
         and:
         def settings = withSettings("""
@@ -65,51 +68,56 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
                     classpath(files("${escapeString(bJar)}"))
                 }
             }
+
             $repositoriesBlock
+
+            dependencies {
+                implementation(files("${escapeString(bPrecompiledJar)}"))
+            }
         """)
         def foo = withFile("b/src/main/kotlin/foo/foo.gradle.kts", "")
 
         when:
         def model = kotlinDslScriptsModelFor()
 
-        then:
-        model.scriptModels.size() == 5
-        model.scriptModels.keySet() == [settings, root, a, b, foo] as Set
+        then: "inferred scripts"
+        def inferredScripts = [settings, root, a, b, foo] as Set
+        model.scriptModels.keySet() == inferredScripts
 
-        and:
-        def settingsClassPath = model.scriptModels[settings].classPath.collect { it.name }
-        !settingsClassPath.contains("buildSrc.jar")
-        !settingsClassPath.contains(rootJar.name)
-        !settingsClassPath.contains(aJar.name)
-        !settingsClassPath.contains(bJar.name)
+        and: "kotlin-dsl"
+        inferredScripts.each { script ->
+            assertContainsGradleKotlinDslJars(canonicalClasspathOf(model, script))
+        }
 
-        and:
-        def rootClassPath = model.scriptModels[root].classPath.collect { it.name }
-        rootClassPath.contains("buildSrc.jar")
-        rootClassPath.contains(rootJar.name)
-        !rootClassPath.contains(aJar.name)
-        !rootClassPath.contains(bJar.name)
+        and: "settings classpath"
+        def settingsClassPath = canonicalClasspathOf(model, settings)
+        assertNotContainsBuildSrc(settingsClassPath)
+        assertContainsGradleKotlinDslJars(settingsClassPath)
+        assertExcludes(settingsClassPath, rootJar, aJar, bJar)
 
-        and:
-        def aClassPath = model.scriptModels[a].classPath.collect { it.name }
-        aClassPath.contains("buildSrc.jar")
-        aClassPath.contains(rootJar.name)
-        aClassPath.contains(aJar.name)
-        !aClassPath.contains(bJar.name)
+        and: "root build script classpath"
+        def rootClassPath = canonicalClasspathOf(model, root)
+        assertContainsBuildSrc(rootClassPath)
+        assertIncludes(rootClassPath, rootJar)
+        assertExcludes(rootClassPath, aJar, bJar)
 
-        and:
-        def bClassPath = model.scriptModels[b].classPath.collect { it.name }
-        bClassPath.contains("buildSrc.jar")
-        bClassPath.contains(rootJar.name)
-        !bClassPath.contains(aJar.name)
-        bClassPath.contains(bJar.name)
+        and: ":a build script classpath"
+        def aClassPath = canonicalClasspathOf(model, a)
+        assertContainsBuildSrc(aClassPath)
+        assertIncludes(aClassPath, rootJar, aJar)
+        assertExcludes(aClassPath, bJar)
 
-        and:
-        def fooClassPath = model.scriptModels[foo].classPath.collect { it.name }
-        !fooClassPath.contains("buildSrc.jar")
-        !fooClassPath.contains(rootJar.name)
-        !fooClassPath.contains(aJar.name)
-        !fooClassPath.contains(bJar.name)
+        and: ":b build script classpath"
+        def bClassPath = canonicalClasspathOf(model, b)
+        assertContainsBuildSrc(bClassPath)
+        assertIncludes(bClassPath, rootJar, bJar)
+        assertExcludes(bClassPath, aJar)
+
+        and: "precompiled script classpath"
+        def fooClassPath = canonicalClasspathOf(model, foo)
+        assertNotContainsBuildSrc(fooClassPath)
+        assertIncludes(fooClassPath, bPrecompiledJar)
+        assertExcludes(fooClassPath, rootJar, aJar, bJar)
     }
 
     def "can fetch model for a set of scripts"() {
@@ -199,7 +207,7 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
         def commonModel = source.commonModel
         commonModel != null
         commonModel.classPath.size() > 0
-        // commonModel.classPath.find { it.name == "buildSrc.jar" } != null // TODO
+        commonModel.classPath.find { it.name == "buildSrc.jar" }
         commonModel.sourcePath.size() > 0
         commonModel.implicitImports.size() > 0
 
@@ -225,5 +233,9 @@ class KotlinDslScriptsModelCrossVersionSpec extends AbstractKotlinScriptModelCro
                 new KotlinDslScriptsModelRequest(scripts.toList())
             )
         }
+    }
+
+    private static List<File> canonicalClasspathOf(KotlinDslScriptsModel model, File script) {
+        return canonicalClasspathOf(model.scriptModels[script])
     }
 }
