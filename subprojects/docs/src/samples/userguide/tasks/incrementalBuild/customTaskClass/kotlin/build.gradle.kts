bf3d5306850bf6c10cee5cd3f4c547bb6b3ff8fa
Lóránt Pintér <lorant@gradle.com>
2019-10-01 09:50:36 +0200

    Userguide: mention runtime validation and remove runtime property override

--- subprojects/docs/src/samples/userguide/tasks/incrementalBuild/customTaskClass/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/userguide/tasks/incrementalBuild/customTaskClass/kotlin/build.gradle.kts
@@ -36,50 +36,57 @@ tasks.register("processTemplatesAdHoc") {
 }
 // end::ad-hoc-task[]
 
+// tag::ad-hoc-task-skip-when-empty[]
+tasks.register("processTemplatesAdHocSkipWhenEmpty") {
+    // ...
+
+    inputs.files(fileTree("src/templates"))
+        .skipWhenEmpty()
+        .withPropertyName("sourceFiles")
+        .withPathSensitivity(PathSensitivity.RELATIVE)
+
+    // ...
+// end::ad-hoc-task-skip-when-empty[]
+    inputs.property("engine", TemplateEngineType.FREEMARKER)
+    inputs.property("templateData.name", "docs")
+    inputs.property("templateData.variables", mapOf("year" to "2013"))
+    outputs.dir("$buildDir/genOutput2")
+        .withPropertyName("outputDir")
+
+    doLast {
+        // Process the templates here
+        copy {
+            into("$buildDir/genOutput2")
+            from(fileTree("src/templates"))
+            expand("year" to "2012")
+        }
+    }
+// tag::ad-hoc-task-skip-when-empty[]
+}
+// end::ad-hoc-task-skip-when-empty[]
+
 // tag::custom-class-runtime-api[]
-tasks.register<ProcessTemplatesNoAnnotations>("processTemplatesRuntime") {
+tasks.register<ProcessTemplates>("processTemplatesWithExtraInputs") {
+    // ...
+// end::custom-class-runtime-api[]
     templateEngine = TemplateEngineType.FREEMARKER
     sourceFiles = fileTree("src/templates")
     templateData = TemplateData("test", mapOf("year" to "2014"))
     outputDir = file("$buildDir/genOutput3")
+// tag::custom-class-runtime-api[]
 
-    inputs.property("engine", templateEngine)
-    inputs.files(sourceFiles)
-        .withPropertyName("sourceFiles")
-        .withPathSensitivity(PathSensitivity.RELATIVE)
-    inputs.property("templateData.name", templateData.name)
-    inputs.property("templateData.variables", templateData.variables)
-    outputs.dir(outputDir)
-        .withPropertyName("outputDir")
+    inputs.file("src/headers/headers.txt")
+        .withPropertyName("headers")
+        .withPathSensitivity(PathSensitivity.NONE)
 }
 // end::custom-class-runtime-api[]
 
-// tag::runtime-api-conf[]
-tasks.register<ProcessTemplatesNoAnnotations>("processTemplatesRuntimeConf") {
-    // ...
-// end::runtime-api-conf[]
+tasks.register<ProcessTemplatesNoAnnotations>("processTemplatesWithoutAnnotations") {
     templateEngine = TemplateEngineType.FREEMARKER
+    sourceFiles = fileTree("src/templates")
     templateData = TemplateData("test", mapOf("year" to "2014"))
     outputDir = file("$buildDir/genOutput3")
-// tag::runtime-api-conf[]
-    sourceFiles = fileTree("src/templates") {
-        include("**/*.fm")
-    }
-
-    inputs.files(sourceFiles)
-        .skipWhenEmpty()
-        .withPropertyName("sourceFiles")
-        .withPathSensitivity(PathSensitivity.RELATIVE)
-    // ...
-// end::runtime-api-conf[]
-    inputs.property("engine", templateEngine)
-    inputs.property("templateData.name", templateData.name)
-    inputs.property("templateData.variables", templateData.variables)
-    outputs.dir(outputDir)
-        .withPropertyName("outputDir")
-// tag::runtime-api-conf[]
 }
-// end::runtime-api-conf[]
 
 // tag::inferred-task-dep-via-outputs[]
 tasks.register<Zip>("packageFiles") {
