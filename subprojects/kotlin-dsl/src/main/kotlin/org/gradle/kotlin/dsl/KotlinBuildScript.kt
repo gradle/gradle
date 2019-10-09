bfc21c810bcbdd15c6e8a89f22a014361cb87616
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2019-10-09 12:10:13 -0300

    Start migrating the `KotlinBuildScript` template to the new API

--- subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/KotlinBuildScript.kt
+++ subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/KotlinBuildScript.kt
@@ -16,56 +16,65 @@
 
 package org.gradle.kotlin.dsl
 
+import org.gradle.api.HasImplicitReceiver
 import org.gradle.api.Project
 import org.gradle.api.initialization.dsl.ScriptHandler
-
+import org.gradle.api.plugins.PluginAware
+import org.gradle.kotlin.dsl.precompile.v1.scriptResolverEnvironmentOf
 import org.gradle.kotlin.dsl.resolver.KotlinBuildScriptDependenciesResolver
+import org.gradle.kotlin.dsl.support.DefaultKotlinScript
 import org.gradle.kotlin.dsl.support.KotlinScriptHost
-import org.gradle.kotlin.dsl.support.delegates.ProjectDelegate
+import org.gradle.kotlin.dsl.support.defaultKotlinScriptHostForProject
 import org.gradle.kotlin.dsl.support.internalError
 import org.gradle.kotlin.dsl.support.invalidPluginsCall
-
 import org.gradle.plugin.use.PluginDependenciesSpec
-
-import kotlin.script.extensions.SamWithReceiverAnnotations
-import kotlin.script.templates.ScriptTemplateAdditionalCompilerArguments
-import kotlin.script.templates.ScriptTemplateDefinition
+import org.jetbrains.kotlin.scripting.definitions.annotationsForSamWithReceivers
+import java.io.File
+import java.util.concurrent.TimeUnit
+import kotlin.script.dependencies.ScriptContents
+import kotlin.script.dependencies.ScriptDependenciesResolver
+import kotlin.script.experimental.annotations.KotlinScript
+import kotlin.script.experimental.api.ExternalSourceCode
+import kotlin.script.experimental.api.ScriptAcceptedLocation
+import kotlin.script.experimental.api.ScriptCompilationConfiguration
+import kotlin.script.experimental.api.acceptedLocations
+import kotlin.script.experimental.api.asSuccess
+import kotlin.script.experimental.api.compilerOptions
+import kotlin.script.experimental.api.defaultImports
+import kotlin.script.experimental.api.ide
+import kotlin.script.experimental.api.implicitReceivers
+import kotlin.script.experimental.api.refineConfiguration
+import kotlin.script.experimental.api.with
+import kotlin.script.experimental.jvm.dependenciesFromClassContext
+import kotlin.script.experimental.jvm.jvm
+import kotlin.script.experimental.jvm.updateClasspath
 
 
 /**
  * Base class for Kotlin build scripts.
  */
-@ScriptTemplateDefinition(
-    resolver = KotlinBuildScriptDependenciesResolver::class,
-    scriptFilePattern = ".*\\.gradle\\.kts"
-)
-@ScriptTemplateAdditionalCompilerArguments([
-    "-jvm-target", "1.8",
-    "-Xjsr305=strict",
-    "-XXLanguage:+NewInference",
-    "-XXLanguage:+SamConversionForKotlinFunctions"
-])
-@SamWithReceiverAnnotations("org.gradle.api.HasImplicitReceiver")
 @GradleDsl
-abstract class KotlinBuildScript(
+@KotlinScript(
+    displayName = "Gradle Project Script",
+    fileExtension = "gradle.kts",
+    filePathPattern = ".*\\.gradle\\.kts",
+    compilationConfiguration = KotlinBuildScriptCompilationConfiguration::class
+)
+open class KotlinBuildScript(
     private val host: KotlinScriptHost<Project>
-) : ProjectDelegate() /* TODO:kotlin-dsl configure Project as implicit receiver */ {
-
-    override val delegate: Project
-        get() = host.target
+) : DefaultKotlinScript(defaultKotlinScriptHostForProject(host.target)), PluginAware by host.target {
 
     /**
      * The [ScriptHandler] for this script.
      */
-    override fun getBuildscript(): ScriptHandler =
-        host.scriptHandler
+    val buildscript: ScriptHandler
+        get() = host.scriptHandler
 
     /**
      * Configures the build script classpath for this project.
      *
      * @see [Project.buildscript]
      */
-    @Suppress("unused")
     open fun buildscript(@Suppress("unused_parameter") block: ScriptHandlerScope.() -> Unit): Unit =
         internalError()
 
@@ -74,7 +83,87 @@ abstract class KotlinBuildScript(
      *
      * @see [PluginDependenciesSpec]
      */
-    @Suppress("unused")
-    fun plugins(@Suppress("unused_parameter") block: PluginDependenciesSpecScope.() -> Unit): Unit =
+    open fun plugins(@Suppress("unused_parameter") block: PluginDependenciesSpec.() -> Unit): Unit =
         invalidPluginsCall()
 }
+
+
+/**
+ * @since 6.0
+ */
+internal
+object KotlinBuildScriptCompilationConfiguration : ScriptCompilationConfiguration({
+
+    implicitReceivers(
+        org.gradle.api.Project::class
+    )
+
+    compilerOptions(
+        "-jvm-target", "1.8",
+        "-Xjsr305=strict",
+        "-XXLanguage:+NewInference",
+        "-XXLanguage:+SamConversionForKotlinFunctions"
+    )
+
+    defaultImports(
+        "org.gradle.kotlin.dsl.*",
+        "org.gradle.api.*"
+    )
+
+    jvm {
+        dependenciesFromClassContext(
+            KotlinBuildScriptCompilationConfiguration::class,
+            "gradle-kotlin-dsl",
+            "gradle-kotlin-dsl-extensions",
+            "gradle-kotlin-dsl-tooling-models",
+            "gradle-api", "groovy-all",
+            "kotlin-stdlib", "kotlin-reflect"
+        )
+    }
+
+    annotationsForSamWithReceivers(
+        HasImplicitReceiver::class
+    )
+
+    ide {
+        acceptedLocations(ScriptAcceptedLocation.Everywhere)
+    }
+
+    refineConfiguration {
+        beforeCompiling { context ->
+
+            val scriptContents = object : ScriptContents {
+                override val annotations: Iterable<Annotation>
+                    get() = emptyList()
+                override val file: File?
+                    get() = (context.script as? ExternalSourceCode)?.externalLocation?.toURI()?.let(::File)
+                override val text: CharSequence?
+                    get() = context.script.text
+            }
+
+            val environment = scriptResolverEnvironmentOf(context)
+
+            val report: (ScriptDependenciesResolver.ReportSeverity, String, ScriptContents.Position?) -> Unit =
+                { severity, message, position ->
+                    // TODO: accumulate diagnostics
+                }
+
+            val result =
+                KotlinBuildScriptDependenciesResolver().run {
+                    resolve(
+                        scriptContents,
+                        environment,
+                        report,
+                        null
+                    )
+                }.get(3, TimeUnit.MINUTES)
+
+            context.compilationConfiguration.with {
+                result?.apply {
+                    updateClasspath(classpath.toList())
+                    defaultImports(imports.toList())
+                }
+            }.asSuccess()
+        }
+    }
+})
