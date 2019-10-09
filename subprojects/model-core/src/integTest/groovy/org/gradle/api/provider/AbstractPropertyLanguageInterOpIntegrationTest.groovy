ab5b45c2cab49a032b1ca018519663a05b53f2f7
Paul Merlin <paul@gradle.com>
2019-10-09 16:45:27 +0200

    Fix more integration tests that were missing the kotlin-eap repo
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/model-core/src/integTest/groovy/org/gradle/api/provider/AbstractPropertyLanguageInterOpIntegrationTest.groovy
+++ subprojects/model-core/src/integTest/groovy/org/gradle/api/provider/AbstractPropertyLanguageInterOpIntegrationTest.groovy
@@ -19,11 +19,14 @@ package org.gradle.api.provider
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.test.fixtures.dsl.GradleDsl
 import org.gradle.test.fixtures.file.TestFile
 import org.gradle.util.Requires
 import org.gradle.util.TestPrecondition
 
 import static org.gradle.integtests.fixtures.KotlinDslTestUtil.kotlinDslBuildSrcScript
+import static org.gradle.integtests.fixtures.RepoScriptBlockUtil.gradlePluginRepositoryDefinition
+import static org.gradle.integtests.fixtures.RepoScriptBlockUtil.kotlinEapRepositoryDefinition
 
 @Requires(TestPrecondition.KOTLIN_SCRIPT)
 abstract class AbstractPropertyLanguageInterOpIntegrationTest extends AbstractIntegrationSpec {
@@ -266,6 +269,12 @@ abstract class AbstractPropertyLanguageInterOpIntegrationTest extends AbstractIn
         pluginDefinesTask()
 
         file("buildSrc/settings.gradle.kts") << """
+            pluginManagement {
+                repositories {
+                    ${gradlePluginRepositoryDefinition(GradleDsl.KOTLIN)}
+                    ${kotlinEapRepositoryDefinition(GradleDsl.KOTLIN)}
+                }
+            }
             include("other")
         """
         file("buildSrc/build.gradle.kts") << """
