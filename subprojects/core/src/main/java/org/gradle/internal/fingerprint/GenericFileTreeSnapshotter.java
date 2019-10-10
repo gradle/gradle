e791a65932b9c83365a0fa5148d685131867b729
Stefan Wolf <wolf@gradle.com>
2019-10-10 19:20:42 +0200

    Add example for generic file tree to Javadoc
    `SingletonFileTree`s aren't generic file trees.

--- subprojects/core/src/main/java/org/gradle/internal/fingerprint/GenericFileTreeSnapshotter.java
+++ subprojects/core/src/main/java/org/gradle/internal/fingerprint/GenericFileTreeSnapshotter.java
@@ -23,6 +23,7 @@ import org.gradle.internal.snapshot.FileSystemSnapshot;
 /**
  * A snapshotter for generic file trees, which are not based on a directory on disk.
  *
+ * Examples of a generic file tree is a {@link org.gradle.api.internal.file.archive.TarFileTree} backed by a non-file resource.
  * This is needed to build a Merkle directory tree from the elements of a file tree obtained by {@link org.gradle.api.file.FileTree#visit(FileVisitor)}.
  */
 public interface GenericFileTreeSnapshotter {
