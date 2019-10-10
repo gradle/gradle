178890cefed76dce6783b4e82dfd7db217e49d6f
Stefan Wolf <wolf@gradle.com>
2019-10-10 13:39:32 +0200

    Add Javadoc to GenericFileTreeSnapshotter

--- subprojects/core/src/main/java/org/gradle/internal/fingerprint/GenericFileTreeSnapshotter.java
+++ subprojects/core/src/main/java/org/gradle/internal/fingerprint/GenericFileTreeSnapshotter.java
@@ -16,9 +16,15 @@
 
 package org.gradle.internal.fingerprint;
 
+import org.gradle.api.file.FileVisitor;
 import org.gradle.api.internal.file.FileTreeInternal;
 import org.gradle.internal.snapshot.FileSystemSnapshot;
 
+/**
+ * A snapshotter for generic file trees, which are not based on a directory on disk.
+ *
+ * This is needed to build a Merkle directory tree from the elements of a file tree obtained by {@link org.gradle.api.file.FileTree#visit(FileVisitor)}.
+ */
 public interface GenericFileTreeSnapshotter {
     FileSystemSnapshot snapshotFileTree(FileTreeInternal tree);
 }
