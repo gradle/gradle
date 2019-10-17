9d8e4bfa86d3ffb7aa0be162f3a7773b01b0c358
Luke Daley <luke@gradle.com>
2019-10-17 17:07:03 +1000

    Ignore AndroidCachingSmokeTest temporarily
    To be investigated

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidCachingSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidCachingSmokeTest.groovy
@@ -18,7 +18,9 @@ package org.gradle.smoketests
 
 import org.eclipse.jgit.api.Git
 import org.gradle.integtests.fixtures.executer.IntegrationTestBuildContext
+import spock.lang.Ignore
 
+@Ignore("Unexpectedly failing - 17-10-19 - LD")
 class AndroidCachingSmokeTest extends AbstractSmokeTest {
 
     def "can cache Santa Tracker Android application"() {
