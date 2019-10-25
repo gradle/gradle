2f9e23b697b195a0a4df461c172adfeac62430d8
Daniel Lacasse <daniel@gradle.com>
2019-10-25 14:35:37 +0300

    Add library publishing sample for Java and Groovy

---
+++ subprojects/docs/src/samples/next-gen/groovy/library-publishing/kotlin/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    groovy
+    `maven-publish`
+    eclipse
+    // See ??? for IntelliJ support
+}
+
+version = "1.0.2"
+group = "org.gradle.sample"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("org.codehaus.groovy:groovy-all:2.5.7")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("library") {
+            from(components.getByName("java"))
+        }
+    }
+    repositories {
+        maven {
+            url = uri("${buildDir}/publishing-repository")
+        }
+    }
+}
