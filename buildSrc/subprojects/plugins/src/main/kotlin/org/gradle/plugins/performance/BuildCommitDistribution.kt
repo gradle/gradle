cd98b4f9b189865c61c12c8a33fea94a941edc16
Stefan Wolf <wolf@gradle.com>
2019-10-27 13:01:46 +0100

    Use directory in root project to build commit distribution
    So directories are short enough for Windows m)

--- buildSrc/subprojects/plugins/src/main/kotlin/org/gradle/plugins/performance/BuildCommitDistribution.kt
+++ buildSrc/subprojects/plugins/src/main/kotlin/org/gradle/plugins/performance/BuildCommitDistribution.kt
@@ -55,7 +55,7 @@ open class BuildCommitDistribution : DefaultTask() {
     fun buildCommitDistribution() {
         val rootProjectDir = project.rootProject.rootDir.absolutePath
         val commit = commitBaseline.map { it.substring(it.lastIndexOf('-') + 1) }
-        tryBuildDistribution(RemoteProject.checkout(this, rootProjectDir, commit.get()))
+        tryBuildDistribution(RemoteProject.checkout(this, rootProjectDir, commit.get(), project.rootProject.buildDir.resolve("bcd")))
         println("Building the commit distribution succeeded, now the baseline is ${commitBaseline.get()}")
     }
 
