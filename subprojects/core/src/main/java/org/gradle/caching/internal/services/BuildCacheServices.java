344b0fb2f037f5bfa6d3456bb7f81c60516a3e9c
Lóránt Pintér <lorant@gradle.com>
2019-10-28 09:07:06 +0100

    Inject hostname to OriginMetadataFactory

--- subprojects/core/src/main/java/org/gradle/caching/internal/services/BuildCacheServices.java
+++ subprojects/core/src/main/java/org/gradle/caching/internal/services/BuildCacheServices.java
@@ -37,6 +37,7 @@ import org.gradle.internal.instantiation.InstantiatorFactory;
 import org.gradle.internal.nativeplatform.filesystem.FileSystem;
 import org.gradle.internal.operations.BuildOperationExecutor;
 import org.gradle.internal.os.OperatingSystem;
+import org.gradle.internal.remote.internal.inet.InetAddressFactory;
 import org.gradle.internal.scopeids.id.BuildInvocationScopeId;
 import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.internal.snapshot.FileSystemMirror;
@@ -60,8 +61,9 @@ public class BuildCacheServices {
     }
 
     OriginMetadataFactory createOriginMetadataFactory(
+        BuildInvocationScopeId buildInvocationScopeId,
         GradleInternal gradleInternal,
-        BuildInvocationScopeId buildInvocationScopeId
+        InetAddressFactory inetAddressFactory
     ) {
         File rootDir = gradleInternal.getRootProject().getRootDir();
         return new OriginMetadataFactory(
@@ -71,7 +73,8 @@ public class BuildCacheServices {
             buildInvocationScopeId.getId().asString(),
             properties -> {
                 properties.setProperty(GRADLE_VERSION_KEY, GradleVersion.current().getVersion());
-            }
+            },
+            inetAddressFactory::getHostname
         );
     }
 
