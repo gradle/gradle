b69efc0758f85d3841977d3ea2d024b5d0d0935d
Adam Murdoch <adam@gradle.com>
2019-10-29 16:59:27 +1100

    Remove unused import

--- subprojects/model-core/src/test/groovy/org/gradle/internal/instantiation/generator/AsmBackedClassGeneratorTest.java
+++ subprojects/model-core/src/test/groovy/org/gradle/internal/instantiation/generator/AsmBackedClassGeneratorTest.java
@@ -56,7 +56,6 @@ import org.gradle.internal.instantiation.InstantiatorFactory;
 import org.gradle.internal.metaobject.BeanDynamicObject;
 import org.gradle.internal.metaobject.DynamicObject;
 import org.gradle.internal.reflect.JavaReflectionUtil;
-import org.gradle.internal.reflect.annotations.impl.DefaultTypeAnnotationMetadataStoreTest;
 import org.gradle.internal.service.DefaultServiceRegistry;
 import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider;
