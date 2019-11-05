4a0900e9d6570c9986c357b7021ef0554aa71589
Donat Csikos <donat@gradle.com>
2019-11-05 15:03:07 +0100

    Make DefaultInternalJvmTestRequest serializable

--- subprojects/tooling-api/src/main/java/org/gradle/tooling/internal/consumer/DefaultInternalJvmTestRequest.java
+++ subprojects/tooling-api/src/main/java/org/gradle/tooling/internal/consumer/DefaultInternalJvmTestRequest.java
@@ -19,8 +19,9 @@ package org.gradle.tooling.internal.consumer;
 import org.gradle.tooling.internal.protocol.test.InternalJvmTestRequest;
 
 import javax.annotation.Nullable;
+import java.io.Serializable;
 
-public class DefaultInternalJvmTestRequest implements InternalJvmTestRequest {
+public class DefaultInternalJvmTestRequest implements InternalJvmTestRequest, Serializable {
     private final String className;
     private final String methodName;
 
