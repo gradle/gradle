7fb528ef4271b01dfae3f2f6986e60fe8bf1f261
Donat Csikos <donat@gradle.com>
2019-11-12 17:21:06 +0100

    Add test coverage for accessing gradle.properties entries in buildSrc and in included builds

---
+++ subprojects/wrapper/src/crossVersionTest/groovy/org/gradle/integtests/WrapperPropertiesLoaderCrossVersionTest.groovy
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.integtests
+
+
+import org.gradle.integtests.fixtures.CrossVersionIntegrationSpec
+import org.gradle.integtests.fixtures.daemon.DaemonLogsAnalyzer
+import org.gradle.integtests.fixtures.executer.GradleDistribution
+import org.gradle.integtests.fixtures.executer.GradleExecuter
+import org.gradle.util.GradleVersion
+import org.junit.Assume
+import spock.lang.Issue
+
+@SuppressWarnings("IntegrationTestFixtures")
+class WrapperPropertiesLoaderCrossVersionTest extends CrossVersionIntegrationSpec {
+
+    @Issue('https://github.com/gradle/gradle/issues/11173')
+    void "System properties defined in gradle.properties are available in buildSrc and in included builds"() {
+        given:
+        GradleDistribution wrapperVersion = previous
+        GradleDistribution executionVersion = current
+        Assume.assumeTrue("skipping $wrapperVersion as its wrapper cannot execute version ${executionVersion.version.version}", wrapperVersion.wrapperCanExecute(executionVersion.version))
+        Assume.assumeTrue("skipping execute version ${executionVersion.version.version} as it is <6.0", executionVersion.version >= GradleVersion.version("6.0"))
+
+        requireOwnGradleUserHomeDir()
+
+        settingsFile << 'includeBuild "includedBuild"'
+        buildFile << '''
+            println("system_property_available in root:                    ${System.getProperty('system_property_available', 'false')} ")
+            println("project_property_available in root:                   ${project.findProperty('project_property_available') ?: 'false'} ")
+            task hello { }
+        '''
+        file('buildSrc/build.gradle') << '''
+            println("system_property_available in buildSrc:                ${System.getProperty('system_property_available', 'false')} ")
+            println("project_property_available in buildSrc:               ${project.findProperty('project_property_available') ?: 'false'} ")
+        '''
+        file('includedBuild/build.gradle') << '''
+            println("system_property_available in includedBuild root:      ${System.getProperty('system_property_available', 'false')} ")
+            println("project_property_available in includedBuild root:     ${project.findProperty('project_property_available') ?: 'false'} ")
+        '''
+        file('includedBuild/buildSrc/build.gradle') << '''
+            println("system_property_available in includedBuild buildSrc:  ${System.getProperty('system_property_available', 'false')} ")
+            println("project_property_available in includedBuild buildSrc: ${project.findProperty('project_property_available') ?: 'false'} ")
+        '''
+        file('gradle.properties') << '''
+            systemProp.system_property_available=true
+            project_property_available=true
+        '''.stripIndent()
+
+        version(wrapperVersion).withTasks('wrapper').run()
+
+        when:
+        GradleExecuter executer = version(wrapperVersion).requireIsolatedDaemons()
+        String output =  executer.usingExecutable('gradlew').withTasks('hello').run().output
+
+        then:
+        output.contains('system_property_available in buildSrc:                true')
+        output.contains('system_property_available in buildSrc:                true')
+        output.contains('project_property_available in buildSrc:               false')
+        output.contains('system_property_available in includedBuild buildSrc:  true')
+        output.contains('project_property_available in includedBuild buildSrc: false')
+        output.contains('system_property_available in includedBuild root:      true')
+        output.contains('project_property_available in includedBuild root:     false')
+        output.contains('system_property_available in root:                    true')
+        output.contains('project_property_available in root:                   true')
+
+        cleanup:
+        cleanupDaemons(executer, executionVersion)
+    }
+
+    static void cleanupDaemons(GradleExecuter executer, GradleDistribution executionVersion) {
+        new DaemonLogsAnalyzer(executer.daemonBaseDir, executionVersion.version.version).killAll()
+    }
+}
+
