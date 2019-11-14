848d8924edd35e4e1f7a03ea31fc03dffa2b406e
Paul Merlin <paul@gradle.com>
2019-11-14 14:06:52 +0100

    Rewrite FailsWithInstantExecutionExtension spock extension
    to account for the combination of spock extensions, junit rules and
    features coming from abstract specs
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/FailsWithInstantExecutionExtension.groovy
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/FailsWithInstantExecutionExtension.groovy
@@ -17,32 +17,89 @@
 package org.gradle.integtests.fixtures
 
 import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
+import org.spockframework.runtime.AbstractRunListener
 import org.spockframework.runtime.extension.AbstractAnnotationDrivenExtension
 import org.spockframework.runtime.extension.IMethodInterceptor
 import org.spockframework.runtime.extension.IMethodInvocation
 import org.spockframework.runtime.model.FeatureInfo
+import org.spockframework.runtime.model.IterationInfo
+import org.spockframework.runtime.model.SpecInfo
 
 
 class FailsWithInstantExecutionExtension extends AbstractAnnotationDrivenExtension<FailsWithInstantExecution> {
 
     @Override
+    void visitSpec(SpecInfo spec) {
+        if (!GradleContextualExecuter.isInstant()) {
+            return
+        }
+        // This listener/interceptor gymnastic is required because of the ordering
+        // of spock hooks when combining extensions, junit rules, unrolled tests and class hierarchies
+        spec.allFeatures
+            .findAll { it.featureMethod.reflection.getAnnotation(FailsWithInstantExecution) != null }
+            .each { feature -> spec.addListener(new RunListener(feature)) }
+    }
+
+    @Override
     void visitFeatureAnnotation(FailsWithInstantExecution annotation, FeatureInfo feature) {
-        if (GradleContextualExecuter.isInstant()) {
-            feature.featureMethod.addInterceptor(new MethodInterceptor())
+        // This override is required to satisfy spock's zealous runtime checks
+    }
+
+    private static class RunListener extends AbstractRunListener {
+
+        private final MethodInterceptor featureInterceptor = new MethodInterceptor()
+        private final MethodInterceptor iterationInterceptor = new MethodInterceptor()
+
+        private final FeatureInfo feature
+
+        RunListener(FeatureInfo feature) {
+            this.feature = feature
+        }
+
+        @Override
+        void beforeFeature(FeatureInfo featureInfo) {
+            if (feature == featureInfo) {
+                println("before feature ${feature.name}")
+                feature.featureMethod.interceptors.add(0, featureInterceptor)
+            }
+        }
+
+        @Override
+        void beforeIteration(IterationInfo iteration) {
+            if (feature == iteration.feature) {
+                println("before iteration ${iteration.name}")
+                feature.iterationInterceptors.add(0, iterationInterceptor)
+            }
+        }
+
+        @Override
+        void afterFeature(FeatureInfo featureInfo) {
+            if (feature == featureInfo) {
+                println("after feature ${feature.name}")
+                def failures = featureInterceptor.failures + iterationInterceptor.failures
+                if (failures.empty) {
+                    throw new UnexpectedSuccessException()
+                } else {
+                    System.err.println("Failed with instant execution as expected:")
+                    failures.each {
+                        it.printStackTrace()
+                    }
+                }
+            }
         }
     }
 
     private static class MethodInterceptor implements IMethodInterceptor {
+
+        List<Throwable> failures = []
+
         @Override
         void intercept(IMethodInvocation invocation) throws Throwable {
+            println("> intercepting ${invocation.feature.name}")
             try {
                 invocation.proceed()
-                throw new UnexpectedSuccessException()
-            } catch (UnexpectedSuccessException ex) {
-                throw ex
             } catch (Throwable ex) {
-                System.err.println("Failed with instant execution as expected:")
-                ex.printStackTrace()
+                failures += ex
             }
         }
     }
