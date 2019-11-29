3adcdad68ae45bf331059aba02d89f5c5b7fc05e
Bo Zhang <bo@gradle.com>
2019-11-29 15:23:49 +0800

    Update subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/repository/ResolvingSnapshotFromPluginRepositorySpec.groovy

--- subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/repository/ResolvingSnapshotFromPluginRepositorySpec.groovy
+++ subprojects/plugin-use/src/integTest/groovy/org/gradle/plugin/repository/ResolvingSnapshotFromPluginRepositorySpec.groovy
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.gradle.plugin.repository
+
 import org.gradle.integtests.fixtures.AbstractDependencyResolutionTest
 import org.gradle.integtests.fixtures.ToBeFixedForInstantExecution
 import org.gradle.test.fixtures.Repository
