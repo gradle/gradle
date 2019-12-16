37d7504e8ff873ea3bea6066d4200f5e662694f1
Stefan Wolf <wolf@gradle.com>
2019-12-16 17:49:39 +0100

    Rebaseline LargeDependencyGraphPerformanceTest
    To unblock `master`.
    See https://github.com/gradle/gradle-private/issues/2881.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/LargeDependencyGraphPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/LargeDependencyGraphPerformanceTest.groovy
@@ -29,7 +29,7 @@ class LargeDependencyGraphPerformanceTest extends AbstractCrossVersionGradleProf
 
     def setup() {
         runner.minimumBaseVersion = '4.8'
-        runner.targetVersions = ["6.1-20191209153604+0000"]
+        runner.targetVersions = ["6.2-20191215230025+0000"]
     }
 
     def "resolve large dependency graph from file repo"() {
