312cc82f58de5cb043514a04d8e5d29d93d65da6
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-01-09 09:58:06 -0300

    Convert `gradle/versioning.gradle` to Kotlin plugin in buidlSrc
    Most of `versioning.gradle` becomes `BuildVersionPlugin`.
    
    The rest becomes `build-receipt.gradle`.
    
    Also, rename `testDependencies.gradle` to `test-dependencies.gradle` for consistency
    with all the other scripts.

---
+++ buildSrc/subprojects/versioning/src/main/kotlin/org/gradle/gradlebuild/versioning/BuildVersionPlugin.kt
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.gradlebuild.versioning
+
+import org.gradle.api.InvalidUserDataException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.build.BuildReceipt
+import org.gradle.gradlebuild.BuildEnvironment
+import org.gradle.kotlin.dsl.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.TimeZone
+
+
+class BuildVersionPlugin : Plugin<Project> {
+    override fun apply(project: Project) = project.setBuildVersionProperties()
+}
+
+
+private
+fun Project.setBuildVersionProperties() {
+    val milestoneNumber: String? by project
+    val rcNumber: String? by project
+    val finalRelease: Any? by project
+    val versionQualifier: String? by project
+    if ((milestoneNumber != null && rcNumber != null) ||
+        (rcNumber != null && finalRelease != null) ||
+        (milestoneNumber != null && finalRelease != null)) {
+        throw InvalidUserDataException(
+            "Cannot set any combination of milestoneNumber, rcNumber and finalRelease " +
+                "at the same time"
+        )
+    }
+
+    val isSnapshot = finalRelease == null && rcNumber == null && milestoneNumber == null
+    val isFinalRelease = finalRelease != null
+    val baseVersion = rootProject.file("version.txt").readText().trim()
+    val buildTimestamp = computeBuildTimestamp()
+    project.version = when {
+        isFinalRelease -> {
+            baseVersion
+        }
+        rcNumber != null -> {
+            "$baseVersion-rc-$rcNumber"
+        }
+        milestoneNumber != null -> {
+            "$baseVersion-milestone-$milestoneNumber"
+        }
+        versionQualifier != null -> {
+            "$baseVersion-$versionQualifier-$buildTimestamp"
+        }
+        else -> {
+            "$baseVersion-$buildTimestamp"
+        }
+    }
+    extra.let { ext ->
+        ext["milestoneNumber"] = milestoneNumber?.toInt()
+        ext["rcNumber"] = rcNumber?.toInt()
+        ext["finalRelease"] = isFinalRelease
+        ext["versionQualifier"] = versionQualifier
+        ext["isSnapshot"] = isSnapshot
+        ext["baseVersion"] = baseVersion
+        ext["buildTimestamp"] = buildTimestamp
+    }
+}
+
+
+private
+fun Project.computeBuildTimestamp(): String {
+    val ignoreIncomingBuildReceipt: Any? by project
+    val incomingBuildReceiptDir = file("incoming-distributions")
+    if (ignoreIncomingBuildReceipt == null && File(incomingBuildReceiptDir, BuildReceipt.BUILD_RECEIPT_FILE_NAME).exists()) {
+        val incomingDistributionsBuildReceipt = BuildReceipt.readBuildReceipt(incomingBuildReceiptDir)
+        val buildTimestamp = incomingDistributionsBuildReceipt["buildTimestamp"] as String
+        println("Using timestamp from incoming build receipt: $buildTimestamp")
+        return buildTimestamp
+    } else {
+        val timestampFormat = SimpleDateFormat("yyyyMMddHHmmssZ").apply {
+            timeZone = TimeZone.getTimeZone("UTC")
+        }
+        val buildTimestamp: String? by project
+        val buildTime = when {
+            buildTimestamp != null -> {
+                timestampFormat.parse(buildTimestamp)
+            }
+            BuildEnvironment.isCiServer || isRunningInstallTask() -> {
+                Date()
+            }
+            else -> {
+                Date().withoutTime()
+            }
+        }
+        return timestampFormat.format(buildTime)
+    }
+}
+
+
+private
+fun Project.isRunningInstallTask() =
+    listOf("install", "installAll")
+        .flatMap { listOf(":$it", it) }
+        .any(gradle.startParameter.taskNames::contains)
+
+
+private
+fun Date.withoutTime(): Date = SimpleDateFormat("yyyy-MM-dd").run {
+    parse(format(this@withoutTime))
+}
