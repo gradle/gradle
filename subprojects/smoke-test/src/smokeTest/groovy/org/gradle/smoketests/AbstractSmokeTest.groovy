fdbe6e6a36ace7b67455e7258843fccf0578c837
Paul Merlin <paul@gradle.com>
2020-01-14 17:49:48 +0100

    Smoke test Kotlin 1.3.70 EAP
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
@@ -92,7 +92,7 @@ abstract class AbstractSmokeTest extends Specification {
         static androidGradle = Versions.of(*AGP_VERSIONS.latestsPlusNightly)
 
         // https://search.maven.org/search?q=g:org.jetbrains.kotlin%20AND%20a:kotlin-project&core=gav
-        static kotlin = Versions.of('1.3.21', '1.3.31', '1.3.41', '1.3.50', '1.3.61')
+        static kotlin = Versions.of('1.3.21', '1.3.31', '1.3.41', '1.3.50', '1.3.61', '1.3.70-eap-42')
 
         // https://plugins.gradle.org/plugin/org.gretty
         static gretty = "3.0.1"
