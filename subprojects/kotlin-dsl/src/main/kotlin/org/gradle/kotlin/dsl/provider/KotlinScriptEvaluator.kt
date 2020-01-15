8b77108286ca27e129c09952207bc1cb24f2cade
Paul Merlin <paul@gradle.com>
2020-01-15 18:03:33 +0100

    TODO
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/provider/KotlinScriptEvaluator.kt
+++ subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/provider/KotlinScriptEvaluator.kt
@@ -139,6 +139,7 @@ class StandardKotlinScriptEvaluator(
             scriptHandler.dependencies,
             "classpath"
         )
+        // TODO:kotlin-dsl remove once not using an EAP
         scriptHandler.repositories.maven {
             it.setUrl("https://dl.bintray.com/kotlin/kotlin-eap/")
         }
