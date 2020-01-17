fa4957d0f82d2d3734997c83359b07bdcb1965a7
Cedric Champeau <cedric@gradle.com>
2020-01-17 13:52:12 +0100

    Avoid eager creation of the file access journal

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/DefaultArtifactCaches.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/DefaultArtifactCaches.java
@@ -16,11 +16,16 @@
 package org.gradle.api.internal.artifacts.ivyservice;
 
 import com.google.common.collect.ImmutableList;
+import org.apache.commons.lang.StringUtils;
 import org.gradle.cache.CacheRepository;
+import org.gradle.cache.PersistentIndexedCache;
 import org.gradle.cache.internal.CacheScopeMapping;
 import org.gradle.cache.internal.UsedGradleVersions;
+import org.gradle.internal.Factory;
 import org.gradle.internal.file.FileAccessTimeJournal;
+import org.gradle.internal.serialize.Serializer;
 
+import java.io.Closeable;
 import java.io.File;
 import java.util.List;
 import java.util.Optional;
@@ -28,14 +33,17 @@ import java.util.Optional;
 public class DefaultArtifactCaches implements ArtifactCachesProvider {
     private final DefaultArtifactCacheMetadata writableCacheMetadata;
     private final DefaultArtifactCacheMetadata readOnlyCacheMetadata;
-    private final WritableArtifactCacheLockingManager writableArtifactCacheLockingManager;
+    private final LateInitWritableArtifactCacheLockingManager writableArtifactCacheLockingManager;
     private final ReadOnlyArtifactCacheLockingManager readOnlyArtifactCacheLockingManager;
 
-    public DefaultArtifactCaches(CacheScopeMapping cacheScopeMapping, CacheRepository cacheRepository, FileAccessTimeJournal fileAccessTimeJournal, UsedGradleVersions usedGradleVersions) {
+    public DefaultArtifactCaches(CacheScopeMapping cacheScopeMapping, CacheRepository cacheRepository, Factory<WritableArtifactCacheLockingParameters> writableArtifactCacheLockingParametersFactory) {
         writableCacheMetadata = new DefaultArtifactCacheMetadata(cacheScopeMapping);
-        writableArtifactCacheLockingManager = new WritableArtifactCacheLockingManager(cacheRepository, writableCacheMetadata, fileAccessTimeJournal, usedGradleVersions);
-        String roCache = System.getProperty(READONLY_CACHE_PROPERTY);
-        if (roCache != null) {
+        writableArtifactCacheLockingManager = new LateInitWritableArtifactCacheLockingManager(() -> {
+            WritableArtifactCacheLockingParameters params = writableArtifactCacheLockingParametersFactory.create();
+            return new WritableArtifactCacheLockingManager(cacheRepository, writableCacheMetadata, params.getFileAccessTimeJournal(), params.getUsedGradleVersions());
+        });
+        String roCache = System.getenv(READONLY_CACHE_ENV_VAR);
+        if (StringUtils.isNotEmpty(roCache)) {
             readOnlyCacheMetadata = new DefaultArtifactCacheMetadata(cacheScopeMapping, new File(roCache));
             readOnlyArtifactCacheLockingManager = new ReadOnlyArtifactCacheLockingManager(cacheRepository, readOnlyCacheMetadata);
         } else {
@@ -81,6 +89,62 @@ public class DefaultArtifactCaches implements ArtifactCachesProvider {
         writableArtifactCacheLockingManager.close();
         if (readOnlyArtifactCacheLockingManager != null) {
             readOnlyArtifactCacheLockingManager.close();
+        }
+    }
+
+    public interface WritableArtifactCacheLockingParameters {
+        FileAccessTimeJournal getFileAccessTimeJournal();
+
+        UsedGradleVersions getUsedGradleVersions();
+    }
+
+    private static class LateInitWritableArtifactCacheLockingManager implements ArtifactCacheLockingManager, Closeable {
+        private final Factory<WritableArtifactCacheLockingManager> factory;
+        private volatile WritableArtifactCacheLockingManager delegate;
+
+        private LateInitWritableArtifactCacheLockingManager(Factory<WritableArtifactCacheLockingManager> factory) {
+            this.factory = factory;
+        }
+
+        private WritableArtifactCacheLockingManager getDelegate() {
+            if (delegate == null) {
+                synchronized (factory) {
+                    if (delegate == null) {
+                        delegate = factory.create();
+                    }
+                }
+            }
+            return delegate;
+        }
+
+        @Override
+        public void close() {
+            getDelegate().close();
+        }
+
+        @Override
+        public <T> T withFileLock(Factory<? extends T> action) {
+            return getDelegate().withFileLock(action);
+        }
+
+        @Override
+        public void withFileLock(Runnable action) {
+            getDelegate().withFileLock(action);
+        }
+
+        @Override
+        public <T> T useCache(Factory<? extends T> action) {
+            return getDelegate().useCache(action);
+        }
+
+        @Override
+        public void useCache(Runnable action) {
+            getDelegate().useCache(action);
+        }
+
+        @Override
+        public <K, V> PersistentIndexedCache<K, V> createCache(String cacheName, Serializer<K> keySerializer, Serializer<V> valueSerializer) {
+            return getDelegate().createCache(cacheName, keySerializer, valueSerializer);
         }
     }
 }
