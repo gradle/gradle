981b229a5d832c0d78c90a46b40d1d25e3c4446f
Stefan Wolf <wolf@gradle.com>
2020-01-23 16:00:20 +0100

    Run test with more invocations

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/android/FasterIncrementalAndroidBuildsPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/android/FasterIncrementalAndroidBuildsPerformanceTest.groovy
@@ -111,6 +111,7 @@ class FasterIncrementalAndroidBuildsPerformanceTest extends AbstractCrossBuildPe
             builder.invocation.args(AndroidGradlePluginVersions.OVERRIDE_VERSION_CHECK)
             builder.invocation.args("-Dorg.gradle.workers.max=8", "--no-build-cache", "--no-scan")
             builder.invocation.useToolingApi()
+            builder.invocationCount(100)
             applyEnterprisePlugin(builder)
             builder.addBuildMutator { InvocationSettings invocationSettings ->
                 new ClearInstantExecutionStateMutator(invocationSettings.projectDir, AbstractCleanupMutator.CleanupSchedule.SCENARIO)
