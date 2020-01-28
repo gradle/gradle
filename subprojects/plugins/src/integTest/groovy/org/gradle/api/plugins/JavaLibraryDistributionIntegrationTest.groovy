a38c8f81e73cf21eafdea630b74db716b74804e7
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-01-28 13:41:19 +0800

    Add documentation to property deprecation message in Distribution

--- subprojects/plugins/src/integTest/groovy/org/gradle/api/plugins/JavaLibraryDistributionIntegrationTest.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/api/plugins/JavaLibraryDistributionIntegrationTest.groovy
@@ -55,10 +55,10 @@ class JavaLibraryDistributionIntegrationTest extends WellBehavedPluginTest {
         def expandDir = file('expanded')
         file('build/distributions/DefaultJavaDistribution.zip').unzipTo(expandDir)
         expandDir.assertHasDescendants(
-                'DefaultJavaDistribution/lib/commons-collections-3.2.2.jar',
-                'DefaultJavaDistribution/lib/commons-cli-1.2.jar',
-                'DefaultJavaDistribution/lib/commons-lang-2.6.jar',
-                'DefaultJavaDistribution/DefaultJavaDistribution.jar')
+            'DefaultJavaDistribution/lib/commons-collections-3.2.2.jar',
+            'DefaultJavaDistribution/lib/commons-cli-1.2.jar',
+            'DefaultJavaDistribution/lib/commons-lang-2.6.jar',
+            'DefaultJavaDistribution/DefaultJavaDistribution.jar')
         expandDir.file('DefaultJavaDistribution/DefaultJavaDistribution.jar').assertIsCopyOf(file('build/libs/DefaultJavaDistribution.jar'))
     }
 
@@ -114,14 +114,14 @@ class JavaLibraryDistributionIntegrationTest extends WellBehavedPluginTest {
         def expandDir = file('expanded')
         file('build/distributions/SuperApp-1.2.zip').unzipTo(expandDir)
         expandDir.assertHasDescendants(
-                'SuperApp-1.2/lib/commons-lang-2.6.jar',
-                'SuperApp-1.2/file1.txt',
-                'SuperApp-1.2/dist1.txt',
-                'SuperApp-1.2/other1.txt',
-                'SuperApp-1.2/dir2/file2.txt',
-                'SuperApp-1.2/dir2/dist2.txt',
-                'SuperApp-1.2/dir2/other2.txt',
-                'SuperApp-1.2/canCreateADistributionWithSrcDistRuntime-1.2.jar')
+            'SuperApp-1.2/lib/commons-lang-2.6.jar',
+            'SuperApp-1.2/file1.txt',
+            'SuperApp-1.2/dist1.txt',
+            'SuperApp-1.2/other1.txt',
+            'SuperApp-1.2/dir2/file2.txt',
+            'SuperApp-1.2/dir2/dist2.txt',
+            'SuperApp-1.2/dir2/other2.txt',
+            'SuperApp-1.2/canCreateADistributionWithSrcDistRuntime-1.2.jar')
     }
 
     def "fails when distribution baseName is null"() {
@@ -140,6 +140,25 @@ class JavaLibraryDistributionIntegrationTest extends WellBehavedPluginTest {
         executer.noDeprecationChecks()
         runAndFail 'distZip'
         failure.assertHasCause "Cannot query the value of property 'distributionBaseName' because it has no value available."
+    }
+
+    def "emits deprecation warning when baseName property is used"() {
+        given:
+        buildFile << """
+            apply plugin:'java-library-distribution'
+
+            distributions {
+                main {
+                    baseName = 'sample'
+                }
+            }
+            """
+
+        expect:
+        executer.expectDocumentedDeprecationWarning("The Distribution.baseName property has been deprecated. This is scheduled to be removed in Gradle 7.0. " +
+            "Please use the distributionBaseName property instead. " +
+            "See https://docs.gradle.org/current/dsl/org.gradle.api.distribution.Distribution.html#org.gradle.api.distribution.Distribution:baseName for more details.")
+        succeeds 'distZip'
     }
 
     def "compile only dependencies are not included in distribution"() {
