fb3767cfda4bfbc905dee2442844a3eec0419662
Bo Zhang <bo@gradle.com>
2020-01-29 13:03:10 +0800

    Monitor file handle leaking on windows (#11900)
    We've been bitten by file leaking for a long time. This PR uses [the custom fork](https://github.com/blindpirate/file-leak-detector/) with some changes:
    
    - Original file leak detector only supports printing open file handles at shutdown time, but for Gradle daemon this doesn't work. We want to print all open file handles at the end of every build. We achieve this goal by setting up a tiny http server inside the daemon JVM and sending HTTP requests to notify it.
    
    So the whole monitor process is:
    
    - Replace `gradle.properties` with `gradle.windows.properties`, which has the javaagent JVM startup parameter.
    - The agent looks for an available port to listen, then prints the port number to `port.txt` file.
    - After each build, a script runs `java gradle/DumpOpenFiles.java`, which reads the port number and sends HTTP request to daemon. The generated file handle dump file will be at the project directory for further investigation if any file leaking happens.

---
+++ gradle/DumpOpenFiles.java
@@ -0,0 +1,29 @@
+import java.time.format.*;
+import java.time.*;
+import java.io.*;
+import java.nio.file.*;
+import java.net.*;
+import java.util.*;
+
+public class DumpOpenFiles {
+    public static void main(String[] args) throws Exception {
+        String url = "http://localhost:" + parsePort() + "/dump";
+        System.out.println("Sending request to " + url);
+        try (BufferedReader response = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
+            String line = null;
+            while ((line = response.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+
+    private static String parsePort() throws Exception {
+        if (new File("port.txt").isFile()) {
+            return Files.readAllLines(new File("port.txt").toPath()).get(0).trim();
+        } else {
+            System.err.println("Port not found, skip");
+            System.exit(0);
+            return "";
+        }
+    }
+}
