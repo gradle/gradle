3a86bbadbe76d4eb1aae6c60dacf41d7b96d25e5
Rene Groeschke <rene@gradle.com>
2020-01-29 14:58:35 +0100

    Apply further feedback on task graph details
    - introduce CalculateTaskGraphBuildOperationType.TaskIdentity interface
    - move PlannedTask to CalculateTaskGraphBuildOperationType.PlannedTask
    - handle multiple transformations in task dependencies

--- buildSrc/subprojects/buildquality/src/main/kotlin/org/gradle/gradlebuild/buildquality/classycle/ClassycleExtension.kt
+++ buildSrc/subprojects/buildquality/src/main/kotlin/org/gradle/gradlebuild/buildquality/classycle/ClassycleExtension.kt
@@ -21,7 +21,6 @@ import org.gradle.api.provider.ListProperty
 
 import org.gradle.kotlin.dsl.*
 
-
 open class ClassycleExtension(project: Project) {
 
     val excludePatterns: ListProperty<String> = project.objects.listProperty()
