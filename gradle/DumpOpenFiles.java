b490f6bd47f382b27e6bede8f39b9bea8b2346f0
Bo Zhang <bo@gradle.com>
2020-01-30 17:01:12 +0800

    Not fail the build if file-leak-detector server down

--- gradle/DumpOpenFiles.java
+++ gradle/DumpOpenFiles.java
@@ -14,6 +14,8 @@ public class DumpOpenFiles {
             while ((line = response.readLine()) != null) {
                 System.out.println(line);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
