91f4cc5489e6ebe37b3df763bd2b3f940d7c044e
Adam Murdoch <adam@gradle.com>
2020-02-12 10:09:14 +1100

    Use a newer Kotlin EAP to build Gradle, if enabled.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -96,6 +96,10 @@ allprojects {
             name = "kotlin-dev"
             url = uri("https://dl.bintray.com/kotlin/kotlin-dev")
         }
+        maven {
+            name = "kotlin-eap"
+            url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
+        }
     }
 }
 
