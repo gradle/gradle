f4e1d67af82dd2369233283df885d1d262a8a3d8
Bo Zhang <bo@gradle.com>
2020-02-18 12:09:13 +0800

    Don't wait forever when dumping open file handles

--- gradle/DumpOpenFilesOnFailure.java
+++ gradle/DumpOpenFilesOnFailure.java
@@ -5,25 +5,40 @@ import java.nio.file.*;
 import java.net.*;
 import java.util.*;
 import java.util.stream.*;
+import java.util.concurrent.*;
 
 public class DumpOpenFiles {
+    private static ExecutorService threadPool = Executors.newCachedThreadPool();
+
     public static void main(String[] args) throws Exception {
         if ("SUCCESS".equals(System.getenv("PREV_BUILD_STATUS"))) {
             System.out.println("All previous build steps succeeds, skip.");
             System.exit(0);
         }
 
-        for (String port : parsePorts()) {
-            String url = "http://localhost:" + port + "/dump";
-            System.out.println("Sending request to " + url);
-            try (BufferedReader response = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
-                String line = null;
-                while ((line = response.readLine()) != null) {
-                    System.out.println(line);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+
+        try {
+            for (String port : parsePorts()) {
+                Future<Void> future = (Future<Void>) threadPool.submit(() -> dumpOpenFilesFor(port));
+                future.get(60, TimeUnit.SECONDS);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
+        }
+    }
+
+    private static void dumpOpenFilesFor(String port) {
+        String url = "http://localhost:" + port + "/dump";
+        System.out.println("Sending request to " + url);
+        try (BufferedReader response = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
+            String line = null;
+            while ((line = response.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
