3eb3ec0692a31bf0d429e026639e2c52041c4296
Guy Brand <guy@gradle.com>
2020-02-25 14:51:13 +0100

    Lower the required free disc space limit on EC2 warmup build

--- .teamcity/Gradle_Util/buildTypes/WarmupEc2Agent.kt
+++ .teamcity/Gradle_Util/buildTypes/WarmupEc2Agent.kt
@@ -10,6 +10,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
 import jetbrains.buildServer.configs.kotlin.v2019_2.Requirement
 import jetbrains.buildServer.configs.kotlin.v2019_2.RequirementType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.freeDiskSpace
 
 object WarmupEc2Agent : BuildType({
     uuid = "980bce31-2a3e-4563-9717-7c03e184f4a4"
@@ -19,6 +20,13 @@ object WarmupEc2Agent : BuildType({
     vcs {
         root(AbsoluteId("Gradle_Branches_GradlePersonalBranches"))
         checkoutMode = CheckoutMode.ON_AGENT
+    }
+
+    features {
+        freeDiskSpace {
+            // Lower the limit such that the agent work directories aren't cleaned during the AMI baking process
+            requiredSpace = "100mb"
+        }
     }
 
     params {
