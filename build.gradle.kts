71518250ab13aa981ffeabfbf1bd765eaa492bac
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-02-26 12:03:18 +0200

    Test Gradle build with Groovy 2.5.10-SNAPSHOT
    Use a nightly version of Gradle that disallows NotYetImplementedASTTransformation in Groovy compiler classloader due to static references to a JUnit class.
    Check in and use a locally repackaged version of groovy-all with Groovy 2.5.10-SNAPSHOT.

--- build.gradle.kts
+++ build.gradle.kts
@@ -194,6 +194,14 @@ allprojects {
             name = "kotlin-eap"
             url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
         }
+        maven {
+            name = "Groovy snapshots"
+            url = uri("https://oss.jfrog.org/artifactory/oss-snapshot-local")
+        }
+        maven {
+            name = "local-groovy-all-snapshot"
+            url = uri("${rootProject.rootDir}/groovy-all-snapshot-repo")
+        }
     }
 
     // patchExternalModules lives in the root project - we need to activate normalization there, too.
