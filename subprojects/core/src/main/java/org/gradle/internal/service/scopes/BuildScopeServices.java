6abcc3fa4a01f276100955f0761096d705237be6
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-03-02 18:34:17 -0300

    Reinstate `PropertiesLoadingSettingsProcessor`
    To ensure `GradleProperties` is loaded before processing the settings scripts of
    any included builds.
    
    The integration with instant execution is mediated by the newly introduced
    `GradlePropertiesController` service which guarantees `GradleProperties` is
    loaded only once during a build and from the correct location.
    
    Fixes #12381

--- subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeServices.java
+++ subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeServices.java
@@ -121,10 +121,12 @@ import org.gradle.initialization.ClassLoaderRegistry;
 import org.gradle.initialization.ClassLoaderScopeListeners;
 import org.gradle.initialization.ClassLoaderScopeRegistry;
 import org.gradle.initialization.DefaultClassLoaderScopeRegistry;
+import org.gradle.initialization.DefaultGradlePropertiesController;
 import org.gradle.initialization.DefaultGradlePropertiesLoader;
 import org.gradle.initialization.DefaultProjectDescriptorRegistry;
 import org.gradle.initialization.DefaultSettingsLoaderFactory;
 import org.gradle.initialization.DefaultSettingsPreparer;
+import org.gradle.initialization.GradlePropertiesController;
 import org.gradle.initialization.IGradlePropertiesLoader;
 import org.gradle.initialization.InitScriptHandler;
 import org.gradle.initialization.InstantiatingBuildLoader;
@@ -133,6 +135,7 @@ import org.gradle.initialization.NotifyingBuildLoader;
 import org.gradle.initialization.ProjectAccessListener;
 import org.gradle.initialization.ProjectDescriptorRegistry;
 import org.gradle.initialization.ProjectPropertySettingBuildLoader;
+import org.gradle.initialization.PropertiesLoadingSettingsProcessor;
 import org.gradle.initialization.RootBuildCacheControllerSettingsProcessor;
 import org.gradle.initialization.ScriptEvaluatingSettingsProcessor;
 import org.gradle.initialization.SettingsEvaluatedCallbackFiringSettingsProcessor;
@@ -271,6 +274,18 @@ public class BuildScopeServices extends DefaultServiceRegistry {
         return new DefaultIsolatedAntBuilder(classPathRegistry, classLoaderFactory, moduleRegistry);
     }
 
+    protected GradleProperties createGradleProperties(
+        GradlePropertiesController gradlePropertiesController
+    ) {
+        return gradlePropertiesController.getGradleProperties();
+    }
+
+    protected GradlePropertiesController createGradlePropertiesController(
+        IGradlePropertiesLoader propertiesLoader
+    ) {
+        return new DefaultGradlePropertiesController(propertiesLoader);
+    }
+
     protected IGradlePropertiesLoader createGradlePropertiesLoader() {
         return new DefaultGradlePropertiesLoader((StartParameterInternal) get(StartParameter.class));
     }
@@ -293,13 +308,6 @@ public class BuildScopeServices extends DefaultServiceRegistry {
 
     protected ProviderFactory createProviderFactory(ValueSourceProviderFactory valueSourceProviderFactory) {
         return new DefaultProviderFactory(valueSourceProviderFactory);
-    }
-
-    protected GradleProperties createGradleProperties(
-        IGradlePropertiesLoader propertiesLoader,
-        BuildLayout buildLayout
-    ) {
-        return propertiesLoader.loadGradleProperties(buildLayout.getRootDirectory());
     }
 
     protected ActorFactory createActorFactory() {
@@ -441,6 +449,7 @@ public class BuildScopeServices extends DefaultServiceRegistry {
         ScriptHandlerFactory scriptHandlerFactory,
         Instantiator instantiator,
         ServiceRegistryFactory serviceRegistryFactory,
+        GradlePropertiesController gradlePropertiesController,
         GradleProperties gradleProperties,
         BuildOperationExecutor buildOperationExecutor,
         TextFileResourceLoader textFileResourceLoader
@@ -448,15 +457,18 @@ public class BuildScopeServices extends DefaultServiceRegistry {
         return new BuildOperationSettingsProcessor(
             new RootBuildCacheControllerSettingsProcessor(
                 new SettingsEvaluatedCallbackFiringSettingsProcessor(
-                    new ScriptEvaluatingSettingsProcessor(
-                        scriptPluginFactory,
-                        new SettingsFactory(
-                            instantiator,
-                            serviceRegistryFactory,
-                            scriptHandlerFactory
+                    new PropertiesLoadingSettingsProcessor(
+                        new ScriptEvaluatingSettingsProcessor(
+                            scriptPluginFactory,
+                            new SettingsFactory(
+                                instantiator,
+                                serviceRegistryFactory,
+                                scriptHandlerFactory
+                            ),
+                            gradleProperties,
+                            textFileResourceLoader
                         ),
-                        gradleProperties,
-                        textFileResourceLoader
+                        gradlePropertiesController
                     )
                 )
             ),
