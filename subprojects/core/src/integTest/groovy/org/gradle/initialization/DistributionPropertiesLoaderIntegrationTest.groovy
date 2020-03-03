5072537fa1afadb507b7a8047433137af5aa5b6a
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-03-03 11:42:06 -0300

    Augment `DistributionPropertiesLoaderIntegrationTest`
    Also test for the availability of Gradle properties in `settings.gradle`.

--- subprojects/core/src/integTest/groovy/org/gradle/initialization/DistributionPropertiesLoaderIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/initialization/DistributionPropertiesLoaderIntegrationTest.groovy
@@ -29,6 +29,11 @@ class DistributionPropertiesLoaderIntegrationTest extends AbstractIntegrationSpe
         settingsFile << '''
             includeBuild 'includedBuild'
             println("system_property_available in settings.gradle:          ${System.getProperty('system_property_available', 'false')} ")
+            try {
+                println("project_property_available in settings.gradle:         ${project_property_available} ")
+            } catch (MissingPropertyException e) {
+                println("project_property_available in settings.gradle:         false ")
+            }
         '''
         buildFile << '''
             println("system_property_available in root:                     ${System.getProperty('system_property_available', 'false')} ")
@@ -45,6 +50,11 @@ class DistributionPropertiesLoaderIntegrationTest extends AbstractIntegrationSpe
         '''
         file('includedBuild/settings.gradle') << '''
             println("system_property_available in included settings.gradle: ${System.getProperty('system_property_available', 'false')} ")
+            try {
+                println("project_property_available in included settings.gradle:${project_property_available} ")
+            } catch (MissingPropertyException e) {
+                println("project_property_available in included settings.gradle:false ")
+            }
         '''
         file('includedBuild/buildSrc/build.gradle') << '''
             println("system_property_available in included buildSrc:        ${System.getProperty('system_property_available', 'false')} ")
@@ -69,7 +79,9 @@ class DistributionPropertiesLoaderIntegrationTest extends AbstractIntegrationSpe
         outputContains('system_property_available in root:                     true')
         outputContains('project_property_available in root:                    true')
         outputContains('system_property_available in settings.gradle:          true')
+        outputContains('project_property_available in settings.gradle:         true')
         outputContains('system_property_available in included settings.gradle: true')
+        outputContains('project_property_available in included settings.gradle:false')
 
         cleanup:
         executer.withArguments("--stop", "--info").run()
