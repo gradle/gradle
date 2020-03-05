776a9356b65cd10890069f752ec9182b20f1905a
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-03-05 14:54:54 +0200

    Do not smoke test AspectJ plugin against JDK14 due to incompatible dependency
    Latest AspectJ 1.9.5 is not compatible with JDK14.

--- subprojects/internal-testing/src/main/groovy/org/gradle/util/TestPrecondition.groovy
+++ subprojects/internal-testing/src/main/groovy/org/gradle/util/TestPrecondition.groovy
@@ -130,6 +130,9 @@ enum TestPrecondition implements org.gradle.internal.Factory<Boolean> {
     JDK12_OR_LATER({
         JavaVersion.current() >= JavaVersion.VERSION_12
     }),
+    JDK13_OR_EARLIER({
+        JavaVersion.current() <= JavaVersion.VERSION_13
+    }),
     JDK7_POSIX({
         NOT_WINDOWS.fulfilled
     }),
