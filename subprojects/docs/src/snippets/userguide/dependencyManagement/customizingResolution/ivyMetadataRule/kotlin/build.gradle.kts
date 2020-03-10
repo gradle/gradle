67d83db26b7ab8abf59896ad159a3f8efddb7117
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-03-10 13:24:03 -0700

    ivy-component-metadata-rule sample should check for IvyModuleDescriptor existence to avoid breaking Maven modules
    Signed-off-by: Roberto Perez Alcolea <rperezalcolea@netflix.com>

--- subprojects/docs/src/snippets/userguide/dependencyManagement/customizingResolution/ivyMetadataRule/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/userguide/dependencyManagement/customizingResolution/ivyMetadataRule/kotlin/build.gradle.kts
@@ -15,6 +15,10 @@ open class IvyVariantDerivationRule : ComponentMetadataRule {
     @Inject open fun getObjects(): ObjectFactory = throw UnsupportedOperationException()
 
     override fun execute(context: ComponentMetadataContext) {
+        if(context.getDescriptor(IvyModuleDescriptor) == null) {
+            return
+        }
+
         context.details.addVariant("runtimeElements", "default") {
             attributes {
                 attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, getObjects().named(LibraryElements.JAR))
