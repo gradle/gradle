c55c269d2403a1ab28aededc5f95119dde8a20ff
Bo Zhang <bo@gradle.com>
2020-03-10 14:11:45 +0800

    Print daemon crash log if any exists
    See https://github.com/gradle/gradle-private/issues/3003
    
    When daemon crashs, we print its crash log.

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/daemon/DaemonLogsAnalyzer.groovy
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/daemon/DaemonLogsAnalyzer.groovy
@@ -56,6 +56,7 @@ class DaemonLogsAnalyzer implements DaemonsFixture {
     }
 
     void killAll() {
+        printCrashLogIfAny()
         allDaemons*.kill()
     }
 
@@ -68,7 +69,7 @@ class DaemonLogsAnalyzer implements DaemonsFixture {
         if (!daemonLogsDir.exists() || !daemonLogsDir.isDirectory()) {
             return []
         }
-        return daemonLogsDir.listFiles().findAll { it.name.endsWith('.log') }.collect { daemonForLogFile(it) }
+        return daemonLogsDir.listFiles().findAll { it.name.endsWith('.log') && !it.name.startsWith('hs_err') }.collect { daemonForLogFile(it) }
     }
 
     List<DaemonFixture> getVisible() {
@@ -94,5 +95,11 @@ class DaemonLogsAnalyzer implements DaemonsFixture {
 
     String getVersion() {
         return version
+    }
+
+    void printCrashLogIfAny() {
+        List<File> crashLogs = daemonLogsDir.listFiles().findAll { it.name.endsWith('.log') && it.name.startsWith('hs_err') }
+        crashLogs.each { println(it.text) }
+        assert crashLogs.empty: "Found crash los: ${crashLogs}"
     }
 }
