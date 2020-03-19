984a9ed46b96c155ffe073af1e4e68e2675df9e2
Bo Zhang <bo@gradle.com>
2020-03-19 16:47:40 +0800

    Fix message

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -149,7 +149,7 @@ if (isCiServer) {
     println("Current machine has ${Runtime.getRuntime().availableProcessors()} CPU cores")
 
     if (OperatingSystem.current().isWindows) {
-        require(Runtime.getRuntime().availableProcessors() >= 6) { "Windows CI machines must have at least 4 CPU cores!" }
+        require(Runtime.getRuntime().availableProcessors() >= 6) { "Windows CI machines must have at least 6 CPU cores!" }
     }
     gradle.buildFinished {
         allprojects.forEach { project ->
