1175474b6b3e2fbb12f1674ceee9bc7463676e61
Benjamin Muskalla <bmuskalla@gradle.com>
2020-03-24 14:03:12 +0100

    Simplify build script

--- subprojects/docs/src/samples/jvm-multi-project-coverage/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/jvm-multi-project-coverage/kotlin/build.gradle.kts
@@ -14,35 +14,31 @@ allprojects {
 
 subprojects {
     apply(plugin = "java")
-    apply(plugin = "jacoco")
 
     dependencies {
         testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     }
 
+    // configure code coverage
+    apply(plugin = "jacoco")
+    val reportTask = task<JacocoReport>("codeCoverageReport")
+    reportTask.sourceSets(sourceSets.main.get())
     tasks.test {
         useJUnitPlatform()
+        reportTask.executionData(this)
     }
-
 }
 
 tasks.register<JacocoReport>("codeCoverageReport") {
-    executionData(fileTree(project.rootDir.absolutePath).include("**/build/jacoco/*.exec"))
-    subprojects.forEach {
-        sourceSets(it.sourceSets.getByName("main"))
-    }
-
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Generates code coverage report for all test tasks in all subprojects."
     reports() {
         xml.isEnabled = false
         html.isEnabled = true
         csv.isEnabled = false
     }
-
-}
-
-
-val reportTask = tasks.get("codeCoverageReport")
-getTasksByName("test", true).forEach {
-    reportTask.dependsOn(it)
+    subprojects.forEach { sub ->
+        dependsOn(sub.getTasksByName("test", true))
+    }
 }
