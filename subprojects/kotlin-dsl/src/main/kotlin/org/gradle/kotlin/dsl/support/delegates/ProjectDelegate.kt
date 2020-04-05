db21fa2e7cd23da4c459d4b1ef73253dfb8dd09c
Jeff Alder <49817386+jeffalder@users.noreply.github.com>
2020-04-05 15:43:23 -0700

    Add zipTree(Map) to Kotlin DSL #11983
    Signed-off-by: Jeff Alder <49817386+jeffalder@users.noreply.github.com>

--- subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/support/delegates/ProjectDelegate.kt
+++ subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/support/delegates/ProjectDelegate.kt
@@ -154,6 +154,9 @@ abstract class ProjectDelegate : Project {
     override fun zipTree(zipPath: Any): FileTree =
         delegate.zipTree(zipPath)
 
+    override fun zipTree(args: MutableMap<String, *>): FileTree =
+        delegate.zipTree(args)
+
     override fun allprojects(action: Action<in Project>) =
         delegate.allprojects(action)
 
