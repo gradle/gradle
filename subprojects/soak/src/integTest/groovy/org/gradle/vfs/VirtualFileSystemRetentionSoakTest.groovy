470ae6aca48450e468c36a926177623cfca62a4d
Bo Zhang <bo@gradle.com>
2020-04-07 09:05:34 +0800

    Disable VirtualFileSystemRetentionSoakTest for now

--- subprojects/soak/src/integTest/groovy/org/gradle/vfs/VirtualFileSystemRetentionSoakTest.groovy
+++ subprojects/soak/src/integTest/groovy/org/gradle/vfs/VirtualFileSystemRetentionSoakTest.groovy
@@ -24,6 +24,7 @@ import org.gradle.internal.vfs.watch.FileWatcherRegistry
 import org.gradle.soak.categories.SoakTest
 import org.gradle.test.fixtures.file.TestFile
 import org.junit.experimental.categories.Category
+import spock.lang.Ignore
 
 @Category(SoakTest)
 class VirtualFileSystemRetentionSoakTest extends DaemonIntegrationSpec implements VfsRetentionFixture {
@@ -63,6 +64,7 @@ class VirtualFileSystemRetentionSoakTest extends DaemonIntegrationSpec implement
         }
     }
 
+    @Ignore('https://github.com/gradle/gradle-private/issues/3041')
     def "file watching works with multiple builds on the same daemon"() {
         def numberOfChangesBetweenBuilds = maxFileChangesWithoutOverflow
 
@@ -97,6 +99,7 @@ class VirtualFileSystemRetentionSoakTest extends DaemonIntegrationSpec implement
         }
     }
 
+    @Ignore('https://github.com/gradle/gradle-private/issues/3041')
     def "file watching works with many changes between two builds"() {
         // Use 40 minutes idle timeout since the test may be running longer with an idle daemon
         executer.withDaemonIdleTimeoutSecs(2400)
