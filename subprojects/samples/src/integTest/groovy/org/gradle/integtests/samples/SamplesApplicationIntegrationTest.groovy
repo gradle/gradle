caf12b0bf9f190cbeaf2896751c9b3b17880860f
Jendrik Johannes <jendrik@gradle.com>
2020-04-07 11:41:45 +0200

    Simplify or move snippet tests that only run on Java 9+

--- subprojects/samples/src/integTest/groovy/org/gradle/integtests/samples/SamplesApplicationIntegrationTest.groovy
+++ subprojects/samples/src/integTest/groovy/org/gradle/integtests/samples/SamplesApplicationIntegrationTest.groovy
@@ -20,9 +20,12 @@ import org.gradle.integtests.fixtures.Sample
 import org.gradle.integtests.fixtures.ScriptExecuter
 import org.gradle.integtests.fixtures.ToBeFixedForInstantExecution
 import org.gradle.test.fixtures.file.TestFile
+import org.gradle.util.Requires
+import org.gradle.util.TestPrecondition
 import org.junit.Rule
 import spock.lang.Unroll
 
+@Requires(TestPrecondition.JDK9_OR_LATER)
 class SamplesApplicationIntegrationTest extends AbstractIntegrationSpec {
 
     @Rule Sample sample = new Sample(temporaryFolder, 'application')
