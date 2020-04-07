3b8baa66e730701962515d20daadc566c461820f
Jendrik Johannes <jendrik@gradle.com>
2020-04-07 08:45:48 +0200

    Add section on building Java Modules

---
+++ subprojects/docs/src/snippets/java-library/module/groovy/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module org.gradle.sample {
+    exports org.gradle.sample;
+    requires com.google.gson;
+}
---
+++ subprojects/docs/src/snippets/java-library/module/kotlin/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+// tag::declareVersion[]
+version = "1.1"
+
+tasks.compileJava {
+    options.javaModuleVersion.set("1.2")
+}
+// end::declareVersion[]
+
+// tag::inferModulePath[]
+java {
+    modularClasspathHandling.inferModulePath.set(true)
+}
+// end::inferModulePath[]
+
+// tag::dependencies[]
+dependencies {
+    implementation("com.google.code.gson:gson:2.8.6")       // real module
+    implementation("org.apache.commons:commons-lang3:3.10") // automatic module
+    implementation("commons-cli:commons-cli:1.4")           // plain library
+}
+// end::dependencies[]
+
+// tag::automaticModuleName[]
+tasks.jar {
+    manifest {
+        attributes("Automatic-Module-Name" to "org.gradle.sample")
+    }
+}
+// end::automaticModuleName[]
