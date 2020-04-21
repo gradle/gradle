a9e73b04ff56533af7abe1dcf0a8154dc99a701d
Adam Murdoch <adam@gradle.com>
2020-04-21 11:20:54 +1000

    Don't discard the Jar manifest when transforming plugin classes.

--- subprojects/core/src/main/java/org/gradle/internal/classpath/DecoratingTransformer.java
+++ subprojects/core/src/main/java/org/gradle/internal/classpath/DecoratingTransformer.java
@@ -23,13 +23,19 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 public class DecoratingTransformer {
     private static final Type SYSTEM_TYPE = Type.getType(System.class);
     private static final Type STRING_TYPE = Type.getType(String.class);
-    private static final Type CLASS_TYPE = Type.getType(Class.class);
     private static final Type INSTRUMENTED_TYPE = Type.getType(Instrumented.class);
+    private static final Attributes.Name DIGEST_ATTRIBUTE = new Attributes.Name("SHA1-Digest");
 
     private final ClasspathWalker classpathWalker;
     private final ClasspathBuilder classpathBuilder;
@@ -48,11 +54,27 @@ public class DecoratingTransformer {
                 reader.accept(new InstrumentingVisitor(classWriter), 0);
                 byte[] bytes = classWriter.toByteArray();
                 builder.put(entry.getName(), bytes);
-            } else if (!entry.getName().equals("META-INF/MANIFEST.MF")) {
+            } else if (entry.getName().equals("META-INF/MANIFEST.MF")) {
+                // Remove the signature from the manifest, as the classes may have been instrumented
+                Manifest manifest = new Manifest(new ByteArrayInputStream(entry.getContent()));
+                manifest.getMainAttributes().remove(Attributes.Name.SIGNATURE_VERSION);
+                Iterator<Map.Entry<String, Attributes>> entries = manifest.getEntries().entrySet().iterator();
+                while (entries.hasNext()) {
+                    Map.Entry<String, Attributes> manifestEntry = entries.next();
+                    Attributes attributes = manifestEntry.getValue();
+                    attributes.remove(DIGEST_ATTRIBUTE);
+                    if (attributes.isEmpty()) {
+                        entries.remove();
+                    }
+                }
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                manifest.write(outputStream);
+                builder.put(entry.getName(), outputStream.toByteArray());
+            } else if (!entry.getName().startsWith("META-INF/") || !entry.getName().endsWith(".SF")) {
+                // Discard signature files, as the classes may have been instrumented
+                // Else, copy resource
                 builder.put(entry.getName(), entry.getContent());
             }
-            // Else - discard the manifest
-            // TODO - filter the manifest to remove digests for instrumented classes
         }));
     }
 
