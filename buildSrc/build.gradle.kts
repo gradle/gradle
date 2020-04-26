ff00e5b9703f2a226072663f9414088d0e1ec591
Paul Merlin <paul@gradle.com>
2020-04-26 12:32:56 +0200

    Update kotlin-dsl-conventions code quality plugin to 0.5.0
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -20,7 +20,7 @@ import java.util.Properties
 plugins {
     java
     `kotlin-dsl` apply false
-    id("org.gradle.kotlin-dsl.ktlint-convention") version "0.4.1" apply false
+    id("org.gradle.kotlin-dsl.ktlint-convention") version "0.5.0" apply false
 }
 
 subprojects {
