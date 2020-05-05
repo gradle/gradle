dd675f712698276d33672e033e6491ad6df200e4
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-05-05 08:58:56 +0300

    Use ProviderFactory APIs for obtaining project properties in sample

--- subprojects/docs/src/samples/user-input/pass-credentials-to-external-tool-via-stdin/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/user-input/pass-credentials-to-external-tool-via-stdin/kotlin/build.gradle.kts
@@ -1,13 +1,24 @@
 val login = tasks.register<Exec>("login") {
-    val username: String by project
-    val password: String by project
-    standardInput = java.io.ByteArrayInputStream("$username\n$password".toByteArray())
+    val USERNAME_PROPERTY = "username"
+    val PASSWORD_PROPERTY = "password"
+    val providers = project.getProviders()
+    val username = providers.gradleProperty(USERNAME_PROPERTY)
+    val password = providers.gradleProperty(PASSWORD_PROPERTY)
+
+    doFirst {
+        if (!username.isPresent() || !password.isPresent()) {
+            throw GradleException(String.format("login task requires '%s' and '%s' properties",
+                USERNAME_PROPERTY, PASSWORD_PROPERTY))
+        }
+    }
+
+    standardInput = java.io.ByteArrayInputStream("${username.orNull}\n${password.orNull}".toByteArray())
     commandLine = listOf("sh", "login.sh")
 }
 
 tasks.register("doAuthenticated") {
     dependsOn(login)
     doLast {
-        println("doAuthenticated")
+        println("Doing authenticated task")
     }
 }
