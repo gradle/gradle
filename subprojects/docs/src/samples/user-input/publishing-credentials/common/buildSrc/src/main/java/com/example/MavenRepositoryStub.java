cd6281c63f883b0e70cd0708429c62fc15589a2b
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-05-07 17:06:50 +0300

    Polish maven publishing with credentials sample and add a README

--- subprojects/docs/src/samples/user-input/publishing-credentials/common/buildSrc/src/main/java/com/example/MavenRepositoryStub.java
+++ subprojects/docs/src/samples/user-input/publishing-credentials/common/buildSrc/src/main/java/com/example/MavenRepositoryStub.java
@@ -7,15 +7,19 @@ import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.notFound;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 import static com.github.tomakehurst.wiremock.client.WireMock.put;
 import static com.github.tomakehurst.wiremock.client.WireMock.unauthorized;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
-public class MavenHttpRepository {
+public class MavenRepositoryStub {
 
+    // tag::credentials[]
     private static final String USERNAME = "secret-user";
     private static final String PASSWORD = "secret-password";
+    // end::credentials[]
 
     private static final int PORT = randomFreePort();
     private static final WireMockServer SERVER = new WireMockServer(wireMockConfig().port(PORT));
@@ -25,6 +29,7 @@ public class MavenHttpRepository {
 
         SERVER.stubFor(put(anyUrl()).willReturn(unauthorized()));
         SERVER.stubFor(put(anyUrl()).withBasicAuth(USERNAME, PASSWORD).willReturn(ok()));
+        SERVER.stubFor(get(anyUrl()).withBasicAuth(USERNAME, PASSWORD).willReturn(notFound()));
     }
 
     public static void stop() {
