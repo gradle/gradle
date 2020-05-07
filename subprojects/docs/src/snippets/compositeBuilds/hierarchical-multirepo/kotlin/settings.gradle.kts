2b739e2453aa6eaddbc110a6a2b1ba7767fd472e
Donat Csikos <donat@gradle.com>
2020-05-07 18:16:42 +0200

    Convert hierarchical multirepo composite build snippet to sample

--- subprojects/docs/src/snippets/compositeBuilds/hierarchical-multirepo/kotlin/settings.gradle.kts
+++ subprojects/docs/src/snippets/compositeBuilds/hierarchical-multirepo/kotlin/settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "multirepo-app"
-
+// tag::include_builds[]
 file("modules").listFiles().forEach { moduleBuild: File ->
     includeBuild(moduleBuild)
 }
+// end::include_builds[]
