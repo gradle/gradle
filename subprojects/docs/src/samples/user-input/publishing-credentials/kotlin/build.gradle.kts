a09d6ff81e4503795ac75b83458200e1d67a6a29
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-05-10 21:04:19 +0300

    Update publishing credentials sample to require credentials only when publishing

--- subprojects/docs/src/samples/user-input/publishing-credentials/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/user-input/publishing-credentials/kotlin/build.gradle.kts
@@ -7,11 +7,6 @@ version = "1.0.2"
 group = "com.example"
 
 // tag::publication[]
-val MAVEN_USERNAME_PROPERTY = "mavenUser"
-val MAVEN_PASSWORD_PROPERTY = "mavenPassword"
-val mavenUser = providers.gradleProperty(MAVEN_USERNAME_PROPERTY)
-val mavenPassword = providers.gradleProperty(MAVEN_PASSWORD_PROPERTY)
-
 publishing {
     publications {
         create<MavenPublication>("library") {
@@ -22,25 +17,31 @@ publishing {
         maven {
             name = "mySecure"
             url = uri(com.example.MavenRepositoryStub.address())
-            credentials {
-                username = mavenUser.orNull
-                password = mavenPassword.orNull
-            }
         }
     }
 }
 // end::publication[]
 
-// tag::validation[]
-tasks.named("publishLibraryPublicationToMySecureRepository") {
-    doFirst {
+// tag::credentials[]
+gradle.taskGraph.whenReady {
+    if (allTasks.any { it.getName() == "publishLibraryPublicationToMySecureRepository" }) {
+        val MAVEN_USERNAME_PROPERTY = "mavenUser"
+        val MAVEN_PASSWORD_PROPERTY = "mavenPassword"
+        val mavenUser = providers.gradleProperty(MAVEN_USERNAME_PROPERTY)
+        val mavenPassword = providers.gradleProperty(MAVEN_PASSWORD_PROPERTY)
         if (!mavenUser.isPresent() || !mavenPassword.isPresent()) {
             throw GradleException(String.format("Publishing requires '%s' and '%s' properties",
                 MAVEN_USERNAME_PROPERTY, MAVEN_PASSWORD_PROPERTY))
         }
+        publishing.repositories.named("mySecure", MavenArtifactRepository::class).configure {
+            credentials {
+                username = mavenUser.get()
+                password = mavenPassword.get()
+            }
+        }
     }
 }
-// end::validation[]
+// end::credentials[]
 
 tasks.named("publishLibraryPublicationToMySecureRepository") {
     doFirst {
