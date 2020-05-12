5fa18dbaa556e4f4bec4314dd97f538615971e65
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-05-12 11:07:44 +0300

    Add .forUseAtConfigurationTime() for credential properties evaluated at configuration time

--- subprojects/docs/src/samples/credentials-handling/pass-credentials-to-external-tool-via-stdin/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/credentials-handling/pass-credentials-to-external-tool-via-stdin/kotlin/build.gradle.kts
@@ -1,8 +1,8 @@
 val login = tasks.register<Exec>("login") {
     val USERNAME_PROPERTY = "username"
     val PASSWORD_PROPERTY = "password"
-    val username = providers.gradleProperty(USERNAME_PROPERTY)
-    val password = providers.gradleProperty(PASSWORD_PROPERTY)
+    val username = providers.gradleProperty(USERNAME_PROPERTY).forUseAtConfigurationTime()
+    val password = providers.gradleProperty(PASSWORD_PROPERTY).forUseAtConfigurationTime()
 
     doFirst {
         if (!username.isPresent() || !password.isPresent()) {
