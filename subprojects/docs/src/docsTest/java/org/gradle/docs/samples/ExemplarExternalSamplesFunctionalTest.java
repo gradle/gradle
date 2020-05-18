75d2315d23a25f13abb8ed16e8a5b47d38b9ed16
Sterling Greene <sterling@gradle.com>
2020-05-17 23:59:22 -0400

    Fix checkstyle

--- subprojects/docs/src/docsTest/java/org/gradle/docs/samples/ExemplarExternalSamplesFunctionalTest.java
+++ subprojects/docs/src/docsTest/java/org/gradle/docs/samples/ExemplarExternalSamplesFunctionalTest.java
@@ -1,4 +1,19 @@
-//CHECKSTYLE:OFF
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.gradle.docs.samples;
 
 import org.gradle.integtests.fixtures.FailOnDeprecationSampleModifier;
@@ -15,7 +30,6 @@ import org.gradle.samples.test.normalizer.GradleOutputNormalizer;
 import org.gradle.samples.test.runner.GradleSamplesRunner;
 import org.gradle.samples.test.runner.SampleModifiers;
 import org.gradle.samples.test.runner.SamplesOutputNormalizers;
-import org.gradle.samples.test.runner.SamplesRoot;
 import org.junit.runner.RunWith;
 
 @RunWith(GradleSamplesRunner.class)
