a96dc9da1549276fa6a6868b6eac56eff630eb17
Stefan Wolf <wolf@gradle.com>
2020-05-19 23:11:33 +0200

    Do not invalidate the VFS
    deleting everything within the directory
    should be good enough.

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/AbstractIntegrationSpec.groovy
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/AbstractIntegrationSpec.groovy
@@ -18,7 +18,6 @@ package org.gradle.integtests.fixtures
 import org.gradle.api.Action
 import org.gradle.integtests.fixtures.build.BuildTestFile
 import org.gradle.integtests.fixtures.build.BuildTestFixture
-import org.gradle.integtests.fixtures.daemon.DaemonLogsAnalyzer
 import org.gradle.integtests.fixtures.executer.ArtifactBuilder
 import org.gradle.integtests.fixtures.executer.ExecutionFailure
 import org.gradle.integtests.fixtures.executer.ExecutionResult
@@ -98,10 +97,6 @@ class AbstractIntegrationSpec extends Specification {
     }
 
     def cleanup() {
-        if (GradleContextualExecuter.vfsRetention) {
-            new DaemonLogsAnalyzer(executer.daemonBaseDir)
-                .notifyAboutChangedFiles(temporaryFolder.testDirectory)
-        }
         executer.cleanup()
     }
 
