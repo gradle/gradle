599b7074044d19f386dca3804766f8867cc8905e
Louis Jacomet <louis@gradle.com>
2020-06-03 09:37:43 +0200

    Fix typo in version declaration snippet

--- subprojects/docs/src/snippets/dependencyManagement/declaringDependencies-concreteVersion/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/dependencyManagement/declaringDependencies-concreteVersion/kotlin/build.gradle.kts
@@ -54,13 +54,13 @@ dependencies {
            strictly("1.7.15")
         }
     }
-    
+
     // or...
     implementation("org.slf4j:slf4j-api:[1.7, 1.8[!!1.7.25")
     // is equivalent to
     implementation("org.slf4j:slf4j-api") {
         version {
-           strictly([1.7, 1.8[")
+           strictly("[1.7, 1.8[")
            prefer("1.7.25")
         }
     }
