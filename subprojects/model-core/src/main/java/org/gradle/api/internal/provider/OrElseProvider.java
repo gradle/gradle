5658a92268baf479b7085558909d0d10ecc22b0d
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-06-04 13:40:45 -0300

    Let `OrElseProvider` consider both `left` and `right` providers as value producers
    Which ensures task dependencies from both providers are visited and included in
    the graph.

--- subprojects/model-core/src/main/java/org/gradle/api/internal/provider/OrElseProvider.java
+++ subprojects/model-core/src/main/java/org/gradle/api/internal/provider/OrElseProvider.java
@@ -35,11 +35,7 @@ class OrElseProvider<T> extends AbstractMinimalProvider<T> {
 
     @Override
     public ValueProducer getProducer() {
-        if (left.isPresent()) {
-            return left.getProducer();
-        } else {
-            return right.getProducer();
-        }
+        return left.getProducer().plus(right.getProducer());
     }
 
     @Override
