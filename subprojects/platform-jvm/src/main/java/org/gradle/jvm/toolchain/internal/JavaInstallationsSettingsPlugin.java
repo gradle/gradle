df8ab2573e61e84e8efc331dfc785a600b11437f
Benjamin Muskalla <bmuskalla@gradle.com>
2020-06-09 13:29:13 +0200

    Introduce settings plugin for java installation

---
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/internal/JavaInstallationsSettingsPlugin.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.jvm.toolchain.internal;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.initialization.Settings;
+import org.gradle.jvm.toolchain.JavaInstallationContainer;
+
+import javax.inject.Inject;
+
+public class JavaInstallationsSettingsPlugin implements Plugin<Settings> {
+
+    private final JavaInstallationContainer container;
+
+    @Inject
+    public JavaInstallationsSettingsPlugin(JavaInstallationContainer container) {
+        this.container = container;
+    }
+
+    public void apply(Settings settings) {
+        settings.getExtensions().add("javaInstallations", container);
+    }
+
+}
