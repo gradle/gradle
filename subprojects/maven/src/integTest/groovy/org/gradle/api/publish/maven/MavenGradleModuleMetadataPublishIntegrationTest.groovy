c9512172dd5a20a35a6ed4b1bf62d6619ad80481
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-06-16 19:04:57 -0300

    Remove `@ToBeFixedForInstantExecution` from `MavenGradleModuleMetadataPublishIntegrationTest`

--- subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenGradleModuleMetadataPublishIntegrationTest.groovy
+++ subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenGradleModuleMetadataPublishIntegrationTest.groovy
@@ -62,7 +62,6 @@ class TestCapability implements Capability {
 """
     }
 
-    @ToBeFixedForInstantExecution
     def "fails to generate metadata for component with no variants"() {
         given:
         settingsFile << "rootProject.name = 'root'"
@@ -94,7 +93,6 @@ class TestCapability implements Capability {
   - This publication must publish at least one variant"""
     }
 
-    @ToBeFixedForInstantExecution
     def "fails to generate Gradle metadata if 2 variants have the same attributes"() {
         given:
         settingsFile.text = """
@@ -195,7 +193,6 @@ class TestCapability implements Capability {
         module.parsedModuleMetadata.variants.size() == 2
     }
 
-    @ToBeFixedForInstantExecution
     def "fails to generate Gradle metadata if 2 variants have the same name"() {
         given:
         settingsFile.text = """
@@ -243,7 +240,6 @@ class TestCapability implements Capability {
   - It is invalid to have multiple variants with the same name ('api')"""
     }
 
-    @ToBeFixedForInstantExecution
     def "fails to generate Gradle metadata if a variant doesn't have attributes"() {
         given:
         settingsFile.text = """
@@ -289,7 +285,6 @@ class TestCapability implements Capability {
   - Variant 'api' must declare at least one attribute."""
     }
 
-    @ToBeFixedForInstantExecution
     def "fails to produce Gradle metadata if no dependencies have version information"() {
         given:
         settingsFile.text = """
@@ -336,7 +331,6 @@ class TestCapability implements Capability {
     }
 
     @Unroll
-    @ToBeFixedForInstantExecution
     def "publishes Gradle metadata redirection marker when Gradle metadata task is enabled (enabled=#enabled)"() {
         given:
         settingsFile.text = """
