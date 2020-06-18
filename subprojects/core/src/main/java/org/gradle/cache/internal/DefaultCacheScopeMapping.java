e913ced79466535d0fd42bf534097132216e2866
Stefan Wolf <wolf@gradle.com>
2020-06-18 17:40:34 +0200

    Extract cache key name pattern to constant

--- subprojects/core/src/main/java/org/gradle/cache/internal/DefaultCacheScopeMapping.java
+++ subprojects/core/src/main/java/org/gradle/cache/internal/DefaultCacheScopeMapping.java
@@ -27,11 +27,13 @@ import org.gradle.util.GradleVersion;
 import javax.annotation.Nullable;
 import java.io.File;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class DefaultCacheScopeMapping implements CacheScopeMapping, GlobalCache {
 
     @VisibleForTesting
     public static final String GLOBAL_CACHE_DIR_NAME = "caches";
+    private static final Pattern CACHE_KEY_NAME_PATTERN = Pattern.compile("\\p{Alpha}+[-/.\\w]*");
 
     private final File globalCacheDir;
     private final File projectCacheDir;
@@ -45,7 +47,7 @@ public class DefaultCacheScopeMapping implements CacheScopeMapping, GlobalCache 
 
     @Override
     public File getBaseDirectory(@Nullable Object scope, String key, VersionStrategy versionStrategy) {
-        if (key.equalsIgnoreCase("projects") || key.equalsIgnoreCase("tasks") || !key.matches("\\p{Alpha}+[-/.\\w]*")) {
+        if (key.equalsIgnoreCase("projects") || key.equalsIgnoreCase("tasks") || !CACHE_KEY_NAME_PATTERN.matcher(key).matches()) {
             throw new IllegalArgumentException(String.format("Unsupported cache key '%s'.", key));
         }
         File cacheRootDir = getRootDirectory(scope);
