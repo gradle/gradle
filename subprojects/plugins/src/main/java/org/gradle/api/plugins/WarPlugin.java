7195b5a87080da8c3146b609b9b6ce6845f46292
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-06-19 14:13:14 -0300

    Replace anonymous by lambda in `WarPlugin`

--- subprojects/plugins/src/main/java/org/gradle/api/plugins/WarPlugin.java
+++ subprojects/plugins/src/main/java/org/gradle/api/plugins/WarPlugin.java
@@ -16,7 +16,6 @@
 
 package org.gradle.api.plugins;
 
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
@@ -63,31 +62,24 @@ public class WarPlugin implements Plugin<Project> {
         final WarPluginConvention pluginConvention = new DefaultWarPluginConvention(project);
         project.getConvention().getPlugins().put("war", pluginConvention);
 
-        project.getTasks().withType(War.class).configureEach(new Action<War>() {
-            @Override
-            public void execute(War task) {
-                task.from((Callable) () -> pluginConvention.getWebAppDir());
-                task.dependsOn((Callable) () -> project.getConvention()
-                    .getPlugin(JavaPluginConvention.class)
-                    .getSourceSets()
-                    .getByName(SourceSet.MAIN_SOURCE_SET_NAME)
-                    .getRuntimeClasspath());
-                task.classpath((Callable) () -> {
-                    FileCollection runtimeClasspath = project.getConvention().getPlugin(JavaPluginConvention.class)
-                            .getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME).getRuntimeClasspath();
-                    Configuration providedRuntime = project.getConfigurations().getByName(
-                            PROVIDED_RUNTIME_CONFIGURATION_NAME);
-                    return runtimeClasspath.minus(providedRuntime);
-                });
-            }
+        project.getTasks().withType(War.class).configureEach(task -> {
+            task.from((Callable) () -> pluginConvention.getWebAppDir());
+            task.dependsOn((Callable) () -> project.getConvention()
+                .getPlugin(JavaPluginConvention.class)
+                .getSourceSets()
+                .getByName(SourceSet.MAIN_SOURCE_SET_NAME)
+                .getRuntimeClasspath());
+            task.classpath((Callable) () -> {
+                FileCollection runtimeClasspath = project.getConvention().getPlugin(JavaPluginConvention.class)
+                    .getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME).getRuntimeClasspath();
+                Configuration providedRuntime = project.getConfigurations().getByName(PROVIDED_RUNTIME_CONFIGURATION_NAME);
+                return runtimeClasspath.minus(providedRuntime);
+            });
         });
 
-        TaskProvider<War> war = project.getTasks().register(WAR_TASK_NAME, War.class, new Action<War>() {
-            @Override
-            public void execute(War war) {
-                war.setDescription("Generates a war archive with all the compiled classes, the web-app content and the libraries.");
-                war.setGroup(BasePlugin.BUILD_GROUP);
-            }
+        TaskProvider<War> war = project.getTasks().register(WAR_TASK_NAME, War.class, warTask -> {
+            warTask.setDescription("Generates a war archive with all the compiled classes, the web-app content and the libraries.");
+            warTask.setGroup(BasePlugin.BUILD_GROUP);
         });
 
         PublishArtifact warArtifact = new LazyPublishArtifact(war);
@@ -98,10 +90,10 @@ public class WarPlugin implements Plugin<Project> {
 
     public void configureConfigurations(ConfigurationContainer configurationContainer) {
         Configuration provideCompileConfiguration = configurationContainer.create(PROVIDED_COMPILE_CONFIGURATION_NAME).setVisible(false).
-                setDescription("Additional compile classpath for libraries that should not be part of the WAR archive.");
+            setDescription("Additional compile classpath for libraries that should not be part of the WAR archive.");
         Configuration provideRuntimeConfiguration = configurationContainer.create(PROVIDED_RUNTIME_CONFIGURATION_NAME).setVisible(false).
-                extendsFrom(provideCompileConfiguration).
-                setDescription("Additional runtime classpath for libraries that should not be part of the WAR archive.");
+            extendsFrom(provideCompileConfiguration).
+            setDescription("Additional runtime classpath for libraries that should not be part of the WAR archive.");
         configurationContainer.getByName(JavaPlugin.COMPILE_CONFIGURATION_NAME).extendsFrom(provideCompileConfiguration);
         configurationContainer.getByName(JavaPlugin.RUNTIME_CONFIGURATION_NAME).extendsFrom(provideRuntimeConfiguration);
     }
