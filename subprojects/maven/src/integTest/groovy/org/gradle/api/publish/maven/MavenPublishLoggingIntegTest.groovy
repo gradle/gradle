f12227a5fde46ff53dabccd3884190e4b5030199
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-06-23 04:58:57 -0300

    Remove unused import

--- subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishLoggingIntegTest.groovy
+++ subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishLoggingIntegTest.groovy
@@ -16,7 +16,7 @@
 
 package org.gradle.api.publish.maven
 
-import org.gradle.integtests.fixtures.ToBeFixedForInstantExecution
+
 import org.gradle.integtests.fixtures.publish.maven.AbstractMavenPublishIntegTest
 import org.gradle.util.SetSystemProperties
 import org.junit.Rule
