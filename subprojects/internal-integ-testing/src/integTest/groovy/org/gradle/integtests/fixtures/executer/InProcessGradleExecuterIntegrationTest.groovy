8dd055d5933c06252d903864fc3823611e60ec83
Stefan Wolf <wolf@gradle.com>
2020-06-23 13:36:23 +0200

    Consistently use file system
    instead of file-system.

--- subprojects/internal-integ-testing/src/integTest/groovy/org/gradle/integtests/fixtures/executer/InProcessGradleExecuterIntegrationTest.groovy
+++ subprojects/internal-integ-testing/src/integTest/groovy/org/gradle/integtests/fixtures/executer/InProcessGradleExecuterIntegrationTest.groovy
@@ -25,8 +25,8 @@ import spock.lang.IgnoreIf
 import spock.lang.Specification
 import spock.lang.Unroll
 
-// Ignored for file-system watching since
-// - For file-system watching, we start an isolated daemon, which by definition is not an in process daemon, so the test doesn't make much sense.
+// Ignored for file system watching since
+// - For file system watching, we start an isolated daemon, which by definition is not an in process daemon, so the test doesn't make much sense.
 // - The daemon then leaves back some running daemons which write to the registry and cause an error for `verifyTestFilesCleanup`.
 @IgnoreIf({ GradleContextualExecuter.watchFs })
 class InProcessGradleExecuterIntegrationTest extends Specification {
