355185b09ee6d6e1962bfab17bdf4975588f8f59
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-06-23 10:52:35 -0300

    Remove `@ToBeFixedForInstantExecution` from `MavenPublishMultiProjectIntegTest`

--- subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishMultiProjectIntegTest.groovy
+++ subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishMultiProjectIntegTest.groovy
@@ -30,7 +30,6 @@ class MavenPublishMultiProjectIntegTest extends AbstractMavenPublishIntegTest {
     def project2 = javaLibrary(mavenRepo.module("org.gradle.test", "project2", "2.0"))
     def project3 = javaLibrary(mavenRepo.module("org.gradle.test", "project3", "3.0"))
 
-    @ToBeFixedForInstantExecution
     def "project dependency correctly reflected in POM"() {
         createBuildScripts()
 
@@ -41,7 +40,6 @@ class MavenPublishMultiProjectIntegTest extends AbstractMavenPublishIntegTest {
         projectsCorrectlyPublished()
     }
 
-    @ToBeFixedForInstantExecution
     def "project dependencies reference publication identity of dependent project"() {
         def project3 = javaLibrary(mavenRepo.module("changed.group", "changed-artifact-id", "changed"))
 
@@ -127,7 +125,6 @@ project(":project3") {
         succeeds "publish"
     }
 
-    @ToBeFixedForInstantExecution
     def "referenced project can have multiple additional publications that contain a child of some other publication"() {
         createBuildScripts("""
 // TODO - replace this with a public API when available
@@ -166,7 +163,6 @@ project(":project3") {
         project1.assertApiDependencies("org.gradle.test:project2:2.0", "custom:custom3:456")
     }
 
-    @ToBeFixedForInstantExecution
     def "maven-publish plugin does not take archivesBaseName into account when publishing"() {
         createBuildScripts("""
 project(":project2") {
@@ -181,7 +177,6 @@ project(":project2") {
         projectsCorrectlyPublished()
     }
 
-    @ToBeFixedForInstantExecution
     def "maven-publish plugin does not take mavenDeployer.pom.artifactId into account when publishing"() {
         executer.expectDeprecationWarning()
 
@@ -221,7 +216,6 @@ project(":project2") {
         return true
     }
 
-    @ToBeFixedForInstantExecution
     def "maven-publish plugin uses target project name for project dependency when target project does not have maven-publish plugin applied"() {
         executer.expectDeprecationWarning()
 
@@ -328,7 +322,6 @@ project(":project2") {
     }
 
     @Issue("GRADLE-3366")
-    @ToBeFixedForInstantExecution
     def "project dependency excludes are correctly reflected in pom when using maven-publish plugin"() {
         given:
         settingsFile << """
