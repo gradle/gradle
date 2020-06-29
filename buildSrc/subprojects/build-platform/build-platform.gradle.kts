bc956b8ac2b8542262315bdd37d4ed85151ea4cf
Jendrik Johannes <jendrik@gradle.com>
2020-06-29 17:22:33 +0200

    Add more version constraints to build platform

--- buildSrc/subprojects/build-platform/build-platform.gradle.kts
+++ buildSrc/subprojects/build-platform/build-platform.gradle.kts
@@ -20,14 +20,34 @@ plugins {
 
 // Here you should declare versions which should be shared by the different modules of buildSrc itself
 val javaParserVersion = "3.6.11"
+val asmVersion = "7.1"
 
 dependencies {
     constraints {
+        // Gradle Plugins
+        api("com.gradle.publish:plugin-publish-plugin:0.11.0")
+        api("me.champeau.gradle:japicmp-gradle-plugin:0.2.9")
+        api("org.gradle:test-retry-gradle-plugin:1.1.6")
+        api("org.gradle.kotlin:gradle-kotlin-dsl-conventions:0.5.0")
+
+        // Java Libraries
         api("com.github.javaparser:javaparser-core:$javaParserVersion")
         api("com.github.javaparser:javaparser-symbol-solver-core:$javaParserVersion")
+        api("com.google.guava:guava:27.1-jre")
+        api("com.google.code.gson:gson:2.7")
+        api("com.nhaarman:mockito-kotlin:1.6.0")
+        api("com.thoughtworks.qdox:qdox:2.0-M9")
+        api("junit:junit:4.13")
+        api("org.codenarc:CodeNarc:1.5")
+        api("org.eclipse.jgit:org.eclipse.jgit:5.7.0.202003110725-r")
+        api("org.javassist:javassist:3.23.0-GA")
+        api("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.1.0")
+        api("org.jsoup:jsoup:1.11.3")
+        api("org.junit.jupiter:junit-jupiter-api:5.6.1")
+        api("org.ow2.asm:asm:$asmVersion")
+        api("org.ow2.asm:asm-commons:$asmVersion")
         api("xerces:xercesImpl:2.12.0") {
             because("Maven Central and JCenter disagree on version 2.9.1 metadata")
         }
-        api("com.google.guava:guava:27.1-jre")
     }
 }
