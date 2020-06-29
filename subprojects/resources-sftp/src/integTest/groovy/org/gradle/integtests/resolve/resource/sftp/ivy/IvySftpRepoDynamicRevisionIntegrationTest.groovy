45043c63c10efa34af17c060e8071537ddcc4795
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-06-29 15:23:36 -0300

    Add `FAILS_CLEANUP_ASSERTIONS` to `IvySftpRepoDynamicRevisionIntegrationTest`

--- subprojects/resources-sftp/src/integTest/groovy/org/gradle/integtests/resolve/resource/sftp/ivy/IvySftpRepoDynamicRevisionIntegrationTest.groovy
+++ subprojects/resources-sftp/src/integTest/groovy/org/gradle/integtests/resolve/resource/sftp/ivy/IvySftpRepoDynamicRevisionIntegrationTest.groovy
@@ -20,7 +20,7 @@ import org.gradle.integtests.fixtures.ToBeFixedForInstantExecution
 import org.gradle.integtests.resolve.resource.sftp.AbstractSftpDependencyResolutionTest
 
 class IvySftpRepoDynamicRevisionIntegrationTest extends AbstractSftpDependencyResolutionTest {
-    @ToBeFixedForInstantExecution
+    @ToBeFixedForInstantExecution(skip = ToBeFixedForInstantExecution.Skip.FAILS_CLEANUP_ASSERTIONS)
     def "uses latest version from version range and latest status"() {
         given:
         buildFile << """
