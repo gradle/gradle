c3dbf88708d9b08e2e04afea80a47b21b78a0533
Jendrik Johannes <jendrik@gradle.com>
2020-07-03 12:15:39 +0200

    Remove use of 'afterEvaluate {}'
    This is now possible as we apply the plugin more selectively.

--- buildSrc/subprojects/buildquality/src/main/kotlin/gradlebuild/task-properties-validation.gradle.kts
+++ buildSrc/subprojects/buildquality/src/main/kotlin/gradlebuild/task-properties-validation.gradle.kts
@@ -22,25 +22,15 @@ plugins {
 val validateTaskName = "validatePlugins"
 val reportFileName = "task-properties/report.txt"
 
-afterEvaluate {
-    // This is in an after evaluate block to defer the decision until after the `java-gradle-plugin` may have been applied, so as to not collide with it
-    // It would be better to use some convention plugins instead, that apply a fixes set of plugins (including this one)
-    if (plugins.hasPlugin("java-base")) {
-        val validateTask = if (plugins.hasPlugin("java-gradle-plugin")) {
-            tasks.named<ValidatePlugins>(validateTaskName)
-        } else {
-            tasks.register<ValidatePlugins>(validateTaskName)
-        }
-        validateTask {
-            configureValidateTask()
-        }
-        tasks.named("codeQuality") {
-            dependsOn(validateTask)
-        }
-        tasks.withType<Test>().configureEach {
-            shouldRunAfter(validateTask)
-        }
-    }
+val validateTask = tasks.register<ValidatePlugins>(validateTaskName)
+validateTask {
+    configureValidateTask()
+}
+tasks.named("codeQuality") {
+    dependsOn(validateTask)
+}
+tasks.withType<Test>().configureEach {
+    shouldRunAfter(validateTask)
 }
 
 fun ValidatePlugins.configureValidateTask() {
@@ -48,6 +38,5 @@ fun ValidatePlugins.configureValidateTask() {
     classes.from(main.output)
     classpath.from(main.runtimeClasspath)
     outputFile.set(project.reporting.baseDirectory.file(reportFileName))
-    failOnWarning.set(true)
     enableStricterValidation.set(true)
 }
