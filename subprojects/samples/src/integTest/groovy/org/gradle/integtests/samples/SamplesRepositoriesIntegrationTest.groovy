3c2c4112ebfeaaa08941b4b02a7600a3595f8be4
Paul Merlin <paul@gradle.com>
2020-07-04 13:08:50 +0200

    Remove unused import
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/samples/src/integTest/groovy/org/gradle/integtests/samples/SamplesRepositoriesIntegrationTest.groovy
+++ subprojects/samples/src/integTest/groovy/org/gradle/integtests/samples/SamplesRepositoriesIntegrationTest.groovy
@@ -18,7 +18,6 @@ package org.gradle.integtests.samples
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.Sample
-import org.gradle.integtests.fixtures.ToBeFixedForInstantExecution
 import org.gradle.integtests.fixtures.UsesSample
 import org.gradle.test.fixtures.file.LeaksFileHandles
 import org.junit.Rule
