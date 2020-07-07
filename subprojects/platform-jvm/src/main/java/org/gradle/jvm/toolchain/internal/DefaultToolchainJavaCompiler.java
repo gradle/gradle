576be8aca401553eaed7082f64e0963804fdded8
Benjamin Muskalla <bmuskalla@gradle.com>
2020-07-07 12:50:40 +0200

    Toolchain selection should be logger
    This is to properly test this and further help users diagnose which
    toolchain was used. The log message is aligned with the log output of
    the concrete compiler backends.

--- subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/internal/DefaultToolchainJavaCompiler.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/internal/DefaultToolchainJavaCompiler.java
@@ -21,13 +21,17 @@ import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.WorkResult;
 import org.gradle.jvm.toolchain.JavaCompiler;
 import org.gradle.language.base.internal.compile.CompileSpec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class DefaultJavaCompiler implements JavaCompiler {
+public class DefaultToolchainJavaCompiler implements JavaCompiler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultToolchainJavaCompiler.class);
 
     private final JavaToolchain javaToolchain;
     private final JavaCompilerFactory compilerFactory;
 
-    public DefaultJavaCompiler(JavaToolchain javaToolchain, JavaCompilerFactory compilerFactory) {
+    public DefaultToolchainJavaCompiler(JavaToolchain javaToolchain, JavaCompilerFactory compilerFactory) {
         this.javaToolchain = javaToolchain;
         this.compilerFactory = compilerFactory;
     }
@@ -39,7 +43,7 @@ public class DefaultJavaCompiler implements JavaCompiler {
 
     @SuppressWarnings("unchecked")
     public <T extends CompileSpec> WorkResult execute(T spec) {
-        System.out.println("Toolchain selected: " + javaToolchain.getJavaMajorVersion());
+        LOGGER.info("Compiling with toolchain '{}'.", javaToolchain.getDisplayName());
         final Class<T> specType = (Class<T>) spec.getClass();
         return compilerFactory.create(specType).execute(spec);
     }
