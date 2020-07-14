cf16e5e11f28dd0a50f08fa5e617e628bd586b9e
Benjamin Muskalla <bmuskalla@gradle.com>
2020-07-14 20:21:48 +0200

    Add missing javadoc

--- subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/JavaToolchainSpec.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/JavaToolchainSpec.java
@@ -17,11 +17,24 @@
 package org.gradle.jvm.toolchain;
 
 import org.gradle.api.Describable;
+import org.gradle.api.Incubating;
 import org.gradle.api.JavaVersion;
 import org.gradle.api.provider.Property;
 
+/**
+ * Requirements for selecting a Java toolchain.
+ * <p>
+ * A toolchain is a JRE/JDK used by the tasks of a build. Tasks of a build may require one or more of the tools javac, java, or javadoc) of a toolchain.
+ * Depending on the needs of a build, only toolchains matching specific characteristics can be used to run a build or a specific task of a build.
+ *
+ * @since 6.7
+ */
+@Incubating
 public interface JavaToolchainSpec extends Describable {
 
+    /**
+     * The exact version of the Java language that the toolchain is required to support.
+     */
     Property<JavaVersion> getLanguageVersion();
 
 }
