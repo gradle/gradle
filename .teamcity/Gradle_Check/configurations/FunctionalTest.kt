b103e315fd8ff18633f7aa036ea6b97e8b640447
Bo Zhang <bo@gradle.com>
2020-07-14 15:46:01 +0800

    Add scan tag for test-distribution

--- .teamcity/Gradle_Check/configurations/FunctionalTest.kt
+++ .teamcity/Gradle_Check/configurations/FunctionalTest.kt
@@ -46,7 +46,7 @@ class FunctionalTest(
             listOf(""""-PtestJavaHome=%${testCoverage.os}.${testCoverage.testJvmVersion}.${testCoverage.vendor}.64bit%"""") +
                 buildScanTags.map { buildScanTag(it) } +
                 buildScanValues.map { buildScanCustomValue(it.key, it.value) } +
-                if (testCoverage.testDistribution) "-DenableTestDistribution=true" else "" +
+                if (testCoverage.testDistribution) "-DenableTestDistribution=true -Dscan.tag.test-distribution" else "" +
                     extraParameters
             ).filter { it.isNotBlank() }.joinToString(separator = " "),
         timeout = testCoverage.testType.timeout,
