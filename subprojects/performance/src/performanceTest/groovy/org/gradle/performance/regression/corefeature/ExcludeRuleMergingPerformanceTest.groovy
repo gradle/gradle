67e55b350a4640ea216843fd42e2462eb0f392cd
Bo Zhang <bo@gradle.com>
2020-07-22 12:30:49 +0800

    Rebaseline old snapshots
    Because they're going to expire

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
@@ -25,7 +25,7 @@ class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionGradleProfil
 
     def setup() {
         runner.minimumBaseVersion = '4.9'
-        runner.targetVersions = ["6.5-20200424145747+0000"]
+        runner.targetVersions = ["6.7-20200721220031+0000"]
     }
 
     def "merge exclude rules"() {
