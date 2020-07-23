2566fcdb51226f1757e2565c9b11efd093a9ff3b
Johannes Bühler <johannes@gradle.com>
2020-07-23 17:49:24 +0200

    gradle enterprise plugin 3.4 - part 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise").version("3.3.4")
+    id("com.gradle.enterprise").version("3.4")
     id("com.gradle.enterprise.gradle-enterprise-conventions-plugin").version("0.3")
 }
 
