70290b066cb1dd2c8750741ced06bc43c36609eb
Lóránt Pintér <lorant@gradle.com>
2020-07-27 13:50:17 +0200

    Fix some warnings in DistributionIntegrationSpec

--- subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -27,6 +27,7 @@ import org.gradle.util.PreconditionVerifier
 import org.junit.Rule
 import spock.lang.Shared
 
+import java.nio.charset.StandardCharsets
 import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
 
@@ -193,7 +194,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
         assert !contentsDir.file("lib/plugins/tools.jar").exists()
     }
 
-    protected void assertDocsExist(TestFile contentsDir, String version) {
+    protected static void assertDocsExist(TestFile contentsDir, String version) {
         // Javadoc
         contentsDir.file('docs/javadoc/index.html').assertIsFile()
         contentsDir.file('docs/javadoc/index.html').assertContents(containsString("Gradle API ${version}"))
@@ -219,11 +220,10 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
 
     private static void assertIsGradleApiMetadataJar(TestFile jar) {
         new JarTestFixture(jar.canonicalFile).with {
-            def apiDeclaration = GUtil.loadProperties(IOUtils.toInputStream(content("gradle-api-declaration.properties")))
+            def apiDeclaration = GUtil.loadProperties(IOUtils.toInputStream(content("gradle-api-declaration.properties"), StandardCharsets.UTF_8))
             assert apiDeclaration.size() == 2
             assert apiDeclaration.getProperty("includes").contains(":org/gradle/api/**:")
             assert apiDeclaration.getProperty("excludes").split(":").size() == 1
         }
     }
-
 }
