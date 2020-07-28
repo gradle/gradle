b50061f32986b2d79388025ecb6f9f32664c2cd8
Benjamin Muskalla <bmuskalla@gradle.com>
2020-07-28 12:26:01 +0200

    Introduce remote binary as abstraction for the AdoptOpenJdk API

--- subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/install/internal/AdoptOpenJdkRemoteBinary.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/install/internal/AdoptOpenJdkRemoteBinary.java
@@ -16,14 +16,26 @@
 
 package org.gradle.jvm.toolchain.install.internal;
 
+import com.google.common.io.Files;
 import net.rubygrapefruit.platform.SystemInfo;
+import org.apache.commons.io.FileUtils;
+import org.gradle.api.GradleException;
 import org.gradle.internal.os.OperatingSystem;
 import org.gradle.jvm.toolchain.JavaToolchainSpec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class AdoptOpenJdkRemoteBinary {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdoptOpenJdkRemoteBinary.class);
 
     private final SystemInfo systemInfo;
     private final OperatingSystem operatingSystem;
@@ -34,9 +46,36 @@ public class AdoptOpenJdkRemoteBinary {
         this.operatingSystem = operatingSystem;
     }
 
-    public URI getUri(JavaToolchainSpec spec) {
+    // TODO: [bm] this might be downloading the same jdk multiple times in parallel builds
+    // TODO: [bm] Alternative is using RepositoryTransportFactory+ExternalResourceAccessor+ChecksumService
+    // Needs to be tied up with a FileStore impl as well
+    // Adds risk as AdoptOpenJDK is hosted on S3 which does not allow HEAD requests; they
+    // currently have a workaround to support HEAD requests for the "Gradle" User agent
+    // Not using accessor might help us to move all of this into jvmServices for TD reuse
+    public File download(JavaToolchainSpec spec) {
+        File tmpFile = new File(Files.createTempDir(), toFilename(spec));
+        int defaultTimeout = (int) TimeUnit.MINUTES.toMillis(5);
+        URL source = toDownloadUrl(spec);
+        LOGGER.info("Downloading {} to {}", source, tmpFile);
+        try {
+            FileUtils.copyURLToFile(source, tmpFile, defaultTimeout, defaultTimeout);
+        } catch (IOException e) {
+            throw new GradleException("Could not download JDK from " + source, e);
+        }
+        return tmpFile;
+    }
+
+    private URL toDownloadUrl(JavaToolchainSpec spec) {
+        try {
+            return constructUri(spec).toURL();
+        } catch (MalformedURLException e) {
+            throw new GradleException("Could not construct URL to download JDK for given toolchain requirements: " + spec.getDisplayName(), e);
+        }
+    }
+
+    private URI constructUri(JavaToolchainSpec spec) {
         return URI.create(getServerBaseUri() +
-            "v3/binary/latest/" + spec.getLanguageVersion().get().getMajorVersion() +
+            "v3/binary/latest/" + getMajorVersion(spec) +
             "/" +
             determineReleaseState() +
             "/" +
@@ -44,6 +83,21 @@ public class AdoptOpenJdkRemoteBinary {
             "/" +
             determineArch() +
             "/jdk/hotspot/normal/adoptopenjdk");
+    }
+
+    private String toFilename(JavaToolchainSpec spec) {
+        return String.format("adoptopenjdk-%s-%s-%s.%s", getMajorVersion(spec), determineArch(), determineOs(), determineFileExtension());
+    }
+
+    private String determineFileExtension() {
+        if (operatingSystem.isWindows()) {
+            return "zip";
+        }
+        return "tar.gz";
+    }
+
+    private String getMajorVersion(JavaToolchainSpec spec) {
+        return spec.getLanguageVersion().get().getMajorVersion();
     }
 
     private String determineArch() {
