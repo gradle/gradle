5fb42095abb09277a673e3e642f76124a6f05ff2
Benjamin Muskalla <bmuskalla@gradle.com>
2020-07-28 12:50:37 +0200

    Add integration test for toolchain download

---
+++ subprojects/platform-jvm/src/integTest/groovy/org/gradle/jvm/toolchain/JavaToolchainDownloadIntegrationTest.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.jvm.toolchain
+
+import org.gradle.integtests.fixtures.AbstractPluginIntegrationTest
+
+// TODO: [bm] Should we keep this test as it downloads a whole JDK?
+class JavaToolchainDownloadIntegrationTest extends AbstractPluginIntegrationTest {
+
+    def "can download missing jdk automatically"() {
+        buildFile << """
+            import org.gradle.jvm.toolchain.internal.JavaToolchainQueryService
+            import org.gradle.jvm.toolchain.internal.DefaultToolchainSpec
+
+            apply plugin: "java"
+
+            java {
+                toolchain {
+                    languageVersion = JavaVersion.VERSION_14
+                }
+            }
+        """
+
+        file("src/main/java/Foo.java") << "public class Foo {}"
+
+        when:
+        result = executer
+            .withArguments("-Porg.gradle.java.installations.auto-detect=false", "--info")
+            .withTasks("compileJava")
+            .run()
+
+        then:
+        outputContains("Compiling with toolchain '" +
+            executer.gradleUserHomeDir.absolutePath +
+            "/jdks/jdk-14.0.2+12/")
+        javaClassFile("Foo.class").exists()
+    }
+
+}
