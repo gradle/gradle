f9027ffe33f45f5ffd83d26372510935447d1cab
Benjamin Muskalla <bmuskalla@gradle.com>
2020-07-28 21:06:18 +0200

    Support provisioning a jdk using an archive

--- subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/install/internal/JdkCacheDirectory.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/install/internal/JdkCacheDirectory.java
@@ -16,6 +16,10 @@
 
 package org.gradle.jvm.toolchain.install.internal;
 
+import org.apache.commons.io.FilenameUtils;
+import org.gradle.api.file.EmptyFileVisitor;
+import org.gradle.api.file.FileTree;
+import org.gradle.api.file.FileVisitDetails;
 import org.gradle.api.internal.file.FileOperations;
 import org.gradle.initialization.GradleUserHomeDirProvider;
 import org.slf4j.Logger;
@@ -24,23 +28,26 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 public class JdkCacheDirectory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JdkCacheDirectory.class);
 
-    private final GradleUserHomeDirProvider homeDirProvider;
     private final FileOperations operations;
+    private final File jdkDirectory;
 
     public JdkCacheDirectory(GradleUserHomeDirProvider homeDirProvider, FileOperations operations) {
-        this.homeDirProvider = homeDirProvider;
         this.operations = operations;
+        // TODO: [bm] any reason to use CacheScopeMapping instead?
+        this.jdkDirectory = new File(homeDirProvider.getGradleUserHomeDirectory(), "jdks");
     }
 
     public Set<File> listJavaHomes() {
-        final File[] files = getJdksDirectory().listFiles();
+        final File[] files = jdkDirectory.listFiles();
         if (files != null) {
             return Arrays.stream(files).filter(File::isDirectory).map(this::findJavaHome).collect(Collectors.toSet());
         }
@@ -54,9 +61,43 @@ public class JdkCacheDirectory {
         return potentialHome;
     }
 
-    // TODO: [bm] any reason to use CacheScopeMapping instead?
-    private File getJdksDirectory() {
-        return new File(homeDirProvider.getGradleUserHomeDirectory(), "jdks");
+    /**
+     * Unpacks and installs the given JDK archive. Returns a file pointing to the java home directory.
+     */
+    public File provisionFromArchive(File jdkArchive) {
+        return findJavaHome(unpack(jdkArchive));
+    }
+
+    private File unpack(File jdkArchive) {
+        final FileTree fileTree = asFileTree(jdkArchive);
+        operations.copy(spec -> {
+            spec.from(fileTree);
+            spec.into(jdkDirectory);
+        });
+        final String rootName = getRootDirectory(fileTree);
+        final File installLocation = new File(jdkDirectory, rootName);
+        LOGGER.info("Installed {} into {}", jdkArchive.getName(), installLocation);
+        return installLocation;
+    }
+
+    // TODO: [bm] anyway to cancel after first visit? or a better way to find the single root
+    private String getRootDirectory(FileTree fileTree) {
+        AtomicReference<File> rootDir = new AtomicReference<>();
+        fileTree.visit(new EmptyFileVisitor() {
+            @Override
+            public void visitDir(FileVisitDetails details) {
+                rootDir.compareAndSet(null, details.getFile());
+            }
+        });
+        return rootDir.get().getName();
+    }
+
+    private FileTree asFileTree(File jdkArchive) {
+        final String extension = FilenameUtils.getExtension(jdkArchive.getName());
+        if (Objects.equals(extension, "zip")) {
+            return operations.zipTree(jdkArchive);
+        }
+        return operations.tarTree(operations.getResources().gzip(jdkArchive));
     }
 
 }
