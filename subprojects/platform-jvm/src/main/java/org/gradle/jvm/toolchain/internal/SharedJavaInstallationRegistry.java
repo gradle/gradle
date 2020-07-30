77b49e764dba78777a470e9daa023ce58b2370ec
Benjamin Muskalla <bmuskalla@gradle.com>
2020-07-30 11:50:54 +0200

    Installation registry should not cache installations
    This should be cached on a higher level as other parts of the system may
    dynamically add more installations in the discovery process (e.g. by
    downloading)

--- subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/internal/SharedJavaInstallationRegistry.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/internal/SharedJavaInstallationRegistry.java
@@ -17,9 +17,6 @@
 package org.gradle.jvm.toolchain.internal;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
 import com.google.common.collect.Sets;
 import org.gradle.api.GradleException;
 import org.gradle.api.logging.Logger;
@@ -31,15 +28,12 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 
 public class SharedJavaInstallationRegistry {
 
     private final Set<InstallationSupplier> suppliers = Sets.newConcurrentHashSet();
-    private final Supplier<Set<File>> finalizedInstallations = Suppliers.memoize(this::mapToDirectories);
-    private final AtomicBoolean finalized = new AtomicBoolean();
     private final Logger logger;
 
     @Inject
@@ -58,20 +52,10 @@ public class SharedJavaInstallationRegistry {
     }
 
     void add(InstallationSupplier provider) {
-        Preconditions.checkArgument(!finalized.get(), "Installation must not be mutated after being finalized");
         suppliers.add(provider);
     }
 
     public Set<File> listInstallations() {
-        finalizeValue();
-        return finalizedInstallations.get();
-    }
-
-    private void finalizeValue() {
-        finalized.compareAndSet(false, true);
-    }
-
-    private Set<File> mapToDirectories() {
         return suppliers.stream()
             .map(InstallationSupplier::get)
             .flatMap(Set::stream)
