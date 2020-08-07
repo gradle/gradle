8ad007bd915d2baf77eaae90fb15aa5a7518f8c1
Bo Zhang <bo@gradle.com>
2020-08-07 18:43:17 +0800

    Rebaseline improved scenarios

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/nativeplatform/RealWorldNativePluginPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/nativeplatform/RealWorldNativePluginPerformanceTest.groovy
@@ -27,7 +27,7 @@ import spock.lang.Unroll
 class RealWorldNativePluginPerformanceTest extends AbstractCrossVersionGradleInternalPerformanceTest {
 
     def setup() {
-        runner.targetVersions = ["6.7-20200723220251+0000"]
+        runner.targetVersions = ["6.7-20200806220106+0000"]
         runner.minimumBaseVersion = "4.0"
     }
 
