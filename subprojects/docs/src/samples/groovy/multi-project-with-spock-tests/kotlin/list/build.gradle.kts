ee14c1712da6246cb9fe50ada21b47df1fed9acf
Jendrik Johannes <jendrik@gradle.com>
2020-08-10 09:37:33 +0200

    Update Groovy and Spock versions in Groovy multi-project sample
    This is to make the sample up-to-date and to get rid of the
    illegal access warnings.

--- subprojects/docs/src/samples/groovy/multi-project-with-spock-tests/kotlin/list/build.gradle.kts
+++ subprojects/docs/src/samples/groovy/multi-project-with-spock-tests/kotlin/list/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("org.codehaus.groovy:groovy-all:2.5.11")
-    testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
+    implementation("org.codehaus.groovy:groovy-all:3.0.5")
+    testImplementation("org.spockframework:spock-core:2.0-M3-groovy-3.0")
 }
--- subprojects/docs/src/samples/groovy/multi-project-with-spock-tests/kotlin/utilities/build.gradle.kts
+++ subprojects/docs/src/samples/groovy/multi-project-with-spock-tests/kotlin/utilities/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("org.codehaus.groovy:groovy-all:2.5.11")
+    implementation("org.codehaus.groovy:groovy-all:3.0.5")
+    testImplementation("org.spockframework:spock-core:2.0-M3-groovy-3.0")
     implementation(project(":list"))
 }
