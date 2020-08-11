d7c4060b47b3c6e42d11cc0b3fdf4bece68c7392
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-08-11 10:59:36 -0300

    Reuse `BuildLogicChangeFixture` in `buildSrc` tests

--- subprojects/instant-execution/src/integTest/groovy/org/gradle/instantexecution/InstantExecutionBuildSrcChangesIntegrationTest.groovy
+++ subprojects/instant-execution/src/integTest/groovy/org/gradle/instantexecution/InstantExecutionBuildSrcChangesIntegrationTest.groovy
@@ -16,62 +16,58 @@
 
 package org.gradle.instantexecution
 
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
-import org.gradle.integtests.fixtures.KotlinDslTestUtil
-import org.gradle.test.fixtures.file.TestFile
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
+import org.gradle.instantexecution.fixtures.BuildLogicChangeFixture
 import spock.lang.Unroll
 
 import static org.junit.Assume.assumeFalse
 
 class InstantExecutionBuildSrcChangesIntegrationTest extends AbstractInstantExecutionIntegrationTest {
 
-    private static final String TASK_NAME = "greet"
-    private static final String ORIGINAL_GREETING = "Hello!"
-    private static final String CHANGED_GREETING = "G'day!"
-
     @Unroll
-    def "invalidates cache upon change to buildSrc #language project (#change)"() {
+    def "invalidates cache upon change to buildSrc #changeFixtureSpec"() {
         given:
         def instant = newInstantExecutionFixture()
-        def fixture = new BuildSrcChangeFixture(testDirectory, language, change)
-        fixture.setup()
+        def changeFixture = changeFixtureSpec.fixtureForProjectDir(file('buildSrc'))
+        changeFixture.setup()
+        buildFile << """
+            plugins { id('$changeFixture.pluginId') }
+        """
 
         when:
         if (isKotlinBuildSrc) {
-            instantRunLenient()
+            instantRunLenient changeFixture.task
         } else {
-            instantRun()
+            instantRun changeFixture.task
         }
 
         then:
-        outputContains ORIGINAL_GREETING
-
-        when:
-        fixture.applyChange()
-        if (isKotlinBuildSrc) {
-            instantRunLenient()
-        } else {
-            instantRun()
-        }
-
-        then:
-        outputContains fixture.expectedOutputAfterChange
+        outputContains changeFixture.expectedOutputBeforeChange
         instant.assertStateStored()
 
         when:
-        instantRun()
+        changeFixture.applyChange()
+        if (isKotlinBuildSrc) {
+            instantRunLenient changeFixture.task
+        } else {
+            instantRun changeFixture.task
+        }
 
         then:
-        outputContains fixture.expectedOutputAfterChange
+        outputContains changeFixture.expectedOutputAfterChange
+        instant.assertStateStored()
+
+        when:
+        instantRun changeFixture.task
+
+        then:
+        outputContains changeFixture.expectedOutputAfterChange
         instant.assertStateLoaded()
 
         where:
-        [language_, change_] << [BuildSrcLanguage.values(), BuildSrcChange.values()].combinations()
-        language = language_ as BuildSrcLanguage
-        change = change_ as BuildSrcChange
-
-        isKotlinBuildSrc = language == BuildSrcLanguage.KOTLIN
+        changeFixtureSpec << BuildLogicChangeFixture.specs()
+        isKotlinBuildSrc = changeFixtureSpec.language == BuildLogicChangeFixture.Language.KOTLIN
     }
 
     @Unroll
@@ -86,15 +82,14 @@ class InstantExecutionBuildSrcChangesIntegrationTest extends AbstractInstantExec
         def instant = newInstantExecutionFixture()
         file("buildSrc/build.gradle.kts").text = """
 
-            import org.gradle.api.provider.*
-
-            interface Params: ValueSourceParameters {
+            interface Params: $ValueSourceParameters.name {
                 val value: Property<String>
             }
 
-            abstract class IsCi : ValueSource<String, Params> {
+            abstract class IsCi : $ValueSource.name<String, Params> {
                 override fun obtain(): String? = parameters.value.orNull
             }
+
             val ciProvider = providers.of(IsCi::class.java) {
                 parameters.value.set(providers.systemProperty("test_is_ci").forUseAtConfigurationTime())
             }
@@ -151,147 +146,5 @@ class InstantExecutionBuildSrcChangesIntegrationTest extends AbstractInstantExec
         'system property'     | 'providers.systemProperty("test_is_ci")' | '-Dtest_is_ci=true'
         'Gradle property'     | 'providers.gradleProperty("test_is_ci")' | '-Ptest_is_ci=true'
         'gradle.properties'   | 'providers.gradleProperty("test_is_ci")' | ''
-    }
-
-    private instantRun() {
-        instantRun TASK_NAME
-    }
-
-    private instantRunLenient() {
-        instantRunLenient TASK_NAME
-    }
-
-    static class BuildSrcChangeFixture {
-
-        private final TestFile projectDir
-        private final BuildSrcLanguage language
-        private final BuildSrcChange change
-
-        BuildSrcChangeFixture(TestFile projectDir, BuildSrcLanguage language, BuildSrcChange change) {
-            this.projectDir = projectDir
-            this.language = language
-            this.change = change
-        }
-
-        void setup() {
-            file("build.gradle") << """
-                task $TASK_NAME(type: GreetTask)
-            """
-            file("buildSrc/build.gradle.kts") << kotlinDslBuildSrcScript
-            writeGreetTask "GreetTask", ORIGINAL_GREETING
-            switch (change) {
-                case BuildSrcChange.CHANGE_RESOURCE:
-                    writeBuildSrcResource ""
-                    break
-            }
-        }
-
-        void applyChange() {
-            switch (change) {
-                case BuildSrcChange.CHANGE_SOURCE:
-                    writeGreetTask "GreetTask", CHANGED_GREETING
-                    break
-                case BuildSrcChange.ADD_SOURCE:
-                    writeGreetTask "AnotherTask", CHANGED_GREETING
-                    break
-                case BuildSrcChange.CHANGE_RESOURCE:
-                case BuildSrcChange.ADD_RESOURCE:
-                    writeBuildSrcResource "42"
-                    break
-            }
-        }
-
-        String getExpectedOutputAfterChange() {
-            switch (change) {
-                case BuildSrcChange.CHANGE_SOURCE:
-                    return CHANGED_GREETING
-                default:
-                    return ORIGINAL_GREETING
-            }
-        }
-
-        private String getKotlinDslBuildSrcScript() {
-            switch (language) {
-                case BuildSrcLanguage.JAVA:
-                    return "plugins { `java-library` }"
-                case BuildSrcLanguage.GROOVY:
-                    return "plugins { groovy }"
-                case BuildSrcLanguage.KOTLIN:
-                    return KotlinDslTestUtil.kotlinDslBuildSrcScript
-            }
-        }
-
-        private void writeGreetTask(String className, String greeting) {
-            switch (language) {
-                case BuildSrcLanguage.JAVA:
-                    writeJavaTask(className, greeting)
-                    break
-                case BuildSrcLanguage.GROOVY:
-                    writeGroovyTask(className, greeting)
-                    break
-                case BuildSrcLanguage.KOTLIN:
-                    writeKotlinTask(className, greeting)
-                    break
-            }
-        }
-
-        private void writeJavaTask(String className, String greeting) {
-            writeBuildSrcSource "java", "${className}.java", """
-                public class $className extends ${DefaultTask.name} {
-                    @${TaskAction.name} void greet() { System.out.println("$greeting"); }
-                }
-            """
-        }
-
-        private void writeGroovyTask(String className, String greeting) {
-            writeBuildSrcSource "groovy", "${className}.groovy", """
-                public class $className extends ${DefaultTask.name} {
-                    @${TaskAction.name} void greet() { System.out.println("$greeting"); }
-                }
-            """
-        }
-
-        private void writeKotlinTask(String className, String greeting) {
-            writeBuildSrcSource "kotlin", "${className}.kt", """
-                open class $className : ${DefaultTask.name}() {
-                    @${TaskAction.name} fun greet() { println("$greeting") }
-                }
-            """
-        }
-
-        private void writeBuildSrcResource(String text) {
-            writeBuildSrcSource "resources", "resource.txt", text
-        }
-
-        private void writeBuildSrcSource(String sourceSet, String fileName, String text) {
-            file("buildSrc/src/main/" + sourceSet + "/" + fileName).text = text
-        }
-
-        private TestFile file(String path) {
-            projectDir.file(path)
-        }
-    }
-
-    enum BuildSrcChange {
-        CHANGE_SOURCE,
-        ADD_SOURCE,
-        CHANGE_RESOURCE,
-        ADD_RESOURCE
-
-        @Override
-        String toString() {
-            name().toLowerCase().replace('_', ' ')
-        }
-    }
-
-    enum BuildSrcLanguage {
-        JAVA,
-        GROOVY,
-        KOTLIN
-
-        @Override
-        String toString() {
-            name().toLowerCase().capitalize()
-        }
     }
 }
--- subprojects/instant-execution/src/integTest/groovy/org/gradle/instantexecution/InstantExecutionIncludedBuildChangesIntegrationTest.groovy
+++ subprojects/instant-execution/src/integTest/groovy/org/gradle/instantexecution/InstantExecutionIncludedBuildChangesIntegrationTest.groovy
@@ -16,15 +16,10 @@
 
 package org.gradle.instantexecution
 
-import groovy.transform.Immutable
-import org.gradle.api.DefaultTask
-import org.gradle.api.Plugin
-import org.gradle.api.Project
+
 import org.gradle.api.provider.ValueSource
 import org.gradle.api.provider.ValueSourceParameters
-import org.gradle.api.tasks.TaskAction
-import org.gradle.integtests.fixtures.KotlinDslTestUtil
-import org.gradle.test.fixtures.file.TestFile
+import org.gradle.instantexecution.fixtures.BuildLogicChangeFixture
 import spock.lang.Unroll
 
 import static org.junit.Assume.assumeFalse
@@ -41,7 +36,7 @@ class InstantExecutionIncludedBuildChangesIntegrationTest extends AbstractInstan
             includeBuild 'build-logic'
         """
         buildFile << """
-            plugins { id('${fixture.pluginId}') }
+            plugins { id('$fixture.pluginId') }
         """
 
         when:
@@ -84,11 +79,11 @@ class InstantExecutionIncludedBuildChangesIntegrationTest extends AbstractInstan
         fixture.setup()
         fixture.buildFile << """
 
-            interface Params : ${ValueSourceParameters.name} {
+            interface Params : $ValueSourceParameters.name {
                 val value: Property<String>
             }
 
-            abstract class IsCi : ${ValueSource.name}<String, Params> {
+            abstract class IsCi : $ValueSource.name<String, Params> {
                 override fun obtain(): String? = parameters.value.orNull
             }
 
@@ -111,7 +106,7 @@ class InstantExecutionIncludedBuildChangesIntegrationTest extends AbstractInstan
             includeBuild 'build-logic'
         """
         buildFile << """
-            plugins { id('${fixture.pluginId}') }
+            plugins { id('$fixture.pluginId') }
         """
 
         when:
@@ -146,209 +141,5 @@ class InstantExecutionIncludedBuildChangesIntegrationTest extends AbstractInstan
         'system property'     | 'providers.systemProperty("test_is_ci")' | '-Dtest_is_ci=true'
         'Gradle property'     | 'providers.gradleProperty("test_is_ci")' | '-Ptest_is_ci=true'
         'gradle.properties'   | 'providers.gradleProperty("test_is_ci")' | ''
-    }
-
-    static class BuildLogicChangeFixture {
-
-        static List<Spec> specs() {
-            [Kind.values(), Language.values()].combinations().collect { Kind kind, Language language ->
-                new Spec(language, kind)
-            }
-        }
-
-        @Immutable
-        static class Spec {
-            Language language
-            Kind kind
-
-            @Override
-            String toString() {
-                "$language project ($kind)"
-            }
-
-            BuildLogicChangeFixture fixtureForProjectDir(TestFile projectDir) {
-                new BuildLogicChangeFixture(projectDir, language, kind)
-            }
-        }
-
-        enum Kind {
-            CHANGE_SOURCE,
-            ADD_SOURCE,
-            CHANGE_RESOURCE,
-            ADD_RESOURCE
-
-            @Override
-            String toString() {
-                name().toLowerCase().replace('_', ' ')
-            }
-        }
-
-        enum Language {
-            JAVA,
-            GROOVY,
-            KOTLIN
-
-            String getFileExtension() {
-                switch (this) {
-                    case JAVA:
-                        return 'java'
-                    case GROOVY:
-                        return 'groovy'
-                    case KOTLIN:
-                        return 'kt'
-                }
-            }
-
-            @Override
-            String toString() {
-                name().toLowerCase().capitalize()
-            }
-        }
-
-        static final String pluginId = 'build-logic'
-        static final String task = 'greet'
-        static final String originalGreeting = 'Hello!'
-        static final String changedGreeting = "G'day!"
-
-        private final TestFile projectDir
-        private final Language language
-        private final Kind kind
-        private final TestFile buildFile
-
-        BuildLogicChangeFixture(TestFile projectDir, Language language = Language.JAVA, Kind kind = Kind.CHANGE_SOURCE) {
-            this.projectDir = projectDir
-            this.language = language
-            this.kind = kind
-            this.buildFile = file("build.gradle.kts")
-        }
-
-        void setup() {
-            buildFile << kotlinDslScriptForLanguage
-            buildFile << """
-                gradlePlugin {
-                    plugins {
-                        register("$pluginId") {
-                            id = "$pluginId"
-                            implementationClass = "BuildLogicPlugin"
-                        }
-                    }
-                }
-            """
-            writeBuildLogicPlugin()
-            writeGreetTask "GreetTask", originalGreeting
-            switch (kind) {
-                case Kind.CHANGE_RESOURCE:
-                    writeResource ""
-                    break
-            }
-        }
-
-        void applyChange() {
-            switch (kind) {
-                case Kind.CHANGE_SOURCE:
-                    writeGreetTask "GreetTask", changedGreeting
-                    break
-                case Kind.ADD_SOURCE:
-                    writeGreetTask "AnotherTask", changedGreeting
-                    break
-                case Kind.CHANGE_RESOURCE:
-                case Kind.ADD_RESOURCE:
-                    writeResource "42"
-                    break
-            }
-        }
-
-        String getExpectedOutputBeforeChange() {
-            originalGreeting
-        }
-
-        String getExpectedOutputAfterChange() {
-            switch (kind) {
-                case Kind.CHANGE_SOURCE:
-                    return changedGreeting
-                default:
-                    return originalGreeting
-            }
-        }
-
-        private String getKotlinDslScriptForLanguage() {
-            """
-                plugins {
-                    $languagePlugin
-                    `java-gradle-plugin`
-                }
-
-                ${language == Language.KOTLIN
-                ? KotlinDslTestUtil.kotlinDslBuildSrcConfig
-                : ''}
-            """
-        }
-
-        private String getLanguagePlugin() {
-            switch (language) {
-                case Language.JAVA:
-                    return "`java-library`"
-                case Language.GROOVY:
-                    return "groovy"
-                case Language.KOTLIN:
-                    return "`kotlin-dsl-base`"
-            }
-        }
-
-        private void writeBuildLogicPlugin() {
-            switch (language) {
-                case Language.JAVA:
-                case Language.GROOVY:
-                    writeSourceFile language.fileExtension, "BuildLogicPlugin.${language.fileExtension}", """
-                        public class BuildLogicPlugin implements ${Plugin.name}<${Project.name}> {
-                            public void apply(${Project.name} project) {
-                                project.getTasks().register("$task", GreetTask.class);
-                            }
-                        }
-                    """
-                    break
-                case Language.KOTLIN:
-                    writeSourceFile "kotlin", "BuildLogicPlugin.kt", """
-                        class BuildLogicPlugin : ${Plugin.name}<${Project.name}> {
-                            override fun apply(project: ${Project.name}) {
-                                project.tasks.register("$task", GreetTask::class.java);
-                            }
-                        }
-                    """
-                    break
-            }
-        }
-
-        private void writeGreetTask(String className, String greeting) {
-            switch (language) {
-                case Language.GROOVY:
-                case Language.JAVA:
-                    writeSourceFile language.fileExtension, "${className}.${language.fileExtension}", """
-                        public class ${className} extends ${DefaultTask.name} {
-                            @${TaskAction.name} void greet() { System.out.println("${greeting}"); }
-                        }
-                    """
-                    break
-                case Language.KOTLIN:
-                    writeSourceFile "kotlin", "${className}.kt", """
-                        open class ${className} : ${DefaultTask.name}() {
-                            @${TaskAction.name} fun greet() { println("${greeting}") }
-                        }
-                    """
-                    break
-            }
-        }
-
-        private void writeResource(String text) {
-            writeSourceFile "resources", "resource.txt", text
-        }
-
-        private void writeSourceFile(String sourceSet, String fileName, String text) {
-            file("src/main/" + sourceSet + "/" + fileName).text = text
-        }
-
-        private TestFile file(String path) {
-            projectDir.file(path)
-        }
     }
 }
