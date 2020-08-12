77d7a039e75a0891c759827bcba8124f9c364b77
Jendrik Johannes <jendrik@gradle.com>
2020-08-12 12:17:41 +0200

    Introduce a subproject in hierarchical composite build sample
    We keep a build.gradle in the root for the "publishDeps" lifecycle
    task. This shows a useful use of the root build script (which is
    not used anymore in many other samples).

--- subprojects/docs/src/samples/build-organization/composite-builds/hierarchical-multirepo/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/build-organization/composite-builds/hierarchical-multirepo/kotlin/build.gradle.kts
@@ -1,30 +1,3 @@
-plugins {
-    java
-    application
-    idea
-}
-
-group = "org.sample"
-version = "1.0"
-
-application {
-    mainClass.set("org.sample.myapp.Main")
-}
-
-dependencies {
-    implementation("org.sample:number-utils:1.0")
-    implementation("org.sample:string-utils:1.0")
-}
-
-repositories {
-    ivy {
-        url = uri(project.file("../local-repo"))
-    }
-    jcenter()
-}
-
-// tag::publishDeps[]
 tasks.register("publishDeps") {
-    dependsOn(gradle.includedBuilds.map { it.task(":publishIvyPublicationToIvyRepository") })
+    dependsOn(gradle.includedBuilds.map { it.task(":publishMavenPublicationToMavenRepository") })
 }
-// end::publishDeps[]
