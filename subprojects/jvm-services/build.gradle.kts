3ffe2ef1df36b984eccb096e7adab995d2e76b11
Benjamin Muskalla <bmuskalla@gradle.com>
2020-08-14 14:35:06 +0200

    Move installation suppliers to jvm-services

--- subprojects/jvm-services/build.gradle.kts
+++ subprojects/jvm-services/build.gradle.kts
@@ -6,7 +6,9 @@ description = "JVM invocation and inspection abstractions"
 
 dependencies {
     implementation(project(":base-services"))
+    implementation(project(":core-api"))
     implementation(project(":process-services"))
+    implementation(libs.inject)
 
     testImplementation(project(":native"))
     testImplementation(project(":core-api"))
