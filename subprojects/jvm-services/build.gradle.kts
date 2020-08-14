15f681d855c35cc12cb013b21e4325924d9c354c
Benjamin Muskalla <bmuskalla@gradle.com>
2020-08-14 15:06:44 +0200

    Move installation registry to jvm-services

--- subprojects/jvm-services/build.gradle.kts
+++ subprojects/jvm-services/build.gradle.kts
@@ -8,13 +8,16 @@ dependencies {
     implementation(project(":base-services"))
     implementation(project(":core-api"))
     implementation(project(":process-services"))
+    implementation(project(":logging"))
     implementation(libs.inject)
+    implementation(libs.guava)
 
     testImplementation(project(":native"))
-    testImplementation(project(":core-api"))
     testImplementation(project(":file-collections"))
     testImplementation(project(":snapshots"))
     testImplementation(project(":resources"))
     testImplementation(libs.slf4jApi)
     testImplementation(testFixtures(project(":core")))
+
+    integTestDistributionRuntimeOnly(project(":distributions-core"))
 }
