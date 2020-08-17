de2766e096f4d720bf2f014adb063fb51a8efe06
Jendrik Johannes <jendrik@gradle.com>
2020-08-17 13:40:07 +0200

    Move description statement
    In a Groovy DSL script, it would not be allows to do this before the
    `plugins {}` block.

--- subprojects/normalization-java/normalization-java.gradle.kts
+++ subprojects/normalization-java/normalization-java.gradle.kts
@@ -13,12 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-description = "API extraction for Java"
 
 plugins {
     id("gradlebuild.distribution.api-java")
     id("gradlebuild.publish-public-libraries")
 }
+
+description = "API extraction for Java"
 
 dependencies {
     implementation(project(":base-annotations"))
