108bbe670c46cc1754509d2229aa0bdf31c941ca
Donat Csikos <donat@gradle.com>
2020-08-18 17:21:32 +0200

    Adjust buildlifecycle/projectEvaluateEvents snippet

--- subprojects/docs/src/snippets/buildlifecycle/projectEvaluateEvents/kotlin/settings.gradle.kts
+++ subprojects/docs/src/snippets/buildlifecycle/projectEvaluateEvents/kotlin/settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "projectEvaluateEvents"
+rootProject.name = "project-evaluate-events"
 include("project-a", "project-b")
 
 project(":project-a").buildFileName = "../project-a.gradle.kts"
