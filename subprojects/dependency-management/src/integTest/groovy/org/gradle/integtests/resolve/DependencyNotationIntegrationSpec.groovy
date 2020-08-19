46e63282b01d1d70af6df8a18cf2d7b337434f72
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-08-19 09:45:55 -0300

    Fix merge gone wrong after `@ToBeFixedFor{InstantExecution=>ConfigurationCache}`

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/DependencyNotationIntegrationSpec.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/DependencyNotationIntegrationSpec.groovy
@@ -264,7 +264,7 @@ task checkDeps
         succeeds "check"
     }
 
-    @ToBeFixedForInstantExecution(because = "Dependency report task isn't compatible")
+    @ToBeFixedForConfigurationCache(because = "Dependency report task isn't compatible")
     def "warns if using a configuration as a dependency"() {
         given:
         buildFile << """
