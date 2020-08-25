46e05edbeaf4ac6531c1cb9d82a40bf1f35f431f
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-08-25 15:17:35 +0300

    Remove unreferenced Groovy snippets ant their corresponding tests
    Those are not referenced in the documentation.

--- subprojects/docs/src/snippets/groovy/multiproject/groovy/testproject/src/main/groovy/org/gradle/GroovyPerson.groovy
+++
@@ -1,9 +0,0 @@
-package org.gradle
-
-class GroovyPerson {
-    String readProperty() throws IOException {
-        Properties properties = new Properties()
-        properties.load(getClass().getClassLoader().getResourceAsStream("org/gradle/main.properties"))
-        properties.properties.main
-    }
-}
