3683c6594b0e1520b83af881e681ff58090de21b
Bo Zhang <bo@gradle.com>
2020-08-27 10:40:00 +0800

    Fix os name case

--- .teamcity/Gradle_Check/configurations/BuildJavaVersions.kt
+++ .teamcity/Gradle_Check/configurations/BuildJavaVersions.kt
@@ -4,6 +4,6 @@ import common.JvmVendor
 import common.JvmVersion
 import common.Os
 
-fun buildJavaHome(os: Os = Os.LINUX) = "%$os.${JvmVersion.java11}.${JvmVendor.openjdk}.64bit%"
+fun buildJavaHome(os: Os = Os.LINUX) = "%${os.name.toLowerCase()}.${JvmVersion.java11}.${JvmVendor.openjdk}.64bit%"
 
 fun individualPerformanceTestJavaHome(os: Os) = "%$os.${JvmVersion.java8}.${JvmVendor.oracle}.64bit%"
