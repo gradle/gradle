921ba2e3ee72fb4e252053021180bb470eeca74d
Bo Zhang <bo@gradle.com>
2020-08-27 11:00:55 +0800

    Fix performance coordinator name case

--- .teamcity/Gradle_Check/configurations/BuildJavaVersions.kt
+++ .teamcity/Gradle_Check/configurations/BuildJavaVersions.kt
@@ -6,4 +6,4 @@ import common.Os
 
 fun buildJavaHome(os: Os = Os.LINUX) = "%${os.name.toLowerCase()}.${JvmVersion.java11}.${JvmVendor.openjdk}.64bit%"
 
-fun individualPerformanceTestJavaHome(os: Os) = "%$os.${JvmVersion.java8}.${JvmVendor.oracle}.64bit%"
+fun individualPerformanceTestJavaHome(os: Os) = "%${os.name.toLowerCase()}.${JvmVersion.java8}.${JvmVendor.oracle}.64bit%"
