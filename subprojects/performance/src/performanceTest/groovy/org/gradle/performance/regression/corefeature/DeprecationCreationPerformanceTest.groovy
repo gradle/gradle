2f063c3d6269a956a0325065d1687329d4370b8f
Bo Zhang <bo@gradle.com>
2020-08-28 11:44:50 +0800

    Rebaseline tiny regressions
    We observed tiny and accumulated regressions, rebaseline them.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/DeprecationCreationPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/DeprecationCreationPerformanceTest.groovy
@@ -26,7 +26,7 @@ class DeprecationCreationPerformanceTest extends AbstractCrossVersionGradleProfi
         runner.tasksToRun = ['help']
         runner.gradleOpts = ["-Xms1g", "-Xmx1g"]
         runner.minimumBaseVersion = '4.9'
-        runner.targetVersions = ["6.7-20200804220106+0000"]
+        runner.targetVersions = ["6.7-20200827220028+0000"]
         when:
         def result = runner.run()
 
