00579bdea2f581debbd9ea8c28890afd78548dcf
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-08-31 10:34:43 +0300

    Remove cross-configuration from multiproject-outgoingArtifact snippet
    Also update the custom task code to use managed properties

--- subprojects/docs/src/snippets/multiproject/dependencies-outgoingArtifact/kotlin/producer/build.gradle.kts
+++ subprojects/docs/src/snippets/multiproject/dependencies-outgoingArtifact/kotlin/producer/build.gradle.kts
@@ -1,10 +1,16 @@
+plugins {
+    id("java-library")
+}
+
+version = "1.0"
+
 val buildInfo by tasks.registering(BuildInfo::class) {
-    version = project.version.toString()
-    outputFile = file("$buildDir/generated-resources/build-info.properties")
+    version.set(project.version.toString())
+    outputFile.set(file("$buildDir/generated-resources/build-info.properties"))
 }
 
 sourceSets {
     main {
-        output.dir(buildInfo.get().outputFile.parentFile, "builtBy" to buildInfo)
+        output.dir(buildInfo.get().outputFile.getAsFile().get().parentFile, "builtBy" to buildInfo)
     }
 }
