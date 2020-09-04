a161009ac09ba7f0e5737a82a0ab4bc8edaeba79
Jendrik Johannes <jendrik@gradle.com>
2020-09-04 12:12:34 +0200

    Support multiple subprojects in TemplateFactory

--- subprojects/build-init/src/main/java/org/gradle/buildinit/plugins/internal/ConditionalTemplateOperation.java
+++ subprojects/build-init/src/main/java/org/gradle/buildinit/plugins/internal/ConditionalTemplateOperation.java
@@ -17,11 +17,13 @@ package org.gradle.buildinit.plugins.internal;
 
 import org.gradle.internal.Factory;
 
+import java.util.List;
+
 public class ConditionalTemplateOperation implements TemplateOperation {
-    private final TemplateOperation[] optionalOperations;
+    private final List<TemplateOperation> optionalOperations;
     private final Factory<Boolean> condition;
 
-    public ConditionalTemplateOperation(Factory<Boolean> precondition, TemplateOperation... optionalOperations) {
+    public ConditionalTemplateOperation(Factory<Boolean> precondition, List<TemplateOperation> optionalOperations) {
         this.condition = precondition;
         this.optionalOperations = optionalOperations;
     }
