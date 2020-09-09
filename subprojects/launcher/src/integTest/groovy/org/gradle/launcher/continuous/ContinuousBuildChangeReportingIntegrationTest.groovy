d69f0b6c62485bbb92cc856e23c026c3b36df6d5
Bo Zhang <bo@gradle.com>
2020-09-09 15:10:53 +0800

    Make method names unique
    We keep getting weird errors from launcher project. It seems
    that the duplicate test names screwed up the test infrastructure.

--- subprojects/launcher/src/integTest/groovy/org/gradle/launcher/continuous/ContinuousBuildChangeReportingIntegrationTest.groovy
+++ subprojects/launcher/src/integTest/groovy/org/gradle/launcher/continuous/ContinuousBuildChangeReportingIntegrationTest.groovy
@@ -21,6 +21,7 @@ import org.gradle.integtests.fixtures.AbstractContinuousIntegrationTest
 import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
 import org.gradle.internal.os.OperatingSystem
 import org.gradle.test.fixtures.file.TestFile
+import spock.lang.Unroll
 
 import static org.gradle.internal.filewatch.DefaultFileSystemChangeWaiterFactory.QUIET_PERIOD_SYSPROP
 import static org.gradle.internal.filewatch.DefaultFileWatcherEventListener.SHOW_INDIVIDUAL_CHANGES_LIMIT
@@ -83,7 +84,8 @@ class ContinuousBuildChangeReportingIntegrationTest extends AbstractContinuousIn
         assertReportsChanges(inputFiles.collect { new ChangeEntry('new file', it) }, true)
     }
 
-    def "should report the changes when files are removed"(changesCount) {
+    @Unroll
+    def "should report the changes when files are removed with #changesCount"(changesCount) {
         given:
         def inputFiles = (1..changesCount).collect { inputDir.file("input${it}.txt") }
         inputFiles.each { it.text = 'New input file' }
@@ -102,7 +104,8 @@ class ContinuousBuildChangeReportingIntegrationTest extends AbstractContinuousIn
         changesCount << [1, changesLimit, 11]
     }
 
-    def "should report the changes when files are modified"(changesCount) {
+    @Unroll
+    def "should report the changes when files are modified #changesCount"(changesCount) {
         given:
         def inputFiles = (1..changesCount).collect { inputDir.file("input${it}.txt") }
         inputFiles.each { it.text = 'New input file' }
@@ -121,7 +124,8 @@ class ContinuousBuildChangeReportingIntegrationTest extends AbstractContinuousIn
         changesCount << [1, changesLimit, 11]
     }
 
-    def "should report the changes when directories are created"(changesCount) {
+    @Unroll
+    def "should report the changes when directories are created #changesCount"(changesCount) {
         given:
         def inputDirectories = (1..changesCount).collect { inputDir.file("input${it}Directory") }
         boolean expectMoreChanges = (changesCount > changesLimit)
@@ -139,8 +143,8 @@ class ContinuousBuildChangeReportingIntegrationTest extends AbstractContinuousIn
         changesCount << [1, changesLimit, 11]
     }
 
-
-    def "should report the changes when directories are deleted"(changesCount) {
+    @Unroll
+    def "should report the changes when directories are deleted #changesCount"(changesCount) {
         given:
         def inputDirectories = (1..changesCount).collect { inputDir.file("input${it}Directory").createDir() }
         boolean expectMoreChanges = (changesCount > changesLimit)
