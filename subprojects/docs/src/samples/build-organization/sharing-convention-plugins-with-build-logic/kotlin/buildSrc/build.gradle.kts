5c739a624483fb2cd7c208706ff43485efe663c9
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-09-09 14:49:30 +0300

    Simplify sample by using precompiled plugin instead of binary Java one

--- subprojects/docs/src/samples/build-organization/sharing-convention-plugins-with-build-logic/kotlin/buildSrc/build.gradle.kts
+++ subprojects/docs/src/samples/build-organization/sharing-convention-plugins-with-build-logic/kotlin/buildSrc/build.gradle.kts
@@ -1,35 +1,11 @@
 // tag::plugins[]
 plugins {
-    id("java-gradle-plugin")
     id("myproject.java-conventions")
+    `kotlin-dsl`
 }
-// end::plugins[]
 
 repositories {
-    jcenter()
+    // for kotlin-dsl plugin
+    gradlePluginPortal()
 }
-
-dependencies {
-    testImplementation("junit:junit:4.13")
-}
-
-gradlePlugin {
-    val greeting by plugins.creating {
-        id = "com.example.plugin.greeting"
-        implementationClass = "com.example.plugin.GreetingPlugin"
-    }
-}
-
-val functionalTest by sourceSets.creating
-gradlePlugin.testSourceSets(functionalTest)
-
-configurations[functionalTest.implementationConfigurationName].extendsFrom(configurations.testImplementation.get())
-
-val functionalTestTask = tasks.register<Test>("functionalTest") {
-    testClassesDirs = functionalTest.output.classesDirs
-    classpath = configurations[functionalTest.runtimeClasspathConfigurationName] + functionalTest.output
-}
-
-tasks.check.configure {
-    dependsOn(functionalTestTask)
-}
+// end::plugins[]
---
+++ subprojects/docs/src/samples/build-organization/sharing-convention-plugins-with-build-logic/kotlin/buildSrc/src/main/kotlin/myproject.greeting.gradle.kts
@@ -0,0 +1,3 @@
+tasks.register<com.example.GreetingTask>("greet") {
+    greeting.set("Hello from 'myproject.greeting' plugin")
+}
