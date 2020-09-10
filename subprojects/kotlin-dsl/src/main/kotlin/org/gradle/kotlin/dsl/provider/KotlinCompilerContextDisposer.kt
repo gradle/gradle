56106e6a1357441d1fbbb1d6e6620bdc51b97f82
Paul Merlin <paul@gradle.com>
2020-09-10 17:19:12 +0200

    Hackily dispose kotlin compiler environment once projects are evaluated
    Signed-off-by: Paul Merlin <paul@gradle.com>

---
+++ subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/provider/KotlinCompilerContextDisposer.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.kotlin.dsl.provider
+
+import org.gradle.api.invocation.Gradle
+import org.gradle.internal.InternalBuildAdapter
+import org.gradle.internal.concurrent.Stoppable
+import org.gradle.internal.event.ListenerManager
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import kotlin.reflect.KClass
+import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.companionObjectInstance
+import kotlin.reflect.full.functions
+import kotlin.reflect.jvm.isAccessible
+
+
+/**
+ * Disposes Kotlin compiler environment once all scripts are compiled.
+ */
+class KotlinCompilerContextDisposer(
+    private val listenerManager: ListenerManager
+) : InternalBuildAdapter(), Stoppable {
+
+    init {
+        listenerManager.addListener(this)
+    }
+
+    override fun stop() {
+        listenerManager.removeListener(this)
+    }
+
+    override fun projectsEvaluated(gradle: Gradle) {
+        // TODO replace this reflective hack by direct compiler API invocation once available
+        val compilerEnvClass: KClass<*> = KotlinCoreEnvironment::class
+        val companionClass: KClass<*> = compilerEnvClass.companionObject!!
+        val companionInstance: Any = compilerEnvClass.companionObjectInstance!!
+        companionClass.functions
+            .first { it.name == "disposeApplicationEnvironment" }
+            .let { disposeFunction ->
+                disposeFunction.isAccessible = true
+                disposeFunction.call(companionInstance)
+            }
+    }
+}
