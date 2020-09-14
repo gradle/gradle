f3d6d9f1f4cfcbcb1f5a42fa3b90a2576b81f0ae
Louis Jacomet <louis@gradle.com>
2020-09-14 16:47:10 +0200

    Leverage toolchains in Java project building chapter
    Issue #14225

--- subprojects/docs/src/snippets/java/crossCompilation/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/java/crossCompilation/kotlin/build.gradle.kts
@@ -13,36 +13,10 @@ dependencies {
 }
 
 // tag::java-cross-compilation[]
-require(hasProperty("javaHome")) { "Set the property 'javaHome' in your your gradle.properties pointing to a Java 6 or 7 installation" }
-require(hasProperty("targetJavaVersion")) { "Set the property 'targetJavaVersion' in your your gradle.properties to '1.6' or '1.7'" }
-
-val javaHome: String by project
-val targetJavaVersion: String by project
-
 java {
-    sourceCompatibility = JavaVersion.toVersion(targetJavaVersion)
-}
-
-val javaExecutablesPath = File(javaHome, "bin")
-fun javaExecutable(execName: String): String {
-    val executable = File(javaExecutablesPath, execName)
-    require(executable.exists()) { "There is no ${execName} executable in ${javaExecutablesPath}" }
-    return executable.toString()
-}
-tasks.withType<JavaCompile>().configureEach {
-    options.apply {
-        isFork = true
-        forkOptions.javaHome = file(javaHome)
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(7))
     }
-}
-tasks.withType<Javadoc>().configureEach {
-    executable = javaExecutable("javadoc")
-}
-tasks.withType<Test>().configureEach {
-    executable = javaExecutable("java")
-}
-tasks.withType<JavaExec>().configureEach {
-    executable = javaExecutable("java")
 }
 // end::java-cross-compilation[]
 
