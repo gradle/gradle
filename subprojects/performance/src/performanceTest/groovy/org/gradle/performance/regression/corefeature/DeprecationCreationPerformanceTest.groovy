b244dfb7c2cd418f175405007656380fd2d9b979
Bo Zhang <bo@gradle.com>
2020-09-16 00:39:21 -0700

    Update subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/DeprecationCreationPerformanceTest.groovy

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/DeprecationCreationPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/DeprecationCreationPerformanceTest.groovy
@@ -24,7 +24,7 @@ class DeprecationCreationPerformanceTest extends AbstractCrossVersionPerformance
         runner.testProject = "generateLotsOfDeprecationWarnings"
         runner.tasksToRun = ['help']
         runner.gradleOpts = ["-Xms1g", "-Xmx1g"]
-        runner.minimumBaseVersion = '6.0'
+        runner.minimumBaseVersion = '6.3'
         runner.targetVersions = ["6.7-20200827220028+0000"]
         when:
         def result = runner.run()
