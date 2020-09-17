b0e8ead8026292d583612a43eee8ab8b7b69b7b5
Adam Murdoch <adam@gradle.com>
2020-09-17 10:01:06 +1000

    Update docs for `ToolingModelBuilderRegistry` to mention that it is available to plugins via service injection.

--- subprojects/core-api/src/main/java/org/gradle/tooling/provider/model/ToolingModelBuilderRegistry.java
+++ subprojects/core-api/src/main/java/org/gradle/tooling/provider/model/ToolingModelBuilderRegistry.java
@@ -21,6 +21,8 @@ import org.gradle.internal.service.scopes.ServiceScope;
 
 /**
  * A registry of tooling model builders. Adding a builder to this registry makes a model (or models) available via the tooling API.
+ *
+ * <p>An instance of this type can be injected into a task, plugin or other object by annotating a public constructor or property getter method with {@code javax.inject.Inject}.
  */
 @ServiceScope(Scopes.Build.class)
 public interface ToolingModelBuilderRegistry {
