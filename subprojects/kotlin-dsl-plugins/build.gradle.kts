9cbf59f6d2032dafa83b15afd4f6334bbe7cee45
Paul Merlin <paul@gradle.com>
2020-09-17 14:57:09 +0200

    Bump :kotlin-dsl-plugins version for 1.4
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl-plugins/build.gradle.kts
+++ subprojects/kotlin-dsl-plugins/build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
 description = "Kotlin DSL Gradle Plugins deployed to the Plugin Portal"
 
 group = "org.gradle.kotlin"
-version = "1.3.7"
+version = "1.4.0"
 
 base.archivesBaseName = "plugins"
 
