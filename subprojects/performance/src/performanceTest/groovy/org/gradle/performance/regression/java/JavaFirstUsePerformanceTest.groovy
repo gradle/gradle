805457f7533a75dee907f69c3490d4535d94cd9a
Bo Zhang <bo@gradle.com>
2020-09-21 10:01:31 +0800

    Rebaseline JavaFirstUsePerformanceTest
    https://github.com/gradle/gradle-private/issues/3211

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaFirstUsePerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaFirstUsePerformanceTest.groovy
@@ -32,7 +32,7 @@ import static org.gradle.performance.generator.JavaTestProject.LARGE_MONOLITHIC_
 class JavaFirstUsePerformanceTest extends AbstractCrossVersionPerformanceTest {
 
     def setup() {
-        runner.targetVersions = ["6.7-20200910233038+0000"]
+        runner.targetVersions = ["6.8-20200920220028+0000"]
     }
 
     @Unroll
