fd98a37f14e62163f24f8fe8fe7af8b904cf2249
Paul Merlin <paul@gradle.com>
2020-09-22 11:58:50 +0200

    Upgrade jmh plugin to 0.5.2
    with configuration cache support
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- buildSrc/subprojects/build-platform/build.gradle.kts
+++ buildSrc/subprojects/build-platform/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
         api("com.gradle.publish:plugin-publish-plugin:0.11.0")
         api("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:0.7")
         api("me.champeau.gradle:japicmp-gradle-plugin:0.2.9")
-        api("me.champeau.gradle:jmh-gradle-plugin:0.5.0")
+        api("me.champeau.gradle:jmh-gradle-plugin:0.5.2")
         api("org.asciidoctor:asciidoctor-gradle-plugin:1.5.10")
         api("org.gradle:test-retry-gradle-plugin:1.1.7")
         api("org.gradle.kotlin:gradle-kotlin-dsl-conventions:0.5.0")
