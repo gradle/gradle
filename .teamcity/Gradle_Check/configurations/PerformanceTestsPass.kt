4128fbd91babe20b928d2af96ef238441db6def9
Stefan Wolf <wolf@gradle.com>
2020-09-26 14:16:27 +0200

    Extract common code for channel specification

--- .teamcity/Gradle_Check/configurations/PerformanceTestsPass.kt
+++ .teamcity/Gradle_Check/configurations/PerformanceTestsPass.kt
@@ -26,7 +26,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.AbsoluteId
 import model.CIBuildModel
 import model.PerformanceTestType
 import projects.PerformanceTestProject
-import java.util.Locale
 
 class PerformanceTestsPass(model: CIBuildModel, performanceTestProject: PerformanceTestProject) : BaseGradleBuildType(model, init = {
     uuid = performanceTestProject.uuid + "_Trigger"
@@ -42,7 +41,7 @@ class PerformanceTestsPass(model: CIBuildModel, performanceTestProject: Performa
         param("env.JAVA_HOME", os.buildJavaHome())
         param("env.BUILD_BRANCH", "%teamcity.build.branch%")
         param("performance.db.username", "tcagent")
-        param("performance.channel", "${type.channel}${if (os == Os.LINUX) "" else "-${os.name.toLowerCase(Locale.US)}"}-%teamcity.build.branch%")
+        param("performance.channel", performanceTestProject.performanceTestCoverage.channel())
     }
 
     features {
