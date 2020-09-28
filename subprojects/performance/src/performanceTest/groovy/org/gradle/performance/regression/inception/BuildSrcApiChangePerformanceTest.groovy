e4f4a505d3ec0471d5ba1933b5fe4cc7488267dd
Vaidotas Valuckas <vvaluckas@gradle.com>
2020-09-28 10:23:54 +0300

    Rebaseline performance tests where Kotlin compilation speeds improved

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/inception/BuildSrcApiChangePerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/inception/BuildSrcApiChangePerformanceTest.groovy
@@ -26,7 +26,7 @@ import org.junit.experimental.categories.Category
 class BuildSrcApiChangePerformanceTest extends AbstractCrossVersionPerformanceTest {
 
     def setup() {
-        def targetVersion = "6.7-20200812220226+0000"
+        def targetVersion = "6.8-20200927220040+0000"
         runner.targetVersions = [targetVersion]
         runner.minimumBaseVersion = GradleVersion.version(targetVersion).baseVersion.version
     }
