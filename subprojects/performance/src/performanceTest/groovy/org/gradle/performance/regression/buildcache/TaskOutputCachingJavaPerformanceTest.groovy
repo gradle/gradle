44258715914f561abc1a0f6ed3201c2581b20cfd
Stefan Wolf <wolf@gradle.com>
2020-09-29 16:08:40 +0200

    Do not re-package the cached artifacts
    for some build cache performance tests

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/buildcache/TaskOutputCachingJavaPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/buildcache/TaskOutputCachingJavaPerformanceTest.groovy
@@ -16,19 +16,11 @@
 
 package org.gradle.performance.regression.buildcache
 
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
-import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
-import org.gradle.internal.hash.Hashing
 import org.gradle.performance.fixture.CrossVersionPerformanceTestRunner
 import org.gradle.performance.generator.JavaTestProject
-import org.gradle.profiler.BuildContext
-import org.gradle.profiler.BuildMutator
 import org.gradle.profiler.mutations.ApplyAbiChangeToJavaSourceFileMutator
 import org.gradle.profiler.mutations.ApplyNonAbiChangeToJavaSourceFileMutator
 import org.gradle.test.fixtures.keystore.TestKeyStore
-
-import java.util.zip.GZIPInputStream
-import java.util.zip.GZIPOutputStream
 
 class TaskOutputCachingJavaPerformanceTest extends AbstractTaskOutputCachingPerformanceTest {
 
@@ -45,7 +37,6 @@ class TaskOutputCachingJavaPerformanceTest extends AbstractTaskOutputCachingPerf
         pushToRemote = true
         runner.useDaemon = false
         runner.addBuildMutator { cleanLocalCache() }
-        runner.addBuildMutator() { touchCacheArtifacts() }
 
         when:
         def result = runner.run()
@@ -60,7 +51,6 @@ class TaskOutputCachingJavaPerformanceTest extends AbstractTaskOutputCachingPerf
         pushToRemote = true
         runner.useDaemon = false
         runner.addBuildMutator { cleanLocalCache() }
-        runner.addBuildMutator { touchCacheArtifacts() }
 
         def keyStore = TestKeyStore.init(temporaryFolder.file('ssl-keystore'))
         keyStore.enableSslWithServerCert(buildCacheServer)
@@ -112,7 +102,6 @@ class TaskOutputCachingJavaPerformanceTest extends AbstractTaskOutputCachingPerf
         setupTestProject(runner)
         runner.args += "--parallel"
         pushToRemote = false
-        runner.addBuildMutator { touchCacheArtifacts() }
 
         when:
         def result = runner.run()
@@ -128,7 +117,6 @@ class TaskOutputCachingJavaPerformanceTest extends AbstractTaskOutputCachingPerf
         runner.addBuildMutator { new ApplyAbiChangeToJavaSourceFileMutator(new File(it.projectDir, testProject.config.fileToChangeByScenario['assemble'])) }
         runner.args += "--parallel"
         pushToRemote = false
-        runner.addBuildMutator { touchCacheArtifacts() }
 
         when:
         def result = runner.run()
@@ -144,65 +132,12 @@ class TaskOutputCachingJavaPerformanceTest extends AbstractTaskOutputCachingPerf
         runner.addBuildMutator { new ApplyNonAbiChangeToJavaSourceFileMutator(new File(it.projectDir, testProject.config.fileToChangeByScenario['assemble'])) }
         runner.args += "--parallel"
         pushToRemote = false
-        runner.addBuildMutator { touchCacheArtifacts() }
 
         when:
         def result = runner.run()
 
         then:
         result.assertCurrentVersionHasNotRegressed()
-    }
-
-    private BuildMutator touchCacheArtifacts() {
-        new BuildMutator() {
-            @Override
-            void beforeBuild(BuildContext context) {
-                touchCacheArtifactsDir(cacheDir)
-                if (buildCacheServer.running) {
-                    touchCacheArtifactsDir(buildCacheServer.cacheDir)
-                }
-            }
-        }
-    }
-
-    // We change the file dates inside the archives to work around unfairness caused by
-    // reusing FileCollectionSnapshots based on the file dates in versions before Gradle 4.2
-    void touchCacheArtifactsDir(File dir) {
-        def startTime = System.currentTimeMillis()
-        int count = 0
-        dir.eachFile { File cacheArchiveFile ->
-            if (cacheArchiveFile.name ==~ /[a-z0-9]{${Hashing.defaultFunction().hexDigits}}/) {
-                def tempFile = temporaryFolder.file("re-tar-temp")
-                tempFile.withOutputStream { outputStream ->
-                    def tarOutput = new TarArchiveOutputStream(new GZIPOutputStream(outputStream))
-                    tarOutput.setLongFileMode(TarArchiveOutputStream.LONGFILE_POSIX)
-                    tarOutput.setBigNumberMode(TarArchiveOutputStream.BIGNUMBER_POSIX)
-                    tarOutput.setAddPaxHeadersForNonAsciiNames(true)
-                    cacheArchiveFile.withInputStream { inputStream ->
-                        def tarInput = new TarArchiveInputStream(new GZIPInputStream(inputStream))
-                        while (true) {
-                            def tarEntry = tarInput.nextTarEntry
-                            if (tarEntry == null) {
-                                break
-                            }
-
-                            tarEntry.setModTime(tarEntry.modTime.time + 3743)
-                            tarOutput.putArchiveEntry(tarEntry)
-                            if (!tarEntry.directory) {
-                                tarOutput << tarInput
-                            }
-                            tarOutput.closeArchiveEntry()
-                        }
-                    }
-                    tarOutput.close()
-                }
-                assert cacheArchiveFile.delete()
-                assert tempFile.renameTo(cacheArchiveFile)
-            }
-            count++
-        }
-        def time = System.currentTimeMillis() - startTime
-        println "Changed file dates in $count cache artifacts in $dir in ${time} ms"
     }
 
     static def setupTestProject(CrossVersionPerformanceTestRunner runner) {
