7333b0cb754158de477346a2e190bf7784a44f03
Lóránt Pintér <lorant@gradle.com>
2020-10-09 13:07:20 +0200

    Improve test label

--- subprojects/tooling-api/src/testFixtures/groovy/org/gradle/integtests/tooling/fixture/ToolingApiExecution.groovy
+++ subprojects/tooling-api/src/testFixtures/groovy/org/gradle/integtests/tooling/fixture/ToolingApiExecution.groovy
@@ -38,7 +38,7 @@ class ToolingApiExecution extends AbstractMultiTestRunner.Execution implements T
 
     @Override
     protected String getDisplayName() {
-        return "TAPI: ${displayName(toolingApi.version)}, target: ${displayName(gradle.version)}"
+        return "TAPI ${displayName(toolingApi.version)} -> Gradle ${displayName(gradle.version)}"
     }
 
     @Override
