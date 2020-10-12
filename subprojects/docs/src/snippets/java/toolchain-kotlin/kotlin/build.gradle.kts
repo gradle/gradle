b0d91b1f224d3105a60813c33c93298732036125
Louis Jacomet <louis@gradle.com>
2020-10-12 12:38:55 +0200

    Add example for Java home wiring
    Issue #14225

---
+++ subprojects/docs/src/snippets/java/toolchain-kotlin/kotlin/build.gradle.kts
@@ -0,0 +1,15 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+// tag::compiler-kotlin[]
+val compiler = javaToolchains.compilerFor {
+    languageVersion.set(JavaLanguageVersion.of(11))
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    kotlinOptions.jdkHome = compiler.get().metadata.installationPath.asFile.absolutePath
+}
+// end::compiler-kotlin[]
