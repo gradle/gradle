c5a7a72fc60693163c3fd990a5287318d83304cd
Paul Merlin <paul@gradle.com>
2020-10-15 09:57:27 +0200

    Raise -all dist size a bit more
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/AllDistributionIntegrationSpec.groovy
+++ subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/AllDistributionIntegrationSpec.groovy
@@ -34,7 +34,7 @@ class AllDistributionIntegrationSpec extends DistributionIntegrationSpec {
 
     @Override
     int getMaxDistributionSizeBytes() {
-        return 144 * 1024 * 1024
+        return 145 * 1024 * 1024
     }
 
     def allZipContents() {
