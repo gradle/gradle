a211d529a6abcce8cdbf2816377295d9fc9f1826
Stefan Wolf <wolf@gradle.com>
2020-10-19 17:27:44 +0200

    Use equals for checking for jprofiler

--- subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/AbstractBuildExperimentRunner.java
+++ subprojects/internal-performance-testing/src/main/groovy/org/gradle/performance/fixture/AbstractBuildExperimentRunner.java
@@ -75,7 +75,7 @@ public abstract class AbstractBuildExperimentRunner implements BuildExperimentRu
         optionParser.accepts("profiler");
         ProfilerFactory.configureParser(optionParser);
         ProfilerFactory profilerFactory = ProfilerFactory.of(Collections.singletonList(profilerName));
-        String[] options = profilerName.startsWith("jprofiler")
+        String[] options = profilerName.equals("jprofiler")
             ? new String[] { "--profile", "jprofiler", "--jprofiler-home", System.getenv("JPROFILER_HOME") }
             : new String[] {};
         return profilerFactory.createFromOptions(optionParser.parse(options));
