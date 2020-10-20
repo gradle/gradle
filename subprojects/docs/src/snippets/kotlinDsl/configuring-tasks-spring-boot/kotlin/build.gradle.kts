9bdf25081a9c621166416ca455ffa167fd1db61a
Jendrik Johannes <jendrik@gradle.com>
2020-10-20 13:05:35 +0200

    Move 'Migrating build logic from Groovy to Kotlin' into user manual
    Content from:
    guides.gradle.org/migrating-build-logic-from-groovy-to-kotlin/
    
    The content is mostly kept in place with minor updates.

---
+++ subprojects/docs/src/snippets/kotlinDsl/configuring-tasks-spring-boot/kotlin/build.gradle.kts
@@ -0,0 +1,36 @@
+// tag::lazy[]
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.springframework.boot.gradle.tasks.run.BootRun
+
+// tag::accessors[]
+plugins {
+    java
+    id("org.springframework.boot") version "2.3.4.RELEASE"
+}
+
+// end::lazy[]
+// end::accessors[]
+
+// tag::accessors[]
+tasks.bootJar {
+    archiveFileName.set("app.jar")
+    mainClassName = "com.example.demo.Demo"
+}
+
+tasks.bootRun {
+    main = "com.example.demo.Demo"
+    args("--spring.profiles.active=demo")
+}
+// end::accessors[]
+
+// tag::lazy[]
+tasks.named<BootJar>("bootJar") {
+    archiveFileName.set("app.jar")
+    mainClassName = "com.example.demo.Demo"
+}
+
+tasks.named<BootRun>("bootRun") {
+    main = "com.example.demo.Demo"
+    args("--spring.profiles.active=demo")
+}
+// end::lazy[]
