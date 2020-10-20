539fdbcd4163342b99afac1d2307ffb3bd5a680b
Stefan Wolf <wolf@gradle.com>
2020-10-20 13:13:19 +0200

    Add memory settings to createLotsOfTasks

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/TaskCreationPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/TaskCreationPerformanceTest.groovy
@@ -31,7 +31,6 @@ class TaskCreationPerformanceTest extends AbstractCrossVersionPerformanceTest {
     def "create many tasks"() {
         given:
         runner.tasksToRun = ['help']
-        runner.gradleOpts = ["-Xms1g", "-Xmx1g"]
         runner.targetVersions = ["6.7-20200824220048+0000"]
         runner.runs = 60
 
