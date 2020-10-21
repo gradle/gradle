81fa7597bbd376f5f1d05143cabd50c523f318ae
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2020-10-21 15:48:44 -0300

    Remove `@ToBeFixedForConfigurationCache` from `IncrementalBuildOutputOriginIntegrationTest`

--- subprojects/core/src/integTest/groovy/org/gradle/api/tasks/outputorigin/IncrementalBuildOutputOriginIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/tasks/outputorigin/IncrementalBuildOutputOriginIntegrationTest.groovy
@@ -19,7 +19,6 @@ package org.gradle.api.tasks.outputorigin
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.OriginFixture
 import org.gradle.integtests.fixtures.ScopeIdsFixture
-import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
 import org.gradle.integtests.fixtures.UnsupportedWithConfigurationCache
 import org.junit.Rule
 
@@ -160,7 +159,6 @@ class IncrementalBuildOutputOriginIntegrationTest extends AbstractIntegrationSpe
         originBuildInvocationId(":buildSrc:w") == origin
     }
 
-    @ToBeFixedForConfigurationCache(because = "composite builds")
     def "composite participant tasks advertise build id"() {
         given:
         ["a", "b"].each {
