bff7d2d0d19ab7f39134d10eac3f482b1e8c77af
Gary Hale <gary@gradle.com>
2020-11-11 13:50:51 -0500

    Revert "Remove annotations for internal APIs that test-distribution no longer uses (#14093)"
    This reverts commit a71f4a8a1ba38738bcd2a4508b7e649bf095f3d9.
    
    Requested by @marcphilipp.

--- subprojects/core/src/main/java/org/gradle/api/internal/tasks/properties/PropertyVisitor.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/tasks/properties/PropertyVisitor.java
@@ -18,12 +18,14 @@ package org.gradle.api.internal.tasks.properties;
 
 import org.gradle.api.tasks.FileNormalizer;
 import org.gradle.internal.fingerprint.impl.DirectorySensitivity;
+import org.gradle.internal.scan.UsedByScanPlugin;
 
 import javax.annotation.Nullable;
 
 /**
  * Visits properties of beans which are inputs, outputs, destroyables or local state.
  */
+@UsedByScanPlugin("test-distribution")
 public interface PropertyVisitor {
     void visitInputFileProperty(String propertyName, boolean optional, boolean skipWhenEmpty, DirectorySensitivity directorySensitivity, boolean incremental, @Nullable Class<? extends FileNormalizer> fileNormalizer, PropertyValue value, InputFilePropertyType filePropertyType);
 
@@ -35,7 +37,9 @@ public interface PropertyVisitor {
 
     void visitLocalStateProperty(Object value);
 
+    @UsedByScanPlugin("test-distribution")
     class Adapter implements PropertyVisitor {
+        @UsedByScanPlugin("test-distribution")
         @Override
         public void visitInputFileProperty(String propertyName, boolean optional, boolean skipWhenEmpty, DirectorySensitivity directorySensitivity, boolean incremental, @Nullable Class<? extends FileNormalizer> fileNormalizer, PropertyValue value, InputFilePropertyType filePropertyType) {
         }
@@ -45,6 +49,7 @@ public interface PropertyVisitor {
         }
 
         @Override
+        @UsedByScanPlugin("test-distribution")
         public void visitOutputFileProperty(String propertyName, boolean optional, PropertyValue value, OutputFilePropertyType filePropertyType) {
         }
 
