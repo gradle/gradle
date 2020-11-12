948c6bacf1dc82081213cd9d66d4792cb384c363
Cedric Champeau <cedric@gradle.com>
2020-11-12 20:06:47 +0100

    Initial documentation for the version catalog
    This commit introduces documentation for the whole "version catalog" feature, which includes
    declaring a version catalog in settings, in an external TOML file, and via the version catalog
    plugin.

---
+++ subprojects/docs/src/snippets/dependencyManagement/catalogs-settings/kotlin/build.gradle.kts
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    `java-library`
+    checkstyle
+}
+
+// tag::simple_dependency_use[]
+dependencies {
+    implementation(libs.groovy)
+}
+// end::simple_dependency_use[]
+
+// tag::use_version[]
+checkstyle {
+    // will use the version declared in the catalog
+    toolVersion = libs.versions.checkstyle.get()
+}
+// end::use_version[]
+
+// tag::use_catalog[]
+dependencies {
+    implementation(libs.groovy)
+    implementation(libs.groovyJson)
+    implementation(libs.groovyNio)
+}
+// end::use_catalog[]
+
+// tag::use_catalog_equiv[]
+dependencies {
+    implementation("org.codehaus.groovy:groovy:3.0.5")
+    implementation("org.codehaus.groovy:groovy-json:3.0.5")
+    implementation("org.codehaus.groovy:groovy-nio:3.0.5")
+}
+// end::use_catalog_equiv[]
+
+// tag::use_dependency_bundle[]
+dependencies {
+    implementation(libs.bundles.groovy)
+}
+// end::use_dependency_bundle[]
