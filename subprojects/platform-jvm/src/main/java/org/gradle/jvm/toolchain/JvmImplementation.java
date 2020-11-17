aa1ebba2dfd663cf2ea54dbe2bd05805152b043d
Benjamin Muskalla <bmuskalla@gradle.com>
2020-11-17 15:45:42 +0100

    Add missing javadoc for JvmImplementation

--- subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/JvmImplementation.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/JvmImplementation.java
@@ -27,7 +27,19 @@ import org.gradle.api.Incubating;
 @Incubating
 public final class JvmImplementation {
 
+    /**
+     * Vendor-specific virtual machine implementation.
+     *
+     * Acts as a placeholder and matches any implementation from any vendor.
+     * Usually used to override specific implementation requests.
+     */
     public static final JvmImplementation VENDOR_SPECIFIC = new JvmImplementation("vendor-specific");
+
+    /**
+     * Eclipse OpenJ9 (previously known as IBM J9) virtual machine implementation.
+     *
+     * Matches only virtual machine implementations using the OpenJ9/IBM J9 runtime engine.
+     */
     public static final JvmImplementation J9 = new JvmImplementation("J9");
 
     private final String displayName;
