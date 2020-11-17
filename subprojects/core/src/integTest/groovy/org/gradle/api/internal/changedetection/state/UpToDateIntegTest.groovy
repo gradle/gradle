e212625051773d1a8229bea6d35c2f79d2fdc77f
Lóránt Pintér <lorant@gradle.com>
2020-11-17 22:33:43 +0100

    Add test for task staying up-to-date after adding null output property

--- subprojects/core/src/integTest/groovy/org/gradle/api/internal/changedetection/state/UpToDateIntegTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/internal/changedetection/state/UpToDateIntegTest.groovy
@@ -18,6 +18,7 @@ package org.gradle.api.internal.changedetection.state
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import spock.lang.Issue
+import spock.lang.Unroll
 
 class UpToDateIntegTest extends AbstractIntegrationSpec {
 
@@ -159,5 +160,36 @@ public abstract class CreateEmptyDirectory extends DefaultTask {
         then:
         skipped customTask
         result.output =~ /Skipping task '${customTask}' as it is up-to-date\./
+    }
+
+    @Unroll
+    def "registering an optional #type output property with a null value keeps task up-to-date"() {
+        buildFile << """
+            task customTask(type: CustomTask) {
+                outputFile = file("\$buildDir/outputFile")
+                if (project.hasProperty("addNullOutput")) {
+                    outputs.$type(null).optional()
+                }
+            }
+
+            class CustomTask extends DefaultTask {
+                @OutputFile
+                File outputFile
+
+                @TaskAction
+                public void doStuff() {
+                    outputFile.text = "output"
+                }
+            }
+        """
+
+        run "customTask"
+        when:
+        run "customTask", "-PaddNullOutput"
+        then:
+        skipped ":customTask"
+
+        where:
+        type << ["dir", "file"]
     }
 }
