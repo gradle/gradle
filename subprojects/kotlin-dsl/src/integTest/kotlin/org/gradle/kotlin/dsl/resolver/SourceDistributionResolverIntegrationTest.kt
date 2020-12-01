c078e496135c042aeeb48a62b0d18635a99ffca6
Cedric Champeau <cedric@gradle.com>
2020-12-01 14:03:24 +0100

    Fix Kotlin DSL sync when failing on project repositories
    When using the central repositories declaration and the Kotlin DSL,
    IDE sync could fail if the repositories mode was set to failing on
    project repositories.
    
    The reason is that the Kotlin DSL sources provider was adding a
    repository transparently, to resolve the Gradle sources, then removed
    it silently.
    
    However, this behavior is not allowed when failing on project
    repositories.
    
    This commit fixes the problem by making sure the Kotlin DSL sources
    provider works in isolation, not leaking any repository to the
    project. To do this, we introduced an _internal_ detached resolver,
    which is basically an isolated dependency resolution engine, with
    its own repositories, configurations, dependencies, ...
    
    The goal of this PR is _not_ to provide a public API for this, but
    use a pragmatic solution for fixing the problem mentioned above.

--- subprojects/kotlin-dsl/src/integTest/kotlin/org/gradle/kotlin/dsl/resolver/SourceDistributionResolverIntegrationTest.kt
+++ subprojects/kotlin-dsl/src/integTest/kotlin/org/gradle/kotlin/dsl/resolver/SourceDistributionResolverIntegrationTest.kt
@@ -1,6 +1,8 @@
 package org.gradle.kotlin.dsl.resolver
 
+import org.gradle.integtests.fixtures.RepoScriptBlockUtil.mavenCentralRepositoryDefinition
 import org.gradle.kotlin.dsl.fixtures.AbstractKotlinIntegrationTest
+import org.gradle.test.fixtures.dsl.GradleDsl
 
 import org.junit.Test
 
@@ -9,6 +11,37 @@ class SourceDistributionResolverIntegrationTest : AbstractKotlinIntegrationTest(
 
     @Test
     fun `can download source distribution`() {
+
+        withBuildScript(
+            """
+
+            val sourceDirs =
+                ${SourceDistributionResolver::class.qualifiedName}(project).run {
+                    sourceDirs()
+                }
+            require(sourceDirs.isNotEmpty()) {
+                "Expected source directories but got none"
+            }
+
+            """
+        )
+
+        build()
+    }
+
+    @Test
+    fun `can download source distribution when repositories are declared in settings`() {
+
+        withSettings(
+            """
+            dependencyResolutionManagement {
+                repositories {
+                    ${mavenCentralRepositoryDefinition(GradleDsl.KOTLIN)}
+                }
+                repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+            }
+            """.trimIndent()
+        )
 
         withBuildScript(
             """
