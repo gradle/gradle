1336780da8b70eb614d1fd04fd24ee4f87471644
Bo Zhang <bo@gradle.com>
2020-12-02 13:29:20 +0800

    Rebaseline two performance tests
    Due to tiny regressions

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaABIChangePerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaABIChangePerformanceTest.groovy
@@ -40,7 +40,7 @@ class JavaABIChangePerformanceTest extends AbstractCrossVersionPerformanceTest {
             def fileToChange = new File(it.projectDir, testProject.config.fileToChangeByScenario['assemble'])
             return isGroovyProject ? new ApplyAbiChangeToGroovySourceFileMutator(fileToChange) : new ApplyAbiChangeToJavaSourceFileMutator(fileToChange)
         }
-        runner.targetVersions = ["6.8-20201028230040+0000"]
+        runner.targetVersions = ["6.9-20201201230040+0000"]
         if (isGroovyProject) {
             runner.minimumBaseVersion = '5.0'
         }
