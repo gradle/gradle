ff8ee187277a64108e9a40f9d9c76c094df07891
Bo Zhang <bo@gradle.com>
2020-12-03 18:59:27 +0800

    Make ktlint happy

--- buildSrc/subprojects/basics/src/main/kotlin/gradlebuild/basics/BuildEnvironment.kt
+++ buildSrc/subprojects/basics/src/main/kotlin/gradlebuild/basics/BuildEnvironment.kt
@@ -20,7 +20,9 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.internal.os.OperatingSystem
 
+
 fun Project.testDistributionEnabled() = providers.systemProperty("enableTestDistribution").forUseAtConfigurationTime().orNull?.toBoolean() == true
+
 
 object BuildEnvironment {
 
