b18d7c9d8a5e4f8a88e4c1af18938e8e9ada6a5c
Cedric Champeau <cedric@gradle.com>
2020-12-09 19:32:22 +0100

    Fix exception being rethrown without context

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/RepositoriesDeclaredInSettingsIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/RepositoriesDeclaredInSettingsIntegrationTest.groovy
@@ -21,6 +21,7 @@ import org.gradle.integtests.fixtures.RequiredFeature
 import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
 import org.gradle.test.fixtures.plugin.PluginBuilder
 import org.gradle.test.fixtures.server.http.MavenHttpPluginRepository
+import org.gradle.util.GradleVersion
 import org.gradle.util.TestPrecondition
 import org.gradle.util.ToBeImplemented
 import spock.lang.IgnoreIf
@@ -736,7 +737,7 @@ class RepositoriesDeclaredInSettingsIntegrationTest extends AbstractModuleDepend
     }
 
     @Issue("https://github.com/gradle/gradle/issues/15336")
-    @ToBeFixedForConfigurationCache(because = "Decorated exception is not rethrown")
+    @ToBeFixedForConfigurationCache(because = "Decorated exception is not recognized by the configuration cache")
     def "reasonable error message if a dependency cannot be resolved because local repositories differ"() {
         buildFile << """
             repositories {
@@ -755,12 +756,10 @@ class RepositoriesDeclaredInSettingsIntegrationTest extends AbstractModuleDepend
         fails ':checkDeps'
 
         then:
-        // FIXME wolfs
-//        failure.assertThatCause(containsNormalizedString("""Could not resolve all dependencies for configuration ':conf'.
-//The project declares repositories, effectively ignoring the repositories you have declared in the settings.
-//You can figure out how project repositories are declared by configuring your build to fail on project repositories.
-//See https://docs.gradle.org/${GradleVersion.current().version}/userguide/declaring_repositories.html#sub:fail_build_on_project_repositories for details."""))
-        failure.assertThatCause(containsNormalizedString("""Could not resolve all files for configuration ':conf'."""))
+        failure.assertHasCause("""Could not resolve all dependencies for configuration ':conf'.
+The project declares repositories, effectively ignoring the repositories you have declared in the settings.
+You can figure out how project repositories are declared by configuring your build to fail on project repositories.
+See https://docs.gradle.org/${GradleVersion.current().version}/userguide/declaring_repositories.html#sub:fail_build_on_project_repositories for details.""")
     }
 
     void withSettingsPlugin() {
