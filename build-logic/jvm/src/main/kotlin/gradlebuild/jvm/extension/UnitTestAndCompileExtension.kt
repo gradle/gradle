45220a7f0bd12cdd63ebc4322fd8084974fda16b
Benjamin Muskalla <bmuskalla@gradle.com>
2020-12-10 23:36:22 +0100

    Fix unused imports

--- build-logic/jvm/src/main/kotlin/gradlebuild/jvm/extension/UnitTestAndCompileExtension.kt
+++ build-logic/jvm/src/main/kotlin/gradlebuild/jvm/extension/UnitTestAndCompileExtension.kt
@@ -16,9 +16,6 @@
 
 package gradlebuild.jvm.extension
 
-import org.gradle.api.JavaVersion
-import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.kotlin.dsl.*
