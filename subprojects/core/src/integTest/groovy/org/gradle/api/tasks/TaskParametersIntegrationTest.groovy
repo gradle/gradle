f0fabe9221265d5bf5f7714300135f98b4e6bb49
Lóránt Pintér <lorant@gradle.com>
2020-12-11 10:11:03 +0100

    Add test for validation warning being displayed for skipped task

--- subprojects/core/src/integTest/groovy/org/gradle/api/tasks/TaskParametersIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/tasks/TaskParametersIntegrationTest.groovy
@@ -398,6 +398,31 @@ task someTask {
         outputContains("Invalidating VFS because task ':invalid' failed validation")
     }
 
+    def "validation warnings are reported even when task is skipped"() {
+        buildFile << """
+            class InvalidTask extends SourceTask {
+                @Optional @Input File inputFile
+
+                @TaskAction void execute() {
+                    println "Executed"
+                }
+            }
+
+            task invalid(type: InvalidTask)
+        """
+
+        executer.expectDocumentedDeprecationWarning("Property 'inputFile' has @Input annotation used on property of type 'File'. " +
+            "This behaviour has been deprecated and is scheduled to be removed in Gradle 7.0. " +
+            "Execution optimizations are disabled due to the failed validation. " +
+            "See https://docs.gradle.org/current/userguide/more_about_tasks.html#sec:up_to_date_checks for more details.")
+
+        when:
+        run "invalid", "--info"
+        then:
+        executedAndNotSkipped(":invalid")
+        outputContains("Invalidating VFS because task ':invalid' failed validation")
+    }
+
     @Unroll
     def "task can use input property of type #type"() {
         file("buildSrc/src/main/java/SomeTask.java") << """
