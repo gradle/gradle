003f672b53c0029e804ecf6454e2051dc05d099b
Jendrik Johannes <jendrik@gradle.com>
2020-12-15 12:18:37 +0100

    Update 'kotlin-dsl' plugin version

--- build-logic/build-logic-base/settings-plugin/build.gradle.kts
+++ build-logic/build-logic-base/settings-plugin/build.gradle.kts
@@ -8,8 +8,6 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-kotlinDslPluginOptions.experimentalWarning.set(false)
-
 dependencies {
     implementation("com.gradle.enterprise:com.gradle.enterprise.gradle.plugin:3.5")
     implementation("com.gradle.enterprise.gradle-enterprise-conventions-plugin:com.gradle.enterprise.gradle-enterprise-conventions-plugin.gradle.plugin:0.7.1")
