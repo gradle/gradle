53d7c1bcf272ff9bf1fc8e2145b458cb9072d06b
Cedric Champeau <cedric@gradle.com>
2020-12-15 14:28:25 +0100

    Remove the `maven` plugin
    This commit removes the legacy "maven" plugin. Discussion is still
    ongoing whether we should also provide a backport plugin for folks
    who cannot migrate to the new `maven-publish` plugin.

--- subprojects/docs/src/snippets/base/distribution/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/base/distribution/kotlin/build.gradle.kts
@@ -1,15 +1,8 @@
 // tag::use-plugin[]
-// tag::publish-distribution[]
 plugins {
-    // end::publish-distribution[]
     distribution
-// end::use-plugin[]
-// tag::publish-distribution[]
-    maven
-// tag::use-plugin[]
 }
 // end::use-plugin[]
-// end::publish-distribution[]
 
 // tag::configure-distribution[]
 distributions {
@@ -21,17 +14,6 @@ distributions {
     }
 }
 // end::configure-distribution[]
-
-// tag::publish-distribution[]
-
-tasks.named<Upload>("uploadArchives") {
-    repositories.withGroovyBuilder {
-        "mavenDeployer" {
-            "repository"("url" to "file://some/repo")
-        }
-    }
-}
-// end::publish-distribution[]
 
 // tag::custom-distribution[]
 distributions {
