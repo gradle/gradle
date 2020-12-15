e7bdc734cc37bd7a80d5e8c2898e54745ff071e0
Stefan Wolf <wolf@gradle.com>
2020-12-15 15:59:45 +0100

    Rebaseline ExcludeRuleMergingPerformanceTest
    There is a small regression caused by removing the compile
    configuration. The last rebaseline used a version which didn't
    include the offending changes.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
@@ -31,7 +31,7 @@ class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceT
 
     def setup() {
         runner.minimumBaseVersion = '4.9'
-        runner.targetVersions = ["7.0-20201214230040+0000"]
+        runner.targetVersions = ["7.0-20201215144843+0000"]
     }
 
     def "merge exclude rules"() {
