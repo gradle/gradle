8cb7de1ef02d02d757ee148761001bae4d7685bf
Benjamin Muskalla <bmuskalla@gradle.com>
2020-12-17 14:54:15 +0100

    Fix whitespaces in javadoc integration test

--- subprojects/language-java/src/integTest/groovy/org/gradle/javadoc/JavadocIntegrationTest.groovy
+++ subprojects/language-java/src/integTest/groovy/org/gradle/javadoc/JavadocIntegrationTest.groovy
@@ -183,10 +183,10 @@ Joe!""")
         buildFile << """
             apply plugin: 'java'
             import org.gradle.external.javadoc.internal.JavadocOptionFileWriterContext;
-
+            
             class CustomJavadocOptionFileOption implements JavadocOptionFileOption<String> {
                 private String value = "foo"
-
+                
                 public String getValue() {
                     return value
                 }
@@ -205,7 +205,7 @@ Joe!""")
                     writerContext.newLine()
                 }
             }
-
+            
             javadoc {
                 options {
                     addOption(new CustomJavadocOptionFileOption())
@@ -222,7 +222,7 @@ Joe!""")
     def "can use various multi-value options"() {
         buildFile << """
             apply plugin: 'java'
-
+            
             javadoc {
                 options {
                     addMultilineStringsOption("addMultilineStringsOption").setValue([
@@ -253,9 +253,9 @@ Joe!""")
 
         file("build/tmp/javadoc/javadoc.options").assertContents(containsNormalizedString("""-addStringsOption 'a b c'"""))
 
-        file("build/tmp/javadoc/javadoc.options").assertContents(containsNormalizedString("""-addMultilineMultiValueOption
-'a'
--addMultilineMultiValueOption
+        file("build/tmp/javadoc/javadoc.options").assertContents(containsNormalizedString("""-addMultilineMultiValueOption 
+'a' 
+-addMultilineMultiValueOption 
 'b' 'c' """))
     }
 
@@ -275,7 +275,7 @@ Joe!""")
     def "can pass offline links"() {
         buildFile << """
             apply plugin: 'java'
-
+            
             javadoc {
                 options {
                     linksOffline 'https://docs.oracle.com/javase/8/docs/api/', 'gradle/javadocs/jdk'
