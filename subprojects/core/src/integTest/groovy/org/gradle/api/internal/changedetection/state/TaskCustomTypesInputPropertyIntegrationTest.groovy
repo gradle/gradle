e5e14a44ed8ef79aec299ae502c345bbd0dad9c7
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-01-10 18:21:58 +0200

    Fix configuration cache ignore iteration pattern for Spock2

--- subprojects/core/src/integTest/groovy/org/gradle/api/internal/changedetection/state/TaskCustomTypesInputPropertyIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/internal/changedetection/state/TaskCustomTypesInputPropertyIntegrationTest.groovy
@@ -352,7 +352,7 @@ task someTask {
     }
 
     @Unroll
-    @ToBeFixedForConfigurationCache(because = "ClassNotFoundException: ArrayList1_groovyProxy", iterationMatchers = '.*\\[2\\]$')
+    @ToBeFixedForConfigurationCache(because = "ClassNotFoundException: ArrayList1_groovyProxy", iterationMatchers = '.*\\[type: Map, #2\\]$')
     def "task can take as input a collection of custom types from various sources"() {
         def buildSrcType = file("buildSrc/src/main/java/CustomType.java")
         buildSrcType << customSerializableType()
