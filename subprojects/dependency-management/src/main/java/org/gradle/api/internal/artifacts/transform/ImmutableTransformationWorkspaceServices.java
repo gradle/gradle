c239b7c287f73cbdc0e2ee87a089f6bc795be643
Stefan Wolf <wolf@gradle.com>
2021-01-11 10:25:08 +0100

    Revert "Do not keep immutable transforms identity cache between builds"
    We want to keep the identity cache between builds.
    
    This reverts commit 4b65091a01422702228c110d4b3f3ffbe3f26e8c.

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/ImmutableTransformationWorkspaceServices.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/ImmutableTransformationWorkspaceServices.java
@@ -17,8 +17,8 @@
 package org.gradle.api.internal.artifacts.transform;
 
 import com.google.common.collect.ImmutableList;
-import org.gradle.cache.Cache;
 import org.gradle.cache.CacheBuilder;
+import org.gradle.cache.internal.CrossBuildInMemoryCache;
 import org.gradle.internal.Try;
 import org.gradle.internal.execution.UnitOfWork;
 import org.gradle.internal.execution.history.ExecutionHistoryStore;
@@ -31,14 +31,14 @@ import java.io.File;
 
 @NotThreadSafe
 public class ImmutableTransformationWorkspaceServices implements TransformationWorkspaceServices, Closeable {
-    private final Cache<UnitOfWork.Identity, Try<ImmutableList<File>>> identityCache;
+    private final CrossBuildInMemoryCache<UnitOfWork.Identity, Try<ImmutableList<File>>> identityCache;
     private final DefaultImmutableWorkspaceProvider workspaceProvider;
 
     public ImmutableTransformationWorkspaceServices(
         CacheBuilder cacheBuilder,
         FileAccessTimeJournal fileAccessTimeJournal,
         ExecutionHistoryStore executionHistoryStore,
-        Cache<UnitOfWork.Identity, Try<ImmutableList<File>>> identityCache
+        CrossBuildInMemoryCache<UnitOfWork.Identity, Try<ImmutableList<File>>> identityCache
     ) {
         this.workspaceProvider = DefaultImmutableWorkspaceProvider.withExternalHistory(cacheBuilder, fileAccessTimeJournal, executionHistoryStore);
         this.identityCache = identityCache;
@@ -50,7 +50,7 @@ public class ImmutableTransformationWorkspaceServices implements TransformationW
     }
 
     @Override
-    public Cache<UnitOfWork.Identity, Try<ImmutableList<File>>> getIdentityCache() {
+    public CrossBuildInMemoryCache<UnitOfWork.Identity, Try<ImmutableList<File>>> getIdentityCache() {
         return identityCache;
     }
 
