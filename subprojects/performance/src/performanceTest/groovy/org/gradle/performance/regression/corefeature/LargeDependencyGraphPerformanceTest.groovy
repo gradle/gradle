db7eacd34d5240688a4853870508c0c5150f163d
Bo Zhang <bo@gradle.com>
2021-01-12 10:21:12 +0800

    Set min base version of some performance tests
    To 5.x so that they won't fail historical performance tests.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/LargeDependencyGraphPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/LargeDependencyGraphPerformanceTest.groovy
@@ -35,7 +35,7 @@ class LargeDependencyGraphPerformanceTest extends AbstractCrossVersionPerformanc
     public static final String MAX_MEMORY = "-Xmx800m"
 
     def setup() {
-        runner.minimumBaseVersion = '4.8'
+        runner.minimumBaseVersion = '5.6.4'
         runner.targetVersions = ["6.9-20201210230047+0000"]
     }
 
