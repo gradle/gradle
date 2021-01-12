f993066c58d10fb2e2dfaab42af4d855b164b1c1
Donat Csikos <donat@gradle.com>
2021-01-12 17:31:19 +0100

    Minimize Gradle invocations in cross-version test

--- subprojects/integ-test/src/crossVersionTest/groovy/org/gradle/integtests/ArchiveTaskPluginCompatibilityCrossVersionTest.groovy
+++ subprojects/integ-test/src/crossVersionTest/groovy/org/gradle/integtests/ArchiveTaskPluginCompatibilityCrossVersionTest.groovy
@@ -24,7 +24,7 @@ import org.junit.Assume
 @TargetVersions(["6.8"])
 class ArchiveTaskPluginCompatibilityCrossVersionTest extends CrossVersionIntegrationSpec {
 
-    def "API methods removed from AbstractArchiveTask are still available for plugins"(String type) {
+    def "API methods removed from AbstractArchiveTask are still available for plugins"() {
         setup:
         Assume.assumeTrue(previous.version.baseVersion <= GradleVersion.version("6.8") &&
                           current.version.baseVersion >= GradleVersion.version("7.0"))
@@ -68,47 +68,67 @@ class ArchiveTaskPluginCompatibilityCrossVersionTest extends CrossVersionIntegra
         file('plugin/src/main/java/org/example/MyPlugin.java') << """
             package org.example;
 
-            import java.io.File;
             import org.gradle.api.*;
+            import org.gradle.api.tasks.*;
             import org.gradle.api.tasks.bundling.*;
-            import org.gradle.plugins.ear.*;
+            import org.gradle.plugins.ear.Ear;
 
             public class MyPlugin implements Plugin<Project> {
                 public void apply(Project project) {
-                    project.getTasks().register("customArchive", ${type}.class, new Action<$type>(){
+                    project.getTasks().register("customZip", Zip.class, new ConfigurationAction());
+                    project.getTasks().register("customTar", Tar.class, new ConfigurationAction());
+                    project.getTasks().register("customJar", Jar.class, new ConfigurationAction());
+                    project.getTasks().register("customWar", War.class, new ConfigurationAction());
+                    project.getTasks().register("customEar", Ear.class, new ConfigurationAction());
+                    project.getTasks().register("customJvmJar", org.gradle.jvm.tasks.Jar.class, new ConfigurationAction());
+
+                    TaskProvider<Task> customArchives = project.getTasks().register("customArchives");
+                    customArchives.configure(new Action<Task>() {
                         @Override
-                        public void execute($type task) {
-                            configureAndPrintValues(task);
+                        public void execute(Task task) {
+                            task.dependsOn("customZip", "customTar", "customJar", "customWar", "customEar", "customJvmJar");
                         }
                     });
                 }
+            }
+        """
+        file('plugin/src/main/java/org/example/ConfigurationAction.java') << """
+            package org.example;
 
-                static void configureAndPrintValues(AbstractArchiveTask task) {
+            import java.io.File;
+            import org.gradle.api.Action;
+            import org.gradle.api.tasks.bundling.AbstractArchiveTask;
+
+            public class ConfigurationAction implements Action<AbstractArchiveTask> {
+
+                @Override
+                public void execute(AbstractArchiveTask task) {
                     task.setArchiveName("archiveName");
-                    System.out.println("archiveName=" + task.getArchiveName());
+                    System.out.println(task.getName() + " archiveName=" + task.getArchiveName());
 
                     task.setDestinationDir(new File("destinationDir"));
-                    System.out.println("destinationDir=" + task.getDestinationDir());
+                    System.out.println(task.getName() + " destinationDir=" + task.getDestinationDir());
 
                     task.setBaseName("baseName");
-                    System.out.println("baseName=" + task.getBaseName());
+                    System.out.println(task.getName() + " baseName=" + task.getBaseName());
 
                     task.setAppendix("appendix");
-                    System.out.println("appendix=" + task.getAppendix());
+                    System.out.println(task.getName() + " appendix=" + task.getAppendix());
 
                     task.setVersion("version");
-                    System.out.println("version=" + task.getVersion());
+                    System.out.println(task.getName() + " version=" + task.getVersion());
 
                     task.setExtension("extension");
-                    System.out.println("extension=" + task.getExtension());
+                    System.out.println(task.getName() + " extension=" + task.getExtension());
 
                     task.setClassifier("classifier");
-                    System.out.println("classifier=" + task.getClassifier());
+                    System.out.println(task.getName() + " classifier=" + task.getClassifier());
 
-                    System.out.println("archivePath=" + task.getArchivePath());
+                    System.out.println(task.getName() + " archivePath=" + task.getArchivePath());
                 }
             }
         """
+
         file('client/build.gradle') << """
             plugins {
                 id 'java-library'
@@ -132,19 +152,18 @@ class ArchiveTaskPluginCompatibilityCrossVersionTest extends CrossVersionIntegra
 
         when:
         version previous withTasks 'publish' inDirectory(file("plugin")) run()
-        def result = version current requireDaemon() requireIsolatedDaemons() withTasks 'customArchive' inDirectory(file('client')) run()
+        def result = version current requireDaemon() requireIsolatedDaemons() withTasks 'customArchives' inDirectory(file('client')) run()
 
         then:
-        result.output.contains "archiveName=archiveName"
-        result.output.contains "destinationDir=${file('client/destinationDir').absolutePath}"
-        result.output.contains "baseName=baseName"
-        result.output.contains "appendix=appendix"
-        result.output.contains "version=version"
-        result.output.contains "extension=extension"
-        result.output.contains "classifier=classifier"
-        result.output.contains "archivePath=${file('client/destinationDir/archiveName').absolutePath}"
-
-        where:
-        type << ["Zip", "Tar", "Jar", "War", "Ear", "org.gradle.jvm.tasks.Jar"]
+        ["customZip", "customTar", "customJar", "customWar", "customEar", "customJvmJar"].each {taskName ->
+            assert result.output.contains("$taskName archiveName=archiveName")
+            assert result.output.contains("$taskName destinationDir=${file('client/destinationDir').absolutePath}")
+            assert result.output.contains("$taskName baseName=baseName")
+            assert result.output.contains("$taskName appendix=appendix")
+            assert result.output.contains("$taskName version=version")
+            assert result.output.contains("$taskName extension=extension")
+            assert result.output.contains("$taskName classifier=classifier")
+            assert result.output.contains("$taskName archivePath=${file('client/destinationDir/archiveName').absolutePath}")
+        }
     }
 }
