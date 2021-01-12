5304c6c33791d471b394baa10481585081d46379
Benjamin Muskalla <bmuskalla@gradle.com>
2021-01-12 10:21:15 +0100

    Avoid double escaping

--- .teamcity/common/Os.kt
+++ .teamcity/common/Os.kt
@@ -66,7 +66,7 @@ enum class Os(
     fun asName() = name.toLowerCase().capitalize()
 
     fun javaInstallationLocations(): String {
-        return "-Porg.gradle.java.installations.path="  enumValues<JvmVersion>().map { version -> {
+        return "-Porg.gradle.java.installations.paths=" + enumValues<JvmVersion>().map { version -> {
             val vendor = if (version.major >= 11) JvmVendor.openjdk else JvmVendor.oracle
             asPlaceholder(version, vendor)
         }() }.joinToString(",")
