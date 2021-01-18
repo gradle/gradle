b1e1aef1c9b13d48a7da15ed6c1469cfab32f052
Jendrik Johannes <jendrik@gradle.com>
2021-01-18 17:29:43 +0100

    Simplify cross-version Tooling API work item test
    There was a lot of test fixture code inside the test that was not
    necessary for what the test is checking. It made it very hard to
    read the test.

--- subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r51/WorkItemProgressEventCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r51/WorkItemProgressEventCrossVersionSpec.groovy
@@ -18,7 +18,6 @@ package org.gradle.integtests.tooling.r51
 
 import org.gradle.integtests.tooling.fixture.ProgressEvents
 import org.gradle.integtests.tooling.fixture.TargetGradleVersion
-import org.gradle.integtests.tooling.fixture.TextUtil
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
 import org.gradle.integtests.tooling.fixture.ToolingApiVersion
 import org.gradle.tooling.BuildException
@@ -34,9 +33,7 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
         prepareTaskTypeUsingWorker()
         withRunnableClassInBuildSrc()
         buildFile << """
-            task runInWorker(type: WorkerTask) {
-                displayName = "Test Work"
-            }
+            task runInWorker(type: WorkerTask)
         """
     }
 
@@ -47,7 +44,7 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
         then:
         def taskOperation = events.operation("Task :runInWorker")
         taskOperation.task
-        with(taskOperation.descendant("Test Work")) {
+        with(taskOperation.descendant("TestWork")) {
             successful
             workItem
             descriptor.className == "org.gradle.test.TestRunnable"
@@ -62,7 +59,7 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
         then:
         def taskOperation = events.operation("Task :runInWorker")
         taskOperation.task
-        with(taskOperation.descendant("Test Work")) {
+        with(taskOperation.descendant("TestWork")) {
             successful
             buildOperation
         }
@@ -75,7 +72,7 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
         then:
         def taskOperation = events.operation("Task :runInWorker")
         taskOperation.task
-        taskOperation.descendants { it.descriptor.displayName == "Test Work" }.empty
+        taskOperation.descendants { it.descriptor.displayName == "TestWork" }.empty
     }
 
     def "does not report work item progress events when TASK operations are not requested"() {
@@ -88,13 +85,7 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
 
     def "includes failure in progress event"() {
         given:
-        buildFile << """
-            ${getRunnableThatFails(IllegalStateException, "something went horribly wrong")}
-            runInWorker {
-                displayName = null
-                runnableClass = RunnableThatFails
-            }
-        """
+        withRunnableClassThatFails()
 
         when:
         def events = ProgressEvents.create()
@@ -104,11 +95,11 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
         thrown(BuildException)
         def taskOperation = events.operation("Task :runInWorker")
         taskOperation.task
-        with(taskOperation.child("RunnableThatFails")) {
+        with(taskOperation.child("TestWork")) {
             !successful
             workItem
             descriptor instanceof WorkItemOperationDescriptor
-            descriptor.className == "RunnableThatFails"
+            descriptor.className == "org.gradle.test.TestRunnable"
             failures.size() == 1
             with (failures[0]) {
                 message == "something went horribly wrong"
@@ -140,35 +131,10 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
     }
 
     String getTaskTypeUsingWorker() {
-        withParameterClassInBuildSrc()
-        withFileHelperClassInBuildSrc()
-
-        def outputFileDir = temporaryFolder.file("build/workers")
-        def outputFileDirPath = TextUtil.normaliseFileSeparators(outputFileDir.absolutePath)
-
         return """
             import javax.inject.Inject
-            import org.gradle.other.Foo
 
             class WorkerTask extends DefaultTask {
-                @Internal
-                def list = [1, 2, 3]
-                @Internal
-                def outputFileDirPath = "${outputFileDirPath}/\${name}"
-                @Internal
-                def additionalForkOptions = {}
-                @Internal
-                def runnableClass = TestRunnable.class
-                @Internal
-                def additionalClasspath = project.layout.files()
-                @Internal
-                def foo = new Foo()
-                @Internal
-                def displayName = null
-                @Internal
-                def isolationMode = IsolationMode.AUTO
-                @Internal
-                def forkMode = null
 
                 @Inject
                 WorkerExecutor getWorkerExecutor() {
@@ -177,47 +143,8 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
 
                 @TaskAction
                 void executeTask() {
-                    workerExecutor.submit(runnableClass) {
-                        isolationMode = this.isolationMode
-                        displayName = this.displayName
-                        if (isolationMode == IsolationMode.PROCESS) {
-                            forkOptions.maxHeapSize = "64m"
-                        }
-                        forkOptions(additionalForkOptions)
-                        classpath(additionalClasspath)
-                        params = [ list.collect { it as String }, new File(outputFileDirPath), foo ]
-                        if (this.forkMode != null) {
-                            forkMode = this.forkMode
-                        }
-                    }
-                }
-            }
-        """
-    }
-
-    String getRunnableThatCreatesFiles() {
-        return """
-            import java.io.File;
-            import java.util.List;
-            import org.gradle.other.Foo;
-            import org.gradle.test.FileHelper;
-            import java.util.UUID;
-            import javax.inject.Inject;
-            public class TestRunnable implements Runnable {
-                private final List<String> files;
-                protected final File outputDir;
-                private final Foo foo;
-                private static final String id = UUID.randomUUID().toString();
-                @Inject
-                public TestRunnable(List<String> files, File outputDir, Foo foo) {
-                    this.files = files;
-                    this.outputDir = outputDir;
-                    this.foo = foo;
-                }
-                public void run() {
-                    for (String name : files) {
-                        File outputFile = new File(outputDir, name);
-                        FileHelper.write(id, outputFile);
+                    workerExecutor.submit(TestRunnable) { config ->
+                        config.displayName = 'TestWork'
                     }
                 }
             }
@@ -227,78 +154,23 @@ class WorkItemProgressEventCrossVersionSpec extends ToolingApiSpecification {
     void withRunnableClassInBuildSrc() {
         file("buildSrc/src/main/java/org/gradle/test/TestRunnable.java") << """
             package org.gradle.test;
-            $runnableThatCreatesFiles
+            public class TestRunnable implements Runnable {
+                public void run() {
+                }
+            }
         """
-
-        addImportToBuildScript("org.gradle.test.TestRunnable")
-    }
-
-    void addImportToBuildScript(String className) {
         buildFile.text = """
-            import ${className}
+            import org.gradle.test.TestRunnable
             ${buildFile.text}
         """
     }
 
-    void withParameterClassInBuildSrc() {
-        file("buildSrc/src/main/java/org/gradle/other/Foo.java") << """
-            package org.gradle.other;
-            import java.io.Serializable;
-            public class Foo implements Serializable { }
-        """
-    }
-
-    void withFileHelperClassInBuildSrc() {
-        file("buildSrc/src/main/java/org/gradle/test/FileHelper.java") << """
-            $fileHelperClass
-        """
-    }
-
-    String getFileHelperClass() {
-        return """
+    void withRunnableClassThatFails() {
+        file("buildSrc/src/main/java/org/gradle/test/TestRunnable.java").text = """
             package org.gradle.test;
-            
-            import java.io.File;
-            import java.io.PrintWriter;
-            import java.io.BufferedWriter;
-            import java.io.FileWriter;
-            
-            public class FileHelper {
-                static void write(String id, File outputFile) {
-                    PrintWriter out = null;
-                    try {
-                        outputFile.getParentFile().mkdirs();
-                        outputFile.createNewFile();
-                        out = new PrintWriter(new BufferedWriter(new FileWriter(outputFile)));
-                        out.print(id);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    } finally {
-                        if (out != null) {
-                            out.close();
-                        }
-                    }
-                }
-            }
-        """
-    }
-
-    String getRunnableThatFails(Class<? extends RuntimeException> exceptionClass = RuntimeException.class, String message = "Failure from runnable") {
-        return """
-            public class RunnableThatFails implements Runnable {
-                private final File outputDir;
-                
-                @javax.inject.Inject
-                public RunnableThatFails(List<String> files, File outputDir, Foo foo) { 
-                    this.outputDir = outputDir;
-                }
+            public class TestRunnable implements Runnable {
                 public void run() {
-                    try {
-                        throw new ${exceptionClass.name}("$message");
-                    } finally {
-                        outputDir.mkdirs();
-                        new File(outputDir, "finished").createNewFile();
-                    }                    
+                    throw new IllegalStateException("something went horribly wrong");
                 }
             }
         """
