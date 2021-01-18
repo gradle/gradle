401c31af0193c7deaf5e6e5853d2a20ac632df5e
Louis Jacomet <louis@gradle.com>
2021-01-18 12:01:57 +0100

    Revert "Revert "Merge branch 'release'""
    This restores and fixes the last failed merged from release.
    
    This reverts commit 1aa153d2e5c1741818a7d87dcb00a444b50c33dc.

--- subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishMultiProjectIntegTest.groovy
+++ subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishMultiProjectIntegTest.groovy
@@ -36,10 +36,11 @@ class MavenPublishMultiProjectIntegTest extends AbstractMavenPublishIntegTest {
         projectsCorrectlyPublished()
     }
 
-    def "project dependencies reference publication identity of dependent project"() {
+    @Unroll
+    def "project dependencies reference publication identity of dependent project (version mapping: #mapping)"() {
         def project3 = javaLibrary(mavenRepo.module("changed.group", "changed-artifact-id", "changed"))
 
-        createBuildScripts("""
+        def extra = """
 project(":project3") {
     publishing {
         publications.maven {
@@ -49,7 +50,23 @@ project(":project3") {
         }
     }
 }
-""")
+"""
+        if (mapping) {
+            extra = """
+project(":project1") {
+    publishing {
+        publications.maven {
+            versionMapping {
+                usage(Usage.JAVA_API) {
+                    fromResolutionResult()
+                }
+            }
+        }
+    }
+}
+""" + extra
+        }
+        createBuildScripts(extra)
 
         when:
         run "publish"
@@ -68,6 +85,9 @@ project(":project3") {
         resolveArtifacts(project1) {
             expectFiles 'changed-artifact-id-changed.jar', 'project1-1.0.jar', 'project2-2.0.jar'
         }
+
+        where:
+        mapping << [false, true]
     }
 
     def "reports failure when project dependency references a project with multiple conflicting publications"() {
