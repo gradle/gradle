a2e013361a55b1d6fc08ba376d5f7544400778f3
Bo Zhang <bo@gradle.com>
2021-01-18 09:41:45 +0800

    Change CompileAll build configuration UUID
    So we can force all stale branches to use compileAllBuild configuration
    on `master` and perform security checks.

--- .teamcity/Gradle_Check/configurations/CompileAll.kt
+++ .teamcity/Gradle_Check/configurations/CompileAll.kt
@@ -31,6 +31,6 @@ class CompileAll(model: CIBuildModel, stage: Stage) : BaseGradleBuildType(model,
     """.trimIndent()
 }) {
     companion object {
-        fun buildTypeId(model: CIBuildModel) = "${model.projectPrefix}CompileAll"
+        fun buildTypeId(model: CIBuildModel) = "${model.projectPrefix}CompileAllBuild"
     }
 }
