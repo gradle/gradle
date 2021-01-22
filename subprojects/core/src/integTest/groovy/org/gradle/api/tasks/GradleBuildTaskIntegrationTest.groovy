eee7487396c2e6c27747b8ee85c9c42ff478cb92
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2021-01-22 16:16:34 -0300

    Extract `NestedRootBuildRunner` from `GradleBuild` task
    So it can be reused in `GeneratePrecompiledScriptPluginAccessors`.
    
    Incidentally, the extraction fixes a bunch of configuration cache integration
    tests.

--- subprojects/core/src/integTest/groovy/org/gradle/api/tasks/GradleBuildTaskIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/tasks/GradleBuildTaskIntegrationTest.groovy
@@ -19,7 +19,7 @@ package org.gradle.api.tasks
 import org.gradle.initialization.RunNestedBuildBuildOperationType
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.BuildOperationsFixture
-import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
+import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
 import org.gradle.test.fixtures.server.http.BlockingHttpServer
 import org.junit.Rule
 
@@ -27,7 +27,6 @@ class GradleBuildTaskIntegrationTest extends AbstractIntegrationSpec {
 
     def buildOperations = new BuildOperationsFixture(executer, testDirectoryProvider)
 
-    @ToBeFixedForConfigurationCache(because = "GradleBuild task")
     def "handles properties which are not String when calling GradleBuild"() {
         given:
         settingsFile << "rootProject.name = 'parent'"
@@ -46,7 +45,6 @@ class GradleBuildTaskIntegrationTest extends AbstractIntegrationSpec {
         noExceptionThrown()
     }
 
-    @ToBeFixedForConfigurationCache(because = "GradleBuild task")
     def "can set build path"() {
         given:
         settingsFile << "rootProject.name = 'parent'"
@@ -65,7 +63,6 @@ class GradleBuildTaskIntegrationTest extends AbstractIntegrationSpec {
         executed(":bp:t")
     }
 
-    @ToBeFixedForConfigurationCache(because = "GradleBuild task")
     def "fails when build path is not unique"() {
         given:
         settingsFile << "rootProject.name = 'parent'"
@@ -89,7 +86,34 @@ class GradleBuildTaskIntegrationTest extends AbstractIntegrationSpec {
         failure.assertHasCause("Included build $testDirectory has build path :bp which is the same as included build $testDirectory")
     }
 
-    @ToBeFixedForConfigurationCache(because = "GradleBuild task")
+    @Unroll
+    def "shows deprecation warning when accessing #displayName when configuring GradleBuild task"() {
+        given:
+        settingsFile << "rootProject.name = 'parent'"
+        buildFile << """
+            task buildInBuild(type:GradleBuild) {
+                buildFile = 'other.gradle'
+            }
+
+            ${codeUnderTest}
+        """
+        file('other.gradle') << 'assert true'
+
+        when:
+        executer.expectDeprecationWarning()
+        run 'buildInBuild'
+
+        then:
+        outputContains("${displayName} method has been deprecated. This is scheduled to be removed in Gradle 7.0.")
+
+        where:
+        displayName                                | codeUnderTest
+        "StartParameter.setSearchUpwards(boolean)" | "buildInBuild.startParameter.searchUpwards = true"
+        "StartParameter.isSearchUpwards()"         | "buildInBuild.startParameter.searchUpwards"
+        "StartParameter.useEmptySettings()"        | "buildInBuild.startParameter.useEmptySettings()"
+        "StartParameter.isUseEmptySettings()"      | "buildInBuild.startParameter.useEmptySettings"
+    }
+
     def "nested build can use Gradle home directory that is different to outer build"() {
         given:
         def dir = file("other-home")
@@ -114,7 +138,6 @@ println "build script code source: " + getClass().protectionDomain.codeSource.lo
         output.contains("build script code source: ${dir.toURI()}")
     }
 
-    @ToBeFixedForConfigurationCache(because = "GradleBuild task")
     def "nested build can have buildSrc"() {
         given:
         buildFile << """
@@ -158,7 +181,6 @@ println "build script code source: " + getClass().protectionDomain.codeSource.lo
         result.assertTaskExecuted(":buildSrc:otherBuild")
     }
 
-    @ToBeFixedForConfigurationCache(because = "GradleBuild task")
     def "nested build can nest more builds"() {
         given:
         buildFile << """
@@ -188,7 +210,6 @@ println "build script code source: " + getClass().protectionDomain.codeSource.lo
         outputContains(":other:other2:build")
     }
 
-    @ToBeFixedForConfigurationCache(because = "GradleBuild task")
     def "nested build can contain project dependencies"() {
         given:
         buildFile << """
@@ -220,7 +241,6 @@ println "build script code source: " + getClass().protectionDomain.codeSource.lo
     @Rule
     BlockingHttpServer barrier = new BlockingHttpServer()
 
-    @ToBeFixedForConfigurationCache(because = "GradleBuild task")
     def "can run multiple GradleBuild tasks concurrently"() {
         barrier.start()
 
