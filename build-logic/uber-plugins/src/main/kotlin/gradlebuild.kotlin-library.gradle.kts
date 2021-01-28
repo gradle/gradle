b3179e979c7fb9d6490171ef44e129fb05d3e551
Benjamin Muskalla <bmuskalla@gradle.com>
2021-01-28 15:08:14 +0100

    Configure kotlin to use the same toolchain

--- build-logic/uber-plugins/src/main/kotlin/gradlebuild.kotlin-library.gradle.kts
+++ build-logic/uber-plugins/src/main/kotlin/gradlebuild.kotlin-library.gradle.kts
@@ -35,7 +35,8 @@ val transitiveSourcesElements by configurations.getting {
 
 tasks {
     withType<KotlinCompile>().configureEach {
-        configureKotlinCompilerForGradleBuild()
+        val launcher = javaToolchains.launcherFor(java.toolchain)
+        configureKotlinCompilerForGradleBuild(launcher)
         kotlinOptions.allWarningsAsErrors = true
         if (name == "compileTestKotlin") {
             // Make sure the classes dir is used for test compilation (required by tests accessing internal methods) - https://github.com/gradle/gradle/issues/11501
@@ -66,7 +67,7 @@ tasks {
     }
 }
 
-fun KotlinCompile.configureKotlinCompilerForGradleBuild() {
+fun KotlinCompile.configureKotlinCompilerForGradleBuild(launcher: Provider<JavaLauncher>) {
     kotlinOptions {
         incremental = true
         apiVersion = "1.4"
@@ -78,5 +79,6 @@ fun KotlinCompile.configureKotlinCompilerForGradleBuild() {
             "-Xskip-metadata-version-check"
         )
         jvmTarget = "1.8"
+        jdkHome = launcher.get().metadata.installationPath.asFile.absolutePath
     }
 }
