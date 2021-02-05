e6513c1393b6e6e39e776e5b5d1a79ef50615a13
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2021-02-05 14:20:10 -0300

    Temporarily capture `:build-logic-commons:code-quality` `compileClasspath`
    So we can debug what's going on with `generateExternalPluginSpecBuilders`.

--- build-logic-commons/code-quality/build.gradle.kts
+++ build-logic-commons/code-quality/build.gradle.kts
@@ -13,3 +13,19 @@ dependencies {
     implementation("org.gradle.kotlin.kotlin-dsl:org.gradle.kotlin.kotlin-dsl.gradle.plugin:1.4.4")
     implementation("org.gradle.kotlin:gradle-kotlin-dsl-conventions:0.6.0")
 }
+
+tasks {
+
+    // Temporarily capture the compileClasspath so we can investigate
+    // what's going on with generateExternalPluginSpecBuilders.
+    val zipCompileClasspath by registering(Zip::class) {
+        archiveBaseName.set("code-quality-compile-classpath")
+        from(sourceSets.main.map { it.compileClasspath })
+        duplicatesStrategy = DuplicatesStrategy.WARN
+    }
+
+    generateExternalPluginSpecBuilders {
+        require(this is Task)
+        dependsOn(zipCompileClasspath)
+    }
+}
