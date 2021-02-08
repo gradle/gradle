1e1e880604b01100588a5c42be3d13443309e786
Cedric Champeau <cedric@gradle.com>
2021-02-08 11:16:39 +0100

    Replace README with `description`

--- build-logic-settings/README.md
+++
@@ -1,3 +0,0 @@
-# What is this module about?
-
-This module contains settings plugins applied to our build logic.
--- build-logic-settings/build.gradle.kts
+++ build-logic-settings/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,4 @@
  * limitations under the License.
  */
 
-plugins {
-   `kotlin-dsl`
-}
-
-description = "A plugin which applies other plugins to all projects, without relying on the allprojects block"
+description = "This module contains settings plugins applied to our build logic."
