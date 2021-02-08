e2b779787e5fdb67d4f10d0e2ede37d219a2271c
Donat Csikos <donat@gradle.com>
2021-02-08 16:43:59 +0100

    Make build fail when Project.afterEvaluate is called after project is evaluated

--- subprojects/core/src/integTest/groovy/org/gradle/api/ProjectConfigurationIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/ProjectConfigurationIntegrationTest.groovy
@@ -54,14 +54,15 @@ class ProjectConfigurationIntegrationTest extends AbstractIntegrationSpec {
                 }
             }
         '''
-        settingsFile << "include 'a', 'b'"
+        settingsFile << """
+            rootProject.name = 'root'
+            include 'a', 'b'
+        """
 
         expect:
-        executer.expectDocumentedDeprecationWarning("Using method Project.afterEvaluate(Closure) when the project is already evaluated has been deprecated. " +
-            "This will fail with an error in Gradle 7.0. " +
-            "The configuration given is ignored because the project has already been evaluated. To apply this configuration, remove afterEvaluate. " +
-            "Consult the upgrading guide for further information: https://docs.gradle.org/current/userguide/upgrading_version_5.html#calling_project_afterevaluate_on_an_evaluated_project_has_been_deprecated")
-        succeeds()
+        def result = fails()
+        result.assertHasDescription("A problem occurred evaluating root project 'root'.")
+        failure.assertHasCause("Cannot run Project.afterEvaluate(Closure) when the project is already evaluated.")
     }
 
     def "shows deprecation warning when calling Project#afterEvaluate(Action) after the project was evaluated"() {
@@ -89,13 +90,14 @@ class ProjectConfigurationIntegrationTest extends AbstractIntegrationSpec {
                 }
             }
         '''
-        settingsFile << "include 'a', 'b'"
+        settingsFile << """
+            rootProject.name = 'root'
+            include 'a', 'b'
+        """
 
         expect:
-        executer.expectDocumentedDeprecationWarning("Using method Project.afterEvaluate(Action) when the project is already evaluated has been deprecated. " +
-            "This will fail with an error in Gradle 7.0. " +
-            "The configuration given is ignored because the project has already been evaluated. To apply this configuration, remove afterEvaluate. " +
-            "Consult the upgrading guide for further information: https://docs.gradle.org/current/userguide/upgrading_version_5.html#calling_project_afterevaluate_on_an_evaluated_project_has_been_deprecated")
-        succeeds()
+        def result = fails()
+        result.assertHasDescription("A problem occurred evaluating root project 'root'.")
+        failure.assertHasCause("Cannot run Project.afterEvaluate(Action) when the project is already evaluated.")
     }
 }
