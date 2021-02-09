48bf8f2b49c57fa80809fd75b08474fbe059c042
Cedric Champeau <cedric@gradle.com>
2021-02-09 13:31:20 +0100

    Remove temporary ignoring of plugin validation
    3 different projects were ignoring `ValidatePlugins` in their own
    way. In general, this is just a _temporary_ fix which should be
    removed once a compatible wrapper is used. The problem is that
    they all configured the validation using `withType()` which caused
    _all_ tasks of that type to be configured, including the external
    plugin validation.
    
    In practice, this caused extra work to be executed during the
    smoke test, without any good reason. This should make the build
    faster.
    
    Should we need to ignore validation again, it would be better
    to introduce a convention to do this, so that we can track
    and remove.

--- subprojects/configuration-cache/build.gradle.kts
+++ subprojects/configuration-cache/build.gradle.kts
@@ -22,12 +22,6 @@ tasks.configCacheIntegTest {
     enabled = false
 }
 
-// This is a workaround for the validate plugins task trying to inspect classes which have changed but are NOT tasks.
-// For the current project, we simply disable it since there are no tasks in there.
-tasks.withType<ValidatePlugins>().configureEach {
-    enabled = false
-}
-
 dependencies {
     implementation(project(":base-services"))
     implementation(project(":base-services-groovy"))
