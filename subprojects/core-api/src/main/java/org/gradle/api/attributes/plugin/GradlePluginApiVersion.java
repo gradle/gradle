d1d7aca0045c18b784783ea35b8c16eefaa05ad4
Jendrik Johannes <jendrik@gradle.com>
2021-02-16 14:59:38 +0100

    Add @Incubating and @since

--- subprojects/core-api/src/main/java/org/gradle/api/attributes/plugin/GradlePluginApiVersion.java
+++ subprojects/core-api/src/main/java/org/gradle/api/attributes/plugin/GradlePluginApiVersion.java
@@ -16,12 +16,16 @@
 
 package org.gradle.api.attributes.plugin;
 
+import org.gradle.api.Incubating;
 import org.gradle.api.Named;
 import org.gradle.api.attributes.Attribute;
 
 /**
  * Represents a version of the minimal version of the Gradle API required by (variant of) a Gradle plugin.
+ *
+ * @since 7.0
  */
+@Incubating
 public interface GradlePluginApiVersion extends Named {
 
     /**
