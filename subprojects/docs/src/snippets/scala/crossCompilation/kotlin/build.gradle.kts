e5b283cf36099b6281321ab5c27027874efd72f0
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-02-22 10:30:55 +0200

    Update Scala snippet and documentation to use toolchains for cross-compilation
    This also re-enables an otherwise ignored test that relied on hardcoded filesystem location to find JDK6.

--- subprojects/docs/src/snippets/scala/crossCompilation/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/scala/crossCompilation/kotlin/build.gradle.kts
@@ -15,31 +15,8 @@ dependencies {
 
 // tag::scala-cross-compilation[]
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_6
-}
-
-require(hasProperty("java6Home")) { "Set the property 'java6Home' in your your gradle.properties pointing to a Java 6 installation" }
-val java6Home: String by project
-val javaExecutablesPath = File(java6Home, "bin")
-fun javaExecutable(execName: String): String {
-    val executable = File(javaExecutablesPath, execName)
-    require(executable.exists()) { "There is no ${execName} executable in ${javaExecutablesPath}" }
-    return executable.toString()
-}
-
-tasks.withType<ScalaCompile>().configureEach {
-    options.apply {
-        isFork = true
-        forkOptions.javaHome = file(java6Home)
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(6))
     }
-}
-tasks.withType<Test>().configureEach {
-    executable = javaExecutable("java")
-}
-tasks.withType<JavaExec>().configureEach {
-    executable = javaExecutable("java")
-}
-tasks.withType<Javadoc>().configureEach {
-    executable = javaExecutable("javadoc")
 }
 // end::scala-cross-compilation[]
