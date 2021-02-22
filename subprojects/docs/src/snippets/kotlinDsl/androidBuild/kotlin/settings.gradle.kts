2951876bf0506850fbe3e0ca0f93ab677d0011f8
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-02-22 12:05:33 +0200

    Fix Kotlin DSL snippet with Android multi-project
    Upgrade Kotlin and Android plugin versions so that they are compatible with the latest Gradle.

--- subprojects/docs/src/snippets/kotlinDsl/androidBuild/kotlin/settings.gradle.kts
+++ subprojects/docs/src/snippets/kotlinDsl/androidBuild/kotlin/settings.gradle.kts
@@ -1,3 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+}
 // tag::android[]
 include("lib", "app")
 // end::android[]
