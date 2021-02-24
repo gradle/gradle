ad9168f332475e34d2b9f3b48dba803891367f1b
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-02-24 09:48:56 +0200

    Set common Groovy JPMS JVM args for tests
    Illegal reflective accesses turn from warnings to errors on JDK16.
    The --add-opens that are set in production code are also needed in
    unit tests and embedded integration tests when executed on JDK16.

--- build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
+++ build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
@@ -180,11 +180,8 @@ fun Test.configureJvmForTest() {
         }.getOrNull()
     }
     javaLauncher.set(launcher)
-    if (jvmVersionForTest().canCompileOrRun(9)) {
-        // allow embedded executer to modify environment variables
-        jvmArgs("--add-opens", "java.base/java.util=ALL-UNNAMED")
-        // allow embedded executer to inject legacy types into the system classloader
-        jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
+    if (jvmVersionForTest().canCompileOrRun(9) && (isUnitTest() || usesEmbeddedExecuter())) {
+        jvmArgs(org.gradle.internal.jvm.GroovyJpmsWorkarounds.SUPPRESS_COMMON_GROOVY_WARNINGS)
     }
 }
 
@@ -194,6 +191,10 @@ fun Test.addOsAsInputs() {
     inputs.property("operatingSystem", "${OperatingSystem.current().name} ${System.getProperty("os.arch")}")
 }
 
+fun Test.isUnitTest() = listOf("test", "writePerformanceScenarioDefinitions", "writeTmpPerformanceScenarioDefinitions").contains(name)
+
+fun Test.usesEmbeddedExecuter() = name.startsWith("embedded")
+
 fun configureTests() {
     normalization {
         runtimeClasspath {
@@ -201,8 +202,6 @@ fun configureTests() {
             ignore("org/gradle/build-receipt.properties")
         }
     }
-
-    fun Test.isUnitTest() = listOf("test", "writePerformanceScenarioDefinitions", "writeTmpPerformanceScenarioDefinitions").contains(name)
 
     tasks.withType<Test>().configureEach {
         filterEnvironmentVariables()
