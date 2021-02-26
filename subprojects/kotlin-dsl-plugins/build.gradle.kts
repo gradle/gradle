4ef33ac2aebfe2e7fde32722d66beb6ae01af195
Vaidotas Valuckas <vvaluckas@gradle.com>
2021-02-26 12:34:35 +0200

    Bump kotlin-dsl plugin version

--- subprojects/kotlin-dsl-plugins/build.gradle.kts
+++ subprojects/kotlin-dsl-plugins/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 description = "Kotlin DSL Gradle Plugins deployed to the Plugin Portal"
 
 group = "org.gradle.kotlin"
-version = "2.1.2"
+version = "2.1.3"
 
 base.archivesBaseName = "plugins"
 
