19975c2ef82283df67f857581fba6874328f2d04
Jendrik Johannes <jendrik@gradle.com>
2021-03-01 17:01:32 +0100

    Apply test distribution plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,18 @@
 pluginManagement {
     includeBuild("build-logic/build-logic-base")
+    // TODO to allow RC version of 'test-distribution' until is https://github.com/gradle/gradle/issues/15416 fixed
+    repositories {
+        gradlePluginPortal()
+        maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
+        maven { url = uri("https://repo.gradle.org/gradle/enterprise-libs-release-candidates-local") }
+    }
 }
 
 plugins {
     id("gradlebuild.settings-plugins")
     id("gradlebuild.repositories")
+    // TODO directly applied here due to https://github.com/gradle/gradle/issues/15416
+    id("com.gradle.enterprise.test-distribution") version "2.0-rc-5"
 }
 
 rootProject.name = "gradle"
