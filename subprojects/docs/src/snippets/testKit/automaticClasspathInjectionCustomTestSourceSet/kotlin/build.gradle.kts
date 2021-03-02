ca23ee77ba15d1364bf34069544dafaeb8a243fd
Donat Csikos <donat@gradle.com>
2021-03-02 17:06:27 +0100

    Add TODO with explanation

--- subprojects/docs/src/snippets/testKit/automaticClasspathInjectionCustomTestSourceSet/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/testKit/automaticClasspathInjectionCustomTestSourceSet/kotlin/build.gradle.kts
@@ -9,6 +9,7 @@ val functionalTestTask = tasks.register<Test>("functionalTest") {
     group = "verification"
     testClassesDirs = functionalTest.output.classesDirs
     classpath = functionalTest.runtimeClasspath
+    // useJUnitPlatform() // TODO The project is currently broken but fails silently (no tests found). When changing to JUnit Platform the build fails because @TempDir doesn't work in Spock 2.0-M4. We expect that this will be fixed in the next milestone.
 }
 
 tasks.check {
