8d9ca57d6471858cca21ffe00fc58b5b6e5011b9
Paul Merlin <paul@gradle.com>
2021-03-02 12:44:01 +0100

    Make precompiled kotlin-dsl scripts adapters comply with ktlint conventions 0.7.0
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl-integ-tests/src/integTest/kotlin/org/gradle/kotlin/dsl/integration/PrecompiledScriptPluginIntegrationTest.kt
+++ subprojects/kotlin-dsl-integ-tests/src/integTest/kotlin/org/gradle/kotlin/dsl/integration/PrecompiledScriptPluginIntegrationTest.kt
@@ -29,7 +29,7 @@ class PrecompiledScriptPluginIntegrationTest : AbstractPluginIntegrationTest() {
             """
             plugins {
                 `kotlin-dsl`
-                id("org.gradle.kotlin-dsl.ktlint-convention") version "0.6.0"
+                id("org.gradle.kotlin-dsl.ktlint-convention") version "0.7.0"
             }
 
             $repositoriesBlock
