fa8170d33d100b7afd9a0364b3d402ef05405c00
Jendrik Johannes <jendrik@gradle.com>
2021-03-08 10:57:35 +0100

    Cleanup composite build chapter
    - Format snippet and example
    - Remove sentence that makes it sound like composite builds are not
      stable yet
    - Remove dependency substitution improvements from "future plans"
      as it is not clear if it will ever be solved like that

--- subprojects/docs/src/samples/build-organization/composite-builds/basic/kotlin/my-app/app/build.gradle.kts
+++ subprojects/docs/src/samples/build-organization/composite-builds/basic/kotlin/my-app/app/build.gradle.kts
@@ -1,20 +1,20 @@
+// tag::app_dependencies[]
 plugins {
-    application
+    id("application")
 }
-
-group = "org.sample"
-version = "1.0"
 
 application {
     mainClass.set("org.sample.myapp.Main")
 }
 
 dependencies {
-// tag::app_dependencies[]
     implementation("org.sample:number-utils:1.0")
     implementation("org.sample:string-utils:1.0")
-// end::app_dependencies[]
 }
+// end::app_dependencies[]
+
+group = "org.sample"
+version = "1.0"
 
 repositories {
     mavenCentral()
