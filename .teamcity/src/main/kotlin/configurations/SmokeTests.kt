5613db600e3d6e2570cf5e717d11cba137eb656e
Bo Zhang <bo@gradle.com>
2021-03-12 09:29:03 +0800

    Remove extra space in configuration name

--- .teamcity/src/main/kotlin/configurations/SmokeTests.kt
+++ .teamcity/src/main/kotlin/configurations/SmokeTests.kt
@@ -7,7 +7,7 @@ import model.Stage
 
 class SmokeTests(model: CIBuildModel, stage: Stage, testJava: JvmCategory, task: String = "smokeTest") : BaseGradleBuildType(model, stage = stage, init = {
     id("${model.projectId}_${task.capitalize()}s${testJava.version.name.capitalize()}")
-    name = "Smoke Tests with 3rd Party Plugins ($task) - ${testJava.version.name.capitalize()}  Linux"
+    name = "Smoke Tests with 3rd Party Plugins ($task) - ${testJava.version.name.capitalize()} Linux"
     description = "Smoke tests against third party plugins to see if they still work with the current Gradle version"
 
     params {
