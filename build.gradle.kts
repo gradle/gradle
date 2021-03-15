f706ab9c22241cd78758ec91a9d99e13a1070f60
Bo Zhang <bo@gradle.com>
2021-03-15 09:06:53 +0800

    Fail

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,5 @@ plugins {
     id("gradlebuild.quick-check")                // Local development: Convenience task `quickCheck` for running checkstyle/codenarc only on changed files before commit
 }
 
+fail!
 
