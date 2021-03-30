516ae314353459ec65f498a64eb5254dcc0fe24a
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2021-03-30 14:57:22 -0300

    Add description to `:data-structures` project

--- subprojects/data-structures/build.gradle.kts
+++ subprojects/data-structures/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("gradlebuild.distribution.implementation-java")
 }
 
+description = "A set of generic data structures."
+
 dependencies {
     implementation(project(":base-services"))
 }
