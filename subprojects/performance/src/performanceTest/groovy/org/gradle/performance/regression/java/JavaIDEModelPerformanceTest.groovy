2b516d8361297d7975010947025a1e4aa2bb945a
Bo Zhang <bo@gradle.com>
2021-04-20 16:39:08 +0800

    Rebaseline tiny regressions

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaIDEModelPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaIDEModelPerformanceTest.groovy
@@ -33,7 +33,7 @@ import static org.gradle.performance.results.OperatingSystem.LINUX
 class JavaIDEModelPerformanceTest extends AbstractCrossVersionPerformanceTest {
 
     def setup() {
-        runner.targetVersions = ["6.8.2-20210218002439+0000"]
+        runner.targetVersions = ["6.9-20210420081321+0000"]
         runner.minimumBaseVersion = "2.11"
     }
 
