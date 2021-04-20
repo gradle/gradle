658ac7fee02fcf566a724b220435088172e006b6
Bo Zhang <bo@gradle.com>
2021-04-20 16:27:50 +0800

    Add swiftc path on Ubuntu 20.04 into performance test PATH
    We have different swift installation path on Ubuntu 20.04 agents

--- .teamcity/src/main/kotlin/configurations/PerformanceTest.kt
+++ .teamcity/src/main/kotlin/configurations/PerformanceTest.kt
@@ -61,7 +61,7 @@ class PerformanceTest(
             param("env.PERFORMANCE_DB_PASSWORD_TCAGENT", "%performance.db.password.tcagent%")
             when (os) {
                 Os.WINDOWS -> param("env.PATH", "%env.PATH%;C:/Program Files/7-zip")
-                else -> param("env.PATH", "%env.PATH%:/opt/swift/4.2.3/usr/bin")
+                else -> param("env.PATH", "%env.PATH%:/opt/swift/4.2.3/usr/bin:/opt/swift/4.2.4-RELEASE-ubuntu18.04/usr/bin")
             }
         }
         if (testProjects.isNotEmpty()) {
