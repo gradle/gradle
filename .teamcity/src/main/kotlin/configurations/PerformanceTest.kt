78129155f4109674c13954a8d0a328044df5226b
Bo Zhang <bo@gradle.com>
2021-04-30 08:34:49 +0800

    Fix cleanAndroidHome step for windows
    Add the missing os paramter

--- .teamcity/src/main/kotlin/configurations/PerformanceTest.kt
+++ .teamcity/src/main/kotlin/configurations/PerformanceTest.kt
@@ -66,7 +66,7 @@ class PerformanceTest(
         if (testProjects.isNotEmpty()) {
             steps {
                 preBuildSteps()
-                cleanAndroidUserHome()
+                cleanAndroidUserHome(os)
                 killGradleProcessesStep(os)
                 substDirOnWindows(os)
 
