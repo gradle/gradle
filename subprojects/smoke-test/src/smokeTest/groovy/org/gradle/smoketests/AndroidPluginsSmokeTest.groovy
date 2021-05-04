4b7e1f01e6ecea26d4aa686116f2dde4e208e860
Lóránt Pintér <lorant@gradle.com>
2021-05-04 16:08:05 +0200

    Refer to some followup issues

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidPluginsSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidPluginsSmokeTest.groovy
@@ -83,11 +83,10 @@ class AndroidPluginsSmokeTest extends AbstractPluginValidatingSmokeTest implemen
 
         when: 'first build'
         def result = runner
-            .expectDeprecationWarningIf(agpVersion.startsWith('4.0.2') || agpVersion.startsWith('4.1'),
+            .expectLegcyDeprecationWarningIf(agpVersion.startsWith('4.0.2') || agpVersion.startsWith('4.1'),
                 "The WorkerExecutor.submit() method has been deprecated. " +
                     "This is scheduled to be removed in Gradle 8.0. Please use the noIsolation(), classLoaderIsolation() or processIsolation() method instead. " +
-                    "See https://docs.gradle.org/${GradleVersion.current().version}/userguide/upgrading_version_5.html#method_workerexecutor_submit_is_deprecated for more details.",
-                "TODO Add followup issue")
+                    "See https://docs.gradle.org/${GradleVersion.current().version}/userguide/upgrading_version_5.html#method_workerexecutor_submit_is_deprecated for more details.")
             .build()
 
         then:
--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/KotlinPluginSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/KotlinPluginSmokeTest.groovy
@@ -48,7 +48,7 @@ class KotlinPluginSmokeTest extends AbstractPluginValidatingSmokeTest implements
                 "The WorkerExecutor.submit() method has been deprecated. " +
                     "This is scheduled to be removed in Gradle 8.0. Please use the noIsolation(), classLoaderIsolation() or processIsolation() method instead. " +
                     "See https://docs.gradle.org/${GradleVersion.current().version}/userguide/upgrading_version_5.html#method_workerexecutor_submit_is_deprecated for more details.",
-                "TODO Add followup issue"
+                "https://youtrack.jetbrains.com/issue/KT-46019"
             )
             .build()
 
@@ -84,7 +84,7 @@ class KotlinPluginSmokeTest extends AbstractPluginValidatingSmokeTest implements
                 "The WorkerExecutor.submit() method has been deprecated. " +
                     "This is scheduled to be removed in Gradle 8.0. Please use the noIsolation(), classLoaderIsolation() or processIsolation() method instead. " +
                     "See https://docs.gradle.org/${GradleVersion.current().version}/userguide/upgrading_version_5.html#method_workerexecutor_submit_is_deprecated for more details.",
-                "TODO Add followup issue"
+                "https://youtrack.jetbrains.com/issue/KT-46019"
             )
             .build()
 
