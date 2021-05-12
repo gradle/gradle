85a39b10f56618bbf279ad6639dc9048c0d3f02a
Donat Csikos <donat@gradle.com>
2021-05-12 11:23:46 +0200

    Ignore test failing on CI for now

--- subprojects/language-native/src/integTest/groovy/org/gradle/language/swift/SwiftIncrementalBuildIntegrationTest.groovy
+++ subprojects/language-native/src/integTest/groovy/org/gradle/language/swift/SwiftIncrementalBuildIntegrationTest.groovy
@@ -38,6 +38,8 @@ import spock.lang.Ignore
 
 @RequiresInstalledToolChain(ToolChainRequirement.SWIFTC)
 @Ignore("https://github.com/gradle/gradle-private/issues/3387")
+// https://github.com/gradle/gradle-private/issues/3387
+@Requires(TestPrecondition.NOT_EC2_AGENT)
 class SwiftIncrementalBuildIntegrationTest extends AbstractInstalledToolChainIntegrationSpec {
     @ToBeFixedForConfigurationCache
     def "rebuilds application when a single source file changes"() {
