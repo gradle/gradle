8011712934de7921d007dae3299842dc403323fb
Cedric Champeau <cedric@gradle.com>
2021-05-17 14:17:38 +0200

    Introduce `getTypeOriginInspector` on unit of work
    So that future implementations can provide some context, for example
    in what project a task is defined.

--- subprojects/execution/src/main/java/org/gradle/internal/execution/impl/DefaultExecutionEngine.java
+++ subprojects/execution/src/main/java/org/gradle/internal/execution/impl/DefaultExecutionEngine.java
@@ -47,7 +47,7 @@ public class DefaultExecutionEngine implements ExecutionEngine {
             private ExecutionRequestContext createExecutionRequestContext() {
                 WorkValidationContext validationContext = this.validationContext != null
                     ? this.validationContext
-                    : new DefaultWorkValidationContext(documentationRegistry, WorkValidationContext.TypeOriginInspector.NO_OP);
+                    : new DefaultWorkValidationContext(documentationRegistry, work.getTypeOriginInspector());
                 return new ExecutionRequestContext() {
                     @Override
                     public Optional<String> getRebuildReason() {
