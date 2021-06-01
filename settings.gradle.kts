83cb5c5f345a28f849672695d3b991f46320b2cc
François Guillot <francois@gradle.com>
2021-06-01 13:35:31 +0200

    Update Gradle Enterprise Gradle plugin to 3.6.2 (#17332)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise").version("3.6.1")
+    id("com.gradle.enterprise").version("3.6.2")
     id("com.gradle.enterprise.gradle-enterprise-conventions-plugin").version("0.7.2")
     id("gradlebuild.base.allprojects")
     id("gradlebuild.internal.testfiltering")
