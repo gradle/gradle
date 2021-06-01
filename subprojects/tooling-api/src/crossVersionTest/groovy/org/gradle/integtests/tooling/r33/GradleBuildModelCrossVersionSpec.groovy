0acdd5b1748e615e21eb0f625b9154ee01e1c69a
Adam Murdoch <adam@gradle.com>
2021-06-01 14:32:43 +1000

    Fix version ranges on tapi tests.

--- subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r33/GradleBuildModelCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r33/GradleBuildModelCrossVersionSpec.groovy
@@ -78,7 +78,7 @@ class GradleBuildModelCrossVersionSpec extends ToolingApiSpecification {
         includedBuild.projects.size() == 4
     }
 
-    @TargetGradleVersion(">=2.6 <3.3")
+    @TargetGradleVersion(">=3.1 <3.3")
     def "No included builds for old Gradle versions"() {
         singleProjectBuildInRootFolder("root") {
             settingsFile << """
