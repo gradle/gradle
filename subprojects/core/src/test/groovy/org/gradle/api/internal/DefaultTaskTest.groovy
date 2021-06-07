965d01d6b378482a31816cf3ef07954a81711a07
Bo Zhang <bo@gradle.com>
2021-06-07 16:31:59 +0800

    Deliberate failure

--- subprojects/core/src/test/groovy/org/gradle/api/internal/DefaultTaskTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/DefaultTaskTest.groovy
@@ -44,6 +44,7 @@ class DefaultTaskTest extends AbstractTaskTest {
     Object testCustomPropValue
 
     def setup() {
+        throw new IllegalStateException()
         testCustomPropValue = new Object()
         defaultTask = createTask(DefaultTask.class)
         cl = Thread.currentThread().contextClassLoader
