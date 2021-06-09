ec47d2494cdf096eacf62f810d762d43addd72bf
Stefan Wolf <wolf@gradle.com>
2021-06-09 15:37:54 +0200

    Suggest using languageVersion for Kotlin 1.5

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/NestedInputKotlinImplementationTrackingIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/NestedInputKotlinImplementationTrackingIntegrationTest.groovy
@@ -137,7 +137,7 @@ class NestedInputKotlinImplementationTrackingIntegrationTest extends AbstractPlu
         project2.file('build/tmp/myTask/output.txt').text == "hello"
     }
 
-    def "task action defined in Kotlin 1.5 can be tracked when using language versino 1.4"() {
+    def "task action defined in Kotlin 1.5 can be tracked when using language version 1.4"() {
         file("buildSrc/build.gradle.kts") << """
             plugins {
                 kotlin("jvm") version("1.5.10")
