591d545a6b131709d7555e5ea2a301ce3da6ea07
Daniel Clausen <dclausen@gradle.com>
2021-06-11 15:18:44 +0200

    Update GE Gradle plugin to v3.6.3

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/BuildScanPluginSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/BuildScanPluginSmokeTest.groovy
@@ -61,7 +61,8 @@ class BuildScanPluginSmokeTest extends AbstractSmokeTest {
         "3.5.2",
         "3.6",
         "3.6.1",
-        "3.6.2"
+        "3.6.2",
+        "3.6.3"
     ]
 
     private static final VersionNumber FIRST_VERSION_SUPPORTING_CONFIGURATION_CACHE = VersionNumber.parse("3.4")
