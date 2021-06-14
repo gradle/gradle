384b7c13fb2fda04ae6398d38ebb82dc978a6fd0
Stefan Wolf <wolf@gradle.com>
2021-06-14 08:56:09 +0200

    Extract writing transformation result into own class

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultTransformerInvocationFactory.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/DefaultTransformerInvocationFactory.java
@@ -17,11 +17,9 @@
 package org.gradle.api.internal.artifacts.transform;
 
 import com.google.common.collect.ImmutableList;
-import org.gradle.api.UncheckedIOException;
 import org.gradle.api.artifacts.component.ComponentIdentifier;
 import org.gradle.api.artifacts.component.ProjectComponentIdentifier;
 import org.gradle.api.file.FileSystemLocation;
-import org.gradle.api.file.RelativePath;
 import org.gradle.api.internal.file.DefaultFileSystemLocation;
 import org.gradle.api.internal.file.FileCollectionFactory;
 import org.gradle.api.internal.project.ProjectInternal;
@@ -60,19 +58,12 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.OverridingMethodsMustInvokeSuper;
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import static org.gradle.internal.UncheckedException.unchecked;
 import static org.gradle.internal.execution.fingerprint.InputFingerprinter.InputPropertyType.NON_INCREMENTAL;
 import static org.gradle.internal.execution.fingerprint.InputFingerprinter.InputPropertyType.PRIMARY;
 import static org.gradle.internal.file.TreeType.DIRECTORY;
@@ -87,8 +78,6 @@ public class DefaultTransformerInvocationFactory implements TransformerInvocatio
     private static final String SECONDARY_INPUTS_HASH_PROPERTY_NAME = "inputPropertiesHash";
     private static final String OUTPUT_DIRECTORY_PROPERTY_NAME = "outputDirectory";
     private static final String RESULTS_FILE_PROPERTY_NAME = "resultsFile";
-    private static final String INPUT_FILE_PATH_PREFIX = "i/";
-    private static final String OUTPUT_FILE_PATH_PREFIX = "o/";
 
     private final ExecutionEngine executionEngine;
     private final FileSystemAccess fileSystemAccess;
@@ -309,7 +298,8 @@ public class DefaultTransformerInvocationFactory implements TransformerInvocatio
                     File workspace = executionRequest.getWorkspace();
                     InputChangesInternal inputChanges = executionRequest.getInputChanges().orElse(null);
                     ImmutableList<File> result = transformer.transform(inputArtifactProvider, getOutputDir(workspace), dependencies, inputChanges);
-                    return writeResultsFile(workspace, result);
+                    TransformationResultWriter resultWriter = new TransformationResultWriter(inputArtifact, getOutputDir(workspace));
+                    return resultWriter.writeToFile(getResultsFile(workspace), result);
                 }
 
                 @Override
@@ -335,7 +325,8 @@ public class DefaultTransformerInvocationFactory implements TransformerInvocatio
 
         @Override
         public Object loadRestoredOutput(File workspace) {
-            return readResultsFile(workspace);
+            TransformationResultWriter resultWriter = new TransformationResultWriter(inputArtifact, getOutputDir(workspace));
+            return resultWriter.readResultsFile(getResultsFile(workspace));
         }
 
         @Override
@@ -346,62 +337,6 @@ public class DefaultTransformerInvocationFactory implements TransformerInvocatio
         @Override
         public InputFingerprinter getInputFingerprinter() {
             return inputFingerprinter;
-        }
-
-        private TransformationResult writeResultsFile(File workspace, ImmutableList<File> result) {
-            TransformationResult.Builder builder = TransformationResult.builder();
-            File outputDir = getOutputDir(workspace);
-            String outputDirPrefix = outputDir.getPath() + File.separator;
-            String inputFilePrefix = inputArtifact.getPath() + File.separator;
-            List<String> resultFileContents = new ArrayList<>(result.size());
-            result.forEach(file -> {
-                if (file.equals(outputDir)) {
-                    builder.addOutput(outputDir);
-                    resultFileContents.add(OUTPUT_FILE_PATH_PREFIX);
-                } else if (file.equals(inputArtifact)) {
-                    builder.addInputArtifact();
-                    resultFileContents.add(INPUT_FILE_PATH_PREFIX);
-                } else {
-                    String absolutePath = file.getAbsolutePath();
-                    if (absolutePath.startsWith(outputDirPrefix)) {
-                        builder.addOutput(file);
-                        resultFileContents.add(OUTPUT_FILE_PATH_PREFIX + RelativePath.parse(true, absolutePath.substring(outputDirPrefix.length())).getPathString());
-                    } else if (absolutePath.startsWith(inputFilePrefix)) {
-                        String relativePath = RelativePath.parse(true, absolutePath.substring(inputFilePrefix.length())).getPathString();
-                        builder.addInputArtifact(relativePath);
-                        resultFileContents.add(INPUT_FILE_PATH_PREFIX + relativePath);
-                    } else {
-                        throw new IllegalStateException("Invalid result path: " + absolutePath);
-                    }
-                }
-            });
-            unchecked(() -> Files.write(getResultsFile(workspace).toPath(), resultFileContents));
-            return builder.build();
-        }
-
-        private TransformationResult readResultsFile(File workspace) {
-            Path transformerResultsPath = getResultsFile(workspace).toPath();
-            try {
-                TransformationResult.Builder builder = TransformationResult.builder();
-                List<String> paths = Files.readAllLines(transformerResultsPath, StandardCharsets.UTF_8);
-                for (String path : paths) {
-                    if (path.startsWith(OUTPUT_FILE_PATH_PREFIX)) {
-                        builder.addOutput(new File(getOutputDir(workspace), path.substring(2)));
-                    } else if (path.startsWith(INPUT_FILE_PATH_PREFIX)) {
-                        String relativePathString = path.substring(2);
-                        if (relativePathString.isEmpty()) {
-                            builder.addInputArtifact();
-                        } else {
-                            builder.addInputArtifact(relativePathString);
-                        }
-                    } else {
-                        throw new IllegalStateException("Cannot parse result path string: " + path);
-                    }
-                }
-                return builder.build();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
         }
 
         private static File getOutputDir(File workspace) {
