acd35996b6793dd8007012f2f365866803101b81
Bo Zhang <bo@gradle.com>
2021-06-25 16:19:42 +0800

    Revert "Ignore AndroidSantaTrackerSmokeTest.can lint Santa-Tracker for now"
    This reverts commit 8ff7c85f90f54b10a5d9f56b6ed234f4baaac6ca.

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidSantaTrackerSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidSantaTrackerSmokeTest.groovy
@@ -19,7 +19,6 @@ package org.gradle.smoketests
 import org.gradle.integtests.fixtures.UnsupportedWithConfigurationCache
 import org.gradle.util.GradleVersion
 import org.gradle.util.internal.VersionNumber
-import spock.lang.Ignore
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
@@ -101,7 +100,6 @@ class AndroidSantaTrackerSmokeTest extends AbstractAndroidSantaTrackerSmokeTest 
     }
 
     @UnsupportedWithConfigurationCache(iterationMatchers = [AGP_4_0_ITERATION_MATCHER, AGP_4_1_ITERATION_MATCHER, AGP_4_2_ITERATION_MATCHER])
-    @Ignore("https://github.com/gradle/gradle-private/issues/3409")
     def "can lint Santa-Tracker (agp=#agpVersion)"() {
 
         given:
