f7606ac4c686375ff443d7e0b11f3e7b01fc937e
Bo Zhang <bo@gradle.com>
2021-07-07 16:08:35 +0800

    Disable ArtifactTransformCachingIntegrationTest only for ConfigCache executer

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformCachingIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformCachingIntegrationTest.groovy
@@ -29,8 +29,8 @@ import org.gradle.test.fixtures.file.LeaksFileHandles
 import org.gradle.test.fixtures.file.TestFile
 import org.gradle.test.fixtures.server.http.BlockingHttpServer
 import org.junit.Rule
-import spock.lang.Ignore
 import spock.lang.Issue
+import spock.lang.Requires
 import spock.lang.Unroll
 
 import java.util.regex.Pattern
@@ -41,7 +41,8 @@ import static org.gradle.internal.service.scopes.DefaultGradleUserHomeScopeServi
 import static org.gradle.test.fixtures.ConcurrentTestUtil.poll
 import static org.hamcrest.Matchers.containsString
 
-@Ignore("https://github.com/gradle/gradle-private/issues/3413")
+@Issue("https://github.com/gradle/gradle-private/issues/3413")
+@Requires({ !GradleContextualExecuter.configCache })
 class ArtifactTransformCachingIntegrationTest extends AbstractHttpDependencyResolutionTest implements FileAccessTimeJournalFixture, ValidationMessageChecker {
     private final static long MAX_CACHE_AGE_IN_DAYS = LeastRecentlyUsedCacheCleanup.DEFAULT_MAX_AGE_IN_DAYS_FOR_RECREATABLE_CACHE_ENTRIES
 
