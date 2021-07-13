ffc30318f3696d61e7cbf4b709a940a7740e4b84
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2021-07-13 11:11:52 -0300

    Upgrade `asm` version to `9.2`

--- build-logic/build-platform/build.gradle.kts
+++ build-logic/build-platform/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 // Here you should declare versions which should be shared by the different modules of buildSrc itself
 val javaParserVersion = "3.18.0"
 val groovyVersion = "3.0.8"
-val asmVersion = "9.1"
+val asmVersion = "9.2"
 
 val kotlinVersion = providers.gradleProperty("buildKotlinVersion")
     .forUseAtConfigurationTime()
