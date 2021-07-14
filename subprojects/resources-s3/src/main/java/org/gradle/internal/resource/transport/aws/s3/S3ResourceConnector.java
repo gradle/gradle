89e4c38b0e5bde8196d8a11c142b16640aa92fd6
Mikhail Lopatkin <mlopatkin@gradle.com>
2021-07-14 17:57:12 +0300

    Discard the object body when doing metadata request to S3
    Otherwise the client library issues a warning about potential waste.
    There is waste but only 1 byte (Range header is used). I've kept an
    info message for the cases where more than 1 byte is downloaded - just
    in case if someone needs to investigate issues with the S3-compatible
    service that doesn't understand Range headers and returns the whole
    object instead.

--- subprojects/resources-s3/src/main/java/org/gradle/internal/resource/transport/aws/s3/S3ResourceConnector.java
+++ subprojects/resources-s3/src/main/java/org/gradle/internal/resource/transport/aws/s3/S3ResourceConnector.java
@@ -18,6 +18,7 @@ package org.gradle.internal.resource.transport.aws.s3;
 
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
+import com.google.common.io.ByteStreams;
 import org.gradle.internal.IoActions;
 import org.gradle.internal.resource.ReadableContent;
 import org.gradle.internal.resource.metadata.DefaultExternalResourceMetaData;
@@ -72,6 +73,22 @@ public class S3ResourceConnector implements ExternalResourceConnector {
                 objectMetadata.getContentType(),
                 objectMetadata.getETag(),
                 null); // Passing null for sha1 - TODO - consider using the etag which is an MD5 hash of the file (when less than 5Gb)
+        } finally {
+            discardEmptyContentAndClose(s3Object);
+        }
+    }
+
+    private static void discardEmptyContentAndClose(S3Object s3Object) {
+        // Consume the content stream to avoid warning from S3 SDK. The response should have only 1 byte there because Range header was specified.
+        try {
+            long downloadedContentLength = ByteStreams.exhaust(s3Object.getObjectContent());
+            if (downloadedContentLength > 1L) {
+                // This may happen if the endpoint ignores Range HTTP header for whatever reason.
+                LOGGER.info("Downloaded {} bytes of the object content for metadata request which is too much.", downloadedContentLength);
+            }
+        } catch (IOException e) {
+            // Don't complain loudly to the user about the error there because we were discarding the response anyway.
+            LOGGER.debug("Exception while consuming empty object content from metadata request", e);
         } finally {
             IoActions.closeQuietly(s3Object);
         }
