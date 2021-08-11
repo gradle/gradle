689537c9a9e4b166af98f33f627238b89bef25bc
Benedikt Ritter <benedikt@gradle.com>
2021-08-11 11:14:16 +0200

    Add missing filter for test-selection plugin

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -29,6 +29,7 @@ dependencyResolutionManagement {
                 val rcAndMilestonesPattern = "\\d{1,2}?\\.\\d{1,2}?(\\.\\d{1,2}?)?-((rc-\\d{1,2}?)|(milestone-\\d{1,2}?))"
                 includeVersionByRegex("com.gradle", "gradle-enterprise-gradle-plugin", rcAndMilestonesPattern)
                 includeVersionByRegex("com.gradle.enterprise", "test-distribution-gradle-plugin", rcAndMilestonesPattern)
+                includeVersionByRegex("com.gradle.internal", "test-selection-gradle-plugin", rcAndMilestonesPattern)
             }
         }
         maven {
