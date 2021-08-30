f6c8a837f867f1ef5712949dea2db00f575146a2
Lóránt Pintér <lorant@gradle.com>
2021-08-30 12:35:05 +0200

    Clarify Javadoc for caching disabled reasons

--- subprojects/execution/src/main/java/org/gradle/internal/execution/caching/CachingDisabledReasonCategory.java
+++ subprojects/execution/src/main/java/org/gradle/internal/execution/caching/CachingDisabledReasonCategory.java
@@ -49,6 +49,12 @@ public enum CachingDisabledReasonCategory {
 
     /**
      * Work has declared output that is not cacheable.
+     *
+     * Readons for non-cacheable outputs:
+     * <ul>
+     *     <li>an output contains a file tree,</li>
+     *     <li>an output is not tracked.</li>
+     * </ul>
      */
     NON_CACHEABLE_OUTPUT,
 
@@ -69,7 +75,7 @@ public enum CachingDisabledReasonCategory {
      * <ul>
      *     <li>some type used as an input is loaded via an unknown classloader,</li>
      *     <li>a Java lambda was used as an input,</li>
-     *     <li>an input or output is not tracked.</li>
+     *     <li>an input is not tracked.</li>
      * </ul>
      *
      * @see <a href="https://docs.gradle.org/current/userguide/more_about_tasks.html#sec:how_does_it_work">How fingerprinting works</a>
