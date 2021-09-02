02a3894baaa201d115ffde444f92323181da6adb
Thomas Tresansky <ttresansky@gradle.com>
2021-09-02 11:06:31 -0400

    Fix test task created from TestSuitePlugin on Gradle > 7.2.

--- subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r51/TaskOriginCrossVersionSpec.groovy
+++ subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r51/TaskOriginCrossVersionSpec.groovy
@@ -25,6 +25,7 @@ import org.gradle.tooling.BuildLauncher
 import org.gradle.tooling.events.OperationType
 import org.gradle.tooling.events.task.TaskOperationDescriptor
 import org.gradle.tooling.model.UnsupportedMethodException
+import org.gradle.util.GradleVersion
 
 @ToolingApiVersion('>=5.1')
 @TargetGradleVersion('>=5.1')
@@ -63,7 +64,11 @@ class TaskOriginCrossVersionSpec extends ToolingApiSpecification {
         task(':classes').originPlugin.displayName == "org.gradle.api.plugins.JavaBasePlugin"
         task(':jar').originPlugin.displayName == "org.gradle.java"
         task(':assemble').originPlugin.displayName == "org.gradle.language.base.plugins.LifecycleBasePlugin"
-        task(':test').originPlugin.displayName == "org.gradle.java"
+        if (targetVersion > GradleVersion.version("7.2")) {
+            task(':test').originPlugin.displayName == "org.gradle.api.plugins.TestSuitePlugin"
+        } else {
+            task(':test').originPlugin.displayName == "org.gradle.java"
+        }
         task(':check').originPlugin.displayName == "org.gradle.language.base.plugins.LifecycleBasePlugin"
         task(':build').originPlugin.displayName == "org.gradle.language.base.plugins.LifecycleBasePlugin"
     }
