64ae047997536907d2d13e1306b671e245a5400a
Bo Zhang <bo@gradle.com>
2021-09-08 16:29:58 +0800

    Update comments for QuickCrossVersionTestBucket
    Co-authored-by: Stefan Wolf <wolf@gradle.com>

--- .teamcity/src/main/kotlin/model/FunctionalTestBucketProvider.kt
+++ .teamcity/src/main/kotlin/model/FunctionalTestBucketProvider.kt
@@ -11,7 +11,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import java.io.File
 
 /**
- * QuickCrossVersionTest only tests the last major version.
+ * QuickCrossVersionTest only tests the last minor for each major version in the range.
  */
 val QUICK_CROSS_VERSION_BUCKETS = listOf(
     listOf("0.0", "3.0"), // 0.0 <= version < 3.0
