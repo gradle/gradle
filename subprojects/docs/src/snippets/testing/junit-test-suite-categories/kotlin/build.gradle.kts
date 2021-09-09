b50c703896c0d8e0274c0c322a4d749663e2b2b0
Kyle Moore <kmoore@gradle.com>
2021-09-09 14:03:14 -0700

    Apply review feedback. Both groovy and kotlin scripts are correct, but compileTestJava task fails

--- subprojects/docs/src/snippets/testing/junit-test-suite-categories/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/testing/junit-test-suite-categories/kotlin/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     java
-    `jvm-test-suite`
 }
 
 repositories {
@@ -9,18 +8,15 @@ repositories {
 
 testing {
     suites {
-        named<JvmTestSuite>("unitTest") { // FIXME TestSuite with name 'unitTest' not found.
+        named<JvmTestSuite>("test") {
             useJUnit()
-            dependencies {
-                implementation("junit:junit:4.13")
-            }
             targets {
                 all {
                     // tag::test-categories[]
                     testTask.configure {
-                        options { // FIXME Ugly unsafe casts. Do we need to qualify the type of options below?
-                            (this as JUnitOptions).includeCategories("org.gradle.junit.CategoryA")
-                            (this as JUnitOptions).excludeCategories("org.gradle.junit.CategoryB")
+                        useJUnit {
+                            includeCategories("org.gradle.junit.CategoryA")
+                            excludeCategories("org.gradle.junit.CategoryB")
                         }
                     }
                     // end::test-categories[]
