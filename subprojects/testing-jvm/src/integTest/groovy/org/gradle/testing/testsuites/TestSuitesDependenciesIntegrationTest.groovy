3f47089976f22c52147ed7a3548d7f56f88df864
Kyle Moore <kmoore@gradle.com>
2021-09-20 11:44:21 -0700

    Additional coverage for compileOnly, runtimeOnly configurations of test-suites
    Note there may be a bug/inconsistency with project implementation dependencies

--- subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/testsuites/TestSuitesDependenciesIntegrationTest.groovy
+++ subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/testsuites/TestSuitesDependenciesIntegrationTest.groovy
@@ -111,16 +111,123 @@ class TestSuitesDependenciesIntegrationTest extends AbstractIntegrationSpec {
         }
 
         tasks.register('checkConfiguration') {
-                dependsOn test, integTest
-                doLast {
-                    assert configurations.testRuntimeClasspath.files*.name == ['commons-lang3-3.11.jar'] : 'commons-lang3 leaks from the production project dependencies'
-                    assert !configurations.integTestRuntimeClasspath.files*.name.contains('commons-lang3-3.11.jar') : 'integTest does not implicitly depend on the production project'
-                }
+            dependsOn test, integTest
+            doLast {
+                assert configurations.testRuntimeClasspath.files*.name == ['commons-lang3-3.11.jar'] : 'commons-lang3 leaks from the production project dependencies'
+                assert !configurations.integTestRuntimeClasspath.files*.name.contains('commons-lang3-3.11.jar') : 'integTest does not implicitly depend on the production project'
             }
+        }
         """
 
-      expect:
-      succeeds 'checkConfiguration'
-  }
+        expect:
+        succeeds 'checkConfiguration'
+    }
+
+    def 'non-default default test suites have project dependency if explicitly set'() {
+        given:
+        buildFile << """
+        plugins {
+          id 'java'
+        }
+
+        ${mavenCentralRepository()}
+
+        dependencies {
+            // production code requires commons-lang3 at runtime, which will leak into tests' runtime classpaths
+            implementation 'org.apache.commons:commons-lang3:3.11'
+        }
+
+        testing {
+            suites {
+                integTest(JvmTestSuite) {
+                    dependencies {
+                        implementation project(':')
+                    }
+                }
+            }
+        }
+
+        tasks.named('check') {
+            dependsOn testing.suites.integTest
+        }
+
+        tasks.register('checkConfiguration') {
+            dependsOn test, integTest
+            doLast {
+                assert configurations.testCompileClasspath.files*.name == ['commons-lang3-3.11.jar'] : 'commons-lang3 leaks from the production project dependencies' // TODO possible bug; why is commons-lang3 present?
+                assert configurations.testRuntimeClasspath.files*.name == ['commons-lang3-3.11.jar'] : 'commons-lang3 leaks from the production project dependencies'
+                assert configurations.integTestRuntimeClasspath.files*.name.contains('commons-lang3-3.11.jar') : 'integTest explicitly depends on the production project'
+            }
+        }
+        """
+
+        expect:
+        succeeds 'checkConfiguration'
+    }
+
+    def 'user can add dependencies to the implementation, compileOnly and runtimeOnly configurations of a suite'() {
+        given:
+        buildFile << """
+        plugins {
+          id 'java'
+        }
+
+        ${mavenCentralRepository()}
+
+        dependencies {
+            // production code requires commons-lang3 at runtime, which will leak into tests' runtime classpaths
+            implementation 'org.apache.commons:commons-lang3:3.11'
+        }
+
+        testing {
+            suites {
+                test {
+                    dependencies {
+                        implementation 'com.google.guava:guava:30.1.1-jre'
+                        compileOnly 'javax.servlet:servlet-api:3.0-alpha-1'
+                        runtimeOnly 'mysql:mysql-connector-java:8.0.26'
+                    }
+                }
+                integTest(JvmTestSuite) {
+                    // intentionally setting lower versions of the same dependencies on the `test` suite to show that no conflict resolution should be taking place
+                    dependencies {
+                        implementation project(':')
+                        implementation 'com.google.guava:guava:29.0-jre'
+                        compileOnly  'javax.servlet:servlet-api:2.5'
+                        runtimeOnly 'mysql:mysql-connector-java:6.0.6'
+                    }
+                }
+            }
+        }
+
+        tasks.named('check') {
+            dependsOn testing.suites.integTest
+        }
+
+        tasks.register('checkConfiguration') {
+            dependsOn test, integTest
+            doLast {
+                def testCompileClasspathFileNames = configurations.testCompileClasspath.files*.name
+                def testRuntimeClasspathFileNames = configurations.testRuntimeClasspath.files*.name
+
+                assert testCompileClasspathFileNames.containsAll('commons-lang3-3.11.jar', 'servlet-api-3.0-alpha-1.jar', 'guava-30.1.1-jre.jar')
+                assert !testCompileClasspathFileNames.contains('mysql-connector-java-8.0.26.jar'): 'runtimeOnly dependency'
+                assert testRuntimeClasspathFileNames.containsAll('commons-lang3-3.11.jar', 'guava-30.1.1-jre.jar', 'mysql-connector-java-8.0.26.jar')
+                assert !testRuntimeClasspathFileNames.contains('servlet-api-3.0-alpha-1.jar'): 'compileOnly dependency'
+
+                def integTestCompileClasspathFileNames = configurations.integTestCompileClasspath.files*.name
+                def integTestRuntimeClasspathFileNames = configurations.integTestRuntimeClasspath.files*.name
+
+                assert integTestCompileClasspathFileNames.containsAll(/*'commons-lang3-3.11.jar', */'servlet-api-2.5.jar', 'guava-29.0-jre.jar') // TODO possible bug; why is commons-lang3 present in testCompileClasspathFileNames but not integTestCompileClasspathFileNames?
+                assert !integTestCompileClasspathFileNames.contains('mysql-connector-java-6.0.6.jar'): 'runtimeOnly dependency'
+                assert integTestRuntimeClasspathFileNames.containsAll('commons-lang3-3.11.jar', 'guava-29.0-jre.jar', 'mysql-connector-java-6.0.6.jar')
+                assert !integTestRuntimeClasspathFileNames.contains('servlet-api-2.5.jar'): 'compileOnly dependency'
+            }
+        }
+        """
+
+        expect:
+        succeeds 'checkConfiguration'
+    }
 
 }
