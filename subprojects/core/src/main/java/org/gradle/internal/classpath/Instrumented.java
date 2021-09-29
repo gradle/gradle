0749aeaf05fbc9e897261ef1436d401af733ef40
Mikhail Lopatkin <mlopatkin@gradle.com>
2021-09-29 10:13:21 +0200

    Extract access-tracking Properties to a separate class
    This allows reusing parts of it in environment variables access
    tracking. Some unit tests are added along the way.

--- subprojects/core/src/main/java/org/gradle/internal/classpath/Instrumented.java
+++ subprojects/core/src/main/java/org/gradle/internal/classpath/Instrumented.java
@@ -16,28 +16,19 @@
 
 package org.gradle.internal.classpath;
 
-import com.google.common.collect.AbstractIterator;
 import org.codehaus.groovy.runtime.callsite.AbstractCallSite;
 import org.codehaus.groovy.runtime.callsite.CallSite;
 import org.codehaus.groovy.runtime.callsite.CallSiteArray;
 import org.codehaus.groovy.runtime.wrappers.Wrapper;
 
 import javax.annotation.Nullable;
-import java.util.AbstractSet;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public class Instrumented {
     private static final Listener NO_OP = (key, value, consumer) -> {
     };
+
     private static final AtomicReference<Listener> LISTENER = new AtomicReference<>(NO_OP);
 
     public static void setListener(Listener listener) {
@@ -89,7 +80,9 @@ public class Instrumented {
 
     // Called by generated code.
     public static Properties systemProperties(String consumer) {
-        return new DecoratingProperties(System.getProperties(), consumer);
+        return new AccessTrackingProperties(System.getProperties(), (k, v) -> {
+            LISTENER.get().systemPropertyQueried(convertToString(k), convertToString(v), consumer);
+        });
     }
 
     // Called by generated code.
@@ -153,210 +146,6 @@ public class Instrumented {
          * @param consumer The name of the class that is reading the property value
          */
         void systemPropertyQueried(String key, @Nullable Object value, String consumer);
-    }
-
-    private static class DecoratingProperties extends Properties {
-        private final String consumer;
-        private final Properties delegate;
-
-        public DecoratingProperties(Properties delegate, String consumer) {
-            this.consumer = consumer;
-            this.delegate = delegate;
-        }
-
-        @Override
-        public Enumeration<?> propertyNames() {
-            return delegate.propertyNames();
-        }
-
-        @Override
-        public Set<String> stringPropertyNames() {
-            return delegate.stringPropertyNames();
-        }
-
-        @Override
-        public int size() {
-            return delegate.size();
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return delegate.isEmpty();
-        }
-
-        @Override
-        public Enumeration<Object> keys() {
-            return delegate.keys();
-        }
-
-        @Override
-        public Enumeration<Object> elements() {
-            return delegate.elements();
-        }
-
-        @Override
-        public Set<Object> keySet() {
-            return delegate.keySet();
-        }
-
-        @Override
-        public Collection<Object> values() {
-            return delegate.values();
-        }
-
-        @Override
-        public Set<Map.Entry<Object, Object>> entrySet() {
-            return new DecoratingEntrySet(delegate.entrySet(), consumer);
-        }
-
-        @Override
-        public void forEach(BiConsumer<? super Object, ? super Object> action) {
-            delegate.forEach((k, v) -> {
-                LISTENER.get().systemPropertyQueried((String) k, v, consumer);
-                action.accept(k, v);
-            });
-        }
-
-        @Override
-        public void replaceAll(BiFunction<? super Object, ? super Object, ?> function) {
-            delegate.replaceAll(function);
-        }
-
-        @Override
-        public Object putIfAbsent(Object key, Object value) {
-            return delegate.putIfAbsent(key, value);
-        }
-
-        @Override
-        public boolean remove(Object key, Object value) {
-            return delegate.remove(key, value);
-        }
-
-        @Override
-        public boolean replace(Object key, Object oldValue, Object newValue) {
-            return delegate.replace(key, oldValue, newValue);
-        }
-
-        @Override
-        public Object replace(Object key, Object value) {
-            return delegate.replace(key, value);
-        }
-
-        @Override
-        public Object computeIfAbsent(Object key, Function<? super Object, ?> mappingFunction) {
-            return delegate.computeIfAbsent(key, mappingFunction);
-        }
-
-        @Override
-        public Object computeIfPresent(Object key, BiFunction<? super Object, ? super Object, ?> remappingFunction) {
-            return delegate.computeIfPresent(key, remappingFunction);
-        }
-
-        @Override
-        public Object compute(Object key, BiFunction<? super Object, ? super Object, ?> remappingFunction) {
-            return delegate.compute(key, remappingFunction);
-        }
-
-        @Override
-        public Object merge(Object key, Object value, BiFunction<? super Object, ? super Object, ?> remappingFunction) {
-            return delegate.merge(key, value, remappingFunction);
-        }
-
-        @Override
-        public boolean contains(Object value) {
-            return delegate.contains(value);
-        }
-
-        @Override
-        public boolean containsValue(Object value) {
-            return delegate.containsValue(value);
-        }
-
-        @Override
-        public boolean containsKey(Object key) {
-            return delegate.containsKey(key);
-        }
-
-        @Override
-        public Object put(Object key, Object value) {
-            return delegate.put(key, value);
-        }
-
-        @Override
-        public Object setProperty(String key, String value) {
-            return delegate.setProperty(key, value);
-        }
-
-        @Override
-        public Object remove(Object key) {
-            return delegate.remove(key);
-        }
-
-        @Override
-        public void putAll(Map<?, ?> t) {
-            delegate.putAll(t);
-        }
-
-        @Override
-        public void clear() {
-            delegate.clear();
-        }
-
-        @Override
-        public String getProperty(String key) {
-            return getProperty(key, null);
-        }
-
-        @Override
-        public String getProperty(String key, String defaultValue) {
-            String value = delegate.getProperty(key);
-            LISTENER.get().systemPropertyQueried(key, value, consumer);
-            if (value == null) {
-                return defaultValue;
-            }
-            return value;
-        }
-
-        @Override
-        public Object getOrDefault(Object key, Object defaultValue) {
-            return getProperty((String) key, (String) defaultValue);
-        }
-
-        @Override
-        public Object get(Object key) {
-            return getProperty((String) key);
-        }
-    }
-
-    private static class DecoratingEntrySet extends AbstractSet<Map.Entry<Object, Object>> {
-        private final Set<Map.Entry<Object, Object>> delegate;
-        private final String consumer;
-
-        public DecoratingEntrySet(Set<Map.Entry<Object, Object>> delegate, String consumer) {
-            this.delegate = delegate;
-            this.consumer = consumer;
-        }
-
-        @Override
-        public Iterator<Map.Entry<Object, Object>> iterator() {
-            Iterator<Map.Entry<Object, Object>> iterator = delegate.iterator();
-            return new AbstractIterator<Map.Entry<Object, Object>>() {
-                @Override
-                protected Map.Entry<Object, Object> computeNext() {
-                    if (!iterator.hasNext()) {
-                        return endOfData();
-                    }
-                    Map.Entry<Object, Object> entry = iterator.next();
-                    LISTENER.get().systemPropertyQueried((String) entry.getKey(), entry.getValue(), consumer);
-                    return entry;
-                }
-            };
-        }
-
-        @Override
-        public int size() {
-            return delegate.size();
-        }
     }
 
     private static class IntegerSystemPropertyCallSite extends AbstractCallSite {
