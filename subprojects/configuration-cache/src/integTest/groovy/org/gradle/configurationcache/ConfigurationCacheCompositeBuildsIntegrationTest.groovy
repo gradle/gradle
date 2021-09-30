ac98119fdc64a3eb0eff25e7d54316ae2ed4f192
Adam Murdoch <adam@gradle.com>
2021-09-30 10:20:21 +1000

    Re-enable test.

--- subprojects/configuration-cache/src/integTest/groovy/org/gradle/configurationcache/ConfigurationCacheCompositeBuildsIntegrationTest.groovy
+++ subprojects/configuration-cache/src/integTest/groovy/org/gradle/configurationcache/ConfigurationCacheCompositeBuildsIntegrationTest.groovy
@@ -17,20 +17,15 @@
 package org.gradle.configurationcache
 
 import org.gradle.integtests.fixtures.BuildOperationTreeQueries
-import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
 import org.gradle.internal.operations.trace.BuildOperationRecord
 import org.gradle.internal.os.OperatingSystem
 import org.gradle.internal.scan.config.fixtures.ApplyGradleEnterprisePluginFixture
 import org.gradle.test.fixtures.file.TestFile
-import spock.lang.IgnoreIf
-import spock.lang.Issue
 
 import java.util.regex.Pattern
 
 class ConfigurationCacheCompositeBuildsIntegrationTest extends AbstractConfigurationCacheIntegrationTest {
 
-    @Issue("https://github.com/gradle/gradle-private/issues/3437")
-    @IgnoreIf({ GradleContextualExecuter.noDaemon })
     def "can publish build scan with composite build"() {
         given:
         def configurationCache = newConfigurationCacheFixture()
