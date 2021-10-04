03c7692be00c6c389558ff4ce5b53796a7f0622f
József Bartók <jbartok@gradle.com>
2021-10-04 15:31:12 +0200

    Remove dependencies

--- subprojects/bootstrap/build.gradle.kts
+++ subprojects/bootstrap/build.gradle.kts
@@ -6,7 +6,4 @@ gradlebuildJava.usedForStartup()
 
 dependencies {
     implementation(project(":base-services"))
-    implementation(project(":core"))
-    implementation(project(":core-api"))
-    implementation(project(":logging"))
 }
