ff108e8f115d090f12fb57033127f9688c76d8df
Lóránt Pintér <lorant@gradle.com>
2021-10-07 18:49:16 +0200

    Fix unchecked conversion

--- subprojects/model-core/src/testFixtures/groovy/org/gradle/api/internal/provider/ProviderTestUtil.java
+++ subprojects/model-core/src/testFixtures/groovy/org/gradle/api/internal/provider/ProviderTestUtil.java
@@ -24,6 +24,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class ProviderTestUtil {
     public static <T> ProviderInternal<T> withNoValue() {
         return Providers.notDefined();
