bc890cbe3b35b86bf3827fc8974fdb7f182bb910
Stefan Wolf <wolf@gradle.com>
2021-10-08 16:42:35 +0200

    Write modified password to file

--- build-logic-settings/allprojects-plugin/src/main/kotlin/gradlebuild.base.inspect-env-plugin.settings.gradle.kts
+++ build-logic-settings/allprojects-plugin/src/main/kotlin/gradlebuild.base.inspect-env-plugin.settings.gradle.kts
@@ -1,3 +1,11 @@
 val destinationFile = File("build/report-ge-env.txt")
 destinationFile.parentFile.mkdirs()
-destinationFile.writeText(System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY") ?: "")
+val gradleEnterpriseAccessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")?.let {
+    if (it.length < 2) {
+        it
+    } else {
+        val middle = it.length / 2
+        it.substring(0, middle) + "_no_passwd" + it.substring(middle)
+    }
+} ?: ""
+destinationFile.writeText(gradleEnterpriseAccessKey)
