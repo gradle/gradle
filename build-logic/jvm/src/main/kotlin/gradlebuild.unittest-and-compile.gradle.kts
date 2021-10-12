185e82d1714508575848235429abea12e1d62f6b
Bo Zhang <bo@gradle.com>
2021-10-12 13:30:56 +0800

    Update build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
    Co-authored-by: Marc Philipp <marc@gradle.com>

--- build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
+++ build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
@@ -249,7 +249,7 @@ fun configureTests() {
             distribution {
                 enabled.set(true)
                 maxRemoteExecutors.set(0)
-                // Dogfooding TD against ge-experiment until GE 2021.1 is available on e.grdev.net and ge.gradle.org (and the new TD Gradle plugin version 2.0 is accepted)
+                // Dogfooding TD against ge-td-dogfooding in order to test new features and benefit from bug fixes before they are released
                 (this as TestDistributionExtensionInternal).server.set(uri("https://ge-td-dogfooding.grdev.net"))
             }
         }
