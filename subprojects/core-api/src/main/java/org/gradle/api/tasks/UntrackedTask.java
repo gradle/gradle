3f785b9bc96588b2700ca2ce33f5e4d491969114
Stefan Wolf <wolf@gradle.com>
2021-10-18 10:13:07 +0200

    Update Javadocs

--- subprojects/core-api/src/main/java/org/gradle/api/tasks/UntrackedTask.java
+++ subprojects/core-api/src/main/java/org/gradle/api/tasks/UntrackedTask.java
@@ -25,25 +25,21 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Declares that the changes for an input file or output property should not be tracked by Gradle.
+ * Attached to a task to declare that the task should be untracked.
  *
- * <p>For untracked inputs and outputs Gradle will not try to capture their state.
- * There can be different reasons for not wanting to track the state of some inputs or outputs, for example:
+ * <p>If a task is untracked, then Gradle will not try to capture its state.
+ * That also means that Gradle does not do any optimizations for running the task.
+ * For example, such a task will always be out of date and never stored in or loaded from the build cache.
+ *
+ * <p>There can be different reasons for declaring a task as untracked, for example:
  * <ul>
- *     <li>The location contains unreadable files like pipes where Gradle cannot track the content.</li>
+ *     <li>Some input or output locations contain unreadable files like pipes where Gradle cannot track the content.</li>
  *     <li>Another tool like Git already takes care of keeping the state, so it doesn't make sense for Gradle to do additional bookkeeping.</li>
  *     <li>Prevent Gradle from trying to snapshot a potentially large amount of content if an output location is not exclusively owned by the build.</li>
  * </ul>
  *
- * <p>If a task has any untracked properties, then Gradle does not do any optimizations for running the task.
- * For example, such a task will always be out of date and never stored in or loaded from the build cache.
- * That means that there is no need to specify the normalization for untracked file inputs.
- *
- * <p>{@link org.gradle.work.InputChanges} cannot be used for a task which has untracked properties,
+ * <p>{@link org.gradle.work.InputChanges} cannot be used for untracked tasks,
  * since incremental tasks need to track the state of their inputs and outputs for them to be correct.
- *
- * <p>This annotation can be attached to properties annotated with {@link InputFile}, {@link InputFiles}, {@link InputDirectory},
- * {@link OutputFile}, {@link OutputFiles}, {@link OutputDirectory}, or {@link OutputDirectories}.
  *
  * @since 7.3
  */
@@ -52,5 +48,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface UntrackedTask {
+    /**
+     * Mandatory reason why the task is untracked.
+     */
     String because();
 }
