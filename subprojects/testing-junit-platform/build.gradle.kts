46da6389a5ab2a43544e1e6cb26611c72351262a
Thomas Tresansky <ttresansky@gradle.com>
2021-10-25 10:30:23 -0400

    Add description to testing-junit-platform subproject.

--- subprojects/testing-junit-platform/build.gradle.kts
+++ subprojects/testing-junit-platform/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("gradlebuild.distribution.api-java")
 }
 
+
+description = """Support class used to integrate running tests with the JUnit Jupiter testing framework into a Gradle build.
+
+This project should NOT be used as an implementation dependency anywhere."""
+
 dependencies {
     implementation(project(":base-services"))
     implementation(project(":messaging"))
