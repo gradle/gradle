be713c546b50f8a361f0cba020d43d997d9ae891
Bo Zhang <bo@gradle.com>
2021-10-25 16:57:02 +0800

    Rebasline all performance tests on release
    Because many of them are already out-of-date and will be cleaned
    soon.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/nativeplatform/NativeBuildPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/nativeplatform/NativeBuildPerformanceTest.groovy
@@ -33,7 +33,7 @@ import static org.gradle.performance.results.OperatingSystem.LINUX
 class NativeBuildPerformanceTest extends AbstractCrossVersionPerformanceTest {
     def setup() {
         runner.minimumBaseVersion = '4.1' // minimum version that contains new C++ plugins
-        runner.targetVersions = ["7.2-20210720234250+0000"]
+        runner.targetVersions = ["7.3-20211022000247+0000"]
     }
 
     def "up-to-date assemble (native)"() {
