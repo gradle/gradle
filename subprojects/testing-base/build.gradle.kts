b4f83bd3e52d43ec409c5882a45b60549a25427f
Thomas Tresansky <ttresansky@gradle.com>
2021-10-25 10:29:29 -0400

    Add description to testing-base subproject.

--- subprojects/testing-base/build.gradle.kts
+++ subprojects/testing-base/build.gradle.kts
@@ -4,6 +4,13 @@ plugins {
 
 gradlebuildJava.usedInWorkers()
 
+description = """Basic testing related plugins, which establish conventions for testing output directories,
+and setup basic testing-related features lik a testSuites container and the testing extension.  It provides most of the
+testing-related abstract base types and interfaces for things like Test tasks, listeners and filters.
+
+This project is a implementation dependency of many other testing-related subprojects in the Gradle build.
+"""
+
 dependencies {
     implementation(project(":base-services"))
     implementation(project(":messaging"))
