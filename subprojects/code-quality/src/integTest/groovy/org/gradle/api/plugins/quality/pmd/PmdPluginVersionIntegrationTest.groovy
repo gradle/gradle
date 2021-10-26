6444788f86bb651fe927acf415aced47a9c8150c
Adam Murdoch <adam@gradle.com>
2021-10-27 10:32:36 +1100

    Fix a potential deadlock in the PMD tasks.
    Ensure that when iterating over a `Configuration` whose artifact have already been resolved that the calling thread does not release and reacquire its project lock.

--- subprojects/code-quality/src/integTest/groovy/org/gradle/api/plugins/quality/pmd/PmdPluginVersionIntegrationTest.groovy
+++ subprojects/code-quality/src/integTest/groovy/org/gradle/api/plugins/quality/pmd/PmdPluginVersionIntegrationTest.groovy
@@ -15,10 +15,9 @@
  */
 package org.gradle.api.plugins.quality.pmd
 
-import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
+
 import org.gradle.util.internal.VersionNumber
 import org.hamcrest.Matcher
-import spock.lang.IgnoreIf
 import spock.lang.Issue
 
 import static org.gradle.util.Matchers.containsLine
@@ -26,8 +25,6 @@ import static org.hamcrest.CoreMatchers.containsString
 import static org.hamcrest.CoreMatchers.not
 import static org.junit.Assume.assumeTrue
 
-// https://github.com/gradle/gradle-private/issues/3465
-@IgnoreIf({ GradleContextualExecuter.isParallel() })
 class PmdPluginVersionIntegrationTest extends AbstractPmdPluginVersionIntegrationTest {
 
     def setup() {
@@ -145,12 +142,12 @@ class PmdPluginVersionIntegrationTest extends AbstractPmdPluginVersionIntegratio
         }
 """
         expect:
-        fails("check")
+        // Use --continue so that when executing in parallel mode a deterministic set of tests run
+        // without --continue, sometimes both pmd tasks are run and sometimes only the only one task is run
+        fails("check", "--continue")
         failure.assertHasCause("Invalid rulesMinimumPriority '11'.  Valid range 1 (highest) to 5 (lowest).")
-        if (GradleContextualExecuter.isParallel()) {
-            // pmdMain and pmdTest
-            failure.assertHasFailures(2)
-        }
+        // pmdMain and pmdTest
+        failure.assertHasFailures(2)
     }
 
     def "gets reasonable message when priority level threshold is out of range from task"() {
