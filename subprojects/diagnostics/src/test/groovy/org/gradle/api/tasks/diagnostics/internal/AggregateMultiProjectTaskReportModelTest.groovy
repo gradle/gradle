5d518ef1b47b9b17f6b594cbd32b559f9b901335
Thomas Tresansky <ttresansky@gradle.com>
2021-10-29 09:27:32 -0400

    style: Attempt commit unneeded import.

--- subprojects/diagnostics/src/test/groovy/org/gradle/api/tasks/diagnostics/internal/AggregateMultiProjectTaskReportModelTest.groovy
+++ subprojects/diagnostics/src/test/groovy/org/gradle/api/tasks/diagnostics/internal/AggregateMultiProjectTaskReportModelTest.groovy
@@ -15,7 +15,6 @@
  */
 package org.gradle.api.tasks.diagnostics.internal
 
-import org.gradle.api.Project
 import org.gradle.api.Task
 
 class AggregateMultiProjectTaskReportModelTest extends AbstractTaskModelSpec {
