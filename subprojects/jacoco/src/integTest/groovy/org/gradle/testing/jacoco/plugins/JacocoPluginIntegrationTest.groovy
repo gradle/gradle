af4c115635d59c686f2da8c16fa96a3e5c48727a
Thomas Tresansky <ttresansky@gradle.com>
2021-11-01 09:37:07 -0400

    Add tests for resolving jacoco data via configs.

--- subprojects/jacoco/src/integTest/groovy/org/gradle/testing/jacoco/plugins/JacocoPluginIntegrationTest.groovy
+++ subprojects/jacoco/src/integTest/groovy/org/gradle/testing/jacoco/plugins/JacocoPluginIntegrationTest.groovy
@@ -204,4 +204,99 @@ Artifacts
     - build/jacoco/integrationTest.exec (artifactType = exec)
 """)
     }
+
+    def "Jacoco coverage data can be consumed by another task via Dependency Management"() {
+        buildFile << """
+test {
+    jacoco {
+        destinationFile = layout.buildDirectory.file("jacoco/jacocoData.exec").get().asFile
+    }
+}
+"""
+
+        buildFile << """
+// A resolvable configuration to collect JaCoCo coverage data
+def coverageDataConfig = configurations.create("coverageData") {
+    visible = true
+    canBeResolved = true
+    canBeConsumed = false
+    attributes {
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage, Usage.VERIFICATION))
+        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category, Category.DOCUMENTATION))
+        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType, DocsType.JACOCO_COVERAGE))
+    }
+}
+
+dependencies {
+    coverageData project
+}
+
+def testResolve = tasks.register('testResolve') {
+    doLast {
+        assert coverageDataConfig.getResolvedConfiguration().getFiles()*.getName() == ["jacocoData.exec"]
+    }
+}
+
+"""
+        expect:
+        succeeds('test', 'testResolve')
+    }
+
+    def "Jacoco coverage data can be consumed by another task in a different project via Dependency Management"() {
+        def subADir = createDir("subA")
+        final JavaProjectUnderTest subA = new JavaProjectUnderTest(subADir)
+        subA.writeBuildScript()
+        def buildFileA = subADir.file("build.gradle") << """
+test {
+    jacoco {
+        destinationFile = layout.buildDirectory.file("jacoco/subA.exec").get().asFile
+    }
+}
+"""
+
+        def subBDir = createDir("subB")
+        final JavaProjectUnderTest subB = new JavaProjectUnderTest(subBDir)
+        subB.writeBuildScript()
+        def buildFileB = subBDir.file("build.gradle") << """
+test {
+    jacoco {
+        destinationFile = layout.buildDirectory.file("jacoco/subB.exec").get().asFile
+    }
+}
+"""
+
+        settingsFile << """
+include ':subA'
+include ':subB'
+"""
+
+        buildFile << """
+dependencies {
+    implementation project(':subA')
+    implementation project(':subB')
+}
+
+// A resolvable configuration to collect JaCoCo coverage data
+def coverageDataConfig = configurations.create("coverageData") {
+    visible = false
+    canBeResolved = true
+    canBeConsumed = false
+    extendsFrom(configurations.implementation)
+    attributes {
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage, Usage.VERIFICATION))
+        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category, Category.DOCUMENTATION))
+        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType, DocsType.JACOCO_COVERAGE))
+    }
+}
+
+def testResolve = tasks.register('testResolve') {
+    doLast {
+        assert coverageDataConfig.getResolvedConfiguration().getFiles()*.getName() == ["subA.exec", "subB.exec"]
+    }
+}
+
+"""
+        expect:
+        succeeds('testResolve')
+    }
 }
