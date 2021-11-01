d64106e5b3688df5ac3c6f68106e8b951c41880f
Thomas Tresansky <ttresansky@gradle.com>
2021-11-01 14:39:07 -0400

    test: Finish tests for the binary test data variant.

--- subprojects/plugins/src/integTest/groovy/org/gradle/api/plugins/JvmTestSuitePluginIntegrationTest.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/api/plugins/JvmTestSuitePluginIntegrationTest.groovy
@@ -22,187 +22,242 @@ class JvmTestSuitePluginIntegrationTest extends AbstractIntegrationSpec {
 
     def "JVM Test Suites plugin adds outgoing variants for default test suite"() {
         buildFile << """
-plugins {
-    id 'jvm-test-suite'
-    id 'java'
-}
-"""
+            plugins {
+                id 'jvm-test-suite'
+                id 'java'
+            }
+            """
 
         file("src/test/java/SomeTest.java") << """
-import org.junit.Test;
+            import org.junit.Test;
 
-public class SomeTest {
-    @Test public void foo() {}
-}
-"""
+            public class SomeTest {
+                @Test public void foo() {}
+            }
+            """.stripIndent()
 
         expect:
         succeeds "outgoingVariants"
 
         outputContains("""
---------------------------------------------------
-Variant testDataElementsForTest
---------------------------------------------------
-Capabilities
-    - :${getTestDirectory().getName()}:unspecified (default capability)
-Attributes
-    - org.gradle.category      = documentation
-    - org.gradle.docstype      = test-results-bin
-    - org.gradle.targetname    = test
-    - org.gradle.testsuitename = test
-    - org.gradle.testsuitetype = unit-tests
-    - org.gradle.usage         = verification
+            --------------------------------------------------
+            Variant testDataElementsForTest
+            --------------------------------------------------
+            Capabilities
+                - :${getTestDirectory().getName()}:unspecified (default capability)
+            Attributes
+                - org.gradle.category      = documentation
+                - org.gradle.docstype      = test-results-bin
+                - org.gradle.targetname    = test
+                - org.gradle.testsuitename = test
+                - org.gradle.testsuitetype = unit-tests
+                - org.gradle.usage         = verification
 
-Artifacts
-    - build/test-results/test/binary/results.bin (artifactType = binary)
-""")
+            Artifacts
+                - build/test-results/test/binary/results.bin (artifactType = binary)
+            """.stripIndent())
     }
 
-    def "JVM Test Suites plugin outgoing variants for custom test suite"() {
+    def "JVM Test Suites plugin adds outgoing variants for custom test suite"() {
         buildFile << """
-plugins {
-    id 'jvm-test-suite'
-    id 'java'
-}
+            plugins {
+                id 'jvm-test-suite'
+                id 'java'
+            }
 
-testing {
-    suites {
-        integrationTest(JvmTestSuite) {
-            testType = TestType.INTEGRATION_TESTS
+            testing {
+                suites {
+                    integrationTest(JvmTestSuite) {
+                        testType = TestType.INTEGRATION_TESTS
+
+                        dependencies {
+                            implementation project
+                        }
+                    }
+                }
+            }
+            """.stripIndent()
+
+        expect:
+        succeeds "outgoingVariants"
+
+        outputContains("""
+            --------------------------------------------------
+            Variant testDataElementsForIntegrationTest
+            --------------------------------------------------
+            Capabilities
+                - :${getTestDirectory().getName()}:unspecified (default capability)
+            Attributes
+                - org.gradle.category      = documentation
+                - org.gradle.docstype      = test-results-bin
+                - org.gradle.targetname    = integrationTest
+                - org.gradle.testsuitename = integrationTest
+                - org.gradle.testsuitetype = integration-tests
+                - org.gradle.usage         = verification
+
+            Artifacts
+                - build/test-results/integrationTest/binary/results.bin (artifactType = binary)
+            """.stripIndent())
+    }
+
+    def "Test coverage data can be consumed by another task via Dependency Management"() {
+        buildFile << """
+            plugins {
+                id 'jvm-test-suite'
+                id 'java'
+            }
+
+            repositories {
+                ${mavenCentralRepository()}
+            }
+
+            testing {
+                suites {
+                    test {
+                        useJUnit()
+                        dependencies {
+                            implementation project
+                        }
+                    }
+                }
+            }
+            """.stripIndent()
+
+        file("src/test/java/SomeTest.java") << """
+            import org.junit.Test;
+
+            public class SomeTest {
+                @Test public void foo() {}
+            }
+            """.stripIndent()
+
+        buildFile << """
+            // A resolvable configuration to collect JaCoCo coverage data
+            def testDataConfig = configurations.create("testData") {
+                visible = false
+                canBeResolved = true
+                canBeConsumed = false
+                attributes {
+                    attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage, Usage.VERIFICATION))
+                    attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category, Category.DOCUMENTATION))
+                    attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType, DocsType.TEST_RESULTS))
+                }
+            }
 
             dependencies {
-                implementation project
+                testData project
             }
-        }
-    }
-}
-"""
 
-        file("src/integrationTest/java/SomeTest.java") << """
-import org.junit.Test;
+            def testResolve = tasks.register('testResolve') {
+                doLast {
+                    assert testDataConfig.getResolvedConfiguration().getFiles()*.getName() == [test.binaryResultsDirectory.file("results.bin").get().getAsFile().getName()]
+                }
+            }""".stripIndent()
 
-public class SomeTest {
-    @Test public void foo() {}
-}
-"""
-
-        expect:
-        succeeds "outgoingVariants"
-
-        outputContains("""
---------------------------------------------------
-Variant testDataElementsForIntegrationTest
---------------------------------------------------
-Capabilities
-    - :${getTestDirectory().getName()}:unspecified (default capability)
-Attributes
-    - org.gradle.category      = documentation
-    - org.gradle.docstype      = test-results-bin
-    - org.gradle.targetname    = integrationTest
-    - org.gradle.testsuitename = integrationTest
-    - org.gradle.testsuitetype = integration-tests
-    - org.gradle.usage         = verification
-
-Artifacts
-    - build/test-results/integrationTest/binary/results.bin (artifactType = binary)
-""")
-    }
-
-    /*
-    def "Jacoco coverage data can be consumed by another task via Dependency Management"() {
-        buildFile << """
-test {
-    jacoco {
-        destinationFile = layout.buildDirectory.file("jacoco/jacocoData.exec").get().asFile
-    }
-}
-"""
-
-        buildFile << """
-// A resolvable configuration to collect JaCoCo coverage data
-def coverageDataConfig = configurations.create("coverageData") {
-    visible = true
-    canBeResolved = true
-    canBeConsumed = false
-    attributes {
-        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage, Usage.VERIFICATION))
-        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category, Category.DOCUMENTATION))
-        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType, DocsType.JACOCO_COVERAGE))
-    }
-}
-
-dependencies {
-    coverageData project
-}
-
-def testResolve = tasks.register('testResolve') {
-    doLast {
-        assert coverageDataConfig.getResolvedConfiguration().getFiles()*.getName() == ["jacocoData.exec"]
-    }
-}
-
-"""
-        expect:
-        succeeds('test', 'testResolve')
-    }
-
-    def "Jacoco coverage data can be consumed by another task in a different project via Dependency Management"() {
-        def subADir = createDir("subA")
-        final JavaProjectUnderTest subA = new JavaProjectUnderTest(subADir)
-        subA.writeBuildScript()
-        def buildFileA = subADir.file("build.gradle") << """
-test {
-    jacoco {
-        destinationFile = layout.buildDirectory.file("jacoco/subA.exec").get().asFile
-    }
-}
-"""
-
-        def subBDir = createDir("subB")
-        final JavaProjectUnderTest subB = new JavaProjectUnderTest(subBDir)
-        subB.writeBuildScript()
-        def buildFileB = subBDir.file("build.gradle") << """
-test {
-    jacoco {
-        destinationFile = layout.buildDirectory.file("jacoco/subB.exec").get().asFile
-    }
-}
-"""
-
-        settingsFile << """
-include ':subA'
-include ':subB'
-"""
-
-        buildFile << """
-dependencies {
-    implementation project(':subA')
-    implementation project(':subB')
-}
-
-// A resolvable configuration to collect JaCoCo coverage data
-def coverageDataConfig = configurations.create("coverageData") {
-    visible = false
-    canBeResolved = true
-    canBeConsumed = false
-    extendsFrom(configurations.implementation)
-    attributes {
-        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage, Usage.VERIFICATION))
-        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category, Category.DOCUMENTATION))
-        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType, DocsType.JACOCO_COVERAGE))
-    }
-}
-
-def testResolve = tasks.register('testResolve') {
-    doLast {
-        assert coverageDataConfig.getResolvedConfiguration().getFiles()*.getName() == ["subA.exec", "subB.exec"]
-    }
-}
-
-"""
         expect:
         succeeds('testResolve')
     }
 
- */
+    def "Test results data can be consumed by another task in a different project via Dependency Management"() {
+        def subADir = createDir("subA")
+        def buildFileA = subADir.file("build.gradle") << """
+            plugins {
+                id 'jvm-test-suite'
+                id 'java'
+            }
+
+            repositories {
+                ${mavenCentralRepository()}
+            }
+
+            testing {
+                suites {
+                    test {
+                        useJUnit()
+                        dependencies {
+                            implementation project
+                        }
+                    }
+                }
+            }
+            """.stripIndent()
+
+        file("src/test/java/SomeTestA.java") << """
+            import org.junit.Test;
+
+            public class SomeTestA {
+                @Test public void foo() {}
+            }
+            """.stripIndent()
+
+        def subBDir = createDir("subB")
+        def buildFileB = subBDir.file("build.gradle") << """
+            plugins {
+                id 'jvm-test-suite'
+                id 'java'
+            }
+
+            repositories {
+                ${mavenCentralRepository()}
+            }
+
+            testing {
+                suites {
+                    test {
+                        useJUnit()
+                        dependencies {
+                            implementation project
+                        }
+                    }
+                }
+            }
+            """.stripIndent()
+
+        file("src/test/java/SomeTestB.java") << """
+            import org.junit.Test;
+
+            public class SomeTestB {
+                @Test public void foo() {}
+            }
+            """.stripIndent()
+
+        settingsFile << """
+            include ':subA'
+            include ':subB'
+            """.stripIndent()
+
+        buildFile << """
+            plugins {
+                id 'java'
+            }
+
+            dependencies {
+                implementation project(':subA')
+                implementation project(':subB')
+            }
+
+            // A resolvable configuration to collect JaCoCo coverage data
+            def testDataConfig = configurations.create("testData") {
+                visible = false
+                canBeResolved = true
+                canBeConsumed = false
+                extendsFrom(configurations.implementation)
+                attributes {
+                    attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage, Usage.VERIFICATION))
+                    attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category, Category.DOCUMENTATION))
+                    attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType, DocsType.TEST_RESULTS))
+                }
+            }
+
+            def testResolve = tasks.register('testResolve') {
+                doLast {
+                    assert testDataConfig.getResolvedConfiguration().getFiles()*.getName() == [project(':subA').tasks["test"].binaryResultsDirectory.file("results.bin").get().getAsFile().getName(),
+                                                                                               project(':subB').tasks["test"].binaryResultsDirectory.file("results.bin").get().getAsFile().getName()]
+                }
+            }
+
+            """
+        expect:
+        succeeds('testResolve')
+    }
 }
