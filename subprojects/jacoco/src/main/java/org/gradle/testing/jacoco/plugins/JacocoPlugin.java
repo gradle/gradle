d60532d045a2fd9ac743bc71bcbfbe021cb09fae
Thomas Tresansky <ttresansky@gradle.com>
2021-11-01 09:33:04 -0400

    Workaround until attributes are lazily configurable.

--- subprojects/jacoco/src/main/java/org/gradle/testing/jacoco/plugins/JacocoPlugin.java
+++ subprojects/jacoco/src/main/java/org/gradle/testing/jacoco/plugins/JacocoPlugin.java
@@ -130,8 +130,17 @@ public class JacocoPlugin implements Plugin<Project> {
             attributes.attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType.class, DocsType.JACOCO_COVERAGE));
             attributes.attribute(Verification.TEST_SUITE_NAME_ATTRIBUTE, objects.named(Verification.class, suite.getName()));
             attributes.attribute(Verification.TARGET_NAME_ATTRIBUTE, objects.named(Verification.class, suite.getName()));
-            attributes.attribute(TestType.TEST_TYPE_ATTRIBUTE, objects.named(TestType.class, suite.getTestType().get()));
         });
+
+        // TODO: 18791 Probably need to make attributes lazily configurable, since at this configuration time, the .get() returns
+        // the convention value of "unit-test" and doesn't wait until the Test Suite is configured to contain "integration-test".
+        // As a workaround for now, only add this attribute after project is fully evaluated.
+        project.afterEvaluate(p -> {
+            variant.attributes(attributes -> {
+                attributes.attribute(TestType.TEST_TYPE_ATTRIBUTE, objects.named(TestType.class, suite.getTestType().get()));
+            });
+        });
+
 
         variant.getOutgoing().artifact(target.getTestTask().map(task -> task.getExtensions().getByType(JacocoTaskExtension.class).getDestinationFile()));
         return variant;
