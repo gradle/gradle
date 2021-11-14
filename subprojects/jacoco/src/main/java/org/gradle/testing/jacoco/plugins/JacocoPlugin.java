d3a72d5d91886493ec138af3da85d13bca062608
Thomas Tresansky <ttresansky@gradle.com>
2021-11-13 20:48:34 -0500

    fix: Simplify jacoco output artifact mapping.

--- subprojects/jacoco/src/main/java/org/gradle/testing/jacoco/plugins/JacocoPlugin.java
+++ subprojects/jacoco/src/main/java/org/gradle/testing/jacoco/plugins/JacocoPlugin.java
@@ -132,10 +132,10 @@ public class JacocoPlugin implements Plugin<Project> {
             attributes.attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType.class, DocsType.JACOCO_COVERAGE));
             attributes.attribute(Verification.TEST_SUITE_NAME_ATTRIBUTE, objects.named(Verification.class, suite.getName()));
             attributes.attribute(Verification.TARGET_NAME_ATTRIBUTE, objects.named(Verification.class, suite.getName()));
-            attributes.attribute(TestType.TEST_TYPE_ATTRIBUTE, suite.getTestType().flatMap(tt -> project.provider(() -> objects.named(TestType.class, tt))));
+            attributes.attribute(TestType.TEST_TYPE_ATTRIBUTE, suite.getTestType().map(tt -> objects.named(TestType.class, tt)));
         });
 
-        variant.getOutgoing().artifact(target.getTestTask().flatMap(task -> project.provider(() -> task.getExtensions().getByType(JacocoTaskExtension.class).getDestinationFile())), artifact -> {
+        variant.getOutgoing().artifact(target.getTestTask().map(task -> task.getExtensions().getByType(JacocoTaskExtension.class).getDestinationFile()), artifact -> {
             artifact.setType(ArtifactTypeDefinition.BINARY_DATA_TYPE);
             artifact.builtBy(target.getTestTask());
         });
