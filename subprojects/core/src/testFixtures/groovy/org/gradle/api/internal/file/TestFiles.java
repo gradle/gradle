ad33c9fa2289cb761dd0d637d64953abde92682a
Stefan Wolf <wolf@gradle.com>
2021-11-19 18:47:44 +0100

    Remove GenericFileTreeSnapshotter

--- subprojects/core/src/testFixtures/groovy/org/gradle/api/internal/file/TestFiles.java
+++ subprojects/core/src/testFixtures/groovy/org/gradle/api/internal/file/TestFiles.java
@@ -33,9 +33,7 @@ import org.gradle.internal.concurrent.DefaultExecutorFactory;
 import org.gradle.internal.file.Deleter;
 import org.gradle.internal.file.PathToFileResolver;
 import org.gradle.internal.file.impl.DefaultDeleter;
-import org.gradle.internal.fingerprint.GenericFileTreeSnapshotter;
 import org.gradle.internal.fingerprint.impl.DefaultFileCollectionSnapshotter;
-import org.gradle.internal.fingerprint.impl.DefaultGenericFileTreeSnapshotter;
 import org.gradle.internal.hash.DefaultFileHasher;
 import org.gradle.internal.hash.DefaultStreamHasher;
 import org.gradle.internal.nativeintegration.filesystem.FileSystem;
@@ -190,10 +188,6 @@ public class TestFiles {
 
     public static DefaultFileHasher fileHasher() {
         return new DefaultFileHasher(streamHasher());
-    }
-
-    public static GenericFileTreeSnapshotter genericFileTreeSnapshotter() {
-        return new DefaultGenericFileTreeSnapshotter(fileHasher(), new StringInterner());
     }
 
     public static DefaultFileCollectionSnapshotter fileCollectionSnapshotter() {
