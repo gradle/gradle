fbb9cdfffa8f69d360f234ea2f89f72def7f960c
Thomas Tresansky <ttresansky@gradle.com>
2021-12-02 08:13:19 -0500

    Revert "test: Add disambiguating attributes to make tests pass."
    This reverts commit 1064c3b2555bd7afc28b0d99b312a1a79256cb5a.

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformIntegrationTest.groovy
@@ -2271,17 +2271,10 @@ Found the following transforms:
             }
             configurations {
                 compile1 {
-                    attributes {
-                        attribute usage, 'api'
-                        attribute Attribute.of('disambiguator', String), '1' // Ensure unique set of attributes
-                    }
+                    attributes { attribute usage, 'api' }
                 }
                 compile2 {
-
-                    attributes {
-                        attribute usage, 'api'
-                        attribute Attribute.of('disambiguator', String), '2' // Ensure unique set of attributes
-                    }
+                    attributes { attribute usage, 'api' }
                 }
             }
             dependencies {
