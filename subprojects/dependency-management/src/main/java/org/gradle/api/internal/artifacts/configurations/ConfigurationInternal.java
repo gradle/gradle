fcea246f30ebc12b80443860302cea8afdfa3ca4
Thomas Tresansky <ttresansky@gradle.com>
2021-12-07 15:42:25 -0500

    docs: Add code link to docs for clarity.

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/ConfigurationInternal.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/ConfigurationInternal.java
@@ -85,7 +85,7 @@ public interface ConfigurationInternal extends ResolveContext, Configuration, De
     void preventFromFurtherMutation();
 
     /**
-     * Reports whether this configuration uses @Incubating attributes types, such as {@link org.gradle.api.attributes.Category#VERIFICATION}.
+     * Reports whether this configuration uses {@link org.gradle.api.Incubating Incubating} attributes types, such as {@link org.gradle.api.attributes.Category#VERIFICATION}.
      * @return
      */
     boolean isIncubating();
