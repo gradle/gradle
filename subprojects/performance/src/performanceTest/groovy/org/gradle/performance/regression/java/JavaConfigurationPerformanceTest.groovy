453d037cf0f1ef1184827b7baa46f5406f4f35a1
Sterling Greene <sterling@gradle.com>
2021-12-09 15:35:50 -0500

    Re-enable disabled performance tests

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaConfigurationPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaConfigurationPerformanceTest.groovy
@@ -19,7 +19,6 @@ package org.gradle.performance.regression.java
 import org.gradle.performance.AbstractCrossVersionPerformanceTest
 import org.gradle.performance.annotations.RunFor
 import org.gradle.performance.annotations.Scenario
-import spock.lang.Ignore
 
 import static org.gradle.performance.annotations.ScenarioType.PER_COMMIT
 import static org.gradle.performance.results.OperatingSystem.LINUX
@@ -29,7 +28,6 @@ import static org.gradle.performance.results.OperatingSystem.LINUX
 )
 class JavaConfigurationPerformanceTest extends AbstractCrossVersionPerformanceTest {
 
-    @Ignore
     def "configure"() {
         given:
         runner.tasksToRun = ['help']
--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaIDEModelPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaIDEModelPerformanceTest.groovy
@@ -22,7 +22,6 @@ import org.gradle.performance.annotations.Scenario
 import org.gradle.tooling.model.ExternalDependency
 import org.gradle.tooling.model.eclipse.EclipseProject
 import org.gradle.tooling.model.idea.IdeaProject
-import spock.lang.Ignore
 
 import static org.gradle.performance.annotations.ScenarioType.PER_COMMIT
 import static org.gradle.performance.generator.JavaTestProjectGenerator.LARGE_MONOLITHIC_JAVA_PROJECT
@@ -38,7 +37,6 @@ class JavaIDEModelPerformanceTest extends AbstractCrossVersionPerformanceTest {
         runner.minimumBaseVersion = "2.11"
     }
 
-    @Ignore
     def "get IDE model for Eclipse"() {
         given:
         setupRunner()
@@ -91,7 +89,6 @@ class JavaIDEModelPerformanceTest extends AbstractCrossVersionPerformanceTest {
         result.assertCurrentVersionHasNotRegressed()
     }
 
-    @Ignore
     def "get IDE model for IDEA"() {
         given:
         setupRunner()
