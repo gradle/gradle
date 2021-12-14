a3d8fdd4fe779aaa35b663813506f0d64bac9f26
Paul Merlin <paul@gradle.com>
2021-12-14 16:14:42 +0100

    Prefer the latest JNA
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/distributions-dependencies/build.gradle.kts
+++ subprojects/distributions-dependencies/build.gradle.kts
@@ -130,7 +130,7 @@ dependencies {
         api(libs.testng)                { version { strictly("6.3.1"); because("later versions break test cross-version test filtering") }}
         api(libs.tomlj)                 { version { strictly(tomljVersion) }}
         api(libs.trove4j)               { version { strictly("1.0.20181211") }}
-        api(libs.jna)                   { version { strictly("5.6.0") }}
+        api(libs.jna)                   { version { strictly("5.10.0") }}
         // TODO upgrade this AGP version to recent version
         api(libs.agp)                   { version { strictly("3.0.0"); because("We use 3.0.0 for internal performance test") }}
         api(libs.xbeanReflect)          { version { strictly("3.7") }}
