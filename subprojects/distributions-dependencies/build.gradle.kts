c0fb0090e12f4973a2f0adce1224cd8d6bff360b
Stefan Wolf <wolf@gradle.com>
2021-12-17 18:57:05 +0100

    Upgrade to Gradle Profiler 0.17-alpha11

--- subprojects/distributions-dependencies/build.gradle.kts
+++ subprojects/distributions-dependencies/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
         api(libs.googleHttpClient)      { version { strictly(googleApiVersion); because("our GCS version requires 1.25.0") }}
         api(libs.googleHttpClientJackson2) { version { strictly(googleApiVersion); because("our GCS version requires 1.25.0") }}
         api(libs.googleOauthClient)     { version { strictly(googleApiVersion); because("our GCS version requires 1.25.0") }}
-        api(libs.gradleProfiler)        { version { strictly("0.17.0-alpha10") }}
+        api(libs.gradleProfiler)        { version { strictly("0.17.0-alpha11") }}
         api(libs.groovy)                { version { strictly(libs.groovyVersion) }}
         api(libs.groovyAnt)             { version { strictly(libs.groovyVersion) }}
         api(libs.groovyAstbuilder)      { version { strictly(libs.groovyVersion) }}
