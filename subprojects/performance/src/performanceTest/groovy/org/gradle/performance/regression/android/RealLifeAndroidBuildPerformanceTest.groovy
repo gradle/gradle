c27459ec57a7c4fcdf5ea5ee47a3937e8052a903
Bo Zhang <bo@gradle.com>
2021-12-28 18:03:36 +0800

    Rebaseline failing performance tests
    Because they're tiny and flaky.

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/android/RealLifeAndroidBuildPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/android/RealLifeAndroidBuildPerformanceTest.groovy
@@ -33,7 +33,7 @@ class RealLifeAndroidBuildPerformanceTest extends AbstractCrossVersionPerformanc
 
     def setup() {
         runner.args = [AndroidGradlePluginVersions.OVERRIDE_VERSION_CHECK]
-        runner.targetVersions = ["7.4-20211214223504+0000"]
+        runner.targetVersions = ["7.5-20211227231450+0000"]
         AndroidTestProject.useStableAgpVersion(runner)
         // AGP 4.1 requires 6.5+
         // forUseAtConfigurationTime API used in this scenario
