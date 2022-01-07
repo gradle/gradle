f77536e407a0a3a1d26eea3d68dbe29d09002060
Sterling Greene <sterling@gradle.com>
2022-01-07 14:22:00 -0500

    Reduce complexity in MavenResolver
     - Remove outdated tests as MavenResolver no longer returns FixedComponentArtifacts

--- subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/repositories/resolver/MavenResolverTest.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/repositories/resolver/MavenResolverTest.groovy
@@ -32,9 +32,7 @@ import org.gradle.internal.action.ConfigurableRule
 import org.gradle.internal.action.DefaultConfigurableRules
 import org.gradle.internal.action.InstantiatingAction
 import org.gradle.internal.component.external.model.DefaultModuleComponentIdentifier
-import org.gradle.internal.component.external.model.FixedComponentArtifacts
 import org.gradle.internal.component.external.model.MetadataSourcedComponentArtifacts
-import org.gradle.internal.component.external.model.ModuleComponentArtifactMetadata
 import org.gradle.internal.component.external.model.ModuleComponentResolveMetadata
 import org.gradle.internal.component.external.model.MutableModuleComponentResolveMetadata
 import org.gradle.internal.component.external.model.maven.MavenModuleResolveMetadata
@@ -72,40 +70,6 @@ class MavenResolverTest extends Specification {
         then:
         1 * result.resolved(_) >> { args ->
             assert args[0] instanceof MetadataSourcedComponentArtifacts
-        }
-    }
-
-    def "resolve to empty when module is relocated"() {
-        given:
-        module.variants >> ImmutableList.of()
-        module.relocated >> true
-        variant.requiresMavenArtifactDiscovery() >> true
-
-        when:
-        resolver.getLocalAccess().resolveModuleArtifacts(module, variant, result)
-
-        then:
-        1 * result.resolved(_) >> { args ->
-            assert args[0] instanceof FixedComponentArtifacts
-            assert args[0].artifacts.isEmpty()
-        }
-    }
-
-    def "resolve artifact when module's packaging is jar"() {
-        given:
-        module.variants >> ImmutableList.of()
-        module.knownJarPackaging >> true
-        ModuleComponentArtifactMetadata artifact = Mock(ModuleComponentArtifactMetadata)
-        module.artifact('jar', 'jar', null) >> artifact
-        variant.requiresMavenArtifactDiscovery() >> true
-
-        when:
-        resolver.getLocalAccess().resolveModuleArtifacts(module, variant, result)
-
-        then:
-        1 * result.resolved(_) >> { args ->
-            assert args[0] instanceof FixedComponentArtifacts
-            assert args[0].artifacts == [artifact]
         }
     }
 
