93ea0c25a5db375c1fef79f4825daa1f56730e41
Anže Sodja <asodja@gradle.com>
2022-01-11 11:16:48 +0100

    Add test for default methods being discovered

--- subprojects/base-services/build.gradle.kts
+++ subprojects/base-services/build.gradle.kts
@@ -7,6 +7,15 @@ description = "A set of generic services and utilities."
 
 gradlebuildJava.usedInWorkers()
 
+/**
+ * Use Java 8 compatibility for Unit tests, so we can test Java 8 features as well
+ */
+tasks.named<JavaCompile>("compileTestJava") {
+    options.release.set(null)
+    sourceCompatibility = "8"
+    targetCompatibility = "8"
+}
+
 moduleIdentity.createBuildReceipt()
 
 dependencies {
