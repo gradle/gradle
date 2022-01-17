61d8ca0bbfcc78b606dfef9be58736c051dee2e9
József Bartók <jbartok@gradle.com>
2022-01-17 09:59:33 +0200

    Detect archive format before attempting decompress

--- subprojects/core/src/main/java/org/gradle/api/internal/file/archive/TarFileTree.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/file/archive/TarFileTree.java
@@ -15,6 +15,8 @@
  */
 package org.gradle.api.internal.file.archive;
 
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.gradle.api.GradleException;
@@ -39,6 +41,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.apache.commons.compress.archivers.ArchiveStreamFactory.TAR;
 
 public class TarFileTree extends AbstractArchiveFileTree {
     private final Provider<File> tarFileProvider;
@@ -93,10 +97,9 @@ public class TarFileTree extends AbstractArchiveFileTree {
     }
 
     private void visitImpl(FileVisitor visitor, InputStream inputStream) throws IOException {
+        checkFormat(inputStream);
+
         AtomicBoolean stopFlag = new AtomicBoolean();
-
-        //todo: handle other archive types too?
-
         NoCloseTarArchiveInputStream tar = new NoCloseTarArchiveInputStream(inputStream);
         File expandedDir = getExpandedDir();
         ReadableResourceInternal resource = this.resource.get();
@@ -107,6 +110,18 @@ public class TarFileTree extends AbstractArchiveFileTree {
             } else {
                 visitor.visitFile(new DetailsImpl(resource, expandedDir, entry, tar, stopFlag, chmod));
             }
+        }
+    }
+
+    private void checkFormat(InputStream inputStream) throws IOException {
+        String format;
+        try {
+            format = ArchiveStreamFactory.detect(inputStream);
+        } catch (ArchiveException e) {
+            throw new IOException("Archive format detection failed", e);
+        }
+        if (!TAR.equals(format)) {
+            throw new IOException("Expected tar archive format but found " + format);
         }
     }
 
