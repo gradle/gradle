a4fb94163adf1ab4ddf2cd36f44f9966784575e4
Stefan Wolf <wolf@gradle.com>
2022-01-17 09:34:45 +0100

    Add a package cycle test

---
+++ build-logic/buildquality/src/main/kotlin/gradlebuild.arch-test.gradle.kts
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    `java-library`
+    `jvm-test-suite`
+    id("gradlebuild.dependency-modules")
+}
+
+testing {
+    suites {
+        create("archTest", JvmTestSuite::class) {
+            useJUnitJupiter()
+            dependencies {
+                implementation(project)
+                if (project.name != "gradle-kotlin-dsl-accessors" && project.name != "test") {
+                    implementation(project.dependencies.platform(project(":distributions-dependencies")))
+                    implementation(project(":internal-architecture-testing"))
+                }
+            }
+            targets {
+                all {
+                    val archTestClasses = configurations["archTestCompileClasspath"].incoming.artifactView {
+                        withVariantReselection()
+                        attributes {
+                            attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.LIBRARY))
+                            attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_API))
+                            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+                            attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements.CLASSES))
+                        }
+                    }
+                    testTask.configure {
+                        testClassesDirs += archTestClasses.files.filter { it.isDirectory }
+                    }
+                }
+            }
+        }
+    }
+}
