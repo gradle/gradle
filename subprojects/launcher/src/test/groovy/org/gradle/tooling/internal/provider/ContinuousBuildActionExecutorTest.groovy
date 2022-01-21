be1d039c82a3749d0027e2f3d92a40182142f6fe
Stefan Wolf <wolf@gradle.com>
2022-01-21 18:17:17 +0100

    Add unit test for ContinuousBuildActionExecutor

--- subprojects/launcher/src/test/groovy/org/gradle/tooling/internal/provider/ContinuousBuildActionExecutorTest.groovy
+++ subprojects/launcher/src/test/groovy/org/gradle/tooling/internal/provider/ContinuousBuildActionExecutorTest.groovy
@@ -20,32 +20,38 @@ import org.gradle.api.execution.internal.DefaultTaskInputsListeners
 import org.gradle.api.internal.StartParameterInternal
 import org.gradle.api.internal.TaskInternal
 import org.gradle.api.internal.file.TestFiles
+import org.gradle.deployment.internal.Deployment
+import org.gradle.deployment.internal.DeploymentInternal
 import org.gradle.deployment.internal.DeploymentRegistryInternal
 import org.gradle.initialization.BuildRequestMetaData
 import org.gradle.initialization.DefaultBuildCancellationToken
 import org.gradle.initialization.DefaultBuildRequestContext
+import org.gradle.initialization.DefaultContinuousExecutionGate
 import org.gradle.initialization.NoOpBuildEventConsumer
 import org.gradle.integtests.fixtures.RedirectStdIn
 import org.gradle.internal.buildevents.BuildStartedTime
+import org.gradle.internal.buildtree.BuildActionRunner
 import org.gradle.internal.event.DefaultListenerManager
-import org.gradle.internal.filewatch.FileSystemChangeWaiter
-import org.gradle.internal.filewatch.FileSystemChangeWaiterFactory
 import org.gradle.internal.invocation.BuildAction
 import org.gradle.internal.logging.text.TestStyledTextOutputFactory
+import org.gradle.internal.service.scopes.DefaultFileChangeListeners
 import org.gradle.internal.service.scopes.Scope
 import org.gradle.internal.service.scopes.Scopes
 import org.gradle.internal.session.BuildSessionActionExecutor
 import org.gradle.internal.session.BuildSessionContext
+import org.gradle.internal.snapshot.CaseSensitivity
 import org.gradle.internal.time.Time
+import org.gradle.internal.watch.registry.FileWatcherRegistry
 import org.gradle.test.fixtures.concurrent.ConcurrentSpec
 import org.gradle.util.internal.DisconnectableInputStream
-import spock.lang.Ignore
 import spock.lang.Timeout
+import spock.util.concurrent.PollingConditions
 
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
 
 @RedirectStdIn
-@Ignore("TODO wolfs: Fix the unit test")
 class ContinuousBuildActionExecutorTest extends ConcurrentSpec {
 
     def delegate = Mock(BuildSessionActionExecutor)
@@ -55,20 +61,32 @@ class ContinuousBuildActionExecutorTest extends ConcurrentSpec {
     def requestMetadata = Stub(BuildRequestMetaData)
     def requestContext = new DefaultBuildRequestContext(requestMetadata, cancellationToken, new NoOpBuildEventConsumer())
     def startParameter = new StartParameterInternal()
-    def waiterFactory = Mock(FileSystemChangeWaiterFactory)
-    def waiter = Mock(FileSystemChangeWaiter)
-    def listenerManager = new DefaultListenerManager(Scope.Global)
-    def inputsListeners = new DefaultTaskInputsListeners(listenerManager)
-    def deploymentRegistry = Mock(DeploymentRegistryInternal)
+    def globalListenerManager = new DefaultListenerManager(Scope.Global)
+    def userHomeListenerManager = globalListenerManager.createChild(Scopes.UserHome)
+    def inputsListeners = new DefaultTaskInputsListeners(globalListenerManager)
+    def changeListeners = new DefaultFileChangeListeners(userHomeListenerManager)
+    List<Deployment> deployments = []
+    def continuousExecutionGate = new DefaultContinuousExecutionGate()
+    def deploymentRegistry = Stub(DeploymentRegistryInternal) {
+        runningDeployments >> deployments
+        executionGate >> continuousExecutionGate
+    }
     def buildSessionContext = Mock(BuildSessionContext)
-    def executer = executer()
+    def textOutputFactory = new TestStyledTextOutputFactory()
+    def executorService = Executors.newCachedThreadPool()
 
-    private File file = new File('file')
+    def executer = executer()
+    Future<?> runningBuild
+
+    private File file = new File('file').absoluteFile
+    PollingConditions conditions = new PollingConditions(timeout: 60, initialDelay: 0, factor: 1.25)
 
     def setup() {
         action.startParameter >> startParameter
-        waiterFactory.createChangeWaiter(_, _, _) >> waiter
-        waiter.isWatching() >> true
+    }
+
+    def cleanup() {
+        executorService.shutdownNow()
     }
 
     def "uses underlying executer when continuous build is not enabled"() {
@@ -78,8 +96,6 @@ class ContinuousBuildActionExecutorTest extends ConcurrentSpec {
 
         then:
         1 * delegate.execute(action, buildSessionContext)
-        1 * deploymentRegistry.runningDeployments >> []
-        0 * waiterFactory._
     }
 
     def "allows exceptions to propagate for single builds"() {
@@ -103,35 +119,152 @@ class ContinuousBuildActionExecutorTest extends ConcurrentSpec {
 
         then:
         1 * delegate.execute(action, buildSessionContext)
-        1 * deploymentRegistry.runningDeployments >> []
-        0 * waiterFactory._
         System.in instanceof DisconnectableInputStream
         System.in.read() == -1
     }
 
-    def "waits for waiter"() {
+    def "can cancel the build"() {
         when:
-        continuousBuild()
-        1 * delegate.execute(action, buildSessionContext) >> {
-            declareInput(file)
-        }
-        executeBuild()
+        continuousBuildWithInputs()
+        runningBuild = startBuild()
 
         then:
-        1 * waiter.wait(_, _) >> {
-            cancellationToken.cancel()
+        conditions.eventually {
+            lastLogLine == "Waiting for changes to input files of tasks..."
         }
+
+        when:
+        cancellationToken.cancel()
+
+        then:
+        buildExits()
+        lastLogLine == "Build cancelled."
+    }
+
+    def "runs a new build on file change"() {
+        when:
+        continuousBuildWithInputs()
+        startBuild()
+
+        then:
+        conditions.eventually {
+            waitingForChanges()
+        }
+
+        when:
+        changeListeners.broadcastChange(FileWatcherRegistry.Type.MODIFIED, file.toPath())
+
+        then:
+        conditions.eventually {
+            rebuiltBecauseOfChange()
+        }
+
+        when:
+        cancellationToken.cancel()
+        then:
+        buildExits()
+    }
+
+    def "only triggers the build when the gatekeeper is open"() {
+        when:
+        buildWithDeployment()
+        def gatekeeper = continuousExecutionGate.createGateKeeper()
+        startBuild()
+
+        then:
+        conditions.eventually {
+            reloadableDeploymentDetected()
+        }
+
+        when:
+        changeListeners.broadcastChange(FileWatcherRegistry.Type.MODIFIED, file.toPath())
+        // Wait to make sure the build is not triggered
+        Thread.sleep(500)
+        then:
+        waitingForChanges()
+        !changeDetected()
+
+        when:
+        gatekeeper.open()
+        then:
+        conditions.eventually {
+            rebuiltBecauseOfChange()
+        }
+
+        when:
+        cancellationToken.cancel()
+        then:
+        buildExits()
+
+        cleanup:
+        println(textOutputFactory)
     }
 
     def "exits if there are no file system inputs"() {
         when:
         continuousBuild()
         1 * delegate.execute(action, buildSessionContext)
-        executeBuild()
-
         then:
-        waiter.isWatching() >> false
-        0 * waiter.wait(_, _)
+        executeBuild()
+    }
+
+    private void buildExits() {
+        runningBuild.get(1, TimeUnit.SECONDS)
+    }
+
+    private Future<?> startBuild() {
+        runningBuild = executorService.submit {
+            executeBuild()
+        }
+        return runningBuild
+    }
+
+    private void continuousBuildWithInputs() {
+        buildWithInputs()
+        continuousBuild()
+    }
+
+    private void buildWithDeployment() {
+        buildWithInputs()
+        deployments.add(Stub(DeploymentInternal))
+    }
+
+    private void buildWithInputs() {
+        delegate = new BuildSessionActionExecutor() {
+            @Override
+            BuildActionRunner.Result execute(BuildAction action, BuildSessionContext context) {
+                declareInput(file)
+                return BuildActionRunner.Result.of(null)
+            }
+        }
+        executer = executer()
+    }
+
+    private boolean waitingForChanges() {
+        return lastLogLine == "Waiting for changes to input files of tasks..."
+    }
+
+    private void reloadableDeploymentDetected() {
+        assert logLines[-2] == "Reloadable deployment detected. Entering continuous build."
+        assert lastLogLine == "Waiting for changes to input files of tasks..."
+    }
+
+    private void rebuiltBecauseOfChange() {
+        assert changeDetected()
+        assert waitingForChanges()
+    }
+
+    private boolean changeDetected() {
+        logLines[-2] == "Change detected, executing build..." ||
+            lastLogLine == "Change detected, executing build..."
+    }
+
+    private String getLastLogLine() {
+        return logLines.last()
+    }
+
+    private List<String> getLogLines() {
+        return textOutputFactory.toString().readLines().findAll { !it.empty }
     }
 
     private void singleBuild() {
@@ -155,6 +288,6 @@ class ContinuousBuildActionExecutorTest extends ConcurrentSpec {
     }
 
     private ContinuousBuildActionExecutor executer() {
-        new ContinuousBuildActionExecutor(waiterFactory, inputsListeners, new TestStyledTextOutputFactory(), executorFactory, requestContext, cancellationToken, deploymentRegistry, listenerManager.createChild(Scopes.BuildSession), buildExecutionTimer, Time.clock(), _, delegate)
+        new ContinuousBuildActionExecutor(inputsListeners, changeListeners, textOutputFactory, executorFactory, requestContext, cancellationToken, deploymentRegistry, userHomeListenerManager.createChild(Scopes.BuildSession), buildExecutionTimer, Time.clock(), TestFiles.fileSystem(), CaseSensitivity.CASE_SENSITIVE, delegate)
     }
 }
