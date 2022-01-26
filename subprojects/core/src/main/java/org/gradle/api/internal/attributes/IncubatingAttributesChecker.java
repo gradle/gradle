7d15adbebb8b33e5bbb770358eacb36bdf359ddf
Thomas Tresansky <ttresansky@gradle.com>
2022-01-26 07:42:25 -0500

    style: Need import for previous change.

--- subprojects/core/src/main/java/org/gradle/api/internal/attributes/IncubatingAttributesChecker.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/attributes/IncubatingAttributesChecker.java
@@ -27,6 +27,7 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
