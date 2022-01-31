2104336c144e0443c3abc954bf196d54f3da991d
Stefan Wolf <wolf@gradle.com>
2022-01-31 11:43:07 +0100

    Assert that `.tmp` is empty

--- subprojects/core/src/integTest/groovy/org/gradle/api/UndefinedBuildExecutionIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/UndefinedBuildExecutionIntegrationTest.groovy
@@ -102,6 +102,11 @@ class UndefinedBuildExecutionIntegrationTest extends AbstractIntegrationSpec {
         // .tmp: Temporary folder for worker classpath files and configuration caching report intermediate files, not present in project .gradle caches directory
         def filteredContents = (dirContents?.findAll { !(it in ["caches", "native", ".tmp"]) })
         assert !filteredContents
+
+        def tmpDir = dir.file(".tmp")
+        if (tmpDir.exists()) {
+            tmpDir.assertIsEmptyDir()
+        }
     }
 
     private void printFileTree(File dir) {
