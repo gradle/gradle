2efda747f4966295394327fc20e5dea71b187e52
Paul Merlin <paul@gradle.com>
2022-02-09 16:01:47 +0100

    Disable test broken with CC and AGP 7.3
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidPluginsSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidPluginsSmokeTest.groovy
@@ -22,6 +22,7 @@ import org.gradle.internal.reflect.validation.ValidationMessageChecker
 import org.gradle.testkit.runner.TaskOutcome
 
 import static org.gradle.internal.reflect.validation.Severity.ERROR
+
 /**
  * For these tests to run you need to set ANDROID_SDK_ROOT to your Android SDK directory
  *
@@ -63,7 +64,8 @@ class AndroidPluginsSmokeTest extends AbstractPluginValidatingSmokeTest implemen
         ].combinations()
     }
 
-    @UnsupportedWithConfigurationCache(iterationMatchers = AGP_NO_CC_ITERATION_MATCHER)
+    // TODO re-enable this test with AGP 7.3 once a version supporting CC is released
+    @UnsupportedWithConfigurationCache(iterationMatchers = [AGP_NO_CC_ITERATION_MATCHER, ".*agp=7\\.3\\..*"])
     def "android library and application APK assembly (agp=#agpVersion, ide=#ide)"() {
 
         given:
