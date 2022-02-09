a1e4028d2ce24b4bfdc28321b745e1cee0b8f778
Bo Zhang <bo@gradle.com>
2022-02-09 10:27:39 +0800

    Rebasline JavaFirstUsePerformanceTest
    To accept a <1% regression

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaFirstUsePerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaFirstUsePerformanceTest.groovy
@@ -33,7 +33,7 @@ import static org.gradle.performance.results.OperatingSystem.LINUX
 class JavaFirstUsePerformanceTest extends AbstractCrossVersionPerformanceTest {
 
     def setup() {
-        runner.targetVersions = ["7.5-20220201231431+0000"]
+        runner.targetVersions = ["7.5-20220208231448+0000"]
     }
 
     def "first use"() {
