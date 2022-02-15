48de42a4160ab38dd5399b0d14af6f1b9cb5653f
Octavia Togami <otogami@gradle.com>
2022-02-15 14:42:34 -0800

    Allow options with the same name and signature
    This allows options defined in an interface and in a class to coexist, fixes #19868

--- subprojects/core/src/integTest/groovy/org/gradle/api/tasks/options/TaskOptionIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/tasks/options/TaskOptionIntegrationTest.groovy
@@ -251,7 +251,7 @@ Options
     }
 
     @Issue("https://github.com/gradle/gradle/issues/18496")
-    def "consider options from interfaces"() {
+    def "considers options from interfaces"() {
         given:
         buildFile << '''
             interface MyInterface {
@@ -282,7 +282,7 @@ Options
     }
 
     @Issue("https://github.com/gradle/gradle/issues/18496")
-    def "consider options from interfaces with same method defined twice should use last defined value"() {
+    def "options from interfaces with same method defined twice should use last defined value"() {
         given:
         buildFile << '''
             interface MyInterface {
@@ -320,5 +320,122 @@ Options
 
         then:
         result.assertTaskExecuted(':myTask').assertOutputContains('Serial: 4321')
+    }
+
+    def "options from interfaces with same method defined in class should use overridden value"() {
+        given:
+        buildFile << '''
+            interface MyInterface {
+              @Option(
+                option = 'serial',
+                description = 'Target the device with given serial'
+              )
+              @Optional
+              @Input
+              Property<String> getSerial()
+            }
+
+            abstract class MyTask extends DefaultTask implements MyInterface {
+              @Option(
+                option = 'serialNumber',
+                description = 'Target the device with given serial'
+              )
+              @Optional
+              @Input
+              abstract Property<String> getSerial()
+
+              @TaskAction
+              void action() {
+                println "Serial: ${serial.getOrElse('-')}"
+              }
+            }
+
+            tasks.register("myTask", MyTask.class)
+        '''
+
+        when:
+        succeeds('myTask', '--serial=1234', '--serialNumber=4321')
+
+        then:
+        result.assertTaskExecuted(':myTask').assertOutputContains('Serial: 4321')
+    }
+
+    def "options from interfaces with same method defined twice with same name should work"() {
+        given:
+        buildFile << '''
+            interface MyInterface {
+              @Option(
+                option = 'serial',
+                description = 'Target the device with given serial'
+              )
+              @Optional
+              @Input
+              Property<String> getSerial()
+            }
+
+            interface MyInterface1 {
+              @Option(
+                option = 'serial',
+                description = 'Target the device with given serial'
+              )
+              @Optional
+              @Input
+              Property<String> getSerial()
+            }
+
+            abstract class MyTask extends DefaultTask implements MyInterface, MyInterface1{
+              @TaskAction
+              void action() {
+                println "Serial: ${serial.getOrElse('-')}"
+              }
+            }
+
+            tasks.register("myTask", MyTask.class)
+        '''
+
+        when:
+        succeeds('myTask', '--serial=1234')
+
+        then:
+        result.assertTaskExecuted(':myTask').assertOutputContains('Serial: 1234')
+    }
+
+    @Issue("https://github.com/gradle/gradle/issues/19868")
+    def "options from interfaces with same method defined in class with same name should work"() {
+        given:
+        buildFile << '''
+            interface MyInterface {
+              @Option(
+                option = 'serial',
+                description = 'Target the device with given serial'
+              )
+              @Optional
+              @Input
+              Property<String> getSerial()
+            }
+
+            abstract class MyTask extends DefaultTask implements MyInterface {
+              @Option(
+                option = 'serial',
+                description = 'Target the device with given serial'
+              )
+              @Optional
+              @Input
+              abstract Property<String> getSerial()
+
+              @TaskAction
+              void action() {
+                println "Serial: ${serial.getOrElse('-')}"
+              }
+            }
+
+            tasks.register("myTask", MyTask.class)
+        '''
+
+        when:
+        succeeds('myTask', '--serial=1234')
+
+        then:
+        result.assertTaskExecuted(':myTask').assertOutputContains('Serial: 1234')
     }
 }
--- subprojects/core/src/main/java/org/gradle/api/internal/tasks/options/OptionReader.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/tasks/options/OptionReader.java
@@ -35,6 +35,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class OptionReader {
@@ -56,15 +57,21 @@ public class OptionReader {
     }
 
     private void loadClassDescriptorInCache(Object target) {
-        final Collection<OptionElement> optionElements = getOptionElements(target);
+        final Collection<OptionElementAndSignature> optionElements = getOptionElements(target);
         List<JavaMethod<Object, Collection>> optionValueMethods = loadValueMethodForOption(target.getClass());
-        Set<String> processedOptionElements = new HashSet<String>();
-        for (OptionElement optionElement : optionElements) {
-            if (processedOptionElements.contains(optionElement.getOptionName())) {
-                throw new OptionValidationException(String.format("@Option '%s' linked to multiple elements in class '%s'.",
+        Map<String, MethodSignature> processedOptionElements = new HashMap<>();
+        for (OptionElementAndSignature optionElementAndSignature : optionElements) {
+            OptionElement optionElement = optionElementAndSignature.element;
+            MethodSignature signature = optionElementAndSignature.signature;
+
+            if (processedOptionElements.containsKey(optionElement.getOptionName())) {
+                MethodSignature existingSignature = processedOptionElements.get(optionElement.getOptionName());
+                if (!Objects.equals(signature, existingSignature)) {
+                    throw new OptionValidationException(String.format("@Option '%s' linked to multiple elements in class '%s'.",
                         optionElement.getOptionName(), target.getClass().getName()));
+                }
             }
-            processedOptionElements.add(optionElement.getOptionName());
+            processedOptionElements.put(optionElement.getOptionName(), signature);
             JavaMethod<Object, Collection> optionValueMethodForOption = getOptionValueMethodForOption(optionValueMethods, optionElement);
 
             cachedOptionElements.put(target.getClass(), optionElement);
@@ -95,8 +102,18 @@ public class OptionReader {
         return optionValues.value();
     }
 
-    private Collection<OptionElement> getOptionElements(Object target) {
-        List<OptionElement> allOptionElements = new ArrayList<OptionElement>();
+    private static final class OptionElementAndSignature {
+        final OptionElement element;
+        final MethodSignature signature;
+
+        OptionElementAndSignature(OptionElement element, MethodSignature signature) {
+            this.element = element;
+            this.signature = signature;
+        }
+    }
+
+    private Collection<OptionElementAndSignature> getOptionElements(Object target) {
+        List<OptionElementAndSignature> allOptionElements = new ArrayList<>();
         Set<Class<?>> visitedInterfaces = new HashSet<>();
         Deque<Class<?>> interfacesToCheck = new ArrayDeque<>();
 
@@ -104,7 +121,9 @@ public class OptionReader {
             interfacesToCheck.addAll(Arrays.asList(type.getInterfaces()));
 
             allOptionElements.addAll(getMethodAnnotations(type));
-            allOptionElements.addAll(getFieldAnnotations(type));
+            for (OptionElement element : getFieldAnnotations(type)) {
+                allOptionElements.add(new OptionElementAndSignature(element, null));
+            }
         }
 
         while (interfacesToCheck.size() > 0) {
@@ -144,14 +163,14 @@ public class OptionReader {
         return option;
     }
 
-    private List<OptionElement> getMethodAnnotations(Class<?> type) {
-        List<OptionElement> methodOptionElements = new ArrayList<OptionElement>();
+    private List<OptionElementAndSignature> getMethodAnnotations(Class<?> type) {
+        List<OptionElementAndSignature> methodOptionElements = new ArrayList<>();
         for (Method method : type.getDeclaredMethods()) {
             Option option = findOption(method);
             if (option != null) {
                 OptionElement methodOptionDescriptor = MethodOptionElement.create(option, method,
                     optionValueNotationParserFactory);
-                methodOptionElements.add(methodOptionDescriptor);
+                methodOptionElements.add(new OptionElementAndSignature(methodOptionDescriptor, MethodSignature.from(method)));
             }
         }
         return methodOptionElements;
