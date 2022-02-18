433d005a3cb834f2aaba4175b5b66a4b252ad9e2
Stefan Wolf <wolf@gradle.com>
2022-02-18 12:27:18 +0100

    Add test for relocating outputs

--- subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/TaskUpToDateIntegrationTest.groovy
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/TaskUpToDateIntegrationTest.groovy
@@ -189,6 +189,44 @@ class TaskUpToDateIntegrationTest extends AbstractIntegrationSpec {
         (["customTask", "-PnumOutputs=${outputs.size()}"] + outputs.withIndex().collect { value, idx -> "-Poutput${idx}" + (value ? "=${value}" : '') }) as String[]
     }
 
+    def "output files moved from one location to another marks task up-to-date"() {
+        buildFile << """
+            abstract class CustomTask extends DefaultTask {
+                @OutputDirectory
+                abstract DirectoryProperty getOutputDirectory()
+
+                @TaskAction
+                void doAction() {
+                    getOutputDirectory().file("output.txt").get().asFile.text = "output"
+                }
+            }
+
+            task customTask(type: CustomTask) {
+                outputDirectory = project.file(project.providers.gradleProperty('outputDir'))
+            }
+        """
+
+        when:
+        succeeds ":customTask", "-PoutputDir=build/output1"
+        then:
+        executedAndNotSkipped ":customTask"
+
+        when:
+        succeeds ":customTask", "-PoutputDir=build/output2"
+        then:
+        executedAndNotSkipped ":customTask"
+
+        when:
+        succeeds ":customTask", "-PoutputDir=build/output1"
+        then:
+        skipped ":customTask"
+
+        when:
+        succeeds ":customTask", "-PoutputDir=build/output2"
+        then:
+        skipped ":customTask"
+    }
+
     @Issue("https://github.com/gradle/gradle/issues/3073")
     def "optional input changed from null to non-null marks task not up-to-date"() {
         file("input.txt") << "Input data"
