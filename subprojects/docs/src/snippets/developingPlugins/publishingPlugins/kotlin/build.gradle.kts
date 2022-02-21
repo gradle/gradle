0d8c9955f3d3a46400313c4500a59dfdec1d4e6a
József Bartók <jbartok@gradle.com>
2022-02-21 14:24:34 +0200

    Add first set of changes
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/docs/src/snippets/developingPlugins/publishingPlugins/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/developingPlugins/publishingPlugins/kotlin/build.gradle.kts
@@ -1,21 +1,20 @@
 // tag::plugins_block[]
 plugins {
-    id("java-gradle-plugin")                          // <1>
-    id("maven-publish")                               // <2>
-    id("com.gradle.plugin-publish") version "0.20.0"  // <3>
+    id("com.gradle.plugin-publish") version "0.20.0"
 }
 // end::plugins_block[]
 
 // tag::gradle-plugin[]
 group = "io.github.johndoe" // <1>
-version = "1.0"     // <2>
+version = "1.0" // <2>
 
 gradlePlugin {
     plugins { // <3>
         create("greetingsPlugin") { // <4>
             id = "<your plugin identifier>" // <5>
             displayName = "<short displayable name for plugin>" // <6>
-            description = "<Good human-readable description of what your plugin is about>" // <7>
+            description = "<human-readable description of what your plugin is about>" // <7>
+            tags = listOf("tags", "for", "your", "plugins") // <8>
             implementationClass = "<your plugin class>"
         }
     }
@@ -24,9 +23,10 @@ gradlePlugin {
 
 // tag::plugin_bundle[]
 pluginBundle {
-    website = "<substitute your project website>"   // <1>
-    vcsUrl = "<uri to project source repository>"   // <2>
-    tags = listOf("tags", "for", "your", "plugins") // <3>
+    website = "<substitute your project website>" // <1>
+    vcsUrl = "<uri to project source repository>" // <2>
+    description = "<human-readable description of what your plugins are about>" // <3>
+    tags = listOf("tags", "for", "your", "plugins") // <4>
 }
 // end::plugin_bundle[]
 
