7d03100327a82f9cd7e8571b8ec56b119b4d23bb
Anže Sodja <asodja@gradle.com>
2022-02-28 18:25:09 +0100

    Add back Checkstyle java launcher property convetion

--- subprojects/code-quality/src/main/groovy/org/gradle/api/plugins/quality/Checkstyle.java
+++ subprojects/code-quality/src/main/groovy/org/gradle/api/plugins/quality/Checkstyle.java
@@ -159,7 +159,7 @@ public class Checkstyle extends SourceTask implements VerificationTask, Reportin
     }
 
     /**
-     * Optional JavaLauncher for toolchain support
+     * JavaLauncher for toolchain support
      * @since 7.5
      */
     @Incubating
@@ -175,9 +175,7 @@ public class Checkstyle extends SourceTask implements VerificationTask, Reportin
 
     private void runWithProcessIsolation() {
         WorkQueue workQueue = getWorkerExecutor().processIsolation(spec -> {
-            if (javaLauncher.isPresent()) {
-                spec.getForkOptions().setExecutable(javaLauncher.get().getExecutablePath().getAsFile().getAbsolutePath());
-            }
+            spec.getForkOptions().setExecutable(javaLauncher.get().getExecutablePath().getAsFile().getAbsolutePath());
             spec.getClasspath().setFrom(getCheckstyleClasspath());
         });
         workQueue.submit(CheckstyleAction.class, this::setupParameters);
