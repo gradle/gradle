a3abd54e202cfbe433e8798cde14fe05edab2ace
Lóránt Pintér <lorant@gradle.com>
2022-03-07 16:56:44 +0100

    Upgrade JUnit 5 everywhere

--- subprojects/distributions-dependencies/build.gradle.kts
+++ subprojects/distributions-dependencies/build.gradle.kts
@@ -104,9 +104,9 @@ dependencies {
         api(libs.jsr305)                { version { strictly("3.0.2") }}
         api(libs.julToSlf4j)            { version { strictly(slf4jVersion) }}
         api(libs.junit)                 { version { strictly("4.13.2") }}
-        api(libs.junit5JupiterApi)      { version { strictly("5.8.1") }}
-        api(libs.junit5Vintage)         { version { strictly("5.8.1") }}
-        api(libs.junitPlatform)         { version { strictly("1.8.1") }}
+        api(libs.junit5JupiterApi)      { version { strictly("5.8.2") }}
+        api(libs.junit5Vintage)         { version { strictly("5.8.2") }}
+        api(libs.junitPlatform)         { version { strictly("1.8.2") }}
         api(libs.jzlib)                 { version { strictly("1.1.3") }}
         api(libs.kryo)                  { version { strictly("2.24.0") }}
         api(libs.log4jToSlf4j)          { version { strictly(slf4jVersion) }}
