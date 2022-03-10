58e90fcc47086ec3a89429164d58bc1ed6a0d2a9
Adam Murdoch <adam@gradle.com>
2022-03-11 10:58:13 +1100

    When inspecting a task for problems, do not resolve the plugin id for the task if the task does not have any problems.
    Resolving the plugin id is somewhat expensive, so skip this when not needed.

--- subprojects/model-core/src/main/java/org/gradle/internal/reflect/ProblemRecordingTypeValidationContext.java
+++ subprojects/model-core/src/main/java/org/gradle/internal/reflect/ProblemRecordingTypeValidationContext.java
@@ -27,15 +27,19 @@ import org.gradle.internal.reflect.validation.TypeValidationProblem;
 import org.gradle.plugin.use.PluginId;
 
 import javax.annotation.Nullable;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 abstract public class ProblemRecordingTypeValidationContext implements TypeValidationContext {
     private final DocumentationRegistry documentationRegistry;
     private final Class<?> rootType;
-    private final PluginId pluginId;
+    private final Supplier<Optional<PluginId>> pluginId;
 
-    public ProblemRecordingTypeValidationContext(DocumentationRegistry documentationRegistry,
-                                                 @Nullable Class<?> rootType,
-                                                 @Nullable PluginId pluginId) {
+    public ProblemRecordingTypeValidationContext(
+        DocumentationRegistry documentationRegistry,
+        @Nullable Class<?> rootType,
+        Supplier<Optional<PluginId>> pluginId
+    ) {
         this.documentationRegistry = documentationRegistry;
         this.rootType = rootType;
         this.pluginId = pluginId;
@@ -43,14 +47,19 @@ abstract public class ProblemRecordingTypeValidationContext implements TypeValid
 
     @Override
     public void visitTypeProblem(Action<? super TypeProblemBuilder> problemSpec) {
-        DefaultTypeValidationProblemBuilder builder = new DefaultTypeValidationProblemBuilder(documentationRegistry, pluginId);
+        DefaultTypeValidationProblemBuilder builder = new DefaultTypeValidationProblemBuilder(documentationRegistry, pluginId());
         problemSpec.execute(builder);
         recordProblem(builder.build());
     }
 
+    @Nullable
+    private PluginId pluginId() {
+        return pluginId.get().orElse(null);
+    }
+
     @Override
     public void visitPropertyProblem(Action<? super PropertyProblemBuilder> problemSpec) {
-        DefaultPropertyValidationProblemBuilder builder = new DefaultPropertyValidationProblemBuilder(documentationRegistry, pluginId);
+        DefaultPropertyValidationProblemBuilder builder = new DefaultPropertyValidationProblemBuilder(documentationRegistry, pluginId());
         problemSpec.execute(builder);
         builder.forType(rootType);
         recordProblem(builder.build());
