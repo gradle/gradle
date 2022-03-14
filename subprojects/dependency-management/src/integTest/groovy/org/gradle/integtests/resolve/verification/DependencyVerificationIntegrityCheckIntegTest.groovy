21e72309e362d515c44b87b46b64144975621c96
Anže Sodja <asodja@gradle.com>
2022-03-14 14:48:08 +0100

    Additional fixes for tests

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/verification/DependencyVerificationIntegrityCheckIntegTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/verification/DependencyVerificationIntegrityCheckIntegTest.groovy
@@ -669,9 +669,8 @@ If the artifacts are trustworthy, you will need to update the gradle/verificatio
         if (GradleContextualExecuter.isConfigCache()) {
             failure.assertThatDescription(containsText("""Configuration cache problems found in this build.
 
-2 problems were found storing the configuration cache.
-- Task `:compileJava` of type `org.gradle.api.tasks.compile.JavaCompile`: value 'file collection' failed to visit file collection
-- Task `:compileJava` of type `org.gradle.api.tasks.compile.JavaCompile`: value 'configuration ':included:compileClasspath'' failed to visit file collection"""))
+2 problems were found storing the configuration cache, 1 of which seems unique.
+- Task `:compileJava` of type `org.gradle.api.tasks.compile.JavaCompile`: value 'file collection' failed to visit file collection"""))
         }
 
         where:
@@ -1003,7 +1002,7 @@ This can indicate that a dependency has been compromised. Please carefully verif
             failure.assertThatDescription(containsText("""Configuration cache problems found in this build.
 
 1 problem was found storing the configuration cache.
-- Task `:printConfigurations` of type `org.gradle.api.DefaultTask`: value 'file collection' failed to visit file collection"""))
+- Task `:printConfigurations` of type `org.gradle.api.DefaultTask`: value 'configuration ':compileClasspath'' failed to visit file collection"""))
         }
     }
 
@@ -1255,7 +1254,7 @@ This can indicate that a dependency has been compromised. Please carefully verif
             failure.assertThatDescription(containsText("""Configuration cache problems found in this build.
 
 1 problem was found storing the configuration cache.
-- Task `:printConfigurations` of type `PrintConfigurations`: value 'file collection' failed to visit file collection"""))
+- Task `:printConfigurations` of type `PrintConfigurations`: value 'configuration ':compileClasspath'' failed to visit file collection"""))
         }
     }
 }
