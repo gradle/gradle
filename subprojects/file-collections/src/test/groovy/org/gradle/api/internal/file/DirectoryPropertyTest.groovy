d52c509d0e4b6116091c3f916dcc84c7b7e9255f
Stefan Wolf <wolf@gradle.com>
2022-03-18 16:12:09 +0100

    Make sure the FilePropertyTest does test the right things
    When the default resolver and the base directory for the
    directory are the same, we don't see if
    `DirectoryProperty.files()` works correctly.

--- subprojects/file-collections/src/test/groovy/org/gradle/api/internal/file/DirectoryPropertyTest.groovy
+++ subprojects/file-collections/src/test/groovy/org/gradle/api/internal/file/DirectoryPropertyTest.groovy
@@ -28,22 +28,22 @@ class DirectoryPropertyTest extends FileSystemPropertySpec<Directory> {
 
     @Override
     Directory someValue() {
-        return baseDir.dir("dir1").get()
+        return baseDirectory.dir("dir1").get()
     }
 
     @Override
     Directory someOtherValue() {
-        return baseDir.dir("other1").get()
+        return baseDirectory.dir("other1").get()
     }
 
     @Override
     Directory someOtherValue2() {
-        return baseDir.dir("other2").get()
+        return baseDirectory.dir("other2").get()
     }
 
     @Override
     Directory someOtherValue3() {
-        return baseDir.dir("other3").get()
+        return baseDirectory.dir("other3").get()
     }
 
     @Override
@@ -63,30 +63,30 @@ class DirectoryPropertyTest extends FileSystemPropertySpec<Directory> {
 
     def "can view directory as a file tree"() {
         given:
-        def dir1 = tmpDir.createDir("dir1")
+        def dir1 = baseDir.createDir("dir1")
         def file1 = dir1.createFile("sub-dir/file1")
         def file2 = dir1.createFile("file2")
-        def dir2 = tmpDir.createDir("dir2")
+        def dir2 = baseDir.createDir("dir2")
         def file3 = dir2.createFile("other/file3")
 
         expect:
-        def tree1 = baseDir.asFileTree
+        def tree1 = baseDirectory.asFileTree
         tree1.files == [file1, file2, file3] as Set
 
         and:
-        def tree2 = baseDir.dir("dir2").get().asFileTree
+        def tree2 = baseDirectory.dir("dir2").get().asFileTree
         tree2.files == [file3] as Set
     }
 
     def "can view relative paths as a file collection"() {
         given:
-        def fileCollection = baseDir.files("a/b/c", "d", "e/f")
+        def fileCollection = baseDirectory.files("a/b/c", "d", "e/f")
 
         expect:
         fileCollection.files == [
-            tmpDir.file("a/b/c"),
-            tmpDir.file("d"),
-            tmpDir.file("e/f")
+            baseDir.file("a/b/c"),
+            baseDir.file("d"),
+            baseDir.file("e/f")
         ] as Set
     }
 }
