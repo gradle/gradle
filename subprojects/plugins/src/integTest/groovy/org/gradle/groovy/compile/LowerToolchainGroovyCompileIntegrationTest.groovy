edae29e8f12374c9fb581eeb1df28e8e9d04f7e3
Lóránt Pintér <lorant@gradle.com>
2022-03-22 22:02:08 +0100

    Do not test toolchain support with groovy-all in Gradle 4

--- subprojects/plugins/src/integTest/groovy/org/gradle/groovy/compile/LowerToolchainGroovyCompileIntegrationTest.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/groovy/compile/LowerToolchainGroovyCompileIntegrationTest.groovy
@@ -22,6 +22,14 @@ import org.gradle.internal.jvm.Jvm
 class LowerToolchainGroovyCompileIntegrationTest extends AbstractToolchainGroovyCompileIntegrationTest {
 
     @Override
+    def getGroovyJarVariants() {
+        // Do not test with groovy-all with Groovy 4
+        versionNumber.major >= 4
+            ? ["groovy"]
+            : ["groovy-all", "groovy"]
+    }
+
+    @Override
     Jvm computeJdkForTest() {
         AvailableJavaHomes.getAvailableJdk {
             it.languageVersion < Jvm.current().javaVersion && it.languageVersion.isJava8Compatible()
