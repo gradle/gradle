f17f3562a9a6ac4065ad21ec1fc9a8d32430c7e6
Bo Zhang <bo@gradle.com>
2022-04-09 11:09:00 +0800

    Resolve merge conflict

--- build-logic/dependency-modules/src/main/kotlin/gradlebuild/modules/extension/ExternalModulesExtension.kt
+++ build-logic/dependency-modules/src/main/kotlin/gradlebuild/modules/extension/ExternalModulesExtension.kt
@@ -21,7 +21,6 @@ import gradlebuild.modules.model.License
 abstract class ExternalModulesExtension {
 
     val groovyVersion = "3.0.10"
-    val kotlinVersion = "1.6.10"
     val configurationCacheReportVersion = "1.1"
     val kotlinVersion = "1.6.20"
 
