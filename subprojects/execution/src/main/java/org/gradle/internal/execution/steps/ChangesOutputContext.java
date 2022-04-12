1118a1d0216f40aaaaf3fba014a010cca4f08ec1
Stefan Wolf <wolf@gradle.com>
2022-04-12 16:31:30 +0200

    Improve Javadoc
    Co-authored-by: Lóránt Pintér <lorant@gradle.com>

--- subprojects/execution/src/main/java/org/gradle/internal/execution/steps/ChangesOutputContext.java
+++ subprojects/execution/src/main/java/org/gradle/internal/execution/steps/ChangesOutputContext.java
@@ -17,7 +17,7 @@
 package org.gradle.internal.execution.steps;
 
 /**
- * Context necessary for steps which change the outputs.
+ * Context necessary for steps that change the outputs.
  *
  * This context doesn't add any new information, it encodes a requirement
  * in the type system that a step can change the outputs.
