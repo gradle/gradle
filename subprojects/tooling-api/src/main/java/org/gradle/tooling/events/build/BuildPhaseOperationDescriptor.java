b3403dd122079a1b20a9cbeefd5df4eda2bc4e66
Anže Sodja <asodja@gradle.com>
2022-04-17 20:05:38 +0200

    Add bunch of classes for mapping to build phase

---
+++ subprojects/tooling-api/src/main/java/org/gradle/tooling/events/build/BuildPhaseOperationDescriptor.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.tooling.events.build;
+
+import org.gradle.api.Incubating;
+import org.gradle.tooling.events.OperationDescriptor;
+
+/**
+ * @since 7.6
+ */
+@Incubating
+public interface BuildPhaseOperationDescriptor extends OperationDescriptor {
+    /**
+     * Returns the build phase name.
+     *
+     * Can be one of: CONFIGURE_ROOT_BUILD, CONFIGURE_BUILD, CONFIGURE_PROJECT, RUN_MAIN_TASKS, RUN_WORK, UNCATEGORIZED.
+     */
+    String getBuildPhase();
+
+    /**
+     * Returns number of build items this phase will execute.
+     */
+    int getBuildItemsCount();
+}
