a67926b8853251967604ffff9eb5a1246247b4ef
Anže Sodja <asodja@gradle.com>
2022-04-18 21:45:23 +0200

    Add integration tests for build phase events
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r75/BuildPhaseOperationEventCrossVersionTest.groovy
+++ subprojects/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r75/BuildPhaseOperationEventCrossVersionTest.groovy
@@ -16,17 +16,61 @@
 
 package org.gradle.integtests.tooling.r75
 
-import org.gradle.integtests.fixtures.executer.UnderDevelopmentGradleDistribution
 import org.gradle.integtests.tooling.fixture.ProgressEvents
 import org.gradle.integtests.tooling.fixture.TargetGradleVersion
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
+import org.gradle.integtests.tooling.fixture.ToolingApiVersion
+import org.gradle.tooling.GradleConnectionException
+import org.gradle.tooling.events.FailureResult
 import org.gradle.tooling.events.OperationType
+import org.gradle.tooling.events.ProgressEvent
+import org.gradle.tooling.events.SuccessResult
+import org.gradle.tooling.events.build.BuildPhaseFinishEvent
+import org.gradle.tooling.events.build.BuildPhaseOperationDescriptor
+import org.gradle.tooling.events.build.BuildPhaseStartEvent
 
-@TargetGradleVersion(">=7.4")
+@ToolingApiVersion(">=7.5")
+@TargetGradleVersion(">=7.5")
 class BuildPhaseOperationEventCrossVersionTest extends ToolingApiSpecification {
-    def "generates build phase events"() {
+
+    def "generates build phase events for task #taskName and expects #expectedRunAndReportedTasks run tasks"() {
         setupProject()
-        setTargetDist(new UnderDevelopmentGradleDistribution())
+
+        when:
+        def events = ProgressEvents.create()
+        withConnection {
+            it.newBuild().forTasks(taskName)
+                .addProgressListener(events, OperationType.BUILD_PHASE)
+                .run()
+        }
+
+        then:
+        def progressEvents = events.getAll()
+        progressEvents.size() == 6
+
+        // We have 4 projects
+        assertStartEventHas(progressEvents[0], "CONFIGURE_ROOT_BUILD", 4)
+        assertSuccessfulFinishEventHas(progressEvents[1], "CONFIGURE_ROOT_BUILD")
+
+        assertStartEventHas(progressEvents[2], "RUN_MAIN_TASKS", 0)
+        assertSuccessfulFinishEventHas(progressEvents[5], "RUN_MAIN_TASKS")
+
+        assertStartEventHas(progressEvents[3], "RUN_WORK", expectedRunAndReportedTasks)
+        assertSuccessfulFinishEventHas(progressEvents[4], "RUN_WORK")
+
+        where:
+        taskName   | expectedRunAndReportedTasks
+        ":a:taskA" | 1
+        ":a:taskB" | 2
+        ":a:taskC" | 3
+        ":a:taskD" | 1
+    }
+
+    def "reports failure if build fails in configuration phase"() {
+        setupProject()
+        file("a/build.gradle") << """
+            throw new RuntimeException("taskD failed")
+        """
 
         when:
         def events = ProgressEvents.create()
@@ -37,7 +81,68 @@ class BuildPhaseOperationEventCrossVersionTest extends ToolingApiSpecification {
         }
 
         then:
-        events.events.size() == 6
+        thrown(GradleConnectionException.class)
+        def progressEvents = events.getAll()
+        progressEvents.size() == 2
+
+        assertStartEventHas(progressEvents[0], "CONFIGURE_ROOT_BUILD", 4)
+        assertFailedFinishEventHas(progressEvents[1], "CONFIGURE_ROOT_BUILD")
+    }
+
+    def "reports failure if build fails in build phase"() {
+        setupProject()
+        file("a/build.gradle") << """
+            tasks.register("taskE") {
+                doLast {
+                    throw new RuntimeException("taskD failed")
+                }
+            }
+        """
+
+        when:
+        def events = ProgressEvents.create()
+        withConnection {
+            it.newBuild().forTasks(":a:taskE")
+                .addProgressListener(events, OperationType.BUILD_PHASE)
+                .run()
+        }
+
+        then:
+        thrown(GradleConnectionException.class)
+        def progressEvents = events.getAll()
+        progressEvents.size() == 6
+
+        // We have 4 projects
+        assertStartEventHas(progressEvents[0], "CONFIGURE_ROOT_BUILD", 4)
+        assertSuccessfulFinishEventHas(progressEvents[1], "CONFIGURE_ROOT_BUILD")
+
+        assertStartEventHas(progressEvents[2], "RUN_MAIN_TASKS", 0)
+        assertSuccessfulFinishEventHas(progressEvents[5], "RUN_MAIN_TASKS")
+
+        // We will run just 1 task
+        assertStartEventHas(progressEvents[3], "RUN_WORK", 1)
+        assertFailedFinishEventHas(progressEvents[4], "RUN_WORK")
+    }
+
+    boolean assertStartEventHas(ProgressEvent event, String buildPhase, int buildItemsCount) {
+        assert buildPhase && event instanceof BuildPhaseStartEvent
+        assert buildPhase && (event.descriptor as BuildPhaseOperationDescriptor).buildPhase == buildPhase
+        assert buildPhase && (event.descriptor as BuildPhaseOperationDescriptor).buildItemsCount == buildItemsCount
+        return true
+    }
+
+    boolean assertSuccessfulFinishEventHas(ProgressEvent event, String buildPhase) {
+        assert buildPhase && event instanceof BuildPhaseFinishEvent
+        assert buildPhase && (event as BuildPhaseFinishEvent).result instanceof SuccessResult
+        assert buildPhase && (event.descriptor as BuildPhaseOperationDescriptor).buildPhase == buildPhase
+        return true
+    }
+
+    boolean assertFailedFinishEventHas(ProgressEvent event, String buildPhase) {
+        assert buildPhase && event instanceof BuildPhaseFinishEvent
+        assert buildPhase && (event as BuildPhaseFinishEvent).result instanceof FailureResult
+        assert buildPhase && (event.descriptor as BuildPhaseOperationDescriptor).buildPhase == buildPhase
+        return true
     }
 
     def setupProject() {
@@ -53,24 +158,13 @@ class BuildPhaseOperationEventCrossVersionTest extends ToolingApiSpecification {
             tasks.register("taskC") {
                 dependsOn 'taskB'
             }
+            tasks.register("taskD")
         """
         file("b/build.gradle") << """
             tasks.register("taskA")
-            tasks.register("taskB") {
-                dependsOn 'taskA'
-            }
-            tasks.register("taskC") {
-                dependsOn 'taskB'
-            }
         """
         file("c/build.gradle") << """
             tasks.register("taskA")
-            tasks.register("taskB") {
-                dependsOn 'taskA'
-            }
-            tasks.register("taskC") {
-                dependsOn 'taskB'
-            }
         """
     }
 }
