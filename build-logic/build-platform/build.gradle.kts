8b24429339bc182a833e44a819f0e4d6e5a2e144
Sorin Florea <sflorea@gradle.com>
2022-04-19 15:04:23 +0300

    Update Gradle Enterprise Plugin version to 3.10

--- build-logic/build-platform/build.gradle.kts
+++ build-logic/build-platform/build.gradle.kts
@@ -16,7 +16,7 @@ val kotlinVersion = providers.gradleProperty("buildKotlinVersion")
 dependencies {
     constraints {
         // Gradle Plugins
-        api("com.gradle:gradle-enterprise-gradle-plugin:3.9")
+        api("com.gradle:gradle-enterprise-gradle-plugin:3.10")
         api("com.gradle.enterprise:test-distribution-gradle-plugin:2.3") // Sync with `settings.gradle.kts`
         api("org.gradle.guides:gradle-guides-plugin:0.20.1")
         api("com.gradle.publish:plugin-publish-plugin:1.0.0-rc-1")
