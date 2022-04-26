ea7de6dd100a485117cf4938e9b126b1288ca930
Louis Jacomet <louis@gradle.com>
2022-04-26 10:03:46 +0200

    Revert optimisations causing trouble with config cache
    Performance improvements revealed an issue with CC handling of object
    graphs with cycle.
    This reverts these changes to not trigger that problem.
    
    See #19491

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/resolveengine/result/CachingDependencyResultFactory.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/resolveengine/result/CachingDependencyResultFactory.java
@@ -27,18 +27,23 @@ import org.gradle.api.internal.artifacts.result.DefaultUnresolvedDependencyResul
 import org.gradle.internal.resolve.ModuleVersionResolveException;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static java.util.Arrays.asList;
 
 public class CachingDependencyResultFactory {
 
-    private final Map<DefaultUnresolvedDependencyResult, DefaultUnresolvedDependencyResult> unresolvedDependencies = new HashMap<>();
-    private final Map<DefaultResolvedDependencyResult, DefaultResolvedDependencyResult> resolvedDependencies = new HashMap<>();
+    private final Map<List<Object>, DefaultUnresolvedDependencyResult> unresolvedDependencies = new HashMap<>();
+    private final Map<List<Object>, DefaultResolvedDependencyResult> resolvedDependencies = new HashMap<>();
 
     public UnresolvedDependencyResult createUnresolvedDependency(ComponentSelector requested, ResolvedComponentResult from, boolean constraint,
                                                                  ComponentSelectionReason reason, ModuleVersionResolveException failure) {
-        DefaultUnresolvedDependencyResult result = new DefaultUnresolvedDependencyResult(requested, constraint, reason, from, failure);
-        DefaultUnresolvedDependencyResult existing = unresolvedDependencies.putIfAbsent(result, result);
-        return existing != null ? existing : result;
+        List<Object> key = asList(requested, from, constraint);
+        if (!unresolvedDependencies.containsKey(key)) {
+            unresolvedDependencies.put(key, new DefaultUnresolvedDependencyResult(requested, constraint, reason, from, failure));
+        }
+        return unresolvedDependencies.get(key);
     }
 
     public ResolvedDependencyResult createResolvedDependency(ComponentSelector requested,
@@ -46,8 +51,10 @@ public class CachingDependencyResultFactory {
                                                              ResolvedComponentResult selected,
                                                              ResolvedVariantResult resolvedVariant,
                                                              boolean constraint) {
-        DefaultResolvedDependencyResult result = new DefaultResolvedDependencyResult(requested, constraint, selected, resolvedVariant, from);
-        DefaultResolvedDependencyResult existing = resolvedDependencies.putIfAbsent(result, result);
-        return existing != null ? existing : result;
+        List<Object> key = asList(requested, from, selected, resolvedVariant, constraint);
+        if (!resolvedDependencies.containsKey(key)) {
+            resolvedDependencies.put(key, new DefaultResolvedDependencyResult(requested, constraint, selected, resolvedVariant, from));
+        }
+        return resolvedDependencies.get(key);
     }
 }
