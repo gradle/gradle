8a9b54f17150d1acab07490d7550460e335a8257
Stefan Wolf <wolf@gradle.com>
2022-04-27 14:42:04 +0200

    Fix checkstyle issues in API upgrade classes
    - add the copyright
    - change case for non-final static String

--- subprojects/core/src/main/java/org/gradle/internal/upgrade/ApiUpgradeHandler.java
+++ subprojects/core/src/main/java/org/gradle/internal/upgrade/ApiUpgradeHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.gradle.internal.upgrade;
 
 import com.google.common.collect.ImmutableList;
@@ -5,7 +21,7 @@ import org.codehaus.groovy.runtime.callsite.CallSite;
 import org.codehaus.groovy.runtime.callsite.CallSiteArray;
 
 public class ApiUpgradeHandler {
-    private static ApiUpgradeHandler INSTANCE;
+    private static ApiUpgradeHandler instance;
 
     private final ImmutableList<Replacement> replacements;
 
@@ -14,21 +30,21 @@ public class ApiUpgradeHandler {
     }
 
     public void useInstance() {
-        INSTANCE = this;
+        instance = this;
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T invokeReplacement(Object receiver, Object[] args, int methodReplacementIndex) {
-        MethodReplacement<T> methodReplacement = (MethodReplacement<T>) INSTANCE.replacements.get(methodReplacementIndex);
+        MethodReplacement<T> methodReplacement = (MethodReplacement<T>) instance.replacements.get(methodReplacementIndex);
         return methodReplacement.invokeReplacement(receiver, args);
     }
 
     public static void decorateCallSiteArray(CallSiteArray callSites) {
         // TODO: It seems like for worker actions the instance may be null (different classloader)
         //       Though we should detect the situation and not silently ignore it.
-        if (INSTANCE != null) {
+        if (instance != null) {
             for (CallSite callSite : callSites.array) {
-                for (Replacement replacement : INSTANCE.replacements) {
+                for (Replacement replacement : instance.replacements) {
                     replacement.decorateCallSite(callSite).ifPresent(decorated ->
                         callSites.array[callSite.getIndex()] = decorated
                     );
