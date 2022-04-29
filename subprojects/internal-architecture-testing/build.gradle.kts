d784265390f6d5e9ab91ac9bf7ffc5e360be8034
Stefan Wolf <wolf@gradle.com>
2022-04-29 12:47:18 +0200

    Support class excludes for package cycle test

--- subprojects/internal-architecture-testing/build.gradle.kts
+++ subprojects/internal-architecture-testing/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 description = "Collection of test fixtures and tests for architecture testing Gradle code"
 
 dependencies {
+    api(platform(project(":distributions-dependencies")))
     api(libs.archunitJunit5) {
         because("This is what we use to write our architecture tests")
     }
