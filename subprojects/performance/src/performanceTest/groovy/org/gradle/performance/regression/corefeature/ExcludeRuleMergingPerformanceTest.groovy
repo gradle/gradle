d4e63780f8861e832ef34992abbe62c738c8f4b5
Sterling Greene <sterling@gradle.com>
2022-05-02 16:28:25 -0400

    Rebaseline performance tests after incorporating #20148

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
@@ -31,7 +31,7 @@ class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceT
 
     def setup() {
         runner.minimumBaseVersion = '5.6.4'
-        runner.targetVersions = ["7.5-20220206232035+0000"]
+        runner.targetVersions = ["7.5-20220502194924+0000"]
     }
 
     def "merge exclude rules"() {
--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaIDEModelPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/java/JavaIDEModelPerformanceTest.groovy
@@ -33,7 +33,7 @@ import static org.gradle.performance.results.OperatingSystem.LINUX
 class JavaIDEModelPerformanceTest extends AbstractCrossVersionPerformanceTest {
 
     def setup() {
-        runner.targetVersions = ["7.5-20220406232819+0000"]
+        runner.targetVersions = ["7.5-20220502194924+0000"]
         runner.minimumBaseVersion = "2.11"
     }
 
