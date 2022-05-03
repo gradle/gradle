95fa7985b202c288d1e7025b8a8f1fc778cd67c7
Stefan Wolf <wolf@gradle.com>
2022-05-03 17:18:46 +0200

    Add an ugly workaround for convention mapping

--- subprojects/model-core/src/main/java/org/gradle/internal/extensibility/ConventionAwareHelper.java
+++ subprojects/model-core/src/main/java/org/gradle/internal/extensibility/ConventionAwareHelper.java
@@ -19,12 +19,14 @@ package org.gradle.internal.extensibility;
 import groovy.lang.Closure;
 import groovy.lang.MissingPropertyException;
 import org.gradle.api.InvalidUserDataException;
+import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.internal.ConventionMapping;
 import org.gradle.api.internal.IConventionAware;
 import org.gradle.api.plugins.Convention;
 import org.gradle.internal.Cast;
 import org.gradle.internal.deprecation.DeprecationLogger;
 import org.gradle.internal.reflect.JavaPropertyReflectionUtil;
+import org.gradle.internal.reflect.PropertyAccessor;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -67,7 +69,18 @@ public class ConventionAwareHelper implements ConventionMapping, org.gradle.api.
                     .nagUser();
         }
 
-        _mappings.put(propertyName, mapping);
+        if (_source.getClass().getName().contains("Compile") && propertyName.equals("classpath")) {
+            @SuppressWarnings("unchecked")
+            PropertyAccessor<IConventionAware, ConfigurableFileCollection> property = (PropertyAccessor<IConventionAware, ConfigurableFileCollection>) JavaPropertyReflectionUtil.readableProperty(_source.getClass(), propertyName);
+            property.getValue(_source).setFrom(new Callable<Object>() {
+                @Override
+                public Object call() {
+                    return mapping.getValue(_convention, _source);
+                }
+            });
+        } else {
+            _mappings.put(propertyName, mapping);
+        }
         return mapping;
     }
 
