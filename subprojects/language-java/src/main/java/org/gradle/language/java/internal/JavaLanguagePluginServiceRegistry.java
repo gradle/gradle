15c63c23bae3086638e9f17056850f1b836269f0
Stefan Wolf <wolf@gradle.com>
2022-05-05 11:13:01 +0200

    Also rewrite KaptGenerateStubs

--- subprojects/language-java/src/main/java/org/gradle/language/java/internal/JavaLanguagePluginServiceRegistry.java
+++ subprojects/language-java/src/main/java/org/gradle/language/java/internal/JavaLanguagePluginServiceRegistry.java
@@ -99,7 +99,7 @@ public class JavaLanguagePluginServiceRegistry extends AbstractPluginServiceRegi
                     (abstractCompile, value) -> abstractCompile.getSourceCompatibility().set(value)
                 );
             upgradeManager
-                .matchProperty(AbstractCompile.class, FileCollection.class, "classpath", ImmutableList.of(JavaCompile.class.getName(), "org.jetbrains.kotlin.gradle.tasks.AbstractKotlinCompile", "org.jetbrains.kotlin.gradle.tasks.KotlinCompile"))
+                .matchProperty(AbstractCompile.class, FileCollection.class, "classpath", ImmutableList.of(JavaCompile.class.getName(), "org.jetbrains.kotlin.gradle.tasks.AbstractKotlinCompile", "org.jetbrains.kotlin.gradle.tasks.KotlinCompile", "org.jetbrains.kotlin.gradle.internal.KaptGenerateStubsTask"))
                 .replaceWith(
                     AbstractCompile::getClasspath,
                     (abstractCompile, value) -> abstractCompile.getClasspath().setFrom(value)
