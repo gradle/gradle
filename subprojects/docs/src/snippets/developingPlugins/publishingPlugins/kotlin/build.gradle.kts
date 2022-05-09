24a04361fdbf00ecf9d41956bdadad25bbd90c3e
József Bartók <jbartok@gradle.com>
2022-05-09 12:47:35 +0300

    Update documentation to cover latest changes
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/docs/src/snippets/developingPlugins/publishingPlugins/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/developingPlugins/publishingPlugins/kotlin/build.gradle.kts
@@ -4,31 +4,33 @@ plugins {
 }
 // end::plugins_block[]
 
-// tag::gradle-plugin[]
-gradlePlugin {
-    plugins { // <1>
-        create("greetingsPlugin") { // <2>
-            id = "<your plugin identifier>" // <3>
-            displayName = "<short displayable name for plugin>" // <4>
-            description = "<human-readable description of what your plugin is about>" // <5>
-            tags = listOf("tags", "for", "your", "plugins") // <6>
+// tag::global_config[]
+group = "io.github.johndoe" // <1>
+version = "1.0" // <2>
+
+gradlePlugin { // <3>
+    website = "<substitute your project website>" // <4>
+    vcsUrl = "<uri to project source repository>" // <5>
+
+    // ... // <6>
+}
+// end::global_config[]
+
+// tag::per_plugin_config[]
+gradlePlugin { // <1>
+    // ... // <2>
+
+    plugins { // <3>
+        create("greetingsPlugin") { // <4>
+            id = "<your plugin identifier>" // <5>
+            displayName = "<short displayable name for plugin>" // <6>
+            description = "<human-readable description of what your plugin is about>" // <7>
+            tags = listOf("tags", "for", "your", "plugins") // <8>
             implementationClass = "<your plugin class>"
         }
     }
 }
-// end::gradle-plugin[]
-
-// tag::plugin_bundle[]
-group = "io.github.johndoe" // <1>
-version = "1.0" // <2>
-
-pluginBundle {
-    website = "<substitute your project website>" // <3>
-    vcsUrl = "<uri to project source repository>" // <4>
-    description = "<human-readable description of what your plugins are about>" // <5>
-    tags = listOf("tags", "for", "your", "plugins") // <6>
-}
-// end::plugin_bundle[]
+// end::per_plugin_config[]
 
 // tag::local_repository[]
 publishing {
