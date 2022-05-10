80e5a9555ba605832609da943f6da2fa9ba32854
Rob Bavey <rob.bavey@elastic.co>
2022-05-10 09:48:16 -0400

    Add `jdk.io.File.enableADS` to list of immutable system properties
    This commit adds `jdk.io.File.enableADS` to the list of immutable system properties, to enable the property
    to be passed on the command line, rather than set at runtime.
    
    Relates: https://github.com/elastic/logstash/issues/14075
    Signed-off-by: Rob Bavey <rob.bavey@elastic.co>

--- subprojects/core/src/main/java/org/gradle/process/internal/JvmOptions.java
+++ subprojects/core/src/main/java/org/gradle/process/internal/JvmOptions.java
@@ -50,11 +50,12 @@ public class JvmOptions {
     public static final String USER_VARIANT_KEY = "user.variant";
     public static final String JMX_REMOTE_KEY = "com.sun.management.jmxremote";
     public static final String JAVA_IO_TMPDIR_KEY = "java.io.tmpdir";
+    public static final String JDK_ENABLE_ADS_KEY = "jdk.io.File.enableADS";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JvmOptions.class);
 
     public static final Set<String> IMMUTABLE_SYSTEM_PROPERTIES = ImmutableSet.of(
-        FILE_ENCODING_KEY, USER_LANGUAGE_KEY, USER_COUNTRY_KEY, USER_VARIANT_KEY, JMX_REMOTE_KEY, JAVA_IO_TMPDIR_KEY
+        FILE_ENCODING_KEY, USER_LANGUAGE_KEY, USER_COUNTRY_KEY, USER_VARIANT_KEY, JMX_REMOTE_KEY, JAVA_IO_TMPDIR_KEY, JDK_ENABLE_ADS_KEY
     );
 
     // Store this because Locale.default is mutable and we want the unchanged default
