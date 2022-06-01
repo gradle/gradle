df889f6fd91c39a5e838b4868e338708c1ddc98f
Bo Zhang <bo@gradle.com>
2022-06-01 09:10:07 +0800

    Rebaseline stale baselines

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/RichConsolePerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/RichConsolePerformanceTest.groovy
@@ -40,7 +40,7 @@ class RichConsolePerformanceTest extends AbstractCrossVersionPerformanceTest {
         runner.tasksToRun = tasks.split(' ')
         runner.warmUpRuns = 5
         runner.runs = 8
-        runner.targetVersions = ["7.4.1-20220211002507+0000"]
+        runner.targetVersions = ["7.5-20220514015008+0000"]
 
         when:
         def result = runner.run()
