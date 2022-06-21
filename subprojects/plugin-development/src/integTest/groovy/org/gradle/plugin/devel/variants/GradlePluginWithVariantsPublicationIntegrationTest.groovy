ec0083d285d2d2ec19f7e3604c4fa873e53fab22
Paul Merlin <paul@gradle.com>
2022-06-21 16:58:19 +0200

    Add test for compiling plugin against plugin with multiple gradle-version variants
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/plugin-development/src/integTest/groovy/org/gradle/plugin/devel/variants/GradlePluginWithVariantsPublicationIntegrationTest.groovy
+++ subprojects/plugin-development/src/integTest/groovy/org/gradle/plugin/devel/variants/GradlePluginWithVariantsPublicationIntegrationTest.groovy
@@ -17,19 +17,97 @@
 package org.gradle.plugin.devel.variants
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.executer.ExecutionResult
 import org.gradle.util.Requires
 import org.gradle.util.TestPrecondition
 
+// older Gradle version 6.7.1 is used in test
+@Requires(TestPrecondition.JDK15_OR_EARLIER)
 class GradlePluginWithVariantsPublicationIntegrationTest extends AbstractIntegrationSpec {
 
-    @Requires(TestPrecondition.JDK15_OR_EARLIER) // older Gradle version 6.7.1 is used in test
     def "can publish and use Gradle plugin with multiple variants"() {
         given:
+        publishGradlePluginWithVariants()
+
+        and:
+        settingsFile """
+            pluginManagement {
+                repositories {
+                    maven { url = '${mavenRepo.uri}' }
+                }
+            }
+        """
+        buildFile """
+            plugins {
+                id('com.example.greeting') version '1.0'
+            }
+        """
+
+        when:
+        succeeds 'greet'
+
+        then:
+        outputContains("Hello from Gradle 7.0+")
+
+        when:
+        def gradle6Result = runWithGradle6('greet')
+
+        then:
+        gradle6Result.assertOutputContains("Hello from Gradle <7.0")
+    }
+
+    def "can publish and compile against Gradle plugin with multiple variants in buildSrc"() {
+        given:
+        publishGradlePluginWithVariants()
+
+        and:
+        file('buildSrc/settings.gradle') << ''
+        file('buildSrc/build.gradle') << """
+            plugins {
+                id('groovy-gradle-plugin')
+            }
+            dependencies {
+                implementation('com.example:producer:1.0')
+            }
+            repositories {
+                maven { url = '${mavenRepo.uri}' }
+            }
+        """
+        file('buildSrc/src/main/groovy/my-plugin.gradle') << '''
+            plugins {
+                id('com.example.greeting')
+            }
+        '''
+
+        and:
+        settingsFile ""
+        buildFile """
+            plugins {
+                id('my-plugin')
+            }
+        """
+
+        when:
+        succeeds 'greet'
+
+        then:
+        outputContains("Hello from Gradle 7.0+")
+
+        when:
+        def gradle6Result = runWithGradle6('greet')
+
+        then:
+        gradle6Result.assertOutputContains("Hello from Gradle <7.0")
+    }
+
+    private void publishGradlePluginWithVariants() {
+
+        // given:
         def producer = file('producer')
-        def consumer = file('consumer')
         def pluginModule = mavenRepo.module('com.example', 'producer', '1.0')
         def pluginMarker = mavenRepo.module('com.example.greeting', 'com.example.greeting.gradle.plugin', '1.0')
 
+        // and:
         producer.file('settings.gradle') << ''
         producer.file('build.gradle') << """
             plugins {
@@ -82,53 +160,37 @@ class GradlePluginWithVariantsPublicationIntegrationTest extends AbstractIntegra
         producer.file('src/main/java/example/plugin/GreetingPlugin.java') << pluginImplementation('<7.0')
         producer.file('src/gradle7/java/example/plugin/GreetingPlugin.java') << pluginImplementation('7.0+')
 
-        consumer.file('settings.gradle') << """
-            pluginManagement {
-                repositories {
-                    maven { url = '${mavenRepo.uri}' }
-                }
-            }
-        """
-        consumer.file('build.gradle') << """
-            plugins {
-                id('com.example.greeting') version '1.0'
-            }
-        """
-
-        when:
+        // when:
         projectDir(producer)
         succeeds 'publish'
 
-        then:
+        // then:
         pluginModule.assertPublished()
         pluginMarker.assertPublished()
         pluginModule.artifact().assertPublished()
         pluginModule.artifact(classifier: 'gradle7').assertPublished()
 
+        // and:
         def variants = pluginModule.parsedModuleMetadata.variants
-        variants.size() == 4
-        variants[0].name == "apiElements"
-        !variants[0].attributes.containsKey('org.gradle.plugin.api-version')
-        variants[1].name == "runtimeElements"
-        !variants[1].attributes.containsKey('org.gradle.plugin.api-version')
-        variants[2].name == "gradle7ApiElements"
-        variants[2].attributes['org.gradle.plugin.api-version'] == '7.0'
-        variants[3].name == "gradle7RuntimeElements"
-        variants[3].attributes['org.gradle.plugin.api-version'] == '7.0'
+        assert variants.size() == 4
+        assert variants[0].name == "apiElements"
+        assert !variants[0].attributes.containsKey('org.gradle.plugin.api-version')
+        assert variants[1].name == "runtimeElements"
+        assert !variants[1].attributes.containsKey('org.gradle.plugin.api-version')
+        assert variants[2].name == "gradle7ApiElements"
+        assert variants[2].attributes['org.gradle.plugin.api-version'] == '7.0'
+        assert variants[3].name == "gradle7RuntimeElements"
+        assert variants[3].attributes['org.gradle.plugin.api-version'] == '7.0'
 
-        and:
-        projectDir(consumer)
-        succeeds 'greet'
+        // cleanup:
+        projectDir(testDirectory)
+    }
 
-        then:
-        outputContains("Hello from Gradle 7.0+")
-
-        and:
-        def gradle6Executer = buildContext.distribution("6.7.1").executer(temporaryFolder, buildContext)
-        def gradle6Result = gradle6Executer.usingProjectDirectory(consumer).withTasks('greet').run()
-
-        then:
-        gradle6Result.assertOutputContains("Hello from Gradle <7.0")
+    private ExecutionResult runWithGradle6(String... tasks) {
+        return buildContext.distribution("6.7.1")
+            .executer(temporaryFolder, buildContext)
+            .withTasks('greet')
+            .run()
     }
 
     private static String pluginImplementation(String gradleVersion) {
@@ -154,5 +216,4 @@ class GradlePluginWithVariantsPublicationIntegrationTest extends AbstractIntegra
             }
         """
     }
-
 }
