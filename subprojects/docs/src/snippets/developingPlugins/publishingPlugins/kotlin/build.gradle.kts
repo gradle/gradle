ab21cf8141d3953eac1124085f7a9b6768f95016
József Bartók <jbartok@gradle.com>
2022-06-24 15:02:09 +0300

    Fix Kotlin sample
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/docs/src/snippets/developingPlugins/publishingPlugins/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/developingPlugins/publishingPlugins/kotlin/build.gradle.kts
@@ -9,8 +9,8 @@ group = "io.github.johndoe" // <1>
 version = "1.0" // <2>
 
 gradlePlugin { // <3>
-    website = "<substitute your project website>" // <4>
-    vcsUrl = "<uri to project source repository>" // <5>
+    website.set("<substitute your project website>") // <4>
+    vcsUrl.set("<uri to project source repository>") // <5>
 
     // ... // <6>
 }
