4bf8fe99702aea070a08c9333bdd3e0508e5897c
Thomas Tresansky <ttresansky@gradle.com>
2022-07-12 10:10:07 -0400

    Simplify samples to reference configurations as FileCollections
    - This allows for the same local to be used in dependsOn and within the doLast action closure without breaking configuration cache.
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/docs/src/snippets/testing/test-suite-multi-configure-each-matching/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/testing/test-suite-multi-configure-each-matching/kotlin/build.gradle.kts
@@ -46,22 +46,18 @@ testing {
 // end::multi-configure[]
 
 val checkDependencies by tasks.registering {
-    val testRuntimeClasspath = configurations.getByName("testRuntimeClasspath")
-    val integrationTestRuntimeClasspath = configurations.getByName("integrationTestRuntimeClasspath")
-    val functionalTestRuntimeClasspath = configurations.getByName("functionalTestRuntimeClasspath")
-
-    val testRuntimeClasspathFiles = testRuntimeClasspath.files
-    val integrationTestRuntimeClasspathFiles = integrationTestRuntimeClasspath.files
-    val functionalTestRuntimeClasspathFiles = functionalTestRuntimeClasspath.files
+    val testRuntimeClasspath: FileCollection = configurations.getByName("testRuntimeClasspath")
+    val integrationTestRuntimeClasspath: FileCollection = configurations.getByName("integrationTestRuntimeClasspath")
+    val functionalTestRuntimeClasspath: FileCollection = configurations.getByName("functionalTestRuntimeClasspath")
 
     dependsOn(testRuntimeClasspath, integrationTestRuntimeClasspath, functionalTestRuntimeClasspath)
     doLast {
-        assert(testRuntimeClasspathFiles.size == 12)
-        assert(testRuntimeClasspathFiles.any { it.name == "mockito-junit-jupiter-4.6.1.jar" })
-        assert(integrationTestRuntimeClasspathFiles.size == 12)
-        assert(integrationTestRuntimeClasspathFiles.any { it.name == "mockito-junit-jupiter-4.6.1.jar" })
-        assert(functionalTestRuntimeClasspathFiles.size == 3)
-        assert(functionalTestRuntimeClasspathFiles.any { it.name == "junit-4.13.2.jar" })
-        assert(functionalTestRuntimeClasspathFiles.any { it.name == "commons-lang3-3.11.jar" })
+        assert(testRuntimeClasspath.files.size == 12)
+        assert(testRuntimeClasspath.files.any { it.name == "mockito-junit-jupiter-4.6.1.jar" })
+        assert(integrationTestRuntimeClasspath.files.size == 12)
+        assert(integrationTestRuntimeClasspath.files.any { it.name == "mockito-junit-jupiter-4.6.1.jar" })
+        assert(functionalTestRuntimeClasspath.files.size == 3)
+        assert(functionalTestRuntimeClasspath.files.any { it.name == "junit-4.13.2.jar" })
+        assert(functionalTestRuntimeClasspath.files.any { it.name == "commons-lang3-3.11.jar" })
     }
 }
