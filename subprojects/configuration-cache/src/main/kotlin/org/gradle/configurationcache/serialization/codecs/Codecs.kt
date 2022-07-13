16c4503151610c4581d576f28c6031aaf4fd61da
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2022-07-13 11:13:49 -0300

    Let config cache serialize `CopyOnWriteArrayList` instances

--- subprojects/configuration-cache/src/main/kotlin/org/gradle/configurationcache/serialization/codecs/Codecs.kt
+++ subprojects/configuration-cache/src/main/kotlin/org/gradle/configurationcache/serialization/codecs/Codecs.kt
@@ -289,6 +289,7 @@ class Codecs(
         // Only serialize certain List implementations
         bind(arrayListCodec)
         bind(linkedListCodec)
+        bind(copyOnWriteArrayListCodec)
         bind(ImmutableListCodec)
 
         // Only serialize certain Set implementations for now, as some custom types extend Set (eg DomainObjectContainer)
