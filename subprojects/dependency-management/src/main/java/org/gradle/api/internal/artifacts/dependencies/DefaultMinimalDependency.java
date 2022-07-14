882d304d6c3f002cfc44b13a83ee4a482b5517f5
Octavia Togami <otogami@gradle.com>
2022-07-14 15:10:18 -0700

    Make version catalogs implement Dependency
    There's mutation concerns here, but this is a good rough draft
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/dependencies/DefaultMinimalDependency.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/dependencies/DefaultMinimalDependency.java
@@ -21,60 +21,8 @@ import org.gradle.api.artifacts.MutableVersionConstraint;
 
 import java.io.Serializable;
 
-public class DefaultMinimalDependency implements MinimalExternalModuleDependency, Serializable {
-    private final ModuleIdentifier module;
-    private final MutableVersionConstraint versionConstraint;
-    private final int hashCode;
-
+public class DefaultMinimalDependency extends DefaultExternalModuleDependency implements MinimalExternalModuleDependency, Serializable {
     public DefaultMinimalDependency(ModuleIdentifier module, MutableVersionConstraint versionConstraint) {
-        this.module = module;
-        this.versionConstraint = versionConstraint;
-        this.hashCode = doComputeHashCode();
-    }
-
-    @Override
-    public ModuleIdentifier getModule() {
-        return module;
-    }
-
-    @Override
-    public MutableVersionConstraint getVersionConstraint() {
-        return versionConstraint;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        DefaultMinimalDependency that = (DefaultMinimalDependency) o;
-
-        if (!module.equals(that.module)) {
-            return false;
-        }
-        return versionConstraint.equals(that.versionConstraint);
-    }
-
-    @Override
-    public int hashCode() {
-        return hashCode;
-    }
-
-    private int doComputeHashCode() {
-        int result = module.hashCode();
-        result = 31 * result + versionConstraint.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        String versionConstraintAsString = versionConstraint.toString();
-        return versionConstraintAsString.isEmpty()
-            ? module.toString()
-            : module + ":" + versionConstraintAsString;
+        super(module, versionConstraint);
     }
 }
