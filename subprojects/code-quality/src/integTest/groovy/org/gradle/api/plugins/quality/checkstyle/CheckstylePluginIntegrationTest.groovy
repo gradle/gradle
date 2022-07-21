3071143beeec71a95e64d962cc39aca4bde89ff1
Paul Merlin <paul@gradle.com>
2022-07-21 17:39:29 +0200

    Add failing test for regression #21301
    Signed-off-by: Paul Merlin <paul@gradle.com>
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/code-quality/src/integTest/groovy/org/gradle/api/plugins/quality/checkstyle/CheckstylePluginIntegrationTest.groovy
+++ subprojects/code-quality/src/integTest/groovy/org/gradle/api/plugins/quality/checkstyle/CheckstylePluginIntegrationTest.groovy
@@ -15,6 +15,8 @@
  */
 package org.gradle.api.plugins.quality.checkstyle
 import org.gradle.integtests.fixtures.WellBehavedPluginTest
+import org.gradle.util.internal.ToBeImplemented
+import spock.lang.Issue
 
 class CheckstylePluginIntegrationTest extends WellBehavedPluginTest {
     @Override
@@ -26,5 +28,30 @@ class CheckstylePluginIntegrationTest extends WellBehavedPluginTest {
         buildFile << """
             apply plugin: 'groovy'
         """
+    }
+
+    @Issue("https://github.com/gradle/gradle/issues/21301")
+    @ToBeImplemented
+    def "can pass a URL in configProperties"() {
+        given:
+        buildFile """
+            apply plugin: 'checkstyle'
+
+            dependencies { implementation localGroovy() }
+            repositories { mavenCentral() }
+
+            checkstyle {
+                configProperties["some"] = new URL("https://gradle.org/")
+            }
+        """
+        file('src/main/java/Some.java') << """
+            public class Some {}
+        """
+
+        when:
+        fails 'check'
+
+        then:
+        failureHasCause("Could not serialize unit of work.")
     }
 }
