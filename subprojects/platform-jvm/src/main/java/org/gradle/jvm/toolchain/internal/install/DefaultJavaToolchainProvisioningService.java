9437fd5d03bca5ee1b095dfa78197e5fd5fae33c
József Bartók <jbartok@gradle.com>
2022-07-21 10:24:03 +0300

    Issue deprecation warning only if provisioning is actually used

--- subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/internal/install/DefaultJavaToolchainProvisioningService.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/internal/install/DefaultJavaToolchainProvisioningService.java
@@ -19,6 +19,7 @@ package org.gradle.jvm.toolchain.internal.install;
 import org.gradle.api.GradleException;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.provider.ProviderFactory;
+import org.gradle.internal.deprecation.DeprecationLogger;
 import org.gradle.internal.resource.ExternalResource;
 import org.gradle.internal.resource.ResourceExceptions;
 import org.gradle.internal.resource.metadata.ExternalResourceMetaData;
@@ -37,7 +38,6 @@ import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.io.File;
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 
@@ -54,7 +54,7 @@ public class DefaultJavaToolchainProvisioningService implements JavaToolchainPro
 
     }
 
-    private final List<JavaToolchainRepository> toolchainRegistries;
+    private final JavaToolchainRepositoryRegistryInternal toolchainRepositoryRegistry;
 
     private final FileDownloader downloader;
     private final JdkCacheDirectory cacheDirProvider;
@@ -70,7 +70,7 @@ public class DefaultJavaToolchainProvisioningService implements JavaToolchainPro
             ProviderFactory factory,
             BuildOperationExecutor executor
     ) {
-        this.toolchainRegistries = ((JavaToolchainRepositoryRegistryInternal) toolchainRepositoryRegistry).requestedRepositories();
+        this.toolchainRepositoryRegistry = (JavaToolchainRepositoryRegistryInternal) toolchainRepositoryRegistry;
         this.downloader = downloader;
         this.cacheDirProvider = cacheDirProvider;
         this.downloadEnabled = factory.gradleProperty(AUTO_DOWNLOAD).map(Boolean::parseBoolean);
@@ -82,7 +82,14 @@ public class DefaultJavaToolchainProvisioningService implements JavaToolchainPro
             return Optional.empty();
         }
 
-        for (JavaToolchainRepository toolchainRepository : toolchainRegistries) {
+        if (!toolchainRepositoryRegistry.hasExplicitRequests()) {
+            DeprecationLogger.warnOfChangedBehaviour("Starting from Gradle 8.0 there will be no default Java Toolchain Registry.",
+                            "Need to inject such registries via settings plugins and explicitly request them via the 'toolchainManagement' block.")
+                    .undocumented() //TODO (#21082): needs to be documented properly
+                    .nagUser();
+        }
+
+        for (JavaToolchainRepository toolchainRepository : toolchainRepositoryRegistry.requestedRepositories()) {
             Optional<URI> uri = toolchainRepository.toUri(spec);
             if (uri.isPresent()) {
                 return Optional.of(provisionInstallation(spec, uri.get()));
@@ -135,6 +142,7 @@ public class DefaultJavaToolchainProvisioningService implements JavaToolchainPro
     private <T> T wrapInOperation(String displayName, Callable<T> provisioningStep) {
         return buildOperationExecutor.call(new ToolchainProvisioningBuildOperation<>(displayName, provisioningStep));
     }
+
     private static class ToolchainProvisioningBuildOperation<T> implements CallableBuildOperation<T> {
         private final String displayName;
         private final Callable<T> provisioningStep;
@@ -152,8 +160,8 @@ public class DefaultJavaToolchainProvisioningService implements JavaToolchainPro
         @Override
         public BuildOperationDescriptor.Builder description() {
             return BuildOperationDescriptor
-                .displayName(displayName)
-                .progressDisplayName(displayName);
+                    .displayName(displayName)
+                    .progressDisplayName(displayName);
         }
     }
 }
