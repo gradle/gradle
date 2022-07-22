68a808a409b93dd9fbdd9566c9a4e42cb97feca4
Adam Murdoch <adam@gradle.com>
2022-07-22 10:00:40 +1000

    Add an internal option to collect and report on various worker thread statistics.
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/core/src/main/java/org/gradle/execution/plan/DefaultPlanExecutor.java
+++ subprojects/core/src/main/java/org/gradle/execution/plan/DefaultPlanExecutor.java
@@ -16,8 +16,11 @@
 
 package org.gradle.execution.plan;
 
+import org.gradle.StartParameter;
 import org.gradle.api.Action;
 import org.gradle.api.NonNullApi;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 import org.gradle.concurrent.ParallelismConfiguration;
 import org.gradle.initialization.BuildCancellationToken;
 import org.gradle.internal.Cast;
@@ -29,32 +32,32 @@ import org.gradle.internal.concurrent.ManagedExecutor;
 import org.gradle.internal.concurrent.Stoppable;
 import org.gradle.internal.logging.text.TreeFormatter;
 import org.gradle.internal.resources.ResourceLockCoordinationService;
-import org.gradle.internal.time.Time;
-import org.gradle.internal.time.TimeFormatting;
-import org.gradle.internal.time.Timer;
 import org.gradle.internal.work.WorkerLeaseRegistry.WorkerLease;
 import org.gradle.internal.work.WorkerLeaseService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.io.Closeable;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.ToLongFunction;
 
 import static org.gradle.internal.resources.ResourceLockState.Disposition.FINISHED;
 import static org.gradle.internal.resources.ResourceLockState.Disposition.RETRY;
 
 @NonNullApi
 public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultPlanExecutor.class);
+    public static final String STAT_PROPERTY_NAME = "org.gradle.internal.executor.stats";
+    private static final Logger LOGGER = Logging.getLogger(DefaultPlanExecutor.class);
     private final int executorCount;
     private final WorkerLeaseService workerLeaseService;
     private final BuildCancellationToken cancellationToken;
@@ -62,8 +65,16 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
     private final ManagedExecutor executor;
     private final MergedQueues queue;
     private final AtomicBoolean workersStarted = new AtomicBoolean();
+    private final ExecutorStats stats;
 
-    public DefaultPlanExecutor(ParallelismConfiguration parallelismConfiguration, ExecutorFactory executorFactory, WorkerLeaseService workerLeaseService, BuildCancellationToken cancellationToken, ResourceLockCoordinationService coordinationService) {
+    public DefaultPlanExecutor(
+        ParallelismConfiguration parallelismConfiguration,
+        ExecutorFactory executorFactory,
+        WorkerLeaseService workerLeaseService,
+        BuildCancellationToken cancellationToken,
+        ResourceLockCoordinationService coordinationService,
+        StartParameter startParameter
+    ) {
         this.cancellationToken = cancellationToken;
         this.coordinationService = coordinationService;
         int numberOfParallelExecutors = parallelismConfiguration.getMaxWorkerCount();
@@ -73,13 +84,18 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
 
         this.executorCount = numberOfParallelExecutors;
         this.workerLeaseService = workerLeaseService;
+        this.stats = startParameter.getSystemPropertiesArgs().getOrDefault(STAT_PROPERTY_NAME, "false").equalsIgnoreCase("true") ? new CollectingExecutorStats() : new NoOpStats();
         this.queue = new MergedQueues(coordinationService, false);
         this.executor = executorFactory.create("Execution worker");
     }
 
     @Override
     public void stop() {
-        CompositeStoppable.stoppable(queue, executor).stop();
+        try {
+            CompositeStoppable.stoppable(queue, executor).stop();
+        } finally {
+            stats.report();
+        }
     }
 
     @Override
@@ -93,7 +109,7 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
         WorkerLease currentWorkerLease = workerLeaseService.getCurrentWorkerLease();
         MergedQueues thisPlanOnly = new MergedQueues(coordinationService, true);
         thisPlanOnly.add(planDetails);
-        new ExecutorWorker(thisPlanOnly, currentWorkerLease, cancellationToken, coordinationService, workerLeaseService).run();
+        new ExecutorWorker(thisPlanOnly, currentWorkerLease, cancellationToken, coordinationService, workerLeaseService, stats).run();
 
         List<Throwable> failures = new ArrayList<>();
         awaitCompletion(workSource, currentWorkerLease, failures);
@@ -132,7 +148,7 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
         if (workersStarted.compareAndSet(false, true)) {
             LOGGER.debug("Using {} parallel executor threads", executorCount);
             for (int i = 1; i < executorCount; i++) {
-                executor.execute(new ExecutorWorker(queue, null, cancellationToken, coordinationService, workerLeaseService));
+                executor.execute(new ExecutorWorker(queue, null, cancellationToken, coordinationService, workerLeaseService, stats));
             }
         }
     }
@@ -302,27 +318,26 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
         private final BuildCancellationToken cancellationToken;
         private final ResourceLockCoordinationService coordinationService;
         private final WorkerLeaseService workerLeaseService;
+        private final WorkerStats stats;
 
         private ExecutorWorker(
             MergedQueues queue,
             @Nullable WorkerLease workerLease,
             BuildCancellationToken cancellationToken,
             ResourceLockCoordinationService coordinationService,
-            WorkerLeaseService workerLeaseService
+            WorkerLeaseService workerLeaseService,
+            ExecutorStats executorStats
         ) {
             this.queue = queue;
             this.workerLease = workerLease;
             this.cancellationToken = cancellationToken;
             this.coordinationService = coordinationService;
             this.workerLeaseService = workerLeaseService;
+            this.stats = executorStats.startWorker();
         }
 
         @Override
         public void run() {
-            final AtomicLong busy = new AtomicLong(0);
-            Timer totalTimer = Time.startTimer();
-            final Timer executionTimer = Time.startTimer();
-
             boolean releaseLeaseOnCompletion;
             if (workerLease == null) {
                 workerLease = workerLeaseService.newWorkerLease();
@@ -338,24 +353,14 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
                 }
                 Object selected = workItem.selection.getItem();
                 LOGGER.info("{} ({}) started.", selected, Thread.currentThread());
-                executionTimer.reset();
                 execute(selected, workItem.plan, workItem.executor);
-                long duration = executionTimer.getElapsedMillis();
-                busy.addAndGet(duration);
-                if (LOGGER.isInfoEnabled()) {
-                    LOGGER.info("{} ({}) completed. Took {}.", selected, Thread.currentThread(), TimeFormatting.formatDurationVerbose(duration));
-                }
             }
 
             if (releaseLeaseOnCompletion) {
                 coordinationService.withStateLock(() -> workerLease.unlock());
             }
 
-            long total = totalTimer.getElapsedMillis();
-
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Execution worker [{}] finished, busy: {}, idle: {}", Thread.currentThread(), TimeFormatting.formatDurationVerbose(busy.get()), TimeFormatting.formatDurationVerbose(total - busy.get()));
-            }
+            stats.finish();
         }
 
         /**
@@ -366,49 +371,55 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
          */
         @Nullable
         private WorkItem getNextItem(final WorkerLease workerLease) {
-            final MutableReference<WorkItem> selected = MutableReference.empty();
-            coordinationService.withStateLock(resourceLockState -> {
-                if (cancellationToken.isCancellationRequested()) {
-                    queue.cancelExecution();
-                }
-
-                WorkSource.State state = queue.executionState();
-                if (state == WorkSource.State.NoMoreWorkToStart) {
-                    return FINISHED;
-                } else if (state == WorkSource.State.NoWorkReadyToStart) {
-                    // Release worker lease while waiting
-                    if (workerLease.isLockedByCurrentThread()) {
-                        workerLease.unlock();
+            final MutableReference<WorkItem> selected;
+            stats.startSelect();
+            try {
+                selected = MutableReference.empty();
+                coordinationService.withStateLock(resourceLockState -> {
+                    if (cancellationToken.isCancellationRequested()) {
+                        queue.cancelExecution();
                     }
-                    return RETRY;
-                }
-                // Else there may be items ready, acquire a worker lease
 
-                boolean hasWorkerLease = workerLease.isLockedByCurrentThread();
-                if (!hasWorkerLease && !workerLease.tryLock()) {
-                    // Cannot get a lease to run work
-                    return RETRY;
-                }
+                    WorkSource.State state = queue.executionState();
+                    if (state == WorkSource.State.NoMoreWorkToStart) {
+                        return FINISHED;
+                    } else if (state == WorkSource.State.NoWorkReadyToStart) {
+                        // Release worker lease while waiting
+                        if (workerLease.isLockedByCurrentThread()) {
+                            workerLease.unlock();
+                        }
+                        return RETRY;
+                    }
+                    // Else there may be items ready, acquire a worker lease
 
-                WorkSource.Selection<WorkItem> workItem;
-                try {
-                    workItem = queue.selectNext();
-                } catch (Throwable t) {
-                    resourceLockState.releaseLocks();
-                    queue.abortAllAndFail(t);
+                    boolean hasWorkerLease = workerLease.isLockedByCurrentThread();
+                    if (!hasWorkerLease && !workerLease.tryLock()) {
+                        // Cannot get a lease to run work
+                        return RETRY;
+                    }
+
+                    WorkSource.Selection<WorkItem> workItem;
+                    try {
+                        workItem = queue.selectNext();
+                    } catch (Throwable t) {
+                        resourceLockState.releaseLocks();
+                        queue.abortAllAndFail(t);
+                        return FINISHED;
+                    }
+                    if (workItem.isNoMoreWorkToStart()) {
+                        return FINISHED;
+                    } else if (workItem.isNoWorkReadyToStart()) {
+                        // Release worker lease while waiting
+                        workerLease.unlock();
+                        return RETRY;
+                    }
+
+                    selected.set(workItem.getItem());
                     return FINISHED;
-                }
-                if (workItem.isNoMoreWorkToStart()) {
-                    return FINISHED;
-                } else if (workItem.isNoWorkReadyToStart()) {
-                    // Release worker lease while waiting
-                    workerLease.unlock();
-                    return RETRY;
-                }
-
-                selected.set(workItem.getItem());
-                return FINISHED;
-            });
+                });
+            } finally {
+                stats.finishSelect();
+            }
 
             return selected.get();
         }
@@ -416,10 +427,13 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
         private void execute(Object selected, WorkSource<Object> executionPlan, Action<Object> worker) {
             Throwable failure = null;
             try {
+                stats.startExecute();
                 try {
                     worker.execute(selected);
                 } catch (Throwable t) {
                     failure = t;
+                } finally {
+                    stats.finishExecute();
                 }
             } finally {
                 markFinished(selected, executionPlan, failure);
@@ -427,16 +441,174 @@ public class DefaultPlanExecutor implements PlanExecutor, Stoppable {
         }
 
         private void markFinished(Object selected, WorkSource<Object> executionPlan, @Nullable Throwable failure) {
-            coordinationService.withStateLock(() -> {
-                try {
-                    executionPlan.finishedExecuting(selected, failure);
-                } catch (Throwable t) {
-                    queue.abortAllAndFail(t);
-                }
-                // Notify other threads that the item is finished as this may unblock further work
-                // or this might be the last item in the queue
-                coordinationService.notifyStateChange();
-            });
+            stats.startMarkFinished();
+            try {
+                coordinationService.withStateLock(() -> {
+                    try {
+                        executionPlan.finishedExecuting(selected, failure);
+                    } catch (Throwable t) {
+                        queue.abortAllAndFail(t);
+                    }
+                    // Notify other threads that the item is finished as this may unblock further work
+                    // or this might be the last item in the queue
+                    coordinationService.notifyStateChange();
+                });
+            } finally {
+                stats.finishMarkFinished();
+            }
+        }
+    }
+
+    /**
+     * Implementations must be thread safe.
+     */
+    private interface ExecutorStats {
+        void report();
+
+        WorkerStats startWorker();
+    }
+
+    /**
+     * Implementations are only used by the worker thead and do not need to be thread safe.
+     */
+    private interface WorkerStats {
+        void startSelect();
+
+        void finishSelect();
+
+        void startExecute();
+
+        void finishExecute();
+
+        void startMarkFinished();
+
+        void finishMarkFinished();
+
+        void finish();
+    }
+
+    private static class NoOpStats implements WorkerStats, ExecutorStats {
+        @Override
+        public WorkerStats startWorker() {
+            return this;
+        }
+
+        @Override
+        public void startSelect() {
+        }
+
+        @Override
+        public void finishSelect() {
+        }
+
+        @Override
+        public void startExecute() {
+        }
+
+        @Override
+        public void finishExecute() {
+        }
+
+        @Override
+        public void startMarkFinished() {
+        }
+
+        @Override
+        public void finishMarkFinished() {
+        }
+
+        @Override
+        public void finish() {
+        }
+
+        @Override
+        public void report() {
+        }
+    }
+
+    private static class CollectingExecutorStats implements ExecutorStats {
+        private final List<CollectingWorkerStats> workers = new CopyOnWriteArrayList<>();
+
+        @Override
+        public WorkerStats startWorker() {
+            return new CollectingWorkerStats(this);
+        }
+
+        @Override
+        public void report() {
+            LOGGER.lifecycle("WORKER THREAD STATISTICS");
+            int workerCount = workers.size();
+            LOGGER.lifecycle("worker count: " + workerCount);
+            if (workerCount > 0) {
+                LOGGER.lifecycle("average select time: " + format(stats -> stats.totalSelectTime));
+                LOGGER.lifecycle("average execute time: " + format(stats -> stats.totalExecuteTime));
+                LOGGER.lifecycle("average finish time: " + format(stats -> stats.totalMarkFinishedTime));
+            }
+            workers.clear();
+        }
+
+        private String format(ToLongFunction<CollectingWorkerStats> statsProperty) {
+            BigDecimal averageNanos = BigDecimal.valueOf(workers.stream().mapToLong(statsProperty).sum() / workers.size());
+            return DecimalFormat.getNumberInstance().format(averageNanos.divide(BigDecimal.valueOf(1000000), RoundingMode.HALF_UP)) + "ms";
+        }
+    }
+
+    private static class CollectingWorkerStats implements WorkerStats {
+        final long startTime;
+        private final CollectingExecutorStats owner;
+        long finishTime;
+        long startCurrentOperation;
+        long totalSelectTime;
+        long totalExecuteTime;
+        long totalMarkFinishedTime;
+
+        public CollectingWorkerStats(CollectingExecutorStats owner) {
+            this.owner = owner;
+            startTime = System.nanoTime();
+        }
+
+        public void finish() {
+            finishTime = System.nanoTime();
+            owner.workers.add(this);
+        }
+
+        @Override
+        public void startSelect() {
+            startCurrentOperation = System.nanoTime();
+        }
+
+        @Override
+        public void finishSelect() {
+            long duration = System.nanoTime() - startCurrentOperation;
+            if (duration > 0) {
+                totalSelectTime += duration;
+            }
+        }
+
+        @Override
+        public void startExecute() {
+            startCurrentOperation = System.nanoTime();
+        }
+
+        @Override
+        public void finishExecute() {
+            long duration = System.nanoTime() - startCurrentOperation;
+            if (duration > 0) {
+                totalExecuteTime += duration;
+            }
+        }
+
+        @Override
+        public void startMarkFinished() {
+            startCurrentOperation = System.nanoTime();
+        }
+
+        @Override
+        public void finishMarkFinished() {
+            long duration = System.nanoTime() - startCurrentOperation;
+            if (duration > 0) {
+                totalMarkFinishedTime += duration;
+            }
         }
     }
 }
