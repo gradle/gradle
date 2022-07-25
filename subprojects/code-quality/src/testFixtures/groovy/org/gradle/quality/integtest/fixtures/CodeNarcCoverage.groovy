fe23275f1bf8aadd6559f16cf6a9efed8c67fee1
Kyle Moore <kmoore@gradle.com>
2022-07-25 11:56:26 -0700

    Fix CodeNarcPluginVersionIntegrationTest; there is now only 1 compatible version with Groovy 4 and JDK is ireelevant
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/code-quality/src/testFixtures/groovy/org/gradle/quality/integtest/fixtures/CodeNarcCoverage.groovy
+++ subprojects/code-quality/src/testFixtures/groovy/org/gradle/quality/integtest/fixtures/CodeNarcCoverage.groovy
@@ -16,24 +16,12 @@
 
 package org.gradle.quality.integtest.fixtures
 
-import org.gradle.api.JavaVersion
 import org.gradle.api.plugins.quality.CodeNarcPlugin
-import org.gradle.util.internal.VersionNumber
 
 class CodeNarcCoverage {
-    private static final List<String> ALL = [CodeNarcPlugin.DEFAULT_CODENARC_VERSION, "0.17", "0.21", "0.23", "0.24.1", /*"0.25.2",*/ "1.0", "1.6.1"].asImmutable()
-
-    private static final List<String> JDK11_SUPPORTED = versionsAboveInclusive(ALL, "0.23")
-    private static final List<String> JDK14_SUPPORTED = JDK11_SUPPORTED - ['1.6.1', '1.0']
+    private static final List<String> ALL = [CodeNarcPlugin.DEFAULT_CODENARC_VERSION].asImmutable()
 
     static List<String> getSupportedVersionsByJdk() {
-        if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_14)) {
-            return JDK14_SUPPORTED
-        }
-        return JavaVersion.current().java11Compatible ? JDK11_SUPPORTED : ALL
-    }
-
-    private static List<String> versionsAboveInclusive(List<String> versionsToFilter, String threshold) {
-        versionsToFilter.findAll { VersionNumber.parse(it) >= VersionNumber.parse(threshold) }.asImmutable()
+        ALL
     }
 }
