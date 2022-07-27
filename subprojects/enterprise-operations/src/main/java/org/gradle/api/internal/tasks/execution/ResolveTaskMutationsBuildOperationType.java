6e5d4f1ce2c1cb53cfd2be7ad9439c166926ad60
Stefan Wolf <wolf@gradle.com>
2022-07-27 13:23:43 +0200

    Polish Javadoc

--- subprojects/enterprise-operations/src/main/java/org/gradle/api/internal/tasks/execution/ResolveTaskMutationsBuildOperationType.java
+++ subprojects/enterprise-operations/src/main/java/org/gradle/api/internal/tasks/execution/ResolveTaskMutationsBuildOperationType.java
@@ -22,12 +22,12 @@ import org.gradle.internal.operations.BuildOperationType;
  * Represents resolving the mutations of a task.
  * <p>
  * Before task execution ({@link org.gradle.api.internal.tasks.execution.ExecuteTaskBuildOperationType}),
- * the mutation of the tasks need to be resolved. The mutations of the task are the locations it may modify,
+ * the mutations of the task need to be resolved. The mutations of a task are the locations it may modify,
  * e.g. its outputs, destroyables and local state. In order to resolve the mutations, the task input and output
  * properties are detected as well. Since Gradle 7.5, resolving of the mutations happens in a separate node
  * in the execution graph.
  *
- * @since 6.7
+ * @since 7.6
  */
 public final class ResolveTaskMutationsBuildOperationType implements BuildOperationType<ResolveTaskMutationsBuildOperationType.Details, ResolveTaskMutationsBuildOperationType.Result> {
     public interface Details {
