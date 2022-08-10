90a92fc67fd32481bff4c801f9ddcaf3ec8baa3b
Kyle Moore <kmoore@gradle.com>
2022-08-09 17:20:23 -0700

    Revert "perf: Avoid creating intermediate list when copying array"
    This reverts commit 8ea37e2ac493ac65b0f38e10639956bc05692533.
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/core/src/main/java/org/gradle/listener/ClosureBackedMethodInvocationDispatch.java
+++ subprojects/core/src/main/java/org/gradle/listener/ClosureBackedMethodInvocationDispatch.java
@@ -36,7 +36,7 @@ public class ClosureBackedMethodInvocationDispatch implements Dispatch<MethodInv
         if (message.getMethod().getName().equals(methodName)) {
             Object[] parameters = message.getArguments();
             if (closure.getMaximumNumberOfParameters() < parameters.length) {
-                parameters = Arrays.copyOf(parameters, closure.getMaximumNumberOfParameters());
+                parameters = Arrays.asList(parameters).subList(0, closure.getMaximumNumberOfParameters()).toArray();
             }
             closure.call(parameters);
         }
