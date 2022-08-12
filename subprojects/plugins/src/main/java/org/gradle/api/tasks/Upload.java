b42e1ecacf6c3c8592fa04556f543f1a2314aba5
Thomas Tresansky <ttresansky@gradle.com>
2022-08-12 07:31:48 -0400

    Fix arch unit and checkstyle violations

--- subprojects/plugins/src/main/java/org/gradle/api/tasks/Upload.java
+++ subprojects/plugins/src/main/java/org/gradle/api/tasks/Upload.java
@@ -22,7 +22,6 @@ import org.gradle.api.Action;
 import org.gradle.api.InvalidUserCodeException;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
-import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.internal.ConventionTask;
 import org.gradle.api.internal.DocumentationRegistry;
