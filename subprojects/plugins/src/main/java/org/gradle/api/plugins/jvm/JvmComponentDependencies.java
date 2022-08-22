1241228e7a23b783170c0fbd1e44a1f874aac960
Sterling Greene <sterling@gradle.com>
2022-08-22 16:44:35 -0400

    Try Modifiers as a name

--- subprojects/plugins/src/main/java/org/gradle/api/plugins/jvm/JvmComponentDependencies.java
+++ subprojects/plugins/src/main/java/org/gradle/api/plugins/jvm/JvmComponentDependencies.java
@@ -33,7 +33,7 @@ import org.gradle.api.artifacts.dsl.DependencyAdder;
  * @since 7.3
  */
 @Incubating
-public interface JvmComponentDependencies extends PlatformDependencyDecorators, TestFixturesDependencyDecorators, GradleDependencies {
+public interface JvmComponentDependencies extends PlatformDependencyModifiers, TestFixturesDependencyModifiers, GradleDependencies {
     /**
      * Returns a {@link DependencyAdder} to add to the set of implementation dependencies.
      * <p>
