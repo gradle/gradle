4589406491ee3b421604e8a87f7f2046dcaf19fc
Rafael Chaves <rchaves@gradle.com>
2022-09-02 16:44:48 -0300

    Fix most maven-publish integration tests under CC
    Configuration cache causes Maven Publish to resolve
    dependencies eagerly during configuration, so tests
    must declare repositories containing any required dependencies,
    or (if dependency resolution is not the focus)
    avoid the resolution of the `api` configuration during configuration
    caching, which is accomplished via the replacement of `compileJava.classpath`.
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishBasicIntegTest.groovy
+++ subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishBasicIntegTest.groovy
@@ -16,8 +16,6 @@
 
 package org.gradle.api.publish.maven
 
-
-import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
 import org.gradle.integtests.fixtures.publish.maven.AbstractMavenPublishIntegTest
 import org.gradle.test.fixtures.maven.MavenLocalRepository
 import org.gradle.util.SetSystemProperties
@@ -420,7 +418,6 @@ class MavenPublishBasicIntegTest extends AbstractMavenPublishIntegTest {
         succeeds 'publish'
     }
 
-    @ToBeFixedForConfigurationCache(because = "configuration cache doesn't support task failures")
     @Issue("https://github.com/gradle/gradle/issues/15009")
     def "fails publishing if a variant contains a dependency on an enforced platform"() {
         settingsFile << """
@@ -430,6 +427,11 @@ class MavenPublishBasicIntegTest extends AbstractMavenPublishIntegTest {
             plugins {
                 id 'java'
                 id 'maven-publish'
+            }
+
+            tasks.compileJava {
+                // Avoid resolving the classpath when caching the configuration
+                classpath = files()
             }
 
             dependencies {
@@ -458,8 +460,8 @@ In general publishing dependencies to enforced platforms is a mistake: enforced 
     }
 
     @Issue("https://github.com/gradle/gradle/issues/15009")
-    @ToBeFixedForConfigurationCache(because = "configuration cache doesn't support task failures")
     def "can disable validation of publication of dependencies on enforced platforms"() {
+        mavenRepo.module("org", "platform", "1.0").asGradlePlatform().publish()
         settingsFile << """
             rootProject.name = 'publish'
         """
@@ -471,6 +473,11 @@ In general publishing dependencies to enforced platforms is a mistake: enforced 
 
             group = 'com.acme'
             version = '0.999'
+
+            tasks.compileJava {
+                // Avoid resolving the classpath when caching the configuration
+                classpath = files()
+            }
 
             dependencies {
                 implementation enforcedPlatform('org:platform:1.0')
