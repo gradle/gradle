4417fe1ad9f4b2836fb30449977cc2342122c6b0
Rafael Chaves <rchaves@gradle.com>
2022-09-02 16:44:48 -0300

    Fix most maven-publish integration tests under CC
    Configuration cache causes Maven Publish to resolve
    dependencies eagerly during configuration, so tests
    must declare repositories containing any required dependencies,
    or (if dependency resolution is not the focus)
    avoid the resolution of the `api` configuration during configuration
    caching, which is accomplished via the replacement of `compileJava.classpath`.

--- subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishFeaturesJavaIntegTest.groovy
+++ subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/MavenPublishFeaturesJavaIntegTest.groovy
@@ -16,12 +16,10 @@
 
 package org.gradle.api.publish.maven
 
-import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
 import spock.lang.Unroll
 
 class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJavaIntegTest {
 
-    @ToBeFixedForConfigurationCache
     def "can publish java-library with a feature"() {
         mavenRepo.module('org', 'optionaldep', '1.0').withModuleMetadata().publish()
 
@@ -39,6 +37,11 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
                     outgoing.capability("org:optional-feature:\${version}")
                 }
                 compileClasspath.extendsFrom(optionalFeatureImplementation)
+            }
+
+            tasks.compileJava {
+                // Avoid resolving the classpath when caching the configuration
+                classpath = files()
             }
 
             dependencies {
@@ -89,7 +92,6 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
         }
     }
 
-    @ToBeFixedForConfigurationCache
     def "can group dependencies by feature"() {
         mavenRepo.module('org', 'optionaldep-g1', '1.0').publish()
         mavenRepo.module('org', 'optionaldep1-g2', '1.0').publish()
@@ -121,6 +123,11 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
                     outgoing.capability("org:optional-feature2:\${version}")
                 }
                 compileClasspath.extendsFrom(optionalFeature2Implementation)
+            }
+
+            tasks.compileJava {
+                // Avoid resolving the classpath when caching the configuration
+                classpath = files()
             }
 
             dependencies {
@@ -173,7 +180,6 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
     }
 
     @Unroll("publish java-library with feature with additional artifact #id (#optionalFeatureFileName)")
-    @ToBeFixedForConfigurationCache
     def "publish java-library with feature with additional artifact"() {
         mavenRepo.module('org', 'optionaldep', '1.0').withModuleMetadata().publish()
 
@@ -191,6 +197,11 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
                     outgoing.capability("org:optional-feature:\${version}")
                 }
                 compileClasspath.extendsFrom(optionalFeatureImplementation)
+            }
+
+            tasks.compileJava {
+                // Avoid resolving the classpath when caching the configuration
+                classpath = files()
             }
 
             dependencies {
@@ -277,7 +288,6 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
 
     }
 
-    @ToBeFixedForConfigurationCache
     def "can publish java-library with a feature from a configuration with more than one outgoing variant"() {
         mavenRepo.module('org', 'optionaldep', '1.0').withModuleMetadata().publish()
 
@@ -295,6 +305,11 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
                     outgoing.capability("org:optional-feature:\${version}")
                 }
                 compileClasspath.extendsFrom(optionalFeatureImplementation)
+            }
+
+            tasks.compileJava {
+                // Avoid resolving the classpath when caching the configuration
+                classpath = files()
             }
 
             dependencies {
@@ -358,7 +373,6 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
         }
     }
 
-    @ToBeFixedForConfigurationCache
     def "can publish java-library with a feature from a configuration with more than one outgoing variant and filter out variants"() {
         mavenRepo.module('org', 'optionaldep', '1.0').withModuleMetadata().publish()
 
@@ -376,6 +390,11 @@ class MavenPublishFeaturesJavaIntegTest extends AbstractMavenPublishFeaturesJava
                     outgoing.capability("org:optional-feature:\${version}")
                 }
                 compileClasspath.extendsFrom(optionalFeatureImplementation)
+            }
+
+            tasks.compileJava {
+                // Avoid resolving the classpath when caching the configuration
+                classpath = files()
             }
 
             dependencies {
