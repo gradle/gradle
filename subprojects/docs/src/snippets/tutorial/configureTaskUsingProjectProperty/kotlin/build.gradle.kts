8537a91e97a54c9654816990f8fefdcff6ce1031
Siddardha Bezawada <siddardhab123@gmail.com>
2022-09-05 10:29:49 -0700

    Adjust the syntax
    Signed-off-by: Siddardha Bezawada <siddardhab123@gmail.com>
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/docs/src/snippets/tutorial/configureTaskUsingProjectProperty/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/tutorial/configureTaskUsingProjectProperty/kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 tasks.register("performRelease") {
     val isCI = providers.gradleProperty("isCI")
     doLast {
-        if (isCI.isPresent()) {
+        if (isCI.isPresent) {
             println("Performing release actions")
         } else {
             throw InvalidUserDataException("Cannot perform release outside of CI")
