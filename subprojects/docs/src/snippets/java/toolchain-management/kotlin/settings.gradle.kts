ba9962ef3516cfd077b55a8863c7744e78a3e518
József Bartók <jbartok@gradle.com>
2022-09-08 13:14:41 +0300

    Make code snippet tests work

--- subprojects/docs/src/snippets/java/toolchain-management/kotlin/settings.gradle.kts
+++ subprojects/docs/src/snippets/java/toolchain-management/kotlin/settings.gradle.kts
@@ -1,14 +1,21 @@
-rootProject.name = "toolchain-management"
+import org.gradle.api.Incubating
+import org.gradle.api.Plugin
+import org.gradle.api.initialization.Settings
+import org.gradle.jvm.toolchain.JavaToolchainRepository
+import org.gradle.jvm.toolchain.JavaToolchainRepositoryRegistry
+import org.gradle.jvm.toolchain.JavaToolchainSpec
+import org.gradle.jvm.toolchain.JavaToolchainSpecVersion
+import java.net.URI
+import java.util.Optional
+import javax.inject.Inject
+
+apply<AzulPlugin>()
+apply<AdoptiumPlugin>()
 
 // tag::toolchain-management[]
-plugins {
-    id("org.gradle.adopt-open-jdk-repo") version "1.0"
-    id("com.azul.zulu-toolchain-management") version "0.6"
-} // <1>
-
 toolchainManagement {
-    jdks { // <2>
-        add("com_azul_zulu") // <3>
+    jdks { // <1>
+        add("com_azul_zulu") // <2>
             {
                 credentials {
                     username = "user"
@@ -17,8 +24,46 @@ toolchainManagement {
                 authentication {
                     create<DigestAuthentication>("digest")
                 }
-            } // <4>
-        add("org_gradle_adopt_open_jdk") // <5>
+            } // <3>
+        add("org_gradle_adopt_open_jdk") // <4>
     }
 }
 // end::toolchain-management[]
+
+rootProject.name = "toolchain-management"
+
+/**
+ * Mock Azul repository plugin.
+ *
+ * @since 7.6
+ */
+@Incubating
+abstract class AzulPlugin: DummyPlugin("com_azul_zulu")
+
+/**
+ * Mock Adoptium repository plugin.
+ *
+ * @since 7.6
+ */
+@Incubating
+abstract class AdoptiumPlugin: DummyPlugin("org_gradle_adopt_open_jdk")
+
+abstract class DummyPlugin(val repoName: String): Plugin<Settings> {
+
+    @get:Inject
+    protected abstract val toolchainRepositoryRegistry: JavaToolchainRepositoryRegistry
+
+    override fun apply(settings: Settings) {
+        settings.plugins.apply("jdk-toolchains")
+
+        val registry: JavaToolchainRepositoryRegistry = toolchainRepositoryRegistry
+        registry.register(repoName, DummyRepo::class.java, JavaToolchainSpecVersion.currentSpecVersion)
+    }
+
+}
+
+abstract class DummyRepo: JavaToolchainRepository {
+    override fun toUri(spec: JavaToolchainSpec, env: org.gradle.env.BuildEnvironment): Optional<URI> {
+        return Optional.empty()
+    }
+}
