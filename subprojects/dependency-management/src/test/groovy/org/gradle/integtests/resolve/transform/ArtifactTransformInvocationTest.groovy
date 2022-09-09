05316805a9a0168ff428fc525fb8587365497a48
Lóránt Pintér <lorant@gradle.com>
2022-09-09 20:14:32 +0200

    Fix tests

--- subprojects/dependency-management/src/test/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformInvocationTest.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformInvocationTest.groovy
@@ -135,7 +135,7 @@ class ArtifactTransformInvocationTest extends AbstractProjectBuilderSpec {
             TransformationSubject.initial(artifact),
             inputFingerprinter
         )
-        invocation.get()
+        invocation.completeAndGet()
     }
 
     static abstract class IdentityTransform implements TransformAction<TransformParameters.None> {
