5b5e188bf7d59399194465bc097223d817350408
Paul Merlin <paul@gradle.com>
2022-09-13 15:09:39 +0200

    Throws on invalid values instead of using the default
    Signed-off-by: Paul Merlin <paul@gradle.com>
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/kotlin-dsl/src/test/kotlin/org/gradle/kotlin/dsl/concurrent/JavaSystemPropertiesAsyncIOScopeSettingsTest.kt
+++ subprojects/kotlin-dsl/src/test/kotlin/org/gradle/kotlin/dsl/concurrent/JavaSystemPropertiesAsyncIOScopeSettingsTest.kt
@@ -20,6 +20,7 @@ import org.gradle.kotlin.dsl.concurrent.JavaSystemPropertiesAsyncIOScopeSettings
 import org.gradle.kotlin.dsl.concurrent.JavaSystemPropertiesAsyncIOScopeSettings.Companion.IO_ACTION_TIMEOUT_SYSTEM_PROPERTY
 import org.gradle.util.SetSystemProperties
 import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
 import org.junit.Rule
 import org.junit.Test
 
@@ -31,36 +32,69 @@ class JavaSystemPropertiesAsyncIOScopeSettingsTest {
     val setSystemProperties = SetSystemProperties()
 
     @Test
-    fun `can retrieve default value`() {
+    fun `use default timeout when property is not set`() {
         val settings = JavaSystemPropertiesAsyncIOScopeSettings()
         assertEquals(DEFAULT_IO_ACTION_TIMEOUT, settings.ioActionTimeoutMs)
     }
 
     @Test
-    fun `can parse custom value from system property`() {
+    fun `use default timeout when property is an empty string`() {
+        System.setProperty(IO_ACTION_TIMEOUT_SYSTEM_PROPERTY, "")
+        val settings = JavaSystemPropertiesAsyncIOScopeSettings()
+        assertEquals(DEFAULT_IO_ACTION_TIMEOUT, settings.ioActionTimeoutMs)
+    }
+
+    @Test
+    fun `use default timeout when property is a blank string`() {
+        System.setProperty(IO_ACTION_TIMEOUT_SYSTEM_PROPERTY, " ")
+        val settings = JavaSystemPropertiesAsyncIOScopeSettings()
+        assertEquals(DEFAULT_IO_ACTION_TIMEOUT, settings.ioActionTimeoutMs)
+    }
+
+    @Test
+    fun `use custom timeout when property is set`() {
         System.setProperty(IO_ACTION_TIMEOUT_SYSTEM_PROPERTY, "111")
         val settings = JavaSystemPropertiesAsyncIOScopeSettings()
         assertEquals(111L, settings.ioActionTimeoutMs)
     }
 
     @Test
-    fun `uses default value if provided connection timeout is not a valid number`() {
+    fun `throws when property is not a valid number`() {
         System.setProperty(IO_ACTION_TIMEOUT_SYSTEM_PROPERTY, "abc")
         val settings = JavaSystemPropertiesAsyncIOScopeSettings()
-        assertEquals(DEFAULT_IO_ACTION_TIMEOUT, settings.ioActionTimeoutMs)
+        assertThrowsFor("abc") {
+            settings.ioActionTimeoutMs
+        }
     }
 
     @Test
-    fun `uses default value if provided connection timeout is zero`() {
+    fun `throws when property is zero`() {
         System.setProperty(IO_ACTION_TIMEOUT_SYSTEM_PROPERTY, "0")
         val settings = JavaSystemPropertiesAsyncIOScopeSettings()
-        assertEquals(DEFAULT_IO_ACTION_TIMEOUT, settings.ioActionTimeoutMs)
+        assertThrowsFor("0") {
+            settings.ioActionTimeoutMs
+        }
     }
 
     @Test
-    fun `uses default value if provided connection timeout is a negative number`() {
+    fun `throws when property is a negative number`() {
         System.setProperty(IO_ACTION_TIMEOUT_SYSTEM_PROPERTY, "-1")
         val settings = JavaSystemPropertiesAsyncIOScopeSettings()
-        assertEquals(DEFAULT_IO_ACTION_TIMEOUT, settings.ioActionTimeoutMs)
+        assertThrowsFor("-1") {
+            settings.ioActionTimeoutMs
+        }
+    }
+
+    private
+    fun assertThrowsFor(propertyValue: String, block: () -> Unit) {
+        try {
+            block()
+            fail("Expected to fail but succeeded")
+        } catch (ex: Exception) {
+            assertEquals(
+                "Invalid value for system property '$IO_ACTION_TIMEOUT_SYSTEM_PROPERTY': '$propertyValue'. It must be a positive number of milliseconds.",
+                ex.message
+            )
+        }
     }
 }
