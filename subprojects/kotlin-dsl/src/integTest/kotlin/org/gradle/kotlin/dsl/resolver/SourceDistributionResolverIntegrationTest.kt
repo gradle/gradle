e1401cc613991d49741959b25743023eb0e3b4fe
Bo Zhang <bo@gradle.com>
2022-09-13 10:55:26 +0800

    Ignore SourceDistributionResolverIntegrationTest
    This test is trying to download a 6.9.2 snapshot distribution,
    which is impossible because release6x is behind mainline too far.

--- subprojects/kotlin-dsl/src/integTest/kotlin/org/gradle/kotlin/dsl/resolver/SourceDistributionResolverIntegrationTest.kt
+++ subprojects/kotlin-dsl/src/integTest/kotlin/org/gradle/kotlin/dsl/resolver/SourceDistributionResolverIntegrationTest.kt
@@ -3,10 +3,12 @@ package org.gradle.kotlin.dsl.resolver
 import org.gradle.integtests.fixtures.RepoScriptBlockUtil.mavenCentralRepositoryDefinition
 import org.gradle.kotlin.dsl.fixtures.AbstractKotlinIntegrationTest
 import org.gradle.test.fixtures.dsl.GradleDsl
+import org.junit.Ignore
 
 import org.junit.Test
 
 
+@Ignore("This test is trying to download a source ranging in (6.8, 6.9.2-snapshot], but we're behind the mainline too far so can't get any 6.9.2-snapshot")
 class SourceDistributionResolverIntegrationTest : AbstractKotlinIntegrationTest() {
 
     @Test
