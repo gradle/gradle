8c38caf59a96d8bdf5fbb5e4130109389900d79f
Rafael Chaves <rchaves@gradle.com>
2022-09-15 15:59:07 -0300

    Use data classes instead of maps

--- subprojects/docs/src/snippets/initScripts/plugins/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/initScripts/plugins/kotlin/build.gradle.kts
@@ -1,13 +1,17 @@
+import java.net.URI
+
 // tag::show-repos-task[]
 repositories{
     mavenCentral()
 }
 
+data class RepositoryData(val name: String, val url: URI)
+
 tasks.register("showRepositories") {
-    val repositoryData = repositories.withType<MavenArtifactRepository>().map { it.name to it.url }.toMap()
+    val repositoryData = repositories.withType<MavenArtifactRepository>().map { RepositoryData(it.name, it.url) }
     doLast {
         repositoryData.forEach {
-            println("repository: ${it.key} ('${it.value}')")
+            println("repository: ${it.name} ('${it.url}')")
         }
     }
 }
