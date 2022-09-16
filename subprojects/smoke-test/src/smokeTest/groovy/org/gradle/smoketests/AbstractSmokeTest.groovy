81b63d29d0b725af75f97bbc78dc7846caa56559
Thomas Tresansky <ttresansky@gradle.com>
2022-09-16 14:37:01 -0400

    Remove DependencyFactory internal type completely in 7.6
    - It was only needed for Gretty, and there are now upgrade paths for both 3.x and 4.x to versions which don't need this type.
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
@@ -112,7 +112,7 @@ abstract class AbstractSmokeTest extends Specification {
         // https://plugins.gradle.org/plugin/org.gretty
         static gretty = [
             [version: "3.0.9", servletContainer: "jetty9.4", javaMinVersion: JavaVersion.VERSION_1_8],
-            [version: "4.0.0", servletContainer: "jetty11", javaMinVersion: JavaVersion.VERSION_11]
+            [version: "4.0.3", servletContainer: "jetty11", javaMinVersion: JavaVersion.VERSION_11]
         ]
 
         // https://plugins.gradle.org/plugin/org.ajoberstar.grgit
