9be77ef7dcbf3d5e23179b14fc11b9d40bdc0b84
Bo Zhang <bo@gradle.com>
2022-09-16 08:06:38 +0800

    Fix checkstyle errors

--- build-logic/cleanup/src/main/java/gradlebuild/cleanup/services/KillLeakingJavaProcesses.java
+++ build-logic/cleanup/src/main/java/gradlebuild/cleanup/services/KillLeakingJavaProcesses.java
@@ -19,7 +19,6 @@ package gradlebuild.cleanup.services;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
