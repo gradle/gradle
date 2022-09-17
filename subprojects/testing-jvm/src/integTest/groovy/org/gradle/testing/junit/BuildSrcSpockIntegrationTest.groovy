7cefea9aeab185b480893d686cc08c177ee13ef8
Kyle Moore <kmoore@gradle.com>
2022-09-16 17:36:08 -0700

    Use spock 2.1-groovy-3.0 as it seems to bundle a compatible cblib. 2.2 does not, and adding one manually fails with JDK 18

--- subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/BuildSrcSpockIntegrationTest.groovy
+++ subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/BuildSrcSpockIntegrationTest.groovy
@@ -34,15 +34,14 @@ class BuildSrcSpockIntegrationTest extends JUnitMultiVersionIntegrationSpec {
             ${mavenCentralRepository()}
 
             def isAtLeastGroovy4 = org.gradle.util.internal.VersionNumber.parse(GroovySystem.version).major >= 4
-            def spockVersion = isAtLeastGroovy4 ? '2.2-groovy-4.0' : '2.2-groovy-3.0'
+            def spockVersion = isAtLeastGroovy4 ? '2.2-groovy-4.0' : '2.1-groovy-3.0'
 
             dependencies {
                 implementation gradleApi()
                 implementation localGroovy()
 
                 testImplementation '$dependencyNotation',
-                    "org.spockframework:spock-core:\$spockVersion",
-                    'cglib:cglib:3.2.7' // Required by spock 2.2
+                    "org.spockframework:spock-core:\$spockVersion"
             }
         """
         file("src/main/groovy/MockIt.groovy") << """
