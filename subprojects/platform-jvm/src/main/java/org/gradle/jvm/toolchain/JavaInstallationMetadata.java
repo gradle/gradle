a57b7ba382007513d8ece16a4cf26a8f14d46ef8
Alex Semin <asemin@gradle.com>
2022-09-22 14:29:20 +0200

    Polish JavaInstallationMetadata javadoc
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/JavaInstallationMetadata.java
+++ subprojects/platform-jvm/src/main/java/org/gradle/jvm/toolchain/JavaInstallationMetadata.java
@@ -79,7 +79,7 @@ public interface JavaInstallationMetadata {
     Directory getInstallationPath();
 
     /**
-     * Returns true if this installation corresponds to the currently running JVM.
+     * Returns true if this installation corresponds to the build JVM.
      *
      * @since 8.0
      */
