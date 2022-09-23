09eaf40ea28436b5464911efdb89a4ad39f427df
Bo Zhang <bo@gradle.com>
2022-09-23 09:27:53 +0800

    Require exclusive access to build agent host for performance tests
    We are trying to run a Linux build agent and Windows build agent
    on a same bare metal (aka. shared host). Add requirements for
    performance tests since they require exclusive access.
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- .teamcity/src/main/kotlin/common/performance-test-extensions.kt
+++ .teamcity/src/main/kotlin/common/performance-test-extensions.kt
@@ -31,7 +31,8 @@ fun BuildType.applyPerformanceTestSettings(os: Os = Os.LINUX, arch: Arch = Arch.
     """.trimIndent()
     detectHangingBuilds = false
     requirements {
-        requiresNoEc2Agent()
+        requiresNotEc2Agent()
+        requiresNotSharedHost()
     }
     params {
         param("env.JPROFILER_HOME", os.jprofilerHome)
