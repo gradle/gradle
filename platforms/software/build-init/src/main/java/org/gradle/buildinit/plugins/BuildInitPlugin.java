c1e3c7e3a186c30b85bcf9fb439fffc142a2f830
arthursmel <mel.arthurs@gmail.com>
2022-09-25 14:04:27 +0100

    Revert "Init command warn if the current directory is non-empty"
    This reverts commit c2f9e0b06256b4aa15fdff3acd642cdd8fc0ab0c.
    This reverts commit ef2a55d4e6d093b33491803d41425b6cd697f68e.
    This reverts commit cf9849a886569abcc75aad96e0e5a89c50706e70.
    
    Signed-off-by: arthursmel <mel.arthurs@gmail.com>

--- platforms/software/build-init/src/main/java/org/gradle/buildinit/plugins/BuildInitPlugin.java
+++ platforms/software/build-init/src/main/java/org/gradle/buildinit/plugins/BuildInitPlugin.java
@@ -16,11 +16,9 @@
 
 package org.gradle.buildinit.plugins;
 
-import org.apache.commons.io.FileUtils;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
-import org.gradle.api.UncheckedIOException;
 import org.gradle.api.internal.project.ProjectInternal;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.plugins.jvm.internal.JvmPluginServices;
@@ -32,7 +30,6 @@ import org.gradle.internal.file.RelativeFilePathResolver;
 
 import javax.annotation.Nullable;
 import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.Callable;
 
 /**
@@ -58,12 +55,9 @@ public abstract class BuildInitPlugin implements Plugin<Project> {
                 File settingsFile = projectInternal.getGradle().getSettings().getSettingsScript().getResource().getLocation().getFile();
                 FileDetails settingsFileDetails = FileDetails.of(settingsFile, resolver);
 
-                Logger logger = initBuild.getLogger();
-                warnIfProjectDirNotEmpty(project.getProjectDir(), logger);
-
                 initBuild.onlyIf(
                     "There is no build script or settings script",
-                    new InitBuildOnlyIfSpec(buildFileDetails, settingsFileDetails, logger)
+                    new InitBuildOnlyIfSpec(buildFileDetails, settingsFileDetails, initBuild.getLogger())
                 );
 
                 ProjectInternal.DetachedResolver detachedResolver = projectInternal.newDetachedResolver();
@@ -121,16 +115,6 @@ public abstract class BuildInitPlugin implements Plugin<Project> {
             } else {
                 return null;
             }
-        }
-    }
-
-    private static void warnIfProjectDirNotEmpty(File projectDir, Logger logger) {
-        try {
-            if (!FileUtils.isEmptyDirectory(projectDir)) {
-                logger.warn("The directory '" + projectDir.getName() + "' is not empty.");
-            }
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
         }
     }
 
