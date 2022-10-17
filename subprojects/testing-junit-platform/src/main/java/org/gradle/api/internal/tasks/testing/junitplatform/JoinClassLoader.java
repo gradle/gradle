d313ff942c97ecd8883d6378cfb4e7f687e14408
József Bartók <jbartok@gradle.com>
2022-10-17 15:05:59 +0300

    Make CheckStyle happy

--- subprojects/testing-junit-platform/src/main/java/org/gradle/api/internal/tasks/testing/junitplatform/JoinClassLoader.java
+++ subprojects/testing-junit-platform/src/main/java/org/gradle/api/internal/tasks/testing/junitplatform/JoinClassLoader.java
@@ -1,14 +1,18 @@
-// Copyright 2010 Christian d'Heureuse, Inventec Informatik AG, Zurich, Switzerland
-// www.source-code.biz, www.inventec.ch/chdh
-//
-// This module is multi-licensed and may be used under the terms
-// of any of the following licenses:
-//
-//  LGPL, GNU Lesser General Public License, V2 or later, http://www.gnu.org/licenses/lgpl.html
-//  AL, Apache License, V2.0 or later, http://www.apache.org/licenses
-//
-// Please contact the author if you need another license.
-// This module is provided "as is", without warranties of any kind.
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gradle.api.internal.tasks.testing.junitplatform;
 
 import java.io.IOException;
