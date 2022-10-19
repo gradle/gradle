c7783c2c722c37a9beffa6bfbb98833152601f7f
Sergey Igushkin <sigushkin@gradle.com>
2022-10-19 16:47:19 +0500

    Fix minor issues found in the review
    * Remove the extra blank line and extra license header
    * Add a check for the dynamic call stack being not empty
    * Fix the constant in the test countdown latch
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/configuration-cache/src/test/kotlin/org/gradle/configurationcache/DefaultDynamicCallProblemReportingTest.kt
+++ subprojects/configuration-cache/src/test/kotlin/org/gradle/configurationcache/DefaultDynamicCallProblemReportingTest.kt
@@ -25,22 +25,6 @@ import java.util.Collections
 import java.util.concurrent.CountDownLatch
 import kotlin.concurrent.thread
 
-/*
- * Copyright 2022 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 
 class DefaultDynamicCallProblemReportingTest {
     @Test
@@ -81,7 +65,7 @@ class DefaultDynamicCallProblemReportingTest {
     fun `keeps state per thread`() {
         val reporting = DefaultDynamicCallProblemReporting()
 
-        val latch = CountDownLatch(2)
+        val latch = CountDownLatch(3)
         val problem = Any()
         val results = Collections.synchronizedList(mutableListOf<Boolean>())
 
