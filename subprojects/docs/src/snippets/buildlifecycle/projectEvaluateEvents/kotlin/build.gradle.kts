5aa78199ec94fe6c5beccfbda9f3791e20dc07c0
nate contino <ncontino@gradle.com>
2022-10-21 18:48:38 -0400

    Remove allproject antipattern and eliminate now-redundant afterProject example
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/docs/src/snippets/buildlifecycle/projectEvaluateEvents/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/buildlifecycle/projectEvaluateEvents/kotlin/build.gradle.kts
@@ -1,16 +1,16 @@
 // tag::after-evaluate[]
-allprojects {
+gradle.beforeProject {
     // Set a default value
     extra["hasTests"] = false
+}
 
-    afterEvaluate {
-        if (extra["hasTests"] as Boolean) {
-            val projectString = project.toString()
-            println("Adding test task to $projectString")
-            tasks.register("test") {
-                doLast {
-                    println("Running tests for $projectString")
-                }
+gradle.afterProject {
+    if (extra["hasTests"] as Boolean) {
+        val projectString = project.toString()
+        println("Adding test task to $projectString")
+        tasks.register("test") {
+            doLast {
+                println("Running tests for $projectString")
             }
         }
     }
