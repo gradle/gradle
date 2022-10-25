12b035e9330ee02202e2cebe07c6482bb073a175
Lóránt Pintér <lorant@gradle.com>
2022-10-26 01:53:54 +0200

    Separate InputPropertyType from validation actions
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/core/src/main/java/org/gradle/api/internal/tasks/properties/InputFilePropertyType.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/tasks/properties/InputFilePropertyType.java
@@ -17,17 +17,7 @@
 package org.gradle.api.internal.tasks.properties;
 
 public enum InputFilePropertyType {
-    FILE(ValidationActions.INPUT_FILE_VALIDATOR),
-    DIRECTORY(ValidationActions.INPUT_DIRECTORY_VALIDATOR),
-    FILES(ValidationActions.NO_OP);
-
-    private final ValidationAction validationAction;
-
-    InputFilePropertyType(ValidationAction validationAction) {
-        this.validationAction = validationAction;
-    }
-
-    public ValidationAction getValidationAction() {
-        return validationAction;
-    }
+    FILE,
+    DIRECTORY,
+    FILES
 }
