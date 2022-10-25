1a06714d7d4b8e3866074c5ef419d3b90c540f3f
Sterling Greene <sterling@gradle.com>
2022-10-25 12:08:34 -0400

    Fix bad rebase
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/language-groovy/src/test/groovy/org/gradle/api/internal/tasks/compile/DefaultGroovyJavaJointCompileSpecFactoryTest.groovy
+++ subprojects/language-groovy/src/test/groovy/org/gradle/api/internal/tasks/compile/DefaultGroovyJavaJointCompileSpecFactoryTest.groovy
@@ -24,6 +24,7 @@ import org.gradle.internal.os.OperatingSystem
 import org.gradle.jvm.toolchain.JavaInstallationMetadata
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider
+import org.gradle.util.TestUtil
 import org.junit.Rule
 import spock.lang.Specification
 
