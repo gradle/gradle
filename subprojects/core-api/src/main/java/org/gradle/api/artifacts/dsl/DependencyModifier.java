8f5d1b869ff12e60f48068c3733b159d3ec751de
Sterling Greene <sterling@gradle.com>
2022-10-28 09:51:23 -0400

    Fix references to documentation for new APIs

--- subprojects/core-api/src/main/java/org/gradle/api/artifacts/dsl/DependencyModifier.java
+++ subprojects/core-api/src/main/java/org/gradle/api/artifacts/dsl/DependencyModifier.java
@@ -17,6 +17,7 @@
 package org.gradle.api.artifacts.dsl;
 
 import org.gradle.api.Incubating;
+import org.gradle.api.NonExtensible;
 import org.gradle.api.artifacts.ExternalModuleDependency;
 import org.gradle.api.artifacts.MinimalExternalModuleDependency;
 import org.gradle.api.artifacts.ModuleDependency;
@@ -28,24 +29,20 @@ import javax.inject.Inject;
 /**
  * Universal APIs that are available for all {@code dependencies} blocks.
  *
- * <p>
- * NOTE: This API is <strong>incubating</strong> and is likely to change until it's made stable.
- * </p>
- * <br>
- * <p>
- * Most of these methods are not intended to be implemented by end users or plugin authors.
- * Only {@link #modify(ModuleDependency)} should be implemented.
- * </p>
+ * This API is <strong>incubating</strong> and is likely to change until it's made stable.
+ * These methods are not intended to be implemented by end users or plugin authors.
  *
  * @since 8.0
  */
 @Incubating
+@NonExtensible
 public interface DependencyModifier {
     /**
      * A dependency factory is used to convert supported dependency notations into {@link org.gradle.api.artifacts.Dependency} instances.
      *
      * @return a dependency factory
      * @see DependencyFactory
+     * Do not implement this method.
      */
     @Inject
     DependencyFactory getDependencyFactory();
@@ -56,6 +53,7 @@ public interface DependencyModifier {
      * @param dependencyNotation the dependency notation
      * @return the modified dependency
      * @see DependencyFactory#create(CharSequence)
+     * Do not implement this method.
      */
     default ExternalModuleDependency modify(CharSequence dependencyNotation) {
         return modify(getDependencyFactory().create(dependencyNotation));
@@ -66,6 +64,7 @@ public interface DependencyModifier {
      *
      * @param providerConvertibleToDependency the provider
      * @return a provider to the modified dependency
+     * Do not implement this method.
      */
     default Provider<? extends MinimalExternalModuleDependency> modify(ProviderConvertible<? extends MinimalExternalModuleDependency> providerConvertibleToDependency) {
         return providerConvertibleToDependency.asProvider().map(this::modify);
@@ -76,6 +75,7 @@ public interface DependencyModifier {
      *
      * @param providerToDependency the provider
      * @return a provider to the modified dependency
+     * Do not implement this method.
      */
     default <D extends ModuleDependency> Provider<D> modify(Provider<D> providerToDependency) {
         return providerToDependency.map(this::modify);
@@ -88,6 +88,7 @@ public interface DependencyModifier {
      * @param dependency the dependency to modify
      * @return the modified dependency
      * @param <D> the type of the {@link ModuleDependency}
+     * Implement this method.
      */
     <D extends ModuleDependency> D modify(D dependency);
 }
