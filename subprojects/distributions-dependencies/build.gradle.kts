d373f6df8ab14b1189eabf963d773878f67495cd
Robert Sloan <20532531+rbrt-sln@users.noreply.github.com>
2022-11-08 12:43:17 +1100

    Update build.gradle.kts

--- subprojects/distributions-dependencies/build.gradle.kts
+++ subprojects/distributions-dependencies/build.gradle.kts
@@ -16,9 +16,9 @@ description = "Provides a platform dependency to align all distribution versions
 val antVersion = "1.10.11"
 val archunitVersion = "1.0.0-rc1"
 val asmVersion = "9.3"
-val awsS3Version = "1.11.948"
+val awsS3Version = "1.12.261"
 val bouncycastleVersion = "1.68"
-val jacksonVersion = "2.13.3"
+val jacksonVersion = "2.14.0"
 val jaxbVersion = "3.0.0"
 val jettyVersion = "9.4.36.v20210114"
 val junit5Version = "5.8.2"
@@ -108,7 +108,7 @@ dependencies {
         api(libs.joda)                  { version { strictly("2.10.4") }}
         api(libs.joptSimple)            { version { strictly("5.0.4"); because("needed to create profiler in Gradle profiler API") }}
         api(libs.jsch)                  { version { strictly("0.1.55") }}
-        api(libs.jsoup)                 { version { strictly("1.15.1") }}
+        api(libs.jsoup)                 { version { strictly("1.15.3") }}
         api(libs.jsr305)                { version { strictly("3.0.2") }}
         api(libs.julToSlf4j)            { version { strictly(slf4jVersion) }}
         api(libs.junit)                 { version { strictly("4.13.2") }}
