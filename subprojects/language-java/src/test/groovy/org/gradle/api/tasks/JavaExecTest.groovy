d366bee20f751acec5e1b7cab5871fae1a072fe2
József Bartók <jbartok@gradle.com>
2022-11-17 15:00:48 +0200

    Fix failing tests
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/language-java/src/test/groovy/org/gradle/api/tasks/JavaExecTest.groovy
+++ subprojects/language-java/src/test/groovy/org/gradle/api/tasks/JavaExecTest.groovy
@@ -64,9 +64,8 @@ class JavaExecTest extends AbstractProjectBuilderSpec {
 
         then:
         def e = thrown(TaskExecutionException)
-        def cause = TestUtil.getRootCause(e) as InvalidUserDataException
-        cause.message.contains("Specific installation toolchain")
-        cause.message.contains(invalidJavac.parentFile.parentFile.absolutePath)
+        assertHasCause(e, "Toolchain installation '${invalidJavac.parentFile.parentFile.absolutePath}' could not be probed: ")
+        assertHasCause(e, "Cannot run program \"${invalidJavac.parentFile.parentFile.file("bin", "java").absolutePath}\"")
     }
 
     def "fails if custom Java home does not exist"() {
@@ -115,8 +114,7 @@ class JavaExecTest extends AbstractProjectBuilderSpec {
 
         then:
         def e = thrown(AbstractProperty.PropertyQueryException)
-        def cause = TestUtil.getRootCause(e) as InvalidUserDataException
-        cause.message.contains("Specific installation toolchain")
-        cause.message.contains(javaHomeDir.absolutePath)
+        assertHasCause(e, "Toolchain installation '${javaHomeDir.absolutePath}' could not be probed: ")
+        assertHasCause(e, "Cannot run program \"${javaHomeDir.file("bin", "java").absolutePath}\"")
     }
 }
