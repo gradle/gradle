4c8250e2f02cbaac1455ab601985a89ae8eae71d
Anže Sodja <asodja@gradle.com>
2022-11-29 18:36:39 +0100

    Remove some clutter documentation from TypeMetadataWalker and move most of it to top of the interface
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/model-core/src/main/java/org/gradle/internal/properties/annotations/TypeMetadataWalker.java
+++ subprojects/model-core/src/main/java/org/gradle/internal/properties/annotations/TypeMetadataWalker.java
@@ -22,21 +22,30 @@ import java.lang.annotation.Annotation;
 import java.util.function.Supplier;
 
 /***
- * A generalized Type Metadata Walker for Gradle types or instances.
+ * A generalized Type Metadata Walker for traversing Gradle types or instances in a Depth-first order.
  *
- * A walker visits all properties of a type or an instance.
+ * A visited nodes are leaf nodes, i.e. properties, or an intermediate nodes, i.e. nested nodes.
+ *
+ * A property can be:
+ *  - a property of a type/instance
+ *  - an element of an iterable which is annotated with a nested annotation (iterables of iterables are supported)
+ *  - a value of a map annotated which is annotated with a nested annotation (maps of maps are supported)
+ *  - a property of a nested type/instance which is annotated with a nested annotation
+ *
+ * A nested node is a node tha contain properties or other nested nodes. It's marked with a special nested annotation,
+ * normally {@link org.gradle.api.tasks.Nested}. A nested annotation can be passed as a parameter to a walker.
+ * Note: Maps, Iterables and Providers annotated with the nested annotations are automatically flatten.
+ *
+ * TypeMetadataWalker uses {@link TypeMetadataStore} to read nodes' {@link TypeMetadata} and to further discover the tree properties.
+ * Usually a TypeMetadataStore that supports well known Gradle annotations like {@link org.gradle.api.tasks.Input} should be used,
+ * but in general any TypeMetadataStore implementation can be used.
  */
 public interface TypeMetadataWalker<T> {
 
     /**
      * A factory method for a walker that can visit an instance.
-     * A walker is lightweight object so there is no need to cache it, but it holds no state, so it can be reused.
      *
      * Instance walker will throw {@link org.gradle.api.GradleException} in case a nested property cycle is detected.
-     *
-     * @param typeMetadataStore {@link TypeMetadataStore} that holds {@link TypeMetadata} for visited types.
-     * @param nestedAnnotation An annotation that marks a nested property, normally the {@link org.gradle.api.tasks.Nested} annotation.
-     * @return A new instance of TypeMetadataWalker.
      */
     static TypeMetadataWalker<Object> instanceWalker(TypeMetadataStore typeMetadataStore, Class<? extends Annotation> nestedAnnotation) {
         return new AbstractTypeMetadataWalker.InstanceTypeMetadataWalker(typeMetadataStore, nestedAnnotation);
@@ -44,57 +53,20 @@ public interface TypeMetadataWalker<T> {
 
     /**
      * A factory method for a walker that can visit a type.
-     * A walker is lightweight object so there is no need to cache it, but it holds no state, so it can be reused.
      *
      * Type walker can detect a nested property cycle and stop walking the path with a cycle, no exception is thrown.
-     *
-     * @param typeMetadataStore {@link TypeMetadataStore} that holds {@link TypeMetadata} for visited types.
-     * @param nestedAnnotation an annotation that marks a nested property, normally the {@link org.gradle.api.tasks.Nested} annotation.
-     * @return a new instance of TypeMetadataWalker.
      */
     static TypeMetadataWalker<TypeToken<?>> typeWalker(TypeMetadataStore typeMetadataStore, Class<? extends Annotation> nestedAnnotation) {
         return new AbstractTypeMetadataWalker.StaticTypeMetadataWalker(typeMetadataStore, nestedAnnotation);
     }
 
-
-    /**
-     * A start point to traverse a tree of nodes of a type or an instance.
-     *
-     * @param root A root type or an instance that we want to traverse.
-     * @param visitor A {@link NodeMetadataVisitor} that visits the tree of nodes of a type or an instance.
-     */
     void walk(T root, NodeMetadataVisitor<T> visitor);
 
-    /**
-     * A visitor that should be implemented to receive a callbacks when visiting a type or an instance.
-     */
     interface NodeMetadataVisitor<T> {
-
-        /**
-         * A callback that will be called when a root is visited.
-         *
-         * @param typeMetadata A {@link TypeMetadata} of a root node.
-         * @param value A value of a root.
-         */
         void visitRoot(TypeMetadata typeMetadata, T value);
 
-        /**
-         * A callback that will be called when a nested node, i.e. node annotated with 'nestedAnnotation', is visited.
-         *
-         * @param typeMetadata A {@link TypeMetadata} of a nested node.
-         * @param qualifiedName A fully qualified name of a nested node.
-         * @param propertyMetadata A {@link PropertyMetadata} of a nested node.
-         * @param value A value of a nested node.
-         */
         void visitNested(TypeMetadata typeMetadata, String qualifiedName, PropertyMetadata propertyMetadata, T value);
 
-        /**
-         * A callback that will be called when a leaf is visited.
-         *
-         * @param qualifiedName A fully qualified name of a leaf.
-         * @param propertyMetadata A {@link PropertyMetadata} of a leaf.
-         * @param value A leaf value that is lazily invoked. A value supplied by a supplier can be null for instances, but not for types.
-         */
         void visitLeaf(String qualifiedName, PropertyMetadata propertyMetadata, Supplier<T> value);
     }
 }
