fd8c8ae713420b85b3a644d53b47f1adc827644d
Thomas Tresansky <ttresansky@gradle.com>
2022-11-30 08:39:07 -0500

    Revert "Remove deprecated method for Gradle 8.0"
    This reverts commit 4637f48be3bd3af59ba1a44dac5196a194f58d47.

--- subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/DependencyHandlerScope.kt
+++ subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/DependencyHandlerScope.kt
@@ -16,11 +16,13 @@
 
 package org.gradle.kotlin.dsl
 
+import org.gradle.api.Action
 import org.gradle.api.Incubating
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.artifacts.ModuleDependency
+import org.gradle.api.artifacts.dsl.DependencyConstraintHandler
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.provider.Provider
 import org.gradle.api.provider.ProviderConvertible
@@ -44,6 +46,11 @@ private constructor(
 
     override val delegate: DependencyHandler
         get() = dependencies
+
+    @Deprecated(replaceWith = ReplaceWith("constraints"), message = "This method shouldn't be called because the most specific variant should be preferred by the Kotlin compiler", level = DeprecationLevel.HIDDEN)
+    override fun constraints(configureAction: Action<in DependencyConstraintHandler>) {
+        super.constraints(configureAction)
+    }
 
     /**
      * Configures dependency constraint for this project.
