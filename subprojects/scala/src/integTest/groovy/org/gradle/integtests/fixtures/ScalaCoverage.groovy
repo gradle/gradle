45f5277759f987c2731983823b3532b5d412efff
Alex Semin <asemin@gradle.com>
2022-12-01 15:41:19 +0100

    Run Scala plugin test on latest versions

--- subprojects/scala/src/integTest/groovy/org/gradle/integtests/fixtures/ScalaCoverage.groovy
+++ subprojects/scala/src/integTest/groovy/org/gradle/integtests/fixtures/ScalaCoverage.groovy
@@ -24,6 +24,7 @@ class ScalaCoverage {
     static final String[] SCALA_3 = ["3.1.3", "3.2.0", "3.2.1"]
 
     static final String[] DEFAULT = SCALA_2 + SCALA_3
+    static final String[] LATEST_IN_MAJOR = [SCALA_2.last(), SCALA_3.last()]
 
     //to be used with getOrDefault(version, JavaVersion.VERSION_1_8)
     static final Map<String, JavaVersion> SCALA_VERSION_TO_MAX_JAVA_VERSION = ["2.13.6" : JavaVersion.VERSION_17, "2.13.1": JavaVersion.VERSION_12]
