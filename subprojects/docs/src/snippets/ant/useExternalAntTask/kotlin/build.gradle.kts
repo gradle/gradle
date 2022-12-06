d1d86216bb13a3525fb781fd78e7a291cf24f24c
József Bartók <bjozsef@gmail.com>
2022-12-06 11:58:34 +0200

    Update subprojects/docs/src/snippets/ant/useExternalAntTask/kotlin/build.gradle.kts
    Co-authored-by: Paul Merlin <paul@nosphere.org>
    Signed-off-by: jknair0 <jkdeveloper9@gmail.com>

--- subprojects/docs/src/snippets/ant/useExternalAntTask/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/ant/useExternalAntTask/kotlin/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 val checkstyle by configurations.creating
 
 dependencies {
-    "checkstyle"("com.puppycrawl.tools:checkstyle:10.4")
+    "checkstyle"("com.puppycrawl.tools:checkstyle:9.3")
 }
 
 val download = tasks.register<Copy>("download") {
