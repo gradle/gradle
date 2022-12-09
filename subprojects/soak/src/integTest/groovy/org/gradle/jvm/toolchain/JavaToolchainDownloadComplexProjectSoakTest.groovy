b44131c3a891becb94fa4a8406def07db6905955
József Bartók <jbartok@gradle.com>
2022-12-09 11:21:33 +0200

    Fix the issue of provisioning the same toolchain multiple times (backport #23024)
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

---
+++ subprojects/soak/src/integTest/groovy/org/gradle/jvm/toolchain/JavaToolchainDownloadComplexProjectSoakTest.groovy
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.jvm.toolchain
+
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+
+class JavaToolchainDownloadComplexProjectSoakTest  extends AbstractIntegrationSpec {
+
+    def setup() {
+        executer.requireOwnGradleUserHomeDir()
+        executer.withToolchainDownloadEnabled()
+    }
+
+    def "multiple subprojects with identical toolchain definitions"() {
+        given:
+        settingsFile << settingsForBuildWithSubprojects()
+
+        setupSubproject("subproject1", "Foo", "ADOPTIUM")
+        setupSubproject("subproject2", "Bar", "ADOPTIUM")
+
+        when:
+        result = executer
+                .withTasks("compileJava")
+                .run()
+
+        then:
+        !result.plainTextOutput.matches("(?s).*The existing installation will be replaced by the new download.*")
+    }
+
+    def "multiple subprojects with different toolchain definitions"() {
+        given:
+        settingsFile << settingsForBuildWithSubprojects()
+
+        setupSubproject("subproject1", "Foo", "ADOPTIUM")
+        setupSubproject("subproject2", "Bar", "ORACLE")
+
+        when:
+        result = executer
+                .withTasks("compileJava")
+                .withArgument("--info")
+                .run()
+
+        then:
+        result.plainTextOutput.matches("(?s).*Compiling with toolchain.*adoptium.*")
+        result.plainTextOutput.matches("(?s).*Compiling with toolchain.*oracle.*")
+    }
+
+    private String settingsForBuildWithSubprojects() {
+        return """
+            plugins {
+                id 'org.gradle.toolchains.foojay-resolver-convention' version '0.2'
+            }
+            rootProject.name = 'main'
+            
+            include('subproject1')
+            include('subproject2')
+        """
+    }
+
+    private void setupSubproject(String subprojectName, String className, String vendorName) {
+        file("${subprojectName}/build.gradle") << """
+            plugins {
+                id 'java'
+            }
+            
+            java {
+                toolchain {
+                    languageVersion = JavaLanguageVersion.of(11)
+                    vendor = JvmVendorSpec.${vendorName}
+                }
+            }
+        """
+        file("${subprojectName}/src/main/java/${className}.java") << "public class ${className} {}"
+    }
+
+}
