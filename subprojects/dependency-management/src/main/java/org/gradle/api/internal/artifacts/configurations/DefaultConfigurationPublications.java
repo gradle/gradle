f5900f87ea69b9155cb57524f5a5e59613a70641
József Bartók <jbartok@gradle.com>
2022-12-14 14:06:58 +0200

    Revert some unnecessary changes
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfigurationPublications.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfigurationPublications.java
@@ -37,7 +37,6 @@ import org.gradle.api.internal.collections.DomainObjectCollectionFactory;
 import org.gradle.api.internal.file.FileCollectionFactory;
 import org.gradle.api.provider.Provider;
 import org.gradle.internal.DisplayName;
-import org.gradle.internal.FinalizableValue;
 import org.gradle.internal.reflect.Instantiator;
 import org.gradle.internal.typeconversion.NotationParser;
 
@@ -47,7 +46,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-public class DefaultConfigurationPublications implements ConfigurationPublications, FinalizableValue {
+public class DefaultConfigurationPublications implements ConfigurationPublications {
     private final DisplayName displayName;
     private final PublishArtifactSet artifacts;
     private final PublishArtifactSetProvider allArtifacts;
@@ -220,7 +219,7 @@ public class DefaultConfigurationPublications implements ConfigurationPublicatio
         return capabilities == null ? Collections.emptyList() : ImmutableList.copyOf(capabilities);
     }
 
-    public void preventFromFurtherMutation() {
+    void preventFromFurtherMutation() {
         canCreate = false;
         if (variants != null) {
             for (ConfigurationVariant variant : variants) {
