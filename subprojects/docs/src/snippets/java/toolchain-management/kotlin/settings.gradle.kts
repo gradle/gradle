9365cdce524691ec6026edc15e1242e86ec669ae
József Bartók <jbartok@gradle.com>
2022-12-15 14:04:18 +0200

    Adopt content from the 8.0 branch
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/docs/src/snippets/java/toolchain-management/kotlin/settings.gradle.kts
+++ subprojects/docs/src/snippets/java/toolchain-management/kotlin/settings.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver") version("0.3.0")
+}
+
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 import org.gradle.jvm.toolchain.JavaToolchainResolver
@@ -6,25 +10,24 @@ import java.net.URI
 import java.util.Optional
 import javax.inject.Inject
 
-apply<AzulPlugin>()
 apply<AdoptiumPlugin>()
 
 // tag::toolchain-management[]
 toolchainManagement {
     jvm { // <1>
         javaRepositories {
-            repository("azul") { // <2>
-                resolverClass.set(AzulResolver::class.java)
+            repository("foojay") { // <2>
+                resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+            }
+            repository("adoptium") { // <3>
+                resolverClass.set(AdoptiumResolver::class.java)
                 credentials {
                     username = "user"
                     password = "password"
                 }
                 authentication {
                     create<DigestAuthentication>("digest")
-                } // <3>
-            }
-            repository("adoptium") { // <4>
-                resolverClass.set(AdoptiumResolver::class.java)
+                } // <4>
             }
         }
     }
@@ -33,11 +36,7 @@ toolchainManagement {
 
 rootProject.name = "toolchain-management"
 
-abstract class AzulPlugin: DummyPlugin(AzulResolver::class)
-
-abstract class AdoptiumPlugin: DummyPlugin(AdoptiumResolver::class)
-
-abstract class DummyPlugin(val resolverClass: kotlin.reflect.KClass<out JavaToolchainResolver>): Plugin<Settings> {
+abstract class AdoptiumPlugin: Plugin<Settings> {
 
     @get:Inject
     protected abstract val toolchainResolverRegistry: JavaToolchainResolverRegistry
@@ -46,18 +45,12 @@ abstract class DummyPlugin(val resolverClass: kotlin.reflect.KClass<out JavaTool
         settings.plugins.apply("jvm-toolchain-management")
 
         val registry: JavaToolchainResolverRegistry = toolchainResolverRegistry
-        registry.register(resolverClass.java)
+        registry.register(AdoptiumResolver::class.java)
     }
 
 }
 
 abstract class AdoptiumResolver: JavaToolchainResolver {
-    override fun resolve(request: JavaToolchainRequest): Optional<JavaToolchainDownload> {
-        return Optional.empty()
-    }
-}
-
-abstract class AzulResolver: JavaToolchainResolver {
     override fun resolve(request: JavaToolchainRequest): Optional<JavaToolchainDownload> {
         return Optional.empty()
     }
