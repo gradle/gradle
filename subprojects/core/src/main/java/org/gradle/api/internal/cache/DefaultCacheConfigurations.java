e0065164b0ea2a0b3b152cf2f69d3494b089f28a
Thomas Tresansky <ttresansky@gradle.com>
2022-12-19 16:13:16 -0500

    Correct @Inject annotation

--- subprojects/core/src/main/java/org/gradle/api/internal/cache/DefaultCacheConfigurations.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/cache/DefaultCacheConfigurations.java
@@ -36,7 +36,6 @@ public class DefaultCacheConfigurations implements CacheConfigurationsInternal {
         this.createdResourcesConfiguration = createResourceConfiguration(objectFactory, DEFAULT_MAX_AGE_IN_DAYS_FOR_CREATED_CACHE_ENTRIES);
     }
 
-    @Inject
     private static CacheResourceConfiguration createResourceConfiguration(ObjectFactory objectFactory, int defaultDays) {
         CacheResourceConfiguration resourceConfiguration = objectFactory.newInstance(CacheResourceConfiguration.class);
         resourceConfiguration.getRemoveUnusedEntriesAfterDays().convention(defaultDays);
