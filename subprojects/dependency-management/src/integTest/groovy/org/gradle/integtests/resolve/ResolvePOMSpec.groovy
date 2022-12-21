5e88930f173cb88708dcb9109dfefe13d38ae350
Thomas Tresansky <ttresansky@gradle.com>
2022-12-21 17:18:42 -0500

    Add reproducer integration test for issue resolving a pom artifact

---
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/ResolvePOMSpec.groovy
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.integtests.resolve
+
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import spock.lang.Issue
+
+@Issue("https://github.com/gradle/gradle/pull/23245")
+class ResolvePOMSpec extends AbstractIntegrationSpec {
+    def "resolving a @pom artifact from an included build replacing an external library doesn't fail the build"() {
+        given:
+        def mainProjectDir = file("main-project")
+        def includedLoggingProjectDir = file("included-logging")
+
+        mainProjectDir.file("settings.gradle.kts").text = """
+            rootProject.name = "main-project"
+            include("app")
+            includeBuild("../included-logging")
+        """
+
+        mainProjectDir.file("app/build.gradle.kts").text = """
+            plugins {
+                application
+            }
+
+            dependencies {
+                implementation("org.gradle.repro:lib@pom")
+            }
+        """
+
+        includedLoggingProjectDir.file("settings.gradle.kts").text = """
+            rootProject.name = "included-logging"
+            include("lib")
+        """
+
+        includedLoggingProjectDir.file("lib/build.gradle.kts").text = """
+            plugins {
+                `java-library`
+            }
+        """
+
+        includedLoggingProjectDir.file("gradle.properties").text = """
+            group=org.gradle.repro
+            version=0.1.0-SNAPSHOT
+        """
+
+        executer.inDirectory(mainProjectDir)
+
+        expect:
+        succeeds "build"
+    }
+}
