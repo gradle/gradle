bfebd1f70367faea6f7d957113eb4ef51c65eee5
Rafael Chaves <rchaves@gradle.com>
2022-12-23 18:19:35 -0300

    Fix Ivy Publish snippets for CC
    By:
    * punning/aliasing
    * moved `layout.builDirectory` to outside
    the ivy container or else it would fail trying to resolve `buildDirectory`
    against a `GradleRepositoryLayout`
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/docs/src/snippets/ivy-publish/quickstart/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/ivy-publish/quickstart/kotlin/build.gradle.kts
@@ -22,9 +22,10 @@ publishing {
 // end::publish-component[]
 // tag::repositories[]
     repositories {
+        val buildDirectory = layout.buildDirectory
         ivy {
             // change to point to your repo, e.g. http://my.org/repo
-            url = uri(layout.buildDirectory.dir("repo"))
+            url = uri(buildDirectory.dir("repo"))
         }
     }
 // tag::publish-component[]
