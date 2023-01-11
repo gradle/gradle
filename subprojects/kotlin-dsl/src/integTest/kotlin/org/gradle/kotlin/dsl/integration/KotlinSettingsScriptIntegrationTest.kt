fb7d28721e145e4d3f73a768c0f275e253239b00
Paul Merlin <paul@gradle.com>
2023-01-11 14:50:35 +0100

    Integ test access to settings extensions in .gradle.kts settings scripts
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl/src/integTest/kotlin/org/gradle/kotlin/dsl/integration/KotlinSettingsScriptIntegrationTest.kt
+++ subprojects/kotlin-dsl/src/integTest/kotlin/org/gradle/kotlin/dsl/integration/KotlinSettingsScriptIntegrationTest.kt
@@ -6,17 +6,13 @@ import org.gradle.test.fixtures.plugin.PluginBuilder
 import org.gradle.test.fixtures.server.http.MavenHttpPluginRepository
 
 import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Rule
 
 import org.junit.Test
 
 
 class KotlinSettingsScriptIntegrationTest : AbstractKotlinIntegrationTest() {
-
-    @Rule
-    @JvmField
-    val pluginPortal: MavenHttpPluginRepository = MavenHttpPluginRepository.asGradlePluginPortal(executer, mavenRepo)
 
     @Test
     fun `can apply plugin using ObjectConfigurationAction syntax`() {
@@ -49,23 +45,31 @@ class KotlinSettingsScriptIntegrationTest : AbstractKotlinIntegrationTest() {
     @Test
     fun `can apply plugin using plugins block`() {
 
-        PluginBuilder(file("plugin")).run {
-            addSettingsPlugin("println '*42*'", "test.MySettingsPlugin", "MySettingsPlugin")
-            publishAs("g", "m", "1.0", pluginPortal, createExecuter()).allowAll()
-        }
+        val pluginPortal: MavenHttpPluginRepository = MavenHttpPluginRepository.asGradlePluginPortal(executer, mavenRepo)
+        try {
+            pluginPortal.start()
 
-        withSettings(
-            """
-            plugins {
-                id("test.MySettingsPlugin").version("1.0")
+            PluginBuilder(file("plugin")).run {
+                addSettingsPlugin("println '*42*'", "test.MySettingsPlugin", "MySettingsPlugin")
+                publishAs("g", "m", "1.0", pluginPortal, createExecuter()).allowAll()
             }
-            """
-        )
 
-        assertThat(
-            build().output,
-            containsString("*42*")
-        )
+            withSettings(
+                """
+                plugins {
+                    id("test.MySettingsPlugin").version("1.0")
+                }
+                """
+            )
+
+            assertThat(
+                build().output,
+                containsString("*42*")
+            )
+
+        } finally {
+            pluginPortal.stop()
+        }
     }
 
     @Test
@@ -154,6 +158,43 @@ class KotlinSettingsScriptIntegrationTest : AbstractKotlinIntegrationTest() {
 
         assert(
             build("compute").output.contains("*42*")
+        )
+    }
+
+    @Test
+    fun `can access settings extensions`() {
+        withKotlinDslPluginIn("build-logic")
+        withFile("build-logic/src/main/kotlin/MyExtension.kt", """
+            interface MyExtension {
+                fun some(message: String) { println(message) }
+            }
+        """)
+        withFile("build-logic/src/main/kotlin/my-plugin.settings.gradle.kts", """
+            extensions.create<MyExtension>("my")
+        """)
+        withSettings("""
+            pluginManagement {
+                includeBuild("build-logic")
+            }
+            plugins { id("my-plugin") }
+
+            extensions.getByType(MyExtension::class).some("api.get")
+            extensions.configure<MyExtension> { some("api.configure") }
+            the<MyExtension>().some("kotlin.get")
+            configure<MyExtension> { some("kotlin.configure") }
+        """)
+        withBuildScript("""tasks.register("noop")""")
+
+        assertThat(
+            build("noop", "-q").output.trim(),
+            equalTo(
+                """
+                api.get
+                api.configure
+                kotlin.get
+                kotlin.configure
+                """.trimIndent()
+            )
         )
     }
 }
