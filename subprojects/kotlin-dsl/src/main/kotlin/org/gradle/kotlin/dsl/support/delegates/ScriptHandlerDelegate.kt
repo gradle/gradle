6775cfec8d48695af6b750e6219d8481f414ee4b
Paul Merlin <paul@gradle.com>
2023-01-14 13:41:52 +0100

    Polish Kotlin DSL delegates
    by removing spurious sentence from kdoc
    GradleDelegate doesn't contain more information
    
    Signed-off-by: Paul Merlin <paul@gradle.com>
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/support/delegates/ScriptHandlerDelegate.kt
+++ subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/support/delegates/ScriptHandlerDelegate.kt
@@ -31,8 +31,6 @@ import java.net.URI
 
 /**
  * Facilitates the implementation of the [ScriptHandler] interface by delegation via subclassing.
- *
- * See [GradleDelegate] for why this is currently necessary.
  */
 abstract class ScriptHandlerDelegate : ScriptHandler {
 
