eca6cb21eab3dd6f90599731064703cb2f274908
Lóránt Pintér <lorant@gradle.com>
2023-01-27 12:07:01 +0100

    First steps toward next-gen build cache controller
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/build-cache/src/main/java/org/gradle/caching/internal/controller/NextGenBuildCacheController.java
+++ subprojects/build-cache/src/main/java/org/gradle/caching/internal/controller/NextGenBuildCacheController.java
@@ -16,17 +16,32 @@
 
 package org.gradle.caching.internal.controller;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedMap;
+import com.google.gson.Gson;
 import org.gradle.caching.BuildCacheKey;
 import org.gradle.caching.internal.CacheableEntity;
 import org.gradle.caching.internal.controller.service.BuildCacheLoadResult;
+import org.gradle.caching.internal.origin.OriginMetadata;
+import org.gradle.caching.internal.origin.OriginMetadataFactory;
+import org.gradle.internal.hash.HashCode;
 import org.gradle.internal.snapshot.FileSystemSnapshot;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class NextGenBuildCacheController implements BuildCacheController {
+    private final NextGenBuildCacheAccess cacheAccess;
+    private final OriginMetadataFactory originMetadataFactory;
+
     @Override
     public boolean isEnabled() {
         return true;
@@ -38,14 +53,77 @@ public class NextGenBuildCacheController implements BuildCacheController {
     }
 
     @Override
-    public Optional<BuildCacheLoadResult> load(BuildCacheKey cacheKey, CacheableEntity cacheableEntity) {
-        System.out.println("Not loading!");
-        return Optional.empty();
+    public Optional<BuildCacheLoadResult> load(BuildCacheKey manifestCacheKey, CacheableEntity cacheableEntity) {
+        AtomicReference<BuildCacheLoadResult> result = new AtomicReference<>();
+        cacheAccess.load(Collections.singleton(manifestCacheKey), (__, manifestStream) -> {
+            CacheManifest manifest = new Gson().fromJson(new InputStreamReader(manifestStream), CacheManifest.class);
+
+            // TODO Remove task outputs first
+
+            // TODO Do all properties at once instead of doing separate bathches
+            cacheableEntity.visitOutputTrees((propertyName, type, root) -> {
+                List<CacheManifest.ManifestEntry> manifestEntries = manifest.getPropertyManifests().get(propertyName);
+                Map<HashCode, String> contentHashes = manifestEntries.stream()
+                        .collect(ImmutableMap.toImmutableMap(
+                            CacheManifest.ManifestEntry::getContentHash,
+                            CacheManifest.ManifestEntry::getRelativePath
+                        ));
+
+                // TODO Filter out entries that are already in the right place in the output directory
+                // TODO Handle missing entries
+
+                cacheAccess.load(contentHashes.keySet(), (contentHash, inputStream) -> {
+                    String relativePath = contentHashes.get(contentHash);
+                    new File(root, relativePath) << inputStream;
+                });
+            });
+
+            result.set(new BuildCacheLoadResult() {
+                @Override
+                public long getArtifactEntryCount() {
+                    // TODO Set count
+                    return 0;
+                }
+
+                @Override
+                public OriginMetadata getOriginMetadata() {
+                    return manifest.getOriginMetadata();
+                }
+
+                @Override
+                public ImmutableSortedMap<String, FileSystemSnapshot> getResultingSnapshots() {
+                    // TODO Collect snapshots
+                    return null;
+                }
+            });
+        });
+
+        return Optional.ofNullable(result.get());
     }
 
     @Override
     public void store(BuildCacheKey cacheKey, CacheableEntity entity, Map<String, FileSystemSnapshot> snapshots, Duration executionTime) {
-        System.out.println("Not storing!");
+        ImmutableMap.Builder<String, List<CacheManifest.ManifestEntry>> propertyManifests = ImmutableMap.builder();
+
+        entity.visitOutputTrees((propertyName, type, root) -> {
+            // TODO Do this
+        });
+
+        CacheManifest manifest = new CacheManifest(
+            // TODO Set build invocation ID properly
+            new OriginMetadata("", executionTime),
+            propertyManifests.build()
+        );
+
+        cacheAccess.store(propertyManifest.getAllHashes(), (hashCode, outputStream) -> {
+            // TODO Handle only files
+            // TODO Handle empty directories, too
+        });
+
+        cacheAccess.store(Collections.singleton(manifestHash), (__, outputStream) -> {
+            String manifestJson = new Gson().toJson(manifest);
+            outputStream.write(manifestJson.getBytes(StandardCharsets.UTF_8));
+        });
     }
 
     @Override
