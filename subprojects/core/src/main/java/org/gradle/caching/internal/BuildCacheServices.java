d4209eaf103dcb27090838e86b550a249d0c2a5e
Lóránt Pintér <lorant@gradle.com>
2023-01-27 14:10:14 +0100

    Wire in HTTP build cache as remote

--- subprojects/core/src/main/java/org/gradle/caching/internal/BuildCacheServices.java
+++ subprojects/core/src/main/java/org/gradle/caching/internal/BuildCacheServices.java
@@ -21,19 +21,12 @@ import org.gradle.api.internal.GradleInternal;
 import org.gradle.api.internal.cache.StringInterner;
 import org.gradle.api.internal.file.temp.TemporaryFileProvider;
 import org.gradle.api.logging.configuration.ShowStacktrace;
-import org.gradle.caching.BuildCacheEntryReader;
-import org.gradle.caching.BuildCacheEntryWriter;
-import org.gradle.caching.BuildCacheException;
-import org.gradle.caching.BuildCacheKey;
-import org.gradle.caching.BuildCacheService;
+import org.gradle.cache.scopes.GlobalScopedCacheBuilderFactory;
 import org.gradle.caching.configuration.internal.BuildCacheConfigurationInternal;
 import org.gradle.caching.configuration.internal.BuildCacheServiceRegistration;
 import org.gradle.caching.configuration.internal.DefaultBuildCacheConfiguration;
 import org.gradle.caching.configuration.internal.DefaultBuildCacheServiceRegistration;
 import org.gradle.caching.internal.controller.BuildCacheController;
-import org.gradle.caching.internal.controller.DefaultNextGenBuildCacheAccess;
-import org.gradle.caching.internal.controller.NextGenBuildCacheAccess;
-import org.gradle.caching.internal.controller.NextGenBuildCacheController;
 import org.gradle.caching.internal.controller.RootBuildCacheControllerRef;
 import org.gradle.caching.internal.origin.OriginMetadataFactory;
 import org.gradle.caching.internal.packaging.BuildCacheEntryPacker;
@@ -43,15 +36,16 @@ import org.gradle.caching.internal.packaging.impl.GZipBuildCacheEntryPacker;
 import org.gradle.caching.internal.packaging.impl.TarBuildCacheEntryPacker;
 import org.gradle.caching.internal.packaging.impl.TarPackerFileSystemSupport;
 import org.gradle.caching.internal.services.BuildCacheControllerFactory;
+import org.gradle.caching.internal.services.NextGenBuildCacheControllerFactory;
 import org.gradle.caching.local.DirectoryBuildCache;
 import org.gradle.caching.local.internal.DirectoryBuildCacheFileStoreFactory;
 import org.gradle.caching.local.internal.DirectoryBuildCacheServiceFactory;
-import org.gradle.caching.local.internal.H2LocalCacheService;
 import org.gradle.internal.SystemProperties;
 import org.gradle.internal.file.Deleter;
 import org.gradle.internal.file.FileException;
 import org.gradle.internal.hash.ChecksumService;
 import org.gradle.internal.hash.StreamHasher;
+import org.gradle.internal.instantiation.InstanceGenerator;
 import org.gradle.internal.instantiation.InstantiatorFactory;
 import org.gradle.internal.nativeintegration.filesystem.FileSystem;
 import org.gradle.internal.nativeintegration.network.HostnameLookup;
@@ -69,7 +63,6 @@ import org.gradle.util.GradleVersion;
 import org.gradle.util.Path;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -194,26 +187,13 @@ public final class BuildCacheServices extends AbstractPluginServiceRegistry {
                 StringInterner stringInterner
             ) {
                 StartParameter startParameter = gradle.getStartParameter();
+                InstanceGenerator instantiator = instantiatorFactory.inject(serviceRegistry);
                 if (Boolean.getBoolean("org.gradle.unsafe.cache.ng")) {
-                    // TODO Set location from local cache configuration instead
-                    BuildCacheService local = new H2LocalCacheService(new File(startParameter.getGradleUserHomeDir(), "caches/build-cache-2").toPath());
-                    // TODO Use real HTTP build cache service
-                    BuildCacheService remote = new BuildCacheService() {
-                        @Override
-                        public boolean load(BuildCacheKey key, BuildCacheEntryReader reader) throws BuildCacheException {
-                            return false;
-                        }
-
-                        @Override
-                        public void store(BuildCacheKey key, BuildCacheEntryWriter writer) throws BuildCacheException {
-                        }
-
-                        @Override
-                        public void close() throws IOException {
-                        }
-                    };
-                    NextGenBuildCacheAccess cacheAccess = new DefaultNextGenBuildCacheAccess(local, remote);
-                    return new NextGenBuildCacheController(cacheAccess);
+                    return NextGenBuildCacheControllerFactory.create(
+                        serviceRegistry.get(GlobalScopedCacheBuilderFactory.class),
+                        buildCacheConfiguration,
+                        instantiator
+                    );
                 }
                 Path buildIdentityPath = gradle.getIdentityPath();
                 BuildCacheControllerFactory.BuildCacheMode buildCacheMode = startParameter.isBuildCacheEnabled() ? BuildCacheControllerFactory.BuildCacheMode.ENABLED : BuildCacheControllerFactory.BuildCacheMode.DISABLED;
@@ -230,7 +210,7 @@ public final class BuildCacheServices extends AbstractPluginServiceRegistry {
                     remoteAccessMode,
                     logStackTraces,
                     emitDebugLogging,
-                    instantiatorFactory.inject(serviceRegistry),
+                    instantiator,
                     fileSystemAccess,
                     packer,
                     originMetadataFactory,
