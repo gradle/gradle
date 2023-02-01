d181c67dca7170a4ac7e6a7da48b0af69b08a2db
Lóránt Pintér <lorant@gradle.com>
2023-02-01 10:13:27 +0100

    Introduce NextGenBuildCacheHandler

---
+++ subprojects/build-cache/src/main/java/org/gradle/caching/internal/controller/NextGenBuildCacheHandler.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.caching.internal.controller;
+
+import org.gradle.caching.BuildCacheEntryReader;
+import org.gradle.caching.BuildCacheEntryWriter;
+import org.gradle.caching.BuildCacheException;
+import org.gradle.caching.BuildCacheKey;
+
+import java.io.Closeable;
+
+public interface NextGenBuildCacheHandler extends Closeable {
+    boolean contains(BuildCacheKey key);
+
+    boolean load(BuildCacheKey key, BuildCacheEntryReader reader) throws BuildCacheException;
+
+    boolean shouldStore(BuildCacheKey key);
+
+    void store(BuildCacheKey key, BuildCacheEntryWriter writer) throws BuildCacheException;
+
+}
