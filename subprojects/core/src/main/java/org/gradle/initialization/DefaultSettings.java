75d5e411e8eae4e6d4ed2a7e24cab8cf02463e57
Reinhold Degenfellner <rdegenfellner@gradle.com>
2023-02-01 13:05:43 +0100

    Fix word order and formatting
    feedback
    
    - part of #15655
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/core/src/main/java/org/gradle/initialization/DefaultSettings.java
+++ subprojects/core/src/main/java/org/gradle/initialization/DefaultSettings.java
@@ -111,7 +111,7 @@ public abstract class DefaultSettings extends AbstractPluginAware implements Set
     }
 
     private static String getProjectName(File settingsDir) {
-        if(contains(File.listRoots(), settingsDir)) {
+        if (contains(File.listRoots(), settingsDir)) {
             String rootIndicator = settingsDir.toPath().getRoot().toString().replaceAll("[\\\\:\\/]*", "");
             // using "-" to separate the parts of the root project name to allow easier usage in the CLI, just in case.
             return "generated-" + rootIndicator  + (rootIndicator.isEmpty() ? "" : "-") +
