daa3d094b9db7542f326a41c9fdedbe1b799fe3d
Adam Murdoch <adam@gradle.com>
2023-02-03 08:57:18 +1100

    Change SelectedArtifactSet to provide a view of the resolved files, in addition to the resolved artifacts.
    This allows the configuration cache to avoid unnecessarily recreating the artifact instances when only the files are required.

--- subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/configurations/DefaultConfigurationSpec.groovy
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/configurations/DefaultConfigurationSpec.groovy
@@ -52,8 +52,7 @@ import org.gradle.api.internal.artifacts.dsl.PublishArtifactNotationParserFactor
 import org.gradle.api.internal.artifacts.dsl.dependencies.DependencyLockingProvider
 import org.gradle.api.internal.artifacts.ivyservice.DefaultLenientConfiguration
 import org.gradle.api.internal.artifacts.ivyservice.moduleconverter.RootComponentMetadataBuilder
-import org.gradle.api.internal.artifacts.ivyservice.resolveengine.artifact.ArtifactVisitor
-import org.gradle.api.internal.artifacts.ivyservice.resolveengine.artifact.ResolvableArtifact
+import org.gradle.api.internal.artifacts.ivyservice.resolveengine.artifact.ResolvedFileVisitor
 import org.gradle.api.internal.artifacts.ivyservice.resolveengine.artifact.SelectedArtifactSet
 import org.gradle.api.internal.artifacts.ivyservice.resolveengine.artifact.VisitedArtifactSet
 import org.gradle.api.internal.artifacts.ivyservice.resolveengine.projectresult.ResolvedLocalComponentsResult
@@ -477,11 +476,9 @@ class DefaultConfigurationSpec extends Specification {
         def visitedArtifactSet = Stub(VisitedArtifactSet)
 
         _ * visitedArtifactSet.select(_, _, _, _, _) >> Stub(SelectedArtifactSet) {
-            visitArtifacts(_, _) >> { ArtifactVisitor visitor, boolean l ->
+            visitFiles(_, _) >> { ResolvedFileVisitor visitor, boolean l ->
                 files.each {
-                    def artifact = Stub(ResolvableArtifact)
-                    _ * artifact.file >> it
-                    visitor.visitArtifact(null, null, [], artifact)
+                    visitor.visitFile(it)
                 }
                 visitor.endVisitCollection(null)
             }
@@ -514,7 +511,7 @@ class DefaultConfigurationSpec extends Specification {
         def resolvedConfiguration = Stub(ResolvedConfiguration)
 
         _ * visitedArtifactSet.select(_, _, _, _, _) >> Stub(SelectedArtifactSet) {
-            visitArtifacts(_, _) >> { ArtifactVisitor v, boolean l -> v.visitFailure(failure) }
+            visitFiles(_, _) >> { ResolvedFileVisitor v, boolean l -> v.visitFailure(failure) }
         }
         _ * resolvedConfiguration.hasError() >> true
 
