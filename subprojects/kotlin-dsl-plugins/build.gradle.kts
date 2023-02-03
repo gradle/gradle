6aa55a514cd9ab54adaa6c8cd7d6fcd1454b0eda
Paul Merlin <paul@gradle.com>
2023-02-03 15:54:44 +0100

    Bump :kotlin-dsl plugin version
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/kotlin-dsl-plugins/build.gradle.kts
+++ subprojects/kotlin-dsl-plugins/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 description = "Kotlin DSL Gradle Plugins deployed to the Plugin Portal"
 
 group = "org.gradle.kotlin"
-version = "4.0.7"
+version = "4.0.8"
 
 base.archivesName = "plugins"
 
