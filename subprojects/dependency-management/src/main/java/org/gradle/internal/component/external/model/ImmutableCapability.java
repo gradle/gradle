b926e95408b56141ef813c9e0bbabe100ec48d69
Thomas Tresansky <ttresansky@gradle.com>
2023-02-10 15:45:59 -0500

    Add more error info to javadoc of substitute class
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/dependency-management/src/main/java/org/gradle/internal/component/external/model/ImmutableCapability.java
+++ subprojects/dependency-management/src/main/java/org/gradle/internal/component/external/model/ImmutableCapability.java
@@ -20,10 +20,26 @@ import javax.annotation.Nullable;
 
 /**
  * This is a deprecated internal class that exists specifically to be used by the
- * Android and Kotlin Androind plugins, which expect this type.
+ * Android and Kotlin Android plugins, which expect this type.
  * <p>
  * Be careful to not confuse it with the {@link org.gradle.api.internal.capabilities.ImmutableCapability ImmutableCapability}
  * interface.
+ * <p>
+ * Without this class in place these smoke tests fail:
+ * <ul>
+ * <li>{@code KotlinPluginAndroidKotlinDSLSmokeTest} and {@code KotlinPluginAndroidGroovyDSLSmokeTest} with kotlin 1.6.21, agp 7.3.1 - 8.0.0-alpha11.</li>
+ * <li>{@code AndroidPluginsSmokeTest} with 7.3.1 - 8.0.0-alpha11.</li>
+ * <li>{@code AndroidSantaTrackerIncrementalCompilationSmokeTest}, {@code AndroidSantaTrackerCachingCompilationSmokeTest}, {@code AndroidSantaTrackerDeprecationSmokeTest} with the same versions.</li>
+ * </ul>
+ * They all fail with some version (different configurations and task names) of:
+ *
+ * <code>
+ *     Could not resolve all dependencies for configuration ':cityquiz:debugRuntimeClasspath'.
+ *       Could not create task ':santa-tracker:generateDebugFeatureTransitiveDeps'.
+ *         org/gradle/internal/component/external/model/ImmutableCapability
+ * </code>
+ *
+ * The task at fault is {@code com.android.build.gradle.internal.tasks.featuresplit.PackagedDependenciesWriterTask`}.
  *
  * @deprecated Use {@link DefaultImmutableCapability} instead.
  */
