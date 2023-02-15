517574562cec48398c4c5f03c1ca9bc0fe76c8eb
Paul Merlin <paul@gradle.com>
2023-02-15 17:40:40 +0100

    Rollback using broken `kotlin-dsl` plugin 4.0.8

--- build-logic-commons/gradle-plugin/build.gradle.kts
+++ build-logic-commons/gradle-plugin/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     compileOnly("com.gradle:gradle-enterprise-gradle-plugin:3.12.3")
 
     implementation(project(":commons"))
-    implementation("org.gradle.kotlin.kotlin-dsl:org.gradle.kotlin.kotlin-dsl.gradle.plugin:4.0.8")
+    implementation("org.gradle.kotlin.kotlin-dsl:org.gradle.kotlin.kotlin-dsl.gradle.plugin:4.0.7")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     implementation("org.gradle.kotlin:gradle-kotlin-dsl-conventions:0.8.0")
 }
