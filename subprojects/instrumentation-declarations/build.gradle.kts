17feb766207393d061e91167ff2840575b7303a8
Anže Sodja <asodja@gradle.com>
2023-02-22 17:32:54 +0100

    Upgrade type of Checkstyle.maxErrors to Property

--- subprojects/instrumentation-declarations/build.gradle.kts
+++ subprojects/instrumentation-declarations/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     compileOnly(project(":model-core"))
     compileOnly(project(":reporting"))
     compileOnly(libs.groovy)
-    compileOnly(project(":code-quality"))
+    implementation(project(":code-quality"))
 
     // Instrumentation dependencies
     compileOnly(project(":internal-instrumentation-api"))
