4f5dedf715a0a438e2bd7cca52a737e13529c00e
Anže Sodja <asodja@gradle.com>
2023-02-22 17:32:54 +0100

    Upgrade type of Checkstyle.maxErrors to Property

--- platforms/core-runtime/instrumentation-declarations/build.gradle.kts
+++ platforms/core-runtime/instrumentation-declarations/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("gradlebuild.distribution.api-java")
+    id("gradlebuild.instrumented-java-project")
 }
 
 description = "Contains declarations for instrumentation of plugins. Adds interceptors, bytecode upgrades etc."
@@ -14,18 +15,12 @@ dependencies {
     compileOnly(projects.modelCore)
     compileOnly(projects.reporting)
     compileOnly(libs.groovy)
+    // We keep code-quality here, since we would need to separate Groovy and Java sourceset to keep incremental compilation
     compileOnly(projects.codeQuality)
 
-    // Instrumentation dependencies
-    compileOnly(projects.internalInstrumentationApi)
-    compileOnly(libs.asm)
-    compileOnly(libs.asmUtil)
-    compileOnly(libs.asmTree)
-    annotationProcessor(projects.internalInstrumentationProcessor)
-    annotationProcessor(platform(projects.distributionsDependencies))
+    // TODO: Logging is inherited from `gradlebuild.instrumented-project` plugin,
+    //  but project health says it should be explicitly defined,
+    //  should we change it to compileOnly in instrumented-project?
+    implementation(projects.logging)
 }
 
-tasks.named<JavaCompile>("compileJava") {
-    // Without this, javac will complain about unclaimed org.gradle.api.NonNullApi annotation
-    options.compilerArgs.add("-Xlint:-processing")
-}
