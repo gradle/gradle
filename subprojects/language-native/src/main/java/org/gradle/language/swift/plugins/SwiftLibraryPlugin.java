1835817f29758bd81fb28498c05fff6f724b86bd
Thomas Tresansky <ttresansky@gradle.com>
2023-02-27 12:16:20 -0500

    Further use role-based configuration creation API in language-native subproject

--- subprojects/language-native/src/main/java/org/gradle/language/swift/plugins/SwiftLibraryPlugin.java
+++ subprojects/language-native/src/main/java/org/gradle/language/swift/plugins/SwiftLibraryPlugin.java
@@ -19,8 +19,8 @@ package org.gradle.language.swift.plugins;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.attributes.Usage;
+import org.gradle.api.internal.artifacts.configurations.RoleBasedConfigurationContainerInternal;
 import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
@@ -84,7 +84,7 @@ public abstract class SwiftLibraryPlugin implements Plugin<Project> {
     public void apply(final Project project) {
         project.getPluginManager().apply(SwiftBasePlugin.class);
 
-        final ConfigurationContainer configurations = project.getConfigurations();
+        final RoleBasedConfigurationContainerInternal configurations = (RoleBasedConfigurationContainerInternal) project.getConfigurations();
         final ObjectFactory objectFactory = project.getObjects();
         final ProviderFactory providers = project.getProviders();
 
@@ -146,12 +146,11 @@ public abstract class SwiftLibraryPlugin implements Plugin<Project> {
 
             library.getBinaries().whenElementKnown(SwiftSharedLibrary.class, sharedLibrary -> {
                 Names names = ((ComponentWithNames) sharedLibrary).getNames();
-                Configuration apiElements = configurations.create(names.withSuffix("SwiftApiElements"));
+                Configuration apiElements = configurations.consumable(names.withSuffix("SwiftApiElements"));
                 // TODO This should actually extend from the api dependencies, but since Swift currently
                 // requires all dependencies to be treated like api dependencies (with transitivity) we just
                 // use the implementation dependencies here.  See https://bugs.swift.org/browse/SR-1393.
                 apiElements.extendsFrom(((DefaultSwiftSharedLibrary) sharedLibrary).getImplementationDependencies());
-                apiElements.setCanBeResolved(false);
                 apiElements.getAttributes().attribute(Usage.USAGE_ATTRIBUTE, objectFactory.named(Usage.class, Usage.SWIFT_API));
                 apiElements.getAttributes().attribute(LINKAGE_ATTRIBUTE, Linkage.SHARED);
                 apiElements.getAttributes().attribute(DEBUGGABLE_ATTRIBUTE, sharedLibrary.isDebuggable());
@@ -162,7 +161,7 @@ public abstract class SwiftLibraryPlugin implements Plugin<Project> {
 
             library.getBinaries().whenElementKnown(SwiftStaticLibrary.class, staticLibrary -> {
                 Names names = ((ComponentWithNames) staticLibrary).getNames();
-                Configuration apiElements = configurations.create(names.withSuffix("SwiftApiElements"));
+                Configuration apiElements = configurations.consumable(names.withSuffix("SwiftApiElements"));
                 // TODO This should actually extend from the api dependencies, but since Swift currently
                 // requires all dependencies to be treated like api dependencies (with transitivity) we just
                 // use the implementation dependencies here.  See https://bugs.swift.org/browse/SR-1393.
