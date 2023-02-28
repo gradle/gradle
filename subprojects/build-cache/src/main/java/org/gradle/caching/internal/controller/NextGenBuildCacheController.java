43194a3ca23c62dc2231ef19f7af756cb739f470
Anže Sodja <asodja@gradle.com>
2023-02-28 18:10:04 +0100

    Use Files.copy instead of streams in NextGenBuildCacheController

--- subprojects/build-cache/src/main/java/org/gradle/caching/internal/controller/NextGenBuildCacheController.java
+++ subprojects/build-cache/src/main/java/org/gradle/caching/internal/controller/NextGenBuildCacheController.java
@@ -63,7 +63,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UncheckedIOException;
@@ -359,9 +358,7 @@ public class NextGenBuildCacheController implements BuildCacheController {
             cacheAccess.store(manifestIndex, manifestEntry -> new BuildCacheEntryWriter() {
                 @Override
                 public void writeTo(OutputStream output) throws IOException {
-                    try (InputStream input = Files.newInputStream(new File(root, manifestEntry.getRelativePath()).toPath())) {
-                        IOUtils.copyLarge(input, output, bufferProvider.getBuffer());
-                    }
+                    Files.copy(root.toPath().resolve(manifestEntry.getRelativePath()), output);
                 }
 
                 @Override
