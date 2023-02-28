8fbdc6cbdf243e8fa96dcbe14e1266dcd56d1d71
Alex Semin <asemin@gradle.com>
2023-02-28 14:58:41 +0100

    Polish javadocs

--- subprojects/enterprise-operations/src/main/java/org/gradle/api/internal/artifacts/transform/TransformationIdentity.java
+++ subprojects/enterprise-operations/src/main/java/org/gradle/api/internal/artifacts/transform/TransformationIdentity.java
@@ -55,7 +55,8 @@ public interface TransformationIdentity extends NodeIdentity {
     /**
      * Target attributes of the transformed artifact.
      * <p>
-     * The attributes include the source attributes of the artifact before the transformation and
+     * The attributes include all source attributes of the artifact before the transformation,
+     * values for some of which have been changed by the transformation.
      */
     Map<String, String> getTargetAttributes();
 
