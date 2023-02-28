d523f457d3e2051c5d3464228b233cb9ff3a69ee
Anže Sodja <asodja@gradle.com>
2023-02-28 19:44:46 +0100

    Modify example for Kotlin Property assignment so we don't create lazy properties directly

--- subprojects/docs/src/snippets/kotlinDsl/assignment/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/kotlinDsl/assignment/kotlin/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
 }
 
 // tag::assignment[]
+interface MyExtension {
+    val input: Property<String>
+    val output: RegularFileProperty
+}
+
 abstract class MyTask : DefaultTask() {
     @get:Input
     abstract val input: Property<String>
@@ -15,12 +20,15 @@ abstract class MyTask : DefaultTask() {
     }
 }
 
-tasks.register<MyTask>("myTask") {
+val extension = extensions.create<MyExtension>("myExtension").apply {
     input.set("Hello Property") // <1>
     output.set(file("build/myTask/output.txt"))
     input = "Hello Property" // <2>
     ouput = file("build/myTask/output.txt")
-    input = provider { "Hello Property" } // <3>
-    output = provider { file("build/myTask/output.txt") }
+}
+
+tasks.register<MyTask>("myTask") {
+    input = extension.input // <3>
+    output = extension.output
 }
 // end::assignment[]
