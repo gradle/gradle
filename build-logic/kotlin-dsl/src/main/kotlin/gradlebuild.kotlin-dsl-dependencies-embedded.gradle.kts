816df7c70c63f8b64421a885f4f9f52844575970
Paul Merlin <paul@gradle.com>
2023-03-07 17:11:03 +0100

    Use new :kotlin-dsl-plugins version

--- build-logic/kotlin-dsl/src/main/kotlin/gradlebuild.kotlin-dsl-dependencies-embedded.gradle.kts
+++ build-logic/kotlin-dsl/src/main/kotlin/gradlebuild.kotlin-dsl-dependencies-embedded.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 // --- Enable automatic generation of API extensions -------------------
 val apiExtensionsOutputDir = layout.buildDirectory.dir("generated-sources/kotlin")
 
-val publishedKotlinDslPluginVersion = "4.0.7" // TODO:kotlin-dsl
+val publishedKotlinDslPluginVersion = "4.0.10" // TODO:kotlin-dsl
 
 tasks {
     val generateKotlinDependencyExtensions by registering(GenerateKotlinDependencyExtensions::class) {
