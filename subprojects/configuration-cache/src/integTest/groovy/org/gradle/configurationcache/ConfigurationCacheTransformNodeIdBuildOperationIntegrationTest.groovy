abda384b277f926fa7dd69e5fa235a473735bd77
Stefan Wolf <wolf@gradle.com>
2023-03-08 18:30:53 +0100

    Extend test for chained transform nodes
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/configuration-cache/src/integTest/groovy/org/gradle/configurationcache/ConfigurationCacheTransformNodeIdBuildOperationIntegrationTest.groovy
+++ subprojects/configuration-cache/src/integTest/groovy/org/gradle/configurationcache/ConfigurationCacheTransformNodeIdBuildOperationIntegrationTest.groovy
@@ -32,21 +32,53 @@ class ConfigurationCacheTransformNodeIdBuildOperationIntegrationTest extends Abs
     }
 
     def "transform step node ids are stable when using load after store"() {
-        setupBuildWithColorTransformImplementation()
         settingsFile << """
             include 'producer', 'consumer'
         """
 
+        setupBuildWithColorAttributes()
         buildFile << """
             allprojects {
                 dependencies {
-                    registerTransform(MakeGreen) {
+                    registerTransform(MakeColor) {
                         from.attribute(color, 'blue')
+                        to.attribute(color, 'red')
+                        parameters.targetColor.set('red')
+                        parameters.multiplier.set(2)
+                    }
+                    registerTransform(MakeColor) {
+                        from.attribute(color, 'red')
                         to.attribute(color, 'green')
+                        parameters.targetColor.set('green')
+                        parameters.multiplier.set(1)
                     }
                 }
             }
 
+            interface TargetColor extends TransformParameters {
+                @Input
+                Property<String> getTargetColor()
+                @Input
+                Property<Integer> getMultiplier()
+            }
+
+            abstract class MakeColor implements TransformAction<TargetColor> {
+                @InputArtifact
+                abstract Provider<FileSystemLocation> getInputArtifact()
+
+                void transform(TransformOutputs outputs) {
+                    def input = inputArtifact.get().asFile
+                    println "processing [\${input.name}]"
+                    assert input.file
+                    for (def i : 1..parameters.multiplier.get()) {
+                        def output = outputs.file(input.name + "." + parameters.targetColor.get() + "-" + i)
+                        output.text = input.text + "-" + parameters.targetColor.get() + "-" + i
+                    }
+                }
+            }
+        """
+
+        buildFile << """
             project(":consumer") {
                 dependencies {
                     implementation project(":producer")
@@ -59,24 +91,33 @@ class ConfigurationCacheTransformNodeIdBuildOperationIntegrationTest extends Abs
 
         then:
         executedAndNotSkipped(":consumer:resolve")
-        def nodeIdentities = transformStepIdentities
-        def nodeIds = (nodeIdentities*.transformStepNodeId as Set<Long>)
-        def uniqueId = Iterables.getOnlyElement(nodeIds)
+        Map<String, Long> uniqueIdsFromStore = uniqueIdsPerColor
 
         when:
         configurationCacheRun ":consumer:resolve"
         then:
-        def nodeIdentity = Iterables.getOnlyElement(transformStepIdentities)
-        nodeIdentity.transformStepNodeId == uniqueId
+        uniqueIdsPerColor == uniqueIdsFromStore
     }
 
-    private List<Map<String, ?>> getTransformStepIdentities() {
+    private Map<String, Long> getUniqueIdsPerColor() {
+        transformStepIdentities.collectEntries { color, identities ->
+            def nodeIds = (identities*.transformStepNodeId as Set<Long>)
+            [(color): Iterables.getOnlyElement(nodeIds)]
+        }
+    }
+
+    private Map<String, List<Map<String, ?>>> getTransformStepIdentities() {
         def ops = buildOperations.all(CalculateTaskGraphBuildOperationType)
-        return ops.collect { op ->
+        def identities = ops.collect { op ->
             def plannedNodes = op.result.executionPlan as List<Map<String, ?>>
-            def transformStepNodeIdentities = plannedNodes*.nodeIdentity.findAll { it.nodeType.toString() == NodeIdentity.NodeType.TRANSFORM_STEP.name() }
-            assert transformStepNodeIdentities.size() == 1
-            return transformStepNodeIdentities.first() as Map<String, ?>
+            def transformStepNodeIdentities = plannedNodes*.nodeIdentity.findAll { it.nodeType == NodeIdentity.NodeType.TRANSFORM_STEP.name() }
+            return transformStepNodeIdentities
+        }
+        def targetColors = (identities.collect { it*.targetAttributes.color }.flatten() as Set<String>)
+        return targetColors.collectEntries { color ->
+            [(color): identities.collect {plannedTransformSteps ->
+                Iterables.getOnlyElement(plannedTransformSteps.findAll { it.targetAttributes.color == color }) as Map<String, ?>
+            }]
         }
     }
 }
