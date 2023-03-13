ea1658a53264e06cb93d202ddfb1eb249600ad38
Anže Sodja <asodja@gradle.com>
2023-03-13 08:12:12 +0100

    Generate also Groovy interceptor for UpgradedProperty annotation
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/internal-instrumentation-processor/src/main/java/org/gradle/internal/instrumentation/extensions/property/PropertyUpgradeAnnotatedMethodReader.java
+++ subprojects/internal-instrumentation-processor/src/main/java/org/gradle/internal/instrumentation/extensions/property/PropertyUpgradeAnnotatedMethodReader.java
@@ -16,9 +16,6 @@
 
 package org.gradle.internal.instrumentation.extensions.property;
 
-import org.gradle.api.file.DirectoryProperty;
-import org.gradle.api.file.RegularFileProperty;
-import org.gradle.api.provider.Property;
 import org.gradle.internal.instrumentation.api.annotations.UpgradedProperty;
 import org.gradle.internal.instrumentation.model.CallInterceptionRequest;
 import org.gradle.internal.instrumentation.model.CallInterceptionRequestImpl;
@@ -48,17 +45,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static org.gradle.internal.instrumentation.model.CallableKindInfo.GROOVY_PROPERTY;
+import static org.gradle.internal.instrumentation.model.CallableKindInfo.INSTANCE_METHOD;
 import static org.gradle.internal.instrumentation.model.ParameterKindInfo.METHOD_PARAMETER;
+import static org.gradle.internal.instrumentation.model.ParameterKindInfo.RECEIVER;
 import static org.gradle.internal.instrumentation.processor.modelreader.impl.TypeUtils.extractType;
 
 public class PropertyUpgradeAnnotatedMethodReader implements AnnotatedMethodReaderExtension {
 
-    private static final Type PROPERTY = Type.getType(Property.class);
-    private static final Type REGULAR_FILE_PROPERTY = Type.getType(RegularFileProperty.class);
-    private static final Type DIRECTORY_PROPERTY = Type.getType(DirectoryProperty.class);
     private static final Type DEFAULT_TYPE = Type.getType(UpgradedProperty.SameAsGenericType.class);
     // TODO: Should be read from an annotation
-    private static final String INTERCEPTOR_DECLARATION_CLASS_NAME = "org.gradle.internal.classpath.InterceptorDeclaration_JvmBytecodeImplPropertyUpgrades";
+    private static final String INTERCEPTOR_JVM_DECLARATION_CLASS_NAME = "org.gradle.internal.classpath.InterceptorDeclaration_JvmBytecodeImplPropertyUpgrades";
+    private static final String INTERCEPTOR_GROOVY_DECLARATION_CLASS_NAME = "org.gradle.internal.classpath.InterceptorDeclaration_GroovyInterceptorsImplPropertyUpgrades";
 
     @Override
     public Collection<Result> readRequest(ExecutableElement input) {
@@ -69,9 +67,10 @@ public class PropertyUpgradeAnnotatedMethodReader implements AnnotatedMethodRead
 
         try {
             Type originalType = extractOriginalType(input, annotation.get());
-            CallInterceptionRequest getterRequest = createJvmGetterInterceptionRequest(input, originalType);
-            CallInterceptionRequest setterRequest = createJvmSetterInterceptionRequest(input, originalType);
-            return Arrays.asList(new Success(getterRequest), new Success(setterRequest));
+            CallInterceptionRequest groovyPropertyRequest = createGroovyPropertyInterceptionRequest(input, originalType);
+            CallInterceptionRequest jvmGetterRequest = createJvmGetterInterceptionRequest(input, originalType);
+            CallInterceptionRequest jvmSetterRequest = createJvmSetterInterceptionRequest(input, originalType);
+            return Arrays.asList(new Success(groovyPropertyRequest), new Success(jvmGetterRequest), new Success(jvmSetterRequest));
         } catch (AnnotationReadFailure failure) {
             return Collections.singletonList(new InvalidRequest(failure.reason));
         }
@@ -94,10 +93,21 @@ public class PropertyUpgradeAnnotatedMethodReader implements AnnotatedMethodRead
         throw new AnnotationReadFailure("Reading generic types from lazy properties is not yet implemented, set originalType for now." + method.getSimpleName());
     }
 
+    private static CallInterceptionRequest createGroovyPropertyInterceptionRequest(ExecutableElement method, Type originalType) {
+        String propertyName = getPropertyName(method);
+        List<RequestExtra> extras = Arrays.asList(new RequestExtra.OriginatingElement(method), new RequestExtra.InterceptGroovyCalls(INTERCEPTOR_GROOVY_DECLARATION_CLASS_NAME));
+        List<ParameterInfo> parameters = Collections.singletonList(new ParameterInfoImpl("receiver", extractType(method.getEnclosingElement().asType()), RECEIVER));
+        return new CallInterceptionRequestImpl(
+            extractCallableInfo(GROOVY_PROPERTY, method, originalType, propertyName, parameters),
+            extractImplementationInfo(method, originalType, "get", Collections.emptyList()),
+            extras
+        );
+    }
+
     private static CallInterceptionRequest createJvmGetterInterceptionRequest(ExecutableElement method, Type originalType) {
         List<RequestExtra> extras = getJvmRequestExtras(method);
         return new CallInterceptionRequestImpl(
-            extractCallableInfo(method, originalType, method.getSimpleName().toString(), Collections.emptyList()),
+            extractCallableInfo(INSTANCE_METHOD, method, originalType, method.getSimpleName().toString(), Collections.emptyList()),
             extractImplementationInfo(method, originalType, "get", Collections.emptyList()),
             extras
         );
@@ -109,7 +119,7 @@ public class PropertyUpgradeAnnotatedMethodReader implements AnnotatedMethodRead
         List<ParameterInfo> parameters = Collections.singletonList(new ParameterInfoImpl("arg0", originalType, METHOD_PARAMETER));
         List<RequestExtra> extras = getJvmRequestExtras(method);
         return new CallInterceptionRequestImpl(
-            extractCallableInfo(method, returnType, callableName, parameters),
+            extractCallableInfo(INSTANCE_METHOD, method, returnType, callableName, parameters),
             extractImplementationInfo(method, returnType, "set", parameters),
             extras
         );
@@ -119,14 +129,13 @@ public class PropertyUpgradeAnnotatedMethodReader implements AnnotatedMethodRead
     private static List<RequestExtra> getJvmRequestExtras(ExecutableElement method) {
         List<RequestExtra> extras = new ArrayList<>();
         extras.add(new RequestExtra.OriginatingElement(method));
-        extras.add(new RequestExtra.InterceptJvmCalls(INTERCEPTOR_DECLARATION_CLASS_NAME));
+        extras.add(new RequestExtra.InterceptJvmCalls(INTERCEPTOR_JVM_DECLARATION_CLASS_NAME));
         String implementationClass = getGeneratedClassName(method.getEnclosingElement());
         extras.add(new PropertyUpgradeRequestExtra(implementationClass, method.getSimpleName().toString()));
         return extras;
     }
 
-    private static CallableInfo extractCallableInfo(ExecutableElement methodElement, Type returnType, String callableName, List<ParameterInfo> parameter) {
-        CallableKindInfo kindInfo = CallableKindInfo.INSTANCE_METHOD;
+    private static CallableInfo extractCallableInfo(CallableKindInfo kindInfo, ExecutableElement methodElement, Type returnType, String callableName, List<ParameterInfo> parameter) {
         Type owner = extractType(methodElement.getEnclosingElement().asType());
         return new CallableInfoImpl(kindInfo, owner, callableName, returnType, parameter);
     }
@@ -134,7 +143,7 @@ public class PropertyUpgradeAnnotatedMethodReader implements AnnotatedMethodRead
     private static ImplementationInfoImpl extractImplementationInfo(ExecutableElement method, Type returnType, String methodPrefix, List<ParameterInfo> parameters) {
         Type owner = extractType(method.getEnclosingElement().asType());
         Type implementationOwner = Type.getObjectType(getGeneratedClassName(method.getEnclosingElement()));
-        String implementationName = getImplementationMethodName(method, methodPrefix);
+        String implementationName = "access_" + methodPrefix + "_" + getPropertyName(method);
         String implementationDescriptor = Type.getMethodDescriptor(returnType, toArray(owner, parameters));
         return new ImplementationInfoImpl(implementationOwner, implementationName, implementationDescriptor);
     }
@@ -153,10 +162,9 @@ public class PropertyUpgradeAnnotatedMethodReader implements AnnotatedMethodRead
         return array;
     }
 
-    private static String getImplementationMethodName(ExecutableElement method, String methodPrefix) {
+    private static String getPropertyName(ExecutableElement method) {
         String property = method.getSimpleName().toString().replace("get", "");
-        property = Character.toLowerCase(property.charAt(0)) + property.substring(1);
-        return "access_" + methodPrefix + "_" + property;
+        return Character.toLowerCase(property.charAt(0)) + property.substring(1);
     }
 
     // TODO Consolidate with AnnotationCallInterceptionRequestReaderImpl#Failure
