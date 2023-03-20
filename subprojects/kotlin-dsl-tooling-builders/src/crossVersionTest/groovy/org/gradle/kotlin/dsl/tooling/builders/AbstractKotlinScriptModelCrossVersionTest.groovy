4c44ef1b3fffe6f7d492b9a47acd30bc9c12b22a
Paul Merlin <paul@gradle.com>
2023-03-20 17:24:26 +0100

    Fix Kotlin DSL script model cross version tests

--- subprojects/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/AbstractKotlinScriptModelCrossVersionTest.groovy
+++ subprojects/kotlin-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/kotlin/dsl/tooling/builders/AbstractKotlinScriptModelCrossVersionTest.groovy
@@ -132,13 +132,19 @@ abstract class AbstractKotlinScriptModelCrossVersionTest extends ToolingApiSpeci
         return files.collect { it.name }.toSet() as String[]
     }
 
-    protected static void assertContainsGradleKotlinDslJars(List<File> classPath) {
+    protected void assertContainsGradleKotlinDslJars(List<File> classPath) {
+        String gradleApiJar
+        if (targetVersion.baseVersion >= GradleVersion.version("8.2")) {
+            gradleApiJar = "gradle-core-api"
+        } else {
+            gradleApiJar = "gradle-api"
+        }
         def version = "[0-9.]+(-.+?)?"
         assertThat(
             classPath.collect { it.name } as List<String>,
             hasItems(
                 matching("gradle-kotlin-dsl-$version\\.jar"),
-                matching("gradle-api-$version\\.jar"),
+                matching("$gradleApiJar-$version\\.jar"),
                 matching("gradle-kotlin-dsl-extensions-$version\\.jar")
             )
         )
