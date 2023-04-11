ffaae32c83f4435a565e6576df3600c8aacec0e7
Anže Sodja <asodja@gradle.com>
2023-04-11 09:46:53 +0200

    Disable Kotlin classpath snapshot for embedded tests on Windows

--- build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
+++ build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
@@ -224,6 +224,10 @@ fun Test.configureJvmForTest() {
             jvmArgs(listOf("--add-opens", "java.base/java.lang=ALL-UNNAMED")) // Used in tests by ClassLoaderUtils
         }
     }
+    if (usesEmbeddedExecuter() && OperatingSystem.current().isWindows) {
+        // Disable incremental snapshotting for Windows, since it throws OOM
+        jvmArgs("-Dkotlin.incremental.useClasspathSnapshot=false")
+    }
 }
 
 fun Test.addOsAsInputs() {
