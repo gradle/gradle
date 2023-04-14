199117d6e3a323df01442665d5eec38eebdc9da8
Justin Van Dort <jvandort@gradle.com>
2023-04-14 15:25:22 -0400

    Reduce number of ConfigurationRole implementations
    This avoids 5 separate ConfigurationRole implementations by replacing them with a single DefaultConfigurationRole,
    thus avoiding unnecessary duplication.
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

---
+++ subprojects/core/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfigurationRole.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.artifacts.configurations;
+
+/**
+ * Default implementation of {@link ConfigurationRole}.
+ */
+public class DefaultConfigurationRole implements ConfigurationRole {
+
+    private final String name;
+    private final boolean consumable;
+    private final boolean resolvable;
+    private final boolean declarableAgainst;
+    private final boolean consumptionDeprecated;
+    private final boolean resolutionDeprecated;
+    private final boolean declarationDeprecated;
+
+    public DefaultConfigurationRole(
+        String name,
+        boolean consumable,
+        boolean resolvable,
+        boolean declarableAgainst,
+        boolean consumptionDeprecated,
+        boolean resolutionDeprecated,
+        boolean declarationDeprecated
+    ) {
+        this.name = name;
+        this.consumable = consumable;
+        this.resolvable = resolvable;
+        this.declarableAgainst = declarableAgainst;
+        this.consumptionDeprecated = consumptionDeprecated;
+        this.resolutionDeprecated = resolutionDeprecated;
+        this.declarationDeprecated = declarationDeprecated;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean isConsumable() {
+        return consumable;
+    }
+
+    @Override
+    public boolean isResolvable() {
+        return resolvable;
+    }
+
+    @Override
+    public boolean isDeclarableAgainst() {
+        return declarableAgainst;
+    }
+
+    @Override
+    public boolean isConsumptionDeprecated() {
+        return consumptionDeprecated;
+    }
+
+    @Override
+    public boolean isResolutionDeprecated() {
+        return resolutionDeprecated;
+    }
+
+    @Override
+    public boolean isDeclarationAgainstDeprecated() {
+        return declarationDeprecated;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
