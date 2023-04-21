efb016ccaffb1377afaad556f05b94aef6542610
Paul Merlin <paul@gradle.com>
2023-04-21 14:14:25 +0200

    Fix usages of expect(Legacy)DeprecationWarning in AscidoctorPluginSmokeTest
    by adding a test for the latest version
    
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
@@ -77,7 +77,7 @@ abstract class AbstractSmokeTest extends Specification {
         static shadow = Versions.of("8.1.1")
 
         // https://github.com/asciidoctor/asciidoctor-gradle-plugin/tags
-        static asciidoctor = Versions.of("3.3.2")
+        static asciidoctor = Versions.of("3.3.2", "4.0.0-alpha.1")
 
         // https://plugins.gradle.org/plugin/com.github.spotbugs
         static spotbugs = "5.0.13"
