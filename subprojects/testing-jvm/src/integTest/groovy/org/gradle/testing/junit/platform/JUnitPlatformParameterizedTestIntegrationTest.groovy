51efc26b339ac690881e75624530dfe5059e8927
Gary Hale <gary@gradle.com>
2023-04-25 12:26:28 -0400

    Move junit platform under junit package
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/platform/JUnitPlatformParameterizedTestIntegrationTest.groovy
+++ subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/platform/JUnitPlatformParameterizedTestIntegrationTest.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.testing.junitplatform
+package org.gradle.testing.junit.platform
 
 import org.gradle.integtests.fixtures.DefaultTestExecutionResult
 import org.gradle.test.fixtures.file.TestFile
