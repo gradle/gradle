803bc030bdd689a8240f9014eedd94e5df1edda6
Sergey Opivalov <sopivalov@gradle.com>
2023-04-28 11:00:45 +0300

    Store full prohibited hierarchy
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/model-core/src/main/java/org/gradle/internal/instantiation/generator/AsmBackedClassGenerator.java
+++ subprojects/model-core/src/main/java/org/gradle/internal/instantiation/generator/AsmBackedClassGenerator.java
@@ -435,9 +435,15 @@ public class AsmBackedClassGenerator extends AbstractClassGenerator {
         private static final Type BUILD_SERVICE_PROVIDER_TYPE = getType("Lorg/gradle/api/services/internal/BuildServiceProvider;");
         private static final Type INSTRUMENTED_EXECUTION_ACCESS_TYPE = getType("Lorg/gradle/internal/classpath/InstrumentedExecutionAccess;");
 
-        // This set is unlikely will be changing oftenly, so instead of introducing additional level of indirection
-        // we are storing it here despite it's CC-related logic
-        private static final Set<Class<?>> DISALLOWED_AT_EXECUTION_INJECTED_SERVICES_CLASSES = ImmutableSet.of(Project.class, Gradle.class);
+        // This set is unlikely to change often, so instead of introducing an additional level of indirection,
+        // we are storing it here despite its relationship to Configuration Cache logic.
+        // The full prohibited hierarchy is stored because there is no efficient way to check the class hierarchy via `org.objectweb.asm.Type`.
+        private static final Set<Type> DISALLOWED_AT_EXECUTION_INJECTED_SERVICES_TYPES = ImmutableSet.of(
+            getType(Project.class),
+            getType("Lorg/gradle/api/internal/project/ProjectInternal;"),
+            getType(Gradle.class),
+            getType("Lorg/gradle/api/internal/GradleInternal;")
+        );
         private static final Type JAVA_LANG_REFLECT_TYPE = getType(java.lang.reflect.Type.class);
         private static final Type OBJECT_TYPE = getType(Object.class);
         private static final Type CLASS_TYPE = getType(Class.class);
@@ -1168,7 +1174,7 @@ public class AsmBackedClassGenerator extends AbstractClassGenerator {
         }
 
         private BytecodeFragment getInjectedServiceGetterEpilogue(Type serviceType, String getterName) {
-            if (isServiceTypeDisallowedAtExecution(serviceType)) {
+            if (DISALLOWED_AT_EXECUTION_INJECTED_SERVICES_TYPES.contains(serviceType)) {
                 return methodVisitor -> new LocalMethodVisitorScope(methodVisitor) {{
                     // InstrumentedExecutionAccess.disallowedAtExecutionInjectedServiceAccessed(<service-type>,<getter-name>,<this-type-name>)
                     _LDC(serviceType);
@@ -1179,20 +1185,6 @@ public class AsmBackedClassGenerator extends AbstractClassGenerator {
             } else {
                 return BytecodeFragment.NO_OP;
             }
-        }
-
-        private boolean isServiceTypeDisallowedAtExecution(Type serviceType) {
-            Class<?> serviceClass;
-
-            try {
-                serviceClass = Class.forName(serviceType.getClassName());
-            } catch (ClassNotFoundException exception) {
-                return false;
-            }
-
-            return DISALLOWED_AT_EXECUTION_INJECTED_SERVICES_CLASSES
-                .stream()
-                .anyMatch(disallowedServiceClass -> disallowedServiceClass.isAssignableFrom(serviceClass));
         }
 
         @Override
