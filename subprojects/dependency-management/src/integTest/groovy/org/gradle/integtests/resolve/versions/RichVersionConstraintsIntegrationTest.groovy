e4628b5ea88fa3b20aa78b3f4703773d5c692955
Louis Jacomet <louis@gradle.com>
2023-05-02 14:12:15 +0200

    Move version resolution focused tests
    * Moved to a dedicated versions package

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/versions/RichVersionConstraintsIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/versions/RichVersionConstraintsIntegrationTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gradle.integtests.resolve
+package org.gradle.integtests.resolve.versions
 
 import org.gradle.integtests.fixtures.GradleMetadataResolveRunner
 import org.gradle.integtests.fixtures.RequiredFeature
 import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
+import org.gradle.integtests.resolve.AbstractModuleDependencyResolveTest
 import org.gradle.test.fixtures.ivy.IvyModule
 import spock.lang.IgnoreIf
 import spock.lang.Issue
