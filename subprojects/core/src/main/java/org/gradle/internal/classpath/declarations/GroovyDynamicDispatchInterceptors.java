56836ed4f252291fe36e71d203421e3fc8812704
Sergey Igushkin <sigushkin@gradle.com>
2023-05-05 23:06:30 +0400

    Support intercepting Groovy accessors dispatched dynamically

--- subprojects/core/src/main/java/org/gradle/internal/classpath/declarations/GroovyDynamicDispatchInterceptors.java
+++ subprojects/core/src/main/java/org/gradle/internal/classpath/declarations/GroovyDynamicDispatchInterceptors.java
@@ -16,11 +16,19 @@
 
 package org.gradle.internal.classpath.declarations;
 
+import groovy.lang.Closure;
 import groovy.lang.GroovyObject;
+import org.codehaus.groovy.runtime.ScriptBytecodeAdapter;
 import org.codehaus.groovy.runtime.callsite.CallSite;
 import org.gradle.api.NonNullApi;
+import org.gradle.internal.classpath.Instrumented;
 import org.gradle.internal.classpath.InstrumentedGroovyCallsTracker;
 import org.gradle.internal.classpath.InstrumentedGroovyCallsTracker.CallKind;
+import org.gradle.internal.classpath.intercept.AbstractInvocation;
+import org.gradle.internal.classpath.intercept.CallInterceptor;
+import org.gradle.internal.classpath.intercept.InterceptScope;
+import org.gradle.internal.classpath.intercept.Invocation;
+import org.gradle.internal.instrumentation.api.annotations.CallableKind;
 import org.gradle.internal.instrumentation.api.annotations.CallableKind.InstanceMethod;
 import org.gradle.internal.instrumentation.api.annotations.InterceptJvmCalls;
 import org.gradle.internal.instrumentation.api.annotations.ParameterKind.CallerClassName;
@@ -28,7 +36,11 @@ import org.gradle.internal.instrumentation.api.annotations.ParameterKind.Receive
 import org.gradle.internal.instrumentation.api.annotations.SpecificJvmCallInterceptors;
 import org.gradle.internal.instrumentation.api.declarations.InterceptorDeclaration;
 
+import javax.annotation.Nullable;
+
+import static org.gradle.internal.classpath.InstrumentedGroovyCallsTracker.CallKind.GET_PROPERTY;
 import static org.gradle.internal.classpath.InstrumentedGroovyCallsTracker.CallKind.INVOKE_METHOD;
+import static org.gradle.internal.classpath.InstrumentedGroovyCallsTracker.CallKind.SET_PROPERTY;
 
 @SuppressWarnings("NewMethodNamingConvention")
 @NonNullApi
@@ -36,15 +48,92 @@ import static org.gradle.internal.classpath.InstrumentedGroovyCallsTracker.CallK
 public class GroovyDynamicDispatchInterceptors {
     @NonNullApi
     private interface ThrowingCallable<T> {
+        @Nullable
         T call() throws Throwable;
     }
 
-    private static <T> T withEntryPoint(String consumer, String callableName, CallKind kind, ThrowingCallable<T> callable) throws Throwable {
+    private static <T> @Nullable T withEntryPoint(String consumer, String callableName, CallKind kind, ThrowingCallable<T> callable) throws Throwable {
         InstrumentedGroovyCallsTracker.EntryPointCallSite entryPoint = InstrumentedGroovyCallsTracker.enterCall(consumer, callableName, kind);
         try {
             return callable.call();
         } finally {
             InstrumentedGroovyCallsTracker.leaveCall(entryPoint);
+        }
+    }
+
+    @InterceptJvmCalls
+    @InstanceMethod
+    public static Object intercept_callGroovyObjectGetProperty(
+        @Receiver CallSite callSite,
+        Object callReceiver,
+        @CallerClassName String consumer
+    ) throws Throwable {
+        return withEntryPoint(consumer, callSite.getName(), GET_PROPERTY, () -> callSite.callGroovyObjectGetProperty(callReceiver));
+    }
+
+    @InterceptJvmCalls
+    @CallableKind.StaticMethod(ofClass = ScriptBytecodeAdapter.class)
+    public static void intercept_setGroovyObjectProperty(
+        Object messageArgument,
+        /* Sometimes it's null, we can't rely on it, so we still use @CallerClassName */
+        Class<?> senderClass,
+        GroovyObject receiver,
+        String messageName,
+        @CallerClassName String consumer
+    ) throws Throwable {
+        interceptPropertySetter(messageArgument, receiver, messageName, consumer,
+            () -> {
+                ScriptBytecodeAdapter.setGroovyObjectProperty(messageArgument, senderClass, receiver, messageName);
+                return null;
+            });
+    }
+
+    @InterceptJvmCalls
+    @CallableKind.StaticMethod(ofClass = ScriptBytecodeAdapter.class)
+    public static void intercept_setProperty(
+        Object messageArgument,
+        /* Sometimes it's null, we can't rely on it, so we still use @CallerClassName */
+        Class<?> senderClass,
+        Object receiver,
+        String messageName,
+        @CallerClassName String consumer
+    ) throws Throwable {
+        interceptPropertySetter(messageArgument, receiver, messageName, consumer,
+            () -> {
+                ScriptBytecodeAdapter.setProperty(messageArgument, senderClass, receiver, messageName);
+                return null;
+            }
+        );
+    }
+
+    private static void interceptPropertySetter(
+        Object messageArgument,
+        Object receiver,
+        String messageName,
+        String consumer,
+        ThrowingCallable<Void> setOriginalProperty
+    ) throws Throwable {
+        if (!(receiver instanceof Closure)) {
+            CallInterceptor interceptor = Instrumented.INTERCEPTOR_RESOLVER.resolveCallInterceptor(InterceptScope.writesOfPropertiesNamed(messageName));
+            if (interceptor == null) {
+                setOriginalProperty.call();
+            } else {
+                @NonNullApi
+                class SetPropertyInvocationImpl extends AbstractInvocation<Object> {
+                    public SetPropertyInvocationImpl(Object receiver, Object[] args) {
+                        super(receiver, args);
+                    }
+
+                    @Override
+                    public @Nullable Object callOriginal() throws Throwable {
+                        return setOriginalProperty.call();
+                    }
+                }
+                Invocation invocation = new SetPropertyInvocationImpl(receiver, new Object[]{messageArgument});
+                interceptor.doIntercept(invocation, consumer);
+            }
+        } else {
+            withEntryPoint(consumer, messageName, SET_PROPERTY, setOriginalProperty);
         }
     }
 
