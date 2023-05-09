7c25c29c614553bfabb3676bbf917f6321a49543
Katerina Tylindus <ktylindus@gradle.com>
2023-05-09 19:18:17 +0200

    Updated Plugin version to 3.13.2.

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/BuildScanPluginSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/BuildScanPluginSmokeTest.groovy
@@ -87,7 +87,8 @@ class BuildScanPluginSmokeTest extends AbstractSmokeTest {
         "3.12.5",
         "3.12.6",
         "3.13",
-        "3.13.1"
+        "3.13.1",
+        "3.13.2"
     ]
 
     private static final VersionNumber FIRST_VERSION_SUPPORTING_CONFIGURATION_CACHE = VersionNumber.parse("3.4")
