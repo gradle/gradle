91062ff0d0d19d7ffa4b9eea8f7d64d97cbd8895
Adam <897017+aSemy@users.noreply.github.com>
2023-05-19 00:29:49 +0200

    remove duplicated jsoup dependency
    Signed-off-by: Adam <897017+aSemy@users.noreply.github.com>

--- subprojects/distributions-dependencies/build.gradle.kts
+++ subprojects/distributions-dependencies/build.gradle.kts
@@ -116,7 +116,6 @@ dependencies {
         api(libs.joda)                  { version { strictly("2.10.4") }}
         api(libs.joptSimple)            { version { strictly("5.0.4"); because("needed to create profiler in Gradle profiler API") }}
         api(libs.jsoup)                 { version { strictly("1.15.3") }}
-        api(libs.jsoup)                 { version { strictly("1.15.3") }}
         api(libs.jsr305)                { version { strictly("3.0.2") }}
         api(libs.julToSlf4j)            { version { strictly(slf4jVersion) }}
         api(libs.junit)                 { version { strictly("4.13.2") }}
