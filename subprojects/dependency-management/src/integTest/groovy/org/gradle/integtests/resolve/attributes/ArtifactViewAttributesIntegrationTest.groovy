f7482879ed2f0746e5d60e9c286b10784a6604d8
Sterling Greene <sterling@gradle.com>
2023-05-25 11:37:48 -0400

    Rework tests to simplify and make them CC compatible

--- subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/attributes/ArtifactViewAttributesIntegrationTest.groovy
+++ subprojects/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/attributes/ArtifactViewAttributesIntegrationTest.groovy
@@ -16,142 +16,50 @@
 
 package org.gradle.integtests.resolve.attributes
 
-import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 
 /**
- * Tests for [org.gradle.api.artifacts.ArtifactView ArtifactView] that ensure it uses the "live" attributes
- * of the configuration that created it to select files and artifacts.
- *
- * As the necessary attributes are not present on the consumer's compileClasspath configuration until the
- * beforeLocking callback method is called, the previous incorrect behavior of the ArtifactView was to select without
- * considering these attributes.  This has been corrected to use a "live" view
- * of the attributes, to ensure that late changes to the attributes are reflected in the selection.
- *
- * These tests do not use any specific plugin (such as `java-library`) and manually make late changes to the attributes
- * used to resolve the files that are then verified.
+ * Test that ArtifactView has a "live" view of the underlying configuration's attributes.
  */
-class ArtifactViewAttributesIntegrationTest extends AbstractArtifactViewAttributesIntegrationTest {
-    @Override
-    List<String> getExpectedFileNames() {
-        return ["chocolate.jar"]
-    }
+class ArtifactViewAttributesIntegrationTest extends AbstractIntegrationSpec {
+    def "artifact view has live view of underlying configuration attributes"() {
+        buildFile << """
+            def usage = Attribute.of('usage', String)
+            def buildType = Attribute.of('buildType', String)
+            def flavor = Attribute.of('flavor', String)
+            configurations {
+                conf {
+                    attributes {
+                        attribute(usage, 'usage-value')
+                    }
+                }
+            }
 
-    @Override
-    List<String> getExpectedAttributes() {
-        return ['flavor = chocolate']
-    }
-
-    @Override
-    String getTestedClasspathName() {
-        return "consumerConf"
-    }
-
-    def setup() {
-        file("producer/build.gradle") << """
-            interface Flavor extends Named {}
-            def flavor = Attribute.of(Flavor)
+            def view = configurations.conf.incoming.artifactView {
+                attributes {
+                    attribute(buildType, 'buildType-value')
+                }
+            }
 
             configurations {
-                producerConfVanilla {
+                conf {
                     attributes {
-                        attribute(flavor, objects.named(Flavor, 'vanilla'))
-                    }
-                    outgoing {
-                        artifact file('vanilla.jar')
-                    }
-                }
-                producerConfChocolate {
-                    attributes {
-                        attribute(flavor, objects.named(Flavor, 'chocolate'))
-                    }
-                    outgoing {
-                        artifact file('chocolate.jar')
-                    }
-                }
-            }
-        """
-
-        buildFile << """
-            interface Flavor extends Named {}
-            def flavor = Attribute.of(Flavor)
-
-            configurations {
-                consumerConf {
-                    attributes {
-                        attribute(flavor, objects.named(Flavor, 'chocolate'))
+                        attribute(flavor, 'flavor-value')
                     }
                 }
             }
 
-            dependencies {
-                consumerConf(project(":producer"))
+            def map = [:]
+            view.attributes.keySet().each { key ->
+                map[key.name] = view.attributes.getAttribute(key)
             }
-        """
-    }
-
-    @ToBeFixedForConfigurationCache(because = "Need to create an ArtifactView from the incoming ResolvableDependencies at execution time")
-    def "test adding an attribute lately using beforeLocking without produces same files"() {
-        buildFile << """
-            configurations.${testedClasspathName}.beforeLocking {
-                attributes.attribute(flavor, objects.named(Flavor, 'vanilla'))
-            }
-
-            tasks.register('verifyFiles', FilesVerificationTask) {
-                incoming = configurations.${testedClasspathName}.incoming
-            }
+            assert map.size() == 3
+            assert map['usage'] == 'usage-value'
+            assert map['buildType'] == 'buildType-value'
+            assert map['flavor'] == 'flavor-value'
         """
 
         expect:
-        run ':verifyFiles'
-        output.contains("Resolved file: vanilla.jar") // Late attributes have changed the expectations
-        output.contains("Attribute: flavor = vanilla")
-    }
-
-    @ToBeFixedForConfigurationCache(because = "Need to create an ArtifactView from the incoming ResolvableDependencies at execution time")
-    def "test adding an attribute lately without using beforeLocking still produces same files"() {
-        buildFile << """
-            tasks.register('verifyFiles', FilesVerificationTask) {
-                incoming = configurations.${testedClasspathName}.incoming
-                afterCreatingArtifactView = { configurations.${testedClasspathName}.attributes.attribute(flavor, objects.named(Flavor, 'vanilla')) }
-            }
-        """
-
-        expect:
-        run ':verifyFiles'
-        output.contains("Resolved file: vanilla.jar") // Late attributes have changed the expectations
-        output.contains("Attribute: flavor = vanilla")
-    }
-
-    @ToBeFixedForConfigurationCache(because = "Need to create an ArtifactView from the incoming ResolvableDependencies at execution time")
-    def "test adding an attribute lately using beforeLocking without produces same artifacts"() {
-        buildFile << """
-            configurations.${testedClasspathName}.beforeLocking {
-                attributes.attribute(flavor, objects.named(Flavor, 'vanilla'))
-            }
-
-            tasks.register('verifyArtifacts', ArtifactsVerificationTask) {
-                incoming = configurations.${testedClasspathName}.incoming
-            }
-        """
-
-        expect:
-        run ':verifyArtifacts'
-        output.contains("Resolved file: vanilla.jar") // Late attributes have changed the expectations
-        output.contains("Attribute: flavor = vanilla")
-    }
-
-    @ToBeFixedForConfigurationCache(because = "Need to create an ArtifactView from the incoming ResolvableDependencies at execution time")
-    def "test adding an attribute lately without using beforeLocking still produces same artifacts"() {
-        buildFile << """
-            tasks.register('verifyArtifacts', ArtifactsVerificationTask) {
-                incoming = configurations.${testedClasspathName}.incoming
-                afterCreatingArtifactView = { configurations.${testedClasspathName}.attributes.attribute(flavor, objects.named(Flavor, 'vanilla')) }
-            }
-        """
-
-        expect:
-        run ':verifyArtifacts'
-        output.contains("Resolved file: vanilla.jar") // Late attributes have changed the expectations
-        output.contains("Attribute: flavor = vanilla")
+        succeeds 'help'
     }
 }
