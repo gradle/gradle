054cc9e09e0688234cc78a01ba5ca1449654462b
Reinhold Degenfellner <rdegenfellner@gradle.com>
2023-05-26 12:00:18 +0200

    fix tests that fail of Exception wrapping
    moved UncheckedException to other project (should probably go somewhere else)

--- subprojects/build-operations/src/main/java/org/gradle/api/problems/Problems.java
+++ subprojects/build-operations/src/main/java/org/gradle/api/problems/Problems.java
@@ -20,7 +20,6 @@ import org.gradle.api.Incubating;
 import org.gradle.api.problems.interfaces.Problem;
 import org.gradle.api.problems.interfaces.ProblemId;
 import org.gradle.api.problems.interfaces.Severity;
-import org.gradle.internal.UncheckedException;
 import org.gradle.internal.operations.BuildOperationContext;
 import org.gradle.internal.operations.BuildOperationContextTracker;
 
@@ -45,7 +44,21 @@ public class Problems {
             }
         }
         if (problem.getSeverity() == Severity.ERROR) {
-            throw UncheckedException.throwAsUncheckedException(problem.getCause());
+            Throwable t = problem.getCause();
+            RuntimeException result;
+            if (t instanceof InterruptedException) {
+                Thread.currentThread().interrupt();
+            }
+            if (t instanceof RuntimeException) {
+                throw (RuntimeException) t;
+            }
+            if (t instanceof Error) {
+                throw (Error) t;
+            }
+//            if (t instanceof IOException) {
+//                throw new UncheckedIOException(t);
+//            }
+            throw new RuntimeException(t);
         }
     }
 }
