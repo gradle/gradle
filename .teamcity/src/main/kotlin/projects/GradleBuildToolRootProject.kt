923984c16ef5ccba302fa827ec6a214eebd37601
Bo Zhang <bo@gradle.com>
2023-06-16 09:37:54 +0800

    Support building security advisory fork on TeamCity
    We want to build a private fork to fix some security issues.
    This PR supports using the private fork as TeamCity Kotlin DSL versioned
    settings root.

--- .teamcity/src/main/kotlin/projects/GradleBuildToolRootProject.kt
+++ .teamcity/src/main/kotlin/projects/GradleBuildToolRootProject.kt
@@ -1,6 +1,7 @@
 package projects
 
 import common.VersionedSettingsBranch
+import common.isSecurityAdvisoryFork
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 import model.CIBuildModel
 import model.DefaultFunctionalTestBucketProvider
@@ -20,7 +21,9 @@ class GradleBuildToolRootProject(branch: VersionedSettingsBranch) : Project({
     val gradleBuildBucketProvider = DefaultFunctionalTestBucketProvider(model, File("./test-buckets.json"))
     subProject(CheckProject(model, gradleBuildBucketProvider))
 
-    subProject(PromotionProject(model.branch))
-    subProject(UtilProject)
-    subProject(UtilPerformanceProject)
+    if (!isSecurityAdvisoryFork()) {
+        subProject(PromotionProject(model.branch))
+        subProject(UtilProject)
+        subProject(UtilPerformanceProject)
+    }
 })
