b637d287d9c89e84eeaa01189eb17d1497d742cc
Paul Merlin <paul@gradle.com>
2023-06-20 18:00:43 +0200

    Unify kotlin compiler config for building Gradle and binary compatibility checks

--- build-logic/basics/src/main/kotlin/gradlebuild/basics/kotlindsl/kotlin-compiler-configuration.kt
+++ build-logic/basics/src/main/kotlin/gradlebuild/basics/kotlindsl/kotlin-compiler-configuration.kt
@@ -17,12 +17,13 @@
 package gradlebuild.basics.kotlindsl
 
 import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.config.AnalysisFlags
 import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.config.JVMConfigurationKeys
 import org.jetbrains.kotlin.config.JvmAnalysisFlags
-import org.jetbrains.kotlin.config.JvmDefaultMode
+import org.jetbrains.kotlin.config.JvmClosureGenerationScheme
 import org.jetbrains.kotlin.config.LanguageVersion
 import org.jetbrains.kotlin.config.LanguageVersionSettingsImpl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -54,17 +55,18 @@ fun CompilerConfiguration.configureKotlinCompilerForGradleBuild() {
     put(
         CommonConfigurationKeys.LANGUAGE_VERSION_SETTINGS,
         LanguageVersionSettingsImpl(
-            languageVersion = LanguageVersion.KOTLIN_1_4,
-            apiVersion = ApiVersion.KOTLIN_1_4,
+            languageVersion = LanguageVersion.KOTLIN_1_8,
+            apiVersion = ApiVersion.KOTLIN_1_8,
             analysisFlags = mapOf(
                 JvmAnalysisFlags.javaTypeEnhancementState to JavaTypeEnhancementState(
                     Jsr305Settings(ReportLevel.STRICT, ReportLevel.STRICT)
                 ) { ReportLevel.STRICT },
-                JvmAnalysisFlags.jvmDefaultMode to JvmDefaultMode.ENABLE
+                AnalysisFlags.skipMetadataVersionCheck to true,
             )
         )
     )
 
+    put(JVMConfigurationKeys.SAM_CONVERSIONS, JvmClosureGenerationScheme.CLASS)
     put(JVMConfigurationKeys.PARAMETERS_METADATA, true)
     put(JVMConfigurationKeys.JVM_TARGET, org.jetbrains.kotlin.config.JvmTarget.JVM_1_8)
 }
