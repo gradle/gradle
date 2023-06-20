e723a3424dabde7fc1e875fbb39c80462a833e7a
Tom Tresansky <ttresansky@gradle.com>
2023-06-20 08:38:49 -0400

    Update subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
    Co-authored-by: Sterling Greene <big-guy@users.noreply.github.com>

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
@@ -55,7 +55,7 @@ abstract class AbstractSmokeTest extends Specification {
          * @see BuildScanPluginSmokeTest
          */
 
-        // https://github.com/bndtools/bnd/tree/master/gradle-plugins
+        // https://plugins.gradle.org/plugin/biz.aQute.bnd
         static bnd = "6.4.0"
 
         // https://plugins.gradle.org/plugin/com.netflix.nebula.dependency-recommender
