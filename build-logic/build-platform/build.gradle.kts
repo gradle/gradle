065b0e510b06a25395c20a0a0ee9a4406cd108c5
Sterling Greene <sterling@gradle.com>
2023-06-23 16:41:54 -0400

    Update to gradle-guides-plugin 0.22
    This no longer depends on http-builder-ng-core, which is only
    available on JCenter.

--- build-logic/build-platform/build.gradle.kts
+++ build-logic/build-platform/build.gradle.kts
@@ -21,7 +21,7 @@ val kotlinVersion = providers.gradleProperty("buildKotlinVersion")
 
 dependencies {
     constraints {
-        api("org.gradle.guides:gradle-guides-plugin:0.21")
+        api("org.gradle.guides:gradle-guides-plugin:0.22")
         api("com.gradle:gradle-enterprise-gradle-plugin:3.13.4") // Sync with `settings.gradle.kts`
         api("com.gradle.publish:plugin-publish-plugin:1.1.0")
         api("gradle.plugin.org.jetbrains.gradle.plugin.idea-ext:gradle-idea-ext:1.0.1")
