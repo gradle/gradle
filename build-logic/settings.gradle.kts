08d232cd9983b940283a25935bf161bed1cec022
bot-gradle <bot-gradle@gradle.com>
2023-06-25 09:20:02 +0200

    Merge pull request #25497 Use jCenter artifacts from repo.gradle.org
    We uploaded them to there now that they aren't any more on jCenter.
    
    Co-authored-by: Stefan Wolf <wolf@gradle.com>

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -31,7 +31,9 @@ dependencyResolutionManagement {
                 includeVersionByRegex("com.gradle.internal", "test-selection-gradle-plugin", rcAndMilestonesPattern)
             }
         }
-        jcenter {
+        maven {
+            name = "Gradle public repository"
+            url = uri("https://repo.gradle.org/gradle/public")
             content {
                 includeModule("org.openmbee.junit", "junit-xml-parser")
                 includeModule("org.codehaus.groovy.modules", "http-builder-ng-core")
