5dec07bebb1887b1732e26afc00db8a9f3ceabe6
Stefan Wolf <wolf@gradle.com>
2023-06-30 08:58:18 +0200

    Add test for SnapshotTransformInputsBuildOperationResult

---
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/transform/SnapshotTransformInputsBuildOperationResultTest.groovy
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.artifacts.transform
+
+import org.gradle.api.internal.tasks.AbstractSnapshotInputsBuildOperationResultTest
+import org.gradle.api.internal.tasks.properties.InputFilePropertySpec
+import org.gradle.internal.execution.caching.CachingState
+
+class SnapshotTransformInputsBuildOperationResultTest extends AbstractSnapshotInputsBuildOperationResultTest<SnapshotTransformInputsBuildOperationResult> {
+    @Override
+    SnapshotTransformInputsBuildOperationResult createSnapshotInputsBuildOperationResult(CachingState cachingState, Set<InputFilePropertySpec> inputFilePropertySpecs) {
+        return new SnapshotTransformInputsBuildOperationResult(cachingState, inputFilePropertySpecs)
+    }
+}
