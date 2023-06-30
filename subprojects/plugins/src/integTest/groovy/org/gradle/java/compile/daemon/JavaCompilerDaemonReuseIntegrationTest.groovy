7e0f7d3c34fa6d588b0a185451e0c482a98a1ac6
Octavia Togami <otogami@gradle.com>
2023-06-29 17:04:21 -0700

    Allow new reuse tests to run in parallel

--- subprojects/plugins/src/integTest/groovy/org/gradle/java/compile/daemon/JavaCompilerDaemonReuseIntegrationTest.groovy
+++ subprojects/plugins/src/integTest/groovy/org/gradle/java/compile/daemon/JavaCompilerDaemonReuseIntegrationTest.groovy
@@ -19,16 +19,12 @@ package org.gradle.java.compile.daemon
 import org.gradle.api.internal.tasks.execution.ExecuteTaskBuildOperationType
 import org.gradle.api.tasks.compile.AbstractCompilerDaemonReuseIntegrationTest
 import org.gradle.integtests.fixtures.BuildOperationsFixture
-import org.gradle.integtests.fixtures.UnsupportedWithConfigurationCache
-import org.gradle.integtests.fixtures.executer.GradleContextualExecuter
 import org.gradle.integtests.fixtures.jvm.TestJvmComponent
 import org.gradle.language.fixtures.TestJavaComponent
 import org.gradle.workers.internal.ExecuteWorkItemBuildOperationType
 import org.gradle.workers.internal.KeepAliveMode
-import spock.lang.IgnoreIf
 
 import static org.gradle.api.internal.tasks.compile.DaemonJavaCompiler.KEEP_DAEMON_ALIVE_PROPERTY
-
 
 class JavaCompilerDaemonReuseIntegrationTest extends AbstractCompilerDaemonReuseIntegrationTest {
     @Override
@@ -48,10 +44,13 @@ class JavaCompilerDaemonReuseIntegrationTest extends AbstractCompilerDaemonReuse
         return new TestJavaComponent()
     }
 
-    @IgnoreIf({ GradleContextualExecuter.parallel })
-    @UnsupportedWithConfigurationCache(because = "parallel by default")
     def "reuses compiler daemons across multiple builds when enabled"() {
         withSingleProjectSources()
+        buildFile << """
+            tasks.compileMain2Java {
+                dependsOn("compileJava")
+            }
+        """
 
         when:
         args("-D${KEEP_DAEMON_ALIVE_PROPERTY}=${KeepAliveMode.DAEMON.name()}")
@@ -79,19 +78,22 @@ class JavaCompilerDaemonReuseIntegrationTest extends AbstractCompilerDaemonReuse
         compilerDaemonIdentityFile.text.trim() == firstCompilerIdentity
     }
 
-    @IgnoreIf({ GradleContextualExecuter.parallel })
-    @UnsupportedWithConfigurationCache(because = "parallel by default")
     def "log messages from a compiler daemon are associated with the task that generates them"() {
         def buildOperations = new BuildOperationsFixture(executer, temporaryFolder)
 
         withSingleProjectSources()
+        buildFile << """
+            tasks.compileMain2Java {
+                dependsOn("compileJava")
+            }
+        """
         def classWithWarning = """
             class ClassWithWarning {
                 java.util.Date date = new java.util.Date (100, 11, 07);
             }
         """
-        file('src/main/java/ClassWithWarning.java') << classWithWarning
-        file('src/main2/java/ClassWithWarning.java') << classWithWarning
+        file('src/main/java/ClassWithWarning1.java') << classWithWarning
+        file('src/main2/java/ClassWithWarning2.java') << classWithWarning
 
         when:
         args("-D${KEEP_DAEMON_ALIVE_PROPERTY}=${KeepAliveMode.DAEMON.name()}")
@@ -121,14 +123,16 @@ class JavaCompilerDaemonReuseIntegrationTest extends AbstractCompilerDaemonReuse
         and:
         def compilerOperations = buildOperations.all(ExecuteWorkItemBuildOperationType)
         def taskOperations =
-            compilerOperations.collect {
-                buildOperations.parentsOf(it).reverse().find {parent -> buildOperations.isType(parent, ExecuteTaskBuildOperationType) }
+            compilerOperations.collectEntries {
+                def op = buildOperations.parentsOf(it).reverse().find {parent -> buildOperations.isType(parent, ExecuteTaskBuildOperationType) }
+                [op.displayName, it]
             }
 
-        compilerOperations.size() == 2
-        compilerOperations.each {operation ->
-            assert operation.progress.find {progress -> progress.details.spans.any { it.text.contains "ClassWithWarning.java uses or overrides a deprecated API" } }
+        def tasks = ['Task :compileJava', 'Task :compileMain2Java']
+        taskOperations.keySet() == tasks.toSet()
+        tasks.eachWithIndex { taskName, index ->
+            def operation = taskOperations[taskName]
+            assert operation.progress.find {progress -> progress.details.spans.any { it.text.contains "ClassWithWarning${index + 1}.java uses or overrides a deprecated API" } }
         }
-        taskOperations.collect {it.displayName }.containsAll(['Task :compileJava', 'Task :compileMain2Java'])
     }
 }
