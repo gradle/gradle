edd6187c34c2d9b60b15203e795632cdf5c52377
Sterling Greene <sterling@gradle.com>
2023-07-07 16:28:31 -0400

    Recognize certain options as making workers unreliable on Windows
    When arguments like -javaagent are used with a worker, its possible
    for this option to add a file that is held open by the worker across
    builds.
    
    If this is a jar that comes from the dependency cache, this is mostly
    OK.
    
    If this is a jar that comes from another project, this breaks simple
    things like running clean because we're unable to delete the build
    directory without stopping the worker process.
    
    This change causes the daemon option builder to switch to SESSION-persistent
    daemons. This has been the default for a long time and mostly works.
    
    This change doesn't eliminate all possible avenues to cause this problem.
    There are other ways to pass options to the worker process that
    could cause it to hold on to files (even non-jars).

--- subprojects/workers/src/main/java/org/gradle/workers/internal/DaemonForkOptionsBuilder.java
+++ subprojects/workers/src/main/java/org/gradle/workers/internal/DaemonForkOptionsBuilder.java
@@ -16,11 +16,20 @@
 
 package org.gradle.workers.internal;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.gradle.internal.os.OperatingSystem;
 import org.gradle.process.JavaForkOptions;
 import org.gradle.process.internal.JavaForkOptionsFactory;
 import org.gradle.process.internal.JavaForkOptionsInternal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class DaemonForkOptionsBuilder {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DaemonForkOptionsBuilder.class);
+
     private final JavaForkOptionsInternal javaForkOptions;
     private final JavaForkOptionsFactory forkOptionsFactory;
     private KeepAliveMode keepAliveMode = KeepAliveMode.DAEMON;
@@ -47,7 +56,56 @@ public class DaemonForkOptionsBuilder {
     }
 
     public DaemonForkOptions build() {
-        return new DaemonForkOptions(buildJavaForkOptions(), keepAliveMode, classLoaderStructure);
+        JavaForkOptionsInternal forkOptions = buildJavaForkOptions();
+        if (OperatingSystem.current().isWindows() && keepAliveMode == KeepAliveMode.DAEMON) {
+            List<String> jvmArgs = forkOptions.getAllJvmArgs();
+            if (containsUnreliableOptions(jvmArgs)) {
+                LOGGER.info("Worker requested to be persistent, but its JVM arguments may make the worker unreliable. Worker will expire at the end of the build.");
+                return new DaemonForkOptions(forkOptions, KeepAliveMode.SESSION, classLoaderStructure);
+            }
+        }
+        return new DaemonForkOptions(forkOptions, keepAliveMode, classLoaderStructure);
+    }
+
+    /**
+     * Users can add files that are held open by the worker process. This causes problems on Windows with persistent workers because
+     * we cannot delete files that are held by the worker process.
+     *
+     * @param jvmArgs JVM arguments to check
+     * @return true if the command-line arguments contain options that could make the worker process unreliable
+     */
+    @VisibleForTesting
+    static boolean containsUnreliableOptions(List<String> jvmArgs) {
+        // This isn't exhaustive because there are more ways that extra files can be provided
+        // to the worker through diagnostic options, @files or JAVA_TOOL_OPTIONS.
+        List<String> unreliableOptions = Arrays.asList(
+            // Classpath options
+            "-cp", "-classpath", "--class-path",
+            // Module related options
+            "-p", "--module-path", "--upgrade-module-path", "--patch-module"
+        );
+        // These options allow you to use : instead of a space to separate the
+        // option from the value
+        List<String> unreliableOptionPrefixes = Arrays.asList(
+            // bootclasspath can also end with /a or /p
+            "-Xbootclasspath",
+            // Defining a java agent
+            "-javaagent"
+        );
+
+        for (String jvmArg : jvmArgs) {
+            if (jvmArg.startsWith("-")) {
+                if (unreliableOptions.contains(jvmArg)) {
+                    return true;
+                }
+                for (String prefix : unreliableOptionPrefixes) {
+                    if (jvmArg.startsWith(prefix)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     private JavaForkOptionsInternal buildJavaForkOptions() {
