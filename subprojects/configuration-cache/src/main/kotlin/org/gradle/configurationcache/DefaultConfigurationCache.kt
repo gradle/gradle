3cea721df87197ab830828c27cc40ad77ddacf06
Sergey Igushkin <sigushkin@gradle.com>
2023-07-07 22:59:42 +0400

    Remove the redundant call to `Instrumented.discardListener()`

--- subprojects/configuration-cache/src/main/kotlin/org/gradle/configurationcache/DefaultConfigurationCache.kt
+++ subprojects/configuration-cache/src/main/kotlin/org/gradle/configurationcache/DefaultConfigurationCache.kt
@@ -350,10 +350,6 @@ class DefaultConfigurationCache internal constructor(
             }
         }
 
-        // TODO - fingerprint should be collected until the state file has been written, as user code can run during this process
-        // Moving this is currently broken because the Jar task queries provider values when serializing the manifest file tree and this
-        // can cause the provider value to incorrectly be treated as a task graph input
-        Instrumented.discardListener()
         crossConfigurationTimeBarrier()
     }
 
