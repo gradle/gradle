38aadcefe612299852544761ab2c3763fcd0eeed
Tom Tresansky <ttresansky@gradle.com>
2023-07-10 08:13:03 -0400

    Undo some non-changes that are just noise in the diff

--- platforms/jvm/toolchains-jvm/src/integTest/groovy/org/gradle/groovy/compile/AbstractToolchainGroovyCompileIntegrationTest.groovy
+++ platforms/jvm/toolchains-jvm/src/integTest/groovy/org/gradle/groovy/compile/AbstractToolchainGroovyCompileIntegrationTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ abstract class AbstractToolchainGroovyCompileIntegrationTest extends ApiGroovyCo
     def setup() {
         jdk = computeJdkForTest()
         Assume.assumeNotNull(jdk)
-        Assume.assumeTrue(GroovyCoverage.supportsJavaVersion(MultiVersionIntegrationSpec.version, jdk.javaVersion))
+        Assume.assumeTrue(GroovyCoverage.supportsJavaVersion(version, jdk.javaVersion))
 
         executer.beforeExecute {
             withInstallations(jdk)
--- platforms/jvm/toolchains-jvm/src/integTest/groovy/org/gradle/groovy/compile/GroovyCompileToolchainIntegrationTest.groovy
+++ platforms/jvm/toolchains-jvm/src/integTest/groovy/org/gradle/groovy/compile/GroovyCompileToolchainIntegrationTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ class GroovyCompileToolchainIntegrationTest extends MultiVersionIntegrationSpec 
             ${mavenCentralRepository()}
 
             dependencies {
-                implementation "${groovyModuleDependency("groovy", MultiVersionIntegrationSpec.version)}"
+                implementation "${groovyModuleDependency("groovy", version)}"
             }
         """
     }
