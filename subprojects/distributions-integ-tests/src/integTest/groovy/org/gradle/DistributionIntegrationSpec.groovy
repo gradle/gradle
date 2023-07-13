8ec812de43b9a219f49220d4f58af86d384ebe38
Tom Tresansky <ttresansky@gradle.com>
2023-07-13 07:36:24 -0400

    Update plugins jars count

--- subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -57,7 +57,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
      * Change this whenever you add or remove subprojects for distribution-packaged plugins (lib/plugins).
      */
     int getPackagedPluginsJarCount() {
-        48
+        47
     }
 
     /**
