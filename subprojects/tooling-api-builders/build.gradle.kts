32b2e9faf845d10f5b706a17fca2c9579d2a211e
Tom Tresansky <ttresansky@gradle.com>
2023-07-26 14:00:34 -0400

    Restrict dep only needed when running compile task

--- subprojects/tooling-api-builders/build.gradle.kts
+++ subprojects/tooling-api-builders/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation(project(":testing-base"))
     implementation(project(":testing-jvm"))
     implementation(project(":tooling-api"))
-    implementation(project(":toolchains-jvm"))
+    testCompileOnly(project(":toolchains-jvm"))
     implementation(project(":workers"))
 
     implementation(libs.groovy) // for 'Closure'
