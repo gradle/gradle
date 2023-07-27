5c1c9291889b5bf3515a0ac0b9dd8f22e1da6b44
Julia Boes <jboes@gradle.com>
2023-07-27 09:58:03 +0200

    try out shadow plugin

--- subprojects/wrapper/build.gradle.kts
+++ subprojects/wrapper/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import java.util.jar.Attributes
 
 plugins {
     id("gradlebuild.distribution.api-java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+//    id("java")
 }
 
 description = "Bootstraps a Gradle build initiated by the gradlew script"
@@ -32,16 +35,36 @@ dependencies {
     crossVersionTestDistributionRuntimeOnly(project(":distributions-full"))
 }
 
-val executableJar by tasks.registering(Jar::class) {
+//val executableJar by tasks.registering(Jar::class) {
+//    archiveFileName = "gradle-wrapper.jar"
+//    manifest {
+//        attributes.remove(Attributes.Name.IMPLEMENTATION_VERSION.toString())
+//        attributes(Attributes.Name.IMPLEMENTATION_TITLE.toString() to "Gradle Wrapper")
+//    }
+//    from(sourceSets.main.get().output)
+//    from(configurations.runtimeClasspath.get().incoming.artifactView {
+//        attributes.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements.CLASSES))
+//    }.files)
+//}
+//
+//tasks.jar {
+//    from(executableJar)
+//}
+
+val executableJar by tasks.registering(ShadowJar::class) {
     archiveFileName = "gradle-wrapper.jar"
     manifest {
         attributes.remove(Attributes.Name.IMPLEMENTATION_VERSION.toString())
         attributes(Attributes.Name.IMPLEMENTATION_TITLE.toString() to "Gradle Wrapper")
     }
     from(sourceSets.main.get().output)
-    from(configurations.runtimeClasspath.get().incoming.artifactView {
+    from(project.configurations.runtimeClasspath.get().incoming.artifactView {
         attributes.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements.CLASSES))
     }.files)
+
+    minimize {
+        exclude(project(":wrapper-shared"))
+    }
 }
 
 tasks.jar {
