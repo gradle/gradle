a0a40d8fc55a903e29ab92fd3391ab9b0b25a2c7
Bo Zhang <bo@gradle.com>
2023-07-27 10:31:51 +0800

    Change flaky deprecation warnings to "maybe"
    We've observed a lot of flakiness caused by deprecation messages
    in the test case. Now let's change them to "maybe".

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/WithReportDeprecations.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/WithReportDeprecations.groovy
@@ -25,15 +25,22 @@ import static org.gradle.api.internal.DocumentationRegistry.RECOMMENDATION
 @SelfType(BaseDeprecations)
 trait WithReportDeprecations {
     private static final String REPORT_DESTINATION_DEPRECATION = "The Report.destination property has been deprecated. " +
-            "This is scheduled to be removed in Gradle 9.0. " +
-            "Please use the outputLocation property instead. " +
-        String.format(RECOMMENDATION,"information",  new DocumentationRegistry().getDslRefForProperty("org.gradle.api.reporting.Report", "destination"))
+        "This is scheduled to be removed in Gradle 9.0. " +
+        "Please use the outputLocation property instead. " +
+        String.format(RECOMMENDATION, "information", new DocumentationRegistry().getDslRefForProperty("org.gradle.api.reporting.Report", "destination"))
 
     void expectReportDestinationPropertyDeprecation(String agpVersion) {
         runner.expectDeprecationWarningIf(
             VersionNumber.parse(agpVersion).baseVersion < VersionNumber.parse("7.4.1"),
             REPORT_DESTINATION_DEPRECATION,
             "https://github.com/gradle/gradle/issues/21533"
+        )
+    }
+
+    void maybeExpectReportDestinationPropertyDeprecation(String agpVersion) {
+        runner.maybeExpectLegacyDeprecationWarningIf(
+            VersionNumber.parse(agpVersion).baseVersion < VersionNumber.parse("7.4.1"),
+            REPORT_DESTINATION_DEPRECATION
         )
     }
 }
