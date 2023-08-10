ab5f977e3a46933c2c4c0354d55425366daf7068
Bálint Hegyi <bhegyi@gradle.com>
2023-08-10 15:50:12 +0100

    Remove unused import

--- subprojects/model-core/src/main/java/org/gradle/internal/reflect/validation/TypeAwareProblemBuilder.java
+++ subprojects/model-core/src/main/java/org/gradle/internal/reflect/validation/TypeAwareProblemBuilder.java
@@ -17,7 +17,6 @@
 package org.gradle.internal.reflect.validation;
 
 import org.gradle.api.NonNullApi;
-import org.gradle.api.problems.interfaces.ProblemBuilderDefiningDocumentation;
 import org.gradle.api.problems.interfaces.ProblemBuilderDefiningMessage;
 
 import javax.annotation.Nullable;
