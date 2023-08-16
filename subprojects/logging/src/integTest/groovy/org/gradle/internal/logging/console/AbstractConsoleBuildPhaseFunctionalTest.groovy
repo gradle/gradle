b94b078b670dd4f2eebeffd87765b2ef4fc34075
Stefan Wolf <wolf@gradle.com>
2023-08-16 14:53:21 +0200

    Add test for console log progress with non-planned planned steps

--- subprojects/logging/src/integTest/groovy/org/gradle/internal/logging/console/AbstractConsoleBuildPhaseFunctionalTest.groovy
+++ subprojects/logging/src/integTest/groovy/org/gradle/internal/logging/console/AbstractConsoleBuildPhaseFunctionalTest.groovy
@@ -119,7 +119,7 @@ abstract class AbstractConsoleBuildPhaseFunctionalTest extends AbstractConsoleGr
         buildFinished.releaseAll()
 
         and:
-        gradle.waitForFinish()
+        waitForFinish()
     }
 
     @ToBeFixedForConfigurationCache(because = "build listener", skip = ToBeFixedForConfigurationCache.Skip.FAILS_TO_CLEANUP)
@@ -203,7 +203,7 @@ abstract class AbstractConsoleBuildPhaseFunctionalTest extends AbstractConsoleGr
         rootBuildFinished.releaseAll()
 
         and:
-        gradle.waitForFinish()
+        waitForFinish()
     }
 
     @ToBeFixedForConfigurationCache(because = "Gradle.buildFinished", skip = ToBeFixedForConfigurationCache.Skip.LONG_TIMEOUT)
@@ -285,12 +285,94 @@ abstract class AbstractConsoleBuildPhaseFunctionalTest extends AbstractConsoleGr
         rootBuildFinished.releaseAll()
 
         and:
-        gradle.waitForFinish()
+        waitForFinish()
     }
 
     @ToBeFixedForConfigurationCache(because = "Gradle.buildFinished", skip = ToBeFixedForConfigurationCache.Skip.LONG_TIMEOUT)
     def "shows progress bar and percent phase completion with artifact transforms"() {
         given:
+        setupTransformBuild()
+        def jar = server.expectAndBlock('jar')
+        def doubleTransform = server.expectAndBlock('double-transform')
+        def sizeTransform = server.expectAndBlock('size-transform')
+        def resolveTask = server.expectAndBlock('resolve-task')
+        def buildFinished = server.expectAndBlock('build-finished')
+
+        when:
+        gradle = executer.withTasks(":util:resolve").start()
+
+        then:
+        jar.waitForAllPendingCalls()
+        assertHasBuildPhase("0% EXECUTING")
+        jar.releaseAll()
+
+        and:
+        doubleTransform.waitForAllPendingCalls()
+        assertHasBuildPhase("25% EXECUTING")
+        doubleTransform.releaseAll()
+
+        and:
+        sizeTransform.waitForAllPendingCalls()
+        assertHasBuildPhase("50% EXECUTING")
+        sizeTransform.releaseAll()
+
+        and:
+        resolveTask.waitForAllPendingCalls()
+        assertHasBuildPhase("75% EXECUTING")
+        resolveTask.releaseAll()
+
+        and:
+        buildFinished.waitForAllPendingCalls()
+        assertHasBuildPhase("100% EXECUTING")
+        buildFinished.releaseAll()
+
+        and:
+        waitForFinish()
+    }
+
+    @ToBeFixedForConfigurationCache(because = "Gradle.buildFinished", skip = ToBeFixedForConfigurationCache.Skip.LONG_TIMEOUT)
+    def "shows progress bar and percent phase completion with non-planned planned artifact transforms"() {
+        given:
+        setupTransformBuild()
+        def jar = server.expectAndBlock('jar')
+        def resolveTask = server.expectAndBlock('resolve-task')
+        def doubleTransform = server.expectAndBlock('double-transform')
+        def sizeTransform = server.expectAndBlock('size-transform')
+        def buildFinished = server.expectAndBlock('build-finished')
+
+        when:
+        gradle = executer.withTasks(":util:resolveWithoutDependencies").start()
+
+        then:
+        jar.waitForAllPendingCalls()
+        assertHasBuildPhase("0% EXECUTING")
+        jar.releaseAll()
+
+        and:
+        resolveTask.waitForAllPendingCalls()
+        assertHasBuildPhase("50% EXECUTING")
+        resolveTask.releaseAll()
+
+        and:
+        doubleTransform.waitForAllPendingCalls()
+        assertHasBuildPhase("50% EXECUTING")
+        doubleTransform.releaseAll()
+
+        and:
+        sizeTransform.waitForAllPendingCalls()
+        assertHasBuildPhase("50% EXECUTING")
+        sizeTransform.releaseAll()
+
+        and:
+        buildFinished.waitForAllPendingCalls()
+        assertHasBuildPhase("100% EXECUTING")
+        buildFinished.releaseAll()
+
+        and:
+        waitForFinish()
+    }
+
+    def setupTransformBuild() {
         settingsFile << """
             include 'lib'
             include 'util'
@@ -386,48 +468,24 @@ abstract class AbstractConsoleBuildPhaseFunctionalTest extends AbstractConsoleGr
                         size.artifactFiles.files.each { println it }
                     }
                 }
+                task resolveWithoutDependencies {
+                    def size = configurations.compile.incoming.artifactView {
+                        attributes { it.attribute(artifactType, 'size') }
+                    }.artifacts
+
+                    dependsOn(":lib:jar")
+
+                    doLast {
+                        ${server.callFromBuild('resolve-task')}
+                        size.artifactFiles.files.each { println it }
+                    }
+                }
             }
 
             gradle.buildFinished {
                 ${server.callFromBuild('build-finished')}
             }
         """
-        def jar = server.expectAndBlock('jar')
-        def doubleTransform = server.expectAndBlock('double-transform')
-        def sizeTransform = server.expectAndBlock('size-transform')
-        def resolveTask = server.expectAndBlock('resolve-task')
-        def buildFinished = server.expectAndBlock('build-finished')
-
-        when:
-        gradle = executer.withTasks(":util:resolve").start()
-
-        then:
-        jar.waitForAllPendingCalls()
-        assertHasBuildPhase("0% EXECUTING")
-        jar.releaseAll()
-
-        and:
-        doubleTransform.waitForAllPendingCalls()
-        assertHasBuildPhase("25% EXECUTING")
-        doubleTransform.releaseAll()
-
-        and:
-        sizeTransform.waitForAllPendingCalls()
-        assertHasBuildPhase("50% EXECUTING")
-        sizeTransform.releaseAll()
-
-        and:
-        resolveTask.waitForAllPendingCalls()
-        assertHasBuildPhase("75% EXECUTING")
-        resolveTask.releaseAll()
-
-        and:
-        buildFinished.waitForAllPendingCalls()
-        assertHasBuildPhase("100% EXECUTING")
-        buildFinished.releaseAll()
-
-        and:
-        gradle.waitForFinish()
     }
 
     void assertHasBuildPhase(String message) {
@@ -438,5 +496,10 @@ abstract class AbstractConsoleBuildPhaseFunctionalTest extends AbstractConsoleGr
 
     String regexFor(String message) {
         /<.*> $message \[[\dms ]+]/
+    }
+
+    void waitForFinish() {
+        result = gradle.waitForFinish()
+        outputDoesNotContain("More progress was logged than there should be")
     }
 }
