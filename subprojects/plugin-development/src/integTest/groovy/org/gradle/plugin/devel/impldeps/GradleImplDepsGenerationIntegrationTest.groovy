cd2b2f0e992afb1780fd112232c150169c70ed90
József Bartók <jbartok@gradle.com>
2023-08-23 11:14:01 +0300

    Fix failing test

--- subprojects/plugin-development/src/integTest/groovy/org/gradle/plugin/devel/impldeps/GradleImplDepsGenerationIntegrationTest.groovy
+++ subprojects/plugin-development/src/integTest/groovy/org/gradle/plugin/devel/impldeps/GradleImplDepsGenerationIntegrationTest.groovy
@@ -64,7 +64,7 @@ class GradleImplDepsGenerationIntegrationTest extends BaseGradleImplDepsIntegrat
                     assert resolvedArtifacts.find { (it.name =~ 'groovy-(.*)\\\\.jar').matches() }
                     assert resolvedArtifacts.findAll { (it.name =~ 'kotlin-stdlib-(.*)\\\\.jar').matches() }.size() == 2
                     assert resolvedArtifacts.find { (it.name =~ 'kotlin-reflect-(.*)\\\\.jar').matches() }
-                    assert resolvedArtifacts.size() == 21
+                    assert resolvedArtifacts.size() == 19
                 }
             }
         """
