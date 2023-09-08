62462934d83809e7123e3575a91d48296b610162
Lóránt Pintér <lorant@gradle.com>
2023-09-08 15:08:28 +0200

    Simplify validation of plugins used during the build itself
    Do not require listing all passing plugins explicitly.
    Also removed some unused code.

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/GradleBuildExternalPluginsValidationSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/GradleBuildExternalPluginsValidationSmokeTest.groovy
@@ -42,71 +42,10 @@ class GradleBuildExternalPluginsValidationSmokeTest extends AbstractGradleceptio
     }
 
     def "performs static validation of plugins used by the Gradle build"() {
-        when:
-        passingPlugins { id ->
-            id.startsWith('gradlebuild') ||
-            id.startsWith('Gradlebuild') ||
-            id in [
-                'com.diffplug.spotless',
-                'org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin',
-                'org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin',
-                'me.champeau.jmh',
-                'kotlin-sam-with-receiver',
-                'org.jetbrains.gradle.plugin.idea-ext',
-                'org.jetbrains.kotlin.gradle.scripting.internal.ScriptingGradleSubplugin',
-                'org.jetbrains.kotlin.gradle.scripting.internal.ScriptingKotlinGradleSubplugin',
-                'org.jetbrains.kotlin.jvm',
-                'org.jlleitschuh.gradle.ktlint',
-                'org.jlleitschuh.gradle.ktlint.KtlintBasePlugin',
-                'org.jlleitschuh.gradle.ktlint.KtlintIdeaPlugin',
-                'org.jetbrains.kotlin.gradle.targets.js.npm.NpmResolverPlugin',
-                'org.jetbrains.kotlin.js',
-                'org.asciidoctor.gradle.base.AsciidoctorBasePlugin',
-                'org.asciidoctor.gradle.jvm.AsciidoctorJBasePlugin',
-                'org.asciidoctor.gradle.jvm.AsciidoctorJPlugin',
-                'org.asciidoctor.jvm.convert',
-                'com.gradle.plugin-publish',
-                'kotlin',
-                'com.autonomousapps.dependency-analysis',
-                'dev.adamko.dokkatoo.DokkatooBasePlugin$Inject',
-                'dev.adamko.dokkatoo.adapters.DokkatooJavaAdapter$Inject',
-                'dev.adamko.dokkatoo.adapters.DokkatooKotlinAdapter$Inject',
-                'dev.adamko.dokkatoo.adapters.DokkatooAndroidAdapter$Inject',
-                'dev.adamko.dokkatoo.formats.DokkatooHtmlPlugin$Inject',
-            ]
-        }
-
-        then:
-        validatePlugins()
-    }
-
-    void passingPlugins(Closure<Boolean> spec) {
-        allPlugins.passing(spec)
-    }
-
-    void validatePlugins() {
+        expect:
         allPlugins.performValidation([
             "--no-parallel" // make sure we have consistent execution ordering as we skip cached tasks
         ])
-    }
-
-    void inProject(String projectPath, @DelegatesTo(value = ProjectValidation, strategy = Closure.DELEGATE_FIRST) Closure<?> spec) {
-        def validation = new ProjectValidation(projectPath)
-        spec.delegate = validation
-        spec.resolveStrategy = Closure.DELEGATE_FIRST
-        spec()
-    }
-
-    class ProjectValidation {
-        private final String projectPath
-
-        ProjectValidation(String projectPath) {
-            this.projectPath = projectPath
-        }
-
-        void onPlugin(String id, @DelegatesTo(value = PluginValidation, strategy = Closure.DELEGATE_FIRST) Closure<?> spec) {
-            allPlugins.onPlugin(id, projectPath, spec)
-        }
     }
 
     private static class GradleBuildDirLocator implements ProjectBuildDirLocator {
