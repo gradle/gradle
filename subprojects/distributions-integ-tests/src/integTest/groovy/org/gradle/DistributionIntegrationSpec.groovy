86e004c7d54e988287de75b5311d6a358851943f
Paul Merlin <paul@gradle.com>
2023-09-12 14:11:52 +0200

    Adjust Kotlin DSL code and tests for 1.9.20-Beta

--- subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -34,7 +34,7 @@ import static org.hamcrest.MatcherAssert.assertThat
 
 abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
 
-    protected static final THIRD_PARTY_LIB_COUNT = 149
+    protected static final THIRD_PARTY_LIB_COUNT = 148
 
     @Shared String baseVersion = GradleVersion.current().baseVersion.version
 
