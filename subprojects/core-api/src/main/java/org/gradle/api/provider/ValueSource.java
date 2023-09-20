5fbdd9ff8af9afcb99d14ec72f8baf20be7381a7
Mikhail Lopatkin <mlopatkin@gradle.com>
2023-09-20 16:54:10 +0200

    Improve ValueSource javadoc a bit more
    Added a link to the service injection manual and the requirements for
    the implementation.
    
    I've also tried to make the snippet "autotested", but it doesn't work:
    `ValueSourceParameters.None` cannot be resolved without an explicit
    `import org.gradle.api.provider.*` that clutters the snippet.

--- subprojects/core-api/src/main/java/org/gradle/api/provider/ValueSource.java
+++ subprojects/core-api/src/main/java/org/gradle/api/provider/ValueSource.java
@@ -45,15 +45,20 @@ import javax.inject.Inject;
  * considered as an input to the work graph cache.
  * </p>
  * <p>
- * It is possible to have some Gradle services to be injected into the implementation, similar to
- * tasks and plugins. It can be done by adding a parameter to the constructor and annotating the
+ * It is possible to have some Gradle services to be <a href="https://docs.gradle.org/current/userguide/custom_gradle_types.html#service_injection">injected</a>
+ * into the implementation, similar to tasks and plugins.
+ * It can be done by adding a parameter to the constructor and annotating the
  * constructor with the {@code @Inject} annotation:
  * <pre>
- * public abstract class MyValueSource implements ValueSource&lt;...&gt; {
- *     &#064;Inject
+ * public abstract class MyValueSource implements ValueSource&lt;String, ValueSourceParameters.None&gt; {
+ *     private final ExecOperations execOperations;
+ *
+ *     {@literal @}Inject
  *     public MyValueSource(ExecOperations execOperations) {
- *         ...
+ *         this.execOperations = execOperations;
  *     }
+ *
+ *     // ...
  * }
  * </pre>
  * Currently, only a small subset of services is supported:
@@ -83,6 +88,12 @@ import javax.inject.Inject;
  * Starting an external process with a standard API (for example, {@code java.lang.ProcessBuilder}) is
  * also allowed.
  * </p>
+ *
+ * Implementations of ValueSource are subject to the following constraint:
+ * <ul>
+ *     <li>Do not implement {@link #getParameters()} in your class, the method will be implemented by Gradle.</li>
+ * </ul>
+ *
  * @param <T> The type of value obtained from this source.
  * @param <P> The source specific parameter type.
  * @see ProviderFactory#environmentVariable(String)
