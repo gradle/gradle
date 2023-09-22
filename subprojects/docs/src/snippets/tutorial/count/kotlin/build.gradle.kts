4b3b0ad717328c891d518c11a2abfd5085a1847a
Laura Kassovic <lkassovic@gradle.com>
2023-09-21 17:25:47 -0700

    first set of changes for @tresat

--- subprojects/docs/src/snippets/tutorial/count/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/tutorial/count/kotlin/build.gradle.kts
@@ -1,5 +1,12 @@
+internal object Example {
+    fun sum(a: Int, b: Int) {
+        val c = a + b
+        println(c)
+    }
+}
+
 tasks.register("count") {
     doLast {
-        repeat(4) { print("$it ") }
+        repeat(4) { print(Example.sum(it,it)) }
     }
 }
