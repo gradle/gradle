cdd9424c006baca70765b388ac9371fa3a45f73c
Sergey Opivalov <sopivalov@gradle.com>
2023-09-28 16:56:49 +0300

    Add test coverage, extend a condition for eligibility

--- subprojects/core/src/main/java/org/gradle/internal/buildtree/DefaultBuildTreeLifecycleController.java
+++ subprojects/core/src/main/java/org/gradle/internal/buildtree/DefaultBuildTreeLifecycleController.java
@@ -20,6 +20,7 @@ import org.gradle.api.internal.GradleInternal;
 import org.gradle.api.internal.SettingsInternal;
 import org.gradle.execution.EntryTaskSelector;
 import org.gradle.internal.Describables;
+import org.gradle.internal.RunDefaultTasksExecutionRequest;
 import org.gradle.internal.build.BuildLifecycleController;
 import org.gradle.internal.build.ExecutionResult;
 import org.gradle.internal.model.StateTransitionController;
@@ -48,7 +49,8 @@ public class DefaultBuildTreeLifecycleController implements BuildTreeLifecycleCo
         BuildTreeModelCreator modelCreator,
         BuildTreeFinishExecutor finishExecutor,
         StateTransitionControllerFactory controllerFactory,
-        StartParameter startParameter, BuildModelParameters buildModelParameters
+        StartParameter startParameter,
+        BuildModelParameters buildModelParameters
     ) {
         this.buildLifecycleController = buildLifecycleController;
         this.workController = workController;
@@ -97,12 +99,17 @@ public class DefaultBuildTreeLifecycleController implements BuildTreeLifecycleCo
         });
     }
 
+    // Temporary workaround to make incremental sync work. IDEA is requires to execute `help` task
+    // to prevent default tasks be executed during sync. This is forcing all projects configuration.
+    // Must be removed as soon as IDEA will use appropriate API for avoiding any tasks to be executed.
     private Boolean isEligibleToRunTasks() {
         boolean isIsolatedProjectsEnabled = buildModelParameters.isIsolatedProjects();
+        boolean isDefaultTasksRequested = startParameter.getTaskRequests().size() == 1 &&
+            startParameter.getTaskRequests().get(0) instanceof RunDefaultTasksExecutionRequest;
         boolean isHelpTaskOnly = startParameter.getTaskRequests().size() == 1 &&
             startParameter.getTaskRequests().get(0).getArgs().contains("help");
 
-        return !isIsolatedProjectsEnabled || !isHelpTaskOnly;
+        return !isIsolatedProjectsEnabled || !(isDefaultTasksRequested || isHelpTaskOnly);
     }
 
     private <T> T runBuild(Supplier<ExecutionResult<? extends T>> action) {
