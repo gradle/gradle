ab06e028810e5f1fed7cab684f0f8a93f8ea93b0
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2023-09-29 10:46:57 -0300

    Test parallel model building against complex composite build

--- platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/configurationcache/isolated/IsolatedProjectsToolingApiParallelConfigurationIntegrationTest.groovy
+++ platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/configurationcache/isolated/IsolatedProjectsToolingApiParallelConfigurationIntegrationTest.groovy
@@ -16,6 +16,8 @@
 
 package org.gradle.configurationcache.isolated
 
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 import org.gradle.test.fixtures.file.TestFile
 import org.gradle.test.fixtures.server.http.BlockingHttpServer
 import org.junit.Rule
@@ -127,7 +129,7 @@ class IsolatedProjectsToolingApiParallelConfigurationIntegrationTest extends Abs
         fixture.assertStateLoaded()
     }
 
-    def "projects are configured in parallel when projects use plugins from included builds and project scoped model is queried concurrently"() {
+    def "projects are configured in parallel when projects use plugins from included build and project scoped model is queried concurrently"() {
         withSomeToolingModelBuilderPluginInChildBuild("plugins", """
             ${server.callFromBuildUsingExpression("'model-' + project.name")}
         """)
@@ -168,14 +170,138 @@ class IsolatedProjectsToolingApiParallelConfigurationIntegrationTest extends Abs
         }
     }
 
+    /**
+     * Test two projects (:a and :b) depending on plugins from the same two included builds (:plugin-1 and :plugin-2)
+     * which in turn depend on a plugin from another included build (:plugin-0).
+     **/
+    def "projects are configured in parallel when projects use plugins from included builds and project scoped model is queried concurrently"() {
+        given:
+        addConventionPluginImplementation('plugin-0', 'Plugin0')
+        addPluginBuildScript('plugin-0', 'my.plugin-0', 'my.Plugin0')
+        file('plugin-0/settings.gradle') << '''
+        '''
+
+        and:
+        addConventionPluginImplementation('plugin-1', 'Plugin1')
+        file('plugin-1/settings.gradle') << '''
+            includeBuild("../plugin-0")
+        '''
+        file("plugin-1/build.gradle") << '''
+            plugins {
+                id("groovy-gradle-plugin")
+                id("my.plugin-0")
+            }
+            gradlePlugin {
+                plugins {
+                    test {
+                        id = "my.plugin-1"
+                        implementationClass = "my.Plugin1"
+                    }
+                }
+            }
+        '''
+
+        and:
+        addConventionPluginImplementation('plugin-2', 'Plugin2')
+        file('plugin-2/settings.gradle') << '''
+            includeBuild("../plugin-0")
+        '''
+        file("plugin-2/build.gradle") << '''
+            plugins {
+                id("groovy-gradle-plugin")
+                id("my.plugin-0")
+            }
+            gradlePlugin {
+                plugins {
+                    test {
+                        id = "my.plugin-2"
+                        implementationClass = "my.Plugin2"
+                    }
+                }
+            }
+        '''
+
+        and:
+        withSomeToolingModelBuilderPluginInChildBuild("plugins", """
+            ${server.callFromBuildUsingExpression("'model-' + project.name")}
+        """)
+        settingsFile '''
+            includeBuild("plugin-1")
+            includeBuild("plugin-2")
+            includeBuild("plugins")
+            include("a")
+            include("b")
+            rootProject.name = "root"
+        '''
+
+        def buildScriptA = file('a/build.gradle')
+        groovyFile buildScriptA, '''
+            plugins {
+                id("my.plugin-1")
+                id("my.plugin-2")
+                id("my.plugin")
+            }
+        '''
+        configuring(buildScriptA)
+
+        def buildScriptB = file('b/build.gradle')
+        groovyFile buildScriptB, '''
+            plugins {
+                id("my.plugin")
+                id("my.plugin-2")
+                id("my.plugin-1")
+            }
+        '''
+        configuring(buildScriptB)
+
+        and:
+        server.expectConcurrent("configure-a", "configure-b")
+        server.expectConcurrent("model-a", "model-b")
+
+        when:
+        executer.withArguments(ENABLE_CLI)
+        def model = runBuildAction(new FetchCustomModelForEachProjectInParallel())
+
+        then:
+        model.size() == 3
+        model[0] == null
+        model[1].message == "It works from project :a"
+        model[2].message == "It works from project :b"
+
+        and:
+        fixture.assertStateStored {
+            projectsConfigured(':plugin-0', ':plugin-1', ':plugin-2', ':plugins', ':', ':a', ':b')
+            buildModelCreated()
+            modelsCreated(':a', ':b')
+        }
+    }
+
+    private void addConventionPluginImplementation(String targetBuildName, String className) {
+        file("$targetBuildName/src/main/groovy/my/${className}.groovy") << """
+            package my
+
+            abstract class $className implements $Plugin.name<$Project.name> {
+                void apply($Project.name project) {
+                    println "$targetBuildName:${'$'}project.name"
+                }
+            }
+        """.stripIndent()
+    }
+
     TestFile apply(TestFile dir) {
         def buildFile = dir.file("build.gradle")
         buildFile << """
             plugins {
                 id("my.plugin")
             }
+        """
+        configuring(buildFile)
+        return buildFile
+    }
+
+    TestFile configuring(TestFile buildFile) {
+        buildFile << """
             ${server.callFromBuildUsingExpression("'configure-' + project.name")}
         """
-        return buildFile
     }
 }
