43a4f9cc8be14b82f740701b73b7270305a687c3
Justin Van Dort <jvandort@gradle.com>
2023-09-28 22:20:35 -0400

    Restore ARTIFACTS_RESOLVED state
    This is referenced by the nebula gradle-resolution-rules-plugin. It will need to be updated
    before we can remove the enum entry

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/ConfigurationInternal.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/ConfigurationInternal.java
@@ -36,7 +36,11 @@ public interface ConfigurationInternal extends ResolveContext, DeprecatableConfi
     enum InternalState {
         UNRESOLVED,
         BUILD_DEPENDENCIES_RESOLVED,
-        GRAPH_RESOLVED
+        GRAPH_RESOLVED,
+
+        // This state should be removed, but it is referenced by nebula gradle-resolution-rules-plugin.
+        // https://github.com/nebula-plugins/gradle-resolution-rules-plugin/blob/623bbbcd4f187101bc233e46c4d9ec960c02e1a7/src/main/kotlin/nebula/plugin/resolutionrules/configurations.kt#L62
+        ARTIFACTS_RESOLVED
     }
 
     @Override
