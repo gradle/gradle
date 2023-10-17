a1162dc08a4b3a9cd3196e40013c58b50de0c4f6
Justin Van Dort <jvandort@gradle.com>
2023-10-17 17:22:48 -0400

    Depend on test-suites-base since classes were moved

--- platforms/jvm/language-java/build.gradle.kts
+++ platforms/jvm/language-java/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     implementation(project(":build-events"))
     implementation(project(":tooling-api"))
     implementation(project(":toolchains-jvm"))
-    implementation(project(":testing-base"))
+    implementation(project(":test-suites-base"))
 
     implementation(libs.groovy)
     implementation(libs.slf4jApi)
