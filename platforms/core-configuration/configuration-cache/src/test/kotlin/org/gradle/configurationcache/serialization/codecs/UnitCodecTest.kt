c20daec46827d4ff9e3a8ece5d80a2840633183c
Marcin Laskowski <marcin.laskowski@allegro.com>
2023-10-18 11:07:14 +0200

    Add codec for Unit type
    Signed-off-by: Marcin Laskowski <marcin.laskowski@allegro.com>

---
+++ platforms/core-configuration/configuration-cache/src/test/kotlin/org/gradle/configurationcache/serialization/codecs/UnitCodecTest.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.configurationcache.serialization.codecs
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import spock.lang.Issue
+
+
+class UnitCodecTest : AbstractUserTypeCodecTest() {
+
+    @Test
+    @Issue("https://github.com/gradle/gradle/issues/25560")
+    fun `can handle Unit instance`() {
+        configurationCacheRoundtripOf(Unit).run {
+            assertThat(this, `is`(Unit))
+        }
+    }
+}
