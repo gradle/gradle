af516401abaa4f8bfdf6ffc6e74453593e64391d
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-10-31 13:40:10 +0100

    since 8.5 -> since 8.6
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- platforms/jvm/language-java/src/main/java/org/gradle/api/tasks/compile/JavaCompile.java
+++ platforms/jvm/language-java/src/main/java/org/gradle/api/tasks/compile/JavaCompile.java
@@ -351,7 +351,7 @@ public abstract class JavaCompile extends AbstractCompile implements HasCompileO
      * This property can be used to configure alternate Java compilers, like the Eclipse Java Compiler. If set, the jars on this classpath with be scanned for the
      * first alternate implementation of {@link javax.tools.JavaCompiler} (not `com.sun.tools.javac.api.JavacTool`), and use the task will use that compiler instead.
      *
-     * @since 8.5
+     * @since 8.6
      */
     @Incubating
     @CompileClasspath
