e9f73074aabcf81a6fa7fe95fa2a95f3ef3d142b
Paul Merlin <paul@gradle.com>
2023-10-31 09:49:41 +0100

    Let build-logic work with both Kotlin 1.9.10 and 1.9.20
    This will need to be reworked after the next wrapper update

--- build-logic/packaging/src/main/kotlin/gradlebuild.distributions.gradle.kts
+++ build-logic/packaging/src/main/kotlin/gradlebuild.distributions.gradle.kts
@@ -34,8 +34,10 @@ import gradlebuild.packaging.GradleDistributionSpecs.binDistributionSpec
 import gradlebuild.packaging.GradleDistributionSpecs.docsDistributionSpec
 import gradlebuild.packaging.GradleDistributionSpecs.srcDistributionSpec
 import gradlebuild.packaging.tasks.PluginsManifest
+import org.jetbrains.kotlin.gradle.plugin.KotlinBaseApiPlugin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.jar.Attributes
+import kotlin.reflect.full.functions
 
 /**
  * Apply this plugin to let a project build 'Gradle distributions'.
@@ -179,10 +181,27 @@ val gradleApiKotlinExtensions by tasks.registering(GenerateKotlinExtensionsForGr
 }
 
 
-apply<org.jetbrains.kotlin.gradle.plugin.KotlinBaseApiPlugin>()
-plugins.withType(org.jetbrains.kotlin.gradle.plugin.KotlinBaseApiPlugin::class) {
-    registerKotlinJvmCompileTask("compileGradleApiKotlinExtensions")
+apply<KotlinBaseApiPlugin>()
+plugins.withType(KotlinBaseApiPlugin::class) {
+    registerKotlinJvmCompileTask()
 }
+
+fun KotlinBaseApiPlugin.registerKotlinJvmCompileTask() {
+    val taskName = "compileGradleApiKotlinExtensions"
+    val moduleName = "gradle-kotlin-dsl-extensions"
+
+    val kClass = KotlinBaseApiPlugin::class
+    val methodToCall = "registerKotlinJvmCompileTask"
+
+    val method = kClass.functions.firstOrNull() {
+        it.name == methodToCall && it.parameters.size == 3
+    }
+    if (method != null) {
+        kClass.java.getMethod(methodToCall, String::class.java, String::class.java).invoke(this, taskName, moduleName)
+    } else {
+        kClass.java.getMethod(methodToCall, String::class.java).invoke(this, taskName)
+    }
+} // TODO: simplify once wrapper knows about KotlinBaseApiPlugin::registerKotlinJvmCompileTask(String, String)
 
 val compileGradleApiKotlinExtensions = tasks.named("compileGradleApiKotlinExtensions", KotlinCompile::class) {
     configureKotlinCompilerForGradleBuild()
