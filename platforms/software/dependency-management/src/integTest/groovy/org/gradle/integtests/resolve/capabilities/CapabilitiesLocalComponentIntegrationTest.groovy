8c7da124be4fad52ce86767da47024944da4aeba
Justin Van Dort <jvandort@gradle.com>
2023-11-01 14:03:03 -0400

    Ensure artifact varaints have consistent capabilities with graph variants
    The artifact variants found in ArtifactCollection have different capabilities than their corresponding graph variant in the ResolutionResult
    This is because we failed to use the capabilities in the configuration hierarchy when building artifact metadata
    
    In the long run, we want do deprecate inheriting capabilities from the configuration hierarchy, but for now we fix this discrepancy

--- platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/capabilities/CapabilitiesLocalComponentIntegrationTest.groovy
+++ platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/capabilities/CapabilitiesLocalComponentIntegrationTest.groovy
@@ -17,6 +17,7 @@
 package org.gradle.integtests.resolve.capabilities
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import spock.lang.Issue
 
 class CapabilitiesLocalComponentIntegrationTest extends AbstractIntegrationSpec {
 
@@ -75,6 +76,65 @@ class CapabilitiesLocalComponentIntegrationTest extends AbstractIntegrationSpec 
 
         then:
         outputContains("Could not resolve project :.")
+    }
+
+    @Issue("https://github.com/gradle/gradle/issues/26377")
+    def "ResolvedVariantResults reported by ResolutionResult and ArtifactCollection have same capabilities when they are added to configuration in hierarchy"() {
+        settingsFile << "include 'producer'"
+        file("producer/build.gradle") << """
+            group="com.foo"
+
+            task zip(type: Zip) {
+                archiveFileName = "producer.zip"
+                destinationDirectory = layout.buildDirectory
+            }
+
+            configurations {
+                dependencyScope("api") {
+                    outgoing {
+                        capability("com.foo:producer:2.0")
+                        capability("org.bar:extra-capability:1.0")
+                    }
+                }
+                consumable("elements") {
+                    extendsFrom(api)
+                    outgoing.artifact(tasks.zip)
+                    attributes {
+                        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.class, Category.LIBRARY))
+                    }
+                }
+            }
+        """
+
+        buildFile << """
+            configurations {
+                dependencyScope("implementation")
+                resolvable("classpath") {
+                    extendsFrom(implementation)
+                    attributes {
+                        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.class, Category.LIBRARY))
+                    }
+                }
+            }
+
+            dependencies {
+                implementation(project(":producer"))
+            }
+
+            task resolve {
+                def conf = configurations.classpath
+                def root = conf.incoming.resolutionResult.root
+                def artifactVariants = conf.incoming.artifacts.artifacts.collect { it.variant }
+                doLast {
+                    def graphVariant = root.dependencies.find { it.selected.id.projectPath == ":producer" }.resolvedVariant
+                    def artifactVariant = artifactVariants.find { it.owner.projectPath == ":producer" }
+                    assert graphVariant.capabilities == artifactVariant.capabilities
+                }
+            }
+        """
+
+        expect:
+        succeeds("resolve")
     }
 }
 
