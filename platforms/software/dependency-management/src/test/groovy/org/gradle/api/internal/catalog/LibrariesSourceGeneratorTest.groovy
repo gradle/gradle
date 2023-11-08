8f9d1854d137568bbe31b44cc6a384dfa6af4c56
Bálint Hegyi <bhegyi@gradle.com>
2023-11-08 15:15:30 +0000

    Exchange stub with noop emitter

--- platforms/software/dependency-management/src/test/groovy/org/gradle/api/internal/catalog/LibrariesSourceGeneratorTest.groovy
+++ platforms/software/dependency-management/src/test/groovy/org/gradle/api/internal/catalog/LibrariesSourceGeneratorTest.groovy
@@ -36,13 +36,13 @@ import org.gradle.api.internal.provider.DefaultValueSourceProviderFactory
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.problems.Problems
 import org.gradle.api.problems.internal.DefaultProblems
+import org.gradle.api.problems.internal.emitters.NoOpProblemEmitter
 import org.gradle.api.provider.ProviderFactory
 import org.gradle.internal.classpath.ClassPath
 import org.gradle.internal.event.DefaultListenerManager
 import org.gradle.internal.installation.CurrentGradleInstallation
 import org.gradle.internal.isolation.TestIsolatableFactory
 import org.gradle.internal.management.VersionCatalogBuilderInternal
-import org.gradle.internal.operations.BuildOperationProgressEventEmitter
 import org.gradle.internal.service.scopes.Scopes
 import org.gradle.process.ExecOperations
 import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider
@@ -385,8 +385,7 @@ ${nameClash { noIntro().kind('dependency bundles').inConflict('one.cool', 'oneCo
     }
 
     private void generate(String className = 'Generated', @DelegatesTo(strategy = Closure.DELEGATE_FIRST, value = VersionCatalogBuilderInternal) Closure<Void> spec) {
-        def stub = Stub(BuildOperationProgressEventEmitter)
-        def problems = new DefaultProblems(stub)
+        def problems = new DefaultProblems(new NoOpProblemEmitter())
         DefaultVersionCatalogBuilder builder = new DefaultVersionCatalogBuilder(
             "lib",
             Interners.newStrongInterner(),
