7ed1a091af5c3a1f833157633656f866fe01bc24
Bálint Hegyi <bhegyi@gradle.com>
2023-11-08 14:56:59 +0000

    Delete unnecessary imports

--- platforms/ide/problems-api/build.gradle.kts
+++ platforms/ide/problems-api/build.gradle.kts
@@ -32,7 +32,6 @@ gradlebuildJava.usedInWorkers()
 dependencies {
     implementation(project(":base-annotations"))
     implementation(project(":base-services"))
-    implementation(project(":messaging"))
     implementation(project(":enterprise-operations"))
 
     implementation(libs.guava)
--- platforms/software/dependency-management/src/test/groovy/org/gradle/api/internal/catalog/AbstractVersionCatalogTest.groovy
+++ platforms/software/dependency-management/src/test/groovy/org/gradle/api/internal/catalog/AbstractVersionCatalogTest.groovy
@@ -20,7 +20,6 @@ import com.google.common.collect.Interners
 import org.gradle.api.problems.Problems
 import org.gradle.api.problems.internal.DefaultProblems
 import org.gradle.api.problems.internal.emitters.NoOpProblemEmitter
-import org.gradle.internal.operations.BuildOperationProgressEventEmitter
 import org.gradle.util.TestUtil
 import spock.lang.Specification
 import spock.lang.Subject
