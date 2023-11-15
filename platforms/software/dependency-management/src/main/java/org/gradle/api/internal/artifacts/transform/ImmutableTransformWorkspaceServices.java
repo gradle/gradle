795e452cc0a80b437398699b8a495a1e91c847af
Lóránt Pintér <lorant@gradle.com>
2023-11-15 23:50:44 +0100

    Resolve workspace output locations with less overhead
    Resolve produced-only output locations only once after the execution is finished, instead of every time an execution is requested.

--- platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/ImmutableTransformWorkspaceServices.java
+++ platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/ImmutableTransformWorkspaceServices.java
@@ -26,5 +26,5 @@ import java.io.Closeable;
 public interface ImmutableTransformWorkspaceServices extends Closeable {
     ImmutableWorkspaceProvider getWorkspaceProvider();
 
-    Cache<UnitOfWork.Identity, Try<TransformWorkspaceResult>> getIdentityCache();
+    Cache<UnitOfWork.Identity, Try<TransformExecutionResult.TransformWorkspaceResult>> getIdentityCache();
 }
