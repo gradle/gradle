0685077ff6d8e929395b8bb1156d8713e82767ce
Lóránt Pintér <lorant@gradle.com>
2023-11-16 16:27:49 +0100

    Do not capture non-normalized file hash as transform input

--- platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformExecutionBuildOperationIntegrationTest.groovy
+++ platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/transform/ArtifactTransformExecutionBuildOperationIntegrationTest.groovy
@@ -169,7 +169,7 @@ class ArtifactTransformExecutionBuildOperationIntegrationTest extends AbstractIn
             hash != null
             classLoaderHash != null
             implementationClassName == 'MakeGreen'
-            inputValueHashes.keySet() ==~ ['inputArtifactPath', 'inputArtifactSnapshot', 'inputPropertiesHash']
+            inputValueHashes.keySet() ==~ ['inputArtifactPath', 'inputPropertiesHash']
             outputPropertyNames == ['outputDirectory', 'resultsFile']
             inputFileProperties.keySet() ==~ ['inputArtifact', 'inputArtifactDependencies']
             with(inputFileProperties.inputArtifactDependencies) {
