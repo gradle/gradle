8175cd2c4d07fff5897468c3329d4b1b381799f0
Bálint Hegyi <bhegyi@gradle.com>
2023-11-17 13:40:36 +0000

    Update SSHD from 2.10.0 to 2.11.0

--- subprojects/distributions-dependencies/build.gradle.kts
+++ subprojects/distributions-dependencies/build.gradle.kts
@@ -36,7 +36,7 @@ val tomljVersion = "1.0.0"
 val archunitVersion = "1.2.0"
 val bytebuddyVersion = "1.10.20"
 val jettyVersion = "9.4.36.v20210114"
-val sshdVersion = "2.10.0"
+val sshdVersion = "2.11.0"
 
 // For the junit-bom
 javaPlatform.allowDependencies()
