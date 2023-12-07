d663debbfd2e5197c47174178a8f3571408d5c96
Bo Zhang <bo@gradle.com>
2023-12-07 10:22:28 +0800

    Resolve review feedback

--- .github/workflows/CheckBadMerge.groovy
+++ .github/workflows/CheckBadMerge.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
@@ -17,7 +33,10 @@ class CheckBadMerge {
     private static final THREAD_POOL = Executors.newCachedThreadPool()
 
     private static final List<String> MONITORED_FILES = [
-        "subprojects/docs/src/docs/release/notes.md"
+        "subprojects/docs/src/docs/release/notes.md",
+        "platforms/documentation/docs/src/docs/release/notes.md",
+        "subprojects/launcher/src/main/resources/release-features.txt",
+        "platforms/core-runtime/launcher/src/main/resources/release-features.txt"
     ]
 
     static void main(String[] commits) {
