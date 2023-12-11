190f9c8750460e02d0cb20f8ffb0ab3013cc162e
Bálint Hegyi <bhegyi@gradle.com>
2023-12-11 14:22:05 +0000

    Add reasoning why we bundled the protocols together

--- subprojects/core/src/main/java/org/gradle/process/internal/worker/request/ResponseProtocol.java
+++ subprojects/core/src/main/java/org/gradle/process/internal/worker/request/ResponseProtocol.java
@@ -24,6 +24,9 @@ import org.gradle.process.internal.worker.problem.WorkerProblemProtocol;
  * <p>
  * This protocol is extended with the {@link WorkerLoggingProtocol} (for sending log messages)
  * and {@link WorkerProblemProtocol} (for sending {@link org.gradle.api.problems.Problem}s through).
+ * <p>
+ * Bundling these protocols together will use a single connection for all communication,
+ * which means that messages are guaranteed to be delivered <b>in-order</b> (i.e. log and problem messages are delivered before the completion/failed message).
  */
 public interface ResponseProtocol extends WorkerLoggingProtocol, WorkerProblemProtocol {
     /**
