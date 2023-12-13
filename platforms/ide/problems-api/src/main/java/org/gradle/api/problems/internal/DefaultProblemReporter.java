97d9e4bcdacc375acacb7e36e373a949b89e8b35
Bálint Hegyi <bhegyi@gradle.com>
2023-12-13 17:38:39 +0000

    Modify reporter, so it skips emitting if the BOID is null

--- platforms/ide/problems-api/src/main/java/org/gradle/api/problems/internal/DefaultProblemReporter.java
+++ platforms/ide/problems-api/src/main/java/org/gradle/api/problems/internal/DefaultProblemReporter.java
@@ -21,6 +21,7 @@ import org.gradle.api.problems.ProblemSpec;
 import org.gradle.internal.operations.CurrentBuildOperationRef;
 import org.gradle.internal.operations.OperationIdentifier;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class DefaultProblemReporter implements InternalProblemReporter {
@@ -94,23 +95,29 @@ public class DefaultProblemReporter implements InternalProblemReporter {
      * Reports a problem.
      * <p>
      * The current build operation is used as the operation identifier.
+     * <p>
+     * If there is no current build operation, the problem is not reported.
      *
      * @param problem The problem to report.
      */
     @Override
     public void report(Problem problem) {
         OperationIdentifier id = CurrentBuildOperationRef.instance().getId();
-        emitter.emit(transformProblem(problem), id);
+        report(problem, id);
     }
 
     /**
      * Reports a problem with an explicit operation identifier.
+     * <p>
+     * If the operation identifier is null, the problem is not reported.
      *
      * @param problem The problem to report.
      * @param id The operation identifier to associate with the problem.
      */
     @Override
-    public void report(Problem problem, OperationIdentifier id) {
-        emitter.emit(transformProblem(problem), id);
+    public void report(Problem problem, @Nullable OperationIdentifier id) {
+        if (id != null) {
+            emitter.emit(transformProblem(problem), id);
+        }
     }
 }
