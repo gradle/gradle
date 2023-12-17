eec37985c87739d05acb0da6149fad0da9fcaad2
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-12-16 22:51:11 -0800

    Add comment to DefaultCppLibrary
    Signed-off-by: Jocelyn Castellano <admin@malwarefight.wip.la>

--- platforms/native/language-native/src/main/java/org/gradle/language/cpp/internal/DefaultCppLibrary.java
+++ platforms/native/language-native/src/main/java/org/gradle/language/cpp/internal/DefaultCppLibrary.java
@@ -147,6 +147,7 @@ public class DefaultCppLibrary extends DefaultCppComponent implements CppLibrary
     @Override
     public FileTree getPublicHeaderFiles() {
         PatternSet patterns = new PatternSet();
+        // if you would like to add more endings to this pattern, make sure to also edit DefaultCppComponent.java and default.vcxproj.filters
         patterns.include("**/*.h");
         patterns.include("**/*.hpp");
         patterns.include("**/*.h++");
