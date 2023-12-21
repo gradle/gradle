8a463d661df1fae9cf20b8d8842b45a8ffb6e7bd
Tom Tresansky <ttresansky@gradle.com>
2023-12-21 13:24:23 -0500

    Update verification exception javadoc

--- subprojects/core/src/main/java/org/gradle/api/tasks/VerificationException.java
+++ subprojects/core/src/main/java/org/gradle/api/tasks/VerificationException.java
@@ -22,11 +22,13 @@ import org.gradle.api.Incubating;
 /**
  * Signals that a task has failed in a manner which does not prevent consumers of that task's output from running.
  *
- * Tasks can depend upon other task's outcomes (PASS vs. FAIL) and outputs (the files they produce).  A verification
+ * A task can depend upon another task's outcome (PASS vs. FAIL), outputs (the files they produce) or both. A verification
  * failure represents the case where a task has a failed outcome, but has still produced valid output files for consumption.
+ * Tasks that only depend on the other task's outputs are allowed to execute. Tasks that depend on both the outcome and
+ * output cannot execute.
  *
- * These failures are often caused by user code under analysis (such as from running tests, code quality tools, or linters).
- * A failed test, for instance, will cause a failing outcome for the test task, but this shouldn't prevent another task
+ * These failures should be caused by user code under analysis (such as from running tests, code quality checks, or linting errors).
+ * A failed test, for instance, will cause a failing outcome for the test task, but this does not prevent another task
  * from reading and processing the (valid) test results output it produced (perhaps to aggregate multiple test reports).
  *
  * Verification failures do not represent a bug in either the build tool or custom task logic; the responsibility falls to the
