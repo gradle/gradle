183d459b23ca759be4b3f323c59b44ec6c6b4ecd
Sergey Opivalov <sopivalov@gradle.com>
2023-12-26 20:10:18 +0300

    Add `SmokeIdeTestPlugin`

--- build-logic/integration-testing/build.gradle.kts
+++ build-logic/integration-testing/build.gradle.kts
@@ -4,6 +4,21 @@ plugins {
 
 description = "Provides plugins to create and configure integration, cross-version and distribution tests"
 
+gradlePlugin {
+    plugins {
+        register("androidStudioProvisioning") {
+            id = "gradlebuild.android-studio-provisioning"
+            implementationClass = "gradlebuild.integrationtests.ide.AndroidStudioProvisioningPlugin"
+        }
+    }
+    plugins {
+        register("smokeIdeTesting") {
+            id = "gradlebuild.smoke-ide-testing"
+            implementationClass = "gradlebuild.integrationtests.ide.SmokeIdeTestPlugin"
+        }
+    }
+}
+
 dependencies {
     implementation("gradlebuild:basics")
     implementation("gradlebuild:module-identity")
