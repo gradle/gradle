89f0ac1c4f303a4d3136c85f0899dd59869acadd
hfhbd <hfhbd@users.noreply.github.com>
2024-01-02 15:30:49 +0100

    Remove accessing the rootProject in DefaultMavenPublication from a subproject, the directory will always be used with absolute paths.
    Signed-off-by: hfhbd <hfhbd@users.noreply.github.com>

--- platforms/software/maven/src/main/java/org/gradle/api/publish/maven/internal/publication/DefaultMavenPublication.java
+++ platforms/software/maven/src/main/java/org/gradle/api/publish/maven/internal/publication/DefaultMavenPublication.java
@@ -25,6 +25,7 @@ import org.gradle.api.Task;
 import org.gradle.api.artifacts.ModuleVersionIdentifier;
 import org.gradle.api.artifacts.PublishArtifact;
 import org.gradle.api.component.SoftwareComponent;
+import org.gradle.api.file.Directory;
 import org.gradle.api.internal.CollectionCallbackActionDecorator;
 import org.gradle.api.internal.CompositeDomainObjectSet;
 import org.gradle.api.internal.artifacts.DefaultModuleVersionIdentifier;
@@ -82,7 +83,7 @@ public abstract class DefaultMavenPublication implements MavenPublicationInterna
     private final String name;
     private final ImmutableAttributesFactory immutableAttributesFactory;
     private final TaskDependencyFactory taskDependencyFactory;
-    private final File buildDir;
+    private final Directory buildDir;
 
     private final VersionMappingStrategyInternal versionMappingStrategy;
     private final MavenPomInternal pom;
@@ -120,7 +121,7 @@ public abstract class DefaultMavenPublication implements MavenPublicationInterna
         this.immutableAttributesFactory = immutableAttributesFactory;
         this.versionMappingStrategy = versionMappingStrategy;
         this.taskDependencyFactory = taskDependencyFactory;
-        this.buildDir = project.getRootProject().getLayout().getProjectDirectory().getAsFile();
+        this.buildDir = project.getLayout().getProjectDirectory();
 
         MavenComponentParser mavenComponentParser = objectFactory.newInstance(MavenComponentParser.class, mavenArtifactParser);
 
@@ -511,7 +512,7 @@ public abstract class DefaultMavenPublication implements MavenPublicationInterna
         populateFromComponent();
         if (getComponent().isPresent()) {
             MavenPublicationErrorChecker.checkThatArtifactIsPublishedUnmodified(
-                buildDir.toPath().toAbsolutePath(), getComponent().get().getName(),
+                buildDir.getAsFile().toPath().toAbsolutePath(), getComponent().get().getName(),
                 source, mainArtifacts
             );
         }
