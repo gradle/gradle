dd19ca6f46d0f8054425b3d7db462f8e94645a65
Rafael Chaves <rchaves@gradle.com>
2024-01-16 12:51:10 -0300

    Bump kotlin-dsl plugin version for publication

--- platforms/core-configuration/kotlin-dsl-plugins/build.gradle.kts
+++ platforms/core-configuration/kotlin-dsl-plugins/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 description = "Kotlin DSL Gradle Plugins deployed to the Plugin Portal"
 
 group = "org.gradle.kotlin"
-version = "4.2.2"
+version = "4.3.0"
 
 base.archivesName = "plugins"
 
