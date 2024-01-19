75192a2d1cbe2400da503d7303b1c7c53dc6e1d6
Gary Hale <gary@gradle.com>
2024-01-19 17:01:21 -0500

    Remove * import from annotation processor

--- platforms/core-execution/workers/src/integTest/groovy/org/gradle/workers/internal/WorkerDaemonProcessFailureIntegrationTest.groovy
+++ platforms/core-execution/workers/src/integTest/groovy/org/gradle/workers/internal/WorkerDaemonProcessFailureIntegrationTest.groovy
@@ -239,7 +239,10 @@ class WorkerDaemonProcessFailureIntegrationTest extends AbstractDaemonWorkerExec
                 }
             """
             file('processor/src/main/java/WorkerPidProcessor.java') << """
-                import javax.annotation.processing.*;
+                import javax.annotation.processing.AbstractProcessor;
+                import javax.annotation.processing.RoundEnvironment;
+                import javax.annotation.processing.SupportedAnnotationTypes;
+                import javax.annotation.processing.SupportedSourceVersion;
                 import javax.lang.model.SourceVersion;
                 import javax.lang.model.element.TypeElement;
                 import java.util.Set;
