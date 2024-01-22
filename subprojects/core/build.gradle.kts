9cd2ac18254fb4f8de36f0efe6b9a1a4955d1a0c
Louis Jacomet <louis@gradle.com>
2024-01-22 15:56:01 +0100

    Upgrade Apache SSHD and replace jsch
    * Apache SSHD upgrade to 2.10.0 which includes package renames and
    refactoring of our test code
    * Replace com.jcraft:jsch with com.github.mwiede:jsch which is a drop in
     replacement that is maintained
    
     Fixes #19028

--- subprojects/core/build.gradle.kts
+++ subprojects/core/build.gradle.kts
@@ -97,7 +97,6 @@ dependencies {
     }
 
     // Libraries that are not used in this project but required in the distribution
-    runtimeOnly(libs.groovyAnt)
     runtimeOnly(libs.groovyAstbuilder)
     runtimeOnly(libs.groovyConsole)
     runtimeOnly(libs.groovyDateUtil)
@@ -106,6 +105,10 @@ dependencies {
     runtimeOnly(libs.groovyNio)
     runtimeOnly(libs.groovySql)
     runtimeOnly(libs.groovyTest)
+
+    // The bump to SSHD 2.10.0 causes a global exclusion for `groovy-ant` -> `ant-junit`, so forcing it back in here
+    // TODO investigate why we depend on SSHD as a platform for internal-integ-testing
+    runtimeOnly(libs.antJunit)
 
     testImplementation(project(":platform-jvm"))
     testImplementation(project(":platform-native"))
