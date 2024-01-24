8e7191c6d7c3fef6d529a86562e02dc6961a7351
Tom Tresansky <ttresansky@gradle.com>
2024-01-24 10:04:06 -0500

    Extract NoMatchingArtifactVariant describer

--- platforms/software/dependency-management/src/main/java/org/gradle/internal/component/resolution/failure/describer/AbstractResolutionFailureDescriber.java
+++ platforms/software/dependency-management/src/main/java/org/gradle/internal/component/resolution/failure/describer/AbstractResolutionFailureDescriber.java
@@ -20,6 +20,7 @@ import org.gradle.api.attributes.Attribute;
 import org.gradle.api.internal.DocumentationRegistry;
 import org.gradle.api.internal.attributes.AttributeDescriber;
 import org.gradle.internal.component.AbstractVariantSelectionException;
+import org.gradle.internal.component.resolution.failure.ResolutionCandidateAssessor;
 import org.gradle.internal.component.resolution.failure.ResolutionCandidateAssessor.AssessedAttribute;
 import org.gradle.internal.component.resolution.failure.ResolutionCandidateAssessor.AssessedCandidate;
 import org.gradle.internal.logging.text.StyledTextOutput;
@@ -87,6 +88,34 @@ public abstract class AbstractResolutionFailureDescriber<T extends AbstractVaria
         }
         formatter.startChildren();
         formatAttributeSection(formatter, "Unmatched attribute", other);
+        formatter.endChildren();
+    }
+
+    protected void formatAttributeMatchesForIncompatibility(
+        AssessedCandidate assessedCandidate,
+        TreeFormatter formatter,
+        AttributeDescriber describer
+    ) {
+        // None of the nullability warnings are relevant here because the attribute values are only retrieved from collections that will contain them
+        Map<Attribute<?>, ?> compatibleAttrs = assessedCandidate.getCompatibleAttributes().stream()
+            .collect(Collectors.toMap(ResolutionCandidateAssessor.AssessedAttribute::getAttribute, ResolutionCandidateAssessor.AssessedAttribute::getProvided, (a, b) -> a));
+        Map<Attribute<?>, ?> incompatibleAttrs = assessedCandidate.getIncompatibleAttributes().stream()
+            .collect(Collectors.toMap(ResolutionCandidateAssessor.AssessedAttribute::getAttribute, ResolutionCandidateAssessor.AssessedAttribute::getProvided, (a, b) -> a));
+        Map<Attribute<?>, ?> incompatibleConsumerAttrs = assessedCandidate.getIncompatibleAttributes().stream()
+            .collect(Collectors.toMap(ResolutionCandidateAssessor.AssessedAttribute::getAttribute, ResolutionCandidateAssessor.AssessedAttribute::getRequested, (a, b) -> a));
+        List<String> otherValues = assessedCandidate.getOnlyOnRequestAttributes().stream()
+            .map(assessedAttribute -> "Doesn't say anything about " + describer.describeMissingAttribute(assessedAttribute.getAttribute(), assessedAttribute.getRequested()))
+            .sorted()
+            .collect(Collectors.toList());
+
+        if (!compatibleAttrs.isEmpty()) {
+            formatter.append(" declares ").append(style(StyledTextOutput.Style.SuccessHeader, describer.describeAttributeSet(compatibleAttrs)));
+        }
+        formatter.startChildren();
+        if (!incompatibleAttrs.isEmpty()) {
+            formatter.node("Incompatible because this component declares " + style(StyledTextOutput.Style.FailureHeader, describer.describeAttributeSet(incompatibleAttrs)) + " and the consumer needed <FailureHeader>" + describer.describeAttributeSet(incompatibleConsumerAttrs) + "</FailureHeader>");
+        }
+        formatAttributeSection(formatter, "Other compatible attribute", otherValues);
         formatter.endChildren();
     }
 }
