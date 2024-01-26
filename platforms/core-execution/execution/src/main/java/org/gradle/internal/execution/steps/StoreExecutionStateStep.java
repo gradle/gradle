3aa60a45bbb89b3f86be97893b28ea6d68b39e2a
Lóránt Pintér <lorant@gradle.com>
2024-01-26 16:49:34 +0100

    Remove unused type parameter

--- platforms/core-execution/execution/src/main/java/org/gradle/internal/execution/steps/StoreExecutionStateStep.java
+++ platforms/core-execution/execution/src/main/java/org/gradle/internal/execution/steps/StoreExecutionStateStep.java
@@ -51,7 +51,7 @@ public class StoreExecutionStateStep<C extends BeforeExecutionContext, R extends
         return result;
     }
 
-    private static <C extends BeforeExecutionContext, R extends AfterExecutionResult> boolean shouldPreserveFailedState(C context, ExecutionOutputState afterExecutionOutputState) {
+    private static <C extends BeforeExecutionContext> boolean shouldPreserveFailedState(C context, ExecutionOutputState afterExecutionOutputState) {
         // We do not store the history if there was a failure and the outputs did not change, since then the next execution can be incremental.
         // For example the current execution fails because of a compilation failure and for the next execution the source file is fixed,
         // so only the one changed source file needs to be compiled.
