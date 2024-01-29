5f4335c782092821976e9cc98bf6a5b5bcf4d9cb
József Bartók <jbartok@gradle.com>
2024-01-29 10:43:04 +0200

    Remove submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,8 @@ dependencies {
     implementation(kotlin("compiler-embeddable"))
     implementation(kotlin("reflect"))
 
-    api("kotlinx.ast:parser-antlr-kotlin:0.1.0")
-    implementation("kotlinx.ast:grammar-kotlin-parser-antlr-kotlin:0.1.0")
-
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+    implementation("com.strumenta:antlr-kotlin-runtime-jvm:0.1.2")
 
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.jetbrains:annotations:24.0.1")
