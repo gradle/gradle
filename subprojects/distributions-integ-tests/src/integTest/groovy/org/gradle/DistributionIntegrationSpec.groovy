480de7642044d5b2038c264da1f4c2de07fa6f38
József Bartók <jbartok@gradle.com>
2024-01-30 12:17:38 +0200

    Adjust distribution related tests

--- subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -34,7 +34,7 @@ import static org.hamcrest.MatcherAssert.assertThat
 
 abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
 
-    protected static final THIRD_PARTY_LIB_COUNT = 159
+    protected static final THIRD_PARTY_LIB_COUNT = 151
 
     @Shared String baseVersion = GradleVersion.current().baseVersion.version
 
