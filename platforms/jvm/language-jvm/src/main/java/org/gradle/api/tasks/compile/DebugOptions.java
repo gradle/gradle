78a9dfda0ac89c2c76ea7ddc71725b418d7269d1
Justin Van Dort <jvandort@gradle.com>
2024-01-29 21:50:50 -0500

    Refine javadoc

--- platforms/jvm/language-jvm/src/main/java/org/gradle/api/tasks/compile/DebugOptions.java
+++ platforms/jvm/language-jvm/src/main/java/org/gradle/api/tasks/compile/DebugOptions.java
@@ -31,8 +31,8 @@ public class DebugOptions extends AbstractOptions {
     private String debugLevel;
 
     /**
-     * Tells which debugging information is to be generated. The value is a comma-separated
-     * list of any of the following keywords (without spaces in between):
+     * Get a comma-separated list of debug information to be generated during compilation.
+     * The list may contain any of the following keywords (without spaces in between):
      *
      * <dl>
      *     <dt>{@code source}
@@ -43,9 +43,9 @@ public class DebugOptions extends AbstractOptions {
      *     <dd>Local variable debugging information
      * </dl>
      *
-     * Also instead of those values {@code none} can be used.
+     * <p>Alternatively, a value of {@code none} means debug information will not be generated.</p>
      *
-     * By default, only source and line debugging information will be generated.
+     * <p>When the value is null, only source and line debugging information will be generated.</p>
      */
     @Nullable
     @Optional
@@ -55,8 +55,8 @@ public class DebugOptions extends AbstractOptions {
     }
 
     /**
-     * Sets which debug information is to be generated. The value is a comma-separated
-     * list of any of the following keywords (without spaces in between):
+     * Sets which debug information is to be generated during compilation. The value must be a
+     * comma-separated list containing any of the following keywords (without spaces in between):
      *
      * <dl>
      *     <dt>{@code source}
@@ -67,8 +67,12 @@ public class DebugOptions extends AbstractOptions {
      *     <dd>Local variable debugging information
      * </dl>
      *
-     * For example {@code source,lines,vars}. Alternatively {@code none} can be used
-     * to completely disable debug information.
+     * <p>For example {@code source,lines,vars} is a valid value.</p>
+     *
+     * <p>Alternatively setting the value to {@code none} will disable debug information generation.</p>
+     *
+     * <p>Setting this value to null will reset the property to its default value of only
+     * generating line and source debug information.</p>
      */
     public void setDebugLevel(@Nullable String debugLevel) {
         this.debugLevel = debugLevel;
