5450773d847a6a5ba86df1ac6b10310d792d1d64
Bálint Hegyi <bhegyi@gradle.com>
2024-02-15 13:53:40 +0000

    Initial rendering classes

---
+++ platforms/ide/problems/src/main/java/org/gradle/problems/internal/rendering/JavaCompilerProblemRenderer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.problems.internal.rendering;
+
+import org.gradle.api.problems.internal.Problem;
+import org.gradle.problems.rendering.ProblemRenderer;
+
+import java.util.List;
+
+public class JavaCompilerProblemRenderer implements ProblemRenderer {
+
+    private static final String JAVA_COMPILER_CATEGORY = "org.gradle.java-compiler";
+
+    @Override
+    public void render(List<Problem> problems) {
+        problems
+            .stream()
+            .filter(JavaCompilerProblemRenderer::isJavaCompilerProblem)
+            .forEach(JavaCompilerProblemRenderer::printFormattedMessageToErrorStream);
+    }
+
+    private static void printFormattedMessageToErrorStream(Problem problem) {
+        String formatted = (String) problem.getAdditionalData().get("formatted");
+        System.err.println(formatted);
+    }
+
+    private static boolean isJavaCompilerProblem(Problem problem) {
+        return problem.getCategory().getCategory().startsWith(JAVA_COMPILER_CATEGORY);
+    }
+}
