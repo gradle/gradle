3e88842d196c9ae0212995c8c65e7c9dabf1c065
Denes Daniel <pantherdd@gmail.com>
2024-02-21 23:27:11 +0100

    Add more test cases for `ScalaRuntime`
    * Copied from `ScalaRuntimeHelperTest`
    
    Signed-off-by: Denes Daniel <pantherdd@gmail.com>

--- platforms/jvm/scala/src/test/groovy/org/gradle/api/tasks/ScalaRuntimeTest.groovy
+++ platforms/jvm/scala/src/test/groovy/org/gradle/api/tasks/ScalaRuntimeTest.groovy
@@ -34,7 +34,7 @@ class ScalaRuntimeTest extends AbstractProjectBuilderSpec {
         when:
         def classpath = project.scalaRuntime.inferScalaClasspath([new File("other.jar"), new File("scala-library-2.10.1.jar")])
         then:
-        assertHasCorrectDependencies(classpath, ScalaBasePlugin.DEFAULT_ZINC_VERSION)
+        assertHasCorrectScala2Dependencies(classpath, ScalaBasePlugin.DEFAULT_ZINC_VERSION)
     }
 
     def "inferred Scala class path contains 'scala-compiler' repository dependency and 'compiler-bridge' matching 'scala-library' Jar found on class path with specified zinc version"() {
@@ -48,10 +48,10 @@ class ScalaRuntimeTest extends AbstractProjectBuilderSpec {
         when:
         def classpath = project.scalaRuntime.inferScalaClasspath([new File("other.jar"), new File("scala-library-2.10.1.jar")])
         then:
-        assertHasCorrectDependencies(classpath, useZincVersion)
+        assertHasCorrectScala2Dependencies(classpath, useZincVersion)
     }
 
-    private void assertHasCorrectDependencies(classpath, zincVersion) {
+    private void assertHasCorrectScala2Dependencies(classpath, zincVersion) {
         assert classpath instanceof LazilyInitializedFileCollection
         assert classpath.sourceCollections.size() == 1
         with(classpath.sourceCollections[0]) {
@@ -140,5 +140,35 @@ class ScalaRuntimeTest extends AbstractProjectBuilderSpec {
         then:
         GradleException e = thrown()
         e.message.startsWith("Cannot infer Scala class path because no Scala library Jar was found. Does root project 'test-project' declare dependency to scala-library? Searched classpath:")
+    }
+
+    def "allows to find Scala Jar on class path"() {
+        when:
+        def file = project.scalaRuntime.findScalaJar([new File("other.jar"), new File("scala-jdbc-1.5.jar"), new File("scala-compiler-1.7.jar")], "jdbc")
+
+        then:
+        file.name == "scala-jdbc-1.5.jar"
+    }
+
+    def "returns null if Scala Jar not found"() {
+        when:
+        def file = project.scalaRuntime.findScalaJar([new File("other.jar"), new File("scala-jdbc-1.5.jar"), new File("scala-compiler-1.7.jar")], "library")
+
+        then:
+        file == null
+    }
+
+    def "allows to determine version of Scala Jar"() {
+        expect:
+        project.scalaRuntime.getScalaVersion(new File("scala-compiler-2.9.2.jar")) == "2.9.2"
+        project.scalaRuntime.getScalaVersion(new File("scala-jdbc-2.9.2.jar")) == "2.9.2"
+        project.scalaRuntime.getScalaVersion(new File("scala-library-2.10.0-SNAPSHOT.jar")) == "2.10.0-SNAPSHOT"
+        project.scalaRuntime.getScalaVersion(new File("scala-library-2.10.0-rc-3.jar")) == "2.10.0-rc-3"
+    }
+
+    def "returns null if Scala version cannot be determined"() {
+        expect:
+        project.scalaRuntime.getScalaVersion(new File("scala-compiler.jar")) == null
+        project.scalaRuntime.getScalaVersion(new File("groovy-compiler-2.1.0.jar")) == null
     }
 }
