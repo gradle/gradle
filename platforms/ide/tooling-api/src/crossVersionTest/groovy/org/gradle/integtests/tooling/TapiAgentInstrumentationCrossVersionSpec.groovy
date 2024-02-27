4e906accd6f2048039d13343c549571b52194dd9
Reinhold Degenfellner <rdegenfellner@gradle.com>
2024-02-27 09:42:54 +0100

    Move TapiAgentInstrumentationCrossVersionSpec to TAPI project

--- platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/TapiAgentInstrumentationCrossVersionSpec.groovy
+++ platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/TapiAgentInstrumentationCrossVersionSpec.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.internal.agents
+package org.gradle.integtests.tooling
 
 import org.gradle.integtests.tooling.fixture.TargetGradleVersion
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
