003e9962c8a1244020f70f3173fdb831d3586563
Tom Tresansky <ttresansky@gradle.com>
2024-03-01 10:31:12 -0500

    Add more distribution size to allow for publishing snapshot

--- subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegritySpec.groovy
+++ subprojects/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegritySpec.groovy
@@ -35,7 +35,7 @@ class DistributionIntegritySpec extends DistributionIntegrationSpec {
 
     @Override
     int getMaxDistributionSizeBytes() {
-        return 130 * 1024 * 1024
+        return 131 * 1024 * 1024
     }
 
     /**
