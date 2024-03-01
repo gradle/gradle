ef7374aa137aec105111bb216d9d254dc4c3fcb8
Anže Sodja <asodja@gradle.com>
2024-03-01 15:28:43 +0100

    Move some tests from integ-tests to execution-integ-tests

--- platforms/core-execution/execution-integ-tests/build.gradle.kts
+++ platforms/core-execution/execution-integ-tests/build.gradle.kts
@@ -3,3 +3,8 @@ plugins {
 }
 
 description = "Execution engine integration tests with Gradle Build Tool"
+
+dependencies {
+    integTestImplementation(project(":execution"))
+    integTestDistributionRuntimeOnly(project(":distributions-full"))
+}
