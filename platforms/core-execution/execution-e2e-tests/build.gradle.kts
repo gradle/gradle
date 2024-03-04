fb06216f8b410cf425721ad7309fe008f77e8937
Anže Sodja <asodja@gradle.com>
2024-03-04 11:15:40 +0100

    Rename execution-integ-tests to execution-e2e-tests

--- platforms/core-execution/execution-e2e-tests/build.gradle.kts
+++ platforms/core-execution/execution-e2e-tests/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("gradlebuild.internal.java")
 }
 
-description = "Execution engine integration tests with Gradle Build Tool"
+description = "Execution engine end-to-end tests"
 
 dependencies {
     integTestImplementation(project(":execution"))
