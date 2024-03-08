a0712571b13d51ac1571d8c07605d75a83c332b9
Lóránt Pintér <lorant@gradle.com>
2024-03-08 16:03:30 +0100

    Declare service scope for BuildOperationRunner

--- platforms/core-runtime/build-operations/src/main/java/org/gradle/internal/operations/BuildOperationRunner.java
+++ platforms/core-runtime/build-operations/src/main/java/org/gradle/internal/operations/BuildOperationRunner.java
@@ -16,12 +16,16 @@
 
 package org.gradle.internal.operations;
 
+import org.gradle.internal.service.scopes.Scope;
+import org.gradle.internal.service.scopes.ServiceScope;
+
 import javax.annotation.Nullable;
 
 /**
  * Runs build operations: the pieces of work that make up a build.
  * Build operations can be nested inside other build operations.
  */
+@ServiceScope(Scope.Global.class)
 public interface BuildOperationRunner {
     /**
      * Runs the given build operation.
