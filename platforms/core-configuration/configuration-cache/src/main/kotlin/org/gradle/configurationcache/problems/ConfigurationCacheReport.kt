340b149cfeda5f0b077d580e986c85c9b9537238
Alex Semin <asemin@gradle.com>
2024-03-13 11:40:56 +0800

    Hide IntelliJ problems from the report

--- platforms/core-configuration/configuration-cache/src/main/kotlin/org/gradle/configurationcache/problems/ConfigurationCacheReport.kt
+++ platforms/core-configuration/configuration-cache/src/main/kotlin/org/gradle/configurationcache/problems/ConfigurationCacheReport.kt
@@ -122,7 +122,10 @@ class ConfigurationCacheReport(
 
             override fun onDiagnostic(kind: DiagnosticKind, problem: PropertyProblem): State {
                 executor.submit {
-                    writer.writeDiagnostic(kind, decorate(problem))
+                    val p = decorate(problem)
+                    if (!p.isIntellijInitScriptProblem()) {
+                        writer.writeDiagnostic(kind, p)
+                    }
                 }
                 return this
             }
@@ -306,4 +309,13 @@ class ConfigurationCacheReport(
     private
     fun stackTraceStringFor(error: Throwable): String =
         stackTraceExtractor.stackTraceStringFor(error)
+}
+
+
+private
+fun DecoratedPropertyProblem.isIntellijInitScriptProblem(): Boolean {
+    val e = exception ?: return false
+    return e.original.stackTrace.any {
+        it.className.startsWith("ijIdeaPluginConfigurator")
+    }
 }
