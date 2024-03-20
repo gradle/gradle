5e47b69677e236c2f4d04e59daff8073bfae4f4c
Alex Semin <asemin@gradle.com>
2024-03-20 16:56:51 +0800

    Fix IndexedSpec

--- platforms/core-runtime/base-services/src/main/java/org/gradle/util/internal/IndexedSpec.java
+++ platforms/core-runtime/base-services/src/main/java/org/gradle/util/internal/IndexedSpec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 original author or authors.
+ * Copyright 2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,11 +15,14 @@
  */
 package org.gradle.util.internal;
 
+import org.gradle.api.NonNullApi;
+
 /**
  * Represents some predicate against objects of type T with a position in an iterable collection.
  *
  * @param <T> The target type for this spec
  */
+@NonNullApi
 public interface IndexedSpec<T> {
     boolean isSatisfiedBy(int index, T element);
 }
