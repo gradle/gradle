80bc4a9a28b5b8f66dfbc1ebfd6322d921050a0a
Louis Jacomet <louis@gradle.com>
2024-03-20 09:14:10 +0100

    Bump okio to 3.4.0 and woodstox-core to 6.4.0

--- build-logic-commons/build-platform/build.gradle.kts
+++ build-logic-commons/build-platform/build.gradle.kts
@@ -36,6 +36,9 @@ dependencies {
         api("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
         api("org.gradle.kotlin:gradle-kotlin-dsl-conventions:0.9.0")
         api("com.autonomousapps:dependency-analysis-gradle-plugin:1.28.0")
+        api("com.squareup.okio:okio:3.4.0") {
+            because("Bump version brought in by dependency-analysis-gradle-plugin, to resolve CVE-2022-3635")
+        }
 
         // Java Libraries
         api("com.github.javaparser:javaparser-core:$javaParserVersion")
@@ -68,6 +71,9 @@ dependencies {
         api("org.asciidoctor:asciidoctorj-pdf:2.3.10")
         api("dev.adamko.dokkatoo:dokkatoo-plugin:2.0.0")
         api("org.jetbrains.dokka:dokka-core:1.8.10")
+        api("com.fasterxml.woodstox:woodstox-core:6.4.0") {
+            because("CVE-2022-40152 on lower versions")
+        }
         api("com.beust:jcommander:1.78")
         api("org.codehaus.groovy:$groovyVersion")
         api("org.codehaus.groovy.modules.http-builder:http-builder:0.7.2") // TODO maybe change group name when upgrading to Groovy 4
