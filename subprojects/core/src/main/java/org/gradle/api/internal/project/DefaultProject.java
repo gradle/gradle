276396f5ff124c3baf0664bb5ae1c69b1fa2c304
Adam Murdoch <adam@gradle.com>
2024-03-28 06:08:16 +1100

    TODO - minor tweaks

--- subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
@@ -563,7 +563,6 @@ public abstract class DefaultProject extends AbstractPluginAware implements Proj
         return owner.getProjectPath().toString();
     }
 
-
     @Override
     public String getBuildTreePath() {
         return getIdentityPath().getPath();
