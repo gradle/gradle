c951a106fc1281d7478ee7581a7330df7b62577c
Paul Merlin <paul@gradle.com>
2024-03-28 09:24:23 +0100

    Update kotlin-dsl plugins version

--- build-logic-commons/gradle-plugin/build.gradle.kts
+++ build-logic-commons/gradle-plugin/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation(project(":module-identity"))
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:3.1.0")
 
-    implementation("org.gradle.kotlin.kotlin-dsl:org.gradle.kotlin.kotlin-dsl.gradle.plugin:4.3.0")
+    implementation("org.gradle.kotlin.kotlin-dsl:org.gradle.kotlin.kotlin-dsl.gradle.plugin:4.3.1")
     // This Kotlin version should only be updated when updating the above kotlin-dsl version
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     implementation("org.gradle.kotlin:gradle-kotlin-dsl-conventions")
