ebb72298c366d6efd0cc9933849232398804026c
Alex Semin <asemin@gradle.com>
2024-03-29 13:41:26 +0800

    Fix imports

--- subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
@@ -146,7 +146,6 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.concurrent.Callable;
-import java.util.stream.Collectors;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static java.util.Collections.singletonMap;
