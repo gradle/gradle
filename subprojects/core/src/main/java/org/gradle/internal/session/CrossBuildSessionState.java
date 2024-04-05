3a9d8806d0caf884c88d676e4cb8d2fe437eb960
Alex Semin <asemin@gradle.com>
2024-04-06 00:22:57 +0800

    Introduce WorkerLimits service

--- subprojects/core/src/main/java/org/gradle/internal/session/CrossBuildSessionState.java
+++ subprojects/core/src/main/java/org/gradle/internal/session/CrossBuildSessionState.java
@@ -27,7 +27,9 @@ import org.gradle.internal.code.DefaultUserCodeApplicationContext;
 import org.gradle.internal.code.UserCodeApplicationContext;
 import org.gradle.internal.concurrent.CompositeStoppable;
 import org.gradle.internal.concurrent.DefaultParallelismConfiguration;
+import org.gradle.internal.concurrent.DefaultWorkerLimits;
 import org.gradle.internal.concurrent.ExecutorFactory;
+import org.gradle.internal.concurrent.WorkerLimits;
 import org.gradle.internal.event.ListenerManager;
 import org.gradle.internal.logging.sink.OutputEventListenerManager;
 import org.gradle.internal.operations.BuildOperationExecutor;
@@ -105,6 +107,10 @@ public class CrossBuildSessionState implements Closeable {
 
         ParallelismConfiguration createParallelismConfiguration() {
             return new DefaultParallelismConfiguration(startParameter.isParallelProjectExecutionEnabled(), startParameter.getMaxWorkerCount());
+        }
+
+        WorkerLimits createWorkerLimits() {
+            return new DefaultWorkerLimits(startParameter.getMaxWorkerCount());
         }
 
         BuildOperationExecutor createBuildOperationExecutor(
