04a0f2062595aec5a6387b5a0ea715806fb19ee7
Alex Semin <asemin@gradle.com>
2024-04-09 21:43:29 +0800

    Add a test for a future use-case

--- platforms/core-runtime/base-services/src/test/groovy/org/gradle/internal/service/ScopedServiceRegistryTest.groovy
+++ platforms/core-runtime/base-services/src/test/groovy/org/gradle/internal/service/ScopedServiceRegistryTest.groovy
@@ -19,6 +19,7 @@ package org.gradle.internal.service
 
 import org.gradle.internal.service.scopes.Scope
 import org.gradle.internal.service.scopes.ServiceScope
+import org.gradle.util.internal.ToBeImplemented
 import spock.lang.Specification
 
 class ScopedServiceRegistryTest extends Specification {
@@ -134,6 +135,27 @@ class ScopedServiceRegistryTest extends Specification {
         'provider' | { ScopedServiceRegistry it -> it.addProvider(new BuildTreeScopedServiceInterfaceUnscopedImplProvider()) }
     }
 
+    @ToBeImplemented
+    def "fails when registering an unscoped service via #method in strict mode"() {
+        given:
+        def registry = strictScopedRegistry(Scope.BuildTree)
+
+        when:
+        registration(registry)
+
+        then:
+        noExceptionThrown()
+        // TODO: support validation of not annotated services
+//        def exception = thrown(IllegalArgumentException)
+//        exception.message.contains("The service implementation '${UnscopedService.name}' is registered in the 'BuildTree' scope but does not declare it explicitly.")
+
+        where:
+        method     | registration
+        'instance' | { ScopedServiceRegistry it -> it.add(new UnscopedService()) }
+        'type'     | { ScopedServiceRegistry it -> it.register { it.add(UnscopedService) } }
+        'provider' | { ScopedServiceRegistry it -> it.addProvider(new UnscopedServiceProvider()) }
+    }
+
     def "succeeds when registering a multi-scoped service in the correct scope (#scopeName)"() {
         given:
         def registry = scopedRegistry(scope)
@@ -172,6 +194,13 @@ class ScopedServiceRegistryTest extends Specification {
     static class GlobalAndBuildScopedService {}
 
     static class UnscopedService {}
+
+    static class UnscopedServiceProvider {
+        @SuppressWarnings('unused')
+        UnscopedService createScopedService() {
+            return new UnscopedService()
+        }
+    }
 
     // Important that this is an interface, because then `@ServiceScope` is not inherited for implementations
     @ServiceScope(Scope.BuildTree)
