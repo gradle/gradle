c19bf5970dd3c5f26f164a9541bc424d246b3591
Lóránt Pintér <lorant@gradle.com>
2024-04-15 09:55:36 +0200

    Fix UnusedVariable in :persistent-cache

--- platforms/core-execution/persistent-cache/build.gradle.kts
+++ platforms/core-execution/persistent-cache/build.gradle.kts
@@ -10,7 +10,6 @@ description = """Persistent caches on disk and cross process locking.
 
 errorprone {
     disabledChecks.addAll(
-        "UnusedVariable", // 1 occurrences
         "WaitNotInLoop", // 1 occurrences
     )
 }
