a24d758d6bda117c33e48af2502e8b525899d10d
Adam Murdoch <adam@gradle.com>
2024-04-18 09:57:44 +1000

    Expose a single shared `BuildCacheController` instance to included builds.
    The shared instance's behavior changes based on the state of the root build, rather than creating different implementations based on the state of the root build.

--- subprojects/core/src/main/java/org/gradle/caching/internal/controller/impl/RootBuildCacheControllerRef.java
+++ subprojects/core/src/main/java/org/gradle/caching/internal/controller/impl/RootBuildCacheControllerRef.java
@@ -19,6 +19,7 @@ package org.gradle.caching.internal.controller.impl;
 import org.gradle.caching.BuildCacheKey;
 import org.gradle.caching.internal.CacheableEntity;
 import org.gradle.caching.internal.controller.BuildCacheController;
+import org.gradle.caching.internal.controller.NoOpBuildCacheController;
 import org.gradle.caching.internal.controller.service.BuildCacheLoadResult;
 import org.gradle.internal.service.scopes.Scope;
 import org.gradle.internal.service.scopes.ServiceScope;
@@ -27,55 +28,58 @@ import org.gradle.internal.snapshot.FileSystemSnapshot;
 import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 @ServiceScope(Scope.BuildTree.class)
 public class RootBuildCacheControllerRef {
 
-    private BuildCacheController buildCacheController;
+    private final DelegatingBuildCacheController sharedInstance = new DelegatingBuildCacheController();
 
     public void set(BuildCacheController buildCacheController) {
-        // This instance ends up in build/gradle scoped services for nesteds
-        // We don't want to invoke close at that time.
-        // Instead, close it at the root.
-        this.buildCacheController = new CloseShieldBuildCacheController(buildCacheController);
+        sharedInstance.set(buildCacheController);
     }
 
     public BuildCacheController getForNonRootBuild() {
-        if (!isSet()) {
-            throw new IllegalStateException("Root build cache controller not yet assigned");
+        return sharedInstance;
+    }
+
+    private static class DelegatingBuildCacheController implements BuildCacheController {
+        private final AtomicReference<BuildCacheController> delegate = new AtomicReference<>();
+
+        private DelegatingBuildCacheController() {
+            this.delegate.set(NoOpBuildCacheController.INSTANCE);
         }
 
-        return buildCacheController;
-    }
-
-    public boolean isSet() {
-        return buildCacheController != null;
-    }
-
-    private static class CloseShieldBuildCacheController implements BuildCacheController {
-        private final BuildCacheController delegate;
-
-        private CloseShieldBuildCacheController(BuildCacheController delegate) {
-            this.delegate = delegate;
+        void set(BuildCacheController buildCacheController) {
+            if (!this.delegate.compareAndSet(NoOpBuildCacheController.INSTANCE, buildCacheController)) {
+                throw new IllegalStateException("Build cache controller already set");
+            }
         }
 
         @Override
         public boolean isEnabled() {
-            return delegate.isEnabled();
+            return getDelegate().isEnabled();
         }
 
         @Override
         public Optional<BuildCacheLoadResult> load(BuildCacheKey cacheKey, CacheableEntity cacheableEntity) {
-            return delegate.load(cacheKey, cacheableEntity);
+            return getDelegate().load(cacheKey, cacheableEntity);
         }
 
         @Override
         public void store(BuildCacheKey cacheKey, CacheableEntity entity, Map<String, FileSystemSnapshot> snapshots, Duration executionTime) {
-            delegate.store(cacheKey, entity, snapshots, executionTime);
+            getDelegate().store(cacheKey, entity, snapshots, executionTime);
+        }
+
+        private BuildCacheController getDelegate() {
+            return delegate.get();
         }
 
         @Override
         public void close() {
+            // This instance ends up in build/gradle scoped services for nesteds
+            // We don't want to invoke close at that time.
+            // Instead, close it at the root.
         }
     }
 
