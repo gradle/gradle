6eb1691263b8227881a9d9c043884b0db32af3d0
Paul Merlin <paul@gradle.com>
2024-04-20 13:13:44 +0200

    store all connection parameters in the DB
    java home, gradle user home and gradle distribution

--- gradle-client/src/jvmMain/kotlin/org/gradle/client/core/database/Build.kt
+++ gradle-client/src/jvmMain/kotlin/org/gradle/client/core/database/Build.kt
@@ -1,9 +1,13 @@
 package org.gradle.client.core.database
 
+import org.gradle.client.core.gradle.GradleDistribution
 import org.gradle.client.core.util.generateIdentity
 import java.io.File
 
 data class Build(
     val id: String = generateIdentity(),
     val rootDir: File,
+    val javaHomeDir: File? = null,
+    val gradleUserHomeDir: File? = null,
+    val gradleDistribution: GradleDistribution = GradleDistribution.Default,
 )
