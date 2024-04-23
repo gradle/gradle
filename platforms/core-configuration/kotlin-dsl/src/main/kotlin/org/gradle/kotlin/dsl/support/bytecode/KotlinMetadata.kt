caa5e6d4afcbe3b0a06823b323e5c8e9f0be0293
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2024-04-23 16:31:48 -0300

    Polish `KotlinMetadata.kt`
    - Document supported version
    - Reformat comment

--- platforms/core-configuration/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/support/bytecode/KotlinMetadata.kt
+++ platforms/core-configuration/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/support/bytecode/KotlinMetadata.kt
@@ -51,6 +51,10 @@ import org.jetbrains.org.objectweb.asm.Opcodes
 import java.io.File
 
 
+/**
+ * `1.7` for compatibility with Kotlin 1.8.*
+ * (see [JvmMetadataVersion.LATEST_STABLE_SUPPORTED] for details).
+ */
 internal
 val OUTPUT_METADATA_VERSION = JvmMetadataVersion(1, 7)
 
@@ -304,9 +308,9 @@ fun jvmGetterSignatureFor(pluginsExtension: ExtensionSpec): JvmMethodSignature =
 
 internal
 fun jvmGetterSignatureFor(propertyName: String, desc: String): JvmMethodSignature =
-// Accessors honor the kotlin property jvm interop convention.
-// The only difference with JavaBean 1.01 is to prefer `get` over `is` for boolean properties.
-// The following code also complies with Section 8.8 of the spec, "Capitalization of inferred names.".
-// Sun: "However to support the occasional use of all upper-case names,
+    // Accessors honor the kotlin property jvm interop convention.
+    // The only difference with JavaBean 1.01 is to prefer `get` over `is` for boolean properties.
+    // The following code also complies with Section 8.8 of the spec, "Capitalization of inferred names.".
+    // Sun: "However to support the occasional use of all upper-case names,
     //       we check if the first two characters of the name are both upper case and if so leave it alone."
     JvmMethodSignature("get${propertyName.uppercaseFirstChar()}", desc)
