172de486f656bb6f283c6bd2a22aadf285ad9a44
Anže Sodja <asodja@gradle.com>
2024-04-24 12:58:48 +0200

    Use coarse grained caching for instrumentation

--- subprojects/core/src/main/java/org/gradle/internal/service/scopes/GradleUserHomeScopeServices.java
+++ subprojects/core/src/main/java/org/gradle/internal/service/scopes/GradleUserHomeScopeServices.java
@@ -50,6 +50,7 @@ import org.gradle.execution.plan.ToPlannedTaskConverter;
 import org.gradle.groovy.scripts.internal.CrossBuildInMemoryCachingScriptClassCache;
 import org.gradle.groovy.scripts.internal.DefaultScriptSourceHasher;
 import org.gradle.groovy.scripts.internal.GroovyDslWorkspaceProvider;
+import org.gradle.groovy.scripts.internal.InstrumentationClasspathWorkspaceProvider;
 import org.gradle.groovy.scripts.internal.RegistryAwareClassLoaderHierarchyHasher;
 import org.gradle.groovy.scripts.internal.ScriptSourceHasher;
 import org.gradle.initialization.ClassLoaderRegistry;
@@ -259,5 +260,13 @@ public class GradleUserHomeScopeServices extends WorkerSharedUserHomeScopeServic
         CacheConfigurationsInternal cacheConfigurations
     ) {
        return new GroovyDslWorkspaceProvider(cacheBuilderFactory, fileAccessTimeJournal, cacheConfigurations);
+    }
+
+    InstrumentationClasspathWorkspaceProvider createInstrumentationClasspathWorkspaceProvider(
+        GlobalScopedCacheBuilderFactory cacheBuilderFactory,
+        FileAccessTimeJournal fileAccessTimeJournal,
+        CacheConfigurationsInternal cacheConfigurations
+    ) {
+        return new InstrumentationClasspathWorkspaceProvider(cacheBuilderFactory, fileAccessTimeJournal, cacheConfigurations);
     }
 }
