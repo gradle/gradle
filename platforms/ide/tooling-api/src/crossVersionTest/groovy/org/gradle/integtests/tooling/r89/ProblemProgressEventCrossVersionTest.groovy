10798e3c2632db2e3ad9794cac9dc0ba92549837
Reinhold Degenfellner <rdegenfellner@gradle.com>
2024-04-24 08:47:12 +0200

    Fix cross version tests

--- platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r89/ProblemProgressEventCrossVersionTest.groovy
+++ platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r89/ProblemProgressEventCrossVersionTest.groovy
@@ -247,7 +247,7 @@ class ProblemProgressEventCrossVersionTest extends ToolingApiSpecification {
         thrown(BuildException)
         def problems = listener.problems
         validateCompilationProblem(problems, buildFile)
-        problems[0].failure == null
+        problems[0].failure.failure == null
     }
 
 
