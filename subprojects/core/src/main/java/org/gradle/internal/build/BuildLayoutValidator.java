23b637ea1ba69d9f814c4d5f526250c265a9e258
József Bartók <jbartok@gradle.com>
2024-04-25 10:26:30 +0300

    Improve error message wording

--- subprojects/core/src/main/java/org/gradle/internal/build/BuildLayoutValidator.java
+++ subprojects/core/src/main/java/org/gradle/internal/build/BuildLayoutValidator.java
@@ -70,8 +70,8 @@ public class BuildLayoutValidator {
         StringBuilder message = new StringBuilder("Directory '");
         message.append(startParameter.getCurrentDir())
             .append("' does not contain a Gradle build.\n\n")
-            .append("A Gradle build should contain one of the possible settings files (").append(String.join(",", getValidSettingsFileNames())).append(") in its root directory. ")
-            .append("It may also contain one of the possible build files (").append(String.join(",", getValidBuildFileNames())).append(").\n\n")
+            .append("A Gradle build's root directory should contain one of the possible settings files: ").append(String.join(", ", getValidSettingsFileNames())).append(".")
+            .append("It may also contain one of the possible build files: ").append(String.join(", ", getValidBuildFileNames())).append(".\n\n")
             .append("To create a new Gradle build in this directory run '");
         clientMetaData.describeCommand(message, "init");
         message.append("'\n\n")
