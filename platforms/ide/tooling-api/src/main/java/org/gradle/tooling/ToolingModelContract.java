72bbde449fd0c57233e3d9fd908a9324575c4e4e
József Bartók <jbartok@gradle.com>
2024-05-06 11:19:17 +0300

    Write javadoc for marker annotation

--- platforms/ide/tooling-api/src/main/java/org/gradle/tooling/ToolingModelContract.java
+++ platforms/ide/tooling-api/src/main/java/org/gradle/tooling/ToolingModelContract.java
@@ -24,7 +24,39 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * TODO documentation.
+ * Annotation used to mark TAPI model interfaces. On the client side such interfaces
+ * are instantiated via Java dynamic proxies, and we use this annotation when we want
+ * these proxies to have richer behaviour than just implementing the base interface.
+ * <p>
+ * For example. Let's say the TAPI model interface is {@code Animal}, but which also
+ * has child interfaces such as {@code Dog}, {@code Cat}, {@code Bird}. When we
+ * request a model typed as {@code Animal} on the TAPI client side, we will get
+ * a Java dynamic proxy, on which we can call any method from the {@code Animal}
+ * interface, but even if that model was originally let's say a {@code Dog} on the
+ * Gradle daemon side, the proxy will not support any methods specific to {@code Dog},
+ * like let's say {@code Dog.bark()}.
+ * <p>
+ * Basically polymorphism isn't supported by default in TAPI client-side model instances.
+ * This is what this annotation can be used to fix. We can use it to mark the base model
+ * interface ({@code Animal} in the above example) and specify which of its subtypes
+ * should be polymorpically handled on the TAPI client-side.
+ * <p>
+ * Let's say we define the {@code Animal} model interface like this:
+ * <pre>
+ *      &#64;ToolingModelContract(subTypes = [Dog.class, Cat.class])
+ *      interface Animal {}
+ * </pre>
+ * <p>
+ * On the client-side we will be able to write code such as:
+ * <pre>
+ *      if (model instanceof Dog) {
+ *          ((Dog) model).bark();
+ *      } else if (model instanceof Cat) {
+ *          ((Cat) model).meow();
+ *      } else if (model instance of Bird) {
+ *          // will never be true, Bird is not specified as a model contract type
+ *      }
+ * </pre>
  *
  * @since 8.9
  */
@@ -34,7 +66,8 @@ import java.lang.annotation.Target;
 public @interface ToolingModelContract {
 
     /**
-     * TODO documentation.
+     * Child types of a TAPI model interface (marked by this annotation), which will be
+     * handled polymorpically on the TAPI client side.
      *
      * @since 8.9
      */
