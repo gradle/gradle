0c83a37c87d735ce1b8bebc1f6eb0d1f4dd22c42
Reinhold Degenfellner <rdegenfellner@gradle.com>
2024-05-10 12:35:36 +0200

    Use guava cache instead of Map.
    Allows automatic timeout and a maximum size.

--- platforms/ide/tooling-api/src/main/java/org/gradle/tooling/internal/consumer/loader/CachingToolingImplementationLoader.java
+++ platforms/ide/tooling-api/src/main/java/org/gradle/tooling/internal/consumer/loader/CachingToolingImplementationLoader.java
@@ -15,46 +15,73 @@
  */
 package org.gradle.tooling.internal.consumer.loader;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import org.gradle.api.NonNullApi;
 import org.gradle.initialization.BuildCancellationToken;
 import org.gradle.internal.classpath.ClassPath;
 import org.gradle.internal.concurrent.CompositeStoppable;
 import org.gradle.internal.logging.progress.ProgressLoggerFactory;
+import org.gradle.tooling.GradleConnectionException;
 import org.gradle.tooling.internal.consumer.ConnectionParameters;
 import org.gradle.tooling.internal.consumer.Distribution;
 import org.gradle.tooling.internal.consumer.connection.ConsumerConnection;
 import org.gradle.tooling.internal.protocol.InternalBuildProgressListener;
 
 import java.io.Closeable;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
 public class CachingToolingImplementationLoader implements ToolingImplementationLoader, Closeable {
     private final ToolingImplementationLoader loader;
-    private final Map<ClassPath, ConsumerConnection> connections = new HashMap<ClassPath, ConsumerConnection>();
+    private final Cache<ClassPath, ConsumerConnection> connections = CacheBuilder.newBuilder()
+        .maximumSize(15)
+        .build();
 
     public CachingToolingImplementationLoader(ToolingImplementationLoader loader) {
         this.loader = loader;
     }
 
     @Override
-    public ConsumerConnection create(Distribution distribution, ProgressLoggerFactory progressLoggerFactory, InternalBuildProgressListener progressListener, ConnectionParameters connectionParameters, BuildCancellationToken cancellationToken) {
+    public ConsumerConnection create(final Distribution distribution, final ProgressLoggerFactory progressLoggerFactory, final InternalBuildProgressListener progressListener, final ConnectionParameters connectionParameters, final BuildCancellationToken cancellationToken) {
         ClassPath classpath = distribution.getToolingImplementationClasspath(progressLoggerFactory, progressListener, connectionParameters, cancellationToken);
 
-        ConsumerConnection connection = connections.get(classpath);
-        if (connection == null) {
-            connection = loader.create(distribution, progressLoggerFactory, progressListener, connectionParameters, cancellationToken);
-            connections.put(classpath, connection);
+        try {
+            return connections.get(classpath, new ConsumerConnectionCreator(distribution, progressLoggerFactory, progressListener, connectionParameters, cancellationToken));
+        } catch (ExecutionException e) {
+            throw new GradleConnectionException(String.format("Could not create an instance of Tooling API implementation using the specified %s.", distribution.getDisplayName()), e);
         }
-
-        return connection;
     }
 
     @Override
     public void close() {
         try {
-            CompositeStoppable.stoppable(connections.values()).stop();
+            CompositeStoppable.stoppable(connections.asMap().values()).stop();
         } finally {
-            connections.clear();
+            connections.invalidateAll();
         }
+    }
+
+    @NonNullApi
+    private class ConsumerConnectionCreator implements Callable<ConsumerConnection> {
+        private final Distribution distribution;
+        private final ProgressLoggerFactory progressLoggerFactory;
+        private final InternalBuildProgressListener progressListener;
+        private final ConnectionParameters connectionParameters;
+        private final BuildCancellationToken cancellationToken;
+
+        public ConsumerConnectionCreator(Distribution distribution, ProgressLoggerFactory progressLoggerFactory, InternalBuildProgressListener progressListener, ConnectionParameters connectionParameters, BuildCancellationToken cancellationToken) {
+            this.distribution = distribution;
+            this.progressLoggerFactory = progressLoggerFactory;
+            this.progressListener = progressListener;
+            this.connectionParameters = connectionParameters;
+            this.cancellationToken = cancellationToken;
+        }
+
+        @Override
+        public ConsumerConnection call() throws Exception {
+            return loader.create(distribution, progressLoggerFactory, progressListener, connectionParameters, cancellationToken);
+        }
+
     }
 }
