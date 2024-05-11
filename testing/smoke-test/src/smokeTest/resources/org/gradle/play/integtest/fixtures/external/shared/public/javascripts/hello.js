4e85e4d9edf6aaf37a23b1f4baef880716e51c5e
Microondas <143319861+microwavedd@users.noreply.github.com>
2024-05-11 18:46:37 +0200

    Check for console access
    Signed-off-by: Microondas <143319861+microwavedd@users.noreply.github.com>

--- testing/smoke-test/src/smokeTest/resources/org/gradle/play/integtest/fixtures/external/shared/public/javascripts/hello.js
+++ testing/smoke-test/src/smokeTest/resources/org/gradle/play/integtest/fixtures/external/shared/public/javascripts/hello.js
@@ -1,3 +1,8 @@
-if (window.console) {
-    console.log("Welcome to your Play application's JavaScript!");
+// Check if the console object exists and if it supports the log method
+if (window.console && console.log) {
+  console.log("Welcome to your Play application's JavaScript!");
+} else {
+  // Fallback if console is not available
+  alert("Welcome to your Play application's JavaScript!");
 }
+//Useful for older browsers, headless browsers, browser extensions and more
