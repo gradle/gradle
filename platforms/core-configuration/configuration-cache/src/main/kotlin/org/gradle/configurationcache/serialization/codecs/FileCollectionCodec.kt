e5cb9a1b497c61921f1e189d5ae799086e37544f
Sergey Opivalov <sopivalov@gradle.com>
2024-05-21 14:26:49 +0300

    Start using `FileCollectionExecutionTimeValue` in `FileCollectionCodec`

--- platforms/core-configuration/configuration-cache/src/main/kotlin/org/gradle/configurationcache/serialization/codecs/FileCollectionCodec.kt
+++ platforms/core-configuration/configuration-cache/src/main/kotlin/org/gradle/configurationcache/serialization/codecs/FileCollectionCodec.kt
@@ -21,6 +21,7 @@ import org.gradle.api.file.FileTree
 import org.gradle.api.internal.artifacts.configurations.ResolutionBackedFileCollection
 import org.gradle.api.internal.artifacts.ivyservice.resolveengine.artifact.ArtifactSetToFileCollectionFactory
 import org.gradle.api.internal.artifacts.transform.TransformedArtifactSet
+import org.gradle.api.internal.file.FileCollectionExecutionTimeValue
 import org.gradle.api.internal.file.FileCollectionFactory
 import org.gradle.api.internal.file.FileCollectionInternal
 import org.gradle.api.internal.file.FileCollectionStructureVisitor
@@ -40,8 +41,10 @@ import org.gradle.internal.serialize.graph.WriteContext
 import org.gradle.internal.serialize.graph.decodePreservingIdentity
 import org.gradle.internal.serialize.graph.encodePreservingIdentityOf
 import org.gradle.internal.serialize.graph.readList
+import org.gradle.configurationcache.serialization.readNonNull
 import org.gradle.internal.serialize.graph.writeCollection
 import java.io.File
+import kotlin.jvm.optionals.getOrNull
 
 
 internal
@@ -57,9 +60,16 @@ class FileCollectionCodec(
     }
 
     suspend fun WriteContext.encodeContents(value: FileCollectionInternal) {
-        val visitor = CollectingVisitor()
-        value.visitStructure(visitor)
-        writeCollection(visitor.elements)
+        val executionTimeValue = value.calculateExecutionTimeValue().getOrNull()
+        if (executionTimeValue != null) {
+            writeBoolean(true)
+            write(executionTimeValue)
+        } else {
+            writeBoolean(false)
+            val visitor = CollectingVisitor()
+            value.visitStructure(visitor)
+            writeCollection(visitor.elements)
+        }
     }
 
     override suspend fun ReadContext.decode(): FileCollectionInternal {
@@ -70,8 +80,10 @@ class FileCollectionCodec(
         }
     }
 
-    suspend fun ReadContext.decodeContents(): FileCollectionInternal {
-        return fileCollectionFactory.resolving(
+    suspend fun ReadContext.decodeContents(): FileCollectionInternal = if (readBoolean()) {
+        readNonNull<FileCollectionExecutionTimeValue>().toFileCollection(fileCollectionFactory)
+    } else {
+        fileCollectionFactory.resolving(
             readList().map { element ->
                 when (element) {
                     is File -> element
