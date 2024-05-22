61d1be7d90c25bffb03a1f5240ef32831450542d
Lóránt Pintér <lorant@gradle.com>
2024-05-22 10:59:18 +0200

    Fix :workers and :worker-processes dependencies

--- platforms/core-runtime/worker-services/build.gradle.kts
+++ platforms/core-runtime/worker-services/build.gradle.kts
@@ -5,7 +5,3 @@ plugins {
 description = "A set of generic services and utilities to be used form workers. The reason to separate these from :base-services is because workers have a different Java requirement."
 
 gradlebuildJava.usedInWorkers()
-
-dependencies {
-    implementation(projects.javaLanguageExtensions)
-}
