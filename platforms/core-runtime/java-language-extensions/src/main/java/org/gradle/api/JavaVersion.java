c582e639b7aa71131bd913041bd4ac7149660817
Bálint Hegyi <bhegyi@gradle.com>
2024-05-27 14:03:03 +0100

    Add incubating and documentation

--- platforms/core-runtime/java-language-extensions/src/main/java/org/gradle/api/JavaVersion.java
+++ platforms/core-runtime/java-language-extensions/src/main/java/org/gradle/api/JavaVersion.java
@@ -322,15 +322,23 @@ public enum JavaVersion {
         return versionName;
     }
 
+    /**
+     * Returns the major version number as a {@link String}
+     *
+     * @see #getMajorVersionNumber() for the integer value
+     * @since 1.8
+     */
     public String getMajorVersion() {
         return String.valueOf(getMajorVersionNumber());
     }
 
     /**
-     * Returns the major version number.
+     * Returns the major version number
      *
+     * @see #getMajorVersion() for the string value
      * @since 8.9
      */
+    @Incubating
     public int getMajorVersionNumber() {
         return ordinal() + 1;
     }
