7fa536674376b507881ace57198bf96fbcc83251
Sterling Greene <sterling@gradle.com>
2024-06-07 17:24:29 -0400

    Use Property<Integer> for new TestNG option

--- platforms/jvm/testing-jvm/src/main/java/org/gradle/api/tasks/testing/testng/TestNGOptions.java
+++ platforms/jvm/testing-jvm/src/main/java/org/gradle/api/tasks/testing/testng/TestNGOptions.java
@@ -22,6 +22,7 @@ import groovy.xml.MarkupBuilder;
 import org.gradle.api.Incubating;
 import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.internal.tasks.testing.testng.TestNGTestClassProcessor;
+import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Internal;
@@ -70,8 +71,6 @@ public abstract class TestNGOptions extends TestFrameworkOptions {
 
     private int threadCount = DEFAULT_THREAD_COUNT;
 
-    private int suiteThreadPoolSize = DEFAULT_SUITE_THREAD_POOL_SIZE_DEFAULT;
-
     private boolean useDefaultListeners;
 
     private String threadPoolFactoryClass;
@@ -102,6 +101,7 @@ public abstract class TestNGOptions extends TestFrameworkOptions {
     @Inject
     public TestNGOptions(ProjectLayout projectLayout) {
         this.projectDir = projectLayout.getProjectDirectory().getAsFile();
+        this.getSuiteThreadPoolSize().convention(DEFAULT_SUITE_THREAD_POOL_SIZE_DEFAULT);
     }
 
     /**
@@ -116,7 +116,7 @@ public abstract class TestNGOptions extends TestFrameworkOptions {
         replace(this.listeners, other.listeners);
         this.parallel = other.parallel;
         this.threadCount = other.threadCount;
-        this.suiteThreadPoolSize = other.suiteThreadPoolSize;
+        this.getSuiteThreadPoolSize().set(other.getSuiteThreadPoolSize());
         this.useDefaultListeners = other.useDefaultListeners;
         this.threadPoolFactoryClass = other.threadPoolFactoryClass;
         this.suiteName = other.suiteName;
@@ -338,18 +338,7 @@ public abstract class TestNGOptions extends TestFrameworkOptions {
      */
     @Internal
     @Incubating
-    public int getSuiteThreadPoolSize() {
-        return suiteThreadPoolSize;
-    }
-
-    /**
-     * Sets a custom number of XML suites will run parallel.
-     * @since 8.9
-     */
-    @Incubating
-    public void setSuiteThreadPoolSize(int suiteThreadPoolSize) {
-        this.suiteThreadPoolSize = suiteThreadPoolSize;
-    }
+    public abstract Property<Integer> getSuiteThreadPoolSize();
 
     @Internal
     public boolean getUseDefaultListeners() {
