36a28fc58c3d990bf0803e163fb0f5efe2048296
Lóránt Pintér <lorant@gradle.com>
2024-06-10 21:40:03 +0200

    Fix dependency issues with Guava

--- build-logic/api-extractor/build.gradle.kts
+++ build-logic/api-extractor/build.gradle.kts
@@ -7,5 +7,10 @@ description = "Extract API classes from JVM classes."
 dependencies {
     implementation(platform("gradlebuild:build-platform"))
     implementation("org.ow2.asm:asm-tree")
-    implementation("com.google.guava:guava")
+    implementation("com.google.guava:guava") {
+        // Used for its nullability annotations, not needed at runtime
+        exclude("org.checkerframework", "checker-qual")
+        exclude("com.google.errorprone", "error_prone_annotations")
+        exclude("com.google.guava", "listenablefuture")
+    }
 }
