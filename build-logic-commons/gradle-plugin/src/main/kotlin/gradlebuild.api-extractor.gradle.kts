293c3edc19838fde8ec791578b3ab44b4c632966
Lóránt Pintér <lorant@gradle.com>
2024-06-12 15:55:33 +0200

    Remove commented-out code

--- build-logic-commons/gradle-plugin/src/main/kotlin/gradlebuild.api-extractor.gradle.kts
+++ build-logic-commons/gradle-plugin/src/main/kotlin/gradlebuild.api-extractor.gradle.kts
@@ -4,17 +4,11 @@ plugins {
     `java-library`
     groovy
 
-//    id("gradlebuild.arch-test")
-//    id("gradlebuild.ci-lifecycle")
     id("gradlebuild.ci-reporting")
-//    id("gradlebuild.configure-ci-artifacts") // CI: Prepare reports to be uploaded to TeamCity
     id("gradlebuild.code-quality")
-//    id("gradlebuild.dependency-modules")
     id("gradlebuild.module-jar")
     id("gradlebuild.repositories")
     id("gradlebuild.reproducible-archives")
-    // TODO Move this to build-logic-commons?
-//    id("gradlebuild.strict-compile")
 }
 
 java {
