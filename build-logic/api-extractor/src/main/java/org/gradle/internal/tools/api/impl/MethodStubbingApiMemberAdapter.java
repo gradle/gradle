7b7f8f47ec54cf03917d5695e0f9f2010db2bcd7
Lóránt Pintér <lorant@gradle.com>
2024-06-13 13:08:02 +0200

    Remove useless method

--- build-logic/api-extractor/src/main/java/org/gradle/internal/tools/api/impl/MethodStubbingApiMemberAdapter.java
+++ build-logic/api-extractor/src/main/java/org/gradle/internal/tools/api/impl/MethodStubbingApiMemberAdapter.java
@@ -50,11 +50,6 @@ public class MethodStubbingApiMemberAdapter extends ClassVisitor {
     }
 
     @Override
-    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-        super.visit(version, access, name, signature, superName, interfaces);
-    }
-
-    @Override
     public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
         MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
         if ((access & ACC_ABSTRACT) == 0) {
