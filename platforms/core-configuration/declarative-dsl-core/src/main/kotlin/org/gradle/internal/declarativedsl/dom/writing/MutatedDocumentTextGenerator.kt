vimanthan3/wed03

Support replacing values in mutations

platforms/core-configuration/declarative-dsl-core/src/main/kotlin/org/gradle/internal/declarativedsl/dom/writing/MutatedDocumentTextGenerator.kt
@@ -33,7 +33,8 @@ class MutatedDocumentTextGenerator {
         mapNames: (ownerTag: ChildTag, name: String) -> String = { _, name -> name },
         removeNodeIf: (ownerTag: ChildTag) -> Boolean = { false },
         insertNodesBefore: (ownerTag: ChildTag) -> List<DeclarativeDocument.DocumentNode> = { emptyList() },
-        insertNodesAfter: (ownerTag: ChildTag) -> List<DeclarativeDocument.DocumentNode> = { emptyList() }
+        insertNodesAfter: (ownerTag: ChildTag) -> List<DeclarativeDocument.DocumentNode> = { emptyList() },
+        replaceValue: (ownerTag: ChildTag.ValueNodeTag) -> DeclarativeDocument.ValueNode? = { null },
     ): String {
         val textBuilder = TrackingCodeTextBuilder()
 
@@ -47,12 +48,15 @@ class MutatedDocumentTextGenerator {
                     ChildTag.Indentation -> textBuilder.appendIndent(tree.originalText.slice(textTreeNode.range))
                     ChildTag.UnstructuredText, ChildTag.LineBreak -> textBuilder.append(tree.originalText.substring(textTreeNode.range), textTreeNode.lineRange.last)
 
-                    ChildTag.AssignmentRhs,
+                    is ChildTag.AssignmentRhs,
                     is ChildTag.BlockElement,
                     is ChildTag.CallArgument -> {
-                        for (child in textTreeNode.children) {
-                            visit(child.childTag, child.subTreeNode, false)
-                        }
+                        (ownerTag as? ChildTag.ValueNodeTag)?.let(replaceValue)?.let { insertSyntheticValue(textBuilder, it, textTreeNode.lineRange.last) }
+                            ?: run {
+                                for (child in textTreeNode.children) {
+                                    visit(child.childTag, child.subTreeNode, false)
+                                }
+                            }
                     }
                 }
             }
@@ -108,6 +112,15 @@ class MutatedDocumentTextGenerator {
         if (needsSeparationAfter) {
             lineBreakIndent()
         }
+    }
+
+    private
+    fun insertSyntheticValue(
+        textBuilder: TrackingCodeTextBuilder,
+        syntheticValue: DeclarativeDocument.ValueNode,
+        endAtOriginalLine: Int
+    ) {
+        textBuilder.append(canonicalCodeGenerator.valueNodeString(syntheticValue), endAtOriginalLine)
     }
 
     private

platforms/core-configuration/declarative-dsl-core/src/main/kotlin/org/gradle/internal/declarativedsl/dom/writing/TextPreservingTree.kt
@@ -42,9 +42,13 @@ class TextPreservingTree internal constructor(
     class SubTreeData(val childTag: ChildTag, val subTreeNode: TextTreeNode)
 
     sealed interface ChildTag {
+        sealed interface ValueNodeTag : ChildTag {
+            val valueNode: DeclarativeDocument.ValueNode
+        }
+
         data object Name : ChildTag
-        data object AssignmentRhs : ChildTag
-        data class CallArgument(val index: Int, val valueNode: DeclarativeDocument.ValueNode) : ChildTag
+        data class AssignmentRhs(override val valueNode: DeclarativeDocument.ValueNode) : ValueNodeTag
+        data class CallArgument(val index: Int, override val valueNode: DeclarativeDocument.ValueNode) : ValueNodeTag
         data class BlockElement(val index: Int, val documentNode: DeclarativeDocument.DocumentNode) : ChildTag
 
         // These do not represent parts of the DOM but serve for preserving the original formatting and comments
@@ -99,7 +103,7 @@ class TextPreservingTreeBuilder {
             is DeclarativeDocument.DocumentNode.PropertyNode -> {
                 listOf(
                     SubTreeData(ChildTag.Name, TextTreeNode(node.sourceData.nameRange(node.name), node.lines, emptyList())),
-                    SubTreeData(ChildTag.AssignmentRhs, nodeForValueNode(node.value))
+                    SubTreeData(ChildTag.AssignmentRhs(node.value), nodeForValueNode(node.value))
                 )
             }
 

gradle/gradle

platforms/core-configuration/declarative-dsl-core/src/main/kotlin/org/gradle/internal/declarativedsl/dom/writing/MutatedDocumentTextGenerator.kt
@@ -33,7 +33,8 @@ class MutatedDocumentTextGenerator {
         mapNames: (ownerTag: ChildTag, name: String) -> String = { _, name -> name },
         removeNodeIf: (ownerTag: ChildTag) -> Boolean = { false },
         insertNodesBefore: (ownerTag: ChildTag) -> List<DeclarativeDocument.DocumentNode> = { emptyList() },
-        insertNodesAfter: (ownerTag: ChildTag) -> List<DeclarativeDocument.DocumentNode> = { emptyList() }
+        insertNodesAfter: (ownerTag: ChildTag) -> List<DeclarativeDocument.DocumentNode> = { emptyList() },
+        replaceValue: (ownerTag: ChildTag.ValueNodeChildTag) -> DeclarativeDocument.ValueNode? = { null },
     ): String {
         val textBuilder = TrackingCodeTextBuilder()
 
@@ -47,12 +48,15 @@ class MutatedDocumentTextGenerator {
                     ChildTag.Indentation -> textBuilder.appendIndent(tree.originalText.slice(textTreeNode.range))
                     ChildTag.UnstructuredText, ChildTag.LineBreak -> textBuilder.append(tree.originalText.substring(textTreeNode.range), textTreeNode.lineRange.last)
 
-                    ChildTag.AssignmentRhs,
+                    is ChildTag.AssignmentRhs,
                     is ChildTag.BlockElement,
                     is ChildTag.CallArgument -> {
-                        for (child in textTreeNode.children) {
-                            visit(child.childTag, child.subTreeNode, false)
-                        }
+                        (ownerTag as? ChildTag.ValueNodeChildTag)?.let(replaceValue)?.let { insertSyntheticValue(textBuilder, it, textTreeNode.lineRange.last) }
+                            ?: run {
+                                for (child in textTreeNode.children) {
+                                    visit(child.childTag, child.subTreeNode, false)
+                                }
+                            }
                     }
                 }
             }
@@ -108,6 +112,15 @@ class MutatedDocumentTextGenerator {
         if (needsSeparationAfter) {
             lineBreakIndent()
         }
+    }
+
+    private
+    fun insertSyntheticValue(
+        textBuilder: TrackingCodeTextBuilder,
+        syntheticValue: DeclarativeDocument.ValueNode,
+        endAtOriginalLine: Int
+    ) {
+        textBuilder.append(canonicalCodeGenerator.valueNodeString(syntheticValue), endAtOriginalLine)
     }
 
     private

platforms/core-configuration/declarative-dsl-core/src/main/kotlin/org/gradle/internal/declarativedsl/dom/writing/TextPreservingTree.kt
@@ -42,9 +42,13 @@ class TextPreservingTree internal constructor(
     class SubTreeData(val childTag: ChildTag, val subTreeNode: TextTreeNode)
 
     sealed interface ChildTag {
+        sealed interface ValueNodeChildTag : ChildTag {
+            val valueNode: DeclarativeDocument.ValueNode
+        }
+
         data object Name : ChildTag
-        data object AssignmentRhs : ChildTag
-        data class CallArgument(val index: Int, val valueNode: DeclarativeDocument.ValueNode) : ChildTag
+        data class AssignmentRhs(override val valueNode: DeclarativeDocument.ValueNode) : ValueNodeChildTag
+        data class CallArgument(val index: Int, override val valueNode: DeclarativeDocument.ValueNode) : ValueNodeChildTag
         data class BlockElement(val index: Int, val documentNode: DeclarativeDocument.DocumentNode) : ChildTag
 
         // These do not represent parts of the DOM but serve for preserving the original formatting and comments
@@ -99,7 +103,7 @@ class TextPreservingTreeBuilder {
             is DeclarativeDocument.DocumentNode.PropertyNode -> {
                 listOf(
                     SubTreeData(ChildTag.Name, TextTreeNode(node.sourceData.nameRange(node.name), node.lines, emptyList())),
-                    SubTreeData(ChildTag.AssignmentRhs, nodeForValueNode(node.value))
+                    SubTreeData(ChildTag.AssignmentRhs(node.value), nodeForValueNode(node.value))
                 )
             }
 