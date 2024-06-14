vimanthan3/wed03

Allow to define vendor for the Daemon JVM criteria
Signed-off-by: Madalin Valceleanu <vmadalin@google.com>

platforms/core-runtime/launcher/src/main/java/org/gradle/launcher/daemon/configuration/DaemonParameters.java
@@ -21,8 +21,9 @@ import org.gradle.api.internal.file.FileCollectionFactory;
 import org.gradle.internal.buildconfiguration.DaemonJvmPropertiesDefaults;
 import org.gradle.internal.jvm.JavaInfo;
 import org.gradle.internal.jvm.JpmsConfiguration;
+import org.gradle.internal.jvm.inspection.JvmVendor;
 import org.gradle.internal.nativeintegration.services.NativeServices.NativeServicesMode;
-import org.gradle.jvm.toolchain.JvmImplementation;
+import org.gradle.jvm.toolchain.JvmVendorSpec;
 import org.gradle.jvm.toolchain.internal.DefaultJvmVendorSpec;
 import org.gradle.jvm.toolchain.internal.DefaultToolchainConfiguration;
 import org.gradle.jvm.toolchain.internal.ToolchainConfiguration;
@@ -32,12 +33,15 @@ import org.gradle.util.internal.GUtil;
 
 import javax.annotation.Nullable;
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import static org.gradle.util.internal.GUtil.toEnum;
 
 public class DaemonParameters {
     static final int DEFAULT_IDLE_TIMEOUT = 3 * 60 * 60 * 1000;
@@ -131,13 +135,27 @@ public class DaemonParameters {
     public void setRequestedJvmCriteria(@Nullable Map<String, String> buildProperties) {
         String requestedVersion = buildProperties.get(DaemonJvmPropertiesDefaults.TOOLCHAIN_VERSION_PROPERTY);
         if (requestedVersion != null) {
+            JavaVersion javaVersion;
             try {
-                JavaVersion javaVersion = JavaVersion.toVersion(requestedVersion);
-                this.requestedJvmCriteria = new DaemonJvmCriteria(javaVersion, DefaultJvmVendorSpec.any(), JvmImplementation.VENDOR_SPECIFIC);
+                javaVersion = JavaVersion.toVersion(requestedVersion);
             } catch (Exception e) {
                 // TODO: This should be pushed somewhere else so we consistently report this message in the right context.
                 throw new IllegalArgumentException(String.format("Value '%s' given for %s is an invalid Java version", requestedVersion, DaemonJvmPropertiesDefaults.TOOLCHAIN_VERSION_PROPERTY));
             }
+
+            JvmVendorSpec javaVendor = DefaultJvmVendorSpec.any();
+            String requestedVendor = buildProperties.get(DaemonJvmPropertiesDefaults.TOOLCHAIN_VENDOR_PROPERTY);
+            try {
+                if (requestedVendor != null) {
+                    JvmVendor.KnownJvmVendor knownJvmVendor = toEnum(JvmVendor.KnownJvmVendor.class, requestedVendor);
+                    javaVendor = DefaultJvmVendorSpec.of(knownJvmVendor);
+                }
+            } catch (Exception e) {
+                // TODO: This should be pushed somewhere else so we consistently report this message in the right context.
+                throw new IllegalArgumentException(String.format("Value '%s' given for %s is an invalid Java vendor. Possible values are %s", requestedVendor, DaemonJvmPropertiesDefaults.TOOLCHAIN_VENDOR_PROPERTY, Arrays.toString(JvmVendor.KnownJvmVendor.values())));
+            }
+
+            this.requestedJvmCriteria = new DaemonJvmCriteria(javaVersion, javaVendor);
         }
     }
 

platforms/core-runtime/launcher/src/main/java/org/gradle/launcher/daemon/context/DefaultDaemonContext.java
@@ -18,6 +18,7 @@ package org.gradle.launcher.daemon.context;
 import com.google.common.base.Joiner;
 import org.gradle.api.JavaVersion;
 import org.gradle.internal.jvm.Jvm;
+import org.gradle.internal.jvm.inspection.JvmVendor;
 import org.gradle.internal.nativeintegration.services.NativeServices.NativeServicesMode;
 import org.gradle.internal.serialize.Decoder;
 import org.gradle.internal.serialize.Encoder;
@@ -45,11 +46,13 @@ public class DefaultDaemonContext implements DaemonContext {
     private final DaemonParameters.Priority priority;
     private final NativeServicesMode nativeServicesMode;
     private final JavaVersion javaVersion;
+    private final JvmVendor.KnownJvmVendor javaVendor;
 
     public DefaultDaemonContext(
         String uid,
         File javaHome,
         JavaVersion javaVersion,
+        JvmVendor.KnownJvmVendor javaVendor,
         File daemonRegistryDir,
         Long pid,
         Integer idleTimeout,
@@ -68,6 +71,7 @@ public class DefaultDaemonContext implements DaemonContext {
         this.applyInstrumentationAgent = applyInstrumentationAgent;
         this.priority = priority;
         this.nativeServicesMode = nativeServicesMode;
+        this.javaVendor = javaVendor;
     }
 
     @Override
@@ -90,6 +94,11 @@ public class DefaultDaemonContext implements DaemonContext {
     @Override
     public JavaVersion getJavaVersion() {
         return javaVersion;
+    }
+
+    @Override
+    public JvmVendor getJavaVendor() {
+        return javaVendor.asJvmVendor();
     }
 
     @Override
@@ -140,6 +149,7 @@ public class DefaultDaemonContext implements DaemonContext {
             String pathname = decoder.readString();
             File javaHome = new File(pathname);
             JavaVersion javaVersion = JavaVersion.valueOf(decoder.readString());
+            JvmVendor.KnownJvmVendor javaVendor = JvmVendor.KnownJvmVendor.values()[decoder.readInt()];
             File registryDir = new File(decoder.readString());
             Long pid = decoder.readBoolean() ? decoder.readLong() : null;
             Integer idle = decoder.readBoolean() ? decoder.readInt() : null;
@@ -152,7 +162,7 @@ public class DefaultDaemonContext implements DaemonContext {
             NativeServicesMode nativeServicesMode = NativeServicesMode.values()[decoder.readSmallInt()];
             DaemonParameters.Priority priority = decoder.readBoolean() ? DaemonParameters.Priority.values()[decoder.readInt()] : null;
 
-            return new DefaultDaemonContext(uid, javaHome, javaVersion, registryDir, pid, idle, daemonOpts, applyInstrumentationAgent, nativeServicesMode, priority);
+            return new DefaultDaemonContext(uid, javaHome, javaVersion, javaVendor, registryDir, pid, idle, daemonOpts, applyInstrumentationAgent, nativeServicesMode, priority);
         }
 
         @Override
@@ -160,6 +170,7 @@ public class DefaultDaemonContext implements DaemonContext {
             encoder.writeNullableString(context.uid);
             encoder.writeString(context.javaHome.getPath());
             encoder.writeString(context.javaVersion.name());
+            encoder.writeInt(context.javaVendor.ordinal());
             encoder.writeString(context.daemonRegistryDir.getPath());
             encoder.writeBoolean(context.pid != null);
             if (context.pid != null) {

platforms/core-runtime/launcher/src/main/java/org/gradle/launcher/daemon/toolchain/DaemonJvmCriteria.java
@@ -17,7 +17,7 @@
 package org.gradle.launcher.daemon.toolchain;
 
 import org.gradle.api.JavaVersion;
-import org.gradle.internal.jvm.Jvm;
+import org.gradle.internal.jvm.inspection.JvmVendor;
 import org.gradle.jvm.toolchain.JvmImplementation;
 import org.gradle.jvm.toolchain.JvmVendorSpec;
 
@@ -25,6 +25,10 @@ public class DaemonJvmCriteria {
     private final JavaVersion javaVersion;
     private final JvmVendorSpec vendorSpec;
     private final JvmImplementation jvmImplementation;
+
+    public DaemonJvmCriteria(JavaVersion javaVersion, JvmVendorSpec vendorSpec) {
+        this(javaVersion, vendorSpec, JvmImplementation.VENDOR_SPECIFIC);
+    }
 
     public DaemonJvmCriteria(JavaVersion javaVersion, JvmVendorSpec vendorSpec, JvmImplementation jvmImplementation) {
         this.javaVersion = javaVersion;
@@ -44,18 +48,12 @@ public class DaemonJvmCriteria {
         return jvmImplementation;
     }
 
-    public boolean isCompatibleWith(Jvm other) {
-        return isCompatibleWith(other.getJavaVersion());
-    }
-
     @Override
     public String toString() {
-        // TODO: Include vendor and implementation
-        return String.format("JVM version '%s'", getJavaVersion());
+        return String.format("JVM version '%s' vendor '%s'", getJavaVersion(), getVendorSpec());
     }
 
-    public boolean isCompatibleWith(JavaVersion javaVersion) {
-        // TODO: Implement comparisons for vendorSpec and jvmImplementation
-        return javaVersion == getJavaVersion(); // && vendorSpec.matches() && jvmImplementation == other.jvmImplementation;
+    public boolean isCompatibleWith(JavaVersion javaVersion, JvmVendor javaVendor) {
+        return javaVersion == getJavaVersion() && vendorSpec.matches(javaVendor.getRawVendor());
     }
 }

platforms/core-runtime/launcher/src/test/groovy/org/gradle/launcher/daemon/server/DaemonRegistryUnavailableExpirationStrategyTest.groovy
@@ -44,7 +44,7 @@ class DaemonRegistryUnavailableExpirationStrategyTest extends Specification {
     def "daemon should expire when registry file is unreachable"() {
         given:
         DaemonRegistryUnavailableExpirationStrategy expirationStrategy = new DaemonRegistryUnavailableExpirationStrategy(daemon)
-        DaemonContext daemonContext = new DefaultDaemonContext("user", null, JavaVersion.current(), tempDir.file("BOGUS"), 51234L, 10000, [] as List<String>, false, NativeServicesMode.ENABLED, DaemonParameters.Priority.NORMAL)
+        DaemonContext daemonContext = new DefaultDaemonContext("user", null, JavaVersion.current(), null, tempDir.file("BOGUS"), 51234L, 10000, [] as List<String>, false, NativeServicesMode.ENABLED, DaemonParameters.Priority.NORMAL)
 
         when:
         1 * daemon.getDaemonContext() >> { daemonContext }
@@ -62,7 +62,7 @@ class DaemonRegistryUnavailableExpirationStrategyTest extends Specification {
                 return "DAEMON_ADDRESS"
             }
         }
-        DaemonContext daemonContext = new DefaultDaemonContext("user", null, JavaVersion.current(), daemonDir, 51234L, 10000, [] as List<String>, false, NativeServicesMode.ENABLED, DaemonParameters.Priority.NORMAL)
+        DaemonContext daemonContext = new DefaultDaemonContext("user", null, JavaVersion.current(), null, daemonDir, 51234L, 10000, [] as List<String>, false, NativeServicesMode.ENABLED, DaemonParameters.Priority.NORMAL)
         DaemonDir daemonDir = new DaemonDir(daemonDir)
         DaemonRegistry registry = new EmbeddedDaemonRegistry()
         daemonDir.getRegistry().createNewFile()

gradle/gradle

platforms/core-runtime/launcher/src/main/java/org/gradle/launcher/daemon/configuration/DaemonParameters.java
@@ -20,10 +20,11 @@ import org.gradle.api.internal.file.FileCollectionFactory;
 import org.gradle.internal.buildconfiguration.DaemonJvmPropertiesDefaults;
 import org.gradle.internal.jvm.JavaInfo;
 import org.gradle.internal.jvm.JpmsConfiguration;
+import org.gradle.internal.jvm.inspection.JvmVendor;
 import org.gradle.internal.nativeintegration.services.NativeServices.NativeServicesMode;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
-import org.gradle.jvm.toolchain.JvmImplementation;
 import org.gradle.jvm.toolchain.internal.DefaultJavaLanguageVersion;
+import org.gradle.jvm.toolchain.JvmVendorSpec;
 import org.gradle.jvm.toolchain.internal.DefaultJvmVendorSpec;
 import org.gradle.jvm.toolchain.internal.DefaultToolchainConfiguration;
 import org.gradle.jvm.toolchain.internal.ToolchainConfiguration;
@@ -33,12 +34,15 @@ import org.gradle.util.internal.GUtil;
 
 import javax.annotation.Nullable;
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import static org.gradle.util.internal.GUtil.toEnum;
 
 public class DaemonParameters {
     static final int DEFAULT_IDLE_TIMEOUT = 3 * 60 * 60 * 1000;
@@ -132,13 +136,27 @@ public class DaemonParameters {
     public void setRequestedJvmCriteria(@Nullable Map<String, String> buildProperties) {
         String requestedVersion = buildProperties.get(DaemonJvmPropertiesDefaults.TOOLCHAIN_VERSION_PROPERTY);
         if (requestedVersion != null) {
+            JavaLanguageVersion javaVersion;
             try {
-                JavaLanguageVersion javaVersion = DefaultJavaLanguageVersion.fromFullVersion(requestedVersion);
-                this.requestedJvmCriteria = new DaemonJvmCriteria(javaVersion, DefaultJvmVendorSpec.any(), JvmImplementation.VENDOR_SPECIFIC);
+                javaVersion = DefaultJavaLanguageVersion.fromFullVersion(requestedVersion);
             } catch (Exception e) {
                 // TODO: This should be pushed somewhere else so we consistently report this message in the right context.
                 throw new IllegalArgumentException(String.format("Value '%s' given for %s is an invalid Java version", requestedVersion, DaemonJvmPropertiesDefaults.TOOLCHAIN_VERSION_PROPERTY));
             }
+
+            JvmVendorSpec javaVendor = DefaultJvmVendorSpec.any();
+            String requestedVendor = buildProperties.get(DaemonJvmPropertiesDefaults.TOOLCHAIN_VENDOR_PROPERTY);
+            try {
+                if (requestedVendor != null) {
+                    JvmVendor.KnownJvmVendor knownJvmVendor = toEnum(JvmVendor.KnownJvmVendor.class, requestedVendor);
+                    javaVendor = DefaultJvmVendorSpec.of(knownJvmVendor);
+                }
+            } catch (Exception e) {
+                // TODO: This should be pushed somewhere else so we consistently report this message in the right context.
+                throw new IllegalArgumentException(String.format("Value '%s' given for %s is an invalid Java vendor. Possible values are %s", requestedVendor, DaemonJvmPropertiesDefaults.TOOLCHAIN_VENDOR_PROPERTY, Arrays.toString(JvmVendor.KnownJvmVendor.values())));
+            }
+
+            this.requestedJvmCriteria = new DaemonJvmCriteria(javaVersion, javaVendor);
         }
     }
 

platforms/core-runtime/launcher/src/main/java/org/gradle/launcher/daemon/context/DefaultDaemonContext.java
@@ -17,6 +17,7 @@ package org.gradle.launcher.daemon.context;
 
 import com.google.common.base.Joiner;
 import org.gradle.internal.jvm.Jvm;
+import org.gradle.internal.jvm.inspection.JvmVendor;
 import org.gradle.internal.nativeintegration.services.NativeServices.NativeServicesMode;
 import org.gradle.internal.serialize.Decoder;
 import org.gradle.internal.serialize.Encoder;
@@ -45,11 +46,13 @@ public class DefaultDaemonContext implements DaemonContext {
     private final DaemonParameters.Priority priority;
     private final NativeServicesMode nativeServicesMode;
     private final JavaLanguageVersion javaVersion;
+    private final JvmVendor.KnownJvmVendor javaVendor;
 
     public DefaultDaemonContext(
         String uid,
         File javaHome,
         JavaLanguageVersion javaVersion,
+        JvmVendor.KnownJvmVendor javaVendor,
         File daemonRegistryDir,
         Long pid,
         Integer idleTimeout,
@@ -68,13 +71,14 @@ public class DefaultDaemonContext implements DaemonContext {
         this.applyInstrumentationAgent = applyInstrumentationAgent;
         this.priority = priority;
         this.nativeServicesMode = nativeServicesMode;
+        this.javaVendor = javaVendor;
     }
 
     @Override
     public String toString() {
         // Changes to this also affect org.gradle.integtests.fixtures.daemon.DaemonContextParser
-        return String.format("DefaultDaemonContext[uid=%s,javaHome=%s,javaVersion=%s,daemonRegistryDir=%s,pid=%s,idleTimeout=%s,priority=%s,applyInstrumentationAgent=%s,nativeServicesMode=%s,daemonOpts=%s]",
-            uid, javaHome, javaVersion, daemonRegistryDir, pid, idleTimeout, priority, applyInstrumentationAgent, nativeServicesMode, Joiner.on(',').join(daemonOpts));
+        return String.format("DefaultDaemonContext[uid=%s,javaHome=%s,javaVersion=%s,javaVendor=%s,daemonRegistryDir=%s,pid=%s,idleTimeout=%s,priority=%s,applyInstrumentationAgent=%s,nativeServicesMode=%s,daemonOpts=%s]",
+            uid, javaHome, javaVersion, javaVendor, daemonRegistryDir, pid, idleTimeout, priority, applyInstrumentationAgent, nativeServicesMode, Joiner.on(',').join(daemonOpts));
     }
 
     @Override
@@ -90,6 +94,11 @@ public class DefaultDaemonContext implements DaemonContext {
     @Override
     public JavaLanguageVersion getJavaVersion() {
         return javaVersion;
+    }
+
+    @Override
+    public JvmVendor getJavaVendor() {
+        return javaVendor.asJvmVendor();
     }
 
     @Override
@@ -140,6 +149,7 @@ public class DefaultDaemonContext implements DaemonContext {
             String pathname = decoder.readString();
             File javaHome = new File(pathname);
             JavaLanguageVersion javaVersion = JavaLanguageVersion.of(decoder.readSmallInt());
+            JvmVendor.KnownJvmVendor javaVendor = JvmVendor.KnownJvmVendor.values()[decoder.readSmallInt()];
             File registryDir = new File(decoder.readString());
             Long pid = decoder.readBoolean() ? decoder.readLong() : null;
             Integer idle = decoder.readBoolean() ? decoder.readInt() : null;
@@ -152,7 +162,7 @@ public class DefaultDaemonContext implements DaemonContext {
             NativeServicesMode nativeServicesMode = NativeServicesMode.values()[decoder.readSmallInt()];
             DaemonParameters.Priority priority = decoder.readBoolean() ? DaemonParameters.Priority.values()[decoder.readInt()] : null;
 
-            return new DefaultDaemonContext(uid, javaHome, javaVersion, registryDir, pid, idle, daemonOpts, applyInstrumentationAgent, nativeServicesMode, priority);
+            return new DefaultDaemonContext(uid, javaHome, javaVersion, javaVendor, registryDir, pid, idle, daemonOpts, applyInstrumentationAgent, nativeServicesMode, priority);
         }
 
         @Override
@@ -160,6 +170,7 @@ public class DefaultDaemonContext implements DaemonContext {
             encoder.writeNullableString(context.uid);
             encoder.writeString(context.javaHome.getPath());
             encoder.writeSmallInt(context.javaVersion.asInt());
+            encoder.writeSmallInt(context.javaVendor.ordinal());
             encoder.writeString(context.daemonRegistryDir.getPath());
             encoder.writeBoolean(context.pid != null);
             if (context.pid != null) {

platforms/core-runtime/launcher/src/main/java/org/gradle/launcher/daemon/toolchain/DaemonJvmCriteria.java
@@ -16,7 +16,7 @@
 
 package org.gradle.launcher.daemon.toolchain;
 
-import org.gradle.internal.jvm.Jvm;
+import org.gradle.internal.jvm.inspection.JvmVendor;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
 import org.gradle.jvm.toolchain.JvmImplementation;
 import org.gradle.jvm.toolchain.JvmVendorSpec;
@@ -25,6 +25,10 @@ public class DaemonJvmCriteria {
     private final JavaLanguageVersion javaVersion;
     private final JvmVendorSpec vendorSpec;
     private final JvmImplementation jvmImplementation;
+
+    public DaemonJvmCriteria(JavaLanguageVersion javaVersion, JvmVendorSpec vendorSpec) {
+        this(javaVersion, vendorSpec, JvmImplementation.VENDOR_SPECIFIC);
+    }
 
     public DaemonJvmCriteria(JavaLanguageVersion javaVersion, JvmVendorSpec vendorSpec, JvmImplementation jvmImplementation) {
         this.javaVersion = javaVersion;
@@ -44,22 +48,12 @@ public class DaemonJvmCriteria {
         return jvmImplementation;
     }
 
-    public boolean isCompatibleWith(Jvm other) {
-        Integer javaVersionMajor = other.getJavaVersionMajor();
-        if (javaVersionMajor == null) {
-            return false;
-        }
-        return isCompatibleWith(JavaLanguageVersion.of(javaVersionMajor));
-    }
-
     @Override
     public String toString() {
-        // TODO: Include vendor and implementation
-        return String.format("JVM version '%s'", getJavaVersion());
+        return String.format("JVM version '%s' vendor '%s'", getJavaVersion(), getVendorSpec());
     }
 
-    public boolean isCompatibleWith(JavaLanguageVersion javaVersion) {
-        // TODO: Implement comparisons for vendorSpec and jvmImplementation
-        return javaVersion.equals(getJavaVersion()); // && vendorSpec.matches() && jvmImplementation == other.jvmImplementation;
+    public boolean isCompatibleWith(JavaLanguageVersion javaVersion, JvmVendor javaVendor) {
+        return javaVersion == getJavaVersion() && vendorSpec.matches(javaVendor.getRawVendor());
     }
 }

platforms/core-runtime/launcher/src/test/groovy/org/gradle/launcher/daemon/server/DaemonRegistryUnavailableExpirationStrategyTest.groovy
@@ -45,7 +45,7 @@ class DaemonRegistryUnavailableExpirationStrategyTest extends Specification {
     def "daemon should expire when registry file is unreachable"() {
         given:
         DaemonRegistryUnavailableExpirationStrategy expirationStrategy = new DaemonRegistryUnavailableExpirationStrategy(daemon)
-        DaemonContext daemonContext = new DefaultDaemonContext("user", null, JavaLanguageVersion.current(), tempDir.file("BOGUS"), 51234L, 10000, [] as List<String>, false, NativeServicesMode.ENABLED, DaemonParameters.Priority.NORMAL)
+        DaemonContext daemonContext = new DefaultDaemonContext("user", null, JavaLanguageVersion.current(), null, tempDir.file("BOGUS"), 51234L, 10000, [] as List<String>, false, NativeServicesMode.ENABLED, DaemonParameters.Priority.NORMAL)
 
         when:
         1 * daemon.getDaemonContext() >> { daemonContext }
@@ -63,7 +63,7 @@ class DaemonRegistryUnavailableExpirationStrategyTest extends Specification {
                 return "DAEMON_ADDRESS"
             }
         }
-        DaemonContext daemonContext = new DefaultDaemonContext("user", null, JavaLanguageVersion.current(), daemonDir, 51234L, 10000, [] as List<String>, false, NativeServicesMode.ENABLED, DaemonParameters.Priority.NORMAL)
+        DaemonContext daemonContext = new DefaultDaemonContext("user", null, JavaLanguageVersion.current(), null, daemonDir, 51234L, 10000, [] as List<String>, false, NativeServicesMode.ENABLED, DaemonParameters.Priority.NORMAL)
         DaemonDir daemonDir = new DaemonDir(daemonDir)
         DaemonRegistry registry = new EmbeddedDaemonRegistry()
         daemonDir.getRegistry().createNewFile()