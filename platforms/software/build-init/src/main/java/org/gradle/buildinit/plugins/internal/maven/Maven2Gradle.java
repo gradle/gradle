3ffbc895080f3a9d7080c5e192ac8b81dd6d6fa8
Lóránt Pintér <lorant@gradle.com>
2024-06-18 10:55:24 +0200

    Fix accidentally introduced org.gradleinternal package
    The types here should have gone into org.gradle.buildinit.plugins.internal.maven instead. The change was likely introduced in https://github.com/gradle/gradle/pull/16248.

--- platforms/software/build-init/src/main/java/org/gradle/buildinit/plugins/internal/maven/Maven2Gradle.java
+++ platforms/software/build-init/src/main/java/org/gradle/buildinit/plugins/internal/maven/Maven2Gradle.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradleinternal.buildinit.plugins.internal.maven;
+package org.gradle.buildinit.plugins.internal.maven;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.maven.model.Exclusion;
