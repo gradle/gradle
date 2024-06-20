610b4542b7dc34409e04f3354a20695860578e0f
Vlad Chesnokov <vchesnokov@gradle.com>
2024-06-20 14:22:28 +0400

    Migrate Delete and DeleteSpec to Provider API

--- platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/delete/DeleteSpecInternal.java
+++ platforms/core-configuration/file-operations/src/main/java/org/gradle/api/internal/file/delete/DeleteSpecInternal.java
@@ -27,11 +27,4 @@ public interface DeleteSpecInternal extends DeleteSpec {
      * @return the paths to be deleted.
      */
     Object[] getPaths();
-
-    /**
-     * Returns whether or not deletion will follow symlinks.
-     *
-     * @return whether or not deletion follows symlinks.
-     */
-    boolean isFollowSymlinks();
 }
