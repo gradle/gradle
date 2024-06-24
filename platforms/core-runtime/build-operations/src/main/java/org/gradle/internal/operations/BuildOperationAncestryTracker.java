b8bff70be5a41bb1c4a5eae2e7fb96b39aa596d6
Bálint Hegyi <bhegyi@gradle.com>
2024-06-24 13:21:46 +0100

    Revert newlines

--- platforms/core-runtime/build-operations/src/main/java/org/gradle/internal/operations/BuildOperationAncestryTracker.java
+++ platforms/core-runtime/build-operations/src/main/java/org/gradle/internal/operations/BuildOperationAncestryTracker.java
@@ -30,6 +30,4 @@ public interface BuildOperationAncestryTracker {
     Optional<OperationIdentifier> findClosestMatchingAncestor(@Nullable OperationIdentifier id, Predicate<? super OperationIdentifier> predicate);
 
     <T> Optional<T> findClosestExistingAncestor(@Nullable OperationIdentifier id, Function<? super OperationIdentifier, T> lookupFunction);
-
-
 }
