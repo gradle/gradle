6e49d01909ea28ee73fbec0b4fcbf66e98620479
Alex Semin <asemin@gradle.com>
2024-06-27 11:10:58 +0200

    Add tests showing the problem

--- subprojects/core/src/integTest/groovy/org/gradle/api/PluginApplicationErrorIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/PluginApplicationErrorIntegrationTest.groovy
@@ -20,6 +20,7 @@ import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.test.fixtures.plugin.PluginBuilder
 import org.gradle.test.precondition.Requires
 import org.gradle.test.preconditions.UnitTestPreconditions
+import org.gradle.util.internal.ToBeImplemented
 import spock.lang.Issue
 
 @Issue("https://github.com/gradle/gradle-private/issues/3247")
@@ -84,5 +85,43 @@ class BrokenPlugin {
         then:
         failure.assertHasCause("Failed to apply plugin class 'BrokenPlugin'")
         failure.assertHasCause("'BrokenPlugin' is neither a plugin or a rule source and cannot be applied.")
+    }
+
+    @ToBeImplemented
+    def "applying a project plugin to settings fails with user-friendly error"() {
+        settingsFile << """
+            plugins {
+                id("base")
+            }
+        """
+
+        when:
+        fails()
+
+        then:
+        failure.assertHasCause("Failed to apply plugin 'org.gradle.base'")
+        // TODO: this error is cryptic
+        failure.assertHasCause("class org.gradle.initialization.DefaultSettings_Decorated cannot be cast to class org.gradle.api.Project")
+    }
+
+    @ToBeImplemented
+    def "applying a settings plugin to a project fails with user-friendly error"() {
+        buildFile << """
+            apply plugin: SomeSettingsPlugin
+
+            class SomeSettingsPlugin implements Plugin<Settings> {
+                void apply(Settings target) {
+                    throw new Exception("Unreachable boom")
+                }
+            }
+        """
+
+        when:
+        fails()
+
+        then:
+        failure.assertHasCause("Failed to apply plugin class 'SomeSettingsPlugin'")
+        // TODO: this error is cryptic
+        failure.assertHasCause("class org.gradle.api.internal.project.DefaultProject_Decorated cannot be cast to class org.gradle.api.initialization.Settings")
     }
 }
