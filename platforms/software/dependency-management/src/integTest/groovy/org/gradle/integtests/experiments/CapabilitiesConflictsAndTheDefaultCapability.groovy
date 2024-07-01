3b883d40d259675ffb31061466f226d6ef1ef444
Tom Tresansky <ttresansky@gradle.com>
2024-07-01 08:42:00 -0400

    Expand examples to demonstrate related working cases

--- platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/experiments/CapabilitiesConflictsAndTheDefaultCapability.groovy
+++ platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/experiments/CapabilitiesConflictsAndTheDefaultCapability.groovy
@@ -16,10 +16,14 @@
 
 package org.gradle.integtests.experiments;
 
-import org.gradle.integtests.fixtures.AbstractIntegrationSpec;
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.executer.ExecutionResult;
 
+/**
+ * Demonstrates how default capabilities are sometimes handled differently from explicit capabilities.
+ */
 class CapabilitiesConflictsAndTheDefaultCapability extends AbstractIntegrationSpec {
-    def "can depend on two configurations from the same project, sharing the default capability"() {
+    def "can NOT dep by name two confs from same project, sharing the same (default) capability - THIS SHOULD FAIL"() {
         buildFile << """
             dependencies {
                 myDeps project(path: ":producer-with-default-capability", configuration: "conf1")
@@ -31,47 +35,69 @@ class CapabilitiesConflictsAndTheDefaultCapability extends AbstractIntegrationSp
         succeeds("doResolve")
 
         then:
-        file("build/resolved/file-1.txt").assertExists()
-        file("build/resolved/file-2.txt").assertExists()
+        assertResolved("file-a1.txt", "file-a2.txt")
 
         when:
         succeeds(":producer-with-default-capability:outgoingVariants")
 
         then:
-        result.assertOutputContains("""
---------------------------------------------------
-Variant conf1
---------------------------------------------------
+        assertVariantHasCapability("conf1", "example:producer-with-default-capability:unspecified")
+        assertVariantHasCapability("conf2", "example:producer-with-default-capability:unspecified")
+    }
 
-Capabilities
-    - example:producer-with-default-capability:unspecified (default capability)""")
-        result.assertOutputContains("""
---------------------------------------------------
-Variant conf2
---------------------------------------------------
+    def "can NOT dep by name two confs from same project, sharing the same capability"() {
+        buildFile << """
+            dependencies {
+                myDeps project(path: ":producer-with-explicit-capability-1", configuration: "conf1")
+                myDeps project(path: ":producer-with-explicit-capability-1", configuration: "conf2")
+            }
+        """
 
-Capabilities
-    - example:producer-with-default-capability:unspecified (default capability)""")
+        when:
+        fails("doResolve")
+
+        then:
+        assertCapabilityConflict("example:producer:1.0")
+    }
+
+    def "can NOT dep by name two confs from different projects, sharing the same capability"() {
+        buildFile << """
+            dependencies {
+                myDeps project(path: ":producer-with-explicit-capability-1", configuration: "conf1")
+                myDeps project(path: ":producer-with-explicit-capability-2", configuration: "conf1")
+            }
+        """
+
+        when:
+        fails("doResolve")
+
+        then:
+        assertCapabilityConflict("example:producer:1.0")
+    }
+
+    def "can NOT dep by name two confs from different projects, sharing the same capability, which happens to be the default capability on one of them"() {
+        buildFile << """
+            dependencies {
+                myDeps project(path: ":producer-with-default-capability", configuration: "conf1")
+                myDeps project(path: ":producer-with-explicit-capability-equal-to-default", configuration: "conf1")
+            }
+        """
+
+        when:
+        fails("doResolve")
+
+        then:
+        assertCapabilityConflict("example:producer-with-default-capability:unspecified")
     }
 
     def setup() {
         settingsFile << """
             rootProject.name = "example"
+
             include "producer-with-default-capability"
-        """
-
-        file("producer-with-default-capability/file-1.txt").text = "file 1 from producer-with-default-capability"
-        file("producer-with-default-capability/file-2.txt").text = "file 2 from producer-with-default-capability"
-
-        file("producer-with-default-capability/build.gradle") << """
-            configurations {
-                consumable("conf1")
-                consumable("conf2")
-            }
-            artifacts {
-                conf1 file("file-1.txt")
-                conf2 file("file-2.txt")
-            }
+            include "producer-with-explicit-capability-1"
+            include "producer-with-explicit-capability-2"
+            include "producer-with-explicit-capability-equal-to-default"
         """
 
         buildFile << """
@@ -87,5 +113,156 @@ Capabilities
                 into("build/resolved")
             }
         """
+
+        setupProducerWithDefaultCapability()
+        setupProducerWithExplicitCapability1()
+        setupProducerWithExplicitCapability2()
+        setupProducerWithExplicitCapabilityEqualToDefault()
+    }
+
+    private void setupProducerWithDefaultCapability() {
+        file("producer-with-default-capability/file-a1.txt").text = "file 1 from producer-with-default-capability"
+        file("producer-with-default-capability/file-a2.txt").text = "file 2 from producer-with-default-capability"
+
+        file("producer-with-default-capability/build.gradle") << """
+            def color = Attribute.of("color", String)
+
+            configurations {
+                consumable("conf1") {
+                    attributes {
+                        attribute(color, "red")
+                    }
+                }
+                consumable("conf2") {
+                    attributes {
+                        attribute(color, "blue")
+                    }
+                }
+            }
+
+            artifacts {
+                conf1 file("file-a1.txt")
+                conf2 file("file-a2.txt")
+            }
+        """
+    }
+
+    private void setupProducerWithExplicitCapability1() {
+        file("producer-with-explicit-capability/file-b1.txt").text = "file 1 from producer-with-explicit-capability-1"
+        file("producer-with-explicit-capability/file-b2.txt").text = "file 2 from producer-with-explicit-capability-1"
+
+        file("producer-with-explicit-capability-1/build.gradle") << """
+            def color = Attribute.of("color", String)
+
+            configurations {
+                consumable("conf1")  {
+                    attributes {
+                        attribute(color, "red")
+                    }
+                    outgoing {
+                        capability("example:producer:1.0")
+                    }
+                }
+                consumable("conf2") {
+                    attributes {
+                        attribute(color, "blue")
+                    }
+                    outgoing {
+                        capability("example:producer:1.0")
+                    }
+                }
+            }
+
+            artifacts {
+                conf1 file("file-b1.txt")
+                conf2 file("file-b2.txt")
+            }
+        """
+    }
+
+    private void setupProducerWithExplicitCapability2() {
+        file("producer-with-explicit-capability/file-c1.txt").text = "file 1 from producer-with-explicit-capability-2"
+        file("producer-with-explicit-capability/file-c2.txt").text = "file 2 from producer-with-explicit-capability-2"
+
+        file("producer-with-explicit-capability-2/build.gradle") << """
+            def color = Attribute.of("color", String)
+
+            configurations {
+                consumable("conf1") {
+                    attributes {
+                        attribute(color, "red")
+                    }
+                    outgoing {
+                        capability("example:producer:1.0")
+                    }
+                }
+                consumable("conf2") {
+                    attributes {
+                        attribute(color, "blue")
+                    }
+                    outgoing {
+                        capability("example:producer:1.0")
+                    }
+                }
+            }
+
+            artifacts {
+                conf1 file("file-c1.txt")
+                conf2 file("file-c2.txt")
+            }
+        """
+    }
+
+    private void setupProducerWithExplicitCapabilityEqualToDefault() {
+        file("producer-with-explicit-capability-equal-to-default/file-d1.txt").text = "file 1 from producer-with-explicit-capability-equal-to-default"
+        file("producer-with-explicit-capability-equal-to-default/file-d2.txt").text = "file 2 from producer-with-explicit-capability-equal-to-default"
+
+        file("producer-with-explicit-capability-equal-to-default/build.gradle") << """
+            def color = Attribute.of("color", String)
+
+            configurations {
+                consumable("conf1") {
+                    attributes {
+                        attribute(color, "red")
+                    }
+                    outgoing {
+                        capability("example:producer-with-default-capability:unspecified (default capability)")
+                    }
+                }
+                consumable("conf2") {
+                    attributes {
+                        attribute(color, "blue")
+                    }
+                    outgoing {
+                        capability("example:producer-with-default-capability:unspecified (default capability)")
+                    }
+                }
+            }
+
+            artifacts {
+                conf1 file("file-d1.txt")
+                conf2 file("file-d2.txt")
+            }
+        """
+    }
+
+    private void assertCapabilityConflict(String capability) {
+        failureCauseContains("Cannot select module with conflict on capability '$capability'")
+    }
+
+    private void assertResolved(String... fileNames) {
+        fileNames.each { fileName ->
+            file("build/resolved/$fileName").assertExists()
+        }
+    }
+
+    private ExecutionResult assertVariantHasCapability(String variantName, String capability) {
+        result.assertOutputContains("""
+--------------------------------------------------
+Variant $variantName
+--------------------------------------------------
+
+Capabilities
+    - $capability""")
     }
 }
