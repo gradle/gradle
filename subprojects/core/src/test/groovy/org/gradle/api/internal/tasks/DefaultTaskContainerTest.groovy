6137391afc5318429da4d8f061fcb53eefdaa0b9
Anže Sodja <asodja@gradle.com>
2024-07-04 14:13:39 +0200

    Fix DefaultTaskContainerTest after adding TaskInstantiationSchemeProvider

--- subprojects/core/src/test/groovy/org/gradle/api/internal/tasks/DefaultTaskContainerTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/api/internal/tasks/DefaultTaskContainerTest.groovy
@@ -37,9 +37,11 @@ import org.gradle.api.internal.project.taskfactory.TaskFactory
 import org.gradle.api.internal.project.taskfactory.TaskIdentity
 import org.gradle.api.internal.project.taskfactory.TaskInstantiator
 import org.gradle.api.internal.project.taskfactory.TestTaskIdentities
+import org.gradle.api.internal.tasks.properties.TaskInstantiationSchemeProvider
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.TaskDependency
+import org.gradle.internal.instantiation.InstantiationScheme
 import org.gradle.internal.reflect.DirectInstantiator
 import org.gradle.internal.service.ServiceRegistry
 import org.gradle.util.Path
@@ -51,6 +53,12 @@ class DefaultTaskContainerTest extends AbstractPolymorphicDomainObjectContainerS
 
     private taskIdentityFactory = TestTaskIdentities.factory()
     private taskFactory = Mock(ITaskFactory)
+    private instantiationSchemeProvider = Mock(TaskInstantiationSchemeProvider) {
+        getInstantiationScheme() >> Mock(InstantiationScheme)
+    }
+    private serviceRegistry = Mock(ServiceRegistry) {
+        get(TaskInstantiationSchemeProvider) >> instantiationSchemeProvider
+    }
     private project = Mock(ProjectInternal, name: "<project>") {
         identityPath(_) >> { String name ->
             Path.path(":project").child(name)
@@ -65,7 +73,7 @@ class DefaultTaskContainerTest extends AbstractPolymorphicDomainObjectContainerS
             getDepth() >> 0
             getProjectPath() >> Path.path(":project")
         }
-        getServices() >> Mock(ServiceRegistry)
+        getServices() >> serviceRegistry
         getTaskDependencyFactory() >> TestFiles.taskDependencyFactory()
         getObjects() >> Stub(ObjectFactory)
     } as ProjectInternal
