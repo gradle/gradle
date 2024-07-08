39aa2c35c5ddef5aeb42245900ef0eb5e7c1b00b
Tom Tresansky <ttresansky@gradle.com>
2024-07-08 10:58:29 -0400

    Relocate tests out of experiments package

--- platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/CapabilitiesConflictsAndTheDefaultCapability.groovy
+++ platforms/software/dependency-management/src/integTest/groovy/org/gradle/integtests/resolve/CapabilitiesConflictsAndTheDefaultCapability.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.integtests.experiments;
+package org.gradle.integtests.resolve;
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.executer.ExecutionResult
