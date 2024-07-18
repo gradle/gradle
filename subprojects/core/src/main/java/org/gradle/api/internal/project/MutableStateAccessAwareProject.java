212d3184543d9d57eb2934e836646dd3eb51dda9
Sergey Opivalov <sopivalov@gradle.com>
2024-07-18 16:39:16 +0300

    Add Javadoc to `MutableStateAccessAwareProject`

--- subprojects/core/src/main/java/org/gradle/api/internal/project/MutableStateAccessAwareProject.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/MutableStateAccessAwareProject.java
@@ -94,6 +94,15 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
+/**
+ * Wrapper for {@link ProjectInternal}, that declares some API methods as access to a mutable state of the project.
+ * <p>
+ * This class enables dynamic property and method dispatch on the `this` bean rather than on the {@link #delegate}.
+ * If the dispatch on `this` fails, the control flow is delegated to {@link #propertyMissing(String)} and
+ * {@link #methodMissing(String, Object)} methods.
+ * <p>
+ * Instances of this class should be created via {@link org.gradle.internal.reflect.Instantiator} to ensure proper runtime decoration.
+ * */
 public abstract class MutableStateAccessAwareProject implements ProjectInternal, DynamicObjectAware {
 
     protected final ProjectInternal delegate;
