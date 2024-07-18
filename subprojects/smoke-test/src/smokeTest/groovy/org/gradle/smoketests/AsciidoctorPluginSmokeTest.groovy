fc731afe6ba2a5437de2a9733b0df0606260f5e3
Louis Jacomet <louis@gradle.com>
2024-07-18 09:23:20 +0000

    Cherry-pick parts of #29952: Disable smoke tests for broken plugins
    Those plugins are broken because of the JCenter changes: some of their
    transitive dependencies are gone.
    
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AsciidoctorPluginSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AsciidoctorPluginSmokeTest.groovy
@@ -62,7 +62,8 @@ class AsciidoctorPluginSmokeTest extends AbstractPluginValidatingSmokeTest {
                 "org.asciidoctor.js.convert",
                 "org.asciidoctor.jvm.convert",
                 "org.asciidoctor.jvm.epub",
-                "org.asciidoctor.jvm.gems",
+                // Plugin broken after JCenter dependency disappeared
+//                "org.asciidoctor.jvm.gems",
                 "org.asciidoctor.jvm.pdf",
             ].collectEntries { plugin ->
                 [(plugin): Versions.of(version)]
@@ -72,7 +73,8 @@ class AsciidoctorPluginSmokeTest extends AbstractPluginValidatingSmokeTest {
                     "org.asciidoctor.decktape",
                     "org.asciidoctor.jvm.leanpub",
                     "org.asciidoctor.jvm.leanpub.dropbox-copy",
-                    "org.asciidoctor.jvm.revealjs",
+                    // Plugin broken after JCenter dependency disappeared
+//                    "org.asciidoctor.jvm.revealjs",
                 ].collectEntries { plugin ->
                     [(plugin): Versions.of(version)]
                 }
