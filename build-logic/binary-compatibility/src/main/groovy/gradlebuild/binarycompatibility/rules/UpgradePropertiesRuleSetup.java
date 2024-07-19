70d7dea309402729c10e905af846c1adf3c69672
Anže Sodja <asodja@gradle.com>
2024-07-19 08:58:27 +0200

    Skip package roots when discovering annotated types
    Fixes #29926.

--- build-logic/binary-compatibility/src/main/groovy/gradlebuild/binarycompatibility/rules/UpgradePropertiesRuleSetup.java
+++ build-logic/binary-compatibility/src/main/groovy/gradlebuild/binarycompatibility/rules/UpgradePropertiesRuleSetup.java
@@ -62,15 +62,13 @@ public class UpgradePropertiesRuleSetup implements SetupRule {
     }
 
     private static Map<AccessorKey, UpgradedProperty> mapCurrentAccessorsOfUpgradedProperties(List<UpgradedProperty> upgradedProperties) {
-        // Some upgrades are reported in multiple projects for some reason, see https://github.com/gradle/gradle/issues/29926
-        return upgradedProperties.stream().collect(Collectors.toMap(AccessorKey::ofUpgradedProperty, Function.identity(), (first, second) -> first));
+        return upgradedProperties.stream().collect(Collectors.toMap(AccessorKey::ofUpgradedProperty, Function.identity()));
     }
 
     private static Map<AccessorKey, ReplacedAccessor> mapOldAccessorsOfUpgradedProperties(List<UpgradedProperty> upgradedProperties) {
         return upgradedProperties.stream()
             .flatMap(UpgradePropertiesRuleSetup::mapOldAccessorsOfUpgradedProperty)
-            // Some upgrades are reported in multiple projects for some reason, see https://github.com/gradle/gradle/issues/29926
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (first, second) -> first));
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     private static Stream<Map.Entry<AccessorKey, ReplacedAccessor>> mapOldAccessorsOfUpgradedProperty(UpgradedProperty upgradedProperty) {
