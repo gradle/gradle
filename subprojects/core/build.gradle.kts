b35256efd9ef01cd7b0cc68972f705c1114e3ea3
Anže Sodja <asodja@gradle.com>
2024-07-31 13:02:16 +0200

    Upgrade GroovyCompileOptions to use lazy properties

--- subprojects/core/build.gradle.kts
+++ subprojects/core/build.gradle.kts
@@ -293,6 +293,8 @@ tasks.test {
 }
 
 tasks.compileTestGroovy {
+    // TODO: Fix for Gradle 10, use groovyOptions.fork instead
+    @Suppress("DEPRECATION")
     groovyOptions.isFork = true
     groovyOptions.forkOptions.run {
         memoryInitialSize = "128M"
