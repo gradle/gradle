af6e4e4bef8c77ed303eb25767a40c03ae175f81
Anže Sodja <asodja@gradle.com>
2024-08-01 16:59:23 +0200

    Upgrade Pmd task to use lazy properties

--- platforms/jvm/code-quality/src/test/groovy/org/gradle/api/plugins/quality/PmdPluginTest.groovy
+++ platforms/jvm/code-quality/src/test/groovy/org/gradle/api/plugins/quality/PmdPluginTest.groovy
@@ -25,6 +25,7 @@ import org.gradle.api.attributes.java.TargetJvmVersion
 import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.api.plugins.ReportingBasePlugin
 import org.gradle.api.tasks.SourceSet
+import org.gradle.integtests.fixtures.RepoScriptBlockUtil
 import org.gradle.internal.jvm.Jvm
 import org.gradle.test.fixtures.AbstractProjectBuilderSpec
 
@@ -38,6 +39,7 @@ class PmdPluginTest extends AbstractProjectBuilderSpec {
 
     def setup() {
         project.pluginManager.apply(PmdPlugin)
+        RepoScriptBlockUtil.configureMavenCentral(project.repositories)
     }
 
     def "applies reporting-base plugin"() {
@@ -112,8 +114,8 @@ class PmdPluginTest extends AbstractProjectBuilderSpec {
         verifyAll(task) {
             description == "Run PMD analysis for ${sourceSet.name} classes"
             source as List == sourceSet.allJava as List
-            pmdClasspath == project.configurations.pmd
-            ruleSets == ["category/java/errorprone.xml"]
+            pmdClasspath.files == project.configurations.pmd.files
+            ruleSets.get() == ["category/java/errorprone.xml"]
             ruleSetConfig == null
             ruleSetFiles.empty
             reports.xml.outputLocation.asFile.get() == project.file("build/reports/pmd/${sourceSet.name}.xml")
@@ -132,8 +134,8 @@ class PmdPluginTest extends AbstractProjectBuilderSpec {
         expect:
         task.description == null
         task.source.empty
-        task.pmdClasspath == project.configurations.pmd
-        task.ruleSets == ["category/java/errorprone.xml"]
+        task.pmdClasspath.files == project.configurations.pmd.files
+        task.ruleSets.get() == ["category/java/errorprone.xml"]
         task.ruleSetConfig == null
         task.ruleSetFiles.empty
         task.reports.xml.outputLocation.asFile.get() == project.file("build/reports/pmd/custom.xml")
@@ -191,8 +193,8 @@ class PmdPluginTest extends AbstractProjectBuilderSpec {
         verifyAll(task) {
             description == "Run PMD analysis for ${sourceSet.name} classes"
             source as List == sourceSet.allJava as List
-            pmdClasspath == project.configurations.pmd
-            ruleSets == ["java-braces", "java-unusedcode"]
+            pmdClasspath.files == project.configurations.pmd.files
+            ruleSets.get() == ["java-braces", "java-unusedcode"]
             ruleSetConfig.asString() == "ruleset contents"
             ruleSetFiles.singleFile == project.file("my-ruleset.xml")
             reports.xml.outputLocation.asFile.get() == project.file("pmd-reports/${sourceSet.name}.xml")
@@ -220,8 +222,8 @@ class PmdPluginTest extends AbstractProjectBuilderSpec {
         expect:
         task.description == null
         task.source.empty
-        task.pmdClasspath == project.configurations.pmd
-        task.ruleSets == ["java-braces", "java-unusedcode"]
+        task.pmdClasspath.files == project.configurations.pmd.files
+        task.ruleSets.get() == ["java-braces", "java-unusedcode"]
         task.ruleSetConfig.asString() == "ruleset contents"
         task.ruleSetFiles.singleFile == project.file("my-ruleset.xml")
         task.reports.xml.outputLocation.asFile.get() == project.file("pmd-reports/custom.xml")
