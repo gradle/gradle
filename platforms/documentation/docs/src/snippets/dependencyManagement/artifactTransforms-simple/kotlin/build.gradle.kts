785df2e627a89ec14aef251464c3a67582cfa02d
Laura Kassovic <lkassovic@gradle.com>
2024-08-05 10:00:00 -0700

    final changes

--- platforms/documentation/docs/src/snippets/dependencyManagement/artifactTransforms-simple/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/dependencyManagement/artifactTransforms-simple/kotlin/build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
     application
 }
 
+dependencies {
+    implementation("com.google.guava:guava:33.2.1-jre")
+}
+
+repositories {
+    mavenCentral()
+}
+
 // tag::artifact-transform-imp[]
 abstract class MyTransform : TransformAction<TransformParameters.None> {
     @get:InputArtifact
@@ -13,16 +21,15 @@ abstract class MyTransform : TransformAction<TransformParameters.None> {
         val inputFile = inputArtifact.get().asFile
         val outputFile = outputs.file(inputFile.name.replace(".jar", "-transformed.jar"))
         // Perform transformation logic here
+        inputFile.copyTo(outputFile, overwrite = true)
     }
 }
 // end::artifact-transform-imp[]
 
 // tag::artifact-transform-use[]
-val artifactType = Attribute.of("artifactType", String::class.java)
-
 configurations.named("runtimeClasspath") {
     attributes {
-        attribute(artifactType, "transformed-jar")
+        attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, "transformed-jar")
     }
 }
 // end::artifact-transform-use[]
@@ -30,10 +37,16 @@ configurations.named("runtimeClasspath") {
 // tag::artifact-transform-registration[]
 dependencies {
     registerTransform(MyTransform::class) {
-        from.attribute(artifactType, "jar")
-        to.attribute(artifactType, "transformed-jar")
+        from.attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, "jar")
+        to.attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, "transformed-jar")
     }
 }
 // end::artifact-transform-registration[]
 
-
+tasks.register("resolve") {
+    val files = configurations.runtimeClasspath.get().incoming.files
+    val fileNames = files.map { it.name }
+    doLast {
+        check(fileNames.contains("guava-33.2.1-jre-transformed.jar"))
+    }
+}
