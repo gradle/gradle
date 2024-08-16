37f92295fa89fa68cc73a98c6ba6530a993e7a71
Christoph Obexer <cobexer@gradle.com>
2024-08-16 13:31:15 +0200

    Smoke Tested Plugins: Update Protobuf Java Plugin
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
+++ testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractSmokeTest.groovy
@@ -126,7 +126,7 @@ abstract class AbstractSmokeTest extends Specification {
         static protobufPlugin = "0.9.4"
 
         // https://central.sonatype.com/artifact/com.google.protobuf/protobuf-java/versions
-        static protobufTools = "4.26.1"
+        static protobufTools = "4.27.3"
 
         // https://plugins.gradle.org/plugin/org.gradle.test-retry
         static testRetryPlugin = "1.5.10"
