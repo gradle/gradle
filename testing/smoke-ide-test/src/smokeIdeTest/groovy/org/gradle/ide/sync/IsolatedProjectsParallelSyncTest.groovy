eb3c5f9975d08ee22efd36403af4fdc5a2964166
Sergey Opivalov <sopivalov@gradle.com>
2024-08-19 17:06:28 +0300

    Force parallel configuration of all projects before model building

--- testing/smoke-ide-test/src/smokeIdeTest/groovy/org/gradle/ide/sync/IsolatedProjectsParallelSyncTest.groovy
+++ testing/smoke-ide-test/src/smokeIdeTest/groovy/org/gradle/ide/sync/IsolatedProjectsParallelSyncTest.groovy
@@ -17,7 +17,6 @@
 package org.gradle.ide.sync
 
 import org.gradle.test.fixtures.server.http.BlockingHttpServer
-import org.gradle.util.internal.ToBeImplemented
 import org.junit.Rule
 
 class IsolatedProjectsParallelSyncTest extends AbstractIdeaSyncTest {
@@ -29,16 +28,11 @@ class IsolatedProjectsParallelSyncTest extends AbstractIdeaSyncTest {
         server.start()
     }
 
-    @ToBeImplemented
     def 'projects are configured in parallel during IDEA sync'() {
         given:
         simpleProject()
         server.expect("configure-root")
-        server.expect("configure-a")
-        server.expect("configure-b")
-        // TODO: isolated expected behaviour for parallel sync
-//        server.expect("configure-root")
-//        server.expectConcurrent("configure-a", "configure-b")
+        server.expectConcurrent("configure-a", "configure-b")
 
         expect:
         ideaSync(IDEA_VERSION)
