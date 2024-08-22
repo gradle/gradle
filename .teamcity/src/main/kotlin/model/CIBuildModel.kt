45c314ff929a27ac1134b12855b8ee53cd386e83
Christoph Obexer <cobexer@gradle.com>
2024-08-22 18:30:07 +0200

    TeamCity: add ConfigCache coverage on macOS and Windows to RfR
    Fixes gradle-private#4295
    
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- .teamcity/src/main/kotlin/model/CIBuildModel.kt
+++ .teamcity/src/main/kotlin/model/CIBuildModel.kt
@@ -133,7 +133,9 @@ data class CIBuildModel(
                 TestCoverage(15, TestType.forceRealizeDependencyManagement, Os.LINUX, JvmCategory.MIN_VERSION, DEFAULT_LINUX_FUNCTIONAL_TEST_BUCKET_SIZE),
                 TestCoverage(33, TestType.allVersionsIntegMultiVersion, Os.LINUX, JvmCategory.MIN_VERSION, ALL_CROSS_VERSION_BUCKETS.size),
                 TestCoverage(34, TestType.allVersionsIntegMultiVersion, Os.WINDOWS, JvmCategory.MIN_VERSION_WINDOWS_MAC, ALL_CROSS_VERSION_BUCKETS.size),
-                TestCoverage(36, TestType.platform, Os.MACOS, JvmCategory.MAX_LTS_VERSION, expectedBucketNumber = 20, arch = Arch.AARCH64)
+                TestCoverage(36, TestType.platform, Os.MACOS, JvmCategory.MAX_LTS_VERSION, expectedBucketNumber = 20, arch = Arch.AARCH64),
+                TestCoverage(37, TestType.configCache, Os.MACOS, JvmCategory.MAX_LTS_VERSION, arch = Arch.AARCH64),
+                TestCoverage(38, TestType.configCache, Os.WINDOWS, JvmCategory.MAX_LTS_VERSION),
             ),
             docsTests = listOf(
                 DocsTestCoverage(Os.MACOS, JvmCategory.MAX_VERSION, listOf(CONFIG_CACHE_DISABLED)),
