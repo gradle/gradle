2a8536d5b642204c840ad20cd42e066cf9679f38
Rafael Chaves <rchaves@gradle.com>
2024-08-22 15:57:56 -0300

    Adapt test expectations for parallel CC store

--- platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/ConfigurationCacheDebugLogIntegrationTest.groovy
+++ platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/ConfigurationCacheDebugLogIntegrationTest.groovy
@@ -72,11 +72,17 @@ class ConfigurationCacheDebugLogIntegrationTest extends AbstractConfigurationCac
         events.contains([profile: "child ':sub' state", type: "C", frame: ":sub:ok"])
 
         and: "task type frame follows task path frame follows LocalTaskNode frame"
-        def firstTaskNodeIndex = events.findIndexOf { it.frame == LocalTaskNode.name }
-        firstTaskNodeIndex > 0
-        events[firstTaskNodeIndex] == [profile: "child ':' state", type: "O", frame: LocalTaskNode.name]
-        events[firstTaskNodeIndex + 1] == [profile: "child ':' state", type: "O", frame: ":ok"]
-        events[firstTaskNodeIndex + 2] == [profile: "child ':' state", type: "O", frame: DefaultTask.name]
+        ["child ':' state", "child ':sub' state"].each {profile ->
+            def firstTaskNodeIndex = events.findIndexOf { it.profile == profile && it.frame == LocalTaskNode.name }
+            firstTaskNodeIndex > 0
+            events[firstTaskNodeIndex] == [profile: "$profile", type: "O", frame: LocalTaskNode.name]
+
+            def secondTaskNodeIndex = events.findIndexOf {it.profile == profile && it.type == "O" && it.frame == ":ok" }
+            firstTaskNodeIndex < secondTaskNodeIndex
+
+            def thirdTaskNodeIndex = events.findIndexOf {it.profile == profile && it.type == "O" && it.frame == DefaultTask.name }
+            secondTaskNodeIndex < thirdTaskNodeIndex
+        }
 
         where:
         enablement << CCDebugEnablement.values()
