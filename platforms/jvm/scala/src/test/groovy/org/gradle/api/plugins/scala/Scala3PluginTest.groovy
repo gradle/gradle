4cba8c245fbeb4fe6f22d89e83211b016e7aff33
Anže Sodja <asodja@gradle.com>
2024-08-22 16:44:51 +0200

    Upgrade ScalaDoc to use lazy properties
    Except for classpath due to issue #30273.

--- platforms/jvm/scala/src/test/groovy/org/gradle/api/plugins/scala/Scala3PluginTest.groovy
+++ platforms/jvm/scala/src/test/groovy/org/gradle/api/plugins/scala/Scala3PluginTest.groovy
@@ -36,7 +36,7 @@ class Scala3PluginTest extends AbstractProjectBuilderSpec {
         def task = project.tasks[ScalaPlugin.SCALA_DOC_TASK_NAME]
         task instanceof ScalaDoc
         task dependsOn(JvmConstants.CLASSES_TASK_NAME, JvmConstants.COMPILE_JAVA_TASK_NAME, 'compileScala')
-        task.destinationDir == project.java.docsDir.file("scaladoc").get().asFile
+        task.destinationDir.asFile.get() == project.java.docsDir.file("scaladoc").get().asFile
         // This assertion is a little tricky, because `task.source` is an empty list since we didn't compile these files, so we check here if [] == []
         task.source as List  == project.sourceSets.main.output.findAll { it.name.endsWith(".tasty") } as List // We take output of main (with tasty files)
         assertThat(task.classpath, sameCollection(project.layout.files(project.sourceSets.main.output, project.sourceSets.main.compileClasspath)))
