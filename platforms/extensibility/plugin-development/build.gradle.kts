86596bcad297f04cf93ec1d4dbdd2175e4fabe9d
Vlad Chesnokov <vchesnokov@gradle.com>
2024-08-22 17:34:00 +0200

    Migrate PluginUnderTestMetadata task to provider API

--- platforms/extensibility/plugin-development/build.gradle.kts
+++ platforms/extensibility/plugin-development/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
 
     implementation(libs.asm)
     implementation(libs.guava)
+    implementation(libs.commonsIo)
 
     testImplementation(projects.fileCollections)
     testImplementation(projects.enterpriseOperations)
