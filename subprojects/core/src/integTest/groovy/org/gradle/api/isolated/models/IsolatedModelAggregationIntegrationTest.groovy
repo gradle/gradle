ff95a0cf9d817eed1ebf38b47ea16cd95be5fc27
Alex Semin <asemin@gradle.com>
2024-08-27 12:47:30 +0200

    Add test for requesting project models from settings

--- subprojects/core/src/integTest/groovy/org/gradle/api/isolated/models/IsolatedModelAggregationIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/isolated/models/IsolatedModelAggregationIntegrationTest.groovy
@@ -16,7 +16,16 @@
 
 package org.gradle.api.isolated.models
 
+import org.gradle.api.flow.FlowAction
+import org.gradle.api.flow.FlowParameters
+import org.gradle.api.flow.FlowProviders
+import org.gradle.api.flow.FlowScope
+import org.gradle.api.provider.Provider
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.internal.isolated.models.IsolatedModelRouter
+import org.gradle.test.fixtures.dsl.GradleDsl
+
+import javax.inject.Inject
 
 class IsolatedModelAggregationIntegrationTest extends AbstractIntegrationSpec {
 
@@ -70,8 +79,6 @@ class IsolatedModelAggregationIntegrationTest extends AbstractIntegrationSpec {
 
             def router = project.services.get(org.gradle.internal.isolated.models.IsolatedModelRouter)
 
-
-
             tasks.register("sum", SummingTask) {
                 Map<String, Provider<RegularFile>> numberFiles =
                     router.getProjectModels(router.key("numberFile", RegularFile), allprojects.collect { it.path })
@@ -90,4 +97,99 @@ class IsolatedModelAggregationIntegrationTest extends AbstractIntegrationSpec {
         outFile.text == "7"
     }
 
+    def "settings plugin can collect models contributed by projects"() {
+        file("build-logic/build.gradle.kts") << """
+            plugins {
+                `kotlin-dsl`
+            }
+
+            ${mavenCentralRepository(GradleDsl.KOTLIN)}
+        """
+
+        file("build-logic/src/main/kotlin/my/ProjectApi.kt") << """
+            package my
+
+            import ${Inject.name}
+            import ${Provider.name}
+            import ${IsolatedModelRouter.name}
+
+            abstract class ProjectApi {
+                @get:Inject
+                abstract val router: IsolatedModelRouter
+
+                fun tag(provider: Provider<String>) {
+                    router.postModel(router.key("tag", String::class.java), router.work(provider))
+                }
+            }
+        """
+
+        file("build-logic/src/main/kotlin/my-settings-plugin.settings.gradle.kts") << """
+            import org.gradle.kotlin.dsl.support.serviceOf
+
+            gradle.lifecycle.beforeProject {
+                extensions.create<my.ProjectApi>("projectApi")
+            }
+
+            val flowScope = serviceOf<${FlowScope.name}>()
+            val flowProviders = serviceOf<${FlowProviders.name}>()
+
+            flowScope.always(ModelCollectingFlow::class) {
+                parameters {
+                    buildResult = flowProviders.buildWorkResult.map { it.failure.isEmpty() }
+                }
+            }
+
+            abstract class ModelCollectingFlow : ${FlowAction.name}<ModelCollectingFlow.Parameters> {
+                interface Parameters : ${FlowParameters.name} {
+                    @get:Input
+                    val buildResult: Property<Boolean>
+
+                    @get:Input
+                    val tags: MapProperty<String, String>
+                }
+
+                // TODO: injecting the router directly leaves a hole of late discovery of models dependent on tasks
+                //  these tasks cannot be executed at this point (in build-finished callback)
+                @get:Inject
+                abstract val router: ${IsolatedModelRouter.name}
+
+                override fun execute(parameters: Parameters) {
+                    val tags: Map<String, Provider<String>> =
+                        router.getProjectModels(router.key("tag", String::class.java), listOf(":", ":a", ":b"))
+
+                    println("Tags after build finished: \${tags.mapValues { it.value.orNull }.toSortedMap()}")
+                }
+            }
+        """
+
+        settingsFile """
+            pluginManagement {
+                includeBuild("build-logic")
+            }
+
+            plugins {
+                id("my-settings-plugin")
+            }
+
+            include("a", "b")
+        """
+
+        buildFile """
+            projectApi.tag(provider { "tag-root" })
+        """
+
+        buildFile "a/build.gradle", """
+            println("Skip contributing a tag")
+        """
+
+        buildFile "b/build.gradle", """
+            projectApi.tag(provider { "tag-b" })
+        """
+
+        when:
+        run "help"
+
+        then:
+        outputContains("Tags after build finished: {:=tag-root, :a=null, :b=tag-b}")
+    }
 }
