552d672a9dd748c48a1e1da0aed073f7afb5296c
Rafael Chaves <rchaves@gradle.com>
2024-08-28 11:45:01 -0300

    Add test case for multiple exceptions
    Replace work graph with task graph

--- platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/ConfigurationCacheParallelStoreIntegrationTest.groovy
+++ platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/ConfigurationCacheParallelStoreIntegrationTest.groovy
@@ -17,6 +17,42 @@
 package org.gradle.internal.cc.impl
 
 class ConfigurationCacheParallelStoreIntegrationTest extends AbstractConfigurationCacheIntegrationTest {
+
+    def "failures while storing different projects are reported"() {
+        given:
+        def projects = (1..5).collect  {"proj$it" }
+        def configurationCache = newConfigurationCacheFixture()
+        settingsFile << """
+            include ${projects.collect {"'${it}'" }.join(',')}
+        """
+
+        def createBuildFile = {
+            file(getDefaultBuildFileName()) << """
+                task t {
+                    outputs.file({ -> throw new RuntimeException("\${ project.identityPath } went BOOM!") })
+                }
+            """
+        }
+
+        projects.each {
+            createDir(it, createBuildFile)
+        }
+
+        when:
+        configurationCacheFails("t")
+
+        then:
+        configurationCache.assertStateStoreFailed()
+        outputContains("Configuration cache entry discarded due to serialization error.")
+        failure.assertHasFailures(1)
+        // we cannot assert on the build script absolute path as it is non-deterministic
+        failure.assertHasLineNumber(3)
+        failure.assertHasDescription("Error while saving task graph")
+        projects.each {
+            failure.assertHasCause("Exception while storing configuration for :$it: :$it went BOOM!")
+        }
+    }
+
     def "parallel store is enabled by default"() {
         given:
         settingsFile.createFile()
@@ -25,8 +61,8 @@ class ConfigurationCacheParallelStoreIntegrationTest extends AbstractConfigurati
         configurationCacheRun("help", "-d")
 
         then:
-        output.contains("[org.gradle.configurationcache] saving work graph in parallel")
-        output.contains("[org.gradle.configurationcache] reading work graph in parallel")
+        output.contains("[org.gradle.configurationcache] saving task graph in parallel")
+        output.contains("[org.gradle.configurationcache] reading task graph in parallel")
     }
 
     def "parallel store may be opted out"() {
@@ -38,9 +74,9 @@ class ConfigurationCacheParallelStoreIntegrationTest extends AbstractConfigurati
 
         then:
         // we allow disabling parallel storing it as a safety measure for builds that might be broken by parallelism
-        output.contains("[org.gradle.configurationcache] saving work graph sequentially")
+        output.contains("[org.gradle.configurationcache] saving task graph sequentially")
         // loading is still parallel
-        output.contains("[org.gradle.configurationcache] reading work graph in parallel")
+        output.contains("[org.gradle.configurationcache] reading task graph in parallel")
     }
 
     def "parallel load may be opted out"() {
@@ -52,8 +88,8 @@ class ConfigurationCacheParallelStoreIntegrationTest extends AbstractConfigurati
 
         then:
         // storing is still parallel
-        output.contains("[org.gradle.configurationcache] saving work graph in parallel")
+        output.contains("[org.gradle.configurationcache] saving task graph in parallel")
         // parallel loading should always be safe, however we will (temporarily) allow disabling it for benchmarking
-        output.contains("[org.gradle.configurationcache] reading work graph sequentially")
+        output.contains("[org.gradle.configurationcache] reading task graph sequentially")
     }
 }
