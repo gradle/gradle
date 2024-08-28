c679682638299495d55d1aee95d4c6268cf6c0fd
Vlad Chesnokov <vchesnokov@gradle.com>
2024-08-28 15:47:59 +0200

    Migrate ConventionReportTask and it's descendants to provider API

--- platforms/software/software-diagnostics/src/test/groovy/org/gradle/api/tasks/diagnostics/ProjectReportTaskTest.groovy
+++ platforms/software/software-diagnostics/src/test/groovy/org/gradle/api/tasks/diagnostics/ProjectReportTaskTest.groovy
@@ -26,7 +26,7 @@ class ProjectReportTaskTest extends AbstractProjectBuilderSpec {
 
     def setup() {
         task = TestUtil.createTask(ProjectReportTask, project)
-        task.renderer.output = output
+        task.renderer.get().output = output
     }
 
     def rendersReportForRootProjectWithChildren() {
