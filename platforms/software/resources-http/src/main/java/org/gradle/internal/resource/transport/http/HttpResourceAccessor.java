f5e7e1a63bafc7dc67f10e17cea5a8440977b3ca
DevDengChao <2325690622@qq.com>
2024-08-30 16:05:14 +0800

    Basic range request implementation
    Signed-off-by: DevDengChao <2325690622@qq.com>

--- platforms/software/resources-http/src/main/java/org/gradle/internal/resource/transport/http/HttpResourceAccessor.java
+++ platforms/software/resources-http/src/main/java/org/gradle/internal/resource/transport/http/HttpResourceAccessor.java
@@ -16,14 +16,21 @@
 
 package org.gradle.internal.resource.transport.http;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.gradle.api.resources.ResourceException;
 import org.gradle.internal.IoActions;
 import org.gradle.internal.resource.ExternalResourceName;
 import org.gradle.internal.resource.metadata.ExternalResourceMetaData;
 import org.gradle.internal.resource.transfer.AbstractExternalResourceAccessor;
 import org.gradle.internal.resource.transfer.ExternalResourceAccessor;
+import org.gradle.internal.resource.transfer.ExternalResourceReadResponse;
+import org.gradle.internal.resource.transfer.UrlExternalResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.net.URI;
 
@@ -32,29 +39,57 @@ public class HttpResourceAccessor extends AbstractExternalResourceAccessor imple
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpResourceAccessor.class);
     private final HttpClientHelper http;
 
+    private long rangeSize = 2 * 1024 * 1024;// 2 Mb
+
     public HttpResourceAccessor(HttpClientHelper http) {
         this.http = http;
     }
 
     @Override
     @Nullable
-    public HttpResponseResource openResource(final ExternalResourceName location, boolean revalidate) {
-        String uri = location.getUri().toString();
-        LOGGER.debug("Constructing external resource: {}", location);
-
-        HttpClientResponse response = http.performGet(uri, revalidate);
-        return wrapResponse(location.getUri(), response);
+    public ExternalResourceReadResponse openResource(final ExternalResourceName location, boolean revalidate) {
+        return onOpenResource(location.getUri(), revalidate, http::performGet);
     }
 
     /**
      * Same as #getResource except that it always gives access to the response body,
      * irrespective of the returned HTTP status code. Never returns {@code null}.
      */
-    public HttpResponseResource getRawResource(final URI uri, boolean revalidate) {
-        String location = uri.toString();
+    public ExternalResourceReadResponse getRawResource(final URI uri, boolean revalidate) {
+        return onOpenResource(uri, revalidate, http::performRawGet);
+    }
+
+    private ExternalResourceReadResponse onOpenResource(final URI location, boolean revalidate, @Nonnull HttpClientResponseProvider provider) {
+        String uri = location.toString();
         LOGGER.debug("Constructing external resource: {}", location);
-        HttpClientResponse response = http.performRawGet(location, revalidate);
-        return wrapResponse(uri, response);
+
+        long skip = 0;// TODO 2024/8/30: read file size from partially downloaded file
+        int round = 0;
+        Long totalBytes = null;
+        long receivedBytes;
+        do {
+            long rangeStart = skip + round * rangeSize;
+            HttpClientResponse response = provider.get(uri, revalidate, rangeStart, rangeStart + rangeSize);
+            int code = response.getStatusLine().getStatusCode();
+            if (code == HttpStatus.SC_OK) { // server dose not support range request
+                return wrapResponse(location, response);
+            } else if (code == HttpStatus.SC_PARTIAL_CONTENT) { // server support range request
+
+                // example of contentRange: "bytes 0-100/5083"
+                String contentRange = response.getHeader(HttpHeaders.CONTENT_RANGE);
+                String[] split = contentRange.replace("bytes ", "").split("/");// ["0-100", "5083"]
+                if (totalBytes == null) {
+                    totalBytes = Long.parseLong(split[1]);
+                }
+                String[] receivedRange = split[0].split("-"); // ["0", "100"]
+                receivedBytes = Long.parseLong(receivedRange[1]);
+                round++;
+                // TODO 2024/8/30: amend response content to the temp file
+            } else {
+                throw new ResourceException("Unexpected response code: " + code + " when fetching resource: " + location);
+            }
+        } while (receivedBytes != totalBytes - 1);
+        return new UrlExternalResource().openResource(new ExternalResourceName("THE TEMP FILE PATH"), revalidate);
     }
 
     @Override
@@ -79,4 +114,13 @@ public class HttpResourceAccessor extends AbstractExternalResourceAccessor imple
         return new HttpResponseResource("GET", uri, response);
     }
 
+    @VisibleForTesting
+    void setRangeSize(long rangeSize) {
+        this.rangeSize = rangeSize;
+    }
+
+    private interface HttpClientResponseProvider {
+        @Nonnull
+        HttpClientResponse get(final String location, boolean revalidate, long rangeStart, long rangeEnd);
+    }
 }
