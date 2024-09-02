40e46af0bde3e772e5ce9b5aeb0be2171920875f
Anže Sodja <asodja@gradle.com>
2024-09-02 16:22:09 +0200

    Upgrade CodeQualityExtension to use lazy properties

--- platforms/documentation/docs/src/snippets/releases/migrating/basic/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/releases/migrating/basic/kotlin/build.gradle.kts
@@ -43,7 +43,7 @@ tasks {
 checkstyle {
     config = resources.text.fromFile("checkstyle.xml", "UTF-8")
     isShowViolations = true
-    isIgnoreFailures = false
+    ignoreFailures = false
 }
 // end::checkstyle[]
 
