82d263438c2489100713f8503015efec5c656252
Anže Sodja <asodja@gradle.com>
2024-09-02 11:17:33 +0200

    Upgrade BaseForkOptions to use lazy properties

--- platforms/jvm/jvm-compiler-worker/src/main/java/org/gradle/api/tasks/compile/BaseForkOptions.java
+++ platforms/jvm/jvm-compiler-worker/src/main/java/org/gradle/api/tasks/compile/BaseForkOptions.java
@@ -15,32 +15,22 @@
  */
 package org.gradle.api.tasks.compile;
 
-import org.apache.commons.lang3.StringUtils;
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Optional;
-import org.gradle.internal.instrumentation.api.annotations.ToBeReplacedByLazyProperty;
-import org.jspecify.annotations.Nullable;
+import org.gradle.internal.instrumentation.api.annotations.ReplacesEagerProperty;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * Fork options for compilation. Only take effect if {@code fork}
  * is {@code true}.
  * @since 1.3
  */
-public class BaseForkOptions implements Serializable {
+public abstract class BaseForkOptions implements Serializable {
     private static final long serialVersionUID = 0;
-
-    private String memoryInitialSize;
-
-    private String memoryMaximumSize;
-
-    private List<String> jvmArgs = new ArrayList<>();
 
     /**
      * Returns the initial heap size for the compiler process.
@@ -48,19 +38,8 @@ public class BaseForkOptions implements Serializable {
      * @since 1.3
      */
     @Internal
-    @ToBeReplacedByLazyProperty
-    public String getMemoryInitialSize() {
-        return memoryInitialSize;
-    }
-
-    /**
-     * Sets the initial heap size for the compiler process.
-     * Defaults to {@code null}, in which case the JVM's default will be used.
-     * @since 1.3
-     */
-    public void setMemoryInitialSize(String memoryInitialSize) {
-        this.memoryInitialSize = memoryInitialSize;
-    }
+    @ReplacesEagerProperty
+    public abstract Property<String> getMemoryInitialSize();
 
     /**
      * Returns the maximum heap size for the compiler process.
@@ -68,43 +47,16 @@ public class BaseForkOptions implements Serializable {
      * @since 1.3
      */
     @Internal
-    @ToBeReplacedByLazyProperty
-    public String getMemoryMaximumSize() {
-        return memoryMaximumSize;
-    }
-
-    /**
-     * Sets the maximum heap size for the compiler process.
-     * Defaults to {@code null}, in which case the JVM's default will be used.
-     * @since 1.3
-     */
-    public void setMemoryMaximumSize(String memoryMaximumSize) {
-        this.memoryMaximumSize = memoryMaximumSize;
-    }
+    @ReplacesEagerProperty
+    public abstract Property<String> getMemoryMaximumSize();
 
     /**
      * Returns any additional JVM arguments for the compiler process.
      * Defaults to the empty list.
      * @since 1.3
      */
-    @Nullable
     @Optional
     @Input
-    @ToBeReplacedByLazyProperty
-    public List<String> getJvmArgs() {
-        return jvmArgs;
-    }
-
-    /**
-     * Sets any additional JVM arguments for the compiler process.
-     * Defaults to the empty list. Empty or null arguments are filtered out because they cause
-     * JVM Launch to fail.
-     * @since 1.3
-     */
-    public void setJvmArgs(@Nullable List<String> jvmArgs) {
-        this.jvmArgs = jvmArgs == null ? null : jvmArgs.stream()
-            .filter(Objects::nonNull)
-            .filter(string -> !StringUtils.isBlank(string))
-            .collect(Collectors.toList());
-    }
+    @ReplacesEagerProperty
+    public abstract ListProperty<String> getJvmArgs();
 }
