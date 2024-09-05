613d0f65ecdd33aa6a4bac8f8a927cbf17e9274d
Vlad Chesnokov <vchesnokov@gradle.com>
2024-09-05 14:23:10 +0200

    Make diagnostics tasks CC compatible

--- platforms/jvm/plugins-java/src/main/java/org/gradle/api/plugins/JavaPlugin.java
+++ platforms/jvm/plugins-java/src/main/java/org/gradle/api/plugins/JavaPlugin.java
@@ -27,7 +27,6 @@ import org.gradle.api.file.FileCollection;
 import org.gradle.api.internal.artifacts.configurations.RoleBasedConfigurationContainerInternal;
 import org.gradle.api.internal.component.SoftwareComponentContainerInternal;
 import org.gradle.api.internal.plugins.DefaultArtifactPublicationSet;
-import org.gradle.api.internal.plugins.DslObject;
 import org.gradle.api.internal.project.ProjectInternal;
 import org.gradle.api.internal.tasks.JvmConstants;
 import org.gradle.api.model.ObjectFactory;
@@ -392,7 +391,7 @@ public abstract class JavaPlugin implements Plugin<Project> {
 
     private static void configureDiagnostics(Project project, JvmFeatureInternal mainFeature) {
         project.getTasks().withType(DependencyInsightReportTask.class).configureEach(task -> {
-            new DslObject(task).getConventionMapping().map("configuration", mainFeature::getCompileClasspathConfiguration);
+            task.getConfiguration().convention(mainFeature.getCompileClasspathConfiguration());
         });
     }
 
