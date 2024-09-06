0605c42c2dfb503e6b342fa1a60d619dcccbe46e
Alex Semin <asemin@gradle.com>
2024-09-06 13:56:58 +0200

    Fix file-operations dependencies

--- platforms/core-configuration/file-operations/build.gradle.kts
+++ platforms/core-configuration/file-operations/build.gradle.kts
@@ -20,8 +20,42 @@ plugins {
 
 description = "Operations on files and directories"
 
+errorprone {
+    disabledChecks.addAll(
+        "ImmutableEnumChecker", // 2 occurrences
+        "InlineMeSuggester", // 1 occurrences
+        "InconsistentCapitalization", // 2 occurrences
+        "ReferenceEquality", // 2 occurrences
+        "OperatorPrecedence", // 5 occurrences
+    )
+}
+
+strictCompile {
+    ignoreRawTypes() // some raw types remain, but must be cleaned up
+}
+
 dependencies {
+    api(projects.baseServices)
     api(projects.coreApi)
-    api(projects.files)
     api(projects.fileCollections)
+    api(projects.fileTemp)
+    api(projects.files)
+    api(projects.hashing)
+    api(projects.logging)
+    api(projects.modelCore)
+    api(projects.native)
+    api(projects.persistentCache)
+    api(projects.resources)
+    api(projects.serviceLookup)
+
+    api(libs.ant)
+    api(libs.commonsCompress)
+    api(libs.groovy)
+    api(libs.guava)
+    api(libs.inject)
+    api(libs.jsr305)
+
+    implementation(libs.commonsIo)
+    implementation(libs.groovyTemplates)
+    implementation(libs.slf4jApi)
 }
