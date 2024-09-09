cbbba3aab3d1a0ad3dbd01f5242e8eedd5bf86be
Alex Semin <asemin@gradle.com>
2024-09-10 00:55:41 +0400

    Polish file-operations build script

--- platforms/core-configuration/file-operations/build.gradle.kts
+++ platforms/core-configuration/file-operations/build.gradle.kts
@@ -18,7 +18,12 @@ plugins {
     id("gradlebuild.distribution.api-java")
 }
 
-description = "Operations on files and directories"
+description = "Operations on files, such as archiving, copying, deleting"
+
+// TODO: ensure code is compliant with requirements for workers
+// This project was part of :core and similarly is included in the Worker runtime classpath (see WorkerProcessClassPathProvider)
+// So it should either be compliant or split into parts that are used and not used for workers.
+//gradlebuildJava.usedInWorkers()
 
 errorprone {
     disabledChecks.addAll(
