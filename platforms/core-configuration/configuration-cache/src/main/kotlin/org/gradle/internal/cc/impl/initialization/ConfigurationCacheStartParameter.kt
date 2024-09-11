97c68c50947902b652e712ed2af9ebadc9c8f8ec
Rafael Chaves <rchaves@gradle.com>
2024-09-11 08:01:13 -0300

    Show in the console whether stats collection was enabled

--- platforms/core-configuration/configuration-cache/src/main/kotlin/org/gradle/internal/cc/impl/initialization/ConfigurationCacheStartParameter.kt
+++ platforms/core-configuration/configuration-cache/src/main/kotlin/org/gradle/internal/cc/impl/initialization/ConfigurationCacheStartParameter.kt
@@ -104,7 +104,11 @@ class ConfigurationCacheStartParameter internal constructor(
      */
     val isParallelLoad = options.getInternalFlag("org.gradle.configuration-cache.internal.parallel-load", true)
 
-    val collectStats = options.getInternalFlag("org.gradle.configuration-cache.internal.collect-stats", false)
+    val collectStats = options.getInternalFlag("org.gradle.configuration-cache.internal.collect-stats", false).also { enabled ->
+        if (enabled) {
+            println("CC object stats collection enabled")
+        }
+    }
 
     val gradleProperties: Map<String, Any?>
         get() = startParameter.projectProperties
