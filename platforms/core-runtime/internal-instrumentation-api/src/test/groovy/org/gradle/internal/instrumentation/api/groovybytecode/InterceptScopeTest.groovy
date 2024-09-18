bf4d4419ea54e8c9892527ad92d20b9873e860f3
Anže Sodja <asodja@gradle.com>
2024-09-18 12:11:01 +0200

    Move InterceptScopeTest to internal-instrumentation-api project
    Since tested class is there.

--- platforms/core-runtime/internal-instrumentation-api/src/test/groovy/org/gradle/internal/instrumentation/api/groovybytecode/InterceptScopeTest.groovy
+++ platforms/core-runtime/internal-instrumentation-api/src/test/groovy/org/gradle/internal/instrumentation/api/groovybytecode/InterceptScopeTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the original author or authors.
+ * Copyright 2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-package org.gradle.internal.classpath.intercept
+package org.gradle.internal.instrumentation.api.groovybytecode
 
-import org.gradle.internal.instrumentation.api.groovybytecode.InterceptScope
+
 import spock.lang.Specification
 
 class InterceptScopeTest extends Specification {
