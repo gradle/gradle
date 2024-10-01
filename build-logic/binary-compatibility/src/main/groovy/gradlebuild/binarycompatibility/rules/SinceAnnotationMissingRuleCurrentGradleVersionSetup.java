799c41c981ae345299d0ee68c9bb0ce5e01bf0a8
Anže Sodja <asodja@gradle.com>
2024-10-01 20:07:11 +0200

    [infra] Compare EAP branch to latest release

--- build-logic/binary-compatibility/src/main/groovy/gradlebuild/binarycompatibility/rules/SinceAnnotationMissingRuleCurrentGradleVersionSetup.java
+++ build-logic/binary-compatibility/src/main/groovy/gradlebuild/binarycompatibility/rules/SinceAnnotationMissingRuleCurrentGradleVersionSetup.java
@@ -24,9 +24,12 @@ import java.util.Map;
 public class SinceAnnotationMissingRuleCurrentGradleVersionSetup implements SetupRule {
 
     private final String currentVersion;
+    private final String currentMasterVersion;
 
     public SinceAnnotationMissingRuleCurrentGradleVersionSetup(Map<String, String> currentVersion) {
         this.currentVersion = currentVersion.get("currentVersion");
+        // Version that is used as "current version" on master, required only for EAP branch
+        this.currentMasterVersion = getCurrentMasterVersion(currentVersion.get("baselineVersion"));
     }
 
     @Override
@@ -34,6 +37,17 @@ public class SinceAnnotationMissingRuleCurrentGradleVersionSetup implements Setu
     public void execute(ViolationCheckContext context) {
         Map<String, Object> userData = (Map<String, Object>) context.getUserData();
         userData.put("currentVersion", currentVersion);
+        userData.put("currentMasterVersion", currentMasterVersion);
     }
 
+    private static String getCurrentMasterVersion(String baselineVersion) {
+        if (baselineVersion == null || baselineVersion.isEmpty() || baselineVersion.split("[.]").length < 2) {
+            throw new IllegalArgumentException("Baseline version must be provided in x.y[.z] format");
+        }
+        String[] versions = baselineVersion.split("[.]");
+        String majorVersion = versions[0];
+        // Minor version can have a suffix, e.g. 8.11-20241004012543+0000
+        String minorVersion = versions[1].split("-")[0];
+        return majorVersion + "." + (Integer.parseInt(minorVersion) + 1);
+    }
 }
