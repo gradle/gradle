bec160ad1be870e659d9e0836634006328595036
Sergey Opivalov <sopivalov@gradle.com>
2024-10-14 19:02:08 +0300

    Forbid cycles in included plugin builds definition in IP mode
    IP mode implies parallel configuration, what is leading to possible deadlocks during plugin resolution from included builds

--- platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/isolated/IsolatedProjectsCompositeBuildIntegrationTest.groovy
+++ platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/isolated/IsolatedProjectsCompositeBuildIntegrationTest.groovy
@@ -16,6 +16,10 @@
 
 package org.gradle.internal.cc.impl.isolated
 
+import org.gradle.test.fixtures.file.TestFile
+
+import java.util.function.Consumer
+
 class IsolatedProjectsCompositeBuildIntegrationTest extends AbstractIsolatedProjectsIntegrationTest {
     def "can build libraries composed from multiple builds"() {
         settingsFile << """
@@ -46,5 +50,123 @@ class IsolatedProjectsCompositeBuildIntegrationTest extends AbstractIsolatedProj
 
         then:
         fixture.assertStateLoaded()
+    }
+
+    def "cycles for plugin builds are prohibited"() {
+        given:
+        includePluginBuild(settingsFile, "plugins-a")
+        includedBuild("plugins-a") { layout ->
+            includePluginBuild(layout.settingsScript, "../plugins-b")
+        }
+        includedBuild("plugins-b") { layout ->
+            includePluginBuild(layout.settingsScript, "../plugins-c")
+        }
+        includedBuild("plugins-c") { layout ->
+            includePluginBuild(layout.settingsScript, "../plugins-a")
+        }
+
+        when:
+        isolatedProjectsFails("help")
+
+        then:
+        failureDescriptionContains("Cycle detected in the included builds definition: :plugins-c -> :plugins-a -> :plugins-b -> :plugins-c")
+    }
+
+    def "transitive cycles for plugin builds are prohibited"() {
+        given:
+        includePluginBuild(settingsFile, "plugins-a")
+        includedBuild("plugins-a") { layout ->
+            includeLibraryBuild(layout.settingsScript, "../library-b")
+        }
+        includedBuild("library-b") { layout ->
+            includeLibraryBuild(layout.settingsScript, "../library-c")
+        }
+        includedBuild("library-c") { layout ->
+            includePluginBuild(layout.settingsScript, "../plugins-a")
+        }
+
+        when:
+        isolatedProjectsFails("help")
+
+        then:
+        failureDescriptionContains("Cycle detected in the included builds definition: :library-c -> :plugins-a -> :library-b -> :library-c")
+    }
+
+    def "introduced-by-settings-plugin cycles for plugins builds are prohibited"() {
+        given:
+        includedBuild("settings-plugins") { layout ->
+            layout.buildScript << """
+                plugins {
+                    id("groovy-gradle-plugin")
+                }
+            """
+            layout.srcMainGroovy.file("my-plugin.settings.gradle") << """
+                pluginManagement {
+                    includeBuild("../build-logic")
+                }
+            """
+        }
+        includedBuild("build-logic") { layout ->
+            includePluginBuild(layout.settingsScript, "../settings-plugins")
+            layout.settingsScript << """
+                plugins {
+                    id("my-plugin")
+                }
+            """
+        }
+        includePluginBuild(settingsFile, "build-logic")
+
+        when:
+        isolatedProjectsFails("help")
+
+        then:
+        failureDescriptionContains("Cycle detected in the included builds definition: :build-logic -> :build-logic")
+    }
+
+    def "cycles for library builds are allowed"() {
+        given:
+        includeLibraryBuild(settingsFile, "library-a")
+        includedBuild("library-a") { layout ->
+            includeLibraryBuild(layout.settingsScript, "../library-b")
+        }
+        includedBuild("library-b") { layout ->
+            includeLibraryBuild(layout.settingsScript, "../library-c")
+        }
+        includedBuild("library-c") { layout ->
+            includeLibraryBuild(layout.settingsScript, "../library-a")
+        }
+
+        expect:
+        isolatedProjectsRun("help")
+    }
+
+    private def includedBuild(String root, Consumer<BuildLayout> configure) {
+        configure(new BuildLayout(file("$root/settings.gradle"), file("$root/build.gradle"), file("$root/src/main/groovy")))
+    }
+
+    private static def includeLibraryBuild(TestFile settingsFile, String build) {
+        settingsFile << """
+            includeBuild("$build")
+       """
+    }
+
+    private static def includePluginBuild(TestFile settingsFile, String build) {
+        settingsFile << """
+            pluginManagement {
+                includeBuild("$build")
+            }
+        """
+    }
+
+    private class BuildLayout {
+        TestFile settingsScript
+        TestFile buildScript
+        TestFile srcMainGroovy
+
+        BuildLayout(TestFile settingsScript, TestFile buildScript, TestFile srcMainGroovy) {
+            this.settingsScript = settingsScript
+            this.buildScript = buildScript
+            this.srcMainGroovy = srcMainGroovy
+        }
     }
 }
