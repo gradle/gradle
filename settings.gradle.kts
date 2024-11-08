ffb1fe62ec0b12f137536aaa14e8ac0f5d863ce0
Rafael Chaves <rchaves@gradle.com>
2024-11-08 16:45:37 -0300

    Upgrade BuildCache properties to lazy

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -318,8 +318,6 @@ FeaturePreviews.Feature.values().forEach { feature ->
     }
 }
 
-fun remoteBuildCacheEnabled(settings: Settings) = settings.buildCache.remote?.isEnabled == true
-
 fun getBuildJavaHome() = System.getProperty("java.home")
 
 gradle.settingsEvaluated {
