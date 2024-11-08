4956ac1730b454ae584793a7f61e7d6c2ef7f6e7
Donát Csikós <donat@gradle.com>
2024-11-08 11:37:34 +0100

    Upgrade DebugOptions to use lazy properties

--- platforms/jvm/language-java/src/test/groovy/org/gradle/api/tasks/compile/DebugOptionsTest.groovy
+++ platforms/jvm/language-java/src/test/groovy/org/gradle/api/tasks/compile/DebugOptionsTest.groovy
@@ -24,13 +24,13 @@ class DebugOptionsTest {
 
     @Test
     void testDebugOptions() {
-        assert debugOptions.debugLevel == null
+        assert !debugOptions.debugLevel.isPresent()
     }
 
     @Test
     void testDefine() {
         debugOptions.debugLevel = null
         debugOptions.define(debugLevel: "extreme")
-        assert debugOptions.debugLevel == "extreme"
+        assert debugOptions.debugLevel.get() == "extreme"
     }
 }
