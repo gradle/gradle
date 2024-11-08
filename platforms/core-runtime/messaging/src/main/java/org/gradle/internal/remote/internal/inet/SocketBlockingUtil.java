5d2b746dc445d2a344900db6cd666ab9cc721d71
Sterling Greene <sterling@gradle.com>
2024-11-08 01:15:00 -0500

    Add extra diagnostics for when configureBlocking fails

--- platforms/core-runtime/messaging/src/main/java/org/gradle/internal/remote/internal/inet/SocketBlockingUtil.java
+++ platforms/core-runtime/messaging/src/main/java/org/gradle/internal/remote/internal/inet/SocketBlockingUtil.java
@@ -16,16 +16,27 @@
 
 package org.gradle.internal.remote.internal.inet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.channels.SocketChannel;
 
 class SocketBlockingUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SocketBlockingUtil.class);
+
     private SocketBlockingUtil() {
     }
 
     static void configureNonblocking(SocketChannel socket) throws IOException {
         // NOTE: we use non-blocking IO as there is no reliable way when using blocking IO to shutdown reads while
         // keeping writes active. For example, Socket.shutdownInput() does not work on Windows.
-        socket.configureBlocking(false);
+        try {
+            socket.configureBlocking(false);
+        } catch (IOException e) {
+            LOGGER.info("socket details: {}", socket);
+            LOGGER.info("Failed to configure socket to non-blocking mode.", e);
+            throw e;
+        }
     }
 }
