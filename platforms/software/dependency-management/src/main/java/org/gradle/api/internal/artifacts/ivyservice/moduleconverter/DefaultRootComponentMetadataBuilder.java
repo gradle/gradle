b4b760df2bf32948192358458ffb2fdbb73de86f
Justin Van Dort <jvandort@gradle.com>
2024-11-08 17:12:35 -0500

    temp

--- platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/moduleconverter/DefaultRootComponentMetadataBuilder.java
+++ platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/ivyservice/moduleconverter/DefaultRootComponentMetadataBuilder.java
@@ -95,6 +95,21 @@ public class DefaultRootComponentMetadataBuilder implements RootComponentMetadat
 
         LocalComponentGraphResolveState rootComponent = getComponentState(owner, metadata);
 
+
+        // TODO: It would be nice if we could calculate the rootVariant once, but it is possible
+        // that the root component changes between build dependency resolution and complete
+        // graph resolution. In 9.0, these changes will be forbidden.
+
+        // TODO: We should not ask the component for a resolvable configuration. Components should only
+        // expose variants -- which are by definition consumable only. Instead, we should create our own
+        // root variant and add it to a new one-off root component that holds only that root variant.
+        // The root variant should not live in a standard local component alongside other (consumable) variants.
+        @SuppressWarnings("deprecation")
+        LocalVariantGraphResolveState rootVariant = rootComponent.getConfigurationLegacy(configurationName);
+        if (rootVariant == null) {
+            throw new IllegalStateException(String.format("Expected root variant '%s' to be present in %s", configurationName, componentIdentifier));
+        }
+
         return new RootComponentState() {
             @Override
             public LocalComponentGraphResolveState getRootComponent() {
@@ -103,19 +118,6 @@ public class DefaultRootComponentMetadataBuilder implements RootComponentMetadat
 
             @Override
             public LocalVariantGraphResolveState getRootVariant() {
-                // TODO: It would be nice if we could calculate the rootVariant once, but it is possible
-                // that the root component changes between build dependency resolution and complete
-                // graph resolution. In 9.0, these changes will be forbidden.
-
-                // TODO: We should not ask the component for a resolvable configuration. Components should only
-                // expose variants -- which are by definition consumable only. Instead, we should create our own
-                // root variant and add it to a new one-off root component that holds only that root variant.
-                // The root variant should not live in a standard local component alongside other (consumable) variants.
-                @SuppressWarnings("deprecation")
-                LocalVariantGraphResolveState rootVariant = rootComponent.getConfigurationLegacy(configurationName);
-                if (rootVariant == null) {
-                    throw new IllegalStateException(String.format("Expected root variant '%s' to be present in %s", configurationName, componentIdentifier));
-                }
                 return rootVariant;
             }
         };
