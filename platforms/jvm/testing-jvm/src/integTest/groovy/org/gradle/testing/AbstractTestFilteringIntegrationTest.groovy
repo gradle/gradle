b621762867acb587dcb8e4cbef7f47c77a1e4913
Anže Sodja <asodja@gradle.com>
2024-11-27 15:27:57 +0100

    Upgrade TestFilter to use lazy properties

--- platforms/jvm/testing-jvm/src/integTest/groovy/org/gradle/testing/AbstractTestFilteringIntegrationTest.groovy
+++ platforms/jvm/testing-jvm/src/integTest/groovy/org/gradle/testing/AbstractTestFilteringIntegrationTest.groovy
@@ -212,7 +212,7 @@ abstract class AbstractTestFilteringIntegrationTest extends AbstractTestingMulti
     def "executes multiple methods from different classes"() {
         buildFile << """
             test {
-              filter.setIncludePatterns 'Foo*.pass*'
+              filter.includePatterns = ['Foo*.pass*']
             }
         """
         file("src/test/java/Foo1Test.java") << """
