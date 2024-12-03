0a8a6389798b8776c507959511684c34257b9a5b
Louis Jacomet <louis@gradle.com>
2024-12-03 11:44:39 +0100

    Generate deprecated method when source is deprecated
    Part of our decoration logic adds Closure taking methods for the Groovy
    DSL when the Gradle API takes an Action.
    
    These methods should be generated with a @Deprecated annotation when the
    source method is itself deprecated.
    
    While this does not have a direct user impact, if we ever publish
    decorated DSL APIs, doing this would make them more correct.

--- platforms/core-configuration/model-core/src/main/java/org/gradle/internal/instantiation/generator/AsmBackedClassGenerator.java
+++ platforms/core-configuration/model-core/src/main/java/org/gradle/internal/instantiation/generator/AsmBackedClassGenerator.java
@@ -1672,6 +1672,8 @@ public class AsmBackedClassGenerator extends AbstractClassGenerator {
                 return;
             }
 
+            boolean deprecated = method.getAnnotation(Deprecated.class) != null;
+
             Type returnType = getType(method.getReturnType());
 
             Type[] originalParameterTypes = collectArray(method.getParameterTypes(), Type.class, Type::getType);
@@ -1686,6 +1688,10 @@ public class AsmBackedClassGenerator extends AbstractClassGenerator {
 
             // GENERATE public <return type> <method>(Closure v) { return <method>(…, ConfigureUtil.configureUsing(v)); }
             publicMethod(method.getName(), methodDescriptor, methodVisitor -> new MethodVisitorScope(methodVisitor) {{
+
+                if (deprecated) {
+                    visitAnnotation("Ljava/lang/Deprecated;", true).visitEnd();
+                }
 
                 // GENERATE <method>(…, ConfigureUtil.configureUsing(v));
                 _ALOAD(0);
