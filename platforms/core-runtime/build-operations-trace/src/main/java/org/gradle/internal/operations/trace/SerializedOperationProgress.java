1531e0bd61d4518205ba6657707802996d7f19ac
Tom Tresansky <ttresansky@gradle.com>
2024-12-03 09:52:56 -0500

    Use Number for JSON deserialization to simplify code and handle -1 sentinels transparently

--- platforms/core-runtime/build-operations-trace/src/main/java/org/gradle/internal/operations/trace/SerializedOperationProgress.java
+++ platforms/core-runtime/build-operations-trace/src/main/java/org/gradle/internal/operations/trace/SerializedOperationProgress.java
@@ -40,8 +40,8 @@ class SerializedOperationProgress implements SerializedOperation {
     }
 
     SerializedOperationProgress(Map<String, ?> map) {
-        this.id = ((Integer) map.get("id")).longValue();
-        this.time = map.get("time") instanceof Integer ? -1L : (Long) map.get("time"); // JSON serialization read the -1 sentinel value as an Int
+        this.id = ((Number) map.get("id")).longValue();
+        this.time = ((Number) map.get("time")).longValue();
         this.details = map.get("details");
         this.detailsClassName = (String) map.get("detailsClassName");
     }
