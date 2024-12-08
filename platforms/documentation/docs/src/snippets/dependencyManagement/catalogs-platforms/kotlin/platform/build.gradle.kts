51936eee266b358a447328921bfb3fd16482d32a
Semyon Gashchenko <sgashchenko@tempo.io>
2024-12-08 17:04:19 +0100

    Fix 'Using a catalog with a platform' code snippet
    Signed-off-by: Semyon Gashchenko <sgashchenko@tempo.io>

--- platforms/documentation/docs/src/snippets/dependencyManagement/catalogs-platforms/kotlin/platform/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/dependencyManagement/catalogs-platforms/kotlin/platform/build.gradle.kts
@@ -8,7 +8,7 @@ javaPlatform {
 
 dependencies {
     constraints {
-        api("org.junit.jupiter:junit-jupiter:5.11.1") // Enforcing version range
-        api("com.google.guava:guava:[33.1.0-jre,)") // Enforcing specific version
+        api("org.junit.jupiter:junit-jupiter:5.11.1") // Enforcing specific version
+        api("com.google.guava:guava:[33.1.0-jre,)") // Enforcing version range
     }
 }
