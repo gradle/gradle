89a4365cfb2504ba6ea574796239001e834e600b
Bálint Hegyi <bhegyi@gradle.com>
2025-01-03 10:49:52 +0000

    Expand testing

--- platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r813/ProviderOperationParametersCrossVersionTest.groovy
+++ platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r813/ProviderOperationParametersCrossVersionTest.groovy
@@ -29,7 +29,7 @@ import org.gradle.tooling.ProjectConnection
     value = IntegTestPreconditions.NotEmbeddedExecutor,
     reason = "In order to pass JVM arguments to the Gradle daemon, we need to use the external executor."
 )
-class AbstractLongRunningOperationCrossVersionTest extends ToolingApiSpecification {
+class ProviderOperationParametersCrossVersionTest extends ToolingApiSpecification {
 
     def setup() {
         buildFile """
@@ -51,7 +51,7 @@ file("system-properties.txt").text = System.getProperties().keySet().join("\\n")
 
     def "#addJvmArguments should not reset properties defined in project gradle properties"() {
         given:
-        propertiesFile << "org.gradle.jvmargs=-Dgradle-properties-arg -Xmx2g"
+        propertiesFile << "org.gradle.jvmargs=-Dgradle-properties-arg"
 
         when:
         withConnection { ProjectConnection connection ->
@@ -63,7 +63,6 @@ file("system-properties.txt").text = System.getProperties().keySet().join("\\n")
         }
 
         then:
-        def jvmArgs = file("jvm-args.txt").text.split("\n")
         def sysProperties = file("system-properties.txt").text.split("\n")
         verifyAll {
             sysProperties.contains("gradle-properties-arg")
@@ -71,10 +70,32 @@ file("system-properties.txt").text = System.getProperties().keySet().join("\\n")
         }
     }
 
-    def "#addJvmArguments should not reset properties defined user home gradle properties"() {
+    def "#addJvmArguments should not reset properties defined in user home gradle properties"() {
         given:
         requireIsolatedUserHome()
-        file("user-home-dir").file("gradle.properties") << "org.gradle.jvmargs=-Dgradle-properties-arg -Xmx2g"
+        file("user-home-dir").file("gradle.properties") << "org.gradle.jvmargs=-Dgradle-properties-arg"
+
+        when:
+        withConnection { ProjectConnection connection ->
+            connection
+                .newBuild()
+                .forTasks("help")
+                .addJvmArguments("-Dadd-jvm-arg")
+                .run()
+        }
+
+        then:
+        def sysProperties = file("system-properties.txt").text.split("\n")
+        verifyAll {
+            sysProperties.contains("gradle-properties-arg")
+            sysProperties.contains("add-jvm-arg")
+        }
+    }
+
+    def "#addJvmArguments should not inflict immutable properties other properties defined in user home gradle properties"() {
+        given:
+        requireIsolatedUserHome()
+        file("user-home-dir").file("gradle.properties") << "org.gradle.jvmargs=-Dgradle-properties-arg -Duser.language=xx"
 
         when:
         withConnection { ProjectConnection connection ->
@@ -87,6 +108,30 @@ file("system-properties.txt").text = System.getProperties().keySet().join("\\n")
 
         then:
         def jvmArgs = file("jvm-args.txt").text.split("\n")
+        jvmArgs.contains("-Duser.language=xx")
+
+        def sysProperties = file("system-properties.txt").text.split("\n")
+        verifyAll {
+            sysProperties.contains("gradle-properties-arg")
+            sysProperties.contains("add-jvm-arg")
+            sysProperties.contains("gradle-properties-arg")
+        }
+    }
+
+    def "#addJvmArguments should not reset properties defined in project gradle properties when using a provider operation"() {
+        given:
+        propertiesFile << "org.gradle.jvmargs=-Dgradle-properties-arg"
+
+        when:
+        withConnection { ProjectConnection connection ->
+            connection
+                .newBuild()
+                .forTasks("help")
+                .addJvmArguments("-Dadd-jvm-arg")
+                .run()
+        }
+
+        then:
         def sysProperties = file("system-properties.txt").text.split("\n")
         verifyAll {
             sysProperties.contains("gradle-properties-arg")
