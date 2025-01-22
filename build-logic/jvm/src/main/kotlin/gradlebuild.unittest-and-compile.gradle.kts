b2f24c73c037f967de3fd0cd08c5820c676122ec
Christoph Obexer <cobexer@gradle.com>
2025-01-22 14:57:06 +0100

    Drop explicitly configuring Java toolchains after selecting an appropriate one via daemon toolchains
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
+++ build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
@@ -84,10 +84,6 @@ val gradlebuildJava = extensions.create<UnitTestAndCompileExtension>("gradlebuil
     usesJdkInternals.convention(false)
 }
 
-// Use the Java 17 compiler, when possible, to perform compilation.
-// This does not mean we target Java 17 bytecode. The target bytecode
-// is controlled by the `gradlebuildJava.targetVersion` property.
-configureDefaultToolchain(17)
 enforceCompatibility(gradlebuildJava)
 
 removeTeamcityTempProperty()
@@ -107,22 +103,6 @@ fun configureCompileDefaults() {
     tasks.withType<GroovyCompile>().configureEach {
         groovyOptions.encoding = "utf-8"
         configureCompileTask(options)
-    }
-}
-
-/**
- * When possible, use a Java compiler with the given version when
- * performing compilation. This does not necessarily mean we are
- * emitting bytecode for the given version.
- * <p>
- * In some cases, the toolchain used here may be overridden, for
- * example when compiling Groovy code, as it does not support
- * the --release flag, or when targeting much older bytecode versions.
- */
-fun configureDefaultToolchain(toolchainVersion: Int) {
-    java.toolchain {
-        languageVersion = JavaLanguageVersion.of(toolchainVersion)
-        vendor = JvmVendorSpec.ADOPTIUM
     }
 }
 
@@ -186,7 +166,6 @@ fun enforceJavaCompatibility(targetVersion: Provider<Int>, useRelease: Provider<
                     provider { JavaLanguageVersion.of(11) }
                 }
             }
-            vendor = JvmVendorSpec.ADOPTIUM
         }
     }
 
@@ -368,7 +347,9 @@ fun Test.configureJvmForTest() {
     jvmArgumentProviders.add(CiEnvironmentProvider(this))
     val launcher = project.javaToolchains.launcherFor {
         languageVersion = jvmVersionForTest()
-        vendor = project.testJavaVendor.orElse(JvmVendorSpec.ADOPTIUM)
+        if (project.testJavaVendor.isPresent) {
+            vendor = project.testJavaVendor
+        }
     }
     javaLauncher = launcher
     if (jvmVersionForTest().canCompileOrRun(9)) {
