680151b0bf5ae8cbbb808f4832cc105c374a1cc6
Justin Van Dort <jvandort@gradle.com>
2025-01-27 14:08:57 -0500

    Document the scalaVersion property
    Add section recomending the new property to declare the scala version
    Update existing snippets to use the new property and use consistent syntax across Groovy and Kotlin

--- platforms/documentation/docs/src/snippets/scala/compilerPlugin/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/scala/compilerPlugin/kotlin/build.gradle.kts
@@ -1,14 +1,17 @@
 plugins {
-    scala
+    id("scala")
 }
 
 repositories {
     mavenCentral()
 }
 
+scala {
+    scalaVersion = "2.13.12"
+}
+
 // tag::compiler-plugin[]
 dependencies {
-    implementation("org.scala-lang:scala-library:2.13.12")
     scalaCompilerPlugins("org.typelevel:kind-projector_2.13.12:0.13.2")
 }
 // end::compiler-plugin[]
