63d9b5413b22e518d3cb9d8345b77a3840c677b6
Laura Kassovic <lkassovic@gradle.com>
2025-01-27 22:35:49 -0800

    fix checkstyle error and start fixing duplicate ids error

--- build-logic/documentation/src/main/groovy/gradlebuild/docs/GradleReleaseNotesPlugin.java
+++ build-logic/documentation/src/main/groovy/gradlebuild/docs/GradleReleaseNotesPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,6 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import java.nio.charset.Charset;
-import java.time.LocalDate;
 
 /**
  * Opinionated plugin that generates the release notes for a Gradle release.
