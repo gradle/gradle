23ec28d43ce2c6b0a941214f493f2b8ad4fc9543
Mikhail Lopatkin <mlopatkin@gradle.com>
2025-01-28 15:36:40 +0100

    Support += for ConfigurableFileCollections in Kotlin DSL
    This is a backwards-compatible change. While FileCollection supported
    plus for a while, += can only fall back to that when the target is a var
    of a FileCollection type, which doesn't trigger new += behavior. A var
    of ConfigurableFileCollection type could not be +=, because the result
    of + is a FileCollection, which isn't assignable back to the variable.

--- platforms/core-configuration/kotlin-dsl/src/test/kotlin/org/gradle/kotlin/dsl/ConfigurableFileCollectionExtensionsTest.kt
+++ platforms/core-configuration/kotlin-dsl/src/test/kotlin/org/gradle/kotlin/dsl/ConfigurableFileCollectionExtensionsTest.kt
@@ -1,11 +1,12 @@
 package org.gradle.kotlin.dsl
 
 import org.gradle.api.file.ConfigurableFileCollection
-
+import org.gradle.api.file.FileCollection
+import org.hamcrest.CoreMatchers.not
 import org.hamcrest.CoreMatchers.sameInstance
 import org.hamcrest.MatcherAssert.assertThat
-
 import org.junit.Test
+import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
@@ -23,5 +24,33 @@ class ConfigurableFileCollectionExtensionsTest {
 
         verify(fileCollection).setFrom(value)
         assertThat(delegatedProperty, sameInstance(fileCollection))
+    }
+
+    @Test
+    fun `plusAssign falls back to plus when used with FileCollection var`() {
+        val orig = mock<ConfigurableFileCollection> {
+            on { plus(org.mockito.kotlin.any()) }.doAnswer { mock<FileCollection>() }
+        }
+
+        val b = mock<FileCollection>()
+
+        var a: FileCollection = orig
+        a += b
+
+        verify(orig).plus(b)
+        assertThat(a, not(sameInstance(orig)))
+    }
+
+    @Test
+    fun `plusAssign modifies ConfigurableFileCollection in-place`() {
+        val orig = mock<ConfigurableFileCollection>()
+        val b = mock<FileCollection>()
+
+        @Suppress("CanBeVal")
+        var a = orig
+        a += b
+
+        verify(orig).from(b)
+        assertThat(a, sameInstance(orig))
     }
 }
