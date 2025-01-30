64ddfdfafba70ca2d1b36112d6f51e5ad4971057
Laura Kassovic <lkassovic@gradle.com>
2025-01-29 22:27:09 -0800

    update distro size

--- testing/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegritySpec.groovy
+++ testing/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegritySpec.groovy
@@ -35,7 +35,7 @@ class DistributionIntegritySpec extends DistributionIntegrationSpec {
 
     @Override
     int getMaxDistributionSizeBytes() {
-        return 135 * 1024 * 1024
+        return 136 * 1024 * 1024
     }
 
     /**
