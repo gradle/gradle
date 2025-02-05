8b5f5391bf7d9730129f669992b3cc0df24e6e7a
Paul Merlin <paul@gradle.com>
2025-02-05 10:20:56 +0100

    Move :distributions-core to testing/
    This distribution is only used for testing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -87,7 +87,6 @@ val core = platform("core") {
         subproject("daemon-protocol")
         subproject("daemon-services")
         subproject("daemon-server")
-        subproject("distributions-core")
         subproject("file-temp")
         subproject("files")
         subproject("functional")
@@ -298,6 +297,7 @@ module("enterprise") {
 testing {
     subproject("architecture-test")
     subproject("distributions-basics")
+    subproject("distributions-core")
     subproject("distributions-integ-tests")
     subproject("integ-test")
     subproject("internal-architecture-testing")
