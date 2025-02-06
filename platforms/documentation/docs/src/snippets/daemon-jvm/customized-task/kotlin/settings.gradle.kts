c313f33926f27db50e282ad92ca521d92663c091
Laura Kassovic <lkassovic@gradle.com>
2025-02-06 14:08:38 -0800

    clean up language and add jvm daemon criteria tests/snippets

--- platforms/documentation/docs/src/snippets/daemon-jvm/customized-task/kotlin/settings.gradle.kts
+++ platforms/documentation/docs/src/snippets/daemon-jvm/customized-task/kotlin/settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
 rootProject.name = "customized-task"
