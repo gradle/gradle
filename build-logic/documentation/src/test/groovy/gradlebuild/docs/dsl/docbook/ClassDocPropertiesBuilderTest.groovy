5912851343f53dad3cd06c435a98dd140542bc94
Laura Kassovic <lkassovic@gradle.com>
2025-02-12 20:45:37 -0800

    update tests

--- build-logic/documentation/src/test/groovy/gradlebuild/docs/dsl/docbook/ClassDocPropertiesBuilderTest.groovy
+++ build-logic/documentation/src/test/groovy/gradlebuild/docs/dsl/docbook/ClassDocPropertiesBuilderTest.groovy
@@ -63,12 +63,9 @@ class ClassDocPropertiesBuilderTest extends XmlSpecification {
         }
 
         then:
-        doc.classProperties.size() == 5
+        doc.classProperties.size() == 2
         doc.classProperties[0].name == 'a'
         doc.classProperties[1].name == 'b'
-        doc.classProperties[2].name == 'c'
-        doc.classProperties[3].name == 'd'
-        doc.classProperties[4].name == 'e'
 
         _ * classMetaData.findProperty('b') >> propertyB
         _ * classMetaData.findProperty('a') >> propertyA
@@ -76,76 +73,6 @@ class ClassDocPropertiesBuilderTest extends XmlSpecification {
         _ * superDoc.classProperties >> [propertyDocC, propertyDocA]
         _ * superType1.classProperties >> [propertyDoc('d')]
         _ * superType2.classProperties >> [propertyDoc('d'), propertyDoc('e')]
-    }
-
-    def canAttachAdditionalValuesToProperty() {
-        ClassMetaData classMetaData = classMetaData()
-        PropertyMetaData propertyA = property('a', classMetaData, comment: 'prop a')
-        PropertyMetaData propertyB = property('b', classMetaData, comment: 'prop b')
-        ClassDoc superDoc = classDoc()
-        ClassDoc superType = classDoc("org.gradle.SuperType")
-        ExtraAttributeDoc inheritedValue = new ExtraAttributeDoc(parse('<td>inherited</td>'), parse('<td>inherited</td>'))
-        ExtraAttributeDoc overriddenValue = new ExtraAttributeDoc(parse('<td>general value</td>'), parse('<td>general</td>'))
-        PropertyDoc inheritedPropertyA = propertyDoc('a', additionalValues: [inheritedValue, overriddenValue])
-        PropertyDoc inheritedPropertyB = propertyDoc('b', additionalValues: [inheritedValue, overriddenValue])
-        PropertyDoc inheritedPropertyC = propertyDoc('c', additionalValues: [inheritedValue, overriddenValue])
-
-        def content = parse('''
-<section>
-    <section><title>Properties</title>
-        <table>
-            <thead><tr><td>Name</td><td>inherited</td><td>added</td><td>overridden <overrides>general value</overrides></td></tr></thead>
-            <tr><td>a</td><td>specific1</td><td>specific2</td><td>specific3</td></tr>
-            <tr><td>b</td><td></td><td/><td/></tr>
-        </table>
-    </section>
-    <section><title>Methods</title><table><thead><tr></tr></thead></table></section>
-</section>
-''')
-
-        when:
-        ClassDoc doc = withCategories {
-            def doc = new ClassDoc('org.gradle.Class', content, document, classMetaData, null)
-            doc.superClass = superDoc
-            doc.interfaces << superType
-            builder.build(doc)
-            return doc
-        }
-
-        then:
-        doc.classProperties.size() == 3
-
-        def prop = doc.classProperties[0]
-        prop.name == 'a'
-        prop.additionalValues.size() == 3
-        format(prop.additionalValues[0].title) == 'inherited'
-        format(prop.additionalValues[0].value) == 'specific1'
-        format(prop.additionalValues[1].title) == 'overridden'
-        format(prop.additionalValues[1].value) == 'specific3'
-        format(prop.additionalValues[2].title) == 'added'
-        format(prop.additionalValues[2].value) == 'specific2'
-
-        def prop2 = doc.classProperties[1]
-        prop2.name == 'b'
-        prop2.additionalValues.size() == 2
-        format(prop2.additionalValues[0].title) == 'inherited'
-        format(prop2.additionalValues[0].value) == 'inherited'
-        format(prop2.additionalValues[1].title) == 'overridden'
-        format(prop2.additionalValues[1].value) == 'general'
-
-        def prop3 = doc.classProperties[2]
-        prop3.name == 'c'
-        prop3.additionalValues.size() == 2
-        format(prop3.additionalValues[0].title) == 'inherited'
-        format(prop3.additionalValues[0].value) == 'inherited'
-        format(prop3.additionalValues[1].title) == 'overridden'
-        format(prop3.additionalValues[1].value) == 'general'
-
-        _ * classMetaData.findProperty('b') >> propertyB
-        _ * classMetaData.findProperty('a') >> propertyA
-        _ * classMetaData.superClassName >> 'org.gradle.SuperType'
-        _ * superDoc.classProperties >> [inheritedPropertyA, inheritedPropertyB]
-        _ * superType.classProperties >> [inheritedPropertyC]
     }
 
     def classMetaData(String name = 'org.gradle.Class') {
