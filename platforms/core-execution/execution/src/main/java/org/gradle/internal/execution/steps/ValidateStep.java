8d63b65b14fb0ef3b68efe9920a6efb7f92580fa
Lóránt Pintér <lorant@gradle.com>
2025-02-17 15:05:34 +0100

    Also check dependencies in ValidateStep

--- platforms/core-execution/execution/src/main/java/org/gradle/internal/execution/steps/ValidateStep.java
+++ platforms/core-execution/execution/src/main/java/org/gradle/internal/execution/steps/ValidateStep.java
@@ -53,6 +53,7 @@ public class ValidateStep<C extends BeforeExecutionContext, R extends Result> im
     public R execute(UnitOfWork work, C context) {
         WorkValidationContext validationContext = context.getValidationContext();
         work.validate(validationContext);
+        work.checkDependencies(validationContext);
         context.getBeforeExecutionState()
             .ifPresent(beforeExecutionState -> validateImplementations(work, beforeExecutionState, validationContext));
 
