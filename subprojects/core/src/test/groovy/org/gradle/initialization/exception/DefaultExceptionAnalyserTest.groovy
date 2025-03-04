6bb67356bcde1dddcdf3ecc48941395ff55ce054
Stefan Wolf <wolf@gradle.com>
2025-03-04 23:33:08 +0100

    Fix DefaultExceptionAnalyserTest

--- subprojects/core/src/test/groovy/org/gradle/initialization/exception/DefaultExceptionAnalyserTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/initialization/exception/DefaultExceptionAnalyserTest.groovy
@@ -20,10 +20,13 @@ import org.gradle.api.ProjectConfigurationException
 import org.gradle.api.tasks.TaskExecutionException
 import org.gradle.groovy.scripts.ScriptSource
 import org.gradle.internal.Describables
+import org.gradle.internal.buildevents.ContextAwareExceptionHandler
 import org.gradle.internal.exceptions.Contextual
 import org.gradle.internal.exceptions.LocationAwareException
 import org.gradle.internal.exceptions.MultiCauseException
 import org.gradle.internal.exceptions.ResolutionProvider
+import org.gradle.internal.problems.failure.DefaultFailureFactory
+import org.gradle.internal.problems.failure.FailureFactory
 import org.gradle.problems.Location
 import org.gradle.problems.ProblemDiagnostics
 import org.gradle.problems.buildtree.ProblemDiagnosticsFactory
@@ -31,6 +34,7 @@ import spock.lang.Specification
 
 class DefaultExceptionAnalyserTest extends Specification {
     private final ProblemDiagnosticsFactory diagnosticsFactory = Stub(ProblemDiagnosticsFactory)
+    private final FailureFactory failureFactory = DefaultFailureFactory.withDefaultClassifier()
 
     def 'wraps original exception when it is not a contextual exception'() {
         given:
@@ -46,7 +50,7 @@ class DefaultExceptionAnalyserTest extends Specification {
         def transformedFailure = result[0]
         transformedFailure instanceof LocationAwareException
         transformedFailure.cause.is(failure)
-        // TODO: transformedFailure.reportableCauses.isEmpty()
+        getReportableCauses(transformedFailure).isEmpty()
     }
 
     def 'wraps contextual exception with location aware exception'() {
@@ -64,7 +68,7 @@ class DefaultExceptionAnalyserTest extends Specification {
         transformedFailure instanceof LocationAwareException
 
         transformedFailure.cause.is(failure)
-        // TODO: transformedFailure.reportableCauses.isEmpty()
+        getReportableCauses(transformedFailure).isEmpty()
     }
 
     def 'wraps highest contextual exception with location aware exception'() {
@@ -82,7 +86,7 @@ class DefaultExceptionAnalyserTest extends Specification {
         def transformedFailure = result[0]
         transformedFailure instanceof LocationAwareException
         transformedFailure.cause.is(failure)
-        // TODO: transformedFailure.reportableCauses == [cause]
+        getReportableCauses(transformedFailure) == [cause]
     }
 
     def 'adds location info from stack trace'() {
@@ -159,7 +163,7 @@ class DefaultExceptionAnalyserTest extends Specification {
         def transformedFailure = result[0]
         transformedFailure instanceof LocationAwareException
         transformedFailure.cause.is(failure)
-        // TODO: transformedFailure.reportableCauses == [cause1, cause2]
+        getReportableCauses(transformedFailure) == [cause1, cause2]
     }
 
     def 'uses original exception when it is already location aware'() {
@@ -333,6 +337,10 @@ class DefaultExceptionAnalyserTest extends Specification {
 
     private DefaultExceptionAnalyser analyser() {
         return new DefaultExceptionAnalyser(diagnosticsFactory)
+    }
+
+    private List<Throwable> getReportableCauses(Throwable e) {
+        ContextAwareExceptionHandler.getReportableCauses(failureFactory.create(e)).collect { it.original }
     }
 
     @Contextual
