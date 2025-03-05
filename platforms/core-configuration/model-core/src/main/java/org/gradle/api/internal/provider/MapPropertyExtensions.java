b3d26ccde626dd1f799d968833c795db880a0ff8
Sergey Opivalov <sopivalov@gradle.com>
2025-03-05 12:31:56 +0300

    Support `leftShift` for Collection and Map properties

--- platforms/core-configuration/model-core/src/main/java/org/gradle/api/internal/provider/MapPropertyExtensions.java
+++ platforms/core-configuration/model-core/src/main/java/org/gradle/api/internal/provider/MapPropertyExtensions.java
@@ -19,7 +19,12 @@ package org.gradle.api.internal.provider;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Provider;
 
-public class MapPropertyExtensions {
+import java.util.Map;
+
+@SuppressWarnings("unused") // registered as Groovy extension in ExtensionModule
+public final class MapPropertyExtensions {
+
+    private MapPropertyExtensions() {}
 
     /**
      * Returns a provider that resolves to the value of the mapping of the given key. It will have no value
@@ -78,5 +83,33 @@ public class MapPropertyExtensions {
      */
     public static <V> void propertyMissing(MapProperty<String, V> self, String key, Object value) {
         putAt(self, key, value);
+    }
+
+    /**
+     * Adds map entries to the property value.
+     *
+     * <p>Extension method to support the left shift operator in Groovy.</p>
+     *
+     * @param self the {@link MapProperty}
+     * @param entries the entries
+     * @return self
+     */
+    public static <K, V> MapProperty<K, V> leftShift(MapProperty<K, V> self, Map<? extends K, ? extends V> entries) {
+        self.putAll(entries);
+        return self;
+    }
+
+    /**
+     * Adds a provider of the map entries to the property value.
+     *
+     * <p>Extension method to support the left shift operator in Groovy.</p>
+     *
+     * @param self the {@link MapProperty}
+     * @param provider the entries
+     * @return self
+     */
+    public static <K, V> MapProperty<K, V> leftShift(MapProperty<K, V> self, Provider<? extends Map<? extends K, ? extends V>> provider) {
+        self.putAll(provider);
+        return self;
     }
 }
