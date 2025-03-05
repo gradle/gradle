f9923ca6e8ed49f674738562f4831ef199041bd3
Anže Sodja <asodja@gradle.com>
2025-03-05 17:43:57 +0100

    Use lazy properties in PluginDeclaration

--- platforms/extensibility/plugin-development/src/main/java/org/gradle/plugin/devel/plugins/JavaGradlePluginPlugin.java
+++ platforms/extensibility/plugin-development/src/main/java/org/gradle/plugin/devel/plugins/JavaGradlePluginPlugin.java
@@ -262,10 +262,10 @@ public abstract class JavaGradlePluginPlugin implements Plugin<Project> {
     private void validatePluginDeclarations(Project project, GradlePluginDevelopmentExtension extension) {
         project.afterEvaluate(evaluatedProject -> {
             for (PluginDeclaration declaration : extension.getPlugins()) {
-                if (declaration.getId() == null) {
+                if (!declaration.getId().isPresent()) {
                     throw new IllegalArgumentException(String.format(DECLARATION_MISSING_ID_MESSAGE, declaration.getName()));
                 }
-                if (declaration.getImplementationClass() == null) {
+                if (!declaration.getImplementationClass().isPresent()) {
                     throw new IllegalArgumentException(String.format(DECLARATION_MISSING_IMPLEMENTATION_MESSAGE, declaration.getName()));
                 }
             }
@@ -373,8 +373,8 @@ public abstract class JavaGradlePluginPlugin implements Plugin<Project> {
                     }
                 }
                 for (PluginDeclaration declaration : plugins.get()) {
-                    if (!pluginFileNames.contains(declaration.getId() + ".properties")) {
-                        LOGGER.warn(String.format(DECLARED_PLUGIN_MISSING_MESSAGE, task.getPath(), declaration.getName(), declaration.getId()));
+                    if (!pluginFileNames.contains(declaration.getId().get() + ".properties")) {
+                        LOGGER.warn(String.format(DECLARED_PLUGIN_MISSING_MESSAGE, task.getPath(), declaration.getName(), declaration.getId().get()));
                     }
                 }
             }
@@ -498,7 +498,7 @@ public abstract class JavaGradlePluginPlugin implements Plugin<Project> {
 
         @Override
         public PluginId getPluginId() {
-            return DefaultPluginId.of(pluginDeclaration.getId());
+            return DefaultPluginId.of(pluginDeclaration.getId().get());
         }
     }
 }
