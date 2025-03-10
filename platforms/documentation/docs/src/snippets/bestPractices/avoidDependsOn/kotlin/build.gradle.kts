fa614d7b0a3cc38f13bed5abb772882a6ca847f1
Tom Tresansky <ttresansky@gradle.com>
2025-03-10 11:14:21 -0400

    Update sample code to share exact same core logic

--- platforms/documentation/docs/src/snippets/bestPractices/avoidDependsOn/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/bestPractices/avoidDependsOn/kotlin/build.gradle.kts
@@ -1,45 +1,54 @@
-// tag::avoid-this[]
+// tag::depended-upon-task-setup[]
 abstract class SimplePrintingTask : DefaultTask() {
-    @get:Input
-    abstract val message: Property<String>
+    @get:OutputFile
+    abstract val messageFile: RegularFileProperty
 
-    @TaskAction
+    @get:OutputFile
+    abstract val audienceFile: RegularFileProperty
+
+    @TaskAction // <1>
     fun run() {
-        logger.lifecycle(message.get())
+        messageFile.get().asFile.writeText("Hello")
+        audienceFile.get().asFile.writeText("World")
     }
 }
 
-tasks.register<SimplePrintingTask>("task1") {
-    message = "Hello"
+tasks.register<SimplePrintingTask>("helloWorld") { // <2>
+    messageFile.set(layout.buildDirectory.file("message.txt"))
+    audienceFile.set(layout.buildDirectory.file("audience.txt"))
+}
+// end::depended-upon-task-setup[]
+
+// tag::avoid-this[]
+abstract class SimpleTranslationTask : DefaultTask() {
+    @get:InputFile
+    abstract val messageFile: RegularFileProperty
+
+    @get:OutputFile
+    abstract val translatedFile: RegularFileProperty
+
+    init {
+        messageFile.convention(project.layout.buildDirectory.file("message.txt"))
+        translatedFile.convention(project.layout.buildDirectory.file("translated.txt"))
+    }
+
+    @TaskAction // <1>
+    fun run() {
+        val message = messageFile.get().asFile.readText(Charsets.UTF_8)
+        val translatedMessage = if (message == "Hello") "Bonjour" else "Unknown"
+
+        logger.lifecycle("Translation: " + translatedMessage)
+        translatedFile.get().asFile.writeText(translatedMessage)
+    }
 }
 
-tasks.register<SimplePrintingTask>("task2") {
-    dependsOn(tasks.named("task1")) // <1>
-    message = "World"
+tasks.register<SimpleTranslationTask>("translateBad") {
+    dependsOn(tasks.named("helloWorld")) // <2>
 }
 // end::avoid-this[]
 
 // tag::do-this[]
-abstract class BetterPrintingTask : DefaultTask() {
-    @get:Input
-    abstract val message: Property<String>
-
-    @get:OutputFile
-    abstract val outputFile: RegularFileProperty
-
-    @TaskAction
-    fun run() {
-        logger.lifecycle(message.get())
-        outputFile.get().asFile.writeText(message.get()) // <1>
-    }
-}
-
-tasks.register<BetterPrintingTask>("task3") {
-    message = "Hello"
-}
-
-tasks.register<BetterPrintingTask>("task4") {
-    inputs.file(tasks.named<BetterPrintingTask>("task3").get().outputFile) // <2>
-    message = "World"
+tasks.register<SimpleTranslationTask>("translateGood") {
+    inputs.file(tasks.named<SimplePrintingTask>("helloWorld").get().messageFile) // <1>
 }
 // end::do-this[]
