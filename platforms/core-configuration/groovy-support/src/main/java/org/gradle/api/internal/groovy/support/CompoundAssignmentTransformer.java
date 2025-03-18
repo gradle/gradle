23a07087e9b2af290d0ab8bf2b5322aed837c4b5
Mikhail Lopatkin <mlopatkin@gradle.com>
2025-03-18 09:49:24 +0100

    Copy the lhs node of += expression when transforming
    Sharing the same node in two branches of binary expression (
    `lhs = lhs + rhs`) leads to unwanted failures when other AST
    transformations add node metadata. In particular, the static type
    checker overwrites the setter with getter's metadata, producing invalid
    bytecode.
    
    This was uncovered with Groovy 4, Groovy 3 probably emits the same
    metadata for both node instances.
    
    Add hand-rolled binary compatibility tests
    
    Our binary compatibility tests are tied to public _documented_ APIs
    living outside "internal" packages. We don't want to document += support
    methods at this point, but decoupling existing checks look involved.
    For now, we stick with simple reflection-based tests that will act as
    a safety net.
    
    We might be able to utilize cross-version tests in the future, but we
    need at least one release with this feature to be able to run them.

--- platforms/core-configuration/groovy-support/src/main/java/org/gradle/api/internal/groovy/support/CompoundAssignmentTransformer.java
+++ platforms/core-configuration/groovy-support/src/main/java/org/gradle/api/internal/groovy/support/CompoundAssignmentTransformer.java
@@ -43,6 +43,9 @@ import java.util.Map;
  * See the package documentation for the complete description.
  */
 public class CompoundAssignmentTransformer extends AbstractScriptTransformer {
+    static final String FOR_COMPOUND_ASSIGNMENT_METHOD_NAME = "forCompoundAssignment";
+    static final String TO_ASSIGNMENT_RESULT_METHOD_NAME = "toAssignmentResult";
+
     @Override
     protected int getPhase() {
         return Phases.CANONICALIZATION;
@@ -154,14 +157,14 @@ public class CompoundAssignmentTransformer extends AbstractScriptTransformer {
          * @see CompoundAssignmentExtensions#forCompoundAssignment(Object)
          */
         private Expression applyForCompoundAssignment(Expression receiver) {
-            return callSupportMethodOn(receiver, "forCompoundAssignment");
+            return callSupportMethodOn(receiver, FOR_COMPOUND_ASSIGNMENT_METHOD_NAME);
         }
 
         /**
          * @see CompoundAssignmentExtensions#toAssignmentResult(Object)
          */
         private Expression applyToAssignmentResult(Expression receiver) {
-            return callSupportMethodOn(receiver, "toAssignmentResult");
+            return callSupportMethodOn(receiver, TO_ASSIGNMENT_RESULT_METHOD_NAME);
         }
 
         private Expression callSupportMethodOn(Expression receiver, String methodName) {
--- platforms/core-configuration/model-core/build.gradle.kts
+++ platforms/core-configuration/model-core/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     testImplementation(projects.native)
     testImplementation(projects.resources)
     testImplementation(testFixtures(projects.coreApi))
+    testImplementation(testFixtures(projects.groovySupport))
     testImplementation(testFixtures(projects.languageGroovy))
     testImplementation(testFixtures(projects.modelReflect))
 
