59eb59e24100c09a9b76b67666afddc2193cd6fb
Sergey Igushkin <sigushkin@gradle.com>
2025-03-21 15:33:00 +0300

    Test client-side DCL += analysis in the cross-version test

--- platforms/core-configuration/declarative-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/declarative/dsl/tooling/builders/r89/DeclarativeDslToolingModelsCrossVersionTest.groovy
+++ platforms/core-configuration/declarative-dsl-tooling-builders/src/crossVersionTest/groovy/org/gradle/declarative/dsl/tooling/builders/r89/DeclarativeDslToolingModelsCrossVersionTest.groovy
@@ -36,6 +36,7 @@ import org.gradle.test.fixtures.plugin.PluginBuilder
 import org.gradle.tooling.events.ProgressEvent
 import org.gradle.tooling.events.ProgressListener
 import org.gradle.tooling.events.lifecycle.BuildPhaseStartEvent
+import org.gradle.util.GradleVersion
 
 @TargetGradleVersion(">=8.11")
 @ToolingApiVersion('>=8.11')
@@ -87,7 +88,7 @@ class DeclarativeDslToolingModelsCrossVersionTest extends AbstractDeclarativeDsl
 
     def 'schema contains custom software type from included build'() {
         given:
-        withSoftwareTypePlugins().prepareToExecute()
+        withSoftwareTypePlugins(targetVersion).prepareToExecute()
 
         file("settings.gradle.dcl") << ecosystemPluginInSettings
 
@@ -107,18 +108,21 @@ class DeclarativeDslToolingModelsCrossVersionTest extends AbstractDeclarativeDsl
 
     def 'interpretation sequences obtained via TAPI are suitable for analysis'() {
         given:
-        withSoftwareTypePlugins().prepareToExecute()
+        withSoftwareTypePlugins(targetVersion).prepareToExecute()
 
         file("settings.gradle.dcl") << """
             $ecosystemPluginInSettings
             defaults {
                 testSoftwareType {
                     id = "default"
+                    foo {
+                        ${targetVersion >= GradleVersion.version("8.14") ? 'baz = listOf("qux")' : ''}
+                    }
                 }
             }
         """
 
-        file("build.gradle.dcl") << declarativeScriptThatConfiguresOnlyTestSoftwareTypeFoo
+        file("build.gradle.dcl") << declarativeScriptThatConfiguresOnlyTestSoftwareTypeFoo(targetVersion)
 
         when:
         DeclarativeSchemaModel model = fetchSchemaModel(DeclarativeSchemaModel.class)
@@ -149,6 +153,10 @@ class DeclarativeDslToolingModelsCrossVersionTest extends AbstractDeclarativeDsl
                 unresolvedId = "default"
                 foo {
                     bar = "baz"
+                    ${targetVersion >= GradleVersion.version("8.14") ? """
+                    baz = listOf("qux")
+                    baz += listOf("quux")
+                    """ : ''}
                 }
             }
             unresolvedToTestErrorHandling()
@@ -180,17 +188,20 @@ class DeclarativeDslToolingModelsCrossVersionTest extends AbstractDeclarativeDsl
         """
     }
 
-    static String getDeclarativeScriptThatConfiguresOnlyTestSoftwareTypeFoo() {
+    static String declarativeScriptThatConfiguresOnlyTestSoftwareTypeFoo(GradleVersion targetVersion) {
         return """
             testSoftwareType {
                 foo {
                     bar = "baz"
+                    ${targetVersion >= GradleVersion.version("8.14") ? """
+                    baz += listOf("quux")
+                    """ : ""}
                 }
             }
         """
     }
 
-    PluginBuilder withSoftwareTypePlugins() {
+    PluginBuilder withSoftwareTypePlugins(GradleVersion gradleVersion = GradleVersion.current()) {
         def pluginBuilder = new PluginBuilder(file("plugins"))
         pluginBuilder.addPluginId("com.example.test-software-type-impl", "SoftwareTypeImplPlugin")
         pluginBuilder.addPluginId("com.example.another-software-type-impl", "AnotherSoftwareTypeImplPlugin")
@@ -207,6 +218,7 @@ class DeclarativeDslToolingModelsCrossVersionTest extends AbstractDeclarativeDsl
             import org.gradle.api.provider.ListProperty;
             import org.gradle.api.provider.Property;
 
+            import java.util.ArrayList;
             import javax.inject.Inject;
 
             @Restricted
@@ -236,10 +248,18 @@ class DeclarativeDslToolingModelsCrossVersionTest extends AbstractDeclarativeDsl
                 public abstract static class Foo {
                     public Foo() {
                         this.getBar().convention("nothing");
+                        ${gradleVersion >= GradleVersion.version("8.14") ? """
+                        this.getBaz().convention(new ArrayList<>());
+                        """ : ""}
                     }
 
                     @Restricted
                     public abstract Property<String> getBar();
+
+                    ${gradleVersion >= GradleVersion.version("8.14") ? """
+                    @Restricted
+                    public abstract ListProperty<String> getBaz();
+                    """ : ""}
                 }
             }
         """
@@ -294,6 +314,10 @@ class DeclarativeDslToolingModelsCrossVersionTest extends AbstractDeclarativeDsl
                         task.doLast("print restricted extension content", t -> {
                             System.out.println("id = " + extension.getId().get());
                             System.out.println("bar = " + extension.getFoo().getBar().get());
+
+                            ${gradleVersion >= GradleVersion.version("8.14") ? """
+                            System.out.println("baz = " + extension.getFoo().getBaz().get());
+                            """ : ""}
                         });
                     });
                 }
