48de48758fc3bc62eccebd723f68f9b8d8173e66
Bo Zhang <bo@gradle.com>
2025-03-24 09:38:35 +0800

    Fix nightly snapshot trigger rule
    It seems only working with `+:master` branch filter

--- .teamcity/src/main/kotlin/promotion/PublishNightlySnapshot.kt
+++ .teamcity/src/main/kotlin/promotion/PublishNightlySnapshot.kt
@@ -57,7 +57,8 @@ class PublishNightlySnapshot(
                     // https://www.jetbrains.com/help/teamcity/2022.04/configuring-schedule-triggers.html#general-syntax-1
                     // We want it to be triggered only when there're pending changes in the specific vcs root, i.e. GradleMaster/GradleRelease
                     triggerRules = "+:root=${VersionedSettingsBranch.fromDslContext().vcsRootId()}:."
-                    branchFilter = "+:${VersionedSettingsBranch.fromDslContext().branchName}"
+                    // The promotion itself will be triggered on gradle-promote's master branch
+                    branchFilter = "+:master"
                 }
             }
         }
