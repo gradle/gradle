421e4ed0ae0f173aa5a1f81f433af8b80abfd896
Paul Merlin <paul@gradle.com>
2025-04-07 12:57:53 +0200

    Simplify root settings platform domain
    Without this change, running the gradle/gradle build with a distribution built from current sources fail with:
    
    e: Backend Internal error: Exception during IR lowering
    File being compiled: /Users/user/.gradle/.tmp/gradle-kotlin-dsl-16072820865429943765.tmp/settings.gradle.kts
    The root cause org.jetbrains.kotlin.backend.common.CompilationException was thrown at: org.jetbrains.kotlin.ir.expressions.IrMemberAccessExpression.setDispatchReceiver(IrMemberAccessExpression.kt:259)
    
    To reproduce run:
    ./gradlew -Pgradle_installPath=/opt/gradle-source-build install
    /opt/gradle-source-build/bin/gradle help
    
    This is already fixed in Kotlin 2.1.21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -517,9 +517,8 @@ sealed class ArchitectureElementBuilder(
 
 class ArchitectureModuleBuilder(
     name: String,
-    private val projectScope: ProjectScope
+    private val projectScope: ProjectScope = ProjectScope("platforms/$name"),
 ) : ArchitectureElementBuilder(name) {
-    constructor(name: String) : this(name, ProjectScope("platforms/$name"))
 
     fun subproject(projectName: String) {
         projectScope.subproject(projectName)
@@ -532,12 +531,10 @@ class ArchitectureModuleBuilder(
 
 class PlatformBuilder(
     name: String,
-    private val projectScope: ProjectScope
+    private val projectScope: ProjectScope = ProjectScope("platforms/$name"),
 ) : ArchitectureElementBuilder(name) {
     private val modules = mutableListOf<ArchitectureModuleBuilder>()
     private val uses = mutableListOf<PlatformBuilder>()
-
-    constructor(name: String) : this(name, ProjectScope("platforms/$name"))
 
     fun subproject(projectName: String) {
         projectScope.subproject(projectName)
