9b4ddeaeaa8d7cd592c4d96d0e6c62e41d6ba078
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2025-04-07 14:51:32 -0300

    Adjust `DistributionIntegrationSpec#pluginsJarCount` after module extraction
    To account for newly introduced modules `graph-isolation` an and
    `isolated-action-services`.

--- testing/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
+++ testing/distributions-integ-tests/src/integTest/groovy/org/gradle/DistributionIntegrationSpec.groovy
@@ -149,7 +149,7 @@ abstract class DistributionIntegrationSpec extends AbstractIntegrationSpec {
      * Change this whenever you add or remove subprojects for distribution-packaged plugins (lib/plugins).
      */
     int getPackagedPluginsJarCount() {
-        78
+        80
     }
 
     /**
