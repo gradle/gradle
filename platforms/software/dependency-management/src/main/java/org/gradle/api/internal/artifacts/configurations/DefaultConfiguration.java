024e441c25afe0c994a59f26ebd4129c3e5e8791
Sterling Greene <sterling@gradle.com>
2025-04-10 18:35:31 -0400

    Make extending configurations from other projects an error

--- platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfiguration.java
+++ platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfiguration.java
@@ -394,21 +394,11 @@ public abstract class DefaultConfiguration extends AbstractFileCollection implem
         for (Configuration extended : extendsFrom) {
             ConfigurationInternal other = Objects.requireNonNull(Cast.uncheckedCast(extended));
             if (!domainObjectContext.equals(other.getDomainObjectContext())) {
-
-                String message = String.format(
-                    "Configuration '%s' in %s extends configuration '%s' in %s.",
-                    this.getName(),
-                    this.domainObjectContext.getDisplayName(),
-                    other.getName(),
-                    other.getDomainObjectContext().getDisplayName()
-                );
-
-                DeprecationLogger.deprecateBehaviour(message)
-                    .withAdvice("Configurations can only extend from configurations in the same project.")
-                    .willBeRemovedInGradle9()
-                    .withUpgradeGuideSection(8, "extending_configurations_in_same_project")
-                    .nagUser();
-
+                throw new InvalidUserDataException(String.format(
+                    "%s extends %s. Configurations can only extend from configurations in the same project.",
+                    this.getDisplayName(),
+                    other.getDisplayName()
+                ));
             }
             if (other.getHierarchy().contains(this)) {
                 throw new InvalidUserDataException(String.format(
