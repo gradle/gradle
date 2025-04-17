e50e23eccb097ee77e9e535bce2b81578f8ccaf7
Octavia Togami <otogami@gradle.com>
2025-04-17 13:51:29 -0700

    Re-implement BuildOperationNotificationFixture using BuildOperationsFixture
    This avoids duplicating code and issues with Groovy's JSON serialization

--- subprojects/core/src/integTest/groovy/org/gradle/internal/operations/notify/BuildOperationNotificationContinuousBuildIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/internal/operations/notify/BuildOperationNotificationContinuousBuildIntegrationTest.groovy
@@ -22,12 +22,11 @@ import org.gradle.launcher.exec.RunBuildBuildOperationType
 
 class BuildOperationNotificationContinuousBuildIntegrationTest extends AbstractContinuousIntegrationTest {
 
-    def notifications = new BuildOperationNotificationFixture(testDirectory)
+    def notifications = new BuildOperationNotificationFixture(executer, testDirectoryProvider)
 
-    @ToBeFixedForConfigurationCache(because = "run1.id == run2.id")
+    @ToBeFixedForConfigurationCache(because = "runs extra build due to incorrect change detection on Thing.class")
     def "obtains notifications about init scripts"() {
         when:
-        settingsFile << notifications.registerListener()
         buildFile """
             apply plugin: "java"
         """
@@ -35,17 +34,17 @@ class BuildOperationNotificationContinuousBuildIntegrationTest extends AbstractC
 
         then:
         succeeds("build")
-        def run1 = notifications.op(RunBuildBuildOperationType.Details)
 
         when:
         file("src/main/java/Thing.java").text = "class Thing {\n\n}"
 
         then:
         buildTriggeredAndSucceeded()
-        def run2 = notifications.op(RunBuildBuildOperationType.Details)
+        def runs = notifications.ops(RunBuildBuildOperationType.Details)
 
         and:
-        run1.id != run2.id
+        runs.size() == 2
+        runs[0].id != runs[1].id
     }
 
 }
