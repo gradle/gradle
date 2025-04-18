a40b6eb054dd54bf18692bc43bddf7490dcc4147
Sterling Greene <sterling@gradle.com>
2025-04-18 14:58:16 -0400

    Defer to Gradle 10.0

--- platforms/jvm/toolchains-jvm-shared/src/main/java/org/gradle/jvm/toolchain/internal/DefaultToolchainSpec.java
+++ platforms/jvm/toolchains-jvm-shared/src/main/java/org/gradle/jvm/toolchain/internal/DefaultToolchainSpec.java
@@ -126,7 +126,7 @@ public class DefaultToolchainSpec implements JavaToolchainSpecInternal {
             // https://github.com/gradle/gradle/issues/23155
             // This should make the spec invalid when the enum gets removed
             DeprecationLogger.deprecateBehaviour("Requesting JVM vendor IBM_SEMERU.")
-                .willBeRemovedInGradle9()
+                .willBeRemovedInGradle10()
                 .withUpgradeGuideSection(8, "ibm_semeru_should_not_be_used")
                 .nagUser();
         }
