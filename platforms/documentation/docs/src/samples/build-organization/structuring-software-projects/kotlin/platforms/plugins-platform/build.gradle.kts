90d7f9c4a99d51ab60f03617fefdf7fbe1b1af47
Paul Merlin <paul@gradle.com>
2025-04-20 14:11:01 +0200

    Use Kotlin 2.1.21-RC in tests

--- platforms/documentation/docs/src/samples/build-organization/structuring-software-projects/kotlin/platforms/plugins-platform/build.gradle.kts
+++ platforms/documentation/docs/src/samples/build-organization/structuring-software-projects/kotlin/platforms/plugins-platform/build.gradle.kts
@@ -7,8 +7,8 @@ group = "com.example.platform"
 dependencies {
     constraints {
         api("com.android.tools.build:gradle:8.9.0")
-        api("org.jetbrains.kotlin.android:org.jetbrains.kotlin.android.gradle.plugin:2.1.20")
-        api("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.1.20")
+        api("org.jetbrains.kotlin.android:org.jetbrains.kotlin.android.gradle.plugin:2.1.21-RC")
+        api("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.1.21-RC")
         api("org.springframework.boot:org.springframework.boot.gradle.plugin:3.4.3")
     }
 }
--- platforms/documentation/docs/src/snippets/buildCache/caching-android-projects/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/buildCache/caching-android-projects/kotlin/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 plugins {
-    kotlin("jvm") version "2.1.20"
-    kotlin("kapt") version "2.1.20"
+    kotlin("jvm") version "2.1.21-RC"
+    kotlin("kapt") version "2.1.21-RC"
 }
 
 repositories {
