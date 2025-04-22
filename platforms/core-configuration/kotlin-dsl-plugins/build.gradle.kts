5581427d9f2dafb327d8e63a87edf377e445554f
Paul Merlin <paul@gradle.com>
2025-04-22 10:30:50 +0200

    Prepare :kotlin-dsl-plugins for next iteration

--- platforms/core-configuration/kotlin-dsl-plugins/build.gradle.kts
+++ platforms/core-configuration/kotlin-dsl-plugins/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 description = "Kotlin DSL Gradle Plugins deployed to the Plugin Portal"
 
 group = "org.gradle.kotlin"
-version = "6.1.0"
+version = "6.1.1"
 
 base.archivesName = "plugins"
 
