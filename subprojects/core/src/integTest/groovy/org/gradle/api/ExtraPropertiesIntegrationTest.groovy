81f41f0073862d23a15a0018ec3573270e43ab4e
Mikhail Lopatkin <mlopatkin@gradle.com>
2025-04-24 13:30:52 +0200

    Fix test for CC
    Do not access project extensions in task actions

--- subprojects/core/src/integTest/groovy/org/gradle/api/ExtraPropertiesIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/ExtraPropertiesIntegrationTest.groovy
@@ -17,15 +17,11 @@
 package org.gradle.api
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
-import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
 import org.gradle.integtests.fixtures.build.BuildTestFile
 import spock.lang.Issue
 
-import static org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache.Skip.INVESTIGATE
-
 class ExtraPropertiesIntegrationTest extends AbstractIntegrationSpec {
 
-    @ToBeFixedForConfigurationCache(skip = INVESTIGATE)
     def 'extra properties are inherited to child and grandchild projects'() {
         given:
         extraPropertiesMultiBuild()
@@ -35,7 +31,6 @@ class ExtraPropertiesIntegrationTest extends AbstractIntegrationSpec {
     }
 
     @Issue('GRADLE-3530')
-    @ToBeFixedForConfigurationCache(skip = INVESTIGATE)
     def 'extra properties can be overridden on child projects'() {
         given:
         extraPropertiesMultiBuild('a': 'aValue', 'a:a1': 'aValue') {
@@ -56,26 +51,28 @@ class ExtraPropertiesIntegrationTest extends AbstractIntegrationSpec {
             createDirs("a", "a/a1")
             settingsFile << "include ':a:a1'"
 
-            buildFile << """
+            buildFile """
                 ext.testProp = 'rootValue'
 
                 task checkTestProp {
+                    def testPropValue = provider { testProp }
                     doLast {
-                        assert testProp == 'rootValue'
+                        assert testPropValue.get() == 'rootValue'
                     }
                 }
-            """.stripIndent()
+            """
 
             ['a', 'b', 'a:a1'].each {
-                buildFile << """
-                project(':${it}') {
-                    task checkTestProp {
-                        doLast {
-                            assert testProp == '${expectedPropPerProject[it]}'
+                buildFile """
+                    project(':${it}') {
+                        task checkTestProp {
+                            def testPropValue = provider { testProp }
+                            doLast {
+                                assert testPropValue.get() == '${expectedPropPerProject[it]}'
+                            }
                         }
                     }
-                }
-            """.stripIndent()
+            """
             }
         }
         root.with(configuration)
