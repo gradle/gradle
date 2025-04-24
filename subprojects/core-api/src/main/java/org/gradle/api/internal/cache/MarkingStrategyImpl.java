605f8b0e7267b3e2d39478295d15fbdcbd6af925
Mikhail Lopatkin <mlopatkin@gradle.com>
2025-04-24 20:44:19 +0200

    Fix CC serialization issues of MarkingStrategy
    The restored values weren't equal to the defined constants because of
    serialization. Using enum as implementation avoids these (there can be
    only one enum constant instance).

--- subprojects/core-api/src/main/java/org/gradle/api/internal/cache/MarkingStrategyImpl.java
+++ subprojects/core-api/src/main/java/org/gradle/api/internal/cache/MarkingStrategyImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,17 @@ package org.gradle.api.internal.cache;
 import org.gradle.api.cache.MarkingStrategy;
 
 import java.io.File;
-import java.io.Serializable;
 
-public class CacheDirTagMarkingStrategy implements MarkingStrategy, Serializable {
-    @Override
-    public void tryMarkCacheDirectory(File file) {
-        CacheDirUtil.tryMarkCacheDirectoryByTag(file.toPath());
+public enum MarkingStrategyImpl implements MarkingStrategy {
+    NONE {
+        @Override
+        public void tryMarkCacheDirectory(File file) {
+        }
+    },
+    CACHEDIR_TAG {
+        @Override
+        public void tryMarkCacheDirectory(File file) {
+            CacheDirUtil.tryMarkCacheDirectoryByTag(file.toPath());
+        }
     }
 }
