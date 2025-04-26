50230f7db927eef13571ee7ca02bebd6b597bfd1
Mikhail Lopatkin <mlopatkin@gradle.com>
2025-04-26 10:32:55 +0200

    Disable tests that work with CC but not with IP

--- subprojects/core/src/integTest/groovy/org/gradle/api/ExtraPropertiesIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/ExtraPropertiesIntegrationTest.groovy
@@ -17,11 +17,13 @@
 package org.gradle.api
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.ToBeFixedForIsolatedProjects
 import org.gradle.integtests.fixtures.build.BuildTestFile
 import spock.lang.Issue
 
 class ExtraPropertiesIntegrationTest extends AbstractIntegrationSpec {
 
+    @ToBeFixedForIsolatedProjects(because = "Cross-project configuration")
     def 'extra properties are inherited to child and grandchild projects'() {
         given:
         extraPropertiesMultiBuild()
@@ -31,6 +33,7 @@ class ExtraPropertiesIntegrationTest extends AbstractIntegrationSpec {
     }
 
     @Issue('GRADLE-3530')
+    @ToBeFixedForIsolatedProjects(because = "Cross-project configuration")
     def 'extra properties can be overridden on child projects'() {
         given:
         extraPropertiesMultiBuild('a': 'aValue', 'a:a1': 'aValue') {
