7d56f1214b9aef87854f5915ab45cab8e7cf187f
Tom Tresansky <ttresansky@gradle.com>
2025-05-09 08:45:48 -0400

    Don't hide test framework startup failures in reports
    These should be more visible that just test assertion failures within a test.

--- platforms/software/testing-base/src/main/java/org/gradle/api/internal/tasks/testing/results/StateTrackingTestResultProcessor.java
+++ platforms/software/testing-base/src/main/java/org/gradle/api/internal/tasks/testing/results/StateTrackingTestResultProcessor.java
@@ -17,10 +17,12 @@
 package org.gradle.api.internal.tasks.testing.results;
 
 import org.gradle.api.internal.tasks.testing.DecoratingTestDescriptor;
+import org.gradle.api.internal.tasks.testing.TestFrameworkStartupFailureException;
 import org.gradle.api.internal.tasks.testing.TestCompleteEvent;
 import org.gradle.api.internal.tasks.testing.TestDescriptorInternal;
 import org.gradle.api.internal.tasks.testing.TestResultProcessor;
 import org.gradle.api.internal.tasks.testing.TestStartEvent;
+import org.gradle.api.internal.tasks.testing.TestSuiteExecutionException;
 import org.gradle.api.tasks.testing.TestFailure;
 import org.gradle.api.tasks.testing.TestOutputEvent;
 import org.gradle.api.tasks.testing.TestResult;
@@ -105,7 +107,15 @@ public class StateTrackingTestResultProcessor implements TestResultProcessor {
                     "Received a failure event for test with unknown id '%s'. Registered test ids: '%s'",
                     testId, executing.keySet()));
         }
-        if (testFailure.getDetails().isAssumptionFailure()) {
+        if (testFailure.getDetails().isStartupFailure()) {
+            /*
+             * This is a special case of test failure where the testing framework has completely failed to start up.
+             * We want to treat this not as a test failure, but as a TASK failure, as it indicates a task misconfiguration.
+             * We need to throw an exception here and abort normal failure processing so that this information
+             * isn't buried in the typical test results report.
+             */
+            throw new TestFrameworkStartupFailureException(testFailure);
+        } else if (testFailure.getDetails().isAssumptionFailure()) {
             testState.assumptionFailure = testFailure;
         } else {
             testState.failures.add(testFailure);
