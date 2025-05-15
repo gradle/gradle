5124de9573c26ae2a9a2bdc5375e93aecb50e667
Tom Tresansky <ttresansky@gradle.com>
2025-05-15 12:48:35 -0400

    Make util classes consistently public in example

--- platforms/documentation/docs/src/snippets/bestPractices/modularizeYourBuild-do/groovy/util-guava/src/main/java/org/example/GuavaUtil.java
+++ platforms/documentation/docs/src/snippets/bestPractices/modularizeYourBuild-do/groovy/util-guava/src/main/java/org/example/GuavaUtil.java
@@ -2,7 +2,7 @@ package org.example;
 
 import com.google.common.base.CaseFormat;
 
-class GuavaUtil implements Util {
+public class GuavaUtil implements Util {
     @Override
     public String greet() {
         return "Hello from " + CaseFormat.LOWER_HYPHEN.to(CaseFormat.UPPER_UNDERSCORE, "guava-util");
