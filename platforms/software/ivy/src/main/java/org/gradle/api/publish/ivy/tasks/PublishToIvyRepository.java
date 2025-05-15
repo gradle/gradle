e7f5e0118026e0ad65356a93e9b7b018fe7a5eb1
Rafael Chaves <rchaves@gradle.com>
2025-05-15 17:13:03 -0300

    Revert changes in preparation to moving degradation logic to plugin

--- platforms/software/ivy/src/main/java/org/gradle/api/publish/ivy/tasks/PublishToIvyRepository.java
+++ platforms/software/ivy/src/main/java/org/gradle/api/publish/ivy/tasks/PublishToIvyRepository.java
@@ -40,7 +40,6 @@ import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.PathSensitivity;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.authentication.Authentication;
-import org.gradle.internal.artifacts.repositories.AuthenticationSupportedInternal;
 import org.gradle.internal.instrumentation.api.annotations.ToBeReplacedByLazyProperty;
 import org.gradle.internal.serialization.Cached;
 import org.gradle.internal.serialization.Transient;
@@ -48,7 +47,6 @@ import org.gradle.internal.service.ServiceRegistry;
 import org.gradle.work.DisableCachingByDefault;
 import org.jspecify.annotations.Nullable;
 
-import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import java.net.URI;
 import java.util.Collection;
@@ -84,12 +82,6 @@ public abstract class PublishToIvyRepository extends DefaultTask {
         // They *might* have input files and other dependencies as well though
         // Inputs: The credentials they need may be expressed in a file
         // Dependencies: Can't think of a case here
-    }
-
-    @Nonnull
-    private Provider<Boolean> degradationCondition() {
-        return ((AuthenticationSupportedInternal) getRepository()).isUsingCredentialsProvider()
-            .map(isUsingProvider -> !isUsingProvider);
     }
 
     /**
@@ -157,7 +149,6 @@ public abstract class PublishToIvyRepository extends DefaultTask {
     public void setRepository(IvyArtifactRepository repository) {
         this.repository.set((DefaultIvyArtifactRepository) repository);
         this.credentials.set(((DefaultIvyArtifactRepository) repository).getConfiguredCredentials());
-        getProject().getGradle().requireConfigurationCacheDegradation("Explicit credentials", degradationCondition());
     }
 
     @TaskAction
