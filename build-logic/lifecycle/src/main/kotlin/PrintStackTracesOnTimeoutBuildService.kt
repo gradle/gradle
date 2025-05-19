30044399e25d37903a1db4e752ab926157005162
Bo Zhang <bo@gradle.com>
2025-05-19 14:08:49 +0800

    debug

--- build-logic/lifecycle/src/main/kotlin/PrintStackTracesOnTimeoutBuildService.kt
+++ build-logic/lifecycle/src/main/kotlin/PrintStackTracesOnTimeoutBuildService.kt
@@ -32,13 +32,15 @@ abstract class PrintStackTracesOnTimeoutBuildService @Inject constructor(private
 
     private
     val timer: Timer = Timer(true).apply {
+        println("Schedule PrintStackTracesOnTimeoutBuildService with ${parameters.timeoutMillis.get()} ms")
         schedule(
             timerTask {
                 println("Exec PrintStackTracesOnTimeoutBuildService")
                 execOperations.exec {
                     commandLine(
                         "${System.getProperty("java.home")}/bin/java",
-                        parameters.projectDirectory.file("testing/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/timeout/JavaProcessStackTracesMonitor.java").get().asFile.absolutePath,
+                        parameters.projectDirectory.file("testing/internal-integ-testing/src/main/groovy/org/gradle/integtests/fixtures/timeout/JavaProcessStackTracesMonitor.java")
+                            .get().asFile.absolutePath,
                         parameters.projectDirectory.asFile.get().absolutePath
                     )
                 }
