0f9071f8e57cded996afa681e2dd6845a2fb385a
Aurimas Liutikas <aurimas@google.com>
2025-05-19 14:52:40 -0700

    Add details based on Tom's feedback
    Signed-off-by: Aurimas Liutikas <aurimas@google.com>

--- platforms/documentation/docs/src/snippets/bestPractices/avoidProviderGetOutsideTaskAction/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/bestPractices/avoidProviderGetOutsideTaskAction/kotlin/build.gradle.kts
@@ -8,25 +8,26 @@ abstract class MyTask : DefaultTask() {
 
     @TaskAction
     fun doAction() {
-        // Use get in the task action
         val outputFile = myOutput.get().asFile
-        val outputText = myInput.get()
+        val outputText = myInput.get() // <1>
         println(outputText)
         outputFile.writeText(outputText)
     }
 }
+
+val currentEnvironment: Provider<String> = providers.gradleProperty("currentEnvironment") // <2>
 // end::provider-get-task-setup[]
 
 // tag::avoid-this[]
 tasks.register<MyTask>("avoidThis") {
-    myInput = "currentEnvironment=${providers.gradleProperty("currentEnvironment").get()}"  // <1>
+    myInput = "currentEnvironment=${currentEnvironment.get()}"  // <1>
     myOutput = layout.buildDirectory.get().asFile.resolve("output-avoid.txt")  // <2>
 }
 // end::avoid-this[]
 
 // tag::do-this[]
 tasks.register<MyTask>("doThis") {
-    myInput = providers.gradleProperty("currentEnvironment").map { "currentEnvironment=$it" }  // <1>
+    myInput = currentEnvironment.map { "currentEnvironment=$it" }  // <1>
     myOutput = layout.buildDirectory.file("output-do.txt")  // <2>
 }
 // end::do-this[]
