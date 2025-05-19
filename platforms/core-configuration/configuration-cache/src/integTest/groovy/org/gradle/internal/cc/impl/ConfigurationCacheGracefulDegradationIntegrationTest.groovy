117bd39f4fac12c1fcfda830844a6d1913f7a916
Sergey Opivalov <sopivalov@gradle.com>
2025-05-19 13:25:40 +0200

    More test coverage

--- platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/ConfigurationCacheGracefulDegradationIntegrationTest.groovy
+++ platforms/core-configuration/configuration-cache/src/integTest/groovy/org/gradle/internal/cc/impl/ConfigurationCacheGracefulDegradationIntegrationTest.groovy
@@ -16,9 +16,6 @@
 
 package org.gradle.internal.cc.impl
 
-
-import org.junit.Ignore
-
 class ConfigurationCacheGracefulDegradationIntegrationTest extends AbstractConfigurationCacheIntegrationTest {
 
     def "can declare applied plugin as CC incompatible"() {
@@ -93,6 +90,122 @@ Configuration cache entry discarded because degradation was requested by:
         postBuildOutputContains("""
 Configuration cache entry discarded because degradation was requested by:
 - task `:a` of type `org.gradle.api.DefaultTask`: Project access""")
+    }
+
+    def "a task can require CC degradation for multiple reasons"() {
+        def configurationCache = newConfigurationCacheFixture()
+        buildFile """
+            tasks.register("a") {
+                def shouldAccessTaskProjectProvider = providers.systemProperty("accessTaskProject").map { Boolean.parseBoolean(it) }.orElse(false)
+                def shouldAccessTaskDependenciesProvider = providers.systemProperty("accessTaskDependencies").map { Boolean.parseBoolean(it)}.orElse(false)
+                requireConfigurationCacheDegradation("Project access", shouldAccessTaskProjectProvider)
+                requireConfigurationCacheDegradation("TaskDependencies access", shouldAccessTaskDependenciesProvider)
+
+                doLast {
+                    if (shouldAccessTaskProjectProvider.get()) {
+                        it.project
+                        println "Task's project accessed!"
+                    }
+                    if (shouldAccessTaskDependenciesProvider.get()) {
+                        it.taskDependencies
+                        println "Task's dependencies accessed!"
+                    }
+                }
+            }
+        """
+
+        when:
+        configurationCacheRun("a", *args)
+
+        then:
+        configurationCache.assertNoConfigurationCache()
+        problems.assertResultHasProblems(result) {
+            totalProblemsCount = expectedProblems.size()
+            expectedProblems.forEach { withProblem(it) }
+            withIncompatibleTask(":a", degradationReason)
+        }
+
+        and:
+        expectedProblems.forEach { outputContains(it) }
+
+        where:
+        args                                                          | expectedOutputs                                               | expectedProblems                                                                                                | degradationReason
+        ["-DaccessTaskProject=true"]                                  | ["Task's project accessed!"]                                  | ["Build file 'build.gradle': line 10: invocation of 'Task.project' at execution time is unsupported."]          | "Project access."
+        ["-DaccessTaskProject=true", "-DaccessTaskDependencies=true"] | ["Task's project accessed!", "Task's dependencies accessed!"] | ["Build file 'build.gradle': line 10: invocation of 'Task.project' at execution time is unsupported.",
+                                                                                                                                         "Build file 'build.gradle': line 14: invocation of 'Task.taskDependencies' at execution time is unsupported."] | "Project access, TaskDependencies access."
+    }
+
+    def "a task in included build can require CC degradation"() {
+        def configurationCache = newConfigurationCacheFixture()
+        buildFile("buildLogic/build.gradle", """
+            tasks.register("foo") {
+                requireConfigurationCacheDegradation("Project access", provider { true })
+                doLast {
+                    println "Hello from included build \${project.path}"
+                }
+            }
+        """)
+        settingsFile """
+            includeBuild("buildLogic")
+        """
+
+        when:
+        configurationCacheRun ":buildLogic:foo"
+
+        then:
+        configurationCache.assertNoConfigurationCache()
+        problems.assertResultHasProblems(result) {
+            totalProblemsCount = 1
+            withProblem("Build file 'buildLogic/build.gradle': line 5: invocation of 'Task.project' at execution time is unsupported.")
+            withIncompatibleTask(":buildLogic:foo", "Project access.")
+        }
+
+        and:
+        outputContains("Hello from included build :")
+        postBuildOutputContains("""
+Configuration cache entry discarded because degradation was requested by:
+- task `:buildLogic:foo` of type `org.gradle.api.DefaultTask`: Project access""")
+    }
+
+    def "depending on a CC degrading task from included build introduces CC degradation"() {
+        def configurationCache = newConfigurationCacheFixture()
+        buildFile("buildLogic/build.gradle", """
+            tasks.register("foo") {
+                requireConfigurationCacheDegradation("Project access", provider { true })
+                doLast {
+                    println "Hello from included build \${project.path}"
+                }
+            }
+        """)
+        settingsFile """
+            includeBuild("buildLogic")
+        """
+        buildFile """
+            tasks.register("bar") {
+                dependsOn gradle.includedBuild("buildLogic").task(":foo")
+                doLast {
+                    println "Hello from root build"
+                }
+            }
+        """
+
+        when:
+        configurationCacheRun "bar"
+
+        then:
+        configurationCache.assertNoConfigurationCache()
+        problems.assertResultHasProblems(result) {
+            totalProblemsCount = 1
+            withProblem("Build file 'buildLogic/build.gradle': line 5: invocation of 'Task.project' at execution time is unsupported.")
+            withIncompatibleTask(":buildLogic:foo", "Project access.")
+        }
+
+        and:
+        outputContains("Hello from included build :")
+        outputContains("Hello from root build")
+        postBuildOutputContains("""
+Configuration cache entry discarded because degradation was requested by:
+- task `:buildLogic:foo` of type `org.gradle.api.DefaultTask`: Project access""")
     }
 
     def "no CC degradation if incompatible task is not presented in the task graph"() {
@@ -187,8 +300,4 @@ Configuration cache entry discarded because degradation was requested by:
 Configuration cache entry discarded because degradation was requested by:
 - plugin 'degrading': Build listener registration""")
     }
-    //TODO: Add scenarios with included build tasks:
-    //          1. Only task(degrading) of included build executed.
-    //          2. Task(degrading) of included build used as a dependency for the task of the root build.
-    //      Add scenario with tasks adding a several degradation requests
 }
