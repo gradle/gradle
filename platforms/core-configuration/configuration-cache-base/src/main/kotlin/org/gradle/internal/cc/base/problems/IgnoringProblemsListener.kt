b255e08002763ec70b35f751e70d846d93feafe7
Rafael Chaves <rchaves@gradle.com>
2025-05-28 20:41:00 -0300

    Update ProblemsListener subtypes

--- platforms/core-configuration/configuration-cache-base/src/main/kotlin/org/gradle/internal/cc/base/problems/IgnoringProblemsListener.kt
+++ platforms/core-configuration/configuration-cache-base/src/main/kotlin/org/gradle/internal/cc/base/problems/IgnoringProblemsListener.kt
@@ -16,6 +16,7 @@
 
 package org.gradle.internal.cc.base.problems
 
+import org.gradle.api.Task
 import org.gradle.internal.configuration.problems.ProblemsListener
 import org.gradle.internal.configuration.problems.PropertyProblem
 import org.gradle.internal.configuration.problems.PropertyTrace
@@ -33,4 +34,6 @@ object IgnoringProblemsListener : ProblemsListener {
     override fun onError(trace: PropertyTrace, error: Exception, message: StructuredMessageBuilder) = Unit
 
     override fun forIncompatibleTask(trace: PropertyTrace, reason: String): ProblemsListener = this
+
+    override fun forTask(task: Task): ProblemsListener = this
 }
