3675c089dfc48a5ff1045ed334252566fdd762a6
Sterling Greene <sterling@gradle.com>
2025-06-02 16:34:37 -0400

    Remove use of expectDeprecationWarning
    This has been deprecated for ages

--- platforms/core-runtime/logging/src/integTest/groovy/org/gradle/DeprecationHandlingIntegrationTest.groovy
+++ platforms/core-runtime/logging/src/integTest/groovy/org/gradle/DeprecationHandlingIntegrationTest.groovy
@@ -202,7 +202,7 @@ class DeprecationHandlingIntegrationTest extends AbstractIntegrationSpec {
         if (withFullStacktrace) {
             executer.withFullDeprecationStackTraceEnabled()
         }
-        executer.expectDeprecationWarning()
+        executer.noDeprecationChecks()
         run()
 
         then:
@@ -235,7 +235,7 @@ class DeprecationHandlingIntegrationTest extends AbstractIntegrationSpec {
         if (withFullStacktrace) {
             executer.withFullDeprecationStackTraceEnabled()
         }
-        executer.expectDeprecationWarning()
+        executer.noDeprecationChecks()
         run()
 
         then:
