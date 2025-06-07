a723dc43fd666e9bc87f2a0aab5ec208ecdd1a7b
József Bartók <jbartok@gradle.com>
2025-06-07 10:17:28 +0300

    Partially revert changes done to `DependenciesExtensions`

--- platforms/core-configuration/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/DependenciesExtensions.kt
+++ platforms/core-configuration/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/DependenciesExtensions.kt
@@ -19,6 +19,7 @@
 package org.gradle.kotlin.dsl
 
 import org.gradle.api.Action
+import org.gradle.api.Incubating
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.DependencyConstraint
 import org.gradle.api.artifacts.ExternalModuleDependency
@@ -130,6 +131,7 @@ operator fun DependencyModifier.invoke(dependencyNotation: CharSequence): Extern
  * @see DependencyModifier
  * @since 8.0
  */
+@Incubating
 operator fun DependencyModifier.invoke(dependency: ProviderConvertible<out MinimalExternalModuleDependency>): Provider<out MinimalExternalModuleDependency> = modify(dependency)
 
 
@@ -188,6 +190,7 @@ operator fun DependencyCollector.invoke(files: FileCollection, configuration: Ac
  * @param externalModule external module to add as a dependency
  * @since 8.6
  */
+@Incubating
 operator fun DependencyCollector.invoke(externalModule: ProviderConvertible<out MinimalExternalModuleDependency>) = add(externalModule)
 
 
@@ -198,6 +201,7 @@ operator fun DependencyCollector.invoke(externalModule: ProviderConvertible<out 
  * @param configuration an action to configure the dependency
  * @since 8.6
  */
+@Incubating
 operator fun DependencyCollector.invoke(externalModule: ProviderConvertible<out MinimalExternalModuleDependency>, configuration: Action<in ExternalModuleDependency>) = add(externalModule, configuration)
 
 
