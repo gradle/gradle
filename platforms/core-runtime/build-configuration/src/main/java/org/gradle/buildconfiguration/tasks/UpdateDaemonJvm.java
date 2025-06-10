9c59692d8d60d541b704922c75efa2abd95095bf
Bayram Turgut <137455737+bayramtturgutt@users.noreply.github.com>
2025-06-11 01:57:07 +0300

    Update UpdateDaemonJvm.java
    Signed-off-by: Bayram Turgut <137455737+bayramtturgutt@users.noreply.github.com>

--- platforms/core-runtime/build-configuration/src/main/java/org/gradle/buildconfiguration/tasks/UpdateDaemonJvm.java
+++ platforms/core-runtime/build-configuration/src/main/java/org/gradle/buildconfiguration/tasks/UpdateDaemonJvm.java
@@ -7,8 +7,8 @@
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -24,11 +24,7 @@ import org.gradle.api.problems.internal.GradleCoreProblemGroup;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.SetProperty;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.Internal;
-import org.gradle.api.tasks.Optional;
-import org.gradle.api.tasks.OutputFile;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.*;
 import org.gradle.api.tasks.options.Option;
 import org.gradle.api.tasks.options.OptionValues;
 import org.gradle.internal.buildconfiguration.DaemonJvmPropertiesDefaults;
@@ -48,9 +44,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Generates or updates the Gradle Daemon JVM criteria.
- *
- * This controls the version of the JVM required to run the Gradle Daemon.
+ * Task that generates or updates the Gradle Daemon JVM criteria.
+ * This controls which JVM version and vendor are required to run the Gradle Daemon.
  *
  * @since 8.8
  */
@@ -58,20 +53,12 @@ import java.util.stream.Collectors;
 @Incubating
 public abstract class UpdateDaemonJvm extends DefaultTask {
 
-    /**
-     * The problem id for task configuration problems.
-     *
-     * @since 8.13
-     */
-    public static final ProblemId TASK_CONFIGURATION_PROBLEM_ID = ProblemId.create("task-configuration", "Invalid task configuration", GradleCoreProblemGroup.daemonToolchain().configurationGeneration());
+    public static final ProblemId TASK_CONFIGURATION_PROBLEM_ID =
+        ProblemId.create("task-configuration", "Invalid task configuration",
+            GradleCoreProblemGroup.daemonToolchain().configurationGeneration());
 
     private final DaemonJvmPropertiesModifier daemonJvmPropertiesModifier;
 
-    /**
-     * Constructor.
-     *
-     * @since 8.8
-     */
     @Inject
     public UpdateDaemonJvm(DaemonJvmPropertiesModifier daemonJvmPropertiesModifier) {
         this.daemonJvmPropertiesModifier = daemonJvmPropertiesModifier;
@@ -81,25 +68,21 @@ public abstract class UpdateDaemonJvm extends DefaultTask {
     void generate() {
         IncubationLogger.incubatingFeatureUsed("Daemon JVM criteria");
 
-        final String jvmVendorCriteria;
-        if (getVendor().isPresent()) {
-            jvmVendorCriteria = getVendor().map(v -> ((DefaultJvmVendorSpec)v).toCriteria()).get();
-        } else {
-            jvmVendorCriteria = null; // any vendor is acceptable
-        }
+        String vendorCriteria = getVendor()
+            .map(v -> ((DefaultJvmVendorSpec) v).toCriteria())
+            .orElse(null);
+
         daemonJvmPropertiesModifier.updateJvmCriteria(
             getPropertiesFile().get().getAsFile(),
             getLanguageVersion().get(),
-            jvmVendorCriteria,
+            vendorCriteria,
             getNativeImageCapable().getOrElse(false),
             getToolchainDownloadUrls().get()
         );
     }
 
     /**
-     * The file to write the requested daemon JVM criteria to.
-     *
-     * {@value DaemonJvmPropertiesDefaults#DAEMON_JVM_PROPERTIES_FILE}
+     * Output file for daemon JVM properties.
      *
      * @since 8.8
      */
@@ -108,9 +91,7 @@ public abstract class UpdateDaemonJvm extends DefaultTask {
     public abstract RegularFileProperty getPropertiesFile();
 
     /**
-     * The version of the JVM required to run the Gradle Daemon.
-     * <p>
-     * By convention, for the task created on the root project, Gradle will use the JVM version of the current JVM.
+     * Required JVM language version.
      *
      * @since 8.13
      */
@@ -121,43 +102,42 @@ public abstract class UpdateDaemonJvm extends DefaultTask {
     public abstract Property<JavaLanguageVersion> getLanguageVersion();
 
     /**
-     * Configures the vendor spec for the daemon toolchain properties generation.
+     * Required JVM vendor.
      *
      * @since 8.13
      */
     @Input
     @Optional
-    @Incubating
     @Option(option = "jvm-vendor", description = "The vendor of the JVM required to run the Gradle Daemon.")
+    @Incubating
     public abstract Property<JvmVendorSpec> getVendor();
 
     /**
-     * Returns the supported JVM vendors.
+     * List of supported JVM vendors.
      *
-     * @return supported JVM vendors
      * @since 8.10
      */
     @OptionValues("jvm-vendor")
     public List<String> getAvailableVendors() {
-        return Arrays.stream(JvmVendor.KnownJvmVendor.values()).filter(e -> e!=JvmVendor.KnownJvmVendor.UNKNOWN).map(Enum::name).collect(Collectors.toList());
+        return Arrays.stream(JvmVendor.KnownJvmVendor.values())
+            .filter(v -> v != JvmVendor.KnownJvmVendor.UNKNOWN)
+            .map(Enum::name)
+            .collect(Collectors.toList());
     }
 
     /**
-     * Indicates it the native-image capability is required.
+     * Whether native-image capability is required.
      *
      * @since 8.14
      */
     @Input
     @Optional
-    @Incubating
     @Option(option = "native-image-capable", description = "Indicates if the native-image capability is required.")
+    @Incubating
     public abstract Property<Boolean> getNativeImageCapable();
 
     /**
-     * The set of {@link BuildPlatform} for which download links should be generated.
-     * <p>
-     * By convention, for the task created on the root project, Gradle sources those from the combination of all supported {@link org.gradle.platform.OperatingSystem}
-     * and the following architectures: {@link org.gradle.platform.Architecture#X86_64} and {@link org.gradle.platform.Architecture#AARCH64}.
+     * Platforms for which to generate toolchain download URLs.
      *
      * @since 8.13
      */
@@ -166,12 +146,7 @@ public abstract class UpdateDaemonJvm extends DefaultTask {
     public abstract SetProperty<BuildPlatform> getToolchainPlatforms();
 
     /**
-     * The download URLs for the toolchains for the given platforms.
-     * <p>
-     * By convention, for the task created on the root project, Gradle will combine the {@link #getToolchainPlatforms() build platforms}, {@link #getLanguageVersion() JVM version} and {@link #getVendor()}
-     * to resolve download URLs using the configured {@link org.gradle.jvm.toolchain.JavaToolchainRepository Java toolchain repositories}.
-     * <p>
-     * If the convention applies and no toolchain repositories are defined, an exception will be thrown.
+     * Toolchain download URLs per platform.
      *
      * @since 8.13
      */
