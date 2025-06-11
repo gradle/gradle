85c825b48885bb09b6dabcc766d842f71a9a4ccb
bot-gradle <bot-gradle@gradle.com>
2025-06-11 06:58:24 +0000

    Parallelize docsTest
    We have thousands of samples to test in docsTest. Previously, we ran these tests in four buckets, with each TeamCity build processing only one bucket sequentially. This is obviously inefficient and a big waste of computing resources.
    
    This PR introduces a custom JUnit platform test engine that significantly improves test execution efficiency. The engine implements a novel approach using 50 "carrier" bucket classes, where test samples are evenly mapped to these buckets.
    
    This new architecture enables us to fully leverage both Gradle's parallel test execution capabilities and TeamCity's parallel test features, resulting in much faster test execution times. Also, PTS is supported.
    
    After this change, the total build time is cut by half: 22m -> 11m.
    
    <img width="1070" alt="image" src="https://github.com/user-attachments/assets/f8491f44-483a-45f5-bac2-439c8cdde286" />
    
    <img width="1264" alt="image" src="https://github.com/user-attachments/assets/f11db77b-0c78-413b-9d50-f512f2edf189" />
    
    Co-authored-by: Bo Zhang <bo@gradle.com>
    Co-authored-by: Christoph Obexer <cobexer@gradle.com>
    
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- platforms/documentation/docs/src/docsTest/java/org/gradle/docs/samples/bucket/Buckets.java
+++ platforms/documentation/docs/src/docsTest/java/org/gradle/docs/samples/bucket/Buckets.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.gradle.docs.samples;
+package org.gradle.docs.samples.bucket;
 
 import org.gradle.exemplar.test.normalizer.FileSeparatorOutputNormalizer;
 import org.gradle.exemplar.test.normalizer.GradleOutputNormalizer;
@@ -23,11 +23,13 @@ import org.gradle.exemplar.test.runner.SampleModifiers;
 import org.gradle.exemplar.test.runner.SamplesOutputNormalizers;
 import org.gradle.integtests.fixtures.executer.DependencyReplacingSampleModifier;
 import org.gradle.integtests.fixtures.executer.MoreMemorySampleModifier;
+import org.gradle.integtests.fixtures.executer.RemoveDeadEndCacheServerModifier;
 import org.gradle.integtests.fixtures.logging.ArtifactResolutionOmittingOutputNormalizer;
 import org.gradle.integtests.fixtures.logging.ConfigurationCacheOutputCleaner;
 import org.gradle.integtests.fixtures.logging.ConfigurationCacheOutputNormalizer;
 import org.gradle.integtests.fixtures.logging.DependencyInsightOutputNormalizer;
 import org.gradle.integtests.fixtures.logging.EmbeddedKotlinOutputNormalizer;
+import org.gradle.integtests.fixtures.logging.EmptyLineRemovalOutputNormalizer;
 import org.gradle.integtests.fixtures.logging.EmptyLineTrimmerOutputNormalizer;
 import org.gradle.integtests.fixtures.logging.GradleWelcomeOutputNormalizer;
 import org.gradle.integtests.fixtures.logging.NativeComponentReportOutputNormalizer;
@@ -38,6 +40,9 @@ import org.gradle.integtests.fixtures.logging.SpringBootWebAppTestOutputNormaliz
 import org.gradle.integtests.fixtures.logging.ZincScalaCompilerOutputNormalizer;
 import org.gradle.integtests.fixtures.mirror.SetMirrorsSampleModifier;
 
+/**
+ * What are these empty classes? Please read the doc in {@link org.gradle.docs.samples.SamplesTestEngine}.
+ */
 @SamplesOutputNormalizers({
     SampleOutputNormalizer.class,
     JavaObjectSerializationOutputNormalizer.class,
@@ -54,24 +59,125 @@ import org.gradle.integtests.fixtures.mirror.SetMirrorsSampleModifier;
     PlatformInOutputNormalizer.class,
     SpringBootWebAppTestOutputNormalizer.class,
     EmptyLineTrimmerOutputNormalizer.class,
-    RepositoryMirrorOutputNormalizer.class
+    RepositoryMirrorOutputNormalizer.class,
+    EmptyLineRemovalOutputNormalizer.class,
 })
 @SampleModifiers({
     SetMirrorsSampleModifier.class,
     MoreMemorySampleModifier.class,
-    DependencyReplacingSampleModifier.class
+    DependencyReplacingSampleModifier.class,
+    RemoveDeadEndCacheServerModifier.class,
 })
-/*
- * To run the samples tests:
- *
- * Say you want to run the snippet found at:
- *    src/snippets/dependencyManagement/customizingResolution-consistentResolution
- *
- * then you can run the following command line:
- *
- * ./gradlew :docs:docsTest --tests "*.snippet-dependency-management-customizing-resolution-consistent-resolution*"
- *
- * which would run both Groovy and Kotlin tests.
- */
-abstract class BaseSamplesTest {
+public class Buckets {
+    public static final int BUCKET_NUMBER = 50;
+
+    static {
+        for (int i = 1; i <= BUCKET_NUMBER; i++) {
+            try {
+                Class.forName(Buckets.class.getName().replace("Buckets", "Bucket" + i));
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
+
+class Bucket1 {}
+
+class Bucket2 {}
+
+class Bucket3 {}
+
+class Bucket4 {}
+
+class Bucket5 {}
+
+class Bucket6 {}
+
+class Bucket7 {}
+
+class Bucket8 {}
+
+class Bucket9 {}
+
+class Bucket10 {}
+
+class Bucket11 {}
+
+class Bucket12 {}
+
+class Bucket13 {}
+
+class Bucket14 {}
+
+class Bucket15 {}
+
+class Bucket16 {}
+
+class Bucket17 {}
+
+class Bucket18 {}
+
+class Bucket19 {}
+
+class Bucket20 {}
+
+class Bucket21 {}
+
+class Bucket22 {}
+
+class Bucket23 {}
+
+class Bucket24 {}
+
+class Bucket25 {}
+
+class Bucket26 {}
+
+class Bucket27 {}
+
+class Bucket28 {}
+
+class Bucket29 {}
+
+class Bucket30 {}
+
+class Bucket31 {}
+
+class Bucket32 {}
+
+class Bucket33 {}
+
+class Bucket34 {}
+
+class Bucket35 {}
+
+class Bucket36 {}
+
+class Bucket37 {}
+
+class Bucket38 {}
+
+class Bucket39 {}
+
+class Bucket40 {}
+
+class Bucket41 {}
+
+class Bucket42 {}
+
+class Bucket43 {}
+
+class Bucket44 {}
+
+class Bucket45 {}
+
+class Bucket46 {}
+
+class Bucket47 {}
+
+class Bucket48 {}
+
+class Bucket49 {}
+
+class Bucket50 {}
