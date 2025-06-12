01ca0b8a56a0b423eae4e59978aa302a2a30f912
Bo Zhang <bo@gradle.com>
2025-06-12 17:28:59 +0800

    debug

--- .teamcity/src/main/kotlin/common/VersionedSettingsBranch.kt
+++ .teamcity/src/main/kotlin/common/VersionedSettingsBranch.kt
@@ -53,7 +53,7 @@ data class VersionedSettingsBranch(
 
         private val OLD_RELEASE_PATTERN = "release(\\d+)x".toRegex()
 
-        fun fromDslContext(): VersionedSettingsBranch = VersionedSettingsBranch(DslContext.getParameter("branch", "master"))
+        fun fromDslContext(): VersionedSettingsBranch = VersionedSettingsBranch(DslContext.apply { require(!isRelative) }.getParameter("branch", "master"))
 
         private fun determineNightlyPromotionTriggerHour(branchName: String) =
             when (branchName) {
