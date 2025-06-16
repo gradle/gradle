5b4318d065f5df8094ac3822cbc4337c5720b67e
Tom Tresansky <ttresansky@gradle.com>
2025-06-16 15:53:10 -0400

    Refine and expand the Avoid using eager APIs on File Collections item

--- platforms/documentation/docs/src/snippets/bestPractices/avoidJdkCollectionApiOnFileCollections-do/kotlin/lib/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/bestPractices/avoidJdkCollectionApiOnFileCollections-do/kotlin/lib/build.gradle.kts
@@ -9,19 +9,9 @@ dependencies {
 }
 
 // tag::do-this[]
-abstract class FileCounterTask: DefaultTask() {
-    @get:InputFiles
-    abstract val countMe: ConfigurableFileCollection
-
-    @TaskAction
-    fun countFiles() {
-        logger.lifecycle("Count: " + countMe.files.size)
-    }
-}
-
-tasks.register<FileCounterTask>("goodCountingTask") {
-    countMe.from(configurations.runtimeClasspath) // <1>
-    countMe.from(layout.projectDirectory.file("extra.txt"))
+tasks.register<Zip>("goodZippingTask") {
+    from(configurations.runtimeClasspath) // <1>
+    from(layout.projectDirectory.file("extra.txt"))
     logger.lifecycle("Resolved: " + (configurations.runtimeClasspath.get().state == RESOLVED))
 }
 // end::do-this[]
--- platforms/documentation/docs/src/snippets/bestPractices/avoidJdkCollectionApiOnFileCollections/groovy/settings.gradle
+++
@@ -1,1 +0,0 @@
-rootProject.name = "avoid-jdk-collection-api-on-file-collections"
