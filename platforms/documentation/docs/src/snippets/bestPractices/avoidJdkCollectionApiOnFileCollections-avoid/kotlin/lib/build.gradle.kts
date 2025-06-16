6b516d5d956748cb9983d9b6eb0e634a54f85c71
Tom Tresansky <ttresansky@gradle.com>
2025-06-16 15:53:10 -0400

    Refine and expand the Avoid using eager APIs on File Collections item

--- platforms/documentation/docs/src/snippets/bestPractices/avoidJdkCollectionApiOnFileCollections-avoid/kotlin/lib/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/bestPractices/avoidJdkCollectionApiOnFileCollections-avoid/kotlin/lib/build.gradle.kts
@@ -9,39 +9,22 @@ dependencies {
 }
 
 // tag::avoid-this[]
-abstract class FileCounterTask: DefaultTask() {
-    @get:InputFiles
-    abstract val countMe: ConfigurableFileCollection
-
-    @TaskAction
-    fun countFiles() {
-        logger.lifecycle("Count: " + countMe.files.size)
-    }
-}
-
-tasks.register<FileCounterTask>("badCountingTask") {
+tasks.register<Zip>("badZippingTask") {
     if (!configurations.runtimeClasspath.get().isEmpty()) { // <1>
-        logger.lifecycle("Resolved: " + (configurations.runtimeClasspath.get().state == RESOLVED))
-        countMe.from(configurations.runtimeClasspath)
-    }
-}
-
-tasks.register<FileCounterTask>("badCountingTask2") {
-    val files = configurations.runtimeClasspath.get().files // <2>
-    countMe.from(files)
-    logger.lifecycle("Resolved: " + (configurations.runtimeClasspath.get().state == RESOLVED))
-}
-
-tasks.register<FileCounterTask>("badCountingTask3") {
-    val files = configurations.runtimeClasspath.get() + layout.projectDirectory.file("extra.txt") // <3>
-    countMe.from(files)
-    logger.lifecycle("Resolved: " + (configurations.runtimeClasspath.get().state == RESOLVED))
-}
-
-tasks.register<Zip>("badZippingTask") { // <4>
-    if (!configurations.runtimeClasspath.get().isEmpty()) {
         logger.lifecycle("Resolved: " + (configurations.runtimeClasspath.get().state == RESOLVED))
         from(configurations.runtimeClasspath)
     }
+}
+
+tasks.register<Zip>("badZippingTask2") {
+    val files = configurations.runtimeClasspath.get().files // <2>
+    from(files)
+    logger.lifecycle("Resolved: " + (configurations.runtimeClasspath.get().state == RESOLVED))
+}
+
+tasks.register<Zip>("badZippingTask3") {
+    val files = configurations.runtimeClasspath.get() + layout.projectDirectory.file("extra.txt") // <3>
+    from(files)
+    logger.lifecycle("Resolved: " + (configurations.runtimeClasspath.get().state == RESOLVED))
 }
 // end::avoid-this[]
