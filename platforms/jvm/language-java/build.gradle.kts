omeos-fork/gradle.gradle

Split out compiler workers into separate Gradle projects
When loading a compiler worker classpath to be used as a compiler daemon, we manually pick
and choose which gradle modules/projects should be included in that classpath. Ideally, we would
be able to automatically derive this classpath so there is no need to manually choose which dependenices
end up in the worker classpath.

This commit separates out the java, scala, and groovy compiler daemon worker implementations into separate
Gradle projects, each with minimal dependencies. The dependencies of these projects represent the classes
required by the compiler worker. As such, we can use the transitive closure of this project's dependenices
as the worker classpath instead of manually constructing the classpath.

platforms/jvm/language-java/build.gradle.kts
@@ -4,18 +4,11 @@ plugins {
 
 description = "Source for JavaCompile, JavaExec and Javadoc tasks, it also contains logic for incremental Java compilation"
 
-gradlebuildJava {
-    usesJdkInternals = true
-}
-
 errorprone {
     disabledChecks.addAll(
         "CheckReturnValue", // 2 occurrences
-        "DoNotClaimAnnotations", // 6 occurrences
         "InconsistentCapitalization", // 1 occurrences
         "InvalidInlineTag", // 3 occurrences
-        "MissingCasesInEnumSwitch", // 1 occurrences
-        "MixedMutabilityReturnType", // 3 occurrences
     )
 }
 
@@ -24,7 +17,6 @@ dependencies {
     api(projects.buildEvents)
     api(projects.buildOperations)
     api(projects.buildProcessServices)
-    api(projects.classloaders)
     api(projects.core)
     api(projects.coreApi)
     api(projects.daemonServerWorker)
@@ -33,6 +25,8 @@ dependencies {
     api(projects.fileOperations)
     api(projects.files)
     api(projects.hashing)
+    api(projects.javaCompilerWorker)
+    api(projects.jvmCompilerWorker)
     api(projects.jvmServices)
     api(projects.languageJvm)
     api(projects.modelCore)
@@ -54,24 +48,22 @@ dependencies {
     api(libs.asm)
     api(libs.fastutil)
     api(libs.groovy)
-    api(libs.guava)
-    api(libs.jsr305)
     api(libs.inject)
+    api(libs.jsr305)
+    api(libs.slf4jApi)
 
-    implementation(projects.concurrent)
-    implementation(projects.serviceLookup)
-    implementation(projects.time)
+    implementation(projects.classloaders)
     implementation(projects.fileTemp)
     implementation(projects.logging)
     implementation(projects.loggingApi)
-    implementation(projects.logging)
-    implementation(projects.problemsRendering)
+    implementation(projects.serviceLookup)
+    implementation(projects.time)
     implementation(projects.toolingApi)
 
-    api(libs.slf4jApi)
-    implementation(libs.commonsLang)
     implementation(libs.ant)
     implementation(libs.commonsCompress)
+    implementation(libs.commonsLang)
+    implementation(libs.guava)
 
     runtimeOnly(projects.javaCompilerPlugin)
 
@@ -129,17 +121,6 @@ packageCycles {
 
 integTest.usesJavadocCodeSnippets = true
 
-tasks.javadoc {
-    options {
-        this as StandardJavadocDocletOptions
-        // This project accesses JDK internals, which we need to open up so that javadoc can access them
-        addMultilineStringsOption("-add-exports").value = listOf(
-            "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
-            "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
-            "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED"
-        )
-    }
-}
 tasks.isolatedProjectsIntegTest {
     enabled = false
 }

gradle/gradle

platforms/jvm/language-java/build.gradle.kts
@@ -4,14 +4,9 @@ plugins {
 
 description = "Source for JavaCompile, JavaExec and Javadoc tasks, it also contains logic for incremental Java compilation"
 
-jvmCompile {
-    usesJdkInternals = true
-}
-
 errorprone {
     disabledChecks.addAll(
-        "CheckReturnValue", // 2 occurrences
-        "DoNotClaimAnnotations", // 6 occurrences
+        "CheckReturnValue" // 2 occurrences
     )
 }
 
@@ -20,7 +15,6 @@ dependencies {
     api(projects.buildEvents)
     api(projects.buildOperations)
     api(projects.buildProcessServices)
-    api(projects.classloaders)
     api(projects.core)
     api(projects.coreApi)
     api(projects.daemonServerWorker)
@@ -29,11 +23,12 @@ dependencies {
     api(projects.fileOperations)
     api(projects.files)
     api(projects.hashing)
+    api(projects.javaCompilerWorker)
+    api(projects.jvmCompilerWorker)
     api(projects.jvmServices)
     api(projects.languageJvm)
     api(projects.modelCore)
     api(projects.persistentCache)
-    api(projects.platformBase)
     api(projects.platformJvm)
     api(projects.problemsApi)
     api(projects.processServices)
@@ -51,22 +46,23 @@ dependencies {
     api(libs.asm)
     api(libs.fastutil)
     api(libs.groovy)
-    api(libs.guava)
-    api(libs.jspecify)
     api(libs.inject)
+    api(libs.jspecify)
+    api(libs.slf4jApi)
 
-    implementation(projects.concurrent)
+    implementation(projects.classloaders)
+    implementation(projects.fileTemp)
+    implementation(projects.logging)
+    implementation(projects.loggingApi)
+    implementation(projects.platformBase)
     implementation(projects.serviceLookup)
     implementation(projects.time)
-    implementation(projects.fileTemp)
-    implementation(projects.loggingApi)
-    implementation(projects.problemsRendering)
     implementation(projects.toolingApi)
 
-    api(libs.slf4jApi)
-    implementation(libs.commonsLang)
     implementation(libs.ant)
     implementation(libs.commonsCompress)
+    implementation(libs.commonsLang)
+    implementation(libs.guava)
 
     runtimeOnly(projects.javaCompilerPlugin)
 
@@ -127,17 +123,6 @@ packageCycles {
 
 integTest.usesJavadocCodeSnippets = true
 
-tasks.javadoc {
-    options {
-        this as StandardJavadocDocletOptions
-        // This project accesses JDK internals, which we need to open up so that javadoc can access them
-        addMultilineStringsOption("-add-exports").value = listOf(
-            "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
-            "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
-            "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED"
-        )
-    }
-}
 tasks.isolatedProjectsIntegTest {
     enabled = false
 }