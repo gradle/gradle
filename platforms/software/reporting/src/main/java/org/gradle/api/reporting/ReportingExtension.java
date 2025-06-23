897826a74d96fa0b45e1e35b4d51a1840891cb7f
Sterling Greene <sterling@gradle.com>
2025-06-23 12:16:22 -0400

    Clean up ReportingExtension and deprecate unnecessary methods

--- platforms/software/reporting/src/main/java/org/gradle/api/reporting/ReportingExtension.java
+++ platforms/software/reporting/src/main/java/org/gradle/api/reporting/ReportingExtension.java
@@ -15,13 +15,14 @@
  */
 package org.gradle.api.reporting;
 
+import org.gradle.api.Action;
 import org.gradle.api.ExtensiblePolymorphicDomainObjectContainer;
 import org.gradle.api.Incubating;
 import org.gradle.api.Project;
 import org.gradle.api.file.DirectoryProperty;
-import org.gradle.api.internal.file.FileLookup;
-import org.gradle.api.internal.project.ProjectInternal;
-import org.gradle.internal.instrumentation.api.annotations.ToBeReplacedByLazyProperty;
+import org.gradle.api.reporting.internal.ReportUtilities;
+import org.gradle.internal.deprecation.DeprecationLogger;
+import org.gradle.internal.instrumentation.api.annotations.NotToBeReplacedByLazyProperty;
 
 import javax.inject.Inject;
 import java.io.File;
@@ -30,14 +31,24 @@ import java.io.File;
  * A project extension named "reporting" that provides basic reporting settings and utilities.
  * <p>
  * Example usage:
- * <pre>
+ * <pre class='autoTested'>
+ * plugins {
+ *     id("org.gradle.reporting-base")
+ * }
+ *
  * reporting {
+ *     // change the base directory where all reports are generated
  *     baseDirectory = layout.buildDirectory.dir("our-reports")
  * }
+ *
+ * // A directory for test reports
+ * reporting.baseDirectory.dir("test-reports")
+ *
+ * // A report file
+ * reporting.baseDirectory.file("index.html")
  * </pre>
  * <p>
- * When implementing a task that produces reports, the location of where to generate reports should be obtained
- * via the {@link #file(String)} method of this extension.
+ * When implementing a task that produces reports, the location of where to generate reports should be obtained from {@link #getBaseDirectory()}.
  */
 public abstract class ReportingExtension {
 
@@ -51,16 +62,11 @@ public abstract class ReportingExtension {
      */
     public static final String DEFAULT_REPORTS_DIR_NAME = "reports";
 
-    private final ProjectInternal project;
-    private final DirectoryProperty baseDirectory;
-    private final ExtensiblePolymorphicDomainObjectContainer<ReportSpec> reports;
+    private final Project project;
 
     @Inject
     public ReportingExtension(Project project) {
-        this.project = (ProjectInternal)project;
-        this.baseDirectory = project.getObjects().directoryProperty();
-        this.reports = project.getObjects().polymorphicDomainObjectContainer(ReportSpec.class);
-        baseDirectory.set(project.getLayout().getBuildDirectory().dir(DEFAULT_REPORTS_DIR_NAME));
+        this.project = project;
     }
 
     /**
@@ -68,9 +74,7 @@ public abstract class ReportingExtension {
      *
      * @since 4.4
      */
-    public DirectoryProperty getBaseDirectory() {
-        return baseDirectory;
-    }
+    public abstract DirectoryProperty getBaseDirectory();
 
     /**
      * Creates a file object for the given path, relative to {@link #getBaseDirectory()}.
@@ -79,20 +83,35 @@ public abstract class ReportingExtension {
      *
      * @param path the relative path
      * @return a file object at the given path relative to {@link #getBaseDirectory()}.
+     *
+     * @deprecated Use {@code getBaseDirectory().file(path)} or {@code getBaseDirectory().dir(path)} instead.
+     *
+     * @see DirectoryProperty#file(String)
+     * @see DirectoryProperty#dir(String)
      */
-    public File file(String path) {  // TODO should this take Object?
-        return this.project.getServices().get(FileLookup.class).getFileResolver(getBaseDirectory().getAsFile().get()).resolve(path);
+    @Deprecated
+    public File file(String path) {
+        DeprecationLogger.deprecateMethod(ReportingExtension.class, "file(String)")
+            .replaceWith("getBaseDirectory().file(String) or getBaseDirectory().dir(String)")
+            .willBeRemovedInGradle10()
+            .withUpgradeGuideSection(9, "reporting_extension_file")
+            .nagUser();
+        return getBaseDirectory().file(path).get().getAsFile();
     }
 
-    @ToBeReplacedByLazyProperty
-    // TODO this doesn't belong here, that java plugin should add an extension to this guy with this
+    /**
+     * Provides a default title for API documentation based on the project's name and version.
+     *
+     * @deprecated Use your own way of generating a title for API documentation.
+     */
+    @NotToBeReplacedByLazyProperty(because="this method is deprecated")
+    @Deprecated
     public String getApiDocTitle() {
-        Object version = project.getVersion();
-        if (Project.DEFAULT_VERSION.equals(version)) {
-            return project.getName() + " API";
-        } else {
-            return project.getName() + " " + version + " API";
-        }
+        DeprecationLogger.deprecateMethod(ReportingExtension.class, "getApiDocTitle()")
+            .willBeRemovedInGradle10()
+            .withUpgradeGuideSection(9, "reporting_extension_api_doc_title")
+            .nagUser();
+        return ReportUtilities.getApiDocTitleFor(project);
     }
 
     /**
@@ -102,7 +121,16 @@ public abstract class ReportingExtension {
      * @since 7.4
      */
     @Incubating
-    public ExtensiblePolymorphicDomainObjectContainer<ReportSpec> getReports() {
-        return reports;
+    public abstract ExtensiblePolymorphicDomainObjectContainer<ReportSpec> getReports();
+
+    /**
+     * Add more reports or configure the available reports.
+     *
+     * @param action configuration action for the reports container
+     * @since 9.1.0
+     */
+    @Incubating
+    public void reports(Action<? super ExtensiblePolymorphicDomainObjectContainer<ReportSpec>> action) {
+        action.execute(getReports());
     }
 }
