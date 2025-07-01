774b10292cbcfee7a11c0ca57769358e739ee9ab
Sterling Greene <sterling@gradle.com>
2025-07-01 13:00:51 -0400

    Also test withJavadocJar

--- platforms/jvm/plugins-java/src/integTest/groovy/org/gradle/java/dependencies/JavaFeatureRegressionIntegrationTest.groovy
+++ platforms/jvm/plugins-java/src/integTest/groovy/org/gradle/java/dependencies/JavaFeatureRegressionIntegrationTest.groovy
@@ -20,8 +20,7 @@ import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import spock.lang.Issue
 
 class JavaFeatureRegressionIntegrationTest extends AbstractIntegrationSpec {
-    @Issue("https://github.com/gradle/gradle/issues/34058")
-    def "afterEvaluate called in configurations.all triggered from a create called from a lazy container works"() {
+    def setup() {
         buildFile << """
             plugins {
                 id("java-library")
@@ -32,10 +31,25 @@ class JavaFeatureRegressionIntegrationTest extends AbstractIntegrationSpec {
                     println("hello " + conf.name)
                 }
             }
+        """
+    }
+    @Issue("https://github.com/gradle/gradle/issues/34058")
+    def "afterEvaluate called in configurations.all triggered from a create called from a lazy container works withSourcesJar"() {
+        buildFile << """
             java.withSourcesJar()
         """
         expect:
         succeeds("help")
         output.contains("hello sourcesElements")
+    }
+
+    @Issue("https://github.com/gradle/gradle/issues/34058")
+    def "afterEvaluate called in configurations.all triggered from a create called from a lazy container works withJavadocJar"() {
+        buildFile << """
+            java.withJavadocJar()
+        """
+        expect:
+        succeeds("help")
+        output.contains("hello javadocElements")
     }
 }
