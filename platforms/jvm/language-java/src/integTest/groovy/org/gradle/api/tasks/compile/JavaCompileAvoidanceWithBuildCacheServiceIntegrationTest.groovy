dab88f2ee3fdacc51d21774eeedeea7caee0fd63
Anže Sodja <asodja@gradle.com>
2025-07-08 11:27:55 +0200

    Add integration test to reproduce #32464

--- platforms/jvm/language-java/src/integTest/groovy/org/gradle/api/tasks/compile/JavaCompileAvoidanceWithBuildCacheServiceIntegrationTest.groovy
+++ platforms/jvm/language-java/src/integTest/groovy/org/gradle/api/tasks/compile/JavaCompileAvoidanceWithBuildCacheServiceIntegrationTest.groovy
@@ -16,8 +16,14 @@
 
 package org.gradle.api.tasks.compile
 
+
+import org.apache.commons.io.FileUtils
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.integtests.fixtures.BuildCacheOperationFixtures
+import org.gradle.integtests.fixtures.BuildOperationsFixture
 import org.gradle.integtests.fixtures.DirectoryBuildCacheFixture
+import org.gradle.test.fixtures.dsl.GradleDsl
+import spock.lang.Issue
 
 class JavaCompileAvoidanceWithBuildCacheServiceIntegrationTest extends AbstractIntegrationSpec implements DirectoryBuildCacheFixture {
 
@@ -62,6 +68,72 @@ class JavaCompileAvoidanceWithBuildCacheServiceIntegrationTest extends AbstractI
         executedAndNotSkipped ':b:jar', ':a:compileJava'
     }
 
+    @Issue("https://github.com/gradle/gradle/issues/32464")
+    def "kotlin library on build script classpath and Java classpath doesn't affect Java compilation build cache key"() {
+        given:
+        file("a/build.gradle.kts") << """
+            plugins {
+                `java-library`
+            }
+            ${mavenCentralRepository(GradleDsl.KOTLIN)}
+            dependencies {
+                // We need a Kotlin library with inlined methods
+                implementation("com.squareup.okhttp3:okhttp:4.12.0")
+            }
+        """
+        file("a/settings.gradle.kts") << """
+            rootProject.name = "a"
+        """
+        file('a/src/main/java/A.java') << '''
+            public class A {
+                public void foo() {
+                }
+            }
+        '''
+        file("a/gradle/init.gradle.kts") << """
+            initscript {
+                ${mavenCentralRepository(GradleDsl.KOTLIN)}
+                dependencies {
+                    classpath("com.squareup.okhttp3:okhttp:4.12.0")
+                }
+            }
+
+            // We need to have this line to force the body script to be compiled and classpath snapshotted
+            println("Script body")
+        """
+        def aRelocated = file("a-relocated")
+        FileUtils.copyDirectory(file("a"), aRelocated)
+
+        when:
+        def firstOperations = new BuildCacheOperationFixtures(new BuildOperationsFixture(executer, testDirectoryProvider, "first-operations"))
+        result = executer
+            .withGradleUserHomeDir(testDirectoryProvider.getTestDirectory().file("first-user-home"))
+            .inDirectory(file("a"))
+            .withArgument("--init-script")
+            .withArgument("gradle/init.gradle.kts")
+            .withBuildCacheEnabled()
+            .withTasks("compileJava")
+            .run()
+
+        then:
+        executedAndNotSkipped ':compileJava'
+        def firstCacheKey = firstOperations.getCacheKeyForTask(":compileJava")
+
+        when:
+        def secondOperations = new BuildCacheOperationFixtures(new BuildOperationsFixture(executer, testDirectoryProvider, "second-operations"))
+        result = executer
+            .withGradleUserHomeDir(testDirectoryProvider.getTestDirectory().file("second-user-home"))
+            .inDirectory(aRelocated)
+            .withBuildCacheEnabled()
+            .withTasks("compileJava")
+            .run()
+
+        then:
+        executedAndNotSkipped ':compileJava'
+        def secondCacheKey = secondOperations.getCacheKeyForTask(":compileJava")
+        firstCacheKey == secondCacheKey
+    }
+
     void make_abi_compatible_change_on_b() {
         file('b/src/main/java/B.java').text = '''
             public class B {
@@ -89,9 +161,9 @@ class JavaCompileAvoidanceWithBuildCacheServiceIntegrationTest extends AbstractI
                 apply plugin: 'java'
             }
         '''
-        file('a/build.gradle') << '''
+        file('a/build.gradle.kts') << '''
             dependencies {
-                implementation project(':b')
+                implementation(project(":b"))
             }
         '''
 
