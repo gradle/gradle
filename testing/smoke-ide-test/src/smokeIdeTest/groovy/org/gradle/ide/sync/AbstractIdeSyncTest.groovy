b67c401f543211a43c76439f44789b6348c062e6
Sergey Opivalov <sopivalov@gradle.com>
2025-07-09 15:02:41 +0300

    Bump timeout

--- testing/smoke-ide-test/src/smokeIdeTest/groovy/org/gradle/ide/sync/AbstractIdeSyncTest.groovy
+++ testing/smoke-ide-test/src/smokeIdeTest/groovy/org/gradle/ide/sync/AbstractIdeSyncTest.groovy
@@ -41,7 +41,7 @@ import java.util.concurrent.Executors
  * Provisioned IDEs are cached in the {@link AbstractIdeSyncTest#getIdeHome} directory.
  * @see <a href="https://github.com/gradle/gradle-ide-starter">gradle-ide-starter</a>
  */
-@Timeout(600)
+@Timeout(900)
 @CleanupTestDirectory
 abstract class AbstractIdeSyncTest extends Specification {
 
