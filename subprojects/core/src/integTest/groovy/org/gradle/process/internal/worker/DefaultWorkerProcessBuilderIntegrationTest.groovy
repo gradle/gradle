7c4d987b030532705f7fe5d282bdb98b14305b7e
Mikhail Lopatkin <mlopatkin@gradle.com>
2025-07-12 01:05:56 +0200

    Fix CC-incompatible test

--- subprojects/core/src/integTest/groovy/org/gradle/process/internal/worker/DefaultWorkerProcessBuilderIntegrationTest.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/process/internal/worker/DefaultWorkerProcessBuilderIntegrationTest.groovy
@@ -16,54 +16,52 @@
 
 package org.gradle.process.internal.worker
 
-
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
-import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
-
-import static org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache.Skip.INVESTIGATE
 
 class DefaultWorkerProcessBuilderIntegrationTest extends AbstractIntegrationSpec {
 
-    @ToBeFixedForConfigurationCache(skip = INVESTIGATE)
     def "test classpath does not contain nonexistent entries"() {
         given:
-        file("src/test/java/ClasspathTest.java") << '''
-        import org.junit.Test;
+        javaFile("src/test/java/ClasspathTest.java", '''
+            import org.junit.Test;
 
-        import static org.junit.Assert.assertTrue;
+            import static org.junit.Assert.assertTrue;
 
-        public class ClasspathTest {
-            @Test
-            public void test() {
-                String runtimeClasspath = System.getProperty("java.class.path");
-                System.out.println(runtimeClasspath);
-                assertTrue(runtimeClasspath.contains(System.getProperty("user.home")));
-                assertTrue(!runtimeClasspath.contains("Non exist path"));
+            public class ClasspathTest {
+                @Test
+                public void test() {
+                    String runtimeClasspath = System.getProperty("java.class.path");
+                    System.out.println(runtimeClasspath);
+                    assertTrue(runtimeClasspath.contains(System.getProperty("user.home")));
+                    assertTrue(!runtimeClasspath.contains("Non exist path"));
+                }
             }
-        }
+        ''')
 
-        '''
-        buildFile << """
-        plugins {
-            id "java-library"
-        }
-
-        repositories {
-            mavenCentral()
-        }
-
-        dependencies {
-            testImplementation 'junit:junit:4.13'
-        }
-
-        tasks.test {
-            doFirst {
-                classpath += files(System.getProperty("user.home"),
-                        System.getProperty("user.home") + File.separator + "*",
-                        System.getProperty("user.home") + File.separator + "Non exist path",
-                        System.getProperty("user.home") + File.separator + "Non exist path" + File.separator + "*")
+        buildFile """
+            plugins {
+                id "java-library"
             }
-        }
+
+            repositories {
+                mavenCentral()
+            }
+
+            dependencies {
+                testImplementation 'junit:junit:4.13'
+            }
+
+            tasks.test {
+                def nonExistent = files(
+                    System.getProperty("user.home"),
+                    System.getProperty("user.home") + File.separator + "*",
+                    System.getProperty("user.home") + File.separator + "Non exist path",
+                    System.getProperty("user.home") + File.separator + "Non exist path" + File.separator + "*"
+                )
+                doFirst {
+                    classpath += nonExistent
+                }
+            }
         """
 
         expect:
