b4be739e43116a31305c3b123e8d4c598dc87af8
Gary Hale <gary@gradle.com>
2025-07-21 09:26:08 -0400

    Remove Configuration.visible property

--- subprojects/core/src/main/java/org/gradle/api/internal/artifacts/configurations/ResolveConfigurationDependenciesBuildOperationType.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/artifacts/configurations/ResolveConfigurationDependenciesBuildOperationType.java
@@ -47,8 +47,6 @@ public final class ResolveConfigurationDependenciesBuildOperationType implements
 
         String getBuildPath();
 
-        boolean isConfigurationVisible();
-
         boolean isConfigurationTransitive();
 
         /**
