cd9cffef6de35fbee0fb5c1c17b840f4758959c6
Octavia Togami <otogami@gradle.com>
2025-07-28 11:51:29 -0700

    Initialize mostRecentReleaseExecuter later
    This avoids the assumption in getOtherVersion from becoming an error

--- platforms/core-execution/execution-e2e-tests/src/integTest/groovy/org/gradle/integtests/StaleOutputHistoryLossIntegrationTest.groovy
+++ platforms/core-execution/execution-e2e-tests/src/integTest/groovy/org/gradle/integtests/StaleOutputHistoryLossIntegrationTest.groovy
@@ -23,7 +23,6 @@ import org.gradle.integtests.fixtures.StaleOutputJavaProject
 import org.gradle.integtests.fixtures.UnsupportedWithConfigurationCache
 import org.gradle.integtests.fixtures.executer.AbstractGradleExecuter
 import org.gradle.integtests.fixtures.executer.ExecutionResult
-import org.gradle.integtests.fixtures.executer.GradleExecuter
 import org.gradle.integtests.fixtures.timeout.IntegrationTestTimeout
 import spock.lang.Issue
 
@@ -33,13 +32,14 @@ import static org.gradle.util.internal.GFileUtils.forceDelete
 @IntegrationTestTimeout(240)
 class StaleOutputHistoryLossIntegrationTest extends AbstractIntegrationSpec implements OtherGradleVersionFixture {
 
-    private final GradleExecuter mostRecentReleaseExecuter = otherVersion.executer(temporaryFolder, buildContext) as AbstractGradleExecuter
+    private AbstractGradleExecuter mostRecentReleaseExecuter
 
     def cleanup() {
-        mostRecentReleaseExecuter.cleanup()
+        mostRecentReleaseExecuter?.cleanup()
     }
 
     def setup() {
+        mostRecentReleaseExecuter = otherVersion.executer(temporaryFolder, buildContext) as AbstractGradleExecuter
         buildFile << "apply plugin: 'base'\n"
     }
 
