a8f96005bcacff5c47a638eb3ce018ff1d108a04
Reinhold Degenfellner <rdegenfellner@gradle.com>
2025-07-30 16:45:50 +0200

    create include failing service

--- subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeServices.java
+++ subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeServices.java
@@ -265,6 +265,15 @@ public class BuildScopeServices implements ServiceRegistrationProvider {
             services.registerBuildServices(registration);
         }
     }
+//
+//    @Provides
+//    BuildIncludeListener createBuildIncludeListener(BuildModelParameters buildModelParameters){
+//        if(buildModelParameters.isResilientModelBuilding()){
+//            return new BrokenBuildsCapturingListener();
+//        }
+//        //ignored in non-resilient model building
+//        return (buildState, exception) -> {};
+//    }
 
     @Provides
     OrdinalGroupFactory createOrdinalGroupFactory() {
@@ -777,4 +786,5 @@ public class BuildScopeServices implements ServiceRegistrationProvider {
     protected BuildInitSpecRegistry createInitProjectSpecRegistry() {
         return new BuildInitSpecRegistry();
     }
+
 }
