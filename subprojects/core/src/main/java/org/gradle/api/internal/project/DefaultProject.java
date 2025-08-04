92f44432b4d567c190138bcc6a12be081045946c
Sterling Greene <sterling@gradle.com>
2025-08-04 12:04:37 -0400

    Avoid deprecation nagging for method used by KGP

--- subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
@@ -1480,18 +1480,19 @@ public abstract class DefaultProject extends AbstractPluginAware implements Proj
     @Override
     @Deprecated
     public <T> NamedDomainObjectContainer<T> container(Class<T> type, NamedDomainObjectFactory<T> factory) {
-        DeprecationLogger.deprecateMethod(Project.class, "container(Class)").
-            replaceWith("objects.domainObjectContainer(Class, NamedDomainObjectFactory)").
-            willBeRemovedInGradle10().
-            withUpgradeGuideSection(9, "project_container_methods").
-            nagUser();
+        // KGP uses this method to create a container for the Kotlin DSL
+//        DeprecationLogger.deprecateMethod(Project.class, "container(Class, NamedDomainObjectFactory)").
+//            replaceWith("objects.domainObjectContainer(Class, NamedDomainObjectFactory)").
+//            willBeRemovedInGradle10().
+//            withUpgradeGuideSection(9, "project_container_methods").
+//            nagUser();
         return getServices().get(DomainObjectCollectionFactory.class).newNamedDomainObjectContainer(type, factory);
     }
 
     @Override
     @Deprecated
     public <T> NamedDomainObjectContainer<T> container(Class<T> type, Closure factoryClosure) {
-        DeprecationLogger.deprecateMethod(Project.class, "container(Class)").
+        DeprecationLogger.deprecateMethod(Project.class, "container(Class, Closure)").
             replaceWith("objects.domainObjectContainer(Class, NamedDomainObjectFactory)").
             willBeRemovedInGradle10().
             withUpgradeGuideSection(9, "project_container_methods").
