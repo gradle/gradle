omeos-fork/gradle.gradle

Decouple managed object creation from class generator
Previously, the AbstractClassGenerator via the ManagedObjectFactory needed to know about all the managed types
it could instantiate in a managed object at compile-time. For types like DependencyCollector, this required
the class generator to have knowledge of DependencyCollector.

This commit introduces the ManagedObjectCreator and ManagedObjectProvider annoations to allow injectable managed
object factories to be discovered at runtime.

Factory classes annotated with ManagedObjectProvider are discovered as they are registered in the service registry. These
providers may declare methods annotate with ManagedObjectCreator. These annotated methods are used as candidates for
types that can be injected into Gradle managed types

subprojects/core/src/main/java/org/gradle/internal/service/scopes/BasicGlobalScopeServices.java
@@ -44,6 +44,7 @@ import org.gradle.internal.event.DefaultListenerManager;
 import org.gradle.internal.event.ListenerManager;
 import org.gradle.internal.event.ScopedListenerManager;
 import org.gradle.internal.file.PathToFileResolver;
+import org.gradle.internal.instantiation.generator.ManagedObjectRegistry;
 import org.gradle.internal.nativeintegration.ProcessEnvironment;
 import org.gradle.internal.nativeintegration.filesystem.FileSystem;
 import org.gradle.internal.remote.internal.inet.InetAddressFactory;
@@ -66,6 +67,11 @@ public class BasicGlobalScopeServices implements ServiceRegistrationProvider {
     void configure(ServiceRegistration serviceRegistration) {
         serviceRegistration.add(FileLookup.class, DefaultFileLookup.class);
         serviceRegistration.addProvider(new MessagingServices());
+    }
+
+    @Provides
+    ManagedObjectRegistry createManagedObjectProvider() {
+        return new ManagedObjectRegistry(null);
     }
 
     @Provides

subprojects/core/src/main/java/org/gradle/internal/service/scopes/WorkerSharedProjectScopeServices.java
@@ -48,6 +48,7 @@ import org.gradle.internal.file.Deleter;
 import org.gradle.internal.file.PathToFileResolver;
 import org.gradle.internal.hash.FileHasher;
 import org.gradle.internal.instantiation.InstantiatorFactory;
+import org.gradle.internal.instantiation.generator.ManagedObjectRegistry;
 import org.gradle.internal.nativeintegration.filesystem.FileSystem;
 import org.gradle.internal.reflect.Instantiator;
 import org.gradle.internal.service.Provides;
@@ -73,6 +74,11 @@ public class WorkerSharedProjectScopeServices implements ServiceRegistrationProv
     void configure(ServiceRegistration registration) {
         registration.add(PropertyFactory.class, DefaultPropertyFactory.class);
         registration.add(FilePropertyFactory.class, FileFactory.class, DefaultFilePropertyFactory.class);
+    }
+
+    @Provides
+    ManagedObjectRegistry decorateManagedObjectRegistry(ManagedObjectRegistry parent) {
+        return new ManagedObjectRegistry(parent);
     }
 
     @Provides

gradle/gradle

subprojects/core/src/main/java/org/gradle/internal/service/scopes/BasicGlobalScopeServices.java
@@ -43,6 +43,7 @@ import org.gradle.internal.event.DefaultListenerManager;
 import org.gradle.internal.event.ListenerManager;
 import org.gradle.internal.event.ScopedListenerManager;
 import org.gradle.internal.file.PathToFileResolver;
+import org.gradle.internal.instantiation.managed.ManagedObjectRegistry;
 import org.gradle.internal.nativeintegration.ProcessEnvironment;
 import org.gradle.internal.nativeintegration.filesystem.FileSystem;
 import org.gradle.internal.remote.internal.inet.InetAddressFactory;
@@ -65,6 +66,11 @@ public class BasicGlobalScopeServices implements ServiceRegistrationProvider {
     void configure(ServiceRegistration serviceRegistration) {
         serviceRegistration.add(FileLookup.class, DefaultFileLookup.class);
         serviceRegistration.addProvider(new MessagingServices());
+    }
+
+    @Provides
+    ManagedObjectRegistry createManagedObjectRegistry() {
+        return new ManagedObjectRegistry(null);
     }
 
     @Provides

subprojects/core/src/main/java/org/gradle/internal/service/scopes/WorkerSharedProjectScopeServices.java
@@ -47,6 +47,7 @@ import org.gradle.internal.file.Deleter;
 import org.gradle.internal.file.PathToFileResolver;
 import org.gradle.internal.hash.FileHasher;
 import org.gradle.internal.instantiation.InstantiatorFactory;
+import org.gradle.internal.instantiation.managed.ManagedObjectRegistry;
 import org.gradle.internal.nativeintegration.filesystem.FileSystem;
 import org.gradle.internal.reflect.Instantiator;
 import org.gradle.internal.service.Provides;
@@ -72,6 +73,11 @@ public class WorkerSharedProjectScopeServices implements ServiceRegistrationProv
     void configure(ServiceRegistration registration) {
         registration.add(PropertyFactory.class, DefaultPropertyFactory.class);
         registration.add(FilePropertyFactory.class, FileFactory.class, DefaultFilePropertyFactory.class);
+    }
+
+    @Provides
+    ManagedObjectRegistry decorateManagedObjectRegistry(ManagedObjectRegistry parent) {
+        return new ManagedObjectRegistry(parent);
     }
 
     @Provides