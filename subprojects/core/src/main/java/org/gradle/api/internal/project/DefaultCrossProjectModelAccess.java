16a49c4b909d6e9b3df36404b0f46cd3efb30eb6
Sergey Opivalov <sopivalov@gradle.com>
2025-08-18 14:17:31 +0300

    Unify wrapping around

--- subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultCrossProjectModelAccess.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultCrossProjectModelAccess.java
@@ -32,14 +32,16 @@ import java.util.stream.Collectors;
 public class DefaultCrossProjectModelAccess implements CrossProjectModelAccess {
 
     private final ProjectRegistry projectRegistry;
+    private final ProjectWrapperFactory projectWrapperFactory;
 
-    public DefaultCrossProjectModelAccess(ProjectRegistry projectRegistry) {
+    public DefaultCrossProjectModelAccess(ProjectRegistry projectRegistry, ProjectWrapperFactory projectWrapperFactory) {
         this.projectRegistry = projectRegistry;
+        this.projectWrapperFactory = projectWrapperFactory;
     }
 
     @Override
     public ProjectInternal access(ProjectInternal referrer, ProjectInternal project) {
-        return LifecycleAwareProject.from(project, referrer);
+        return LifecycleAwareProject.wrap(project, referrer, projectWrapperFactory);
     }
 
     @Override
@@ -50,7 +52,7 @@ public class DefaultCrossProjectModelAccess implements CrossProjectModelAccess {
         }
 
         ProjectInternal project = projectRegistry.getProjectInternal(path.asString());
-        return project != null ? LifecycleAwareProject.from(project, referrer) : null;
+        return project != null ? LifecycleAwareProject.wrap(project, referrer, projectWrapperFactory) : null;
     }
 
     @Override
@@ -58,7 +60,7 @@ public class DefaultCrossProjectModelAccess implements CrossProjectModelAccess {
         return target.getOwner().getChildProjects().stream().collect(
             Collectors.toMap(
                 ProjectState::getName,
-                projectState -> LifecycleAwareProject.from(projectState.getMutableModel(), referrer)
+                projectState -> LifecycleAwareProject.wrap(projectState.getMutableModel(), referrer, projectWrapperFactory)
             )
         );
     }
@@ -66,14 +68,14 @@ public class DefaultCrossProjectModelAccess implements CrossProjectModelAccess {
     @Override
     public Set<? extends ProjectInternal> getSubprojects(ProjectInternal referrer, ProjectInternal target) {
         return projectRegistry.getSubProjects(target.getPath()).stream()
-            .map(project -> LifecycleAwareProject.from(project, referrer))
+            .map(project -> LifecycleAwareProject.wrap(project, referrer, projectWrapperFactory))
             .collect(Collectors.toCollection(TreeSet::new));
     }
 
     @Override
     public Set<? extends ProjectInternal> getAllprojects(ProjectInternal referrer, ProjectInternal target) {
         return projectRegistry.getAllProjects(target.getPath()).stream()
-            .map(project -> LifecycleAwareProject.from(project, referrer))
+            .map(project -> LifecycleAwareProject.wrap(project, referrer, projectWrapperFactory))
             .collect(Collectors.toCollection(TreeSet::new));
     }
 
