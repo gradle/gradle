3b2010f295028ba22939cbe87a15f4405ef6c7ba
Christoph Obexer <cobexer@gradle.com>
2025-08-29 13:14:50 +0200

    `java` plugin tests: update test assumptions based on used JVM version
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/plugins/ide/tooling/r211/ToolingApiEclipseModelCrossVersionSpec.groovy
+++ platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/plugins/ide/tooling/r211/ToolingApiEclipseModelCrossVersionSpec.groovy
@@ -17,8 +17,12 @@
 package org.gradle.plugins.ide.tooling.r211
 
 import org.gradle.api.JavaVersion
+import org.gradle.integtests.fixtures.AvailableJavaHomes
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
+import org.gradle.test.precondition.Requires
+import org.gradle.test.preconditions.IntegTestPreconditions
 import org.gradle.tooling.model.eclipse.EclipseProject
+import org.junit.Assume
 
 import static org.gradle.plugins.ide.tooling.r210.ConventionsExtensionsCrossVersionFixture.javaTargetCompatibility
 
@@ -28,18 +32,27 @@ class ToolingApiEclipseModelCrossVersionSpec extends ToolingApiSpecification {
         settingsFile << "rootProject.name = 'root'"
     }
 
+    @Requires(value = [IntegTestPreconditions.Java17HomeAvailable, IntegTestPreconditions.Java21HomeAvailable, IntegTestPreconditions.NotEmbeddedExecutor])
     def "Java project has target bytecode level"() {
+        Assume.assumeTrue("Target Gradle version supports running with Java " + jvm.javaVersionMajor, targetDist.daemonWorksWith(jvm.javaVersionMajor))
+
         given:
         buildFile << "apply plugin: 'java'"
 
         when:
-        EclipseProject rootProject = loadToolingModel(EclipseProject)
+        EclipseProject rootProject = loadToolingModel(EclipseProject, jvm)
 
         then:
-        rootProject.javaSourceSettings.targetBytecodeVersion== JavaVersion.current()
+        rootProject.javaSourceSettings.targetBytecodeVersion == JavaVersion.toVersion(jvm.javaVersion.majorVersion)
+
+        where:
+        jvm << [AvailableJavaHomes.jdk17, AvailableJavaHomes.jdk21]
     }
 
+    @Requires(value = [IntegTestPreconditions.Java17HomeAvailable, IntegTestPreconditions.Java21HomeAvailable, IntegTestPreconditions.NotEmbeddedExecutor])
     def "Java project has jdk"() {
+        Assume.assumeTrue("Target Gradle version supports running with Java " + jvm.javaVersionMajor, targetDist.daemonWorksWith(jvm.javaVersionMajor))
+
         given:
         buildFile << """
 apply plugin: 'java'
@@ -47,12 +60,15 @@ apply plugin: 'java'
 description = org.gradle.internal.jvm.Jvm.current().javaHome.toString()
 """
         when:
-        EclipseProject rootProject = loadToolingModel(EclipseProject)
+        EclipseProject rootProject = loadToolingModel(EclipseProject, jvm)
 
         then:
         rootProject.javaSourceSettings.jdk != null
-        rootProject.javaSourceSettings.jdk.javaVersion == JavaVersion.current()
+        rootProject.javaSourceSettings.jdk.javaVersion == JavaVersion.toVersion(jvm.javaVersion.majorVersion)
         rootProject.javaSourceSettings.jdk.javaHome.toString() == rootProject.gradleProject.description
+
+        where:
+        jvm << [AvailableJavaHomes.jdk17, AvailableJavaHomes.jdk21]
     }
 
     def "target bytecode level respects explicit targetCompatibility configuration"() {
