435e0e9c3fa82161d0f86e22d4640805aeffd0eb
Vincent Potucek <vpotucek@me.com>
2025-09-13 16:11:22 +0200

    Fix `ModifyCollectionInEnhancedForLoop`
    Signed-off-by: Vincent Potucek <vpotucek@me.com>

--- subprojects/core/build.gradle.kts
+++ subprojects/core/build.gradle.kts
@@ -45,7 +45,6 @@ val testInterceptorsImplementation: Configuration by configurations.getting {
 errorprone {
     disabledChecks.addAll(
         "DefaultCharset", // 4 occurrences
-        "ModifyCollectionInEnhancedForLoop", // 1 occurrences
         "NonApiType", // 1 occurrences
         "NonCanonicalType", // 16 occurrences
         "ReferenceEquality", // 2 occurrences
