095a293d3242f7088b26233ff587ec8ad31c4d76
Christoph Obexer <cobexer@gradle.com>
2025-09-18 17:57:10 +0200

    WIP: Revert "Compile configuration cache to Java 17" this should not stay in
    This reverts commit 4fa6a581a995b574132680fa6b61de07fc6fbc6e.

--- platforms/core-configuration/configuration-cache/build.gradle.kts
+++ platforms/core-configuration/configuration-cache/build.gradle.kts
@@ -127,14 +127,6 @@ dependencies {
     crossVersionTestDistributionRuntimeOnly(projects.distributionsCore)
 }
 
-jvmCompile {
-    compilations {
-        named("main") {
-            targetJvmVersion = 17
-        }
-    }
-}
-
 packageCycles {
     excludePatterns.add("org/gradle/internal/cc/**")
 }
