e2ab9b8a310d65b958f44c8927c35ca05b21c670
Tom Tresansky <ttresansky@gradle.com>
2025-09-30 11:24:22 -0400

    Fix sanityCheck errors

--- platforms/software/testing-base-infrastructure/build.gradle.kts
+++ platforms/software/testing-base-infrastructure/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation(projects.serviceRegistryBuilder)
 
     implementation(libs.commonsLang)
+    implementation(libs.guava)
     implementation(libs.slf4jApi)
 
     testImplementation(projects.serviceRegistryImpl)
