fb2c5c5d0c21f5408a06ac2c90a8346ab0950b09
Alex Semin <asemin@gradle.com>
2025-10-10 16:31:53 +0200

    Avoid ProjectState.parent when assigning project names

--- platforms/ide/ide/src/main/java/org/gradle/plugins/ide/internal/configurer/DefaultUniqueProjectNameProvider.java
+++ platforms/ide/ide/src/main/java/org/gradle/plugins/ide/internal/configurer/DefaultUniqueProjectNameProvider.java
@@ -16,14 +16,20 @@
 package org.gradle.plugins.ide.internal.configurer;
 
 import org.gradle.api.Project;
+import org.gradle.api.internal.project.ProjectIdentity;
 import org.gradle.api.internal.project.ProjectState;
 import org.gradle.api.internal.project.ProjectStateRegistry;
+import org.gradle.util.Path;
+import org.jspecify.annotations.Nullable;
 
+import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
 
 public class DefaultUniqueProjectNameProvider implements UniqueProjectNameProvider {
     private final ProjectStateRegistry projectRegistry;
-    private Map<ProjectState, String> deduplicated;
+    private Map<ProjectIdentity, String> deduplicated;
 
     public DefaultUniqueProjectNameProvider(ProjectStateRegistry projectRegistry) {
         this.projectRegistry = projectRegistry;
@@ -32,36 +38,49 @@ public class DefaultUniqueProjectNameProvider implements UniqueProjectNameProvid
     @Override
     public String getUniqueName(Project project) {
         ProjectState projectState = projectRegistry.stateFor(project);
-        String uniqueName = getDeduplicatedNames().get(projectState);
+        String uniqueName = getDeduplicatedNames().get(projectState.getIdentity());
         if (uniqueName != null) {
             return uniqueName;
         }
         return project.getName();
     }
 
-    private synchronized Map<ProjectState, String> getDeduplicatedNames() {
+    private synchronized Map<ProjectIdentity, String> getDeduplicatedNames() {
         if (deduplicated == null) {
-            HierarchicalElementDeduplicator<ProjectState> deduplicator = new HierarchicalElementDeduplicator<ProjectState>(new ProjectPathDeduplicationAdapter());
-            this.deduplicated = deduplicator.deduplicate(projectRegistry.getAllProjects());
+            HierarchicalElementDeduplicator<ProjectIdentity> deduplicator = new HierarchicalElementDeduplicator<>(new ProjectPathDeduplicationAdapter());
+            List<ProjectIdentity> allProjects = projectRegistry.getAllProjects().stream()
+                .map(ProjectState::getIdentity)
+                .collect(toList());
+            this.deduplicated = deduplicator.deduplicate(allProjects);
         }
         return deduplicated;
     }
 
-    private static class ProjectPathDeduplicationAdapter implements HierarchicalElementAdapter<ProjectState> {
+    private class ProjectPathDeduplicationAdapter implements HierarchicalElementAdapter<ProjectIdentity> {
         @Override
-        public String getName(ProjectState element) {
-            return element.getName();
+        public String getName(ProjectIdentity element) {
+            return element.getProjectName();
         }
 
         @Override
-        public String getIdentityName(ProjectState element) {
-            String identityName = element.getIdentityPath().getName();
-            return identityName != null ? identityName : element.getName();
+        public String getIdentityName(ProjectIdentity element) {
+            String identityName = element.getBuildTreePath().getName();
+            return identityName != null ? identityName : element.getProjectName();
         }
 
         @Override
-        public ProjectState getParent(ProjectState element) {
-            return element.getParent();
+        @Nullable
+        public ProjectIdentity getParent(ProjectIdentity element) {
+            // Note that this "parent" may belong to a different build in the build tree
+            Path parentInBuildTreePath = element.getBuildTreePath().getParent();
+            if (parentInBuildTreePath == null) {
+                return null;
+            }
+            ProjectState parentInBuildTreeState = projectRegistry.findProjectState(parentInBuildTreePath);
+            if (parentInBuildTreeState == null) {
+                return null;
+            }
+            return parentInBuildTreeState.getIdentity();
         }
     }
 }
