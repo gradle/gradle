7e61c218620bc6c99ac6b0a23452f6c5b32d4de4
Alex Semin <asemin@gradle.com>
2025-10-13 10:01:00 +0200

    Remove unused method from ProjectInternal

--- subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
@@ -1220,11 +1220,6 @@ public abstract class DefaultProject extends AbstractPluginAware implements Proj
     }
 
     @Override
-    public ServiceRegistryFactory getServiceRegistryFactory() {
-        return services.get(ServiceRegistryFactory.class);
-    }
-
-    @Override
     public AntBuilder ant(Closure configureClosure) {
         return ConfigureUtil.configure(configureClosure, getAnt());
     }
