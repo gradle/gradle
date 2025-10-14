fb54f27b7a83f901ea54b21909fff09c7e382400
Tom Tresansky <ttresansky@gradle.com>
2025-10-14 08:45:56 -0400

    Update example and documentation for accuracy from review

--- platforms/documentation/docs/src/snippets/bestPractices/preferPathSensitivityNone-avoid/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/bestPractices/preferPathSensitivityNone-avoid/kotlin/build.gradle.kts
@@ -25,7 +25,7 @@ val alternateInputsDir = layout.buildDirectory.dir("alternateInputs")
 tasks.register<AnimalSearchTask>("search") {
     find = "cat"
     if (project.hasProperty("useAlternateInput")) { // <2>
-        candidatesFile = alternateInputsDir.flatMap { project.provider { it.file("candidates.txt") } }
+        candidatesFile = alternateInputsDir.map { it.file("candidates.txt") }
     } else {
         candidatesFile = originalCandidatesFile
     }
