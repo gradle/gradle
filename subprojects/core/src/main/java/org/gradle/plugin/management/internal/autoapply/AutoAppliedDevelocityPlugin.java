d270a7ae12c848c79c307b2b0efe3e2e8cda56fb
Daniel Clausen <dclausen@gradle.com>
2025-10-14 11:55:34 +0200

    Update to DV Gradle plugin 4.2.2

--- subprojects/core/src/main/java/org/gradle/plugin/management/internal/autoapply/AutoAppliedDevelocityPlugin.java
+++ subprojects/core/src/main/java/org/gradle/plugin/management/internal/autoapply/AutoAppliedDevelocityPlugin.java
@@ -28,7 +28,7 @@ public final class AutoAppliedDevelocityPlugin {
 
     public static final String GROUP = "com.gradle";
     public static final String NAME = "develocity-gradle-plugin";
-    public static final String VERSION = "4.2.1";
+    public static final String VERSION = "4.2.2";
 
 
     public static final PluginId ID = new DefaultPluginId("com.gradle.develocity");
