1ab725f1a9fe45df254fe6d802857fab535e4ed9
József Bartók <jbartok@gradle.com>
2025-10-15 09:01:23 +0300

    Bump embedded version

--- build-logic/dependency-modules/src/main/kotlin/gradlebuild/modules/extension/ExternalModulesExtension.kt
+++ build-logic/dependency-modules/src/main/kotlin/gradlebuild/modules/extension/ExternalModulesExtension.kt
@@ -28,7 +28,7 @@ abstract class ExternalModulesExtension(bundleGroovyMajor: Int) {
 
     val configurationCacheReportVersion = "1.27"
     val gradleIdeStarterVersion = "0.8.1"
-    val kotlinVersion = "2.2.20"
+    val kotlinVersion = "2.3.0-Beta1"
 
     fun futureKotlin(module: String) = "org.jetbrains.kotlin:kotlin-$module:$kotlinVersion"
 
--- platforms/documentation/docs/src/snippets/plugins/pluginProject/kotlin/database-logic/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/plugins/pluginProject/kotlin/database-logic/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm") version "2.2.20"
+    id("org.jetbrains.kotlin.jvm") version "2.2.20" // TODO: 2.3.0
 }
 
 repositories {
