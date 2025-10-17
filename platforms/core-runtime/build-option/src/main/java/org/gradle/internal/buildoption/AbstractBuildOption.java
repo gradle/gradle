9bc713755f9c4d35ebe8de3c3b3760ef80db6c56
Alex Semin <asemin@gradle.com>
2025-10-17 10:46:45 +0200

    Align definition of enabled boolean flag

--- platforms/core-runtime/build-option/src/main/java/org/gradle/internal/buildoption/AbstractBuildOption.java
+++ platforms/core-runtime/build-option/src/main/java/org/gradle/internal/buildoption/AbstractBuildOption.java
@@ -65,10 +65,6 @@ public abstract class AbstractBuildOption<T, V extends CommandLineOptionConfigur
         return deprecatedProperty;
     }
 
-    protected boolean isTrue(String value) {
-        return value != null && value.trim().equalsIgnoreCase("true");
-    }
-
     protected CommandLineOption configureCommandLineOption(CommandLineParser parser, String[] options, String description, boolean deprecated, boolean incubating) {
         CommandLineOption option = parser.option(options)
             .hasDescription(description);
