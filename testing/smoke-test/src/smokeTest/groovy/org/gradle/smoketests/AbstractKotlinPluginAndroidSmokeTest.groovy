4173b8062de7e1df8fef9eb2dd506a4b79dc4d14
Paul Merlin <paul@gradle.com>
2025-10-23 08:51:38 +0200

    Add documentation to AbstractKotlinPluginAndroidSmokeTest

--- testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractKotlinPluginAndroidSmokeTest.groovy
+++ testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AbstractKotlinPluginAndroidSmokeTest.groovy
@@ -27,6 +27,9 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
  * Abstract base class for testing the Kotlin plugin with Android projects.
  *
  * This test exists to avoid duplicating test logic for Groovy and Kotlin DSLs.
+ *
+ * This test exercise AGP + KGP-android plugins.
+ * This is not supported with AGP >= 9.0 that comes with built-in Kotlin support.
  */
 @LocalOnly(because = "Needs Android environment")
 abstract class AbstractKotlinPluginAndroidSmokeTest extends AbstractSmokeTest implements RunnerFactory {
