61a792b1c2cc79bd22eb6329225b7c287c5da37b
Bálint Hegyi <bhegyi@gradle.com>
2025-10-30 11:16:21 +0000

    Remove semicolons

--- testing/internal-integ-testing/src/main/groovy/org/gradle/test/fixtures/plugin/PluginBuilder.groovy
+++ testing/internal-integ-testing/src/main/groovy/org/gradle/test/fixtures/plugin/PluginBuilder.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import org.gradle.test.fixtures.server.http.MavenHttpPluginRepository
 import org.gradle.util.internal.TextUtil
 
 class PluginBuilder {
-    static final String PLUGIN_MARKER_SUFFIX = ".gradle.plugin";
+    static final String PLUGIN_MARKER_SUFFIX = ".gradle.plugin"
 
     final TestFile projectDir
 
@@ -167,7 +167,7 @@ class PluginBuilder {
             markerModules.add(marker)
         }
 
-        publishTo(executer, artifactFile);
+        publishTo(executer, artifactFile)
 
         return new PluginPublicationResults(module, markerModules)
     }
