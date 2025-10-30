4d8bc6507a66fd4fbb2d73f40abf5e8d034c401a
Bálint Hegyi <bhegyi@gradle.com>
2025-10-30 08:23:13 +0000

    Update copyright, and add the correct mirror

--- platforms/extensibility/test-kit/src/integTest/groovy/org/gradle/testkit/TestKitWithPluginClasspathTests.groovy
+++ platforms/extensibility/test-kit/src/integTest/groovy/org/gradle/testkit/TestKitWithPluginClasspathTests.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.gradle.testkit
 
 import org.gradle.integtests.fixtures.AbstractIntegrationSpec
@@ -37,10 +53,8 @@ class TestKitWithPluginClasspathTests extends AbstractIntegrationSpec {
             include("plugin-tested")
 
             dependencyResolutionManagement {
-                repositories {
-                    // Unfortunately we will need JUnit for the tests in plugin-tested
-                    mavenCentral()
-                }
+                // We will need JUnit for the tests in plugin-tested
+                ${mavenCentralRepository()}
             }
         """
 
