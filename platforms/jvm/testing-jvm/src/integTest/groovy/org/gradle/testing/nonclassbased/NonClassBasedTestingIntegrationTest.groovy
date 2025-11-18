addab47e0ca8572687a6d8a23071cd1283477d94
Tom Tresansky <ttresansky@gradle.com>
2025-11-18 10:02:43 -0500

    Verify TestListener behavior with resource-based testing

--- platforms/jvm/testing-jvm/src/integTest/groovy/org/gradle/testing/nonclassbased/NonClassBasedTestingIntegrationTest.groovy
+++ platforms/jvm/testing-jvm/src/integTest/groovy/org/gradle/testing/nonclassbased/NonClassBasedTestingIntegrationTest.groovy
@@ -16,6 +16,9 @@
 
 package org.gradle.testing.nonclassbased
 
+import static org.gradle.util.Matchers.containsLine
+import static org.gradle.util.Matchers.matchesRegexp
+
 /**
  * Tests that exercise and demonstrate Non-Class-Based Testing using the {@code Test} task
  * and a sample resource-based JUnit Platform Test Engine defined in this project's {@code testFixtures}.
@@ -197,5 +200,54 @@ class NonClassBasedTestingIntegrationTest extends AbstractNonClassBasedTestingIn
         "INFO: Executing resource-based test: Test[file=subSomeOtherTestSpec.rbt, name=other]"].forEach {
             result.getOutput().findAll(it).size() == 1
         }
+    }
+
+    def "can listen for non-class-based tests"() {
+        given:
+        buildFile << """
+            plugins {
+                id 'java-library'
+            }
+
+            ${mavenCentralRepository()}
+
+            class TestListenerImpl implements TestListener {
+                void beforeSuite(TestDescriptor suite) { println "START [\$suite] [\$suite.name]" }
+                void afterSuite(TestDescriptor suite, TestResult result) { println "FINISH [\$suite] [\$suite.name] [\$result.resultType] [\$result.testCount]" }
+                void beforeTest(TestDescriptor test) { println "START [\$test] [\$test.name]" }
+                void afterTest(TestDescriptor test, TestResult result) { println "FINISH [\$test] [\$test.name] [\$result.resultType] [\$result.testCount] [\$result.exception]" }
+            }
+            def listener = new TestListenerImpl()
+
+            testing.suites.test {
+                ${enableEngineForSuite()}
+
+                targets.all {
+                    testTask.configure {
+                        addTestListener(listener)
+                        testDefinitionDirs.from("$DEFAULT_DEFINITIONS_LOCATION")
+                    }
+                }
+            }
+        """
+
+        writeTestDefinitions(DEFAULT_DEFINITIONS_LOCATION)
+
+        when:
+        def result = succeeds("test")
+
+        then:
+        containsLine(result.getOutput(), "START [Gradle Test Run :test] [Gradle Test Run :test]")
+        containsLine(result.getOutput(), "FINISH [Gradle Test Run :test] [Gradle Test Run :test] [SUCCESS] [3]")
+
+        containsLine(result.getOutput(), matchesRegexp("START \\[Gradle Test Executor \\d+\\] \\[Gradle Test Executor \\d+\\]"))
+        containsLine(result.getOutput(), matchesRegexp("FINISH \\[Gradle Test Executor \\d+\\] \\[Gradle Test Executor \\d+\\] \\[SUCCESS\\] \\[3\\]"))
+
+        containsLine(result.getOutput(), "START [Test SomeTestSpec.rbt : foo(UnknownClass)] [SomeTestSpec.rbt : foo]")
+        containsLine(result.getOutput(), "FINISH [Test SomeTestSpec.rbt : foo(UnknownClass)] [SomeTestSpec.rbt : foo] [SUCCESS] [1] [null]")
+        containsLine(result.getOutput(), "START [Test SomeTestSpec.rbt : bar(UnknownClass)] [SomeTestSpec.rbt : bar]")
+        containsLine(result.getOutput(), "FINISH [Test SomeTestSpec.rbt : bar(UnknownClass)] [SomeTestSpec.rbt : bar] [SUCCESS] [1] [null]")
+        containsLine(result.getOutput(), "START [Test subSomeOtherTestSpec.rbt : other(UnknownClass)] [subSomeOtherTestSpec.rbt : other]")
+        containsLine(result.getOutput(), "FINISH [Test subSomeOtherTestSpec.rbt : other(UnknownClass)] [subSomeOtherTestSpec.rbt : other] [SUCCESS] [1] [null]")
     }
 }
