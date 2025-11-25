1a35bfd7796cb9c59fda3451f4f721df373375f0
Gorlesunilkumar <g2005.sunil2005@gmail.com>
2025-11-25 19:17:06 +0530

    docs: fix grammar by changing "module" to "modules" in LockMode comment
    This commit corrects a minor grammatical issue in the LockMode
    documentation comment by changing "module to be added or removed" to
    "modules to be added or removed" for improved clarity.
    
    No functional code changes were made; only documentation comments were
    updated.
    
    Signed-off-by: Sunil <g2005.sunil2005@gmail.com>
    
    
    Signed-off-by: Gorlesunilkumar <g2005.sunil2005@gmail.com>

--- subprojects/core-api/src/main/java/org/gradle/api/artifacts/dsl/LockMode.java
+++ subprojects/core-api/src/main/java/org/gradle/api/artifacts/dsl/LockMode.java
@@ -22,7 +22,8 @@ package org.gradle.api.artifacts.dsl;
  *     <li>{@code DEFAULT} will load the lock state and verify resolution matches it</li>
  *     <li>{@code STRICT} in addition to the {@code DEFAULT} behaviour, will fail resolution if a locked configuration does not have lock state defined</li>
  *     <li>{@code LENIENT} will load the lock state, to anchor dynamic versions, but otherwise be lenient about modifications of the dependency resolution,
- *     allowing versions to change and module to be added or removed</li>
+ *     allowing versions to change and modules to be added or removed
+</li>
  * </ul>
  *
  * @since 6.1
