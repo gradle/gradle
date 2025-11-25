df6a003865165f153fa1c0d212177da2b3cf1d75
Gary Hale <gary@gradle.com>
2025-11-25 11:19:48 -0500

    Update build init test to use latest prototype plugins

--- platforms/software/build-init/src/integTest/groovy/org/gradle/buildinit/specs/internal/BuildInitSpecsIntegrationTest.groovy
+++ platforms/software/build-init/src/integTest/groovy/org/gradle/buildinit/specs/internal/BuildInitSpecsIntegrationTest.groovy
@@ -26,12 +26,11 @@ import org.gradle.test.fixtures.file.LeaksFileHandles
 import org.gradle.test.precondition.Requires
 import org.gradle.test.preconditions.IntegTestPreconditions
 import org.gradle.test.preconditions.UnitTestPreconditions
-import spock.lang.Ignore
 
 @Requires(UnitTestPreconditions.Jdk17OrLater)
 class BuildInitSpecsIntegrationTest extends AbstractInitIntegrationSpec implements TestsBuildInitSpecsViaPlugin, JavaToolchainFixture {
     private static final String DECLARATIVE_JVM_PLUGIN_ID = "org.gradle.experimental.jvm-ecosystem-init"
-    private static final String DECLARATIVE_PLUGIN_VERSION = "0.1.33"
+    private static final String DECLARATIVE_PLUGIN_VERSION = "0.1.48"
     private static final String DECLARATIVE_PLUGIN_SPEC = "$DECLARATIVE_JVM_PLUGIN_ID:$DECLARATIVE_PLUGIN_VERSION"
 
     // Just need an arbitrary Plugin<Settings> here, so use the Declarative Prototype.  Note that we can't use JVM, because
@@ -252,7 +251,6 @@ class BuildInitSpecsIntegrationTest extends AbstractInitIntegrationSpec implemen
         assertWrapperGenerated()
     }
 
-    @Ignore("Until a java application plugin is available that works with the latest binding API")
     @LeaksFileHandles
     @Requires(value = [
         IntegTestPreconditions.Java17HomeAvailable,
@@ -277,7 +275,7 @@ class BuildInitSpecsIntegrationTest extends AbstractInitIntegrationSpec implemen
 }
 
 plugins {
-    id("org.gradle.experimental.jvm-ecosystem").version("0.1.30")
+    id("org.gradle.experimental.jvm-ecosystem").version("0.1.47")
 }
 
 rootProject.name = "example-java-app"
@@ -321,6 +319,7 @@ defaults {
 """)
         assertProjectFileGenerated("app/build.gradle.dcl", """javaApplication {
     mainClass = "org.example.app.App"
+    jvmArguments = listOf("-Xmx2G", "-XX:+HeapDumpOnOutOfMemoryError")
 
     dependencies {
         implementation("org.apache.commons:commons-text:1.11.0")
