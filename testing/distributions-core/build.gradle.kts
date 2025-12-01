5b039f84db0b3af51ce9b62710363d569a346121
Alex Semin <asemin@gradle.com>
2025-12-01 14:20:17 +0100

    Introduce isolated-projects-impl subproject

--- testing/distributions-core/build.gradle.kts
+++ testing/distributions-core/build.gradle.kts
@@ -48,4 +48,7 @@ dependencies {
     pluginsRuntimeOnly(projects.isolatedActionServices) {
         because("This is required for IsolatedProjectEvaluationListenerProvider service implementation.")
     }
+    pluginsRuntimeOnly(projects.isolatedProjectsImpl) {
+        because("We always need a BuildModelControllerServices service implementation")
+    }
 }
