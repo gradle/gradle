75a4bd625ad5398a4d0f7a67253933f1fc9e9312
Tom Tresansky <ttresansky@gradle.com>
2025-12-02 16:05:22 -0500

    Expand with complete example

--- platforms/documentation/docs/src/snippets/bestPractices/uniqueOutputs-simple-do/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/bestPractices/uniqueOutputs-simple-do/kotlin/build.gradle.kts
@@ -1,4 +1,3 @@
-// tag::do-this[]
 abstract class GreetingTask : DefaultTask() {
     @get:Input
     abstract val type: Property<String>
@@ -8,34 +7,35 @@ abstract class GreetingTask : DefaultTask() {
     @TaskAction
     fun run() {
         val message = "Hello " + type.get()
-        outputFile.get().asFile.writeText(message)
+        outputDirectory.file(outFileName).get().asFile.writeText(message) // <2>
     }
 }
 
 abstract class ConsumerTask : DefaultTask() {
     @get:InputFile
-    abstract val inputFile: RegularFileProperty // <2>
+    abstract val inputFile: RegularFileProperty
     @get:OutputFile
     abstract val outputFile: RegularFileProperty
 
     @TaskAction
     fun run() {
-        val message = inputFile.get().asFile.readText()
+        val message = inputFile.get().asFile.text
         outputFile.get().asFile.writeText(message)
     }
 }
 
+// tag::simple-do-this[]
 val greeterA = tasks.register<GreetingTask>("greeterA") {
     type = "a"
-    outputFile = layout.buildDirectory.dir("greetings").map { it.file("a.txt") } // <3>
+    outputDirectory = layout.buildDirectory.dir("greetings")
 }
 tasks.register<GreetingTask>("greeterB") {
     type = "b"
-    outputFile = layout.buildDirectory.dir("greetings").map { it.file("b.txt") }
+    outputDirectory = layout.buildDirectory.dir("greetings-2") // <1>
 }
+// end::simple-do-this[]
 
 tasks.register<ConsumerTask>("consumer") {
-    inputFile = greeterA.map { it.outputFile.get() } // <4>
+    inputDirectory = greeterA.flatMap { it.outputDirectory }
     outputFile = layout.buildDirectory.file("consumerOutput.txt")
 }
-// end::do-this[]
