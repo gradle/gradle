b2136f2e8f5e7e7c6fd46876fa952807cb2eeb9b
József Bartók <jbartok@gradle.com>
2025-12-08 08:46:22 +0200

    Bump embedded Kotlin version to 2.3.0-RC2

--- platforms/documentation/docs/src/snippets/buildCache/caching-android-projects/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/buildCache/caching-android-projects/kotlin/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 plugins {
-    kotlin("jvm") version "2.3.0-RC"
-    kotlin("kapt") version "2.3.0-RC"
+    kotlin("jvm") version "2.3.0-RC2"
+    kotlin("kapt") version "2.3.0-RC2"
 }
 
 repositories {
