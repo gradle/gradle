5553c7f1b828d36fd31d2554ea8796446b26d2df
Sterling Greene <sterling@gradle.com>
2025-12-08 11:10:10 -0500

    Add internal method back

--- platforms/core-runtime/base-services/src/main/java/org/gradle/internal/FileUtils.java
+++ platforms/core-runtime/base-services/src/main/java/org/gradle/internal/FileUtils.java
@@ -17,6 +17,7 @@
 package org.gradle.internal;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.io.FileSystem;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
@@ -58,6 +59,26 @@ public class FileUtils {
             return len1 - len2;
         }
     };
+
+    /**
+     * The character used to replace illegal characters in file names.
+     */
+    private static final char ILLEGAL_CHAR_REPLACEMENT = '-';
+
+    /**
+     * Left for backwards compatibility with <a href="https://github.com/cashapp/paparazzi/issues/2182">Paparazzi</a>
+     */
+    @Deprecated
+    public static String toSafeFileName(String name) {
+        // Use Windows filesystem rules for cross-platform compatibility
+        String result = FileSystem.WINDOWS.toLegalFileName(name, ILLEGAL_CHAR_REPLACEMENT);
+
+        // Replace additional characters that may cause issues in web/HTML contexts
+        return result.replace(' ', ILLEGAL_CHAR_REPLACEMENT)
+            .replace('\t', ILLEGAL_CHAR_REPLACEMENT)
+            .replace('\n', ILLEGAL_CHAR_REPLACEMENT)
+            .replace('\r', ILLEGAL_CHAR_REPLACEMENT);
+    }
 
     /**
      * Returns the outer most files that encompass the given files inclusively.
