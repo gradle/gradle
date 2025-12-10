2ac3b5693494ac1746914e2fc64751dc6f621527
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2025-12-10 06:14:54 -0300

    Make `gradle-collections` available for forked test execution
    Since it is required by `gradle-service-registry-impl`.

--- platforms/jvm/testing-jvm/src/main/java/org/gradle/api/internal/tasks/testing/detection/ForkedTestClasspathFactory.java
+++ platforms/jvm/testing-jvm/src/main/java/org/gradle/api/internal/tasks/testing/detection/ForkedTestClasspathFactory.java
@@ -77,6 +77,7 @@ public class ForkedTestClasspathFactory {
             moduleRegistry.getModule("gradle-service-provider").getImplementationClasspath().getAsURLs(),
             moduleRegistry.getModule("gradle-service-registry-builder").getImplementationClasspath().getAsURLs(),
             moduleRegistry.getModule("gradle-service-registry-impl").getImplementationClasspath().getAsURLs(),
+            moduleRegistry.getModule("gradle-collections").getImplementationClasspath().getAsURLs(),
             moduleRegistry.getModule("gradle-time").getImplementationClasspath().getAsURLs(),
             moduleRegistry.getModule("gradle-testing-base-infrastructure").getImplementationClasspath().getAsURLs(),
             moduleRegistry.getModule("gradle-testing-jvm-infrastructure").getImplementationClasspath().getAsURLs(),
