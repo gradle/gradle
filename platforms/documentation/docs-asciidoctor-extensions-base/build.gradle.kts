e8fe52360afc5a0902a94453b682a7b87d0dae64
Vlad Chesnokov <vchesnokov@gradle.com>
2025-12-16 09:59:23 +0100

    Minify jnr constants to reduce distribution size by ~1Mb
    Also, refactor minify action to avoid referencing exact versions.

--- platforms/documentation/docs-asciidoctor-extensions-base/build.gradle.kts
+++ platforms/documentation/docs-asciidoctor-extensions-base/build.gradle.kts
@@ -1,3 +1,5 @@
+import gradlebuild.basics.classanalysis.Attributes.minified
+
 plugins {
     id("gradlebuild.internal.java")
     groovy
@@ -14,6 +16,18 @@ dependencies {
 
     implementation("commons-io:commons-io:2.11.0")
     testImplementation("org.spockframework:spock-core")
+}
+
+// ascii-doctor depends on JRuby, which has a dependency on jnr-constants, unit tests fail because of that, but this shouldn't happen when it's used as a plugin due to classloader isolation
+configurations.all {
+    resolutionStrategy.dependencySubstitution {
+        substitute(module("com.github.jnr:jnr-constants"))
+            .using(variant(module("com.github.jnr:jnr-constants:0.10.4")) {
+                attributes {
+                    attribute(minified, false)
+                }
+            })
+    }
 }
 
 java {
