72df66bea5ffc9dac1fb0c0e4a540b43171c9981
Paul Merlin <paul@gradle.com>
2025-12-17 15:31:10 +0100

    Update date of the NiA git ref

--- testing/smoke-test/build.gradle.kts
+++ testing/smoke-test/build.gradle.kts
@@ -87,7 +87,7 @@ tasks {
 
     val androidProject by registering(RemoteProject::class) {
         remoteUri = androidProjectGitUri
-        // latest https://github.com/gradle/nowinandroid/tree/smoke-tests-main as of 2025-11-05
+        // latest https://github.com/gradle/nowinandroid/tree/smoke-tests-main as of 2025-12-17
         ref = "e117bd4ee30c7eb1ab3bbdc47f73a3fd0317b64b"
     }
 
