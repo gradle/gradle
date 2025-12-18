ad373bd41d4edc19c54474150b7921038c8e4e08
Sergey Igushkin <sigushkin@gradle.com>
2025-12-18 13:50:52 +0300

    Hide more Gradle API types from Declarative schema
    There were usages of these types as supertypes in some tests
    where the members from the supertypes led to failures in
    schema building.

--- subprojects/core-api/src/main/java/org/gradle/api/Buildable.java
+++ subprojects/core-api/src/main/java/org/gradle/api/Buildable.java
@@ -16,11 +16,13 @@
 package org.gradle.api;
 
 import org.gradle.api.tasks.TaskDependency;
+import org.gradle.declarative.dsl.model.annotations.HiddenInDeclarativeDsl;
 
 /**
  * A {@code Buildable} represents an artifact or set of artifacts which are built by one or more {@link Task}
  * instances.
  */
+@HiddenInDeclarativeDsl
 public interface Buildable {
     /**
      * Returns a dependency which contains the tasks which build this artifact. All {@code Buildable} implementations
