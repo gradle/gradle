56df8553d46893512f6d47ecf41db114d69ea142
József Bartók <jbartok@gradle.com>
2026-01-05 14:45:24 +0200

    Write test for desired behavior

--- platforms/core-configuration/kotlin-dsl-integ-tests/src/integTest/kotlin/org/gradle/kotlin/dsl/integration/PrecompiledScriptPluginIntegrationTest.kt
+++ platforms/core-configuration/kotlin-dsl-integ-tests/src/integTest/kotlin/org/gradle/kotlin/dsl/integration/PrecompiledScriptPluginIntegrationTest.kt
@@ -6,6 +6,7 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.fixtures.AbstractKotlinIntegrationTest
 import org.gradle.kotlin.dsl.fixtures.classEntriesFor
+import org.gradle.kotlin.dsl.support.expectedKotlinDslPluginsVersion
 import org.gradle.test.fixtures.file.LeaksFileHandles
 import org.gradle.test.precondition.Requires
 import org.gradle.test.preconditions.IntegTestPreconditions
@@ -338,6 +339,102 @@ class PrecompiledScriptPluginIntegrationTest : AbstractKotlinIntegrationTest() {
             assertOutputContains("foo")
         }
     }
+
+    @Issue("https://github.com/gradle/gradle/issues/22428")
+    @Test
+    fun `can apply plugin in precompiled script plugin aplied in another precompiled script plugin`() {
+        withBuildScript(
+            """
+                plugins {
+                    id("apply-java")
+                }
+            """
+        )
+
+        withSettings(
+            """
+                pluginManagement.includeBuild("build-logic")
+            """
+        )
+
+        withFolders {
+            "build-logic" {
+                withFile(
+                    "settings.gradle.kts",
+                    """
+                        rootProject.name = "my-repro-project-build-logic"
+    
+                        pluginManagement.includeBuild("meta")
+                        dependencyResolutionManagement.repositories.gradlePluginPortal()
+                    """
+                )
+
+                withFile(
+                    "build.gradle.kts",
+                    """
+                        plugins {
+                            id("my.kotlin-dsl")
+                        }
+                    """
+                )
+
+                withFile(
+                    "src/main/kotlin/apply-java.gradle.kts",
+                    """
+                        plugins {
+                            `java-library`
+                        }
+                    """
+                )
+
+                "meta" {
+                    withFile(
+                        "settings.gradle.kts",
+                        """
+                            rootProject.name = "my-repro-project-build-logic-meta"
+    
+                            dependencyResolutionManagement.repositories.gradlePluginPortal()
+                        """
+                    )
+
+                    withFile(
+                        "build.gradle.kts",
+                        """
+                            import org.gradle.kotlin.dsl.support.expectedKotlinDslPluginsVersion
+    
+                            plugins {
+                                `kotlin-dsl`
+                            }
+    
+                            dependencies {
+                                implementation("org.gradle.kotlin.kotlin-dsl:org.gradle.kotlin.kotlin-dsl.gradle.plugin:${'$'}expectedKotlinDslPluginsVersion")
+                            }
+                        """
+                    )
+
+                    withFile(
+                        "src/main/kotlin/my.kotlin-dsl.gradle.kts",
+                        """
+                            plugins {
+                                `kotlin-dsl`
+                            }
+                        """
+                    )
+                }
+            }
+        }
+
+        buildAndFail(":build").apply {
+            assertThatDescription(
+                containsString(
+                    "Invalid plugin request [id: 'org.gradle.kotlin.kotlin-dsl', version: '${expectedKotlinDslPluginsVersion}']. " +
+                            "Plugin requests from precompiled scripts must not include a version number. " +
+                            "If you have been using the `kotlin-dsl` helper function, then simply replace it by 'id(\"org.gradle.kotlin.kotlin-dsl\")'."
+                )
+            )
+        }
+    }
+
 }
 
 
