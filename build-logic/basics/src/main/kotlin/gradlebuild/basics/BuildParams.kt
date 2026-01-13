bf230b39353dfd955a379ea6049763acce66f87c
Bo Zhang <bo@gradle.com>
2026-01-13 14:09:29 +0800

    No PTS with release7x

--- build-logic/basics/src/main/kotlin/gradlebuild/basics/BuildParams.kt
+++ build-logic/basics/src/main/kotlin/gradlebuild/basics/BuildParams.kt
@@ -325,10 +325,7 @@ val Project.predictiveTestSelectionEnabled: Provider<Boolean>
         .map { it.toBoolean() }
         .orElse(
             buildBranch.zip(buildRunningOnCi) { branch, ci ->
-                val protectedBranches = listOf("master", "release")
-                ci && !protectedBranches.contains(branch)
-                    && !branch.startsWith("pre-test/")
-                    && !branch.startsWith("gh-readonly-queue/")
+                ci && !listOf("master", "release", "gh-readonly-queue/").any { branch.startsWith(it) }
             }
         ).zip(project.rerunAllTests) { enabled, rerunAllTests ->
             enabled && !rerunAllTests
