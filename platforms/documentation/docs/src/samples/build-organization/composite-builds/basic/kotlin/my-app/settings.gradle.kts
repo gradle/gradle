4984e0a97b021a20662526b867827aaad73c23a3
Laura Kassovic <lkassovic@gradle.com>
2026-01-12 17:14:09 -0800

    update existing sample with unused removed sample and redudant snippet

--- platforms/documentation/docs/src/samples/build-organization/composite-builds/basic/kotlin/my-app/settings.gradle.kts
+++ platforms/documentation/docs/src/samples/build-organization/composite-builds/basic/kotlin/my-app/settings.gradle.kts
@@ -1,3 +1,13 @@
 rootProject.name = "my-app"
 
+// tag::mag[]
+pluginManagement {
+    includeBuild("my-plugin")
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+// end::mag[]
+
 include("app")
