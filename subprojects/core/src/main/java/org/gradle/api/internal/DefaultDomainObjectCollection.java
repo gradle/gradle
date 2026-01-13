1f27e8f0a9d829275cc56d5cf7f895387cd8e423
Sterling Greene <sterling@gradle.com>
2026-01-13 15:44:19 -0500

    Remove DomainObjectCollection.findAll

--- subprojects/core/src/main/java/org/gradle/api/internal/DefaultDomainObjectCollection.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/DefaultDomainObjectCollection.java
@@ -35,7 +35,6 @@ import org.gradle.api.specs.Spec;
 import org.gradle.api.specs.Specs;
 import org.gradle.internal.Cast;
 import org.gradle.internal.ImmutableActionSet;
-import org.gradle.internal.deprecation.DeprecationLogger;
 import org.gradle.util.internal.ConfigureUtil;
 
 import java.util.AbstractCollection;
@@ -427,27 +426,6 @@ public class DefaultDomainObjectCollection<T> extends AbstractCollection<T> impl
     @Override
     public int estimatedSize() {
         return store.estimatedSize();
-    }
-
-    @Override
-    @Deprecated
-    public Collection<T> findAll(Closure cl) {
-        return findAll(cl, new ArrayList<T>());
-    }
-
-    @Deprecated
-    protected <S extends Collection<? super T>> S findAll(Closure cl, S matches) {
-        DeprecationLogger.deprecateMethod(DomainObjectCollection.class, "findAll(Closure)")
-            .willBeRemovedInGradle10()
-            .withUpgradeGuideSection(9, "findAll_removal")
-            .nagUser();
-        if (store.constantTimeIsEmpty()) {
-            return matches;
-        }
-        for (T t : filteredStore(createFilter(Specs.<Object>convertClosureToSpec(cl)))) {
-            matches.add(t);
-        }
-        return matches;
     }
 
     /**
