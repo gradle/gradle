8c20928ab9b06c083a78b3d7a1e32c0a83198648
József Bartók <jbartok@gradle.com>
2026-01-29 16:31:31 +0200

    Address review concerns

--- platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r940/DummyModelCrossVersionSpec.groovy
+++ platforms/ide/tooling-api/src/crossVersionTest/groovy/org/gradle/integtests/tooling/r940/DummyModelCrossVersionSpec.groovy
@@ -20,6 +20,7 @@ package org.gradle.integtests.tooling.r940
 import org.gradle.integtests.tooling.fixture.TargetGradleVersion
 import org.gradle.integtests.tooling.fixture.ToolingApiSpecification
 import org.gradle.integtests.tooling.fixture.ToolingApiVersion
+import org.gradle.test.fixtures.dsl.GradleDsl
 import org.gradle.tooling.BuildAction
 import org.gradle.tooling.BuildController
 import org.gradle.tooling.BuildException
@@ -32,43 +33,21 @@ import org.gradle.tooling.model.gradle.GradleBuild
 @TargetGradleVersion('>=9.4.0')
 class DummyModelCrossVersionSpec extends ToolingApiSpecification {
 
+    // This is weird functionality, but it's currently needed by IntelliJ's SYNC process.
+    ///They need the classloader that sees whatever was loaded by init script's buildscript { dependencies { classpath { ... } } }.
+    // Their relevant logic is here: https://github.com/JetBrains/intellij-community/blob/f7ae5ea359f1732d72345ff2298dcf86413a7fd6/plugins/gradle/tooling-extension-impl/src/com/intellij/gradle/toolingExtension/impl/modelSerialization/ToolingSerializerConverter.java#L26
+    //
+    // The purpose of this test class is to make sure the private API they rely on (ProtocolToModelAdapter) won't be modified by us, thus breaking the SYNC process.
+    // If we need to modify it, then we need to talk to them first.
+
     def setup() {
         settingsFile.delete()
-        file('dummy-init.gradle') << """
-import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
-import javax.inject.Inject
-
-beforeSettings { settings ->
-    settings.plugins.apply(DummyModelPlugin)
-}
-
-class DummyModel implements java.io.Serializable {}
-
-class DummyModelBuilder implements org.gradle.tooling.provider.model.internal.BuildScopeModelBuilder {
-    boolean canBuild(String modelName) {
-        return modelName == 'org.gradle.integtests.tooling.r940.DummyModelCrossVersionSpec\$DummyModel'
-    }
-    Object create(org.gradle.internal.build.BuildState target) {
-        return new DummyModel()
-    }
-}
-
-class DummyModelPlugin implements Plugin<Settings> {
-    private final ToolingModelBuilderRegistry registry
-    @Inject
-    DummyModelPlugin(ToolingModelBuilderRegistry registry) {
-        this.registry = registry
     }
 
-    void apply(Settings settings) {
-        println("Applying DummyModelBuilder to :" + settings)
-        registry.register(new DummyModelBuilder())
-    }
-}
-"""
-    }
-
-    def "can query DummyModel with internal BuildScopeModelBuilder API and get correct classloader when no error"() {
+    def "can query DummyModel with internal BuildScopeModelBuilder API and get correct classloader when no error"(GradleDsl dsl) {
+        given:
+        def initScriptFileName = dsl.fileNameFor("dummy-init")
+        file(initScriptFileName) << initScriptContent(dsl)
         settingsKotlinFile << """
             rootProject.name = "root"
         """
@@ -76,16 +55,22 @@ class DummyModelPlugin implements Plugin<Settings> {
         when:
         def classLoaderName = ""
         succeeds {
-            runDummyModelAction(it) {
+            runDummyModelAction(it, initScriptFileName) {
                 classLoaderName = it
             }
         }
 
         then:
-        classLoaderName ==~ /ScriptClassLoader\(groovy-script-.*dummy-init\.gradle-loader\)/
+        assertClassLoaderName(classLoaderName, dsl)
+
+        where:
+        dsl << [GradleDsl.KOTLIN, GradleDsl.GROOVY]
     }
 
     def "can query DummyModel with internal BuildScopeModelBuilder API and get correct classloader when broken settings script"() {
+        given:
+        def initScriptFileName = dsl.fileNameFor("dummy-init")
+        file(initScriptFileName) << initScriptContent(dsl)
         settingsKotlinFile << """
             broken !!!
         """
@@ -93,17 +78,23 @@ class DummyModelPlugin implements Plugin<Settings> {
         when:
         def classLoaderName = ""
         fails {
-            runDummyModelAction(it) {
+            runDummyModelAction(it, initScriptFileName) {
                 classLoaderName = it
             }
         }
 
         then:
         thrown(BuildException)
-        classLoaderName ==~ /ScriptClassLoader\(groovy-script-.*dummy-init\.gradle-loader\)/
+        assertClassLoaderName(classLoaderName, dsl)
+
+        where:
+        dsl << [GradleDsl.KOTLIN, GradleDsl.GROOVY]
     }
 
     def "can query DummyModel with internal BuildScopeModelBuilder API and get correct classloader even if broken settings plugin"() {
+        given:
+        def initScriptFileName = dsl.fileNameFor("dummy-init")
+        file(initScriptFileName) << initScriptContent(dsl)
         settingsKotlinFile << """
             pluginManagement {
                 includeBuild("build-logic")
@@ -137,22 +128,25 @@ class DummyModelPlugin implements Plugin<Settings> {
         when:
         def classLoaderName = ""
         fails {
-            runDummyModelAction(it) {
+            runDummyModelAction(it, initScriptFileName) {
                 classLoaderName = it
             }
         }
 
         then:
         thrown(BuildException)
-        classLoaderName ==~ /ScriptClassLoader\(groovy-script-.*dummy-init\.gradle-loader\)/
+        assertClassLoaderName(classLoaderName, dsl)
+
+        where:
+        dsl << [GradleDsl.KOTLIN, GradleDsl.GROOVY]
     }
 
-    String runDummyModelAction(ProjectConnection connection, IntermediateResultHandler<String> resultHandler) {
+    String runDummyModelAction(ProjectConnection connection, String initScriptFileName, IntermediateResultHandler<String> resultHandler) {
         connection.action()
             .projectsLoaded(new DummyModelAction(), resultHandler)
             .build()
             .forTasks([])
-            .withArguments("--init-script=${file('dummy-init.gradle').absolutePath}")
+            .withArguments("--init-script=${file(initScriptFileName).absolutePath}")
             .run()
     }
 
@@ -173,6 +167,90 @@ class DummyModelPlugin implements Plugin<Settings> {
             Object unpacked = new ProtocolToModelAdapter().unpack(dummyModel);
             ClassLoader modelBuildersClassLoader = unpacked.getClass().getClassLoader()
             return modelBuildersClassLoader.toString()
+        }
+    }
+
+    static def initScriptContent(GradleDsl dsl) {
+        switch (dsl) {
+            case GradleDsl.GROOVY:
+                return """
+                    import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
+                    import javax.inject.Inject
+                    
+                    beforeSettings { settings ->
+                        settings.plugins.apply(DummyModelPlugin)
+                    }
+                    
+                    class DummyModel implements java.io.Serializable {}
+                    
+                    class DummyModelBuilder implements org.gradle.tooling.provider.model.internal.BuildScopeModelBuilder {
+                        boolean canBuild(String modelName) {
+                            return modelName == 'org.gradle.integtests.tooling.r940.DummyModelCrossVersionSpec\$DummyModel'
+                        }
+                        Object create(org.gradle.internal.build.BuildState target) {
+                            return new DummyModel()
+                        }
+                    }
+                    
+                    class DummyModelPlugin implements Plugin<Settings> {
+                        private final ToolingModelBuilderRegistry registry
+                        @Inject
+                        DummyModelPlugin(ToolingModelBuilderRegistry registry) {
+                            this.registry = registry
+                        }
+                    
+                        void apply(Settings settings) {
+                            println("Applying DummyModelBuilder to: " + settings)
+                            registry.register(new DummyModelBuilder())
+                        }
+                    }
+                """
+            case GradleDsl.KOTLIN:
+                return """
+                    import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
+                    import org.gradle.tooling.provider.model.internal.BuildScopeModelBuilder
+                    import org.gradle.internal.build.BuildState
+                    import javax.inject.Inject
+                    
+                    beforeSettings {
+                        apply<DummyModelPlugin>()
+                    }
+                    
+                    class DummyModel : java.io.Serializable
+                    
+                    class DummyModelBuilder : BuildScopeModelBuilder {
+                        override fun canBuild(modelName: String): Boolean {
+                            return modelName == "org.gradle.integtests.tooling.r940.DummyModelCrossVersionSpec\\\$DummyModel"
+                        }
+                    
+                        override fun create(target: BuildState): Any {
+                            return DummyModel()
+                        }
+                    }
+                    
+                    class DummyModelPlugin @Inject constructor(
+                        private val registry: ToolingModelBuilderRegistry
+                    ) : Plugin<Settings> {
+                    
+                        override fun apply(settings: Settings) {
+                            println("Applying DummyModelBuilder to: \$settings")
+                            registry.register(DummyModelBuilder())
+                        }
+                    }
+                """
+            default:
+                throw new RuntimeException("Unhandled DSL: $dsl")
+        }
+    }
+
+    static def assertClassLoaderName(String classLoaderName, GradleDsl dsl) {
+        switch (dsl) {
+            case GradleDsl.GROOVY :
+                return classLoaderName ==~ /ScriptClassLoader\(groovy-script-.*dummy-init\.gradle-loader\)/
+            case GradleDsl.KOTLIN :
+                return classLoaderName ==~ /VisitableURLClassLoader\(.*kotlin-dsl:.*dummy-init\.gradle.kts:.*\)/
+            default:
+                throw new RuntimeException("Unhandled DSL: $dsl")
         }
     }
 }
