60330520a161170b846351f453aa631795c36a92
Christoph Obexer <cobexer@gradle.com>
2026-02-02 16:23:11 +0100

    TeamCity: Drop obsolete workaround
    https://youtrack.jetbrains.com/issue/TW-57330/Gradle-build-always-runs-tests#focus=Comments-27-10249156.0-0
    because it is IP incompatible
    
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>
    (cherry picked from commit 25d9e57730644d4d4d493612970acc1a0f44412c)

--- build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
+++ build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
@@ -87,7 +87,6 @@ val gradlebuildJava = extensions.create<UnitTestAndCompileExtension>("gradlebuil
 
 enforceCompatibility(gradlebuildJava)
 
-removeTeamcityTempProperty()
 addDependencies()
 configureCompileDefaults()
 addCompileAllTasks()
@@ -473,14 +472,6 @@ fun configureTests() {
                 enabled.convention(project.predictiveTestSelectionEnabled)
             }
         }
-    }
-}
-
-fun removeTeamcityTempProperty() {
-    // Undo: https://github.com/JetBrains/teamcity-gradle/blob/e1dc98db0505748df7bea2e61b5ee3a3ba9933db/gradle-runner-agent/src/main/scripts/init.gradle#L818
-    if (project.buildRunningOnCi.get() && project.hasProperty("teamcity")) {
-        @Suppress("UNCHECKED_CAST") val teamcity = project.property("teamcity") as MutableMap<String, Any>
-        teamcity["teamcity.build.tempDir"] = ""
     }
 }
 
