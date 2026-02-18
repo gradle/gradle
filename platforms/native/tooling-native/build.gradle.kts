2cc9908e1300bcc95e168185f7494a7cd3824560
Justin Van Dort <jvandort@gradle.com>
2026-02-18 13:06:08 -0500

    Post split actions for :tooling-native-model-impls

--- platforms/native/tooling-native/build.gradle.kts
+++ platforms/native/tooling-native/build.gradle.kts
@@ -7,17 +7,19 @@ description = "Tooling API model builders for native builds"
 dependencies {
     api(projects.coreApi)
     api(projects.core)
-    api(projects.ide) {
-        because("To pick up various builders (which should live somewhere else)")
-    }
     api(projects.serviceProvider)
     api(projects.toolingApi)
+    api(projects.toolingNativeModelImpls)
 
     implementation(projects.baseServices)
     implementation(projects.fileCollections)
+    implementation(projects.ide) {
+        because("To pick up various builders (which should live somewhere else)")
+    }
     implementation(projects.languageNative)
     implementation(projects.platformNative)
     implementation(projects.testingNative)
+
 
     implementation(libs.guava)
 
