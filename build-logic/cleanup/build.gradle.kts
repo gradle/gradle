2732106319000756aadded37dcd542c2ea7dd016
Donát Csikós <donat@gradle.com>
2026-03-09 15:54:39 +0100

    Remove unused plugin application

--- build-logic/cleanup/build.gradle.kts
+++ build-logic/cleanup/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("gradlebuild.build-logic.kotlin-dsl-gradle-plugin")
-    id("gradlebuild.build-logic.groovy-dsl-gradle-plugin")
 }
 
 description = "Provides a plugin that cleans up after executing tests"
