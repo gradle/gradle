08cc89b9ff6873c2c25e7f04be07b3f3419ed724
József Bartók <jbartok@gradle.com>
2026-03-11 07:41:41 +0200

    Publish new version of `kotlin-dsl` plugin and make use of it

--- build-logic/kotlin-dsl/src/main/kotlin/gradlebuild.kotlin-dsl-dependencies-embedded.gradle.kts
+++ build-logic/kotlin-dsl/src/main/kotlin/gradlebuild.kotlin-dsl-dependencies-embedded.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 // --- Enable automatic generation of API extensions -------------------
 val apiExtensionsOutputDir = layout.buildDirectory.dir("generated-sources/kotlin")
 
-val publishedKotlinDslPluginVersion = "6.5.4"
+val publishedKotlinDslPluginVersion = "6.5.5"
 val libs = project.versionCatalogs.named("libs")
 val kotlinVersion = libs.findVersion("kotlin").get().getStrictVersion()
 
