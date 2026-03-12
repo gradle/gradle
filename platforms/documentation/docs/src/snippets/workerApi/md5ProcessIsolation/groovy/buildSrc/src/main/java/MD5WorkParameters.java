a57d59651525d81b462baf92b99ae76c18a9ecdb
Laura Kassovic <lkassovic@gradle.com>
2026-03-11 21:17:07 -0700

    remove duplicate files in workerapi snippets

--- platforms/documentation/docs/src/snippets/workerApi/md5ProcessIsolation/groovy/buildSrc/src/main/java/MD5WorkParameters.java
+++
@@ -1,7 +0,0 @@
-import org.gradle.api.file.RegularFileProperty;
-import org.gradle.workers.WorkParameters;
-
-public interface MD5WorkParameters extends WorkParameters {
-    RegularFileProperty getSourceFile();
-    RegularFileProperty getMD5File();
-}
