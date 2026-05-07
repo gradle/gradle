2f7c62704479262c6c727059afa67d318afb60a5
Daniel Clausen <dclausen@gradle.com>
2026-05-07 17:11:19 +0200

    Introduce helper method getFileSnapshotLength()

--- subprojects/core/src/main/java/org/gradle/api/internal/tasks/BaseFilePropertyVisitState.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/tasks/BaseFilePropertyVisitState.java
@@ -137,9 +137,7 @@ public abstract class BaseFilePropertyVisitState implements FilePropertyVisitor.
         this.path = snapshot.getAbsolutePath();
         this.name = snapshot.getName();
         this.hash = fingerprint.getNormalizedContentHash();
-        this.length = snapshot instanceof RegularFileSnapshot
-            ? ((RegularFileSnapshot) snapshot).getMetadata().getLength()
-            : 0;
+        this.length = getFileSnapshotLength(snapshot);
 
         boolean isRoot = depth == 0;
         if (isRoot) {
@@ -152,6 +150,19 @@ public abstract class BaseFilePropertyVisitState implements FilePropertyVisitor.
             postRoot();
         }
         return SnapshotVisitResult.CONTINUE;
+    }
+
+    private static long getFileSnapshotLength(FileSystemLocationSnapshot snapshot) {
+        switch (snapshot.getType()) {
+            case RegularFile:
+                return ((RegularFileSnapshot) snapshot).getMetadata().getLength();
+
+            case Missing:
+                return 0;
+
+            default:
+                throw new UnsupportedOperationException("Cannot determine snapshot length for the type " + snapshot.getType());
+        }
     }
 
     @Override
