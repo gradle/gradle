a9e8cde0c15768adcc8129c9fc441ad01b6a36d5
Tom Tresansky <ttresansky@gradle.com>
2026-06-08 16:50:23 -0400

    Fix AGP example snippet

--- platforms/documentation/docs/src/snippets/reference/dependency-management/declaring-dependencies/declaringConfigurations-kmp/common/app/src/main/kotlin/org/example/project/MainActivity.kt
+++ platforms/documentation/docs/src/snippets/reference/dependency-management/declaring-dependencies/declaringConfigurations-kmp/common/app/src/main/kotlin/org/example/project/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +14,4 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
