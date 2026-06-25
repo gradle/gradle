c5a047c8d01f0b6e2415402fa499902eaaff32d3
József Bartók <jbartok@gradle.com>
2026-06-25 17:33:23 +0300

    Bump KGP & embedded Kotlin versions to 2.4.10-RC

--- platforms/documentation/docs/src/snippets/best-practices/kotlinStdLib-avoid/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/best-practices/kotlinStdLib-avoid/kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 // tag::avoid-this[]
 plugins {
-    kotlin("jvm").version("2.4.0")
+    kotlin("jvm").version("2.4.10-RC")
 }
 
 dependencies {
--- platforms/documentation/docs/src/snippets/tutorials/authoring-builds/buildScripts-pluginBuild/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/tutorials/authoring-builds/buildScripts-pluginBuild/kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 // tag::plugin-build-script[]
 plugins {                                                             // <1>
     `java-gradle-plugin`                                              // <2>
-    id("org.jetbrains.kotlin.jvm") version "2.4.0"                 // <3>
+    id("org.jetbrains.kotlin.jvm") version "2.4.10-RC"                 // <3>
 }
 
 repositories {                                                        // <4>
