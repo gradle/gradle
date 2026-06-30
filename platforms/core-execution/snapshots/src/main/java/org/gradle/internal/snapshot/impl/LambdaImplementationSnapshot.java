f6f8a24fc33f8bdbafefc05a733fc0db43d3b457
Reinhold Degenfellner <rdegenfellner@gradle.com>
2026-06-30 14:32:47 +0200

    Enable NullAway in :snapshots
    Mark genuinely-optional fields/returns/params @Nullable (lazy hash, builder
    result/leaveDirectory/getResult, RelativePathTracker.rootName, predicate,
    UnknownImplementationSnapshot.getClassLoaderHash, equals(@Nullable Object)).
    Use requireNonNull where a value is non-null in correct usage but NullAway
    cannot prove it (RelativePathTracker.leave, PathVisitor.getResult).

--- platforms/core-execution/snapshots/src/main/java/org/gradle/internal/snapshot/impl/LambdaImplementationSnapshot.java
+++ platforms/core-execution/snapshots/src/main/java/org/gradle/internal/snapshot/impl/LambdaImplementationSnapshot.java
@@ -107,7 +107,7 @@ public class LambdaImplementationSnapshot extends ImplementationSnapshot {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(@Nullable Object o) {
         if (this == o) {
             return true;
         }
