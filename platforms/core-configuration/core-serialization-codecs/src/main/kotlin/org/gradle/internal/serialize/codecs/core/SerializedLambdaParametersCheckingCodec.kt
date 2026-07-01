9f018d25b6dbf0338c70cde52eea118f81deb9b6
Tom Tresansky <ttresansky@gradle.com>
2026-07-01 08:55:31 -0400

    Fixes after rebasing

--- platforms/core-configuration/core-serialization-codecs/src/main/kotlin/org/gradle/internal/serialize/codecs/core/SerializedLambdaParametersCheckingCodec.kt
+++ platforms/core-configuration/core-serialization-codecs/src/main/kotlin/org/gradle/internal/serialize/codecs/core/SerializedLambdaParametersCheckingCodec.kt
@@ -18,14 +18,11 @@ package org.gradle.internal.serialize.codecs.core
 
 import org.gradle.internal.configuration.problems.DocumentationSection
 import org.gradle.internal.configuration.problems.PropertyTrace
-import org.gradle.internal.serialize.beans.services.unsupportedFieldDeclaredTypes
 import org.gradle.internal.serialize.graph.Codec
 import org.gradle.internal.serialize.graph.IsolateContext
 import org.gradle.internal.serialize.graph.ReadContext
 import org.gradle.internal.serialize.graph.WriteContext
 import org.gradle.internal.serialize.graph.codecs.findCodecThatWidensIncompatibly
-import org.gradle.internal.serialize.graph.encodeBean
-import org.gradle.internal.serialize.graph.decodeBean
 import org.gradle.internal.serialize.graph.logUnsupported
 import org.gradle.internal.serialize.graph.withPropertyTrace
 import org.objectweb.asm.Type
