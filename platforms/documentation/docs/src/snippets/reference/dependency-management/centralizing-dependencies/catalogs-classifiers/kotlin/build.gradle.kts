e4ca5d40086c7fef398b6f0aaba5a10c938fa6df
Qiu Tian <qiu_tian@students.edu.sg>
2026-07-21 21:56:56 +0800

    Document variantOf() for classifiers in version catalogs
    Enhances the version catalog documentation to better explain how to use variantOf() for specifying dependency classifiers. Adds examples for sources and javadoc JARs, which was the original use case in issue #17169.
    
    Fixes #17169
    
    Signed-off-by: Qiu Tian <qiu_tian@students.edu.sg>

--- platforms/documentation/docs/src/snippets/reference/dependency-management/centralizing-dependencies/catalogs-classifiers/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/reference/dependency-management/centralizing-dependencies/catalogs-classifiers/kotlin/build.gradle.kts
@@ -8,6 +8,16 @@ dependencies {
 }
 // end::use_classifier[]
 
+// tag::use_classifier_sources[]
+dependencies {
+    // Add the library's source code for IDE integration
+    implementation(variantOf(libs.my.lib) { classifier("sources") })
+    
+    // Add the library's Javadoc for IDE integration
+    implementation(variantOf(libs.my.lib) { classifier("javadoc") })
+}
+// end::use_classifier_sources[]
+
 // tag::use_artifact_type[]
 dependencies {
     implementation(libs.my.lib) {
