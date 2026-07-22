17356f259e419161a0941ae02bbb9b468c4841a3
József Bartók <jbartok@gradle.com>
2026-07-22 16:34:31 +0300

    Bump Kotlin version to `2.4.20-Beta2`

--- platforms/documentation/docs/src/snippets/tutorials/developing-plugins/plugin-tutorial/kotlin/plugin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/tutorials/developing-plugins/plugin-tutorial/kotlin/plugin/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
     // end::plugin-init[]
      */
     // tag::publish[]
-    id("org.jetbrains.kotlin.jvm") version "2.4.20-Beta1"
+    id("org.jetbrains.kotlin.jvm") version "2.4.20-Beta2"
     // tag::plugin-init[]
 }
 // end::plugin-init[]
