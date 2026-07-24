de5d6b9d6e94dd8ed2c842d01c449bf5df585fe0
Mikhail Lopatkin <mlopatkin@gradle.com>
2026-07-24 10:13:34 +0200

    Respect third-party Java agents in instrumentation on CC hit
    When a third-party agent is attached to the build JVM (e.g. while
    debugging the daemon), we don't use the pre-transformed JARs for
    classpath, but instead replay the transformation at runtime, during
    classloading; this way we can cover whatever tranformations the other
    agents have applied, and they see the original bytecode.
    
    However, we weren't rebuilding the runtime transformation pipeline upon
    configuration cache hits, falling back to the pre-transformed JARs
    instead. This caused two issues:
     - Third-party agent modifications were lost when the Gradle's agent
       replaced them with the pre-transformed bytecode.
     - ClassLoader reuse was broken, resulting in BuildService references
       not being restorable properly.
    
    This commit adds the necessary plumbing for CC hit. We store information
    needed to set up the instrumentation in the classloader spec, and use
    it to rebuild the runtime transformation path upon recreating the
    classloader from the CC entry.

--- platforms/core-configuration/configuration-cache/src/test/kotlin/org/gradle/internal/cc/impl/serialization/codecs/AbstractUserTypeCodecTest.kt
+++ platforms/core-configuration/configuration-cache/src/test/kotlin/org/gradle/internal/cc/impl/serialization/codecs/AbstractUserTypeCodecTest.kt
@@ -181,7 +181,7 @@ abstract class AbstractUserTypeCodecTest {
             isIntegrityCheckEnabled = integrityCheck,
             logger = mock(),
             problemsListener = problemsListener,
-            classDecoder = DefaultClassDecoder(mock(), mock())
+            classDecoder = DefaultClassDecoder(mock(), mock(), mock())
         )
 
     private fun loggingProblemsListener() = object : AbstractProblemsListener() {
