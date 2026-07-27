efc3cedf5039472fcadb4047fef165c13a84aeaf
Reinhold Degenfellner <rdegenfellner@gradle.com>
2026-07-27 10:25:40 +0200

    Add reproducer for issue 35885
    Problems reported from a thread that has no current build operation
    (e.g. an OperationCompletionListener build service dispatched on the
    build-event-listener executor thread) are silently discarded by
    DefaultProblemReporter. The new integration test registers such a
    listener from the settings script, reports a problem from it, and
    expects the problem to be received via the problems build operation
    events. It currently fails with "Only 0 problems received" in both
    the forking and configuration-cache variants.

---
+++ platforms/ide/problems-api/src/integTest/groovy/org/gradle/api/problems/ProblemsServiceWithoutBuildOperationIntegrationTest.groovy
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.problems
+
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import spock.lang.Issue
+
+class ProblemsServiceWithoutBuildOperationIntegrationTest extends AbstractIntegrationSpec {
+
+    def setup() {
+        enableProblemsApiCheck()
+    }
+
+    @Issue("https://github.com/gradle/gradle/issues/35885")
+    def "problem reported from a thread without a current build operation is not lost"() {
+        given:
+        settingsFile """
+            import org.gradle.api.problems.ProblemGroup
+            import org.gradle.api.problems.ProblemId
+            import org.gradle.api.problems.Problems
+            import org.gradle.api.services.BuildService
+            import org.gradle.api.services.BuildServiceParameters
+            import org.gradle.build.event.BuildEventsListenerRegistry
+            import org.gradle.tooling.events.FinishEvent
+            import org.gradle.tooling.events.OperationCompletionListener
+            import javax.inject.Inject
+
+            abstract class BuildObserver implements BuildService<BuildServiceParameters.None>, OperationCompletionListener {
+                @Inject
+                abstract Problems getProblems()
+
+                @Override
+                void onFinish(FinishEvent event) {
+                    ProblemGroup problemGroup = ProblemGroup.create("issues", "issues")
+                    ProblemId id = ProblemId.create("finished", "task finished", problemGroup)
+                    problems.reporter.report(id) {}
+                }
+            }
+
+            def observer = gradle.sharedServices.registerIfAbsent("buildObserver", BuildObserver) {}
+            services.get(BuildEventsListenerRegistry).onTaskCompletion(observer)
+        """
+        buildFile """
+            tasks.register("ok")
+        """
+
+        when:
+        run("ok")
+
+        then:
+        verifyAll(receivedProblem) {
+            definition.id.fqid == 'issues:finished'
+            definition.id.displayName == 'task finished'
+        }
+    }
+}
