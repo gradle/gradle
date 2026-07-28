0743d3e11cc1326073af19e136318cc5e3d4f6ab
Octavia Togami <otogami@gradle.com>
2026-07-28 04:35:32 +0000

    Import the right IoFunction in the resource hasher tests

--- platforms/core-execution/normalization-java/src/test/groovy/org/gradle/api/internal/changedetection/state/LineEndingNormalizingResourceHasherTest.groovy
+++ platforms/core-execution/normalization-java/src/test/groovy/org/gradle/api/internal/changedetection/state/LineEndingNormalizingResourceHasherTest.groovy
@@ -23,7 +23,7 @@ import org.gradle.internal.fingerprint.hashing.RegularFileSnapshotContext
 import org.gradle.internal.fingerprint.hashing.ResourceHasher
 import org.gradle.internal.fingerprint.hashing.ZipEntryContext
 import org.gradle.internal.hash.Hashing
-import org.gradle.internal.io.IoFunction
+import org.gradle.api.internal.file.archive.ZipEntry.IoFunction
 import org.gradle.internal.snapshot.RegularFileSnapshot
 import spock.lang.Specification
 import spock.lang.TempDir
