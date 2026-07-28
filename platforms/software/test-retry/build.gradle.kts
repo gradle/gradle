6292495c6d36cc2ebb54f1ae5b4e9f559c391d87
Tom Tresansky <ttresansky@gradle.com>
2026-07-28 13:12:07 -0400

    Scaffold platforms/software/test-retry subproject

---
+++ platforms/software/test-retry/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("gradlebuild.distribution.api-java")
+    id("gradlebuild.distribution.api-kotlin")
+}
+
+description = "Bundled Gradle plugin that mitigates flaky tests by retrying them when they fail"
+
+dependencies {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -235,6 +235,7 @@ val software = platform("software") {
     subproject("software-diagnostics")
     subproject("testing-base")
     subproject("testing-base-infrastructure")
+    subproject("test-retry")
     subproject("test-suites-base")
     subproject("version-control")
 }
