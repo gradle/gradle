0b7016ec3b7ea9c78062d510862287d195fbc81a
Laura Kassovic <lkassovic@gradle.com>
2026-08-06 20:04:08 -0700

    fix errors

--- build-logic/documentation/src/main/groovy/gradlebuild/docs/FindBrokenExternalLinks.java
+++ build-logic/documentation/src/main/groovy/gradlebuild/docs/FindBrokenExternalLinks.java
@@ -41,7 +41,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
