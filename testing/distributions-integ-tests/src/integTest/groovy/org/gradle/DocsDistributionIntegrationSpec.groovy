d665941b1ac06286a8a59c178720c01ce0fe0404
Christoph Obexer <cobexer@gradle.com>
2026-08-07 09:30:21 +0200

    DocsDistributionIntegrationSpec: update size
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- testing/distributions-integ-tests/src/integTest/groovy/org/gradle/DocsDistributionIntegrationSpec.groovy
+++ testing/distributions-integ-tests/src/integTest/groovy/org/gradle/DocsDistributionIntegrationSpec.groovy
@@ -36,7 +36,7 @@ class DocsDistributionIntegrationSpec extends DistributionIntegrationSpec {
 
     @Override
     int getDistributionSizeMiB() {
-        return 89
+        return 90
     }
 
     @Override
