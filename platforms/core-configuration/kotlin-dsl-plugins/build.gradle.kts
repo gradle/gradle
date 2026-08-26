d3f1f0ab1ff6fe15e3f7fe426acfc4d0f7ffcb86
József Bartók <jbartok@gradle.com>
2026-08-26 18:16:09 +0300

    Publish and make use of new `kotlin-dsl` plugin version

--- platforms/core-configuration/kotlin-dsl-plugins/build.gradle.kts
+++ platforms/core-configuration/kotlin-dsl-plugins/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 description = "Kotlin DSL Gradle Plugins deployed to the Plugin Portal"
 
 group = "org.gradle.kotlin"
-version = "6.7.9"
+version = "6.7.10"
 
 base.archivesName = "plugins"
 
