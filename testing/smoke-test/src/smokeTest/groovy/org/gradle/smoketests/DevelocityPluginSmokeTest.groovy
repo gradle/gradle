a60b400808faec969824c87c5419e94e13aa50b4
jgauthier@gradle.com <jgauthier@gradle.com>
2026-09-17 16:53:45 +0000

    Update Develocity Gradle Plugin to 3.99.2
    *[dv-release plugin v1.3.9](https://github.com/gradle/claude-plugins/tree/main/plugins/develocity/release/dv-release)*

--- testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/DevelocityPluginSmokeTest.groovy
+++ testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/DevelocityPluginSmokeTest.groovy
@@ -171,6 +171,7 @@ class DevelocityPluginSmokeTest extends AbstractSmokeTest {
         "4.4.3",
         "4.5.0",
         "4.5.1",
+        "3.99.2",
     ]
 
     // Current injection scripts support Develocity plugin 3.6.4 and above
