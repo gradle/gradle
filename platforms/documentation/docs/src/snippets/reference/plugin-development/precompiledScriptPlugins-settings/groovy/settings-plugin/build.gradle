36b181477307fc6db0e16582f11daaeb7e34fc8e
lkasso <lkassovic@gradle.com>
2026-09-22 07:54:20 -0700

    Add back mavenCentral to doc snippets

--- platforms/documentation/docs/src/snippets/reference/plugin-development/precompiledScriptPlugins-settings/groovy/settings-plugin/build.gradle
+++ platforms/documentation/docs/src/snippets/reference/plugin-development/precompiledScriptPlugins-settings/groovy/settings-plugin/build.gradle
@@ -4,6 +4,7 @@ plugins {
 
 repositories {
     gradlePluginPortal()
+    mavenCentral()
 }
 
 dependencies {
--- platforms/documentation/docs/src/snippets/reference/plugin-development/precompiledScriptPlugins-settings/kotlin/settings-plugin/settings.gradle.kts
+++ platforms/documentation/docs/src/snippets/reference/plugin-development/precompiledScriptPlugins-settings/kotlin/settings-plugin/settings.gradle.kts
@@ -1,5 +1,6 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        mavenCentral()
     }
 }
